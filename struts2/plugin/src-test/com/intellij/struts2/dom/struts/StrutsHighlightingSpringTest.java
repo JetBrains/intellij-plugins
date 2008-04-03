e730cf212b4d0de5e42a509a13595805a35fde45
Yann Cebron <no_reply@jetbrains.com>
2008-04-03 22:22:18 +0400

    re-try #4, problems with completion seem to be fixed

--- struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsHighlightingSpringTest.java
+++ struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsHighlightingSpringTest.java
@@ -61,7 +61,6 @@ public class StrutsHighlightingSpringTest extends BasicStrutsHighlightingTestCas
     addFile(springFileSet, "spring.xml");
 
     // TODO <alias> does not appear here, see com.intellij.spring.impl.SpringModelImpl#myOwnBeans
-/*
     myFixture.testCompletionVariants("struts-completionvariants-spring.xml",
                                      "META-INF",
                                      "MyClass",
@@ -72,7 +71,6 @@ public class StrutsHighlightingSpringTest extends BasicStrutsHighlightingTestCas
                                      "ognl",
                                      "org",
                                      "template");
-*/
   }
 
   // stuff below is Spring related ===============================================
