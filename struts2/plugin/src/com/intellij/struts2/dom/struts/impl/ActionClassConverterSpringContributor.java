a3541bad0f50eec2f27b39be6509fd4e254734d5
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2008-04-24 12:54:04 +0400

    IDEADEV-26235

--- struts2/plugin/src/com/intellij/struts2/dom/struts/impl/ActionClassConverterSpringContributor.java
+++ struts2/plugin/src/com/intellij/struts2/dom/struts/impl/ActionClassConverterSpringContributor.java
@@ -27,7 +27,6 @@ import com.intellij.psi.PsiReferenceBase;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.spring.SpringManager;
 import com.intellij.spring.SpringModel;
-import com.intellij.spring.model.SpringUtils;
 import com.intellij.spring.model.xml.beans.SpringBeanPointer;
 import com.intellij.struts2.dom.struts.action.ActionClassConverter;
 import com.intellij.util.ArrayUtil;
@@ -89,7 +88,7 @@ public class ActionClassConverterSpringContributor extends ActionClassConverter.
       }
 
       final String beanName = myElement.getValue();
-      final SpringBeanPointer springBean = SpringUtils.findBeanByReferenceName(model, beanName);
+      final SpringBeanPointer springBean = model.findBean(beanName);
 
       return springBean == null ? null : springBean.getBeanClass();
     }
