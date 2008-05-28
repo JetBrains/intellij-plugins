d431e68c0bffe3dbde335129c34efe57ad65f45f
Peter Gromov <peter@jetbrains.com>
2008-05-29 00:59:28 +0400

    IDEADEV-22573 Code completion for package is not corrector in JavaClassReferenceProvider

--- struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsHighlightingSpringTest.java
+++ struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsHighlightingSpringTest.java
@@ -62,7 +62,6 @@ public class StrutsHighlightingSpringTest extends BasicStrutsHighlightingTestCas
 
     // TODO <alias> does not appear here, see com.intellij.spring.impl.SpringModelImpl#myOwnBeans
     myFixture.testCompletionVariants("struts-completionvariants-spring.xml",
-                                     "META-INF",
                                      "MyClass",
                                      "bean1",
                                      "bean2",
