1a92a6aa8e6a3c66cc5fef2a28f10bc77a1fc123
Yann Cebron <no_reply@jetbrains.com>
2008-08-28 22:16:19 +0400

    use JSLanguageInjector (IDEADEV-27304)

--- struts2/plugin/src/com/intellij/struts2/jsp/TaglibJavaScriptInjector.java
+++ struts2/plugin/src/com/intellij/struts2/jsp/TaglibJavaScriptInjector.java
@@ -15,19 +15,12 @@
 
 package com.intellij.struts2.jsp;
 
-import com.intellij.lang.Language;
-import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
-import com.intellij.openapi.components.AbstractProjectComponent;
+import com.intellij.lang.javascript.JSLanguageInjector;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.FileTypeManager;
-import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.fileTypes.StdFileTypes;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.xml.XmlAttribute;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.struts2.reference.ReferenceFilters;
@@ -42,14 +35,7 @@ import java.util.List;
  *
  * @author Yann C&eacute;bron
  */
-@SuppressWarnings({"ComponentNotRegistered"})
-public class TaglibJavaScriptInjector extends AbstractProjectComponent implements MultiHostInjector {
-
-  private Language myJavascriptLanguage;
-
-  public TaglibJavaScriptInjector(final Project project) {
-    super(project);
-  }
+public class TaglibJavaScriptInjector implements MultiHostInjector {
 
   public void getLanguagesToInject(@NotNull final MultiHostRegistrar registrar, @NotNull final PsiElement host) {
     // operate only in JSP(X) files
@@ -62,11 +48,7 @@ public class TaglibJavaScriptInjector extends AbstractProjectComponent implement
     if (ReferenceFilters.NAMESPACE_TAGLIB_STRUTS_UI.isAcceptable(host.getParent().getParent(), null)) {
       @NonNls final String name = ((XmlAttribute) host.getParent()).getName();
       if (name.startsWith("on")) {
-        final Language language = myJavascriptLanguage;
-        final TextRange range = new TextRange(1, host.getTextLength() - 1);
-        registrar.startInjecting(language)
-          .addPlace(null, null, (PsiLanguageInjectionHost) host, range)
-          .doneInjecting();
+        JSLanguageInjector.injectJSIntoAttributeValue(registrar, (XmlAttributeValue) host, false);
       }
     }
   }
@@ -76,12 +58,5 @@ public class TaglibJavaScriptInjector extends AbstractProjectComponent implement
     return Arrays.asList(XmlAttributeValue.class);
   }
 
-  public void initComponent() {
-    final FileType fileType = FileTypeManager.getInstance().getFileTypeByExtension("js");
-    if (!StdFileTypes.UNKNOWN.equals(fileType) && fileType instanceof LanguageFileType) {
-      myJavascriptLanguage = ((LanguageFileType) fileType).getLanguage();
-      InjectedLanguageManager.getInstance(myProject).registerMultiHostInjector(this);
-    } 
-  }
 
 }
