8cce01b4903afecc0a2320cf35feffc83acd7c30
Yann Cebron <no_reply@jetbrains.com>
2008-10-03 21:43:33 +0400

    more lazy: get rid of StrutsProjectComponent, move registerDomConverters() to StrutsManager

--- struts2/plugin/src/com/intellij/struts2/dom/struts/model/StrutsManagerImpl.java
+++ struts2/plugin/src/com/intellij/struts2/dom/struts/model/StrutsManagerImpl.java
@@ -17,9 +17,21 @@ package com.intellij.struts2.dom.struts.model;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.xml.XmlFile;
+import com.intellij.struts2.dom.struts.IncludeFileResolvingConverter;
 import com.intellij.struts2.dom.struts.StrutsRoot;
+import com.intellij.struts2.dom.struts.action.ActionClassConverter;
+import com.intellij.struts2.dom.struts.action.ResultTypeResolvingConverter;
+import com.intellij.struts2.dom.struts.action.StrutsPathReferenceConverter;
+import com.intellij.struts2.dom.struts.impl.*;
+import com.intellij.struts2.dom.struts.impl.path.StrutsPathReferenceConverterImpl;
+import com.intellij.struts2.dom.struts.strutspackage.DefaultInterceptorRefResolveConverter;
+import com.intellij.struts2.dom.struts.strutspackage.InterceptorRefResolveConverter;
+import com.intellij.struts2.dom.struts.strutspackage.StrutsPackageExtendsResolveConverter;
+import com.intellij.struts2.dom.validator.config.ValidatorConfigResolveConverter;
+import com.intellij.struts2.dom.validator.impl.ValidatorConfigResolveConverterImpl;
 import com.intellij.struts2.facet.StrutsFacet;
 import com.intellij.struts2.facet.ui.StrutsFileSet;
+import com.intellij.util.xml.ConverterManager;
 import com.intellij.util.xml.DomManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -37,6 +49,29 @@ public class StrutsManagerImpl extends StrutsManager {
 
   public StrutsManagerImpl(final DomManager domManager) {
     myStrutsModelFactory = new StrutsModelFactory(domManager);
+
+    registerDomConverters(domManager);
+  }
+
+  private static void registerDomConverters(final DomManager domManager) {
+    final ConverterManager converterManager = domManager.getConverterManager();
+    converterManager.registerConverterImplementation(ActionClassConverter.class,
+                                                     new ActionClassConverterImpl());
+    converterManager.registerConverterImplementation(StrutsPackageExtendsResolveConverter.class,
+                                                     new StrutsPackageExtendsResolveConverterImpl());
+    converterManager.registerConverterImplementation(IncludeFileResolvingConverter.class,
+                                                     new IncludeFileResolvingConverterImpl());
+    converterManager.registerConverterImplementation(ResultTypeResolvingConverter.class,
+                                                     new ResultTypeResolvingConverterImpl());
+    converterManager.registerConverterImplementation(InterceptorRefResolveConverter.class,
+                                                     new InterceptorRefResolveConverterImpl());
+    converterManager.registerConverterImplementation(DefaultInterceptorRefResolveConverter.class,
+                                                     new DefaultInterceptorRefResolveConverterImpl());
+    converterManager.registerConverterImplementation(StrutsPathReferenceConverter.class,
+                                                     new StrutsPathReferenceConverterImpl());
+
+    converterManager.registerConverterImplementation(ValidatorConfigResolveConverter.class,
+                                                     new ValidatorConfigResolveConverterImpl());
   }
 
   public boolean isStruts2ConfigFile(@NotNull final XmlFile file) {
