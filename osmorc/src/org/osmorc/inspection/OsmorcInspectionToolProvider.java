41a4a3896d3f3f9bf370f3de6e7e9e540c7dbbe2
Dmitry Jemerov <yole@intellij.com>
2009-05-12 19:58:52 +0400

    refactoring: replace OsmorcApplicationComponent with extensions

---
+++ osmorc/src/org/osmorc/inspection/OsmorcInspectionToolProvider.java
@@ -0,0 +1,15 @@
+package org.osmorc.inspection;
+
+import com.intellij.codeInspection.InspectionToolProvider;
+
+/**
+ * @author yole
+ */
+public class OsmorcInspectionToolProvider implements InspectionToolProvider {
+  public Class[] getInspectionClasses() {
+    return new Class[]{
+        InvalidImportInspection.class,
+        MisspelledHeaderNameInspection.class,
+        UnregisteredActivatorInspection.class};
+  }
+}
