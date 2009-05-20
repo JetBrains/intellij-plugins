46f46b29bfd9a9cd542f9d6dcc9103a8e41c99b5
Alexey Chmutov <alexey.chmutov@swiftteams.com>
2009-05-20 13:31:54 +0400

    Tapestry: clean up

--- core/src/com/intellij/tapestry/core/model/presentation/ParameterReceiverElement.java
+++ core/src/com/intellij/tapestry/core/model/presentation/ParameterReceiverElement.java
@@ -6,74 +6,77 @@ import com.intellij.tapestry.core.java.IJavaField;
 import com.intellij.tapestry.core.model.Library;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
+import gnu.trove.THashMap;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public abstract class ParameterReceiverElement extends PresentationLibraryElement {
 
-    private Map<String, TapestryParameter> _parametersCache;
-    private long _parametersCacheTimestamp;
+  private Map<String, TapestryParameter> _parametersCache;
+  private long _parametersCacheTimestamp;
 
-    ParameterReceiverElement(@Nullable Library library, @NonNull IJavaClassType elementClass, @NonNull TapestryProject project) {
-        super(library, elementClass, project);
+  ParameterReceiverElement(@Nullable Library library, @NonNull IJavaClassType elementClass, @NonNull TapestryProject project) {
+    super(library, elementClass, project);
+  }
+
+  /**
+   * Finds Tapestry parameters.
+   *
+   * @return the declared Tapestry parameters.
+   */
+  public Map<String, TapestryParameter> getParameters() {
+    if (_parametersCache != null && getElementClass().getFile().getFile().lastModified() <= _parametersCacheTimestamp) {
+      return _parametersCache;
     }
 
-    /**
-     * Finds Tapestry parameters.
-     *
-     * @return the declared Tapestry parameters.
-     */
-    public Map<String, TapestryParameter> getParameters() {
-        if (_parametersCache != null && getElementClass().getFile().getFile().lastModified() <= _parametersCacheTimestamp) {
-            return _parametersCache;
-        }
+    _parametersCache = new THashMap<String, TapestryParameter>();
+    _parametersCacheTimestamp = getElementClass().getFile().getFile().lastModified();
 
-        _parametersCache = new HashMap<String, TapestryParameter>();
-        _parametersCacheTimestamp = getElementClass().getFile().getFile().lastModified();
+    Map<String, IJavaField> fields = getElementClass().getFields(true);
 
-        Map<String, IJavaField> fields = getElementClass().getFields(true);
-
-        for (IJavaField field : fields.values())
-            if (field.isPrivate() && field.getAnnotations().containsKey(PARAMETER_ANNOTATION) && field.isValid()) {
-                TapestryParameter parameter = new TapestryParameter(getElementClass(), field);
-                _parametersCache.put(parameter.getName(), parameter);
-            }
-
-        return _parametersCache;
+    for (IJavaField field : fields.values()) {
+      if (field.isPrivate() && field.getAnnotations().containsKey(PARAMETER_ANNOTATION) && field.isValid()) {
+        TapestryParameter parameter = new TapestryParameter(getElementClass(), field);
+        _parametersCache.put(parameter.getName(), parameter);
+      }
     }
 
-    /**
-     * Finds Tapestry required parameters.
-     *
-     * @return the declared Tapestry required parameters.
-     */
-    public Map<String, TapestryParameter> getRequiredParameters() {
-        Map<String, TapestryParameter> parameters = getParameters();
-        Map<String, TapestryParameter> requiredParameters = new HashMap<String, TapestryParameter>();
+    return _parametersCache;
+  }
 
-        for (TapestryParameter parameter : parameters.values())
-            if (parameter.isRequired()) {
-                requiredParameters.put(parameter.getName(), parameter);
-            }
+  /**
+   * Finds Tapestry required parameters.
+   *
+   * @return the declared Tapestry required parameters.
+   */
+  public Map<String, TapestryParameter> getRequiredParameters() {
+    Map<String, TapestryParameter> parameters = getParameters();
+    Map<String, TapestryParameter> requiredParameters = new THashMap<String, TapestryParameter>();
 
-        return requiredParameters;
+    for (TapestryParameter parameter : parameters.values()) {
+      if (parameter.isRequired()) {
+        requiredParameters.put(parameter.getName(), parameter);
+      }
     }
 
-    /**
-     * Finds Tapestry not required parameters.
-     *
-     * @return the declared Tapestry not required parameters.
-     */
-    public Map<String, TapestryParameter> getOptionalParameters() {
-        Map<String, TapestryParameter> parameters = getParameters();
-        Map<String, TapestryParameter> optionalParameters = new HashMap<String, TapestryParameter>();
+    return requiredParameters;
+  }
 
-        for (TapestryParameter parameter : parameters.values())
-            if (!parameter.isRequired()) {
-                optionalParameters.put(parameter.getName(), parameter);
-            }
+  /**
+   * Finds Tapestry not required parameters.
+   *
+   * @return the declared Tapestry not required parameters.
+   */
+  public Map<String, TapestryParameter> getOptionalParameters() {
+    Map<String, TapestryParameter> parameters = getParameters();
+    Map<String, TapestryParameter> optionalParameters = new THashMap<String, TapestryParameter>();
 
-        return optionalParameters;
+    for (TapestryParameter parameter : parameters.values()) {
+      if (!parameter.isRequired()) {
+        optionalParameters.put(parameter.getName(), parameter);
+      }
     }
+
+    return optionalParameters;
+  }
 }
--- core/src/com/intellij/tapestry/intellij/lang/annotator/TemplateTagAnnotator.java
+++ core/src/com/intellij/tapestry/intellij/lang/annotator/TemplateTagAnnotator.java
@@ -40,158 +40,166 @@ import java.util.Map;
  */
 public class TemplateTagAnnotator extends XmlRecursiveElementVisitor implements Annotator {
 
-    private static final Logger _logger = LoggerFactory.getInstance().getLogger(TemplateTagAnnotator.class);
+  private static final Logger _logger = LoggerFactory.getInstance().getLogger(TemplateTagAnnotator.class);
 
-    private AnnotationHolder _annotationHolder;
+  private AnnotationHolder _annotationHolder;
 
-    public TemplateTagAnnotator() {
-        super();
+  public TemplateTagAnnotator() {
+    super();
 
-        _annotationHolder = null;
-    }//Constructor
+    _annotationHolder = null;
+  }//Constructor
 
-    public synchronized void annotate(PsiElement psiElement, AnnotationHolder annotationHolder) {
-        _annotationHolder = annotationHolder;
+  public synchronized void annotate(PsiElement psiElement, AnnotationHolder annotationHolder) {
+    _annotationHolder = annotationHolder;
 
-        psiElement.accept(this);
-    }//annotate
-    
+    psiElement.accept(this);
+  }//annotate
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void visitXmlTag(XmlTag tag) {
 
-        if (ComponentUtils.isComponentTag(new IntellijXmlTag(tag))) {
-            // don't valid these tags
-            /*tag.putUserData(XmlHighlightVisitor.DO_NOT_VALIDATE_KEY, "true");
-            tag.getParent().putUserData(XmlHighlightVisitor.DO_NOT_VALIDATE_KEY, "true");*/
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void visitXmlTag(XmlTag tag) {
 
-            // annotate the tag start
-            _annotationHolder.createInfoAnnotation(tag.getChildren()[1], null).setTextAttributes(TextAttributesKey.find(TemplateColorSettingsPage.TAPESTRY_COMPONENT_TAG_KEY));
+    if (ComponentUtils.isComponentTag(new IntellijXmlTag(tag))) {
+      // don't valid these tags
+      /*tag.putUserData(XmlHighlightVisitor.DO_NOT_VALIDATE_KEY, "true");
+     tag.getParent().putUserData(XmlHighlightVisitor.DO_NOT_VALIDATE_KEY, "true");*/
 
-            // only annotation closing tag if the tag isn't empty like <t:body/>
-            if (!tag.isEmpty()) {
-                _annotationHolder.createInfoAnnotation(tag.getChildren()[tag.getChildren().length - 2], null).setTextAttributes(TextAttributesKey.find(TemplateColorSettingsPage.TAPESTRY_COMPONENT_TAG_KEY));
+      // annotate the tag start
+      _annotationHolder.createInfoAnnotation(tag.getChildren()[1], null)
+          .setTextAttributes(TextAttributesKey.find(TemplateColorSettingsPage.TAPESTRY_COMPONENT_TAG_KEY));
+
+      // only annotation closing tag if the tag isn't empty like <t:body/>
+      if (!tag.isEmpty()) {
+        _annotationHolder.createInfoAnnotation(tag.getChildren()[tag.getChildren().length - 2], null)
+            .setTextAttributes(TextAttributesKey.find(TemplateColorSettingsPage.TAPESTRY_COMPONENT_TAG_KEY));
+      }
+
+      Module module = ProjectRootManager.getInstance(tag.getManager().getProject()).getFileIndex()
+          .getModuleForFile(tag.getContainingFile().getVirtualFile());
+      if (module == null) return;
+      Component component;
+      try {
+        component = TapestryUtils.getComponentFromTag(module, tag);
+      }
+      catch (NotFoundException ex) {
+        component = null;
+      }
+
+      if (component != null) {
+        TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
+
+        IntellijJavaClassType elementClass;
+        try {
+          elementClass =
+              (IntellijJavaClassType)ComponentUtils.findClassFromTemplate(new IntellijResource(tag.getContainingFile()), tapestryProject);
+        }
+        catch (NotFoundException ex) {
+          elementClass = null;
+        }
+
+        // annotate the tag parameters
+        if (elementClass != null) {
+          String tapestryNamespacePrefix = TapestryUtils.getTapestryNamespacePrefix((XmlFile)tag.getContainingFile());
+          for (TapestryParameter parameter : component.getParameters().values()) {
+            final String qname = tapestryNamespacePrefix + ":" + parameter.getName();
+            XmlAttribute attribute = tag.getAttribute(qname);
+
+            if (attribute == null) attribute = tag.getAttribute(parameter.getName(), "");
+
+            if (attribute == null) {
+              if (parameter.isRequired() && !parameterDefineInClass(parameter, elementClass, tag)) {
+                _annotationHolder.createErrorAnnotation(tag.getChildren()[1], "Missing required parameter \"" + parameter.getName() + "\"");
+              }
+
+              continue;
             }
 
-            Component component;
+            // annotate attribute name
+            _annotationHolder.createInfoAnnotation(attribute.getChildren()[0], null)
+                .setTextAttributes(TextAttributesKey.find(TemplateColorSettingsPage.TAPESTRY_COMPONENT_PARAMETER_KEY));
+
+            ResolvedValue resolvedValue;
             try {
-                Module module = ProjectRootManager.getInstance(tag.getManager().getProject()).getFileIndex().getModuleForFile(tag.getContainingFile().getVirtualFile());
-
-                if (module != null) {
-                    component = TapestryUtils.getComponentFromTag(module, tag);
-                } else {
-                    return;
-                }
-            } catch (NotFoundException ex) {
-                component = null;
+              resolvedValue = ValueResolverChain.getInstance()
+                  .resolve(tapestryProject, elementClass, attribute.getValue(), parameter.getDefaultPrefix());
+            }
+            catch (Exception ex) {
+              _logger.error(ex);
+              _annotationHolder.createErrorAnnotation(attribute.getValueElement(), "Invalid value");
+              continue;
             }
 
-            if (component != null) {
-
-                Module module = ProjectRootManager.getInstance(tag.getManager().getProject()).getFileIndex().getModuleForFile(tag.getContainingFile().getVirtualFile());
-                TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
-
-                IntellijJavaClassType elementClass;
-                try {
-                    elementClass = (IntellijJavaClassType) ComponentUtils.findClassFromTemplate(new IntellijResource(tag.getContainingFile()), tapestryProject);
-                } catch (NotFoundException ex) {
-                    elementClass = null;
-                }
-
-                // annotate the tag parameters
-                if (elementClass != null) {
-                    String tapestryNamespacePrefix = TapestryUtils.getTapestryNamespacePrefix((XmlFile) tag.getContainingFile());
-                    for (TapestryParameter parameter : component.getParameters().values()) {
-                        XmlAttribute attribute = tag.getAttribute(tapestryNamespacePrefix+":"+parameter.getName());
-
-                        if (attribute == null)
-                            attribute = tag.getAttribute(parameter.getName(), "");
-
-                        if (attribute == null) {
-                            if (parameter.isRequired() && !parameterDefineInClass(parameter, elementClass, tag))
-                                _annotationHolder.createErrorAnnotation(tag.getChildren()[1], "Missing required parameter \"" + parameter.getName() + "\"");
-
-                            continue;
-                        }
-
-                        // annotate attribute name
-                        _annotationHolder.createInfoAnnotation(attribute.getChildren()[0], null).setTextAttributes(TextAttributesKey.find(TemplateColorSettingsPage.TAPESTRY_COMPONENT_PARAMETER_KEY));
-
-                        ResolvedValue resolvedValue;
-                        try {
-                            resolvedValue = ValueResolverChain.getInstance().resolve(tapestryProject, elementClass, attribute.getValue(), parameter.getDefaultPrefix());
-                        } catch (Exception ex) {
-                            _logger.error(ex);
-
-                            _annotationHolder.createErrorAnnotation(attribute.getValueElement(), "Invalid value");
-
-                            continue;
-                        }
-
-                        if (resolvedValue == null) {
-                            _annotationHolder.createErrorAnnotation(attribute.getValueElement(), "Invalid value");
-
-                            continue;
-                        }
-
-                        IJavaType parameterType = parameter.getParameterField().getType();
-                        if (!TypeCoercionValidator.canCoerce(tapestryProject, resolvedValue.getType(), AbstractValueResolver.getCleanValue(attribute.getValue()), parameterType)) {
-                            _annotationHolder.createErrorAnnotation(attribute.getValueElement(),
-                                    "Can't coerce a " + resolvedValue.getType().getName() + " to a " + (parameterType != null ? parameterType.getName() : "undefined"));
-                        }
-                    }
-                }
-            } else {
-                XmlElement identifierAttribute = TapestryUtils.getComponentIdentifier(tag);
-                if (identifierAttribute != null) {
-                    if (identifierAttribute instanceof XmlAttribute) {
-                        _annotationHolder.createErrorAnnotation(identifierAttribute.getChildren()[0], "Invalid component name");
-                    } else {
-                        _annotationHolder.createErrorAnnotation(identifierAttribute.getNavigationElement(), "Invalid component name");
-                    }
-                }
+            if (resolvedValue == null) {
+              _annotationHolder.createErrorAnnotation(attribute.getValueElement(), "Invalid value");
+              continue;
             }
+
+            IJavaType parameterType = parameter.getParameterField().getType();
+            if (!TypeCoercionValidator
+                .canCoerce(tapestryProject, resolvedValue.getType(), AbstractValueResolver.getCleanValue(attribute.getValue()),
+                           parameterType)) {
+              _annotationHolder.createErrorAnnotation(attribute.getValueElement(), "Can't coerce a " +
+                                                                                   resolvedValue.getType().getName() +
+                                                                                   " to a " +
+                                                                                   (parameterType != null
+                                                                                    ? parameterType.getName()
+                                                                                    : "undefined"));
+            }
+          }
         }
-
-        tag.acceptChildren(this);
-    }//visitXmlTag
-
-    /**
-     * Verify the existence of parameter declaration in elementClass
-     *
-     * @param parameter    the parameter to check
-     * @param elementClass the class to get the fields
-     * @param tag          the component to get the parameters
-     * @return <code>true</code> if the parameter is define in the class, <code>false</code> otherwise.
-     */
-    public boolean parameterDefineInClass(TapestryParameter parameter, IntellijJavaClassType elementClass, XmlTag tag) {
-
-        Map<String, IJavaField> fields = elementClass.getFields(false);
-        boolean parameterDefineInClass = false;
-
-        for (IJavaField field : fields.values()) {
-            if (field.getAnnotations().get(TapestryConstants.COMPONENT_ANNOTATION) != null) {
-                if (field.getName().equals(tag.getAttributeValue("t:id"))) {
-
-                    IJavaAnnotation fieldAnnotation = field.getAnnotations().get(TapestryConstants.COMPONENT_ANNOTATION);
-                    String[] fieldParameters = fieldAnnotation.getParameters().get("parameters");
-
-                    if (fieldParameters != null) {
-                        for (String fieldParameter : fieldParameters) {
-                            if (fieldParameter.split("=").length == 2) {
-                                String parameterName = fieldParameter.split("=")[0];
-                                if (parameterName.equals(parameter.getName()))
-                                    parameterDefineInClass = true;
-                            }
-                        }
-                    }
-                }
-            }
+      }
+      else {
+        XmlElement identifierAttribute = TapestryUtils.getComponentIdentifier(tag);
+        if (identifierAttribute != null) {
+          if (identifierAttribute instanceof XmlAttribute) {
+            _annotationHolder.createErrorAnnotation(identifierAttribute.getChildren()[0], "Invalid component name");
+          }
+          else {
+            _annotationHolder.createErrorAnnotation(identifierAttribute.getNavigationElement(), "Invalid component name");
+          }
         }
-        return parameterDefineInClass;
-    }//parameterDefineInClass
+      }
+    }
+
+    tag.acceptChildren(this);
+  }//visitXmlTag
+
+  /**
+   * Verify the existence of parameter declaration in elementClass
+   *
+   * @param parameter    the parameter to check
+   * @param elementClass the class to get the fields
+   * @param tag          the component to get the parameters
+   * @return <code>true</code> if the parameter is define in the class, <code>false</code> otherwise.
+   */
+  public boolean parameterDefineInClass(TapestryParameter parameter, IntellijJavaClassType elementClass, XmlTag tag) {
+
+    Map<String, IJavaField> fields = elementClass.getFields(false);
+    boolean parameterDefineInClass = false;
+
+    for (IJavaField field : fields.values()) {
+      if (field.getAnnotations().get(TapestryConstants.COMPONENT_ANNOTATION) != null) {
+        if (field.getName().equals(tag.getAttributeValue("t:id"))) {
+
+          IJavaAnnotation fieldAnnotation = field.getAnnotations().get(TapestryConstants.COMPONENT_ANNOTATION);
+          String[] fieldParameters = fieldAnnotation.getParameters().get("parameters");
+
+          if (fieldParameters != null) {
+            for (String fieldParameter : fieldParameters) {
+              if (fieldParameter.split("=").length == 2) {
+                String parameterName = fieldParameter.split("=")[0];
+                if (parameterName.equals(parameter.getName())) parameterDefineInClass = true;
+              }
+            }
+          }
+        }
+      }
+    }
+    return parameterDefineInClass;
+  }//parameterDefineInClass
 
 }//TemplateTagAnnotator
