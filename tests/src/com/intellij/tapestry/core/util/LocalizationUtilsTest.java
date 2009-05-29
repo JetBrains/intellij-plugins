a8df3b97688c728a4bc7c726db859c539509ad24
Alexey Chmutov <alexey.chmutov@swiftteams.com>
2009-05-29 18:52:27 +0400

    Tapestry: test fixed

--- tests/src/com/intellij/tapestry/core/util/LocalizationUtilsTest.java
+++ tests/src/com/intellij/tapestry/core/util/LocalizationUtilsTest.java
@@ -7,31 +7,31 @@ import org.testng.annotations.Test;
  */
 public class LocalizationUtilsTest {
 
-    @Test
-    public void constructor() {
-        new LocalizationUtils();
-    }
+  @Test
+  public void constructor() {
+    new LocalizationUtils();
+  }
 
-    @Test
-    public void unlocalizeFileName() {
-        assert LocalizationUtils.unlocalizeFileName("SomeFile.properties").equals("SomeFile.properties");
+  @Test
+  public void unlocalizeFileName() {
+    assert LocalizationUtils.unlocalizeFileName("SomeFile.properties").equals("SomeFile.properties");
 
-        assert LocalizationUtils.unlocalizeFileName("SomeFile_pt.properties").equals("SomeFile.properties");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_pt.properties").equals("SomeFile.properties");
 
-        assert LocalizationUtils.unlocalizeFileName("SomeFile_yy.properties").equals("SomeFile_yy.properties");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_yy.properties").equals("SomeFile_yy.properties");
 
-        assert LocalizationUtils.unlocalizeFileName("SomeFile_pt_PT.properties").equals("SomeFile.properties");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_pt_PT.properties").equals("SomeFile.properties");
 
-        assert LocalizationUtils.unlocalizeFileName("SomeFile_yy_PT.properties").equals("SomeFile_yy_PT.properties");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_yy_PT.properties").equals("SomeFile_yy_PT.properties");
 
-      assert LocalizationUtils.unlocalizeFileName("SomeFile").equals("SomeFile");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile").equals("SomeFile");
 
-      assert LocalizationUtils.unlocalizeFileName("SomeFile_pt").equals("SomeFile");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_pt").equals("SomeFile");
 
-      assert LocalizationUtils.unlocalizeFileName("SomeFile_yy").equals("SomeFile_yy");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_yy").equals("SomeFile_yy");
 
-      assert LocalizationUtils.unlocalizeFileName("SomeFile_pt_PT").equals("SomeFile.properties");
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_pt_PT").equals("SomeFile");
 
-      assert LocalizationUtils.unlocalizeFileName("SomeFile_yy_PT").equals("SomeFile_yy_PT");
-    }
+    assert LocalizationUtils.unlocalizeFileName("SomeFile_yy_PT").equals("SomeFile_yy_PT");
+  }
 }
