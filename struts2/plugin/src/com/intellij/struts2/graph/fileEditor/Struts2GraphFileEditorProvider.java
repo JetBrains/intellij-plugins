37dd986bb3b6930715ac05d6fac1280f37a3775f
Yann Cebron <no_reply@jetbrains.com>
2009-06-24 00:11:00 +0400

    revert DumbAware for now

--- struts2/plugin/src/com/intellij/struts2/graph/fileEditor/Struts2GraphFileEditorProvider.java
+++ struts2/plugin/src/com/intellij/struts2/graph/fileEditor/Struts2GraphFileEditorProvider.java
@@ -16,7 +16,6 @@ package com.intellij.struts2.graph.fileEditor;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
-import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
@@ -35,7 +34,7 @@ import java.util.Set;
  *
  * @author Yann C&eacute;bron
  */
-public class Struts2GraphFileEditorProvider extends PerspectiveFileEditorProvider implements DumbAware {
+public class Struts2GraphFileEditorProvider extends PerspectiveFileEditorProvider {
 
   public boolean accept(@NotNull final Project project, @NotNull final VirtualFile file) {
     final PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
