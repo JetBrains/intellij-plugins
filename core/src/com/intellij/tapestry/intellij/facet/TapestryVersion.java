6af454361494123517a5e889d0d87e62063f2f57
Alexey Chmutov <alexey.chmutov@swiftteams.com>
2009-06-24 14:07:11 +0400

    Tapestry: dependency on correct version of antlr-runtime lib added

--- core/src/com/intellij/tapestry/intellij/facet/TapestryVersion.java
+++ core/src/com/intellij/tapestry/intellij/facet/TapestryVersion.java
@@ -6,6 +6,7 @@ import static com.intellij.facet.ui.libraries.MavenLibraryUtil.createSubMavenJar
 public enum TapestryVersion {
 
   TAPESTRY_5_1_0_5("5.1.0.5", new LibraryInfo[]{
+      createSubMavenJarInfo("org/antlr", "antlr-runtime", "3.1.1", "org.antlr.runtime.Lexer"),
       createSubMavenJarInfo("org/apache/tapestry", "tapestry-core", "5.1.0.5", "org.apache.tapestry5.corelib.components.Loop"),
       createSubMavenJarInfo("org/apache/tapestry", "tapestry-ioc", "5.1.0.5", "org.apache.tapestry5.ioc.Registry"),
       createSubMavenJarInfo("org/apache/tapestry", "tapestry5-annotations", "5.1.0.5", "org.apache.tapestry5.beaneditor.Validate"),
