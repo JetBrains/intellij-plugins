a1d94e1e5e6644e045503e58984845937fef40c3
Anton Makeev <Anton.Makeev@jetbrains.com>
2009-08-13 13:00:22 +0400

    Find/Highlight Usagesi: use same find target algorithm (UsageTargets) Maven: properties find usages, refactorings +  filtered properties

--- tests/src/com/intellij/tapestry/tests/core/util/IdeaUtilsTest.java
+++ tests/src/com/intellij/tapestry/tests/core/util/IdeaUtilsTest.java
@@ -1,18 +1,14 @@
 package com.intellij.tapestry.tests.core.util;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.actionSystem.ex.DataConstantsEx;
 import com.intellij.tapestry.intellij.util.IdeaUtils;
 import com.intellij.tapestry.tests.core.BaseTestCase;
+import com.intellij.testFramework.MapDataContext;
 import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.Nullable;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import java.util.HashMap;
 
 public class IdeaUtilsTest extends BaseTestCase {
 
@@ -38,11 +34,4 @@ public class IdeaUtilsTest extends BaseTestCase {
         assert !IdeaUtils.isModuleNode(actionEvent);
     }
 
-    static class MapDataContext extends HashMap implements DataContext {
-
-        @Nullable
-        public Object getData(@NonNls String dataId) {
-            return get(dataId);
-        }
-    }
 }
