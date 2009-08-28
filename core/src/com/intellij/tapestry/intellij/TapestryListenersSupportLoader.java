fd9c788240e6da6b063e2b1e488865eedb57e2ee
Alexey Kudravtsev <cdr@jetbrains.com>
2009-08-28 18:01:29 +0400

    cleanup

--- core/src/com/intellij/tapestry/intellij/TapestryListenersSupportLoader.java
+++ core/src/com/intellij/tapestry/intellij/TapestryListenersSupportLoader.java
@@ -4,10 +4,10 @@ import com.intellij.ProjectTopics;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
+import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootEvent;
 import com.intellij.openapi.roots.ModuleRootListener;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.psi.*;
 import com.intellij.tapestry.core.TapestryProject;
 import com.intellij.tapestry.intellij.core.resource.IntellijResource;
@@ -21,150 +21,117 @@ import org.jetbrains.annotations.NotNull;
  */
 public class TapestryListenersSupportLoader implements ProjectComponent {
 
-    private Project _project;
+  private final Project myProject;
 
   public TapestryListenersSupportLoader(Project project) {
-        _project = project;
-    }
+    myProject = project;
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    public void initComponent() {
-    }
+  /**
+   * {@inheritDoc}
+   */
+  public void initComponent() {
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    public void disposeComponent() {
-    }
+  /**
+   * {@inheritDoc}
+   */
+  public void disposeComponent() {
+  }
 
-    /**
-     * {@inheritDoc}
-     */
-    @NotNull
-    public String getComponentName() {
-        return TapestryListenersSupportLoader.class.getName();
-    }
+  /**
+   * {@inheritDoc}
+   */
+  @NotNull
+  public String getComponentName() {
+    return TapestryListenersSupportLoader.class.getName();
+  }
 
-    public void projectOpened() {
+  public void projectOpened() {
 
-        // Listener for file deletion
-        PsiManager.getInstance(_project).addPsiTreeChangeListener(
-                new PsiTreeChangeAdapter() {
-                    public void childRemoved(PsiTreeChangeEvent event) {
-                        Module module = getModuleFromEvent(event);
-                        if (module == null) {
-                            return;
-                        }
+    // Listener for file deletion
+    PsiManager.getInstance(myProject).addPsiTreeChangeListener(new PsiTreeChangeAdapter() {
+      public void childRemoved(PsiTreeChangeEvent event) {
+        Module module = getModuleFromEvent(event);
 
-                        TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
+        if (!TapestryUtils.isTapestryModule(module)) {
+          return;
+        }
+        TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
 
-                        if (!TapestryUtils.isTapestryModule(module)) {
-                            return;
-                        }
-
-                        if (event.getChild() instanceof PsiJavaFile && IdeaUtils.findPublicClass(((PsiJavaFile) event.getChild()).getClasses()) != null) {
-                            tapestryProject.getEventsManager().classDeleted(IdeaUtils.findPublicClass(((PsiJavaFile) event.getChild()).getClasses()).getQualifiedName());
-                        }
-
-                        if (event.getChild() instanceof PsiFile) {
-                            tapestryProject.getEventsManager().fileDeleted(((PsiFile) event.getChild()).getVirtualFile().getPath());
-                        }
-
-                        if (event.getChild() instanceof PsiDirectory) {
-                            tapestryProject.getEventsManager().fileDeleted(((PsiDirectory) event.getChild()).getVirtualFile().getPath());
-                        }
-                    }
-                }
-        );
-
-        // Listener for file creation
-        PsiManager.getInstance(_project).addPsiTreeChangeListener(
-                new PsiTreeChangeAdapter() {
-                    public void childAdded(PsiTreeChangeEvent event) {
-                        Module module = getModuleFromEvent(event);
-                        if (module == null) {
-                            return;
-                        }
-
-                        TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
-
-                        if (!TapestryUtils.isTapestryModule(module)) {
-                            return;
-                        }
-
-                        if (event.getFile() != null) {
-                            tapestryProject.getEventsManager().fileContentsChanged(new IntellijResource(event.getFile()));
-                        }
-
-                        if (!(event.getChild() instanceof PsiFile)) {
-                            return;
-                        }
-
-                        PsiFile psiFile = (PsiFile) event.getChild();
-
-                        if (psiFile instanceof PsiJavaFile) {
-                            tapestryProject.getEventsManager().classCreated(null);
-
-                            return;
-                        }
-
-                        tapestryProject.getEventsManager().fileCreated(psiFile.getVirtualFile().getPath());
-                    }
-
-                    public void childRemoved(PsiTreeChangeEvent event) {
-                        Module module = getModuleFromEvent(event);
-                        if (module == null) {
-                            return;
-                        }
-
-                        if (!TapestryUtils.isTapestryModule(module)) {
-                            return;
-                        }
-
-                        TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
-
-                        if (event.getFile() != null) {
-                            tapestryProject.getEventsManager().fileContentsChanged(new IntellijResource(event.getFile()));
-                        }
-                    }
-                }
-        );
-
-      MessageBusConnection _messageBusConnection = _project.getMessageBus().connect(_project);
-        _messageBusConnection.subscribe(ProjectTopics.PROJECT_ROOTS,
-                new ModuleRootListener() {
-                    public void beforeRootsChange(ModuleRootEvent event) {
-                    }
-
-                    public void rootsChanged(ModuleRootEvent event) {
-                        for (Module module : ModuleManager.getInstance((Project) event.getSource()).getModules()) {
-                            if (!TapestryUtils.isTapestryModule(module)) {
-                                return;
-                            }
-
-                            TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
-                            tapestryProject.getEventsManager().modelChanged();
-                        }
-                    }
-                }
-        );
-    }
-
-    public void projectClosed() {
-    }
-
-    private Module getModuleFromEvent(PsiTreeChangeEvent event) {
-        PsiElement parent = event.getParent();
-        while (parent != null && !(parent instanceof PsiDirectory)) {
-            parent = parent.getParent();
+        PsiElement child = event.getChild();
+        if (child instanceof PsiJavaFile && IdeaUtils.findPublicClass(((PsiJavaFile)child).getClasses()) != null) {
+          tapestryProject.getEventsManager().classDeleted(IdeaUtils.findPublicClass(((PsiJavaFile)child).getClasses()).getQualifiedName());
         }
 
-        if (parent != null) {
-            return ProjectRootManager.getInstance(_project).getFileIndex().getModuleForFile(((PsiDirectory) parent).getVirtualFile());
-        } else {
-            return null;
+        if (child instanceof PsiFile) {
+          tapestryProject.getEventsManager().fileDeleted(((PsiFile)child).getVirtualFile().getPath());
         }
-    }
+
+        if (child instanceof PsiDirectory) {
+          tapestryProject.getEventsManager().fileDeleted(((PsiDirectory)child).getVirtualFile().getPath());
+        }
+
+        if (event.getFile() != null) {
+          tapestryProject.getEventsManager().fileContentsChanged(new IntellijResource(event.getFile()));
+        }
+      }
+
+      public void childAdded(PsiTreeChangeEvent event) {
+        Module module = getModuleFromEvent(event);
+        if (module == null) {
+          return;
+        }
+
+        TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
+
+        if (!TapestryUtils.isTapestryModule(module)) {
+          return;
+        }
+
+        if (event.getFile() != null) {
+          tapestryProject.getEventsManager().fileContentsChanged(new IntellijResource(event.getFile()));
+        }
+
+        if (!(event.getChild() instanceof PsiFile)) {
+          return;
+        }
+
+        PsiFile psiFile = (PsiFile)event.getChild();
+
+        if (psiFile instanceof PsiJavaFile) {
+          tapestryProject.getEventsManager().classCreated(null);
+
+          return;
+        }
+
+        tapestryProject.getEventsManager().fileCreated(psiFile.getVirtualFile().getPath());
+      }
+    });
+
+    MessageBusConnection connection = myProject.getMessageBus().connect(myProject);
+    connection.subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootListener() {
+      public void beforeRootsChange(ModuleRootEvent event) {
+      }
+
+      public void rootsChanged(ModuleRootEvent event) {
+        for (Module module : ModuleManager.getInstance((Project)event.getSource()).getModules()) {
+          if (!TapestryUtils.isTapestryModule(module)) {
+            return;
+          }
+
+          TapestryProject tapestryProject = TapestryModuleSupportLoader.getTapestryProject(module);
+          tapestryProject.getEventsManager().modelChanged();
+        }
+      }
+    });
+  }
+
+  public void projectClosed() {
+  }
+
+  private static Module getModuleFromEvent(PsiTreeChangeEvent event) {
+    PsiElement parent = event.getParent();
+    return parent == null ? null : ModuleUtil.findModuleForPsiElement(parent);
+  }
 }
