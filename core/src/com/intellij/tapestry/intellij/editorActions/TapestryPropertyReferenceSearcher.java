135eaf5aac0e93f60e7e0d12161ae457dc9649be
Gregory Shrago <Gregory.Shrago@jetbrains.com>
2010-06-30 20:45:26 +0400

    ref search optimization

--- core/src/com/intellij/tapestry/intellij/editorActions/TapestryPropertyReferenceSearcher.java
+++ core/src/com/intellij/tapestry/intellij/editorActions/TapestryPropertyReferenceSearcher.java
@@ -1,30 +1,32 @@
 package com.intellij.tapestry.intellij.editorActions;
 
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.psi.*;
-import com.intellij.psi.search.*;
+import com.intellij.openapi.application.QueryExecutorBase;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.UsageSearchContext;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.tapestry.lang.TmlFileType;
 import com.intellij.util.Processor;
-import com.intellij.util.QueryExecutor;
 
 /**
  * @author Alexey Chmutov
  *         Date: Dec 10, 2009
  *         Time: 6:38:33 PM
  */
-public class TapestryPropertyReferenceSearcher implements QueryExecutor<PsiReference, ReferencesSearch.SearchParameters> {
-  public boolean execute(final ReferencesSearch.SearchParameters queryParameters, final Processor<PsiReference> consumer) {
-    final PsiElement refElement = queryParameters.getElementToSearch();
-    if (!(refElement instanceof PsiField)) return true;
-    final String name = ((PsiField)refElement).getName();
-    if (name == null) return true;
+public class TapestryPropertyReferenceSearcher extends QueryExecutorBase<PsiReference, ReferencesSearch.SearchParameters> {
+  public TapestryPropertyReferenceSearcher() {
+    super(true);
+  }
 
-    //SearchScope searchScope = ApplicationManager.getApplication().runReadAction(new Computable<SearchScope>() {
-    //  public SearchScope compute() {
-    //    return queryParameters.getEffectiveSearchScope();
-    //  }
-    //});
+  @Override
+  public void processQuery(ReferencesSearch.SearchParameters queryParameters, Processor<PsiReference> consumer) {
+    final PsiElement refElement = queryParameters.getElementToSearch();
+    if (!(refElement instanceof PsiField)) return;
+    final String name = ((PsiField)refElement).getName();
+    if (name == null) return;
     SearchScope searchScope = queryParameters.getScope();
     if (searchScope instanceof GlobalSearchScope) {
       searchScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)searchScope, TmlFileType.INSTANCE);
@@ -34,26 +36,9 @@ public class TapestryPropertyReferenceSearcher implements QueryExecutor<PsiRefer
         GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.allScope(refElement.getProject()), TmlFileType.INSTANCE)
           .intersectWith(searchScope);
     }
-    final TextOccurenceProcessor processor = new TextOccurenceProcessor() {
-      public boolean execute(PsiElement element, int offsetInElement) {
-        ProgressManager.checkCanceled();
-        final PsiReference[] refs = element.getReferences();
-        for (PsiReference ref : refs) {
-          if (ReferenceRange.containsOffsetInElement(ref, offsetInElement) && ref.isReferenceTo(refElement)) {
-            return consumer.process(ref);
-          }
-        }
-        return true;
-      }
-    };
-    final PsiSearchHelper helper = PsiManager.getInstance(refElement.getProject()).getSearchHelper();
-    final boolean propertyFound =
-      helper.processElementsWithWord(processor, searchScope, name, UsageSearchContext.IN_FOREIGN_LANGUAGES, false);
-    final boolean getterFound =
-      helper.processElementsWithWord(processor, searchScope, "get" + name, UsageSearchContext.IN_FOREIGN_LANGUAGES, false);
-    final boolean setterFound =
-      helper.processElementsWithWord(processor, searchScope, "set" + name, UsageSearchContext.IN_FOREIGN_LANGUAGES, false);
-    return propertyFound || getterFound || setterFound;
+    queryParameters.getOptimizer().searchWord(name, searchScope, UsageSearchContext.IN_FOREIGN_LANGUAGES, false, refElement);
+    queryParameters.getOptimizer().searchWord("get" + name, searchScope, UsageSearchContext.IN_FOREIGN_LANGUAGES, false, refElement);
+    queryParameters.getOptimizer().searchWord("set" + name, searchScope, UsageSearchContext.IN_FOREIGN_LANGUAGES, false, refElement);
   }
 }
 
