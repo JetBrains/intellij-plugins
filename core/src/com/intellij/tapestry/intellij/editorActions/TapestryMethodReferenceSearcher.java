2aa9744ebf3edb8722d43cf0cd992a16ebb41e45
Gregory Shrago <Gregory.Shrago@jetbrains.com>
2010-07-02 17:37:44 +0400

    methodSearchers -> optimizer

--- core/src/com/intellij/tapestry/intellij/editorActions/TapestryMethodReferenceSearcher.java
+++ core/src/com/intellij/tapestry/intellij/editorActions/TapestryMethodReferenceSearcher.java
@@ -1,56 +1,37 @@
 package com.intellij.tapestry.intellij.editorActions;
 
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
-import com.intellij.psi.*;
-import com.intellij.psi.search.*;
+import com.intellij.openapi.application.QueryExecutorBase;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
+import com.intellij.psi.search.UsageSearchContext;
 import com.intellij.psi.search.searches.MethodReferencesSearch;
 import com.intellij.tapestry.intellij.util.TapestryPropertyNamingUtil;
 import com.intellij.tapestry.lang.TmlFileType;
 import com.intellij.util.Processor;
-import com.intellij.util.QueryExecutor;
 
 /**
  * @author Alexey Chmutov
  */
-public class TapestryMethodReferenceSearcher implements QueryExecutor<PsiReference, MethodReferencesSearch.SearchParameters> {
-  public boolean execute(final MethodReferencesSearch.SearchParameters parameters, final Processor<PsiReference> consumer) {
+public class TapestryMethodReferenceSearcher extends QueryExecutorBase<PsiReference, MethodReferencesSearch.SearchParameters> {
+
+  public TapestryMethodReferenceSearcher() {
+    super(true);
+  }
+
+  @Override
+  public void processQuery(MethodReferencesSearch.SearchParameters parameters, Processor<PsiReference> consumer) {
     final PsiMethod method = parameters.getMethod();
-    final Ref<String> propNameRef = Ref.create(null);
-    final Ref<Project> project = Ref.create(null);
-    ApplicationManager.getApplication().runReadAction(new Runnable() {
-      public void run() {
-        if (!method.isValid()) return;
-        project.set(method.getProject());
-        propNameRef.set(TapestryPropertyNamingUtil.getPropertyNameFromAccessor(method));
-      }
-    });
-    if (project.isNull()) return true;
+    final String propName = TapestryPropertyNamingUtil.getPropertyNameFromAccessor(method);
+
     SearchScope searchScope = parameters.getScope();
     if (searchScope instanceof GlobalSearchScope) {
       searchScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)searchScope, TmlFileType.INSTANCE);
     }
-
-    final TextOccurenceProcessor processor = new TextOccurenceProcessor() {
-      public boolean execute(PsiElement element, int offsetInElement) {
-        ProgressManager.checkCanceled();
-        final PsiReference[] refs = element.getReferences();
-        for (PsiReference ref : refs) {
-          if (ReferenceRange.containsOffsetInElement(ref, offsetInElement) && ref.isReferenceTo(method)) {
-            return consumer.process(ref);
-          }
-        }
-        return true;
-      }
-    };
-    final PsiSearchHelper helper = PsiManager.getInstance(project.get()).getSearchHelper();
-    final String propName = propNameRef.get();
-    final boolean propertyFound =
-      propName != null && helper.processElementsWithWord(processor, searchScope, propName, UsageSearchContext.IN_FOREIGN_LANGUAGES, false);
-    final boolean methodFound =
-      helper.processElementsWithWord(processor, searchScope, method.getName(), UsageSearchContext.IN_FOREIGN_LANGUAGES, false);
-    return propertyFound || methodFound;
+    if (propName != null) {
+      parameters.getOptimizer().searchWord(propName, searchScope, UsageSearchContext.IN_FOREIGN_LANGUAGES, false, method);
+    }
+    parameters.getOptimizer().searchWord(method.getName(), searchScope, UsageSearchContext.IN_FOREIGN_LANGUAGES, false, method);
   }
 }
