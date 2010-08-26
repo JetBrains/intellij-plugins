d5b8c14fa10a7640e1705fd3ae7c6cf9cdb4ef4d
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2010-08-26 20:42:57 +0400

    removed deprecated in Idea 6 method FlexLexer.reset(CharSequence, int)

--- core/src/com/intellij/tapestry/psi/_TelLexer.java
+++ core/src/com/intellij/tapestry/psi/_TelLexer.java
@@ -369,19 +369,6 @@ class _TelLexer implements FlexLexer {
     yybegin(initialState);
   }
 
-  // For Demetra compatibility
-  public void reset(CharSequence buffer, int initialState){
-    resetAll();
-    zzBuffer = buffer;
-    zzBufferArray = null; 
-    zzCurrentPos = zzMarkedPos = zzStartRead = 0;
-    zzPushbackPos = 0;
-    zzAtEOF = false;
-    zzAtBOL = true;
-    zzEndRead = buffer.length();
-    yybegin(initialState);
-  }
-
   /**
    * Refills the input buffer.
    *
