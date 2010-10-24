f5efbd4d4f2762d468dcd8d00820651364bc3b7a
Yann Cebron <yann.cebron@googlemail.com>
2010-10-24 16:41:18 +0200

    fix StrutsFileSetCheckingAnnotator.AddToFileSetFix fileset chooser popup appearance

--- struts2/plugin/src/com/intellij/struts2/annotators/StrutsFileSetCheckingAnnotator.java
+++ struts2/plugin/src/com/intellij/struts2/annotators/StrutsFileSetCheckingAnnotator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 The authors
+ * Copyright 2010 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -184,7 +184,7 @@ public class StrutsFileSetCheckingAnnotator implements Annotator {
                   return super.onChosen(selectedValue, finalChoice);
                 }
               };
-      JBPopupFactory.getInstance().createListPopup(step).showInBestPositionFor(DataManager.getInstance().getDataContext());
+      JBPopupFactory.getInstance().createListPopup(step).showInBestPositionFor(DataManager.getInstance().getDataContext(editor.getComponent()));
     }
   }
 
