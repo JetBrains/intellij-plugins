b4a9b8a94361fdf317fc9b7a84bc90c217abcf2a
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2010-11-07 20:36:00 +0300

    initial impl

---
+++ flex/tools/flexmojos-flex-configs-generator/src/main/java/com/intellij/flex/maven/IdeaConfigurator.java
@@ -0,0 +1,263 @@
+package com.intellij.flex.maven;
+
+import org.apache.maven.model.Build;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.annotations.Component;
+import org.sonatype.flexmojos.compiler.*;
+import org.sonatype.flexmojos.configurator.Configurator;
+import org.sonatype.flexmojos.generator.iface.StringUtil;
+import org.sonatype.flexmojos.plugin.utilities.SourceFileResolver;
+
+import java.io.*;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+@Component(role = Configurator.class)
+public class IdeaConfigurator implements Configurator {
+  @SuppressWarnings({"FieldCanBeLocal"})
+  private MavenProject project;
+  private OutputStreamWriter out;
+
+  private static final Map<String, String> childTagNameMap = new HashMap<String, String>(5);
+
+  static {
+    childTagNameMap.put("keep-as3-metadata", "name");
+    childTagNameMap.put("include-namespaces", "uri");
+    childTagNameMap.put("include-classes", "class");
+    childTagNameMap.put("locale", "locale-element");
+    childTagNameMap.put("managers", "manager-class");
+  }
+
+  // todo buildConfiguration/buildConfiguration — add throws MojoExecutionException, MojoFailureException (like org.apache.maven.plugin.Mojo.execute)?
+
+  @Override
+  public void buildConfiguration(ICommandLineConfiguration configuration, Map<String, Object> parameters) {
+    try {
+      init(parameters);
+      build(configuration, ICommandLineConfiguration.class, "\n\t");
+
+      out.append("\n\t<file-specs>\n\t\t<path-element>").append(getSourceFile().getAbsolutePath()).append("</path-element>\n\t</file-specs>");
+      close();
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  // todo temp hack, how we can get source file for mxmlc mojo?
+  private File getSourceFile() {
+    return SourceFileResolver.resolveSourceFile(project.getCompileSourceRoots(), null, project.getGroupId(), project.getArtifactId());
+  }
+
+  @Override
+  public void buildConfiguration(ICompcConfiguration configuration, Map<String, Object> parameters) {
+    try {
+      init(parameters);
+      build(configuration, ICompcConfiguration.class, "\n\t");
+      close();
+    }
+    catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private void init(Map<String, Object> parameters) throws IOException {
+    project = (MavenProject) parameters.get("project");
+
+    Build build = project.getBuild();
+    StringBuilder pathBuilder = new StringBuilder(build.getDirectory()).append(File.separatorChar);
+    pathBuilder.append(build.getFinalName());
+    // todo how I can get classifier param from flexmojos configuration?
+//    pathBuilder.append(classifierElement == null ? "" : "-" + classifierElement.getTextNormalize());
+    // todo suffix must be "-config-report.xml" (avoid conflict with flexmojos config report), but I use "-configs.xml" for compatibility with current flex idea plugin
+//    pathBuilder.append("-config-report.xml");
+    pathBuilder.append("-configs.xml");
+
+
+    File configFile = new File(pathBuilder.toString());
+    out = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream(configFile)), "utf-8");
+    out.write("<flex-config>");
+  }
+
+  private void close() throws IOException {
+    out.write("\n</flex-config>");
+    out.close();
+  }
+
+  @SuppressWarnings({"ConstantConditions"})
+  private <E> void build(E configuration, Class<? extends E> configClass, String indent) throws Exception {
+    for (Method method : configClass.getDeclaredMethods()) {
+      if (method.getParameterTypes().length != 0 || !Modifier.isPublic(method.getModifiers())) {
+        continue;
+      }
+
+      String methodName = method.getName();
+      if (methodName.equals("getLoadConfig") || methodName.equals("getDumpConfig")) {
+        continue;
+      }
+
+      Object value = method.invoke(configuration);
+      if (value == null) {
+        continue;
+      }
+
+      Class<?> returnType = method.getReturnType();
+      String name = parseName(methodName);
+
+      if (value instanceof IFlexConfiguration) {
+        String configurationName = name.substring(0, name.length() - 14);
+        out.append(indent).append('<').append(configurationName).append('>');
+        build(value, returnType, indent + "\t");
+        out.append(indent).append("</").append(configurationName).append('>');
+      }
+      else if (configuration instanceof IASDocConfiguration && "footer".equals(name)) {
+        // todo
+        throw new UnsupportedOperationException();
+      }
+      else if (configuration instanceof IFontsConfiguration && "managers".equals(name)) {
+        // todo
+        throw new UnsupportedOperationException();
+      }
+      else if (value instanceof IRuntimeSharedLibraryPath || value instanceof IRuntimeSharedLibraryPath[]) {
+        final IRuntimeSharedLibraryPath[] values;
+        if (returnType.isArray()) {
+          //noinspection ConstantConditions
+          values = (IRuntimeSharedLibraryPath[]) value;
+        }
+        else {
+          //noinspection ConstantConditions
+          values = new IRuntimeSharedLibraryPath[]{(IRuntimeSharedLibraryPath) value};
+        }
+
+        for (IRuntimeSharedLibraryPath arg : values) {
+          out.write("\n\t<");
+          out.write(name);
+          out.write(">\n\t\t<path-element>");
+          out.write(arg.pathElement());
+          out.write("</path-element>");
+
+          //noinspection unchecked
+          for (Map.Entry<String, String> entry : (Set<Map.Entry<String, String>>) arg.rslUrl().entrySet()) {
+            out.write("\n\t\t<rsl-url>");
+            out.write(entry.getKey());
+            out.write("</rsl-url>");
+
+            if (entry.getValue() != null) {
+              out.write("\n\t\t<policy-file-url>");
+              out.write(entry.getValue());
+              out.write("</policy-file-url>");
+            }
+          }
+
+          out.write("\n\t</");
+          out.write(name);
+          out.write('>');
+        }
+      }
+      else if (value instanceof IFlexArgument || value instanceof IFlexArgument[]) {
+        IFlexArgument[] values;
+        Class<?> type = returnType;
+        if (type.isArray()) {
+          //noinspection ConstantConditions
+          values = (IFlexArgument[]) value;
+          type = returnType.getComponentType();
+        }
+        else {
+          //noinspection ConstantConditions
+          values = new IFlexArgument[]{(IFlexArgument) value};
+          type = returnType;
+        }
+
+        for (IFlexArgument iFlexArgument : values) {
+          out.append(indent).append('<').append(name).append('>');
+
+          for (String argMethodName : (String[]) type.getField("ORDER").get(iFlexArgument)) {
+            Object argValue = type.getDeclaredMethod(argMethodName).invoke(iFlexArgument);
+            if (argValue == null) {
+              throw new UnsupportedOperationException();
+            }
+            else if (argValue instanceof Collection<?> || argValue.getClass().isArray()) {
+              throw new UnsupportedOperationException();
+            }
+            else if (argValue instanceof Map<?, ?>) {
+              throw new UnsupportedOperationException();
+//              Map<?, ?> map = ((Map<?, ?>) argValue);
+//              for (Object argValue1 : map.entrySet()) {
+//                @SuppressWarnings({"unchecked"}) Map.Entry<String, ?> entry = (Map.Entry<String, ?>) argValue1;
+//                out.append(indent).append("\t<").append(entry.getKey()).append('>').append(entry.getValue().toString()).append("</").append(entry.getKey()).append('>');
+//              }
+            }
+            else {
+              writeTag(indent, argMethodName, (String) argValue);
+            }
+          }
+
+          out.append(indent).append("</").append(name).append('>');
+        }
+      }
+      else if (configuration instanceof IMetadataConfiguration && (name.equals("language") || name.equals("creator") || name.equals("publisher"))) {
+        for (String v : (String[]) value) {
+          out.append(indent).append("<").append(name).append(">").append(v).append("</").append(name).append('>');
+        }
+      }
+      else if (returnType.isArray() || value instanceof Collection<?>) {
+        Object[] values;
+        if (returnType.isArray()) {
+          //noinspection ConstantConditions
+          values = (Object[]) value;
+        }
+        else {
+          values = ((Collection<?>) value).toArray();
+        }
+
+        out.append(indent).append('<').append(name);
+
+        // fucking adobe, ability to compile pure AS3 project without fucking themes — node must be present, but empty (relevant only for "theme", but we are ready for adobe surprises)
+        if (values.length == 0) {
+          out.write("/>");
+        }
+        else {
+          out.write('>');
+
+          String childTagName = childTagNameMap.get(name);
+          if (childTagName == null) {
+            childTagName = "path-element";
+          }
+
+          for (Object v : values) {
+            writeTag(indent, childTagName, v.toString());
+          }
+          out.append(indent).append("</").append(name).append('>');
+        }
+      }
+      else {
+        out.append(indent).append('<').append(name).append('>');
+        out.write(value.toString());
+        out.append("</").append(name).append('>');
+      }
+    }
+  }
+
+  private void writeTag(String indent, String name, String value) throws IOException {
+    out.append(indent).append("\t<").append(name).append(">").append(value).append("</").append(name).append('>');
+  }
+
+  // todo may be share this?
+  private static String parseName(String name) {
+    String[] nodes = StringUtil.splitCamelCase(StringUtil.removePrefix(name));
+
+    StringBuilder finalName = new StringBuilder();
+    for (String node : nodes) {
+      if (finalName.length() != 0) {
+        finalName.append('-');
+      }
+      finalName.append(node.toLowerCase());
+    }
+
+    return finalName.toString();
+  }
+}
