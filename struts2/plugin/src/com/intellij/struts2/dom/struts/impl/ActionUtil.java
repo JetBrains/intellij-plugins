3175b097b792b05d4f96bbf777e79561c15bdd8b
Yann Cebron <yann.cebron@googlemail.com>
2011-04-02 16:17:25 +0200

    IDEA-65327: relax exception signature requirements for action methods

--- struts2/plugin/src/com/intellij/struts2/dom/struts/impl/ActionUtil.java
+++ struts2/plugin/src/com/intellij/struts2/dom/struts/impl/ActionUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 The authors
+ * Copyright 2011 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,7 +17,6 @@ package com.intellij.struts2.dom.struts.impl;
 
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.*;
@@ -69,21 +68,18 @@ final class ActionUtil {
    * @return Methods suitable for action execution.
    */
   static List<PsiMethod> findActionMethods(@NotNull final PsiClass actionClass) {
-    final Project project = actionClass.getProject();
-    final PsiElementFactory psiElementFactory = JavaPsiFacade.getInstance(project).getElementFactory();
-
     final Module module = ModuleUtil.findModuleForPsiElement(actionClass);
     if (module == null) {
       return Collections.emptyList();
     }
+
     final GlobalSearchScope scope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module, false);
 
+    final PsiElementFactory psiElementFactory = JavaPsiFacade.getInstance(actionClass.getProject()).getElementFactory();
     final PsiClassType stringType = psiElementFactory.createTypeByFQClassName(CommonClassNames.JAVA_LANG_STRING,
                                                                               scope);
     final PsiClassType resultType = psiElementFactory.createTypeByFQClassName("com.opensymphony.xwork2.Result",
                                                                               scope);
-    final PsiClassType exceptionType = psiElementFactory.createTypeByFQClassName(CommonClassNames.JAVA_LANG_EXCEPTION,
-                                                                                 scope);
 
     final List<PsiMethod> actionMethods = new SmartList<PsiMethod>();
     for (final PsiMethod psiMethod : actionClass.getAllMethods()) {
@@ -114,18 +110,6 @@ final class ActionUtil {
       if (PropertyUtil.isSimplePropertyGetter(psiMethod) &&
           actionClass.findFieldByName(PropertyUtil.getPropertyName(psiMethod), true) != null) {
         continue;
-      }
-
-      // only "throws java.lang.Exception" or no throws at all
-      final PsiClassType[] exceptionTypes = psiMethod.getThrowsList().getReferencedTypes();
-      if (exceptionTypes.length > 1) {
-        continue;
-      }
-
-      if (exceptionTypes.length == 1) {
-        if (!exceptionTypes[0].equals(exceptionType)) {
-          continue;
-        }
       }
 
       // return type "java.lang.String" or "com.opensymphony.xwork2.Result"
--- struts2/plugin/testData/strutsXmlCompletion/src/ActionClass.java
+++ struts2/plugin/testData/strutsXmlCompletion/src/ActionClass.java
@@ -1,4 +1,19 @@
-public class ActionClass {
+/*
+ * Copyright 2011 The authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class A2ctionClass {
 
   private String myField;
 
@@ -21,10 +36,6 @@ public class ActionClass {
   // invalid action-method
   public String getMyField() {
     return myField;
-  }
-
-  public String invalidActionMethodDueToWrongExceptionType() throws IllegalStateException {
-    return null;
   }
 
   public boolean invalidActionMethodDueToWrongReturnType() throws Exception {
