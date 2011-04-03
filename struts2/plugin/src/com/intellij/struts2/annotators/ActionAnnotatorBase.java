eac877e18cc44d97bd61adabc98a18f43d04b09e
Yann Cebron <yann.cebron@googlemail.com>
2011-04-03 16:59:54 +0200

    "Go To Result": gutter icon to navigate to any result(s) directly from action method

--- struts2/plugin/src/com/intellij/struts2/annotators/ActionAnnotatorBase.java
+++ struts2/plugin/src/com/intellij/struts2/annotators/ActionAnnotatorBase.java
@@ -21,33 +21,44 @@ import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
 import com.intellij.ide.util.PsiElementListCellRenderer;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtil;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiModifier;
+import com.intellij.openapi.paths.PathReference;
+import com.intellij.psi.*;
 import com.intellij.struts2.StrutsBundle;
 import com.intellij.struts2.StrutsIcons;
 import com.intellij.struts2.dom.struts.action.Action;
+import com.intellij.struts2.dom.struts.action.Result;
 import com.intellij.struts2.dom.struts.model.StrutsManager;
 import com.intellij.struts2.dom.struts.model.StrutsModel;
 import com.intellij.struts2.facet.StrutsFacet;
+import com.intellij.util.NotNullFunction;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xml.DomElement;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Base class for annotating Action-Classes.
- * Provides gutter icon navigation to &lt;action&gt; declaration(s) in struts.xml.
+ * Provides gutter icon navigation to &lt;action&gt; declaration(s) in struts.xml and
+ * navigation to result(s) from action methods.
  *
  * @author Yann C&eacute;bron
  */
 abstract class ActionAnnotatorBase implements LineMarkerProvider {
 
   private static DomElementListCellRenderer ACTION_RENDERER;
+
+  private static final NotNullFunction<PathReference, Collection<? extends PsiElement>> PATH_REFERENCE_CONVERTER =
+    new NotNullFunction<PathReference, Collection<? extends PsiElement>>() {
+      @NotNull
+      @Override
+      public Collection<? extends PsiElement> fun(final PathReference pathReference) {
+        final PsiElement resolve = pathReference.resolve();
+        return resolve != null ? Collections.singletonList(resolve) : Collections.<PsiElement>emptyList();
+      }
+    };
 
   /**
    * Determine the Action-PsiClass.
@@ -113,6 +124,38 @@ abstract class ActionAnnotatorBase implements LineMarkerProvider {
 
       lineMarkerInfos.add(gutterIconBuilder.createLineMarkerInfo(element));
     }
+
+
+    // annotate action-methods with result(s)
+    final Map<PsiMethod, Set<PathReference>> pathReferenceMap = new HashMap<PsiMethod, Set<PathReference>>();
+    for (final Action action : actions) {
+      final PsiMethod method = action.searchActionMethod();
+      if (method == null) {
+        continue;
+      }
+
+      final Set<PathReference> pathReferences = new HashSet<PathReference>();
+      final List<Result> results = action.getResults();
+      for (final Result result : results) {
+        final PathReference pathReference = result.getValue();
+        ContainerUtil.addIfNotNull(pathReferences, pathReference);
+      }
+
+      final Set<PathReference> toStore = ContainerUtil.getOrCreate(pathReferenceMap, method, new HashSet<PathReference>());
+      toStore.addAll(pathReferences);
+      pathReferenceMap.put(method, toStore);
+    }
+
+    for (final Map.Entry<PsiMethod, Set<PathReference>> entries : pathReferenceMap.entrySet()) {
+      final NavigationGutterIconBuilder<PathReference> gutterIconBuilder =
+        NavigationGutterIconBuilder.create(StrutsIcons.RESULT, PATH_REFERENCE_CONVERTER)
+          .setPopupTitle(StrutsBundle.message("annotators.action.goto.result"))
+          .setTargets(entries.getValue())
+          .setTooltipTitle(StrutsBundle.message("annotators.action.goto.result.tooltip"));
+
+      lineMarkerInfos.add(gutterIconBuilder.createLineMarkerInfo(entries.getKey()));
+    }
+
   }
 
   private static synchronized PsiElementListCellRenderer getActionRenderer() {
