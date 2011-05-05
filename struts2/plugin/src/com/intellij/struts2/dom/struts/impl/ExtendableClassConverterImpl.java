d9b70ac921bbe853cbac81ddac6b42f7a4f8d545
Yann Cebron <yann.cebron@googlemail.com>
2011-05-05 20:58:53 +0200

    final

--- struts2/plugin/src/com/intellij/struts2/dom/struts/impl/ExtendableClassConverterImpl.java
+++ struts2/plugin/src/com/intellij/struts2/dom/struts/impl/ExtendableClassConverterImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2008 The authors
+ * Copyright 2011 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -11,7 +11,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package com.intellij.struts2.dom.struts.impl;
@@ -88,7 +87,7 @@ public class ExtendableClassConverterImpl extends ExtendableClassConverter {
     final JavaClassReferenceProvider classReferenceProvider =
         new JavaClassReferenceProvider() {
           @Override
-          public GlobalSearchScope getScope(Project project) {
+          public GlobalSearchScope getScope(final Project project) {
             return scope;
           }
         };
