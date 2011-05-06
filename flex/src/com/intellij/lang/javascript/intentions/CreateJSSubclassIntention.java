9a17f725b7d2639e11cbb4d45004d9d7e29e4b90
Kirill.Safonov <kirill.safonov@jetbrains.com>
2011-05-06 21:19:37 +0400

    ActionScript 'Create class from usage', 'Create subclass' actions implementation refactoring

--- flex/src/com/intellij/lang/javascript/intentions/CreateJSSubclassIntention.java
+++ flex/src/com/intellij/lang/javascript/intentions/CreateJSSubclassIntention.java
@@ -5,41 +5,33 @@ import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.javascript.JSTokenTypes;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
-import com.intellij.lang.javascript.flex.ECMAScriptImportOptimizer;
 import com.intellij.lang.javascript.flex.FlexBundle;
-import com.intellij.lang.javascript.flex.ImportUtils;
+import com.intellij.lang.javascript.generation.JSNamedElementNode;
 import com.intellij.lang.javascript.generation.JavaScriptImplementMethodsHandler;
 import com.intellij.lang.javascript.psi.JSFile;
 import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList;
 import com.intellij.lang.javascript.psi.ecmal4.JSClass;
 import com.intellij.lang.javascript.psi.ecmal4.JSPackageStatement;
 import com.intellij.lang.javascript.psi.ecmal4.JSReferenceList;
-import com.intellij.lang.javascript.psi.impl.JSPsiImplUtils;
 import com.intellij.lang.javascript.validation.fixes.CreateClassDialog;
 import com.intellij.lang.javascript.validation.fixes.CreateClassOrInterfaceAction;
+import com.intellij.lang.javascript.validation.fixes.ImplementMethodsFix;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.Computable;
-import com.intellij.openapi.util.NullableComputable;
-import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Consumer;
 import com.intellij.util.IncorrectOperationException;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
-import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class CreateJSSubclassIntention extends PsiElementBaseIntentionAction {
   private @NonNls static final String IMPL_SUFFIX = "Impl";
@@ -170,70 +162,19 @@ public class CreateJSSubclassIntention extends PsiElementBaseIntentionAction {
       templateName = dialog.getTemplateName();
       targetDirectory = dialog.getTargetDirectory();
     }
-    doCreateSubclass(jsClass, className, packageName, templateName, targetDirectory);
-  }
 
-  private void doCreateSubclass(final JSClass baseClass,
-                                final String className,
-                                final String packageName,
-                                final String templateName,
-                                final PsiDirectory targetDirectory) {
-    final Project project = baseClass.getProject();
-    final Ref<PsiElement> createdElementRef = new Ref<PsiElement>();
-
-    final String error = ApplicationManager.getApplication().runWriteAction(new NullableComputable<String>() {
-      public String compute() {
-        try {
-          final Properties additionalTemplateProperties = new Properties();
-          final String propName = baseClass.isInterface() ? "Implemented_interface_name" : "Super_class_name";
-          final String superClassQname = baseClass.getQualifiedName();
-          additionalTemplateProperties.setProperty(propName, superClassQname);
-          final PsiElement createdElement = CreateClassOrInterfaceAction
-            .createClass(className, packageName, targetDirectory, templateName, additionalTemplateProperties);
-
-          createdElementRef.set(createdElement);
-
-          final String superClassPackage = StringUtil.getPackageName(superClassQname);
-          final JSClass createdClass = JSPsiImplUtils.findClass((JSFile)createdElement);
-          if (!StringUtil.isEmpty(superClassPackage) && !superClassPackage.equals(packageName) && createdClass != null) {
-            ImportUtils.insertImportStatements(createdClass, Collections.singletonList(superClassQname));
-          }
-          new ECMAScriptImportOptimizer().processFile(createdElement.getContainingFile()).run();
-        }
-        catch (IncorrectOperationException e) {
-          return e.getMessage();
-        }
-        catch (Exception e) {
-          Logger.getInstance(getClass().getName()).error(e);
-        }
-        return null;
-      }
-    });
-
-    if (error != null) {
-      Messages.showErrorDialog(project, error, getTitle(baseClass));
-      return;
-    }
-
-    final PsiElement createdElement = createdElementRef.get();
-    final VirtualFile createdFile = createdElement == null ? null : createdElement.getContainingFile().getVirtualFile();
-    if (createdFile != null) {
-      final Editor newEditor = FileEditorManager.getInstance(project).openTextEditor(new OpenFileDescriptor(project, createdFile), true);
-      if (createdElement instanceof JSFile && newEditor != null) {
-        final JSClass createdClass = JSPsiImplUtils.findClass((JSFile)createdElement);
-        if (createdClass != null) {
-          newEditor.getCaretModel().moveToOffset(createdClass.getTextOffset());
-
-          if (baseClass.isInterface()) {
-            final JavaScriptImplementMethodsHandler implementMethodsHandler = new JavaScriptImplementMethodsHandler();
-            implementMethodsHandler.setSkipMemberChooserDialog(true);
-            implementMethodsHandler.invoke(project, newEditor, createdElement.getContainingFile());
-          }
-          else {
-            CreateClassOrInterfaceAction.makeSureConstructorMatchesSuper(createdClass);
+    JSClass createdClass = CreateClassOrInterfaceAction
+      .createClass(templateName, className, packageName, jsClass, targetDirectory, getTitle(jsClass), new Consumer<JSClass>() {
+        @Override
+        public void consume(final JSClass aClass) {
+          if (!aClass.isInterface() && jsClass.isInterface()) {
+            new MyImplementMethodsHandler(aClass).execute();
           }
         }
-      }
+      });
+
+    if (createdClass != null) {
+      createdClass.navigate(true);
     }
   }
 
@@ -243,5 +184,21 @@ public class CreateJSSubclassIntention extends PsiElementBaseIntentionAction {
 
   private static String suggestSubclassName(final String name) {
     return name + IMPL_SUFFIX;
+  }
+
+  private static class MyImplementMethodsHandler extends JavaScriptImplementMethodsHandler {
+    private final JSClass myClass;
+
+    public MyImplementMethodsHandler(JSClass aClass) {
+      myClass = aClass;
+    }
+
+    public void execute() {
+      Collection<JSNamedElementNode> candidates = new ArrayList<JSNamedElementNode>();
+      collectCandidates(myClass, candidates);
+      ImplementMethodsFix fix = new ImplementMethodsFix(myClass);
+      fix.addElementsToProcessFrom(candidates);
+      fix.invoke(myClass.getProject(), null, myClass.getContainingFile());
+    }
   }
 }
