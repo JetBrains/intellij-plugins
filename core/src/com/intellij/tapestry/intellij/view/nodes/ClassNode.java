0fedd3766435fe689b2e644f65814a94ce599f88
Konstantin Bulenkov <kb@jetbrains.com>
2011-06-22 14:50:13 +0400

    platform icons

--- core/src/com/intellij/tapestry/intellij/view/nodes/ClassNode.java
+++ core/src/com/intellij/tapestry/intellij/view/nodes/ClassNode.java
@@ -4,13 +4,13 @@ import com.intellij.ide.projectView.PresentationData;
 import com.intellij.ide.util.treeView.AbstractTreeBuilder;
 import com.intellij.openapi.module.Module;
 import com.intellij.psi.PsiClassOwner;
-import com.intellij.util.Icons;
+import com.intellij.util.PlatformIcons;
 
 public class ClassNode extends TapestryNode {
 
     public ClassNode(PsiClassOwner psiClassOwner, Module module, AbstractTreeBuilder treeBuilder) {
         super(module, treeBuilder);
 
-        init(psiClassOwner, new PresentationData(psiClassOwner.getName(), psiClassOwner.getName(), Icons.CLASS_ICON, Icons.CLASS_ICON, null));
+        init(psiClassOwner, new PresentationData(psiClassOwner.getName(), psiClassOwner.getName(), PlatformIcons.CLASS_ICON, PlatformIcons.CLASS_ICON, null));
     }
 }
