d7de9c75a6acadf47d9e14fceddae70fb9d6b2c5
Kirill.Safonov <kirill.safonov@jetbrains.com>
2011-07-08 16:39:58 +0400

    Flex IDE: show app icon for internal file types (IDEA-71776)

---
+++ flex/ideSrc/com/intellij/flexIde/FlexIdeIconProvider.java
@@ -0,0 +1,29 @@
+package com.intellij.flexIde;
+
+import com.intellij.ide.FileIconProvider;
+import com.intellij.ide.highlighter.InternalFileType;
+import com.intellij.openapi.application.ex.ApplicationInfoEx;
+import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.IconLoader;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class FlexIdeIconProvider implements FileIconProvider, DumbAware {
+
+  private static final Icon ICON = IconLoader.findIcon(ApplicationInfoEx.getInstanceEx().getSmallIconUrl());
+
+  @Nullable
+  @Override
+  public Icon getIcon(VirtualFile file, int flags, @Nullable Project project) {
+    FileType fileType = file.getFileType();
+    if (fileType instanceof InternalFileType) {
+      return ICON;
+    }
+    return null;
+  }
+
+}
