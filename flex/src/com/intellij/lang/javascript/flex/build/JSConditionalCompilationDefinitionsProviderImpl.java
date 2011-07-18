0b534511a2703e5dfa91825d212f06b37a888be0
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2011-07-18 18:18:12 +0400

    IDEA-72195 Support conditional compiling variables set as Additional compiler options using space separator (e.d. -define CONFIG::DEBUG true)

--- flex/src/com/intellij/lang/javascript/flex/build/JSConditionalCompilationDefinitionsProviderImpl.java
+++ flex/src/com/intellij/lang/javascript/flex/build/JSConditionalCompilationDefinitionsProviderImpl.java
@@ -9,14 +9,17 @@ import com.intellij.openapi.util.Ref;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Function;
+import com.intellij.util.Processor;
 import gnu.trove.THashMap;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
 
 import static com.intellij.lang.javascript.flex.build.FlexBuildConfiguration.ConditionalCompilationDefinition;
 import static com.intellij.lang.javascript.flex.build.FlexCompilerConfigFileUtil.*;
@@ -32,14 +35,14 @@ public class JSConditionalCompilationDefinitionsProviderImpl implements JSCondit
   public boolean containsConstant(final Module module, final String namespace, final String constantName) {
     if (module != null && !StringUtil.isEmpty(namespace) && !StringUtil.isEmpty(constantName)) {
       final boolean searchForPrefix = constantName.indexOf(CompletionUtil.DUMMY_IDENTIFIER_TRIMMED) != -1;
-      final String searchedName = namespace + "::" + (searchForPrefix ? "":constantName);
+      final String searchedName = namespace + "::" + (searchForPrefix ? "" : constantName);
       final Ref<Boolean> result = new Ref<Boolean>(false);
 
-      iterateConditionalCompilationDefinitions(module, new Function<ConditionalCompilationDefinition, Boolean>() {
-        public Boolean fun(final ConditionalCompilationDefinition definition) {
+      processConditionalCompilationDefinitions(module, new Processor<ConditionalCompilationDefinition>() {
+        public boolean process(final ConditionalCompilationDefinition definition) {
           if ((searchForPrefix && definition.NAME.startsWith(searchedName)) ||
               (!searchForPrefix && definition.NAME.equals(searchedName))
-             ) {
+            ) {
             result.set(true);
             return false;
           }
@@ -58,8 +61,8 @@ public class JSConditionalCompilationDefinitionsProviderImpl implements JSCondit
     if (module != null && !StringUtil.isEmpty(namespace)) {
       final String beginning = namespace + "::";
 
-      iterateConditionalCompilationDefinitions(module, new Function<ConditionalCompilationDefinition, Boolean>() {
-        public Boolean fun(final ConditionalCompilationDefinition definition) {
+      processConditionalCompilationDefinitions(module, new Processor<ConditionalCompilationDefinition>() {
+        public boolean process(final ConditionalCompilationDefinition definition) {
           if (definition.NAME.startsWith(beginning)) {
             result.add(definition.NAME.substring(beginning.length()));
           }
@@ -75,8 +78,8 @@ public class JSConditionalCompilationDefinitionsProviderImpl implements JSCondit
     final Collection<String> result = new ArrayList<String>();
 
     if (module != null) {
-      iterateConditionalCompilationDefinitions(module, new Function<ConditionalCompilationDefinition, Boolean>() {
-        public Boolean fun(final ConditionalCompilationDefinition definition) {
+      processConditionalCompilationDefinitions(module, new Processor<ConditionalCompilationDefinition>() {
+        public boolean process(final ConditionalCompilationDefinition definition) {
           result.add(definition.NAME);
           return true;
         }
@@ -86,19 +89,18 @@ public class JSConditionalCompilationDefinitionsProviderImpl implements JSCondit
     return result;
   }
 
-  private void iterateConditionalCompilationDefinitions(final Module module,
-                                                        final Function<ConditionalCompilationDefinition, Boolean> iterator) {
+  private void processConditionalCompilationDefinitions(final Module module, final Processor<ConditionalCompilationDefinition> processor) {
     outer:
     for (FlexBuildConfiguration config : FlexBuildConfiguration.getConfigForFlexModuleOrItsFlexFacets(module)) {
       final Collection<ConditionalCompilationDefinition> definitions = config.USE_CUSTOM_CONFIG_FILE
                                                                        ? getDefinitionsFromConfigFile(config.CUSTOM_CONFIG_FILE)
                                                                        : config.CONDITIONAL_COMPILATION_DEFINITION_LIST;
       for (ConditionalCompilationDefinition definition : definitions) {
-        if (!iterator.fun(definition)) break outer;
+        if (!processor.process(definition)) break outer;
       }
 
       for (ConditionalCompilationDefinition definition : getDefinitionsFromCompilerOptions(config.ADDITIONAL_COMPILER_OPTIONS)) {
-        if (!iterator.fun(definition)) break outer;
+        if (!processor.process(definition)) break outer;
       }
     }
   }
@@ -160,7 +162,7 @@ public class JSConditionalCompilationDefinitionsProviderImpl implements JSCondit
 
     final Collection<ConditionalCompilationDefinition> result = new ArrayList<ConditionalCompilationDefinition>();
 
-    for (StringTokenizer stringTokenizer = new CommandLineTokenizer(compilerOptions); stringTokenizer.hasMoreTokens();) {
+    for (CommandLineTokenizer stringTokenizer = new CommandLineTokenizer(compilerOptions); stringTokenizer.hasMoreTokens(); ) {
       final String token = stringTokenizer.nextToken();
       for (String option : CONDITIONAL_COMPILATION_DEFINITION_OPTION_ALIASES) {
         if (token.startsWith("-" + option + "=") || token.startsWith("-" + option + "+=")) {
@@ -176,9 +178,33 @@ public class JSConditionalCompilationDefinitionsProviderImpl implements JSCondit
             }
           }
         }
+        else if (token.equals("-" + option)) {
+          if (stringTokenizer.countTokens() >= 2) {
+            final String name = stringTokenizer.peekNextToken();
+            if (name.matches(FlexCompiler.CONDITIONAL_COMPILATION_VARIABLE_PATTERN)) {
+              stringTokenizer.nextToken(); // advance tokenizer position
+              final String value = stringTokenizer.peekNextToken();
+              if (isOptionValue(value)) {
+                stringTokenizer.nextToken(); // advance tokenizer position
+
+                final ConditionalCompilationDefinition definition = new ConditionalCompilationDefinition();
+                definition.NAME = name;
+                definition.VALUE = value;
+                result.add(definition);
+              }
+            }
+          }
+        }
       }
     }
 
     return result;
+  }
+
+  private static boolean isOptionValue(final String text) {
+    if (text.startsWith("-")) {  // option or negative number
+      return text.length() > 1 && Character.isDigit(text.charAt(1));
+    }
+    return !text.startsWith("+");
   }
 }
