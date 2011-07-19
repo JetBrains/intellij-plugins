28c852f71d8f24979c8f4277b1cd26625a1b0a69
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2011-07-19 18:44:03 +0400

    IDEA-57546 Impossible to set breakpoint in MXML files that contain item renderer

--- flex/tools/idea-fdb-fix/idea_fdb_3_fix/src/flex/tools/debugger/cli/DebugCLI.java
+++ flex/tools/idea-fdb-fix/idea_fdb_3_fix/src/flex/tools/debugger/cli/DebugCLI.java
@@ -3314,21 +3314,37 @@ public class DebugCLI implements Runnable, SourceLocator
 	 *			otherwise try to locate a matching source file in given swf.
 	 * @return null if the swf does not contain this source file
 	 */
-	Location findAndEnableBreak(SwfInfo swf, SourceFile f, int line) throws NotConnectedException, InProgressException
-	{
-		int fileId = f.getId();
-		if (swf != null)
-		{
-			SourceFile sameFile = m_fileInfo.similarFileInSwf(swf, f);
-			if (sameFile != null)
-				fileId = sameFile.getId();
-			else 
-				fileId = -1;
-		}
+    private Location findAndEnableBreak(final SwfInfo swf, final SourceFile file, final int line) throws NotConnectedException,
+                                                                                                         InProgressException {
+      if (swf == null) {
+        return breakEnableRequest(file.getId(), line);
+      }
 
-		Location l = (fileId > -1) ? breakEnableRequest(fileId, line) : null;
-		return l;
-	}
+      for (final SourceFile similarFile : getSimilarSourceFilesInSwf(swf, file)) {
+        final Location location = breakEnableRequest(similarFile.getId(), line);
+        if (location != null) {
+          return location;
+        }
+      }
+
+      return null;
+    }
+
+  private List<SourceFile> getSimilarSourceFilesInSwf(final SwfInfo info, final SourceFile file) throws InProgressException {
+    if (!info.isProcessingComplete()) {
+      throw new InProgressException();
+    }
+
+    final List<SourceFile> result = new LinkedList<SourceFile>();
+
+    for (final SourceFile each : info.getSourceList(m_session)) {
+      if (m_fileInfo.filesMatch(file, each)) {
+        result.add(each);
+      }
+    }
+
+    return result;
+  }
 
 	/**
 	 * Received when a breakpoint has been removed (or disabled)
