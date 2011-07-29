f53f5d1b6464ab4bda9ebc88fb3e13f1f1a9883f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2011-07-29 22:32:20 +0400

    fix IDEA-72575, but example is not working in any case — FTETextField — WTF? TypeError: Error #1034: Type Coercion failed: cannot convert TopLevelSystemManager@f443c59 to mx.managers.SystemManager.

--- flex/tools/flex-ui-designer/idea-plugin/src/com/intellij/flex/uiDesigner/mxml/MxmlWriter.java
+++ flex/tools/flex-ui-designer/idea-plugin/src/com/intellij/flex/uiDesigner/mxml/MxmlWriter.java
@@ -37,7 +37,7 @@ import static com.intellij.flex.uiDesigner.mxml.PropertyProcessor.IGNORE;
 import static com.intellij.flex.uiDesigner.mxml.PropertyProcessor.PRIMITIVE;
 
 public class MxmlWriter {
-  private static final Pattern FLEX_SDK_ABSTRACT_CLASSES = Pattern.compile("^(mx|spark)\\.(.*)?[^\\.]+Classes\\.[^.]+$");
+  private static final Pattern FLEX_SDK_ABSTRACT_CLASSES = Pattern.compile("^(mx|spark)\\.(.*)?Base$");
 
   static final int EMPTY_CLASS_OR_PROPERTY_NAME = 0;
 
@@ -379,7 +379,7 @@ public class MxmlWriter {
     }
   }
 
-  private boolean isProjectComponent(ClassBackedElementDescriptor descriptor) {
+  private static boolean isProjectComponent(ClassBackedElementDescriptor descriptor) {
     final JSClass jsClass = (JSClass)descriptor.getDeclaration();
     assert jsClass != null;
     PsiFile psiFile = jsClass.getContainingFile();
