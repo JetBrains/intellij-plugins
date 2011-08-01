d06f74327c09252d5c2ff7340ea41c23e4e6c3a0
Kirill.Safonov <kirill.safonov@jetbrains.com>
2011-08-01 16:13:23 +0400

    Flex UI Deisgner: correct heuristics for debug paths

--- flex/tools/flex-ui-designer/idea-plugin/src/com/intellij/flex/uiDesigner/DebugPathManager.java
+++ flex/tools/flex-ui-designer/idea-plugin/src/com/intellij/flex/uiDesigner/DebugPathManager.java
@@ -20,52 +20,62 @@ public final class DebugPathManager {
   private static String fudHome;
 
   static {
+    ideaHome = getRootByClass(DebugPathManager.class);
     Application app = ApplicationManager.getApplication();
     if (app == null) {
-      IS_DEV = true; // e.g. SwcDependenciesSorter.main() is running
+      // running SwcDependenciesSorter
+      assert ideaHome != null;
+      fudHome = ideaHome + FLEX_TOOLS_FLEX_UI_DESIGNER;
+      IS_DEV = true; // not used actually
+      ADL_EXECUTABLE = null; // not used
+      ADL_RUNTIME = null; // not used
     }
     else if (app.isUnitTestMode()) {
+      // running tests
       IS_DEV = true;
-    }
-    else {
-      IS_DEV = !"jar".equals(DebugPathManager.class.getResource("").getProtocol());
-    }
-
-    if (app != null && app.isUnitTestMode()) {
-      // classpath contains classes from the IDEA project
-      ideaHome = getRootByClass(DebugPathManager.class);
+      assert ideaHome != null;
       fudHome = ideaHome + FLEX_TOOLS_FLEX_UI_DESIGNER;
+
+      String adlExecutable = System.getProperty("adl.executable");
+      if (adlExecutable == null) {
+        if (SystemInfo.isMac) {
+          adlExecutable = "/Developer/SDKs/flex_4.5.1/bin/adl";
+        }
+        else {
+          throw new IllegalStateException("Please define 'adl.executable' to point to ADL executable");
+        }
+      }
+      ADL_EXECUTABLE = adlExecutable;
+
+      String adlRuntime = System.getProperty("adl.runtime");
+      if (adlRuntime == null) {
+        if (SystemInfo.isMac) {
+          adlRuntime = "/Library/Frameworks";
+        }
+        else {
+          throw new IllegalStateException("Please define 'adl.runtime' to point to ADL runtime");
+        }
+      }
+      ADL_RUNTIME = adlRuntime;
+    }
+    else if (ideaHome != null) {
+      // running bundled product, or from IDEA project
+      IS_DEV = false;
+      ADL_EXECUTABLE = null; // not used
+      ADL_RUNTIME = null; // not used
+      ideaHome = null; // not used
+      fudHome = null; // not used
     }
     else {
-      // classpath contains classes from plugins sandbox
+      // running from flex-ui-designer plugin project
+      IS_DEV = true;
       fudHome = System.getProperty("fud.home");
       if (fudHome == null) {
         throw new IllegalStateException("Please define 'fud.home' to point to 'IDEA" + FLEX_TOOLS_FLEX_UI_DESIGNER + "' folder");
       }
-      ideaHome = null; // we need it only in tests
+      ADL_EXECUTABLE = null; // not used
+      ADL_RUNTIME = null; // not used
     }
-
-    String adlExecutable = System.getProperty("adl.executable");
-    if (adlExecutable == null) {
-      if (SystemInfo.isMac) {
-        adlExecutable = "/Developer/SDKs/flex_4.5.1/bin/adl";
-      }
-      else {
-        throw new IllegalStateException("Please define 'adl.executable' to point to ADL executable");
-      }
-    }
-    ADL_EXECUTABLE = adlExecutable;
-
-    String adlRuntime = System.getProperty("adl.runtime");
-    if (adlRuntime == null) {
-      if (SystemInfo.isMac) {
-        adlRuntime = "/Library/Frameworks";
-      }
-      else {
-        throw new IllegalStateException("Please define 'adl.runtime' to point to ADL runtime");
-      }
-    }
-    ADL_RUNTIME = adlRuntime;
   }
 
   public static String getIdeaHome() {
