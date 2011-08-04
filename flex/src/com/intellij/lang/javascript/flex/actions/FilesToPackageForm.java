e99f5a6299495e02ff6ba703b517dd7cd1ad068d
Evgeny Zakrevsky <evgeny.zakrevsky@jetbrains.com>
2011-08-04 17:41:12 +0400

    border's font switched from Bold to Simple and some refactor

--- flex/src/com/intellij/lang/javascript/flex/actions/FilesToPackageForm.java
+++ flex/src/com/intellij/lang/javascript/flex/actions/FilesToPackageForm.java
@@ -180,7 +180,7 @@ public class FilesToPackageForm {
   }
 
   public void setPanelTitle(final String title) {
-    myMainPanel.setBorder(IdeBorderFactory.createTitledBorderBoldWithIndent(title));
+    myMainPanel.setBorder(IdeBorderFactory.createTitledBorderSimpleWithIndent(title));
   }
 
   public void stopEditing() {
