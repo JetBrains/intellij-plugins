a0e7a03c32e304224997a883fd3b2def0bdee383
Kirill.Safonov <kirill.safonov@jetbrains.com>
2011-08-29 15:01:09 +0400

    ensure BC name is unique in model

--- flex/src/com/intellij/lang/javascript/flex/projectStructure/FlexIdeBuildConfigurationManager.java
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/FlexIdeBuildConfigurationManager.java
@@ -10,16 +10,20 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.project.ModuleAdapter;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.xmlb.annotations.AbstractCollection;
 
 import java.util.*;
 
 @State(name = "FlexIdeBuildConfigurationManager", storages = {@Storage(file = "$MODULE_FILE$")})
 public class FlexIdeBuildConfigurationManager implements PersistentStateComponent<FlexIdeBuildConfigurationManager.State> {
+
+  private static final Logger LOG = Logger.getInstance(FlexIdeBuildConfigurationManager.class.getName());
 
   private final Module myModule;
   private FlexIdeBuildConfiguration[] myConfigurations = new FlexIdeBuildConfiguration[]{new FlexIdeBuildConfiguration()};
@@ -67,7 +71,7 @@ public class FlexIdeBuildConfigurationManager implements PersistentStateComponen
   void setBuildConfigurations(final FlexIdeBuildConfiguration[] configurations) {
     assert configurations.length > 0;
     ApplicationManager.getApplication().assertWriteAccessAllowed();
-    myConfigurations = Arrays.copyOf(configurations, configurations.length);
+    myConfigurations = getValidatedConfigurations(Arrays.asList(configurations));
   }
 
   public State getState() {
@@ -82,12 +86,27 @@ public class FlexIdeBuildConfigurationManager implements PersistentStateComponen
       myConfigurations = new FlexIdeBuildConfiguration[]{new FlexIdeBuildConfiguration()};
     }
     else {
-      myConfigurations = state.myConfigurations.toArray(new FlexIdeBuildConfiguration[state.myConfigurations.size()]);
+      myConfigurations = getValidatedConfigurations(state.myConfigurations);
       for (FlexIdeBuildConfiguration configuration : myConfigurations) {
         configuration.initialize(myModule.getProject());
       }
     }
     myModuleLevelCompilerOptions = state.myModuleLevelCompilerOptions.clone();
+  }
+
+  private static FlexIdeBuildConfiguration[] getValidatedConfigurations(Collection<FlexIdeBuildConfiguration> configurations) {
+    LinkedHashMap<String, FlexIdeBuildConfiguration> name2configuration =
+      new LinkedHashMap<String, FlexIdeBuildConfiguration>(configurations.size());
+    for (FlexIdeBuildConfiguration configuration : configurations) {
+      if (StringUtil.isEmpty(configuration.NAME)) {
+        LOG.error("Empty build configuration name");
+      }
+      if (name2configuration.put(configuration.NAME, configuration) != null) {
+        LOG.error("Duplicate build configuration name: " + configuration.NAME);
+      }
+    }
+
+    return name2configuration.values().toArray(new FlexIdeBuildConfiguration[name2configuration.size()]);
   }
 
   public static class State {
