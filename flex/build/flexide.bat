385ed5a6c2a7fdd9606bc0ac7f23500d58ca3fc1
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2011-08-30 14:52:29 +0200

    .bat scripts fixed and unified

--- flex/build/flexide.bat
+++ flex/build/flexide.bat
@@ -1,52 +1,56 @@
-@echo off
+@ECHO OFF
 
 ::----------------------------------------------------------------------
 :: Flex IDE Startup Script
 ::----------------------------------------------------------------------
 
 :: ---------------------------------------------------------------------
-:: Before you run Flex IDE specify the location of the
-:: JDK 1.5 installation directory which will be used for running Flex IDE
+:: Location of the JDK 1.6 installation directory
+:: which will be used for running the IDE.
 :: ---------------------------------------------------------------------
-IF "%FLEXIDE_JDK%" == "" SET FLEXIDE_JDK=%JDK_HOME%
-IF "%FLEXIDE_JDK%" == "" goto error
+SET JDK=%FLEXIDE_JDK%
+IF "%JDK%" == "" SET JDK=%JDK_HOME%
+IF "%JDK%" == "" GOTO error
+
+SET JAVA_EXE=%JDK%\jre\bin\java.exe
+IF NOT EXIST "%JAVA_EXE%" GOTO error
 
 :: ---------------------------------------------------------------------
-:: Before you run Flex IDE specify the location of the
-:: directory where Flex IDE is installed
+:: Location of the directory where the IDE is installed
 :: In most cases you do not need to change the settings below.
 :: ---------------------------------------------------------------------
-SET FLEXIDE_HOME=..
+SET IDE_BIN_DIR=%~dp0
+SET IDE_HOME=%IDE_BIN_DIR%\..
 
-SET JAVA_EXE=%FLEXIDE_JDK%\jre\bin\java.exe
-IF NOT EXIST "%JAVA_EXE%" goto error
+SET MAIN_CLASS_NAME=%FLEXIDE_MAIN_CLASS_NAME%
+IF "%MAIN_CLASS_NAME%" == "" SET MAIN_CLASS_NAME=com.intellij.idea.Main
 
-IF "%FLEXIDE_MAIN_CLASS_NAME%" == "" SET FLEXIDE_MAIN_CLASS_NAME=com.intellij.idea.Main
-
-IF NOT "%FLEXIDE_PROPERTIES%" == "" set FLEXIDE_PROPERTIES_PROPERTY="-Didea.properties.file=%FLEXIDE_PROPERTIES%"
+IF NOT "%FLEXIDE_PROPERTIES%" == "" SET IDE_PROPERTIES_PROPERTY="-Didea.properties.file=%FLEXIDE_PROPERTIES%"
 
 :: ---------------------------------------------------------------------
-:: You may specify your own JVM arguments in flexide.exe.vmoptions file. Put one option per line there.
+:: You may specify your own JVM arguments in .vmoptions file.
+:: Put one option per line there.
 :: ---------------------------------------------------------------------
+SET VM_OPTIONS_FILE=%IDE_BIN_DIR%\flexide.exe.vmoptions
 SET ACC=
-FOR /F "delims=" %%i in (%FLEXIDE_HOME%\bin\flexide.exe.vmoptions) DO call %FLEXIDE_HOME%\bin\append.bat "%%i"
+FOR /F "delims=" %%i IN (%VM_OPTIONS_FILE%) DO CALL %IDE_BIN_DIR%\append.bat "%%i"
 
-set REQUIRED_FLEXIDE_JVM_ARGS=-Xbootclasspath/a:%FLEXIDE_HOME%/lib/boot.jar -Didea.platform.prefix=Flex -Didea.no.jre.check=true -Didea.paths.selector=@@system_selector@@ %FLEXIDE_PROPERTIES_PROPERTY% %REQUIRED_FLEXIDE_JVM_ARGS%
-SET JVM_ARGS=%ACC% %REQUIRED_FLEXIDE_JVM_ARGS%
+SET REQUIRED_JVM_ARGS=-Xbootclasspath/a:%IDE_HOME%/lib/boot.jar -Didea.paths.selector=@@system_selector@@ %IDE_PROPERTIES_PROPERTY%
+SET SPECIAL_JVM_ARGS=-Didea.platform.prefix=Flex -Didea.no.jre.check=true
+SET JVM_ARGS=%ACC% %REQUIRED_JVM_ARGS% %SPECIAL_JVM_ARGS% %REQUIRED_FLEXIDE_JVM_ARGS%
 
 SET OLD_PATH=%PATH%
-SET PATH=%FLEXIDE_HOME%\bin;%PATH%
+SET PATH=%IDE_BIN_DIR%;%PATH%
 
-SET CLASS_PATH=%FLEXIDE_HOME%\lib\bootstrap.jar
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_HOME%\lib\util.jar
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_HOME%\lib\jdom.jar
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_HOME%\lib\log4j.jar
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_HOME%\lib\extensions.jar
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_HOME%\lib\trove4j.jar
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_HOME%\lib\jna.jar
-
+SET CLASS_PATH=%IDE_HOME%\lib\bootstrap.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\util.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\jdom.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\log4j.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\extensions.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\trove4j.jar
+SET CLASS_PATH=%CLASS_PATH%;%IDE_HOME%\lib\jna.jar
 :: TODO[yole]: remove
-SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_JDK%\lib\tools.jar
+SET CLASS_PATH=%CLASS_PATH%;%JDK%\lib\tools.jar
 
 :: ---------------------------------------------------------------------
 :: You may specify additional class paths in FLEXIDE_CLASS_PATH variable.
@@ -54,14 +58,16 @@ SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_JDK%\lib\tools.jar
 :: ---------------------------------------------------------------------
 IF NOT "%FLEXIDE_CLASS_PATH%" == "" SET CLASS_PATH=%CLASS_PATH%;%FLEXIDE_CLASS_PATH%
 
-"%JAVA_EXE%" %JVM_ARGS% -cp "%CLASS_PATH%" %FLEXIDE_MAIN_CLASS_NAME% %*
+"%JAVA_EXE%" %JVM_ARGS% -cp "%CLASS_PATH%" %MAIN_CLASS_NAME% %*
 
 SET PATH=%OLD_PATH%
-goto end
+GOTO end
+
 :error
-echo ---------------------------------------------------------------------
-echo ERROR: cannot start Flex IDE.
-echo No JDK found to run Flex IDE. Please validate either FLEXIDE_JDK or JDK_HOME points to valid JDK installation
-echo ---------------------------------------------------------------------
-pause
+ECHO ---------------------------------------------------------------------
+ECHO ERROR: cannot start Flex IDE.
+ECHO No JDK found. Please validate either FLEXIDE_JDK or JDK_HOME points to valid JDK installation.
+ECHO ---------------------------------------------------------------------
+PAUSE
+
 :end
