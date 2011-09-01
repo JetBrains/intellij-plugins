e2e8d324ce5be494795360a08e6b629163371a68
Kirill.Safonov <kirill.safonov@jetbrains.com>
2011-09-01 12:24:32 +0400

    choose BC dialog: use simple JTree

--- flex/src/com/intellij/lang/javascript/flex/projectStructure/ui/ChooseBuildConfigurationDialog.java
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/ui/ChooseBuildConfigurationDialog.java
@@ -4,20 +4,19 @@ import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.util.Disposer;
-import com.intellij.ui.ScrollPaneFactory;
-import com.intellij.ui.treeStructure.SimpleNode;
-import com.intellij.ui.treeStructure.SimpleTree;
-import com.intellij.ui.treeStructure.SimpleTreeBuilder;
-import com.intellij.ui.treeStructure.SimpleTreeStructure;
+import com.intellij.ui.*;
+import com.intellij.ui.treeStructure.Tree;
 import com.intellij.util.Function;
 import com.intellij.util.containers.ContainerUtil;
+import com.intellij.util.containers.Convertor;
+import com.intellij.util.ui.tree.TreeUtil;
 
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
 import javax.swing.tree.TreeSelectionModel;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
@@ -26,21 +25,17 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author ksafonov
  */
 public class ChooseBuildConfigurationDialog extends DialogWrapper {
-  private final Project myProject;
-  private SimpleTree myTree;
-  private SimpleTreeBuilder myTreeBuilder;
   private final Map<Module, List<FlexIdeBCConfigurable>> myTreeItems;
-  private Set<BCNode> mySelection;
+  private Tree myTree;
+  private DefaultMutableTreeNode[] mySelection;
 
   public ChooseBuildConfigurationDialog(Project project, Map<Module, List<FlexIdeBCConfigurable>> treeItems) {
     super(project, true);
-    myProject = project;
     myTreeItems = treeItems;
     setTitle("Add Dependency");
     init();
@@ -58,26 +53,62 @@ public class ChooseBuildConfigurationDialog extends DialogWrapper {
 
   @Override
   protected JComponent createCenterPanel() {
-    SimpleNode root = new RootNode(myProject);
-    final DefaultTreeModel treeModel = new DefaultTreeModel(new DefaultMutableTreeNode());
-    myTree = new SimpleTree(treeModel);
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+    for (Module module : myTreeItems.keySet()) {
+      DefaultMutableTreeNode moduleNode = new DefaultMutableTreeNode(module, true);
+      root.add(moduleNode);
+      for (FlexIdeBCConfigurable bc : myTreeItems.get(module)) {
+        DefaultMutableTreeNode bcNode = new DefaultMutableTreeNode(bc, false);
+        moduleNode.add(bcNode);
+      }
+    }
+    myTree = new Tree(new DefaultTreeModel(root));
     myTree.setRootVisible(false);
+    new TreeSpeedSearch(myTree, new Convertor<TreePath, String>() {
+      @Override
+      public String convert(TreePath o) {
+        Object lastPathComponent = o.getLastPathComponent();
+        return getText((DefaultMutableTreeNode)lastPathComponent);
+      }
+    }, true).setComparator(new SpeedSearchBase.SpeedSearchComparator(false));
+    TreeUIHelper.getInstance().installTreeSpeedSearch(myTree);
     myTree.getSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
-    SimpleTreeStructure s = new SimpleTreeStructure.Impl(root);
-    myTreeBuilder = new SimpleTreeBuilder(myTree, treeModel, s, null);
-    Disposer.register(getDisposable(), myTreeBuilder);
-    myTree.setModel(treeModel);
     myTree.getSelectionModel().addTreeSelectionListener(new TreeSelectionListener() {
       @Override
       public void valueChanged(TreeSelectionEvent e) {
-        mySelection = myTreeBuilder.getSelectedElements(BCNode.class);
-        setOKActionEnabled(!mySelection.isEmpty());
+        mySelection = myTree.getSelectedNodes(DefaultMutableTreeNode.class, new Tree.NodeFilter<DefaultMutableTreeNode>() {
+          @Override
+          public boolean accept(DefaultMutableTreeNode node) {
+            return node.getUserObject() instanceof FlexIdeBCConfigurable;
+          }
+        });
+        setOKActionEnabled(mySelection.length > 0);
+      }
+    });
+    myTree.setCellRenderer(new ColoredTreeCellRenderer() {
+      @Override
+      public void customizeCellRenderer(JTree tree,
+                                        Object value,
+                                        boolean selected,
+                                        boolean expanded,
+                                        boolean leaf,
+                                        int row,
+                                        boolean hasFocus) {
+        DefaultMutableTreeNode treeNode = (DefaultMutableTreeNode)value;
+        append(getText(treeNode));
+        Object object = treeNode.getUserObject();
+        if (object instanceof Module) {
+          setIcon(ModuleType.get((Module)object).getNodeIcon(expanded));
+        }
+        else {
+          setIcon(((FlexIdeBCConfigurable)object).getIcon());
+        }
       }
     });
 
     JPanel p = new JPanel(new BorderLayout());
     p.add(ScrollPaneFactory.createScrollPane(myTree), BorderLayout.CENTER);
-    myTreeBuilder.expandAll(null);
+    TreeUtil.expandAll(myTree);
     myTree.addMouseListener(new MouseAdapter() {
       @Override
       public void mouseClicked(MouseEvent e) {
@@ -97,77 +128,27 @@ public class ChooseBuildConfigurationDialog extends DialogWrapper {
     return p;
   }
 
+  private static String getText(DefaultMutableTreeNode node) {
+    Object object = node.getUserObject();
+    if (object instanceof Module) {
+      Module module = (Module)object;
+      return module.getName();
+    }
+    else if (object instanceof FlexIdeBCConfigurable) {
+      FlexIdeBCConfigurable configurable = (FlexIdeBCConfigurable)object;
+      return configurable.getTreeNodeText();
+    }
+    else {
+      return null;
+    }
+  }
+
   public FlexIdeBCConfigurable[] getSelectedConfigurables() {
-    return ContainerUtil.map2Array(mySelection, FlexIdeBCConfigurable.class, new Function<BCNode, FlexIdeBCConfigurable>() {
+    return ContainerUtil.map2Array(mySelection, FlexIdeBCConfigurable.class, new Function<DefaultMutableTreeNode, FlexIdeBCConfigurable>() {
       @Override
-      public FlexIdeBCConfigurable fun(BCNode bcNode) {
-        return bcNode.configurable;
+      public FlexIdeBCConfigurable fun(DefaultMutableTreeNode node) {
+        return (FlexIdeBCConfigurable)node.getUserObject();
       }
     });
-  }
-
-  private class RootNode extends SimpleNode {
-    public RootNode(Project project) {
-      super(project);
-    }
-
-    @Override
-    public SimpleNode[] getChildren() {
-      return ContainerUtil.map2Array(myTreeItems.keySet(), SimpleNode.class, new Function<Module, SimpleNode>() {
-        @Override
-        public SimpleNode fun(Module module) {
-          return new ModuleNode(module);
-        }
-      });
-    }
-  }
-
-  private class ModuleNode extends SimpleNode {
-
-    private final Module module;
-
-    public ModuleNode(Module module) {
-      this.module = module;
-      myName = module.getName();
-      myOpenIcon = ModuleType.get(module).getNodeIcon(true);
-      myClosedIcon = ModuleType.get(module).getNodeIcon(false);
-    }
-
-    @Override
-    public boolean isAutoExpandNode() {
-      return true;
-    }
-
-    @Override
-    public SimpleNode[] getChildren() {
-      return ContainerUtil
-        .map2Array(myTreeItems.get(module), BCNode.class, new Function<FlexIdeBCConfigurable, BCNode>() {
-          @Override
-          public BCNode fun(FlexIdeBCConfigurable configurable) {
-            return new BCNode(configurable);
-          }
-        });
-    }
-  }
-
-  private static class BCNode extends SimpleNode {
-
-    private final FlexIdeBCConfigurable configurable;
-
-    public BCNode(FlexIdeBCConfigurable bc) {
-      this.configurable = bc;
-      myName = configurable.getTreeNodeText();
-      myOpenIcon = myClosedIcon = configurable.getIcon();
-    }
-
-    @Override
-    public SimpleNode[] getChildren() {
-      return NO_CHILDREN;
-    }
-
-    @Override
-    public boolean isAlwaysLeaf() {
-      return true;
-    }
   }
 }
