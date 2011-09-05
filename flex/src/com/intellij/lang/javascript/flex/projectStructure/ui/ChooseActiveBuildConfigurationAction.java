679159cd114f36d8e8b8221f18d381ad936045a9
Kirill.Safonov <kirill.safonov@swiftteams.com>
2011-09-05 18:48:24 +0400

    Choose active BC action: don't disable action button when dropdown is shown

--- flex/src/com/intellij/lang/javascript/flex/projectStructure/ui/ChooseActiveBuildConfigurationAction.java
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/ui/ChooseActiveBuildConfigurationAction.java
@@ -1,14 +1,10 @@
 package com.intellij.lang.javascript.flex.projectStructure.ui;
 
-import com.intellij.ide.DataManager;
 import com.intellij.lang.javascript.flex.FlexModuleType;
 import com.intellij.lang.javascript.flex.projectStructure.FlexIdeBuildConfigurationManager;
 import com.intellij.lang.javascript.flex.projectStructure.FlexIdeUtils;
 import com.intellij.lang.javascript.flex.projectStructure.options.FlexIdeBuildConfiguration;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Editor;
@@ -18,7 +14,9 @@ import com.intellij.openapi.module.ModuleUtil;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.popup.ListPopup;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -89,6 +87,25 @@ public class ChooseActiveBuildConfigurationAction extends ComboBoxAction impleme
       group.add(new BCAction(myLastModule, c));
     }
     return group;
+  }
+
+  @Override
+  protected ComboBoxButton createComboBoxButton(Presentation presentation) {
+    return new ComboBoxButton(presentation) {
+      @Override
+      protected ListPopup createPopup(Runnable onDispose) {
+        final DataContext popupCreationDataContext = getDataContext();
+
+        ListPopup popup = super.createPopup(onDispose);
+        popup.setDataProvider(new DataProvider() {
+          @Override
+          public Object getData(@NonNls String dataId) {
+            return popupCreationDataContext.getData(dataId);
+          }
+        });
+        return popup;
+      }
+    };
   }
 
   private static class BCAction extends DumbAwareAction {
