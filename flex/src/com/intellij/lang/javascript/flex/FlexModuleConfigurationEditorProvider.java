a439bef84cd8e2984e824867921319be82e0ae61
Kirill.Safonov <kirill.safonov@swiftteams.com>
2011-09-25 23:18:43 +0400

    cleanup

--- flex/src/com/intellij/lang/javascript/flex/FlexModuleConfigurationEditorProvider.java
+++ flex/src/com/intellij/lang/javascript/flex/FlexModuleConfigurationEditorProvider.java
@@ -1,8 +1,6 @@
 package com.intellij.lang.javascript.flex;
 
 import com.intellij.lang.javascript.flex.build.FlexCompilerSettingsEditor;
-import com.intellij.lang.javascript.flex.projectStructure.FlexIdeUtils;
-import com.intellij.lang.javascript.flex.projectStructure.ui.FlexModuleEditor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleConfigurationEditor;
 import com.intellij.openapi.module.ModuleType;
@@ -11,10 +9,6 @@ import com.intellij.openapi.roots.ui.configuration.DefaultModuleConfigurationEdi
 import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationEditorProvider;
 import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationState;
 import com.intellij.peer.PeerFactory;
-import com.intellij.util.PlatformUtils;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Maxim.Mossienko
@@ -23,16 +17,13 @@ public class FlexModuleConfigurationEditorProvider implements ModuleConfiguratio
   public ModuleConfigurationEditor[] createEditors(final ModuleConfigurationState state) {
     final Module module = state.getRootModel().getModule();
     if (ModuleType.get(module) != FlexModuleType.getInstance()) return ModuleConfigurationEditor.EMPTY;
-
-    List<ModuleConfigurationEditor> result = new ArrayList<ModuleConfigurationEditor>();
-    if (PlatformUtils.isFlexIde()) {
-      result.add(new FlexModuleEditor(state));
-    }
-    else {
-      result.add(PeerFactory.getInstance().createModuleConfigurationEditor(module.getName(), state));
-      result.add(DefaultModuleConfigurationEditorFactory.getInstance().createClasspathEditor(state));
-      result.add(new FlexCompilerSettingsEditor(module, state.getRootModel().getModuleExtension(CompilerModuleExtension.class)));
-    }
-    return result.toArray(new ModuleConfigurationEditor[result.size()]);
+    return new ModuleConfigurationEditor[] {
+      PeerFactory.getInstance().createModuleConfigurationEditor(
+        module.getName(), state
+      ),
+      DefaultModuleConfigurationEditorFactory.getInstance().createClasspathEditor(state),
+      new FlexCompilerSettingsEditor(state.getRootModel().getModule(),
+                                     state.getRootModel().getModuleExtension(CompilerModuleExtension.class))};
   }
+
 }
