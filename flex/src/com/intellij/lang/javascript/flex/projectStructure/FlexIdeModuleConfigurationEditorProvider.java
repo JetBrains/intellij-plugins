cd9fd0f9839438d07ac3759822ae9b42f7ee1f21
Kirill.Safonov <kirill.safonov@swiftteams.com>
2011-09-25 17:32:11 +0400

    Flex IDE: hide Flex/Air/AirMobile/FlexUnit run configurations

---
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/FlexIdeModuleConfigurationEditorProvider.java
@@ -0,0 +1,22 @@
+package com.intellij.lang.javascript.flex.projectStructure;
+
+import com.intellij.lang.javascript.flex.FlexModuleType;
+import com.intellij.lang.javascript.flex.projectStructure.ui.FlexModuleEditor;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleConfigurationEditor;
+import com.intellij.openapi.module.ModuleType;
+import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationEditorProvider;
+import com.intellij.openapi.roots.ui.configuration.ModuleConfigurationState;
+
+/**
+ * @author ksafonov
+ */
+public class FlexIdeModuleConfigurationEditorProvider implements ModuleConfigurationEditorProvider {
+  public ModuleConfigurationEditor[] createEditors(final ModuleConfigurationState state) {
+    Module module = state.getRootModel().getModule();
+    if (ModuleType.get(module) != FlexModuleType.getInstance()) {
+      return ModuleConfigurationEditor.EMPTY;
+    }
+    return new ModuleConfigurationEditor[]{new FlexModuleEditor(state)};
+  }
+}
