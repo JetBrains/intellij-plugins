1b5384fba723a0761c763fd49f525ff87e37dcde
Kirill.Safonov <kirill.safonov@jetbrains.com>
2011-09-29 16:37:46 +0400

    Convert IDEA project to Flex IDE: exclude non-Flex modules from project

--- flex/src/com/intellij/lang/javascript/flex/projectStructure/conversion/FlexIdeConverter.java
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/conversion/FlexIdeConverter.java
@@ -1,11 +1,19 @@
 package com.intellij.lang.javascript.flex.projectStructure.conversion;
 
 import com.intellij.conversion.*;
+import com.intellij.conversion.impl.ModuleSettingsImpl;
+import com.intellij.openapi.module.impl.ModuleManagerImpl;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.roots.impl.ProjectRootManagerImpl;
+import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.util.JDOMUtil;
+import com.intellij.util.containers.ContainerUtil;
 import org.jdom.Element;
 
 import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * User: ksafonov
@@ -52,10 +60,39 @@ class FlexIdeConverter extends ProjectConverter {
 
     getParams().projectSdkName = projectRootManager.getAttributeValue(ProjectRootManagerImpl.PROJECT_JDK_NAME_ATTR);
     getParams().projectSdkType = projectRootManager.getAttributeValue(ProjectRootManagerImpl.PROJECT_JDK_TYPE_ATTR);
+
+    removeNonFlexModulesFromProject();
+  }
+
+  private void removeNonFlexModulesFromProject() throws CannotConvertException {
+    ComponentManagerSettings modulesSettings = myContext.getModulesSettings();
+    if (modulesSettings == null) return;
+    final Element modulesManager = modulesSettings.getComponentElement(ModuleManagerImpl.COMPONENT_NAME);
+    if (modulesManager == null) return;
+
+    final Element modulesElement = modulesManager.getChild(ModuleManagerImpl.ELEMENT_MODULES);
+    List<Element> modules = JDOMUtil.getChildren(modulesElement, ModuleManagerImpl.ELEMENT_MODULE);
+    Collection<Element> modulesToRemove =
+      ContainerUtil.findAll(modules, new Condition<Element>() {
+        @Override
+        public boolean value(Element module) {
+          String filePath = module.getAttributeValue(ModuleManagerImpl.ATTRIBUTE_FILEPATH);
+          ModuleSettings moduleSettings = myContext.getModuleSettings(ModuleSettingsImpl.getModuleName(new File(filePath)));
+          return moduleSettings == null || !FlexIdeModuleConverter.isFlexModule(moduleSettings);
+        }
+      });
+    if (!modulesToRemove.isEmpty()) {
+      modules.removeAll(modulesToRemove);
+    }
   }
 
   @Override
   public void postProcessingFinished() throws CannotConvertException {
     getParams().saveGlobalLibraries();
+  }
+
+  @Override
+  public Collection<File> getAdditionalAffectedFiles() {
+    return Collections.singleton(myContext.getModulesSettings().getFile());
   }
 }
