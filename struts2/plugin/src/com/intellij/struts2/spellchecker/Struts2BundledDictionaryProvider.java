74d9cc4945f4ec90775f10b1fe649dc8dc6ca90f
Yann Cebron <yann.cebron@googlemail.com>
2011-10-20 16:57:36 +0200

    provide Struts2 specific dictionary

---
+++ struts2/plugin/src/com/intellij/struts2/spellchecker/Struts2BundledDictionaryProvider.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2011 The authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.intellij.struts2.spellchecker;
+
+import com.intellij.spellchecker.BundledDictionaryProvider;
+
+/**
+ * Provides bundled dictionary with Struts2 specific words.
+ *
+ * @author Yann C&eacute;bron
+ */
+public class Struts2BundledDictionaryProvider implements BundledDictionaryProvider {
+
+  @Override
+  public String[] getBundledDictionaries() {
+    return new String[]{"struts2.dic"};
+  }
+
+}
