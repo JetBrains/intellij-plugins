dd904b6700a6bd9bd80fedca7a57f662ed7ec274
Yann Cebron <yann.cebron@googlemail.com>
2011-11-08 21:00:10 +0100

    use CSSLanguage directly

--- struts2/plugin/src/com/intellij/struts2/jsp/TaglibCssInlineStyleInjector.java
+++ struts2/plugin/src/com/intellij/struts2/jsp/TaglibCssInlineStyleInjector.java
@@ -15,6 +15,7 @@
 
 package com.intellij.struts2.jsp;
 
+import com.intellij.lang.css.CSSLanguage;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.openapi.fileTypes.StdFileTypes;
@@ -22,7 +23,6 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
-import com.intellij.psi.css.CssFileType;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.struts2.StrutsConstants;
 import org.jetbrains.annotations.NotNull;
@@ -54,7 +54,7 @@ public class TaglibCssInlineStyleInjector implements MultiHostInjector {
   public void getLanguagesToInject(@NotNull final MultiHostRegistrar registrar, @NotNull final PsiElement context) {
     if (CSS_ELEMENT_PATTERN.accepts(context)) {
       final TextRange range = new TextRange(1, context.getTextLength() - 1);
-      registrar.startInjecting(CssFileType.INSTANCE.getLanguage())
+      registrar.startInjecting(CSSLanguage.INSTANCE)
                .addPlace("inline.style {", "}", (PsiLanguageInjectionHost) context, range)
                .doneInjecting();
     }
