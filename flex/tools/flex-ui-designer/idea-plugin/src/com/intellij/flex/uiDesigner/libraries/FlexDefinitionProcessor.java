66bf171f075e8f148e4e34f8a32dc0845728ca21
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2011-11-16 12:15:33 +0400

    fix flex injection for flex sdk 4.1

--- flex/tools/flex-ui-designer/idea-plugin/src/com/intellij/flex/uiDesigner/libraries/FlexDefinitionProcessor.java
+++ flex/tools/flex-ui-designer/idea-plugin/src/com/intellij/flex/uiDesigner/libraries/FlexDefinitionProcessor.java
@@ -16,7 +16,7 @@ import java.nio.charset.CharsetEncoder;
 import java.nio.charset.CodingErrorAction;
 import java.util.Map;
 
-public class FlexDefinitionProcessor implements DefinitionProcessor {
+class FlexDefinitionProcessor implements DefinitionProcessor {
   private static final String STYLE_PROTO_CHAIN = "mx.styles:StyleProtoChain";
   private static final String SKINNABLE_COMPONENT = "spark.components.supportClasses:SkinnableComponent";
   private static final String MX_CORE = "mx.core:";
@@ -41,7 +41,7 @@ public class FlexDefinitionProcessor implements DefinitionProcessor {
       flipDefinition(definition, definitionMap, SKINNABLE_COMPONENT);
     }
     else if (StringUtil.equals(name, "mx.containers:Panel")) {
-      definition.doAbcData.abcModifier = new MethodAccessModifier("setControlBar", "addChildAt", null, false);
+      definition.doAbcData.abcModifier = new MethodAccessModifier("setControlBar", vGreaterOrEquals4_5 ? "addChildAt" : null, null, false);
     }
     else {
       final boolean mxCore = StringUtil.startsWith(name, MX_CORE);
