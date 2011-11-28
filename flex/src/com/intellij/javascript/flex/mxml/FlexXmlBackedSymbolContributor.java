5ae828419d2ba4b4152582ccf78a32a858fe297c
Alexey Kudravtsev <cdr@intellij.com>
2011-11-28 14:20:38 +0400

    notnull

--- flex/src/com/intellij/javascript/flex/mxml/FlexXmlBackedSymbolContributor.java
+++ flex/src/com/intellij/javascript/flex/mxml/FlexXmlBackedSymbolContributor.java
@@ -6,10 +6,12 @@ import com.intellij.navigation.NavigationItem;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.ArrayUtil;
 import gnu.trove.THashSet;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 
 public class FlexXmlBackedSymbolContributor implements ChooseByNameContributor {
+  @NotNull
   public String[] getNames(Project project, boolean includeNonProjectItems) {
     Collection<String> result = new THashSet<String>();
     result.addAll(FlexXmlBackedMembersIndex.getSymbolNames(project));
@@ -17,6 +19,7 @@ public class FlexXmlBackedSymbolContributor implements ChooseByNameContributor {
     return ArrayUtil.toStringArray(result);
   }
 
+  @NotNull
   public NavigationItem[] getItemsByName(String name, final String pattern, Project project, boolean includeNonProjectItems) {
     Collection<NavigationItem> result = new THashSet<NavigationItem>();
     result.addAll(FlexXmlBackedMembersIndex.getItemsByName(name, project));
