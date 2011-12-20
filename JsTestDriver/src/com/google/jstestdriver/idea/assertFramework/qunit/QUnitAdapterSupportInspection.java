a1a5df28cad1941adc85fe55f00e56b25912911c
Sergey Simonchik <Sergey.Simonchik@jetbrains.com>
2011-12-20 19:46:13 +0400

    add assertion adapter support popup is available even if method is resolved to JS C Style comment

---
+++ JsTestDriver/src/com/google/jstestdriver/idea/assertFramework/qunit/QUnitAdapterSupportInspection.java
@@ -0,0 +1,52 @@
+package com.google.jstestdriver.idea.assertFramework.qunit;
+
+import com.google.inject.Provider;
+import com.google.jstestdriver.idea.assertFramework.support.AbstractAddAdapterSupportInspection;
+import com.google.jstestdriver.idea.assertFramework.qunit.jsSrc.QUnitAdapterSrcMarker;
+import com.google.jstestdriver.idea.util.JsPsiUtils;
+import com.google.jstestdriver.idea.util.VfsUtils;
+import com.intellij.lang.javascript.psi.JSExpression;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.util.List;
+
+public class QUnitAdapterSupportInspection extends AbstractAddAdapterSupportInspection {
+
+  public QUnitAdapterSupportInspection() {
+    super("QUnit", new Provider<List<VirtualFile>>() {
+      @Override
+      public List<VirtualFile> get() {
+        String[] relativePaths = new String[]{"equiv.js", "QUnitAdapter.js"};
+        return VfsUtils.findVirtualFilesByResourceNames(QUnitAdapterSrcMarker.class, relativePaths);
+      }
+    });
+  }
+
+  @Override
+  protected boolean isSuitableMethod(String methodName, JSExpression[] methodArguments) {
+    if (methodArguments.length == 0) {
+      return false;
+    }
+    if ("module".equals(methodName) && JsPsiUtils.isStringElement(methodArguments[0])) {
+      if (methodArguments.length == 1) {
+        return true;
+      }
+      if (methodArguments.length == 2 && JsPsiUtils.isObjectElement(methodArguments[1])) {
+        return true;
+      }
+    }
+    if ("test".equals(methodName) && JsPsiUtils.isStringElement(methodArguments[0])) {
+      if (methodArguments.length == 1) {
+        return true;
+      }
+      if (methodArguments.length == 2 && JsPsiUtils.isFunctionExpressionElement(methodArguments[1])) {
+        return true;
+      }
+      if (methodArguments.length == 3 && JsPsiUtils.isNumberElement(methodArguments[1]) && JsPsiUtils.isFunctionExpressionElement(methodArguments[2])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
---
+++ JsTestDriver/src/com/google/jstestdriver/idea/assertFramework/support/AddAdapterSupportQuickFix.java
@@ -0,0 +1,72 @@
+package com.google.jstestdriver.idea.assertFramework.support;
+
+import com.google.inject.Provider;
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.AsyncResult;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.FileContentUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+class AddAdapterSupportQuickFix implements LocalQuickFix {
+
+  private final String myAssertionFrameworkName;
+  private final Provider<List<VirtualFile>> myAdapterSourceFilesProvider;
+
+  public AddAdapterSupportQuickFix(String assertionFrameworkName, Provider<List<VirtualFile>> adapterSourceFilesProvider) {
+    myAssertionFrameworkName = assertionFrameworkName;
+    myAdapterSourceFilesProvider = adapterSourceFilesProvider;
+  }
+
+  @NotNull
+  @Override
+  public String getFamilyName() {
+    return getName();
+  }
+
+  @NotNull
+  @Override
+  public String getName() {
+    return "Add " + myAssertionFrameworkName + " adapter support for JsTestDriver";
+  }
+
+  @Override
+  public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor descriptor) {
+    PsiFile psiFile = descriptor.getPsiElement().getContainingFile();
+    if (psiFile != null) {
+      doIt(project, psiFile);
+    }
+  }
+
+  public void doIt(@NotNull final Project project, @NotNull final PsiFile psiFile) {
+    ApplicationManager.getApplication().invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        AddAdapterSupportDialog dialog = new AddAdapterSupportDialog(
+          project,
+          myAssertionFrameworkName,
+          myAdapterSourceFilesProvider
+        );
+        final AsyncResult<Boolean> result = dialog.showAndGetOk();
+        final VirtualFile virtualFile = psiFile.getVirtualFile();
+        if (virtualFile != null) {
+          result.doWhenDone(new AsyncResult.Handler<Boolean>() {
+            @Override
+            public void run(Boolean ok) {
+              if (ok) {
+                FileContentUtil.reparseFiles(project, Arrays.<VirtualFile>asList(virtualFile), true);
+              }
+            }
+          });
+        }
+      }
+    });
+  }
+
+}
