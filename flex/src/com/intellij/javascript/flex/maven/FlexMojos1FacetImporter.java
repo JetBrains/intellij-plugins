66057fae476f86504b6361691b029a6c25239133
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2012-01-25 15:37:03 +0400

    Drop flexmojos 1, flexmojos 2 and flex israfil support

--- flex/src/com/intellij/javascript/flex/maven/FlexMojos1FacetImporter.java
+++
@@ -1,7 +0,0 @@
-package com.intellij.javascript.flex.maven;
-
-public class FlexMojos1FacetImporter extends FlexFacetImporter {
-  public FlexMojos1FacetImporter() {
-    super("info.rvin.mojo", "flex-compiler-mojo");
-  }
-}
--- flex/src/com/intellij/javascript/flex/maven/FlexMojos2FacetImporter.java
+++
@@ -1,7 +0,0 @@
-package com.intellij.javascript.flex.maven;
-
-public class FlexMojos2FacetImporter extends FlexFacetImporter {
-  public FlexMojos2FacetImporter() {
-    super("info.flex-mojos", "flex-compiler-mojo");
-  }
-}
