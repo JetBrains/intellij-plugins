829e9a7be5a7663c0aa719fffe290364bbe4a61a
Sergey Simonchik <Sergey.Simonchik@jetbrains.com>
2012-02-27 15:34:18 +0400

    node.js express app generator fixed for Windows

---
+++ JsTestDriver/test/testSrc/com/google/jstestdriver/idea/assertFramework/jstd/JstdHighlightingTest.java
@@ -0,0 +1,39 @@
+package com.google.jstestdriver.idea.assertFramework.jstd;
+
+import com.google.jstestdriver.idea.JsTestDriverTestUtils;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.lang.javascript.JSDaemonAnalyzerTestCase;
+
+import java.io.IOException;
+
+public class JstdHighlightingTest extends JSDaemonAnalyzerTestCase {
+  @Override
+  protected String getBasePath() {
+    return "/assertFramework/jstd/highlighting";
+  }
+
+  @Override
+  protected String getExtension() {
+    return "js";
+  }
+
+  @Override
+  protected LocalInspectionTool[] configureLocalInspectionTools() {
+    return new LocalInspectionTool[] {};
+  }
+
+  private void defaultTest() throws IOException {
+    doTestFor(true, getTestName(false)+".js");
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return JsTestDriverTestUtils.getTestDataDir().getAbsolutePath();
+  }
+
+  public void testJstdTestCaseQuickFixAvailable() throws Exception {
+    JstdAssertionFrameworkSupportInspection inspection = new JstdAssertionFrameworkSupportInspection();
+    enableInspectionTool(inspection);
+    defaultTest();
+  }
+}
