50b5b78d5ef748fc5d217c4c45601468918c06ea
kirill.safonov <kirill.safonov@jetbrains.com>
2012-03-14 18:15:11 +0400

    AIR package dialog: remove nbsp-s which are rendered incorrectly on Mac OS

--- flex/src/com/intellij/lang/javascript/flex/actions/airpackage/AirPackageDialog.java
+++ flex/src/com/intellij/lang/javascript/flex/actions/airpackage/AirPackageDialog.java
@@ -247,7 +247,7 @@ public class AirPackageDialog extends DialogWrapper {
     if (!bcsWithDisabledCompilation.isEmpty()) {
       final StringBuilder bcs = new StringBuilder();
       for (FlexIdeBuildConfiguration bc : bcsWithDisabledCompilation) {
-        bcs.append("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>").append(StringUtil.escapeXml(bc.getName())).append("</b><br>");
+        bcs.append("<b>").append(StringUtil.escapeXml(bc.getName())).append("</b><br>");
       }
       final String message = FlexBundle.message("package.bc.with.disabled.compilation", bcsWithDisabledCompilation.size(), bcs.toString());
       final int answer =
