a2f79bffe1c82280af2caabdebc80b0dd2f156a8
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2012-03-28 15:24:26 +0400

    recognize 'jsTestDriver-coverage.conf' files as JstdFileType

--- JsTestDriver/src/com/google/jstestdriver/idea/config/JstdConfigFileLoader.java
+++ JsTestDriver/src/com/google/jstestdriver/idea/config/JstdConfigFileLoader.java
@@ -15,33 +15,79 @@
  */
 package com.google.jstestdriver.idea.config;
 
-import com.intellij.openapi.fileTypes.*;
+import com.google.common.base.Joiner;
+import com.intellij.openapi.fileTypes.ExtensionFileNameMatcher;
+import com.intellij.openapi.fileTypes.FileNameMatcher;
+import com.intellij.openapi.fileTypes.FileTypeConsumer;
+import com.intellij.openapi.fileTypes.FileTypeFactory;
+import com.intellij.openapi.util.text.StringUtil;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class JstdConfigFileLoader extends FileTypeFactory {
 
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
     JstdConfigFileType jstdConfigFileType = JstdConfigFileType.INSTANCE;
-    FileNameMatcher[] matchers = getFileNameMatchers();
+    FileNameMatcher[] matchers = {
+      new ExtensionFileNameMatcher(JstdConfigFileType.INSTANCE.getDefaultExtension()),
+      new CustomFileNameMatcher()
+    };
     consumer.consume(jstdConfigFileType, matchers);
   }
 
-  private static FileNameMatcher[] getFileNameMatchers() {
-    List<FileNameMatcher> matchers = new ArrayList<FileNameMatcher>();
-    matchers.add(new ExtensionFileNameMatcher(JstdConfigFileType.INSTANCE.getDefaultExtension()));
-    String[] namesWithoutExt = {"jsTestDriver", "js-test-driver", "js_test_driver", "jstd"};
-    String[] extensions = {"conf", "yml", "yaml"};
-    for (String nameWithoutExt : namesWithoutExt) {
-      for (String ext : extensions) {
-        ExactFileNameMatcher fileNameMatcher = new ExactFileNameMatcher(nameWithoutExt + "." + ext);
-        matchers.add(fileNameMatcher);
+  /**
+   * Matches some predefined fileNames.
+   * The motivation is that configuration file is often named 'jsTestDriver.conf'.
+   * Examples of matched fileNames: 'jsTestDriver.conf', 'jsTestDriver.yaml', 'jstd.yml', 'jsTestDriver-coverage.conf', etc
+   */
+  private static class CustomFileNameMatcher implements FileNameMatcher {
+
+    private static final String[] PREFIXES = {"jsTestDriver", "js-test-driver", "js_test_driver", "jstd"};
+    private static final String[] DOT_SUFFIXES = {".conf", ".yml", ".yaml"};
+    private static final String COMMON_PREFIX;
+    private static final int COMMON_PREFIX_LENGTH;
+    static {
+      String common = PREFIXES[0];
+      for (int i = 1; i < PREFIXES.length; i++) {
+        common = StringUtil.commonPrefix(common, PREFIXES[i]);
       }
+      COMMON_PREFIX = common;
+      COMMON_PREFIX_LENGTH = COMMON_PREFIX.length();
     }
-    return matchers.toArray(new FileNameMatcher[matchers.size()]);
+
+    @Override
+    public boolean accept(@NonNls @NotNull String fileName) {
+      if (COMMON_PREFIX_LENGTH > 0) {
+        if (!fileName.startsWith(COMMON_PREFIX)) {
+          return false;
+        }
+      }
+      for (String prefix : PREFIXES) {
+        if (fileName.startsWith(prefix)) {
+          if (hasSuitableExtension(fileName)) {
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+
+    @NotNull
+    @Override
+    public String getPresentableString() {
+      Joiner joiner = Joiner.on("|");
+      return "(" + joiner.join(PREFIXES) + ")*(" + joiner.join(DOT_SUFFIXES) + ")";
+    }
+
+    private static boolean hasSuitableExtension(@NotNull String fileName) {
+      for (String dotSuffix : DOT_SUFFIXES) {
+        if (fileName.endsWith(dotSuffix)) {
+          return true;
+        }
+      }
+      return false;
+    }
   }
 
 }
