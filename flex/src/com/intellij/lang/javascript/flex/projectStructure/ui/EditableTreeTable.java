d58273ee1a94443a40d31f3f6a8f3fa50373d49a
irengrig <Irina.Chernushina@jetbrains.com>
2012-05-10 17:39:01 +0400

    SVN: svn tree conflicts are show in details pane; accept theirs, accept mine variants; tests for all conflict types

--- flex/src/com/intellij/lang/javascript/flex/projectStructure/ui/EditableTreeTable.java
+++ flex/src/com/intellij/lang/javascript/flex/projectStructure/ui/EditableTreeTable.java
@@ -94,7 +94,7 @@ abstract class EditableTreeTable<T> extends TreeTable {
         column.setPreferredWidth(width);
         column.setMinWidth(width);
       }
-      else if ((maxStringValue = columnInfo.getMaxStringValue()) != null) {
+      else if ((maxStringValue = columnInfo.getMaxStringValue(this)) != null) {
         int width = getFontMetrics(getFont()).stringWidth(maxStringValue) + columnInfo.getAdditionalWidth();
         width = Math.max(width, headerSize.width);
         column.setPreferredWidth(width);
@@ -268,8 +268,8 @@ abstract class EditableTreeTable<T> extends TreeTable {
     }
 
     @Override
-    public String getMaxStringValue() {
-      return myDelegate.getMaxStringValue();
+    public String getMaxStringValue(JTable table) {
+      return myDelegate.getMaxStringValue(table);
     }
   }
 }
