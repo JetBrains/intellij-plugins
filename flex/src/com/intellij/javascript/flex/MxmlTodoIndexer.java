02de13f639c921a59d55563e713426384c1f71cd
Evgeny Pasynkov <Evgeny.Pasynkov@jetbrains.com>
2012-06-13 12:12:09 +0200

    Move CacheManager to "indexing" module Move todo-related stuff back to lang-impl - they'r far too not ready to be moved

--- flex/src/com/intellij/javascript/flex/MxmlTodoIndexer.java
+++ flex/src/com/intellij/javascript/flex/MxmlTodoIndexer.java
@@ -4,7 +4,6 @@ import com.intellij.lang.javascript.flex.XmlBackedJSClassImpl;
 import com.intellij.lang.javascript.psi.JSFile;
 import com.intellij.lang.javascript.psi.resolve.JSResolveUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.impl.cache.impl.id.IdTableBuilding;
 import com.intellij.psi.impl.cache.impl.id.PlatformIdTableBuilding;
 import com.intellij.psi.impl.cache.impl.idCache.XmlTodoIndexer;
 import com.intellij.psi.impl.cache.impl.todo.TodoIndexEntry;
