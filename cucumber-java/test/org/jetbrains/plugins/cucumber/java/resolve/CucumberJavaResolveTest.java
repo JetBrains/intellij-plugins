e2cabc80b9639433fe6dd26801765d3608ff32a0
Andrey Vokin <andrey.vokin@gmail.com>
2012-07-25 15:27:41 +0400

    Tests for cucumber-java added

--- cucumber-java/test/org/jetbrains/plugins/cucumber/java/resolve/CucumberJavaResolveTest.java
+++ cucumber-java/test/org/jetbrains/plugins/cucumber/java/resolve/CucumberJavaResolveTest.java
@@ -11,8 +11,14 @@ import org.jetbrains.plugins.cucumber.resolve.CucumberResolveTest;
  * Date: 7/20/12
  */
 public class CucumberJavaResolveTest extends CucumberResolveTest {
-  public void testNavigationFromStepToStepDef() throws Exception {
+  public void testNavigationFromStepToStepDef01() throws Exception {
     doTest("stepResolve_01", "I p<caret>ay 25", "i_pay");
+  }
+  public void testNavigationFromStepToStepDef02() throws Exception {
+    doTest("stepResolve_01", "the followi<caret>ng groceries", "the_following_groceries");
+  }
+  public void testNavigationFromStepToStepDef03() throws Exception {
+    doTest("stepResolve_01", "my change sh<caret>ould be 4", "my_change_should_be_");
   }
 
   @Nullable
