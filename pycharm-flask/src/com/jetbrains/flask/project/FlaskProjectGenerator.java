d06a10ba7532ccbea714d177f2c86bf3330c326d
Dmitry Jemerov <yole@jetbrains.com>
2012-08-08 13:45:33 +0200

    install Flask if not installed in the selected interpreter

--- pycharm-flask/src/com/jetbrains/flask/project/FlaskProjectGenerator.java
+++ pycharm-flask/src/com/jetbrains/flask/project/FlaskProjectGenerator.java
@@ -7,6 +7,9 @@ import com.intellij.ide.fileTemplates.FileTemplateUtil;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.ui.Messages;
@@ -15,14 +18,19 @@ import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.jetbrains.python.newProject.PyFrameworkProjectGenerator;
+import com.jetbrains.python.newProject.PyNewProjectSettings;
+import com.jetbrains.python.packaging.PyExternalProcessException;
+import com.jetbrains.python.packaging.PyPackageManager;
 import com.jetbrains.python.psi.PyPsiFacade;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+
 /**
  * @author yole
  */
-public class FlaskProjectGenerator implements PyFrameworkProjectGenerator {
+public class FlaskProjectGenerator implements PyFrameworkProjectGenerator<PyNewProjectSettings> {
   @Nls
   @Override
   public String getName() {
@@ -40,12 +48,31 @@ public class FlaskProjectGenerator implements PyFrameworkProjectGenerator {
   }
 
   @Override
-  public Object showGenerationSettings(VirtualFile baseDir) throws ProcessCanceledException {
-    return null;
+  public PyNewProjectSettings showGenerationSettings(VirtualFile baseDir) throws ProcessCanceledException {
+    return new PyNewProjectSettings();
   }
 
   @Override
-  public void generateProject(final Project project, final VirtualFile baseDir, Object settings, Module module) {
+  public void generateProject(final Project project, final VirtualFile baseDir, final PyNewProjectSettings settings, Module module) {
+    if (settings.installFramework()) {
+      ProgressManager.getInstance().run(new Task.Backgroundable(project, "Installing Flask", false) {
+        @Override
+        public void run(@NotNull ProgressIndicator indicator) {
+          indicator.setText("Installing Flask...");
+          final PyPackageManager packageManager = PyPackageManager.getInstance(settings.getSdk());
+          try {
+            packageManager.install("Flask");
+          }
+          catch (final PyExternalProcessException e) {
+            SwingUtilities.invokeLater(new Runnable() {
+              @Override
+              public void run() {
+                packageManager.showInstallationError(project, "Install Flask failed", e.toString());
+              }
+            });
+          }
+        }});
+    }
     final PsiDirectory projectDir = PsiManager.getInstance(project).findDirectory(baseDir);
     new WriteCommandAction.Simple(project) {
       @Override
