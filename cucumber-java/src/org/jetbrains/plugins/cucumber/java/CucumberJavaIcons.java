73c9237fdeae05d9ae5b745c86eefa6a5d7a0118
Andrey Vokin <andrey.vokin@gmail.com>
2012-08-08 14:31:33 +0400

    RunConfiguration for cucumber-java via SMRunner started

---
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJavaIcons.java
@@ -0,0 +1,13 @@
+package org.jetbrains.plugins.cucumber.java;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * User: Andrey.Vokin
+ * Date: 8/6/12
+ */
+public interface CucumberJavaIcons {
+  Icon RUN_CONFIGURATION_ICON = IconLoader.getIcon("/org/jetbrains/plugins/cucumber/cucumberJavaRunConfiguration.png");
+}
