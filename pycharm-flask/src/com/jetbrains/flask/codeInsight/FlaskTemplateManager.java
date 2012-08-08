6924b4a762d3e52c9cc3f16543cd913f760e4b70
Dmitry Jemerov <yole@jetbrains.com>
2012-08-08 18:40:39 +0200

    references to templates in string literals for Flask (part of PY-7145)

---
+++ pycharm-flask/src/com/jetbrains/flask/codeInsight/FlaskTemplateManager.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.flask.codeInsight;
+
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.psi.PyCallExpression;
+import com.jetbrains.python.psi.PyFunction;
+import com.jetbrains.python.psi.PyRecursiveElementVisitor;
+import com.jetbrains.python.psi.PyStringLiteralExpression;
+import com.jetbrains.python.psi.impl.PyPsiUtils;
+import com.jetbrains.python.psi.resolve.PyResolveContext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yole
+ */
+public class FlaskTemplateManager {
+  public static boolean isTemplateReference(PyStringLiteralExpression expr) {
+    PyCallExpression call = PsiTreeUtil.getParentOfType(expr, PyCallExpression.class);
+    if (call != null) {
+      int index = PyPsiUtils.findArgumentIndex(call, expr);
+      if (index == 0 && call.isCalleeText(FlaskNames.RENDER_TEMPLATE)) {
+        PyCallExpression.PyMarkedCallee callee = call.resolveCallee(PyResolveContext.noImplicits());
+        if (callee != null && callee.getCallable().getContainingFile().getName().equals("templating.py")) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public static List<PyStringLiteralExpression> collectTemplateReferences(PyFunction function) {
+    final List<PyStringLiteralExpression> result = new ArrayList<PyStringLiteralExpression>();
+    function.acceptChildren(new PyRecursiveElementVisitor() {
+      @Override
+      public void visitPyStringLiteralExpression(PyStringLiteralExpression node) {
+        if (isTemplateReference(node)) {
+          result.add(node);
+        }
+      }
+    });
+    return result;
+  }
+}
---
+++ pycharm-flask/src/com/jetbrains/flask/codeInsight/references/FlaskTemplateReferenceProvider.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.flask.codeInsight.references;
+
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.util.ProcessingContext;
+import com.jetbrains.flask.codeInsight.FlaskTemplateManager;
+import com.jetbrains.python.psi.PyStringLiteralExpression;
+import com.jetbrains.python.templateLanguages.TemplateFileReferenceSet;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author yole
+ */
+public class FlaskTemplateReferenceProvider extends PsiReferenceProvider {
+  @NotNull
+  @Override
+  public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
+    PyStringLiteralExpression stringLiteral = (PyStringLiteralExpression)element;
+    if (FlaskTemplateManager.isTemplateReference(stringLiteral)) {
+      return new TemplateFileReferenceSet(stringLiteral, null).getAllReferences();
+
+    }
+    return PsiReference.EMPTY_ARRAY;
+  }
+}
