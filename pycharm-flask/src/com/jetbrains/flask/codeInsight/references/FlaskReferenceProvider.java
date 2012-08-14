3fd547b88a8f4e733bdccbab5f3c3c6e632f81bf
Dmitry Jemerov <yole@jetbrains.com>
2012-08-14 17:43:27 +0200

    reference from parameter in app.route string to function parameter (part of PY-7148)

--- pycharm-flask/src/com/jetbrains/flask/codeInsight/references/FlaskReferenceProvider.java
+++ pycharm-flask/src/com/jetbrains/flask/codeInsight/references/FlaskReferenceProvider.java
@@ -18,12 +18,19 @@ package com.jetbrains.flask.codeInsight.references;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.PsiReferenceProvider;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.flask.codeInsight.FlaskNames;
 import com.jetbrains.flask.codeInsight.FlaskTemplateManager;
+import com.jetbrains.flask.codeInsight.WerkzeugRoutingRule;
+import com.jetbrains.python.psi.PyDecorator;
+import com.jetbrains.python.psi.PyExpression;
 import com.jetbrains.python.psi.PyStringLiteralExpression;
 import com.jetbrains.python.templateLanguages.TemplateFileReferenceSet;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author yole
@@ -40,6 +47,25 @@ public class FlaskReferenceProvider extends PsiReferenceProvider {
     else if (FlaskTemplateManager.isCallArgument(stringLiteral, 0, FlaskNames.URL_FOR, FlaskNames.HELPERS_PY)) {
       return new PsiReference[] { new FlaskViewMethodReference(stringLiteral) };
     }
+    else if (isRouteDecoratorArgument(stringLiteral)) {
+      WerkzeugRoutingRule rule = WerkzeugRoutingRule.parse(stringLiteral);
+      List<PsiReference> refs = new ArrayList<PsiReference>();
+      for (WerkzeugRoutingRule.Parameter parameter : rule.parameters) {
+        refs.add(new FlaskRouteParameterReference(stringLiteral, parameter));
+      }
+      return refs.toArray(new PsiReference[refs.size()]);
+    }
     return PsiReference.EMPTY_ARRAY;
+  }
+
+  private static boolean isRouteDecoratorArgument(PyStringLiteralExpression literal) {
+    PyDecorator decorator = PsiTreeUtil.getParentOfType(literal, PyDecorator.class);
+    if (decorator != null && FlaskViewMethodReference.isRouteDecorator(decorator)) {
+      PyExpression[] arguments = decorator.getArguments();
+      if (arguments.length > 0 && literal == arguments[0]) {
+        return true;
+      }
+    }
+    return false;
   }
 }
