f2b12f7a81e08d1b0bacc5368c18ee57d7937232
Sergey Simonchik <Sergey.Simonchik@jetbrains.com>
2012-08-16 13:36:24 +0400

    WI-12651 better fix (cherry-picked from 8a89c12)

--- JsTestDriver/src/com/google/jstestdriver/idea/server/ui/JstdToolWindowManager.java
+++ JsTestDriver/src/com/google/jstestdriver/idea/server/ui/JstdToolWindowManager.java
@@ -1,12 +1,17 @@
 package com.google.jstestdriver.idea.server.ui;
 
+import com.google.jstestdriver.idea.execution.JstdSettingsUtil;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowAnchor;
 import com.intellij.openapi.wm.ToolWindowEP;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.openapi.wm.ex.ToolWindowManagerEx;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -32,21 +37,47 @@ public class JstdToolWindowManager {
     ToolWindowManagerEx toolWindowManagerEx = ToolWindowManagerEx.getInstanceEx(myProject);
     ToolWindow toolWindow = toolWindowManagerEx.getToolWindow(TOOL_WINDOW_ID);
     if (toolWindow == null) {
-      ToolWindowEP toolWindowEP = new ToolWindowEP() {
-        @Override
-        public ToolWindowFactory getToolWindowFactory() {
-          return new JstdToolWindow();
-        }
-      };
-      toolWindowEP.id = TOOL_WINDOW_ID;
-      toolWindowEP.anchor = ToolWindowAnchor.BOTTOM.toString();
-      toolWindowEP.secondary = false;
-      toolWindowEP.icon = "/com/google/jstestdriver/idea/icons/toolWindowTestDriver.png";
-      toolWindowEP.canCloseContents = false;
+      ToolWindowEP toolWindowEP = getJstdToolWindowEP();
       toolWindowManagerEx.initToolWindow(toolWindowEP);
       toolWindow = toolWindowManagerEx.getToolWindow(TOOL_WINDOW_ID);
     }
     return toolWindow;
+  }
+
+  @Nullable
+  private static ToolWindowEP getJstdToolWindowEP() {
+    for (ToolWindowEP ep : ToolWindowEP.EP_NAME.getExtensions()) {
+      if (ep.id.equals(TOOL_WINDOW_ID)) {
+        return ep;
+      }
+    }
+    return null;
+  }
+
+  public static class ToolWindowCondition implements Condition<Project> {
+    @Override
+    public boolean value(Project project) {
+      return JstdSettingsUtil.areJstdConfigFilesInProject(project);
+    }
+  }
+
+  public static class Factory implements ToolWindowFactory {
+    @Override
+    public void createToolWindowContent(Project project, ToolWindow toolWindow) {
+      toolWindow.setAvailable(true, null);
+      toolWindow.setToHideOnEmptyContent(true);
+
+      final ContentManager contentManager = toolWindow.getContentManager();
+      JstdToolWindowPanel component = new JstdToolWindowPanel();
+      final Content content = contentManager.getFactory().createContent(component, null, false);
+      content.setDisposer(project);
+      content.setCloseable(false);
+
+      content.setPreferredFocusableComponent(component.getPreferredFocusedComponent());
+      contentManager.addContent(content);
+
+      contentManager.setSelectedContent(content, true);
+    }
   }
 
 }
