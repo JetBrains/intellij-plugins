eca505b34b0c0375480e3790de3a32b6c4db1bf7
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2012-08-21 13:27:17 +0400

    WI-12718 JSTestDriver: update existing run configurations on renaming file/class/method - fixed for 'Config file', 'All in directory', 'JavaScript test file'

--- JsTestDriver/src/com/google/jstestdriver/idea/execution/JstdRunConfigurationRefactoringHandler.java
+++ JsTestDriver/src/com/google/jstestdriver/idea/execution/JstdRunConfigurationRefactoringHandler.java
@@ -12,130 +12,31 @@ import org.jetbrains.annotations.Nullable;
 
 public class JstdRunConfigurationRefactoringHandler {
 
-  private final JstdRunConfiguration myRunConfiguration;
-
-  public JstdRunConfigurationRefactoringHandler(@NotNull JstdRunConfiguration runConfiguration) {
-    myRunConfiguration = runConfiguration;
-  }
-
   @Nullable
-  public RefactoringElementListener getRefactoringElementListener(@Nullable PsiElement element) {
+  public static RefactoringElementListener getRefactoringElementListener(@NotNull JstdRunConfiguration configuration,
+                                                                         @Nullable PsiElement element) {
     if (element == null) {
       return null;
     }
-    final TestType testType = myRunConfiguration.getRunSettings().getTestType();
-    if (testType == TestType.ALL_CONFIGS_IN_DIRECTORY) {
-      return provideRefactoringDirectoryListener(element);
-    } else if (testType == TestType.CONFIG_FILE) {
-      return provideRefactoringConfigFileListener(element);
-    } else if (testType == TestType.JS_FILE) {
-      return provideRefactoringJsFileListener(element);
+    VirtualFile fileAtElement = toVirtualFile(element);
+    if (fileAtElement == null) {
+      return null;
+    }
+    JstdRunSettings settings = configuration.getRunSettings();
+    String path = fileAtElement.getPath();
+    if (settings.getTestType() == TestType.ALL_CONFIGS_IN_DIRECTORY) {
+      if (settings.getDirectory().equals(path)) {
+        return new FilePathRefactoringElementListener(configuration, false, false, true);
+      }
+    } else {
+      if (settings.getJsFilePath().equals(path)) {
+        return new FilePathRefactoringElementListener(configuration, false, true, false);
+      }
+      if (settings.getConfigFile().equals(path)) {
+        return new FilePathRefactoringElementListener(configuration, true, false, false);
+      }
     }
     return null;
-  }
-
-  @Nullable
-  private RefactoringElementListener provideRefactoringDirectoryListener(@NotNull PsiElement element) {
-    final JstdRunSettings currentRunSettings = myRunConfiguration.getRunSettings();
-    VirtualFile directory = toVirtualFile(element);
-    if (directory != null && currentRunSettings.getDirectory().equals(directory.getPath())) {
-      return new RefactoringElementListener() {
-
-        @Override
-        public void elementMoved(@NotNull PsiElement newElement) {
-          refactorIt(newElement);
-        }
-
-        @Override
-        public void elementRenamed(@NotNull PsiElement newElement) {
-          refactorIt(newElement);
-        }
-
-        private void refactorIt(@NotNull PsiElement newElement) {
-          VirtualFile newDirectory = toVirtualFile(newElement);
-          if (newDirectory != null) {
-            JstdRunSettings newRunSettings = new JstdRunSettings.Builder(currentRunSettings)
-                .setDirectory(newDirectory.getPath())
-                .build();
-            updateRunSettings(newRunSettings);
-          }
-        }
-
-      };
-    }
-    return null;
-  }
-
-  @Nullable
-  private RefactoringElementListener provideRefactoringConfigFileListener(PsiElement element) {
-    final JstdRunSettings currentRunSettings = myRunConfiguration.getRunSettings();
-    VirtualFile configFile = toVirtualFile(element);
-    if (configFile != null && currentRunSettings.getConfigFile().equals(configFile.getPath())) {
-      return new RefactoringElementListener() {
-
-        @Override
-        public void elementMoved(@NotNull PsiElement newElement) {
-          refactorIt(newElement);
-        }
-
-        @Override
-        public void elementRenamed(@NotNull PsiElement newElement) {
-          refactorIt(newElement);
-        }
-
-        private void refactorIt(PsiElement newElement) {
-          VirtualFile newConfigFile = toVirtualFile(newElement);
-          if (newConfigFile != null) {
-            JstdRunSettings newRunSettings = new JstdRunSettings.Builder(currentRunSettings)
-                .setConfigFile(newConfigFile.getPath())
-                .build();
-            updateRunSettings(newRunSettings);
-          }
-        }
-
-      };
-    }
-    return null;
-  }
-
-  @Nullable
-  private RefactoringElementListener provideRefactoringJsFileListener(PsiElement element) {
-    final JstdRunSettings currentRunSettings = myRunConfiguration.getRunSettings();
-    VirtualFile jsFile = toVirtualFile(element);
-    if (jsFile != null && currentRunSettings.getJsFilePath().equals(jsFile.getPath())) {
-      return new RefactoringElementListener() {
-
-        @Override
-        public void elementMoved(@NotNull PsiElement newElement) {
-          refactorIt(newElement);
-        }
-
-        @Override
-        public void elementRenamed(@NotNull PsiElement newElement) {
-          refactorIt(newElement);
-        }
-
-        private void refactorIt(PsiElement newElement) {
-          VirtualFile newJsFile = toVirtualFile(newElement);
-          if (newJsFile != null) {
-            JstdRunSettings newRunSettings = new JstdRunSettings.Builder(currentRunSettings)
-                .setJSFilePath(newJsFile.getPath())
-                .build();
-            updateRunSettings(newRunSettings);
-          }
-        }
-
-      };
-    }
-    return null;
-  }
-
-  private void updateRunSettings(JstdRunSettings newRunSettings) {
-    boolean generatedName = myRunConfiguration.isGeneratedName();
-    myRunConfiguration.setRunSettings(newRunSettings);
-    if (generatedName) {
-      myRunConfiguration.setName(myRunConfiguration.suggestedName());
-    }
   }
 
   @Nullable
@@ -145,6 +46,61 @@ public class JstdRunConfigurationRefactoringHandler {
       return psiFileSystemItem.getVirtualFile();
     }
     return null;
+  }
+
+  private static class FilePathRefactoringElementListener implements RefactoringElementListener {
+
+    private final JstdRunConfiguration myConfiguration;
+    private final boolean myIsConfigFile;
+    private final boolean myIsJsTestFile;
+    private final boolean myIsAllInDirectory;
+
+    private FilePathRefactoringElementListener(@NotNull JstdRunConfiguration configuration,
+                                               boolean isConfigFile,
+                                               boolean isJsTestFile,
+                                               boolean isAllInDirectory) {
+      myConfiguration = configuration;
+      myIsConfigFile = isConfigFile;
+      myIsJsTestFile = isJsTestFile;
+      myIsAllInDirectory = isAllInDirectory;
+    }
+
+    @Override
+    public void elementMoved(@NotNull PsiElement newElement) {
+      refactorIt(newElement);
+    }
+
+    @Override
+    public void elementRenamed(@NotNull PsiElement newElement) {
+      refactorIt(newElement);
+    }
+
+    private void refactorIt(PsiElement newElement) {
+      VirtualFile newFile = toVirtualFile(newElement);
+      if (newFile != null) {
+        String newPath = newFile.getPath();
+        JstdRunSettings.Builder settingsBuilder = new JstdRunSettings.Builder(myConfiguration.getRunSettings());
+        if (myIsConfigFile) {
+          settingsBuilder.setConfigFile(newPath);
+        }
+        if (myIsJsTestFile) {
+          settingsBuilder.setJSFilePath(newPath);
+        }
+        if (myIsAllInDirectory) {
+          settingsBuilder.setDirectory(newPath);
+        }
+        updateRunSettings(settingsBuilder.build());
+      }
+    }
+
+    private void updateRunSettings(@NotNull JstdRunSettings newRunSettings) {
+      boolean generatedName = myConfiguration.isGeneratedName();
+      myConfiguration.setRunSettings(newRunSettings);
+      if (generatedName) {
+        myConfiguration.setName(myConfiguration.resetGeneratedName());
+      }
+    }
+
   }
 
 }
