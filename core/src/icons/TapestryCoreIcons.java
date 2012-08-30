012573d5b7e092e5f700d64c7c7300178dd5ed43
Maxim Shafirov <max@jetbrains.com>
2012-08-30 16:08:24 +0400

    Refer to originating class so IconLoader doesn't have to guess via stack frame reflection

--- core/src/icons/TapestryCoreIcons.java
+++ core/src/icons/TapestryCoreIcons.java
@@ -9,26 +9,30 @@ import javax.swing.*;
  * Don't repeat mistakes of others ;-)
  */
 public class TapestryCoreIcons {
-  public static final Icon Arrow_left = IconLoader.getIcon("/com/intellij/tapestry/core/icons/arrow_left.png"); // 16x16
-  public static final Icon Arrow_refresh = IconLoader.getIcon("/com/intellij/tapestry/core/icons/arrow_refresh.png"); // 16x16
-  public static final Icon Arrow_right = IconLoader.getIcon("/com/intellij/tapestry/core/icons/arrow_right.png"); // 16x16
-  public static final Icon Bullet_go = IconLoader.getIcon("/com/intellij/tapestry/core/icons/bullet_go.png"); // 16x16
-  public static final Icon CompactBasePackage = IconLoader.getIcon("/com/intellij/tapestry/core/icons/compactBasePackage.png"); // 16x16
-  public static final Icon Component = IconLoader.getIcon("/com/intellij/tapestry/core/icons/component.png"); // 16x16
-  public static final Icon Components = IconLoader.getIcon("/com/intellij/tapestry/core/icons/components.png"); // 16x16
-  public static final Icon Folder = IconLoader.getIcon("/com/intellij/tapestry/core/icons/folder.png"); // 16x16
-  public static final Icon GroupElementFiles = IconLoader.getIcon("/com/intellij/tapestry/core/icons/groupElementFiles.png"); // 16x16
-  public static final Icon House = IconLoader.getIcon("/com/intellij/tapestry/core/icons/house.png"); // 16x16
-  public static final Icon Inject_service = IconLoader.getIcon("/com/intellij/tapestry/core/icons/inject_service.png"); // 16x16
-  public static final Icon Ioc = IconLoader.getIcon("/com/intellij/tapestry/core/icons/ioc.png"); // 16x16
-  public static final Icon IocModule = IconLoader.getIcon("/com/intellij/tapestry/core/icons/iocModule.png"); // 16x16
-  public static final Icon Mixin = IconLoader.getIcon("/com/intellij/tapestry/core/icons/mixin.png"); // 16x16
-  public static final Icon Mixins = IconLoader.getIcon("/com/intellij/tapestry/core/icons/mixins.png"); // 16x16
-  public static final Icon Page = IconLoader.getIcon("/com/intellij/tapestry/core/icons/page.png"); // 16x16
-  public static final Icon Pages = IconLoader.getIcon("/com/intellij/tapestry/core/icons/pages.png"); // 16x16
-  public static final Icon Private_service = IconLoader.getIcon("/com/intellij/tapestry/core/icons/private_service.png"); // 16x16
-  public static final Icon Public_service = IconLoader.getIcon("/com/intellij/tapestry/core/icons/public_service.png"); // 16x16
-  public static final Icon Structure = IconLoader.getIcon("/com/intellij/tapestry/core/icons/structure.png"); // 16x16
-  public static final Icon Tapestry_logo_small = IconLoader.getIcon("/com/intellij/tapestry/core/icons/tapestry_logo_small.png"); // 16x18
-  public static final Icon TapestryToolWindow = IconLoader.getIcon("/com/intellij/tapestry/core/icons/tapestryToolWindow.png"); // 13x13
+  private static Icon load(String path) {
+    return IconLoader.getIcon(path, TapestryCoreIcons.class);
+  }
+
+  public static final Icon Arrow_left = load("/com/intellij/tapestry/core/icons/arrow_left.png"); // 16x16
+  public static final Icon Arrow_refresh = load("/com/intellij/tapestry/core/icons/arrow_refresh.png"); // 16x16
+  public static final Icon Arrow_right = load("/com/intellij/tapestry/core/icons/arrow_right.png"); // 16x16
+  public static final Icon Bullet_go = load("/com/intellij/tapestry/core/icons/bullet_go.png"); // 16x16
+  public static final Icon CompactBasePackage = load("/com/intellij/tapestry/core/icons/compactBasePackage.png"); // 16x16
+  public static final Icon Component = load("/com/intellij/tapestry/core/icons/component.png"); // 16x16
+  public static final Icon Components = load("/com/intellij/tapestry/core/icons/components.png"); // 16x16
+  public static final Icon Folder = load("/com/intellij/tapestry/core/icons/folder.png"); // 16x16
+  public static final Icon GroupElementFiles = load("/com/intellij/tapestry/core/icons/groupElementFiles.png"); // 16x16
+  public static final Icon House = load("/com/intellij/tapestry/core/icons/house.png"); // 16x16
+  public static final Icon Inject_service = load("/com/intellij/tapestry/core/icons/inject_service.png"); // 16x16
+  public static final Icon Ioc = load("/com/intellij/tapestry/core/icons/ioc.png"); // 16x16
+  public static final Icon IocModule = load("/com/intellij/tapestry/core/icons/iocModule.png"); // 16x16
+  public static final Icon Mixin = load("/com/intellij/tapestry/core/icons/mixin.png"); // 16x16
+  public static final Icon Mixins = load("/com/intellij/tapestry/core/icons/mixins.png"); // 16x16
+  public static final Icon Page = load("/com/intellij/tapestry/core/icons/page.png"); // 16x16
+  public static final Icon Pages = load("/com/intellij/tapestry/core/icons/pages.png"); // 16x16
+  public static final Icon Private_service = load("/com/intellij/tapestry/core/icons/private_service.png"); // 16x16
+  public static final Icon Public_service = load("/com/intellij/tapestry/core/icons/public_service.png"); // 16x16
+  public static final Icon Structure = load("/com/intellij/tapestry/core/icons/structure.png"); // 16x16
+  public static final Icon Tapestry_logo_small = load("/com/intellij/tapestry/core/icons/tapestry_logo_small.png"); // 16x18
+  public static final Icon TapestryToolWindow = load("/com/intellij/tapestry/core/icons/tapestryToolWindow.png"); // 13x13
 }
