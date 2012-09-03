9b63667b8220add2417ddc771986523f85663cf2
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2012-09-03 12:12:31 +0400

    find mapping by library name

--- JsTestDriver/src/com/google/jstestdriver/idea/assertFramework/library/JstdLibraryUtil.java
+++ JsTestDriver/src/com/google/jstestdriver/idea/assertFramework/library/JstdLibraryUtil.java
@@ -1,33 +1,19 @@
 package com.google.jstestdriver.idea.assertFramework.library;
 
-import com.google.common.collect.ImmutableList;
-import com.google.common.collect.Lists;
 import com.google.jstestdriver.idea.assertFramework.jstd.jsSrc.JstdDefaultAssertionFrameworkSrcMarker;
-import com.intellij.ProjectTopics;
 import com.intellij.lang.javascript.library.JSLibraryManager;
 import com.intellij.lang.javascript.library.JSLibraryMappings;
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.project.ProjectManager;
-import com.intellij.openapi.project.ProjectManagerAdapter;
-import com.intellij.openapi.project.ProjectManagerListener;
-import com.intellij.openapi.roots.ModuleRootAdapter;
-import com.intellij.openapi.roots.ModuleRootEvent;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.messages.MessageBusConnection;
-import com.intellij.webcore.libraries.ScriptingLibraryManager;
-import com.intellij.webcore.libraries.ScriptingLibraryMappings;
 import com.intellij.webcore.libraries.ScriptingLibraryModel;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
@@ -39,7 +25,6 @@ public class JstdLibraryUtil {
 
   private static final AtomicBoolean INITIALIZED = new AtomicBoolean(false);
   private static volatile Boolean JSTD_LIBRARY_EXISTS = null;
-  private static volatile List<VirtualFile> JSTD_LIBRARY_MAPPINGS = null;
 
   private static void init(@NotNull Project project) {
     if (INITIALIZED.compareAndSet(false, true)) {
@@ -49,15 +34,11 @@ public class JstdLibraryUtil {
 
   private static void doInit(@NotNull final Project project) {
     ApplicationManager.getApplication().runReadAction(new Runnable() {
-      @SuppressWarnings("AssignmentToStaticFieldFromInstanceMethod")
       @Override
       public void run() {
-        JSLibraryManager libraryManager = ServiceManager.getService(project, JSLibraryManager.class);
+        JSLibraryManager libraryManager = JSLibraryManager.getInstance(project);
         LibraryTable libraryTable = libraryManager.getLibraryTable(ScriptingLibraryModel.LibraryLevel.GLOBAL);
         libraryTable.addListener(new MyLibraryChangeWatcher());
-
-        ProjectManagerListener rootListener = new ProjectRootsWatcher(project);
-        ProjectManager.getInstance().addProjectManagerListener(rootListener);
       }
     });
   }
@@ -88,7 +69,7 @@ public class JstdLibraryUtil {
           if (libVirtualFile == null) {
             return false;
           }
-          ScriptingLibraryManager libraryManager = ServiceManager.getService(project, JSLibraryManager.class);
+          JSLibraryManager libraryManager = JSLibraryManager.getInstance(project);
           for (ScriptingLibraryModel libraryModel : libraryManager.getAllLibraries()) {
             String libraryName = libraryModel.getName();
             if (libraryName != null && libraryName.startsWith(LIBRARY_NAME)) {
@@ -106,9 +87,10 @@ public class JstdLibraryUtil {
   }
 
   private static boolean isCorrectMapping(@NotNull Project project, @NotNull VirtualFile file) {
-    List<VirtualFile> roots = getCachedUsageScope(project);
+    JSLibraryMappings jsLibraryMappings = JSLibraryMappings.getInstance(project);
+    List<VirtualFile> usageScope = jsLibraryMappings.getMappingsByLibraryNamePrefix(LIBRARY_NAME);
     String filePath = file.getPath();
-    for (VirtualFile root : roots) {
+    for (VirtualFile root : usageScope) {
       if (root == null) {
         return true;
       }
@@ -117,44 +99,6 @@ public class JstdLibraryUtil {
       }
     }
     return false;
-  }
-
-  private static List<VirtualFile> getCachedUsageScope(@NotNull final Project project) {
-    List<VirtualFile> mappings = JSTD_LIBRARY_MAPPINGS;
-    if (mappings == null) {
-      mappings = ApplicationManager.getApplication().runReadAction(new Computable<List<VirtualFile>>() {
-        @Override
-        public List<VirtualFile> compute() {
-          return getUsageScope(project);
-        }
-      });
-      JSTD_LIBRARY_MAPPINGS = mappings;
-    }
-    return mappings;
-  }
-
-  private static List<VirtualFile> getUsageScope(@NotNull Project project) {
-    ApplicationManager.getApplication().assertReadAccessAllowed();
-
-    JSLibraryMappings libraryMappings = ServiceManager.getService(project, JSLibraryMappings.class);
-    Map<VirtualFile, ScriptingLibraryModel> allMappings = libraryMappings.getMappings();
-    List<VirtualFile> roots = Lists.newArrayList();
-    for (Map.Entry<VirtualFile, ScriptingLibraryModel> entry : allMappings.entrySet()) {
-      ScriptingLibraryModel libraryModel = entry.getValue();
-      final List<ScriptingLibraryModel> libraryModels;
-      if (libraryModel instanceof ScriptingLibraryMappings.CompoundLibrary) {
-        ScriptingLibraryMappings.CompoundLibrary compoundLibrary = (ScriptingLibraryMappings.CompoundLibrary) libraryModel;
-        libraryModels = ImmutableList.copyOf(compoundLibrary.getLibraries());
-      } else {
-        libraryModels = Collections.singletonList(libraryModel);
-      }
-      for (ScriptingLibraryModel model : libraryModels) {
-        if (LIBRARY_NAME.equals(model.getName())) {
-          roots.add(entry.getKey());
-        }
-      }
-    }
-    return roots;
   }
 
   private static class MyLibraryChangeWatcher implements LibraryTable.Listener {
@@ -178,21 +122,4 @@ public class JstdLibraryUtil {
     }
   }
 
-  private static class ProjectRootsWatcher extends ProjectManagerAdapter {
-    private ProjectRootsWatcher(@NotNull Project project) {
-      projectOpened(project);
-    }
-
-    @Override
-    public void projectOpened(Project project) {
-      MessageBusConnection conn = project.getMessageBus().connect(project);
-      conn.subscribe(ProjectTopics.PROJECT_ROOTS, new ModuleRootAdapter() {
-        @SuppressWarnings("AssignmentToStaticFieldFromInstanceMethod")
-        @Override
-        public void rootsChanged(final ModuleRootEvent event) {
-          JSTD_LIBRARY_MAPPINGS = null;
-        }
-      });
-    }
-  }
 }
