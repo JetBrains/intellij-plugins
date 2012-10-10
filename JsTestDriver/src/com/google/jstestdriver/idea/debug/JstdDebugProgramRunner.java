42ffd2b6da99951430e3c37f48faebd8cb1aaa2f
develar <develar@gmail.com>
2012-10-10 17:32:29 +0400

    WI-12768

--- JsTestDriver/src/com/google/jstestdriver/idea/debug/JstdDebugProgramRunner.java
+++ JsTestDriver/src/com/google/jstestdriver/idea/debug/JstdDebugProgramRunner.java
@@ -5,6 +5,7 @@ import com.google.jstestdriver.idea.execution.JstdRunConfiguration;
 import com.google.jstestdriver.idea.execution.JstdRunConfigurationVerifier;
 import com.google.jstestdriver.idea.execution.JstdTestRunnerCommandLineState;
 import com.google.jstestdriver.idea.server.ui.JstdToolWindowPanel;
+import com.intellij.chromeConnector.connection.V8Connection;
 import com.intellij.chromeConnector.debugger.ChromeDebuggerEngine;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.ExecutionResult;
@@ -18,12 +19,15 @@ import com.intellij.execution.runners.GenericProgramRunner;
 import com.intellij.execution.ui.RunContentDescriptor;
 import com.intellij.javascript.debugger.engine.JSDebugEngine;
 import com.intellij.javascript.debugger.impl.DebuggableFileFinder;
+import com.intellij.javascript.debugger.impl.JSDebugProcess;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.util.Consumer;
 import com.intellij.xdebugger.XDebugProcess;
 import com.intellij.xdebugger.XDebugProcessStarter;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebuggerManager;
+import org.chromium.sdk.JavascriptVm;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -34,7 +38,6 @@ import java.io.PrintWriter;
  * @author Sergey Simonchik
  */
 public class JstdDebugProgramRunner extends GenericProgramRunner {
-
   private static final String DEBUG_RUNNER_ID = JstdDebugProgramRunner.class.getSimpleName();
 
   @NotNull
@@ -97,10 +100,17 @@ public class JstdDebugProgramRunner extends GenericProgramRunner {
     XDebugSession xDebugSession = xDebuggerManager.startSession(this, env, contentToReuse, new XDebugProcessStarter() {
       @NotNull
       public XDebugProcess start(@NotNull final XDebugSession session) {
-        return debugEngine.createDebugProcess(session, fileFinder, connection, url, executionResult);
+        JSDebugProcess debugProcess = debugEngine.createDebugProcess(session, fileFinder, connection, url, executionResult);
+        ((V8Connection)debugProcess.getBrowserConnection()).queueRequest(new Consumer<JavascriptVm>() {
+          @Override
+          public void consume(JavascriptVm javascriptVm) {
+            resumeJstdClientRunning(executionResult.getProcessHandler());
+          }
+        });
+        return debugProcess;
       }
     });
-    resumeJstdClientRunning(executionResult.getProcessHandler());
+
 
     return xDebugSession.getRunContentDescriptor();
   }
@@ -112,5 +122,4 @@ public class JstdDebugProgramRunner extends GenericProgramRunner {
     writer.println(TestRunner.DEBUG_SESSION_STARTED);
     writer.flush();
   }
-
 }
