21c84296631e547373da1208c9d7032dc4a78c71
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2012-11-02 14:24:21 +0400

    flex-jps module moved and renamed

---
+++ flex/jps-plugin/src/com/intellij/jps/flex/build/FlexBuilderService.java
@@ -0,0 +1,29 @@
+package com.intellij.jps.flex.build;
+
+import com.intellij.flex.build.FlexBuildTargetType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.jps.builders.BuildTargetType;
+import org.jetbrains.jps.incremental.BuilderService;
+import org.jetbrains.jps.incremental.ModuleLevelBuilder;
+import org.jetbrains.jps.incremental.TargetBuilder;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class FlexBuilderService extends BuilderService {
+
+  public List<? extends BuildTargetType<?>> getTargetTypes() {
+    return Collections.singletonList(FlexBuildTargetType.INSTANCE);
+  }
+
+  @NotNull
+  public List<? extends TargetBuilder<?, ?>> createBuilders() {
+    return Arrays.asList(/*new FlexBuilder()*/);
+  }
+
+  @NotNull
+  public List<? extends ModuleLevelBuilder> createModuleLevelBuilders() {
+    return Arrays.asList(new FlexResourceBuilder());
+  }
+}
