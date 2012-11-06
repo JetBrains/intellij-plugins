80aecdd6f62657b8bb880315d9189dce12874c08
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-11-06 16:24:41 +0400

    IDEA-93965 Cucumber for Groovy: Find Step Usages doesn't work for Scenario Outline steps

--- cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/steps/search/GrStepDefinitionPresentationProvider.java
+++ cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/steps/search/GrStepDefinitionPresentationProvider.java
@@ -3,6 +3,8 @@ package org.jetbrains.plugins.cucumber.groovy.steps.search;
 import com.intellij.navigation.ItemPresentation;
 import com.intellij.navigation.ItemPresentationProvider;
 import com.intellij.navigation.ItemPresentationProviders;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.CucumberBundle;
 import org.jetbrains.plugins.cucumber.groovy.GrCucumberUtil;
@@ -14,6 +16,8 @@ import javax.swing.*;
  * @author Max Medvedev
  */
 public class GrStepDefinitionPresentationProvider implements ItemPresentationProvider<GrMethodCall> {
+  private static final Logger LOG = Logger.getInstance(GrStepDefinitionPresentationProvider.class);
+
   @Nullable
   @Override
   public ItemPresentation getPresentation(final GrMethodCall item) {
@@ -30,7 +34,10 @@ public class GrStepDefinitionPresentationProvider implements ItemPresentationPro
       @Nullable
       @Override
       public String getLocationString() {
-        return ItemPresentationProviders.getItemPresentation(item.getContainingFile()).getPresentableText();
+        final PsiFile file = item.getContainingFile();
+        final ItemPresentation presentation = ItemPresentationProviders.getItemPresentation(file);
+        LOG.assertTrue(presentation != null, file.getClass().getName());
+        return presentation.getPresentableText();
       }
 
       @Nullable
