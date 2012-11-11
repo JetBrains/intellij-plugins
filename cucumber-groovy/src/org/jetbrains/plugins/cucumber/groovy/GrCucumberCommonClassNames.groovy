b4245a4e07af2f1f5e68d474492ae8ef4fc42f7a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2012-11-11 13:47:24 +0100

    Attempt to fix brain-dead Groovy compilation

--- cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/GrCucumberCommonClassNames.groovy
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.cucumber.groovy
-
-/**
- * @author Max Medvedev
- */
-interface GrCucumberCommonClassNames {
-  String CUCUMBER_RUNTIME_GROOVY_HOOKS = 'cucumber.runtime.groovy.Hooks'
-  String CUCUMBER_RUNTIME_GROOVY = 'cucumber.runtime.groovy'
-}
---
+++ cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/GrCucumberCommonClassNames.java
@@ -0,0 +1,9 @@
+package org.jetbrains.plugins.cucumber.groovy;
+
+/**
+ * @author Max Medvedev
+ */
+interface GrCucumberCommonClassNames {
+  String CUCUMBER_RUNTIME_GROOVY_HOOKS = "cucumber.runtime.groovy.Hooks";
+  String CUCUMBER_RUNTIME_GROOVY = "cucumber.runtime.groovy";
+}
