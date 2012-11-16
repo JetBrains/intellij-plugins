28429fa98194bc7abfddbea108a0485834f58c0f
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2012-11-16 18:14:33 +0400

    IDEA-61855 While analysing FlexUnit output, accessing filesystem may be inacceptably slow IDEA-74969 IDEA freezes during GXT development (console filtering is slow)

--- flex/src/com/intellij/lang/javascript/flex/FlexStackTraceFilter.java
+++ flex/src/com/intellij/lang/javascript/flex/FlexStackTraceFilter.java
@@ -2,18 +2,15 @@ package com.intellij.lang.javascript.flex;
 
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.filters.HyperlinkInfo;
-import com.intellij.execution.filters.OpenFileHyperlinkInfo;
+import com.intellij.flex.FlexCommonUtils;
 import com.intellij.ide.util.DefaultPsiElementCellRenderer;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.PopupChooserBuilder;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.psi.PsiFile;
@@ -21,44 +18,24 @@ import com.intellij.psi.PsiManager;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiUtilCore;
-import com.intellij.ui.GuiUtils;
 import com.intellij.ui.awt.RelativePoint;
 import com.intellij.ui.components.JBList;
+import com.intellij.util.PathUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.StringTokenizer;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FlexStackTraceFilter implements Filter {
 
-  // taken from RStackTraceFilter
-
-  // Disk name for Windows
-  private static final String DISK_PATTERN = "\\p{Alpha}:";
-
-  private static final String FILENAME_PATTERN = "[^:/\\\\]+";
-
-  // Extension pattern
-  public static final String EXT_PATTERN = "\\.[\\p{Graph}^:/\\\\\\?]+";
-
-  private static final String LINE_NUM_PATTERN = ":(\\d+)";
-  // Path use both type separators(for Unix and Windows )
-  private static final String PATTERN = "(?:/|\\\\)[^:]*" + FILENAME_PATTERN + "(?:" + EXT_PATTERN + ")?";
-
-  private static final Pattern WIN_CPATTERN = Pattern.compile("\\[((" + DISK_PATTERN + PATTERN + ")" + LINE_NUM_PATTERN + ")\\]");
-
-  private static final Pattern UNIX_CPATTERN = Pattern.compile("\\[((" + PATTERN + ")" + LINE_NUM_PATTERN + ")\\]");
-
+  public static final String GLOBAL_PREFIX = "global/";
   private final Project myProject;
+
+  private static final String AT = "at ";
 
   public FlexStackTraceFilter(Project project) {
     myProject = project;
@@ -66,111 +43,97 @@ public class FlexStackTraceFilter implements Filter {
 
   @Nullable
   public Result applyFilter(final String line, final int entireLength) {
-    if (line.indexOf('[') < 0) {
-      return null;
+    //    at org.flexunit::Assert$/fail()[E:\hudson\jobs\FlexUnit4-Flex4.1\workspace\FlexUnit4\src\org\flexunit\Assert.as:294]
+    //    at global/org.flexunit.asserts::fail()[E:\hudson\jobs\FlexUnit4-Flex4.1\workspace\FlexUnit4\src\org\flexunit\asserts\fail.as:39]
+    //    at foo::SomeTest/testSmth()[/untitled5/app/testSrc/foo/SomeTest.as:19]
+    //    at Function/http://adobe.com/AS3/2006/builtin::apply()
+    //    at mx.core::UIComponent/set initialized()[C:\autobuild\3.3.0\frameworks\projects\framework\src\mx\core\UIComponent.as:1169]
+
+    final String trimmed = line.trim();
+    final int bracketOpenIndex = line.lastIndexOf('[');
+
+    if (!trimmed.startsWith(AT) || bracketOpenIndex < 0 || !trimmed.endsWith("]")) return null;
+
+    final int bracketCloseIndex = line.lastIndexOf(']');
+    final String pathAndLineNumber = FileUtil.toSystemIndependentName(line.substring(bracketOpenIndex + 1, bracketCloseIndex));
+
+    String filePath = pathAndLineNumber; // "E:\hudson\jobs\FlexUnit4-Flex4.1\workspace\FlexUnit4\src\org\flexunit\Assert.as:294"
+    int lineNumber = -1;
+
+    final int colonIndex = pathAndLineNumber.lastIndexOf(':');
+    if (colonIndex > 0) {
+      try {
+        lineNumber = Integer.parseInt(pathAndLineNumber.substring(colonIndex + 1)) - 1; // 294 - 1 = 293
+        filePath =
+          pathAndLineNumber.substring(0, colonIndex);  // "E:\hudson\jobs\FlexUnit4-Flex4.1\workspace\FlexUnit4\src\org\flexunit\Assert.as"
+      }
+      catch (NumberFormatException ignore) {/*unlucky*/}
     }
 
-    Pattern p = SystemInfo.isWindows ? WIN_CPATTERN : UNIX_CPATTERN;
-    Matcher m = p.matcher(line);
-    if (!m.find()) {
-      p = SystemInfo.isWindows ? UNIX_CPATTERN : WIN_CPATTERN;
-      m = p.matcher(line);
-      if (!m.find()) {
-        return null;
+    if (filePath.startsWith("//")) return null;  // UNC path
+
+    final String fileName = PathUtil.getFileName(filePath); // Assert.as
+    if (!FlexCommonUtils.isSourceFile(fileName)) return null; // Flex filter cares only about *.as and *.mxml
+
+    final int atIndex = line.indexOf(AT);
+    String fqnInfo = line.substring(atIndex + AT.length(), bracketOpenIndex).trim(); // "org.flexunit::Assert$/fail()"
+    fqnInfo = StringUtil.trimStart(fqnInfo, GLOBAL_PREFIX); // for case like "global/org.flexunit.asserts::fail()"
+
+    final int slashOrParenIndex = StringUtil.indexOfAny(fqnInfo, "/(");
+
+    if (slashOrParenIndex > 0) {
+      String somethingLikeFqn = fqnInfo.substring(0, slashOrParenIndex);  // "org.flexunit::Assert$"
+
+      if (somethingLikeFqn.endsWith("$")) {
+        somethingLikeFqn = somethingLikeFqn.substring(0, somethingLikeFqn.length() - 1); // "org.flexunit::Assert"
+      }
+
+      final int dotIndex = fileName.lastIndexOf('.');
+      final String dotExtension = dotIndex < 0 ? "" : fileName.substring(dotIndex);
+      final String fileNameWithoutExtension = FileUtil.getNameWithoutExtension(fileName);
+
+      if (somethingLikeFqn.equals(fileNameWithoutExtension) || somethingLikeFqn.endsWith("::" + fileNameWithoutExtension)) {
+        final String relativePath =
+          StringUtil.replace(somethingLikeFqn, "::", "/").replace('.', '/') + dotExtension; // "org/flexunit/Assert.as"
+
+        if (filePath.endsWith(relativePath)) {
+          final int textStartOffset = entireLength - line.length();
+          final int highlightEndOffset = textStartOffset + bracketCloseIndex;
+
+          return applyFlexStackTraceFilter(filePath, relativePath, lineNumber, highlightEndOffset);
+        }
       }
     }
 
-    final int textStartOffset = entireLength - line.length();
-    final int highlightStartOffset = textStartOffset + m.start(1);
-    final int highlightEndOffset = textStartOffset + m.end(1);
-    int lineNumber = 0;
-    try {
-      lineNumber = StringUtil.isEmpty(m.group(3)) ? 0 : Integer.parseInt(m.group(3)) - 1;
-    }
-    catch (NumberFormatException ignore) {/*ignore*/}
-
-    final String filePath = m.group(2);
-
-    final File file = new File(filePath);
-    if (!file.isFile()) {
-      return applyFlexStackTraceFilter(line, filePath, highlightEndOffset, lineNumber);
-    }
-
-    final Ref<VirtualFile> vFile = new Ref<VirtualFile>();
-
-    try {
-      GuiUtils.runOrInvokeAndWait(new Runnable() {
-        public void run() {
-          vFile.set(LocalFileSystem.getInstance().refreshAndFindFileByIoFile(file));
-        }
-      });
-    }
-    catch (InvocationTargetException e) {
-      // skip
-    }
-    catch (InterruptedException e) {
-      // skip
-    }
-
-    if (vFile.isNull()) {
-      return null;
-    }
-
-    final OpenFileHyperlinkInfo info = new OpenFileHyperlinkInfo(myProject, vFile.get(), lineNumber);
-    return new Result(highlightStartOffset, highlightEndOffset, info);
+    return null;
   }
 
   @Nullable
-  private Result applyFlexStackTraceFilter(final String line,
-                                           final String _filePath,
-                                           final int highlightEndOffset,
-                                           final int lineNumber) {
-    // 	at mx.core::UIComponent/set initialized()[C:\autobuild\3.3.0\frameworks\projects\framework\src\mx\core\UIComponent.as:1169]
-    // 	at Main/launchTask()[C:\path\Main.mxml:144]
+  private Result applyFlexStackTraceFilter(final String filePath,
+                                           final String matchingRelativePath,
+                                           final int lineNumber,
+                                           final int highlightEndOffset) {
+    final Collection<VirtualFile> result = new ArrayList<VirtualFile>();
 
-    final String filePath = FileUtil.toSystemIndependentName(_filePath);
-    final String fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
-    final String fileNameWithoutExtension = FileUtil.getNameWithoutExtension(fileName);
+    final Collection<VirtualFile> files =
+      FilenameIndex.getVirtualFilesByName(myProject, PathUtil.getFileName(filePath), GlobalSearchScope.allScope(myProject));
 
-    final String AT = "at ";
-    final int atIndex = line.indexOf(AT);
-    final int slashIndex = line.indexOf("/", atIndex);
-
-    if (atIndex >= 0 && slashIndex > atIndex) {
-      final String somethingLikeFqn = line.substring(atIndex + AT.length(), slashIndex);
-
-      if (somethingLikeFqn.equals(fileNameWithoutExtension) || somethingLikeFqn.endsWith("::" + fileNameWithoutExtension)) {
-        final StringTokenizer tokenizer = new StringTokenizer(somethingLikeFqn, ".:", false);
-        final StringBuilder relativePathBuffer = new StringBuilder();
-        while (tokenizer.hasMoreTokens()) {
-          if (relativePathBuffer.length() > 0) {
-            relativePathBuffer.append('/');
-          }
-          relativePathBuffer.append(tokenizer.nextToken());
-        }
-        relativePathBuffer.append('.');
-        relativePathBuffer.append(FileUtil.getExtension(fileName));
-
-        final String relativePathToLookFor = relativePathBuffer.toString();
-        if (filePath.endsWith(relativePathToLookFor)) {
-          final Collection<VirtualFile> result = new ArrayList<VirtualFile>();
-
-          final Collection<VirtualFile> files =
-            FilenameIndex.getVirtualFilesByName(myProject, fileName, GlobalSearchScope.allScope(myProject));
-
-          for (final VirtualFile file : files) {
-            if (file.getPath().endsWith(relativePathToLookFor)) {
-              result.add(file);
-            }
-          }
-
-          if (!result.isEmpty()) {
-            final int highlightStartOffset =
-              highlightEndOffset - relativePathToLookFor.length() - (lineNumber > 0 ? (String.valueOf(lineNumber).length() + 1) : 0);
-            return new Result(highlightStartOffset, highlightEndOffset, new OpenOneOfSeveralFilesHyperlinkInfo(result, lineNumber));
-          }
-        }
+    for (final VirtualFile file : files) {
+      if (file.getPath().endsWith(matchingRelativePath)) {
+        result.add(file);
       }
+    }
+
+    if (!result.isEmpty()) {
+      final int colonLineNumberLength = lineNumber > 0 ? (":".length() + String.valueOf(lineNumber).length()) : 0;
+      int highlightingLength = matchingRelativePath.length() + colonLineNumberLength;
+
+      if (result.size() == 1 && result.iterator().next().getPath().equals(filePath)) {
+        highlightingLength = filePath.length() + colonLineNumberLength;
+      }
+
+      final int highlightStartOffset = highlightEndOffset - highlightingLength;
+      return new Result(highlightStartOffset, highlightEndOffset, new OpenOneOfSeveralFilesHyperlinkInfo(result, lineNumber));
     }
 
     return null;
