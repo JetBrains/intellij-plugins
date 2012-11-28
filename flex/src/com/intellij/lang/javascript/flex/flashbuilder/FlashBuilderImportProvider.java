b3365785c1d64599a3fd01c5646226d9e1d8bdc2
Dmitry Avdeev <Dmitry.Avdeev@jetbrains.com>
2012-11-28 19:29:13 +0400

    Importing for Flex & Eclipse (cherry picked from commit 0c93a7b9027fc56009498a1220bcd1dcc672dfce)

--- flex/src/com/intellij/lang/javascript/flex/flashbuilder/FlashBuilderImportProvider.java
+++ flex/src/com/intellij/lang/javascript/flex/flashbuilder/FlashBuilderImportProvider.java
@@ -2,15 +2,37 @@ package com.intellij.lang.javascript.flex.flashbuilder;
 
 import com.intellij.ide.util.projectWizard.ModuleWizardStep;
 import com.intellij.ide.util.projectWizard.WizardContext;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
+import org.jetbrains.annotations.Nullable;
 
 public class FlashBuilderImportProvider extends ProjectImportProvider {
 
+  private final FlashBuilderOpenProcessor myProcessor;
+
   public FlashBuilderImportProvider(final FlashBuilderImporter builder) {
     super(builder);
+    myProcessor = new FlashBuilderOpenProcessor(builder);
   }
 
   public ModuleWizardStep[] createSteps(final WizardContext context) {
     return new ModuleWizardStep[]{new SelectDirWithFlashBuilderProjectsStep(context), new SelectFlashBuilderImportedProjectsStep(context)};
+  }
+
+  @Override
+  protected boolean canImportFromFile(VirtualFile file) {
+    return myProcessor.canOpenProject(file);
+  }
+
+  @Override
+  public String getPathToBeImported(VirtualFile file) {
+    return file.getPath();
+  }
+
+  @Nullable
+  @Override
+  public String getFileSample() {
+    return "<b>Flash Builder</b> project file (" + StringUtil.join(myProcessor.getSupportedExtensions(), ", ") + ")";
   }
 }
