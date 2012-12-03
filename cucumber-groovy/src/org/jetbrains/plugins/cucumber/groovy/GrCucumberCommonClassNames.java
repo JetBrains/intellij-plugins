204b85f0b039ee19a41c853f8230aa1f836216d6
Max Medvedev <maxim.medvedev@jetbrains.com>
2012-12-03 15:30:45 +0400

    IDEA-96316 Cucumber-groovy: cucumber 1.1.X API changes

--- cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/GrCucumberCommonClassNames.java
+++ cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/GrCucumberCommonClassNames.java
@@ -2,7 +2,21 @@ package org.jetbrains.plugins.cucumber.groovy;
 
 import org.jetbrains.annotations.NonNls;
 
-public interface GrCucumberCommonClassNames {
-  @NonNls String CUCUMBER_RUNTIME_GROOVY_HOOKS = "cucumber.runtime.groovy.Hooks";
-  @NonNls String CUCUMBER_RUNTIME_GROOVY = "cucumber.runtime.groovy";
+public class GrCucumberCommonClassNames {
+  @NonNls private static final String CUCUMBER_RUNTIME_GROOVY_HOOKS_1_0 = "cucumber.runtime.groovy.Hooks";
+  @NonNls private static final String CUCUMBER_RUNTIME_GROOVY_1_0 = "cucumber.runtime.groovy";
+
+  @NonNls private static final String CUCUMBER_RUNTIME_GROOVY_HOOKS_1_1 = "cucumber.api.groovy.Hooks";
+  @NonNls private static final String CUCUMBER_RUNTIME_GROOVY_1_1 = "cucumber.api.groovy";
+
+
+  public static boolean isHookClassName(String qname) {
+    return CUCUMBER_RUNTIME_GROOVY_HOOKS_1_0.equals(qname) || CUCUMBER_RUNTIME_GROOVY_HOOKS_1_1.equals(qname);
+  }
+
+  public  static boolean isCucumberRuntimeGroovyPackage(String packageName) {
+    return CUCUMBER_RUNTIME_GROOVY_1_0.equals(packageName) || CUCUMBER_RUNTIME_GROOVY_1_1.equals(packageName);
+  }
+
+  private GrCucumberCommonClassNames() {}
 }
---
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/config/CucumberConfigUtil.java
@@ -0,0 +1,88 @@
+package org.jetbrains.plugins.cucumber.java.config;
+
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleUtilCore;
+import com.intellij.openapi.roots.*;
+import com.intellij.openapi.roots.libraries.Library;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.CachedValuesManager;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Max Medvedev
+ */
+public class CucumberConfigUtil {
+  private static final Pattern CUCUMBER_PATTERN = Pattern.compile("cucumber-core-(.*)\\.jar");
+
+  @NonNls
+  private static final String CUCUMBER_CLI_MAIN_1_0 = "cucumber.cli.Main";
+
+  @NonNls
+  private static final String CUCUMBER_API_CLI_MAIN_1_1 = "cucumber.api.cli.Main";
+
+  @Nullable
+  public static String getCucumberCoreVersion(@NotNull PsiElement place) {
+    final Module module = ModuleUtilCore.findModuleForPsiElement(place);
+    if (module == null) return null;
+
+    return CachedValuesManager.getManager(module.getProject()).getCachedValue(module, new CachedValueProvider<String>() {
+      @Nullable
+      @Override
+      public Result<String> compute() {
+        return Result.create(getCucumberCoreVersionImpl(module), ProjectRootManager.getInstance(module.getProject()));
+      }
+    });
+  }
+
+  @Nullable
+  private static String getCucumberCoreVersionImpl(Module module) {
+    for (OrderEntry orderEntry : ModuleRootManager.getInstance(module).getOrderEntries()) {
+      if (orderEntry instanceof LibraryOrderEntry) {
+        final String libraryName = ((LibraryOrderEntry)orderEntry).getLibraryName();
+        final Library library = ((LibraryOrderEntry)orderEntry).getLibrary();
+
+        //libraryName is null for simple jar entries
+        if ((libraryName == null || libraryName.toLowerCase().contains("cucumber")) && library != null) {
+          final VirtualFile[] files = library.getFiles(OrderRootType.CLASSES);
+          for (VirtualFile file : files) {
+            final String version = getVersionByFile(file);
+            if (version != null) return version;
+          }
+        }
+      }
+    }
+
+    return getSimpleVersionFromMainClass(module);
+  }
+
+  private static String getSimpleVersionFromMainClass(Module module) {
+    final JavaPsiFacade facade = JavaPsiFacade.getInstance(module.getProject());
+
+    final PsiClass oldMain = facade.findClass(CUCUMBER_CLI_MAIN_1_0, module.getModuleWithLibrariesScope());
+    if (oldMain != null) return "1.0";
+
+    final PsiClass newMain = facade.findClass(CUCUMBER_API_CLI_MAIN_1_1, module.getModuleWithLibrariesScope());
+    if (newMain != null) return "1.1";
+
+    return null;
+  }
+
+  @Nullable
+  private static String getVersionByFile(VirtualFile file) {
+    final String name = file.getName();
+    final Matcher matcher = CUCUMBER_PATTERN.matcher(name);
+    if (matcher.matches() && matcher.groupCount() == 1) {
+      return matcher.group(1);
+    }
+    return null;
+  }
+}
