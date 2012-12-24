6c57d7aa0e8ac809535872cf532ffb2f80f20c81
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2012-12-24 20:12:44 +0400

    BuildRootDescriptor default impl

--- flex/flex-shared/src/com/intellij/flex/build/FlexSourceRootDescriptor.java
+++ flex/flex-shared/src/com/intellij/flex/build/FlexSourceRootDescriptor.java
@@ -1,45 +1,12 @@
 package com.intellij.flex.build;
 
-import com.intellij.openapi.util.io.FileUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.jps.builders.BuildRootDescriptor;
 import org.jetbrains.jps.builders.BuildTarget;
-import org.jetbrains.jps.cmdline.ProjectDescriptor;
+import org.jetbrains.jps.builders.impl.BuildRootDescriptorImpl;
 
 import java.io.File;
-import java.io.FileFilter;
 
-class FlexSourceRootDescriptor extends BuildRootDescriptor {
-  private final File myRoot;
-  private BuildTarget myTarget;
-
+class FlexSourceRootDescriptor extends BuildRootDescriptorImpl {
   public FlexSourceRootDescriptor(final BuildTarget target, final File root) {
-    myTarget = target;
-    myRoot = root;
-  }
-
-  @Override
-  public String getRootId() {
-    return FileUtil.toSystemIndependentName(myRoot.getAbsolutePath());
-  }
-
-  @Override
-  public File getRootFile() {
-    return myRoot;
-  }
-
-  @Override
-  public BuildTarget<?> getTarget() {
-    return myTarget;
-  }
-
-  @Override
-  public FileFilter createFileFilter(@NotNull ProjectDescriptor descriptor) {
-    return new FileFilter() {
-      @Override
-      public boolean accept(File pathname) {
-        return true;
-      }
-    };
+    super(target, root);
   }
 }
