7db71bedc91321c1f4c7ae15b4dea32ec26ea8fd
sergey.vasiliev <sergey.vasiliev@jetbrains.com>
2013-01-30 15:17:45 +0100

    spring mixed model (fileset configs)

--- struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsHighlightingSpringTest.java
+++ struts2/plugin/src-test/com/intellij/struts2/dom/struts/StrutsHighlightingSpringTest.java
@@ -23,7 +23,6 @@ import com.intellij.spring.facet.SpringFacet;
 import com.intellij.spring.facet.SpringFacetConfiguration;
 import com.intellij.spring.facet.SpringFacetType;
 import com.intellij.spring.facet.SpringFileSet;
-import com.intellij.spring.facet.appContextDescriptors.XmlContextDescriptor;
 import com.intellij.struts2.Struts2ProjectDescriptorBuilder;
 import com.intellij.testFramework.LightProjectDescriptor;
 import com.intellij.util.containers.ContainerUtil;
@@ -118,7 +117,7 @@ public class StrutsHighlightingSpringTest extends StrutsLightHighlightingTestCas
 
     final SpringFacetConfiguration configuration = springFacet.getConfiguration();
     final Set<SpringFileSet> list = configuration.getFileSets();
-    @NonNls final SpringFileSet fileSet = new XmlContextDescriptor("", "default", configuration);
+    @NonNls final SpringFileSet fileSet = new SpringFileSet("", "default", configuration);
     list.add(fileSet);
 
     for (final String springXmlPath : springXmlPaths) {
