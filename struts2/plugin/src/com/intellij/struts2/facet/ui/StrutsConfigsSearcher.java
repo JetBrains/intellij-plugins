c38e95eb32453b00b6197238e0195c15785d3ab3
Yann Cébron <yann.cebron@jetbrains.com>
2013-02-26 09:37:28 +0100

    use ConfigFileSearcher

--- struts2/plugin/src/com/intellij/struts2/facet/ui/StrutsConfigsSearcher.java
+++ struts2/plugin/src/com/intellij/struts2/facet/ui/StrutsConfigsSearcher.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007 The authors
+ * Copyright 2013 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,60 +15,43 @@
 package com.intellij.struts2.facet.ui;
 
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleUtil;
-import com.intellij.openapi.vfs.JarFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.xml.XmlFile;
 import com.intellij.struts2.dom.struts.StrutsRoot;
-import com.intellij.util.containers.MultiMap;
+import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xml.DomFileElement;
 import com.intellij.util.xml.DomService;
+import com.intellij.xml.config.ConfigFileSearcher;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Yann C&eacute;bron
  */
-public class StrutsConfigsSearcher {
+public class StrutsConfigsSearcher extends ConfigFileSearcher {
 
-  private final Module module;
-  private final MultiMap<Module, PsiFile> myFiles = new MultiMap<Module, PsiFile>();
-  private final MultiMap<VirtualFile, PsiFile> myJars = new MultiMap<VirtualFile, PsiFile>();
-
-  public StrutsConfigsSearcher(final Module module) {
-    this.module = module;
+  public StrutsConfigsSearcher(@NotNull Module module) {
+    super(module, module.getProject());
   }
 
-  public void search() {
-    myFiles.clear();
-    myJars.clear();
+  @Override
+  public Set<PsiFile> search(@Nullable Module module, @NotNull Project project) {
+    assert module != null;
 
     final List<DomFileElement<StrutsRoot>> elements =
-      DomService.getInstance().getFileElements(StrutsRoot.class, module.getProject(),
+      DomService.getInstance().getFileElements(StrutsRoot.class, project,
                                                GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module));
 
-    for (final DomFileElement<StrutsRoot> element : elements) {
-      final XmlFile file = element.getFile();
-      final VirtualFile jar = JarFileSystem.getInstance().getVirtualFileForJar(file.getVirtualFile());
-      if (jar != null) {
-        myJars.putValue(jar, file);
-      } else {
-        final Module module = ModuleUtil.findModuleForPsiElement(file);
-        if (module != null) {
-          myFiles.putValue(module, file);
-        }
+    return ContainerUtil.map2Set(elements, new Function<DomFileElement<StrutsRoot>, PsiFile>() {
+      @Override
+      public PsiFile fun(DomFileElement<StrutsRoot> element) {
+        return element.getFile();
       }
-    }
+    });
   }
-
-  public MultiMap<Module, PsiFile> getFilesByModules() {
-    return myFiles;
-  }
-
-  public MultiMap<VirtualFile, PsiFile> getJars() {
-    return myJars;
-  }
-
 }
