23d0ea6c8329a06b42e10f23dce0db6c6c9d1264
Max Medvedev <maxim.medvedev@jetbrains.com>
2013-03-13 16:51:59 +0400

    EA-44744 - assert: readAction

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/search/CucumberJavaMethodUsageSearcher.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/search/CucumberJavaMethodUsageSearcher.java
@@ -1,6 +1,8 @@
 package org.jetbrains.plugins.cucumber.java.steps.search;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.QueryExecutorBase;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiMethod;
@@ -10,6 +12,7 @@ import com.intellij.psi.search.searches.MethodReferencesSearch;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.util.Processor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.cucumber.CucumberUtil;
 import org.jetbrains.plugins.cucumber.java.CucumberJavaUtil;
 import org.jetbrains.plugins.cucumber.psi.GherkinFileType;
 
@@ -19,21 +22,29 @@ import org.jetbrains.plugins.cucumber.psi.GherkinFileType;
  */
 public class CucumberJavaMethodUsageSearcher extends QueryExecutorBase<PsiReference, MethodReferencesSearch.SearchParameters> {
   @Override
-  public void processQuery(@NotNull MethodReferencesSearch.SearchParameters p, @NotNull final Processor<PsiReference> consumer) {
+  public void processQuery(@NotNull final MethodReferencesSearch.SearchParameters p, @NotNull final Processor<PsiReference> consumer) {
     final PsiMethod method = p.getMethod();
-    final PsiAnnotation stepAnnotation = CucumberJavaUtil.getCucumberStepAnnotation(method);
-    if (stepAnnotation != null) {
-      final String regexp = CucumberJavaUtil.getPatternFromStepDefinition(stepAnnotation);
-      final String word = org.jetbrains.plugins.cucumber.CucumberUtil.getTheBiggestWordToSearchByIndex(regexp);
-      if (StringUtil.isEmpty(word)) {
-        return;
-      }
 
-      if (p.getScope() instanceof GlobalSearchScope) {
-        GlobalSearchScope restrictedScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)p.getScope(),
-                                                                                            GherkinFileType.INSTANCE);
-        ReferencesSearch.search(new ReferencesSearch.SearchParameters(method, restrictedScope, false, p.getOptimizer())).forEach(consumer);
+    final String regexp = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
+      @Override
+      public String compute() {
+        final PsiAnnotation stepAnnotation = CucumberJavaUtil.getCucumberStepAnnotation(method);
+        return stepAnnotation != null ? CucumberJavaUtil.getPatternFromStepDefinition(stepAnnotation) : null;
       }
+    });
+
+    if (regexp == null) {
+      return;
+    }
+
+    final String word = CucumberUtil.getTheBiggestWordToSearchByIndex(regexp);
+    if (StringUtil.isEmpty(word)) {
+      return;
+    }
+
+    if (p.getScope() instanceof GlobalSearchScope) {
+      GlobalSearchScope restrictedScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)p.getScope(), GherkinFileType.INSTANCE);
+      ReferencesSearch.search(new ReferencesSearch.SearchParameters(method, restrictedScope, false, p.getOptimizer())).forEach(consumer);
     }
   }
 }
