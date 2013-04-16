3758d914a500f6b7190592fb82e4a1c199ec44ef
Dmitry Jemerov <yole@jetbrains.com>
2013-04-16 18:50:32 +0200

    move Flex-specific quickfixes to Flex plugin

---
+++ flex/src/com/intellij/lang/javascript/validation/fixes/CreateFlexMobileViewIntentionAndFix.java
@@ -0,0 +1,66 @@
+package com.intellij.lang.javascript.validation.fixes;
+
+import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.lang.javascript.JSBundle;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.Pair;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+
+public class CreateFlexMobileViewIntentionAndFix extends CreateMxmlFileIntentionBase implements LocalQuickFix {
+  private final boolean myAskForPackage;
+
+  @NotNull
+  public String getText() {
+    return JSBundle.message("create.mobile.view", myClassName);
+  }
+
+  public CreateFlexMobileViewIntentionAndFix(final String classFqn, final PsiElement element, final boolean askForPackage) {
+    super(classFqn, element);
+    myAskForPackage = askForPackage;
+  }
+
+  protected String getFileText() {
+    return "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+           "<s:View xmlns:fx=\"http://ns.adobe.com/mxml/2009\" xmlns:s=\"library://ns.adobe.com/flex/spark\">\n" +
+           "\n" +
+           "</s:View>\n";
+  }
+
+  @NotNull
+  public String getName() {
+    return getText();
+  }
+
+  public void applyFix(@NotNull final Project project, @NotNull final ProblemDescriptor descriptor) {
+    invoke(project, null, myElement.getContainingFile());
+  }
+
+  protected Pair<String, PsiDirectory> getFileTextAndDir(final @NotNull Module module) {
+    if (!myAskForPackage) return super.getFileTextAndDir(module);
+
+    final PsiDirectory targetDirectory;
+
+    if (ApplicationManager.getApplication().isUnitTestMode()) {
+      targetDirectory = ApplicationManager.getApplication().runWriteAction(new Computable<PsiDirectory>() {
+        public PsiDirectory compute() {
+          return CreateClassOrInterfaceFix.findOrCreateDirectory("foo", myElement);
+        }
+      });
+    }
+    else {
+      final ChoosePackageDialog dialog = new ChoosePackageDialog(module, getText(), myPackageName, myElement.getContainingFile());
+      dialog.show();
+      if (!dialog.isOK()) return Pair.empty();
+
+      targetDirectory = dialog.getTargetDirectory();
+    }
+
+    return Pair.create(getFileText(), targetDirectory);
+  }
+}
