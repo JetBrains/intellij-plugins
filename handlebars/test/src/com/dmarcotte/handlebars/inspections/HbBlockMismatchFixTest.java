28cfe058cdd6401b9099ccaf1226ddb807a2cd61
Dmitry Jemerov <yole@jetbrains.com>
2013-04-16 12:32:00 +0200

    restore the operation of the test

--- handlebars/test/src/com/dmarcotte/handlebars/inspections/HbBlockMismatchFixTest.java
+++ handlebars/test/src/com/dmarcotte/handlebars/inspections/HbBlockMismatchFixTest.java
@@ -1,30 +1,50 @@
 package com.dmarcotte.handlebars.inspections;
 
 import com.dmarcotte.handlebars.util.HbTestUtils;
+import com.intellij.codeInsight.intention.IntentionAction;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 import org.jetbrains.annotations.NotNull;
 
 public class HbBlockMismatchFixTest extends LightPlatformCodeInsightFixtureTestCase {
+  public HbBlockMismatchFixTest() {
+    PlatformTestCase.initPlatformLangPrefix();
+  }
+
+  @Override
+  protected boolean isWriteActionRequired() {
+    return false;
+  }
+
   public void testWrongCloseBlock1() {
-    doTest();
+    doTest("Change block end");
   }
 
   public void testWrongCloseBlock2() {
-    doTest();
+    doTest("Change block end");
   }
 
   public void testWrongOpenBlock1() {
-    doTest();
+    doTest("Change block start");
   }
 
   public void testWrongOpenBlock2() {
-    doTest();
+    doTest("Change block start");
   }
 
-  private void doTest() {
+  private void doTest(String intentionHint) {
     myFixture.configureByFile("inspections/before" + getTestName(false) + ".hbs");
-    // TODO
+    final IntentionAction intention = myFixture.findSingleIntention(intentionHint);
+    new WriteCommandAction.Simple(myFixture.getProject()) {
+      @Override
+      protected void run() throws Throwable {
+        myFixture.launchAction(intention);
+      }
+    }.execute();
+    myFixture.checkResultByFile("inspections/after" + getTestName(false) + ".hbs");
   }
+
 
   @Override
   protected String getBasePath() {
