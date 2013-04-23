611b72a8ba564630702ae16312ab07ad27f1f217
Yann Cébron <yann.cebron@jetbrains.com>
2013-04-23 19:33:53 +0200

    split StrutsFrameworkDetector

--- struts2/plugin/src/com/intellij/struts2/facet/StrutsFacetType.java
+++ struts2/plugin/src/com/intellij/struts2/facet/StrutsFacetType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 The authors
+ * Copyright 2013 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,28 +16,16 @@
 package com.intellij.struts2.facet;
 
 import com.intellij.facet.Facet;
-import com.intellij.facet.FacetConfiguration;
 import com.intellij.facet.FacetType;
-import com.intellij.framework.detection.FacetBasedFrameworkDetector;
-import com.intellij.framework.detection.FileContentPattern;
-import com.intellij.j2ee.web.WebUtilImpl;
 import com.intellij.javaee.web.facet.WebFacet;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.openapi.module.JavaModuleType;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.patterns.ElementPattern;
-import com.intellij.struts2.StrutsConstants;
-import com.intellij.struts2.dom.struts.StrutsRoot;
-import com.intellij.util.indexing.FileContent;
 import icons.Struts2Icons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.util.Set;
 
 /**
  * Description-type for {@link StrutsFacet}.
@@ -77,39 +65,4 @@ public class StrutsFacetType extends FacetType<StrutsFacet, StrutsFacetConfigura
   public String getHelpTopic() {
     return "reference.settings.project.structure.facets.struts2.facet";
   }
-
-
-  public static class StrutsFrameworkDetector
-      extends FacetBasedFrameworkDetector<StrutsFacet, StrutsFacetConfiguration> {
-    public StrutsFrameworkDetector() {
-      super("struts2");
-    }
-
-    @Override
-    public FacetType<StrutsFacet, StrutsFacetConfiguration> getFacetType() {
-      return getInstance();
-    }
-
-    @NotNull
-    @Override
-    public FileType getFileType() {
-      return StdFileTypes.XML;
-    }
-
-    @NotNull
-    @Override
-    public ElementPattern<FileContent> createSuitableFilePattern() {
-      return FileContentPattern.fileContent()
-                               .withName(StrutsConstants.STRUTS_XML_DEFAULT_FILENAME)
-                               .xmlWithRootTag(StrutsRoot.TAG_NAME);
-    }
-
-    @Override
-    public boolean isSuitableUnderlyingFacetConfiguration(final FacetConfiguration underlying,
-                                                          final StrutsFacetConfiguration configuration,
-                                                          final Set<VirtualFile> files) {
-      return WebUtilImpl.isWebFacetConfigurationContainingFiles(underlying, files);
-    }
-  }
-
 }
