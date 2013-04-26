a4338968d63f43e18da288c9707309df723c6c85
Yann Cébron <yann.cebron@jetbrains.com>
2013-04-26 15:47:07 +0200

    IDEA-106272 support <param> in <interceptor>

--- struts2/dom-api/src/com/intellij/struts2/dom/struts/strutspackage/Interceptor.java
+++ struts2/dom-api/src/com/intellij/struts2/dom/struts/strutspackage/Interceptor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 The authors
+ * Copyright 2013 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -18,6 +18,7 @@ package com.intellij.struts2.dom.struts.strutspackage;
 import com.intellij.ide.presentation.Presentation;
 import com.intellij.psi.PsiClass;
 import com.intellij.struts2.dom.ExtendableClassConverter;
+import com.intellij.struts2.dom.params.ParamsElement;
 import com.intellij.util.xml.Attribute;
 import com.intellij.util.xml.Convert;
 import com.intellij.util.xml.ExtendClass;
@@ -27,11 +28,10 @@ import com.intellij.util.xml.GenericAttributeValue;
  * @author Yann C&eacute;bron
  */
 @Presentation(typeName = "Interceptor", icon = "AllIcons.Nodes.Plugin")
-public interface Interceptor extends InterceptorOrStackBase {
+public interface Interceptor extends InterceptorOrStackBase, ParamsElement {
 
   @Attribute(value = "class")
   @ExtendClass(value = "com.opensymphony.xwork2.interceptor.Interceptor", allowAbstract = false, allowInterface = false)
   @Convert(ExtendableClassConverter.class)
   GenericAttributeValue<PsiClass> getInterceptorClass();
-
 }
