df93ab7ae1d0388e6d6b58b47a4234a6834bb8d8
Fedor Korotkov <fedor.korotkov@gmail.com>
2013-05-29 16:29:20 +0400

    WEB-7887 Support code folding for Dart

---
+++ src/com/jetbrains/lang/dart/ide/folding/DartFoldingBuilder.java
@@ -0,0 +1,52 @@
+package com.jetbrains.lang.dart.ide.folding;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.folding.FoldingBuilder;
+import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.util.TextRange;
+import com.jetbrains.lang.dart.DartTokenTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fedorkorotkov.
+ */
+public class DartFoldingBuilder implements FoldingBuilder {
+  @NotNull
+  @Override
+  public FoldingDescriptor[] buildFoldRegions(@NotNull ASTNode node, @NotNull Document document) {
+    List<FoldingDescriptor> list = new ArrayList<FoldingDescriptor>();
+    buildFolding(node, list);
+    FoldingDescriptor[] descriptors = new FoldingDescriptor[list.size()];
+    return list.toArray(descriptors);
+  }
+
+  private static void buildFolding(ASTNode node, List<FoldingDescriptor> list) {
+    boolean isBlock = node.getElementType() == DartTokenTypes.BLOCK || node.getElementType() == DartTokenTypes.CLASS_BODY;
+    if (isBlock && !node.getTextRange().isEmpty()) {
+      final TextRange range = node.getTextRange();
+      list.add(new FoldingDescriptor(node, range));
+    }
+    for (ASTNode child : node.getChildren(null)) {
+      buildFolding(child, list);
+    }
+  }
+
+  @Nullable
+  @Override
+  public String getPlaceholderText(@NotNull ASTNode node) {
+    if (node.getElementType() == DartTokenTypes.CLASS_BODY) {
+      return "...";
+    }
+    return "{...}";
+  }
+
+  @Override
+  public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+    return false;
+  }
+}
