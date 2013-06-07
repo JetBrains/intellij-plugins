6248bd6f01e4e8295adf0ff821464b6fb8a4495a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2013-06-07 11:51:19 +0400

    lang: deprecated API usages eliminated (from Tapestry)

--- core/src/com/intellij/tapestry/intellij/lang/TemplateColorSettingsPage.java
+++ core/src/com/intellij/tapestry/intellij/lang/TemplateColorSettingsPage.java
@@ -1,9 +1,9 @@
 package com.intellij.tapestry.intellij.lang;
 
+import com.intellij.ide.highlighter.JavaHighlightingColors;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.HighlighterColors;
 import com.intellij.openapi.editor.JspHighlighterColors;
-import com.intellij.openapi.editor.SyntaxHighlighterColors;
 import com.intellij.openapi.editor.XmlHighlighterColors;
 import com.intellij.openapi.editor.colors.CodeInsightColors;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
@@ -11,6 +11,7 @@ import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 import com.intellij.openapi.options.colors.AttributesDescriptor;
 import com.intellij.openapi.options.colors.ColorDescriptor;
 import com.intellij.openapi.options.colors.ColorSettingsPage;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.tapestry.lang.TmlHighlighter;
 import icons.TapestryIcons;
 import org.jetbrains.annotations.NonNls;
@@ -18,7 +19,6 @@ import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,21 +26,31 @@ import java.util.Map;
  * Tapestry template color settings.
  */
 public class TemplateColorSettingsPage implements ColorSettingsPage {
-
-  public static final TextAttributesKey TAG_NAME = createTextAttributesKey("TAPESTRY_COMPONENT_TAG", XmlHighlighterColors.HTML_TAG_NAME);
-  public static final TextAttributesKey ATTR_NAME = createTextAttributesKey("TAPESTRY_COMPONENT_PARAMATER", XmlHighlighterColors.HTML_ATTRIBUTE_NAME);
-  public static final TextAttributesKey TEL_BOUNDS = createTextAttributesKey("TEL_BOUNDS", SyntaxHighlighterColors.KEYWORD);
-  public static final TextAttributesKey TEL_IDENT = createTextAttributesKey("TEL_IDENT", CodeInsightColors.LOCAL_VARIABLE_ATTRIBUTES);
-  public static final TextAttributesKey TEL_DOT = createTextAttributesKey("TEL_DOT", SyntaxHighlighterColors.DOT);
-  public static final TextAttributesKey TEL_NUMBER = createTextAttributesKey("TEL_NUMBER", SyntaxHighlighterColors.NUMBER);
-  public static final TextAttributesKey TEL_PARENTHS = createTextAttributesKey("TEL_PARENTHS", SyntaxHighlighterColors.PARENTHS);
-  public static final TextAttributesKey TEL_BRACKETS = createTextAttributesKey("TEL_BRACKETS", SyntaxHighlighterColors.BRACKETS);
-  public static final TextAttributesKey TEL_STRING = createTextAttributesKey("TEL_STRING", SyntaxHighlighterColors.STRING);
-  public static final TextAttributesKey TEL_BACKGROUND = createTextAttributesKey("TEL_BACKGROUND", JspHighlighterColors.JSP_SCRIPTING_BACKGROUND);
-  public static final TextAttributesKey TEL_BAD_CHAR = createTextAttributesKey("TEL_BAD_CHAR", HighlighterColors.BAD_CHARACTER);
+  public static final TextAttributesKey TAG_NAME =
+    createTextAttributesKey("TAPESTRY_COMPONENT_TAG", XmlHighlighterColors.HTML_TAG_NAME);
+  public static final TextAttributesKey ATTR_NAME =
+    createTextAttributesKey("TAPESTRY_COMPONENT_PARAMATER", XmlHighlighterColors.HTML_ATTRIBUTE_NAME);
+  public static final TextAttributesKey TEL_BOUNDS =
+    createTextAttributesKey("TEL_BOUNDS", JavaHighlightingColors.KEYWORD);
+  public static final TextAttributesKey TEL_IDENT =
+    createTextAttributesKey("TEL_IDENT", CodeInsightColors.LOCAL_VARIABLE_ATTRIBUTES);
+  public static final TextAttributesKey TEL_DOT =
+    createTextAttributesKey("TEL_DOT", JavaHighlightingColors.DOT);
+  public static final TextAttributesKey TEL_NUMBER =
+    createTextAttributesKey("TEL_NUMBER", JavaHighlightingColors.NUMBER);
+  public static final TextAttributesKey TEL_PARENTHS =
+    createTextAttributesKey("TEL_PARENTHS", JavaHighlightingColors.PARENTHESES);
+  public static final TextAttributesKey TEL_BRACKETS =
+    createTextAttributesKey("TEL_BRACKETS", JavaHighlightingColors.BRACKETS);
+  public static final TextAttributesKey TEL_STRING =
+    createTextAttributesKey("TEL_STRING", JavaHighlightingColors.STRING);
+  public static final TextAttributesKey TEL_BACKGROUND =
+    createTextAttributesKey("TEL_BACKGROUND", JspHighlighterColors.JSP_SCRIPTING_BACKGROUND);
+  public static final TextAttributesKey TEL_BAD_CHAR =
+    createTextAttributesKey("TEL_BAD_CHAR", HighlighterColors.BAD_CHARACTER);
 
   private static TextAttributesKey createTextAttributesKey(@NonNls String externalName, TextAttributesKey defaultTextAttr) {
-    return TextAttributesKey.createTextAttributesKey(externalName, defaultTextAttr.getDefaultAttributes());
+    return TextAttributesKey.createTextAttributesKey(externalName, defaultTextAttr);
   }
 
   private static AttributesDescriptor createAttributesDescriptor(String displayNameKey, TextAttributesKey textAttributesKey) {
@@ -98,22 +108,21 @@ public class TemplateColorSettingsPage implements ColorSettingsPage {
   private static final Map<String, TextAttributesKey> ourAdditionalHighlightingTagToDescriptorMap;
   private static final String ourColorSettingsText;
   static {
-    Logger logger = Logger.getInstance(TemplateColorSettingsPage.class.getName());
-    byte[] templateBytes = null;
+    Logger logger = Logger.getInstance(TemplateColorSettingsPage.class);
+    String template = "";
     try {
-      InputStream templateStream = TemplateColorSettingsPage.class.getResourceAsStream("templateColorSettingsText.html");
-      templateBytes = new byte[templateStream.available()];
-      if (templateStream.read(templateBytes) == 0) {
+      //noinspection IOResourceOpenedButNotSafelyClosed
+      template = FileUtil.loadTextAndClose(TemplateColorSettingsPage.class.getResourceAsStream("templateColorSettingsText.html"));
+      if (template.isEmpty()) {
         logger.warn("Template color settings demo text is empty");
       }
     }
     catch (IOException ex) {
       logger.error(ex);
     }
-    ourColorSettingsText = new String(templateBytes != null ? templateBytes : new byte[0]).replace('\r', ' ');
+    ourColorSettingsText = template.replace('\r', ' ');
     ourAdditionalHighlightingTagToDescriptorMap = new HashMap<String, TextAttributesKey>();
     ourAdditionalHighlightingTagToDescriptorMap.put("componenTagName", TAG_NAME);
     ourAdditionalHighlightingTagToDescriptorMap.put("componenTagAttribute", ATTR_NAME);
   }
-
 }
