a896a890ae707eadde0405f95d0508a42558f5a5
Fedor Korotkov <fedor.korotkov@gmail.com>
2013-06-25 16:34:26 +0400

    Dart: removed settings action

--- Dart/src/com/jetbrains/lang/dart/ide/settings/DartSettingsConfigurable.java
+++ Dart/src/com/jetbrains/lang/dart/ide/settings/DartSettingsConfigurable.java
@@ -2,17 +2,19 @@ package com.jetbrains.lang.dart.ide.settings;
 
 import com.intellij.openapi.options.Configurable;
 import com.intellij.openapi.options.ConfigurationException;
+import com.intellij.openapi.options.SearchableConfigurable;
 import com.intellij.openapi.project.Project;
 import com.jetbrains.lang.dart.DartBundle;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 
 /**
  * @author: Fedor.Korotkov
  */
-public class DartSettingsConfigurable implements Configurable {
+public class DartSettingsConfigurable  implements SearchableConfigurable, Configurable.NoScroll {
   private DartSettingsUI mySettingsUI;
   private Project myProject;
 
@@ -65,5 +67,17 @@ public class DartSettingsConfigurable implements Configurable {
 
   @Override
   public void disposeUIResources() {
+  }
+
+  @NotNull
+  @Override
+  public String getId() {
+    return "dart.settings";
+  }
+
+  @Nullable
+  @Override
+  public Runnable enableSearch(String option) {
+    return null;
   }
 }
