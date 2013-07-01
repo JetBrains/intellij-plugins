72ec5761615e7126ce7302f7a5dd88f854b1712c
Maxim.Mossienko <Maxim.Mossienko@jetbrains.com>
2013-07-01 15:53:50 +0200

    do not index html in libraries

--- Dart/src/com/jetbrains/lang/dart/ide/index/DartInputFilter.java
+++ Dart/src/com/jetbrains/lang/dart/ide/index/DartInputFilter.java
@@ -1,11 +1,22 @@
 package com.jetbrains.lang.dart.ide.index;
 
 import com.intellij.openapi.fileTypes.StdFileTypes;
+import com.intellij.openapi.vfs.JarFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.indexing.DefaultFileTypeSpecificInputFilter;
 import com.jetbrains.lang.dart.DartFileType;
 
 public class DartInputFilter extends DefaultFileTypeSpecificInputFilter {
   public static DartInputFilter INSTANCE = new DartInputFilter();
+
+  @Override
+  public boolean acceptInput(VirtualFile file) {
+    boolean accepts = super.acceptInput(file);
+    if (accepts && file.getFileType() == StdFileTypes.HTML) {
+      accepts = !(file.getFileSystem() instanceof JarFileSystem);
+    }
+    return accepts;
+  }
 
   public DartInputFilter() {
     super(DartFileType.INSTANCE, StdFileTypes.HTML);
