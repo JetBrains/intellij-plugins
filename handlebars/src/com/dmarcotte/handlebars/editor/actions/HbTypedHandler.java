JetBrains/intellij-plugins

commit ec05f51a7292795f633ed7939e7a2f2546c2ee55

Hb: Avoid some inappropriate close tag inserts
Only auto-insert close mustaches for unclosed open mustaches, and only
on the "}" which completes the close tag.  This avoids many odd/wrong
auto-inserts

handlebars/src/com/dmarcotte/handlebars/editor/actions/HbTypedHandler.java
@@ -4,10 +4,7 @@ import com.dmarcotte.handlebars.HbLanguage;
 import com.dmarcotte.handlebars.config.HbConfig;
 import com.dmarcotte.handlebars.file.HbFileViewProvider;
 import com.dmarcotte.handlebars.parsing.HbTokenTypes;
-import com.dmarcotte.handlebars.psi.HbCloseBlockMustache;
-import com.dmarcotte.handlebars.psi.HbMustacheName;
-import com.dmarcotte.handlebars.psi.HbPsiUtil;
-import com.dmarcotte.handlebars.psi.HbSimpleInverse;
+import com.dmarcotte.handlebars.psi.*;
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.editor.CaretModel;
@@ -76,9 +73,13 @@ public class HbTypedHandler extends TypedHandlerDelegate {
 
     PsiElement elementAtCaret = provider.findElementAt(offset - 1, HbLanguage.class);
 
-    PsiElement openTag = HbPsiUtil.findParentOpenTagElement(elementAtCaret);
+    if (elementAtCaret == null || elementAtCaret.getNode().getElementType() != HbTokenTypes.CLOSE) {
+      return;
+    }
 
-    if (openTag != null && openTag.getChildren().length > 1) {
+    HbOpenBlockMustache openTag = HbPsiUtil.findParentOpenTagElement(elementAtCaret);
+
+    if (openTag != null && openTag.getPairedElement() == null && openTag.getChildren().length > 1) {
       HbMustacheName mustacheName = PsiTreeUtil.findChildOfType(openTag, HbMustacheName.class);
 
       if (mustacheName != null) {

handlebars/test/src/com/dmarcotte/handlebars/editor/actions/HbTypedHandlerTest.java
@@ -97,6 +97,16 @@ public class HbTypedHandlerTest extends HbActionHandlerTest {
     doCharTest('}', "{{#foo.bar.[baz bat]}<caret>", "{{#foo.bar.[baz bat]}}<caret>{{/foo.bar.[baz bat]}}");
   }
 
+  public void testNoInsertCloseTagForExtraStaches() {
+    HbConfig.setAutoGenerateCloseTagEnabled(true);
+    doCharTest('}', "{{#foo}}<caret>", "{{#foo}}}<caret>");
+  }
+
+  public void testNoInsertCloseTagForAlreadyClosedStaches() {
+    HbConfig.setAutoGenerateCloseTagEnabled(true);
+    doCharTest('}', "{{#foo}<caret>{{/foo}}", "{{#foo}}<caret>{{/foo}}");
+  }
+
   public void testRegularStache() {
     // ensure that nothing special happens for regular 'staches, whether autoGenerateCloseTag is enabled or not
 

commit 1c6574f76abf16999f69da584d73d869a6cb3c0c

Hb: Fix auto-insert close tag regression
ec05f51a7292795f633ed7939e7a2f2546c2ee55 introduced an issue which
prevented nested close tags from being inserted.  Reverting the part
of that commit which attempted to suppress auto-insert of already closed
tags solves this.

handlebars/src/com/dmarcotte/handlebars/editor/actions/HbTypedHandler.java
@@ -132,7 +132,7 @@ public class HbTypedHandler extends TypedHandlerDelegate {
 
     HbOpenBlockMustache openTag = HbPsiUtil.findParentOpenTagElement(elementAtCaret);
 
-    if (openTag != null && openTag.getPairedElement() == null && openTag.getChildren().length > 1) {
+    if (openTag != null && openTag.getChildren().length > 1) {
       HbMustacheName mustacheName = PsiTreeUtil.findChildOfType(openTag, HbMustacheName.class);
 
       if (mustacheName != null) {

handlebars/test/src/com/dmarcotte/handlebars/editor/actions/HbTypedHandlerTest.java
@@ -52,10 +52,12 @@ public class HbTypedHandlerTest extends HbActionHandlerTest {
     HbConfig.setAutoGenerateCloseTagEnabled(true);
     doCharTest('}', "foo {{#bar<caret>", "foo {{#bar}}<caret>{{/bar}}");
     doCharTest('}', "foo {{^bar<caret>", "foo {{^bar}}<caret>{{/bar}}");
+    doCharTest('}', "foo {{#bar}}{{#baz<caret>{{/bar}}", "foo {{#bar}}{{#baz}}<caret>{{/baz}}{{/bar}}");
 
     HbConfig.setAutoGenerateCloseTagEnabled(false);
     doCharTest('}', "foo {{#bar<caret>", "foo {{#bar}}<caret>");
     doCharTest('}', "foo {{^bar<caret>", "foo {{^bar}}<caret>");
+    doCharTest('}', "foo {{#bar}}{{#baz<caret>{{/bar}}", "foo {{#bar}}{{#baz}}<caret>{{/bar}}");
   }
 
   public void testCloseTripleBraces() {
@@ -78,6 +80,11 @@ public class HbTypedHandlerTest extends HbActionHandlerTest {
     doCharTest('}', "{{#foo}<caret>", "{{#foo}}<caret>");
     doCharTest('}', "{{#foo bar baz}<caret>", "{{#foo bar baz}}<caret>");
     doCharTest('}', "{{#foo bar baz bat=\"bam\"}<caret>", "{{#foo bar baz bat=\"bam\"}}<caret>");
+  }
+
+  public void testInsertCloseTagForNestedBlocks() {
+    HbConfig.setAutoGenerateCloseTagEnabled(true);
+    doCharTest('}', "{{#foo}}{{#bar}<caret>{{/foo}}", "{{#foo}}{{#bar}}<caret>{{/bar}}{{/foo}}");
   }
 
   public void testInsertCloseTagForOpenInverseStache() {
@@ -113,11 +120,6 @@ public class HbTypedHandlerTest extends HbActionHandlerTest {
   public void testNoInsertCloseTagForExtraStaches() {
     HbConfig.setAutoGenerateCloseTagEnabled(true);
     doCharTest('}', "{{#foo}}<caret>", "{{#foo}}}<caret>");
-  }
-
-  public void testNoInsertCloseTagForAlreadyClosedStaches() {
-    HbConfig.setAutoGenerateCloseTagEnabled(true);
-    doCharTest('}', "{{#foo}<caret>{{/foo}}", "{{#foo}}<caret>{{/foo}}");
   }
 
   public void testRegularStache() {