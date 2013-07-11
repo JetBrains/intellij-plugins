7e2ad9e87b44bf71f828b4f3551ba54b710782b7
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-07-11 16:31:15 +0200

    simplify AbcMergerTest

--- flex/tools/flex-ui-designer/idea-plugin/testSrc/com/intellij/flex/uiDesigner/libraries/AbcMergerTest.java
+++ flex/tools/flex-ui-designer/idea-plugin/testSrc/com/intellij/flex/uiDesigner/libraries/AbcMergerTest.java
@@ -1,17 +1,12 @@
 package com.intellij.flex.uiDesigner.libraries;
 
 import com.intellij.flex.uiDesigner.DesignerTests;
-import com.intellij.flex.uiDesigner.abc.AbcTranscoder;
 import com.intellij.openapi.util.Condition;
-import com.intellij.openapi.util.Pair;
-import com.intellij.util.xml.NanoXmlUtil;
-import gnu.trove.THashMap;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.CharArrayReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
@@ -27,42 +22,28 @@ public class AbcMergerTest {
 
   @After
   public void runAfterEveryTest() {
-    //noinspection ResultOfMethodCallIgnored
-    out.delete();
+    Assert.assertTrue(out.delete());
   }
 
   @Test
   public void merge() throws IOException {
-    final THashMap<CharSequence, Definition> definitionMap = new THashMap<CharSequence, Definition>(AbcTranscoder.HASHING_STRATEGY);
-    final Set<CharSequence> globalDefinitions = LibraryUtil.getDefinitions(LibraryUtil.getTestGlobalLibrary(false));
-
-    final AbcMerger abcMerger = new AbcMerger(definitionMap, out, null);
-    final Pair<CharArrayReader, ByteArrayInputStream> data =
-      LibraryUtil.openSwc(new File(DesignerTests.getTestDataPath(), "lib/MinimalComps_0_9_10.swc"));
-
-    NanoXmlUtil.parse(data.first, new CatalogXmlBuilder(definitionMap, new Condition<String>() {
-      @Override
-      public boolean value(String name) {
-        return globalDefinitions.contains(name);
-      }
-    }));
-
-    abcMerger.process(data.second);
-    abcMerger.close();
+    doTest(new LightLibrary(new File(DesignerTests.getTestDataPath(), "lib/MinimalComps_0_9_10.swc")));
   }
 
   @Test
   public void merge2() throws IOException {
-    final Set<CharSequence> globalDefinitions = LibraryUtil.getDefinitions(LibraryUtil.getTestGlobalLibrary(false));
+    doTest(new LightLibrary(new File(DesignerTests.getTestDataPath(), "lib/IDEA-104608/IpgVideoPlayerAssets.swc")),
+           new LightLibrary(new File(DesignerTests.getTestDataPath(), "lib/IDEA-104608/IpgVptLoaderAssets.swc")));
+  }
 
+  private void doTest(Library... libraries) throws IOException {
+    final Set<CharSequence> globalDefinitions = LibraryUtil.getDefinitions(LibraryUtil.getTestGlobalLibrary(false));
     LibrarySorter librarySorter = new LibrarySorter();
-    librarySorter.sort(Arrays.<Library>asList(new LightLibrary(new File(DesignerTests.getTestDataPath(), "lib/IDEA-104608/IpgVideoPlayerAssets.swc")), new LightLibrary(new File(DesignerTests.getTestDataPath(), "lib/IDEA-104608/IpgVptLoaderAssets.swc"))),
-                       File.createTempFile("test", ""),
-                       new Condition<String>() {
-                         @Override
-                         public boolean value(String name) {
-                           return globalDefinitions.contains(name);
-                         }
-                       }, false);
+    librarySorter.sort(Arrays.asList(libraries), out, new Condition<String>() {
+      @Override
+      public boolean value(String name) {
+        return globalDefinitions.contains(name);
+      }
+    }, false);
   }
 }
