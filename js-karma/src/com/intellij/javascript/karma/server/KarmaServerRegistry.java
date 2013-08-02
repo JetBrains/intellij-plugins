cb063922a1e39bd7cdfcc1f3d0389fcb57fa8837
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-08-02 15:41:29 +0400

    error message handling when starting Karma server

--- js-karma/src/com/intellij/javascript/karma/server/KarmaServerRegistry.java
+++ js-karma/src/com/intellij/javascript/karma/server/KarmaServerRegistry.java
@@ -1,50 +1,25 @@
 package com.intellij.javascript.karma.server;
 
+import com.intellij.concurrency.JobScheduler;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.util.CatchingConsumer;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Sergey Simonchik
  */
 public class KarmaServerRegistry {
 
-  private static Map<String, KarmaServer> myServers = new ConcurrentHashMap<String, KarmaServer>();
-  private static Set<String> myStartingServers = Collections.synchronizedSet(ContainerUtil.<String>newHashSet());
-
-  public static void startServer(@NotNull final File nodeInterpreter,
-                                 @NotNull final File karmaPackageDir,
-                                 @NotNull final File configurationFile,
-                                 final CatchingConsumer<KarmaServer, IOException> consumer) {
-    final String key = configurationFile.getAbsolutePath();
-    if (myStartingServers.contains(key)) {
-      return;
-    }
-    myStartingServers.add(key);
-    ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          KarmaServer server = new KarmaServer(nodeInterpreter, karmaPackageDir, configurationFile);
-          myServers.put(configurationFile.getAbsolutePath(), server);
-          myStartingServers.remove(key);
-          consumer.consume(server);
-        }
-        catch (IOException e) {
-          consumer.consume(e);
-        }
-      }
-    });
-  }
+  private static final Logger LOG = Logger.getInstance(KarmaServerRegistry.class);
+  private static ConcurrentMap<String, KarmaServer> myServers = new ConcurrentHashMap<String, KarmaServer>();
+  private static ConcurrentMap<String, Object> myStartingServers = new ConcurrentHashMap<String, Object>();
 
   @Nullable
   public static KarmaServer getServerByConfigurationFile(@NotNull File configurationFile) {
@@ -52,13 +27,46 @@ public class KarmaServerRegistry {
     return myServers.get(path);
   }
 
-  public static void registerServer(@NotNull KarmaServer karmaServer) {
-    String path = karmaServer.getConfigurationFile().getAbsolutePath();
-    myServers.put(path, karmaServer);
+  public static void startServer(@NotNull final File nodeInterpreter,
+                                 @NotNull final File karmaPackageDir,
+                                 @NotNull final File configurationFile,
+                                 final CatchingConsumer<KarmaServer, Exception> consumer) {
+    final String configPath = configurationFile.getAbsolutePath();
+    if (myStartingServers.putIfAbsent(configPath, configPath) != null) {
+      LOG.warn(new Throwable("Unexpected subsequent karma server starting:"
+                             + "\n  nodeInterpreter: " + nodeInterpreter
+                             + "\n  karmaPackageDir: " + karmaPackageDir
+                             + "\n  configurationFile: " + configurationFile
+      ));
+      JobScheduler.getScheduler().schedule(new Runnable() {
+        @Override
+        public void run() {
+          startServer(nodeInterpreter, karmaPackageDir, configurationFile, consumer);
+        }
+      }, 100, TimeUnit.MILLISECONDS);
+    }
+    ApplicationManager.getApplication().executeOnPooledThread(new Runnable() {
+      @Override
+      public void run() {
+        try {
+          KarmaServer server = new KarmaServer(nodeInterpreter, karmaPackageDir, configurationFile);
+          myServers.put(configurationFile.getAbsolutePath(), server);
+          consumer.consume(server);
+          server.doWhenTerminated(new KarmaServerTerminatedListener() {
+            @Override
+            public void onTerminated(int exitCode) {
+              myServers.remove(configPath);
+            }
+          });
+        }
+        catch (Exception e) {
+          consumer.consume(e);
+        }
+        finally {
+          myStartingServers.remove(configPath);
+        }
+      }
+    });
   }
 
-  public static void serverTerminated(@NotNull KarmaServer server) {
-    String path = server.getConfigurationFile().getAbsolutePath();
-    myServers.remove(path);
-  }
 }
