daeeb0f01d4facd6c91968e893846347012cc1a3
Fedor Korotkov <fedor.korotkov@gmail.com>
2013-08-05 13:08:23 +0400

    Tapestry: Fixed tests

--- tests/src/com/intellij/tapestry/tests/TapestryFindUsagesTest.java
+++ tests/src/com/intellij/tapestry/tests/TapestryFindUsagesTest.java
@@ -47,7 +47,7 @@ public class TapestryFindUsagesTest extends TapestryBaseTestCase {
     final GlobalSearchScope scope = GlobalSearchScope.projectScope(myFixture.getProject());
     Query<PsiReference> query = referenceTo instanceof PsiMethod
                                 ? MethodReferencesSearch.search((PsiMethod)referenceTo, scope, false)
-                                : ReferencesSearch.search(referenceTo, scope, false);
+                                : ReferencesSearch.search(referenceTo, scope, true);
     return query.toArray(new PsiReference[0]);
   }
 
