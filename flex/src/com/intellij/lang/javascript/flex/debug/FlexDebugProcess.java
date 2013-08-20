32ea8d0a7c6fce0c6bcedcb9a6c6e60bf62c41fe
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2013-08-20 15:26:08 +0400

    fdb fix works for Flex SDK 4.10

--- flex/src/com/intellij/lang/javascript/flex/debug/FlexDebugProcess.java
+++ flex/src/com/intellij/lang/javascript/flex/debug/FlexDebugProcess.java
@@ -332,7 +332,7 @@ public class FlexDebugProcess extends XDebugProcess {
                 StringUtil.compareVersionNumbers(myDebuggerVersion, "4.6.1") < 0)
                ||
                (StringUtil.compareVersionNumbers(myDebuggerVersion, "4.8") >= 0 &&
-                StringUtil.compareVersionNumbers(myDebuggerVersion, "4.10") < 0)) {
+                StringUtil.compareVersionNumbers(myDebuggerVersion, "4.11") < 0)) {
         classpath = FlexCommonUtils.getPathToBundledJar("idea-fdb-4.5.0.20967-fix.jar") + File.pathSeparator + classpath;
       }
     }
