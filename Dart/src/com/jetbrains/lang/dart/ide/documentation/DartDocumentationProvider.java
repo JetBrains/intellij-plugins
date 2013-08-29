aa2d0c464ed2f06e2b11462438958d267022a428
Fedor Korotkov <fedor.korotkov@gmail.com>
2013-08-29 16:24:23 +0400

    WEB-7672 What about Dart documentation? (cherry picked from commit 9dc22a2)
    CR-WS-68

--- Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProvider.java
+++ Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProvider.java
@@ -1,8 +1,10 @@
 package com.jetbrains.lang.dart.ide.documentation;
 
 import com.intellij.lang.documentation.DocumentationProvider;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.lang.dart.DartComponentType;
@@ -10,13 +12,18 @@ import com.jetbrains.lang.dart.psi.DartClass;
 import com.jetbrains.lang.dart.psi.DartComponent;
 import com.jetbrains.lang.dart.util.DartPresentableUtil;
 import com.jetbrains.lang.dart.util.DartResolveUtil;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author: Fedor.Korotkov
  */
 public class DartDocumentationProvider implements DocumentationProvider {
+  private static final String BASE_DART_DOC_URL = "http://api.dartlang.org/docs/releases/latest/";
+
   @Override
   public String getQuickNavigateInfo(PsiElement element, PsiElement originalElement) {
     return null;
@@ -24,7 +31,57 @@ public class DartDocumentationProvider implements DocumentationProvider {
 
   @Override
   public List<String> getUrlFor(PsiElement element, PsiElement originalElement) {
-    return null;
+    if (!(element instanceof DartComponent) && !(element.getParent() instanceof DartComponent)) {
+      return null;
+    }
+    final DartComponent namedComponent = (DartComponent)(element instanceof DartComponent ? element : element.getParent());
+    final String componentName = namedComponent.getName();
+    if (componentName == null || !namedComponent.isPublic()) {
+      return null;
+    }
+
+    final List<String> result = new ArrayList<String>();
+
+    final PsiManager psiManager = element.getManager();
+    List<VirtualFile> library = DartResolveUtil.findLibrary(element.getContainingFile());
+    for (VirtualFile libraryRoot : library) {
+      final PsiFile libPsiFile = psiManager.findFile(libraryRoot);
+      String libName = libPsiFile != null ? DartResolveUtil.getLibraryName(libPsiFile) : null;
+      final String docUrl = libName != null ? constructDocUrl(namedComponent, componentName, libName) : null;
+      if (docUrl != null) {
+        result.add(docUrl);
+      }
+    }
+
+    return result;
+  }
+
+  @Nls
+  private static String constructDocUrl(DartComponent namedComponent, String componentName, @NotNull String libName) {
+    // class:     http://api.dartlang.org/docs/releases/latest/args/ArgParser.html
+    // method:    http://api.dartlang.org/docs/releases/latest/args/ArgParser.html#addCommand
+    // function:  http://api.dartlang.org/docs/releases/latest/observe.html#toObservable
+
+    final StringBuilder resultUrl = new StringBuilder(BASE_DART_DOC_URL);
+    resultUrl.append(libName);
+
+    final DartClass dartClass = PsiTreeUtil.getParentOfType(namedComponent, DartClass.class, true);
+    final DartComponentType componentType = DartComponentType.typeOf(namedComponent);
+
+    if (dartClass != null) {
+      // method
+      resultUrl.append('/').append(dartClass.getName()).append(".html").append('#').append(componentName);
+    }
+    else if (componentType == DartComponentType.CLASS || componentType == DartComponentType.INTERFACE) {
+      // class
+      resultUrl.append('/').append(componentName).append(".html");
+    }
+    else {
+      // function
+      resultUrl.append(".html").append('#').append(componentName);
+    }
+
+    return resultUrl.toString();
   }
 
   @Override
