79044f37fa4cbae2a5ddfc506dd75dcefdd225a4
Fedor Korotkov <fedor.korotkov@gmail.com>
2013-09-04 12:00:25 +0400

    Dart: missed }

--- Dart/src/com/jetbrains/lang/dart/psi/impl/DartFileReferenceImpl.java
+++ Dart/src/com/jetbrains/lang/dart/psi/impl/DartFileReferenceImpl.java
@@ -203,6 +203,7 @@ public class DartFileReferenceImpl extends DartExpressionImpl implements DartRef
         );
       }
     }, FileReference.EMPTY);
+  }
 
   public static class DartPathOrLibraryManipulator implements ElementManipulator<DartPathOrLibraryReference> {
     @Override
