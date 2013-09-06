e673d242ff43e53b1c45d1eda9acbc0a356c3c0a
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-09-06 17:14:30 +0400

    WEB-9176 Karma console: hyperlink is not highlighted (cherry picked from commit e910cb6) http://crucible.labs.intellij.net/cru/CR-WS-108

--- js-karma/src/com/intellij/javascript/karma/tree/KarmaBrowserErrorFilter.java
+++ js-karma/src/com/intellij/javascript/karma/tree/KarmaBrowserErrorFilter.java
@@ -21,11 +21,11 @@ public class KarmaBrowserErrorFilter implements Filter {
 
   private static final Pattern[] LINE_PATTERNS = new Pattern[] {
     //at http://localhost:9876/base/spec/personSpec.js?1368878723000:22
-    Pattern.compile("^\\s*at (http://localhost:\\d+/base/([^?]+)?.*)$"),
+    Pattern.compile("^\\s*at (http://[^:]+:\\d+/base/([^?]+)?.*)$"),
 
     //at http://localhost:9876/absolute/home/segrey/WebstormProjects/karma-chai-sample/test/test.js?1378466989000:1
-    Pattern.compile("^\\s*at (http://localhost:\\d+/absolute(/[^?]+)?.*)$"),
-};
+    Pattern.compile("^\\s*at (http://[^:]+:\\d+/absolute(/[^?]+)?.*)$")
+  };
 
   private final Project myProject;
   private final KarmaConfig myConfig;
@@ -38,22 +38,32 @@ public class KarmaBrowserErrorFilter implements Filter {
   @Nullable
   @Override
   public Result applyFilter(String line, int entireLength) {
-    Matcher m = findMatcher(line);
-    if (m != null) {
-      int hyperlinkStartInd = m.start(1);
-      int hyperlinkEndInd = m.end(1);
-      File file = findFile(m.group(2));
+    LinkInfo linkInfo = createLinkInfo(line);
+    if (linkInfo != null) {
+      File file = findFile(linkInfo.getPath());
       if (file != null) {
         VirtualFile virtualFile = VfsUtil.findFileByIoFile(file, false);
         if (virtualFile != null) {
-          int lineNumber = getLineFrom(m.group(1));
-          lineNumber = Math.max(-1, lineNumber - 1);
+          int lineNumber = Math.max(-1, linkInfo.getLineNumber() - 1);
           HyperlinkInfo link = new OpenFileHyperlinkInfo(myProject, virtualFile, lineNumber);
-          return new Filter.Result(hyperlinkStartInd, hyperlinkEndInd, link);
+          return new Filter.Result(linkInfo.getHyperlinkStartInd(), linkInfo.getHyperlinkEndInd(), link);
         }
       }
     }
     return null;
+  }
+
+  @Nullable
+  public static LinkInfo createLinkInfo(@NotNull String line) {
+    Matcher m = findMatcher(line);
+    if (m == null) {
+      return null;
+    }
+    int hyperlinkStartInd = m.start(1);
+    int hyperlinkEndInd = m.end(1);
+    String path = m.group(2);
+    int lineNumber = getLineFrom(m.group(1));
+    return new LinkInfo(hyperlinkStartInd, hyperlinkEndInd, path, lineNumber);
   }
 
   @Nullable
@@ -95,6 +105,60 @@ public class KarmaBrowserErrorFilter implements Filter {
     }
     catch (NumberFormatException e) {
       return -1;
+    }
+  }
+
+  public static class LinkInfo {
+    private final int myHyperlinkStartInd;
+    private final int myHyperlinkEndInd;
+    private final String myPath;
+    private final int myLineNumber;
+
+    public LinkInfo(int hyperlinkStartInd, int hyperlinkEndInd, @NotNull String path, int lineNumber) {
+      myHyperlinkStartInd = hyperlinkStartInd;
+      myHyperlinkEndInd = hyperlinkEndInd;
+      myPath = path;
+      myLineNumber = lineNumber;
+    }
+
+    public int getHyperlinkStartInd() {
+      return myHyperlinkStartInd;
+    }
+
+    public int getHyperlinkEndInd() {
+      return myHyperlinkEndInd;
+    }
+
+    public String getPath() {
+      return myPath;
+    }
+
+    public int getLineNumber() {
+      return myLineNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+
+      LinkInfo linkInfo = (LinkInfo)o;
+
+      if (myHyperlinkEndInd != linkInfo.myHyperlinkEndInd) return false;
+      if (myHyperlinkStartInd != linkInfo.myHyperlinkStartInd) return false;
+      if (myLineNumber != linkInfo.myLineNumber) return false;
+      if (!myPath.equals(linkInfo.myPath)) return false;
+
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = myHyperlinkStartInd;
+      result = 31 * result + myHyperlinkEndInd;
+      result = 31 * result + myPath.hashCode();
+      result = 31 * result + myLineNumber;
+      return result;
     }
   }
 
