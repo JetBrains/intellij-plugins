9f4984980302cc947b3b939eb9940120f789cd7e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-09-24 13:58:56 +0200

    extract XDebuggerEditorsProviderBase, init JavaBreakpointType (is not registered, so, not in action now)

--- flex/src/com/intellij/lang/javascript/flex/debug/FlexDebuggerEditorsProvider.java
+++ flex/src/com/intellij/lang/javascript/flex/debug/FlexDebuggerEditorsProvider.java
@@ -2,35 +2,31 @@ package com.intellij.lang.javascript.flex.debug;
 
 import com.intellij.javascript.JSDebuggerSupportUtils;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
-import com.intellij.openapi.editor.Document;
+import com.intellij.lang.javascript.psi.JSElementFactory;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
-import com.intellij.xdebugger.XSourcePosition;
-import com.intellij.xdebugger.evaluation.EvaluationMode;
-import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.xdebugger.evaluation.XDebuggerEditorsProviderBase;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * Created by IntelliJ IDEA.
-* User: Maxim.Mossienko
-* Date: Mar 6, 2008
-* Time: 7:14:46 PM
-* To change this template use File | Settings | File Templates.
-*/
-class FlexDebuggerEditorsProvider extends XDebuggerEditorsProvider {
+// duplicates JSDebuggerEditorsProvider
+class FlexDebuggerEditorsProvider extends XDebuggerEditorsProviderBase {
+  @Override
   @NotNull
-    public FileType getFileType() {
+  public FileType getFileType() {
     return JavaScriptSupportLoader.JAVASCRIPT;
   }
 
-  @NotNull
-    public Document createDocument(@NotNull final Project project,
-                                   @NotNull final String text, @Nullable final XSourcePosition position, @NotNull EvaluationMode mode) {
-    return JSDebuggerSupportUtils.createDocument(
-        text,
-        project,
-        position != null ? position.getFile():null, position != null ? position.getOffset():-1
-    );
+  @Override
+  protected PsiFile createExpressionCodeFragment(@NotNull Project project, @NotNull String text, @Nullable PsiElement context, boolean isPhysical) {
+    return JSElementFactory.createExpressionCodeFragment(project, text, context, true);
+  }
+
+  @Override
+  protected PsiElement getContextElement(@NotNull VirtualFile virtualFile, int offset, @NotNull Project project) {
+    return JSDebuggerSupportUtils.getContextElement(virtualFile, offset, project);
   }
 }
