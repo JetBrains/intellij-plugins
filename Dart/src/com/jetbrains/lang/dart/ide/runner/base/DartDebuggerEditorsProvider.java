036a00c26741bc54565eec81318e0811825f8712
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-09-24 13:58:57 +0200

    extract XDebuggerEditorsProviderBase, init JavaBreakpointType (is not registered, so, not in action now)

--- Dart/src/com/jetbrains/lang/dart/ide/runner/base/DartDebuggerEditorsProvider.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/base/DartDebuggerEditorsProvider.java
@@ -1,31 +1,24 @@
 package com.jetbrains.lang.dart.ide.runner.base;
 
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
-import com.intellij.xdebugger.XSourcePosition;
-import com.intellij.xdebugger.evaluation.EvaluationMode;
-import com.intellij.xdebugger.evaluation.XDebuggerEditorsProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.xdebugger.evaluation.XDebuggerEditorsProviderBase;
 import com.jetbrains.lang.dart.DartFileType;
+import com.jetbrains.lang.dart.util.DartElementGenerator;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * @author: Fedor.Korotkov
- */
-public class DartDebuggerEditorsProvider extends XDebuggerEditorsProvider {
+public class DartDebuggerEditorsProvider extends XDebuggerEditorsProviderBase {
+  @Override
   @NotNull
   public FileType getFileType() {
     return DartFileType.INSTANCE;
   }
 
-  @NotNull
-  public Document createDocument(@NotNull final Project project,
-                                 @NotNull final String text, @Nullable final XSourcePosition position, @NotNull EvaluationMode mode) {
-    return DartDebuggerSupportUtils.createDocument(
-      text,
-      project,
-      position != null ? position.getFile() : null, position != null ? position.getOffset() : -1
-    );
+  @Override
+  protected PsiFile createExpressionCodeFragment(@NotNull Project project, @NotNull String text, @Nullable PsiElement context, boolean isPhysical) {
+    return DartElementGenerator.createExpressionCodeFragment(project, text, context, true);
   }
 }
