0540aa8cf7b3e0a6815a52db9edc22e214bef353
Andrey Vokin <andrey.vokin@jetbrains.com>
2013-10-17 19:44:24 +0400

    IDEA-114442 Run single scenario broken when there is a description.

--- cucumber/src/org/jetbrains/plugins/cucumber/psi/impl/GherkinStepsHolderBase.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/psi/impl/GherkinStepsHolderBase.java
@@ -9,6 +9,7 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.psi.GherkinStep;
 import org.jetbrains.plugins.cucumber.psi.GherkinStepsHolder;
 import org.jetbrains.plugins.cucumber.psi.GherkinTag;
+import org.jetbrains.plugins.cucumber.psi.GherkinTokenTypes;
 
 public abstract class GherkinStepsHolderBase extends GherkinPsiElementBase implements GherkinStepsHolder {
   protected GherkinStepsHolderBase(@NotNull ASTNode node) {
@@ -17,7 +18,12 @@ public abstract class GherkinStepsHolderBase extends GherkinPsiElementBase imple
 
   @Override
   public final String getScenarioName() {
-    return getElementText();
+    ASTNode node = getNode().getFirstChildNode();
+    while (node != null && node.getElementType() != GherkinTokenTypes.TEXT) {
+      node = node.getTreeNext();
+    }
+
+    return node != null ? node.getText() : "";
   }
 
   @Override
