9fc7ff244c67c71613c2723f52a149e22a47d32c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2013-10-25 15:00:12 +0400

    Context menu item to open Dart-aware HTML files in Dartium browser (cherry picked from commit 7f8c61a264f64399feb764d0ad01c1ebea276746)

---
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/OpenInDartiumAction.java
@@ -0,0 +1,83 @@
+package com.jetbrains.lang.dart.ide.actions;
+
+import com.intellij.ide.browsers.Url;
+import com.intellij.ide.browsers.UrlOpener;
+import com.intellij.ide.browsers.WebBrowserService;
+import com.intellij.ide.browsers.WebBrowserUrlProvider;
+import com.intellij.ide.highlighter.HtmlFileType;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlAttributeValue;
+import com.intellij.psi.xml.XmlFile;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.XmlBundle;
+import com.jetbrains.lang.dart.DartBundle;
+import com.jetbrains.lang.dart.ide.settings.DartSettingsUtil;
+import icons.DartIcons;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.event.InputEvent;
+
+/*
+  This action is a temporary solution for Google announcement at Devoxx.
+  In future this action, 'Open in Browser' (OpenFileInDefaultBrowserAction) and 'Preview file in' (WebOpenInAction) actions need to be merged into one.
+  todo Vladimir Krivosheev, Alexander Doroshko
+ */
+public class OpenInDartiumAction extends AnAction {
+  public OpenInDartiumAction() {
+    super(DartBundle.message("open.in.dartium.action"), DartBundle.message("open.in.dartium.action.description"), DartIcons.Dartium_16);
+  }
+
+  public void update(final AnActionEvent e) {
+    final PsiFile psiFile = CommonDataKeys.PSI_FILE.getData(e.getDataContext());
+
+    final boolean available = psiFile instanceof XmlFile &&
+                              DartSettingsUtil.getDartiumPath() != null &&
+                              isHtmlFileWithDartScript(((XmlFile)psiFile));
+    e.getPresentation().setEnabled(available);
+    e.getPresentation().setVisible(available);
+  }
+
+  public void actionPerformed(final AnActionEvent e) {
+    final PsiFile psiFile = CommonDataKeys.PSI_FILE.getData(e.getDataContext());
+    if (!(psiFile instanceof XmlFile)) return;
+    final String dartiumPath = DartSettingsUtil.getDartiumPath();
+    if (dartiumPath == null || !isHtmlFileWithDartScript(((XmlFile)psiFile))) return;
+
+    try {
+      Url url = WebBrowserService.getInstance().getUrlToOpen(psiFile, (e.getModifiers() & InputEvent.SHIFT_MASK) != 0);
+      if (url != null) {
+        FileDocumentManager.getInstance().saveAllDocuments(); // for file and linked resources
+        UrlOpener.launchBrowser(url.toExternalForm(), DartSettingsUtil.DARTIUM);
+      }
+    }
+    catch (WebBrowserUrlProvider.BrowserException e1) {
+      Messages.showErrorDialog(e1.getMessage(), XmlBundle.message("browser.error"));
+    }
+  }
+
+  private static boolean isHtmlFileWithDartScript(final @NotNull XmlFile psiFile) {
+    final VirtualFile vFile = psiFile.getVirtualFile();
+    if (vFile == null || vFile.getFileType() != HtmlFileType.INSTANCE) return false;
+
+    final String text = psiFile.getText();
+    int i = -1;
+    while ((i = text.indexOf("application/dart", i + 1)) != -1) {
+      final PsiElement element = psiFile.findElementAt(i);
+      if (element != null && element.getParent() instanceof XmlAttributeValue) {
+        final XmlTag tag = PsiTreeUtil.getParentOfType(element, XmlTag.class);
+        if (tag != null && "script".equalsIgnoreCase(tag.getName())) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
--- Dart/src/com/jetbrains/lang/dart/ide/settings/DartSettingsUtil.java
+++ Dart/src/com/jetbrains/lang/dart/ide/settings/DartSettingsUtil.java
@@ -1,30 +1,49 @@
 package com.jetbrains.lang.dart.ide.settings;
 
+import com.intellij.CommonBundle;
+import com.intellij.execution.configurations.RuntimeConfigurationError;
+import com.intellij.ide.browsers.BrowsersConfiguration;
+import com.intellij.ide.browsers.WebBrowser;
 import com.intellij.ide.util.PropertiesComponent;
 import com.intellij.lang.javascript.library.JSLibraryMappings;
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.util.NullableComputable;
+import com.intellij.openapi.util.SystemInfo;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.webcore.libraries.ScriptingLibraryModel;
 import com.jetbrains.lang.dart.DartBundle;
+import icons.DartIcons;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-/**
- * @author: Fedor.Korotkov
- */
+import java.io.File;
+
 public class DartSettingsUtil {
-  public static final String DART_SDK_PATH = "dart_sdk_path";
+
+  public static final WebBrowser DARTIUM = WebBrowser
+    .createCustomBrowser(BrowsersConfiguration.BrowserFamily.CHROME, "Dartium", DartIcons.Dartium_16, new NullableComputable<String>() {
+      @Nullable
+      public String compute() {
+        return getDartiumPath();
+      }
+    }, DartBundle.message("dartium.not.configured", CommonBundle.settingsActionPath()));
+
+  public static final String DART_SDK_PATH_PROPERTY_NAME = "dart_sdk_path";
 
   public static void setSettings(DartSettings settings) {
     final PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
-    propertiesComponent.setValue(DART_SDK_PATH, settings.getSdkPath());
+    propertiesComponent.setValue(DART_SDK_PATH_PROPERTY_NAME, settings.getSdkPath());
   }
 
   @NotNull
   public static DartSettings getSettings() {
     final PropertiesComponent propertiesComponent = PropertiesComponent.getInstance();
-    final String value = propertiesComponent.getValue(DART_SDK_PATH);
+    final String value = propertiesComponent.getValue(DART_SDK_PATH_PROPERTY_NAME);
     return new DartSettings(value == null ? "" : value);
   }
 
@@ -36,5 +55,54 @@ public class DartSettingsUtil {
         return DartBundle.message("dart.sdk.name").equals(model.getName());
       }
     });
+  }
+
+  @Nullable
+  public static String getDartiumPath() {
+    try {
+      return getDartiumPathOrThrowErrorWithQuickFix(null);
+    }
+    catch (RuntimeConfigurationError error) {
+      return null;
+    }
+  }
+
+  @NotNull
+  public static String getDartiumPathOrThrowErrorWithQuickFix(final @Nullable Project project) throws RuntimeConfigurationError {
+    final String sdkPath = getSettings().getSdkPath();
+    if (StringUtil.isEmptyOrSpaces(sdkPath)) {
+      throwErrorWithQuickFix(project, DartBundle.message("dart.sdk.not.configured"));
+    }
+
+    final File sdkDir = new File(sdkPath);
+    if (!sdkDir.isDirectory()) {
+      throwErrorWithQuickFix(project, DartBundle.message("dart.sdk.bad.path", sdkPath));
+    }
+
+    final File dartDir = sdkDir.getParentFile();
+
+    final String relativePath = SystemInfo.isMac ? "chromium/Chromium.app"
+                                                 : SystemInfo.isWindows ? "chromium/chrome.exe"
+                                                                        : "chromium/chrome";
+
+    final File dartiumPath = new File(dartDir, relativePath);
+    if (!dartiumPath.exists()) {
+      throwErrorWithQuickFix(project, DartBundle.message("dartium.not.found", dartiumPath.getPath()));
+    }
+    return FileUtil.toSystemIndependentName(dartiumPath.getPath());
+  }
+
+  private static void throwErrorWithQuickFix(final @Nullable Project project,
+                                             final String message) throws RuntimeConfigurationError {
+    if (project == null) {
+      throw new RuntimeConfigurationError(message);
+    }
+    else {
+      throw new RuntimeConfigurationError(message, new Runnable() {
+        public void run() {
+          ShowSettingsUtil.getInstance().showSettingsDialog(project, DartBundle.message("dart.title"));
+        }
+      });
+    }
   }
 }
