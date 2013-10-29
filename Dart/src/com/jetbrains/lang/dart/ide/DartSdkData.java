f311fe182a20105a9965558cb9db03ba46d19d63
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2013-10-29 15:47:10 +0400

    Dead code: Dart SDK type (cherry picked from commit 1d4751b)

--- Dart/src/com/jetbrains/lang/dart/ide/DartSdkData.java
+++ Dart/src/com/jetbrains/lang/dart/ide/DartSdkData.java
@@ -1,18 +1,8 @@
 package com.jetbrains.lang.dart.ide;
 
-import com.intellij.openapi.components.PersistentStateComponent;
-import com.intellij.openapi.projectRoots.SdkAdditionalData;
-import com.intellij.util.xmlb.XmlSerializerUtil;
-
-/**
- * @author: Fedor.Korotkov
- */
-public class DartSdkData implements SdkAdditionalData, PersistentStateComponent<DartSdkData> {
+public class DartSdkData {
   private String homePath = "";
   private String version = "";
-
-  public DartSdkData() {
-  }
 
   public DartSdkData(String homePath, String version) {
     this.homePath = homePath;
@@ -25,19 +15,5 @@ public class DartSdkData implements SdkAdditionalData, PersistentStateComponent<
 
   public String getVersion() {
     return version;
-  }
-
-  @SuppressWarnings({"CloneDoesntCallSuperClone"})
-  @Override
-  public Object clone() throws CloneNotSupportedException {
-    return super.clone();
-  }
-
-  public DartSdkData getState() {
-    return this;
-  }
-
-  public void loadState(DartSdkData state) {
-    XmlSerializerUtil.copyBean(state, this);
   }
 }
