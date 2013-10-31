81cb2b769f3ffa70a9f745d6d5a16d2387cfba0a
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2013-10-31 14:27:39 +0400

    do not use deprecated DelegatingRuntimeConfiguration

--- CFML/src/com/intellij/coldFusion/mxunit/CfmlUnitConsoleProperties.java
+++ CFML/src/com/intellij/coldFusion/mxunit/CfmlUnitConsoleProperties.java
@@ -16,12 +16,11 @@
 package com.intellij.coldFusion.mxunit;
 
 import com.intellij.execution.Executor;
-import com.intellij.execution.junit.RuntimeConfigurationProducer;
 import com.intellij.execution.testframework.sm.runner.SMTRunnerConsoleProperties;
 
 public class CfmlUnitConsoleProperties extends SMTRunnerConsoleProperties {
   public CfmlUnitConsoleProperties(final CfmlUnitRunConfiguration config, Executor executor) {
-    super(new RuntimeConfigurationProducer.DelegatingRuntimeConfiguration<CfmlUnitRunConfiguration>(config), "CfmlUnit", executor);
+    super(config, "CfmlUnit", executor);
   }
 
   @Override
