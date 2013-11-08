97166d8c42a8ad26adc5f08759b75872ca327bfb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2013-11-08 16:41:41 +0100

    IDEA-115787 Better resolvement for JSF page URLs

--- Dart/src/com/jetbrains/lang/dart/ide/actions/OpenInDartiumAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/OpenInDartiumAction.java
@@ -1,21 +1,15 @@
 package com.jetbrains.lang.dart.ide.actions;
 
-import com.intellij.ide.browsers.Url;
-import com.intellij.ide.browsers.UrlOpener;
-import com.intellij.ide.browsers.WebBrowserService;
-import com.intellij.ide.browsers.WebBrowserUrlProvider;
+import com.intellij.ide.browsers.OpenFileInDefaultBrowserAction;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlFile;
 import com.intellij.psi.xml.XmlTag;
-import com.intellij.xml.XmlBundle;
 import com.intellij.xml.util.HtmlUtil;
 import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.ide.settings.DartSettingsUtil;
@@ -34,6 +28,7 @@ public class OpenInDartiumAction extends AnAction {
     super(DartBundle.message("open.in.dartium.action"), DartBundle.message("open.in.dartium.action.description"), DartIcons.Dartium_16);
   }
 
+  @Override
   public void update(final AnActionEvent e) {
     final PsiFile psiFile = CommonDataKeys.PSI_FILE.getData(e.getDataContext());
 
@@ -44,22 +39,14 @@ public class OpenInDartiumAction extends AnAction {
     e.getPresentation().setVisible(available);
   }
 
+  @Override
   public void actionPerformed(final AnActionEvent e) {
     final PsiFile psiFile = CommonDataKeys.PSI_FILE.getData(e.getDataContext());
     if (!(psiFile instanceof XmlFile)) return;
     final String dartiumPath = DartSettingsUtil.getDartiumPath();
     if (dartiumPath == null || !isHtmlFileWithDartScript(((XmlFile)psiFile))) return;
 
-    try {
-      Url url = WebBrowserService.getInstance().getUrlToOpen(psiFile, (e.getModifiers() & InputEvent.SHIFT_MASK) != 0);
-      if (url != null) {
-        FileDocumentManager.getInstance().saveAllDocuments(); // for file and linked resources
-        UrlOpener.launchBrowser(url.toExternalForm(), DartSettingsUtil.DARTIUM);
-      }
-    }
-    catch (WebBrowserUrlProvider.BrowserException e1) {
-      Messages.showErrorDialog(e1.getMessage(), XmlBundle.message("browser.error"));
-    }
+    OpenFileInDefaultBrowserAction.doOpen(psiFile, (e.getModifiers() & InputEvent.SHIFT_MASK) != 0, DartSettingsUtil.DARTIUM);
   }
 
   public static boolean isHtmlFileWithDartScript(final @NotNull XmlFile psiFile) {
