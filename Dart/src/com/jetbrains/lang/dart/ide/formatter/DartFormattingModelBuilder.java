fedcf8c2677886db212459bf51af986cccd56586
Dmitry Jemerov <yole@jetbrains.com>
2013-11-11 15:13:20 +0100

    remove redundant delegating class

--- Dart/src/com/jetbrains/lang/dart/ide/formatter/DartFormattingModelBuilder.java
+++ Dart/src/com/jetbrains/lang/dart/ide/formatter/DartFormattingModelBuilder.java
@@ -2,6 +2,7 @@ package com.jetbrains.lang.dart.ide.formatter;
 
 import com.intellij.formatting.FormattingModel;
 import com.intellij.formatting.FormattingModelBuilder;
+import com.intellij.formatting.FormattingModelProvider;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
@@ -19,10 +20,10 @@ public class DartFormattingModelBuilder implements FormattingModelBuilder {
   @Override
   public FormattingModel createModel(PsiElement element, CodeStyleSettings settings) {
     final PsiFile psiFile = element.getContainingFile();
-    return new DartFormattingModel(
+    return FormattingModelProvider.createFormattingModelForPsiFile(
       psiFile,
-      settings,
-      new DartBlock(psiFile instanceof DartFile ? psiFile.getNode() : element.getNode(), null, null, settings)
+      new DartBlock(psiFile instanceof DartFile ? psiFile.getNode() : element.getNode(), null, null, settings),
+      settings
     );
   }
 
