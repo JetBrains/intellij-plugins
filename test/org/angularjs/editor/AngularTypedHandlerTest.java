f3a08778157b1278ba7cd1d18bd8078e02d4930b
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2013-11-27 15:29:14 +0400

    typed handler test

---
+++ test/org/angularjs/editor/AngularTypedHandlerTest.java
@@ -0,0 +1,34 @@
+package org.angularjs.editor;
+
+import com.intellij.ide.highlighter.HtmlFileType;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import org.angularjs.settings.AngularJSConfig;
+
+/**
+ * Created by denofevil on 27/11/13.
+ */
+public class AngularTypedHandlerTest extends LightPlatformCodeInsightFixtureTestCase {
+    public void testBracketsClosing() {
+        myFixture.configureByText(HtmlFileType.INSTANCE, "{<caret>");
+        myFixture.type("{");
+        myFixture.checkResult("{{<caret>}}");
+    }
+
+    public void testBracketsNotClosingTwice() {
+        myFixture.configureByText(HtmlFileType.INSTANCE, "{<caret>}}");
+        myFixture.type("{");
+        myFixture.checkResult("{{<caret>}}");
+    }
+
+    public void testInserWhitespace() {
+        boolean oldWhitespace = AngularJSConfig.getInstance().INSERT_WHITESPACE;
+        try {
+            AngularJSConfig.getInstance().INSERT_WHITESPACE = true;
+            myFixture.configureByText(HtmlFileType.INSTANCE, "{<caret>");
+            myFixture.type("{");
+            myFixture.checkResult("{{ <caret> }}");
+        } finally {
+            AngularJSConfig.getInstance().INSERT_WHITESPACE = oldWhitespace;
+        }
+    }
+}
