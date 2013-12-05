28273b5b5b6a067ec7125d179d48bed65bb4a9d6
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2013-12-05 17:55:57 +0400

    cleanup

--- AngularJS/src/org/angularjs/codeInsight/attributes/AngularAttributeDescriptor.java
+++ AngularJS/src/org/angularjs/codeInsight/attributes/AngularAttributeDescriptor.java
@@ -3,17 +3,12 @@ package org.angularjs.codeInsight.attributes;
 import com.intellij.lang.javascript.index.AngularDirectivesIndex;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.PsiReferenceBase;
-import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlElement;
 import com.intellij.util.ArrayUtil;
 import com.intellij.util.indexing.FileBasedIndex;
 import com.intellij.util.indexing.ID;
 import com.intellij.xml.impl.BasicXmlAttributeDescriptor;
 import org.angularjs.index.AngularIndexUtil;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Dennis.Ushakov
@@ -90,56 +85,5 @@ public class AngularAttributeDescriptor extends BasicXmlAttributeDescriptor {
       return AngularIndexUtil.resolve(xmlElement.getProject(), myIndex, value);
     }
     return super.getEnumeratedValueDeclaration(xmlElement, value);
-  }
-
-  public PsiReference[] getReferences(PsiElement element) {
-    if (myIndex == null) return PsiReference.EMPTY_ARRAY;
-
-    return new PsiReference[]{
-      new AngularAttributeReference((XmlAttributeValue)element, myIndex)
-    };
-  }
-
-  protected static class AngularAttributeReference extends PsiReferenceBase<XmlAttributeValue> {
-    private final ID<String,Void> myIndex;
-
-    public AngularAttributeReference(XmlAttributeValue value, final ID<String, Void> index) {
-      super(value, value.getValueTextRange().shiftRight(1 - value.getTextOffset()));
-      myIndex = index;
-    }
-
-    @Nullable
-    @Override
-    public PsiElement resolve() {
-      final XmlAttributeValue element = getElement();
-      final String key = element.getValue();
-      return AngularIndexUtil.resolve(element.getProject(), myIndex, key);
-    }
-
-    @NotNull
-    @Override
-    public Object[] getVariants() {
-      return ArrayUtil.EMPTY_OBJECT_ARRAY;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-
-      AngularAttributeReference reference = (AngularAttributeReference)o;
-
-      if (!getRangeInElement().equals(reference.getRangeInElement())) return false;
-      if (!getElement().equals(reference.getElement())) return false;
-
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      int result = getRangeInElement().hashCode();
-      result = 31 * result + getElement().hashCode();
-      return result;
-    }
   }
 }
