66d7b82d370bc974bf0a01e3a64ae30808a47891
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2013-12-12 14:08:13 +0400

    don't use karma original runner, because of extra logging (cherry picked from commit d2ef99a) CR-WS-290

--- js-karma/src/js_reporter/karma-intellij/lib/intellijRunner.js
+++ js-karma/src/js_reporter/karma-intellij/lib/intellijRunner.js
@@ -1,22 +1,80 @@
-var cli = require('./intellijCli')
+var http = require('http')
+  , cli = require('./intellijCli')
   , intellijUtil = require('./intellijUtil')
-  , RESUME_TEST_RUNNING_MESSAGE = 'resume-test-running';
+  , RESUME_TEST_RUNNING_MESSAGE = 'resume-test-running'
+  , EXIT_CODE_BUF = new Buffer('\x1FEXIT');
+
+/**
+ * @param {Buffer} buffer
+ * @returns {Buffer}
+ */
+function stripExitCodeInfo(buffer) {
+  if (!Buffer.isBuffer(buffer)) {
+    return buffer;
+  }
+  var tailPos = buffer.length - EXIT_CODE_BUF.length - 1;
+  if (tailPos < 0) {
+    return buffer;
+  }
+  for (var i = 0; i < EXIT_CODE_BUF.length; i++) {
+    if (buffer[tailPos + i] !== EXIT_CODE_BUF[i]) {
+      return buffer;
+    }
+  }
+  if (tailPos === 0) {
+    return null;
+  }
+  return buffer.slice(0, tailPos);
+}
+
+function runWithConfig(config) {
+  var options = {
+    hostname: config.hostname,
+    path: config.urlRoot + 'run',
+    port: config.port,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
+  var request = http.request(options, function(response) {
+    response.on('data', function(buffer) {
+      var out = stripExitCodeInfo(buffer);
+      if (out != null) {
+        process.stdout.write(out);
+      }
+    });
+  });
+
+  request.on('error', function(e) {
+    if (e.code === 'ECONNREFUSED') {
+      console.error('There is no server listening on port %d', options.port);
+    } else {
+      throw e;
+    }
+  });
+
+  request.end(JSON.stringify({
+    args: config.clientArgs,
+    removedFiles: config.removedFiles,
+    changedFiles: config.changedFiles,
+    addedFiles: config.addedFiles,
+    refresh: config.refresh
+  }));
+}
 
 function runTests() {
-  var runner = cli.requireKarmaModule('lib/runner.js');
   var serverPort = cli.getServerPort();
   var urlRoot = cli.getUrlRoot() || '/';
   if (urlRoot.charAt(urlRoot.length - 1) !== '/') {
     urlRoot = urlRoot + '/';
   }
-  runner.run(
-    {
-      port: serverPort,
-      refresh: false,
-      urlRoot: urlRoot
-    },
-    function() {}
-  );
+  runWithConfig({
+    port: serverPort,
+    refresh: false,
+    urlRoot: urlRoot
+  });
 }
 
 if (cli.isDebug()) {
