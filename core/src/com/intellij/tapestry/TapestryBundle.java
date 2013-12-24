d3d206e29a05da3c775414bb7e310aafa468d8fc
Alexey Kudravtsev <cdr@intellij.com>
2013-12-24 19:26:05 +0400

    notnull

--- core/src/com/intellij/tapestry/TapestryBundle.java
+++ core/src/com/intellij/tapestry/TapestryBundle.java
@@ -6,6 +6,7 @@ package com.intellij.tapestry;
 
 import com.intellij.CommonBundle;
 import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
 import java.lang.ref.Reference;
@@ -16,24 +17,24 @@ import java.util.ResourceBundle;
  * @author Alexey Chmutov
  */
 public class TapestryBundle {
-    private static Reference<ResourceBundle> ourBundle;
 
-    @NonNls
-    protected static final String PATH_TO_BUNDLE = "messages.TapestryBundle";
+  public static String message(@NotNull @PropertyKey(resourceBundle = PATH_TO_BUNDLE) String key, @NotNull Object... params) {
+    return CommonBundle.message(getBundle(), key, params);
+  }
 
-    private TapestryBundle() {
+  private static Reference<ResourceBundle> ourBundle;
+  @NonNls
+  protected static final String PATH_TO_BUNDLE = "messages.TapestryBundle";
+
+  private TapestryBundle() {
+  }
+
+  private static ResourceBundle getBundle() {
+    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
+    if (bundle == null) {
+      bundle = ResourceBundle.getBundle(PATH_TO_BUNDLE);
+      ourBundle = new SoftReference<ResourceBundle>(bundle);
     }
-
-    public static String message(@PropertyKey(resourceBundle = PATH_TO_BUNDLE)String key, Object... params) {
-        return CommonBundle.message(getBundle(), key, params);
-    }
-
-    private static ResourceBundle getBundle() {
-      ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-      if (bundle == null) {
-        bundle = ResourceBundle.getBundle(PATH_TO_BUNDLE);
-        ourBundle = new SoftReference<ResourceBundle>(bundle);
-      }
-      return bundle;
-    }
+    return bundle;
+  }
 }
