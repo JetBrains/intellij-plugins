f76acf913867be9b034615fdbbb89755ddc8507a
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2013-12-24 18:20:30 +0400

    WEB-10160 Dart plugin Spelling corrector

---
+++ Dart/testData/highlighting/Spelling.dart
@@ -0,0 +1,10 @@
+// no_typo <TYPO descr="Typo: In word 'typoo'">typoo</TYPO> CorrectSpelling <TYPO descr="Typo: In word 'Baad'">Baad</TYPO><TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>
+
+var correctSpelling;
+var <TYPO descr="Typo: In word 'baad'">baad</TYPO><TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>;
+
+class CorrectSpelling{}
+class <TYPO descr="Typo: In word 'Baad'">Baad</TYPO><TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>{}
+
+void correctSpelling(){}
+void <TYPO descr="Typo: In word 'baad'">baad</TYPO><TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>(){}
