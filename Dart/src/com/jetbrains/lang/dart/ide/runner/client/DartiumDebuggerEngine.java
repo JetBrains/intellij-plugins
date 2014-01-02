4c5cbcd8d990a9d6c96c3c58c9c7f32bd0bd4814
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2014-01-02 14:03:03 +0100

    remove "Open in Dartium" action

--- Dart/src/com/jetbrains/lang/dart/ide/runner/client/DartiumDebuggerEngine.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/client/DartiumDebuggerEngine.java
@@ -3,12 +3,24 @@ package com.jetbrains.lang.dart.ide.runner.client;
 import com.intellij.chromeConnector.debugger.ChromeDebuggerEngine;
 import com.intellij.execution.configurations.RuntimeConfigurationError;
 import com.intellij.ide.browsers.WebBrowser;
+import com.intellij.ide.browsers.actions.BaseOpenInBrowserAction;
+import com.intellij.ide.browsers.actions.OpenInBrowserActionProducer;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlFile;
-import com.jetbrains.lang.dart.ide.actions.OpenInDartiumAction;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.xml.util.HtmlUtil;
 import com.jetbrains.lang.dart.ide.settings.DartSettingsUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.List;
 
 public class DartiumDebuggerEngine extends ChromeDebuggerEngine {
   public DartiumDebuggerEngine() {
@@ -27,9 +39,40 @@ public class DartiumDebuggerEngine extends ChromeDebuggerEngine {
   }
 
   @Override
-  protected boolean isPreferredEngineForFile(final PsiFile psiFile) {
+  protected boolean isPreferredEngineForFile(@NotNull PsiFile psiFile) {
     return psiFile instanceof XmlFile &&
            DartSettingsUtil.getDartiumPath() != null &&
-           OpenInDartiumAction.isHtmlFileWithDartScript(((XmlFile)psiFile));
+           isHtmlFileWithDartScript(psiFile);
+  }
+
+  private static boolean isHtmlFileWithDartScript(@Nullable PsiFile psiFile) {
+    if (psiFile == null || !HtmlUtil.isHtmlFile(psiFile)) {
+      return false;
+    }
+
+    final String text = psiFile.getText();
+    int i = -1;
+    while ((i = text.indexOf("application/dart", i + 1)) != -1) {
+      final PsiElement element = psiFile.findElementAt(i);
+      if (element != null && element.getParent() instanceof XmlAttributeValue) {
+        if (HtmlUtil.isScriptTag(PsiTreeUtil.getParentOfType(element, XmlTag.class))) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  final static class MyOpenInBrowserActionProducer extends OpenInBrowserActionProducer {
+    @Override
+    public List<AnAction> getActions() {
+      return Collections.<AnAction>singletonList(new BaseOpenInBrowserAction(DartSettingsUtil.DARTIUM) {
+        @Nullable
+        @Override
+        protected WebBrowser getBrowser(@NotNull AnActionEvent event) {
+          return DartSettingsUtil.getDartiumPath() == null ? null : DartSettingsUtil.DARTIUM;
+        }
+      });
+    }
   }
 }
