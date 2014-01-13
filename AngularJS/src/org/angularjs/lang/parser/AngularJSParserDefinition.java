d199dd6b9756e4caece7a86638adc52a1f8328ab
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-01-14 01:53:40 +0400

    correct language of file element type

--- AngularJS/src/org/angularjs/lang/parser/AngularJSParserDefinition.java
+++ AngularJS/src/org/angularjs/lang/parser/AngularJSParserDefinition.java
@@ -2,8 +2,11 @@ package org.angularjs.lang.parser;
 
 import com.intellij.lang.PsiParser;
 import com.intellij.lang.javascript.JavascriptParserDefinition;
+import com.intellij.lang.javascript.types.JSFileElementType;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.tree.IFileElementType;
+import org.angularjs.lang.AngularJSLanguage;
 import org.angularjs.lang.lexer.AngularJSLexer;
 import org.jetbrains.annotations.NotNull;
 
@@ -11,6 +14,8 @@ import org.jetbrains.annotations.NotNull;
  * @author Dennis.Ushakov
  */
 public class AngularJSParserDefinition extends JavascriptParserDefinition {
+  private static final IFileElementType FILE = new JSFileElementType(AngularJSLanguage.INSTANCE);
+
   @NotNull
   @Override
   public Lexer createLexer(Project project) {
@@ -21,5 +26,10 @@ public class AngularJSParserDefinition extends JavascriptParserDefinition {
   @Override
   public PsiParser createParser(Project project) {
     return new AngularParser();
+  }
+
+  @Override
+  public IFileElementType getFileNodeType() {
+    return FILE;
   }
 }
