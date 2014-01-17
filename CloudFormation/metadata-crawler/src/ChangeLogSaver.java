3fbf9f9cf37687ec5388cd8eca3f57ce4c1cfea4
Leonid Shalupov <shalupov@diverse.org.ru>
2014-01-17 21:56:59 +0100

    write changelog in txt format

--- CloudFormation/metadata-crawler/src/ChangeLogSaver.java
+++ CloudFormation/metadata-crawler/src/ChangeLogSaver.java
@@ -1,16 +1,16 @@
-import org.apache.commons.io.IOUtils;
+import net.htmlparser.jericho.Source;
+import org.apache.commons.io.FileUtils;
 
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.File;
 import java.net.URL;
 
 public class ChangeLogSaver {
   public static void saveChangeLog() throws Exception {
     URL url = new URL("http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/DocumentHistory.html");
-    try (InputStream input = url.openStream();
-         OutputStream output = new FileOutputStream("testData/CloudFormation-ChangeLog.html")) {
-      IOUtils.copy(input, output);
-    }
+
+    Source source = new Source(url);
+    String renderedText = source.getRenderer().toString();
+
+    FileUtils.write(new File("testData/CloudFormation-ChangeLog.txt"), renderedText);
   }
 }
