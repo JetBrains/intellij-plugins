891325e08b7236592ac916d8960d25e5ff30913f
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-01-18 13:47:13 +0400

    prioritize angular variants

--- AngularJS/src/org/angularjs/codeInsight/AngularJSCompletionContributor.java
+++ AngularJS/src/org/angularjs/codeInsight/AngularJSCompletionContributor.java
@@ -3,11 +3,11 @@ package org.angularjs.codeInsight;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResultSet;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.lang.Language;
-import com.intellij.lang.javascript.completion.JavaScriptCompletionData;
+import com.intellij.lang.javascript.completion.JSLookupUtilImpl;
 import com.intellij.lang.javascript.psi.JSNamedElement;
 import com.intellij.lang.javascript.psi.impl.JSReferenceExpressionImpl;
+import com.intellij.lang.javascript.psi.resolve.VariantsProcessor;
 import com.intellij.openapi.application.AccessToken;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.psi.PsiReference;
@@ -19,17 +19,18 @@ import org.angularjs.lang.AngularJSLanguage;
  * @author Dennis.Ushakov
  */
 public class AngularJSCompletionContributor extends CompletionContributor {
+  private static final int NG_VARIABLE_PRIORITY = VariantsProcessor.LookupPriority.LOCAL_SCOPE_MAX_PRIORITY;
+
   @Override
   public void fillCompletionVariants(final CompletionParameters parameters, final CompletionResultSet result) {
     if (!getElementLanguage(parameters).is(AngularJSLanguage.INSTANCE)) return;
     PsiReference ref = parameters.getPosition().getContainingFile().findReferenceAt(parameters.getOffset());
 
     if (ref instanceof JSReferenceExpressionImpl) {
-      final JavaScriptCompletionData.JSInsertHandler insertHandler = new JavaScriptCompletionData.JSInsertHandler();
       AngularJSProcessor.process(parameters.getPosition(), new Consumer<JSNamedElement>() {
         @Override
         public void consume(JSNamedElement element) {
-          result.consume(LookupElementBuilder.createWithIcon(element).withInsertHandler(insertHandler));
+          result.consume(JSLookupUtilImpl.createPrioritizedLookupItem(element, element.getName(), NG_VARIABLE_PRIORITY, false, false));
         }
       });
     }
