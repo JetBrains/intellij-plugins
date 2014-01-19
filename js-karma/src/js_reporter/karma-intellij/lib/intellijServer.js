2a75f237ff4e6ff78c373a3779a94216cb8022c9
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2014-01-20 00:21:16 +0400

    terminate karma server if it's an orphan process

--- js-karma/src/js_reporter/karma-intellij/lib/intellijServer.js
+++ js-karma/src/js_reporter/karma-intellij/lib/intellijServer.js
@@ -4,3 +4,12 @@ var cli = require('./intellijCli.js')
 server.start({
   configFile: require.resolve('./intellij.conf.js')
 });
+
+// Prevent karma server from being an orphan process.
+// For example, if WebStorm is killed using SIGKILL, karma server will still be alive.
+// When WebStorm is terminated, karma server's standard input is closed automatically.
+process.stdin.resume();
+process.stdin.on('close', function () {
+  // terminating orphan process
+  process.exit(123);
+});
