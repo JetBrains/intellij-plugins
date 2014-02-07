3b1e2fdb2987de2e97feea74d82283c5dfc26034
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2014-02-07 18:33:19 +0400

    fix test source: invalidate caches on unsetting JS language level

--- JsTestDriver/test/testSrc/com/google/jstestdriver/idea/assertFramework/jstd/JstdHighlightingTest.java
+++ JsTestDriver/test/testSrc/com/google/jstestdriver/idea/assertFramework/jstd/JstdHighlightingTest.java
@@ -1,10 +1,12 @@
 package com.google.jstestdriver.idea.assertFramework.jstd;
 
 import com.google.jstestdriver.idea.JstdTestRoot;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
 import com.intellij.codeInspection.LocalInspectionTool;
 import com.intellij.lang.javascript.JSDaemonAnalyzerTestCase;
 
 import java.io.IOException;
+import java.util.Collection;
 
 public class JstdHighlightingTest extends JSDaemonAnalyzerTestCase {
   @Override
@@ -22,8 +24,9 @@ public class JstdHighlightingTest extends JSDaemonAnalyzerTestCase {
     return new LocalInspectionTool[] {};
   }
 
-  private void defaultTest() throws IOException {
-    doTestFor(true, getTestName(true)+".js");
+  @Override
+  protected Collection<HighlightInfo> defaultTest() throws IOException {
+    return doTestFor(true, getTestName(true)+".js");
   }
 
   @Override
