47e839a2dbbbc67a2d1660de9df7c09fa8ec25de
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2014-02-14 18:03:11 +0100

    osmorc: NPE in bundle compiler

--- osmorc/osmorc-jps-plugin/src/org/jetbrains/jps/osmorc/model/impl/OsmorcGlobalExtensionProperties.java
+++ osmorc/osmorc-jps-plugin/src/org/jetbrains/jps/osmorc/model/impl/OsmorcGlobalExtensionProperties.java
@@ -1,5 +1,6 @@
 package org.jetbrains.jps.osmorc.model.impl;
 
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.xmlb.annotations.AbstractCollection;
 import org.jetbrains.jps.osmorc.model.JpsFrameworkInstanceDefinition;
 import org.jetbrains.jps.osmorc.model.JpsLibraryBundlificationRule;
@@ -10,10 +11,9 @@ import java.util.List;
  * @author michael.golubev
  */
 public class OsmorcGlobalExtensionProperties {
-
   @AbstractCollection(elementTag = "frameworkDefinition")
-  public List<JpsFrameworkInstanceDefinition> myFrameworkInstanceDefinitions;
+  public List<JpsFrameworkInstanceDefinition> myFrameworkInstanceDefinitions = ContainerUtil.emptyList();
 
   @AbstractCollection(elementTag = "libraryBundlificationRule")
-  public List<JpsLibraryBundlificationRule> myLibraryBundlificationRules;
+  public List<JpsLibraryBundlificationRule> myLibraryBundlificationRules = ContainerUtil.emptyList();
 }
