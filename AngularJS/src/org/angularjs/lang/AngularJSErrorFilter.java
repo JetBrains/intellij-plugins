421c796c28f622bedf71dc5ebc4fc4589434ef17
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-02-14 15:40:42 +0400

    angular error suppressor is too aggressive

--- AngularJS/src/org/angularjs/lang/AngularJSErrorFilter.java
+++ AngularJS/src/org/angularjs/lang/AngularJSErrorFilter.java
@@ -1,9 +1,13 @@
 package org.angularjs.lang;
 
 import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
+import com.intellij.lang.injection.InjectedLanguageManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiErrorElement;
-import org.angularjs.editor.AngularJSBracesUtil;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlAttribute;
 import org.angularjs.index.AngularIndexUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -14,10 +18,22 @@ public class AngularJSErrorFilter extends HighlightErrorFilter {
   @Override
   public boolean shouldHighlightErrorElement(@NotNull PsiErrorElement error) {
     final Project project = error.getProject();
-    if ("CSS".equals(error.getLanguage().getID()) && AngularIndexUtil.hasAngularJS(project)) {
-      final String textNearError = error.getParent().getText().substring(error.getStartOffsetInParent());
-      return textNearError.startsWith(AngularJSBracesUtil.getInjectionEnd(project));
+    if ("CSS".equals(error.getLanguage().getID()) && PsiTreeUtil.getParentOfType(error, XmlAttribute.class) != null &&
+        AngularIndexUtil.hasAngularJS(project)) {
+      final PsiFile file = error.getContainingFile();
+
+      PsiErrorElement nextError = error;
+      while (nextError != null) {
+        if (hasAngularInjectionAt(project, file, nextError.getTextOffset())) return false;
+        nextError = PsiTreeUtil.getNextSiblingOfType(nextError, PsiErrorElement.class);
+      }
     }
     return true;
+  }
+
+  private static boolean hasAngularInjectionAt(final Project project, final PsiFile file, final int offset) {
+    final PsiElement injection =
+      InjectedLanguageManager.getInstance(project).findInjectedElementAt(file, offset);
+    return injection != null && injection.getContainingFile().getLanguage() == AngularJSLanguage.INSTANCE;
   }
 }
