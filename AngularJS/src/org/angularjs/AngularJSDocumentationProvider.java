4b49f0304998f43336cb5eb5f87c07b73c78ae11
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-02-18 18:25:25 +0400

    updated url, fixed documentation

--- AngularJS/src/org/angularjs/AngularJSDocumentationProvider.java
+++ AngularJS/src/org/angularjs/AngularJSDocumentationProvider.java
@@ -7,7 +7,9 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlTokenType;
 import org.angularjs.index.AngularDirectivesDocIndex;
 import org.angularjs.index.AngularIndexUtil;
 import org.jetbrains.annotations.NotNull;
@@ -25,9 +27,9 @@ public class AngularJSDocumentationProvider extends DocumentationProviderEx {
   public PsiElement getCustomDocumentationElement(@NotNull Editor editor,
                                                   @NotNull PsiFile file,
                                                   @Nullable PsiElement element) {
-    if (element instanceof JSNamedElementProxy) {
-      final String name = ((JSNamedElementProxy)element).getName();
-      return AngularIndexUtil.resolve(element.getProject(), AngularDirectivesDocIndex.INDEX_ID, name);
+    final IElementType elementType = element != null ? element.getNode().getElementType() : null;
+    if (elementType == XmlTokenType.XML_NAME || elementType == XmlTokenType.XML_TAG_NAME) {
+      return AngularIndexUtil.resolve(element.getProject(), AngularDirectivesDocIndex.INDEX_ID, element.getText());
     }
     return null;
   }
@@ -49,7 +51,7 @@ public class AngularJSDocumentationProvider extends DocumentationProviderEx {
         for (int i = 1; i < words.length; i++) {
           words[i] = StringUtil.capitalize(words[i]);
         }
-        return Collections.singletonList("http://docs.angularjs.org/api/ng.directive:" + StringUtil.join(words));
+        return Collections.singletonList("http://docs.angularjs.org/api/ng/directive/" + StringUtil.join(words));
       }
     }
     return null;
