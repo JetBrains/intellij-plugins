9db5dd4b42f4de21b2c7484a62a5dd01c0d4a6f8
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-03-05 14:34:40 +0400

    fix indexing for 1.2.14

--- AngularJS/test/org/angularjs/codeInsight/data/filters/angular.js
+++ AngularJS/test/org/angularjs/codeInsight/data/filters/angular.js
@@ -1,36 +1,45 @@
 /**
+ * @ngdoc directive
  * @name ng.directive:ngModel
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngChange
  */
 
 /**
  * @name ng.directive:ngApp
+ * @ngdoc directive
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngBind
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngController
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngInit
  */
 
 /**
+ * @ngdoc filter
  * @name ng.directive:ngRepeat
  */
 
 /**
+ * @ngdoc filter
  * @name ng.filter:json
  */
 
 /**
+ * @ngdoc filter
  * @name ng.filter:lowercase
  */
 angular.directive({ngModel: ngModelDirective})
--- AngularJS/test/org/angularjs/codeInsight/data/injections/angular.js
+++ AngularJS/test/org/angularjs/codeInsight/data/injections/angular.js
@@ -1,20 +1,25 @@
 /**
+ * @ngdoc directive
  * @name ng.directive:ngModel
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngChange
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngApp
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngBind
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngController
  * @param {expression} ngController Name of a globally accessible constructor function or an
  *     {@link guide/expression expression} that on the current scope evaluates to a
@@ -23,11 +28,13 @@
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngInit
  * @param {expression} ngInit {@link guide/expression Expression} to eval.
  */
 
 /**
+ * @ngdoc directive
  * @name ng.directive:ngRepeat
  * @param {repeat_expression} ngRepeat The expression indicating how to enumerate a collection. These
  *   formats are currently supported:
