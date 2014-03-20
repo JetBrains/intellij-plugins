023bf4988a1f17cd0ea54508628e1d418f7075eb
Michael Golubev <michael.golubev@jetbrains.com>
2014-03-20 13:40:15 +0100

    IDEA-122452 - Make fails on Error: java.lang.NullPointerException since 13.1 (cherry picked from commit 6a8ab58) [r=Nikolay.Chashnikov] (cherry picked from commit 9321e8b)

--- osmorc/osmorc-jps-plugin/src/org/jetbrains/jps/osmorc/model/impl/JpsOsmorcGlobalExtensionSerializer.java
+++ osmorc/osmorc-jps-plugin/src/org/jetbrains/jps/osmorc/model/impl/JpsOsmorcGlobalExtensionSerializer.java
@@ -3,6 +3,7 @@ package org.jetbrains.jps.osmorc.model.impl;
 import com.intellij.util.xmlb.XmlSerializer;
 import org.jdom.Element;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.jetbrains.jps.model.JpsGlobal;
 import org.jetbrains.jps.model.serialization.JpsGlobalExtensionSerializer;
 import org.jetbrains.jps.osmorc.model.JpsOsmorcExtensionService;
@@ -19,6 +20,15 @@ public class JpsOsmorcGlobalExtensionSerializer extends JpsGlobalExtensionSerial
   @Override
   public void loadExtension(@NotNull JpsGlobal element, @NotNull Element componentTag) {
     OsmorcGlobalExtensionProperties properties = XmlSerializer.deserialize(componentTag, OsmorcGlobalExtensionProperties.class);
+    doLoadExtension(properties);
+  }
+
+  @Override
+  public void loadExtensionWithDefaultSettings(@NotNull JpsGlobal global) {
+    doLoadExtension(null);
+  }
+
+  private static void doLoadExtension(@Nullable OsmorcGlobalExtensionProperties properties) {
     if (properties == null) {
       properties = new OsmorcGlobalExtensionProperties();
     }
