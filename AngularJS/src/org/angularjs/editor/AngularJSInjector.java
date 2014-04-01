eb9b3ee10555819a754150b782a5fe2185cabfb6
Dennis Ushakov <dennis.ushakov@jetbrains.com>
2014-04-02 01:15:20 +0400

    enable injections in injection start/end do not conflict with template (cherry picked from commit 0e04ea4) r=ulitin

--- AngularJS/src/org/angularjs/editor/AngularJSInjector.java
+++ AngularJS/src/org/angularjs/editor/AngularJSInjector.java
@@ -3,16 +3,15 @@ package org.angularjs.editor;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.lang.javascript.JSTargetedInjector;
-import com.intellij.psi.xml.XmlTokenType;
-import org.angularjs.codeInsight.attributes.AngularAttributesRegistry;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.psi.MultiplePsiFilesPerDocumentFileViewProvider;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.impl.source.xml.XmlAttributeValueImpl;
 import com.intellij.psi.impl.source.xml.XmlTextImpl;
 import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlTokenType;
+import org.angularjs.codeInsight.attributes.AngularAttributesRegistry;
 import org.angularjs.index.AngularIndexUtil;
 import org.angularjs.lang.AngularJSLanguage;
 import org.jetbrains.annotations.NotNull;
@@ -26,9 +25,6 @@ import java.util.List;
 public class AngularJSInjector implements MultiHostInjector, JSTargetedInjector {
   @Override
   public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
-    // inject only in non-template languages, because otherwise we can get conflict with other templating mechanisms (e.g. Handlebars)
-    if (context.getContainingFile().getViewProvider() instanceof MultiplePsiFilesPerDocumentFileViewProvider) return;
-
     // check that we have angular directives indexed before injecting
     final Project project = context.getProject();
     if (!AngularIndexUtil.hasAngularJS(project)) return;
@@ -45,13 +41,17 @@ public class AngularJSInjector implements MultiHostInjector, JSTargetedInjector 
     }
 
     if (context instanceof XmlTextImpl || context instanceof XmlAttributeValueImpl) {
+      final String start = AngularJSBracesUtil.getInjectionStart(project);
+      final String end = AngularJSBracesUtil.getInjectionEnd(project);
+
+      if (AngularJSBracesUtil.hasConflicts(start, end, context.getContainingFile())) return;
+
       final String text = context.getText();
       int startIndex;
       int endIndex = -1;
       do {
-        final String start = AngularJSBracesUtil.getInjectionStart(project);
         startIndex = text.indexOf(start, endIndex);
-        endIndex = startIndex >= 0 ? text.indexOf(AngularJSBracesUtil.getInjectionEnd(project), startIndex) : -1;
+        endIndex = startIndex >= 0 ? text.indexOf(end, startIndex) : -1;
         endIndex = endIndex > 0 ? endIndex : text.length();
         final PsiElement injectionCandidate = startIndex >= 0 ? context.findElementAt(startIndex) : null;
         if (injectionCandidate != null && injectionCandidate.getNode().getElementType() != XmlTokenType.XML_COMMENT_CHARACTERS) {
