254d681ce416f16cc646535639751e61fc86728f
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-04-10 17:36:51 +0400

    DartLineBreakpointType even when debugging in browser (cherry picked from commit a70453a)

--- Dart/src/com/jetbrains/lang/dart/ide/runner/client/DartWebDebugAware.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/client/DartWebDebugAware.java
@@ -1,26 +1,32 @@
 package com.jetbrains.lang.dart.ide.runner.client;
 
 import com.intellij.openapi.fileTypes.FileType;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.xdebugger.breakpoints.XBreakpointType;
+import com.intellij.xdebugger.breakpoints.XLineBreakpointType;
 import com.jetbrains.javascript.debugger.JavaScriptDebugAware;
-import com.jetbrains.lang.dart.DartFileType;
+import com.jetbrains.lang.dart.ide.runner.DartLineBreakpointType;
 import com.jetbrains.lang.dart.psi.DartId;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 final class DartWebDebugAware extends JavaScriptDebugAware {
-  @NotNull
+  @Nullable
   @Override
   public FileType getFileType() {
-    return DartFileType.INSTANCE; // todo return null in 13.1.1 (when JavaScriptDebugAware.isOnlySourceMappedBreakpoints() is introduced)
+    return null;
   }
 
-  // todo uncomment in 13.1.1 (when JavaScriptDebugAware.isOnlySourceMappedBreakpoints() is introduced)
-  //@Nullable
-  //public XLineBreakpointType<?> getBreakpointTypeClass(@NotNull final Project project) {
-  //  return XBreakpointType.EXTENSION_POINT_NAME.findExtension(DartLineBreakpointType.class);
-  //}
+  public boolean isOnlySourceMappedBreakpoints() {
+    return false;
+  }
+
+  @Nullable
+  public XLineBreakpointType<?> getBreakpointTypeClass(@NotNull final Project project) {
+    return XBreakpointType.EXTENSION_POINT_NAME.findExtension(DartLineBreakpointType.class);
+  }
 
   @Nullable
   @Override
