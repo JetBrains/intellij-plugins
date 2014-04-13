fff360c772340875af95491857648de4f17d1ebf
Leonid Shalupov <shalupov@diverse.org.ru>
2014-04-13 23:41:14 +0200

    "ref" live template

---
+++ CloudFormation/src/com/intellij/aws/cloudformation/CloudFormationCodeContextType.java
@@ -0,0 +1,21 @@
+package com.intellij.aws.cloudformation;
+
+import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.lang.javascript.JavaScriptCodeContextType;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class CloudFormationCodeContextType extends TemplateContextType {
+  @NonNls private static final String CLOUDFORMATION_TEMPLATE = "AWS_CLOUD_FORMATION";
+
+  public CloudFormationCodeContextType() {
+    super(CLOUDFORMATION_TEMPLATE, CloudFormationBundle.getString("aws.cloudformation.template.context.type"),
+          JavaScriptCodeContextType.class);
+  }
+
+  @Override
+  public boolean isInContext(@NotNull PsiFile file, int offset) {
+    return CloudFormationPsiUtils.isCloudFormationFile(file);
+  }
+}
