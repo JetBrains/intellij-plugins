979ac474d9527bc8126b31bbb828f6cf369efb36
pquitslund <pquitslund@gmail.com>
2014-04-29 14:12:40 -0700

    Generator peer hookup fix.

--- Dart/src/com/jetbrains/lang/dart/ide/module/DartModuleBuilder.java
+++ Dart/src/com/jetbrains/lang/dart/ide/module/DartModuleBuilder.java
@@ -1,6 +1,8 @@
 package com.jetbrains.lang.dart.ide.module;
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder;
+import com.intellij.ide.util.projectWizard.ModuleWizardStep;
+import com.intellij.ide.util.projectWizard.SettingsStep;
 import com.intellij.openapi.module.ModifiableModuleModel;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleType;
@@ -8,6 +10,7 @@ import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModifiableRootModel;
 import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -53,6 +56,30 @@ public class DartModuleBuilder extends ModuleBuilder {
       doGenerate(myTemplate, module);
     }
     return module;
+  }
+
+  @Nullable
+  @Override
+  public ModuleWizardStep modifySettingsStep(@NotNull SettingsStep settingsStep) {
+    final DartProjectGenerator.GeneratorPeer peer = myTemplate.getPeer();
+    peer.buildUI(settingsStep);
+    return new ModuleWizardStep() {
+      @Override
+      public JComponent getComponent() {
+        return null;
+      }
+
+      @Override
+      public void updateDataModel() {
+      }
+
+      @Override
+      public boolean validate() throws ConfigurationException {
+        ValidationInfo info = peer.validate();
+        if (info != null) throw new ConfigurationException(info.message);
+        return true;
+      }
+    };
   }
 
   private static <T> void doGenerate(final DartProjectTemplate<T> template, final Module module) {
