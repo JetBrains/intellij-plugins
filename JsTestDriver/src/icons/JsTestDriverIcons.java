0c0c3d0d1bdc1952d944397a31f4630c7d490d3d
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2014-05-19 22:37:41 +0400

    don't register JsTestDriver server at startup (it will be registered on jstd test run)

--- JsTestDriver/src/icons/JsTestDriverIcons.java
+++ JsTestDriver/src/icons/JsTestDriverIcons.java
@@ -13,15 +13,6 @@ public class JsTestDriverIcons {
     return IconLoader.getIcon(path, JsTestDriverIcons.class);
   }
 
-  
-  public static class Browsers {
-    public static final Icon Chrome = load("/com/google/jstestdriver/idea/icons/browsers/Chrome.png"); // 32x32
-    public static final Icon Firefox = load("/com/google/jstestdriver/idea/icons/browsers/Firefox.png"); // 32x32
-    public static final Icon IE = load("/com/google/jstestdriver/idea/icons/browsers/IE.png"); // 32x32
-    public static final Icon Opera = load("/com/google/jstestdriver/idea/icons/browsers/Opera.png"); // 32x32
-    public static final Icon Safari = load("/com/google/jstestdriver/idea/icons/browsers/Safari.png"); // 32x32
-
-  }
   public static final Icon JsTestDriver = load("/com/google/jstestdriver/idea/icons/JsTestDriver.png"); // 16x16
   public static final Icon ToolWindowTestDriver = load("/com/google/jstestdriver/idea/icons/toolWindowTestDriver.png"); // 13x13
 }
