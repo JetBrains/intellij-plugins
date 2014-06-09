e145e8cf1d97839ecb969035da99c11a569f9e34
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-06-09 13:46:23 +0400

    one time binding support #WEB-12409 fixed (cherry picked from commit dea3731) r=zolotov

--- AngularJS/src/org/angularjs/lang/lexer/AngularJSTokenTypes.java
+++ AngularJS/src/org/angularjs/lang/lexer/AngularJSTokenTypes.java
@@ -9,4 +9,5 @@ public interface AngularJSTokenTypes extends JSTokenTypes {
   AngularJSTokenType ESCAPE_SEQUENCE = new AngularJSTokenType("ESCAPE_SEQUENCE");
   AngularJSTokenType INVALID_ESCAPE_SEQUENCE = new AngularJSTokenType("INVALID_ESCAPE_SEQUENCE");
   AngularJSTokenType TRACK_BY_KEYWORD = new AngularJSTokenType("TRACK_BY_KEYWORD");
+  AngularJSTokenType ONE_TIME_BINDING = new AngularJSTokenType("ONE_TIME_BINDING");
 }
