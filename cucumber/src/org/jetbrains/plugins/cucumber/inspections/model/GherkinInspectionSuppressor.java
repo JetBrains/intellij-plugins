d17a68fcb89167d609e8ad173290a4cc256b2d16
Anna Kozlova <anna.kozlova@jetbrains.com>
2014-06-17 09:42:53 +0400

    language suppressors api to provide suppressions by default as well as for multi-language inspections

---
+++ cucumber/src/org/jetbrains/plugins/cucumber/inspections/model/GherkinInspectionSuppressor.java
@@ -0,0 +1,19 @@
+package org.jetbrains.plugins.cucumber.inspections.model;
+
+import com.intellij.codeInspection.InspectionSuppressor;
+import com.intellij.codeInspection.SuppressQuickFix;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.cucumber.inspections.suppress.GherkinSuppressionUtil;
+
+public class GherkinInspectionSuppressor implements InspectionSuppressor {
+  @Override
+  public boolean isSuppressedFor(@NotNull PsiElement element, String toolId) {
+    return GherkinSuppressionUtil.isSuppressedFor(element, toolId);
+  }
+
+  @Override
+  public SuppressQuickFix[] getSuppressActions(@NotNull PsiElement element, String toolShortName) {
+    return GherkinSuppressionUtil.getDefaultSuppressActions(element, toolShortName);
+  }
+}
