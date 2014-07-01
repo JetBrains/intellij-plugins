19a60e03db9f30439ac72dffa9c5275eea2481c4
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2014-07-01 19:54:17 +0400

    better name http://crucible.labs.intellij.net/cru/CR-IC-5600#c27839

--- js-karma/src/com/intellij/javascript/karma/execution/KarmaRunConfigurationEditor.java
+++ js-karma/src/com/intellij/javascript/karma/execution/KarmaRunConfigurationEditor.java
@@ -1,6 +1,6 @@
 package com.intellij.javascript.karma.execution;
 
-import com.intellij.execution.configuration.EnvironmentVariablesTextField;
+import com.intellij.execution.configuration.EnvironmentVariablesTextFieldWithBrowseButton;
 import com.intellij.javascript.karma.KarmaBundle;
 import com.intellij.javascript.nodejs.CompletionModuleInfo;
 import com.intellij.javascript.nodejs.NodeModuleSearchUtil;
@@ -41,7 +41,7 @@ public class KarmaRunConfigurationEditor extends SettingsEditor<KarmaRunConfigur
   private final TextFieldWithHistoryWithBrowseButton myNodeInterpreterPathTextFieldWithBrowseButton;
   private final TextFieldWithHistoryWithBrowseButton myKarmaPackageDirPathTextFieldWithBrowseButton;
   private final TextFieldWithHistoryWithBrowseButton myConfigPathTextFieldWithBrowseButton;
-  private final EnvironmentVariablesTextField myEnvironmentVariablesTextField;
+  private final EnvironmentVariablesTextFieldWithBrowseButton myEnvVarsComponent;
   private final JTextField myBrowsers;
   private final JPanel myRootComponent;
 
@@ -50,7 +50,7 @@ public class KarmaRunConfigurationEditor extends SettingsEditor<KarmaRunConfigur
     myNodeInterpreterPathTextFieldWithBrowseButton = NodeUIUtil.createNodeInterpreterTextField(project);
     myKarmaPackageDirPathTextFieldWithBrowseButton = createKarmaPackageDirPathTextField(project);
     myConfigPathTextFieldWithBrowseButton = createConfigurationFileTextField(project);
-    myEnvironmentVariablesTextField = new EnvironmentVariablesTextField();
+    myEnvVarsComponent = new EnvironmentVariablesTextFieldWithBrowseButton();
     myBrowsers = createBrowsersTextField();
     JComponent browsersDescription = createBrowsersDescription();
     myRootComponent = new FormBuilder()
@@ -61,7 +61,7 @@ public class KarmaRunConfigurationEditor extends SettingsEditor<KarmaRunConfigur
       .addComponent(new JSeparator(), 8)
       .addLabeledComponent(KarmaBundle.message("runConfiguration.node_interpreter.label"), myNodeInterpreterPathTextFieldWithBrowseButton, 8)
       .addLabeledComponent(KarmaBundle.message("runConfiguration.karma_package_dir.label"), myKarmaPackageDirPathTextFieldWithBrowseButton)
-      .addLabeledComponent(KarmaBundle.message("runConfiguration.environment.label"), myEnvironmentVariablesTextField.getComponent())
+      .addLabeledComponent(KarmaBundle.message("runConfiguration.environment.label"), myEnvVarsComponent)
       .getPanel();
   }
 
@@ -186,8 +186,8 @@ public class KarmaRunConfigurationEditor extends SettingsEditor<KarmaRunConfigur
     setTextAndAddToHistory(myConfigPathTextFieldWithBrowseButton.getChildComponent(), runSettings.getConfigPath());
     myBrowsers.setText(runSettings.getBrowsers());
 
-    myEnvironmentVariablesTextField.setEnvs(runSettings.getEnvVars());
-    myEnvironmentVariablesTextField.setPassParentEnvs(runSettings.isPassParentEnvVars());
+    myEnvVarsComponent.setEnvs(runSettings.getEnvVars());
+    myEnvVarsComponent.setPassParentEnvs(runSettings.isPassParentEnvVars());
   }
 
   private static void setTextAndAddToHistory(@NotNull TextFieldWithHistory textFieldWithHistory, @Nullable String text) {
@@ -201,8 +201,8 @@ public class KarmaRunConfigurationEditor extends SettingsEditor<KarmaRunConfigur
     KarmaRunSettings.Builder builder = new KarmaRunSettings.Builder();
     builder.setConfigPath(configPath);
     builder.setBrowsers(StringUtil.notNullize(myBrowsers.getText()));
-    builder.setEnvVars(myEnvironmentVariablesTextField.getEnvs());
-    builder.setPassParentEnvVars(myEnvironmentVariablesTextField.isPassParentEnvs());
+    builder.setEnvVars(myEnvVarsComponent.getEnvs());
+    builder.setPassParentEnvVars(myEnvVarsComponent.isPassParentEnvs());
     runConfiguration.setRunSettings(builder.build());
     String karmaNodePackageDir = myKarmaPackageDirPathTextFieldWithBrowseButton.getChildComponent().getText();
     KarmaGlobalSettingsUtil.storeKarmaPackageDir(myProject, karmaNodePackageDir);
