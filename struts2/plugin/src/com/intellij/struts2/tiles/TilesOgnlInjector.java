28d28e8d6a33b7cab81001de5c04839f588d8e43
Yann Cébron <yann.cebron@jetbrains.com>
2014-07-02 11:37:40 +0200

    TilesOgnlInjector: cleanup, license header

--- struts2/plugin/src/com/intellij/struts2/tiles/TilesOgnlInjector.java
+++ struts2/plugin/src/com/intellij/struts2/tiles/TilesOgnlInjector.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 The authors
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.intellij.struts2.tiles;
 
 import com.intellij.jam.model.util.JamCommonUtil;
@@ -35,7 +50,7 @@ public class TilesOgnlInjector implements MultiHostInjector {
   @Override
   public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
     final PsiFile containingFile = context.getContainingFile();
-   if (!JamCommonUtil.isPlainXmlFile(containingFile)) {
+    if (!JamCommonUtil.isPlainXmlFile(containingFile)) {
       return;
     }
 
@@ -43,7 +58,7 @@ public class TilesOgnlInjector implements MultiHostInjector {
     if (!((XmlAttributeValue)context).getValue().startsWith(OGNL_PREFIX)) {
       return;
     }
-    
+
     PsiElement parent = context.getParent();
     if (parent instanceof XmlAttribute) {
       String name = ((XmlAttribute)parent).getLocalName();
@@ -57,7 +72,6 @@ public class TilesOgnlInjector implements MultiHostInjector {
             .addPlace(OgnlLanguage.EXPRESSION_PREFIX, OgnlLanguage.EXPRESSION_SUFFIX,
                       (PsiLanguageInjectionHost)context, ognlTextRange)
             .doneInjecting();
-          
         }
       }
     }
