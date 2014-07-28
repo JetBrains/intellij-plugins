1dfb5732583bbe192bf61433eb191701de67154b
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-07-28 16:47:31 +0400

    move setting "open htmls as handlebars" from meteor to handlebar

---
+++ handlebars/src/com/dmarcotte/handlebars/file/HbLanguageSubstitutor.java
@@ -0,0 +1,22 @@
+package com.dmarcotte.handlebars.file;
+
+import com.dmarcotte.handlebars.HbLanguage;
+import com.dmarcotte.handlebars.config.HbConfig;
+import com.intellij.lang.Language;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.LanguageSubstitutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class HbLanguageSubstitutor extends LanguageSubstitutor {
+  @Nullable
+  @Override
+  public Language getLanguage(@NotNull VirtualFile file, @NotNull Project project) {
+    if (HbConfig.shouldOpenHtmlAsHandlebars(project)) {
+      return HbLanguage.INSTANCE;
+    }
+
+    return null;
+  }
+}
