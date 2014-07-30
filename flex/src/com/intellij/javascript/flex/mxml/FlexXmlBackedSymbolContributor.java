fa02a7dae69934937d4de460221fad6ca6efc3eb
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2014-07-30 18:21:00 +0400

    fix go to JS class

--- flex/src/com/intellij/javascript/flex/mxml/FlexXmlBackedSymbolContributor.java
+++ flex/src/com/intellij/javascript/flex/mxml/FlexXmlBackedSymbolContributor.java
@@ -24,7 +24,7 @@ public class FlexXmlBackedSymbolContributor implements ChooseByNameContributor {
   public NavigationItem[] getItemsByName(String name, final String pattern, Project project, boolean includeNonProjectItems) {
     Collection<NavigationItem> result = new THashSet<NavigationItem>();
     result.addAll(FlexXmlBackedMembersIndex.getItemsByName(name, project));
-    for (NavigationItem item : JavaScriptClassContributor.getItemsByNameStatic(name, pattern, project, includeNonProjectItems)) {
+    for (NavigationItem item : JavaScriptClassContributor.getItemsByNameStatic(name, project, includeNonProjectItems)) {
       if (item instanceof XmlBackedJSClassImpl) {
         result.add(item);
       }
