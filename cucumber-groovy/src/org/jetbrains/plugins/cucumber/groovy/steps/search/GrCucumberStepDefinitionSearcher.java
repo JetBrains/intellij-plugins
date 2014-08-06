730a945873e4d76110846e42d6413d591fc28e79
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2014-08-07 01:27:03 +0400

    CucumberUtils refactored to prevent copy/paste

--- cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/steps/search/GrCucumberStepDefinitionSearcher.java
+++ cucumber-groovy/src/org/jetbrains/plugins/cucumber/groovy/steps/search/GrCucumberStepDefinitionSearcher.java
@@ -1,18 +1,11 @@
-
 package org.jetbrains.plugins.cucumber.groovy.steps.search;
 
 import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.NullableComputable;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.pom.PomTarget;
 import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
-import com.intellij.psi.search.PsiSearchHelper;
-import com.intellij.psi.search.SearchScope;
-import com.intellij.psi.search.TextOccurenceProcessor;
-import com.intellij.psi.search.UsageSearchContext;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.util.Processor;
 import com.intellij.util.QueryExecutor;
@@ -21,7 +14,6 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.CucumberUtil;
 import org.jetbrains.plugins.cucumber.groovy.GrCucumberUtil;
 import org.jetbrains.plugins.cucumber.groovy.steps.GrStepDefinition;
-import org.jetbrains.plugins.cucumber.steps.search.CucumberStepSearchUtil;
 import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrMethodCall;
 
 /**
@@ -29,7 +21,8 @@ import org.jetbrains.plugins.groovy.lang.psi.api.statements.expressions.GrMethod
  */
 public class GrCucumberStepDefinitionSearcher implements QueryExecutor<PsiReference, ReferencesSearch.SearchParameters> {
   @Override
-  public boolean execute(@NotNull final ReferencesSearch.SearchParameters queryParameters, @NotNull final Processor<PsiReference> consumer) {
+  public boolean execute(@NotNull final ReferencesSearch.SearchParameters queryParameters,
+                         @NotNull final Processor<PsiReference> consumer) {
     final PsiElement element = ApplicationManager.getApplication().runReadAction(new NullableComputable<PsiElement>() {
       @Override
       public PsiElement compute() {
@@ -46,41 +39,8 @@ public class GrCucumberStepDefinitionSearcher implements QueryExecutor<PsiRefere
         return GrCucumberUtil.getStepDefinitionPatternText((GrMethodCall)element);
       }
     });
-    if (StringUtil.isEmptyOrSpaces(regexp)) return true;
 
-    final String word = CucumberUtil.getTheBiggestWordToSearchByIndex(regexp);
-    if (StringUtil.isEmptyOrSpaces(word)) return true;
-
-    final SearchScope searchScope = CucumberStepSearchUtil.restrictScopeToGherkinFiles(new Computable<SearchScope>() {
-      public SearchScope compute() {
-        return queryParameters.getEffectiveSearchScope();
-      }
-    });
-
-
-    // As far as default CacheBasedRefSearcher doesn't look for references in string we have to write out own to handle this correctly
-    final TextOccurenceProcessor processor = new TextOccurenceProcessor() {
-      @Override
-      public boolean execute(@NotNull final PsiElement occurrence, int offsetInElement) {
-        return ApplicationManager.getApplication().runReadAction(new Computable<Boolean>() {
-          @NotNull
-          @Override
-          public Boolean compute() {
-            if (!processRefs(occurrence, element, consumer)) return false;
-
-            PsiElement parent = occurrence.getParent();
-            if (parent != null) {
-              if (!processRefs(parent, element, consumer)) return false;
-            }
-            return true;
-          }
-        });
-      }
-    };
-
-    short context = UsageSearchContext.IN_STRINGS | UsageSearchContext.IN_CODE;
-    PsiSearchHelper instance = PsiSearchHelper.SERVICE.getInstance(element.getProject());
-    return instance.processElementsWithWord(processor, searchScope, word, context, true);
+    return CucumberUtil.findGherkinReferencesToElement(element, regexp, consumer, queryParameters.getEffectiveSearchScope());
   }
 
   public static PsiElement getStepDefinition(final PsiElement element) {
@@ -96,14 +56,5 @@ public class GrCucumberStepDefinitionSearcher implements QueryExecutor<PsiRefere
     }
 
     return null;
-  }
-
-  private static boolean processRefs(PsiElement refOwner, PsiElement toSearchFor, Processor<PsiReference> consumer) {
-    for (PsiReference ref : refOwner.getReferences()) {
-      if (ref != null && ref.isReferenceTo(toSearchFor)) {
-        if (!consumer.process(ref)) return false;
-      }
-    }
-    return true;
   }
 }
