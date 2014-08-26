ac43be9f2d9d0e53220f735ee24fa21800eebfe5
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2014-08-26 14:12:14 +0400

    flex: flexreferenceimporter should always work in unambiguous fly mode

--- flex/src/com/intellij/lang/javascript/flex/FlexReferenceImporter.java
+++ flex/src/com/intellij/lang/javascript/flex/FlexReferenceImporter.java
@@ -5,11 +5,8 @@ import com.intellij.codeInsight.daemon.impl.CollectHighlightsUtil;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
 import com.intellij.lang.javascript.psi.JSFile;
 import com.intellij.lang.javascript.psi.JSReferenceExpression;
-import com.intellij.openapi.command.CommandProcessor;
-import com.intellij.openapi.command.UndoConfirmationPolicy;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.actionSystem.DocCommandGroupId;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiPolyVariantReference;
@@ -25,20 +22,17 @@ import java.util.List;
 public class FlexReferenceImporter implements ReferenceImporter {
   @Override
   public boolean autoImportReferenceAtCursor(@NotNull final Editor editor, @NotNull final PsiFile file) {
-    return doAutoImportReferenceAt(editor, file, editor.getCaretModel().getOffset(), false);
+    return doAutoImportReferenceAt(editor, file, editor.getCaretModel().getOffset());
   }
 
   @Override
   public boolean autoImportReferenceAt(@NotNull final Editor editor, @NotNull final PsiFile file, final int offset) {
-    // this method is only called from com.intellij.codeInsight.daemon.impl.ShowAutoImportPass.importUnambiguousImports()
-    // when 'Add unambiguous imports on the fly' option is enabled
-    return doAutoImportReferenceAt(editor, file, offset, true);
+    return doAutoImportReferenceAt(editor, file, offset);
   }
 
   private static boolean doAutoImportReferenceAt(final Editor editor,
                                                  final PsiFile file,
-                                                 final int offset,
-                                                 final boolean unambiguousOnTheFly) {
+                                                 final int offset) {
     if (!(file instanceof JSFile) || file.getLanguage() != JavaScriptSupportLoader.ECMA_SCRIPT_L4) return false;
 
     Document document = editor.getDocument();
@@ -50,22 +44,11 @@ public class FlexReferenceImporter implements ReferenceImporter {
     for (PsiElement element : elements) {
       if (element instanceof JSReferenceExpression && ((JSReferenceExpression)element).getQualifier() == null) {
         if (((JSReferenceExpression)element).multiResolve(false).length == 0) {
-          runImport(editor, unambiguousOnTheFly, (PsiPolyVariantReference)element);
+          new AddImportECMAScriptClassOrFunctionAction(editor, (PsiPolyVariantReference)element, true).execute();
           return true;
         }
       }
     }
     return false;
-  }
-
-  private static void runImport(final Editor editor, final boolean unambiguousOnTheFly, final PsiPolyVariantReference element) {
-    Document doc = editor.getDocument();
-    DocCommandGroupId group = DocCommandGroupId.noneGroupId(doc);
-    CommandProcessor.getInstance().executeCommand(editor.getProject(), new Runnable() {
-      @Override
-      public void run() {
-        new AddImportECMAScriptClassOrFunctionAction(editor, element, unambiguousOnTheFly).execute();
-      }
-    }, "import", group, UndoConfirmationPolicy.DEFAULT, doc);
   }
 }
