a3533c709ede472a8c78125a9c38f2a7073b8185
nik <Nikolay.Chashnikov@jetbrains.com>
2014-09-11 21:28:55 +0400

    annotations reformatted

--- js-karma/src/com/intellij/javascript/karma/execution/KarmaPackageDirSetting.java
+++ js-karma/src/com/intellij/javascript/karma/execution/KarmaPackageDirSetting.java
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 @State(
   name = "JsKarmaPackageDirSetting",
   storages = {
-    @Storage( file = StoragePathMacros.WORKSPACE_FILE)
+    @Storage(file = StoragePathMacros.WORKSPACE_FILE)
   }
 )
 public class KarmaPackageDirSetting implements PersistentStateComponent<Element> {
