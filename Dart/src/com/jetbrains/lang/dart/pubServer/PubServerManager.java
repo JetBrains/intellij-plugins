ddd11836f469c493552ee3b69fba32497ddcb41c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-09-24 15:19:42 +0400

    Dart: Stop Pub Serve if pubspec.yaml contents changed (part of WEB-13124)

--- Dart/src/com/jetbrains/lang/dart/pubServer/PubServerManager.java
+++ Dart/src/com/jetbrains/lang/dart/pubServer/PubServerManager.java
@@ -8,8 +8,9 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.openapi.vfs.*;
 import com.intellij.util.net.NetUtils;
+import com.jetbrains.lang.dart.util.PubspecYamlUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.FullHttpRequest;
 import org.jetbrains.annotations.NotNull;
@@ -39,6 +40,51 @@ public class PubServerManager implements Disposable {
 
   public PubServerManager(@NotNull Project project) {
     this.project = project;
+
+    VirtualFileManager.getInstance()
+      .addVirtualFileListener(new VirtualFileAdapter() {
+                                @Override
+                                public void beforePropertyChange(@NotNull final VirtualFilePropertyEvent event) {
+                                  if (VirtualFile.PROP_NAME.equals(event.getPropertyName())) {
+                                    contentsChanged(event);
+                                  }
+                                }
+
+                                @Override
+                                public void beforeFileMovement(@NotNull final VirtualFileMoveEvent event) {
+                                  contentsChanged(event);
+                                }
+
+                                @Override
+                                public void fileDeleted(@NotNull final VirtualFileEvent event) {
+                                  contentsChanged(event);
+                                }
+
+                                @Override
+                                public void contentsChanged(@NotNull final VirtualFileEvent event) {
+                                  final VirtualFile file = event.getFile();
+                                  if (PubspecYamlUtil.PUBSPEC_YAML.equals(file.getName()) &&
+                                      file.getFileSystem() == LocalFileSystem.getInstance()) {
+                                    pubspecYamlChanged(file);
+                                  }
+                                }
+                              },
+                              project);
+  }
+
+  private void pubspecYamlChanged(@NotNull final VirtualFile file) {
+    final VirtualFile mainDir = file.getParent();
+    if (mainDir == null) return;
+
+    // todo remove subfolder iteration when administration is done via admin port
+    for (VirtualFile subdir : mainDir.getChildren()) {
+      if (!subdir.isDirectory()) continue;
+
+      final PubServerService service = dartProjectToPubService.getIfPresent(subdir);
+      if (service != null) {
+        Disposer.dispose(service);
+      }
+    }
   }
 
   public void send(@NotNull ChannelHandlerContext clientContext,
