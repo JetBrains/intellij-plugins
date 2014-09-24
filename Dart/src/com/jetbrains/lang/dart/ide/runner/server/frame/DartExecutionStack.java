c6cd14f70c40c504662344243edc7b7968f779a3
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-09-24 18:26:22 +0400

    simplify DartExecutionStack implementation

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/frame/DartExecutionStack.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/frame/DartExecutionStack.java
@@ -9,46 +9,31 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class DartExecutionStack extends XExecutionStack {
-  @NotNull private final DartCommandLineDebugProcess myDebugProcess;
-  @Nullable private DartStackFrame myTopFrame;
-  @NotNull private final List<VmCallFrame> myVmCallFrames;
+  @NotNull private final List<DartStackFrame> myStackFrames;
 
   public DartExecutionStack(@NotNull final DartCommandLineDebugProcess debugProcess,
                             @NotNull final List<VmCallFrame> vmCallFrames,
-                            @Nullable final VmValue exception) {
+                            @Nullable VmValue exception) {
     super("");
-    myDebugProcess = debugProcess;
-    myTopFrame = vmCallFrames.isEmpty() ? null : new DartStackFrame(myDebugProcess, vmCallFrames.get(0), exception);
-    myVmCallFrames = vmCallFrames;
+    myStackFrames = new ArrayList<DartStackFrame>(vmCallFrames.size());
+    for (VmCallFrame vmCallFrame : vmCallFrames) {
+      myStackFrames.add(new DartStackFrame(debugProcess, vmCallFrame, exception));
+      exception = null; // add exception to the top frame only
+    }
   }
 
   @Override
   @Nullable
   public XStackFrame getTopFrame() {
-    return myTopFrame;
+    return myStackFrames.isEmpty() ? null : myStackFrames.get(0);
   }
 
   @Override
-  public void computeStackFrames(int firstFrameIndex, final XStackFrameContainer container) {
-    List<DartStackFrame> res = new ArrayList<DartStackFrame>();
-    // add top frame if needed
-    if (firstFrameIndex == 0 && !myVmCallFrames.isEmpty()) {
-      res.add(myTopFrame);
-      firstFrameIndex = 1;
-    }
-
-    List<VmCallFrame> toAdd = firstFrameIndex >= myVmCallFrames.size() ?
-                              Collections.<VmCallFrame>emptyList()
-                              : myVmCallFrames.subList(firstFrameIndex, myVmCallFrames.size());
-
-    for (VmCallFrame frame : toAdd) {
-      res.add(new DartStackFrame(myDebugProcess, frame, null));
-    }
-
-    container.addStackFrames(res, true);
+  public void computeStackFrames(int firstFrameIndex, @NotNull final XStackFrameContainer container) {
+    final List<DartStackFrame> result = firstFrameIndex == 0 ? myStackFrames : myStackFrames.subList(firstFrameIndex, myStackFrames.size());
+    container.addStackFrames(result, true);
   }
 }
