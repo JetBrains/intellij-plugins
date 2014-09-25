5e90ebe4609e591b0eb32ffd426597f03c23a703
Yann Cébron <yann.cebron@jetbrains.com>
2014-09-25 21:18:55 +0200

    OGNL: fix SequenceExpressionPsiTest.testNestedSimpleIntegerLiteralSequence

--- struts2/ognl/src-test/com/intellij/lang/ognl/psi/SequenceExpressionPsiTest.java
+++ struts2/ognl/src-test/com/intellij/lang/ognl/psi/SequenceExpressionPsiTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 The authors
+ * Copyright 2014 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -52,7 +52,7 @@ public class SequenceExpressionPsiTest extends PsiTestCase {
     final OgnlConditionalExpression expression =
       (OgnlConditionalExpression)parseSingleExpression("a == true ? { 1,2 } : { 2,3 }");
     assertElementType(OgnlTypes.SEQUENCE_EXPRESSION, expression.getThen());
-    assertElementType(OgnlTypes.SEQUENCE_EXPRESSION, expression.getThen());
+    assertElementType(OgnlTypes.SEQUENCE_EXPRESSION, expression.getElse());
   }
 
   private OgnlSequenceExpression parse(@Language(value = OgnlLanguage.ID,
