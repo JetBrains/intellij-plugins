379883f1f8cf41ac2bddb9226b68399d3f0017e5
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2014-10-03 12:32:24 +0400

    IDEA-130359 Overlapping collapse areas not supported correctly
    added a test for Dart

---
+++ Dart/testData/folding/CustomRegionsOverlappingWithCommentFoldings.dart
@@ -0,0 +1,12 @@
+// comment
+<fold text='Some' expand='true'>// region Some
+// comment
+
+class A <fold text='{...}' expand='true'>{
+  // comment
+  <fold text='inner' expand='true'>// region inner
+  // comment
+
+  // endregion</fold>
+}</fold>
+// endregion</fold>
