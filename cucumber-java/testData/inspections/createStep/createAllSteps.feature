6f48e9fa4c4e0cf6cb29391d94fb82e382c1d6d8
Andrey Vokin <andrey.vokin@jetbrains.com>
2014-10-09 13:03:33 +0400

    Cucumber. Create all step defs doesn't create one definition twice

--- cucumber-java/testData/inspections/createStep/createAllSteps.feature
+++ cucumber-java/testData/inspections/createStep/createAllSteps.feature
@@ -3,4 +3,5 @@ Feature: test
     Given step
     Given <warning descr="Undefined step reference: test">te<caret>st</warning>
     Given <warning descr="Undefined step reference: super test">super test</warning>
+    Given <warning descr="Undefined step reference: test">test</warning>
 
--- cucumber/src/org/jetbrains/plugins/cucumber/inspections/CucumberCreateAllStepsFix.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/inspections/CucumberCreateAllStepsFix.java
@@ -1,15 +1,21 @@
 package org.jetbrains.plugins.cucumber.inspections;
 
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiReference;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.CucumberBundle;
+import org.jetbrains.plugins.cucumber.CucumberUtil;
 import org.jetbrains.plugins.cucumber.psi.GherkinFeature;
 import org.jetbrains.plugins.cucumber.psi.GherkinFile;
 import org.jetbrains.plugins.cucumber.psi.GherkinStep;
 import org.jetbrains.plugins.cucumber.psi.GherkinStepsHolder;
 import org.jetbrains.plugins.cucumber.steps.AbstractStepDefinition;
 import org.jetbrains.plugins.cucumber.steps.reference.CucumberStepReference;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 /**
  * User: Andrey.Vokin
@@ -29,6 +35,7 @@ public class CucumberCreateAllStepsFix extends CucumberCreateStepFixBase {
       return;
     }
 
+    final Set<String> createdStepDefPatterns = new HashSet<String>();
     final GherkinFile gherkinFile = (GherkinFile)probableGherkinFile;
     for (GherkinFeature feature : gherkinFile.getFeatures()) {
       for (GherkinStepsHolder stepsHolder : feature.getScenarios()) {
@@ -39,7 +46,13 @@ public class CucumberCreateAllStepsFix extends CucumberCreateStepFixBase {
 
             final AbstractStepDefinition definition = ((CucumberStepReference)reference).resolveToDefinition();
             if (definition == null) {
-              createFileOrStepDefinition(step, file);
+              String pattern = Pattern.quote(step.getStepName());
+              pattern = StringUtil.trimEnd(StringUtil.trimStart(pattern, "\\Q"), "\\E");
+              pattern = CucumberUtil.prepareStepRegexp(pattern);
+              if (!createdStepDefPatterns.contains(pattern)) {
+                createFileOrStepDefinition(step, file);
+                createdStepDefPatterns.add(pattern);
+              }
             }
           }
         }
