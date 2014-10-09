6eb4ded0e6bfb3ede6b00568ef1b1fcdf6a43591
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-10-09 11:44:02 +0400

    Dart Observatory works in Chrome-family browsers only.

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/OpenDartObservatoryUrlAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/OpenDartObservatoryUrlAction.java
@@ -1,13 +1,21 @@
 package com.jetbrains.lang.dart.ide.runner.server;
 
-import com.intellij.ide.BrowserUtil;
+import com.intellij.ide.browsers.BrowserFamily;
+import com.intellij.ide.browsers.BrowserLauncher;
+import com.intellij.ide.browsers.WebBrowser;
+import com.intellij.ide.browsers.WebBrowserManager;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.Condition;
 import com.intellij.util.net.NetUtils;
+import com.jetbrains.browserConnection.BrowserConnectionManager;
 import com.jetbrains.lang.dart.DartBundle;
 import icons.DartIcons;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.io.webSocket.Client;
+
+import java.util.List;
 
 public class OpenDartObservatoryUrlAction extends DumbAwareAction {
   private final int myObservatoryPort;
@@ -26,6 +34,47 @@ public class OpenDartObservatoryUrlAction extends DumbAwareAction {
 
   @Override
   public void actionPerformed(@NotNull final AnActionEvent e) {
-    BrowserUtil.browse("http://" + NetUtils.getLocalHostString() + ":" + myObservatoryPort);
+    final String url = "http://" + NetUtils.getLocalHostString() + ":" + myObservatoryPort;
+
+    openUrlInChromeFamilyBrowser(url);
+  }
+
+  /**
+   * Opens new tab in any already open Chrome-family browser, if none found - start any new Chrome-family browser
+   */
+  public static void openUrlInChromeFamilyBrowser(@NotNull final String url) {
+    try {
+      final BrowserConnectionManager connectionManager = BrowserConnectionManager.getInstance();
+      final Client chromeClient = connectionManager.findClient(new Condition<Client>() {
+        @Override
+        public boolean value(final Client client) {
+          final WebBrowser browser = connectionManager.getBrowser(client);
+          return browser != null && browser.getFamily() == BrowserFamily.CHROME;
+        }
+      });
+
+      if (chromeClient != null) {
+        BrowserConnectionManager.getInstance().openUrl(chromeClient, url);
+      }
+      else {
+        openInAnyChromeFamilyBrowser(url);
+      }
+    }
+    catch (Throwable t) {
+      // ClassNotFound in Community Edition or if JavaScriptDebug plugin disabled
+      openInAnyChromeFamilyBrowser(url);
+    }
+  }
+
+  private static void openInAnyChromeFamilyBrowser(@NotNull final String url) {
+    final List<WebBrowser> chromeBrowsers = WebBrowserManager.getInstance().getBrowsers(new Condition<WebBrowser>() {
+      @Override
+      public boolean value(final WebBrowser browser) {
+        return browser.getFamily() == BrowserFamily.CHROME;
+      }
+    }, true);
+
+
+    BrowserLauncher.getInstance().browse(url, chromeBrowsers.isEmpty() ? null : chromeBrowsers.get(0));
   }
 }
