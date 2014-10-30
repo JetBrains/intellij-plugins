3370e5335a0ddb5f3c11081b5214468a05d4aee7
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-10-30 14:11:28 +0300

    Optimization and fixes for Dart generics folding

--- Dart/src/com/jetbrains/lang/dart/folding/DartCodeFoldingSettings.java
+++ Dart/src/com/jetbrains/lang/dart/folding/DartCodeFoldingSettings.java
@@ -4,7 +4,19 @@ import com.intellij.openapi.components.ServiceManager;
 
 public abstract class DartCodeFoldingSettings {
 
+<<<<<<< HEAD
   @SuppressWarnings({"WeakerAccess"}) public boolean COLLAPSE_GENERIC_PARAMETERS = false;
+=======
+@State(
+  name = "DartCodeFoldingSettings",
+  storages = {
+    @Storage(
+      file = StoragePathMacros.APP_CONFIG + "/editor.codeinsight.xml"
+    )}
+)
+public class DartCodeFoldingSettings implements PersistentStateComponent<DartCodeFoldingSettings>, ExportableComponent {
+  private boolean myCollapseGenericParams = false;
+>>>>>>> 0531d5f... Optimization and fixes for Dart generics folding
 
   public static DartCodeFoldingSettings getInstance() {
     return ServiceManager.getService(DartCodeFoldingSettings.class);
@@ -12,5 +24,16 @@ public abstract class DartCodeFoldingSettings {
 
   public abstract boolean isCollapseGenericParameters();
 
+<<<<<<< HEAD
   public abstract void setCollapseGenericParameters(boolean value);
+=======
+  // property name must be equal to checkBox() argument in DartCodeFoldingOptionsProvider
+  public boolean isCollapseGenericParameters() {
+    return myCollapseGenericParams;
+  }
+
+  public void setCollapseGenericParameters(final boolean collapseGenericParams) {
+    myCollapseGenericParams = collapseGenericParams;
+  }
+>>>>>>> 0531d5f... Optimization and fixes for Dart generics folding
 }
