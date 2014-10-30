d36f3ce89dbe603f5989335341b346e2a2b874e8
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-10-30 15:21:06 +0300

    test Dart folding for nested generics

--- Dart/testData/folding/TypeArguments.dart
+++ Dart/testData/folding/TypeArguments.dart
@@ -3,7 +3,7 @@ sclass A<T>{}
 // verify the type arguments in class declarations aren't folded
 class B<T> extends A<T> {}
 
-List<String> list1 = new List<fold text='<~>' expand='true'><String></fold>();
+List<Map<String,Object>> list1 = new List<fold text='<~>' expand='true'><Map<String,Object>></fold>();
 
 f() <fold text='{...}' expand='true'>{
 A<String> a = new A<fold text='<~>' expand='true'><String></fold>();
