7a6685dde6b80006d1695b58a3ab377ce7a76aaa
pquitslund <pquitslund@gmail.com>
2014-11-06 13:40:09 -0800

    Dart documentation URL fixes.

--- Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProvider.java
+++ Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProvider.java
@@ -1,7 +1,7 @@
 package com.jetbrains.lang.dart.ide.documentation;
 
-import com.google.common.annotations.VisibleForTesting;
 import com.intellij.lang.documentation.DocumentationProvider;
+import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -9,31 +9,17 @@ import com.jetbrains.lang.dart.DartComponentType;
 import com.jetbrains.lang.dart.psi.DartClass;
 import com.jetbrains.lang.dart.psi.DartComponent;
 import com.jetbrains.lang.dart.util.DartResolveUtil;
-import gnu.trove.THashSet;
+import com.jetbrains.lang.dart.util.DartUrlResolver;
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 public class DartDocumentationProvider implements DocumentationProvider {
   private static final String BASE_DART_DOC_URL = "http://api.dartlang.org/docs/releases/latest/";
-  private static final String STD_LIB_PREFIX = "dart.";
-  private static final String DOM_LIB_PREFIX = "dart.dom.";
-
-
-  // Scraped 08/16/2014
-  private static final Set<String> APIDOC_HOSTED_PACKAGES = new THashSet<String>(Arrays.asList(
-    "analyzer", "args", "barback", "code_transformers", "collection", "crypto", "csslib", "custom_element",
-    "fixnum", "html5lib", "http", "http_parser", "http_server", "intl", "json_rpc_2", "logging",
-    "matcher", "math", "mine", "mock", "oauth2", "observe", "path", "polymer", "polymer_expressions", "scheduled_test",
-    "serialization", "shelf", "smoke", "source_maps", "stack_trace", "string_scanner", "template_binding",
-    "typed_data", "unittest", "utf", "watcher", "web_components", "yaml"
-  ));
 
   @Override
   public String getQuickNavigateInfo(PsiElement element, PsiElement originalElement) {
@@ -52,25 +38,50 @@ public class DartDocumentationProvider implements DocumentationProvider {
       return null;
     }
 
-    final String docUrl = constructDocUrl(namedComponent, componentName, DartResolveUtil.getLibraryName(element.getContainingFile()));
+    final String libraryName = getLibraryName(element);
+    final String docUrl = constructDocUrl(namedComponent, componentName, libraryName);
     return docUrl == null ? null : Collections.singletonList(docUrl);
+  }
+
+  private String getLibraryName(@NotNull final PsiElement element) {
+    final VirtualFile virtualFile = DartResolveUtil.getRealVirtualFile(element.getContainingFile());
+    if (virtualFile == null) {
+      return null;
+    }
+
+    final DartUrlResolver urlResolver =
+      DartUrlResolver.getInstance(element.getProject(), virtualFile);
+
+    final String dartUrl = urlResolver.getDartUrlForFile(virtualFile);
+    final String prefix = StringUtils.substringBefore(dartUrl, "/");
+    // "dart:html" -> "dart_html"
+    if (prefix.startsWith("dart:")) {
+      return prefix.replaceFirst(":", "_");
+    }
+    // "package:unittest" -> "unittest"
+    if (prefix.startsWith("package:")) {
+      return prefix.substring(8);
+    }
+
+    return null;
   }
 
   @Nls
   private static String constructDocUrl(DartComponent namedComponent, String componentName, @NotNull String libName) {
-    // class:     http://api.dartlang.org/docs/releases/latest/args/ArgParser.html
-    // method:    http://api.dartlang.org/docs/releases/latest/args/ArgParser.html#addCommand
-    // function:  http://api.dartlang.org/docs/releases/latest/observe.html#toObservable
+    // class:     http://api.dartlang.org/docs/releases/latest/dart_core/Object.html
+    // method:    http://api.dartlang.org/docs/releases/latest/dart_core/Object.html#id_toString
+    // property:  http://api.dartlang.org/docs/releases/latest/dart_core/Object.html#id_hashCode
+    // function:  http://api.dartlang.org/docs/releases/latest/dart_math.html#id_cos
 
-    final StringBuilder resultUrl = constructDocUrlPrefix(libName);
-    if (resultUrl == null) return null;
+
+    final StringBuilder resultUrl = new StringBuilder(BASE_DART_DOC_URL).append(libName);
 
     final DartClass dartClass = PsiTreeUtil.getParentOfType(namedComponent, DartClass.class, true);
     final DartComponentType componentType = DartComponentType.typeOf(namedComponent);
 
     if (dartClass != null) {
       // method
-      resultUrl.append('/').append(dartClass.getName()).append(".html").append('#').append(componentName);
+      resultUrl.append('/').append(dartClass.getName()).append(".html#id_").append(componentName);
     }
     else if (componentType == DartComponentType.CLASS) {
       // class
@@ -78,29 +89,10 @@ public class DartDocumentationProvider implements DocumentationProvider {
     }
     else {
       // function
-      resultUrl.append(".html").append('#').append(componentName);
+      resultUrl.append(".html#id_").append(componentName);
     }
 
     return resultUrl.toString();
-  }
-
-  @VisibleForTesting
-  public static StringBuilder constructDocUrlPrefix(String libName) {
-    final StringBuilder resultUrl = new StringBuilder(BASE_DART_DOC_URL);
-    if (libName.startsWith(STD_LIB_PREFIX)) {
-      final String toReplace = libName.startsWith(DOM_LIB_PREFIX) ? DOM_LIB_PREFIX : STD_LIB_PREFIX;
-      resultUrl.append("dart_").append(libName.substring(toReplace.length()));
-    }
-    else {
-      libName = StringUtils.substringBefore(libName, ".");
-      if (APIDOC_HOSTED_PACKAGES.contains(libName)) {
-        resultUrl.append(libName);
-      }
-      else {
-        return null;
-      }
-    }
-    return resultUrl;
   }
 
   @Override
