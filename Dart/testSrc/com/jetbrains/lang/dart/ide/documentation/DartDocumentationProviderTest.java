5b8572e04efa7eb0266305085744f95e32d321f5
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-11-07 17:57:44 +0300

    NPE in DartDocumentationProvider and minor optimization

--- Dart/testSrc/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProviderTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProviderTest.java
@@ -1,27 +1,52 @@
 package com.jetbrains.lang.dart.ide.documentation;
 
-import com.intellij.codeInsight.TargetElementUtilBase;
+import com.intellij.codeInsight.documentation.DocumentationManager;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
 import com.jetbrains.lang.dart.DartCodeInsightFixtureTestCase;
+import com.jetbrains.lang.dart.psi.DartComponentName;
+import com.jetbrains.lang.dart.psi.DartId;
+import com.jetbrains.lang.dart.psi.DartReference;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class DartDocumentationProviderTest extends DartCodeInsightFixtureTestCase {
 
-
-  private static final DartDocumentationProvider myProvider = new DartDocumentationProvider();
-
-  private void doTest(String expectedDoc, String fileContents) throws Exception {
+  private void doTest(@Nullable final String expectedDocUrl, @NotNull final String fileContents) throws Exception {
     final int caretOffset = fileContents.indexOf("<caret>");
     assertTrue(caretOffset != -1);
 
     final String realContents = fileContents.substring(0, caretOffset) + fileContents.substring(caretOffset + "<caret>".length());
     myFixture.configureByText("test.dart", realContents);
 
-    final PsiReference reference = TargetElementUtilBase.findReference(myFixture.getEditor(), caretOffset);
-    assertNotNull("No reference at offset " + caretOffset, reference);
+    final PsiElement elementAtOffset = myFixture.getFile().findElementAt(caretOffset);
+    final PsiElement dartId = elementAtOffset == null ? null : elementAtOffset.getParent();
+    final PsiElement parent = dartId instanceof DartId ? dartId.getParent() : null;
 
-    final PsiElement resolvedElement = reference.resolve();
-    assertEquals(expectedDoc, myProvider.getUrlFor(resolvedElement, resolvedElement).get(0));
+    final PsiElement docTarget;
+    if (parent instanceof DartReference) {
+      docTarget = ((DartReference)parent).resolve();
+      assertNotNull(docTarget);
+    }
+    else if (parent instanceof DartComponentName) {
+      docTarget = parent;
+    }
+    else {
+      fail("Element to look docs for not found");
+      return;
+    }
+
+    final List<String> docUrls = DocumentationManager.getProviderFromElement(docTarget).getUrlFor(docTarget, null);
+
+    if (expectedDocUrl == null) {
+      assertNull(docUrls);
+    }
+    else {
+      assertNotNull(docUrls);
+      assertEquals(StringUtil.join(docUrls, "; "), expectedDocUrl);
+    }
   }
 
   public void testObjectClass() throws Exception {
@@ -39,4 +64,7 @@ public class DartDocumentationProviderTest extends DartCodeInsightFixtureTestCas
            "var s = new Object().<caret>toString();\n");
   }
 
+  public void testNoDocUrl() throws Exception {
+    doTest(null, "class Fo<caret>o {}");
+  }
 }
