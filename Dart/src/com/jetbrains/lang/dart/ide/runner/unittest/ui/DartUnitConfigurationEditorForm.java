e80eaf1c72181433f567d2671e08e25be9b29702
pquitslund <pquitslund@gmail.com>
2014-11-20 10:47:18 -0800

    DartUnit configuration test method/group validation (WEB-1560).

--- Dart/src/com/jetbrains/lang/dart/ide/runner/unittest/ui/DartUnitConfigurationEditorForm.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/unittest/ui/DartUnitConfigurationEditorForm.java
@@ -9,23 +9,34 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.TextFieldWithBrowseButton;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.ui.EnumComboBoxModel;
-import com.intellij.ui.ListCellRendererWrapper;
-import com.intellij.ui.RawCommandLineEditor;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.PsiElementProcessor;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.ui.*;
 import com.intellij.ui.components.JBCheckBox;
 import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.ide.runner.server.ui.DartCommandLineConfigurationEditorForm;
+import com.jetbrains.lang.dart.ide.runner.unittest.DartTestLocationProvider;
 import com.jetbrains.lang.dart.ide.runner.unittest.DartUnitRunConfiguration;
+import com.jetbrains.lang.dart.ide.runner.unittest.DartUnitRunConfigurationProducer;
 import com.jetbrains.lang.dart.ide.runner.unittest.DartUnitRunnerParameters;
+import com.jetbrains.lang.dart.psi.DartCallExpression;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Locale;
 
 import static com.jetbrains.lang.dart.ide.runner.unittest.DartUnitRunnerParameters.Scope;
 
+@SuppressWarnings("Since15")
 public class DartUnitConfigurationEditorForm extends SettingsEditor<DartUnitRunConfiguration> {
   private JPanel myMainPanel;
   private JComboBox myScopeCombo;
@@ -38,6 +49,7 @@ public class DartUnitConfigurationEditorForm extends SettingsEditor<DartUnitRunC
   private RawCommandLineEditor myArguments;
   private TextFieldWithBrowseButton myWorkingDirectory;
   private EnvironmentVariablesComponent myEnvironmentVariables;
+  private final Color myStandardForegroundColor;
 
   public DartUnitConfigurationEditorForm(final Project project) {
     DartCommandLineConfigurationEditorForm.initDartFileTextWithBrowse(project, myFileField);
@@ -57,8 +69,18 @@ public class DartUnitConfigurationEditorForm extends SettingsEditor<DartUnitRunC
       @Override
       public void actionPerformed(ActionEvent e) {
         onScopeChanged();
+        onTestNameChanged(project); // Scope changes can invalidate test label
       }
     });
+
+    myTestNameField.getDocument().addDocumentListener(new DocumentAdapter() {
+      @Override
+      protected void textChanged(final DocumentEvent e) {
+        onTestNameChanged(project);
+      }
+    });
+
+    myStandardForegroundColor = myTestNameField.getForeground();
 
     myVMOptions.setDialogCaption(DartBundle.message("config.vmoptions.caption"));
     myArguments.setDialogCaption(DartBundle.message("config.progargs.caption"));
@@ -108,6 +130,48 @@ public class DartUnitConfigurationEditorForm extends SettingsEditor<DartUnitRunC
     myTestNameLabel.setText(scope == Scope.GROUP
                             ? DartBundle.message("dart.unit.group.name")
                             : DartBundle.message("dart.unit.method.name"));
+  }
+
+  private void onTestNameChanged(final Project project) {
+    final Scope scope = (Scope)myScopeCombo.getSelectedItem();
+    final String testLabel = myTestNameField.getText();
+    final String filePath = myFileField.getText();
+    if (filePath != null) {
+      final VirtualFile file = LocalFileSystem.getInstance().findFileByPath(filePath);
+      if (file != null) {
+        final PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+        if (psiFile != null) {
+
+          PsiElementProcessor.CollectElements<PsiElement> collector = new PsiElementProcessor.CollectElements<PsiElement>() {
+            @Override
+            public boolean execute(@NotNull final PsiElement element) {
+
+              if (element instanceof DartCallExpression) {
+                DartCallExpression expression = (DartCallExpression)element;
+                if (scope == Scope.METHOD && DartUnitRunConfigurationProducer.isTest(expression) ||
+                    DartUnitRunConfigurationProducer.isGroup(expression)) {
+                  if (DartTestLocationProvider.testLabelMatches(expression, testLabel)) {
+                    // Collect the match
+                    super.execute(expression);
+                  }
+                }
+              }
+              return getCollection().isEmpty();
+            }
+          };
+
+          PsiTreeUtil.processElements(psiFile, collector);
+
+          if (collector.getCollection().isEmpty()) {
+            myTestNameField.setForeground(JBColor.RED);
+            myTestNameField.setToolTipText(DartBundle.message("config.test.unfound.label.warning", testLabel));
+          } else {
+            myTestNameField.setForeground(myStandardForegroundColor);
+            myTestNameField.setToolTipText(null);
+          }
+        }
+      }
+    }
   }
 
   @NotNull
