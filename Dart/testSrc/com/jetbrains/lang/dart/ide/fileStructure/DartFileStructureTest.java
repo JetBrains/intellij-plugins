1b8f3cb0d3a40ec94830b05be6a6716ba281c71b
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2014-12-01 19:31:58 +0300

    Cleanup FileStructureTestBase

--- Dart/testSrc/com/jetbrains/lang/dart/ide/fileStructure/DartFileStructureTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/ide/fileStructure/DartFileStructureTest.java
@@ -1,20 +1,15 @@
 package com.jetbrains.lang.dart.ide.fileStructure;
 
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.testFramework.FileStructureTestBase;
 import com.intellij.testFramework.PlatformTestUtil;
-import org.junit.Assert;
-
-import java.io.File;
 
 import static com.jetbrains.lang.dart.util.DartTestUtils.BASE_TEST_DATA_PATH;
 
 public class DartFileStructureTest extends FileStructureTestBase {
 
   @Override
-  protected void checkTree() throws Exception {
-    final String expected = FileUtil.loadFile(new File(BASE_TEST_DATA_PATH + getBasePath() + "/" + getTreeFileName()), true);
-    Assert.assertEquals(expected.trim(), PlatformTestUtil.print(getTree(), true).trim());
+  protected void checkTree() {
+    assertSameLinesWithFile(BASE_TEST_DATA_PATH + getBasePath() + "/" + getTreeFileName(), PlatformTestUtil.print(getTree(), true).trim());
   }
 
   @Override
@@ -28,19 +23,19 @@ public class DartFileStructureTest extends FileStructureTestBase {
     return "/fileStructure/";
   }
 
-  public void testConstructor() throws Exception {
+  public void testConstructor() {
     checkTree();
   }
 
-  public void testEnums() throws Exception {
+  public void testEnums() {
     checkTree();
   }
 
-  public void testInsideClass() throws Exception {
+  public void testInsideClass() {
     checkTree();
   }
 
-  public void testTopLevel() throws Exception {
+  public void testTopLevel() {
     checkTree();
   }
 }
