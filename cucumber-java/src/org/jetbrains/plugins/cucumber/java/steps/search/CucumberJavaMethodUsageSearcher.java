d35647fa1c56d5a4608cae27bee5eb9f16c8d8ef
peter <peter@jetbrains.com>
2014-12-01 19:07:42 +0100

    cucumber method usage search: use effective scope

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/search/CucumberJavaMethodUsageSearcher.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/search/CucumberJavaMethodUsageSearcher.java
@@ -1,13 +1,12 @@
 package org.jetbrains.plugins.cucumber.java.steps.search;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.QueryExecutorBase;
-import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiAnnotation;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.SearchScope;
 import com.intellij.psi.search.searches.MethodReferencesSearch;
 import com.intellij.psi.search.searches.ReferencesSearch;
 import com.intellij.util.Processor;
@@ -21,18 +20,16 @@ import org.jetbrains.plugins.cucumber.psi.GherkinFileType;
  * Date: 7/27/12
  */
 public class CucumberJavaMethodUsageSearcher extends QueryExecutorBase<PsiReference, MethodReferencesSearch.SearchParameters> {
+  public CucumberJavaMethodUsageSearcher() {
+    super(true);
+  }
+
   @Override
   public void processQuery(@NotNull final MethodReferencesSearch.SearchParameters p, @NotNull final Processor<PsiReference> consumer) {
     final PsiMethod method = p.getMethod();
 
-    final String regexp = ApplicationManager.getApplication().runReadAction(new Computable<String>() {
-      @Override
-      public String compute() {
-        final PsiAnnotation stepAnnotation = CucumberJavaUtil.getCucumberStepAnnotation(method);
-        return stepAnnotation != null ? CucumberJavaUtil.getPatternFromStepDefinition(stepAnnotation) : null;
-      }
-    });
-
+    final PsiAnnotation stepAnnotation = CucumberJavaUtil.getCucumberStepAnnotation(method);
+    final String regexp = stepAnnotation != null ? CucumberJavaUtil.getPatternFromStepDefinition(stepAnnotation) : null;
     if (regexp == null) {
       return;
     }
@@ -42,8 +39,9 @@ public class CucumberJavaMethodUsageSearcher extends QueryExecutorBase<PsiRefere
       return;
     }
 
-    if (p.getScope() instanceof GlobalSearchScope) {
-      GlobalSearchScope restrictedScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)p.getScope(), GherkinFileType.INSTANCE);
+    SearchScope scope = p.getEffectiveSearchScope();
+    if (scope instanceof GlobalSearchScope) {
+      GlobalSearchScope restrictedScope = GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)scope, GherkinFileType.INSTANCE);
       ReferencesSearch.search(new ReferencesSearch.SearchParameters(method, restrictedScope, false, p.getOptimizer())).forEach(consumer);
     }
   }
