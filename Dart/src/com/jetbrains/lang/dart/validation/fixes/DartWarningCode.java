5ec9b5da8c892350ca07edaf8f261b95d63d47a5
Jaime Wren <jwren@google.com>
2014-12-01 15:09:09 -0800

    Dart plugin cleanup- removed error code fixes dead code

--- Dart/src/com/jetbrains/lang/dart/validation/fixes/DartWarningCode.java
+++ Dart/src/com/jetbrains/lang/dart/validation/fixes/DartWarningCode.java
@@ -1,7 +1,6 @@
 package com.jetbrains.lang.dart.validation.fixes;
 
 import com.intellij.codeInsight.intention.IntentionAction;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.util.SmartList;
 import com.jetbrains.lang.dart.ide.DartWritingAccessProvider;
@@ -17,14 +16,36 @@ import java.util.List;
 import static com.jetbrains.lang.dart.validation.fixes.DartFixesUtil.findFixesForUnresolved;
 import static com.jetbrains.lang.dart.validation.fixes.DartFixesUtil.isStaticContext;
 
-public enum DartResolverErrorCode {
+public enum DartWarningCode {
+  UNDEFINED_METHOD {
+    @NotNull
+    @Override
+    public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
+      // The method '%s' is not defined for the class '%s'
+      String functionName = DartPresentableUtil.findFirstQuotedWord(message);
+      if (functionName == null) {
+        return Collections.emptyList();
+      }
+      return Arrays.asList(new CreateDartMethodAction(functionName, isStaticContext(file, startOffset)));
+    }
+  },
+  UNDEFINED_FUNCTION {
+    @NotNull
+    @Override
+    public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
+      // The function '%s' is not defined
+      return findFixesForUnresolved(file, startOffset);
+    }
+  },
   UNDEFINED_IDENTIFIER {
     @NotNull
     @Override
     public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
+      // Undefined name '%s'
       return findFixesForUnresolved(file, startOffset);
     }
-  }, UNDEFINED_CLASS {
+  },
+  UNDEFINED_CLASS {
     @NotNull
     @Override
     public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
@@ -42,6 +63,7 @@ public enum DartResolverErrorCode {
     @NotNull
     @Override
     public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
+      // There is no such getter '%s' in '%s'
       String name = DartPresentableUtil.findFirstQuotedWord(message);
       return name == null ?
              Collections.<IntentionAction>emptyList() :
@@ -52,53 +74,20 @@ public enum DartResolverErrorCode {
     @NotNull
     @Override
     public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
+      // There is no such setter '%s' in '%s'
       String name = DartPresentableUtil.findFirstQuotedWord(message);
       return name == null ?
              Collections.<IntentionAction>emptyList() :
              Arrays.asList(new CreateDartGetterSetterAction(name, false, isStaticContext(file, startOffset)));
     }
   },
-  CANNOT_RESOLVE_METHOD {
+  UNDEFINED_OPERATOR {
     @NotNull
     @Override
     public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
-      // cannot resolve method '%s'
-      String functionName = DartPresentableUtil.findLastQuotedWord(message);
-      return functionName == null ? Collections.<IntentionAction>emptyList()
-                                  : Arrays.asList(new CreateGlobalDartFunctionAction(functionName));
-    }
-  }, CANNOT_RESOLVE_METHOD_IN_CLASS {
-    @NotNull
-    @Override
-    public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
-      // cannot resolve method '%s' in class '%s'
-      String functionName = DartPresentableUtil.findFirstQuotedWord(message);
-      return functionName == null ? Collections.<IntentionAction>emptyList()
-                                  : Arrays.asList(new CreateDartMethodAction(functionName, true));
-    }
-  }, FIELD_DOES_NOT_HAVE_A_GETTER {
-    @NotNull
-    @Override
-    public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
-      // Field does not have a getter
-      PsiElement elementAt = file.findElementAt(startOffset);
-      return elementAt == null ? Collections.<IntentionAction>emptyList() : Arrays.asList(new CreateDartGetterSetterAction(
-        elementAt.getText(),
-        true,
-        true
-      ));
-    }
-  }, FIELD_DOES_NOT_HAVE_A_SETTER {
-    @NotNull
-    @Override
-    public List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message) {
-      // Field does not have a setter
-      PsiElement elementAt = file.findElementAt(startOffset);
-      return elementAt == null ? Collections.<IntentionAction>emptyList() : Arrays.asList(new CreateDartGetterSetterAction(
-        elementAt.getText(),
-        false,
-        true
-      ));
+      // There is no such operator '%s' in '%s'
+      String operator = DartPresentableUtil.findFirstQuotedWord(message);
+      return operator == null ? Collections.<IntentionAction>emptyList() : Arrays.asList(new CreateDartOperatorAction(operator));
     }
   },
   STATIC_ACCESS_TO_INSTANCE_MEMBER {
@@ -170,7 +159,7 @@ public enum DartResolverErrorCode {
   @NotNull
   public abstract List<? extends IntentionAction> getFixes(@NotNull PsiFile file, int startOffset, @NotNull String message);
 
-  public static DartResolverErrorCode findError(String code) {
+  public static DartWarningCode findError(String code) {
     try {
       return valueOf(code);
     }
