d46215b3c106d80edfc40ddefca7b650b868c788
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2014-12-15 20:42:44 +0300

    More testing for Dart line doc comments splitting

--- Dart/testSrc/com/jetbrains/lang/dart/editor/DartCommentLineBreakingTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/editor/DartCommentLineBreakingTest.java
@@ -6,10 +6,14 @@ import com.jetbrains.lang.dart.DartCodeInsightFixtureTestCase;
 public class DartCommentLineBreakingTest extends DartCodeInsightFixtureTestCase {
 
   public void testCommentLineBreak1() {
-    myFixture.configureByText("test.dart", "/// Hello Dart");
-    myFixture.getEditor().getCaretModel().moveToOffset("/// Hello".length());
+    myFixture.configureByText("test.dart", "  /// HelloDart \n//");
+    myFixture.getEditor().getCaretModel().moveToOffset("  /// Hello".length());
     myFixture.type("\n");
-    assertEquals("/// Hello\n/// Dart", myFixture.getEditor().getDocument().getCharsSequence().toString());
+    final String text = myFixture.getEditor().getDocument().getText();
+    assertEquals("  /// Hello\n  /// Dart \n//", text);
+    myFixture.getEditor().getCaretModel().moveToOffset("  /// Hello\n  /// Dart".length());
+    myFixture.type("\n");
+    assertEquals("  /// Hello\n  /// Dart\n  \n//", myFixture.getEditor().getDocument().getText());
   }
 
 }
