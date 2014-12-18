7b799b9c81cf4b8d1e4940b8a52414b6577fa4da
pquitslund <pquitslund@gmail.com>
2014-12-18 10:05:27 -0800

    Fix for Dart normal parameter name finding.

--- Dart/src/com/jetbrains/lang/dart/ide/template/macro/DartMethodParametersMacro.java
+++ Dart/src/com/jetbrains/lang/dart/ide/template/macro/DartMethodParametersMacro.java
@@ -2,27 +2,13 @@ package com.jetbrains.lang.dart.ide.template.macro;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.Lists;
-import com.intellij.codeInsight.template.Expression;
-import com.intellij.codeInsight.template.ExpressionContext;
-import com.intellij.codeInsight.template.ListResult;
-import com.intellij.codeInsight.template.Result;
-import com.intellij.codeInsight.template.TemplateContextType;
-import com.intellij.codeInsight.template.TextResult;
+import com.intellij.codeInsight.template.*;
 import com.intellij.codeInsight.template.macro.MethodParametersMacro;
 import com.intellij.openapi.util.Condition;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.jetbrains.lang.dart.ide.template.DartTemplateContextType;
-import com.jetbrains.lang.dart.psi.DartComponent;
-import com.jetbrains.lang.dart.psi.DartComponentName;
-import com.jetbrains.lang.dart.psi.DartDefaultFormalNamedParameter;
-import com.jetbrains.lang.dart.psi.DartFormalParameterList;
-import com.jetbrains.lang.dart.psi.DartFunctionDeclarationWithBody;
-import com.jetbrains.lang.dart.psi.DartFunctionDeclarationWithBodyOrNative;
-import com.jetbrains.lang.dart.psi.DartFunctionSignature;
-import com.jetbrains.lang.dart.psi.DartMethodDeclaration;
-import com.jetbrains.lang.dart.psi.DartNamedFormalParameters;
-import com.jetbrains.lang.dart.psi.DartNormalFormalParameter;
+import com.jetbrains.lang.dart.psi.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -38,12 +24,10 @@ public class DartMethodParametersMacro extends MethodParametersMacro {
       return null;
     }
 
-
     List<Result> result = Lists.newArrayList();
     for (String name : parameterNames) {
       result.add(new TextResult(name));
     }
-
     return new ListResult(result);
   }
 
@@ -71,32 +55,28 @@ public class DartMethodParametersMacro extends MethodParametersMacro {
 
     List<String> results = Lists.newArrayList();
 
-    final List<DartNormalFormalParameter> normalFormalParameters = parameterList.getNormalFormalParameterList();
-    for (DartNormalFormalParameter parameter : normalFormalParameters) {
-      final DartFunctionSignature signature = parameter.getFunctionSignature();
-      if (signature != null) {
-        final String name = signature.getName();
-        if (name != null) {
-          results.add(name);
-        }
-      }
+    for (DartNormalFormalParameter parameter : parameterList.getNormalFormalParameterList()) {
+      findAndAddName(results, parameter);
     }
 
     final DartNamedFormalParameters namedFormalParameters = parameterList.getNamedFormalParameters();
     if (namedFormalParameters != null) {
       for (DartDefaultFormalNamedParameter parameter : namedFormalParameters.getDefaultFormalNamedParameterList()) {
-        final DartComponentName componentName = parameter.getNormalFormalParameter().findComponentName();
-        if (componentName != null) {
-          final String name = componentName.getName();
-          if (name != null) {
-            results.add(name);
-          }
-        }
+        findAndAddName(results, parameter.getNormalFormalParameter());
       }
-      
     }
 
     return results;
+  }
+
+  private static void findAndAddName(@NotNull final List<String> results, @NotNull final DartNormalFormalParameter parameter) {
+    final DartComponentName componentName = parameter.findComponentName();
+    if (componentName != null) {
+      final String name = componentName.getName();
+      if (name != null) {
+        results.add(name);
+      }
+    }
   }
 
 }
