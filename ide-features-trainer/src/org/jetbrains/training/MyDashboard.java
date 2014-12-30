3be3bd91a9b2c354fd749bfbfe2b90a8a41526f5
Sergey Karashevich <karashevich@gmail.com>
2014-12-30 20:48:55 +0300

    Dashboard added
    GitOrigin-RevId: d5d9a05d501f4caabbc7f737cc18ed744a8602d8

---
+++ ide-features-trainer/src/org/jetbrains/training/MyDashboard.java
@@ -0,0 +1,38 @@
+package org.jetbrains.training;
+
+import apple.awt.CColor;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by karashevich on 30/12/14.
+ */
+public class MyDashboard extends JFrame{
+    private JTextField myTextField;
+    private JButton okButton;
+    private JPanel myPanel;
+
+    public MyDashboard() {
+        super("MyDashboard");
+
+        setContentPane(myPanel);
+
+        pack();
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        okButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String s = myTextField.getText();
+                myTextField.setText(s + " Button pushed!");
+
+            }
+        });
+
+         setVisible(true);
+    }
+
+}
