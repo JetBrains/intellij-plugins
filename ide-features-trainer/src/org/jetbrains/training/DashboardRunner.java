a07c5cefa3545aa6cd347c21c991ccd7d9ef9a17
Sergey Karashevich <karashevich@gmail.com>
2014-12-30 20:48:55 +0300

    Dashboard added
    GitOrigin-RevId: 9ef711f481656124c726e88f3c7af74c95ce6493

---
+++ ide-features-trainer/src/org/jetbrains/training/DashboardRunner.java
@@ -0,0 +1,12 @@
+package org.jetbrains.training;
+
+/**
+ * Created by karashevich on 30/12/14.
+ */
+public class DashboardRunner {
+
+    public static void main(String[] args) {
+        MyDashboard md = new MyDashboard();
+
+    }
+}
