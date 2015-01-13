06bb90cd2f7e3dcdf38b90373016b0616fcb6de1
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-01-13 11:49:28 +0300

    Handlebars: cleanup moveUp test

--- handlebars/test/src/com/dmarcotte/handlebars/editor/actions/HbMoverTest.java
+++ handlebars/test/src/com/dmarcotte/handlebars/editor/actions/HbMoverTest.java
@@ -1,16 +1,10 @@
 package com.dmarcotte.handlebars.editor.actions;
 
 import com.dmarcotte.handlebars.util.HbTestUtils;
-import com.intellij.codeInsight.editorActions.moveUpDown.MoveStatementUpAction;
-import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.actionSystem.EditorActionHandler;
+import com.intellij.openapi.actionSystem.IdeActions;
 import com.intellij.testFramework.PlatformTestCase;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
-import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.File;
-
 
 public class HbMoverTest extends LightPlatformCodeInsightFixtureTestCase {
 
@@ -19,47 +13,24 @@ public class HbMoverTest extends LightPlatformCodeInsightFixtureTestCase {
     PlatformTestCase.initPlatformLangPrefix();
   }
 
-  public void testMoveXmlWhenOpenHtmlAsHandlebars() throws Exception {
+  public void testMoveXmlWhenOpenHtmlAsHandlebars() {
     HbTestUtils.setOpenHtmlAsHandlebars(true, getProject(), getTestRootDisposable());
     doTest("hbs");
   }
 
-  public void testMoveHbsTag() throws Exception {
+  public void testMoveHbsTag() {
     doTest("hbs");
   }
 
-  private void doTest(String ext) throws Exception {
-    final String baseName = getBasePath() + '/' + getTestName(true);
-    final String fileName = baseName + "." + ext;
-
-    @NonNls String afterFileName = baseName + "_after." + ext;
-    EditorActionHandler handler = new MoveStatementUpAction().getHandler();
-    performAction(fileName, handler, afterFileName);
+  private void doTest(@NotNull String ext) {
+    myFixture.configureByFile(getTestName(true) + "." + ext);
+    myFixture.performEditorAction(IdeActions.ACTION_MOVE_STATEMENT_UP_ACTION);
+    myFixture.checkResultByFile(getTestName(true) + "_after." + ext);
   }
-
-  private void performAction(final String fileName, final EditorActionHandler handler, final String afterFileName) throws Exception {
-    myFixture.configureByFile(fileName);
-    final boolean enabled = handler.isEnabled(myFixture.getEditor(), null);
-    assertEquals("not enabled for " + afterFileName, new File(getTestDataPath(), afterFileName).exists(), enabled);
-    if (enabled) {
-      WriteCommandAction.runWriteCommandAction(null, new Runnable() {
-        @Override
-        public void run() {
-          handler.execute(myFixture.getEditor(), null);
-        }
-      });
-      myFixture.checkResultByFile(afterFileName);
-    }
-  }
-
-  @Override
-  protected String getBasePath() {
-    return "mover";
-  }
-
+  
   @NotNull
   @Override
   protected String getTestDataPath() {
-    return HbTestUtils.BASE_TEST_DATA_PATH;
+    return HbTestUtils.BASE_TEST_DATA_PATH + "/mover";
   }
 }
