d9949f2e460601edc3433cd0d3aaff94abfc37e2
Alexander Zolotov <Alexander.Zolotov@jetbrains.com>
2015-01-13 14:07:16 +0300

    Emmet: use language instead of file type while creating file in order to avoid language substitutors

---
+++ handlebars/test/src/com/dmarcotte/handlebars/editor/templates/HbsEmmetTest.java
@@ -0,0 +1,33 @@
+package com.dmarcotte.handlebars.editor.templates;
+
+import com.dmarcotte.handlebars.file.HbFileType;
+import com.dmarcotte.handlebars.psi.HbPsiFile;
+import com.dmarcotte.handlebars.util.HbTestUtils;
+import com.intellij.codeInsight.template.TemplateManager;
+import com.intellij.codeInsight.template.impl.TemplateSettings;
+import com.intellij.psi.PsiFile;
+import com.intellij.testFramework.PlatformTestCase;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+
+public class HbsEmmetTest extends LightPlatformCodeInsightFixtureTestCase {
+
+  @Override
+  protected void setUp() throws Exception {
+    PlatformTestCase.initPlatformLangPrefix();
+    super.setUp();
+  }
+
+  public void testSimpleTags() {
+    myFixture.configureByText(HbFileType.INSTANCE, "div>span<caret>");
+    TemplateManager.getInstance(getProject()).startTemplate(myFixture.getEditor(), TemplateSettings.TAB_CHAR);
+    myFixture.checkResult("<div><span></span></div>");
+  }
+
+  public void testSimpleTagsWithHtmlSubstitutor() {
+    HbTestUtils.setOpenHtmlAsHandlebars(true, getProject(), getTestRootDisposable());
+    final PsiFile file = myFixture.configureByText("test.html", "div>span<caret>");
+    assertInstanceOf(file, HbPsiFile.class);
+    TemplateManager.getInstance(getProject()).startTemplate(myFixture.getEditor(), TemplateSettings.TAB_CHAR);
+    myFixture.checkResult("<div><span></span></div>");
+  }
+}
