5efc45b20739f7c5a02afd9ea5de9557fc96e379
Yann Cébron <yann.cebron@jetbrains.com>
2015-02-24 11:16:34 +0100

    OGNL: re-gen all due to header copyright year change

--- struts2/ognl/gen/com/intellij/lang/ognl/psi/OgnlFqnTypeExpression.java
+++ struts2/ognl/gen/com/intellij/lang/ognl/psi/OgnlFqnTypeExpression.java
@@ -17,6 +17,10 @@
 // Generated from ognl.bnf, do not modify
 package com.intellij.lang.ognl.psi;
 
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElement;
+
 public interface OgnlFqnTypeExpression extends OgnlExpression {
 
 }
