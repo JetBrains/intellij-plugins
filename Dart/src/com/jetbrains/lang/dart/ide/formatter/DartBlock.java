6a38996073c1e5adcb9f6f6d12959e04afc5fb35
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-03-06 23:00:46 +0300

    WEB-15292 [Dart] Switch syntax is not automatically correctly indented

--- Dart/src/com/jetbrains/lang/dart/ide/formatter/DartBlock.java
+++ Dart/src/com/jetbrains/lang/dart/ide/formatter/DartBlock.java
@@ -3,23 +3,31 @@ package com.jetbrains.lang.dart.ide.formatter;
 import com.intellij.formatting.*;
 import com.intellij.formatting.templateLanguages.BlockWithParent;
 import com.intellij.lang.ASTNode;
-import com.intellij.psi.PsiWhiteSpace;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
 import com.intellij.psi.formatter.FormatterUtil;
 import com.intellij.psi.formatter.common.AbstractBlock;
 import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
+import com.jetbrains.lang.dart.DartFileType;
 import com.jetbrains.lang.dart.DartLanguage;
-import com.jetbrains.lang.dart.DartTokenTypes;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author: Fedor.Korotkov
- */
+import static com.jetbrains.lang.dart.DartTokenTypes.*;
+
 public class DartBlock extends AbstractBlock implements BlockWithParent {
+
+  private static final TokenSet STATEMENTS_WITH_OPTIONAL_BRACES = TokenSet.create(
+    IF_STATEMENT, WHILE_STATEMENT, FOR_STATEMENT
+  );
+
+  private static final TokenSet LAST_TOKENS_IN_SWITCH_CASE = TokenSet.create(
+    BREAK_STATEMENT, CONTINUE_STATEMENT, RETURN_STATEMENT, THROW_STATEMENT
+  );
+
   private final DartIndentProcessor myIndentProcessor;
   private final DartSpacingProcessor mySpacingProcessor;
   private final DartWrappingProcessor myWrappingProcessor;
@@ -27,7 +35,6 @@ public class DartBlock extends AbstractBlock implements BlockWithParent {
   private final CodeStyleSettings mySettings;
   private Wrap myChildWrap = null;
   private final Indent myIndent;
-  private boolean myChildrenBuilt = false;
   private BlockWithParent myParent;
 
   protected DartBlock(ASTNode node,
@@ -55,7 +62,6 @@ public class DartBlock extends AbstractBlock implements BlockWithParent {
 
   @Override
   protected List<Block> buildChildren() {
-    myChildrenBuilt = true;
     if (isLeaf()) {
       return EMPTY;
     }
@@ -73,7 +79,7 @@ public class DartBlock extends AbstractBlock implements BlockWithParent {
     final IElementType childType = child.getElementType();
     final Wrap wrap = myWrappingProcessor.createChildWrap(child, Wrap.createWrap(WrapType.NONE, false), myChildWrap);
 
-    if (childType == DartTokenTypes.ASSIGNMENT_OPERATOR) {
+    if (childType == ASSIGNMENT_OPERATOR) {
       myChildWrap = wrap;
     }
     return wrap;
@@ -81,7 +87,7 @@ public class DartBlock extends AbstractBlock implements BlockWithParent {
 
   @Nullable
   protected Alignment createChildAlignment(ASTNode child) {
-    if (child.getElementType() != DartTokenTypes.LPAREN && child.getElementType() != DartTokenTypes.BLOCK) {
+    if (child.getElementType() != LPAREN && child.getElementType() != BLOCK) {
       return myAlignmentProcessor.createChildAlignment();
     }
     return null;
@@ -90,41 +96,58 @@ public class DartBlock extends AbstractBlock implements BlockWithParent {
   @NotNull
   @Override
   public ChildAttributes getChildAttributes(final int newIndex) {
-    int index = newIndex;
-    ASTBlock prev = null;
-    do {
-      if (index == 0) {
-        break;
-      }
-      prev = (ASTBlock)getSubBlocks().get(index - 1);
-      index--;
-    }
-    while (prev.getNode().getElementType() == DartTokenTypes.SEMICOLON || prev.getNode() instanceof PsiWhiteSpace);
-
     final IElementType elementType = myNode.getElementType();
-    final IElementType prevType = prev == null ? null : prev.getNode().getElementType();
-    if (prevType == DartTokenTypes.LBRACE) {
+    final DartBlock previousBlock = newIndex == 0 ? null : (DartBlock)getSubBlocks().get(newIndex - 1);
+    final IElementType previousType = previousBlock == null ? null : previousBlock.getNode().getElementType();
+
+    if (previousType == LBRACE) {
       return new ChildAttributes(Indent.getNormalIndent(), null);
     }
-    if (isEndsWithRPAREN(elementType, prevType)) {
+
+    if (previousType == RPAREN && STATEMENTS_WITH_OPTIONAL_BRACES.contains(elementType)) {
       return new ChildAttributes(Indent.getNormalIndent(), null);
     }
-    if (index == 0) {
+
+    if (previousType == COLON && (elementType == SWITCH_CASE || elementType == DEFAULT_CASE)) {
+      return new ChildAttributes(Indent.getNormalIndent(), null);
+    }
+
+    if (previousType == SWITCH_CASE || previousType == DEFAULT_CASE) {
+      if (previousBlock != null) {
+        final List<Block> subBlocks = previousBlock.getSubBlocks();
+        if (!subBlocks.isEmpty()) {
+          final DartBlock lastChildInPrevBlock = (DartBlock)subBlocks.get(subBlocks.size() - 1);
+          final List<Block> subSubBlocks = lastChildInPrevBlock.getSubBlocks();
+          if (!subSubBlocks.isEmpty()) {
+            final DartBlock lastChildInLastChildInPrevBlock = (DartBlock)subSubBlocks.get(subSubBlocks.size() - 1);
+            final IElementType typeOfLastChildInPrevBlock = lastChildInLastChildInPrevBlock.getNode().getElementType();
+
+            if (LAST_TOKENS_IN_SWITCH_CASE.contains(typeOfLastChildInPrevBlock) ||
+                typeOfLastChildInPrevBlock == SEMICOLON && lastButOneIsThrowStatement(subSubBlocks)) {
+              return new ChildAttributes(Indent.getNormalIndent(), null);  // e.g. Enter after BREAK_STATEMENT
+            }
+          }
+        }
+      }
+
+      final int indentSize = mySettings.getIndentSize(DartFileType.INSTANCE) * 2;
+      return new ChildAttributes(Indent.getIndent(Indent.Type.SPACES, indentSize, false, false), null);
+    }
+
+    if (previousBlock == null) {
       return new ChildAttributes(Indent.getNoneIndent(), null);
     }
-    return new ChildAttributes(prev.getIndent(), prev.getAlignment());
+
+    return new ChildAttributes(previousBlock.getIndent(), previousBlock.getAlignment());
+  }
+
+  private static boolean lastButOneIsThrowStatement(@NotNull final List<Block> blocks) {
+    return blocks.size() > 1 && ((DartBlock)blocks.get(blocks.size() - 2)).getNode().getElementType() == THROW_STATEMENT;
   }
 
   @Override
   public boolean isLeaf() {
     return false;
-  }
-
-  private static boolean isEndsWithRPAREN(IElementType elementType, IElementType prevType) {
-    return prevType == DartTokenTypes.RPAREN &&
-           (elementType == DartTokenTypes.IF_STATEMENT ||
-            elementType == DartTokenTypes.FOR_STATEMENT ||
-            elementType == DartTokenTypes.WHILE_STATEMENT);
   }
 
   @Override
