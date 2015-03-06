94aa660be31cc8ec5b74b4b8069cf5d02363ce9c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-03-06 14:26:53 +0300

    WEB-10325 dart debug: support "quick evaluate expression"

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/frame/DartDebuggerEvaluator.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/frame/DartDebuggerEvaluator.java
@@ -1,18 +1,26 @@
 package com.jetbrains.lang.dart.ide.runner.server.frame;
 
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.xdebugger.XSourcePosition;
+import com.intellij.xdebugger.evaluation.ExpressionInfo;
 import com.intellij.xdebugger.evaluation.XDebuggerEvaluator;
 import com.jetbrains.lang.dart.ide.runner.server.DartCommandLineDebugProcess;
 import com.jetbrains.lang.dart.ide.runner.server.google.VmCallFrame;
 import com.jetbrains.lang.dart.ide.runner.server.google.VmCallback;
 import com.jetbrains.lang.dart.ide.runner.server.google.VmResult;
 import com.jetbrains.lang.dart.ide.runner.server.google.VmValue;
+import com.jetbrains.lang.dart.psi.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
 
-class DartDebuggerEvaluator extends XDebuggerEvaluator {
+public class DartDebuggerEvaluator extends XDebuggerEvaluator {
   @NotNull private final DartCommandLineDebugProcess myDebugProcess;
   @NotNull private final VmCallFrame myVmCallFrame;
 
@@ -47,5 +55,61 @@ class DartDebuggerEvaluator extends XDebuggerEvaluator {
     catch (IOException e) {
       callback.errorOccurred(e.toString());
     }
+  }
+
+  @Nullable
+  @Override
+  public ExpressionInfo getExpressionInfoAtOffset(@NotNull final Project project,
+                                                  @NotNull final Document document,
+                                                  final int offset,
+                                                  final boolean sideEffectsAllowed) {
+    final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+    final PsiElement contextElement = psiFile == null ? null : psiFile.findElementAt(offset);
+    return contextElement == null ? null : getExpressionInfo(contextElement);
+  }
+
+  @Nullable
+  public static ExpressionInfo getExpressionInfo(@NotNull final PsiElement contextElement) {
+    // todo if sideEffectsAllowed return method call like "foo()", not only "foo"
+    /** WEB-11715
+     dart psi: notes.text
+
+     REFERENCE_EXPRESSION
+     REFERENCE_EXPRESSION “notes”
+     PsiElement(.) “.”
+     REFERENCE_EXPRESSION “text”
+     */
+    // find topmost reference, but stop if argument list found
+    DartReference reference = null;
+    PsiElement element = contextElement;
+    while (true) {
+      if (element instanceof DartReference) {
+        reference = (DartReference)element;
+      }
+
+      element = element.getParent();
+      if (element == null ||
+          // int.parse(slider.value) - we must return reference expression "slider.value", but not the whole expression
+          element instanceof DartArgumentList ||
+          // "${seeds} seeds" - we must return only "seeds"
+          element instanceof DartLongTemplateEntry ||
+          element instanceof DartCallExpression ||
+          element instanceof DartFunctionBody || element instanceof DartBlock) {
+        break;
+      }
+    }
+
+    if (reference != null) {
+      TextRange textRange = reference.getTextRange();
+      // note<CURSOR>s.text - the whole reference expression is notes.txt, but we must return only notes
+      int endOffset = contextElement.getTextOffset() + contextElement.getTextLength();
+      if (textRange.getEndOffset() != endOffset) {
+        textRange = new TextRange(textRange.getStartOffset(), endOffset);
+      }
+      return new ExpressionInfo(textRange);
+    }
+
+    PsiElement parent = contextElement.getParent();
+    return parent instanceof DartId ? new ExpressionInfo(parent.getTextRange()) : null;
   }
 }
