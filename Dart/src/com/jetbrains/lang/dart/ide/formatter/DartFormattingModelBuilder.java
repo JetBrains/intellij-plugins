a20ff8c5d2c3dd621cce7d976683b87789cbf33c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-03-13 17:20:54 +0300

    use DocumentBasedFormattingModel in Dart

--- Dart/src/com/jetbrains/lang/dart/ide/formatter/DartFormattingModelBuilder.java
+++ Dart/src/com/jetbrains/lang/dart/ide/formatter/DartFormattingModelBuilder.java
@@ -2,29 +2,25 @@ package com.jetbrains.lang.dart.ide.formatter;
 
 import com.intellij.formatting.FormattingModel;
 import com.intellij.formatting.FormattingModelBuilder;
-import com.intellij.formatting.FormattingModelProvider;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.formatter.DocumentBasedFormattingModel;
 import com.jetbrains.lang.dart.psi.DartFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-/**
- * @author fedor.korotkov
- */
 public class DartFormattingModelBuilder implements FormattingModelBuilder {
   @NotNull
   @Override
-  public FormattingModel createModel(PsiElement element, CodeStyleSettings settings) {
+  public FormattingModel createModel(@NotNull final PsiElement element, @NotNull final CodeStyleSettings settings) {
+    // element can be DartFile, DartEmbeddedContent, DartExpressionCodeFragment
     final PsiFile psiFile = element.getContainingFile();
-    return FormattingModelProvider.createFormattingModelForPsiFile(
-      psiFile,
-      new DartBlock(psiFile instanceof DartFile ? psiFile.getNode() : element.getNode(), null, null, settings),
-      settings
-    );
+    final ASTNode rootNode = psiFile instanceof DartFile ? psiFile.getNode() : element.getNode();
+    final DartBlock rootBlock = new DartBlock(rootNode, null, null, settings);
+    return new DocumentBasedFormattingModel(rootBlock, element.getProject(), settings, psiFile.getFileType(), psiFile);
   }
 
   @Nullable
