187010c6b981fb21925f663d755fd920389880ff
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-03-18 13:19:22 +0100

    osgi: tests fixed

--- osmorc/src_test/org/osmorc/LightOsgiFixtureTestCase.java
+++ osmorc/src_test/org/osmorc/LightOsgiFixtureTestCase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.osmorc;
 
 import com.intellij.facet.FacetManager;
@@ -31,10 +46,7 @@ public abstract class LightOsgiFixtureTestCase extends LightCodeInsightFixtureTe
       File annotations = new File(annotationsPath);
       PsiTestUtil.addLibrary(module, model, "annotations", annotations.getParent(), annotations.getName());
 
-      OsmorcFacet facet = FacetManager.getInstance(module).addFacet(OsmorcFacetType.getInstance(), "OSGi", null);
-      facet.getConfiguration().setUseProjectDefaultManifestFileLocation(false);
-      facet.getConfiguration().setManifestLocation("META-INF/MANIFEST.MF");
-      facet.getConfiguration().setManifestGenerationMode(ManifestGenerationMode.Manually);
+      FacetManager.getInstance(module).addFacet(OsmorcFacetType.getInstance(), "OSGi", null);
     }
   };
 
@@ -49,7 +61,12 @@ public abstract class LightOsgiFixtureTestCase extends LightCodeInsightFixtureTe
   @Override
   protected void setUp() throws Exception {
     super.setUp();
+
     myFacet = OsmorcFacet.getInstance(myModule);
     assertNotNull(myFacet);
+
+    myFacet.getConfiguration().setUseProjectDefaultManifestFileLocation(false);
+    myFacet.getConfiguration().setManifestLocation("META-INF/MANIFEST.MF");
+    myFacet.getConfiguration().setManifestGenerationMode(ManifestGenerationMode.Manually);
   }
 }
