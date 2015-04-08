80e9ee1a5bce5ad21f4f25289169a1142e454b4f
Sergey Karashevich <karashevich@gmail.com>
2015-04-08 15:13:36 +0300

    HintPanel with GridLayout
    GitOrigin-RevId: 5966f662b331b89e05de5a81c74ab74d3ea452ae

--- ide-features-trainer/src/org/jetbrains/training/graphics/HintPanel.java
+++ ide-features-trainer/src/org/jetbrains/training/graphics/HintPanel.java
@@ -19,6 +19,7 @@ public class HintPanel extends JPanel implements Disposable {
 
     private Color backGroundColor = new Color(0, 0 ,0, 190);
     private Color textColor = new Color(150, 150, 150, 190);
+    private int fontSize = 14;
     private JLabel myLabel;
 
     @Nullable
@@ -29,14 +30,38 @@ public class HintPanel extends JPanel implements Disposable {
 
         balloonShown = false;
 
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+
         setOpaque(false);
         setPreferredSize(dimension);
         setSize(dimension);
         myLabel = new JLabel("");
+//        myLabel.setHorizontalTextPosition(JLabel.LEFT);
+//        myLabel.setVerticalAlignment(JLabel.NORTH);
+        myLabel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+//        myLabel.setMaximumSize(new Dimension((int) (dimension.getWidth() - 28), 20));
+//        myLabel.setMinimumSize(new Dimension((int) (dimension.getWidth() - 28), 20));
+//        myLabel.setPreferredSize(new Dimension((int) (dimension.getWidth() - 28), 20));
+
+
+        JLabel checkLabel = new JLabel("✓");
+
+        JLabel myLabel2;
+        myLabel2 = new JLabel("Custom text here");
+        myLabel2.setHorizontalTextPosition(JLabel.LEFT);
+        myLabel2.setVerticalAlignment(JLabel.NORTH);
+        myLabel2.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
 //        custom settings
 //        myLabel.setForeground(textColor);
 
+        int heightGap = 14;
+
+        this.setBorder(BorderFactory.createEmptyBorder(12, 12, 0, 12));
         this.add(myLabel);
+        this.add(Box.createRigidArea(new Dimension(0, 10)));
+        this.add(checkLabel);
+        this.add(myLabel2);
 
         setFocusable(false);
         setVisible(true);
@@ -59,7 +84,8 @@ public class HintPanel extends JPanel implements Disposable {
                     .setDisposable(this)
                     .setCloseButtonEnabled(true)
                     .setFillColor(new Color(230, 227, 193, 190))
-                    .setBorderColor(new Color(160, 160, 160, 255))
+//                    .setBorderColor(new Color(160, 160, 160, 255))
+                    .setBorderColor(new Color(0, 0, 0, 0))
                     .setDialogMode(false)
                     .setAnimationCycle(0);
 
