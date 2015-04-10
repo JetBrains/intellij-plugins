9e518a9f2c1593bc730b21add743e41a0bcb9fc5
Sergey Karashevich <karashevich@gmail.com>
2015-04-10 12:21:18 +0300

    WinCommand has been implemented
    GitOrigin-RevId: 1b2ac1701868ec9184d74bb4e09b511269d91d69

---
+++ ide-features-trainer/src/org/jetbrains/training/commands/WinCommand.java
@@ -0,0 +1,77 @@
+package org.jetbrains.training.commands;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.util.Alarm;
+import org.jdom.Element;
+import org.jetbrains.training.BadCourseException;
+import org.jetbrains.training.BadLessonException;
+import org.jetbrains.training.Command;
+import org.jetbrains.training.LessonIsOpenedException;
+import org.jetbrains.training.editor.MouseListenerHolder;
+import org.jetbrains.training.graphics.DetailPanel;
+import org.jetbrains.training.lesson.Lesson;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Created by karashevich on 30/01/15.
+ */
+public class WinCommand extends Command {
+
+    public WinCommand(){
+        super(CommandType.WIN);
+    }
+
+    @Override
+    public void execute(final Queue<Element> elements, final Lesson lesson, final Editor editor, final AnActionEvent e, final Document document, final String target, final DetailPanel infoPanel, final MouseListenerHolder mouseListenerHolder) throws InterruptedException {
+
+        Element element = elements.poll();
+
+        String description = "";
+
+        if (element.getAttribute("description") != null) description = element.getAttribute("description").getValue();
+        infoPanel.setText(description);
+        infoPanel.greenalize();
+        lesson.setPassed(true);
+
+        if (lesson.getParentCourse().hasNotPassedLesson()) {
+            infoPanel.setButtonText("Next lesson");
+            try {
+                infoPanel.addButtonAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            infoPanel.hideButton();
+                            infoPanel.dispose();
+                            lesson.hintPanel.dispose();
+                            lesson.onNextLesson();
+                        } catch (BadLessonException e1) {
+                            e1.printStackTrace();
+                        } catch (ExecutionException e1) {
+                            e1.printStackTrace();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        } catch (FontFormatException e1) {
+                            e1.printStackTrace();
+                        } catch (InterruptedException e1) {
+                            e1.printStackTrace();
+                        } catch (BadCourseException e1) {
+                            e1.printStackTrace();
+                        } catch (LessonIsOpenedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
+            infoPanel.showButton();
+        }
+
+    }
+}
