4d0e2cddc47e65c9f9b5efe8a46c29083457201c
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-04-13 12:59:26 +0200

    IDEA-139006 (parent workspace check added)

--- osmorc/src/org/jetbrains/osgi/bnd/imp/BndModuleImportProvider.java
+++ osmorc/src/org/jetbrains/osgi/bnd/imp/BndModuleImportProvider.java
@@ -15,12 +15,16 @@
  */
 package org.jetbrains.osgi.bnd.imp;
 
+import aQute.bnd.build.Workspace;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.projectImport.ProjectImportProvider;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.osmorc.i18n.OsmorcBundle;
+
+import java.io.File;
 
 public class BndModuleImportProvider extends ProjectImportProvider {
   public BndModuleImportProvider() {
@@ -29,9 +33,13 @@ public class BndModuleImportProvider extends ProjectImportProvider {
 
   @Override
   public boolean canImport(@NotNull VirtualFile fileOrDir, @Nullable Project project) {
-    return BndProjectImporter.getWorkspace(project) != null &&
-           (!fileOrDir.isDirectory() && BndProjectImporter.BND_FILE.equals(fileOrDir.getName()) ||
-            fileOrDir.isDirectory() && fileOrDir.findChild(BndProjectImporter.BND_FILE) != null);
+    Workspace ws = BndProjectImporter.getWorkspace(project);
+    if (ws == null) return false;
+
+    File projectDir = fileOrDir.isDirectory() ? new File(fileOrDir.getPath()) : new File(fileOrDir.getPath()).getParentFile();
+    return FileUtil.filesEqual(ws.getBase(), projectDir.getParentFile()) &&
+           projectDir.isDirectory() &&
+           new File(projectDir, BndProjectImporter.BND_FILE).isFile();
   }
 
   @Override
