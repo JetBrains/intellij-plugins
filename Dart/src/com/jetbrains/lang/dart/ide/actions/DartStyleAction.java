782fcad16408185b132335502bad974a94e320a2
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-04-16 16:31:19 +0300

    Dart Style action: show hint with result

--- Dart/src/com/jetbrains/lang/dart/ide/actions/DartStyleAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/DartStyleAction.java
@@ -1,40 +1,41 @@
 package com.jetbrains.lang.dart.ide.actions;
 
 import com.google.dart.server.generated.types.SourceEdit;
+import com.intellij.codeInsight.hint.HintManager;
+import com.intellij.codeInsight.hint.HintManagerImpl;
+import com.intellij.codeInsight.hint.HintUtil;
 import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.openapi.vfs.ReadonlyStatusHandler;
-import com.intellij.openapi.vfs.VfsUtilCore;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileVisitor;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiUtilBase;
+import com.intellij.ui.LightweightHint;
+import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.DartFileType;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
-import com.jetbrains.lang.dart.sdk.DartSdk;
 import icons.DartIcons;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
+import javax.swing.*;
 import java.util.List;
 
 public class DartStyleAction extends AnAction implements DumbAware {
 
-  private final static String DART_STYLE_COMMAND_NAME = "Reformat with Dart Style";
+  private static final Logger LOG = Logger.getInstance(DartStyleAction.class.getName());
 
   public DartStyleAction() {
-    super("Dart Style", "", DartIcons.Dart_16);
+    super(DartBundle.message("dart.style.action.name"), DartBundle.message("dart.style.action.description"), DartIcons.Dart_16);
   }
 
   @Override
@@ -97,18 +98,53 @@ public class DartStyleAction extends AnAction implements DumbAware {
         DartAnalysisServerService.FormatResult formatResult =
           DartAnalysisServerService.getInstance().edit_format(path, caretOffset, 0);
 
-        if (formatResult != null && formatResult.getEdits() != null && formatResult.getEdits().size() == 1) {
-          for (SourceEdit edit : formatResult.getEdits()) {
-            document.replaceString(0, document.getTextLength(), edit.getReplacement());
-            editor.getCaretModel().moveToOffset(formatResult.getOffset());
-          }
+        if (formatResult == null) {
+          showHintLater(editor, DartBundle.message("dart.style.failed"), true);
+          LOG.warn("Unexpected response from edit_format, formatResult is null");
+          return;
+        }
+
+        final List<SourceEdit> edits = formatResult.getEdits();
+        if (edits == null || edits.size() == 0) {
+          showHintLater(editor, DartBundle.message("dart.style.already.good"), false);
+        }
+        else if (edits.size() == 1) {
+          document.replaceString(0, document.getTextLength(), edits.get(0).getReplacement());
+          editor.getCaretModel().moveToOffset(formatResult.getOffset());
+          showHintLater(editor, DartBundle.message("dart.style.success"), false);
+        }
+        else {
+          showHintLater(editor, DartBundle.message("dart.style.failed"), true);
+          LOG.warn("Unexpected response from edit_format, formatResult.getEdits().size() = " + edits.size());
         }
       }
     };
+
     ApplicationManager.getApplication().runWriteAction(new Runnable() {
       @Override
       public void run() {
-        CommandProcessor.getInstance().executeCommand(project, runnable, DART_STYLE_COMMAND_NAME, null);
+        CommandProcessor.getInstance().executeCommand(project, runnable, DartBundle.message("dart.style.action.name"), null);
+      }
+    });
+  }
+
+  private static void showHintLater(@NotNull final Editor editor, @NotNull final String text, final boolean error) {
+    ApplicationManager.getApplication().invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        final JComponent component = error ? HintUtil.createErrorLabel(text)
+                                           : HintUtil.createInformationLabel(text);
+        final LightweightHint hint = new LightweightHint(component);
+        HintManagerImpl.getInstanceImpl().showEditorHint(hint, editor, HintManager.UNDER,
+                                                         HintManager.HIDE_BY_ANY_KEY |
+                                                         HintManager.HIDE_BY_TEXT_CHANGE |
+                                                         HintManager.HIDE_BY_SCROLLING,
+                                                         0, false);
+      }
+    }, ModalityState.NON_MODAL, new Condition() {
+      @Override
+      public boolean value(Object o) {
+        return editor.isDisposed() || !editor.getComponent().isShowing();
       }
     });
   }
