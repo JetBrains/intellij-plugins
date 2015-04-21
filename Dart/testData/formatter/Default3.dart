717ecdf07f3cb76bfe549cff6676773d4cd27ae1
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-04-21 16:24:03 +0300

    test for Dart Object literal braces formatting (WEB-14575)

--- Dart/testData/formatter/Default3.dart
+++ Dart/testData/formatter/Default3.dart
@@ -1,4 +1,4 @@
 class Test1{String a;foo(){}int b;}class Test2 {foo(){}int b;}
 abstract class Test3{foo(){if(true){}int c = 239;while(true){}}int b;}
 @Foo
-void  foo  (  )  {  }
+void  foo  (  )  { var  foo  =  {} ; }
