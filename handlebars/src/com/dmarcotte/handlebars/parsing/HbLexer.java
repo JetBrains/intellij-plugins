002f98afd369391e3d3a0882b882c39a4b36128b
Dennis Ushakov <dennis.ushakov@gmail.com>
2015-05-25 20:03:46 +0300

    optimize getMergeFunction #WEB-16683 fixed

--- handlebars/src/com/dmarcotte/handlebars/parsing/HbLexer.java
+++ handlebars/src/com/dmarcotte/handlebars/parsing/HbLexer.java
@@ -6,40 +6,42 @@ import com.intellij.lexer.MergingLexerAdapterBase;
 import com.intellij.psi.tree.IElementType;
 
 public class HbLexer extends MergingLexerAdapterBase {
+  private static final MergeFunction MERGE_FUNCTION = new MergeFunction() {
+    @Override
+    public IElementType merge(final IElementType type, final Lexer originalLexer) {
+      if (HbTokenTypes.COMMENT_OPEN != type) {
+        return type;
+      }
+
+      if (originalLexer.getTokenType() == HbTokenTypes.COMMENT_CONTENT) {
+        originalLexer.advance();
+      }
+
+      if (originalLexer.getTokenType() == HbTokenTypes.COMMENT_CLOSE) {
+        originalLexer.advance();
+        return HbTokenTypes.COMMENT;
+      }
+
+      if (originalLexer.getTokenType() == null) {
+        return HbTokenTypes.UNCLOSED_COMMENT;
+      }
+
+
+      if (originalLexer.getTokenType() == HbTokenTypes.UNCLOSED_COMMENT) {
+        originalLexer.advance();
+        return HbTokenTypes.UNCLOSED_COMMENT;
+      }
+
+      return type;
+    }
+  };
+
   public HbLexer() {
     super(new HbRawLexer());
   }
 
   @Override
   public MergeFunction getMergeFunction() {
-    return new MergeFunction() {
-      @Override
-      public IElementType merge(final IElementType type, final Lexer originalLexer) {
-        if (HbTokenTypes.COMMENT_OPEN != type) {
-          return type;
-        }
-
-        if (originalLexer.getTokenType() == HbTokenTypes.COMMENT_CONTENT) {
-          originalLexer.advance();
-        }
-
-        if (originalLexer.getTokenType() == HbTokenTypes.COMMENT_CLOSE) {
-          originalLexer.advance();
-          return HbTokenTypes.COMMENT;
-        }
-
-        if (originalLexer.getTokenType() == null) {
-          return HbTokenTypes.UNCLOSED_COMMENT;
-        }
-
-
-        if (originalLexer.getTokenType() == HbTokenTypes.UNCLOSED_COMMENT) {
-          originalLexer.advance();
-          return HbTokenTypes.UNCLOSED_COMMENT;
-        }
-
-        return type;
-      }
-    };
+    return MERGE_FUNCTION;
   }
 }
