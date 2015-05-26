130068d2491dc45bcb4fc137f9846ed8ce93b0cd
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-05-26 12:18:55 +0200

    Cleanup (unneeded platform prefix initialization)

--- osmorc/src_test/org/osmorc/HeavyOsgiFixtureTestCase.java
+++ osmorc/src_test/org/osmorc/HeavyOsgiFixtureTestCase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.osmorc;
 
 import com.intellij.openapi.application.Result;
@@ -7,7 +22,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.impl.ProjectImpl;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.testFramework.IdeaTestCase;
 import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
 import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
 import com.intellij.testFramework.fixtures.JavaTestFixtureFactory;
@@ -24,10 +38,6 @@ import static org.junit.Assert.assertNotNull;
 public abstract class HeavyOsgiFixtureTestCase {
   protected TempDirTestFixture myTempDirFixture;
   protected IdeaProjectTestFixture myFixture;
-
-  public HeavyOsgiFixtureTestCase() {
-    IdeaTestCase.initPlatformPrefix();
-  }
 
   @Before
   public void setUp() throws Exception {
