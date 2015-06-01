57dfff99c7e2782e213bc6bb75ee14fe29dd1a7a
Steve Messick <messick@google.com>
2015-06-01 16:06:05 -0700

    Improve Dart formatter spacing logic.

---
+++ Dart/testData/formatter/Expressions_after.dart
@@ -0,0 +1,14 @@
+m() {
+  - - -1;
+  - --1;
+  -++-1;
+  +--+1;
+  <int>[];
+  <int, int>{};
+  -~!foo;
+  x && y;
+  throw new FormatException(
+      "This is a long exception message.");
+  ;
+  ;
+}
