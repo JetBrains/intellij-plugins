40a1c1d11ad8f6bebf9dc53024792b5e980b5103
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-06-05 19:40:05 +0300

    FlexApplicationComponent optimizations (it is not an application component any more)

--- flex/src/com/intellij/javascript/flex/FlexApplicationComponent.java
+++ flex/src/com/intellij/javascript/flex/FlexApplicationComponent.java
@@ -10,7 +10,6 @@ import com.intellij.lang.Language;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
 import com.intellij.lang.javascript.flex.MxmlFileType;
 import com.intellij.lang.javascript.psi.ecmal4.impl.JSAttributeImpl;
-import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
@@ -18,16 +17,15 @@ import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.filters.position.NamespaceFilter;
 import com.intellij.psi.filters.position.RootTagFilter;
+import com.intellij.psi.meta.MetaDataContributor;
 import com.intellij.psi.meta.MetaDataRegistrar;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-/**
- * @author yole
- */
-public class FlexApplicationComponent extends FileTypeFactory implements ApplicationComponent {
+// In fact it is not an application component any more.
+public class FlexApplicationComponent extends FileTypeFactory implements MetaDataContributor, StandardResourceProvider {
   private static final Icon ICON = AllIcons.FileTypes.JavaClass;
 
   public static final FileType SWF_FILE_TYPE = new FileType() {
@@ -74,19 +72,12 @@ public class FlexApplicationComponent extends FileTypeFactory implements Applica
     {"1.0", "1.1", "1.5", "1.5.1", "1.5.2", "1.5.3", "2.0", "2.5", "2.6", "2.7", "3.0", "3.1", "3.2", "3.3", "3.4", "3.5", "3.6", "3.7",
       "3.8", "3.9", "4.0", "13.0", "14.0", "15.0", "16.0", "17.0", "18.0", "19.0"};
 
-  @NotNull
-  public String getComponentName() {
-    return "FlexApplicationComponent";
-  }
-
-  public void initComponent() {
-    MetaDataRegistrar.getInstance().registerMetaData(
+  @Override
+  public void contributeMetaData(@NotNull final MetaDataRegistrar registrar) {
+    registrar.registerMetaData(
       new RootTagFilter(new NamespaceFilter(MxmlJSClass.MXML_URIS)),
       FlexMxmlNSDescriptor.class
     );
-  }
-
-  public void disposeComponent() {
   }
 
   public void createFileTypes(final @NotNull FileTypeConsumer consumer) {
@@ -94,16 +85,14 @@ public class FlexApplicationComponent extends FileTypeFactory implements Applica
     consumer.consume(MXML, "mxml;fxg");
   }
 
-  public static class ResourceProvider implements StandardResourceProvider {
 
-    public void registerResources(ResourceRegistrar registrar) {
-      registrar.addStdResource(HTTP_WWW_ADOBE_COM_2006_FLEX_CONFIG, "/schemas/FlexCompilerSchema.xsd", FlexApplicationComponent.class);
+  public void registerResources(ResourceRegistrar registrar) {
+    registrar.addStdResource(HTTP_WWW_ADOBE_COM_2006_FLEX_CONFIG, "/schemas/FlexCompilerSchema.xsd", FlexApplicationComponent.class);
 
-      registrar.addStdResource(JSAttributeImpl.URN_FLEX_META, "/schemas/KnownMetaData.dtd", FlexApplicationComponent.class);
+    registrar.addStdResource(JSAttributeImpl.URN_FLEX_META, "/schemas/KnownMetaData.dtd", FlexApplicationComponent.class);
 
-      for (String version : AIR_VERSIONS) {
-        registerAirDescriptorSchema(registrar, version);
-      }
+    for (String version : AIR_VERSIONS) {
+      registerAirDescriptorSchema(registrar, version);
     }
   }
 
