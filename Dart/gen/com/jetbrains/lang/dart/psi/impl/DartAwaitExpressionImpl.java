d3dc8fe1f93129e27d43ae81ebc50a316861f2d8
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-06-08 20:53:42 +0300

    DartAwaitExpression implements DartReference

--- Dart/gen/com/jetbrains/lang/dart/psi/impl/DartAwaitExpressionImpl.java
+++ Dart/gen/com/jetbrains/lang/dart/psi/impl/DartAwaitExpressionImpl.java
@@ -11,7 +11,7 @@ import static com.jetbrains.lang.dart.DartTokenTypes.*;
 import com.jetbrains.lang.dart.psi.*;
 import com.jetbrains.lang.dart.util.DartPsiImplUtil;
 
-public class DartAwaitExpressionImpl extends DartExpressionImpl implements DartAwaitExpression {
+public class DartAwaitExpressionImpl extends DartReferenceImpl implements DartAwaitExpression {
 
   public DartAwaitExpressionImpl(ASTNode node) {
     super(node);
