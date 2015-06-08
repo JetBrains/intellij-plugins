4c92760db428c1e6bba7290f3372a05a4b715497
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-06-08 21:18:55 +0300

    WEB-15322 Intellisense after await (Dart)

--- Dart/src/com/jetbrains/lang/dart/psi/impl/DartReferenceImpl.java
+++ Dart/src/com/jetbrains/lang/dart/psi/impl/DartReferenceImpl.java
@@ -124,11 +124,13 @@ public class DartReferenceImpl extends DartExpressionImpl implements DartReferen
       final DartClass dartClass = PsiTreeUtil.getParentOfType(this, DartClass.class);
       return dartClass == null ? DartClassResolveResult.EMPTY : dartClass.getSuperClassResolvedOrObjectClass();
     }
+
     if (this instanceof DartNewExpression) {
       final DartClassResolveResult result = DartResolveUtil.resolveClassByType(PsiTreeUtil.getChildOfType(this, DartType.class));
       result.specialize(this);
       return result;
     }
+
     if (this instanceof DartCallExpression) {
       final DartExpression expression = ((DartCallExpression)this).getExpression();
       final DartClassResolveResult leftResult = tryGetLeftResolveResult(expression);
@@ -139,12 +141,29 @@ public class DartReferenceImpl extends DartExpressionImpl implements DartReferen
         return result;
       }
     }
+
     if (this instanceof DartCascadeReferenceExpression) {
       DartReference[] children = PsiTreeUtil.getChildrenOfType(this, DartReference.class);
       if (children != null && children.length == 2) {
         return children[0].resolveDartClass();
       }
     }
+
+    if (this instanceof DartAwaitExpression) {
+      final DartExpression expression = ((DartAwaitExpression)this).getExpression();
+      if (expression instanceof DartReference) {
+        final DartClassResolveResult result = ((DartReference)expression).resolveDartClass();
+        final DartClass resolvedClass = result.getDartClass();
+        if (resolvedClass != null && "Future".equals(resolvedClass.getName())) {
+          final DartClassResolveResult unwrappedFuture = result.getSpecialization().get(resolvedClass, "T");
+          return unwrappedFuture == null ? DartClassResolveResult.EMPTY : unwrappedFuture;
+        }
+        else {
+          return result;
+        }
+      }
+    }
+
     return DartResolveUtil.getDartClassResolveResult(resolve(), tryGetLeftResolveResult(this).getSpecialization());
   }
 
