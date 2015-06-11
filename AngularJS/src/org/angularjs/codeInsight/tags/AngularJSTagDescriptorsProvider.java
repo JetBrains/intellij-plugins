697a9d80f4d40624e0b6ed02166f9264998fd5de
Dennis Ushakov <dennis.ushakov@gmail.com>
2015-06-11 19:08:21 +0300

    Revert "loading namespace descriptor is no longer needed"
    This reverts commit 6d8edee2b22396035979f49206b5f4fa3cd6d218.

--- AngularJS/src/org/angularjs/codeInsight/tags/AngularJSTagDescriptorsProvider.java
+++ AngularJS/src/org/angularjs/codeInsight/tags/AngularJSTagDescriptorsProvider.java
@@ -10,7 +10,9 @@ import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.Processor;
 import com.intellij.xml.XmlElementDescriptor;
+import com.intellij.xml.XmlNSDescriptor;
 import com.intellij.xml.XmlTagNameProvider;
+import com.intellij.xml.impl.schema.AnyXmlElementDescriptor;
 import org.angularjs.codeInsight.DirectiveUtil;
 import org.angularjs.index.AngularIndexUtil;
 import org.jetbrains.annotations.NotNull;
@@ -46,7 +48,14 @@ public class AngularJSTagDescriptorsProvider implements XmlElementDescriptorProv
     if (!(xmlTag instanceof HtmlTag && AngularIndexUtil.hasAngularJS(xmlTag.getProject()))) return null;
 
     final String directiveName = DirectiveUtil.normalizeAttributeName(xmlTag.getName());
-    final JSImplicitElement directive = DirectiveUtil.getTagDirective(directiveName, xmlTag.getProject());
+    final XmlNSDescriptor nsDescriptor = xmlTag.getNSDescriptor(xmlTag.getNamespace(), false);
+    final XmlElementDescriptor descriptor = nsDescriptor != null ? nsDescriptor.getElementDescriptor(xmlTag) : null;
+    if (descriptor != null && !(descriptor instanceof AnyXmlElementDescriptor)) {
+      return null;
+    }
+
+    final Project project = xmlTag.getProject();
+    final JSImplicitElement directive = DirectiveUtil.getTagDirective(directiveName, project);
 
     return directive != null ? new AngularJSTagDescriptor(directiveName, directive) : null;
   }
