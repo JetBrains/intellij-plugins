ae3f0353a1cce00cfcc2e4a51dcd85b145251e82
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-07-20 17:34:43 +0300

    Remove duplicate code

--- flex/src/com/intellij/lang/javascript/flex/actions/airpackage/AirPackageDialog.java
+++ flex/src/com/intellij/lang/javascript/flex/actions/airpackage/AirPackageDialog.java
@@ -1,18 +1,15 @@
 package com.intellij.lang.javascript.flex.actions.airpackage;
 
-import com.intellij.flex.FlexCommonUtils;
 import com.intellij.flex.model.bc.BuildConfigurationNature;
-import com.intellij.flex.model.bc.OutputType;
 import com.intellij.flex.model.bc.TargetPlatform;
 import com.intellij.lang.javascript.flex.FlexBundle;
 import com.intellij.lang.javascript.flex.actions.FlexBCTree;
 import com.intellij.lang.javascript.flex.build.FlashProjectStructureProblem;
-import com.intellij.lang.javascript.flex.projectStructure.model.*;
-import com.intellij.lang.javascript.flex.projectStructure.ui.AirPackagingConfigurableBase;
-import com.intellij.lang.javascript.flex.sdk.FlexmojosSdkType;
+import com.intellij.lang.javascript.flex.build.ValidateFlashConfigurationsPrecompileTask;
+import com.intellij.lang.javascript.flex.projectStructure.model.AirPackagingOptions;
+import com.intellij.lang.javascript.flex.projectStructure.model.FlexBuildConfiguration;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.projectRoots.Sdk;
 import com.intellij.openapi.roots.ui.configuration.projectRoot.daemon.ProjectStructureProblemType;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.ui.Messages;
@@ -20,25 +17,16 @@ import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
-import com.intellij.psi.xml.XmlFile;
-import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.Consumer;
-import com.intellij.util.PathUtil;
 import com.intellij.util.ui.UIUtil;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -211,7 +199,7 @@ public class AirPackageDialog extends DialogWrapper {
       }
 
       final Ref<String> firstErrorRef = new Ref<String>();
-      checkPackagingOptions(moduleAndBC.first, bc, new Consumer<FlashProjectStructureProblem>() {
+      ValidateFlashConfigurationsPrecompileTask.checkPackagingOptions(moduleAndBC.first, bc, new Consumer<FlashProjectStructureProblem>() {
         public void consume(final FlashProjectStructureProblem problem) {
           if (problem.severity == ProjectStructureProblemType.Severity.ERROR && firstErrorRef.isNull()) {
             firstErrorRef.set(problem.errorMessage);
@@ -226,196 +214,6 @@ public class AirPackageDialog extends DialogWrapper {
     }
 
     return null;
-  }
-
-  public static void checkPackagingOptions(final Module module,
-                                           final FlexBuildConfiguration bc,
-                                           final Consumer<FlashProjectStructureProblem> errorConsumer) {
-    if (bc.getOutputType() != OutputType.Application) return;
-
-    if (bc.getTargetPlatform() == TargetPlatform.Desktop) {
-      checkPackagingOptions(module, bc.getSdk(), bc.getAirDesktopPackagingOptions(), false,
-                            PathUtil.getParentPath(bc.getActualOutputFilePath()), errorConsumer);
-    }
-    else if (bc.getTargetPlatform() == TargetPlatform.Mobile) {
-      if (bc.getAndroidPackagingOptions().isEnabled()) {
-        checkPackagingOptions(module, bc.getSdk(), bc.getAndroidPackagingOptions(), false,
-                              PathUtil.getParentPath(bc.getActualOutputFilePath()), errorConsumer);
-      }
-      if (bc.getIosPackagingOptions().isEnabled()) {
-        checkPackagingOptions(module, bc.getSdk(), bc.getIosPackagingOptions(), false,
-                              PathUtil.getParentPath(bc.getActualOutputFilePath()), errorConsumer);
-      }
-    }
-  }
-  
-  private static void checkPackagingOptions(final Module module,
-                                            final @Nullable Sdk sdk,
-                                            final AirPackagingOptions packagingOptions,
-                                            final boolean isForIosSimulator,
-                                            final String outputFolderPath,
-                                            final Consumer<FlashProjectStructureProblem> errorConsumer) {
-    final String device = packagingOptions instanceof AndroidPackagingOptions
-                          ? "Android"
-                          : packagingOptions instanceof IosPackagingOptions
-                            ? "iOS"
-                            : "";
-    if (!packagingOptions.isUseGeneratedDescriptor()) {
-      if (packagingOptions.getCustomDescriptorPath().isEmpty()) {
-        errorConsumer.consume(FlashProjectStructureProblem
-                                .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions,
-                                                               FlexBundle.message("custom.descriptor.not.set", device),
-                                                               AirPackagingConfigurableBase.Location.CustomDescriptor));
-      }
-      else {
-        final VirtualFile descriptorFile = LocalFileSystem.getInstance().findFileByPath(packagingOptions.getCustomDescriptorPath());
-        if (descriptorFile == null || descriptorFile.isDirectory()) {
-          errorConsumer.consume(FlashProjectStructureProblem
-                                  .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-                                    .message("custom.descriptor.not.found", device,
-                                             FileUtil.toSystemDependentName(packagingOptions.getCustomDescriptorPath())),
-                                                                 AirPackagingConfigurableBase.Location.CustomDescriptor));
-        }
-        else if (sdk != null && sdk.getSdkType() != FlexmojosSdkType.getInstance()) {
-          checkAirVersionIfCustomDescriptor(module, sdk, packagingOptions, errorConsumer, false, "does not matter");
-        }
-      }
-    }
-
-    if (packagingOptions.getPackageFileName().isEmpty()) {
-      errorConsumer.consume(FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-        .message("package.file.name.not.set", device), AirPackagingConfigurableBase.Location.PackageFileName));
-    }
-
-    for (AirPackagingOptions.FilePathAndPathInPackage entry : packagingOptions.getFilesToPackage()) {
-      final String fullPath = entry.FILE_PATH;
-      String relPathInPackage = entry.PATH_IN_PACKAGE;
-      if (relPathInPackage.startsWith("/")) {
-        relPathInPackage = relPathInPackage.substring(1);
-      }
-
-      if (fullPath.isEmpty()) {
-        errorConsumer.consume(FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-          .message("packaging.options.empty.file.name", device), AirPackagingConfigurableBase.Location.FilesToPackage));
-      }
-      else {
-        final VirtualFile file = LocalFileSystem.getInstance().findFileByPath(fullPath);
-        if (file == null) {
-          errorConsumer.consume(FlashProjectStructureProblem
-                                  .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-                                    .message("packaging.options.file.not.found", device, FileUtil.toSystemDependentName(fullPath)),
-                                                                 AirPackagingConfigurableBase.Location.FilesToPackage));
-        }
-
-        if (relPathInPackage.isEmpty()) {
-          errorConsumer.consume(FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-            .message("packaging.options.empty.relative.path", device), AirPackagingConfigurableBase.Location.FilesToPackage));
-        }
-
-        if (file != null && file.isDirectory()) {
-          if (FileUtil.isAncestor(file.getPath(), outputFolderPath, false)) {
-            errorConsumer.consume(FlashProjectStructureProblem
-                                    .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-                                      .message("folder.to.package.includes.output", device, file.getPresentableUrl()),
-                                                                   AirPackagingConfigurableBase.Location.FilesToPackage));
-          }
-          else if (!relPathInPackage.isEmpty() && !".".equals(relPathInPackage) && !fullPath.endsWith("/" + relPathInPackage)) {
-            errorConsumer.consume(
-              FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-                .message("packaging.options.relative.path.not.matches", device, FileUtil.toSystemDependentName(relPathInPackage)),
-                                                                         AirPackagingConfigurableBase.Location.FilesToPackage));
-          }
-        }
-      }
-    }
-
-    if (packagingOptions instanceof IosPackagingOptions) {
-      final String path = packagingOptions.getSigningOptions().getIOSSdkPath();
-      if (!path.isEmpty() && !new File(path).isDirectory()) {
-        errorConsumer.consume(FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-          .message("packaging.options.bad.ios.sdk.path", device, FileUtil.toSystemDependentName(path)),
-                                                                                         AirPackagingConfigurableBase.Location.IosSdkPath));
-      }
-    }
-
-    final AirSigningOptions signingOptions = packagingOptions.getSigningOptions();
-    if (packagingOptions instanceof IosPackagingOptions && !isForIosSimulator) {
-      final String provisioningProfilePath = signingOptions.getProvisioningProfilePath();
-      if (provisioningProfilePath.isEmpty()) {
-        errorConsumer.consume(FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-          .message("ios.provisioning.profile.not.set"), AirPackagingConfigurableBase.Location.ProvisioningProfile));
-      }
-      else {
-        final VirtualFile provisioningProfile = LocalFileSystem.getInstance().findFileByPath(provisioningProfilePath);
-        if (provisioningProfile == null || provisioningProfile.isDirectory()) {
-          errorConsumer.consume(FlashProjectStructureProblem
-                                  .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-                                    .message("ios.provisioning.profile.not.found", FileUtil.toSystemDependentName(provisioningProfilePath)),
-                                                                 AirPackagingConfigurableBase.Location.ProvisioningProfile));
-        }
-      }
-    }
-
-    final boolean tempCertificate = packagingOptions instanceof IosPackagingOptions ? isForIosSimulator
-                                                                                    : signingOptions.isUseTempCertificate();
-    if (!tempCertificate) {
-      final String keystorePath = signingOptions.getKeystorePath();
-      if (keystorePath.isEmpty()) {
-        errorConsumer.consume(FlashProjectStructureProblem.createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions,
-                                                                                         FlexBundle.message("keystore.not.set", device),
-                                                                                         AirPackagingConfigurableBase.Location.Keystore));
-      }
-      else {
-        final VirtualFile keystore = LocalFileSystem.getInstance().findFileByPath(keystorePath);
-        if (keystore == null || keystore.isDirectory()) {
-          errorConsumer.consume(FlashProjectStructureProblem
-                                  .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.ERROR, packagingOptions, FlexBundle
-                                    .message("keystore.not.found", device, FileUtil.toSystemDependentName(keystorePath)),
-                                                                 AirPackagingConfigurableBase.Location.Keystore));
-        }
-      }
-    }
-  }
-
-  public static void checkAirVersionIfCustomDescriptor(final Module module,
-                                                       final Sdk sdk,
-                                                       final AirPackagingOptions packagingOptions,
-                                                       final Consumer<FlashProjectStructureProblem> errorConsumer,
-                                                       final boolean errorMessageForRunConfigValidation,
-                                                       final String bcName) {
-    if (packagingOptions.isUseGeneratedDescriptor()) return;
-    if (packagingOptions.getCustomDescriptorPath().isEmpty()) return;
-    final VirtualFile descriptorFile = LocalFileSystem.getInstance().findFileByPath(packagingOptions.getCustomDescriptorPath());
-    if (descriptorFile == null || descriptorFile.isDirectory()) return;
-    if (sdk.getSdkType() == FlexmojosSdkType.getInstance()) return;
-
-
-    final PsiFile file = PsiManager.getInstance(module.getProject()).findFile(descriptorFile);
-    final XmlTag rootTag = file instanceof XmlFile ? ((XmlFile)file).getRootTag() : null;
-    final String ns = rootTag == null ? null : rootTag.getNamespace();
-    final String nsVersion = ns != null && ns.startsWith(FlexCommonUtils.AIR_NAMESPACE_BASE)
-                             ? ns.substring(FlexCommonUtils.AIR_NAMESPACE_BASE.length())
-                             : null;
-    final String airSdkVersion = FlexCommonUtils.getAirVersion(sdk.getHomePath(), sdk.getVersionString());
-
-    if (nsVersion != null && airSdkVersion != null && !nsVersion.equals(airSdkVersion)) {
-      final String message;
-      if (errorMessageForRunConfigValidation) {
-        message = FlexBundle.message("bc.0.module.1.air.version.mismatch.warning", bcName, module.getName(), nsVersion, airSdkVersion,
-                                     FileUtil.toSystemDependentName(descriptorFile.getPath()));
-      }
-      else {
-        final String device = packagingOptions instanceof AndroidPackagingOptions
-                              ? "Android"
-                              : packagingOptions instanceof IosPackagingOptions
-                                ? "iOS"
-                                : "";
-        message = FlexBundle.message("air.version.mismatch.warning", device, descriptorFile.getName(), nsVersion, airSdkVersion);
-      }
-      errorConsumer.consume(FlashProjectStructureProblem
-                              .createPackagingOptionsProblem(ProjectStructureProblemType.Severity.WARNING, packagingOptions, message,
-                                                             AirPackagingConfigurableBase.Location.CustomDescriptor));
-    }
   }
 
   protected void doOKAction() {
