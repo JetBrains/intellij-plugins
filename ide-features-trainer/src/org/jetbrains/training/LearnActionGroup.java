91d19d62cc42bccc4001fb5ad8f9ee44d03ffab7
Sergey Karashevich <karashevich@gmail.com>
2015-07-20 21:12:23 +0300

    cleanup menu
    GitOrigin-RevId: 101893a0ef094e70ff857ebee649c692a2c600e1

---
+++ ide-features-trainer/src/org/jetbrains/training/LearnActionGroup.java
@@ -0,0 +1,9 @@
+package org.jetbrains.training;
+
+import com.intellij.openapi.actionSystem.DefaultActionGroup;
+
+/**
+ * Created by karashevich on 20/07/15.
+ */
+public class LearnActionGroup extends DefaultActionGroup {
+}
