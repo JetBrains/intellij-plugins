60aa039d40af39975f54911d7466118695e72a83
Konstantin Shcheglov <scheglov@google.com>
2015-07-21 11:50:15 -0700

    Add 'Reanalyze Sources' action.

---
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/ReanalyzeAction.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.lang.dart.ide.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.project.DumbAware;
+import com.jetbrains.lang.dart.DartBundle;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import icons.DartIcons;
+
+public class ReanalyzeAction extends AnAction implements DumbAware {
+  public ReanalyzeAction() {
+    super(DartBundle.message("dart.reanalyze.action.name"), DartBundle.message("dart.reanalyze.action.description"), DartIcons.Dart_16);
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    DartAnalysisServerService.getInstance().analysis_reanalyze(null);
+  }
+}
