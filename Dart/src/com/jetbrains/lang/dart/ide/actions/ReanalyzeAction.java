e9112de646b12291a0e07ec062eaa68e5d8c452b
Konstantin Shcheglov <scheglov@google.com>
2015-07-22 10:01:57 -0700

    Enable/disable ReanalyzeAction, add to DartProblemsViewPanel.

--- Dart/src/com/jetbrains/lang/dart/ide/actions/ReanalyzeAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/ReanalyzeAction.java
@@ -17,18 +17,82 @@ package com.jetbrains.lang.dart.ide.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.search.FileTypeIndex;
+import com.intellij.psi.search.GlobalSearchScopesCore;
 import com.jetbrains.lang.dart.DartBundle;
+import com.jetbrains.lang.dart.DartFileType;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
 import icons.DartIcons;
+import org.jetbrains.annotations.NotNull;
 
 public class ReanalyzeAction extends AnAction implements DumbAware {
   public ReanalyzeAction() {
     super(DartBundle.message("dart.reanalyze.action.name"), DartBundle.message("dart.reanalyze.action.description"), DartIcons.Dart_16);
   }
 
+  private static boolean isEnabled(AnActionEvent event) {
+    final Project project = event.getProject();
+    if (project == null) {
+      return false;
+    }
+
+    final Editor editor = event.getData(CommonDataKeys.EDITOR);
+    if (editor != null) {
+      final Document document = editor.getDocument();
+      final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+      return isDartFile(psiFile);
+    }
+
+    final VirtualFile[] filesAndDirs = CommonDataKeys.VIRTUAL_FILE_ARRAY.getData(event.getDataContext());
+    if (filesAndDirs == null) {
+      return false;
+    }
+    return hasDartFile(project, filesAndDirs);
+  }
+
+  private static boolean isDartFile(PsiFile file) {
+    return file != null && file.getFileType() == DartFileType.INSTANCE;
+  }
+
+  private static boolean isDartFile(VirtualFile file) {
+    return file != null && file.getFileType() == DartFileType.INSTANCE;
+  }
+
+  private static boolean hasDartFile(@NotNull Project project, @NotNull VirtualFile[] files) {
+    for (VirtualFile fileOrDir : files) {
+      if (!fileOrDir.isDirectory() && isDartFile(fileOrDir)) {
+        return true;
+      }
+    }
+
+    for (VirtualFile fileOrDir : files) {
+      if (fileOrDir.isDirectory() &&
+          FileTypeIndex.containsFileOfType(DartFileType.INSTANCE, GlobalSearchScopesCore.directoryScope(project, fileOrDir, true))) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
   @Override
   public void actionPerformed(AnActionEvent e) {
     DartAnalysisServerService.getInstance().analysis_reanalyze(null);
+  }
+
+  @Override
+  public void update(AnActionEvent event) {
+    final Presentation presentation = event.getPresentation();
+    final boolean enabled = isEnabled(event);
+    presentation.setEnabledAndVisible(enabled);
   }
 }
--- Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartProblemsViewPanel.java
+++ Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartProblemsViewPanel.java
@@ -1,8 +1,13 @@
 package com.jetbrains.lang.dart.ide.errorTreeView;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.errorTreeView.NewErrorTreeViewPanel;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DefaultActionGroup;
 import com.intellij.openapi.project.Project;
+import com.jetbrains.lang.dart.DartBundle;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
 
 public class DartProblemsViewPanel extends NewErrorTreeViewPanel {
 
@@ -14,6 +19,13 @@ public class DartProblemsViewPanel extends NewErrorTreeViewPanel {
   @Override
   protected void fillRightToolbarGroup(DefaultActionGroup group) {
     super.fillRightToolbarGroup(group);
+    group.add(new AnAction(DartBundle.message("dart.reanalyze.action.name"), DartBundle.message("dart.reanalyze.action.description"),
+                           AllIcons.Actions.ForceRefresh) {
+      @Override
+      public void actionPerformed(AnActionEvent e) {
+        DartAnalysisServerService.getInstance().analysis_reanalyze(null);
+      }
+    });
   }
 
   @Override
