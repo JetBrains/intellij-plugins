3f11c709dc39910e4aded52ca69aeb3a318c12ac
nik <Nikolay.Chashnikov@jetbrains.com>
2015-07-24 18:07:24 +0300

    tapestry: fixed "Cannot find standard resource" problem

--- core/src/com/intellij/tapestry/intellij/TapestryResourceProvider.java
+++ core/src/com/intellij/tapestry/intellij/TapestryResourceProvider.java
@@ -12,8 +12,8 @@ public class TapestryResourceProvider implements StandardResourceProvider {
   @Override
   public void registerResources(ResourceRegistrar registrar) {
     registrar.addIgnoredResource(TapestryConstants.PARAMETERS_NAMESPACE);
-    registrar.addStdResource(TapestryConstants.TEMPLATE_NAMESPACE, "/META-INF/tapestry_5_1_0.xsd");
-    registrar.addStdResource(TapestryConstants.TEMPLATE_NAMESPACE2, "/META-INF/tapestry_5_0_0.xsd");
-    registrar.addStdResource(TapestryConstants.TEMPLATE_NAMESPACE3, "/META-INF/tapestry_5_3.xsd");
+    registrar.addStdResource(TapestryConstants.TEMPLATE_NAMESPACE, "/META-INF/tapestry_5_1_0.xsd", getClass());
+    registrar.addStdResource(TapestryConstants.TEMPLATE_NAMESPACE2, "/META-INF/tapestry_5_0_0.xsd", getClass());
+    registrar.addStdResource(TapestryConstants.TEMPLATE_NAMESPACE3, "/META-INF/tapestry_5_3.xsd", getClass());
   }
 }
