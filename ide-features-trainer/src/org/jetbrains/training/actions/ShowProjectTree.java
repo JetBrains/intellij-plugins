1a48c61a00633149c3d236a21be4c09635e1f253
Sergey Karashevich <karashevich@gmail.com>
2015-08-05 12:37:55 +0300

    Learn UI highlight UI panel
    GitOrigin-RevId: ad4a2dec53c6280fe8560b911a2af61a6f855a12

--- ide-features-trainer/src/org/jetbrains/training/actions/ShowProjectTree.java
+++ ide-features-trainer/src/org/jetbrains/training/actions/ShowProjectTree.java
@@ -3,15 +3,15 @@ package org.jetbrains.training.actions;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.training.util.LearnUiUtil;
-import sun.plugin.util.UIUtil;
 
 /**
  * Created by karashevich on 28/07/15.
  */
-public class GetEditorWindow extends AnAction {
+public class ShowProjectTree extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        LearnUiUtil.getInstance().getEditorWindow(anActionEvent.getProject());
+//        LearnUiUtil.getInstance().getEditorWindow(anActionEvent.getProject());
+        LearnUiUtil.getInstance().highlightIdeComponent(LearnUiUtil.IdeComponent.PROJECT_TREE, anActionEvent.getProject());
     }
 }
