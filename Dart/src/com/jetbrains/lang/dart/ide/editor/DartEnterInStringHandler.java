a7ebdb73ab641864b1f3d360cd08108919302e6a
Steve Messick <messick@google.com>
2015-08-31 15:54:44 -0700

    Correctly handle raw and interpolated strings.

--- Dart/src/com/jetbrains/lang/dart/ide/editor/DartEnterInStringHandler.java
+++ Dart/src/com/jetbrains/lang/dart/ide/editor/DartEnterInStringHandler.java
@@ -31,26 +31,52 @@ public class DartEnterInStringHandler extends EnterHandlerDelegateAdapter {
     Document document = editor.getDocument();
     ASTNode token = psiAtOffset.getNode();
     IElementType type = token.getElementType();
+    if (type == DartTokenTypes.RAW_SINGLE_QUOTED_STRING) {
+      char quote = token.getText().charAt(1);
+      breakString("r" + quote, String.valueOf(quote), caretOffsetRef, caretAdvanceRef, document);
+      return Result.Default;
+    }
     if (type == DartTokenTypes.REGULAR_STRING_PART) {
       token = token.getTreeNext();
+      if (token == null) return Result.Continue; // Can't happen with current grammar.
       type = token.getElementType();
       // At this point we might have type == TokenType.ERROR_ELEMENT if the string is unterminated.
       // We're ignoring that case since we don't have a rich enough set of signals to deduce where or
       // even if an additional quote should be added. Since strings are by default terminated it should be rare.
+      if (type == DartTokenTypes.SHORT_TEMPLATE_ENTRY) {
+        token = psiAtOffset.getNode().getTreePrev();
+        if (token == null) return Result.Continue;
+        if (token.getElementType() == DartTokenTypes.OPEN_QUOTE) {
+          String quote = token.getText().trim();
+          if (quote.length() == 1) {
+            breakString(quote, quote, caretOffsetRef, caretAdvanceRef, document);
+            return Result.Default;
+          }
+        }
+      }
     }
     if (type == DartTokenTypes.CLOSING_QUOTE) {
-      // The final effect is to insert matching close-quote, newline, indent, matching open-quote.
-      char literalStart = token.getText().charAt(0);
-      StringBuilder quotes = new StringBuilder();
-      quotes.append(literalStart);
-      quotes.append(literalStart);
-      document.insertString(caretOffset, quotes.toString());
-      caretOffset++;
-      caretAdvance++;
-      caretOffsetRef.set(caretOffset);
-      caretAdvanceRef.set(caretAdvance);
-      return Result.Default;
+      String quote = token.getText().trim();
+      if (quote.length() == 1) {
+        breakString(quote, quote, caretOffsetRef, caretAdvanceRef, document);
+        return Result.Default;
+      }
     }
     return Result.Continue;
+  }
+
+  private static void breakString(@NotNull final String startQuote,
+                                  @NotNull final String endQuote,
+                                  @NotNull final Ref<Integer> caretOffsetRef,
+                                  @NotNull final Ref<Integer> caretAdvanceRef,
+                                  @NotNull final Document document) {
+    // The final effect is to insert matching close-quote, newline, indent, matching open-quote.
+    int caretOffset = caretOffsetRef.get().intValue();
+    int caretAdvance = caretAdvanceRef.get().intValue();
+    document.insertString(caretOffset, endQuote + startQuote);
+    caretOffset += endQuote.length();
+    caretAdvance += startQuote.length();
+    caretOffsetRef.set(caretOffset);
+    caretAdvanceRef.set(caretAdvance);
   }
 }
