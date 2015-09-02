ceb523502d14d539551b82cbbf16a75a5486fcc7
Sergey.Karashevich <karashevich@gmail.com>
2015-09-02 19:35:18 +0300

    small alignment fix
    GitOrigin-RevId: ee687de9ff5eede555f75a896f713c4dc344e973

--- ide-features-trainer/src/org/jetbrains/training/graphics/TransparentPanel.java
+++ ide-features-trainer/src/org/jetbrains/training/graphics/TransparentPanel.java
@@ -1,6 +1,9 @@
 package org.jetbrains.training.graphics;
 
+import org.jetbrains.training.util.MyClassLoader;
+
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -32,47 +35,17 @@ public class TransparentPanel extends JFrame
             // Set some layout, say FlowLayout
             setLayout(new FlowLayout());
 
+            Image image = MyClassLoader.getInstance().getImageResourceAsStream("checkmark.png");  //this generates an image file
+            ImageIcon checkmarkIcon = new ImageIcon(image);
+
+
             // Create a JPanel
-            p1=new DetailPanel(new Dimension(500,60));
-
-            //Label settings
-            JLabel myLabel = new JLabel();
-            myLabel.setForeground(new Color(245, 245, 245, 255));
-            Font font = myLabel.getFont();
-            Font newFont = new Font(font.getName(), font.getStyle(), 18);
-            myLabel.setFont(newFont);
-
-            p1.setLayout(new GridBagLayout());
-            p1.add(myLabel, new GridBagConstraints());
-            p1.addMouseListener(new MouseListener() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    p1.setVisible(false);
-                }
-
-                @Override
-                public void mousePressed(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseReleased(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseEntered(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-
-                }
-            });
-
-            myLabel.setText("Welcome to IntelliJ IDEA");
-
+            JPanel p1 = new JPanel();
+            JLabel jLabel = new JLabel("Test JLabel");
+            jLabel.setIcon(checkmarkIcon);
+            jLabel.setHorizontalTextPosition(SwingConstants.LEFT);
+            jLabel.setBorder(new LineBorder(Color.BLACK, 1));
+            p1.add(jLabel);
 
             // Add the panels to the JFrame
             add(p1);
