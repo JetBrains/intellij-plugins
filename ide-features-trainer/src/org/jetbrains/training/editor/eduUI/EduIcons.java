d99bfdf340f4460dad456534c1f05ee2c7a2d9ff
Sergey Karashevich <karashevich@gmail.com>
2015-09-07 19:46:23 +0300

    checkmarks in text
    GitOrigin-RevId: 157a0dc20463c32e820cf33fcecf010183afb2e7

--- ide-features-trainer/src/org/jetbrains/training/editor/eduUI/EduIcons.java
+++ ide-features-trainer/src/org/jetbrains/training/editor/eduUI/EduIcons.java
@@ -7,4 +7,5 @@ public interface EduIcons {
     public static String CHECKMARK_GRAY = "/img/checkmarkGray.png";
     public static String CHECKMARK_BLUE = "/img/checkmarkBlue.png";
     public static String CHECKMARK_GREEN = "/img/checkmarkGreen.png";
+    public static String CHECKMARK_GRAY_12 = "/img/checkmark12.png";
 }
