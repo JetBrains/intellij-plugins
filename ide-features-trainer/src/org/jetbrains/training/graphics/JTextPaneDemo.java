81e52488c4ed961791b17c4e95d53fdec509b10b
Sergey Karashevich <karashevich@gmail.com>
2015-09-08 14:51:27 +0300

    checkmark task fix
    GitOrigin-RevId: 17534cae157d26945c02ac442d8b592ce115ac63

--- ide-features-trainer/src/org/jetbrains/training/graphics/JTextPaneDemo.java
+++ ide-features-trainer/src/org/jetbrains/training/graphics/JTextPaneDemo.java
@@ -3,16 +3,17 @@ package org.jetbrains.training.graphics;
 /**
  * Created by Sergey.Karashevich on 9/1/2015.
  */
+import com.intellij.openapi.util.IconLoader;
+import org.jetbrains.training.editor.eduUI.EduIcons;
 import org.jetbrains.training.util.MyClassLoader;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.swing.*;
-import javax.swing.text.AttributeSet;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.text.*;
 
 public class JTextPaneDemo extends JFrame {
 
@@ -78,10 +79,25 @@ public class JTextPaneDemo extends JFrame {
 
         textPane.setEditable(false);
 
+        StyleContext context = new StyleContext();
+        StyledDocument document = textPane.getStyledDocument();
+        Style labelStyle = context.getStyle(StyleContext.DEFAULT_STYLE);
+        final BufferedImage imageResourceAsStream = MyClassLoader.getInstance().getImageResourceAsStream("checkmark12.png");
+        Icon icon = new ImageIcon(imageResourceAsStream);
+        JLabel jlabel = new JLabel(icon);
+        jlabel.setAlignmentY(0.8f);
+        StyleConstants.setComponent(labelStyle, jlabel);
+
+
         textPane.setParagraphAttributes(PARAGRAPH_STYLE, true);
         insertText("\n<html>Put cursor before any word. Select it with </html>", REGULAR);
-        insertText("Meta + Up", BOLD);
+        insertText("Meta + Up ", BOLD);
         setEndSelection();
+        try {
+            document.insertString(document.getLength(), " try this model", labelStyle);
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
         insertText("\nNow let's select the whole line with ", REGULAR);
         insertText("Meta + Alt + Up", BOLD);
         setEndSelection();
