45a2f48a3c9d66099ebd823a4c771012a1edc89e
Sergey Karashevich <karashevich@gmail.com>
2015-09-10 18:37:29 +0300

    state to the CourseManager
    GitOrigin-RevId: 9b6310c9f46a8992eb1b4a549e0d281fc0257b3a

--- ide-features-trainer/src/training/lesson/CourseManager.java
+++ ide-features-trainer/src/training/lesson/CourseManager.java
@@ -4,6 +4,10 @@ import com.intellij.ide.scratch.ScratchFileService;
 import com.intellij.ide.scratch.ScratchRootType;
 import com.intellij.lang.Language;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
 import com.intellij.openapi.fileEditor.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.*;
@@ -27,6 +31,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
@@ -34,13 +39,18 @@ import java.util.concurrent.ExecutionException;
 /**
  * Created by karashevich on 11/03/15.
  */
-public class CourseManager{
+@State(
+        name="TRAINING_PLUGIN_COURSES",
+        storages={
+                @Storage(id="other", file = StoragePathMacros.APP_CONFIG + "/training_plugin_courses.xml")
+        }
+)
+public class CourseManager implements PersistentStateComponent<CourseManager.State>{
 
     public static final CourseManager INSTANCE = new CourseManager();
-    public static final Dimension DIMENSION = new Dimension(500, 60);
 
-    public ArrayList<Course> courses;
     private HashMap<Course, VirtualFile> mapCourseVirtualFile;
+    private State myState;
 
 
     public static CourseManager getInstance(){
@@ -49,9 +59,9 @@ public class CourseManager{
 
     public CourseManager() {
         //init courses; init default course by default
-        courses = new ArrayList<Course>();
-        mapCourseVirtualFile = new HashMap<Course, VirtualFile>();
 
+        mapCourseVirtualFile = new HashMap<Course, VirtualFile>();
+        myState = new State();
 
         try {
 //            final Course defaultCourse = Course.initCourse("EditorBasics.xml");
@@ -76,21 +86,17 @@ public class CourseManager{
             if(element.getName().equals(GenerateCourseXml.COURSE_TYPE_ATTR)) {
                 String courseFilename = element.getAttribute(GenerateCourseXml.COURSE_NAME_ATTR).getValue();
                 final Course course = Course.initCourse(courseFilename);
-                courses.add(course);
+                myState.addCourse(course);
             }
         }
     }
 
 
-    @Nullable
-    public Course getAnyCourse(){
-        if(courses == null || courses.size() == 0) return null;
-        return courses.get(0);
-    }
 
     @Nullable
     public Course getCourseById(String id){
-        if(courses == null || courses.size() == 0) return null;
+        final Course[] courses = myState.getCourses();
+        if(courses == null || courses.length == 0) return null;
 
         for(Course course: courses){
             if(course.getId().toUpperCase().equals(id.toUpperCase())) return course;
@@ -276,6 +282,32 @@ public class CourseManager{
 //        final SdkProblemDialog dialog = new SdkProblemDialog(project, "at least JDK 1.6 or IDEA SDK with corresponding JDK");
         final SdkProblemDialog dialog = new SdkProblemDialog(project, sdkMessage);
         dialog.show();
+    }
+
+    class State {
+        public ArrayList<Course> courses;
+
+        public State() {
+            courses = new ArrayList<Course>();
+        }
+
+        public void addCourse(Course course){
+            courses.add(course);
+        }
+
+        @Nullable
+        public Course[] getCourses(){
+            if (courses == null) return null;
+            return courses.toArray(new Course[courses.size()]);
+        }
+    }
+
+    public State getState(){
+        return myState;
+    }
+
+    public void loadState(State state){
+
     }
 
 }
