f5490c76d9069491d468f7b722513933a71244d7
Sergey Karashevich <karashevich@gmail.com>
2015-09-10 16:24:50 +0300

    checkmarks have been added to Menu Items
    GitOrigin-RevId: 67752bb9d7d00e61e4d403a43821e9aa8340f314

--- ide-features-trainer/src/training/editor/eduUI/EduIcons.java
+++ ide-features-trainer/src/training/editor/eduUI/EduIcons.java
@@ -4,8 +4,9 @@ package training.editor.eduUI;
  * Created by karashevich on 03/09/15.
  */
 public interface EduIcons {
-    public static String CHECKMARK_GRAY = "/img/checkmarkGray.png";
-    public static String CHECKMARK_BLUE = "/img/checkmarkBlue.png";
-    public static String CHECKMARK_GREEN = "/img/checkmarkGreen.png";
-    public static String CHECKMARK_GRAY_12 = "/img/checkmark12.png";
+    String CHECKMARK_GRAY = "/img/checkmarkGray.png";
+    String CHECKMARK_DARK_GRAY = "/img/checkmarkDarkGray.png";
+    String CHECKMARK_BLUE = "/img/checkmarkBlue.png";
+    String CHECKMARK_GREEN = "/img/checkmarkGreen.png";
+    String CHECKMARK_GRAY_12 = "/img/checkmark12.png";
 }
