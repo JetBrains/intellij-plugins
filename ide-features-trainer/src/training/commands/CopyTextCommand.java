95e1e69ca9756e466d279444a113111acfe799f4
Sergey Karashevich <karashevich@gmail.com>
2015-09-17 18:16:45 +0300

    nonundoable action fix
    GitOrigin-RevId: 36491db40190e6c8c2bb4cb7b8f1f29be9dbd626

--- ide-features-trainer/src/training/commands/CopyTextCommand.java
+++ ide-features-trainer/src/training/commands/CopyTextCommand.java
@@ -1,7 +1,14 @@
 package training.commands;
 
 import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.command.undo.BasicUndoableAction;
+import com.intellij.openapi.command.undo.DocumentReference;
+import com.intellij.openapi.command.undo.DocumentReferenceManager;
+import com.intellij.openapi.command.undo.UndoManager;
+import com.intellij.psi.PsiDocumentManager;
 import com.intellij.util.DocumentUtil;
 import org.jdom.Element;
 
@@ -21,13 +28,26 @@ public class CopyTextCommand extends Command {
 //        updateDescription(element, infoPanel, editor);
 
         final String finalText = (element.getContent().isEmpty() ? "" : element.getContent().get(0).getValue());
-        DocumentUtil.writeInRunUndoTransparentAction(new Runnable() {
+        WriteCommandAction.runWriteCommandAction(executionList.getProject(), new Runnable() {
             @Override
             public void run() {
+                final DocumentReference documentReference = DocumentReferenceManager.getInstance().create(executionList.getEditor().getDocument());
+                UndoManager.getInstance(executionList.getProject()).nonundoableActionPerformed(documentReference, false);
                 executionList.getEditor().getDocument().insertString(0, finalText);
+                PsiDocumentManager.getInstance(executionList.getProject()).commitDocument(executionList.getEditor().getDocument());
 
+                UndoManager.getInstance(executionList.getProject()).undoableActionPerformed(new BasicUndoableAction() {
+                    @Override
+                    public void undo() {
+                    }
+
+                    @Override
+                    public void redo() {
+                    }
+                });
             }
         });
+
 
         startNextCommand(executionList);
 
