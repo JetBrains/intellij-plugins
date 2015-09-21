2f03ae9c081853988b271a0e1232a99263349f65
Sergey Karashevich <karashevich@gmail.com>
2015-09-21 16:02:16 +0300

    Extract method has been added
    GitOrigin-RevId: 8962fab24683f0db38ca5f798f7065bf2f9d915c

--- ide-features-trainer/src/training/util/Test.java
+++ ide-features-trainer/src/training/util/Test.java
@@ -1,54 +1,30 @@
-package training.util;
-
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseListener;
 
-/**
- * Created by karashevich on 30/07/15.
- */
-public class Test
-{
-    private static boolean isBlocked = false;
-    private static MouseListener ml;
+public class Test{
+    private static JButton glassButton;
+    private static JPanel glass;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+
+        final Dimension myDimension = new Dimension(100, 100);
+
         JButton button = new JButton("Test");
-        button.setPreferredSize(new Dimension(100, 100));
+        button.setPreferredSize(myDimension);
 
-        final JButton glassButton = new JButton("Block");
-
+        glassButton = new JButton("Block");
         JPanel panel = new JPanel();
         panel.add(button);
 
-        final JPanel glass = new JPanel();
+        glass = new JPanel();
         glass.setOpaque(false);
         glass.add(glassButton);
 
-        final JFrame frame = new JFrame();
+        JFrame frame = new JFrame();
         frame.setGlassPane(glass);
         glass.setVisible(true);
         frame.setContentPane(panel);
         frame.pack();
         frame.setVisible(true);
-
-        glassButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent ae) {
-                if (isBlocked) {
-                    glass.removeMouseListener(ml);
-                    glassButton.setText("Block");
-                } else {
-                    ml = new MouseAdapter() { };
-                    glass.addMouseListener(ml);
-                    glassButton.setText("Unblock");
-                }
-
-                isBlocked = !isBlocked;
-            }
-        });
     }
 }
