dcdecd0ac4a7bca47237405ea2f6469eedb7e788
Dennis Ushakov <dennis.ushakov@gmail.com>
2015-09-22 15:28:53 +0300

    EA-72180

--- AngularJS/src/org/angularjs/codeInsight/attributes/AngularAttributesRegistry.java
+++ AngularJS/src/org/angularjs/codeInsight/attributes/AngularAttributesRegistry.java
@@ -45,7 +45,7 @@ public class AngularAttributesRegistry {
     final JSImplicitElement directive = AngularIndexUtil.resolve(parent.getProject(), AngularDirectivesDocIndex.KEY, attributeName);
     if (directive != null) {
       final String restrict = directive.getTypeString();
-      final String type = restrict.split(";", -1)[2];
+      final String type = restrict != null ? restrict.split(";", -1)[2] : "";
       return type.contains("object literal") || type.equals("mixed");
     }
     return false;
