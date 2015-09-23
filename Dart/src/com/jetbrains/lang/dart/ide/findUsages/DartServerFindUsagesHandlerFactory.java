c1fa1c287a1146e58e6507519bf22112203d49c0
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-09-23 19:38:45 +0300

    fix DartServerFindUsagesHandlerFactory.canFindUsages

--- Dart/src/com/jetbrains/lang/dart/ide/findUsages/DartServerFindUsagesHandlerFactory.java
+++ Dart/src/com/jetbrains/lang/dart/ide/findUsages/DartServerFindUsagesHandlerFactory.java
@@ -18,9 +18,7 @@ package com.jetbrains.lang.dart.ide.findUsages;
 import com.intellij.find.findUsages.FindUsagesHandler;
 import com.intellij.find.findUsages.FindUsagesHandlerFactory;
 import com.intellij.psi.PsiElement;
-import com.jetbrains.lang.dart.psi.DartLibraryNameElement;
-import com.jetbrains.lang.dart.psi.DartNamedElement;
-import com.jetbrains.lang.dart.psi.DartUriElement;
+import com.jetbrains.lang.dart.DartLanguage;
 import com.jetbrains.lang.dart.resolve.DartResolver;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -29,7 +27,7 @@ public class DartServerFindUsagesHandlerFactory extends FindUsagesHandlerFactory
   @Override
   public boolean canFindUsages(@NotNull PsiElement element) {
     if (DartResolver.isServerDrivenResolution()) {
-      return element instanceof DartNamedElement || element instanceof DartUriElement || element instanceof DartLibraryNameElement;
+      return element.getLanguage() == DartLanguage.INSTANCE;
     }
     return false;
   }
