49bd54cd6aca29419ae6a96d08d8dbd6eb0cfa1a
Steve Messick <messick@google.com>
2015-09-28 07:56:25 -0700

    Testing with package:test

---
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/test/DartTestRunnerParameters.java
@@ -0,0 +1,50 @@
+package com.jetbrains.lang.dart.ide.runner.test;
+
+import com.jetbrains.lang.dart.ide.runner.server.DartCommandLineRunnerParameters;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class DartTestRunnerParameters extends DartCommandLineRunnerParameters implements Cloneable {
+
+  public enum Scope {
+    ALL("All in file"), GROUP("Test group"), METHOD("Single test");
+
+    private final String myPresentableName;
+
+    Scope(final String name) {
+      myPresentableName = name;
+    }
+
+    public String getPresentableName() {
+      return myPresentableName;
+    }
+  }
+
+  private @NotNull Scope myScope = Scope.ALL;
+  private @Nullable String myTestName = null;
+
+  @NotNull
+  public Scope getScope() {
+    return myScope;
+  }
+
+  public void setScope(@SuppressWarnings("NullableProblems") final Scope scope) {
+    if (scope != null) { // null in case of corrupted storage
+      myScope = scope;
+    }
+  }
+
+  @Nullable
+  public String getTestName() {
+    return myTestName;
+  }
+
+  public void setTestName(final @Nullable String name) {
+    myTestName = name;
+  }
+
+  @Override
+  protected final DartTestRunnerParameters clone() {
+    return (DartTestRunnerParameters)super.clone();
+  }
+}
