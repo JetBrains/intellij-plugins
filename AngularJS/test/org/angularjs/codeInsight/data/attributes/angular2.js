a31fcd79915c842b33134e16e6b0cd7a88a02797
Dennis Ushakov <dennis.ushakov@gmail.com>
2015-10-06 17:05:39 +0300

    fix angular 2 detection: ng-non-bindable directive is gone, use ng-for now (cherry picked from commit d0f174d)

--- AngularJS/test/org/angularjs/codeInsight/data/attributes/angular2.js
+++ AngularJS/test/org/angularjs/codeInsight/data/attributes/angular2.js
@@ -1,10 +1,3 @@
-@Directive({
-    selector: '[ng-non-bindable]',
-    compileChildren: false
-})
-export class NonBindable {
-}
-
 @Directive({selector: '[ng-for][ng-for-of]', properties: ['ngForOf'], lifecycle: [onCheck]})
 export class NgFor {
 }
