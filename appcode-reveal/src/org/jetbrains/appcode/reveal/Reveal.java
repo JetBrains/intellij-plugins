f933ba83fd2a3bcbd3707683d5563d33a8282dec
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2015-10-14 18:25:18 +0200

    OC-12524 Reveal integration issue: "Cannot refresh Reveal. Cannot find AppleScript engine"

--- appcode-reveal/src/org/jetbrains/appcode/reveal/Reveal.java
+++ appcode-reveal/src/org/jetbrains/appcode/reveal/Reveal.java
@@ -12,8 +12,8 @@ import com.jetbrains.cidr.xcode.plist.Plist;
 import com.jetbrains.cidr.xcode.plist.PlistDriver;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.ide.script.IdeScriptException;
 
-import javax.script.ScriptException;
 import java.io.File;
 import java.util.List;
 
@@ -127,7 +127,7 @@ public class Reveal {
               true
       );
     }
-    catch (ScriptException e) {
+    catch (IdeScriptException e) {
       LOG.info("Reveal script failed:\n" + script);
       throw new ExecutionException("Cannot refresh Reveal: " + e.getMessage(), e);
     }
