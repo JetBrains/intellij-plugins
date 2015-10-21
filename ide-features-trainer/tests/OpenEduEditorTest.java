9dd42bf0eb3f16c834cfd627bc9759b8e114157e
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2015-10-21 19:15:47 +0300

    first Test for EduEditor
    GitOrigin-RevId: 4532f23cc556e051e6031506e5edce76a962caec

--- ide-features-trainer/tests/OpenEduEditorTest.java
+++ ide-features-trainer/tests/OpenEduEditorTest.java
@@ -1,9 +1,9 @@
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.LightPlatformTestCase;
-import com.intellij.testFramework.PlatformTestUtil;
-import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
 import training.editor.EduEditor;
 import training.lesson.CourseManager;
 
@@ -11,27 +11,21 @@ import training.lesson.CourseManager;
  * Created by karashevich on 16/10/15.
  */
 
-
-
 public class OpenEduEditorTest extends LightPlatformTestCase {
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
 
 
-    public void testSomething(){
-        assertEquals(true, true);
-    }
-
-    public void testOpenEduEditor(){
+    public void testOpenEduEditor() throws Exception {
         final Project project = LightPlatformTestCase.getProject();
         final PsiFile file = LightPlatformTestCase.createFile("Test.java", "");
         final VirtualFile vf = file.getVirtualFile();
 
-        final EduEditor eduEditor = CourseManager.getInstance().getEduEditor(project, vf);
-        assertEquals(eduEditor != null, true);
+        EduEditor eduEditor = null;
+        try {
+            eduEditor = CourseManager.getInstance().getEduEditor(project, vf);
+            assertNotNull(eduEditor);
+        } finally {
+            Disposer.dispose(eduEditor);
+        }
     }
-
 }
