9843e636a219efe33181f8659574bbcfa71e8d90
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2015-10-21 18:18:01 +0300

    DSGN-2410: Icons for markdown editor toolbar

--- markdown/gen/icons/MarkdownIcons.java
+++ markdown/gen/icons/MarkdownIcons.java
@@ -13,8 +13,23 @@ public class MarkdownIcons {
     return IconLoader.getIcon(path, MarkdownIcons.class);
   }
 
-  public static final Icon Bold = load("/icons/Bold.png"); // 16x16
-  public static final Icon Code = load("/icons/Code.png"); // 16x16
-  public static final Icon Italic = load("/icons/Italic.png"); // 16x16
+  
+  public static class EditorActions {
+    public static final Icon Bold = load("/icons/editor_actions/Bold.png"); // 16x16
+    public static final Icon Code_span = load("/icons/editor_actions/Code_span.png"); // 16x16
+    public static final Icon Header_level_down = load("/icons/editor_actions/Header_level_down.png"); // 16x16
+    public static final Icon Header_level_up = load("/icons/editor_actions/Header_level_up.png"); // 16x16
+    public static final Icon Italic = load("/icons/editor_actions/Italic.png"); // 16x16
+    public static final Icon Link = load("/icons/editor_actions/Link.png"); // 16x16
+    public static final Icon Strike_through = load("/icons/editor_actions/Strike_through.png"); // 16x16
+
+  }
+  
+  public static class Layout {
+    public static final Icon Editor_only = load("/icons/layout/Editor_only.png"); // 16x16
+    public static final Icon Editor_preview = load("/icons/layout/Editor_preview.png"); // 16x16
+    public static final Icon Preview_only = load("/icons/layout/Preview_only.png"); // 16x16
+
+  }
   public static final Icon MarkdownPlugin = load("/icons/MarkdownPlugin.png"); // 16x16
 }
