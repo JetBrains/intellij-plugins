d3195a2e5fa896eef34a4f6057117729ee9ac091
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2015-10-28 17:51:31 +0300

    New lesson test implemented
    GitOrigin-RevId: a925f2f145228569e6e0846e08fbf3c0fcb7e739

--- ide-features-trainer/src/training/editor/eduUI/DemoModeUI.java
+++ ide-features-trainer/src/training/editor/eduUI/DemoModeUI.java
@@ -1,26 +1,13 @@
 package training.editor.eduUI;
 
-import com.intellij.openapi.editor.EditorFactory;
-import com.intellij.openapi.editor.event.EditorEventMulticaster;
-import com.intellij.openapi.editor.event.SelectionEvent;
-import com.intellij.openapi.editor.event.SelectionListener;
-import com.intellij.openapi.fileEditor.FileEditor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.StatusBar;
-import com.intellij.openapi.wm.StatusBarWidget;
 import com.intellij.openapi.wm.ex.WindowManagerEx;
 import com.intellij.openapi.wm.impl.IdeFrameImpl;
-import com.intellij.openapi.wm.impl.status.EditorBasedWidget;
 import com.intellij.ui.JBColor;
-import com.intellij.util.Consumer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import training.editor.EduEditor;
-import training.lesson.EducationBundle;
 
 import java.awt.*;
-import java.awt.event.MouseEvent;
 
 /**
  * Created by karashevich on 26/10/15.
@@ -29,12 +16,16 @@ public class DemoModeUI {
 
     final private static JBColor demoCurtainColor = new JBColor(new Color(42, 42, 42, 26), new Color(128, 128, 128, 26));
     private DemoModeWidget demoModeWidget = null;
+    private Project myProject = null;
 
     public static JBColor getDemoCurtainColor() {
         return demoCurtainColor;
     }
 
     public void addDemoModeWidget(Project project, EduEditor eduEditor) {
+
+        myProject = project;
+
         final IdeFrameImpl frame = WindowManagerEx.getInstanceEx().getFrame(project);
         final StatusBar statusBar = frame.getStatusBar();
 
@@ -45,12 +36,28 @@ public class DemoModeUI {
                 if (demoModeWidget == null) {
                     demoModeWidget = new DemoModeWidget(project);
                     statusBar.addWidget(demoModeWidget, "before Position", eduEditor);
-                    statusBar.updateWidget(demoModeWidget.ID());
                 } else {
                     statusBar.addWidget(demoModeWidget, "before Position", eduEditor);
-                    statusBar.updateWidget(demoModeWidget.ID());
                 }
             }
+            statusBar.updateWidget(demoModeWidget.ID());
         }
+    }
+
+    public void updateDemoModeWidget() {
+        if (myProject == null) return;
+
+        final IdeFrameImpl frame = WindowManagerEx.getInstanceEx().getFrame(myProject);
+        final StatusBar statusBar = frame.getStatusBar();
+
+        statusBar.updateWidget(demoModeWidget.ID());
+    }
+
+    public void removeDemoModeWidget() {
+        final IdeFrameImpl frame = WindowManagerEx.getInstanceEx().getFrame(myProject);
+        final StatusBar statusBar = frame.getStatusBar();
+
+        statusBar.removeWidget(demoModeWidget.ID());
+        demoModeWidget = null;
     }
 }
