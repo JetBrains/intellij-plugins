7e70aa7850f882017dbf71e6294d5148d07b7c76
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-10-29 17:07:54 +0300

    WEB-18674 Dart. Unused elements are not highlighted.

--- Dart/testData/analysisServer/highlighting/ErrorsHighlighting.dart
+++ Dart/testData/analysisServer/highlighting/ErrorsHighlighting.dart
@@ -1,20 +1,20 @@
-import <weak_warning descr="Unused import" type="UNUSED_SYMBOL">"dart:html"</weak_warning>;
+import <warning descr="Unused import" type="UNUSED_SYMBOL">"dart:html"</warning>;
 import "dart:core";
-import <weak_warning descr="Duplicate import" type="UNUSED_SYMBOL">"dart:core"</weak_warning>;
+import <warning descr="Duplicate import" type="UNUSED_SYMBOL">"dart:core"</warning>;
 import <error descr="Target of URI does not exist: 'dart:incorrect'" type="ERROR">"dart:incorrect"</error>;
 
 main() {
   <warning descr="The function 'foo' is not defined" type="WARNING">foo</warning>();
-  <weak_warning descr="'bar' is deprecated" type="DEPRECATED">bar</weak_warning>();
+  <warning descr="'bar' is deprecated" type="DEPRECATED">bar</warning>();
   // todo smth
-  var <weak_warning descr="The value of the local variable 'unused' is not used" type="UNUSED_SYMBOL">unused</weak_warning>;
+  var <warning descr="The value of the local variable 'unused' is not used" type="UNUSED_SYMBOL">unused</warning>;
   return;
-  <weak_warning descr="Dead code" type="UNUSED_SYMBOL">1 + 1;</weak_warning>
+  <warning descr="Dead code" type="UNUSED_SYMBOL">1 + 1;</warning>
 }
 
 @deprecated
 <weak_warning descr="This function declares a return type of 'int', but does not end with a return statement" type="WEAK_WARNING">int</weak_warning> bar(){}
 
-class <weak_warning descr="The class '_Foo' is not used" type="UNUSED_SYMBOL">_Foo</weak_warning> {
-  int <weak_warning descr="The value of the field '_unusedField' is not used" type="UNUSED_SYMBOL">_unusedField</weak_warning>;
+class <warning descr="The class '_Foo' is not used" type="UNUSED_SYMBOL">_Foo</warning> {
+  int <warning descr="The value of the field '_unusedField' is not used" type="UNUSED_SYMBOL">_unusedField</warning>;
 }
