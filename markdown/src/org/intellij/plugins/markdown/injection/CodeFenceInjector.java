84e3e95ef1e0491e6d1b7506137facd224a1cc99
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2015-10-30 19:13:59 +0300

    RUBY-17234: Try to guess fence language if info string is present

--- markdown/src/org/intellij/plugins/markdown/injection/CodeFenceInjector.java
+++ markdown/src/org/intellij/plugins/markdown/injection/CodeFenceInjector.java
@@ -1,5 +1,6 @@
 package org.intellij.plugins.markdown.injection;
 
+import com.intellij.lang.Language;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.openapi.util.TextRange;
@@ -7,9 +8,11 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.intellij.plugins.intelliLang.inject.InjectedLanguage;
 import org.intellij.plugins.intelliLang.inject.TemporaryPlacesRegistry;
+import org.intellij.plugins.markdown.lang.MarkdownTokenTypes;
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownCodeFenceContentImpl;
 import org.intellij.plugins.markdown.lang.psi.impl.MarkdownCodeFenceImpl;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,14 +23,16 @@ public class CodeFenceInjector implements MultiHostInjector {
     if (!(context instanceof MarkdownCodeFenceImpl)) {
       return;
     }
-
-    final TemporaryPlacesRegistry registry = TemporaryPlacesRegistry.getInstance(context.getProject());
-    final InjectedLanguage language = registry.getLanguageFor(((MarkdownCodeFenceImpl)context), context.getContainingFile());
-    if (language == null || language.getLanguage() == null) {
+    if (PsiTreeUtil.findChildOfType(context, MarkdownCodeFenceContentImpl.class) == null) {
       return;
     }
 
-    registrar.startInjecting(language.getLanguage());
+    final Language language = findLangForInjection(((MarkdownCodeFenceImpl)context));
+    if (language == null) {
+      return;
+    }
+
+    registrar.startInjecting(language);
     boolean isFirst = true;
     for (MarkdownCodeFenceContentImpl content : PsiTreeUtil.findChildrenOfType(context, MarkdownCodeFenceContentImpl.class)) {
       registrar.addPlace(isFirst ? null : "\n",
@@ -43,5 +48,25 @@ public class CodeFenceInjector implements MultiHostInjector {
   @Override
   public List<? extends Class<? extends PsiElement>> elementsToInjectIn() {
     return Collections.singletonList(MarkdownCodeFenceImpl.class);
+  }
+
+  @Nullable
+  private static Language findLangForInjection(@NotNull MarkdownCodeFenceImpl element) {
+    final TemporaryPlacesRegistry registry = TemporaryPlacesRegistry.getInstance(element.getProject());
+    final InjectedLanguage language = registry.getLanguageFor(element, element.getContainingFile());
+    if (language != null) {
+      return language.getLanguage();
+    }
+
+    final PsiElement fenceLangElement = element.findPsiChildByType(MarkdownTokenTypes.FENCE_LANG);
+    if (fenceLangElement == null) {
+      return null;
+    }
+    return guessLanguageByFenceLang(fenceLangElement.getText());
+  }
+
+  @Nullable
+  private static Language guessLanguageByFenceLang(@NotNull String langName) {
+    return LanguageGuesser.INSTANCE.guessLanguage(langName);
   }
 }
---
+++ markdown/src/org/intellij/plugins/markdown/injection/LanguageGuesser.java
@@ -0,0 +1,48 @@
+package org.intellij.plugins.markdown.injection;
+
+import com.intellij.lang.Language;
+import com.intellij.lexer.EmbeddedTokenTypesProvider;
+import com.intellij.openapi.extensions.Extensions;
+import com.intellij.openapi.util.NotNullLazyValue;
+import com.intellij.util.containers.HashMap;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+enum LanguageGuesser {
+  INSTANCE;
+
+  private NotNullLazyValue<List<EmbeddedTokenTypesProvider>> embeddedTokenTypeProviders = new NotNullLazyValue<List<EmbeddedTokenTypesProvider>>() {
+    @NotNull
+    @Override
+    protected List<EmbeddedTokenTypesProvider> compute() {
+      return Arrays.asList(Extensions.getExtensions(EmbeddedTokenTypesProvider.EXTENSION_POINT_NAME));
+    }
+  };
+
+  private NotNullLazyValue<Map<String, Language>> langIdToLanguage = new NotNullLazyValue<Map<String, Language>>() {
+    @NotNull
+    @Override
+    protected Map<String, Language> compute() {
+      final HashMap<String, Language> result = new HashMap<String, Language>();
+      for (Language language : Language.getRegisteredLanguages()) {
+        result.put(language.getID().toLowerCase(Locale.US), language);
+      }
+      return result;
+    }
+  };
+
+  @Nullable
+  public Language guessLanguage(@NotNull String languageName) {
+    for (EmbeddedTokenTypesProvider provider : embeddedTokenTypeProviders.getValue()) {
+      if (provider.getName().equalsIgnoreCase(languageName)) {
+        return provider.getElementType().getLanguage();
+      }
+    }
+    return langIdToLanguage.getValue().get(languageName.toLowerCase(Locale.US));
+  }
+}
