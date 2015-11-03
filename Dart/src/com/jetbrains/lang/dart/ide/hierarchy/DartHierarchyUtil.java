f3288c1604c862dc27e8f4c2bcd4e81f80288473
Konstantin Shcheglov <scheglov@google.com>
2015-11-03 13:58:50 -0800

    Use DAS for type hierarchy.

--- Dart/src/com/jetbrains/lang/dart/ide/hierarchy/DartHierarchyUtil.java
+++ Dart/src/com/jetbrains/lang/dart/ide/hierarchy/DartHierarchyUtil.java
@@ -4,8 +4,19 @@ import com.intellij.ide.hierarchy.HierarchyBrowserManager;
 import com.intellij.ide.util.treeView.AlphaComparator;
 import com.intellij.ide.util.treeView.NodeDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.ide.marker.DartServerOverrideMarkerProvider;
+import com.jetbrains.lang.dart.psi.DartClass;
+import com.jetbrains.lang.dart.psi.DartComponent;
+import org.dartlang.analysis.server.protocol.Element;
+import org.dartlang.analysis.server.protocol.Location;
+import org.dartlang.analysis.server.protocol.TypeHierarchyItem;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Comparator;
+import java.util.List;
 
 public class DartHierarchyUtil {
   private static final Comparator<NodeDescriptor> NODE_DESCRIPTOR_COMPARATOR = new Comparator<NodeDescriptor>() {
@@ -17,6 +28,14 @@ public class DartHierarchyUtil {
   private DartHierarchyUtil() {
   }
 
+  @Nullable
+  public static DartClass findDartClass(@NotNull final Project project, @NotNull final TypeHierarchyItem item) {
+    final Element classElement = item.getClassElement();
+    final Location location = classElement.getLocation();
+    final DartComponent component = DartServerOverrideMarkerProvider.findDartComponent(project, location);
+    return component instanceof DartClass ? (DartClass)component : null;
+  }
+
   public static Comparator<NodeDescriptor> getComparator(Project project) {
     if (HierarchyBrowserManager.getInstance(project).getState().SORT_ALPHABETICALLY) {
       return AlphaComparator.INSTANCE;
@@ -24,5 +43,12 @@ public class DartHierarchyUtil {
     else {
       return NODE_DESCRIPTOR_COMPARATOR;
     }
+  }
+
+  @NotNull
+  public static List<TypeHierarchyItem> getTypeHierarchyItems(@NotNull DartClass dartClass) {
+    final VirtualFile file = dartClass.getContainingFile().getVirtualFile();
+    final int nameOffset = dartClass.getTextRange().getStartOffset();
+    return DartAnalysisServerService.getInstance().search_getTypeHierarchy(file, nameOffset, false);
   }
 }
---
+++ Dart/src/com/jetbrains/lang/dart/ide/hierarchy/type/DartServerSupertypesHierarchyTreeStructure.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2000-2015 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jetbrains.lang.dart.ide.hierarchy.type;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import com.intellij.ide.hierarchy.HierarchyNodeDescriptor;
+import com.intellij.ide.hierarchy.HierarchyTreeStructure;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.ArrayUtil;
+import com.jetbrains.lang.dart.psi.DartClass;
+import com.jetbrains.lang.dart.util.DartResolveUtil;
+import org.dartlang.analysis.server.protocol.TypeHierarchyItem;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.jetbrains.lang.dart.ide.hierarchy.DartHierarchyUtil.findDartClass;
+import static com.jetbrains.lang.dart.ide.hierarchy.DartHierarchyUtil.getTypeHierarchyItems;
+
+public final class DartServerSupertypesHierarchyTreeStructure extends HierarchyTreeStructure {
+
+  public DartServerSupertypesHierarchyTreeStructure(final Project project, final DartClass dartClass) {
+    super(project, new DartTypeHierarchyNodeDescriptor(project, null, dartClass, true));
+  }
+
+  @NotNull
+  protected final Object[] buildChildren(@NotNull final HierarchyNodeDescriptor descriptor) {
+    final DartClass dartClass = ((DartTypeHierarchyNodeDescriptor)descriptor).getDartClass();
+    if (dartClass == null) {
+      return ArrayUtil.EMPTY_OBJECT_ARRAY;
+    }
+
+    final List<TypeHierarchyItem> items = getTypeHierarchyItems(dartClass);
+    addSuperClassHierarchy(Sets.<TypeHierarchyItem>newHashSet(), myProject, items, items.get(0), descriptor);
+
+    return descriptor.getCachedChildren();
+  }
+
+  private static void addSuperClassHierarchy(@NotNull final Set<TypeHierarchyItem> stackItems,
+                                             @NotNull final Project project,
+                                             @NotNull final List<TypeHierarchyItem> items,
+                                             @NotNull final TypeHierarchyItem item,
+                                             @NotNull final HierarchyNodeDescriptor descriptor) {
+    if (!stackItems.add(item)) {
+      descriptor.setCachedChildren(ArrayUtil.EMPTY_OBJECT_ARRAY);
+      return;
+    }
+
+    List<HierarchyNodeDescriptor> superDescriptors = Lists.newArrayList();
+    try {
+      // superclass
+      final Integer superIndex = item.getSuperclass();
+      if (superIndex != null) {
+        addSuperClassNode(stackItems, project, items, descriptor, superDescriptors, superIndex);
+      }
+      // mixins
+      for (int index : item.getMixins()) {
+        addSuperClassNode(stackItems, project, items, descriptor, superDescriptors, index);
+      }
+      // interfaces
+      for (int index : item.getInterfaces()) {
+        addSuperClassNode(stackItems, project, items, descriptor, superDescriptors, index);
+      }
+    }
+    finally {
+      stackItems.remove(item);
+    }
+    descriptor.setCachedChildren(superDescriptors.toArray(new HierarchyNodeDescriptor[superDescriptors.size()]));
+  }
+
+  private static void addSuperClassNode(@NotNull final Set<TypeHierarchyItem> stackItems,
+                                        @NotNull final Project project,
+                                        @NotNull final List<TypeHierarchyItem> items,
+                                        @NotNull final HierarchyNodeDescriptor parentNode,
+                                        @NotNull final List<HierarchyNodeDescriptor> descriptors,
+                                        final int index) {
+    final TypeHierarchyItem superItem = items.get(index);
+    if (DartResolveUtil.OBJECT.equals(superItem.getClassElement().getName())) {
+      return;
+    }
+    final DartClass superClass = findDartClass(project, superItem);
+    final HierarchyNodeDescriptor superDescriptor = new DartTypeHierarchyNodeDescriptor(project, parentNode, superClass, false);
+    descriptors.add(superDescriptor);
+    addSuperClassHierarchy(stackItems, project, items, superItem, superDescriptor);
+  }
+}
