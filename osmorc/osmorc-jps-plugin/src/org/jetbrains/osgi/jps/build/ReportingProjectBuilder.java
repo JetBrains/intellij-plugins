7c18c34113243bff1a2d305a673ff6e7101ec9f0
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-11-09 16:09:06 +0100

    [osgi] contract violation in logging methods (IDEA-147285)

--- osmorc/osmorc-jps-plugin/src/org/jetbrains/osgi/jps/build/ReportingProjectBuilder.java
+++ osmorc/osmorc-jps-plugin/src/org/jetbrains/osgi/jps/build/ReportingProjectBuilder.java
@@ -30,19 +30,19 @@ public class ReportingProjectBuilder extends ProjectBuilder {
   @Override
   public SetLocation error(String format, Object... args) {
     myReporter.error(formatArrays(format, args), null, null);
-    return null;
+    return MuteLocation.INSTANCE;
   }
 
   @Override
   public SetLocation error(String format, Throwable t, Object... args) {
     myReporter.error(formatArrays(format, args), t, null);
-    return null;
+    return MuteLocation.INSTANCE;
   }
 
   @Override
   public SetLocation warning(String format, Object... args) {
     myReporter.warning(formatArrays(format, args), null, null);
-    return null;
+    return MuteLocation.INSTANCE;
   }
 
   @Override
