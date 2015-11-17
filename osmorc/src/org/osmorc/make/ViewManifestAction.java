52da61e2f8988bdee3d3596067dc7a525a316aeb
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2015-11-17 15:07:39 +0100

    [osgi] View Generated Manifest action fixed

--- osmorc/src/org/osmorc/make/ViewManifestAction.java
+++ osmorc/src/org/osmorc/make/ViewManifestAction.java
@@ -22,75 +22,57 @@
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package org.osmorc.make;
 
-import com.intellij.ide.IdeBundle;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.application.ApplicationNamesInfo;
-import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.OpenFileDescriptor;
-import com.intellij.openapi.fileEditor.ex.FileEditorProviderManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.JarFileSystem;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.osmorc.i18n.OsmorcBundle;
+
+import java.util.jar.JarFile;
 
 /**
  * Action which allows view the manifest of a given jar file. The action will open the manifest in a new editor tab.
  *
  * @author <a href="janthomae@janthomae.de">Jan Thom&auml;</a>
- * @version $Id:$
  */
 public class ViewManifestAction extends AnAction implements DumbAware {
-  private final String pathToJar;
+  private final String myJarFilePath;
 
-  public ViewManifestAction(String title, String pathToJar) {
-    this.pathToJar = pathToJar;
+  public ViewManifestAction(String title, String jarFilePath) {
+    myJarFilePath = jarFilePath;
     getTemplatePresentation().setText(title);
   }
 
   @Override
-  public void actionPerformed(AnActionEvent e) {
+  public void actionPerformed(@NotNull AnActionEvent e) {
+    Project project = e.getProject();
+    if (project == null) return;
 
-    @Nullable Project project = CommonDataKeys.PROJECT.getData(e.getDataContext());
-    if (project == null) {
-      return;
-    }
-
-    VirtualFile jarFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(pathToJar);
+    VirtualFile jarFile = LocalFileSystem.getInstance().refreshAndFindFileByPath(myJarFilePath);
     if (jarFile == null) {
-      Messages.showErrorDialog(project, "It seems that the bundle JAR does not exist. Please rebuild the project and try again.",
-                               "Cannot open manifest");
+      String message = OsmorcBundle.message("view.manifest.no.jar");
+      Notifications.Bus.notify(new Notification("OSGi", OsmorcBundle.message("view.manifest.title"), message, NotificationType.WARNING), project);
       return;
     }
 
-    // that seems a rather cumbersome way of opening a file... maybe there is a more efficient way to do this....
-    VirtualFile jarRoot = JarFileSystem.getInstance().getJarRootForLocalFile(jarFile);
-    if (jarRoot != null) {
-      final VirtualFile manifestFile = jarRoot.findFileByRelativePath("META-INF/MANIFEST.MF");
-
-      if (manifestFile == null) {
-        Messages.showErrorDialog(project, "There is no manifest in the bundle JAR. Please check the facet settings, rebuild and try again.",
-                                 "Cannot open manifest");
-        return;
-      }
-
-      FileEditorProviderManager editorProviderManager = FileEditorProviderManager.getInstance();
-      if (editorProviderManager.getProviders(project, manifestFile).length == 0) {
-        Messages.showMessageDialog(project, IdeBundle.message("error.files.of.this.type.cannot.be.opened",
-                                                              ApplicationNamesInfo.getInstance().getProductName()),
-                                   IdeBundle.message("title.cannot.open.file"), Messages.getErrorIcon());
-        return;
-      }
-
-      OpenFileDescriptor descriptor = new OpenFileDescriptor(project, manifestFile);
-      FileEditorManager.getInstance(project).openTextEditor(descriptor, true);
+    String manifestPath = jarFile.getPath() + JarFileSystem.JAR_SEPARATOR + JarFile.MANIFEST_NAME;
+    VirtualFile manifestFile = JarFileSystem.getInstance().refreshAndFindFileByPath(manifestPath);
+    if (manifestFile == null) {
+      String message = OsmorcBundle.message("view.manifest.missing");
+      Notifications.Bus.notify(new Notification("OSGi", OsmorcBundle.message("view.manifest.title"), message, NotificationType.WARNING), project);
+      return;
     }
+
+    new OpenFileDescriptor(project, manifestFile).navigate(true);
   }
 }
