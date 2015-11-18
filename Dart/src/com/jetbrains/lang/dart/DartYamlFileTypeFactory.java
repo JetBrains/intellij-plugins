1c1d508caa64e65978a99b6c569c71eb62b041e9
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-11-18 15:11:37 +0300

    register .analysis_options file as YAML

--- Dart/src/com/jetbrains/lang/dart/DartYamlFileTypeFactory.java
+++ Dart/src/com/jetbrains/lang/dart/DartYamlFileTypeFactory.java
@@ -1,13 +1,19 @@
 package com.jetbrains.lang.dart;
 
+import com.intellij.openapi.fileTypes.ExactFileNameMatcher;
+import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.yaml.YAMLFileType;
 
 public class DartYamlFileTypeFactory extends FileTypeFactory {
   @Override
   public void createFileTypes(@NotNull final FileTypeConsumer fileTypeConsumer) {
-    fileTypeConsumer.consume(YAMLFileType.YML, "analysis_options");
+    // Do not use YAMLFileType.YML directly to avoid class loaders conflict in IDEA Community + Dart Plugin project setup.
+    // The problem is that YAMLFileType is instantiated twice in such project setup: by PluginClassLoader and by UrlClassLoader
+    final FileType yamlFileType = fileTypeConsumer.getStandardFileTypeByName("YAML");
+    if (yamlFileType != null) {
+      fileTypeConsumer.consume(yamlFileType, new ExactFileNameMatcher(".analysis_options"));
+    }
   }
 }
