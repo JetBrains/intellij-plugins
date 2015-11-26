ecd3f88c3d3fe2aa04a2a4514dd317538185aeeb
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-11-26 18:59:09 +0300

    RestartDartAnalysisServerAction

--- Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/ReanalyzeDartSourcesAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/ReanalyzeDartSourcesAction.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jetbrains.lang.dart.ide.actions;
+package com.jetbrains.lang.dart.ide.errorTreeView;
 
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -23,7 +23,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
-import com.jetbrains.lang.dart.ide.errorTreeView.DartProblemsViewImpl;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
