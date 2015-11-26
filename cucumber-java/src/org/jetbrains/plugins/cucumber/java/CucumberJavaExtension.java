30908d83edd01223fad8fbf6fa07b4c49146820c
Andrey Vokin <andrey.vokin@jetbrains.com>
2015-11-26 18:34:42 +0300

    Cucumber for java. JavaPsiFacade.findClass used to find step def annotation

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJavaExtension.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJavaExtension.java
@@ -2,10 +2,10 @@ package org.jetbrains.plugins.cucumber.java;
 
 import com.intellij.ide.highlighter.JavaFileType;
 import com.intellij.openapi.module.Module;
+import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiMethod;
-import com.intellij.psi.impl.java.stubs.index.JavaFullClassNameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.AnnotatedElementsSearch;
 import com.intellij.util.Query;
@@ -18,7 +18,6 @@ import org.jetbrains.plugins.cucumber.java.steps.JavaStepDefinitionCreator;
 import org.jetbrains.plugins.cucumber.steps.AbstractStepDefinition;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -41,16 +40,8 @@ public class CucumberJavaExtension extends AbstractCucumberJavaExtension {
   public List<AbstractStepDefinition> loadStepsFor(@Nullable PsiFile featureFile, @NotNull Module module) {
     final GlobalSearchScope dependenciesScope = module.getModuleWithDependenciesAndLibrariesScope(true);
 
-    Collection<PsiClass> stepDefAnnotationCandidates = JavaFullClassNameIndex.getInstance().get(
-      CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION.hashCode(), module.getProject(), dependenciesScope);
-
-    PsiClass stepDefAnnotationClass = null;
-    for (PsiClass candidate : stepDefAnnotationCandidates) {
-      if (CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION.equals(candidate.getQualifiedName())) {
-        stepDefAnnotationClass = candidate;
-        break;
-      }
-    }
+    PsiClass stepDefAnnotationClass = JavaPsiFacade.getInstance(module.getProject()).findClass(CUCUMBER_RUNTIME_JAVA_STEP_DEF_ANNOTATION,
+                                                                                               dependenciesScope);
     if (stepDefAnnotationClass == null) {
       return Collections.emptyList();
     }
