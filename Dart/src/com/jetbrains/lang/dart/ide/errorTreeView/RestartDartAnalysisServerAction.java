d44f81fdb4241bb15aa431815541d46c08503659
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-12-02 20:23:07 +0300

    Dart problems view cosmetics

--- Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/RestartDartAnalysisServerAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/RestartDartAnalysisServerAction.java
@@ -1,13 +1,12 @@
 package com.jetbrains.lang.dart.ide.errorTreeView;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.LayeredIcon;
 import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import icons.DartIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,7 +14,7 @@ public class RestartDartAnalysisServerAction extends DumbAwareAction {
   public RestartDartAnalysisServerAction() {
     super(DartBundle.message("dart.restart.server.action.name"),
           DartBundle.message("dart.restart.server.action.name"),
-          new LayeredIcon(AllIcons.Debugger.KillProcess, AllIcons.Nodes.RunnableMark));
+          DartIcons.Restart_server);
   }
 
   @Override
