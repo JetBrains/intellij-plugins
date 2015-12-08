38cdc6ef10cbf3eb814dd39f8e7a2a0100c71f65
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-12-08 15:16:20 +0300

    new Dart Analysis Server client

---
+++ Dart/thirdPartySrc/analysisServer/com/google/dart/server/GetReachableSourcesConsumer.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2015, the Dart project authors.
+ * 
+ * Licensed under the Eclipse Public License v1.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.dart.server;
+
+import org.dartlang.analysis.server.protocol.RequestError;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The interface {@code GetReachableSourcesConsumer} defines the behavior of objects that consume
+ * the transitive closure of reachable sources for a given file.
+ *
+ * @coverage dart.server
+ */
+public interface GetReachableSourcesConsumer extends Consumer {
+  /**
+   * @param sources a mapping from source URIs to directly reachable source URIs. For example, a
+   *          file "foo.dart" that imports "bar.dart" would have the corresponding mapping {
+   *          "file:///foo.dart" : ["file:///bar.dart"] }. If "bar.dart" has further imports (or
+   *          exports) there will be a mapping from the URI "file:///bar.dart" to them. To check if
+   *          a specific URI is reachable from a given file, clients can check for its presence in
+   *          the resulting key set.
+   */
+  public void computedReachableSources(Map<String, List<String>> sources);
+
+  /**
+   * If a transitive closure cannot be passed back, some {@link RequestError} is passed back
+   * instead.
+   *
+   * @param requestError the reason why a result was not passed back
+   */
+  public void onError(RequestError requestError);
+}
---
+++ Dart/thirdPartySrc/analysisServer/com/google/dart/server/utilities/instrumentation/HealthUtils.java
@@ -0,0 +1,114 @@
+package com.google.dart.server.utilities.instrumentation;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.reflect.Method;
+
+/**
+ * The class {@code HealthReporter} acts as a general mechanism for reporting system state
+ * information to the instrumentation system
+ */
+public class HealthUtils {
+
+  //Copied from org.eclipse.ui.internal.HeapStatus
+  public static long getMaxMem() {
+    long max = Long.MAX_VALUE;
+    try {
+      // Must use reflect to allow compilation against JCL/Foundation
+      Method maxMemMethod = Runtime.class.getMethod("maxMemory", new Class[0]); //$NON-NLS-1$
+      Object o = maxMemMethod.invoke(Runtime.getRuntime(), new Object[0]);
+      if (o instanceof Long) {
+        max = ((Long) o).longValue();
+      }
+    } catch (Exception e) {
+      // ignore if method missing or if there are other failures trying to determine the max
+    }
+    return max;
+  }
+
+  public static void logMemory(InstrumentationBuilder instrumentation) {
+
+    instrumentation.metric("MexMemory-FeedbackUtils", getMaxMem());
+    instrumentation.metric("TotalMemory", Runtime.getRuntime().totalMemory());
+    instrumentation.metric("FreeMemory", Runtime.getRuntime().freeMemory());
+
+  }
+
+  /**
+   * Log data about threads to the provided instrumentation builder
+   * 
+   * @param instrumentation builder to log data to
+   */
+  public static void logThreads(InstrumentationBuilder instrumentation) {
+    java.lang.management.ThreadMXBean th = ManagementFactory.getThreadMXBean();
+    ThreadInfo[] thInfos = th.getThreadInfo(th.getAllThreadIds(), Integer.MAX_VALUE);
+
+    instrumentation.metric("threads-count", thInfos.length);
+
+    for (ThreadInfo thInfo : thInfos) {
+      if (thInfo == null) {
+        instrumentation.metric("Thread-Name", "<unknown>");
+        continue;
+      }
+      instrumentation.metric("Thread-Name", thInfo.getThreadName());
+      instrumentation.metric("Thread-ID", thInfo.getThreadId());
+      instrumentation.metric("Thread-State", thInfo.getThreadState().toString());
+
+      instrumentation.metric("Blocked-Count", thInfo.getBlockedCount());
+      instrumentation.metric("Blocked-Time", thInfo.getBlockedTime());
+
+      instrumentation.metric("Waited-Count", thInfo.getWaitedCount());
+      instrumentation.metric("Waited-Time", thInfo.getWaitedTime());
+
+      instrumentation.data(
+          "Thread-ST",
+          Base64.encodeBytes(stackTraceToString(thInfo.getStackTrace()).getBytes()));
+
+    }
+  }
+
+  /**
+   * Report the health state of the editor to instrumentation The method can be run on any thread
+   * and will return immediately, performing all of its work async
+   * 
+   * @param reason what was the cause of the health check being reported
+   */
+  public static void ReportHealth(final String reason) {
+
+    //Async create and dispatch the thread
+    new Thread() {
+      @Override
+      public void run() {
+        ReportHealthImpl(reason);
+      }
+    }.start();
+
+  }
+
+  /**
+   * Health reporter implementation, this must be implemented so that it can run on any thread
+   * 
+   * @param reason
+   */
+  private static void ReportHealthImpl(String reason) {
+    InstrumentationBuilder instrumentation = Instrumentation.builder("HealthReport");
+
+    try {
+      instrumentation.metric("Reason", reason);
+
+      logMemory(instrumentation);
+      logThreads(instrumentation);
+    } finally {
+      instrumentation.log();
+    }
+  }
+
+  private static String stackTraceToString(StackTraceElement[] elements) {
+    StringBuilder sb = new StringBuilder();
+    for (StackTraceElement element : elements) {
+      sb.append(element.toString());
+      sb.append("\n");
+    }
+    return sb.toString();
+  }
+}
