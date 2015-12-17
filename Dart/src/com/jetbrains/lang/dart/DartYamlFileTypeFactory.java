594713d5069668c30b49c4dd4f8fe7c8cdd2b680
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2015-12-17 16:49:35 +0300

    WEB-19007 Send code completion requests for `.analysis_options` files.

--- Dart/src/com/jetbrains/lang/dart/DartYamlFileTypeFactory.java
+++ Dart/src/com/jetbrains/lang/dart/DartYamlFileTypeFactory.java
@@ -7,13 +7,16 @@ import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
 
 public class DartYamlFileTypeFactory extends FileTypeFactory {
+
+  public static final String DOT_ANALYSIS_OPTIONS = ".analysis_options";
+
   @Override
   public void createFileTypes(@NotNull final FileTypeConsumer fileTypeConsumer) {
     // Do not use YAMLFileType.YML directly to avoid class loaders conflict in IDEA Community + Dart Plugin project setup.
     // The problem is that YAMLFileType is instantiated twice in such project setup: by PluginClassLoader and by UrlClassLoader
     final FileType yamlFileType = fileTypeConsumer.getStandardFileTypeByName("YAML");
     if (yamlFileType != null) {
-      fileTypeConsumer.consume(yamlFileType, new ExactFileNameMatcher(".analysis_options"));
+      fileTypeConsumer.consume(yamlFileType, new ExactFileNameMatcher(DOT_ANALYSIS_OPTIONS));
     }
   }
 }
