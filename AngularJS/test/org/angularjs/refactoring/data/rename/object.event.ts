09f8413b731b0826fbcc06f0285c6151cd4f1cf6
Dennis Ushakov <dennis.ushakov@gmail.com>
2016-01-19 16:46:24 +0300

    fix decorator search - check call expression (cherry picked from commit af247bc)

--- AngularJS/test/org/angularjs/refactoring/data/rename/object.event.ts
+++ AngularJS/test/org/angularjs/refactoring/data/rename/object.event.ts
@@ -3,8 +3,8 @@ import {EventEmitter} from "events";
 
 @Component({selector: 'todo-cmp'})
 export class TodoCmp {
-    @Input model;
-    @Output complete2 = new EventEmitter(); // TypeScript supports initializing fields
+    @Input() model;
+    @Output() complete2 = new EventEmitter(); // TypeScript supports initializing fields
 
     onCompletedButton() {
         this.complete2.emit("completed"); // this fires an event
