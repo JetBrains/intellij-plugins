50602d48412a51e48bc71ef59fcb6f682cced1fb
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2016-02-15 02:24:48 +0300

    CSS: better handling attribute-selectors
    - completion/rename/usages for id- and class- attribute selectors
    - better structure view
    - completion ids and classes from html files (WEB-150)

--- flex/src/com/intellij/javascript/flex/css/FlexClassOrIdUsagesProvider.java
+++ flex/src/com/intellij/javascript/flex/css/FlexClassOrIdUsagesProvider.java
@@ -3,9 +3,11 @@ package com.intellij.javascript.flex.css;
 import com.intellij.lang.javascript.psi.JSLiteralExpression;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.css.CssSelectorSuffix;
 import com.intellij.psi.css.CssTerm;
 import com.intellij.psi.css.impl.CssTermTypes;
 import com.intellij.psi.css.usages.CssClassOrIdUsagesProvider;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,11 +16,11 @@ import com.intellij.psi.css.usages.CssClassOrIdUsagesProvider;
  * Time: 5:40:52 PM
  */
 public class FlexClassOrIdUsagesProvider implements CssClassOrIdUsagesProvider {
-  public boolean isUsage(PsiElement classOrIdSelector, PsiElement candidate, int offsetInCandidate) {
+  public boolean isUsage(@NotNull CssSelectorSuffix selectorSuffix, @NotNull PsiElement candidate, int offsetInCandidate) {
     if ((candidate instanceof CssTerm && ((CssTerm)candidate).getTermType() == CssTermTypes.IDENT) ||
         candidate instanceof JSLiteralExpression) {
       final PsiReference ref = candidate.findReferenceAt(offsetInCandidate);
-      return ref != null && ref.isReferenceTo(classOrIdSelector);
+      return ref != null && ref.isReferenceTo(selectorSuffix);
     }
     return false;
   }
