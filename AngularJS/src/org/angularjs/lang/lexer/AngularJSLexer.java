8a0bd47ad9b2744404f03930709ae476b7c072e0
Dennis Ushakov <dennis.ushakov@gmail.com>
2016-02-25 14:57:21 +0300

    cleanup (cherry picked from commit d73dc0c)

--- AngularJS/src/org/angularjs/lang/lexer/AngularJSLexer.java
+++ AngularJS/src/org/angularjs/lang/lexer/AngularJSLexer.java
@@ -5,13 +5,11 @@ import com.intellij.lexer.FlexAdapter;
 import com.intellij.lexer.MergingLexerAdapter;
 import com.intellij.psi.tree.TokenSet;
 
-import java.io.Reader;
-
 /**
  * @author Dennis.Ushakov
  */
 public class AngularJSLexer extends MergingLexerAdapter {
   public AngularJSLexer() {
-    super(new FlexAdapter(new _AngularJSLexer((Reader)null)), TokenSet.create(JSTokenTypes.STRING_LITERAL));
+    super(new FlexAdapter(new _AngularJSLexer(null)), TokenSet.create(JSTokenTypes.STRING_LITERAL));
   }
 }
