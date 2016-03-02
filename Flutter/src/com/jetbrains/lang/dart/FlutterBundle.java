18d09a0c69f616fdd183fb2b03f66ff31305759b
Steve Messick <messick@google.com>
2016-03-02 12:57:25 -0800

    Initial definition of plugin for flutter

---
+++ Flutter/src/com/jetbrains/lang/dart/FlutterBundle.java
@@ -0,0 +1,32 @@
+package com.jetbrains.lang.dart;
+
+import com.intellij.CommonBundle;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.PropertyKey;
+
+import java.lang.ref.Reference;
+import java.lang.ref.SoftReference;
+import java.util.ResourceBundle;
+
+public class FlutterBundle {
+
+  private static Reference<ResourceBundle> ourBundle;
+  @NonNls private static final String BUNDLE = "com.jetbrains.lang.dart.FlutterBundle";
+
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return CommonBundle.message(getBundle(), key, params);
+  }
+
+  private FlutterBundle() {
+  }
+
+  private static ResourceBundle getBundle() {
+    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
+    if (bundle == null) {
+      bundle = ResourceBundle.getBundle(BUNDLE);
+      ourBundle = new SoftReference<ResourceBundle>(bundle);
+    }
+    return bundle;
+  }
+}
