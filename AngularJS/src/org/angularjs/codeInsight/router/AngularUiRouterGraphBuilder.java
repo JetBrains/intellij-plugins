1b9dd605d529f0460888ebbbdc670efa75dbbfa0
irengrig <Irina.Chernushina@jetbrains.com>
2016-03-31 17:18:49 +0200

    angular ui-router diagram: fix bug with showing templates defined in states

--- AngularJS/src/org/angularjs/codeInsight/router/AngularUiRouterGraphBuilder.java
+++ AngularJS/src/org/angularjs/codeInsight/router/AngularUiRouterGraphBuilder.java
@@ -102,7 +102,7 @@ public class AngularUiRouterGraphBuilder {
 
         if (templateUrl != null) {
           final AngularUiRouterNode templateNode = getOrCreateTemplateNode(provider, templateUrl, null);
-          edges.add(new AngularUiRouterEdge(node, templateNode));
+          edges.add(new AngularUiRouterEdge(templateNode, node, "provides"));
 
           if (state.hasViews()) {
             if (state.isAbstract()) {
@@ -271,7 +271,7 @@ public class AngularUiRouterGraphBuilder {
       final String fullUrl = templateUrl;
       final int idx = fullUrl.lastIndexOf('/');
       templateUrl = idx >= 0 ? templateUrl.substring(idx + 1) : templateUrl;
-      template = template == null ? myTemplatesMap.get(templateUrl) : template;
+      template = template == null ? myTemplatesMap.get(fullUrl) : template;
       if (template == null) {
         // file not found
         final DiagramObject templateObject = new DiagramObject(Type.template, templateUrl, null);
