090ac272a452f11ebdbabb5d46c668d14837b0a3
Anton Makeev <Anton.Makeev@jetbrains.com>
2016-04-15 19:18:38 +0200

    CPP-6396 Rework variables, values and children calculation: LLValue & LLValueData API, renderers rework, use 'native' printers results where possible

--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/renderers/MotionObjectRenderer.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/renderers/MotionObjectRenderer.java
@@ -2,9 +2,9 @@ package org.jetbrains.plugins.ruby.motion.run.renderers;
 
 import com.intellij.execution.ExecutionException;
 import com.intellij.xdebugger.frame.XCompositeNode;
-import com.jetbrains.cidr.execution.debugger.backend.DBCannotEvaluateException;
 import com.jetbrains.cidr.execution.debugger.backend.DBUserException;
 import com.jetbrains.cidr.execution.debugger.backend.LLValue;
+import com.jetbrains.cidr.execution.debugger.backend.LLValueData;
 import com.jetbrains.cidr.execution.debugger.evaluation.CidrPhysicalValue;
 import com.jetbrains.cidr.execution.debugger.evaluation.CidrValue;
 import com.jetbrains.cidr.execution.debugger.evaluation.EvaluationContext;
@@ -30,7 +30,7 @@ public class MotionObjectRenderer extends ValueRenderer {
   @Override
   protected String doComputeValue(@NotNull EvaluationContext context) throws ExecutionException, DBUserException {
     return TextUtil.removeQuoting(
-      context.evaluate("(char *)[[(id)rb_inspect(" + myValue.getVar().getPointer() + ") description] UTF8String]").getReadableValue());
+      context.evaluateData("(char *)[[(id)rb_inspect(" + myValue.getVarData(context).getPointer() + ") description] UTF8String]").getPresentableValue());
   }
 
   @Override
@@ -45,11 +45,11 @@ public class MotionObjectRenderer extends ValueRenderer {
   }
 
   private static int count(EvaluationContext context, LLValue instanceVariablesNames) throws ExecutionException, DBUserException {
-    return (int)context.evaluate(context.castIDToNumber("[" + getSelf(instanceVariablesNames) + " count]", "unsigned int")).intValue();
+    return (int)context.evaluateData(context.castIDToNumber("[" + getSelf(instanceVariablesNames, context) + " count]", "unsigned int")).intValue();
   }
 
-  private LLValue getInstanceVariablesNames(EvaluationContext context) throws ExecutionException, DBCannotEvaluateException {
-    return context.evaluate(EvaluationContext.cast("rb_obj_instance_variables(" + getSelf() + ")", "id"));
+  private LLValue getInstanceVariablesNames(EvaluationContext context) throws ExecutionException, DBUserException {
+    return context.evaluate(EvaluationContext.cast("rb_obj_instance_variables(" + getSelf(context) + ")", "id"));
   }
 
   @Override
@@ -61,11 +61,11 @@ public class MotionObjectRenderer extends ValueRenderer {
     final int count = count(context, names);
     for (int i = 0; i < count; i++) {
       final String selName = EvaluationContext.cast("sel_registerName(\"objectAtIndex:\")", "id");
-      final String nameExpr = EvaluationContext.cast("objc_msgSend(" + getSelf(names) + ", " + selName + ", " + i + ")", "id");
-      final LLValue name = context.evaluate(nameExpr);
+      final String nameExpr = EvaluationContext.cast("objc_msgSend(" + getSelf(names, context) + ", " + selName + ", " + i + ")", "id");
+      final LLValueData name = context.evaluateData(nameExpr);
       final String namePointer = "(char *)[[" + name.getPointer() + " description] UTF8String]";
-      final String ivarName = TextUtil.removeQuoting(context.evaluate(namePointer).getReadableValue());
-      final String ivarExpr = getChildEvaluationExpression(ivarName);
+      final String ivarName = TextUtil.removeQuoting(context.evaluateData(namePointer).getPresentableValue());
+      final String ivarExpr = getChildEvaluationExpression(context, ivarName);
       final LLValue ivar = context.evaluate(ivarExpr);
       children.add(new MotionMemberValue(ivar, 
                                          ivarName, 
@@ -75,9 +75,10 @@ public class MotionObjectRenderer extends ValueRenderer {
     CidrValue.addAllTo(children, container);
   }
 
-  private String getChildEvaluationExpression(String ivarName) {
+  private String getChildEvaluationExpression(@NotNull EvaluationContext context, String ivarName)
+    throws ExecutionException, DBUserException {
     final String ivarNameExpr = EvaluationContext.cast("rb_intern(\"" + ivarName + "\")", "char *");
-    return EvaluationContext.cast("rb_ivar_get(" + getSelf() + ", " + ivarNameExpr + ")", "id");
+    return EvaluationContext.cast("rb_ivar_get(" + getSelf(context) + ", " + ivarNameExpr + ")", "id");
   }
 
   @Override
@@ -85,11 +86,11 @@ public class MotionObjectRenderer extends ValueRenderer {
     return true;
   }
 
-  private String getSelf() {
-    return EvaluationContext.cast(myValue.getVar().getPointerSafely(), "id");
+  private String getSelf(@NotNull EvaluationContext context) throws ExecutionException, DBUserException {
+    return EvaluationContext.cast(myValue.getVarData(context).getPointer(), "id");
   }
 
-  private static String getSelf(LLValue value) throws DBCannotEvaluateException {
-    return EvaluationContext.cast(value.getPointer(), "id");
+  private static String getSelf(LLValue value, @NotNull EvaluationContext context) throws DBUserException, ExecutionException {
+    return EvaluationContext.cast(context.getData(value).getPointer(), "id");
   }
 }
--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/renderers/MotionValueRendererFactory.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/renderers/MotionValueRendererFactory.java
@@ -1,14 +1,15 @@
 package org.jetbrains.plugins.ruby.motion.run.renderers;
 
 import com.intellij.execution.ExecutionException;
-import com.jetbrains.cidr.execution.debugger.CidrDebuggerSettings;
 import com.jetbrains.cidr.execution.debugger.backend.DBCannotEvaluateException;
-import com.jetbrains.cidr.execution.debugger.backend.LLValue;
-import com.jetbrains.cidr.execution.debugger.evaluation.CidrPhysicalValue;
+import com.jetbrains.cidr.execution.debugger.backend.DBUserException;
+import com.jetbrains.cidr.execution.debugger.backend.LLValueData;
 import com.jetbrains.cidr.execution.debugger.evaluation.CustomValueRendererFactory;
 import com.jetbrains.cidr.execution.debugger.evaluation.EvaluationContext;
+import com.jetbrains.cidr.execution.debugger.evaluation.ValueRendererFactory;
 import com.jetbrains.cidr.execution.debugger.evaluation.renderers.NSCollectionValueRenderer;
 import com.jetbrains.cidr.execution.debugger.evaluation.renderers.ValueRenderer;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
@@ -17,16 +18,15 @@ import org.jetbrains.annotations.Nullable;
 public class MotionValueRendererFactory implements CustomValueRendererFactory {
   @Nullable
   @Override
-  public ValueRenderer createRendererLeading(CidrDebuggerSettings settings, CidrPhysicalValue value,
-                                             LLValue var,
-                                             EvaluationContext context) throws ExecutionException {
+  public ValueRenderer createRendererLeading(@NotNull ValueRendererFactory.FactoryContext context) throws ExecutionException, DBUserException {
     try {
-      if (var.isValidPointer()) {
-        context.evaluate(EvaluationContext.cast("rb_inspect(" + EvaluationContext.cast(var.getPointer(), "id") + ")", "id"));
+      LLValueData data = context.getLLValueData();
+      if (data.isValidPointer()) {
+        context.getEvaluationContext().evaluate(EvaluationContext.cast("rb_inspect(" + EvaluationContext.cast(data.getPointer(), "id") + ")", "id"));
         // we want ObjC renderers for our collections
         // Ruby collections are inheriting from native ones and don't have any useful instance variables
-        final ValueRenderer collectionRenderer = NSCollectionValueRenderer.createIfNSCollection(context, value, var);
-        return collectionRenderer != null ? collectionRenderer : new MotionObjectRenderer(value);
+        final ValueRenderer collectionRenderer = NSCollectionValueRenderer.createIfNSCollection(context);
+        return collectionRenderer != null ? collectionRenderer : new MotionObjectRenderer(context.getPhysicalValue());
       }
     } catch (DBCannotEvaluateException ignored) {}
     return null;
@@ -34,10 +34,7 @@ public class MotionValueRendererFactory implements CustomValueRendererFactory {
 
   @Nullable
   @Override
-  public ValueRenderer createRendererTrailing(CidrDebuggerSettings settings,
-                                              CidrPhysicalValue value,
-                                              LLValue var,
-                                              EvaluationContext context) throws ExecutionException {
+  public ValueRenderer createRendererTrailing(@NotNull ValueRendererFactory.FactoryContext context) throws ExecutionException {
     return null;
   }
 }
