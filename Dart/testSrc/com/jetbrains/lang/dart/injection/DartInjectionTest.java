c8b82efaad272a2428cbd26ac7d9487b3c16fb19
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2016-04-19 18:45:39 +0300

    Do not use closed-source test class in Dart tests

--- Dart/testSrc/com/jetbrains/lang/dart/injection/DartInjectionTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/injection/DartInjectionTest.java
@@ -1,11 +1,15 @@
 package com.jetbrains.lang.dart.injection;
 
-import com.intellij.codeInsight.injected.InjectedLanguageTestCase;
+import com.intellij.codeInsight.daemon.quickFix.LightQuickFixTestCase;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.impl.DebugUtil;
+import com.intellij.psi.impl.source.tree.injected.InjectedLanguageUtil;
 import com.intellij.testFramework.ParsingTestCase;
 import com.jetbrains.lang.dart.util.DartTestUtils;
 import org.jetbrains.annotations.NotNull;
 
-public class DartInjectionTest extends InjectedLanguageTestCase {
+public class DartInjectionTest extends LightQuickFixTestCase {
   @NotNull
   @Override
   protected String getTestDataPath() {
@@ -15,6 +19,11 @@ public class DartInjectionTest extends InjectedLanguageTestCase {
   private void doTest() throws Exception {
     configureByFile(getTestName(false) + ".dart");
     ParsingTestCase.doCheckResult(getTestDataPath(), getTestName(false) + "." + "txt", toParseTreeText(myFile));
+  }
+
+  private static String toParseTreeText(PsiFile file) {
+    return DebugUtil.psiToString(file, false, false, (psiElement, consumer) -> InjectedLanguageUtil
+      .enumerate(psiElement, (PsiLanguageInjectionHost.InjectedPsiVisitor)(injectedPsi, places) -> consumer.consume(injectedPsi)));
   }
 
   public void testHtmlInStrings() throws Exception {
