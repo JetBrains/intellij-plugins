c0328e10b73098871df55e16f47aad8dae0b32a2
irengrig <Irina.Chernushina@jetbrains.com>
2016-04-28 15:14:35 +0200

    angular.js, get rid of AngularModuleDependencyIndex, calculate module dependencies on the fly when creating ui router diagram

--- AngularJS/src/org/angularjs/index/AngularModuleIndex.java
+++ AngularJS/src/org/angularjs/index/AngularModuleIndex.java
@@ -1,8 +1,14 @@
 package org.angularjs.index;
 
-import com.intellij.lang.javascript.psi.JSImplicitElementProvider;
+import com.intellij.lang.javascript.psi.*;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.stubs.StubIndexKey;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author Dennis.Ushakov
@@ -14,5 +20,30 @@ public class AngularModuleIndex extends AngularIndexBase {
   @Override
   public StubIndexKey<String, JSImplicitElementProvider> getKey() {
     return KEY;
+  }
+
+  public static List<String> findDependenciesInModuleDeclaration(JSCallExpression call) {
+    final JSExpression methodExpression = call.getMethodExpression();
+    if (methodExpression instanceof JSReferenceExpression &&
+        ((JSReferenceExpression)methodExpression).getQualifier() != null &&
+        AngularJSIndexingHandler.MODULE.equals(((JSReferenceExpression)methodExpression).getReferenceName())) {
+      final JSExpression[] arguments = call.getArguments();
+      if (arguments.length > 1 && arguments[0] instanceof JSLiteralExpression
+          && ((JSLiteralExpression) arguments[0]).isQuotedLiteral()
+          && arguments[1] instanceof JSArrayLiteralExpression) {
+        final JSArrayLiteralExpression array = (JSArrayLiteralExpression)arguments[1];
+        final JSExpression[] children = array.getExpressions();
+        final Set<String> dependencies = new HashSet<String>();
+        for (JSExpression child : children) {
+          if (child instanceof JSLiteralExpression && ((JSLiteralExpression)child).isQuotedLiteral()) {
+            dependencies.add(StringUtil.unquoteString(child.getText()));
+          }
+        }
+        if (!dependencies.isEmpty()) {
+          return new ArrayList<>(dependencies);
+        }
+      }
+    }
+    return null;
   }
 }
