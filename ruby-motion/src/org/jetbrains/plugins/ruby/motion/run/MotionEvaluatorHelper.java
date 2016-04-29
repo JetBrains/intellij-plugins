2f79690fa1d753e2f41ed22d72b12ccd2293d6c3
Anton Makeev <Anton.Makeev@jetbrains.com>
2016-04-30 01:19:17 +0200

    CPP-6592 Lazily collect threads and frames on stepping (part 2, files with no logic change)
    + send only one stop reason per thread from LLDB
    + some unnecessary SetSelectedThread/SetSelectedFrame calls removed
    + always send execute console command to LLDB (in stead of trying to send to the debugged process)
    + threadId migrated to long (as in LLDB)
    + additional tests for threading
    + error stack frame removed (it could lead to class cast in CidrDebugProcess.getCurrentThreadId/getCurrentFrameIndex)

--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/MotionEvaluatorHelper.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/MotionEvaluatorHelper.java
@@ -26,10 +26,9 @@ public class MotionEvaluatorHelper extends CidrEvaluatorHelper {
                                                   @NotNull DebuggerDriver driver,
                                                   @NotNull XExpression expression,
                                                   XSourcePosition sourcePosition,
-                                                  int frameNumber,
-                                                  int threadId) throws ExecutionException, DBUserException {
+                                                  long threadId, int frameIndex) throws ExecutionException, DBUserException {
     final String stringExpression = expression.getExpression();
-    final LLValue result = driver.evaluate(threadId, frameNumber, stringExpression);
+    final LLValue result = driver.evaluate(threadId, frameIndex, stringExpression);
     return Pair.create(result, stringExpression);
   }
 }
