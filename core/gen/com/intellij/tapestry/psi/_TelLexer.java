a3945ec708b5dbebc3988f66ef76ca72ca778492
Gregory.Shrago <gregory.shrago@jetbrains.com>
2016-05-11 21:39:49 +0300

    JFlex: regen all lexers

--- core/gen/com/intellij/tapestry/psi/_TelLexer.java
+++ core/gen/com/intellij/tapestry/psi/_TelLexer.java
@@ -357,14 +357,8 @@ class _TelLexer implements FlexLexer {
       the source of the yytext() string */
   private CharSequence zzBuffer = "";
 
-  /** this buffer may contains the current text array to be matched when it is cheap to acquire it */
-  private char[] zzBufferArray;
-
   /** the textposition at the last accepting state */
   private int zzMarkedPos;
-
-  /** the textposition at the last state to be included in yytext */
-  private int zzPushbackPos;
 
   /** the current text position in the buffer */
   private int zzCurrentPos;
@@ -424,19 +418,17 @@ class _TelLexer implements FlexLexer {
     return map;
   }
 
-  public final int getTokenStart(){
+  public final int getTokenStart() {
     return zzStartRead;
   }
 
-  public final int getTokenEnd(){
+  public final int getTokenEnd() {
     return getTokenStart() + yylength();
   }
 
-  public void reset(CharSequence buffer, int start, int end,int initialState){
+  public void reset(CharSequence buffer, int start, int end, int initialState) {
     zzBuffer = buffer;
-    zzBufferArray = com.intellij.util.text.CharArrayUtil.fromSequenceWithoutCopying(buffer);
     zzCurrentPos = zzMarkedPos = zzStartRead = start;
-    zzPushbackPos = 0;
     zzAtEOF  = false;
     zzAtBOL = true;
     zzEndRead = end;
@@ -493,7 +485,7 @@ class _TelLexer implements FlexLexer {
    * @return the character at position pos
    */
   public final char yycharat(int pos) {
-    return zzBufferArray != null ? zzBufferArray[zzStartRead+pos]:zzBuffer.charAt(zzStartRead+pos);
+    return zzBuffer.charAt(zzStartRead+pos);
   }
 
 
@@ -576,7 +568,6 @@ class _TelLexer implements FlexLexer {
     int zzMarkedPosL;
     int zzEndReadL = zzEndRead;
     CharSequence zzBufferL = zzBuffer;
-    char[] zzBufferArrayL = zzBufferArray;
     char [] zzCMapL = ZZ_CMAP;
 
     int [] zzTransL = ZZ_TRANS;
