23022a63ba5d290e8a5f520f477e556cf1a65d0b
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2016-05-12 16:14:41 +0300

    First interaction
    GitOrigin-RevId: 73f7e58fdd98567e82b9d13362ad4290f0f8a77e

--- ide-features-trainer/src/training/component/LearnProjectComponent.java
+++ ide-features-trainer/src/training/component/LearnProjectComponent.java
@@ -1,17 +1,34 @@
 package training.component;
 
+import com.intellij.ide.ui.UISettings;
+import com.intellij.ide.util.PropertiesComponent;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.components.ProjectComponent;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectUtil;
 import com.intellij.openapi.startup.StartupManager;
-import com.intellij.openapi.wm.ToolWindow;
-import com.intellij.openapi.wm.ToolWindowAnchor;
-import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.openapi.ui.popup.Balloon;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.wm.*;
+import com.intellij.openapi.wm.impl.IdeRootPane;
+import com.intellij.openapi.wm.impl.StripeButton;
+import com.intellij.openapi.wm.impl.ToolWindowsPane;
+import com.intellij.ui.GotItMessage;
+import com.intellij.ui.awt.RelativePoint;
+import com.intellij.util.Alarm;
+import com.intellij.util.ui.UIUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import training.learn.CourseManager;
+import training.learn.LearnBundle;
+import training.ui.LearnToolWindow;
 import training.ui.LearnToolWindowFactory;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by karashevich on 17/03/16.
@@ -30,6 +47,14 @@ public class LearnProjectComponent implements ProjectComponent {
     public void projectOpened() {
         registerLearnToolWindow(myProject);
         CourseManager.getInstance().updateToolWindow(myProject);
+
+        StartupManager.getInstance(myProject).registerPostStartupActivity(new Runnable() {
+            @Override
+            public void run() {
+                pluginFirstStart();
+            }
+        });
+
     }
 
     @Override
@@ -62,7 +87,53 @@ public class LearnProjectComponent implements ProjectComponent {
         if (toolWindow == null) {
             toolWindowManager.registerToolWindow(LearnToolWindowFactory.LEARN_TOOL_WINDOW, true, ToolWindowAnchor.LEFT, myProject, true);
         }
-
-
     }
+
+    public void pluginFirstStart(){
+        final String key = "learn.toolwindow.button.info.shown";
+        //TODO: remove before release
+        PropertiesComponent.getInstance().setValue(key, String.valueOf(false));
+        if (UISettings.getInstance().HIDE_TOOL_STRIPES) {
+            UISettings.getInstance().HIDE_TOOL_STRIPES = false;
+            UISettings.getInstance().fireUISettingsChanged();
+        }
+
+        ApplicationManager.getApplication().invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                final StripeButton learnStripeButton = getLearnStripeButton();
+                if (learnStripeButton == null) return;
+
+                if (!PropertiesComponent.getInstance().isTrueValue(key)) {
+                    PropertiesComponent.getInstance().setValue(key, String.valueOf(true));
+                    final Alarm alarm = new Alarm();
+                    alarm.addRequest(new Runnable() {
+                        @Override
+                        public void run() {
+                            GotItMessage.createMessage(LearnBundle.message("learn.tool.window.quick.access.title"), LearnBundle.message("learn.tool.window.quick.access.message"))
+                                    .setDisposable(myProject)
+                                    .show(new RelativePoint(learnStripeButton, new Point(20, 30)), Balloon.Position.atRight);
+                            Disposer.dispose(alarm);
+                        }
+                    }, 20000);
+                }
+            }
+        });
+    }
+
+    @Nullable
+    private StripeButton getLearnStripeButton(){
+
+        StripeButton learnStripeButton = null;
+        final JRootPane rootPane = ((JFrame) WindowManager.getInstance().getIdeFrame(myProject)).getRootPane();
+        ToolWindowsPane pane = UIUtil.findComponentOfType(rootPane, ToolWindowsPane.class);
+        final java.util.List<StripeButton> componentsOfType = UIUtil.findComponentsOfType(pane, StripeButton.class);
+        for (StripeButton stripeButton : componentsOfType) {
+            if (stripeButton.getText().equals(LearnToolWindowFactory.LEARN_TOOL_WINDOW))
+                learnStripeButton = stripeButton;
+        }
+
+        return learnStripeButton;
+    }
+
 }
