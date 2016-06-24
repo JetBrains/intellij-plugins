dc363280b251915cd1667a3f4d9548b9ea981c85
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2016-06-24 13:48:12 -0700

    CSS: simplify inclusion context extension

--- AngularJS/src/org/angularjs/codeInsight/AngularJSCssInclusionContext.java
+++ AngularJS/src/org/angularjs/codeInsight/AngularJSCssInclusionContext.java
@@ -7,8 +7,6 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.css.CssFile;
-import com.intellij.psi.css.CssStylesheet;
-import com.intellij.psi.css.StylesheetFile;
 import com.intellij.psi.css.resolve.CssInclusionContext;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.SmartList;
@@ -47,11 +45,5 @@ public class AngularJSCssInclusionContext extends CssInclusionContext {
       }
     }
     return PsiFile.EMPTY_ARRAY;
-  }
-
-  @Nullable
-  @Override
-  public CssStylesheet getStylesheet(@NotNull PsiFile candidate) {
-    return candidate instanceof StylesheetFile ? ((StylesheetFile)candidate).getStylesheet() : null;
   }
 }
