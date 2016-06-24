ebbfbe5ffe65d9d5db5b8993648cd25baf53c5a0
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2016-06-24 13:05:28 +0300

    fix tests

--- Dart/testSrc/com/jetbrains/lang/dart/ide/moveCode/DartCodeMoverTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/ide/moveCode/DartCodeMoverTest.java
@@ -7,7 +7,7 @@ import com.jetbrains.lang.dart.DartCodeInsightFixtureTestCase;
 abstract public class DartCodeMoverTest extends DartCodeInsightFixtureTestCase {
 
   protected void doTest() {
-    final String testName = getTestName(true);
+    final String testName = getTestName(false);
     myFixture.configureByFile(testName + ".dart");
     myFixture.performEditorAction(IdeActions.ACTION_MOVE_STATEMENT_UP_ACTION);
     myFixture.checkResultByFile(testName + "_afterUp.dart", true);
