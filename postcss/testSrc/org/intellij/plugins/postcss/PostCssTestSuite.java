6fca113beb8bfe6e8f71614cff882598a8eb3ba4
Ilya Bochkarev <ilya.bochkarev@jetbrains.com>
2016-06-27 16:19:59 +0300

    PostCSS: test suite created

---
+++ postcss/testSrc/org/intellij/plugins/postcss/PostCssTestSuite.java
@@ -0,0 +1,33 @@
+package org.intellij.plugins.postcss;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.intellij.plugins.postcss.lexer.PostCssLexerTest;
+import org.intellij.plugins.postcss.parser.PostCssParsingTest;
+
+@SuppressWarnings({"JUnitTestClassNamingConvention", "JUnitTestCaseWithNoTests"})
+public class PostCssTestSuite extends TestCase {
+  public static Test suite() {
+    TestSuite testSuite = new TestSuite("All PostCss");
+    testSuite.addTest(WithoutPerformance.suite());
+    return testSuite;
+  }
+
+  public static class WithoutPerformance extends TestCase {
+    public static Test suite() {
+      TestSuite suite = new TestSuite("All PostCss without performance");
+      suite.addTest(Fast.suite());
+      return suite;
+    }
+  }
+
+  public static class Fast extends TestCase {
+    public static Test suite() {
+      TestSuite suite = new TestSuite("Fast PostCss");
+      suite.addTestSuite(PostCssLexerTest.class);
+      suite.addTestSuite(PostCssParsingTest.class);
+      return suite;
+    }
+  }
+}
