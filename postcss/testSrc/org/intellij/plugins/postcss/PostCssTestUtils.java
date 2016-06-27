136f7ab9651999ea6c0ffac7fde4f7c500b2fca5
Ilya Bochkarev <ilya.bochkarev@jetbrains.com>
2016-06-27 13:06:38 +0300

    PostCSS: lexer and lexer ampersand test added

---
+++ postcss/testSrc/org/intellij/plugins/postcss/PostCssTestUtils.java
@@ -0,0 +1,47 @@
+package org.intellij.plugins.postcss;
+
+import com.intellij.openapi.application.PathManager;
+import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.openapi.util.text.StringUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.net.URL;
+
+public final class PostCssTestUtils {
+  private static final String DIR_MARKER = "org/intellij/plugins/postcss";
+
+  @NotNull
+  public static String getFullTestDataPath(@NotNull final Class clazz) {
+    final String classFullPath = getClassFullPath(clazz);
+    final String packagePath = classFullPath.substring(0, classFullPath.lastIndexOf('/'));
+    return FileUtil.join(getRootTestDataPath(), packagePath.substring(packagePath.indexOf(DIR_MARKER) + DIR_MARKER.length()));
+  }
+
+  @NotNull
+  public static String getRootTestDataPath() {
+    return FileUtil.join(PathManager.getHomePath(), "plugins", "postcss", "testData");
+  }
+
+  /**
+   * Return relative path to the test data. Path is relative to the
+   * {@link PathManager#getHomePath()}
+   *
+   * @return relative path to the test data.
+   */
+  @NotNull
+  public static String getTestDataBasePath(@NotNull final Class clazz) {
+    return "/" + StringUtil.notNullize(FileUtil.getRelativePath(PathManager.getHomePath(), getFullTestDataPath(clazz), File.separatorChar));
+  }
+
+  @NotNull
+  private static String getClassFullPath(@NotNull final Class clazz) {
+    String name = clazz.getSimpleName() + ".class";
+    final URL url = clazz.getResource(name);
+    return url.getPath();
+  }
+
+
+  private PostCssTestUtils() {
+  }
+}
---
+++ postcss/testSrc/org/intellij/plugins/postcss/lexer/PostCssLexerTest.java
@@ -0,0 +1,31 @@
+package org.intellij.plugins.postcss.lexer;
+
+import com.intellij.lexer.Lexer;
+import com.intellij.testFramework.LexerTestCase;
+import com.intellij.testFramework.TestDataPath;
+import org.intellij.plugins.postcss.PostCssTestUtils;
+
+@TestDataPath("$CONTENT_ROOT/testData/lexer/")
+public class PostCssLexerTest extends LexerTestCase {
+  public void testComments() {
+    doTest();
+  }
+
+  public void testAmpersand() {
+    doTest();
+  }
+
+  private void doTest() {
+    doFileTest("pcss");
+  }
+
+  @Override
+  protected Lexer createLexer() {
+    return new PostCssLexer();
+  }
+
+  @Override
+  protected String getDirPath() {
+    return PostCssTestUtils.getTestDataBasePath(getClass());
+  }
+}
