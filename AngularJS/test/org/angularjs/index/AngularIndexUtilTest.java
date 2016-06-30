9f6438b7b0660564b2f96592bfea2dc2bd8db94e
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2016-06-30 14:29:22 +0300

    angular: add test for hasAngularJS2

---
+++ AngularJS/test/org/angularjs/index/AngularIndexUtilTest.java
@@ -0,0 +1,20 @@
+package org.angularjs.index;
+
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+import org.angularjs.AngularTestUtil;
+import org.junit.Assert;
+
+public class AngularIndexUtilTest extends LightPlatformCodeInsightFixtureTestCase {
+
+  @Override
+  protected String getTestDataPath() {
+    return AngularTestUtil.getBaseTestDataPath(getClass()) + "injections";
+  }
+
+  public void testHasAngularJS2() throws Exception {
+    myFixture.configureByText("ng_for.js", "NgFor.decorators = [\n" +
+                                           "    { type: core_1.Directive, args: [{ selector: '[ngFor][ngForOf]', inputs: ['ngForTrackBy', 'ngForOf', 'ngForTemplate'] },] },\n" +
+                                           "];");
+    Assert.assertTrue(AngularIndexUtil.hasAngularJS2(myFixture.getProject()));
+  }
+}
