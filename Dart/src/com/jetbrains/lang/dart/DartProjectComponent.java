7fb88aa9b54cce5c8436f3b501bb30aa1683e269
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2016-07-07 16:57:58 +0300

    Outdated code

--- Dart/src/com/jetbrains/lang/dart/DartProjectComponent.java
+++ Dart/src/com/jetbrains/lang/dart/DartProjectComponent.java
@@ -10,11 +10,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.*;
 import com.intellij.openapi.roots.impl.ModifiableModelCommitter;
 import com.intellij.openapi.roots.impl.libraries.ApplicationLibraryTable;
-import com.intellij.openapi.roots.impl.libraries.LibraryEx;
 import com.intellij.openapi.roots.libraries.Library;
 import com.intellij.openapi.roots.libraries.LibraryTable;
 import com.intellij.openapi.roots.libraries.LibraryTablesRegistrar;
-import com.intellij.openapi.roots.libraries.PersistentLibraryKind;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.Condition;
 import com.intellij.openapi.util.ModificationTracker;
@@ -28,14 +26,8 @@ import com.intellij.openapi.vfs.VirtualFileVisitor;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.util.PairConsumer;
 import com.intellij.util.SmartList;
 import com.intellij.util.containers.ContainerUtil;
-import com.intellij.xdebugger.XDebuggerManager;
-import com.intellij.xdebugger.XSourcePosition;
-import com.intellij.xdebugger.breakpoints.XBreakpoint;
-import com.intellij.xdebugger.breakpoints.XBreakpointManager;
-import com.jetbrains.lang.dart.ide.runner.DartLineBreakpointType;
 import com.jetbrains.lang.dart.ide.runner.client.DartiumUtil;
 import com.jetbrains.lang.dart.sdk.DartSdk;
 import com.jetbrains.lang.dart.sdk.DartSdkLibraryPresentationProvider;
@@ -45,7 +37,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -87,12 +78,8 @@ public class DartProjectComponent extends AbstractProjectComponent {
 
   public void projectOpened() {
     StartupManager.getInstance(myProject).runWhenProjectIsInitialized(() -> {
-      deleteDartSdkGlobalLibConfiguredInVeryOldIde();
-
       ensureCorrectDartSdkLibName();
       updateDependenciesOnDartSdkLib();
-
-      removeJSBreakpointsInDartFiles(myProject);
 
       DartiumUtil.removeUnsupportedAsyncFlag();
 
@@ -256,36 +243,6 @@ public class DartProjectComponent extends AbstractProjectComponent {
     }
 
     return false;
-  }
-
-  private static void removeJSBreakpointsInDartFiles(final Project project) {
-    final XBreakpointManager breakpointManager = XDebuggerManager.getInstance(project).getBreakpointManager();
-    final Collection<XBreakpoint<?>> toRemove = new ArrayList<XBreakpoint<?>>();
-
-    for (XBreakpoint<?> breakpoint : breakpointManager.getAllBreakpoints()) {
-      final XSourcePosition position = breakpoint.getSourcePosition();
-      if (position != null &&
-          position.getFile().getFileType() == DartFileType.INSTANCE &&
-          !(breakpoint.getType() instanceof DartLineBreakpointType)) {
-        toRemove.add(breakpoint);
-      }
-    }
-
-    if (!toRemove.isEmpty()) {
-      ApplicationManager.getApplication().runWriteAction(() -> {
-        for (XBreakpoint<?> breakpoint : toRemove) {
-          breakpointManager.removeBreakpoint(breakpoint);
-        }
-      });
-    }
-  }
-
-  private static void deleteDartSdkGlobalLibConfiguredInVeryOldIde() {
-    final LibraryEx library = (LibraryEx)ApplicationLibraryTable.getApplicationTable().getLibraryByName("Dart SDK");
-    final PersistentLibraryKind<?> kind = library == null ? null : library.getKind();
-    if (library != null && kind != null && "javaScript".equals(kind.getKindId())) {
-      ApplicationManager.getApplication().runWriteAction(() -> ApplicationLibraryTable.getApplicationTable().removeLibrary(library));
-    }
   }
 
   public static void excludeBuildAndPackagesFolders(final @NotNull Module module, final @NotNull VirtualFile pubspecYamlFile) {
