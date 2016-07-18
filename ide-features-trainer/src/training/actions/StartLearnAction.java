decbaafc05ad183d860f01f4c895f4c487aac192
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2016-07-18 14:15:09 +0300

    fix register PostStartupActivity for StartLearning from WelcomeScreen
    GitOrigin-RevId: d5b1a4ed1cb43884712fe3dd7fed25d5da80f054

--- ide-features-trainer/src/training/actions/StartLearnAction.java
+++ ide-features-trainer/src/training/actions/StartLearnAction.java
@@ -1,22 +1,16 @@
 package training.actions;
 
 import com.intellij.icons.AllIcons;
-import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.ActionManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.actionSystem.ex.ActionUtil;
-import com.intellij.openapi.actionSystem.impl.SimpleDataContext;
 import com.intellij.openapi.application.ApplicationNamesInfo;
-import com.intellij.openapi.wm.IdeFocusManager;
-import training.learn.CourseManager;
 import training.learn.LearnBundle;
 
-import java.awt.*;
-
 /**
- * Created by jetbrains on 15/07/16.
+ * Created by karashevich on 15/07/16.
  */
 public class StartLearnAction extends AnAction {
 
