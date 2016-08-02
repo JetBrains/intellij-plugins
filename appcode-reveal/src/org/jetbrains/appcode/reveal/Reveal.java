f3f27944a427a1787f406ddcfb1bc6467c0975fa
Tony Arnold <tony@thecocoabots.com>
2016-08-02 16:05:18 +1000

    Flesh out support for scanning for multiple available versions of Reveal on the user’s local disk

--- appcode-reveal/src/org/jetbrains/appcode/reveal/Reveal.java
+++ appcode-reveal/src/org/jetbrains/appcode/reveal/Reveal.java
@@ -18,44 +18,57 @@ import org.jetbrains.annotations.Nullable;
 import org.jetbrains.ide.script.IdeScriptException;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Reveal {
   public static final Logger LOG = Logger.getInstance("#" + Reveal.class.getPackage().getName());
 
-  @Nullable
-  private static File getRevealBundle() {
+  @NotNull
+  private static List<String> applicationBundleIdentifiers() {
+    List<String> identifiers = new ArrayList<String>() {{
+      add("com.ittybittyapps.Reveal2");
+      add("com.ittybittyapps.dev.Reveal2");
+      add("com.ittybittyapps.dev.Reveal");
+      add("com.ittybottyapps.Reveal");
+    }};
 
-    String path = NSWorkspace.absolutePathForAppBundleWithIdentifier("com.ittybittyapps.Reveal");
-    if (path == null) return null;
-    
-    File result = new File(path);
-    return result.exists() ? result : null;
+    return identifiers;
+  }
+
+  @NotNull
+  private static List<File> getRevealApplicationBundles() {
+    List<File> applicationBundles = new ArrayList<File>();
+
+    for (String identifier: applicationBundleIdentifiers()) {
+      String path = NSWorkspace.absolutePathForAppBundleWithIdentifier(identifier);
+      if (path != null) {
+        File file = new File(path);
+        if (file.exists()) {
+          applicationBundles.add(file);
+        }
+      }
+    }
+
+    return applicationBundles;
   }
 
   @Nullable
-  private static File getRevealInspectionScript() {
-    File bundle = getRevealBundle();
-    if (bundle == null) return null;
-
+  private static File getRevealInspectionScript(@NotNull File bundle) {
     File result = new File(bundle, "/Contents/Resources/InspectApplication.scpt");
     return result.exists() ? result : null;
   }
 
-  @Contract("null -> null")
-  public static File getRevealLib(@Nullable AppleSdk sdk) {
-    if (sdk == null) return null;
-
-    File bundle = getRevealBundle();
-    if (bundle == null) return null;
-
+  @Nullable
+  public static File getRevealLib(@NotNull File bundle, AppleSdk sdk) {
     ApplePlatform platform = sdk.getPlatform();
     String libraryPath = null;
 
     if (platform.isIOS()) {
-      libraryPath = "/Contents/SharedSupport/iOS-Libraries/libReveal.dylib";
+      libraryPath = "/Contents/SharedSupport/iOS-Libraries/RevealServer.framework";
     } else if (platform.isTv()) {
-      libraryPath = "/Contents/SharedSupport/tvOS-Libraries/libReveal-tvOS.dylib";
+      libraryPath = "/Contents/SharedSupport/tvOS-Libraries/RevealServer.framework";
     }
 
     if (libraryPath == null) return null;
@@ -64,21 +77,18 @@ public class Reveal {
     return result.exists() ? result : null;
   }
 
-  public static boolean isCompatible() {
-    Version version = getRevealVersion();
+  public static boolean isCompatible(@NotNull File bundle) {
+    Version version = getRevealVersion(bundle);
     return version != null && version.isOrGreaterThan(2299);
   }
 
-  public static boolean isCompatibleWithRevealOnePointSixOrHigher() {
-    Version version = getRevealVersion();
+  public static boolean isCompatibleWithRevealOnePointSixOrHigher(@NotNull File bundle) {
+    Version version = getRevealVersion(bundle);
     return version != null && version.isOrGreaterThan(5589);
   }
 
   @Nullable
-  public static Version getRevealVersion() {
-    File bundle = getRevealBundle();
-    if (bundle == null) return null;
-
+  public static Version getRevealVersion(@NotNull File bundle) {
     Plist plist = PlistDriver.readAnyFormatSafe(new File(bundle, "Contents/Info.plist"));
     if (plist == null) return null;
 
@@ -93,19 +103,19 @@ public class Reveal {
                        parts.size() > 2 ? StringUtil.parseInt(parts.get(2), 0) : 0);
   }
 
-  public static void refreshReveal(@NotNull String bundleID, @NotNull String deviceName) throws ExecutionException {
+  public static void refreshReveal(@NotNull File revealBundle, @NotNull String bundleID, @NotNull String deviceName) throws ExecutionException {
     UsageTrigger.trigger("appcode.reveal.showInReveal");
 
-    if (isCompatibleWithRevealOnePointSixOrHigher()) {
-      refreshRevealPostOnePointSix(bundleID, deviceName);
+    if (isCompatibleWithRevealOnePointSixOrHigher(revealBundle)) {
+      refreshRevealPostOnePointSix(revealBundle, bundleID, deviceName);
     } else {
       refreshRevealPreOnePointSix(bundleID, deviceName);
     }
   }
 
-  private static void refreshRevealPostOnePointSix(@NotNull String bundleID, @NotNull String deviceName) throws ExecutionException {
-    // Reveal 1.6 bundles the refresh script with the application — execute it using osascript
-    File inspectionScript = getRevealInspectionScript();
+  private static void refreshRevealPostOnePointSix(@NotNull File revealBundle, @NotNull String bundleID, @NotNull String deviceName) throws ExecutionException {
+    // Reveal 1.6 and later bundle the refresh script with the application — execute it using osascript
+    File inspectionScript = getRevealInspectionScript(revealBundle);
     if (inspectionScript == null) {
       throw new ExecutionException("Cannot refresh Reveal. Inspection script could not be found.");
     }
