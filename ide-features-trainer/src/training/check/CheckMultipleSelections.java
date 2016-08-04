9348fc1f580a5c645e65d3104c14827ef6a6e97a
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2016-08-04 15:41:55 +0300

    TP-45 fixed "Multiple selections" last check bug
    GitOrigin-RevId: 5db75fae609ab17079f2dc0e3cd7ee14ce5c38f3

--- ide-features-trainer/src/training/check/CheckMultipleSelections.java
+++ ide-features-trainer/src/training/check/CheckMultipleSelections.java
@@ -5,12 +5,9 @@ import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiLiteralExpression;
-import com.intellij.psi.PsiMethodCallExpression;
 import com.intellij.psi.html.HtmlTag;
 import com.intellij.psi.util.PsiTreeUtil;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -20,32 +17,34 @@ public class CheckMultipleSelections implements Check{
 
     Project project;
     Editor editor;
-    ArrayList<HtmlTag> htmlTagsTH;
 
     @Override
     public void set(Project project, Editor editor) {
         this.project = project;
         this.editor = editor;
-        htmlTagsTH = new ArrayList<>();
     }
 
     @Override
     public void before() {
-        final Document document = editor.getDocument();
-        final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
 
-        final Collection<HtmlTag> childrenOfType1 = PsiTreeUtil.findChildrenOfType(psiFile, HtmlTag.class);
-        for (HtmlTag htmlTag: childrenOfType1){
-            if (htmlTag.getName().equals("th")) htmlTagsTH.add(htmlTag);
-        }
     }
 
     @Override
     public boolean check() {
-        for (HtmlTag htmlTag : htmlTagsTH) {
-            if (!htmlTag.getName().equals("td")) return false;
+
+        final Document document = editor.getDocument();
+        final PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+
+        final Collection<HtmlTag> childrenOfType1 = PsiTreeUtil.findChildrenOfType(psiFile, HtmlTag.class);
+
+        int count = 0;
+
+        for (HtmlTag htmlTag: childrenOfType1){
+            if (htmlTag.getName().equals("th")) return false;
+            if (htmlTag.getName().equals("td")) count++;
         }
-        return true;
+
+        return count == 6;
 
     }
 
