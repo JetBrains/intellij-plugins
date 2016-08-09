976077201590e897f23083d842932ec3cb875522
Ilya Bochkarev <ilya.bochkarev@jetbrains.com>
2016-08-09 16:52:19 +0300

    CSS: mediaType condition in dumb aware completion simplified; PostCSS: unnecessary code removed from completion

--- postcss/src/org/intellij/plugins/postcss/completion/PostCssDumbAwareCompletionContributor.java
+++ postcss/src/org/intellij/plugins/postcss/completion/PostCssDumbAwareCompletionContributor.java
@@ -1,33 +1,22 @@
 package org.intellij.plugins.postcss.completion;
 
-import com.intellij.codeInsight.completion.*;
-import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.codeInsight.completion.CompletionContributor;
+import com.intellij.codeInsight.completion.CompletionParameters;
+import com.intellij.codeInsight.completion.CompletionResultSet;
 import com.intellij.openapi.project.DumbAware;
-import com.intellij.patterns.PatternCondition;
-import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.css.CssBlock;
 import com.intellij.psi.css.CssRuleset;
-import com.intellij.psi.css.descriptor.CssMediaType;
 import com.intellij.psi.css.impl.CssElementTypes;
-import com.intellij.psi.css.impl.CssMediaQueryImpl;
 import com.intellij.psi.css.impl.util.completion.CssAddSpaceWithBracesInsertHandler;
 import com.intellij.psi.css.util.CssCompletionUtil;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.util.ProcessingContext;
 import org.intellij.plugins.postcss.completion.handler.PostCssOneLineAtRuleInsertHandler;
 import org.intellij.plugins.postcss.psi.PostCssPsiUtil;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Locale;
-
-import static com.intellij.patterns.PlatformPatterns.psiElement;
-
 public class PostCssDumbAwareCompletionContributor extends CompletionContributor implements DumbAware {
-  public PostCssDumbAwareCompletionContributor() {
-    extend(CompletionType.BASIC, mediaType(), new CssMediaTypeCompletionProvider());
-  }
 
   public void fillCompletionVariants(@NotNull final CompletionParameters parameters, @NotNull CompletionResultSet result) {
     result = CssCompletionUtil.fixPrefixForVendorPrefixes(parameters, result);
@@ -57,27 +46,4 @@ public class PostCssDumbAwareCompletionContributor extends CompletionContributor
     }
   }
 
-  private static PsiElementPattern.Capture<PsiElement> mediaType() {
-    return psiElement(CssElementTypes.CSS_IDENT).withParent(CssMediaQueryImpl.class)
-      .with(new PatternCondition<PsiElement>("inside PostCSS") {
-        @Override
-        public boolean accepts(@NotNull PsiElement element, ProcessingContext context) {
-          return PostCssPsiUtil.isInsidePostCss(element);
-        }
-      });
-  }
-
-  //TODO use com.intellij.psi.css.impl.util.completion.provider.CssMediaTypeCompletionProvider when PostCSS will be pat of API
-  private static class CssMediaTypeCompletionProvider extends CompletionProvider<CompletionParameters> {
-    @Override
-    protected void addCompletions(@NotNull CompletionParameters parameters,
-                                  ProcessingContext context,
-                                  @NotNull CompletionResultSet result) {
-      for (CssMediaType type : CssMediaType.values()) {
-        if (type != CssMediaType.UNKNOWN) {
-          result.addElement(LookupElementBuilder.create(type.name().toLowerCase(Locale.US)));
-        }
-      }
-    }
-  }
 }
