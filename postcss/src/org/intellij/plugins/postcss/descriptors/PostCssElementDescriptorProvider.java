c15919dad63fcf1694f90f966e88c62551b6757b
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2016-08-23 21:56:34 +0300

    PostCSS: fix tests

--- postcss/src/org/intellij/plugins/postcss/descriptors/PostCssElementDescriptorProvider.java
+++ postcss/src/org/intellij/plugins/postcss/descriptors/PostCssElementDescriptorProvider.java
@@ -36,7 +36,7 @@ public class PostCssElementDescriptorProvider extends CssElementDescriptorProvid
       }
       PsiElement identifier = ((CssPseudoClass)context).getNameIdentifier();
       String selectorName = identifier != null ? identifier.getText() : null;
-      if (selectorName == null || selectorName.startsWith("--")) {
+      if (selectorName == null || !selectorName.startsWith("--")) {
         return Collections.emptyList();
       }
       Collection<PostCssCustomSelector> customSelectors = PostCssCustomSelectorIndex.getCustomSelectors(selectorName.substring(2), context);
