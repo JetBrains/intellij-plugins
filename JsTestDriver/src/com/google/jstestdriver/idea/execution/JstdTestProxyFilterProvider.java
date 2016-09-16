680bb9c540de4b466f0d06cc98074f86fccc75d7
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2016-09-16 14:48:20 +0300

    move browser filters to new API

--- JsTestDriver/src/com/google/jstestdriver/idea/execution/JstdTestProxyFilterProvider.java
+++ JsTestDriver/src/com/google/jstestdriver/idea/execution/JstdTestProxyFilterProvider.java
@@ -2,23 +2,14 @@ package com.google.jstestdriver.idea.execution;
 
 import com.intellij.execution.filters.Filter;
 import com.intellij.execution.testframework.sm.runner.TestProxyFilterProvider;
-import com.intellij.javascript.testFramework.util.BrowserStacktraceFilter;
+import com.intellij.javascript.testFramework.util.BrowserStacktraceFilters;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.util.Function;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
 
-/**
-* @author Sergey Simonchik
-*/
 public class JstdTestProxyFilterProvider implements TestProxyFilterProvider {
-
-  private static final String DEFAULT_PATH_PREFIX = "/test/";
 
   private final Project myProject;
 
@@ -32,8 +23,7 @@ public class JstdTestProxyFilterProvider implements TestProxyFilterProvider {
     if ("browser".equals(nodeType) && nodeArguments != null) {
       final File basePath = new File(nodeArguments);
       if (basePath.isAbsolute() && basePath.isDirectory()) {
-        Function<String, File> fileFinder = path -> findFileByPath(basePath, path);
-        return new BrowserStacktraceFilter(myProject, nodeName, fileFinder);
+        return BrowserStacktraceFilters.createFilter(nodeName, myProject, basePath.getAbsolutePath());
       }
     }
     if ("browserError".equals(nodeType) && nodeArguments != null) {
@@ -44,33 +34,4 @@ public class JstdTestProxyFilterProvider implements TestProxyFilterProvider {
     }
     return null;
   }
-
-  @Nullable
-  private static File findFileByPath(@NotNull File basePath, @NotNull String urlStr) {
-    File file = findFileByBasePath(basePath, urlStr);
-    if (file != null) {
-      return file;
-    }
-    try {
-      URL url = new URL(urlStr);
-      String path = url.getPath();
-      path = StringUtil.trimStart(path, DEFAULT_PATH_PREFIX);
-      return findFileByBasePath(basePath, path);
-    } catch (MalformedURLException ignored) {
-    }
-    return null;
-  }
-
-  @Nullable
-  private static File findFileByBasePath(@NotNull File basePath, @NotNull String subPath) {
-    File file = new File(basePath, subPath);
-    if (!file.isFile()) {
-      File absoluteFile = new File(subPath);
-      if (absoluteFile.isAbsolute() && absoluteFile.isFile()) {
-        file = absoluteFile;
-      }
-    }
-    return file.isFile() ? file : null;
-  }
-
 }
--- js-karma/src/com/intellij/javascript/karma/tree/KarmaBrowserErrorFilter.java
+++ js-karma/src/com/intellij/javascript/karma/tree/KarmaBrowserErrorFilter.java
@@ -1,165 +1,31 @@
 package com.intellij.javascript.karma.tree;
 
-import com.intellij.execution.filters.Filter;
-import com.intellij.execution.filters.HyperlinkInfo;
-import com.intellij.execution.filters.OpenFileHyperlinkInfo;
+import com.intellij.execution.filters.PatternBasedFileHyperlinkFilter;
+import com.intellij.execution.filters.PatternBasedFileHyperlinkRawDataFinder;
+import com.intellij.execution.filters.PatternHyperlinkFormat;
+import com.intellij.execution.filters.PatternHyperlinkPart;
 import com.intellij.javascript.karma.KarmaConfig;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VfsUtil;
-import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * @author Sergey Simonchik
- */
-public class KarmaBrowserErrorFilter implements Filter {
+public class KarmaBrowserErrorFilter extends PatternBasedFileHyperlinkFilter {
 
-  private static final Pattern[] LINE_PATTERNS = new Pattern[] {
-    //at http://localhost:9876/base/spec/personSpec.js?1368878723000:22
-    Pattern.compile("^\\s*at (http://[^:]+:\\d+/base/([^?]+).*)$"),
-
-    //at http://localhost:9876/absolute/home/segrey/WebstormProjects/karma-chai-sample/test/test.js?1378466989000:1
-    Pattern.compile("^\\s*at (http://[^:]+:\\d+/absolute([^?]+).*)$")
-  };
-
-  private final Project myProject;
-  private final KarmaConfig myConfig;
+  public static final PatternBasedFileHyperlinkRawDataFinder FINDER = new PatternBasedFileHyperlinkRawDataFinder(
+    new PatternHyperlinkFormat[] {
+      //at http://localhost:9876/base/spec/personSpec.js?1368878723000:22
+      new PatternHyperlinkFormat(Pattern.compile("^\\s*at (http://[^:]+:\\d+/base/([^?]+).*(:\\d+))$"),
+                                 true, true,
+                                 PatternHyperlinkPart.HYPERLINK, PatternHyperlinkPart.PATH, PatternHyperlinkPart.LINE),
+      //at http://localhost:9876/absolute/home/segrey/WebstormProjects/karma-chai-sample/test/test.js?1378466989000:1
+      new PatternHyperlinkFormat(Pattern.compile("^\\s*at (http://[^:]+:\\d+/absolute([^?]+).*(:\\d+))$"),
+                                 true, true,
+                                 PatternHyperlinkPart.HYPERLINK, PatternHyperlinkPart.PATH, PatternHyperlinkPart.LINE),
+    }
+  );
 
   public KarmaBrowserErrorFilter(@NotNull Project project, @NotNull KarmaConfig config) {
-    myProject = project;
-    myConfig = config;
+    super(project, config.getBasePath(), FINDER);
   }
-
-  @Nullable
-  @Override
-  public Result applyFilter(String line, int entireLength) {
-    LinkInfo linkInfo = createLinkInfo(line);
-    if (linkInfo != null) {
-      File file = findFile(linkInfo.getPath());
-      if (file != null) {
-        VirtualFile virtualFile = VfsUtil.findFileByIoFile(file, false);
-        if (virtualFile != null) {
-          int lineNumber = Math.max(-1, linkInfo.getLineNumber() - 1);
-          HyperlinkInfo link = new OpenFileHyperlinkInfo(myProject, virtualFile, lineNumber);
-          return new Filter.Result(linkInfo.getHyperlinkStartInd(), linkInfo.getHyperlinkEndInd(), link);
-        }
-      }
-    }
-    return null;
-  }
-
-  @Nullable
-  public static LinkInfo createLinkInfo(@NotNull String line) {
-    Matcher m = findMatcher(line);
-    if (m == null) {
-      return null;
-    }
-    int hyperlinkStartInd = m.start(1);
-    int hyperlinkEndInd = m.end(1);
-    String path = m.group(2);
-    int lineNumber = getLineFrom(m.group(1));
-    return new LinkInfo(hyperlinkStartInd, hyperlinkEndInd, path, lineNumber);
-  }
-
-  @Nullable
-  private static Matcher findMatcher(@NotNull String line) {
-    for (Pattern pattern : LINE_PATTERNS) {
-      Matcher m = pattern.matcher(line);
-      if (m.matches()) {
-        return m;
-      }
-    }
-    return null;
-  }
-
-  @Nullable
-  private File findFile(@NotNull String path) {
-    File absFile = new File(path);
-    if (absFile.isAbsolute() && absFile.isFile()) {
-      return absFile;
-    }
-    String basePath = myConfig.getBasePath();
-    File baseDir = new File(basePath);
-    if (baseDir.isDirectory()) {
-      File file = new File(baseDir, path);
-      if (file.isFile()) {
-        return file;
-      }
-    }
-    return null;
-  }
-
-  private static int getLineFrom(@NotNull String hyperlink) {
-    int lastColonInd = hyperlink.lastIndexOf(':');
-    if (lastColonInd == -1) {
-      return -1;
-    }
-    String lineStr = hyperlink.substring(lastColonInd + 1);
-    try {
-      return Integer.parseInt(lineStr);
-    }
-    catch (NumberFormatException e) {
-      return -1;
-    }
-  }
-
-  public static class LinkInfo {
-    private final int myHyperlinkStartInd;
-    private final int myHyperlinkEndInd;
-    private final String myPath;
-    private final int myLineNumber;
-
-    public LinkInfo(int hyperlinkStartInd, int hyperlinkEndInd, @NotNull String path, int lineNumber) {
-      myHyperlinkStartInd = hyperlinkStartInd;
-      myHyperlinkEndInd = hyperlinkEndInd;
-      myPath = path;
-      myLineNumber = lineNumber;
-    }
-
-    public int getHyperlinkStartInd() {
-      return myHyperlinkStartInd;
-    }
-
-    public int getHyperlinkEndInd() {
-      return myHyperlinkEndInd;
-    }
-
-    public String getPath() {
-      return myPath;
-    }
-
-    public int getLineNumber() {
-      return myLineNumber;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-
-      LinkInfo linkInfo = (LinkInfo)o;
-
-      if (myHyperlinkEndInd != linkInfo.myHyperlinkEndInd) return false;
-      if (myHyperlinkStartInd != linkInfo.myHyperlinkStartInd) return false;
-      if (myLineNumber != linkInfo.myLineNumber) return false;
-      if (!myPath.equals(linkInfo.myPath)) return false;
-
-      return true;
-    }
-
-    @Override
-    public int hashCode() {
-      int result = myHyperlinkStartInd;
-      result = 31 * result + myHyperlinkEndInd;
-      result = 31 * result + myPath.hashCode();
-      result = 31 * result + myLineNumber;
-      return result;
-    }
-  }
-
 }
