8955b2465383a2b956e567570c3535570a2d49ae
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2016-09-20 20:39:07 +0300

    Test for optional new/const in Dart

--- Dart/testData/parsing/HardCases1.dart
+++ Dart/testData/parsing/HardCases1.dart
@@ -12,6 +12,26 @@ main(){
   }
 }
 
+class Foo {
+  @Foo(parameters: [[1, 1], [2, 4]])
+  foo() {
+    bar( themes: const {
+      "Default": const Theme(const {LoginForm: const _LoginForm("pure-form")}),
+      "Stacked": const Theme(const {LoginForm: const _LoginForm("pure-form pure-form-stacked")})
+    });
+
+    bar( themes: {
+      "Default": Theme({LoginForm: _LoginForm("pure-form")}),
+      "Stacked": Theme({LoginForm: _LoginForm("pure-form pure-form-stacked")})
+    });
+
+    var now = new DateTime();
+    var elm = new Element.html('<u>54</u>');
+
+    var now1 = DateTime();
+    var elm1 = Element.html('<u>54</u>');
+  }
+}
 
 void get _process
   native "return process;";
