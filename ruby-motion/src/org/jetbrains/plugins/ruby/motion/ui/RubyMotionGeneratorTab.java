ae1325def66e55bbafb0b458714de4427a65f153
Dmitry.Krasilschikov <kradima@jetbrains.com>
2016-10-30 13:04:03 +0300

    replace implicit sdks combo with RubySdkPanel

--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/ui/RubyMotionGeneratorTab.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/ui/RubyMotionGeneratorTab.java
@@ -1,33 +1,22 @@
 package org.jetbrains.plugins.ruby.motion.ui;
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
 import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.projectRoots.impl.SdkConfigurationUtil;
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.ui.VerticalFlowLayout;
 import com.intellij.ui.EnumComboBoxModel;
-import com.intellij.ui.SpeedSearchBase;
-import com.intellij.util.NullableConsumer;
 import com.intellij.util.PlatformUtils;
-import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.ruby.RBundle;
+import org.jetbrains.plugins.ruby.RubySdkPanel;
 import org.jetbrains.plugins.ruby.motion.RubyMotionUtilImpl;
 import org.jetbrains.plugins.ruby.rails.facet.ui.wizard.ui.RubyFacetEditorTab;
 import org.jetbrains.plugins.ruby.remote.RubyRemoteInterpreterManager;
 import org.jetbrains.plugins.ruby.ruby.sdk.RubySdkType;
 import org.jetbrains.plugins.ruby.ruby.sdk.RubySdkUtil;
-import org.jetbrains.plugins.ruby.ruby.sdk.jruby.JRubySdkType;
-import org.jetbrains.plugins.ruby.ruby.ui.RubySdkRenderer;
-import org.jetbrains.plugins.ruby.utils.RubyUIUtil;
-import org.jetbrains.plugins.ruby.version.management.rbenv.gemsets.RbenvGemsetManager;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -37,16 +26,11 @@ import java.util.List;
  * @author Dennis.Ushakov
  */
 public class RubyMotionGeneratorTab extends RubyFacetEditorTab {
-  private static final String ADD_RUBY_SDK = "Add Ruby SDK...";
   private static final int COMBO_WIDTH = PlatformUtils.isRubyMine() ? 320 : 150;
-
-  public static final Condition<Sdk> IS_PURE_RBENV_SDK = sdk -> !RbenvGemsetManager.isGemsetSdk(sdk);
-
   private final RubyMotionSettingsHolder mySettingsHolder;
-  private ComboBox myRubyInterpreterComboBox;
+  private JPanel mySettingsPane;
   private JPanel myContentPane;
   private ComboBox myProjectType;
-  private JLabel myRubyLabel;
   private JLabel myProjectTypeLabel;
 
   public RubyMotionGeneratorTab(final RubyMotionSettingsHolder settingsHolder) {
@@ -59,31 +43,26 @@ public class RubyMotionGeneratorTab extends RubyFacetEditorTab {
     return RBundle.message("ruby.motion.wizard.tab.project.generator.title");
   }
 
+  static class SdkPanel extends RubySdkPanel{
+    @Override
+    protected Sdk[] getSdks() {
+      List<Sdk> sdkList = new ArrayList<>();
+      for (Sdk sdk : ProjectJdkTable.getInstance().getSdksOfType(RubySdkType.getInstance())) {
+        final RubyRemoteInterpreterManager manager = RubyRemoteInterpreterManager.getInstance();
+        if (!RubySdkUtil.isRuby18(sdk) && !manager.isRemoteSdk(sdk)) {
+          sdkList.add(sdk);
+        }
+      }
+      return sdkList.toArray(new Sdk[0]);
+    }
+  }
+
   @NotNull
   @Override
   public JComponent createComponent() {
-    myRubyLabel.setDisplayedMnemonic('S');
-    myRubyInterpreterComboBox.setRenderer(new RubySdkRenderer(true, true));
-    refreshSdkList();
-    if (mySettingsHolder.getSdk() != null) {
-      myRubyInterpreterComboBox.setSelectedItem(mySettingsHolder.getSdk());
-    }
-    myRubyInterpreterComboBox.addActionListener(new ActionListener() {
-      @Override
-      public void actionPerformed(ActionEvent e) {
-        if (!SpeedSearchBase.hasActiveSpeedSearch(myRubyInterpreterComboBox)) {
-          final Object selectedItem = myRubyInterpreterComboBox.getSelectedItem();
-          if (ADD_RUBY_SDK.equals(selectedItem)) {
-            addSdk();
-            return;
-          }
-          final Sdk sdk = (Sdk)selectedItem;
-          if (sdk != null) {
-            mySettingsHolder.setSdk(sdk);
-          }
-        }
-      }
-    });
+    final JPanel sdkPanel = new SdkPanel().getSettingsPanel(mySettingsHolder);
+    myContentPane.add(sdkPanel);
+    myContentPane.add(mySettingsPane);
 
     myProjectTypeLabel.setDisplayedMnemonic('T');
     myProjectType.setModel(new EnumComboBoxModel<>(RubyMotionUtilImpl.ProjectType.class));
@@ -98,63 +77,20 @@ public class RubyMotionGeneratorTab extends RubyFacetEditorTab {
     return myContentPane;
   }
 
-  private void addSdk() {
-    Project project = CommonDataKeys.PROJECT.getData(DataManager.getInstance().getDataContext(myContentPane));
-    final NullableConsumer<Sdk> onSdkCreatedCallBack = sdk -> {
-      if (sdk != null) {
-        SdkConfigurationUtil.addSdk(sdk);
-        refreshSdkList();
-        myRubyInterpreterComboBox.setSelectedItem(sdk);
-      }
-      else {
-        myRubyInterpreterComboBox.setSelectedItem(mySettingsHolder.getSdk());
-        myRubyInterpreterComboBox.repaint();
-      }
-    };
-    SdkConfigurationUtil.createSdk(project,
-                                   ProjectJdkTable.getInstance().getAllJdks(),
-                                   onSdkCreatedCallBack,
-                                   RubySdkType.getInstance(),
-                                   JRubySdkType.getInstance());
-  }
-
-  private void refreshSdkList() {
-    List<Sdk> sdkList = new ArrayList<>();
-    for (Sdk sdk : ProjectJdkTable.getInstance().getSdksOfType(RubySdkType.getInstance())) {
-      final RubyRemoteInterpreterManager manager = RubyRemoteInterpreterManager.getInstance();
-      if (!RubySdkUtil.isRuby18(sdk) && !manager.isRemoteSdk(sdk)) {
-        sdkList.add(sdk);
-      }
-    }
-
-    sdkList = ContainerUtil.filter(sdkList, IS_PURE_RBENV_SDK);
-
-    myRubyInterpreterComboBox.setModel(RubyUIUtil.createSdkComboboxModel(sdkList, null, ADD_RUBY_SDK));
-  }
-
   @Override
   public boolean isModified() {
     return true;
   }
 
   @Override
-  public void reset() {
-
-  }
-
-  @Override
-  public void disposeUIResources() {
-    myContentPane = null;
-  }
+  public void reset() {}
 
   public void hideInterpreterPanel() {
-    myContentPane.getComponent(0).setVisible(false);
+    mySettingsPane.getComponent(0).setVisible(false);
   }
 
   private void createUIComponents() {
-    myContentPane = new JPanel(new BorderLayout());
-    myRubyInterpreterComboBox = new ComboBox(COMBO_WIDTH);
+    myContentPane = new JPanel(new VerticalFlowLayout());
     myProjectType = new ComboBox(COMBO_WIDTH);
-    RubySdkRenderer.installSdkNameSpeedSearch(myRubyInterpreterComboBox);
   }
 }
