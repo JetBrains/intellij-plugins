75f620ca73ed0d854e6d95d24928f54f18bff97a
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2016-11-11 11:37:50 +0100

    [osgi] ignores refresh errors from Bnd plugins (EA-91531)

--- osmorc/src/org/jetbrains/osgi/bnd/imp/BndProjectImporter.java
+++ osmorc/src/org/jetbrains/osgi/bnd/imp/BndProjectImporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2015 JetBrains s.r.o.
+ * Copyright 2000-2016 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -55,6 +55,7 @@ import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileManager;
 import com.intellij.pom.java.LanguageLevel;
+import com.intellij.util.ObjectUtils;
 import com.intellij.util.PathUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
@@ -600,11 +601,17 @@ public class BndProjectImporter {
     }
   }
 
-  private static void refreshRepositories(Workspace workspace) throws Exception {
+  private static void refreshRepositories(Workspace workspace) {
     List<RepositoryPlugin> plugins = workspace.getPlugins(RepositoryPlugin.class);
     for (RepositoryPlugin plugin : plugins) {
       if (plugin instanceof Refreshable) {
-        ((Refreshable)plugin).refresh();
+        try {
+          ((Refreshable)plugin).refresh();
+        }
+        catch (Exception e) {
+          LOG.warn(ObjectUtils.notNull(e.getMessage(), "NPE") + ", plugin=" + plugin);
+          LOG.debug(e);
+        }
       }
     }
   }
