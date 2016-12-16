023d947f2b536ed101e40b22117d351d0d14f527
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2016-12-16 13:29:23 +0300

    Revert "Allow handlebars language to be injected"
    This reverts commit 43dfc9135e60f2d0a9cc27a0130ee16b651dc76e.

--- handlebars/src/com/dmarcotte/handlebars/HbLanguage.java
+++ handlebars/src/com/dmarcotte/handlebars/HbLanguage.java
@@ -1,6 +1,5 @@
 package com.dmarcotte.handlebars;
 
-import com.intellij.lang.InjectableLanguage;
 import com.intellij.lang.Language;
 import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.fileTypes.StdFileTypes;
@@ -9,7 +8,7 @@ import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class HbLanguage extends Language implements TemplateLanguage, InjectableLanguage {
+public class HbLanguage extends Language implements TemplateLanguage {
   public static final HbLanguage INSTANCE = new HbLanguage();
 
   @SuppressWarnings("SameReturnValue") // ideally this would be public static, but the static inits in the tests get cranky when we do that
