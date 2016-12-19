4123441ad636ed51a5003efa23c8e49b8fa23b18
irengrig <Irina.Chernushina@jetbrains.com>
2016-12-19 10:45:46 +0100

    javascript: dependency diagram: angular(1) extension to show angular modules dependencies on module diagram + some ui improvements (show provider icon next to exported member, mark with a color)

--- AngularJS/src/org/angularjs/codeInsight/attributes/AngularModulesProvider.java
+++ AngularJS/src/org/angularjs/codeInsight/attributes/AngularModulesProvider.java
@@ -1,20 +1,38 @@
 package org.angularjs.codeInsight.attributes;
 
 import com.intellij.lang.javascript.modules.diagramm.JSModuleConnectionProvider;
+import com.intellij.lang.javascript.psi.JSCallExpression;
+import com.intellij.lang.javascript.psi.JSFile;
+import com.intellij.lang.javascript.psi.stubs.JSImplicitElement;
+import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
-import com.intellij.ui.JBColor;
+import com.intellij.psi.SmartPointerManager;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.stubs.StubIndex;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.CommonProcessors;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.util.indexing.IdFilter;
+import icons.AngularJSIcons;
+import org.angularjs.index.AngularIndexUtil;
+import org.angularjs.index.AngularModuleIndex;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author Irina.Chernushina on 12/15/2016.
  */
 public class AngularModulesProvider implements JSModuleConnectionProvider {
+  public static final Color ANGULAR_COLOR = new Color(221, 27, 22);
+
   @Override
   public Color getEdgeColor() {
-    return JBColor.RED;
+    return ANGULAR_COLOR;
   }
 
   @Override
@@ -22,12 +40,51 @@ public class AngularModulesProvider implements JSModuleConnectionProvider {
     return "AngularJS";
   }
 
+  @Nullable
   @Override
-  public List<Link > getDependencies(@NotNull PsiFile file) {
-    // todo debug
-    //final SmartPointerManager spm = SmartPointerManager.getInstance(file.getProject());
-    //return Collections.singletonList(
-    //  new Link(spm.createSmartPsiElementPointer(file), spm.createSmartPsiElementPointer(file), file.getName()));
-    return null;
+  public Icon getIcon() {
+    return AngularJSIcons.AngularJS;
+  }
+
+  @Override
+  public List<Link> getDependencies(@NotNull PsiFile file) {
+    final Project project = file.getProject();
+    if (!AngularIndexUtil.hasAngularJS(project) && !AngularIndexUtil.hasAngularJS2(project)) return null;
+    if (!(file instanceof JSFile)) return null;
+
+    final SmartPointerManager spm = SmartPointerManager.getInstance(project);
+    final List<Link> result = new ArrayList<>();
+    final CommonProcessors.CollectProcessor<String> processor = new CommonProcessors.CollectProcessor<>();
+    final GlobalSearchScope fileScope = GlobalSearchScope.fileScope(file);
+    final int fileId = FileBasedIndex.getFileId(file.getVirtualFile());
+    StubIndex.getInstance().processAllKeys(AngularModuleIndex.KEY, processor, fileScope,
+                                           new IdFilter() {
+                                             @Override
+                                             public boolean containsFileId(int id) {
+                                               return id == fileId;
+                                             }
+                                           });
+    for (String key : processor.getResults()) {
+      AngularIndexUtil.multiResolve(project, AngularModuleIndex.KEY, key,
+                                    element -> {
+        if (!file.equals(element.getContainingFile())) return true;
+        // todo whether it would blink
+                                      final JSCallExpression expression = PsiTreeUtil.getParentOfType(element, JSCallExpression.class);
+                                      if (expression != null) {
+                                        final List<String> dependencies = AngularModuleIndex.findDependenciesInModuleDeclaration(expression);
+                                        if (dependencies != null) {
+                                          for (String dependency : dependencies) {
+                                            final JSImplicitElement resolve = AngularIndexUtil.resolve(project, AngularModuleIndex.KEY, dependency);
+                                            if (resolve != null) {
+                                              result.add(new Link(spm.createSmartPsiElementPointer(element.getNavigationElement()),
+                                                                  spm.createSmartPsiElementPointer(resolve.getNavigationElement()), key, resolve.getName()));
+                                            }
+                                          }
+                                        }
+                                      }
+                                      return true;
+                                    });
+    }
+    return result;
   }
 }
