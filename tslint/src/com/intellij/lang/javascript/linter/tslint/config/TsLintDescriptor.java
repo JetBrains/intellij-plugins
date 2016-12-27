57e14a38accc939b38d1c856b8d3f02f830553c5
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2016-12-27 18:25:18 +0300

    review fix: revert descriptors to nearly-original condition. move initialization inside runWhenProjectIsInitialized otherwise order is hard to predict.

--- tslint/src/com/intellij/lang/javascript/linter/tslint/config/TsLintDescriptor.java
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/config/TsLintDescriptor.java
@@ -2,7 +2,7 @@ package com.intellij.lang.javascript.linter.tslint.config;
 
 import com.intellij.lang.javascript.linter.JSLinterConfigFileUtil;
 import com.intellij.lang.javascript.linter.JSLinterConfiguration;
-import com.intellij.lang.javascript.linter.JSLinterDescriptorBase;
+import com.intellij.lang.javascript.linter.JSLinterDescriptor;
 import com.intellij.lang.javascript.linter.tslint.ide.TsLintConfigFileType;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * @author Irina.Chernushina on 11/24/2016.
  */
-public class TsLintDescriptor extends JSLinterDescriptorBase {
+public class TsLintDescriptor extends JSLinterDescriptor {
 
   @Nullable
   @Override
