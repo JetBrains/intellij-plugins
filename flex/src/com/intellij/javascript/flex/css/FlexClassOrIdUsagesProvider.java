5c99552325fb9aa708ac88b5a653ac226b8b1319
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2016-12-30 14:43:06 +0300

    CSS: consider CSS selectors used if they show up in JS string literals (WEB-4585)

--- flex/src/com/intellij/javascript/flex/css/FlexClassOrIdUsagesProvider.java
+++ flex/src/com/intellij/javascript/flex/css/FlexClassOrIdUsagesProvider.java
@@ -2,26 +2,15 @@ package com.intellij.javascript.flex.css;
 
 import com.intellij.lang.javascript.psi.JSLiteralExpression;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiReference;
-import com.intellij.psi.css.CssSelectorSuffix;
 import com.intellij.psi.css.CssTerm;
 import com.intellij.psi.css.impl.CssTermTypes;
-import com.intellij.psi.css.usages.CssClassOrIdUsagesProvider;
+import com.intellij.psi.css.usages.CssClassOrIdReferenceBasedUsagesProvider;
 import org.jetbrains.annotations.NotNull;
 
-/**
- * Created by IntelliJ IDEA.
- * User: Eugene.Kudelevsky
- * Date: Mar 7, 2010
- * Time: 5:40:52 PM
- */
-public class FlexClassOrIdUsagesProvider implements CssClassOrIdUsagesProvider {
-  public boolean isUsage(@NotNull CssSelectorSuffix selectorSuffix, @NotNull PsiElement candidate, int offsetInCandidate) {
-    if ((candidate instanceof CssTerm && ((CssTerm)candidate).getTermType() == CssTermTypes.IDENT) ||
-        candidate instanceof JSLiteralExpression) {
-      final PsiReference ref = candidate.findReferenceAt(offsetInCandidate);
-      return ref != null && ref.isReferenceTo(selectorSuffix);
-    }
-    return false;
+public class FlexClassOrIdUsagesProvider extends CssClassOrIdReferenceBasedUsagesProvider {
+  @Override
+  protected boolean acceptElement(@NotNull PsiElement candidate) {
+    return (candidate instanceof CssTerm && ((CssTerm)candidate).getTermType() == CssTermTypes.IDENT) ||
+           candidate instanceof JSLiteralExpression;
   }
 }
