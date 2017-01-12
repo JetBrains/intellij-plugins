ebd92af93c26bacba9e64b0fa4f7dea46dfee473
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-01-12 13:26:43 +0300

    vue icons

---
+++ vuejs/gen/icons/VuejsIcons.java
@@ -0,0 +1,17 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run build/scripts/icons.gant instead
+ */
+public class VuejsIcons {
+  private static Icon load(String path) {
+    return IconLoader.getIcon(path, VuejsIcons.class);
+  }
+
+  public static final Icon Vue = load("/icons/vue.png"); // 16x16
+}
