2aa54fbbed20fd7d574d0e5d6963382d12727ddf
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2017-01-27 17:00:34 +0300

    remove shortcutter; fix deadlock on CopyTextCommand
    GitOrigin-RevId: d04fdab596578b5f75bd6be0fa1dac66a93c8fe6

--- ide-features-trainer/src/training/commands/CopyTextCommand.java
+++ ide-features-trainer/src/training/commands/CopyTextCommand.java
@@ -1,14 +1,14 @@
 package training.commands;
 
-import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.command.undo.BasicUndoableAction;
 import com.intellij.openapi.command.undo.DocumentReference;
 import com.intellij.openapi.command.undo.DocumentReferenceManager;
 import com.intellij.openapi.command.undo.UndoManager;
 import com.intellij.openapi.editor.EditorGutter;
 import com.intellij.openapi.editor.ex.EditorGutterComponentEx;
-import com.intellij.openapi.util.Computable;
 import com.intellij.psi.PsiDocumentManager;
+import com.intellij.util.DocumentUtil;
 import org.jdom.Element;
 
 /**
@@ -16,7 +16,7 @@ import org.jdom.Element;
  */
 public class CopyTextCommand extends Command {
 
-    public CopyTextCommand(){
+    CopyTextCommand() {
         super(CommandType.COPYTEXT);
     }
 
@@ -27,29 +27,26 @@ public class CopyTextCommand extends Command {
 //        updateDescription(element, infoPanel, editor);
 
         final String finalText = (element.getContent().isEmpty() ? "" : element.getContent().get(0).getValue());
-        if(WriteCommandAction.runWriteCommandAction(executionList.getProject(), new Computable<Boolean>() {
+        DocumentUtil.writeInRunUndoTransparentAction(() -> {
+            final DocumentReference documentReference = DocumentReferenceManager.getInstance().create(executionList.getEditor().getDocument());
+            UndoManager.getInstance(executionList.getProject()).nonundoableActionPerformed(documentReference, false);
+            executionList.getEditor().getDocument().insertString(0, finalText);
+        });
+
+        PsiDocumentManager.getInstance(executionList.getProject()).commitDocument(executionList.getEditor().getDocument());
+
+        CommandProcessor.getInstance().executeCommand(executionList.getProject(), () -> {UndoManager.getInstance(executionList.getProject()).undoableActionPerformed(new BasicUndoableAction() {
             @Override
-            public Boolean compute() {
-                final DocumentReference documentReference = DocumentReferenceManager.getInstance().create(executionList.getEditor().getDocument());
-                UndoManager.getInstance(executionList.getProject()).nonundoableActionPerformed(documentReference, false);
-                executionList.getEditor().getDocument().insertString(0, finalText);
-                PsiDocumentManager.getInstance(executionList.getProject()).commitDocument(executionList.getEditor().getDocument());
-
-                updateGutter(executionList);
-
-                UndoManager.getInstance(executionList.getProject()).undoableActionPerformed(new BasicUndoableAction() {
-                    @Override
-                    public void undo() {
-                    }
-
-                    @Override
-                    public void redo() {
-                    }
-                });
-
-                return true;
+            public void undo() {
             }
-        })) startNextCommand(executionList);
+
+            @Override
+            public void redo() {
+            }
+        });}, null, null);
+        updateGutter(executionList);
+
+        startNextCommand(executionList);
     }
 
     private void updateGutter(ExecutionList executionList) {
