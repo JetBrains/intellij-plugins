e762db710ba1b1c4fc9cc997c05251ff4bdea6c4
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2017-01-27 22:49:26 +0300

    Cleanup (dead code; obsolete dependencies)

--- osmorc/src_test/org/jetbrains/osgi/project/BundleManifestTest.kt
+++ osmorc/src_test/org/jetbrains/osgi/project/BundleManifestTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright 2000-2017 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,11 +15,8 @@
  */
 package org.jetbrains.osgi.project
 
+import org.junit.Assert.*
 import org.junit.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFalse
-import kotlin.test.assertNull
-import kotlin.test.assertTrue
 
 class BundleManifestTest {
   @Test fun bundleSymbolicName() {
