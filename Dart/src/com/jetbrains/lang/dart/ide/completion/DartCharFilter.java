70255eb1253d0ea32973da4daf4de55a1cafeaa6
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-01-27 17:40:31 +0300

    WEB-25158 Colon should not be used to accept URI completion

---
+++ Dart/src/com/jetbrains/lang/dart/ide/completion/DartCharFilter.java
@@ -0,0 +1,24 @@
+package com.jetbrains.lang.dart.ide.completion;
+
+import com.intellij.codeInsight.lookup.CharFilter;
+import com.intellij.codeInsight.lookup.Lookup;
+import com.intellij.psi.PsiElement;
+import com.jetbrains.lang.dart.psi.DartStringLiteralExpression;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class DartCharFilter extends CharFilter {
+  @Nullable
+  @Override
+  public Result acceptChar(final char c, final int prefixLength, @NotNull final Lookup lookup) {
+    if (!lookup.isCompletion()) return null;
+
+    final PsiElement element = lookup.getPsiElement();
+    final PsiElement parent = element == null ? null : element.getParent();
+    if (parent instanceof DartStringLiteralExpression) {
+      return Result.ADD_TO_PREFIX;
+    }
+
+    return null;
+  }
+}
