8d1be028354e13586044deb5927471e524afae95
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-02-02 00:02:03 +0300

    search for component selector usages too (WEB-23370)

--- AngularJS/src/org/angularjs/findUsages/AngularJSFindUsagesHandlerFactory.java
+++ AngularJS/src/org/angularjs/findUsages/AngularJSFindUsagesHandlerFactory.java
@@ -1,8 +1,16 @@
 package org.angularjs.findUsages;
 
+import com.intellij.find.findUsages.FindUsagesHandler;
 import com.intellij.lang.javascript.findUsages.JavaScriptFindUsagesHandlerFactory;
+import com.intellij.lang.javascript.psi.JSCallExpression;
+import com.intellij.lang.javascript.psi.ecma6.ES6Decorator;
+import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList;
+import com.intellij.lang.javascript.psi.ecmal4.JSClass;
+import com.intellij.lang.javascript.psi.stubs.JSElementIndexingData;
 import com.intellij.psi.PsiElement;
+import com.intellij.util.ArrayUtil;
 import org.angularjs.codeInsight.DirectiveUtil;
+import org.angularjs.index.AngularIndexUtil;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -11,6 +19,30 @@ import org.jetbrains.annotations.NotNull;
 public class AngularJSFindUsagesHandlerFactory extends JavaScriptFindUsagesHandlerFactory {
   @Override
   public boolean canFindUsages(@NotNull PsiElement element) {
-    return DirectiveUtil.getDirective(element) != null;
+    return DirectiveUtil.getDirective(element) != null || element instanceof JSClass;
+  }
+
+  @Override
+  public FindUsagesHandler createFindUsagesHandler(@NotNull PsiElement element, boolean forHighlightUsages) {
+    if (!forHighlightUsages && element instanceof JSClass && AngularIndexUtil.hasAngularJS2(element.getProject())) {
+      return new JavaScriptFindUsagesHandlerFactory.JavaScriptFindUsagesHandler(element) {
+        @NotNull
+        @Override
+        public PsiElement[] getPrimaryElements() {
+          JSAttributeList list = ((JSClass)element).getAttributeList();
+          if (list != null && list.getFirstChild() instanceof ES6Decorator) {
+            PsiElement call = list.getFirstChild().getLastChild();
+            if (call instanceof JSCallExpression) {
+              JSElementIndexingData data = ((JSCallExpression)call).getIndexingData();
+              if (data != null && data.getImplicitElements() != null) {
+                return ArrayUtil.mergeArrays(super.getPrimaryElements(), data.getImplicitElements().toArray(PsiElement.EMPTY_ARRAY));
+              }
+            }
+          }
+          return super.getPrimaryElements();
+        }
+      };
+    }
+    return super.createFindUsagesHandler(element, forHighlightUsages);
   }
 }
