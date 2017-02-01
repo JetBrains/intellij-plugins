776925d4a37fc9407b20b4ce444a27fa2d2348eb
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-02-01 18:30:15 +0300

    WEB-23979 don't show errors from parser for files analyzed by Dart server

---
+++ Dart/src/com/jetbrains/lang/dart/highlight/DartHighlightingErrorFilter.java
@@ -0,0 +1,23 @@
+package com.jetbrains.lang.dart.highlight;
+
+import com.intellij.codeInsight.highlighting.HighlightErrorFilter;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiErrorElement;
+import com.jetbrains.lang.dart.DartLanguage;
+import com.jetbrains.lang.dart.util.DartResolveUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class DartHighlightingErrorFilter extends HighlightErrorFilter {
+  @Override
+  public boolean shouldHighlightErrorElement(@NotNull final PsiErrorElement element) {
+    if (!(element.getLanguage() == DartLanguage.INSTANCE)) return true;
+
+    final VirtualFile file = DartResolveUtil.getRealVirtualFile(element.getContainingFile());
+    if (file != null && file.isInLocalFileSystem() && ProjectFileIndex.getInstance(element.getProject()).isInContent(file)) {
+      return false;
+    }
+
+    return true;
+  }
+}
