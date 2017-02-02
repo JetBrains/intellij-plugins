5db4f56e8a6fa6676792aba0b48f3caddc06b22f
Victor Kropp <victor.kropp@gmail.com>
2017-02-02 17:27:43 +0100

    skip special targets in go to symbol
    GitOrigin-RevId: 5d624c6e5cd8d9739d05e986970b4247f3ac60e8

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileChooseByNameContributor.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileChooseByNameContributor.kt
@@ -4,6 +4,6 @@ import com.intellij.navigation.ChooseByNameContributor
 import com.intellij.openapi.project.Project
 
 class MakefileChooseByNameContributor : ChooseByNameContributor {
-  override fun getItemsByName(name: String, pattern: String, project: Project, includeNonProjectItems: Boolean) = findTargets(project, name).toTypedArray()
-  override fun getNames(project: Project, includeNonProjectItems: Boolean) = findTargets(project).map { it.text }.filterNot(String::isNullOrEmpty).toTypedArray()
+  override fun getItemsByName(name: String, pattern: String, project: Project, includeNonProjectItems: Boolean) = findTargets(project, name).filterNot { it.isSpecialTarget }.toTypedArray()
+  override fun getNames(project: Project, includeNonProjectItems: Boolean) = findTargets(project).filterNot { it.isSpecialTarget }.map { it.text }.filterNot(String::isNullOrEmpty).toTypedArray()
 }
