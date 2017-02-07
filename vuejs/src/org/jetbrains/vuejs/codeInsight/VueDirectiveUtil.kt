de8e7a2754e3d56c0dd240dbd032273731a02592
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-02-07 20:42:29 +0300

    data property of vue component should be a function

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueDirectiveUtil.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueDirectiveUtil.kt
@@ -1,6 +1,10 @@
 package org.jetbrains.vuejs.codeInsight
 
+import com.intellij.lang.ecmascript6.psi.JSExportAssignment
+import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
+import com.intellij.lang.javascript.psi.JSProperty
 import com.intellij.openapi.util.text.StringUtil
+import org.jetbrains.vuejs.VueFileType
 
 fun fromAsset(text: String): String {
   val split = StringUtil.unquoteString(text).split("(?=[A-Z])".toRegex()).dropLastWhile(String::isEmpty).toTypedArray()
@@ -17,4 +21,11 @@ fun toAsset(name: String): String {
   }
   return StringUtil.join(*words)
 }
+
+
+fun isComponent(property: JSProperty): Boolean {
+  return property.parent is JSObjectLiteralExpression && property.parent.parent is JSExportAssignment &&
+         property.containingFile.fileType == VueFileType.INSTANCE
+}
+
 
---
+++ vuejs/src/org/jetbrains/vuejs/inspections/DataFunctionInspection.kt
@@ -0,0 +1,49 @@
+package org.jetbrains.vuejs.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.LocalQuickFixOnPsiElement
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.lang.ecmascript6.psi.ES6FunctionProperty
+import com.intellij.lang.javascript.psi.*
+import com.intellij.lang.javascript.psi.impl.JSChangeUtil
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.PsiFile
+import org.jetbrains.vuejs.codeInsight.isComponent
+
+class DataFunctionInspection : LocalInspectionTool() {
+  override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
+    return object: JSElementVisitor() {
+      override fun visitJSProperty(node: JSProperty?) {
+        if ("data" != node?.name || node?.value is JSFunction || node?.value is JSReferenceExpression) return
+
+
+        if (isComponent(node!!)) {
+          val quickFixes:Array<LocalQuickFix> = if (node.value is JSObjectLiteralExpression) arrayOf(WrapWithFunctionFix(node.value!!)) else emptyArray()
+          holder.registerProblem(node.nameIdentifier!!, "Data property should be a function", *quickFixes)
+        }
+      }
+
+      override fun visitES6FunctionProperty(functionProperty: ES6FunctionProperty?) {
+        return
+      }
+    }
+  }
+}
+
+class WrapWithFunctionFix(psiElement: PsiElement) : LocalQuickFixOnPsiElement(psiElement) {
+  override fun getText() = "Wrap data object in function"
+
+  override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
+    val expression = startElement as JSObjectLiteralExpression
+    val property = expression.parent as JSProperty
+
+    val newProperty = (JSChangeUtil.createExpressionWithContext("{ data() {return ${expression.text}}}", property)!!.psi as JSObjectLiteralExpression).firstProperty
+    property.replace(newProperty)
+  }
+
+  override fun getFamilyName() = "Vue"
+
+}
