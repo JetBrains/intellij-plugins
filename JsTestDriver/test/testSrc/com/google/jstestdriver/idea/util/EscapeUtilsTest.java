2e8d4b10f6f902e0b406ce26a48cb7c2a43c2415
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2017-02-08 15:16:30 +0300

    cleanup EscapeUtils

--- JsTestDriver/test/testSrc/com/google/jstestdriver/idea/util/EscapeUtilsTest.java
+++ JsTestDriver/test/testSrc/com/google/jstestdriver/idea/util/EscapeUtilsTest.java
@@ -1,34 +1,27 @@
 package com.google.jstestdriver.idea.util;
 
 import com.google.jstestdriver.idea.rt.util.EscapeUtils;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * @author Sergey Simonchik
- */
 public class EscapeUtilsTest {
 
-  @org.junit.Test
+  @Test
   public void testJoinSplit() throws Exception {
     List<String> strs = Arrays.asList("a", "", "aba", "abacaba", "_", "q", "\\1\\2\n");
     String encoded = EscapeUtils.join(strs, 'a');
-    System.out.println("encoded: " + encoded);
     List<String> decoded = EscapeUtils.split(encoded, 'a');
-    System.out.println("decoded: " + decoded);
     Assert.assertEquals(strs, decoded);
   }
 
-  @org.junit.Test
+  @Test
   public void testPath() throws Exception {
     List<String> strs = Arrays.asList("C:\\dir\\file1", "C:\\dir\\file2");
     String encoded = EscapeUtils.join(strs, ',');
-    System.out.println("encoded: " + encoded);
     List<String> decoded = EscapeUtils.split(encoded, ',');
-    System.out.println("decoded: " + decoded);
     Assert.assertEquals(strs, decoded);
   }
 
@@ -37,9 +30,25 @@ public class EscapeUtilsTest {
     List<String> list = Arrays.asList("hello", "");
     String encoded = EscapeUtils.join(list, ',');
     Assert.assertEquals("hello,", encoded);
-    System.out.println("encoded: " + encoded);
     List<String> decoded = EscapeUtils.split(encoded, ',');
-    System.out.println("decoded: " + decoded);
+    Assert.assertEquals(list, decoded);
+  }
+
+  @Test
+  public void testEmptyStrings() throws Exception {
+    List<String> list = Arrays.asList("", "", "");
+    String encoded = EscapeUtils.join(list, '.');
+    Assert.assertEquals("..", encoded);
+    List<String> decoded = EscapeUtils.split(encoded, '.');
+    Assert.assertEquals(list, decoded);
+  }
+
+  @Test
+  public void testSeparatorStrings() throws Exception {
+    List<String> list = Arrays.asList(".", "..", "...");
+    String encoded = EscapeUtils.join(list, '.');
+    Assert.assertEquals("\\..\\.\\..\\.\\.\\.", encoded);
+    List<String> decoded = EscapeUtils.split(encoded, '.');
     Assert.assertEquals(list, decoded);
   }
 }
