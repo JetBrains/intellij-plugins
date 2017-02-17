78ad987c870c3cf3fcbb6e230ff9b609dd2bbdd3
Victor Kropp <victor.kropp@gmail.com>
2017-02-17 23:03:46 +0100

    simple completion test
    GitOrigin-RevId: 5a56aa1c5a9fc1901db5df0ab74fb23a0fe3140d

---
+++ makefile/src/test/kotlin/MakefileCompletionTest.kt
@@ -0,0 +1,10 @@
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
+
+class MakefileCompletionTest : LightPlatformCodeInsightFixtureTestCase() {
+  fun testSimple() = doTest("b", "c", "d", "${getTestName(true)}.mk")
+
+  fun doTest(vararg variants: String) = myFixture.testCompletionVariants("$testDataPath/$basePath/${getTestName(true)}.mk", *variants)
+
+  override fun getTestDataPath() = "testData"
+  override fun getBasePath() = "completion"
+}
