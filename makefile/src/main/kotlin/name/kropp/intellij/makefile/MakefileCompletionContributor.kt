07687c23ed4978839e7aed116495135f7b691f83
Victor Kropp <victor.kropp@gmail.com>
2017-02-17 23:03:30 +0100

    do not suggest keyword in rules
    GitOrigin-RevId: 2d7d136c16454c9001118853f3ffa8de9eceb079

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileCompletionContributor.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileCompletionContributor.kt
@@ -9,9 +9,9 @@ import name.kropp.intellij.makefile.psi.MakefileTypes
 class MakefileCompletionContributor : CompletionContributor() {
   private val keywords = listOf("include", "define", "undefine", "override", "export", "private", "vpath", "ifeq", "ifneq", "ifdef", "ifndef")
   init {
-    extend(CompletionType.BASIC, PlatformPatterns.psiElement(MakefileTypes.IDENTIFIER),
+    extend(CompletionType.BASIC, PlatformPatterns.psiElement(MakefileTypes.IDENTIFIER).andNot(PlatformPatterns.psiElement().inside(PlatformPatterns.psiElement(MakefileTypes.RULE))),
         object : CompletionProvider<CompletionParameters>() {
-      override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext?, resultSet: CompletionResultSet) {
+      override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, resultSet: CompletionResultSet) {
         resultSet.addAllElements(keywords.map { LookupElementBuilder.create(it) })
       }
     })
