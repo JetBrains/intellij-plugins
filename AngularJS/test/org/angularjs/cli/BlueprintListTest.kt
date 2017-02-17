69d951ee3e2f6908b05ca40b4dfa97af0182e1ed
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-02-17 22:55:29 +0300

    update test for new @angular/cli help output

--- AngularJS/test/org/angularjs/cli/BlueprintListTest.kt
+++ AngularJS/test/org/angularjs/cli/BlueprintListTest.kt
@@ -11,145 +11,113 @@ import java.util.*
 class BlueprintListTest : LightPlatformTestCase() {
   fun testList() {
     val output = """
+
   Available blueprints:
-    angular-cli:
+    @angular/cli:
       class <name> <options...>
         --spec (Boolean)
+          aliases: -spec
       component <name> <options...>
-        --flat (Boolean) (Default: false)
+        --flat (Boolean)
+          aliases: -flat
         --inline-template (Boolean)
-          aliases: -it
+          aliases: -it, --inlineTemplate
         --inline-style (Boolean)
-          aliases: -is
-        --prefix (Boolean) (Default: true)
+          aliases: -is, --inlineStyle
+        --prefix (String) (Default: null)
+          aliases: --prefix <value>
         --spec (Boolean)
-      component-test <name>
+          aliases: -spec
+        --view-encapsulation (String)
+          aliases: -ve <value>, --viewEncapsulation <value>
+        --change-detection (String)
+          aliases: -cd <value>, --changeDetection <value>
+        --skip-import (Boolean) (Default: false)
+          aliases: --skipImport
+        --module (String)
+          aliases: -m <value>, --module <value>
+        --export (Boolean) (Default: false)
+          aliases: --export
       directive <name> <options...>
-        --flat (Boolean) (Default: true)
-        --prefix (Boolean) (Default: true)
+        --flat (Boolean)
+          aliases: -flat
+        --prefix (String) (Default: null)
+          aliases: --prefix <value>
         --spec (Boolean)
+          aliases: -spec
+        --skip-import (Boolean) (Default: false)
+          aliases: --skipImport
+        --module (String)
+          aliases: -m <value>, --module <value>
+        --export (Boolean) (Default: false)
+          aliases: --export
       enum <name>
       interface <interface-type>
-      mobile <name> <options...>
-        --source-dir (String) (Default: src)
-          aliases: -sd <value>
-        --prefix (String) (Default: app)
-          aliases: -p <value>
-        --mobile (Boolean) (Default: false)
-      model <name> <attr:type>
-        Generates an ng-data model.
       module <name> <options...>
         --spec (Boolean)
+          aliases: -spec
+        --flat (Boolean)
+          aliases: -flat
         --routing (Boolean) (Default: false)
+          aliases: --routing
       ng2 <name> <options...>
         --source-dir (String) (Default: src)
-          aliases: -sd <value>
+          aliases: -sd <value>, --sourceDir <value>
         --prefix (String) (Default: app)
-          aliases: -p <value>
+          aliases: -p <value>, --prefix <value>
         --style (String) (Default: css)
-        --mobile (Boolean) (Default: false)
+          aliases: --style <value>
         --routing (Boolean) (Default: false)
+          aliases: --routing
         --inline-style (Boolean) (Default: false)
-          aliases: -is
+          aliases: -is, --inlineStyle
         --inline-template (Boolean) (Default: false)
-          aliases: -it
+          aliases: -it, --inlineTemplate
+        --skip-git (Boolean) (Default: false)
+          aliases: -sg, --skipGit
       pipe <name> <options...>
-        --flat (Boolean) (Default: true)
+        --flat (Boolean)
+          aliases: -flat
         --spec (Boolean)
-      route <name>
-      route-test <name>
+          aliases: -spec
+        --skip-import (Boolean) (Default: false)
+          aliases: --skipImport
+        --module (String)
+          aliases: -m <value>, --module <value>
+        --export (Boolean) (Default: false)
+          aliases: --export
       service <name> <options...>
-        --flat (Boolean) (Default: true)
+        --flat (Boolean)
+          aliases: -flat
         --spec (Boolean)
-      service-test <name>
-    angular-cli:
-      acceptance-test <name>
-        Generates an acceptance test for a feature.
-      adapter <name> <options...>
-        Generates an ng-data adapter.
-        --base-class (String)
-      adapter-test <name>
-        Generates an ng-data adapter unit test
-      addon <name>
-        The default blueprint for angular-cli addons.
-      addon-import <name>
-        Generates an import wrapper.
-      app <name>
-        The default blueprint for angular-cli projects.
-      blueprint <name>
-        Generates a blueprint and definition.
-      component-addon <name>
-        Generates a component. Name must contain a hyphen.
-      controller <name>
-        Generates a controller.
-      controller-test <name>
-        Generates a controller unit test.
-      helper <name>
-        Generates a helper function.
-      helper-addon <name>
-        Generates an import wrapper.
-      helper-test <name>
-        Generates a helper unit test.
-      http-mock <endpoint-path>
-        Generates a mock api endpoint in /api prefix.
-      http-proxy <local-path> <remote-url>
-        Generates a relative proxy to another server.
-      in-repo-addon <name>
-        The blueprint for addon in repo angular-cli addons.
-      initializer <name>
-        Generates an initializer.
-      initializer-addon <name>
-        Generates an import wrapper.
-      initializer-test <name>
-        Generates an initializer unit test.
-      instance-initializer <name>
-        Generates an instance initializer.
-      instance-initializer-addon <name>
-        Generates an import wrapper.
-      instance-initializer-test <name>
-        Generates an instance initializer unit test.
-      lib <name>
-        Generates a lib directory for in-repo addons.
-      mixin <name>
-        Generates a mixin.
-      mixin-test <name>
-        Generates a mixin unit test.
-      model-test <name>
-        Generates a model unit test.
-      resource <name>
-        Generates a model, template, route, and registers the route with the router.
-      route-addon <name>
-        Generates import wrappers for a route and its template.
-      serializer <name>
-        Generates an ng-data serializer.
-      serializer-test <name>
-        Generates a serializer unit test.
-      server <name>
-        Generates a server directory for mocks and proxies.
-      template <name>
-        Generates a template.
-      test-helper <name>
-        Generates a test helper.
-      transform <name>
-        Generates an ng-data value transform.
-      transform-test <name>
-        Generates a transform unit test.
-      util <name>
-        Generates a simple utility module/function.
-      util-test <name>
-        Generates a util unit test.
-      vendor-shim <name>
-        Generates an ES6 module shim for global libraries.
+          aliases: -spec
+        --module (String)
+          aliases: -m <value>, --module <value>
 
-undefined
+ng generate <blueprint> <options...>
+  Generates new code from blueprints.
+  aliases: g
+  --dry-run (Boolean) (Default: false)
+    aliases: -d, --dryRun
+  --verbose (Boolean) (Default: false)
+    aliases: -v, --verbose
+  --pod (Boolean) (Default: false)
+    aliases: -p, -pod
+  --classic (Boolean) (Default: false)
+    aliases: -c, --classic
+  --dummy (Boolean) (Default: false)
+    aliases: -dum, -id, --dummy
+  --in-repo-addon (String) (Default: null)
+    aliases: --in-repo <value>, -ir <value>, --inRepoAddon <value>
 
     """
 
     val blueprints = BlueprintParser().parse(output)
-    val requiredBlueprints = Arrays.asList("class", "component", "model")
+    val requiredBlueprints = Arrays.asList("class", "component", "module")
     val existingBlueprints = ContainerUtil.filter(blueprints) { requiredBlueprints.contains(it.name) }
     TestCase.assertEquals(3, existingBlueprints.size)
-    TestCase.assertEquals(5, existingBlueprints[1].args.size)
-    TestCase.assertNotNull(existingBlueprints[2].description)
+    TestCase.assertEquals(listOf("--flat", "--inline-template", "--inline-style", "--prefix", "--spec",
+                                 "--view-encapsulation", "--change-detection", "--skip-import", "--module", "--export"),
+                          existingBlueprints[1].args)
   }
 }
