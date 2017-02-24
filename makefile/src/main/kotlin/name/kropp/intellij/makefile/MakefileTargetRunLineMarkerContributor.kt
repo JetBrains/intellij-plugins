7d7ed7e8616ba923d19a6db0693084402c883ff8
Victor Kropp <victor.kropp@gmail.com>
2017-02-24 22:38:10 +0100

    do not show line run marker for special targets fixes #32
    Signed-off-by: Victor Kropp <victor.kropp@gmail.com>
    
    GitOrigin-RevId: def630c01c4ab0ef459a974e2e63d440abf52586

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileTargetRunLineMarkerContributor.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileTargetRunLineMarkerContributor.kt
@@ -7,7 +7,10 @@ import name.kropp.intellij.makefile.psi.MakefileRule
 class MakefileTargetRunLineMarkerContributor : RunLineMarkerContributor() {
   override fun getInfo(element: PsiElement): Info? {
     if (element is MakefileRule) {
-      return Info(MakefileTargetIcon, { "" }, element.targets.filterNot { it.isSpecialTarget }.map(::MakefileRunTargetAction).toTypedArray())
+      val targets = element.targets.filterNot { it.isSpecialTarget }
+      if (targets.any()) {
+        return Info(MakefileTargetIcon, { "" }, targets.map(::MakefileRunTargetAction).toTypedArray())
+      }
     }
     return null
   }
