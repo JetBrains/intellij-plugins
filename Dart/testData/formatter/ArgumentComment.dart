dfb7580744cb29292fa730468620ae87894b31ee
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-02-28 12:22:08 +0300

    better dart test initial data

--- Dart/testData/formatter/ArgumentComment.dart
+++ Dart/testData/formatter/ArgumentComment.dart
@@ -9,9 +9,9 @@ class MyApp extends StatelessWidget {
         //
         // Try running your application with "flutter run". You'll see
         // the application has a blue toolbar. Then, without quitting
-        // the app, try changing the primarySwatch below to Colors.green
+      // the app, try changing the primarySwatch below to Colors.green
         // and press "r" in the console where you ran "flutter run".
-        // We call this a "hot reload". Notice that the counter didn't
+          // We call this a "hot reload". Notice that the counter didn't
         // reset back to zero -- the application is not restarted.
         primarySwatch: Colors.blue,
       ),
