844de0af7d1eef90187bf27b08892db0a7574c5d
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2017-03-09 21:25:59 +0300

    rename JSInjectionUtil -> JSFormattableInjectionUtil

--- AngularJS/src/org/angularjs/editor/Angular2Injector.java
+++ AngularJS/src/org/angularjs/editor/Angular2Injector.java
@@ -4,7 +4,7 @@ import com.intellij.lang.Language;
 import com.intellij.lang.css.CSSLanguage;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
-import com.intellij.lang.javascript.inject.JSInjectionUtil;
+import com.intellij.lang.javascript.inject.JSFormattableInjectionUtil;
 import com.intellij.lang.javascript.psi.*;
 import com.intellij.lang.javascript.psi.impl.JSLiteralExpressionImpl;
 import com.intellij.openapi.project.Project;
@@ -90,7 +90,7 @@ public class Angular2Injector implements MultiHostInjector {
 
         final TextRange range = ElementManipulators.getValueTextRange(context);
         registrar.startInjecting(language).addPlace(null, null, (PsiLanguageInjectionHost)context, range).doneInjecting();
-        JSInjectionUtil.setReformattableInjection(registrar);
+        JSFormattableInjectionUtil.setReformattableInjection(registrar);
       }
     }
     return false;
