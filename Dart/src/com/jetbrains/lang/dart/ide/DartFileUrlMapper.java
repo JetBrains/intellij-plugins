cb03dae6dc69f0143e6210446d2e792f61d112e4
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-03-16 22:57:07 +0300

    Correctly map Dart package:... URIs to files when debugging in Chrome

--- Dart/src/com/jetbrains/lang/dart/ide/DartFileUrlMapper.java
+++ Dart/src/com/jetbrains/lang/dart/ide/DartFileUrlMapper.java
@@ -5,16 +5,24 @@ import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.Url;
+import com.intellij.util.Urls;
 import com.jetbrains.javascript.debugger.FileUrlMapper;
 import com.jetbrains.lang.dart.DartFileType;
 import com.jetbrains.lang.dart.sdk.DartSdk;
 import com.jetbrains.lang.dart.util.DartUrlResolver;
+import com.jetbrains.lang.dart.util.PubspecYamlUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.builtInWebServer.WebServerPathToFileManager;
+import org.jetbrains.ide.BuiltInServerManagerImpl;
+
+import java.util.Collections;
+import java.util.List;
 
 import static com.jetbrains.lang.dart.util.DartUrlResolver.PACKAGE_PREFIX;
 import static com.jetbrains.lang.dart.util.PubspecYamlUtil.PUBSPEC_YAML;
@@ -22,6 +30,38 @@ import static com.jetbrains.lang.dart.util.PubspecYamlUtil.PUBSPEC_YAML;
 final class DartFileUrlMapper extends FileUrlMapper {
   private static final String SDK_URL_MARKER = "/packages/$sdk/lib/";
   private static final String PACKAGE_URL_MARKER = "/" + DartUrlResolver.PACKAGES_FOLDER_NAME + "/";
+
+  @NotNull
+  @Override
+  public List<Url> getUrls(@NotNull final VirtualFile file, @NotNull final Project project, @Nullable final String currentAuthority) {
+    if (currentAuthority == null || file.getFileType() != DartFileType.INSTANCE) return Collections.emptyList();
+
+    final String dartUri = DartUrlResolver.getInstance(project, file).getDartUrlForFile(file);
+    if (!dartUri.startsWith(PACKAGE_PREFIX)) return Collections.emptyList();
+
+    if (BuiltInServerManagerImpl.isOnBuiltInWebServerByAuthority(currentAuthority)) {
+      // for built-in server:
+      // package:PackageName/subdir/foo.dart -> http://localhost:63342/PackageName/MayBeModuleName/MayBeRelPathToDartProject/packages/PackageName/subdir/foo.dart
+      final VirtualFile pubspec = PubspecYamlUtil.findPubspecYamlFile(project, file);
+      if (pubspec == null) return Collections.emptyList();
+
+      final VirtualFile dartRoot = pubspec.getParent();
+      final String dartRootUrlPath = WebServerPathToFileManager.getInstance(project).getPath(dartRoot);
+      if (dartRootUrlPath == null) return Collections.emptyList();
+
+      //BuiltInWebBrowserUrlProviderKt.getBuiltInServerUrls(pubspec, project, currentAuthority);
+      final Url dartRootUrl = Urls.newHttpUrl(currentAuthority, "/" + project.getName() + "/" + dartRootUrlPath);
+
+      final String urlPath = StringUtil.trimEnd(dartRootUrl.getPath(), "/") + "/web/packages/" + dartUri.substring(PACKAGE_PREFIX.length());
+      return Collections.singletonList(Urls.newHttpUrl(currentAuthority, urlPath));
+    }
+    else {
+      // for any other server (e.g. localhost:8181):
+      // package:PackageName/subdir/foo.dart -> http://localhost:8181/packages/PackageName/subdir/foo.dart
+      final String urlPath = "/packages/" + dartUri.substring(PACKAGE_PREFIX.length());
+      return Collections.singletonList(Urls.newHttpUrl(currentAuthority, urlPath));
+    }
+  }
 
   @Nullable
   @Override
