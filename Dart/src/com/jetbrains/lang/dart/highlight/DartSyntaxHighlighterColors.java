ab6084c95ffc0d9cd0fe50025665d19f6b4ab5bf
Devon Carew <devoncarew@google.com>
2017-03-22 12:46:34 -0700

    make the errors/warnings/hints display more consistent

--- Dart/src/com/jetbrains/lang/dart/highlight/DartSyntaxHighlighterColors.java
+++ Dart/src/com/jetbrains/lang/dart/highlight/DartSyntaxHighlighterColors.java
@@ -91,9 +91,9 @@ public class DartSyntaxHighlighterColors {
   public static final TextAttributesKey ERROR =
     createTextAttributesKey(DART_ERROR, CodeInsightColors.ERRORS_ATTRIBUTES);
   public static final TextAttributesKey WARNING =
-    createTextAttributesKey(DART_WARNING, CodeInsightColors.ERRORS_ATTRIBUTES);
+    createTextAttributesKey(DART_WARNING, CodeInsightColors.WEAK_WARNING_ATTRIBUTES);
   public static final TextAttributesKey HINT =
-    createTextAttributesKey(DART_HINT, CodeInsightColors.WARNINGS_ATTRIBUTES);
+    createTextAttributesKey(DART_HINT, CodeInsightColors.NOT_USED_ELEMENT_ATTRIBUTES);
 
   public static final TextAttributesKey BLOCK_COMMENT =
     createTextAttributesKey(DART_BLOCK_COMMENT, DefaultLanguageHighlighterColors.BLOCK_COMMENT);
