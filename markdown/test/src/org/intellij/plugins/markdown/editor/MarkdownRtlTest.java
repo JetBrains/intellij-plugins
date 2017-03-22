01c0ea10aa76d5a1c4a99e2d042f526c45ba2e25
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2017-03-22 17:16:51 +0300

    RUBY-19358: proper RTL support for markdown
    (cherry picked from commit 6e2b3db r=IDEA-CR-19625)

--- markdown/test/src/org/intellij/plugins/markdown/editor/MarkdownRtlTest.java
+++ markdown/test/src/org/intellij/plugins/markdown/editor/MarkdownRtlTest.java
@@ -3,7 +3,15 @@ package org.intellij.plugins.markdown.editor;
 import com.intellij.openapi.editor.impl.AbstractRtlTest;
 
 public class MarkdownRtlTest extends AbstractRtlTest {
+  private static final String SAMPLE_TEXT = "RRRR*R* __RRR__ RR,RR RR-RR RR (RRRR) RR 'RRR' \"RR\" <RRRRR> [RR]|\n" +
+                                            "\n" +
+                                            "|RRR:|\n" +
+                                            "\n" +
+                                            "* |RRR!|\n" +
+                                            "* |~RRR~|\n" +
+                                            "|RRRRRR";
+
   public void testBasicCase() throws Exception {
-    checkBidiRunBoundaries("R R", "md");
+    checkBidiRunBoundaries(SAMPLE_TEXT, "md");
   }
 }
