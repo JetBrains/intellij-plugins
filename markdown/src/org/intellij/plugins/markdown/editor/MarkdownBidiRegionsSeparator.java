6e2b3db09751f77d975466b3b9213dd959070500
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2017-03-22 17:16:51 +0300

    RUBY-19358: proper RTL support for markdown

--- markdown/src/org/intellij/plugins/markdown/editor/MarkdownBidiRegionsSeparator.java
+++ markdown/src/org/intellij/plugins/markdown/editor/MarkdownBidiRegionsSeparator.java
@@ -2,10 +2,28 @@ package org.intellij.plugins.markdown.editor;
 
 import com.intellij.openapi.editor.bidi.TokenSetBidiRegionsSeparator;
 import com.intellij.psi.tree.TokenSet;
-import org.intellij.plugins.markdown.lang.MarkdownTokenTypes;
+
+import static org.intellij.plugins.markdown.lang.MarkdownTokenTypes.*;
 
 public class MarkdownBidiRegionsSeparator extends TokenSetBidiRegionsSeparator {
+
+  public static final TokenSet PARAGRAPH_CONTENTS = TokenSet.create(TEXT,
+                                                                    WHITE_SPACE,
+                                                                    SINGLE_QUOTE,
+                                                                    DOUBLE_QUOTE,
+                                                                    LPAREN,
+                                                                    RPAREN,
+                                                                    LBRACKET,
+                                                                    RBRACKET,
+                                                                    LT,
+                                                                    GT,
+                                                                    COLON,
+                                                                    EXCLAMATION_MARK,
+                                                                    EMPH,
+                                                                    TILDE,
+                                                                    BACKTICK);
+
   public MarkdownBidiRegionsSeparator() {
-    super(TokenSet.create(MarkdownTokenTypes.TEXT, MarkdownTokenTypes.WHITE_SPACE));
+    super(PARAGRAPH_CONTENTS);
   }
 }
