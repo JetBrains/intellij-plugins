f6573271011ece68b2190aa4bf9cd36f96724f42
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2017-04-11 13:42:45 +0200

    IDEA-169290: enable CSP in markdown preview browser
    (cherry picked from commit 5f67c13), IDEA-CR-20203
    
    (cherry picked from commit 206af8f2655fb97124e3ef6ac18ecc419090eb2f)

--- markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/SafeOpener.java
+++ markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/SafeOpener.java
@@ -2,33 +2,25 @@ package org.intellij.plugins.markdown.ui.preview.javafx;
 
 import com.intellij.ide.BrowserUtil;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.util.containers.ContainerUtil;
 import com.intellij.util.io.NettyKt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Locale;
 import java.util.Set;
 
 class SafeOpener {
   private static final Logger LOG = Logger.getInstance(SafeOpener.class);
 
-  private static final Set<String> SCHEMES = new HashSet<>(Arrays.asList(
+  private static final Set<String> SCHEMES = ContainerUtil.newTroveSet(
     "http",
     "https"
-  ));
+  );
 
-  private static final Set<String> LOCALHOSTS = new HashSet<>(Arrays.asList(
-    "localhost",
-    "127.0.0.1",
-    "::1",
-    "0.0.0.0"
-  ));
-
-  private static final Set<String> SAFE_LOCAL_EXTENSIONS = new HashSet<>(Arrays.asList(
+  private static final Set<String> SAFE_LOCAL_EXTENSIONS = ContainerUtil.newTroveSet(
     "md",
     "png",
     "gif",
@@ -37,7 +29,7 @@ class SafeOpener {
     "bmp",
     "svg",
     "html"
-  ));
+  );
 
   private SafeOpener() {
   }
@@ -72,7 +64,6 @@ class SafeOpener {
 
   private static boolean isLocalHost(@Nullable String hostName) {
     return hostName == null
-           || LOCALHOSTS.contains(hostName.toLowerCase(Locale.US))
            || hostName.startsWith("127.")
            || hostName.endsWith(":1")
            || NettyKt.isLocalHost(hostName, false, false);
