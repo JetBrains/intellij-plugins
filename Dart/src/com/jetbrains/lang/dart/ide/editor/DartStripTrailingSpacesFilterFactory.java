8322eedf54c9652894d594e1da9ea048bdcf77db
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-04-17 21:05:24 +0300

    do not strip trailing spaces in Dart multiline string literals

--- Dart/src/com/jetbrains/lang/dart/ide/editor/DartStripTrailingSpacesFilterFactory.java
+++ Dart/src/com/jetbrains/lang/dart/ide/editor/DartStripTrailingSpacesFilterFactory.java
@@ -1,9 +1,9 @@
 package com.jetbrains.lang.dart.ide.editor;
 
+import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.impl.PsiBasedStripTrailingSpacesFilter;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.tree.IElementType;
@@ -37,10 +37,10 @@ public class DartStripTrailingSpacesFilterFactory extends PsiBasedStripTrailingS
     protected void process(@NotNull final PsiFile psiFile) {
       PsiElementVisitor visitor = new DartRecursiveVisitor() {
         @Override
-        public void visitStringLiteralExpression(@NotNull DartStringLiteralExpression o) {
-          super.visitStringLiteralExpression(o);
-          for (PsiElement child : o.getChildren()) {
-            final IElementType type = child.getNode().getElementType();
+        public void visitStringLiteralExpression(@NotNull DartStringLiteralExpression literalExpression) {
+          super.visitStringLiteralExpression(literalExpression);
+          for (ASTNode child = literalExpression.getNode().getFirstChildNode(); child != null; child = child.getTreeNext()) {
+            final IElementType type = child.getElementType();
             if (type == RAW_TRIPLE_QUOTED_STRING || type == REGULAR_STRING_PART) {
               // keep trailing spaces in multiline string literals
               disableRange(child.getTextRange(), false);
