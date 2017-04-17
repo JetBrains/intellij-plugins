b8cf6bfcc4f5e75aee27bc22e0561481f7104e83
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-04-17 20:21:49 +0300

    do not strip trailing spaces in Dart multiline string literals

---
+++ Dart/src/com/jetbrains/lang/dart/ide/editor/DartStripTrailingSpacesFilterFactory.java
@@ -0,0 +1,54 @@
+package com.jetbrains.lang.dart.ide.editor;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.impl.PsiBasedStripTrailingSpacesFilter;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import com.jetbrains.lang.dart.DartLanguage;
+import com.jetbrains.lang.dart.psi.DartRecursiveVisitor;
+import com.jetbrains.lang.dart.psi.DartStringLiteralExpression;
+import org.jetbrains.annotations.NotNull;
+
+import static com.jetbrains.lang.dart.DartTokenTypes.RAW_TRIPLE_QUOTED_STRING;
+import static com.jetbrains.lang.dart.DartTokenTypes.REGULAR_STRING_PART;
+
+public class DartStripTrailingSpacesFilterFactory extends PsiBasedStripTrailingSpacesFilter.Factory {
+
+  @NotNull
+  @Override
+  protected PsiBasedStripTrailingSpacesFilter createFilter(@NotNull final Document document) {
+    return new DartStripTrailingSpacesFilter(document);
+  }
+
+  @Override
+  protected boolean isApplicableTo(@NotNull final Language language) {
+    return language == DartLanguage.INSTANCE;
+  }
+
+  private static class DartStripTrailingSpacesFilter extends PsiBasedStripTrailingSpacesFilter {
+    protected DartStripTrailingSpacesFilter(@NotNull final Document document) {
+      super(document);
+    }
+
+    @Override
+    protected void process(@NotNull final PsiFile psiFile) {
+      PsiElementVisitor visitor = new DartRecursiveVisitor() {
+        @Override
+        public void visitStringLiteralExpression(@NotNull DartStringLiteralExpression o) {
+          super.visitStringLiteralExpression(o);
+          for (PsiElement child : o.getChildren()) {
+            final IElementType type = child.getNode().getElementType();
+            if (type == RAW_TRIPLE_QUOTED_STRING || type == REGULAR_STRING_PART) {
+              // keep trailing spaces in multiline string literals
+              disableRange(child.getTextRange(), false);
+            }
+          }
+        }
+      };
+      psiFile.accept(visitor);
+    }
+  }
+}
