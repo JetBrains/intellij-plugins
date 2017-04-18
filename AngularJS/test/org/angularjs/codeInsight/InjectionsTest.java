bc802f445e5f2f9e533ea49c723ee1335a169bd0
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-04-18 16:21:23 +0300

    support ng-repeat-start/-end variable scopes (WEB-26504)

--- AngularJS/test/org/angularjs/codeInsight/InjectionsTest.java
+++ AngularJS/test/org/angularjs/codeInsight/InjectionsTest.java
@@ -111,6 +111,15 @@ public class InjectionsTest extends LightPlatformCodeInsightFixtureTestCase {
     checkVariableResolve("val<caret>ue", "value", JSVariable.class);
   }
 
+  public void testNgRepeatEndCompletion() {
+    myFixture.testCompletion("ngRepeatEnd.html", "ngRepeatEnd.after.html", "angular.js");
+  }
+
+  public void testNgRepeatEndResolve() {
+    myFixture.configureByFiles("ngRepeatEnd.resolve.html", "angular.js");
+    checkVariableResolve("aNo<caret>te", "aNote", JSVariable.class);
+  }
+
   public void testNgControllerAliasCompletion() {
     myFixture.testCompletionTyping("ngControllerAlias.html", "\n", "ngControllerAlias.after.html", "angular.js", "custom.js");
   }
