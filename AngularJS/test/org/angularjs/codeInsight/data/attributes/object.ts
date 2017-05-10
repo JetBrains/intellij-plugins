70318d852180702263ee9f73633f83381d5b8ad5
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-05-10 14:23:37 +0300

    tests for one-time string literal composite type

--- AngularJS/test/org/angularjs/codeInsight/data/attributes/object.ts
+++ AngularJS/test/org/angularjs/codeInsight/data/attributes/object.ts
@@ -5,9 +5,12 @@ import {EventEmitter} from "events";
 class TodoCmp {
     @Input() model: object;
     @Input() oneTime: string;
+    @Input() oneTimeList: FloatPlaceholderType;
     @Output() complete = new EventEmitter(); // TypeScript supports initializing fields
 
     onCompletedButton() {
         this.complete.emit("completed"); // this fires an event
     }
 }
+
+export type FloatPlaceholderType = 'always' | 'never' | 'auto';
