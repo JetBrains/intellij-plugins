d8d2de50a26eebc6a689b4096779f178ed176baa
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-05-17 15:15:08 +0300

    IDEA-173017 JavaFX WebView not HiDPI rendering on Windows 10

--- markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/JFXPanelWrapper.java
+++ markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/JFXPanelWrapper.java
@@ -1,11 +1,16 @@
 package org.intellij.plugins.markdown.ui.preview.javafx;
 
+import com.intellij.util.FieldAccessor;
+import com.intellij.util.ui.JBUI;
+import com.intellij.util.ui.UIUtil;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 
 import java.awt.*;
 
 public class JFXPanelWrapper extends JFXPanel {
+  private static FieldAccessor<Integer> myScaleFactorAccessor = new FieldAccessor<>(JFXPanel.class, "scaleFactor");
+
   public JFXPanelWrapper() {
     Platform.setImplicitExit(false);
   }
@@ -20,5 +25,15 @@ public class JFXPanelWrapper extends JFXPanel {
   @Override
   public Dimension getMinimumSize() {
     return new Dimension(0, 0);
+  }
+
+  @Override
+  public void addNotify() {
+    // JFXPanel is scaled asynchronously after first repaint, what may lead
+    // to showing unscaled content. To work it around, set "scaleFactor" ahead.
+    if (UIUtil.isJreHiDPIEnabled() && myScaleFactorAccessor.isAvailable()) {
+      myScaleFactorAccessor.set(this, Math.round(JBUI.sysScale(this)));
+    }
+    super.addNotify();
   }
 }
