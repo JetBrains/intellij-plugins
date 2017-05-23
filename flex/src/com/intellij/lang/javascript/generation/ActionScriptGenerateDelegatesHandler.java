09e27bd0537b48a68a08120de7b09ddd12a27cf6
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-05-23 18:00:33 +0300

    actionscript: extract common class to the top level

--- flex/src/com/intellij/lang/javascript/generation/ActionScriptGenerateDelegatesHandler.java
+++ flex/src/com/intellij/lang/javascript/generation/ActionScriptGenerateDelegatesHandler.java
@@ -196,10 +196,10 @@ public class ActionScriptGenerateDelegatesHandler extends BaseJSGenerateHandler 
     BaseCreateMethodsFix fix = new BaseCreateMethodsFix<JSNamedElement>(jsClass) {
 
       final JavaScriptGenerateAccessorHandler.MyBaseCreateMethodsFix generateGetterFix =
-        new JavaScriptGenerateAccessorHandler.MyBaseCreateMethodsFix(JavaScriptGenerateAccessorHandler.GenerationMode.Getter, jsClass,
+        new JavaScriptGenerateAccessorHandler.MyBaseCreateMethodsFix(JSGetterSetterGenerationMode.Getter, jsClass,
                                                                      null, false, field.getName());
       final JavaScriptGenerateAccessorHandler.MyBaseCreateMethodsFix generateSetterFix =
-        new JavaScriptGenerateAccessorHandler.MyBaseCreateMethodsFix(JavaScriptGenerateAccessorHandler.GenerationMode.Setter, jsClass,
+        new JavaScriptGenerateAccessorHandler.MyBaseCreateMethodsFix(JSGetterSetterGenerationMode.Setter, jsClass,
                                                                      null, false, field.getName());
 
       @Override
