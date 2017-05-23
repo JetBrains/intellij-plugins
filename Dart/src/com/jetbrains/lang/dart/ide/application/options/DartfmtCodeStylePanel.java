40822c5674ec687fa6ce8e7e0652e7d735da874c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2017-05-23 15:42:50 +0300

    Added right margin info to the Dartfmt tab

--- Dart/src/com/jetbrains/lang/dart/ide/application/options/DartfmtCodeStylePanel.java
+++ Dart/src/com/jetbrains/lang/dart/ide/application/options/DartfmtCodeStylePanel.java
@@ -1,12 +1,17 @@
 package com.jetbrains.lang.dart.ide.application.options;
 
 import com.intellij.application.options.CodeStyleAbstractPanel;
+import com.intellij.application.options.codeStyle.CodeStyleSchemesModel;
+import com.intellij.application.options.codeStyle.CodeStyleSettingsListener;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.highlighter.EditorHighlighter;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.options.ConfigurationException;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.ui.components.JBLabel;
+import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.DartFileType;
+import com.jetbrains.lang.dart.DartLanguage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -16,9 +21,23 @@ public class DartfmtCodeStylePanel extends CodeStyleAbstractPanel {
 
   private JPanel myMainPanel;
   private JCheckBox myDelegateToDartfmtCheckBox;
+  private JBLabel myRightMarginLabel;
 
   public DartfmtCodeStylePanel(@NotNull final CodeStyleSettings settings) {
     super(settings);
+  }
+
+  @Override
+  public void setModel(@Nullable CodeStyleSchemesModel model) {
+    super.setModel(model);
+    if (model != null) {
+      model.addListener(new CodeStyleSettingsListener() {
+        @Override
+        public void afterCurrentSettingsChanged() {
+          updateRightMarginLabel();
+        }
+      });
+    }
   }
 
   private static DartCodeStyleSettings getDartCodeStyleSettings(@NotNull final CodeStyleSettings settings) {
@@ -72,5 +91,10 @@ public class DartfmtCodeStylePanel extends CodeStyleAbstractPanel {
   @Override
   protected void resetImpl(@NotNull final CodeStyleSettings settings) {
     myDelegateToDartfmtCheckBox.setSelected(getDartCodeStyleSettings(settings).DELEGATE_TO_DARTFMT);
+    myRightMarginLabel.setText(DartBundle.message("dartfmt.right.margin", settings.getRightMargin(DartLanguage.INSTANCE)));
+  }
+
+  public void updateRightMarginLabel() {
+    myRightMarginLabel.setText(DartBundle.message("dartfmt.right.margin", getSettings().getRightMargin(DartLanguage.INSTANCE)));
   }
 }
