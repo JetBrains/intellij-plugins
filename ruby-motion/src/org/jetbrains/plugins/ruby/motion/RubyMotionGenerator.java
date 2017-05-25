60b73d2ed509abf006736e12f495e81bb1777ac9
Dmitry.Krasilschikov <kradima@jetbrains.com>
2017-05-25 17:56:59 +0300

    RUBY-19607 move ruby project generator settings to peer

--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/RubyMotionGenerator.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/RubyMotionGenerator.java
@@ -1,21 +1,18 @@
 package org.jetbrains.plugins.ruby.motion;
 
-import com.intellij.facet.ui.ValidationResult;
-import com.intellij.openapi.Disposable;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.startup.StartupManager;
-import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.SystemInfo;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.platform.HideableProjectGenerator;
+import com.intellij.platform.ProjectGeneratorPeer;
 import icons.RubyIcons;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.ruby.RBundle;
-import org.jetbrains.plugins.ruby.motion.ui.RubyMotionGeneratorTabBase;
 import org.jetbrains.plugins.ruby.motion.ui.RubyMotionSettingsHolder;
 import org.jetbrains.plugins.ruby.ruby.RModuleUtil;
 import org.jetbrains.plugins.ruby.wizard.RubyFrameworkProjectGenerator;
@@ -25,11 +22,12 @@ import javax.swing.*;
 /**
  * @author Dennis.Ushakov
  */
-public class RubyMotionGenerator extends RubyFrameworkProjectGenerator<RubyMotionSettingsHolder>
-  implements HideableProjectGenerator {
-  private static final String TITLE = RBundle.message("ruby.motion.application.settings");
-  private RubyMotionSettingsHolder mySettings;
-  private RubyMotionGeneratorTabBase myGeneratorTab;
+public class RubyMotionGenerator extends RubyFrameworkProjectGenerator<RubyMotionSettingsHolder> implements HideableProjectGenerator {
+  @NotNull
+  @Override
+  protected ProjectGeneratorPeer<RubyMotionSettingsHolder> createPeer() {
+    return new RubyMotionGeneratorPeer();
+  }
 
   @NotNull
   @Nls
@@ -61,33 +59,9 @@ public class RubyMotionGenerator extends RubyFrameworkProjectGenerator<RubyMotio
     });
   }
 
-  @NotNull
-  @Override
-  public ValidationResult doValidate(@NotNull String baseDirPath) {
-    return RubyMotionUtil.getInstance().rubyMotionPresent() ? ValidationResult.OK : new ValidationResult("RubyMotion is not installed");
-  }
-
   @Override
   public boolean isHidden() {
     return !SystemInfo.isMac;
-  }
-
-  @NotNull
-  @Override
-  public RubyMotionSettingsHolder createSettings() {
-    return new RubyMotionSettingsHolder();
-  }
-
-  @NotNull
-  @Override
-  public JPanel getSettingsPanel(@NotNull RubyMotionSettingsHolder settings, @NotNull Disposable parentDisposable) {
-    if (myGeneratorTab == null) {
-      myGeneratorTab = new RubyMotionGeneratorTabBase(settings);
-      Disposer.register(parentDisposable, () -> myGeneratorTab = null);
-    }
-    mySettings = settings;
-    final JComponent generatorTabComponent = myGeneratorTab.createComponent();
-    return (JPanel)generatorTabComponent;
   }
 
   @NotNull
