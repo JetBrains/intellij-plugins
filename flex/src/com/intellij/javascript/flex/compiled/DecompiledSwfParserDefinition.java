5c6d45bd251ddccd30f586ade8ceba4f592c9a8c
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2017-05-29 15:34:08 +0300

    register JS stub file element types in stubElementTypeHolder extension

--- flex/src/com/intellij/javascript/flex/compiled/DecompiledSwfParserDefinition.java
+++ flex/src/com/intellij/javascript/flex/compiled/DecompiledSwfParserDefinition.java
@@ -1,12 +1,11 @@
 package com.intellij.javascript.flex.compiled;
 
-import com.intellij.javascript.flex.FlexApplicationComponent;
 import com.intellij.lang.javascript.dialects.ECMAL4ParserDefinition;
-import com.intellij.lang.javascript.types.JSFileElementType;
 import com.intellij.psi.tree.IFileElementType;
 
+import static com.intellij.javascript.flex.compiled.DecompiledSwfElementTypes.FILE_TYPE;
+
 public class DecompiledSwfParserDefinition extends ECMAL4ParserDefinition {
-  private static final IFileElementType FILE_TYPE = JSFileElementType.create(FlexApplicationComponent.DECOMPILED_SWF);
 
   @Override
   public IFileElementType getFileNodeType() {
