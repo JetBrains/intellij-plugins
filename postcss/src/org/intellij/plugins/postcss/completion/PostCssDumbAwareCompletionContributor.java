f9d4bd15ab511749908d10ef6f9b5e20f8578658
Alexander Zolotov <alexander.zolotov@jetbrains.com>
2017-05-30 16:25:36 +0300

    PostCSS: completion for in nested rulesets (WEB-24619)

--- postcss/src/org/intellij/plugins/postcss/completion/PostCssDumbAwareCompletionContributor.java
+++ postcss/src/org/intellij/plugins/postcss/completion/PostCssDumbAwareCompletionContributor.java
@@ -3,21 +3,58 @@ package org.intellij.plugins.postcss.completion;
 import com.intellij.codeInsight.completion.CompletionContributor;
 import com.intellij.codeInsight.completion.CompletionParameters;
 import com.intellij.codeInsight.completion.CompletionResultSet;
+import com.intellij.codeInsight.completion.CompletionType;
 import com.intellij.openapi.project.DumbAware;
+import com.intellij.patterns.ElementPattern;
+import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.css.CssBlock;
 import com.intellij.psi.css.CssRuleset;
+import com.intellij.psi.css.CssSimpleSelector;
 import com.intellij.psi.css.impl.CssElementTypes;
 import com.intellij.psi.css.impl.util.completion.CssAddSpaceWithBracesInsertHandler;
+import com.intellij.psi.css.impl.util.completion.provider.PropertyNamesCompletionProvider;
+import com.intellij.psi.css.impl.util.completion.provider.TagsCompletionProvider;
 import com.intellij.psi.css.util.CssCompletionUtil;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
 import com.intellij.psi.util.PsiTreeUtil;
+import org.intellij.plugins.postcss.PostCssElementTypes;
 import org.intellij.plugins.postcss.completion.handler.PostCssOneLineAtRuleInsertHandler;
 import org.intellij.plugins.postcss.psi.PostCssPsiUtil;
 import org.jetbrains.annotations.NotNull;
 
+import static com.intellij.patterns.PlatformPatterns.psiElement;
+import static com.intellij.patterns.StandardPatterns.or;
+import static com.intellij.patterns.StandardPatterns.string;
+import static com.intellij.psi.css.impl.util.completion.CssDumbAwareCompletionContributor.propertyName;
+
 public class PostCssDumbAwareCompletionContributor extends CompletionContributor implements DumbAware {
+  private static final TokenSet BLOCK_START_OR_END_SET =
+    TokenSet.create(CssElementTypes.CSS_LBRACE, CssElementTypes.CSS_SEMICOLON, CssElementTypes.CSS_RBRACE);
+
   private static final PostCssOneLineAtRuleInsertHandler ONE_LINE_STATEMENT_HANDLER = new PostCssOneLineAtRuleInsertHandler();
+
+  public PostCssDumbAwareCompletionContributor() {
+    extend(CompletionType.BASIC, selector(), new TagsCompletionProvider());
+    extend(CompletionType.BASIC, propertyDeclaration(), new PropertyNamesCompletionProvider());
+  }
+
+  private static ElementPattern<? extends PsiElement> propertyDeclaration() {
+    return inPostCssFile(CssElementTypes.CSS_IDENT)
+      .withParent(CssSimpleSelector.class).inside(CssBlock.class)
+      .afterLeafSkipping(or(emptyElement(), spaceElement()), blockStartOrEnd())
+      .beforeLeafSkipping(or(emptyElement(), spaceElement()), blockStartOrEnd());
+  }
+
+  private static ElementPattern<PsiElement> selector() {
+    return inPostCssFile(CssElementTypes.CSS_IDENT).andOr(propertyName(), propertyName());
+  }
+
+  private static PsiElementPattern.Capture<PsiElement> inPostCssFile(@NotNull IElementType type) {
+    return psiElement(type).inside(psiElement(PostCssElementTypes.POST_CSS_STYLESHEET));
+  }
 
   public void fillCompletionVariants(@NotNull final CompletionParameters parameters, @NotNull final CompletionResultSet result) {
     final CompletionResultSet resultSet = CssCompletionUtil.fixPrefixForVendorPrefixes(parameters, result);
@@ -37,6 +74,18 @@ public class PostCssDumbAwareCompletionContributor extends CompletionContributor
     }
   }
 
+  private static ElementPattern<PsiElement> blockStartOrEnd() {
+    return or(psiElement().withElementType(BLOCK_START_OR_END_SET), psiElement().withText(string().contains("\n")));
+  }
+
+  private static PsiElementPattern.Capture<PsiElement> emptyElement() {
+    return psiElement().withText("");
+  }
+
+  private static PsiElementPattern.Capture<PsiElement> spaceElement() {
+    return psiElement().withText(" ");
+  }
+
   private static void addNest(@NotNull PsiElement position, @NotNull CompletionResultSet result) {
     PsiElement parent = position.getParent();
     if (parent instanceof PsiErrorElement) {
@@ -51,5 +100,4 @@ public class PostCssDumbAwareCompletionContributor extends CompletionContributor
       result.addElement(CssCompletionUtil.lookupForKeyword("@nest", new CssAddSpaceWithBracesInsertHandler(false)));
     }
   }
-
 }
