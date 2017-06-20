14159cf0eeb9995c0e9de66fa5c2d630b37cbbd1
Valentin Fondaratov <valentin.fondaratov@jetbrains.com>
2017-06-20 11:40:43 +0300

    add RubyMotion license headers and license desc in root

--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/MotionDeviceProcessHandler.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/run/MotionDeviceProcessHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.plugins.ruby.motion.run;
 
 import com.intellij.execution.ExecutionException;
@@ -7,7 +22,6 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Ref;
 import com.intellij.util.PathUtil;
-import com.intellij.util.Processor;
 import com.jetbrains.cidr.execution.ExecutionResult;
 import com.jetbrains.cidr.execution.ProcessHandlerWithPID;
 import com.jetbrains.cidr.execution.deviceSupport.AMDevice;
--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/ui/RubyMotionGeneratorTabBase.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/ui/RubyMotionGeneratorTabBase.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2017 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jetbrains.plugins.ruby.motion.ui;
 
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
@@ -8,12 +23,12 @@ import com.intellij.util.PlatformUtils;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.ruby.RBundle;
-import org.jetbrains.plugins.ruby.wizard.RubySdkPanel;
 import org.jetbrains.plugins.ruby.motion.RubyMotionUtilImpl;
 import org.jetbrains.plugins.ruby.rails.facet.ui.wizard.ui.RubyFacetEditorTab;
 import org.jetbrains.plugins.ruby.remote.RubyRemoteInterpreterManager;
 import org.jetbrains.plugins.ruby.ruby.sdk.RubySdkType;
 import org.jetbrains.plugins.ruby.ruby.sdk.RubySdkUtil;
+import org.jetbrains.plugins.ruby.wizard.RubySdkPanel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
