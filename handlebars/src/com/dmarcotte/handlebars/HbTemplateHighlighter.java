5a24b57190fb723c3c525e768bd1c2fd56a072e3
Alexey Kudravtsev <cdr@intellij.com>
2017-07-11 18:23:33 +0300

    removed deprecated method usage

--- handlebars/src/com/dmarcotte/handlebars/HbTemplateHighlighter.java
+++ handlebars/src/com/dmarcotte/handlebars/HbTemplateHighlighter.java
@@ -5,9 +5,7 @@ import com.intellij.lang.Language;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.ex.util.LayerDescriptor;
 import com.intellij.openapi.editor.ex.util.LayeredLexerEditorHighlighter;
-import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.openapi.fileTypes.StdFileTypes;
-import com.intellij.openapi.fileTypes.SyntaxHighlighter;
+import com.intellij.openapi.fileTypes.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings;
@@ -22,15 +20,15 @@ public class HbTemplateHighlighter extends LayeredLexerEditorHighlighter {
     // highlighter for outer lang
     FileType type = null;
     if (project == null || virtualFile == null) {
-      type = StdFileTypes.PLAIN_TEXT;
+      type = FileTypes.PLAIN_TEXT;
     }
     else {
       Language language = TemplateDataLanguageMappings.getInstance(project).getMapping(virtualFile);
       if (language != null) type = language.getAssociatedFileType();
       if (type == null) type = HbLanguage.getDefaultTemplateLang();
     }
-    @SuppressWarnings("deprecation") // deprecated in IDEA 12, still needed in IDEA 11 TODO remove when IDEA 11 support is dropped
-      SyntaxHighlighter outerHighlighter = SyntaxHighlighter.PROVIDER.create(type, project, virtualFile);
+
+    SyntaxHighlighter outerHighlighter = SyntaxHighlighterFactory.getSyntaxHighlighter(type, project, virtualFile);
 
     registerLayer(HbTokenTypes.CONTENT, new LayerDescriptor(outerHighlighter, ""));
   }
