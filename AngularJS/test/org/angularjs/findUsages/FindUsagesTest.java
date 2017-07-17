da84d5bf51670411e252adb25f2a696d82950e04
Dennis Ushakov <dennis.ushakov@gmail.com>
2017-07-17 19:48:41 +0300

    mark @HostListener/@HostBinding annotated fields as used (WEB-25006)
    (cherry picked from commit c447117)
    
    (cherry picked from commit 8227d50)

--- AngularJS/test/org/angularjs/findUsages/FindUsagesTest.java
+++ AngularJS/test/org/angularjs/findUsages/FindUsagesTest.java
@@ -1,7 +1,12 @@
 package org.angularjs.findUsages;
 
+import com.intellij.lang.javascript.JSTestUtils;
+import com.intellij.lang.javascript.dialects.JSLanguageLevel;
+import com.intellij.lang.javascript.inspections.JSUnusedGlobalSymbolsInspection;
+import com.intellij.lang.javascript.inspections.JSUnusedLocalSymbolsInspection;
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 import com.intellij.usageView.UsageInfo;
+import com.intellij.util.ThrowableRunnable;
 import org.angularjs.AngularTestUtil;
 
 import java.util.Collection;
@@ -26,6 +31,15 @@ public class FindUsagesTest extends LightPlatformCodeInsightFixtureTestCase {
   public void testDirectiveFromHtml() {
     doTest();
   }
+
+  public void testId() throws Exception {
+    JSTestUtils.testWithinLanguageLevel(JSLanguageLevel.ES6, getProject(), (ThrowableRunnable<Exception>)() -> {
+      myFixture.enableInspections(JSUnusedLocalSymbolsInspection.class, JSUnusedGlobalSymbolsInspection.class);
+      myFixture.configureByFiles("object.ts");
+      myFixture.checkHighlighting();
+    });
+  }
+
 
   private void doTest() {
     final Collection<UsageInfo> infos = myFixture.testFindUsages(getTestName(true) + ".html", "angular.js");
