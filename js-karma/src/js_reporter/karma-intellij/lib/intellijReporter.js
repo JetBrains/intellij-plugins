26484037f48dafa77b07f0231a5aadbb147e6c3c
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2017-07-19 21:11:38 +0300

    WEB-8927 Karma: status message should be more relevant
    (cherry picked from commit 2636359)

--- js-karma/src/js_reporter/karma-intellij/lib/intellijReporter.js
+++ js-karma/src/js_reporter/karma-intellij/lib/intellijReporter.js
@@ -53,55 +53,6 @@ function createSpecNode(suiteNode, suiteNames, specName) {
   return specNode;
 }
 
-function sendBrowserEvents(eventType, connectionId2BrowserObjA, connectionId2BrowserObjB, addAutoCapturingInfo) {
-  for (var connectionId in connectionId2BrowserObjA) {
-    if (connectionId2BrowserObjA.hasOwnProperty(connectionId)) {
-      if (!connectionId2BrowserObjB.hasOwnProperty(connectionId)) {
-        var browser = connectionId2BrowserObjA[connectionId];
-        var event = {id: connectionId, name: browser.name};
-        if (addAutoCapturingInfo) {
-          event.isAutoCaptured = isAutoCapturedBrowser(browser);
-        }
-        intellijUtil.sendIntellijEvent(eventType, event);
-      }
-    }
-  }
-}
-
-function isAutoCapturedBrowser(browser) {
-  if (browser.launchId != null) {
-    return true;
-  }
-  var idStr = browser.id;
-  if (intellijUtil.isString(idStr)) {
-    return /^\d+$/.test(idStr);
-  }
-  return false;
-}
-
-function startBrowsersTracking(globalEmitter) {
-  var oldConnectionId2BrowserObj = {};
-  globalEmitter.on('browsers_change', function(capturedBrowsers) {
-    if (!capturedBrowsers.forEach) {
-      // filter out events from Browser object
-      return;
-    }
-    var newConnectionId2BrowserObj = {};
-    var proceed = true;
-    capturedBrowsers.forEach(function(newBrowser) {
-      if (!newBrowser.id || !newBrowser.name || newBrowser.id === newBrowser.name) {
-        proceed = false;
-      }
-      newConnectionId2BrowserObj[newBrowser.id] = newBrowser;
-    });
-    if (proceed) {
-      sendBrowserEvents('browserConnected', newConnectionId2BrowserObj, oldConnectionId2BrowserObj, true);
-      sendBrowserEvents('browserDisconnected', oldConnectionId2BrowserObj, newConnectionId2BrowserObj, false);
-      oldConnectionId2BrowserObj = newConnectionId2BrowserObj;
-    }
-  });
-}
-
 // Makes sure that only intellijReporter is allowed to output.
 // Otherwise we'll get messages doubling and possible prefixing '##[teamcity ]' with other output that results
 // in test output tree breaking.
@@ -168,8 +119,8 @@ function filterSuiteNames(suiteNames) {
 
 function IntellijReporter(config, fileList, formatError, globalEmitter, injector) {
   require('./kjhtml/kjhtml-specFilter-patch').apply(config.files);
+  require('./karma-browser-tracker').startBrowserTracking(globalEmitter);
   var logManager = new LogManager();
-  startBrowsersTracking(globalEmitter);
   this.adapters = [];
   var totalTestCount, uncheckedBrowserCount;
 
