f2efe6f3c546841708f03858bb73d1a14f11d211
Irina.Chernushina <irina.chernushina@jetbrains.com>
2017-07-21 18:09:40 +0200

    AngularJSInjector: extract xml text/attribute injection for reuse
    - also, pay attention that different types of comment symbols may occur
    to be found
    - skip space symbols as possible injection holders
    - ignore single-line comment symbols "//" if specified as custom delimiter
    
    (cherry picked from commit 0414308)

--- AngularJS/src/org/angularjs/editor/AngularJSInjector.java
+++ AngularJS/src/org/angularjs/editor/AngularJSInjector.java
@@ -5,20 +5,15 @@ import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
 import com.intellij.lang.xml.XMLLanguage;
 import com.intellij.lang.javascript.JSInjectionBracesUtil;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.lang.javascript.psi.stubs.JSImplicitElement;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.util.text.InjectorMatchingEndFinder;
-import com.intellij.psi.ElementManipulators;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
 import com.intellij.psi.impl.source.xml.XmlAttributeValueImpl;
 import com.intellij.psi.impl.source.xml.XmlTextImpl;
-import com.intellij.psi.templateLanguages.OuterLanguageElement;
 import com.intellij.psi.xml.XmlAttribute;
-import com.intellij.psi.xml.XmlTokenType;
 import com.intellij.util.NullableFunction;
 import org.angularjs.codeInsight.attributes.AngularAttributesRegistry;
 import org.angularjs.index.AngularIndexUtil;
@@ -33,10 +28,12 @@ import java.util.List;
  * @author Dennis.Ushakov
  */
 public class AngularJSInjector implements MultiHostInjector {
-  private static final Logger LOG = Logger.getInstance(AngularJSInjector.class);
   public static final NullableFunction<PsiElement, Pair<String, String>> BRACES_FACTORY = JSInjectionBracesUtil
-    .createDelimitersFactory(AngularJSLanguage.INSTANCE.getDisplayName(),
-                             (project, key) -> AngularIndexUtil.resolve(project, AngularInjectionDelimiterIndex.KEY, key));
+    .delimitersFactory(AngularJSLanguage.INSTANCE.getDisplayName(),
+                       (project, key) -> {
+                               final JSImplicitElement element = AngularIndexUtil.resolve(project, AngularInjectionDelimiterIndex.KEY, key);
+                               return element != null ? Pair.create(element.getTypeString(), element) : null;
+                             });
 
   @Override
   public void getLanguagesToInject(@NotNull MultiHostRegistrar registrar, @NotNull PsiElement context) {
@@ -68,40 +65,9 @@ public class AngularJSInjector implements MultiHostInjector {
     if (context instanceof XmlTextImpl || context instanceof XmlAttributeValueImpl) {
       final Pair<String, String> braces = BRACES_FACTORY.fun(context);
       if (braces == null) return;
-      final String start = braces.getFirst();
-      final String end = braces.getSecond();
 
-      final String text = context.getText();
-      int endIndex = -1;
-      int afterStart = -1;
-      int searchStart = -2;
-      while (Math.max(endIndex, afterStart) > searchStart) {
-        searchStart = Math.max(endIndex, afterStart);
-        final int startIdx = text.indexOf(start, searchStart);
-        if (startIdx < 0) return;
-        afterStart = startIdx + start.length();
-        assert afterStart >= 0;
-
-        endIndex = afterStart;
-        endIndex = InjectorMatchingEndFinder.findMatchingEnd(start, end, text, endIndex);
-        endIndex = endIndex > 0 ? endIndex : ElementManipulators.getValueTextRange(context).getEndOffset();
-        final PsiElement injectionCandidate = context.findElementAt(afterStart);
-        if (injectionCandidate != null && injectionCandidate.getNode().getElementType() != XmlTokenType.XML_COMMENT_CHARACTERS &&
-           !(injectionCandidate instanceof OuterLanguageElement)) {
-          if (afterStart > endIndex) {
-            if (ApplicationManager.getApplication().isInternal()) {
-              LOG.error("Braces: " + start + "," + end + "\n" +
-                        "Text: \"" + text + "\"" + "\n" +
-                        "Interval: (" + afterStart + "," + endIndex + ")" + "\n" +
-                        "File: " + context.getContainingFile().getName() + ", language:" + context.getContainingFile().getLanguage());
-            }
-            return;
-          }
-          registrar.startInjecting(AngularJSLanguage.INSTANCE).
-                    addPlace(null, null, (PsiLanguageInjectionHost)context, new TextRange(afterStart, endIndex)).
-                    doneInjecting();
-        }
-      }
+      JSInjectionBracesUtil
+        .injectInXmlTextByDelimiters(registrar, context, AngularJSLanguage.INSTANCE, braces.getFirst(), braces.getSecond());
     }
   }
 
