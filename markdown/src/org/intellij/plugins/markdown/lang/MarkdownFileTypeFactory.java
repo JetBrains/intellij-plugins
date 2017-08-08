32cffdae096fa49e085f2d170031c373f2bec1cf
Dmitry.Krasilschikov <kradima@jetbrains.com>
2017-08-08 17:25:49 +0300

    RUBY-19479 recognize '.markdown' extension

--- markdown/src/org/intellij/plugins/markdown/lang/MarkdownFileTypeFactory.java
+++ markdown/src/org/intellij/plugins/markdown/lang/MarkdownFileTypeFactory.java
@@ -22,6 +22,6 @@ import org.jetbrains.annotations.NotNull;
 public class MarkdownFileTypeFactory extends FileTypeFactory {
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
-    consumer.consume(MarkdownFileType.INSTANCE);
+    consumer.consume(MarkdownFileType.INSTANCE, "md;markdown");
   }
 }
