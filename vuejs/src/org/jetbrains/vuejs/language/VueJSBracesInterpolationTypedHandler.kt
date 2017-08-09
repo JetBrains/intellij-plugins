bef6a478d3b9dc68c6774ab524447b496ba1e6de
Irina.Chernushina <Irina.Chernushina@jetbrains.com>
2017-08-09 18:09:50 +0200

    vue: do complete interpolation delimiters only in vue/html

--- vuejs/src/org/jetbrains/vuejs/language/VueJSBracesInterpolationTypedHandler.kt
+++ vuejs/src/org/jetbrains/vuejs/language/VueJSBracesInterpolationTypedHandler.kt
@@ -1,11 +1,13 @@
 package org.jetbrains.vuejs.language
 
 import com.intellij.codeInsight.editorActions.TypedHandlerDelegate
+import com.intellij.ide.highlighter.HtmlFileType
 import com.intellij.lang.javascript.JSInjectionBracesUtil
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import org.jetbrains.vuejs.VueFileType
 
 /**
  * @author Irina.Chernushina on 7/24/2017.
@@ -15,7 +17,8 @@ class VueJSBracesInterpolationTypedHandler : TypedHandlerDelegate() {
     VueInjector.BRACES_FACTORY)
 
   override fun beforeCharTyped(c: Char, project: Project, editor: Editor, file: PsiFile, fileType: FileType): TypedHandlerDelegate.Result {
-    if (!org.jetbrains.vuejs.index.hasVue(project)) return Result.CONTINUE
+    if (!org.jetbrains.vuejs.index.hasVue(project) ||
+        fileType != VueFileType.INSTANCE && fileType != HtmlFileType.INSTANCE) return Result.CONTINUE
     return myBracesCompleter.beforeCharTyped(c, project, editor, file)
   }
 }
