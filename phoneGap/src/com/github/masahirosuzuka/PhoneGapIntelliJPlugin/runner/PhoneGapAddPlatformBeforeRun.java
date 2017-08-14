c7be68db0b6e25a3bf98ab3b5be90b17095ba6b8
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-08-14 13:41:44 +0300

    phonegap: better handling before task + fix timeouts + rename WEB-27615
    IDEA-CR-23802
    (cherry picked from commit 7e3ed5d)

--- phoneGap/src/com/github/masahirosuzuka/PhoneGapIntelliJPlugin/runner/PhoneGapAddPlatformBeforeRun.java
+++ phoneGap/src/com/github/masahirosuzuka/PhoneGapIntelliJPlugin/runner/PhoneGapAddPlatformBeforeRun.java
@@ -6,17 +6,26 @@ import com.intellij.execution.BeforeRunTask;
 import com.intellij.execution.BeforeRunTaskProvider;
 import com.intellij.execution.ExecutionHelper;
 import com.intellij.execution.configurations.RunConfiguration;
+import com.intellij.execution.impl.ConsoleViewImpl;
+import com.intellij.execution.process.CapturingProcessHandler;
 import com.intellij.execution.process.ProcessOutput;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.Ref;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.content.MessageView;
 import com.intellij.util.concurrency.Semaphore;
 import com.intellij.util.containers.ContainerUtil;
 import icons.PhoneGapIcons;
@@ -24,6 +33,7 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -31,6 +41,8 @@ import java.util.concurrent.TimeUnit;
 public class PhoneGapAddPlatformBeforeRun extends BeforeRunTaskProvider<PhoneGapAddPlatformBeforeRun.PhoneGapAddPlatformTask> {
 
   public static final Key<PhoneGapAddPlatformTask> ID = Key.create("PhonegapTask");
+  public static final long INIT_TIMEOUT = TimeUnit.MINUTES.toMillis(10);
+  public static final long LOCK_TIMEOUT = TimeUnit.MINUTES.toMillis(11);
 
   @Override
   public Key<PhoneGapAddPlatformTask> getId() {
@@ -91,13 +103,20 @@ public class PhoneGapAddPlatformBeforeRun extends BeforeRunTaskProvider<PhoneGap
     final Semaphore targetDone = new Semaphore();
     final Ref<Boolean> result = new Ref<>(true);
     final List<Exception> exceptions = new ArrayList<>();
-    ApplicationManager.getApplication().invokeAndWait(() -> {
+    final String tabText = PhoneGapBundle.message("phonegap.before.task.init.title", line.getPlatformName());
+    targetDone.down();
+
+    ApplicationManager.getApplication().invokeLater(() -> {
 
       //Save all opened documents
       FileDocumentManager.getInstance().saveAllDocuments();
-      targetDone.down();
 
-      new Task.Backgroundable(project, PhoneGapBundle.message("phonegap.before.task.init.title", line.getPlatformName()), true) {
+      JPanel panel = new JPanel(new BorderLayout());
+      ConsoleViewImpl console = new ConsoleViewImpl(project, true);
+      panel.add(console.getComponent(), BorderLayout.CENTER);
+      createViewForOutput(console, panel, project, tabText);
+
+      new Task.Backgroundable(project, tabText, true) {
 
         public boolean shouldStartInBackground() {
           return true;
@@ -107,11 +126,25 @@ public class PhoneGapAddPlatformBeforeRun extends BeforeRunTaskProvider<PhoneGap
           try {
             String platform = phoneGapRunConfiguration.getPlatform();
             assert platform != null;
-            ProcessOutput output = line.platformAdd(platform);
-            if (output.getExitCode() != 0) {
-              ExecutionHelper
-                .showOutput(project, output, PhoneGapBundle.message("phonegap.before.task.init.title", line.getPlatformName()), null, true);
+
+            CapturingProcessHandler handler = line.platformAdd(platform);
+            console.attachToProcess(handler);
+
+
+            ProcessOutput output = handler.runProcessWithProgressIndicator(indicator, (int)INIT_TIMEOUT);
+            if (output.isCancelled()) {
               result.set(false);
+            }
+            else {
+              String stdout = output.getStdout();
+              String stderr = output.getStderr();
+              if (outputContains(stdout, stderr, "Platform " + platform + " already exists") ||
+                  outputContains(stdout, stderr, "Platform " + platform + " already added")) {
+                ApplicationManager.getApplication().invokeLater(() -> {
+                  final MessageView messageView = ServiceManager.getService(project, MessageView.class);
+                  removeContents(messageView, null, tabText);
+                });
+              }
             }
           }
           catch (final Exception e) {
@@ -125,10 +158,8 @@ public class PhoneGapAddPlatformBeforeRun extends BeforeRunTaskProvider<PhoneGap
       }.queue();
     }, ModalityState.NON_MODAL);
 
-    if (!targetDone.waitFor(TimeUnit.MINUTES.toMillis(2))) {
-      ExecutionHelper
-        .showErrors(project, ContainerUtil.createMaybeSingletonList(new RuntimeException("Timeout")),
-                    PhoneGapBundle.message("phonegap.before.task.init.error"), null);
+    if (!targetDone.waitFor(LOCK_TIMEOUT)) {
+      ExecutionHelper.showErrors(project, ContainerUtil.createMaybeSingletonList(new RuntimeException("Timeout")), tabText, null);
     }
     else if (!exceptions.isEmpty()) {
       ExecutionHelper.showErrors(project, exceptions, PhoneGapBundle.message("phonegap.before.task.init.error"), null);
@@ -137,6 +168,41 @@ public class PhoneGapAddPlatformBeforeRun extends BeforeRunTaskProvider<PhoneGap
 
     return result.get();
   }
+
+  public boolean outputContains(String stdout, String stderr, String text1) {
+    return StringUtil.contains(stdout, text1) || StringUtil.contains(stderr, text1);
+  }
+
+
+  private static void createViewForOutput(ConsoleViewImpl console,
+                                          @NotNull final JComponent component,
+                                          @NotNull final Project myProject,
+                                          @NotNull final String tabDisplayName) {
+    CommandProcessor commandProcessor = CommandProcessor.getInstance();
+    commandProcessor.executeCommand(myProject, () -> {
+      final MessageView messageView = ServiceManager.getService(myProject, MessageView.class);
+      final Content content = ContentFactory.SERVICE.getInstance().createContent(component, tabDisplayName, true);
+      messageView.getContentManager().addContent(content);
+      messageView.getContentManager().setSelectedContent(content);
+      Disposer.register(content, console);
+      removeContents(messageView, content, tabDisplayName);
+    }, "Open " + tabDisplayName, null);
+  }
+
+  private static void removeContents(@NotNull MessageView messageView,
+                                     @Nullable final Content skip,
+                                     @NotNull final String tabDisplayName) {
+    Content[] contents = messageView.getContentManager().getContents();
+    for (Content content : contents) {
+      if (content.isPinned()) continue;
+      if (tabDisplayName.equals(content.getDisplayName()) && content != skip) {
+        if (messageView.getContentManager().removeContent(content, true)) {
+          content.release();
+        }
+      }
+    }
+  }
+
 
   public static class PhoneGapAddPlatformTask extends BeforeRunTask<PhoneGapAddPlatformTask> {
 
