cbdbcf6e9e666e68a91d7331d01b225920415f5f
Yann Cébron <yann.cebron@jetbrains.com>
2017-08-16 15:04:59 +0200

    [struts2] ParamValueConverter: fix potential NPE

--- struts2/dom-api/src/com/intellij/struts2/dom/params/ParamValueConverter.java
+++ struts2/dom-api/src/com/intellij/struts2/dom/params/ParamValueConverter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 The authors
+ * Copyright 2017 The authors
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -40,7 +40,7 @@ public class ParamValueConverter extends WrappingConverter {
 
     // skip values containing expressions
     final String text = domElement.getRawText();
-    if (text.contains("${")) {
+    if (text == null || text.contains("${")) {
       return null;
     }
 
