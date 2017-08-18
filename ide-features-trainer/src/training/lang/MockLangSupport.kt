40a610e790831fa051e497f82b275f507112f070
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2017-08-18 19:31:16 +0300

    cleanup; add SimpleProject as a template for a LearnProject
    GitOrigin-RevId: 54490d04fdc819aa8835d48367fb5a69a5849b7b

--- ide-features-trainer/src/training/lang/MockLangSupport.kt
+++ ide-features-trainer/src/training/lang/MockLangSupport.kt
@@ -9,6 +9,10 @@ import com.intellij.openapi.projectRoots.SdkTypeId
  * @author Sergey Karashevich
  */
 class MockLangSupport(override val FILE_EXTENSION: String) : AbstractLangSupport() {
+    override fun importLearnProject(): Project? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
     override fun checkSdkCompatibility(sdk: Sdk, sdkTypeId: SdkTypeId) {
         throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
