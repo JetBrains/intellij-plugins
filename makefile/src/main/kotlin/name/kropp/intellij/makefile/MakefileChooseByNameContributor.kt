a1247699de88224c3216a9b8877b2f1a3421e379
Victor Kropp <victor.kropp@gmail.com>
2017-09-08 16:47:17 +0200

    register index, make goto use index
    GitOrigin-RevId: f9b40e0b73b0d3cec5afff76c3bccb7015856e4c

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileChooseByNameContributor.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileChooseByNameContributor.kt
@@ -5,5 +5,5 @@ import com.intellij.openapi.project.Project
 
 class MakefileChooseByNameContributor : ChooseByNameContributor {
   override fun getItemsByName(name: String, pattern: String, project: Project, includeNonProjectItems: Boolean) = findTargets(project, name).filterNot { it.isSpecialTarget }.toTypedArray()
-  override fun getNames(project: Project, includeNonProjectItems: Boolean) = findTargets(project).filterNot { it.isSpecialTarget }.map { it.text }.filterNot(String::isNullOrEmpty).toTypedArray()
+  override fun getNames(project: Project, includeNonProjectItems: Boolean) = findAllTargets(project).filterNot(String::isNullOrEmpty).toTypedArray()
 }
--- makefile/src/main/kotlin/name/kropp/intellij/makefile/utils.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/utils.kt
@@ -2,28 +2,13 @@ package name.kropp.intellij.makefile
 
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiManager
-import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiTreeUtil
-import com.intellij.util.indexing.FileBasedIndex
 import name.kropp.intellij.makefile.psi.MakefileTarget
 
-fun findTargets(project: Project): List<MakefileTarget> {
-  val psiManager = PsiManager.getInstance(project)
-  return FileBasedIndex.getInstance()
-      .getContainingFiles(FileTypeIndex.NAME, MakefileFileType, GlobalSearchScope.allScope(project))
-      .mapNotNull { psiManager.findFile(it) }
-      .flatMap(::findTargets)
-}
+fun findAllTargets(project: Project) = MakefileTargetIndex.getAllKeys(project)
 
-fun findTargets(project: Project, name: String): List<MakefileTarget> {
-  val psiManager = PsiManager.getInstance(project)
-  return FileBasedIndex.getInstance()
-      .getContainingFiles(FileTypeIndex.NAME, MakefileFileType, GlobalSearchScope.allScope(project))
-      .mapNotNull { psiManager.findFile(it) }
-      .flatMap(::findTargets)
-      .filter { it.text == name }
-}
+fun findTargets(project: Project, name: String): Collection<MakefileTarget> =
+    MakefileTargetIndex.get(name, project, GlobalSearchScope.allScope(project))
 
 fun findTargets(psiFile: PsiFile) = PsiTreeUtil.findChildrenOfType(psiFile, MakefileTarget::class.java).asIterable()
