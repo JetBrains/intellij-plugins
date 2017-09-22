d3fe4fec051a0b0d4ea66ca7bccdccb624bafed5
Anton Lobov <Anton.Lobov@jetbrains.com>
2017-09-22 10:01:26 +0200

    improve working with literal types: widening, inference, resolve (fixes WEB-27494, WEB-25908, WEB-28673, partially fixes WEB-26285)

--- flex/src/com/intellij/lang/javascript/inspections/actionscript/ActionScriptAnnotatingVisitor.java
+++ flex/src/com/intellij/lang/javascript/inspections/actionscript/ActionScriptAnnotatingVisitor.java
@@ -1390,9 +1390,9 @@ public class ActionScriptAnnotatingVisitor extends TypedJSAnnotatingVisitor {
       final JSType numberType = JSNamedType.createType(NUMBER_CLASS_NAME, JSTypeSourceFactory.createTypeSource(lOperand, true),
                                                        JSContext.INSTANCE);
       myTypeChecker.checkExpressionIsAssignableToType(lOperand, numberType,
-                                                      "javascript.expression.type.implicitly.coerced.to.unrelated.type", null);
+                                                      "javascript.expression.type.implicitly.coerced.to.unrelated.type", null, false);
       myTypeChecker.checkExpressionIsAssignableToType(rOperand, numberType,
-                                                      "javascript.expression.type.implicitly.coerced.to.unrelated.type", null);
+                                                      "javascript.expression.type.implicitly.coerced.to.unrelated.type", null, false);
     }
   }
 
@@ -1454,7 +1454,7 @@ public class ActionScriptAnnotatingVisitor extends TypedJSAnnotatingVisitor {
         arguments[0],
         BOOLEAN_CLASS_NAME,
         "javascript.argument.type.mismatch",
-        null);
+        null, false);
     }
   }
 
--- flex/src/com/intellij/lang/javascript/inspections/actionscript/ActionScriptTypeChecker.java
+++ flex/src/com/intellij/lang/javascript/inspections/actionscript/ActionScriptTypeChecker.java
@@ -61,7 +61,7 @@ public class ActionScriptTypeChecker extends JSTypeChecker<Annotation> {
     final JSType type = p.getType();
     Pair<Annotation, String> annotationAndExprType =
       checkExpressionIsAssignableToType(expr, type, problemKey,
-                                        allowChangeVariableTypeFix ? p : null);
+                                        allowChangeVariableTypeFix ? p : null, true);
 
     if (annotationAndExprType != null &&
         p.getParent() instanceof JSParameterList &&
