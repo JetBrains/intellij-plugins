8188c75884fefb03c9934003f6f95f1712d673b2
Irina.Chernushina <irina.chernushina@jetbrains.com>
2017-10-18 14:58:39 +0200

    vue.js: completion contributor to reuse code from local resolve
    r=Ushakov
    (cherry picked from commit d4545d3)

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSCompletionContributor.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSCompletionContributor.kt
@@ -4,26 +4,22 @@ import com.intellij.codeInsight.completion.CompletionContributor
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.lang.ecmascript6.psi.ES6ExportDefaultAssignment
-import com.intellij.lang.injection.InjectedLanguageManager
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
-import com.intellij.psi.xml.XmlFile
 
 /**
  * @author Irina.Chernushina on 7/31/2017.
  */
 class VueJSCompletionContributor : CompletionContributor() {
   override fun fillCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) {
-    val xmlFile = InjectedLanguageManager.getInstance(parameters.position.project).getTopLevelFile(
-      parameters.position) as? XmlFile ?: return
-
-    val embeddedScriptContents = org.jetbrains.vuejs.codeInsight.findModule(xmlFile) ?: return
-    val defaultExport = com.intellij.lang.ecmascript6.resolve.ES6PsiUtil.findDefaultExport(embeddedScriptContents)
-    if (defaultExport is ES6ExportDefaultAssignment && defaultExport.stubSafeElement is JSObjectLiteralExpression) {
-      getComponentInnerDetailsFromObjectLiteral(defaultExport.stubSafeElement as JSObjectLiteralExpression)
-        .forEach { result.addElement(LookupElementBuilder.create(it.name)) }
-    }
-
+    if (fillVueCompletionVariants(parameters, result)) return
     super.fillCompletionVariants(parameters, result)
+  }
+
+  private fun fillVueCompletionVariants(parameters: CompletionParameters, result: CompletionResultSet) : Boolean {
+    val scriptWithExport = findScriptWithExport(parameters.position.originalElement) ?: return false
+    val defaultExport = scriptWithExport.second
+    val obj = defaultExport.stubSafeElement as? JSObjectLiteralExpression ?: return false
+    getComponentInnerDetailsFromObjectLiteral(obj).forEach { result.addElement(LookupElementBuilder.create(it.name)) }
+    return true
   }
 }
