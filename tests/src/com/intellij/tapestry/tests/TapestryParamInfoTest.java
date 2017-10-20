0fcddd5467fbc32d4f869f696d2665c9ec04cec2
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2017-10-20 14:41:52 +0300

    stop testing deprecated methods

--- tests/src/com/intellij/tapestry/tests/TapestryParamInfoTest.java
+++ tests/src/com/intellij/tapestry/tests/TapestryParamInfoTest.java
@@ -2,41 +2,36 @@ package com.intellij.tapestry.tests;
 
 import com.intellij.codeInsight.hint.api.impls.XmlParameterInfoHandler;
 import com.intellij.javaee.ExternalResourceManagerEx;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.testFramework.utils.parameterInfo.MockCreateParameterInfoContext;
 import com.intellij.testFramework.utils.parameterInfo.MockParameterInfoUIContext;
-import com.intellij.xml.XmlAttributeDescriptor;
 import com.intellij.xml.XmlElementDescriptor;
-import com.intellij.xml.impl.schema.XmlAttributeDescriptorImpl;
 import com.intellij.xml.util.XmlUtil;
 
 /**
  * @author Alexey.Chmutov
  */
 public class TapestryParamInfoTest extends TapestryBaseTestCase {
-  public void testTmlTagAttrs() throws Exception {
+  public void testTmlTagAttrs() {
     addComponentToProject("Count");
-    doTest("end mixins start value");
+    doTest();
   }
 
-  public void testHtmlTagAttrs() throws Exception {
+  public void testHtmlTagAttrs() {
     final ExternalResourceManagerEx manager = ExternalResourceManagerEx.getInstanceEx();
     final String doctype = manager.getDefaultHtmlDoctype(myFixture.getProject());
     manager.setDefaultHtmlDoctype(XmlUtil.XHTML_URI, myFixture.getProject());
     try {
       addComponentToProject("Count");
-      doTest(
-        // two lang attrs: one for xml:lang
-        "class dir end id lang lang mixins onclick ondblclick onkeydown onkeypress onkeyup onmousedown onmousemove onmouseout onmouseover onmouseup start style title value");
+      doTest();
     }
     finally {
       manager.setDefaultHtmlDoctype(doctype, myFixture.getProject());
     }
   }
 
-  private void doTest(String attrs) {
+  private void doTest() {
     initByComponent();
 
     XmlParameterInfoHandler handler = new XmlParameterInfoHandler();
@@ -50,15 +45,6 @@ public class TapestryParamInfoTest extends TapestryBaseTestCase {
     final XmlElementDescriptor descriptor = (XmlElementDescriptor)items[0];
     MockParameterInfoUIContext context = new MockParameterInfoUIContext<PsiElement>(tag);
     handler.updateUI(descriptor, context);
-
-    String joined = StringUtil.join(handler.getParametersForDocumentation(descriptor, createContext), o -> {
-      if (o instanceof XmlAttributeDescriptorImpl) {
-//          return o.
-      }
-      return ((XmlAttributeDescriptor)o).getName();
-    }, " ");
-    assertEquals(attrs, joined);
-
   }
 
   @Override
