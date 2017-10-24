3a73f374032e176f33815d6aee8dccd67d4535b0
Irina.Chernushina <irina.chernushina@jetbrains.com>
2017-10-24 16:55:37 +0200

    json schema: do not keep schema virtual file(s) in static fields...
    ... of schema providers factories: different project-related user data (including project reference) could (and is actually) cached on this file objects, which leads to leaks
    r=Starovoyt
    (cherry picked from commit 05a5c9d)

--- tslint/src/com/intellij/lang/javascript/linter/tslint/schema/TsLintJsonSchemaProviderFactory.java
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/schema/TsLintJsonSchemaProviderFactory.java
@@ -2,33 +2,27 @@ package com.intellij.lang.javascript.linter.tslint.schema;
 
 import com.intellij.lang.javascript.EmbeddedJsonSchemaFileProvider;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.containers.ContainerUtil;
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider;
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collections;
 import java.util.List;
 
 
 public final class TsLintJsonSchemaProviderFactory implements JsonSchemaProviderFactory {
-
   private static final String TSLINT_SCHEMA_JSON = "tslint-schema.json";
   private static final String TSLINT_JSON_SCHEMA_DIR = "/tslintJsonSchema";
 
   @NotNull
-  private final List<JsonSchemaFileProvider> myProviders;
-
-  public TsLintJsonSchemaProviderFactory() {
-    EmbeddedJsonSchemaFileProvider provider = new EmbeddedJsonSchemaFileProvider(TSLINT_SCHEMA_JSON,
-                                                                                 TsLintJsonSchemaProviderFactory.class,
-                                                                                 TSLINT_JSON_SCHEMA_DIR + '/',
-                                                                                 "tslint.json");
-    myProviders = ContainerUtil.createMaybeSingletonList(provider);
+  private static EmbeddedJsonSchemaFileProvider createProvider() {
+    return new EmbeddedJsonSchemaFileProvider(TSLINT_SCHEMA_JSON,
+                                              TsLintJsonSchemaProviderFactory.class, TSLINT_JSON_SCHEMA_DIR + '/', "tslint.json");
   }
 
   @NotNull
   @Override
   public List<JsonSchemaFileProvider> getProviders(@NotNull final Project project) {
-    return myProviders;
+    return Collections.singletonList(createProvider());
   }
 }
