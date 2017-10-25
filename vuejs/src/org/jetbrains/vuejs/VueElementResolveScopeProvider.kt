b82ec4085ce219a1f596a92067d6b1b40d81b3f9
Irina.Chernushina <irina.chernushina@jetbrains.com>
2017-10-25 17:56:41 +0200

    vue.js + ts types: vue element scope provider to include vue files
    r=Starovoyt, Ulitin
    (cherry picked from commit c1598c8)

--- vuejs/src/org/jetbrains/vuejs/VueElementResolveScopeProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/VueElementResolveScopeProvider.kt
@@ -3,6 +3,7 @@ package org.jetbrains.vuejs
 import com.intellij.lang.javascript.DialectDetector
 import com.intellij.lang.javascript.ecmascript6.TypeScriptResolveScopeProvider
 import com.intellij.lang.javascript.psi.resolve.JSElementResolveScopeProvider
+import com.intellij.lang.typescript.library.TypeScriptLibraryProvider
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
@@ -10,6 +11,13 @@ import com.intellij.psi.search.GlobalSearchScope
 class VueElementResolveScopeProvider : JSElementResolveScopeProvider {
   private val tsProvider = object: TypeScriptResolveScopeProvider() {
     override fun isApplicable(file: VirtualFile): Boolean = true
+
+    override fun restrictByFileType(file: VirtualFile,
+                                    libraryService: TypeScriptLibraryProvider,
+                                    moduleAndLibraryScope: GlobalSearchScope): GlobalSearchScope {
+      return super.restrictByFileType(file, libraryService, moduleAndLibraryScope).
+        uniteWith(GlobalSearchScope.getScopeRestrictedByFileTypes(moduleAndLibraryScope, VueFileType.INSTANCE))
+    }
   }
 
   override fun getElementResolveScope(element: PsiElement): GlobalSearchScope? {
