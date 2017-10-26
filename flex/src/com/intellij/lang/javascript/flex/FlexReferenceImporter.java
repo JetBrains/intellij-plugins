fbaea01a5ef31bed7225d73c6b5e99266a8c7fce
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-10-26 15:48:51 +0300

    typescript: add a new option insert import on the fly WEB-29047

--- flex/src/com/intellij/lang/javascript/flex/FlexReferenceImporter.java
+++ flex/src/com/intellij/lang/javascript/flex/FlexReferenceImporter.java
@@ -1,23 +1,18 @@
 package com.intellij.lang.javascript.flex;
 
 import com.intellij.codeInsight.daemon.ReferenceImporter;
-import com.intellij.codeInsight.daemon.impl.CollectHighlightsUtil;
 import com.intellij.lang.javascript.JavaScriptSupportLoader;
 import com.intellij.lang.javascript.psi.JSFile;
 import com.intellij.lang.javascript.psi.JSReferenceExpression;
-import com.intellij.openapi.editor.Document;
+import com.intellij.lang.javascript.psi.resolve.JSImportHandlingUtil;
 import com.intellij.openapi.editor.Editor;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiPolyVariantReference;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
 
 /**
  * @author Maxim.Mossienko
- *         Date: Apr 14, 2008
- *         Time: 11:43:22 PM
+ * Date: Apr 14, 2008
+ * Time: 11:43:22 PM
  */
 public class FlexReferenceImporter implements ReferenceImporter {
   @Override
@@ -36,19 +31,10 @@ public class FlexReferenceImporter implements ReferenceImporter {
     if (!ActionScriptAutoImportOptionsProvider.isAddUnambiguousImportsOnTheFly()) return false;
     if (!(file instanceof JSFile) || file.getLanguage() != JavaScriptSupportLoader.ECMA_SCRIPT_L4) return false;
 
-    Document document = editor.getDocument();
-    int lineNumber = document.getLineNumber(offset);
-    int startOffset = document.getLineStartOffset(lineNumber);
-    int endOffset = document.getLineEndOffset(lineNumber);
-
-    List<PsiElement> elements = CollectHighlightsUtil.getElementsInRange(file, startOffset, endOffset);
-    for (PsiElement element : elements) {
-      if (element instanceof JSReferenceExpression && ((JSReferenceExpression)element).getQualifier() == null) {
-        if (((JSReferenceExpression)element).multiResolve(false).length == 0) {
-          new AddImportECMAScriptClassOrFunctionAction(editor, (PsiPolyVariantReference)element, true).execute();
-          return true;
-        }
-      }
+    JSReferenceExpression expression = JSImportHandlingUtil.findUnresolvedReferenceExpression(editor, file, offset);
+    if (expression != null) {
+      new AddImportECMAScriptClassOrFunctionAction(editor, expression, true).execute();
+      return true;
     }
     return false;
   }
