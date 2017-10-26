882e7d5cb461e862bb903adc8a88e0e52d754fea
anstarovoyt <andrey.starovoyt@jetbrains.com>
2017-10-26 17:21:57 +0300

    typescript: merge fix (2)

--- tslint/src/com/intellij/lang/javascript/linter/tslint/TsLintConfigReferenceContributor.kt
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/TsLintConfigReferenceContributor.kt
@@ -4,6 +4,7 @@ import com.intellij.json.psi.JsonArray
 import com.intellij.json.psi.JsonFile
 import com.intellij.json.psi.JsonProperty
 import com.intellij.json.psi.JsonStringLiteral
+import com.intellij.lang.ecmascript6.resolve.JSFileReferencesUtil.getSimpleReferencesPathProvider
 import com.intellij.lang.javascript.linter.tslint.config.TsLintConfiguration
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PlatformPatterns.psiFile
