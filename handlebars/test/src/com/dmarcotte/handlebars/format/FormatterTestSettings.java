01f510e09d2cbf82a5516c86aaccea8725ed7f18
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2017-10-27 18:25:20 +0300

    HTML code style settings migration
    Lost and restored.

--- handlebars/test/src/com/dmarcotte/handlebars/format/FormatterTestSettings.java
+++ handlebars/test/src/com/dmarcotte/handlebars/format/FormatterTestSettings.java
@@ -3,6 +3,7 @@ package com.dmarcotte.handlebars.format;
 import com.dmarcotte.handlebars.config.HbConfig;
 import com.intellij.openapi.fileTypes.StdFileTypes;
 import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.formatter.xml.HtmlCodeStyleSettings;
 
 /**
  * Provides a setup and tear down for tests to use to set up the app test fixture
@@ -25,13 +26,13 @@ public class FormatterTestSettings {
     myPrevIndentSize = mySettings.getIndentOptions(StdFileTypes.HTML).INDENT_SIZE;
     mySettings.getIndentOptions(StdFileTypes.HTML).INDENT_SIZE = 4;
 
-    myPrevDoNotIndentSetting = mySettings.HTML_DO_NOT_INDENT_CHILDREN_OF;
-    mySettings.HTML_DO_NOT_INDENT_CHILDREN_OF = "";
+    myPrevDoNotIndentSetting = mySettings.getCustomSettings(HtmlCodeStyleSettings.class).HTML_DO_NOT_INDENT_CHILDREN_OF;
+    mySettings.getCustomSettings(HtmlCodeStyleSettings.class).HTML_DO_NOT_INDENT_CHILDREN_OF = "";
   }
 
   public void tearDown() {
     HbConfig.setFormattingEnabled(myPrevFormatSetting);
     mySettings.getIndentOptions(StdFileTypes.HTML).INDENT_SIZE = myPrevIndentSize;
-    mySettings.HTML_DO_NOT_INDENT_CHILDREN_OF = myPrevDoNotIndentSetting;
+    mySettings.getCustomSettings(HtmlCodeStyleSettings.class).HTML_DO_NOT_INDENT_CHILDREN_OF = myPrevDoNotIndentSetting;
   }
 }
