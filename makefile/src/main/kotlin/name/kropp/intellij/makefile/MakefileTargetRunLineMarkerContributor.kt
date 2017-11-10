416c01a93170db8f79a075d80ea1b41e9ce96ecf
Victor Kropp <victor.kropp@gmail.com>
2017-11-10 18:32:19 +0100

    put line markers on leaf nodes, fix warning
    GitOrigin-RevId: c57fe339bd376c2b4f311d44511ff5f6c31be5c7

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileTargetRunLineMarkerContributor.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileTargetRunLineMarkerContributor.kt
@@ -2,14 +2,21 @@ package name.kropp.intellij.makefile
 
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
 import com.intellij.psi.PsiElement
-import name.kropp.intellij.makefile.psi.MakefileRule
+import name.kropp.intellij.makefile.psi.MakefileTarget
+import name.kropp.intellij.makefile.psi.MakefileTargets
+import name.kropp.intellij.makefile.psi.MakefileTypes
 
 class MakefileTargetRunLineMarkerContributor : RunLineMarkerContributor() {
   override fun getInfo(element: PsiElement): Info? {
-    if (element is MakefileRule) {
-      val targets = element.targets.filterNot { it.isSpecialTarget }
-      if (targets.any()) {
-        return Info(MakefileTargetIcon, { "" }, targets.map(::MakefileRunTargetAction).toTypedArray())
+    if (element.node.elementType == MakefileTypes.IDENTIFIER) {
+      val target = element.parent
+      if (target is MakefileTarget) {
+        val targets = target.parent as MakefileTargets
+        val targetList = targets.targetList
+        if (targetList.firstOrNull() == target &&
+            targetList.any { !it.isSpecialTarget }) {
+          return Info(MakefileTargetIcon, { "" }, targetList.map(::MakefileRunTargetAction).toTypedArray())
+        }
       }
     }
     return null
