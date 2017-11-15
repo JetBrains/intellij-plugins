385c2aac640e730d1419263e8be2ff1c8efc8884
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2017-11-15 21:36:04 +0300

    update java LearnProject; remove redundant jdom 1.1 lib dependency
    GitOrigin-RevId: 63403bdc8db5c9e87896c727041619bd65412caf

--- ide-features-trainer/src/training/lang/MockLangSupport.kt
+++ ide-features-trainer/src/training/lang/MockLangSupport.kt
@@ -1,6 +1,5 @@
 package training.lang
 
-import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkTypeId
@@ -9,28 +8,29 @@ import com.intellij.openapi.projectRoots.SdkTypeId
  * @author Sergey Karashevich
  */
 class MockLangSupport(override val FILE_EXTENSION: String) : AbstractLangSupport() {
-    override fun importLearnProject(): Project? {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  override fun createProject(projectName: String, projectToClose: Project?): Project? {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 
-    override fun checkSdkCompatibility(sdk: Sdk, sdkTypeId: SdkTypeId) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  override fun importLearnProject(): Project? {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 
-    override fun applyToProjectAfterConfigure(): (Project) -> Unit {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  override fun checkSdkCompatibility(sdk: Sdk, sdkTypeId: SdkTypeId) {
+    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 
-    override fun acceptLang(ext: String): Boolean {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  override fun applyToProjectAfterConfigure(): (Project) -> Unit {
+    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 
-    override fun applyProjectSdk(project: Project) {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  override fun acceptLang(ext: String): Boolean {
+    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 
-    override fun getModuleBuilder(): ModuleBuilder {
-        throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+  override fun applyProjectSdk(project: Project) {
+    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
 
 }
