d2df20123fb97fb65d90d4c60a90aaa49d89e632
Irina.Chernushina <Irina.Chernushina@jetbrains.com>
2017-11-16 11:52:37 +0100

    vue.js analysis factory to better cover the unresolved ref cases
    we should suggest only rename reference quick fix for unresolved references
    WEB-29388 Remove Create variable/method/function intention inside vue template

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueAnalysisHandlersFactory.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueAnalysisHandlersFactory.kt
@@ -1,10 +1,14 @@
 package org.jetbrains.vuejs.codeInsight
 
+import com.intellij.codeInspection.LocalQuickFix
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.javascript.JSAnalysisHandlersFactory
+import com.intellij.lang.javascript.highlighting.JSFixFactory
+import com.intellij.lang.javascript.psi.JSCallExpression
 import com.intellij.lang.javascript.psi.JSReferenceExpression
 import com.intellij.lang.javascript.validation.JSAnnotatingVisitor
 import com.intellij.psi.PsiElement
+import com.intellij.psi.ResolveResult
 
 /**
  * @author Irina.Chernushina on 11/7/2017.
@@ -12,8 +16,19 @@ import com.intellij.psi.PsiElement
 class VueAnalysisHandlersFactory : JSAnalysisHandlersFactory() {
   override fun createAnnotatingVisitor(psiElement: PsiElement, holder: AnnotationHolder): JSAnnotatingVisitor {
     return object: JSAnnotatingVisitor(psiElement, holder) {
-      override fun suggestCreateVarFromUsage(node: JSReferenceExpression?): Boolean {
-        return false
+      override fun addCreateFromUsageFixes(node: JSReferenceExpression?,
+                                           resolveResults: Array<out ResolveResult>?,
+                                           fixes: MutableList<LocalQuickFix>?,
+                                           inTypeContext: Boolean,
+                                           ecma: Boolean): Boolean {
+        return inTypeContext
+      }
+
+      override fun addCreateFromUsageFixesForCall(node: JSCallExpression,
+                                                  referenceExpression: JSReferenceExpression,
+                                                  resolveResults: Array<out ResolveResult>,
+                                                  quickFixes: MutableList<LocalQuickFix>) {
+        quickFixes.add(JSFixFactory.getInstance().renameReferenceFix())
       }
     }
   }
