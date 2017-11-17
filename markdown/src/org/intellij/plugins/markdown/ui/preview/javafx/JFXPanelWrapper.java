3825adb90270d0166c1ab73c2e069c0acb71008f
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-11-17 19:37:55 +0300

    RUBY-19897 Duplicated JavaFX markdown preview panels
    IDEA-CR-27034
    
    (cherry picked from commit dfc0f51)

--- markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/JFXPanelWrapper.java
+++ markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/JFXPanelWrapper.java
@@ -3,6 +3,8 @@ package org.intellij.plugins.markdown.ui.preview.javafx;
 import com.intellij.util.FieldAccessor;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.UIUtil;
+import com.sun.javafx.embed.EmbeddedSceneInterface;
+import com.sun.javafx.tk.TKScene;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 
@@ -29,9 +31,16 @@ public class JFXPanelWrapper extends JFXPanel {
 
   @Override
   public void addNotify() {
-    // JFXPanel is scaled asynchronously after first repaint, what may lead
-    // to showing unscaled content. To work it around, set "scaleFactor" ahead.
-    if (UIUtil.isJreHiDPIEnabled()) myScaleFactorAccessor.set(this, Math.round(JBUI.sysScale(this)));
     super.addNotify();
+    if (UIUtil.isJreHiDPIEnabled()) {
+      // JFXPanel is scaled asynchronously after first repaint, what may lead
+      // to showing unscaled content. To work it around, set "scaleFactor" ahead.
+      int scale = Math.round(JBUI.sysScale(this));
+      myScaleFactorAccessor.set(this, scale);
+      TKScene tks = getScene().impl_getPeer();
+      if (tks instanceof EmbeddedSceneInterface) {
+        ((EmbeddedSceneInterface)tks).setPixelScaleFactor(scale);
+      }
+    }
   }
 }
