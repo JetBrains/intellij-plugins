4f488a0d0bb94d3c400c50f6cb5ec050cfb2aec4
Anton Tarasov <anton.tarasov@jetbrains.com>
2017-11-20 18:37:32 +0300

    RUBY-19897 [followup] Duplicated JavaFX markdown preview panels
    IDEA-CR-27034
    
    (cherry picked from commit 00c4323)

--- markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/JFXPanelWrapper.java
+++ markdown/src/org/intellij/plugins/markdown/ui/preview/javafx/JFXPanelWrapper.java
@@ -7,6 +7,7 @@ import com.sun.javafx.embed.EmbeddedSceneInterface;
 import com.sun.javafx.tk.TKScene;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
 
 import java.awt.*;
 
@@ -37,9 +38,14 @@ public class JFXPanelWrapper extends JFXPanel {
       // to showing unscaled content. To work it around, set "scaleFactor" ahead.
       int scale = Math.round(JBUI.sysScale(this));
       myScaleFactorAccessor.set(this, scale);
-      TKScene tks = getScene().impl_getPeer();
-      if (tks instanceof EmbeddedSceneInterface) {
-        ((EmbeddedSceneInterface)tks).setPixelScaleFactor(scale);
+      Scene scene = getScene();
+      // If scene is null then it will be set later and super.setEmbeddedScene(..) will init its scale properly,
+      // otherwise explicitly set scene scale to match JFXPanel.scaleFactor.
+      if (scene != null) {
+        TKScene tks = scene.impl_getPeer();
+        if (tks instanceof EmbeddedSceneInterface) {
+          ((EmbeddedSceneInterface)tks).setPixelScaleFactor(scale);
+        }
       }
     }
   }
