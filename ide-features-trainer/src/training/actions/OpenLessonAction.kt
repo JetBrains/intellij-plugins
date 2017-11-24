edb8c5d2da0044509668e5aae15f9eb5ec556a1d
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2017-11-24 21:31:29 +0300

    rename warning on already passed post-startup activity
    GitOrigin-RevId: 6cad86258dbc699135b516e31879e60ec5d560fe

--- ide-features-trainer/src/training/actions/OpenLessonAction.kt
+++ ide-features-trainer/src/training/actions/OpenLessonAction.kt
@@ -229,7 +229,7 @@ class OpenLessonAction : AnAction() {
 
   private fun openLessonWhenLearnProjectStart(lesson: Lesson?, myLearnProject: Project) {
     val startupManager = StartupManager.getInstance(myLearnProject)
-    if (startupManager is StartupManagerEx && startupManager.postStartupActivityPassed()) LOG.error("DANGER-DANGER-DANGER")
+    if (startupManager is StartupManagerEx && startupManager.postStartupActivityPassed()) LOG.warn("Post startup activity has been already passed")
     startupManager.registerPostStartupActivity {
       val toolWindowManager = ToolWindowManager.getInstance(myLearnProject)
       val learnToolWindow = toolWindowManager.getToolWindow(LearnToolWindowFactory.LEARN_TOOL_WINDOW)
