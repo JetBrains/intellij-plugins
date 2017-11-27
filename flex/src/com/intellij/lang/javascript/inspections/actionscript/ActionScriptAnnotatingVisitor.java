12b6b7f6688e82d987ab1e9412b6d85adeff99b5
Anton Lobov <Anton.Lobov@jetbrains.com>
2017-11-27 15:55:52 +0100

    TS: 'public' in generate (WEB-29895, WEB-28683, WEB-29913)

--- flex/src/com/intellij/lang/javascript/inspections/actionscript/ActionScriptAnnotatingVisitor.java
+++ flex/src/com/intellij/lang/javascript/inspections/actionscript/ActionScriptAnnotatingVisitor.java
@@ -607,7 +607,7 @@ public class ActionScriptAnnotatingVisitor extends TypedJSAnnotatingVisitor {
     }
 
     public void invoke(@NotNull final Project project, final Editor editor, final PsiFile file) throws IncorrectOperationException {
-      JSAttributeListWrapper w = new JSAttributeListWrapper(myNode.getAttributeList());
+      JSAttributeListWrapper w = new JSAttributeListWrapper(myNode.getAttributeList(), project);
       w.overrideModifier(JSAttributeList.ModifierType.OVERRIDE, true);
       w.applyTo(myNode);
     }
