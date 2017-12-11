5a93941fecc612e6b4335a08d85baee6c382d52a
Alexey Kudravtsev <cdr@intellij.com>
2017-12-11 19:42:18 +0300

    cleanup

--- markdown/src/org/intellij/plugins/markdown/lang/psi/MarkdownASTFactory.java
+++ markdown/src/org/intellij/plugins/markdown/lang/psi/MarkdownASTFactory.java
@@ -14,7 +14,7 @@ import org.jetbrains.annotations.Nullable;
 public class MarkdownASTFactory extends ASTFactory {
   @Nullable
   @Override
-  public CompositeElement createComposite(IElementType type) {
+  public CompositeElement createComposite(@NotNull IElementType type) {
     if (type == MarkdownElementTypes.CODE_FENCE) {
       return new MarkdownCodeFenceImpl(type);
     }
