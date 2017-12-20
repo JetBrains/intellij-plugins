fcd384e3920b3956e80f36331ae2467a923fb2b7
Irina.Chernushina <Irina.Chernushina@jetbrains.com>
2017-12-20 17:20:05 +0100

    vue.js, optimize imports test when we need to remove the whole statement

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/language/VueOptimizeImportTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/language/VueOptimizeImportTest.kt
@@ -1,10 +1,13 @@
 package org.jetbrains.vuejs.language
 
+import com.intellij.codeInsight.actions.OptimizeImportsAction
+import com.intellij.ide.DataManager
 import com.intellij.lang.javascript.JSTestUtils
 import com.intellij.lang.javascript.JavaScriptFormatterTestBase
 import com.intellij.lang.javascript.refactoring.JSOptimizeImportTestBase
 import com.intellij.lang.javascript.typescript.TypeScriptFormatterTest
 import com.intellij.openapi.application.PathManager
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.util.Consumer
 
 class VueOptimizeImportTest : JSOptimizeImportTestBase() {
@@ -22,6 +25,16 @@ class VueOptimizeImportTest : JSOptimizeImportTestBase() {
       val name = getTestName(false)
       myFixture.configureByFiles(name + ".vue", name + "_2.ts")
       runOptimizeAction(defaultExtension)
+    }
+  }
+
+  fun testUnusedImportStatement() {
+    JSTestUtils.testES6<RuntimeException>(project) {
+      myFixture.configureByFiles("VueSimpleOptimize_2.ts")
+      myFixture.configureByText("UnusedImportStatement.vue", """<script lang="ts">import {Foo, Foo2} from './VueSimpleOptimize_2';</script>""")
+      OptimizeImportsAction.actionPerformedImpl(DataManager.getInstance().getDataContext(myFixture.editor.contentComponent))
+      FileDocumentManager.getInstance().saveAllDocuments()
+      myFixture.checkResult("<script></script>")
     }
   }
 }
