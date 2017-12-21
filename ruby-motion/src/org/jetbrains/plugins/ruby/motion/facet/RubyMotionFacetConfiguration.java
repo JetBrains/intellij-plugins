8f53379786d36d0b900743b4cc7e75adf2f16d2b
nik <Nikolay.Chashnikov@jetbrains.com>
2017-12-21 19:54:20 +0300

    cleanup: mark read/writeExternal in FacetConfiguration as default
    And remove empty implementations.

--- ruby-motion/src/org/jetbrains/plugins/ruby/motion/facet/RubyMotionFacetConfiguration.java
+++ ruby-motion/src/org/jetbrains/plugins/ruby/motion/facet/RubyMotionFacetConfiguration.java
@@ -19,9 +19,6 @@ import com.intellij.facet.FacetConfiguration;
 import com.intellij.facet.ui.FacetEditorContext;
 import com.intellij.facet.ui.FacetEditorTab;
 import com.intellij.facet.ui.FacetValidatorsManager;
-import com.intellij.openapi.util.InvalidDataException;
-import com.intellij.openapi.util.WriteExternalException;
-import org.jdom.Element;
 
 /**
  * @author Dennis.Ushakov
@@ -31,10 +28,4 @@ public class RubyMotionFacetConfiguration implements FacetConfiguration {
   public FacetEditorTab[] createEditorTabs(FacetEditorContext editorContext, FacetValidatorsManager validatorsManager) {
     return new FacetEditorTab[0];
   }
-
-  @Override
-  public void readExternal(Element element) throws InvalidDataException {}
-
-  @Override
-  public void writeExternal(Element element) throws WriteExternalException {}
 }
