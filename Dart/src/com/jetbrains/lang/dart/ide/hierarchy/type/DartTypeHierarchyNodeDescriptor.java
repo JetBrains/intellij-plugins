e52da2a1f82e8750a43272001a49ad792cf7b3e2
Max Medvedev <maxim.medvedev@jetbrains.com>
2018-01-17 13:30:24 +0300

    [type hierarchy] introduce common `installIcon`

--- Dart/src/com/jetbrains/lang/dart/ide/hierarchy/type/DartTypeHierarchyNodeDescriptor.java
+++ Dart/src/com/jetbrains/lang/dart/ide/hierarchy/type/DartTypeHierarchyNodeDescriptor.java
@@ -1,13 +1,11 @@
 package com.jetbrains.lang.dart.ide.hierarchy.type;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.ide.hierarchy.HierarchyNodeDescriptor;
 import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ui.util.CompositeAppearance;
 import com.intellij.openapi.util.Comparing;
 import com.intellij.psi.PsiElement;
-import com.intellij.ui.LayeredIcon;
 import com.jetbrains.lang.dart.psi.DartClass;
 import com.jetbrains.lang.dart.util.DartResolveUtil;
 import org.jetbrains.annotations.NotNull;
@@ -38,12 +36,7 @@ public final class DartTypeHierarchyNodeDescriptor extends HierarchyNodeDescript
       return invalidElement();
     }
 
-    if (changes && myIsBase) {
-      final LayeredIcon icon = new LayeredIcon(2);
-      icon.setIcon(getIcon(), 0);
-      icon.setIcon(AllIcons.Hierarchy.Base, 1, -AllIcons.Hierarchy.Base.getIconWidth() / 2, 0);
-      setIcon(icon);
-    }
+    installIcon(changes);
 
     final CompositeAppearance oldText = myHighlightedText;
 
