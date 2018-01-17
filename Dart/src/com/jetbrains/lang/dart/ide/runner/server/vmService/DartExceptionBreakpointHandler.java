34b03279044629614a58e88228a3b6371a365817
Devon Carew <devoncarew@google.com>
2018-01-18 02:22:12 +0300

    use session.areBreakpointsMuted to help determine the break on Dart exceptions mode
    (r/AlexD)

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/vmService/DartExceptionBreakpointHandler.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/vmService/DartExceptionBreakpointHandler.java
@@ -1,6 +1,7 @@
 package com.jetbrains.lang.dart.ide.runner.server.vmService;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebuggerManager;
 import com.intellij.xdebugger.breakpoints.XBreakpoint;
 import com.intellij.xdebugger.breakpoints.XBreakpointHandler;
@@ -31,7 +32,11 @@ public class DartExceptionBreakpointHandler extends XBreakpointHandler<XBreakpoi
   }
 
   @NotNull
-  public static ExceptionPauseMode getBreakOnExceptionMode(@Nullable final XBreakpoint<DartExceptionBreakpointProperties> bp) {
+  public static ExceptionPauseMode getBreakOnExceptionMode(@NotNull XDebugSession session,
+                                                           @Nullable final XBreakpoint<DartExceptionBreakpointProperties> bp) {
+    if (session.areBreakpointsMuted()) {
+      return ExceptionPauseMode.None;
+    }
     if (bp == null) return ExceptionPauseMode.Unhandled; // Default to breaking on unhandled exceptions.
     if (!bp.isEnabled()) return ExceptionPauseMode.None;
     return bp.getProperties().isBreakOnAllExceptions() ? ExceptionPauseMode.All : ExceptionPauseMode.Unhandled;
@@ -41,7 +46,7 @@ public class DartExceptionBreakpointHandler extends XBreakpointHandler<XBreakpoi
   public void registerBreakpoint(@NotNull final XBreakpoint<DartExceptionBreakpointProperties> breakpoint) {
     final VmServiceWrapper vmServiceWrapper = myDebugProcess.getVmServiceWrapper();
     if (vmServiceWrapper != null) {
-      vmServiceWrapper.setExceptionPauseMode(getBreakOnExceptionMode(breakpoint));
+      vmServiceWrapper.setExceptionPauseMode(getBreakOnExceptionMode(myDebugProcess.getSession(), breakpoint));
     }
   }
 
