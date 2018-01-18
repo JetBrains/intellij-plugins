c86358eb91a22e394af36ff8980572272f3988f9
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2018-01-18 17:34:02 +0300

    simplify

--- CFML/src/com/intellij/coldFusion/injection/CfmlLanguageInjectionSupport.kt
+++ CFML/src/com/intellij/coldFusion/injection/CfmlLanguageInjectionSupport.kt
@@ -2,11 +2,8 @@ package com.intellij.coldFusion.injection
 
 import com.intellij.coldFusion.model.psi.impl.CfmlTagImpl
 import com.intellij.coldFusion.patterns.CfmlPatterns
-import com.intellij.openapi.util.Ref
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiLanguageInjectionHost
 import org.intellij.plugins.intelliLang.inject.AbstractLanguageInjectionSupport
-import org.intellij.plugins.intelliLang.inject.config.BaseInjection
 
 /**
  * @author Sergey Karashevich
@@ -17,16 +14,13 @@ class CfmlLanguageInjectionSupport : AbstractLanguageInjectionSupport() {
 
   override fun getPatternClasses() = arrayOf(CfmlPatterns::class.java)
 
-  override fun isApplicableTo(host: PsiLanguageInjectionHost) =  (host is CfmlTagImpl && host.name != null && host.name.toLowerCase() == "cfquery")
+  override fun isApplicableTo(host: PsiLanguageInjectionHost) = (host is CfmlTagImpl && host.name != null && host.name.toLowerCase() == "cfquery")
 
   override fun useDefaultInjector(host: PsiLanguageInjectionHost): Boolean = false
 
   override fun getHelpId(): String? = null
 
-  override fun findCommentInjection(host: PsiElement, commentRef: Ref<PsiElement>?): BaseInjection? = super.findCommentInjection(host, commentRef)
-
   companion object {
     val SUPPORT_ID = "cfml"
   }
-
 }
