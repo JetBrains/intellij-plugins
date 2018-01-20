338d8934dfb9750165e042f815d4a6b57a4c1898
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2018-01-20 16:13:44 +0300

    Cleanup (drops obsolete runtime version checks)

--- error-prone/jps-plugin/src/org/intellij/errorProne/ErrorProneJavaCompilingTool.java
+++ error-prone/jps-plugin/src/org/intellij/errorProne/ErrorProneJavaCompilingTool.java
@@ -1,11 +1,10 @@
 package org.intellij.errorProne;
 
-import com.intellij.openapi.util.SystemInfo;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.builders.java.CannotCreateJavaCompilerException;
 import org.jetbrains.jps.builders.java.JavaCompilingTool;
 
-import javax.tools.JavaCompiler;
+import javax.tools.*;
 import java.io.File;
 import java.util.Collections;
 import java.util.List;
@@ -33,9 +32,6 @@ public class ErrorProneJavaCompilingTool extends JavaCompilingTool {
   @NotNull
   @Override
   public JavaCompiler createCompiler() throws CannotCreateJavaCompilerException {
-    if (!SystemInfo.isJavaVersionAtLeast("1.8")) {
-      throw new CannotCreateJavaCompilerException("Error-prone compiler requires JDK 1.8 to run");
-    }
     try {
       return (JavaCompiler)Class.forName("com.google.errorprone.ErrorProneJavaCompiler").newInstance();
     }
