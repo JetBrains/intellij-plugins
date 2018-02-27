3826b6b8654a1d82aecac7dc70cf3391c407a762
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-02-27 16:20:49 +0300

    WEB-31403 Dart Structure View missing sort button (IDEA-CR-29914)

--- Dart/src/com/jetbrains/lang/dart/ide/structure/DartStructureViewModel.java
+++ Dart/src/com/jetbrains/lang/dart/ide/structure/DartStructureViewModel.java
@@ -1,9 +1,23 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.lang.dart.ide.structure;
 
 import com.intellij.ide.structureView.StructureViewModel;
 import com.intellij.ide.structureView.StructureViewTreeElement;
 import com.intellij.ide.structureView.TextEditorBasedStructureViewModel;
 import com.intellij.ide.structureView.impl.common.PsiTreeElementBase;
+import com.intellij.ide.util.treeView.smartTree.Sorter;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiFile;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
@@ -81,6 +95,12 @@ class DartStructureViewModel extends TextEditorBasedStructureViewModel implement
   @Override
   public boolean isAlwaysLeaf(StructureViewTreeElement element) {
     return false;
+  }
+
+  @NotNull
+  @Override
+  public Sorter[] getSorters() {
+    return new Sorter[]{Sorter.ALPHA_SORTER};
   }
 
   private static class DartStructureViewRootElement extends PsiTreeElementBase<PsiFile> {
