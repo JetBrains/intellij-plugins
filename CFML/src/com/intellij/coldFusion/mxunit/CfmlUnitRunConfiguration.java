f54a3b5c3f78f5d293a60cf410a61e8d3b6d8693
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-03-15 13:02:50 +0100

    get rid of http-client-3.1 — CFML

--- CFML/src/com/intellij/coldFusion/mxunit/CfmlUnitRunConfiguration.java
+++ CFML/src/com/intellij/coldFusion/mxunit/CfmlUnitRunConfiguration.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.coldFusion.mxunit;
 
 import com.intellij.coldFusion.CfmlBundle;
@@ -85,7 +83,7 @@ public class CfmlUnitRunConfiguration extends LocatableConfigurationBase {
   }
 
   @Override
-  public RunProfileState getState(@NotNull Executor executor, @NotNull final ExecutionEnvironment env) throws ExecutionException {
+  public RunProfileState getState(@NotNull Executor executor, @NotNull final ExecutionEnvironment env) {
     return new RunProfileState() {
       @Override
       public ExecutionResult execute(Executor executor, @NotNull ProgramRunner runner) throws ExecutionException {
@@ -170,7 +168,7 @@ public class CfmlUnitRunConfiguration extends LocatableConfigurationBase {
     CfmlUnitRemoteTestsRunner.
       executeScript(getRunnerParameters(), processHandler/*webPath, componentFile,
                     params.getScope() == CfmlUnitRunnerParameters.Scope.Method ? params.getMethod() : "",
-                    processHandler*/, getProject());
+                    processHandler*/);
   }
 
   protected CfmlUnitRunnerParameters createRunnerParametersInstance() {
