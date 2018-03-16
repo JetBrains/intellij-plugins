9f212ddc24dd483e35950e4ec2650534101a0762
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-03-16 12:58:52 +0300

    fix DartServerIntentionsTest

--- Dart/testSrc/com/jetbrains/dart/analysisServer/DartServerIntentionsTest.java
+++ Dart/testSrc/com/jetbrains/dart/analysisServer/DartServerIntentionsTest.java
@@ -1,8 +1,22 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.dart.analysisServer;
 
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
 import com.jetbrains.lang.dart.util.DartTestUtils;
 import org.jetbrains.annotations.NotNull;
@@ -14,6 +28,7 @@ public class DartServerIntentionsTest extends CodeInsightFixtureTestCase {
     DartTestUtils.configureDartSdk(myModule, myFixture.getTestRootDisposable(), true);
     DartAnalysisServerService.getInstance(getProject()).serverReadyForRequest(getProject());
     myFixture.setTestDataPath(DartTestUtils.BASE_TEST_DATA_PATH + getBasePath());
+    ((CodeInsightTestFixtureImpl)myFixture).canChangeDocumentDuringHighlighting(true);
   }
 
   @Override
