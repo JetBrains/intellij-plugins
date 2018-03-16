a1a1659676de27a5c538f9aeba2e2b9b9805651c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-03-16 12:53:22 +0300

    fix dart server test data

--- Dart/testSrc/com/jetbrains/dart/analysisServer/DartServerResolverTest.java
+++ Dart/testSrc/com/jetbrains/dart/analysisServer/DartServerResolverTest.java
@@ -1,3 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.dart.analysisServer;
 
 import com.intellij.codeInsight.TargetElementUtil;
@@ -205,7 +218,7 @@ public class DartServerResolverTest extends CodeInsightFixtureTestCase {
       "lib/file2.dart",
 
       "import '<caret expected='lib/file1.dart'>file1.dart<caret expected='lib/file1.dart'>';\n" +
-      "import '<caret expected='lib/file1.dart'>.<caret expected='lib/file1.dart'>/<caret expected='lib/file1.dart'>file1.dart<caret expected='lib/file1.dart'>';\n" +
+      "import '.<caret expected='lib/file1.dart'>/<caret expected='lib/file1.dart'>file1.dart<caret expected='lib/file1.dart'>';\n" +
       //"import '<caret expected='lib/file1.dart'>..<caret expected='lib/file1.dart'>/<caret expected='lib/file1.dart'>lib<caret expected='lib/file1.dart'>/<caret expected='lib/file1.dart'>file1.dart<caret expected='lib/file1.dart'>';\n" +
       "import '<caret expected='lib/src/file9.dart'>src<caret expected='lib/src/file9.dart'>/<caret expected='lib/src/file9.dart'>file9.dart<caret expected='lib/src/file9.dart'>';\n" +
       // todo not sure that DartStringLiteralExpression should be here as reference
