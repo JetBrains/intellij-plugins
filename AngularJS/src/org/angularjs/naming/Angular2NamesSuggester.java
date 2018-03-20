7fa18db6830fb84fe8ff5edb504b02c2b0018c90
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2018-03-20 17:04:22 +0300

    Angular2NamesSuggester: not sure why we check for single decorator
    (cherry picked from commit b70b0b3)
    IDEA-CR-30851

--- AngularJS/src/org/angularjs/naming/Angular2NamesSuggester.java
+++ AngularJS/src/org/angularjs/naming/Angular2NamesSuggester.java
@@ -20,7 +20,6 @@ import com.intellij.lang.javascript.psi.JSExpression;
 import com.intellij.lang.javascript.psi.JSNamedElement;
 import com.intellij.lang.javascript.psi.JSReferenceExpression;
 import com.intellij.lang.javascript.psi.ecma6.ES6Decorator;
-import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList;
 import com.intellij.lang.javascript.psi.ecmal4.JSClass;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -50,17 +49,37 @@ public class Angular2NamesSuggester implements JSNamesSuggester {
 
   @Nullable
   private static String getAngularSpecificFileName(@NotNull JSClass jsClass, @NotNull String newElementName) {
-    JSAttributeList attributeList = jsClass.getAttributeList();
-    if (attributeList == null) {
+    ES6Decorator[] decorators = PsiTreeUtil.getChildrenOfType(jsClass.getAttributeList(), ES6Decorator.class);
+    if (decorators == null) {
       return null;
     }
 
-    ES6Decorator[] decorators = PsiTreeUtil.getChildrenOfType(attributeList, ES6Decorator.class);
-    if (decorators == null || decorators.length != 1) {
-      return null;
+    for (ES6Decorator decorator : decorators) {
+      String referenceName = getDecoratorName(decorator);
+      if (referenceName == null) return null;
+
+      String entityName = AngularDecoratorEntityMap.get(referenceName);
+      if (entityName != null) {
+        String name;
+        if (StringUtil.endsWith(newElementName, entityName)) {
+          name = newElementName.substring(0, newElementName.length() - entityName.length());
+        }
+        else {
+          name = newElementName;
+        }
+
+        String[] parts = name.split(JSNameSuggestionsUtil.SPLIT_BY_CAMEL_CASE_REGEX);
+        String finalName = StringUtil.join(parts, StringUtil::toLowerCase, "-");
+        return (StringUtil.isEmpty(finalName) ? "" : finalName + ".")
+               + StringUtil.toLowerCase(entityName);
+      }  
     }
 
-    ES6Decorator decorator = decorators[0];
+    return null;
+  }
+
+  @Nullable
+  private static String getDecoratorName(@NotNull ES6Decorator decorator) {
     JSExpression expression = decorator.getExpression();
     if (!(expression instanceof JSCallExpression)) {
       return null;
@@ -75,24 +94,6 @@ public class Angular2NamesSuggester implements JSNamesSuggester {
     if (referenceName == null) {
       return null;
     }
-
-    String entityName = AngularDecoratorEntityMap.get(referenceName);
-    if (entityName != null) {
-      String name;
-      if (StringUtil.endsWith(newElementName, entityName)) {
-        name = newElementName.substring(0, newElementName.length() - entityName.length());
-      }
-      else {
-        name = newElementName;
-      }
-
-      String[] parts = name.split(JSNameSuggestionsUtil.SPLIT_BY_CAMEL_CASE_REGEX);
-      String finalName = StringUtil.join(parts, StringUtil::toLowerCase, "-");
-      return (StringUtil.isEmpty(finalName) ? "" : finalName + ".")
-             + StringUtil.toLowerCase(entityName);
-
-    }
-
-    return null;
+    return referenceName;
   }
 }
--- AngularJS/test/org/angularjs/refactoring/data/rename/componentWithRelatedFiles/before/foo-bar.component.ts
+++ AngularJS/test/org/angularjs/refactoring/data/rename/componentWithRelatedFiles/before/foo-bar.component.ts
@@ -1,3 +1,9 @@
+function customDecorator() {
+    return function (ignored) {
+    }
+}
+
+@customDecorator()
 @Component({selector: 'foo-bar'})
 export class FooBar<caret>Component {
 }
