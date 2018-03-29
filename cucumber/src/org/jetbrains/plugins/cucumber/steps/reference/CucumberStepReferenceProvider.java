4ed7255726566f1a8362154324ab5a64f214077c
Andrey Vokin <andrey.vokin@jetbrains.com>
2018-03-29 14:56:07 +0300

    cucumber: move token set creation to private static constant

--- cucumber/src/org/jetbrains/plugins/cucumber/steps/reference/CucumberStepReferenceProvider.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/steps/reference/CucumberStepReferenceProvider.java
@@ -9,36 +9,33 @@ import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.TokenSet;
 import com.intellij.util.ProcessingContext;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.cucumber.psi.GherkinElementTypes;
-import org.jetbrains.plugins.cucumber.psi.GherkinTokenTypes;
 import org.jetbrains.plugins.cucumber.psi.impl.GherkinStepImpl;
 
+import static com.intellij.psi.tree.TokenSet.WHITE_SPACE;
+import static org.jetbrains.plugins.cucumber.psi.GherkinElementTypes.STEP_PARAMETER;
+import static org.jetbrains.plugins.cucumber.psi.GherkinTokenTypes.*;
+
 public class CucumberStepReferenceProvider extends PsiReferenceProvider {
+  private static final TokenSet TEXT_AND_PARAM_SET = TokenSet.create(TEXT, STEP_PARAMETER_TEXT, STEP_PARAMETER_BRACE, STEP_PARAMETER);
+  private static final TokenSet TEXT_PARAM_AND_WHITE_SPACE_SET = TokenSet.orSet(TEXT_AND_PARAM_SET, WHITE_SPACE);
+
   @NotNull
   @Override
   public PsiReference[] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
     if (element instanceof GherkinStepImpl) {
-
-      TokenSet textAndParamSet = TokenSet.create(GherkinTokenTypes.TEXT, GherkinTokenTypes.STEP_PARAMETER_TEXT,
-                                                 GherkinTokenTypes.STEP_PARAMETER_BRACE, GherkinElementTypes.STEP_PARAMETER);
-      ASTNode textNode = element.getNode().findChildByType(textAndParamSet);
-      textAndParamSet = TokenSet.orSet(textAndParamSet, TokenSet.create(TokenType.WHITE_SPACE));
+      ASTNode textNode = element.getNode().findChildByType(TEXT_AND_PARAM_SET);
       if (textNode != null) {
         int start = textNode.getTextRange().getStartOffset();
         int end = textNode.getTextRange().getEndOffset();
         int endBeforeSpace = end;
         textNode = textNode.getTreeNext();
-        while (textNode != null && textAndParamSet.contains(textNode.getElementType())) {
-          if (textNode.getElementType() == TokenType.WHITE_SPACE) {
-            endBeforeSpace = end;
-          } else {
-            endBeforeSpace = textNode.getTextRange().getEndOffset();
-          }
+        while (textNode != null && TEXT_PARAM_AND_WHITE_SPACE_SET.contains(textNode.getElementType())) {
+          endBeforeSpace = textNode.getElementType() == TokenType.WHITE_SPACE ? end : textNode.getTextRange().getEndOffset();
           end = textNode.getTextRange().getEndOffset();
           textNode = textNode.getTreeNext();
         }
-        TextRange tr = new TextRange(start, endBeforeSpace);
-        CucumberStepReference reference =  new CucumberStepReference(element, tr.shiftRight(-element.getTextOffset()));
+        TextRange textRange = new TextRange(start, endBeforeSpace);
+        CucumberStepReference reference =  new CucumberStepReference(element, textRange.shiftRight(-element.getTextOffset()));
         return new PsiReference[] {reference};
       }
     }
