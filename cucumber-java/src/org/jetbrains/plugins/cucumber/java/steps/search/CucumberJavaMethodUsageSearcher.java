b3678a48fdd18b767f5dba0293945f3dddca1ac7
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2018-04-03 15:26:15 +0200

    make Query accept wildcard processor: update executor implementations

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/search/CucumberJavaMethodUsageSearcher.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/search/CucumberJavaMethodUsageSearcher.java
@@ -1,3 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package org.jetbrains.plugins.cucumber.java.steps.search;
 
 import com.intellij.openapi.application.QueryExecutorBase;
@@ -21,7 +34,7 @@ public class CucumberJavaMethodUsageSearcher extends QueryExecutorBase<PsiRefere
   }
 
   @Override
-  public void processQuery(@NotNull final MethodReferencesSearch.SearchParameters p, @NotNull final Processor<PsiReference> consumer) {
+  public void processQuery(@NotNull final MethodReferencesSearch.SearchParameters p, @NotNull final Processor<? super PsiReference> consumer) {
     SearchScope scope = p.getEffectiveSearchScope();
     if (!(scope instanceof GlobalSearchScope)) {
       return;
