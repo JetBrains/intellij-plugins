554d20dbdafe4c1182fdff66d86f154c5dc51f43
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-04-05 17:40:36 +0300

    IDEA-189467 "Create Field" fix generates property with broken syntax
    IDEA-CR-31446

--- flex/flex-tests/testSrc/com/intellij/flex/highlighting/ActionScriptHighlightingTest.java
+++ flex/flex-tests/testSrc/com/intellij/flex/highlighting/ActionScriptHighlightingTest.java
@@ -1,3 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.flex.highlighting;
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
@@ -1932,6 +1945,14 @@ public class ActionScriptHighlightingTest extends ActionScriptDaemonAnalyzerTest
   public void testNoTypeGuessFromAsdoc2() throws Exception {
     final Collection<HighlightInfo> infos = doTestFor(true, getTestName(false) + ".as");
     findAndInvokeActionWithExpectedCheck("Create Field 'aa'", "as", infos);
+  }
+
+  public void testCreateFieldInOtherFile() throws Exception {
+    final String testName = getTestName(false);
+    final Collection<HighlightInfo> infos = doTestFor(true, testName + ".as", testName + "_other.as");
+    findAndInvokeIntentionAction(infos, "Create Field 'zzz'", myEditor, myFile);
+    setActiveEditor(createEditor(myFile.getVirtualFile().findFileByRelativePath("../" + getTestName(false) + "_other.as")));
+    checkResultByFile(BASE_PATH + getTestName(false) + "_other_after.as");
   }
 
   public void testNoImportSuggestForTestClass() throws Exception {
