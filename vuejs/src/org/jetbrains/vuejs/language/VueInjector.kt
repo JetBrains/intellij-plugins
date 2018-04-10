0f15e05573f766f44d2345cbf66bf8bc22a683e2
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-04-10 18:20:30 +0300

    disable JS injection in 'slot' and 'ref' Vue attributes
    (part of WEB-31701)

--- vuejs/src/org/jetbrains/vuejs/language/VueInjector.kt
+++ vuejs/src/org/jetbrains/vuejs/language/VueInjector.kt
@@ -1,3 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package org.jetbrains.vuejs.language
 
 import com.intellij.lang.injection.MultiHostInjector
@@ -28,9 +41,6 @@ import org.jetbrains.vuejs.index.VueOptionsIndex
 import org.jetbrains.vuejs.index.resolve
 import java.util.*
 
-/**
- * @author Irina.Chernushina on 7/19/2017.
- */
 class VueInjector : MultiHostInjector {
   companion object {
     private val delimitersOptionHolders = setOf("Vue.config.delimiters", "Vue.options.delimiters")
@@ -78,7 +88,7 @@ class VueInjector : MultiHostInjector {
 
     // this supposed to work in <template lang="jade"> attribute values
     if (context is XmlAttributeValueImpl && !context.value.isNullOrBlank() && context.parent is XmlAttribute
-        && VueAttributesProvider.DEFAULT.contains((context.parent as XmlAttribute).name)) {
+        && VueAttributesProvider.HAVE_JS_AS_VALUE.contains((context.parent as XmlAttribute).name)) {
       val embedded = PsiTreeUtil.getChildOfType(context, JSEmbeddedContent::class.java)
       if (embedded != null && VueJSLanguage.INSTANCE != embedded.language) {
         val literal = PsiTreeUtil.getChildOfType(embedded, JSLiteralExpressionImpl::class.java)
---
+++ vuejs/vuejs-tests/testData/injection/AttrValueInjection.vue
@@ -0,0 +1,23 @@
+<template>
+  <div ref="someRef"></div>
+  <div slot="someSlot"></div>
+
+  <div v-text="someText"></div>
+  <div v-html="someHtml"></div>
+  <div v-show="someShow"></div>
+  <div v-if="someIf"></div>
+  <div v-else="someElse"></div>
+  <div v-else-if="someElseIf"></div>
+  <div v-for="someFor"></div>
+  <div v-on="someOn"></div>
+  <div v-model="someModel"></div>
+  <div v-pre="somePre"></div>
+  <div v-cloak="someCloak"></div>
+  <div v-once="someOnce"></div>
+
+  <div v-bind="someBind"></div>
+  <div v-bind:key="someBind"></div>
+  <div v-bind:is="someBind"></div>
+  <div :key="someBind"></div>
+  <div :is="someBind"></div>
+</template>
