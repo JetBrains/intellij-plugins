3a94e19f21306785ded0be779fd9a145cc60b632
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-04-23 12:59:01 +0300

    introduced a separate icon for breakpoints being validated - IDEA-190220

--- Dart/src/com/jetbrains/lang/dart/ide/runner/DartLineBreakpointType.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/DartLineBreakpointType.java
@@ -1,5 +1,19 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.lang.dart.ide.runner;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.xdebugger.breakpoints.XLineBreakpointTypeBase;
@@ -7,6 +21,9 @@ import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.DartFileType;
 import com.jetbrains.lang.dart.ide.runner.base.DartDebuggerEditorsProvider;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
 
 public class DartLineBreakpointType extends XLineBreakpointTypeBase {
 
@@ -16,5 +33,11 @@ public class DartLineBreakpointType extends XLineBreakpointTypeBase {
 
   public boolean canPutAt(@NotNull final VirtualFile file, final int line, @NotNull final Project project) {
     return DartFileType.INSTANCE.equals(file.getFileType());
+  }
+
+  @Nullable
+  @Override
+  public Icon getValidatingIcon() {
+    return AllIcons.Debugger.Db_validate_breakpoint;
   }
 }
