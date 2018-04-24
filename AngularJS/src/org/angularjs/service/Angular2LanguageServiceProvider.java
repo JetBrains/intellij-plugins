c7986f6f6f5b7164a58be982cca8f97856867366
anstarovoyt <andrey.starovoyt@jetbrains.com>
2018-04-24 18:41:57 +0300

    angular: fix possible concurrency issue

--- AngularJS/src/org/angularjs/service/Angular2LanguageServiceProvider.java
+++ AngularJS/src/org/angularjs/service/Angular2LanguageServiceProvider.java
@@ -4,16 +4,16 @@ package org.angularjs.service;
 import com.intellij.lang.javascript.service.JSLanguageServiceProvider;
 import com.intellij.lang.typescript.compiler.TypeScriptCompilerSettings;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.AtomicNotNullLazyValue;
 import com.intellij.openapi.util.Disposer;
-import com.intellij.openapi.util.NotNullLazyValue;
 import org.jetbrains.annotations.NotNull;
 
 public class Angular2LanguageServiceProvider implements JSLanguageServiceProvider {
-  private final NotNullLazyValue<Angular2LanguageService> myLanguageService;
+  private final AtomicNotNullLazyValue<Angular2LanguageService> myLanguageService;
 
   public Angular2LanguageServiceProvider(Project project) {
     myLanguageService =
-      NotNullLazyValue.createValue(() -> {
+      AtomicNotNullLazyValue.createValue(() -> {
         Angular2LanguageService service = new Angular2LanguageService(project, TypeScriptCompilerSettings.getSettings(project));
         Disposer.register(project, service);
 
