5815d97c9c2be8dd17a898521a1fa2b9e077f886
Leonid Shalupov <leonid@shalupov.com>
2018-04-27 21:54:34 +0300

    Revert "fix exception in CloudFormationReportSubmitter by correctly making a request"
    This reverts commit 810a55d

--- CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationReportSubmitter.kt
+++ CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationReportSubmitter.kt
@@ -114,11 +114,11 @@ class CloudFormationReportSubmitter : ErrorReportSubmitter() {
                 con.allowUserInteraction = false
                 con.setRequestProperty("Content-Type", "text/xml")
                 con.setFixedLengthStreamingMode(reportBytes.size)
-              }.connect {
-                val os = it.connection.outputStream
+
+                val os = con.outputStream
                 os.write(reportBytes)
                 os.close()
-
+              }.connect {
                 val response = it.connection.inputStream.readBytes()
                 try {
                   LOG.info("Reporting endpoint answered: ${response.toString(Charsets.UTF_8)}")
