4a69eabe17243cffa33fde856fa6617c5d4f8810
Leonid Shalupov <leonid@shalupov.com>
2018-04-28 12:57:28 +0300

    save serverless examples

--- CloudFormation/metadata-crawler/src/main/kotlin/MetadataCrawlerMain.kt
+++ CloudFormation/metadata-crawler/src/main/kotlin/MetadataCrawlerMain.kt
@@ -1,6 +1,7 @@
 object MetadataCrawlerMain {
   @JvmStatic fun main(args: Array<String>) {
     OfficialExamplesSaver.save()
+    OfficialExamplesSaver.saveServerless()
     ChangeLogSaver.saveChangeLog()
     ResourceTypesSaver.saveResourceTypes()
   }
--- CloudFormation/metadata-crawler/src/main/kotlin/OfficialExamplesSaver.kt
+++ CloudFormation/metadata-crawler/src/main/kotlin/OfficialExamplesSaver.kt
@@ -4,6 +4,13 @@ import org.apache.commons.lang3.StringUtils
 import org.jsoup.Jsoup
 import java.io.File
 import java.net.URL
+import java.nio.file.Files
+import java.io.IOException
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.FileSystems
+import java.nio.file.FileVisitResult
+import java.nio.file.Path
 
 object OfficialExamplesSaver {
   fun save() {
@@ -23,8 +30,44 @@ object OfficialExamplesSaver {
         continue
       }
 
-      println("Downloading " + fileUrl)
+      println("Downloading $fileUrl")
       FileUtils.copyURLToFile(fileUrl, localFile)
     }
+  }
+
+  fun saveServerless() {
+    val targetRoot = File("testData/serverless-application-model/src")
+
+    val tempFile = Files.createTempFile("serverless-application-model-master", ".zip")
+    tempFile.toFile().deleteOnExit()
+
+    val url = URL("https://github.com/awslabs/serverless-application-model/archive/master.zip")
+    FileUtils.copyURLToFile(url, tempFile.toFile())
+
+    val zipFs = FileSystems.newFileSystem(tempFile, null)
+
+    val rootPath = zipFs.rootDirectories.toList().single()
+
+    if (targetRoot.exists()) {
+      FileUtils.deleteDirectory(targetRoot)
+    }
+
+    val pathInZip = rootPath.resolve("serverless-application-model-master").resolve("examples")
+    Files.walkFileTree(pathInZip, object : SimpleFileVisitor<Path>() {
+      @Throws(IOException::class)
+      override fun visitFile(filePath: Path, attrs: BasicFileAttributes): FileVisitResult {
+        val fileName = pathInZip.relativize(filePath).toString().replace("/", "_")
+
+        if (!fileName.endsWith(".yaml")) return FileVisitResult.CONTINUE
+
+        val targetPath = targetRoot.toPath().resolve(fileName)
+        println(targetPath)
+
+        Files.createDirectories(targetPath.parent)
+        Files.copy(filePath, targetPath)
+
+        return FileVisitResult.CONTINUE
+      }
+    })
   }
 }
