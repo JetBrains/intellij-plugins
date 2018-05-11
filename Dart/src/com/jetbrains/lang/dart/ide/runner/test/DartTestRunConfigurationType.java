a81b02969ac6593ec3d1b6906507de88cae3fba1
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-05-11 10:55:47 +0200

    IDEA-CR-32484 Do not evaluate icons while loading extensions

--- Dart/src/com/jetbrains/lang/dart/ide/runner/test/DartTestRunConfigurationType.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/test/DartTestRunConfigurationType.java
@@ -1,3 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.jetbrains.lang.dart.ide.runner.test;
 
 import com.intellij.execution.configurations.ConfigurationFactory;
@@ -16,7 +29,7 @@ import org.jetbrains.annotations.NotNull;
 public class DartTestRunConfigurationType extends ConfigurationTypeBase implements DumbAware {
   protected DartTestRunConfigurationType() {
     super("DartTestRunConfigurationType", DartBundle.message("runner.test.configuration.name"),
-          DartBundle.message("runner.test.configuration.description"), DartIcons.Dart_test);
+          DartBundle.message("runner.test.configuration.description"), lazyIcon(() -> DartIcons.Dart_test));
     addFactory(new DartTestConfigurationFactory(this));
   }
 
