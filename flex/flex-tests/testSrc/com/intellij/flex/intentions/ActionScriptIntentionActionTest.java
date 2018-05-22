b82c93bb02ded715640769bf7f717443d615d493
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-05-22 21:11:22 +0300

    IDEA-191387 Flex intentions produce PSI-text inconsistencies, part 2

--- flex/flex-tests/testSrc/com/intellij/flex/intentions/ActionScriptIntentionActionTest.java
+++ flex/flex-tests/testSrc/com/intellij/flex/intentions/ActionScriptIntentionActionTest.java
@@ -1,8 +1,20 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.flex.intentions;
 
 import com.intellij.codeInsight.intention.IntentionAction;
 import com.intellij.flex.util.FlexTestUtils;
-import com.intellij.testFramework.PsiTestUtil;
 import com.intellij.testFramework.fixtures.CodeInsightTestUtil;
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import org.intellij.idea.lang.javascript.intention.JSIntentionBundle;
@@ -33,11 +45,8 @@ public class ActionScriptIntentionActionTest extends LightCodeInsightFixtureTest
   }
 
   public void testMergeDeclarationAndInitialization() {
-    PsiTestUtil.disablePsiTextConsistencyChecks(getTestRootDisposable());
     final String dirName = "mergeDeclarationAndInitialization/";
-
     final String intentionActionName = JSIntentionBundle.message("initialization.merge-declaration-and-initialization.display-name");
-
     doIntentionTest(dirName, "", intentionActionName);
   }
 
