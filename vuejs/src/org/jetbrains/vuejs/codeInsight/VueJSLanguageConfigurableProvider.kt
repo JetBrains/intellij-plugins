5ab8b72eae2b1c203f92222de76eca570c4b8ba5
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2018-05-24 16:59:12 +0300

    vueJS: fix rename in templates, WEB-32949

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSLanguageConfigurableProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSLanguageConfigurableProvider.kt
@@ -1,9 +1,13 @@
 package org.jetbrains.vuejs.codeInsight
 
+import com.intellij.lang.ASTFactory
+import com.intellij.lang.ASTNode
+import com.intellij.lang.javascript.JSTokenTypes
 import com.intellij.lang.javascript.psi.JSInheritedLanguagesConfigurableProvider
 import com.intellij.lang.javascript.psi.JSParenthesizedExpression
 import com.intellij.lang.javascript.psi.impl.JSChangeUtil
 import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.codeStyle.CodeEditUtil
 
 class VueJSLanguageConfigurableProvider : JSInheritedLanguagesConfigurableProvider() {
   override fun isNeedToBeTerminated(element: PsiElement) = false
@@ -14,4 +18,9 @@ class VueJSLanguageConfigurableProvider : JSInheritedLanguagesConfigurableProvid
     return if (created is JSParenthesizedExpression) created.innerExpression else null
   }
 
+  override fun createNameIdentifierFromText(text: String, context: PsiElement): ASTNode? {
+    val node = ASTFactory.leaf(JSTokenTypes.IDENTIFIER, text)
+    CodeEditUtil.setNodeGenerated(node, true)
+    return node
+  }
 }
