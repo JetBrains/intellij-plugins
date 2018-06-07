4d1c4859532eb464cddf66b280bed8100c1a1640
Victor Kropp <victor.kropp@gmail.com>
2018-06-07 22:22:34 +0200

    fix test
    GitOrigin-RevId: 97b8e490dfaab7aed953924a9d7845f7bb1b8262

--- makefile/src/test/kotlin/MakefileStructureViewTest.kt
+++ makefile/src/test/kotlin/MakefileStructureViewTest.kt
@@ -1,20 +1,13 @@
-import com.intellij.ide.structureView.newStructureView.StructureViewComponent
-import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
-import name.kropp.intellij.makefile.MakefileStructureViewElement
-import org.hamcrest.collection.IsCollectionWithSize.hasSize
-import org.hamcrest.core.Is.`is`
-import org.junit.Assert.assertThat
+import com.intellij.testFramework.*
+import com.intellij.testFramework.fixtures.*
 
 class MakefileStructureViewTest : LightPlatformCodeInsightFixtureTestCase() {
   fun testSimple() {
     val filename = "${getTestName(true)}.mk"
     myFixture.configureByFile("$testDataPath/$basePath/$filename")
     myFixture.testStructureView {
-      val root = it.treeStructure.rootElement as StructureViewComponent.StructureViewTreeElementWrapper
-      val file = root.value as MakefileStructureViewElement
-      assertThat(file.presentation.presentableText, `is`(filename))
-
-      assertThat(root.children, hasSize(3))
+      PlatformTestUtil.expandAll(it.tree)
+      PlatformTestUtil.assertTreeEqual(it.tree, "-simple.mk\n all\n hello\n world\n")
     }
   }
 
