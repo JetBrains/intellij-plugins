543487cae0c6fea11106690f85884a763b856b2c
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-06-11 12:07:29 +0300

    WEB-30682 ngIf then/else block shows the error (statement expected)
    r=Starovoyt
    (cherry picked from commit 31067831edfeb101809f1e203a36e0d8a62dec21)

--- AngularJS/test/org/angularjs/lang/parser/data/ngIf.js
+++ AngularJS/test/org/angularjs/lang/parser/data/ngIf.js
@@ -9,3 +9,8 @@ condition; let conditionResult; else bar
 condition; let conditionResult
 userObservable as user; else loading
 userObservable | async as user; else loading
+condition; then foo; else bar
+condition; then; else bar
+condition; then foo; else
+condition; then; else
+condition; then foo; else bar; let conditionResult
