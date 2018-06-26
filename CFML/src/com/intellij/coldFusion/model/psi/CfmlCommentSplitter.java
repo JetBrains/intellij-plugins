101c7b4aa800f7c32f920d50aac4d6dee0ef94db
neuro159 <neuro159@gmail.com>
2018-06-26 12:14:28 +0200

    Spellchecker matcher Allocation

--- CFML/src/com/intellij/coldFusion/model/psi/CfmlCommentSplitter.java
+++ CFML/src/com/intellij/coldFusion/model/psi/CfmlCommentSplitter.java
@@ -1,18 +1,16 @@
-/*
- * Copyright 2000-2013 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.coldFusion.model.psi;
 
 import com.intellij.openapi.util.TextRange;
@@ -25,12 +23,13 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CfmlCommentSplitter extends BaseSplitter {
 
   @NotNull
-  private static final Pattern CFML = Pattern.compile("</?[Cc][Ff](\\S)+");
+  private static final Matcher CFML = Pattern.compile("</?[Cc][Ff](\\S)+").matcher("");
 
   @NotNull
   private static final Splitter ps = PlainTextSplitter.getInstance();
