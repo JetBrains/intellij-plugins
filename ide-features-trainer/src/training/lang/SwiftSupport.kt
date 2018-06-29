1e7fd8da975213679ba83fbff2e1c9c09f341381
Stanislav Dombrovsky <stanislav.dombrovsky@jetbrains.com>
2018-06-29 21:53:20 +0300

    Initial training plugin for Swift. Needs correcting.
    GitOrigin-RevId: 41a24f5a0cffd81cd94ebcd96beeca0c3caa5802

---
+++ ide-features-trainer/src/training/lang/SwiftSupport.kt
@@ -0,0 +1,39 @@
+package training.lang
+
+import com.intellij.openapi.application.ApplicationNamesInfo
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.Sdk
+import com.intellij.openapi.projectRoots.SdkTypeId
+import com.intellij.openapi.wm.ToolWindowAnchor
+import training.project.ProjectUtils
+
+
+/**
+ * @author Sergey Karashevich
+ */
+class SwiftSupport : AbstractLangSupport() {
+    private val acceptableLanguages = setOf("swift")
+    override fun acceptLang(ext: String) = acceptableLanguages.contains(ext.toLowerCase())
+    override val FILE_EXTENSION: String
+        get() = "swift"
+
+
+    override fun importLearnProject(): Project? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    override fun applyProjectSdk(project: Project) {
+    }
+    override fun applyToProjectAfterConfigure(): (Project) -> Unit = {
+    }
+    override fun checkSdkCompatibility(sdk: Sdk, sdkTypeId: SdkTypeId) {
+    }
+    override fun needToCheckSDK(): Boolean {
+        return false
+    }
+    override fun createProject(projectName: String, projectToClose: Project?): Project? {
+        return ProjectUtils.importOrOpenProject("/learnProjects/"+ApplicationNamesInfo.getInstance().fullProductName.toLowerCase() + "_swift/LearnProjectSwift", "LearnProject")
+    }
+    override fun getToolWindowAnchor(): ToolWindowAnchor {
+        return ToolWindowAnchor.RIGHT
+    }
+}
