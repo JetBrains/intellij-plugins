64bbc1c5f16c05cd541187cf4fa268336e62b2db
anstarovoyt <andrey.starovoyt@jetbrains.com>
2018-07-06 18:15:52 +0300

    angular service: support node 9 and higher

--- AngularJS/gen/ngLanguageService/angular-plugin.js
+++ AngularJS/gen/ngLanguageService/angular-plugin.js
@@ -39,6 +39,7 @@ function createPluginClass(state) {
         AngularLanguagePlugin.prototype.createSessionClass = function (ts_impl, defaultOptionsHolder) {
             var sessionClass = _super.prototype.createSessionClass.call(this, ts_impl, defaultOptionsHolder);
             if (ts_impl["ide_processed"]) {
+                var tmpReflect = typeof Reflect != 'undefined' && Reflect.apply ? Reflect : null;
                 var requiredObject = require(state.ngServicePath);
                 var ng = requiredObject;
                 if (typeof requiredObject == "function") {
@@ -48,6 +49,15 @@ function createPluginClass(state) {
                 }
                 ts_impl["ng_service"] = ng;
                 ts_impl["ideUtil"] = util;
+                //workaround for https://github.com/angular/angular/issues/21420
+                if (tmpReflect != null && !Reflect.apply) {
+                    loggerImpl.serverLogger("Restore Reflect after ng service loading", true);
+                    var ngReflect_1 = Reflect;
+                    Reflect = tmpReflect;
+                    Object.keys(ngReflect_1).forEach(function (key) {
+                        Reflect[key] = ngReflect_1[key] || Reflect[key];
+                    });
+                }
                 if (!isVersionCompatible(ng, util, ts_impl)) {
                     ts_impl["ngIncompatible"] = true;
                 }
