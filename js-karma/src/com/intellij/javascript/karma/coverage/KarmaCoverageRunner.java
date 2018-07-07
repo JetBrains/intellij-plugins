a2589db00a4169e6db5343fcc32494e00c9dcc07
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2018-07-08 00:00:02 +0300

    js coverage: extract loading ProjectData from lcov file

--- js-karma/src/com/intellij/javascript/karma/coverage/KarmaCoverageRunner.java
+++ js-karma/src/com/intellij/javascript/karma/coverage/KarmaCoverageRunner.java
@@ -5,22 +5,14 @@ import com.intellij.coverage.CoverageRunner;
 import com.intellij.coverage.CoverageSuite;
 import com.intellij.javascript.karma.KarmaConfig;
 import com.intellij.javascript.karma.server.KarmaServer;
-import com.intellij.javascript.nodejs.interpreter.NodeInterpreterUtil;
-import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreter;
-import com.intellij.javascript.testFramework.coverage.CoverageSerializationUtils;
-import com.intellij.javascript.testFramework.coverage.LcovCoverageReport;
+import com.intellij.javascript.testing.CoverageProjectDataLoader;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.rt.coverage.data.ClassData;
-import com.intellij.rt.coverage.data.LineData;
 import com.intellij.rt.coverage.data.ProjectData;
 import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public class KarmaCoverageRunner extends CoverageRunner {
 
@@ -46,7 +38,7 @@ public class KarmaCoverageRunner extends CoverageRunner {
       File basePathDir = new File(basePath);
       if (basePathDir.isAbsolute() && basePathDir.isDirectory()) {
         try {
-          return readProjectData(sessionDataFile, basePathDir, myKarmaServer.getServerSettings().getNodeInterpreter());
+          return CoverageProjectDataLoader.readProjectData(sessionDataFile, basePathDir, myKarmaServer.getServerSettings().getNodeInterpreter());
         }
         catch (Exception e) {
           LOG.warn("Can't read coverage data", e);
@@ -58,33 +50,6 @@ public class KarmaCoverageRunner extends CoverageRunner {
 
   public void setKarmaServer(@NotNull KarmaServer karmaServer) {
     myKarmaServer = karmaServer;
-  }
-
-  @NotNull
-  private static ProjectData readProjectData(@NotNull File dataFile,
-                                             @NotNull File basePath,
-                                             @NotNull NodeJsInterpreter interpreter) throws IOException {
-    ProjectData projectData = new ProjectData();
-    LcovCoverageReport report = CoverageSerializationUtils.readLCOV(dataFile, basePath,
-                                                                    path -> NodeInterpreterUtil.convertRemotePathToLocal(path, interpreter));
-    for (Map.Entry<String, List<LcovCoverageReport.LineHits>> entry : report.getInfo().entrySet()) {
-      String filePath = entry.getKey();
-      ClassData classData = projectData.getOrCreateClassData(filePath);
-      int max = 0;
-      List<LcovCoverageReport.LineHits> lineHitsList = entry.getValue();
-      if (lineHitsList.size() > 0) {
-        LcovCoverageReport.LineHits lastLineHits = lineHitsList.get(lineHitsList.size() - 1);
-        max = lastLineHits.getLineNumber();
-      }
-      LineData[] lines = new LineData[max + 1];
-      for (LcovCoverageReport.LineHits lineHits : lineHitsList) {
-        LineData lineData = new LineData(lineHits.getLineNumber(), null);
-        lineData.setHits(lineHits.getHits());
-        lines[lineHits.getLineNumber()] = lineData;
-      }
-      classData.setLines(lines);
-    }
-    return projectData;
   }
 
   @Override
