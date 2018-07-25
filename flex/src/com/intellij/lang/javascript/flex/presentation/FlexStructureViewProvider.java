661da16bcdf3771f08a2cf84a0085fa46cdbdc51
Dmitry Jemerov <yole@jetbrains.com>
2018-07-25 18:25:24 +0200

    Convert JS structure view elements to PsiTreeElementBase

--- flex/src/com/intellij/lang/javascript/flex/presentation/FlexStructureViewProvider.java
+++ flex/src/com/intellij/lang/javascript/flex/presentation/FlexStructureViewProvider.java
@@ -1,3 +1,16 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.intellij.lang.javascript.flex.presentation;
 
 import com.intellij.ide.structureView.*;
@@ -71,7 +84,7 @@ public class FlexStructureViewProvider implements XmlStructureViewBuilderProvide
         }
       };
       processor.setLocalResolve(true);
-      myElement.processDeclarations(processor, ResolveState.initial(), myElement, myElement);
+      getElement().processDeclarations(processor, ResolveState.initial(), getElement(), getElement());
       myFile.acceptChildren(new XmlRecursiveElementVisitor() {
         @Override
         public void visitXmlTag(final XmlTag tag) {
