14521220683c6535dedf54574e63d51619b09b76
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-08-03 17:42:53 +0200

    avoid icon initialization if not need

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/DartCommandLineRunConfigurationType.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/DartCommandLineRunConfigurationType.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.runner.server;
 
 import com.intellij.execution.configurations.ConfigurationFactory;
@@ -21,6 +9,7 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.LazyUtil;
 import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.DartFileType;
 import icons.DartIcons;
@@ -35,7 +24,7 @@ public class DartCommandLineRunConfigurationType extends ConfigurationTypeBase i
     super("DartCommandLineRunConfigurationType",
           DartBundle.message("runner.command.line.configuration.name"),
           DartBundle.message("runner.command.line.configuration.description"),
-          lazyIcon(() -> DartIcons.Dart_16));
+          LazyUtil.create(() -> DartIcons.Dart_16));
     addFactory(new ConfigurationFactory(this) {
       @Override
       public String getName() {
--- osmorc/src/org/jetbrains/osgi/bnd/run/BndRunConfigurationType.java
+++ osmorc/src/org/jetbrains/osgi/bnd/run/BndRunConfigurationType.java
@@ -1,20 +1,9 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.osgi.bnd.run;
 
 import com.intellij.execution.configurations.*;
 import com.intellij.openapi.project.Project;
+import com.intellij.util.LazyUtil;
 import icons.OsmorcIdeaIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.osgi.bnd.imp.BndProjectImporter;
@@ -32,7 +21,7 @@ public class BndRunConfigurationType extends ConfigurationTypeBase {
   }
 
   public BndRunConfigurationType() {
-    super(ID, message("bnd.configuration.name"), message("bnd.configuration.description"), lazyIcon(() -> OsmorcIdeaIcons.Bnd));
+    super(ID, message("bnd.configuration.name"), message("bnd.configuration.description"), LazyUtil.create(() -> OsmorcIdeaIcons.Bnd));
     addFactory(new LaunchFactory(this));
     addFactory(new TestFactory(this));
   }
