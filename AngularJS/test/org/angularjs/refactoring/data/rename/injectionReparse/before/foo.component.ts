0a048deaff3b5472ee71a7cd56a2b914214afcf7
Dennis Ushakov <dennis.ushakov@gmail.com>
2018-08-06 20:25:37 +0300

    use proper language for xml text encoding (WEB-31258)
    (cherry picked from commit 7cbb34b18dcf097181d1a8ddc2ed143c5a77851f)
    (cherry picked from commit f76df8760c9c5ded285e13e5b4ffe0ec80b9eb61)

---
+++ AngularJS/test/org/angularjs/refactoring/data/rename/injectionReparse/before/foo.component.ts
@@ -0,0 +1,19 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-my-component2',
+    templateUrl: './foo.component.html'
+})
+export class MyComponentComponent implements OnInit {
+    @Input() produkt: Product;
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
+
+export class Product {
+    price: number;
+}
