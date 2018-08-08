789c19c5a3e844667e5a3a5c74b75ed4a9b50d49
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2018-08-08 15:36:40 +0200

    cleanup

--- flex/src/com/intellij/lang/javascript/flex/run/FlashRunConfigurationProducer.java
+++ flex/src/com/intellij/lang/javascript/flex/run/FlashRunConfigurationProducer.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.javascript.flex.run;
 
 import com.intellij.execution.RunManager;
@@ -30,6 +31,7 @@ public class FlashRunConfigurationProducer extends RunConfigurationProducer<Flas
     super(FlashRunConfigurationType.getInstance());
   }
 
+  @Override
   protected boolean setupConfigurationFromContext(final FlashRunConfiguration configuration,
                                                   final ConfigurationContext context,
                                                   final Ref<PsiElement> sourceElement) {
@@ -77,6 +79,7 @@ public class FlashRunConfigurationProducer extends RunConfigurationProducer<Flas
     return true;
   }
 
+  @Override
   public boolean isConfigurationFromContext(final FlashRunConfiguration configuration, final ConfigurationContext context) {
     final Module module = context.getModule();
     if (module == null || ModuleType.get(module) != FlexModuleType.getInstance()) return false;
