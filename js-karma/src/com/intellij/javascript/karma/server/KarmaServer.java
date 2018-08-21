b611d56315ade574b319d07a5de1c31c0afe6adc
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2018-08-21 20:03:04 +0300

    JS debugger: pass --source-map when debugging ng tests (WEB-34474)

--- js-karma/src/com/intellij/javascript/karma/server/KarmaServer.java
+++ js-karma/src/com/intellij/javascript/karma/server/KarmaServer.java
@@ -216,6 +216,10 @@ public class KarmaServer {
         }
         commandLine.addParameters("--karma-config", configFile.getAbsolutePath());
         commandLineFolder.addPlaceholderText("--karma-config=" + userConfigFileName);
+
+        if (serverSettings.isDebug()) {
+          commandLine.addParameter("--source-map"); // added in version 6.0.0
+        }
       }
     }
     else {
