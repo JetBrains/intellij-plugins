8c5844ca97ccc6b600346adb06767ce07b69930f
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-08-31 16:46:34 +0200

    WEB-34615 Support Dart 2.1 super mixins
    (IDEA-CR-36616)

---
+++ Dart/gen/com/jetbrains/lang/dart/psi/impl/DartMixinDeclarationImpl.java
@@ -0,0 +1,79 @@
+// Copyright 2000-2018 JetBrains s.r.o.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is a generated file. Not intended for manual editing.
+package com.jetbrains.lang.dart.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.util.PsiTreeUtil;
+import static com.jetbrains.lang.dart.DartTokenTypes.*;
+import com.jetbrains.lang.dart.psi.*;
+import com.jetbrains.lang.dart.util.DartPsiImplUtil;
+
+public class DartMixinDeclarationImpl extends AbstractDartPsiClass implements DartMixinDeclaration {
+
+  public DartMixinDeclarationImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  public void accept(@NotNull DartVisitor visitor) {
+    visitor.visitMixinDeclaration(this);
+  }
+
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof DartVisitor) accept((DartVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public DartClassBody getClassBody() {
+    return findNotNullChildByClass(DartClassBody.class);
+  }
+
+  @Override
+  @NotNull
+  public DartComponentName getComponentName() {
+    return findNotNullChildByClass(DartComponentName.class);
+  }
+
+  @Override
+  @Nullable
+  public DartInterfaces getInterfaces() {
+    return findChildByClass(DartInterfaces.class);
+  }
+
+  @Override
+  @NotNull
+  public List<DartMetadata> getMetadataList() {
+    return PsiTreeUtil.getChildrenOfTypeAsList(this, DartMetadata.class);
+  }
+
+  @Override
+  @Nullable
+  public DartOnMixins getOnMixins() {
+    return findChildByClass(DartOnMixins.class);
+  }
+
+  @Override
+  @Nullable
+  public DartTypeParameters getTypeParameters() {
+    return findChildByClass(DartTypeParameters.class);
+  }
+
+}
