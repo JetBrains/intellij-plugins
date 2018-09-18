a4e72813b51a14885b8283c5486b659e61cc8212
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-09-18 16:36:50 +0300

    Tune New Vue Component action availability

--- vuejs/src/org/jetbrains/vuejs/CreateVueSingleFileComponentAction.kt
+++ vuejs/src/org/jetbrains/vuejs/CreateVueSingleFileComponentAction.kt
@@ -1,22 +1,28 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
 import com.intellij.lang.javascript.JavaScriptSupportLoader
+import com.intellij.openapi.actionSystem.DataContext
+import com.intellij.openapi.actionSystem.PlatformDataKeys
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
 import icons.VuejsIcons
+import org.jetbrains.vuejs.index.hasVue
 
-/**
- * @author Irina.Chernushina on 10/4/2017.
- */
 class CreateVueSingleFileComponentAction : CreateFileFromTemplateAction(VueBundle.message("vue.create.single.file.component.action.text"),
-                                                                        VueBundle.message("vue.create.single.file.component.action.description"),
+                                                                        VueBundle.message(
+                                                                          "vue.create.single.file.component.action.description"),
                                                                         VuejsIcons.Vue), DumbAware {
   companion object {
     val VUE_TEMPLATE_NAME: String = "Vue Single File Component"
     private val name = VueBundle.message("vue.create.single.file.component.action.text")
+  }
+
+  override fun isAvailable(dataContext: DataContext): Boolean {
+    return super.isAvailable(dataContext) && hasVue(PlatformDataKeys.PROJECT.getData(dataContext) ?: return false)
   }
 
   override fun buildDialog(project: Project?, directory: PsiDirectory?, builder: CreateFileFromTemplateDialog.Builder?) {
