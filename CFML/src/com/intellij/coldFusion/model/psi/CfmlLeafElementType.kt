871b8d48a4daee235170ee24216c3e988d7e9539
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2018-09-24 18:04:46 +0300

    [cfml] let cfif tag in cfquery be the valid host for SQL injection

--- CFML/src/com/intellij/coldFusion/model/psi/CfmlLeafElementType.kt
+++ CFML/src/com/intellij/coldFusion/model/psi/CfmlLeafElementType.kt
@@ -32,9 +32,15 @@ class CfmlLeafElementType(s: String) : CfmlElementType(s), ILeafElementType {
   }
 }
 
-class CfmlLeafPsiElement(cfmlLeafElementType: CfmlLeafElementType, leafText: CharSequence) : LeafPsiElement(cfmlLeafElementType, leafText), PsiLanguageInjectionHost {
+class CfmlLeafPsiElement(cfmlLeafElementType: CfmlLeafElementType, leafText: CharSequence) : LeafPsiElement(cfmlLeafElementType,
+                                                                                                            leafText), PsiLanguageInjectionHost {
 
-  override fun isValidHost(): Boolean = (this.parent is CfmlTagImpl && (this.parent as CfmlTagImpl).name?.toLowerCase() == "cfquery")
+  override fun isValidHost(): Boolean {
+    if (this.parent !is CfmlTagImpl) return false
+    val isCfquery = (this.parent as CfmlTagImpl).name?.toLowerCase() == "cfquery"
+    val isCfifInCfquery = (this.parent as CfmlTagImpl).name?.toLowerCase() == "cfif" && (this.parent?.parent as CfmlTagImpl).name?.toLowerCase() == "cfquery"
+    return isCfquery || isCfifInCfquery
+  }
 
   override fun updateText(text: String): PsiLanguageInjectionHost = replaceWithText(text) as PsiLanguageInjectionHost
 
