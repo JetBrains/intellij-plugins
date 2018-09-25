8dfb97ee97d1503a3f832e6806b262b194db323b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-09-25 15:15:14 +0200

    WEB-34218 - add test

--- AngularJS/src/org/angular2/codeInsight/Angular2LanguageConfigurableProvider.java
+++ AngularJS/src/org/angular2/codeInsight/Angular2LanguageConfigurableProvider.java
@@ -1,14 +1,9 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.codeInsight;
 
-import com.intellij.lang.javascript.psi.JSExpressionStatement;
 import com.intellij.lang.javascript.psi.JSInheritedLanguagesConfigurableProvider;
-import com.intellij.lang.javascript.psi.JSParenthesizedExpression;
-import com.intellij.lang.javascript.psi.impl.JSChangeUtil;
 import com.intellij.psi.PsiElement;
-import com.intellij.util.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class Angular2LanguageConfigurableProvider extends JSInheritedLanguagesConfigurableProvider {
   @Override
@@ -16,15 +11,4 @@ public class Angular2LanguageConfigurableProvider extends JSInheritedLanguagesCo
     return false;
   }
 
-  @Nullable
-  @Override
-  protected PsiElement createExpressionFromText(@NotNull String text,
-                                                @NotNull PsiElement element) {
-    JSExpressionStatement created =
-      JSChangeUtil.createStatementPsiFromTextWithContext("(" + text + ")", element, JSExpressionStatement.class);
-    JSParenthesizedExpression parenthesized = ObjectUtils.tryCast(created != null
-                                                                  ? created.getExpression()
-                                                                  : null, JSParenthesizedExpression.class);
-    return parenthesized != null ? parenthesized.getInnerExpression() : null;
-  }
 }
---
+++ AngularJS/test/org/angular2/refactoring/AngularChangeSignatureTest.java
@@ -0,0 +1,34 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.refactoring;
+
+import com.intellij.lang.javascript.JSChangeSignatureTestBase;
+import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList;
+import com.intellij.lang.javascript.refactoring.changeSignature.JSParameterInfo;
+import org.angularjs.AngularTestUtil;
+import org.jetbrains.annotations.NotNull;
+
+public class AngularChangeSignatureTest extends JSChangeSignatureTestBase {
+
+  @Override
+  protected String getTestDataPath() {
+    return AngularTestUtil.getBaseTestDataPath(getClass());
+  }
+
+  @Override
+  protected String[] getActiveFileNames() {
+    return new String[] {"component.ts"};
+  }
+
+  @NotNull
+  @Override
+  protected String getTestRoot() {
+    return "changeSignature/";
+  }
+
+
+  public void testAddParamAndRenameAndSetReturnType() {
+    doTest("bar", JSAttributeList.AccessType.PUBLIC, "number",
+           new JSParameterInfo("stringParam", "string", "", "\"def\"", -1));
+  }
+
+}
