2dceadb936bd1aa1b80d57b3c63e8bcafb13dd6e
anstarovoyt <andrey.starovoyt@jetbrains.com>
2018-09-28 19:08:13 +0300

    typescript: cache annotation provider per file

--- AngularJS/src/org/angular2/settings/AngularSettings.java
+++ AngularJS/src/org/angular2/settings/AngularSettings.java
@@ -6,6 +6,8 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.ModificationTracker;
+import com.intellij.openapi.util.SimpleModificationTracker;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,7 +17,10 @@ public class AngularSettings implements
                              TypeScriptServerServiceSettings,
                              PersistentStateComponent<AngularSettings.Settings> {
 
+  private final SimpleModificationTracker myModificationTracker = new SimpleModificationTracker();  
+  
   private Settings myState = new AngularSettings.Settings();
+  
 
   public static AngularSettings get(Project project) {
     return ServiceManager.getService(project, AngularSettings.class);
@@ -34,6 +39,9 @@ public class AngularSettings implements
 
   @Override
   public void loadState(@NotNull AngularSettings.Settings state) {
+    if (myState == null || myState.useService != state.useService) {
+      myModificationTracker.incModificationCount();
+    }
     myState = state;
   }
 
@@ -44,11 +52,20 @@ public class AngularSettings implements
 
   @Override
   public void setUseService(boolean useService) {
+    if (myState.useService != useService) {
+      myModificationTracker.incModificationCount();
+    }
     myState.useService = useService;
   }
 
   @Override
   public String getTitle() {
     return "Angular Language Service";
+  }
+
+  @NotNull
+  @Override
+  public ModificationTracker getModificationTracker() {
+    return myModificationTracker;
   }
 }
