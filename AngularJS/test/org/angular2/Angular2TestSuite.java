79a17f7dbe249bc439cb4b6d53642f5aecf67d77
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-10-08 13:23:35 +0200

    Include Angular 2 tests in Angular JS test suite.

---
+++ AngularJS/test/org/angular2/Angular2TestSuite.java
@@ -0,0 +1,53 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2;
+
+import org.angular2.cli.BlueprintListTest;
+import org.angular2.codeInsight.*;
+import org.angular2.formatting.FormattingTest;
+import org.angular2.index.AngularIndexUtilTest;
+import org.angular2.inspections.EmptyEventHandlerInspectionTest;
+import org.angular2.lang.expr.lexer.Angular2LexerSpecTest;
+import org.angular2.lang.expr.lexer.Angular2LexerTest;
+import org.angular2.lang.expr.parser.Angular2ParserSpecTest;
+import org.angular2.lang.expr.parser.Angular2ParserTest;
+import org.angular2.lang.html.highlighting.Angular2HtmlHighlightingTest;
+import org.angular2.lang.html.lexer.Angular2HtmlLexerSpecTest;
+import org.angular2.lang.html.lexer.Angular2HtmlLexerTest;
+import org.angular2.lang.html.parser.Angular2HtmlParsingTest;
+import org.angular2.refactoring.AngularChangeSignatureTest;
+import org.angular2.refactoring.DirectiveRenameTest;
+import org.angular2.refactoring.RenameTest;
+import org.angular2.service.Angular2ServiceTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  BlueprintListTest.class,
+  Angular2LexerSpecTest.class,
+  Angular2LexerTest.class,
+  Angular2ParserSpecTest.class,
+  Angular2ParserTest.class,
+  Angular2HtmlLexerSpecTest.class,
+  Angular2HtmlLexerTest.class,
+  Angular2HtmlHighlightingTest.class,
+  Angular2HtmlParsingTest.class,
+  AttributesTest.class,
+  ContextTest.class,
+  InjectionsTest.class,
+  InspectionsTest.class,
+  IntentionsTest.class,
+  NgForTest.class,
+  PipesTest.class,
+  ScopesTest.class,
+  TagsTest.class,
+  FormattingTest.class,
+  AngularIndexUtilTest.class,
+  EmptyEventHandlerInspectionTest.class,
+  AngularChangeSignatureTest.class,
+  DirectiveRenameTest.class,
+  RenameTest.class,
+  Angular2ServiceTest.class,
+})
+public class Angular2TestSuite {
+}
--- AngularJS/test/org/angularjs/AngularJSTestSuite.java
+++ AngularJS/test/org/angularjs/AngularJSTestSuite.java
@@ -1,38 +1,41 @@
 package org.angularjs;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+import org.angular2.Angular2TestSuite;
 import org.angularjs.codeInsight.*;
 import org.angularjs.codeInsight.messageFormat.AngularMessageFormatAnnotatorTest;
 import org.angularjs.editor.AngularTypedHandlerTest;
 import org.angularjs.findUsages.FindUsagesTest;
 import org.angularjs.index.AngularDirectiveCommentParsingTest;
 import org.angularjs.lang.lexer.AngularJSLexerTest;
+import org.angularjs.lang.parser.AngularJSParserTest;
 import org.angularjs.refactoring.DirectiveRenameTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  AngularJSLexerTest.class,
+  AngularJSParserTest.class,
+  AttributesTest.class,
+  DependencyInjectionTest.class,
+  DocumentationTest.class,
+  FiltersTest.class,
+  InjectionsTest.class,
+  NgRepeatTest.class,
+  RoutingTest.class,
+  TagsTest.class,
+  AngularTypedHandlerTest.class,
+  FindUsagesTest.class,
+  AngularDirectiveCommentParsingTest.class,
+  AngularJSInjectorMatchingEndFinderTest.class,
+  AngularMessageFormatAnnotatorTest.class,
+  DirectiveRenameTest.class,
+  AngularUiRouterTest.class,
+  ControllerTest.class,
+  NgDocTest.class,
+  Angular2TestSuite.class
+})
 public class AngularJSTestSuite {
 
-  public static Test suite() {
-    final TestSuite testSuite = new TestSuite(AngularJSTestSuite.class.getSimpleName());
-
-    testSuite.addTestSuite(AngularJSLexerTest.class);
-    //testSuite.addTestSuite(AngularJSParserTest.class); seems it can't handle @Test annotation
-    testSuite.addTestSuite(AttributesTest.class);
-    testSuite.addTestSuite(DependencyInjectionTest.class);
-    testSuite.addTestSuite(DocumentationTest.class);
-    testSuite.addTestSuite(FiltersTest.class);
-    testSuite.addTestSuite(InjectionsTest.class);
-    testSuite.addTestSuite(NgRepeatTest.class);
-    testSuite.addTestSuite(RoutingTest.class);
-    testSuite.addTestSuite(TagsTest.class);
-    testSuite.addTestSuite(AngularTypedHandlerTest.class);
-    testSuite.addTestSuite(FindUsagesTest.class);
-    testSuite.addTestSuite(AngularDirectiveCommentParsingTest.class);
-    testSuite.addTestSuite(AngularJSInjectorMatchingEndFinderTest.class);
-    testSuite.addTestSuite(AngularMessageFormatAnnotatorTest.class);
-    testSuite.addTestSuite(DirectiveRenameTest.class);
-    testSuite.addTestSuite(AngularUiRouterTest.class);
-    testSuite.addTestSuite(ControllerTest.class);
-    return testSuite;
-  }
 }
