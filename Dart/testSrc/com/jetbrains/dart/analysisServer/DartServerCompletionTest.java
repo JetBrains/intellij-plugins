f724029e53a689bb797838dfb90998104f93e65d
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-10-16 19:53:06 +0300

    WEB-35068 Dart completion prevents from typing a number literal

--- Dart/testSrc/com/jetbrains/dart/analysisServer/DartServerCompletionTest.java
+++ Dart/testSrc/com/jetbrains/dart/analysisServer/DartServerCompletionTest.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.dart.analysisServer;
 
 import com.intellij.codeInsight.completion.CompletionType;
@@ -173,5 +161,18 @@ public class DartServerCompletionTest extends CodeInsightFixtureTestCase {
     myFixture.assertPreferredCompletionItems(0, "AXX.one", "AXX.two", "main", "const", "false", "new", "null", "true",
                                              "AbstractClassInstantiationError", "ArgumentError", "AssertionError", "AXB", "AXB.four",
                                              "AXB.three", "AXX", "BidirectionalIterator");
+  }
+
+  public void testNoCompletionAfterDigit() {
+    myFixture.configureByText("foo.dart",
+                              "main() {\n" +
+                              "  return Foo(bar: 1<caret>, );\n" +
+                              "}\n" +
+                              "class Foo {\n" +
+                              "  final double bar, baz;\n" +
+                              "  const Foo({ this.bar, this.baz, });\n" +
+                              "}\n");
+    myFixture.completeBasic();
+    assertNull(myFixture.getLookup());
   }
 }
