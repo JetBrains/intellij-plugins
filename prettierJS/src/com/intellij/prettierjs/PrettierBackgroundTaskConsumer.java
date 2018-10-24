74ed0e8f5317476f1d26a084fe659c2ef5098678
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-10-24 15:02:00 +0300

    IDEA-CR-38551 Avoid Windows-specific executable extension in a File Watcher template to keep IDE project files shareable
    (on Windows the File Watcher engine will add a required file extension automatically)

--- prettierJS/src/com/intellij/prettierjs/PrettierBackgroundTaskConsumer.java
+++ prettierJS/src/com/intellij/prettierjs/PrettierBackgroundTaskConsumer.java
@@ -1,22 +1,9 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.prettierjs;
 
 import com.intellij.ide.macro.FilePathRelativeToProjectRootMacro;
 import com.intellij.ide.macro.ProjectFileDirMacro;
 import com.intellij.lang.javascript.JavaScriptFileType;
-import com.intellij.openapi.util.SystemInfo;
 import com.intellij.plugins.watcher.config.BackgroundTaskConsumer;
 import com.intellij.plugins.watcher.model.TaskOptions;
 import com.intellij.psi.PsiBundle;
@@ -40,7 +27,7 @@ public class PrettierBackgroundTaskConsumer extends BackgroundTaskConsumer {
 
     String projectFileDirMacro = new ProjectFileDirMacro().getName();
     String projectRootRelativePathMacro = new FilePathRelativeToProjectRootMacro().getName();
-    options.setProgram("$" + projectFileDirMacro + "$" + "/node_modules/.bin/prettier" + (SystemInfo.isWindows ? ".cmd" : ""));
+    options.setProgram("$" + projectFileDirMacro + "$" + "/node_modules/.bin/prettier");
     options.setArguments("--write $" + projectRootRelativePathMacro + "$");
     options.setWorkingDir("$" + projectFileDirMacro + "$");
 
