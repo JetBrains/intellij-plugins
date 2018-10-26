759c94e5f8a53ed47ad43508e8a0967befd7b913
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-10-26 11:58:13 +0200

    IDEA-CR-38751: WEB-35485 Do not allow for manual injection of Angular2 expression language.
    (cherry picked from commit 5a2effa86504a8b2e7e3494776a27766029418e8)

--- AngularJS/src/org/angular2/lang/expr/Angular2Language.java
+++ AngularJS/src/org/angular2/lang/expr/Angular2Language.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.lang.expr;
 
+import com.intellij.lang.DependentLanguage;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.javascript.DialectOptionHolder;
 import com.intellij.lang.javascript.JSLanguageDialect;
@@ -8,7 +9,7 @@ import com.intellij.lang.javascript.parsing.JavaScriptParser;
 import org.angular2.lang.expr.parser.Angular2Parser;
 import org.jetbrains.annotations.NotNull;
 
-public class Angular2Language extends JSLanguageDialect {
+public class Angular2Language extends JSLanguageDialect implements DependentLanguage {
   public static final Angular2Language INSTANCE = new Angular2Language();
 
   protected Angular2Language() {
