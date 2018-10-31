06ef504490b4b05ab99acc8b3f6ed52ed57e4ad7
anstarovoyt <andrey.starovoyt@jetbrains.com>
2018-10-31 12:15:13 +0300

    IDEA-CR-39004: vue: tests for WEB-34818
    (cherry picked from commit d08c83faaa690f5ea59f08b04f92dd9312142456)

---
+++ vuejs/vuejs-tests/testData/ts_imports/SimpleDeclare/simpleTyping.ts
@@ -0,0 +1,5 @@
+declare module "my-mod" {
+    class MyMod {
+        do1();
+    }
+}
---
+++ vuejs/vuejs-tests/testData/ts_imports/TypesModule/TypesModule.vue
@@ -0,0 +1,8 @@
+<template><div></div></template>
+<script lang="ts">
+  //no errors
+  import { Meteor, <error>NoMeteor</error> } from 'meteor/meteor';
+
+  console.log(Meteor);
+
+</script>
