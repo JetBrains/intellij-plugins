d08c83faaa690f5ea59f08b04f92dd9312142456
anstarovoyt <andrey.starovoyt@jetbrains.com>
2018-10-31 12:15:13 +0300

    vue: tests for WEB-34818

---
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/language/VueTypeScriptModuleTest.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.language
+
+import com.intellij.lang.javascript.modules.JSImportHighlightingAndCompletionTestBase
+
+private val BASE_PATH = "/ts_imports"
+
+class VueTypeScriptModuleTest: JSImportHighlightingAndCompletionTestBase() {
+  
+  override fun getBasePath(): String = BASE_PATH
+  override fun getExtension(): String = "vue"
+  override fun getTestDataPath(): String = getVueTestDataPath()
+
+  fun testTypesModule() {
+    doTestWithCopyDirectory()
+  }
+
+  fun testSimpleDeclare() {
+    doTestWithCopyDirectory()
+  }
+}
