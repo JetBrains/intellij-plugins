f2f5ccf038009571f71a0b6649a20a481b7e9b85
Egor Ushakov <egor.ushakov@jetbrains.com>
2018-11-01 14:54:23 +0300

    IDEA-CR-39075: avoid to String conversion where not needed
    (cherry picked from commit 0b10f792480de0512365d02d272354ed56c0314c)

--- AngularJS/src/org/angular2/entities/metadata/Angular2MetadataFileTypeFactory.java
+++ AngularJS/src/org/angular2/entities/metadata/Angular2MetadataFileTypeFactory.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.entities.metadata;
 
-import com.intellij.openapi.fileTypes.FileNameMatcher;
+import com.intellij.openapi.fileTypes.FileNameMatcherEx;
 import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
@@ -9,10 +9,9 @@ import org.jetbrains.annotations.NotNull;
 public class Angular2MetadataFileTypeFactory extends FileTypeFactory {
   @Override
   public void createFileTypes(@NotNull FileTypeConsumer consumer) {
-    consumer.consume(Angular2MetadataFileType.INSTANCE, new FileNameMatcher() {
-
+    consumer.consume(Angular2MetadataFileType.INSTANCE, new FileNameMatcherEx() {
       @Override
-      public boolean accept(@NotNull String fileName) {
+      public boolean acceptsCharSequence(@NotNull CharSequence fileName) {
         return false;
       }
 
