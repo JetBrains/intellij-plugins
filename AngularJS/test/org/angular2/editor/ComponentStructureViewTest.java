749cb22f6889730b9f6609bdd40f878346e2e470
Dennis Ushakov <dennis.ushakov@gmail.com>
2018-11-12 21:38:07 +0300

    don't show frameworks implicit elements in structure view (WEB-35673)

---
+++ AngularJS/test/org/angular2/editor/ComponentStructureViewTest.java
@@ -0,0 +1,25 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.editor;
+
+import com.intellij.lang.javascript.JSAbstractStructureViewTest;
+import com.intellij.lang.javascript.JSTestUtils;
+import org.angularjs.AngularTestUtil;
+
+public class ComponentStructureViewTest extends JSAbstractStructureViewTest {
+  @Override
+  protected String getBasePath() {
+    return AngularTestUtil.getBaseTestDataPath(getClass());
+  }
+
+  @Override
+  protected String getTestDataPath() {
+    return AngularTestUtil.getBaseTestDataPath(getClass());
+  }
+
+  public void testStructure() {
+    JSTestUtils.testES6(getProject(), () -> {
+      myFixture.configureByFiles("package.json");
+      trivialOneFileTest("ts");
+    });
+  }
+}
---
+++ AngularJS/test/org/angular2/editor/data/Structure.ts
@@ -0,0 +1,19 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+@Component({
+    selector: 'my-customer',
+    templateUrl: './custom.html',
+    properties: {
+        'id': 'dependency'
+    }
+})
+class Dependency {
+    id: string;
+}
+
+<structure>
+<node text="Structure.ts">
+  <node text="Dependency">
+    <node text="id: string" icon="FIELD_ICON"/>
+  </node>
+</node>
+</structure>
