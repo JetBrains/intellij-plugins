7c0317e1d76a65d4c213452e5a720c0df8a2b461
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-11-12 19:38:52 +0300

    WEB-35712 Dart Indexer doesn't work with mixins yet
    (IDEA-CR-39797)

--- Dart/src/com/jetbrains/lang/dart/util/DartResolveUtil.java
+++ Dart/src/com/jetbrains/lang/dart/util/DartResolveUtil.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.util;
 
 import com.google.common.collect.Lists;
@@ -615,7 +603,16 @@ public class DartResolveUtil {
 
   @Nullable
   public static DartClassMembers getBody(@Nullable final DartClass dartClass) {
-    final DartClassBody body = dartClass instanceof DartClassDefinition ? ((DartClassDefinition)dartClass).getClassBody() : null;
+    final DartClassBody body;
+    if (dartClass instanceof DartClassDefinition) {
+      body = ((DartClassDefinition)dartClass).getClassBody();
+    }
+    else if (dartClass instanceof DartMixinDeclaration) {
+      body = ((DartMixinDeclaration)dartClass).getClassBody();
+    }
+    else {
+      body = null;
+    }
     return body == null ? null : body.getClassMembers();
   }
 
---
+++ Dart/testSrc/com/jetbrains/lang/dart/navigation/DartGoToSymbolTest.java
@@ -0,0 +1,44 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.lang.dart.navigation;
+
+import com.intellij.navigation.ChooseByNameContributor;
+import com.intellij.navigation.NavigationItem;
+import com.intellij.util.ArrayUtil;
+import com.jetbrains.lang.dart.DartCodeInsightFixtureTestCase;
+import com.jetbrains.lang.dart.ide.DartSymbolContributor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class DartGoToSymbolTest extends DartCodeInsightFixtureTestCase {
+
+  private void assertNamesFound(ChooseByNameContributor contributor, String... names) {
+    Collection<String> actual = new ArrayList<>();
+    for (String name : names) {
+      for (NavigationItem item : contributor.getItemsByName(name, "", getProject(), false)) {
+        actual.add(item.getName());
+      }
+    }
+    assertSameElements(actual, names);
+  }
+
+  public void testGoToSymbol() {
+    myFixture.addFileToProject("foo.dart",
+                               "fooBarBaz1() {}\n" +
+                               "class fooBarBaz2 {\n" +
+                               "  var fooBarBaz3;\n" +
+                               "  fooBarBaz4(){}\n" +
+                               "}\n" +
+                               "mixin fooBarBaz5 {\n" +
+                               "  var fooBarBaz6;\n" +
+                               "  fooBarBaz7(){}\n" +
+                               "}\n");
+
+    DartSymbolContributor contributor = new DartSymbolContributor();
+    String[] allNames = contributor.getNames(getProject(), false);
+    assertFalse(ArrayUtil.contains("fooBarBaz0", allNames));
+    assertFalse(ArrayUtil.contains("FooBarBaz1", allNames));
+    assertFalse(ArrayUtil.contains("fooBarBaz8", allNames));
+    assertNamesFound(contributor, "fooBarBaz1", "fooBarBaz2", "fooBarBaz3", "fooBarBaz4", "fooBarBaz5", "fooBarBaz6", "fooBarBaz7");
+  }
+}
