0b89139883e4300793a6b8631764695de4aea996
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-11-13 11:13:10 +0100

    IDEA-CR-39817: WEB-34753 Lifecycle hooks should not be in the completion list
    (cherry picked from commit 27fd1c67504e28fbc7c7bdb6117da9fa64481409)

--- AngularJS/src/org/angular2/codeInsight/Angular2CompletionContributor.java
+++ AngularJS/src/org/angular2/codeInsight/Angular2CompletionContributor.java
@@ -10,10 +10,10 @@ import com.intellij.lang.javascript.completion.JSLookupUtilImpl;
 import com.intellij.lang.javascript.psi.JSReferenceExpression;
 import com.intellij.lang.javascript.psi.impl.JSReferenceExpressionImpl;
 import com.intellij.openapi.application.ReadAction;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.css.resolve.HtmlCssClassOrIdReference;
 import com.intellij.psi.util.PsiUtilCore;
+import com.intellij.util.containers.ContainerUtil;
 import org.angular2.Angular2DecoratorUtil;
 import org.angular2.entities.Angular2EntitiesProvider;
 import org.angular2.lang.expr.Angular2Language;
@@ -28,6 +28,10 @@ public class Angular2CompletionContributor extends CompletionContributor {
   private static final JSLookupPriority NG_VARIABLE_PRIORITY = JSLookupPriority.LOCAL_SCOPE_MAX_PRIORITY;
   private static final JSLookupPriority NG_PRIVATE_VARIABLE_PRIORITY = JSLookupPriority.LOCAL_SCOPE_MAX_PRIORITY_EXOTIC;
 
+  private static final Set<String> NG_LIFECYCLE_HOOKS = ContainerUtil.newHashSet(
+    "ngOnChanges", "ngOnInit", "ngDoCheck", "ngOnDestroy", "ngAfterContentInit",
+    "ngAfterContentChecked", "ngAfterViewInit", "ngAfterViewChecked");
+
   @Override
   public void fillCompletionVariants(@NotNull final CompletionParameters parameters, @NotNull final CompletionResultSet result) {
     if (!getElementLanguage(parameters).is(Angular2Language.INSTANCE)) return;
@@ -36,15 +40,15 @@ public class Angular2CompletionContributor extends CompletionContributor {
     PsiReference ref = parameters.getPosition().getContainingFile().findReferenceAt(parameters.getOffset());
 
     if (ref instanceof JSReferenceExpressionImpl && ((JSReferenceExpressionImpl)ref).getQualifier() == null) {
-      final PsiElement parent = ((JSReferenceExpressionImpl)ref).getParent();
-      if (addPipeVariants(result, parameters, (JSReferenceExpression)ref, parent)) {
+      if (addPipeVariants(result, parameters, (JSReferenceExpression)ref)) {
         result.stopHere();
         return;
       }
       final Set<String> contributedElements = new HashSet<>();
       Angular2Processor.process(parameters.getPosition(), element -> {
         final String name = element.getName();
-        if (name != null && contributedElements.add(name + "#" + JSLookupUtilImpl.getTypeAndTailTexts(element, null))) {
+        if (name != null && !NG_LIFECYCLE_HOOKS.contains(name)
+            && contributedElements.add(name + "#" + JSLookupUtilImpl.getTypeAndTailTexts(element, null))) {
           result.consume(JSLookupUtilImpl.createPrioritizedLookupItem(
             element, name, Angular2DecoratorUtil.isPrivateMember(element) ? NG_PRIVATE_VARIABLE_PRIORITY : NG_VARIABLE_PRIORITY, false,
             false));
@@ -60,8 +64,7 @@ public class Angular2CompletionContributor extends CompletionContributor {
 
   private static boolean addPipeVariants(final CompletionResultSet result,
                                          CompletionParameters parameters,
-                                         JSReferenceExpression ref,
-                                         PsiElement parent) {
+                                         JSReferenceExpression ref) {
     if (ref instanceof Angular2PipeReferenceExpression) {
       addResults(result, parameters, Angular2EntitiesProvider.getAllPipeNames(ref.getProject()));
       return true;
