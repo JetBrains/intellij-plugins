68dcbd0cb72f5930fa24cd7993c54a9038cc681d
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-11-13 17:04:54 +0100

    WEB-35755 Support canonical form attributes validation and fix issues with case sensitivity

--- AngularJS/src/org/angular2/codeInsight/attributes/Angular2EventHandlerDescriptor.java
+++ AngularJS/src/org/angular2/codeInsight/attributes/Angular2EventHandlerDescriptor.java
@@ -2,6 +2,7 @@
 package org.angular2.codeInsight.attributes;
 
 import com.intellij.psi.PsiElement;
+import org.angular2.lang.html.parser.Angular2AttributeNameParser;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
@@ -9,8 +10,14 @@ import java.util.List;
 public class Angular2EventHandlerDescriptor extends Angular2AttributeDescriptor {
 
   public Angular2EventHandlerDescriptor(@NotNull String attributeName,
-                                        @NotNull String bindingName,
+                                        boolean isInTemplateTag,
                                         @NotNull List<PsiElement> elements) {
-    super(attributeName, bindingName, elements);
+    super(attributeName, isInTemplateTag, elements);
+  }
+
+  public Angular2EventHandlerDescriptor(@NotNull String attributeName,
+                                        @NotNull Angular2AttributeNameParser.AttributeInfo info,
+                                        @NotNull List<PsiElement> elements) {
+    super(attributeName, info, elements);
   }
 }
