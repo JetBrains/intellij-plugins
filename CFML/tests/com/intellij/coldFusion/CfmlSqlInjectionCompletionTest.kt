eff783213b09d71f323842a189cc4f967badd7f4
Gregory.Shrago <gregory.shrago@jetbrains.com>
2018-11-16 23:59:19 +0300

    make cfml not depend on sql.tests and sql.dialects
    (cherry picked from commit cd4556f971efd39b7cccdab70cf3033c7a98280c)

--- CFML/tests/com/intellij/coldFusion/CfmlSqlInjectionCompletionTest.kt
+++ CFML/tests/com/intellij/coldFusion/CfmlSqlInjectionCompletionTest.kt
@@ -1,13 +1,14 @@
 package com.intellij.coldFusion
 
 import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.database.util.SqlDialects
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.sql.completion.SqlCompletionTestCase
 import com.intellij.sql.dialects.SqlDialectMappings
 import com.intellij.sql.dialects.SqlLanguageDialect
-import com.intellij.sql.formatter.settings.SqlCodeStyleConst
-import com.intellij.sql.psi.SqlLanguage
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
 import com.intellij.util.FileContentUtil
+import com.intellij.util.containers.ContainerUtil
 import java.util.*
 
 /**
@@ -15,7 +16,7 @@ import java.util.*
  *
  * @author Sergey Karashevich
  */
-class CfmlSqlInjectionCompletionTest : SqlCompletionTestCase(null) {
+class CfmlSqlInjectionCompletionTest : LightPlatformCodeInsightFixtureTestCase() {
 
   val CFML_QUERY_TEMPLATE = "<cfquery><insert></cfquery>"
 
@@ -24,63 +25,49 @@ class CfmlSqlInjectionCompletionTest : SqlCompletionTestCase(null) {
   override fun getBasePath() = "/injection"
 
   fun testUpdate() =
-    doTestVariants("create table ttt ( ); update <caret>", CompletionType.BASIC, 1, SqlCompletionTestCase.MatchType.CONTAINS, "ttt")
+    doTestVariants("create table ttt ( ); update <caret>", CompletionType.BASIC, 1, true, "ttt")
 
   fun testInsert() =
-    doTestVariants("create table ttt ( ); insert into <caret>", CompletionType.BASIC, 1, SqlCompletionTestCase.MatchType.CONTAINS, "ttt")
+    doTestVariants("create table ttt ( ); insert into <caret>", CompletionType.BASIC, 1, true, "ttt")
 
   fun testSelect() =
-    doTestVariants("create table ttt ( ccc int); select <caret>", CompletionType.BASIC, 1, SqlCompletionTestCase.MatchType.CONTAINS, "ccc")
-
-
-  fun testFrom() {
-    sqlSettings.KEYWORD_CASE = SqlCodeStyleConst.TO_LOWER
-    doTestVariants("select * <caret>", CompletionType.BASIC, 1, SqlCompletionTestCase.MatchType.CONTAINS, "from")
-  }
-
-  fun testSelectFromOrder() =
-    doTestOrder("create table a(a1 int, a2 int);" +
-                "create table b(b1 int, b2 int);" +
-                "create table c(c1 int, c2 int);" +
-                "select <caret> from c, b", CompletionType.BASIC,
-                Arrays.asList("c1", "c2", "b1", "b2", "c", "b"))
-
+    doTestVariants("create table ttt ( ccc int); select <caret>", CompletionType.BASIC, 1, true, "ccc")
 
   // IDEA-90682
   fun testTableColumnListCompletion() =
     doTestVariants(
       "CREATE TABLE new_table (col INT, col2 INT, col3 INT DEFAULT 1, col4 INT DEFAULT 1, col5 INT); INSERT INTO new_table (<caret>)",
-      CompletionType.BASIC, 0, SqlCompletionTestCase.MatchType.CONTAINS, "col, col2, col3, col4, col5", "col, col2, col5")
+      CompletionType.BASIC, 0, true, "col, col2, col3, col4, col5", "col, col2, col5")
 
 
   fun testSmartCompletionInsert2() =
     doTestVariants("CREATE TABLE new_table (col INT, col2 INT, col3 INT, col4 INT, col5 INT); INSERT INTO new_table (id, <caret>)",
-                   CompletionType.BASIC, 0, SqlCompletionTestCase.MatchType.INVERTED, "col, col2, col3, col4, col5")
+                   CompletionType.BASIC, 0, false, "col, col2, col3, col4, col5")
 
 
   fun testRecoveredSelectCompletion() =
     doTestVariants("CREATE TABLE a (aa int);CREATE TABLE b (bb int);SELECT c.<caret> c.aa from a c",
-                   SqlCompletionTestCase.MatchType.INVERTED, "bb")
+                   CompletionType.BASIC, 1, false, "bb")
 
 
-  fun testFastConsumeBreakesCompletion() =
-    doTestContains("SELECT (1 <caret>)", "IN")
+  fun testFastConsumeBreaksCompletion() =
+    doTestVariants("SELECT (1 <caret>)", CompletionType.BASIC, 1, true, "IN")
 
   fun testNoExtraCompletion() =
-    doTestVariants("CREATE TABLE t1(foo int); SELECT tt.f<caret>aq FROM t1 tt;", SqlCompletionTestCase.MatchType.INVERTED, "faq")
+    doTestVariants("CREATE TABLE t1(foo int); SELECT tt.f<caret>aq FROM t1 tt;", CompletionType.BASIC, 1, false, "faq")
 
-  override fun doTestVariants(sql: String,
+  fun doTestVariants(sql: String,
                               type: CompletionType,
                               count: Int,
-                              matchType: SqlCompletionTestCase.MatchType,
+                              contains: Boolean,
                               vararg variants: String) {
-    val dialect = SqlLanguage.INSTANCE.dialects.find { it.id == "SQL92" }
+    val dialect = SqlDialects.findDialectById("SQL92")
     SqlDialectMappings.getInstance(project).setMapping(null, dialect as SqlLanguageDialect)
     val file = myFixture.file
     if (file != null) FileContentUtil.reparseFiles(project, listOf<VirtualFile>(file.virtualFile), false)
     println(javaClass.simpleName + "." + getTestName(true) + ": " + dialect.getID())
     configureByTemplate(CFML_QUERY_TEMPLATE, sql)
-    doTestVariantsInner(type, count, matchType, *variants)
+    myFixture.testVariantsInner(type, count, contains, variants)
   }
 
   private fun configureByTemplate(cfmlTemplate: String, sqlInjection: String) {
@@ -88,4 +75,25 @@ class CfmlSqlInjectionCompletionTest : SqlCompletionTestCase(null) {
     myFixture.configureByText("a.cfml", CFML_FILE_TEXT)
   }
 
+}
+
+fun CodeInsightTestFixture.testVariantsInner(
+  type: CompletionType,
+  count: Int,
+  contains: Boolean,
+  variants: Array<out String>) {
+  complete(type, count)
+  val stringList = lookupElementStrings
+  LightPlatformCodeInsightFixtureTestCase.assertNotNull(stringList)
+  if (contains) {
+    val missing = ArrayList(Arrays.asList(*variants))
+    missing.removeAll(stringList!!)
+    LightPlatformCodeInsightFixtureTestCase.assertTrue(
+      "Missing variants: " + missing + "\nTop 10:\n" + ContainerUtil.getFirstItems(stringList, 10), missing.isEmpty())
+  }
+  else {
+    val missing = ArrayList(Arrays.asList(*variants))
+    missing.retainAll(stringList!!)
+    LightPlatformCodeInsightFixtureTestCase.assertTrue("Present variants: $missing", missing.isEmpty())
+  }
 }
