2b4edc7c45c33ca03ad0051d3e200a7bc8920f00
anstarovoyt <andrey.starovoyt@jetbrains.com>
2018-11-19 18:13:46 +0300

    typescript: fix WEB-35803 add vue files to file includes index

---
+++ vuejs/src/org/jetbrains/vuejs/index/VueES6ModuleFileIncludeProvider.kt
@@ -0,0 +1,53 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.index
+
+import com.intellij.lang.ecmascript6.index.ES6FileIncludeProvider
+import com.intellij.lang.ecmascript6.psi.impl.ES6ImportPsiUtil
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.impl.include.FileIncludeInfo
+import com.intellij.psi.impl.include.FileIncludeProvider
+import com.intellij.util.Consumer
+import com.intellij.util.indexing.FileContent
+import org.jetbrains.vuejs.VueFileType
+import org.jetbrains.vuejs.VueLanguage
+import org.jetbrains.vuejs.codeInsight.findModule
+
+/**
+ * ES6FileIncludeProvider doesn't work for vue files but we need these files in index for building ts imports graph
+ * @see ES6FileIncludeProvider
+ */
+class VueES6ModuleFileIncludeProvider : FileIncludeProvider() {
+
+  override fun getId(): String {
+    return VueLanguage.INSTANCE.id
+  }
+
+  override fun acceptFile(file: VirtualFile): Boolean {
+    return file.fileType == VueFileType.INSTANCE
+  }
+
+  override fun registerFileTypesUsedForIndexing(fileTypeSink: Consumer<FileType>) {
+    fileTypeSink.consume(VueFileType.INSTANCE)
+  }
+
+  override fun getIncludeInfos(content: FileContent): Array<FileIncludeInfo> {
+    if (!ES6FileIncludeProvider.checkTextHasFromKeyword(content)) return emptyArray()
+
+    val psiFile = content.psiFile
+    val module = findModule(psiFile) ?: return emptyArray()
+
+    val importDeclarations = ES6ImportPsiUtil.getImportDeclarations(module)
+
+    val result = importDeclarations.map {
+      val importModuleText = it.importModuleText
+      if (importModuleText != null) return@map importModuleText
+      val fromClause = it.fromClause ?: return@map null
+      val referenceText = fromClause.referenceText ?: return@map null
+      return@map StringUtil.unquoteString(referenceText)
+    }.filterNotNull().toSet()
+
+    return ES6FileIncludeProvider.buildFileIncludeInfo(result)
+  }
+}
---
+++ vuejs/vuejs-tests/testData/ts_imports/AmbientTypingsInVue/AmbientTypingsInVue.vue
@@ -0,0 +1,13 @@
+
+
+<template><div></div></template>
+<script lang="ts">
+import * as Bar2 from 'myModule';
+
+new Bar2.Bar2().do2();
+new Bar2.Bar2().<error>do1</error>();
+
+globalVar;
+<error>globalVar1</error>;
+
+</script>
