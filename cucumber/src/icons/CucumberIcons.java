f382eb773caf47c74f1d955f16447e379b357c28
Andrey Vokin <andrey.vokin@jetbrains.com>
2018-11-20 10:46:57 +0100

    ruby, cucumber: duplicated icon moved to AllIcons.Nodes

--- cucumber/src/icons/CucumberIcons.java
+++ cucumber/src/icons/CucumberIcons.java
@@ -15,6 +15,4 @@ public final class CucumberIcons {
   }
 
   public static final Icon Cucumber = load("/org/jetbrains/plugins/cucumber/icons/cucumber.svg"); // 16x16
-  public static final Icon Steps_group_closed = load("/org/jetbrains/plugins/cucumber/icons/steps_group_closed.png"); // 16x16
-  public static final Icon Steps_group_opened = load("/org/jetbrains/plugins/cucumber/icons/steps_group_opened.png"); // 16x16
 }
--- cucumber/src/org/jetbrains/plugins/cucumber/psi/structure/GherkinStructureViewElement.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/psi/structure/GherkinStructureViewElement.java
@@ -1,9 +1,11 @@
 package org.jetbrains.plugins.cucumber.psi.structure;
 
+import com.intellij.icons.AllIcons;
 import com.intellij.ide.structureView.StructureViewTreeElement;
 import com.intellij.ide.structureView.impl.common.PsiTreeElementBase;
 import com.intellij.navigation.NavigationItem;
 import com.intellij.psi.PsiElement;
+import icons.CucumberIcons;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.*;
 import org.jetbrains.plugins.cucumber.psi.impl.GherkinFeatureHeaderImpl;
@@ -44,10 +46,10 @@ public class GherkinStructureViewElement extends PsiTreeElementBase<PsiElement> 
     final PsiElement element = getElement();
     if (element instanceof GherkinFeature
         || element instanceof GherkinStepsHolder) {
-      return open ? icons.CucumberIcons.Steps_group_opened : icons.CucumberIcons.Steps_group_closed;
+      return AllIcons.Nodes.LogFolder;
     }
     if (element instanceof GherkinStep) {
-      return icons.CucumberIcons.Cucumber;
+      return CucumberIcons.Cucumber;
     }
     return null;
   }
