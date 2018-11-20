a19003c26d366aae028c635e3b7f494a3476615c
Yann Cébron <yann.cebron@jetbrains.com>
2018-11-20 19:35:58 +0100

    RubyMotionLightFixtureTestCase: super.tearDown() in finally

--- ruby-motion/test/org/jetbrains/plugins/ruby/motion/RubyMotionLightFixtureTestCase.java
+++ ruby-motion/test/org/jetbrains/plugins/ruby/motion/RubyMotionLightFixtureTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2017 JetBrains s.r.o.
+ * Copyright 2000-2018 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,8 +43,12 @@ public abstract class RubyMotionLightFixtureTestCase extends RubyLightFixtureTes
 
   @Override
   protected void tearDown() throws Exception {
-    RubyVMOptions.resetForcedLanguageLevel();
-    super.tearDown();
+    try {
+      RubyVMOptions.resetForcedLanguageLevel();
+    }
+    finally {
+      super.tearDown();
+    }
   }
 
   @Override
