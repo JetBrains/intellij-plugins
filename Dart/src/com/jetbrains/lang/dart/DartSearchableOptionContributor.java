bc04f65b76059c3d4d2ea6a534ca88baf0dc9d61
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-11-20 18:15:43 +0300

    Preference dialog filtering doesn't work for the 'Show closing labels in Dart source code' option in Community Edition
    WEB-35257

---
+++ Dart/src/com/jetbrains/lang/dart/DartSearchableOptionContributor.java
@@ -0,0 +1,23 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.lang.dart;
+
+import com.intellij.ide.ui.search.SearchableOptionContributor;
+import com.intellij.ide.ui.search.SearchableOptionProcessor;
+import org.jetbrains.annotations.NotNull;
+
+public class DartSearchableOptionContributor extends SearchableOptionContributor {
+  @Override
+  public void processOptions(@NotNull SearchableOptionProcessor processor) {
+    processor.addOptions(DartBundle.message("dart.editor.showClosingLabels.text"), null,
+                         DartBundle.message("dart.editor.showClosingLabels.text"), "editor.preferences.appearance", null, false);
+    processor.addOptions("dart " + DartBundle.message("dialog.paste.on.import.title"), null,
+                         "Dart: " + DartBundle.message("dialog.paste.on.import.title"), "editor.preferences.import", null, false);
+    processor.addOptions(DartBundle.message("checkbox.collapse.parts"), null,
+                         "Code Folding: collapse Dart parts by default", "editor.preferences.folding", null, false);
+    processor.addOptions(DartBundle.message("checkbox.collapse.generic.parameters"), null,
+                         "Code Folding: collapse Dart generic parameters by default", "editor.preferences.folding", null, false);
+    processor.addOptions("dart " + DartBundle.message("dart.smartKeys.insertDefaultArgValues.text"), null,
+                         "Dart: " + DartBundle.message("dart.smartKeys.insertDefaultArgValues.text"), "editor.preferences.smartKeys", null,
+                         false);
+  }
+}
