4c17731a2108611fcf01dc78d8eee80cfac2c729
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-11-20 18:27:33 +0100

    IDEA-CR-40175: Improve logging to resolve EA-132113
    (cherry picked from commit 537ad4f35a56e1ebd2d0493dfbe095f9aec5e128)

--- AngularJS/src/org/angular2/lang/html/psi/impl/Angular2HtmlBoundAttributeImpl.java
+++ AngularJS/src/org/angular2/lang/html/psi/impl/Angular2HtmlBoundAttributeImpl.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.lang.html.psi.impl;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiErrorElement;
 import com.intellij.psi.impl.source.xml.XmlAttributeImpl;
@@ -14,6 +15,8 @@ import org.jetbrains.annotations.NotNull;
 import static com.intellij.psi.xml.XmlTokenType.XML_NAME;
 
 public class Angular2HtmlBoundAttributeImpl extends XmlAttributeImpl implements Angular2HtmlBoundAttribute {
+
+  private static final Logger LOG = Logger.getInstance(Angular2HtmlBoundAttributeImpl.class);
 
   public Angular2HtmlBoundAttributeImpl(@NotNull Angular2ElementType elementType) {
     super(elementType);
@@ -31,7 +34,10 @@ public class Angular2HtmlBoundAttributeImpl extends XmlAttributeImpl implements 
 
   protected AttributeInfo getAttributeInfo() {
     AttributeInfo info = Angular2AttributeNameParser.parseBound(getName());
-    assert info.elementType == getElementType();
+    if(info.elementType != getElementType()) {
+      LOG.error("Element type mismatch on attribute info. Expected " + getElementType()
+                + ", but got " + info.elementType +". Error for " + toString(), new Throwable());
+    }
     return info;
   }
 
