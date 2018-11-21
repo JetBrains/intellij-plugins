c5e70004a847b97d8b0d1c00a7a00cbfc9693db3
Sergey Malenkov <sergey.malenkov@jetbrains.com>
2018-11-21 17:44:41 +0300

    UX-375: update JsTestDriverIcons

--- JsTestDriver/src/icons/JsTestDriverIcons.java
+++ JsTestDriver/src/icons/JsTestDriverIcons.java
@@ -8,11 +8,11 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public class JsTestDriverIcons {
+public final class JsTestDriverIcons {
   private static Icon load(String path) {
     return IconLoader.getIcon(path, JsTestDriverIcons.class);
   }
 
-  public static final Icon JsTestDriver = load("/com/google/jstestdriver/idea/icons/JsTestDriver.png"); // 16x16
-  public static final Icon ToolWindowTestDriver = load("/com/google/jstestdriver/idea/icons/toolWindowTestDriver.png"); // 13x13
+  public static final Icon JsTestDriver = load("/com/google/jstestdriver/idea/icons/JsTestDriver.svg"); // 16x16
+  public static final Icon ToolWindowTestDriver = load("/com/google/jstestdriver/idea/icons/toolWindowTestDriver.svg"); // 13x13
 }
