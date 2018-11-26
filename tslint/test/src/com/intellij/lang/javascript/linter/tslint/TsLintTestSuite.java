5241cb8d5975e54b95a77a5b29427f582d775ed2
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2018-11-26 17:15:48 +0300

    IDEA-CR-40480 followup: rollback unnecessary changes

--- tslint/test/src/com/intellij/lang/javascript/linter/tslint/TsLintTestSuite.java
+++ tslint/test/src/com/intellij/lang/javascript/linter/tslint/TsLintTestSuite.java
@@ -5,12 +5,12 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-  TsLintCodeStyleImportBasicTest.class,
-  TsLintCodeStyleImportIntegrationTest.class,
   TsLintHighlightingTest.class,
   TsLintConfigCompletionTest.class,
   TsLintConfigHighlightingTest.class,
-  TsLintResolveTest.class
+  TsLintResolveTest.class,
+  TsLintCodeStyleImportBasicTest.class,
+  TsLintCodeStyleImportIntegrationTest.class
 })
 public class TsLintTestSuite {
 }
