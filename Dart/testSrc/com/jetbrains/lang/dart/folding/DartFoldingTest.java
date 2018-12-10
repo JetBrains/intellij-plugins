607f910eb2a0d5902409f0167f481b6c9065f940
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-12-10 13:33:46 +0300

    WEB-14771 Custom code folding regions do not work in Dart code in some cases.

--- Dart/testSrc/com/jetbrains/lang/dart/folding/DartFoldingTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/folding/DartFoldingTest.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.folding;
 
 import com.intellij.codeInsight.folding.CodeFoldingSettings;
@@ -126,6 +127,10 @@ public class DartFoldingTest extends DartCodeInsightFixtureTestCase {
   }
 
   public void testNewExpression() {
+    doTest();
+  }
+
+  public void testCustomRegions() {
     doTest();
   }
 }
