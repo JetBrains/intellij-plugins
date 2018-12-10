b9d29f926c663788261be5fe1188b766c6449252
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-12-10 13:33:46 +0300

    WEB-14771 Custom code folding regions do not work in Dart code in some cases.
    (IDEA-CR-41292)

---
+++ Dart/testData/folding/CustomRegions.dart
@@ -0,0 +1,33 @@
+<fold text='top' expand='true'>//<editor-fold desc="top">
+class Foo <fold text='{...}' expand='true'>{
+  <fold text='1' expand='true'>//<editor-fold desc="1">
+  static List<String> names = [];
+  //</editor-fold></fold>
+  static List<String> appellations = [];
+
+  <fold text='2' expand='true'>//<editor-fold desc="2">
+  Foo() {}
+  Foo.fromJSON(String jsonString) {}
+  //</editor-fold></fold>
+
+  String toString() => "";
+
+  <fold text='3' expand='true'>//<editor-fold desc="3">
+  main() <fold text='{...}' expand='true'>{
+    <fold text='4' expand='true'>//<editor-fold desc="4">
+    var a;
+    //</editor-fold></fold>
+    var b;
+    <fold text='5' expand='true'>//<editor-fold desc="5">
+    var c;
+    //</editor-fold></fold>
+    var d;
+    <fold text='6' expand='true'>//<editor-fold desc="6">
+    var e;
+    //</editor-fold></fold>
+  }</fold>
+
+  static bar(String jsonString) => 1;
+//</editor-fold></fold>
+}</fold>
+//</editor-fold></fold>
