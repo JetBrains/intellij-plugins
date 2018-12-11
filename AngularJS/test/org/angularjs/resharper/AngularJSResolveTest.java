3271d4d1036b9fdb32530e19def335a35ee5cfa7
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2018-12-11 10:34:48 +0100

    WEB-36149 Migrate AngularJS ReSharper symbol resolve tests to WebStorm
    - ignore failing resolve tests.

--- AngularJS/test/org/angularjs/resharper/AngularJSResolveTest.java
+++ AngularJS/test/org/angularjs/resharper/AngularJSResolveTest.java
@@ -4,8 +4,11 @@ package org.angularjs.resharper;
 import com.intellij.lang.javascript.resharper.JSReSharperSymbolResolveTestBase;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.testFramework.TestDataPath;
+import com.intellij.util.containers.ContainerUtil;
 import org.angularjs.AngularTestUtil;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
 
 
 @TestDataPath("$R#_SYMBOL_RESOLVE_TEST_ROOT/DependencyInjection")
@@ -17,5 +20,31 @@ public class AngularJSResolveTest extends JSReSharperSymbolResolveTestBase {
            + "/Resolve/"
            + StringUtil.trimStart(klass.getAnnotation(TestDataPath.class).value(),
                                   "$R#_SYMBOL_RESOLVE_TEST_ROOT");
+  }
+
+  private static final Set<String> IGNORED_TESTS = ContainerUtil.newHashSet(
+    "InjectBuiltinServices.js",
+    "InjectBuiltinServicesByStringLiteral.js",
+    "InjectByStringLiteral.js",
+    "InjectFunctionConstant.js",
+    "InjectFunctionValue.js",
+    "InjectIntoAnimation.js",
+    "InjectIntoConfig.js",
+    "InjectIntoDirective.js",
+    "InjectIntoFactory.js",
+    "InjectIntoFilter.js",
+    "InjectIntoProvider.js",
+    "InjectIntoRun.js",
+    "InjectIntoService.js",
+    "InjectNumberConstant.js",
+    "InjectProvider.js",
+    "InjectProviderValue.js",
+    "InjectStringConstant.js",
+    "InjectStringValue.js"
+  );
+
+  @Override
+  protected boolean isExcluded() {
+    return IGNORED_TESTS.contains(myTestFile);
   }
 }
