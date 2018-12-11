71c9da8acdea95cec5c7d31fac0170a53c74efae
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2018-12-11 12:18:35 +0300

    TRNNG-CR-2 Refactor LessonContext and TaskContext
    Remove redundant task name
    
    GitOrigin-RevId: da32ba974d075bb3b7083e64476453cb62e87af4

--- ide-features-trainer/src/training/commands/kotlin/TaskContext.kt
+++ ide-features-trainer/src/training/commands/kotlin/TaskContext.kt
@@ -1,25 +1,115 @@
 package training.commands.kotlin
 
+import com.intellij.find.FindManager
+import com.intellij.find.FindResult
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.command.undo.BasicUndoableAction
+import com.intellij.openapi.command.undo.DocumentReferenceManager
+import com.intellij.openapi.command.undo.UndoManager
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.editor.LogicalPosition
+import com.intellij.openapi.editor.ex.EditorGutterComponentEx
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.util.DocumentUtil
+import org.jdom.input.SAXBuilder
+import training.learn.ActionsRecorder
 import training.learn.lesson.LessonManager
 import training.learn.lesson.kimpl.KLesson
-import training.learn.lesson.kimpl.LessonContext
+import training.ui.Message
 import java.util.concurrent.CompletableFuture
-import kotlin.test.assertTrue
 
-class TaskContext(val taskName: String?, val lesson: KLesson, val editor: Editor, val project: Project) {
+class TaskContext(val lesson: KLesson, val editor: Editor, val project: Project) {
   val checkFutures: MutableList<CompletableFuture<Boolean>> = mutableListOf()
   val triggerFutures: MutableList<CompletableFuture<Boolean>> = mutableListOf()
-}
 
-/**
- * Start a new task in a lesson context. Task may have an optional name.
- */
-fun LessonContext.task(taskName: String? = null, taskContent: TaskContext.() -> Unit) {
-  val taskContext = TaskContext(taskName, this.first, this.second, this.third)
-  taskContext.apply(taskContent)
-  taskContext.checkFutures.forEach { assertTrue(it.get()) }
-  taskContext.triggerFutures.forEach { assertTrue(it.get()) }
-  LessonManager.getInstance(this.first).passExercise()
+  /**
+   * Write a text to the learn panel (panel with a learning tasks).
+   */
+  fun text(text: String) {
+    val wrappedText = "<root><text>$text</text></root>"
+    val textAsElement = SAXBuilder().build(wrappedText.byteInputStream()).rootElement.getChild("text")
+    LessonManager.getInstance(this.lesson).addMessages(Message.convert(textAsElement)) //support old format
+  }
+
+  fun copyCode(code: String) {
+    ApplicationManager.getApplication().invokeAndWait {
+      val document = editor.document
+      DocumentUtil.writeInRunUndoTransparentAction {
+        val documentReference = DocumentReferenceManager.getInstance().create(document)
+        UndoManager.getInstance(project).nonundoableActionPerformed(documentReference, false)
+        document.insertString(0, code)
+      }
+      PsiDocumentManager.getInstance(project).commitDocument(document)
+      doUndoableAction(project)
+      updateGutter(editor)
+    }
+  }
+
+  fun caret(offset: Int) {
+    runInEdt { editor.caretModel.moveToOffset(offset) }
+  }
+
+  fun caret(line: Int, column: Int) {
+    runInEdt { editor.caretModel.moveToLogicalPosition(LogicalPosition(line - 1, column - 1)) }
+  }
+
+  fun caret(text: String) {
+    runInEdt {
+      val start = getStartOffsetForText(text, editor, project)
+      editor.caretModel.moveToOffset(start.startOffset)
+    }
+  }
+
+  fun trigger(actionId: String) {
+    val recorder = ActionsRecorder(project, editor.document)
+    LessonManager.getInstance(lesson).registerActionsRecorder(recorder)
+    this.triggerFutures.add(recorder.futureAction(actionId))
+  }
+
+  fun triggers(vararg actionIds: String) {
+    val recorder = ActionsRecorder(project, editor.document)
+    LessonManager.getInstance(lesson).registerActionsRecorder(recorder)
+    this.triggerFutures.add(recorder.futureListActions(actionIds.toList()))
+  }
+
+  fun action(id: String): String {
+    return "<action>$id</action>"
+  }
+
+  private fun getStartOffsetForText(text: String, editor: Editor, project: Project): FindResult {
+    val document = editor.document
+
+    val findManager = FindManager.getInstance(project)
+    val model = findManager.findInFileModel.clone()
+    model.isGlobal = false
+    model.isReplaceState = false
+    model.stringToFind = text
+    return FindManager.getInstance(project).findString(document.charsSequence, 0, model)
+  }
+
+  private fun doUndoableAction(project: Project) {
+    CommandProcessor.getInstance().executeCommand(project, {
+      UndoManager.getInstance(project).undoableActionPerformed(object : BasicUndoableAction() {
+        override fun undo() {}
+        override fun redo() {}
+      })
+    }, null, null)
+  }
+
+  private fun updateGutter(editor: Editor) {
+    val editorGutterComponentEx = editor.gutter as EditorGutterComponentEx
+    editorGutterComponentEx.revalidateMarkup()
+  }
+
+  private inline fun runInEdt(crossinline runnable: () -> Unit) {
+    val app = ApplicationManager.getApplication()
+    if (app.isDispatchThread) {
+      runnable()
+    } else {
+      app.invokeLater({ runnable() }, ModalityState.defaultModalityState())
+    }
+  }
 }
