f9a85fb2121c96c4a1a591b614a3bb3873a75971
peter <peter@jetbrains.com>
2018-12-12 14:22:44 +0100

    convert some heavy-initializing word scanners into lighter IdIndexers

---
+++ postcss/src/org/intellij/plugins/postcss/usages/PostCssIdIndexer.java
@@ -0,0 +1,19 @@
+package org.intellij.plugins.postcss.usages;
+
+import com.intellij.lang.cacheBuilder.WordsScanner;
+import com.intellij.psi.impl.cache.impl.id.ScanningIdIndexer;
+
+/**
+ * @author peter
+ */
+public class PostCssIdIndexer extends ScanningIdIndexer {
+  @Override
+  protected WordsScanner createScanner() {
+    return new PostCssFindUsagesProvider().getWordsScanner();
+  }
+
+  @Override
+  public int getVersion() {
+    return 0;
+  }
+}
