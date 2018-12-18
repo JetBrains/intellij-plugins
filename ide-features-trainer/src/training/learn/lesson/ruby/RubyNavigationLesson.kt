fae3b55a40397339ff0378ff180ea2bff9ec6f5c
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2018-12-18 10:50:12 +0300

    Add project-related Ruby lesson
    GitOrigin-RevId: 489223357ad6fff720068414897b45acbdaae667

---
+++ ide-features-trainer/src/training/learn/lesson/ruby/RubyNavigationLesson.kt
@@ -0,0 +1,27 @@
+package training.learn.lesson.ruby
+
+import training.learn.interfaces.Module
+import training.learn.lesson.kimpl.KLesson
+import training.learn.lesson.kimpl.LessonContext
+
+class RubyNavigationLesson(module: Module) : KLesson("Basic Navigation", module, "ruby") {
+  override val lessonContent: LessonContext.() -> Unit
+    get() = {
+
+      triggerTask("GotoDeclaration") {
+        caret(20, 45)
+        text("Use ${action(it)} to jump to the declaration of a class or interface.")
+      }
+
+      triggerTask("GotoClass") {
+        text("Try to find a class with ${action(it)}")
+      }
+      triggerTask("QuickImplementations") {
+        text("Write 'Us' and press ${action(it)} to see the definition of the selected class")
+      }
+      complete()
+    }
+
+  override val existedFile: String?
+    get() = "lib/active_support/core_ext/date/calculations.rb"
+}
