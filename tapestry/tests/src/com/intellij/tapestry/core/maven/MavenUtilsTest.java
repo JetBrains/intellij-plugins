aa0c49d949e3422a541a06e8e63c168a58c13dbf
nik <Nikolay.Chashnikov@jetbrains.com>
2018-12-20 17:21:54 +0300

    tapestry tests: don't create files in working directory

--- tapestry/tests/src/com/intellij/tapestry/core/maven/MavenUtilsTest.java
+++ tapestry/tests/src/com/intellij/tapestry/core/maven/MavenUtilsTest.java
@@ -1,13 +1,14 @@
 package com.intellij.tapestry.core.maven;
 
+import com.intellij.openapi.util.io.FileUtil;
 import org.custommonkey.xmlunit.XMLAssert;
 import org.custommonkey.xmlunit.exceptions.XpathException;
+import org.jetbrains.annotations.NotNull;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,8 +26,7 @@ public class MavenUtilsTest {
     public void createMavenSupport_check_dependencies() throws IOException, XpathException, SAXException {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(true, false, null, null, null, "group", "artifact", "1.1", null);
 
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        String pom = getResultXml();
+        String pom = generatePomXmlText(mavenConfiguration);
 
         XMLAssert.assertXpathExists("/project/dependencies/dependency/groupId[text()='org.apache.tapestry']", pom);
         XMLAssert.assertXpathExists("/project/dependencies/dependency/artifactId[text()='tapestry-core']", pom);
@@ -40,8 +40,7 @@ public class MavenUtilsTest {
 
         MavenConfiguration mavenConfiguration = new MavenConfiguration(true, true, null, null, null, "group", "artifact", "1.1", repositories);
 
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        String pom = getResultXml();
+        String pom = generatePomXmlText(mavenConfiguration);
 
         XMLAssert.assertXpathExists("/project/repositories/repository/id[text()='id1']", pom);
         XMLAssert.assertXpathExists("/project/repositories/repository/name[text()='name1']", pom);
@@ -51,8 +50,7 @@ public class MavenUtilsTest {
 
         repositories.clear();
         repositories.add(new RemoteRepositoryDescription("url2", "id2", "name2", false, false));
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        pom = getResultXml();
+        pom = generatePomXmlText(mavenConfiguration);
 
         XMLAssert.assertXpathExists("/project/repositories/repository/id[text()='id2']", pom);
         XMLAssert.assertXpathExists("/project/repositories/repository/name[text()='name2']", pom);
@@ -63,8 +61,7 @@ public class MavenUtilsTest {
     public void createMavenSupport_with_parent_pom() throws IOException, XpathException, SAXException {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(true, false, "parentGroup", "parentArtifact", "1.0", "group", "artifact", "1.1", null);
 
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        String pom = getResultXml();
+        String pom = generatePomXmlText(mavenConfiguration);
 
         XMLAssert.assertXpathExists("/project/parent/groupId[text()='parentGroup']", pom);
         XMLAssert.assertXpathExists("/project/parent/artifactId[text()='parentArtifact']", pom);
@@ -74,42 +71,28 @@ public class MavenUtilsTest {
     @Test
     public void createMavenSupport_default_version() throws IOException, XpathException, SAXException {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", null, null);
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        String pom = getResultXml();
+        String pom = generatePomXmlText(mavenConfiguration);
         XMLAssert.assertXpathExists("/project/version[text()='1.0-SNAPSHOT']", pom);
 
         mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", "", null);
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        pom = getResultXml();
-        XMLAssert.assertXpathExists("/project/version[text()='1.0-SNAPSHOT']", pom);
+        XMLAssert.assertXpathExists("/project/version[text()='1.0-SNAPSHOT']", generatePomXmlText(mavenConfiguration));
 
         mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", "1.0", null);
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        pom = getResultXml();
-        XMLAssert.assertXpathExists("/project/version[text()='1.0']", pom);
+        XMLAssert.assertXpathExists("/project/version[text()='1.0']", generatePomXmlText(mavenConfiguration));
     }
 
     @Test
     public void createMavenSupport_valid_header() throws IOException, XpathException, SAXException {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", null, null);
 
-        MavenUtils.createMavenSupport("target", mavenConfiguration, "5");
-        String pom = getResultXml();
-
+        String pom = generatePomXmlText(mavenConfiguration);
         XMLAssert.assertXpathExists("/project/modelVersion[text()='4.0.0']", pom);
     }
 
-    private String getResultXml() {
-        try {
-            InputStream stream = new FileInputStream("target/pom.xml");
-            byte[] bytes = new byte[stream.available()];
-            stream.read(bytes);
-
-            return new String(bytes);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return "";
+    @NotNull
+    private static String generatePomXmlText(MavenConfiguration mavenConfiguration) throws IOException {
+        File targetDirectory = FileUtil.createTempDirectory("MavenUtilsTest", null);
+        MavenUtils.createMavenSupport(targetDirectory.getAbsolutePath(), mavenConfiguration, "5");
+        return FileUtil.loadFile(new File(targetDirectory, "pom.xml"));
     }
 }
