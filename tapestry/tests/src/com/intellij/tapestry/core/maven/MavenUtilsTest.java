8226828f64e8649a438dcfb75a7a8401af74595d
nik <Nikolay.Chashnikov@jetbrains.com>
2018-12-21 10:37:32 +0300

    tapestry: use modern version of maven-model library
    This is needed to generate proper pom.xml and avoid version conflict (maven-model comes via dependencies of intellij.java.aetherDependencyResolver anyway).

--- tapestry/tests/src/com/intellij/tapestry/core/maven/MavenUtilsTest.java
+++ tapestry/tests/src/com/intellij/tapestry/core/maven/MavenUtilsTest.java
@@ -1,9 +1,15 @@
 package com.intellij.tapestry.core.maven;
 
 import com.intellij.openapi.util.io.FileUtil;
+import com.intellij.util.containers.ContainerUtil;
+import org.custommonkey.xmlunit.NamespaceContext;
+import org.custommonkey.xmlunit.SimpleNamespaceContext;
 import org.custommonkey.xmlunit.XMLAssert;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.custommonkey.xmlunit.exceptions.XpathException;
 import org.jetbrains.annotations.NotNull;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.xml.sax.SAXException;
 
@@ -16,6 +22,18 @@ import java.util.List;
  * @author <a href="mailto:hugo.palma@logical-software.com">Hugo Palma</a>
  */
 public class MavenUtilsTest {
+    private NamespaceContext myOldContext;
+
+    @BeforeMethod
+    public void setUp() {
+        myOldContext = XMLUnit.getXpathNamespaceContext();
+        XMLUnit.setXpathNamespaceContext(new SimpleNamespaceContext(ContainerUtil.stringMap("ns", "http://maven.apache.org/POM/4.0.0")));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        XMLUnit.setXpathNamespaceContext(myOldContext);
+    }
 
     @Test
     public void constructor() {
@@ -27,10 +45,9 @@ public class MavenUtilsTest {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(true, false, null, null, null, "group", "artifact", "1.1", null);
 
         String pom = generatePomXmlText(mavenConfiguration);
-
-        XMLAssert.assertXpathExists("/project/dependencies/dependency/groupId[text()='org.apache.tapestry']", pom);
-        XMLAssert.assertXpathExists("/project/dependencies/dependency/artifactId[text()='tapestry-core']", pom);
-        XMLAssert.assertXpathExists("/project/dependencies/dependency/version[text()='5']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:dependencies/ns:dependency/ns:groupId[text()='org.apache.tapestry']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:dependencies/ns:dependency/ns:artifactId[text()='tapestry-core']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:dependencies/ns:dependency/ns:version[text()='5']", pom);
     }
 
     @Test
@@ -42,19 +59,19 @@ public class MavenUtilsTest {
 
         String pom = generatePomXmlText(mavenConfiguration);
 
-        XMLAssert.assertXpathExists("/project/repositories/repository/id[text()='id1']", pom);
-        XMLAssert.assertXpathExists("/project/repositories/repository/name[text()='name1']", pom);
-        XMLAssert.assertXpathExists("/project/repositories/repository/url[text()='url1']", pom);
-        XMLAssert.assertXpathExists("/project/repositories/repository/releases", pom);
-        XMLAssert.assertXpathExists("/project/repositories/repository/snapshots", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:id[text()='id1']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:name[text()='name1']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:url[text()='url1']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:releases", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:snapshots", pom);
 
         repositories.clear();
         repositories.add(new RemoteRepositoryDescription("url2", "id2", "name2", false, false));
         pom = generatePomXmlText(mavenConfiguration);
 
-        XMLAssert.assertXpathExists("/project/repositories/repository/id[text()='id2']", pom);
-        XMLAssert.assertXpathExists("/project/repositories/repository/name[text()='name2']", pom);
-        XMLAssert.assertXpathExists("/project/repositories/repository/url[text()='url2']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:id[text()='id2']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:name[text()='name2']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:repositories/ns:repository/ns:url[text()='url2']", pom);
     }
 
     @Test
@@ -63,22 +80,22 @@ public class MavenUtilsTest {
 
         String pom = generatePomXmlText(mavenConfiguration);
 
-        XMLAssert.assertXpathExists("/project/parent/groupId[text()='parentGroup']", pom);
-        XMLAssert.assertXpathExists("/project/parent/artifactId[text()='parentArtifact']", pom);
-        XMLAssert.assertXpathExists("/project/parent/version[text()='1.0']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:parent/ns:groupId[text()='parentGroup']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:parent/ns:artifactId[text()='parentArtifact']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:parent/ns:version[text()='1.0']", pom);
     }
 
     @Test
     public void createMavenSupport_default_version() throws IOException, XpathException, SAXException {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", null, null);
         String pom = generatePomXmlText(mavenConfiguration);
-        XMLAssert.assertXpathExists("/project/version[text()='1.0-SNAPSHOT']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:version[text()='1.0-SNAPSHOT']", pom);
 
         mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", "", null);
-        XMLAssert.assertXpathExists("/project/version[text()='1.0-SNAPSHOT']", generatePomXmlText(mavenConfiguration));
+        XMLAssert.assertXpathExists("/ns:project/ns:version[text()='1.0-SNAPSHOT']", generatePomXmlText(mavenConfiguration));
 
         mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", "1.0", null);
-        XMLAssert.assertXpathExists("/project/version[text()='1.0']", generatePomXmlText(mavenConfiguration));
+        XMLAssert.assertXpathExists("/ns:project/ns:version[text()='1.0']", generatePomXmlText(mavenConfiguration));
     }
 
     @Test
@@ -86,7 +103,7 @@ public class MavenUtilsTest {
         MavenConfiguration mavenConfiguration = new MavenConfiguration(false, false, null, null, null, "group", "artifact", null, null);
 
         String pom = generatePomXmlText(mavenConfiguration);
-        XMLAssert.assertXpathExists("/project/modelVersion[text()='4.0.0']", pom);
+        XMLAssert.assertXpathExists("/ns:project/ns:modelVersion[text()='4.0.0']", pom);
     }
 
     @NotNull
