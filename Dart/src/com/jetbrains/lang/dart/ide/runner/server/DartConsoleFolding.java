9cba6186da1cf6ae3c4f8ab9426b7e6a758fec46
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2018-12-21 18:19:43 +0300

    Fix Dart command line in console folding

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/DartConsoleFolding.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/DartConsoleFolding.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.runner.server;
 
 import com.intellij.execution.ConsoleFolding;
@@ -85,7 +73,7 @@ public class DartConsoleFolding extends ConsoleFolding {
     final String fullText = StringUtil.join(lines, "\n");
     if (lines.size() > 2 ||
         lines.size() == 2 && !lines.get(1).startsWith(DartConsoleFilter.OBSERVATORY_LISTENING_ON) ||
-        lines.get(0).contains(DART_MARKER) && !lines.get(0).contains(TEST_RUNNER_MARKER)) {
+        !lines.get(0).contains(DART_MARKER) && !lines.get(0).contains(TEST_RUNNER_MARKER)) {
       // unexpected text
       return fullText;
     }
@@ -120,7 +108,7 @@ public class DartConsoleFolding extends ConsoleFolding {
       b.append(foldTestRunnerCommand(line));
     }
     else {
-      return fullText; // can' happen
+      return fullText; // can't happen
     }
 
     if (lines.size() == 2 && lines.get(1).startsWith(DartConsoleFilter.OBSERVATORY_LISTENING_ON)) {
