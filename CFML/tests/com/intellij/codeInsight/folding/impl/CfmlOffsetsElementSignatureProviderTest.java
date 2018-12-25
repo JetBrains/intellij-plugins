4523c06fc284344b6eeba06b4f76c394290a73ac
Dmitry Batrak <Dmitry.Batrak@jetbrains.com>
2018-12-25 16:36:40 +0300

    IDEA-203118 Split FoldingPolicyTest to plugin modules where they belong
    also split related tests - PsiNamesElementSignatureProviderTest, OffsetsElementSignatureProviderTest

---
+++ CFML/tests/com/intellij/codeInsight/folding/impl/CfmlOffsetsElementSignatureProviderTest.java
@@ -0,0 +1,43 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.intellij.codeInsight.folding.impl;
+
+import com.intellij.lang.html.HTMLLanguage;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
+
+public class CfmlOffsetsElementSignatureProviderTest extends LightPlatformCodeInsightFixtureTestCase {
+  private final OffsetsElementSignatureProvider myProvider = new OffsetsElementSignatureProvider();
+
+  public void testMultiRootFile() {
+    String text =
+      "<cfcomponent>\n" +
+      "    <cffunction name=\"f\">\n" +
+      "        <table>\n" +
+      "            <tbody>\n" +
+      "                <span id=\"my-id\" <cfif shouldExpand>style=\"display:none;\"</cfif>><a href=\"javascript:expand()\">\n" +
+      "            </tbody>\n" +
+      "        </table>\n" +
+      "    </cffunction>\n" +
+      "</cfcomponent>\n";
+    myFixture.configureByText("test.cfc", text);
+
+    int startOffset = text.indexOf("<cfif");
+    int endOffset = text.indexOf("</cfif>");
+    TextRange targetRange = TextRange.create(startOffset, endOffset);
+
+    PsiElement e = myFixture.getFile().getViewProvider().findElementAt(startOffset, HTMLLanguage.INSTANCE);
+    for (; e != null; e = e.getParent()) {
+      if (targetRange.equals(e.getTextRange())) {
+        break;
+      }
+    }
+    assertNotNull(e);
+    assertEquals(targetRange, e.getTextRange());
+
+    String signature = myProvider.getSignature(e);
+    assertNotNull(signature);
+
+    assertSame(e, myProvider.restoreBySignature(e.getContainingFile(), signature, null));
+  }
+}
