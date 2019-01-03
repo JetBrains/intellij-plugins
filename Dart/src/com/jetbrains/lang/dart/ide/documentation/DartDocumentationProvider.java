60db83c79d01e173b81815d795e53c0e1fc50d0b
Jaime Wren <jwren@google.com>
2019-01-03 11:27:50 -0800

    DartDocumentationProvider fix: NPE prevention from a PsiElement.getContainingFile() call.

--- Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProvider.java
+++ Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocumentationProvider.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.documentation;
 
 import com.intellij.lang.documentation.DocumentationProvider;
@@ -28,6 +16,7 @@ import com.jetbrains.lang.dart.psi.*;
 import com.jetbrains.lang.dart.util.DartResolveUtil;
 import com.jetbrains.lang.dart.util.DartUrlResolver;
 import org.dartlang.analysis.server.protocol.HoverInformation;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -192,13 +181,17 @@ public class DartDocumentationProvider implements DocumentationProvider {
     return null;
   }
 
+  @Contract("null -> null")
   @Nullable
-  private static HoverInformation getSingleHover(final PsiElement element) {
-    if (element != null) {
-      final PsiFile psiFile = element.getContainingFile();
-      final int offset = element.getTextOffset();
-      return getSingleHover(psiFile, offset);
+  private static HoverInformation getSingleHover(@Nullable final PsiElement element) {
+    if (element == null) {
+      return null;
     }
-    return null;
+    final PsiFile psiFile = element.getContainingFile();
+    if (psiFile == null) {
+      return null;
+    }
+    final int offset = element.getTextOffset();
+    return getSingleHover(psiFile, offset);
   }
 }
