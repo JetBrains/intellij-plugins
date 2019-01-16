1de3be005d22c9d6d8d0ff257b699ce195852af9
Dmitry Jemerov <yole@jetbrains.com>
2019-01-16 19:04:38 +0100

    Encapsulate and add property change listener for CodeInsightSettings.SELECT_AUTOPOPUP_SUGGESTIONS_BY_CHARS

--- flex/flex-tests/testSrc/com/intellij/flex/completion/FlexAutoPopupTest.groovy
+++ flex/flex-tests/testSrc/com/intellij/flex/completion/FlexAutoPopupTest.groovy
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.flex.completion
 
 import com.intellij.codeInsight.CodeInsightSettings
@@ -46,12 +34,12 @@ class FlexAutoPopupTest extends CompletionAutoPopupTestCase {
 
     super.setUp()
     runInEdtAndWait { FlexTestUtils.setupFlexSdk(myModule, getTestName(false), FlexAutoPopupTest.class, myFixture.getProjectDisposable()) }
-    CodeInsightSettings.instance.SELECT_AUTOPOPUP_SUGGESTIONS_BY_CHARS = true
+    CodeInsightSettings.instance.selectAutopopupSuggestionsByChars = true
   }
 
   @Override
   protected void tearDown() {
-    CodeInsightSettings.instance.SELECT_AUTOPOPUP_SUGGESTIONS_BY_CHARS = false
+    CodeInsightSettings.instance.selectAutopopupSuggestionsByChars = false
     CodeInsightSettings.instance.COMPLETION_CASE_SENSITIVE = CodeInsightSettings.FIRST_LETTER
     super.tearDown()
   }
