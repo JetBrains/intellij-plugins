5a1f0839227057a366a99fa73859e0866b3bc6ca
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-01-17 19:15:45 +0300

    Translate duplicate lesson to Kotlin DSL
    GitOrigin-RevId: 7f21d5d2a44c66e8fb5e8a919b461940d8d0b90b

---
+++ ide-features-trainer/src/training/learn/lesson/general/DuplicateLesson.kt
@@ -0,0 +1,28 @@
+package training.learn.lesson.general
+
+import training.learn.interfaces.Module
+import training.learn.lesson.kimpl.KLesson
+import training.learn.lesson.kimpl.LessonContext
+import training.learn.lesson.kimpl.LessonSample
+
+class DuplicateLesson(module: Module, lang: String, private val sample: LessonSample) :
+    KLesson("Duplicate", module, lang) {
+  override val lessonContent: LessonContext.() -> Unit
+    get() = {
+      prepareSample(sample)
+
+      triggerTask("EditorDuplicate") {
+        text("Duplicate any line with ${action(it)}")
+      }
+      triggerTask("EditorDuplicate") {
+        text("You can do the same thing with multiple lines, too. Simply select two or more lines and duplicate them with ${action(it)}")
+        testActions("EditorUp", "EditorLineStart", "EditorDownWithSelection", "EditorDownWithSelection")
+        check({
+          val selection = editor.selectionModel
+          val start = selection.selectionStartPosition?.line ?: 0
+          val end = selection.selectionEndPosition?.line ?: 0
+          end - start
+        }, { _, new -> new >= 2 })
+      }
+    }
+}
