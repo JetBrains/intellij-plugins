fc2e5a45d35987fb07ecd67346c81ee0d2b229b5
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-01-22 14:32:33 +0300

    Revert "TRNNG-CR-2 Fix concurrent modification exception"
    This reverts commit 5f855e90
    
    GitOrigin-RevId: 17f886d08e4136bbe45b8864275a558d716024f9

--- ide-features-trainer/src/training/commands/kotlin/TaskContext.kt
+++ ide-features-trainer/src/training/commands/kotlin/TaskContext.kt
@@ -41,9 +41,7 @@ class TaskContext(val lesson: KLesson, val editor: Editor, val project: Project,
   fun text(text: String) {
     val wrappedText = "<root><text>$text</text></root>"
     val textAsElement = SAXBuilder().build(wrappedText.byteInputStream()).rootElement.getChild("text")
-    ApplicationManager.getApplication().invokeLater {
-      LessonManager.getInstance(this.lesson).addMessages(Message.convert(textAsElement)) //support old format
-    }
+    LessonManager.getInstance(this.lesson).addMessages(Message.convert(textAsElement)) //support old format
   }
 
   fun setDocumentCode(code: String) {
