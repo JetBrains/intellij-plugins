41231d9ab720079d01f052a74dbaaa4c352a66d8
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-01-23 17:56:16 +0300

    Translate move lesson to Kotlin DSL
    GitOrigin-RevId: 59f4b4e15e0b57860378669bca366e1ddf238243

---
+++ ide-features-trainer/src/training/learn/lesson/general/MoveLesson.kt
@@ -0,0 +1,40 @@
+package training.learn.lesson.general
+
+import com.intellij.diff.tools.util.text.LineOffsetsUtil
+import com.intellij.openapi.vcs.ex.compareLines
+import com.intellij.util.containers.ContainerUtil
+import training.commands.ShowLineNumberCommand
+import training.learn.interfaces.Module
+import training.learn.lesson.kimpl.KLesson
+import training.learn.lesson.kimpl.LessonContext
+import training.learn.lesson.kimpl.LessonSample
+
+class MoveLesson(module: Module, lang: String, private val sample: LessonSample) :
+    KLesson("Move", module, lang) {
+  override val lessonContent: LessonContext.() -> Unit
+    get() = {
+      prepareSample(sample)
+      ShowLineNumberCommand.showLineNumber(editor)
+
+      triggerTask("MoveLineDown") {
+        text("Rearranging lines usually takes two actions: cut and paste. In this IDE, you can do it with just one: " +
+            "pressing ${action(it)} will pull down the current line.")
+      }
+      triggerTask("MoveLineUp") {
+        text("Similarly, to pull a line up, use ${action(it)}")
+      }
+      triggerTask("MoveStatementUp") {
+        text("Now try moving the whole method up with ${action(it)}")
+        testActions("EditorUp")
+        check({ editor.document.text }, { before, now ->
+          val changes = ContainerUtil.newArrayList(
+              compareLines(before, now, LineOffsetsUtil.create(before), LineOffsetsUtil.create(now)
+              ).iterateChanges())
+
+          // Check, that more than 2 lines where changed
+          !changes.isEmpty() &&
+              (changes[0].end1 - changes[0].start1 > 1 || changes[0].end2 - changes[0].start2 > 1)
+        })
+      }
+    }
+}
