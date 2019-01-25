e1965cf46bdc5071fdcf50d1873d060328eac178
Daniil Ovchinnikov <daniil.ovchinnikov@jetbrains.com>
2019-01-25 15:50:07 +0300

    [groovy] cucumber: use repository library for tests

--- cucumber-groovy/test/org/jetbrains/plugins/cucumber/groovy/GrCucumberLightTestCase.groovy
+++ cucumber-groovy/test/org/jetbrains/plugins/cucumber/groovy/GrCucumberLightTestCase.groovy
@@ -1,43 +1,18 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.groovy
-import com.intellij.openapi.module.Module
-import com.intellij.openapi.roots.ContentEntry
-import com.intellij.openapi.roots.ModifiableRootModel
-import com.intellij.openapi.roots.OrderRootType
-import com.intellij.openapi.roots.libraries.Library
-import com.intellij.openapi.vfs.JarFileSystem
-import com.intellij.openapi.vfs.VirtualFile
+
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.annotations.NotNull
-import org.jetbrains.plugins.groovy.GroovyLightProjectDescriptor
 import org.jetbrains.plugins.groovy.LightGroovyTestCase
+
 /**
  * @author Max Medvedev
  */
 abstract class GrCucumberLightTestCase extends LightGroovyTestCase {
-  static class GrCucumberLightProjectDescriptor extends GroovyLightProjectDescriptor {
-    @Override
-    void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
-      super.configureModule(module, model, contentEntry)
-
-      final Library.ModifiableModel modifiableModel = model.moduleLibraryTable.createLibrary("GROOVY_CUCUMBER").modifiableModel
-      TestUtils.mockGroovyCucumberLibraryNames.each { jar ->
-        final VirtualFile libJar = JarFileSystem.instance.refreshAndFindFileByPath("$jar!/")
-        assert libJar != null
-        modifiableModel.addRoot(libJar, OrderRootType.CLASSES)
-      }
-      modifiableModel.commit()
-    }
-
-    protected GrCucumberLightProjectDescriptor() {
-      super(org.jetbrains.plugins.groovy.util.TestUtils.mockGroovy2_1LibraryName)
-    }
-
-    public static final INSTANCE = new GrCucumberLightProjectDescriptor()
-  }
 
   @NotNull
   @Override
   protected LightProjectDescriptor getProjectDescriptor() {
-    return GrCucumberLightProjectDescriptor.INSTANCE
+    return TestUtils.DESCRIPTOR
   }
 }
--- cucumber-groovy/test/org/jetbrains/plugins/cucumber/groovy/TestUtils.groovy
+++ cucumber-groovy/test/org/jetbrains/plugins/cucumber/groovy/TestUtils.groovy
@@ -1,12 +1,21 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.groovy
+
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.testFramework.LightProjectDescriptor
+import org.jetbrains.plugins.groovy.LibraryLightProjectDescriptor
+import org.jetbrains.plugins.groovy.RepositoryTestLibrary
+
+import static org.jetbrains.plugins.groovy.GroovyProjectDescriptors.LIB_GROOVY_2_1
+
 /**
  * @author Max Medvedev
  */
 class TestUtils {
-  public static final String CUCUMBER_GROOVY_1_0_14 = 'cucumber-groovy-1.0.14.jar'
-  public static final String CUCUMBER_CORE_1_0_14 = 'cucumber-core-1.0.14.jar'
+  public static final LightProjectDescriptor DESCRIPTOR = new LibraryLightProjectDescriptor(
+    LIB_GROOVY_2_1 + new RepositoryTestLibrary('info.cukes:cucumber-core:1.0.14', 'info.cukes:cucumber-groovy:1.0.14')
+  )
 
   private TestUtils() {}
 
@@ -16,14 +25,5 @@ class TestUtils {
 
   private static String getAbsolutePluginPath() {
     return FileUtil.toSystemIndependentName(new File(PathManager.homePath, "contrib/cucumber-groovy/").path)
-  }
-
-  static String getMockGroovyCucumberLibraryHome() {
-    return "$absoluteTestDataPath/mockGroovyCucumberLib-1.0.14"
-  }
-
-  static List<String> getMockGroovyCucumberLibraryNames() {
-    String home = mockGroovyCucumberLibraryHome
-    return ["$home/$CUCUMBER_GROOVY_1_0_14", "$home/$CUCUMBER_CORE_1_0_14"]
   }
 }
