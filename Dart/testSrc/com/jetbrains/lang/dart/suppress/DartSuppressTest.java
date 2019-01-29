de0fc51d03510187afc9fec3221e6e376dde9fa3
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-01-29 17:31:54 +0300

    Dart suppression actions: do not suggest to suppress compiler errors; show at top level if no quick fixes available.

--- Dart/testSrc/com/jetbrains/lang/dart/suppress/DartSuppressTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/suppress/DartSuppressTest.java
@@ -1,14 +1,16 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.suppress;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.CommandProcessor;
 import com.jetbrains.lang.dart.DartCodeInsightFixtureTestCase;
 import com.jetbrains.lang.dart.ide.annotator.DartProblemGroup;
+import org.dartlang.analysis.server.protocol.AnalysisErrorSeverity;
 
 public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
 
   private boolean isSuppressActionAvailable(final boolean eolComment) {
-    return new DartProblemGroup.DartSuppressAction("", "", eolComment)
+    return new DartProblemGroup.DartSuppressAction("", AnalysisErrorSeverity.WARNING, false, eolComment)
       .isAvailable(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret());
   }
 
@@ -16,6 +18,13 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
     myFixture.configureByText("foo.dart", "var <caret>a;");
     assertTrue(isSuppressActionAvailable(false));
     assertTrue(isSuppressActionAvailable(true));
+
+    assertFalse("should not be available for errors",
+                new DartProblemGroup.DartSuppressAction("", AnalysisErrorSeverity.ERROR, false, false)
+                  .isAvailable(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret()));
+    assertFalse("should not be available for errors",
+                new DartProblemGroup.DartSuppressAction("", AnalysisErrorSeverity.ERROR, false, true)
+                  .isAvailable(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret()));
 
     myFixture.configureByText("foo.dart", "//ignore something\n var <caret>a; // ignore :something");
     assertTrue(isSuppressActionAvailable(false));
@@ -45,7 +54,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testAddPrevLineComment() {
     myFixture.configureByText("foo.dart", "var <caret>a;");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", false)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, false)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("// ignore: x\nvar <caret>a;");
@@ -54,7 +63,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testAddPrevLineCommentWithOffset() {
     myFixture.configureByText("foo.dart", "//a\n   var <caret>a;");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", false)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, false)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("//a\n   // ignore: x\n   var <caret>a;");
@@ -63,7 +72,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testUpdatePrevLineComment() {
     myFixture.configureByText("foo.dart", "     //     ignore:     y     \n   var <caret>a;");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", false)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, false)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("     //     ignore:     y, x\n   var <caret>a;");
@@ -72,7 +81,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testAddEolComment() {
     myFixture.configureByText("foo.dart", "var <caret>a;");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", true)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, true)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("var <caret>a; // ignore: x");
@@ -81,7 +90,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testAddEolCommentTrimSpaces() {
     myFixture.configureByText("foo.dart", "var <caret>a;     ");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", true)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, true)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("var <caret>a; // ignore: x");
@@ -90,7 +99,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testAddEolCommentBeforeOtherComment() {
     myFixture.configureByText("foo.dart", "var <caret>a; //comment");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", true)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, true)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("var <caret>a; // ignore: x, //comment");
@@ -99,7 +108,7 @@ public class DartSuppressTest extends DartCodeInsightFixtureTestCase {
   public void testUpdateEolComment() {
     myFixture.configureByText("foo.dart", "var <caret>a;     //     ignore:     y     \n");
     CommandProcessor.getInstance().executeCommand(getProject(), () -> ApplicationManager.getApplication().runWriteAction(
-      () -> new DartProblemGroup.DartSuppressAction("x", "", true)
+      () -> new DartProblemGroup.DartSuppressAction("x", "", false, true)
         .invoke(getProject(), myFixture.getEditor(), myFixture.getElementAtCaret())), null, null);
 
     myFixture.checkResult("var <caret>a;     //     ignore:     y, x\n");
