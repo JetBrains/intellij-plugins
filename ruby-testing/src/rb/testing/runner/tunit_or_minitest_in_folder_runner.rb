730b7cb47e63171f7d07b5de6f20c3feb39e73a6
Dmitry.Krasilschikov <kradima@jetbrains.com>
2019-01-31 18:07:18 +0200

    IDEA-CR-41945 RUBY-23292 better rails versions comparing
    (cherry picked from commit 1db2d4a440b1f90c8d5f26166a3d302e682cbf68)

--- ruby-testing/src/rb/testing/runner/tunit_or_minitest_in_folder_runner.rb
+++ ruby-testing/src/rb/testing/runner/tunit_or_minitest_in_folder_runner.rb
@@ -83,8 +83,10 @@ def drb_launch_tests(drb_runner, test_scripts, test_scripts_names, test_names)
     test_name_pattern = get_test_name_pattern(test_names)
 
     rails = Gem.loaded_specs['rails']
-    version = rails ? rails.version.to_s : IntelliJ::RAILS_VERSION
-    if version && version >= '4'
+
+    version = rails ? rails.version : Gem::Version.new(IntelliJ::RAILS_VERSION || 0)
+
+    if version && version >= Gem::Version.new('4')
       cmdline << 'rake'
       cmdline << 'test'
 
