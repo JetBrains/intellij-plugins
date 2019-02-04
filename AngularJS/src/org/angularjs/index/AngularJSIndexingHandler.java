7a48f9241209d8c283c1ff4c83cedecbfb8594b5
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-02-04 10:05:41 +0100

    EA-134481 Fix NPE in AngularJS indexer

--- AngularJS/src/org/angularjs/index/AngularJSIndexingHandler.java
+++ AngularJS/src/org/angularjs/index/AngularJSIndexingHandler.java
@@ -445,7 +445,7 @@ public class AngularJSIndexingHandler extends FrameworkIndexingHandler {
       final int start = line.indexOf(PARAM);
       if (start >= 0) {
         final JSDocumentationUtils.DocTag docTag = JSDocumentationUtils.getDocTag(line.substring(start));
-        if (docTag != null) {
+        if (docTag != null && docTag.matchName != null) {
           for (String paramName : StringUtil.split(docTag.matchName, "|")) {
             if (restrict.equals(DEFAULT_RESTRICTIONS) || restrict.contains("A")) {
               result.add(Pair.pair(paramName, "A;" + tag + (directiveName.equals(paramName) ? "" : "=" + directiveName) + ";"
