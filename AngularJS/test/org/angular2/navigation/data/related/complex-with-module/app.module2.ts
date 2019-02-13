4c240829929eea97fdf99d1ac704a39683dce663
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-02-13 18:09:39 +0100

    IDEA-CR-43326: WEB-37284 Angular - list module as component's related symbol
    (cherry picked from commit d8213d93eca696fae76c5a6815b0b94c245e3e10)

---
+++ AngularJS/test/org/angular2/navigation/data/related/complex-with-module/app.module2.ts
@@ -0,0 +1,16 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import {NgModule} from '@angular/core';
+import {AppModule} from "./app.module";
+import {AppComponent} from "./app.component.2";
+
+@NgModule({
+    imports: [
+        AppModule
+    ],
+    declarations: [
+        AppComponent
+    ]
+})
+export class AppModule2 {
+
+}
