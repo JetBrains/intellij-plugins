71603ae6ae08fca16463a988883e8488d8b5e699
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-02-13 12:15:10 +0300

    Dart Intention Actions are not applicable for network files

--- Dart/src/com/jetbrains/lang/dart/assists/DartQuickAssistIntention.java
+++ Dart/src/com/jetbrains/lang/dart/assists/DartQuickAssistIntention.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.assists;
 
 import com.intellij.CommonBundle;
@@ -93,7 +81,9 @@ public class DartQuickAssistIntention implements IntentionAction, Comparable<Int
 
   @Override
   public boolean isAvailable(@NotNull Project project, Editor editor, PsiFile file) {
-    if (quickAssistSet == null || !(file instanceof DartFile)) return false;
+    if (quickAssistSet == null || !(file instanceof DartFile) || !DartAnalysisServerService.isLocalAnalyzableFile(file.getVirtualFile())) {
+      return false;
+    }
 
     final List<SourceChange> sourceChanges = quickAssistSet.getQuickAssists(editor, file);
     if (sourceChanges.size() <= index) {
