30fdeb274c2dff6d4c149b66aedd9b70aaa3e9b0
Leonid Shalupov <leonid@shalupov.com>
2019-02-26 03:10:27 +0300

    parallel: correctly handle exceptions

--- CloudFormation/metadata-crawler/src/main/kotlin/parallel.kt
+++ CloudFormation/metadata-crawler/src/main/kotlin/parallel.kt
@@ -1,21 +1,15 @@
-import java.util.Collections
+import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import java.util.concurrent.TimeUnit
 
 // Thanks https://github.com/holgerbrandl/kutils/blob/master/src/main/kotlin/de/mpicbg/scicomp/kutils/ParCollections.kt
 // See also https://stackoverflow.com/a/35638609 / https://stackoverflow.com/questions/34697828/parallel-operations-on-kotlin-collections
 fun <T, R> Iterable<T>.pmap(numThreads: Int = maxOf(Runtime.getRuntime().availableProcessors() - 2, 1), exec: ExecutorService = Executors.newFixedThreadPool(numThreads), transform: (T) -> R): List<R> {
-  // note default size is just an inlined version of kotlin.collections.collectionSizeOrDefault
-  val defaultSize = if (this is Collection<*>) this.size else 10
-  val destination = Collections.synchronizedList(ArrayList<R>(defaultSize))
 
-  for (item in this) {
-    exec.submit { destination.add(transform(item)) }
+  try {
+    val result = exec.invokeAll(this.map { item -> Callable<R> { transform(item) } })
+    return result.map { it.get() }
+  } finally {
+    exec.shutdownNow()
   }
-
-  exec.shutdown()
-  exec.awaitTermination(1, TimeUnit.DAYS)
-
-  return ArrayList<R>(destination)
 }
