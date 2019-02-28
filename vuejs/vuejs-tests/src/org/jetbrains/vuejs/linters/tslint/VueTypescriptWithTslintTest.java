243b069e70a5ef336a9cbcf4157f653c06f1b90a
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-02-28 19:47:54 +0300

    test for WEB-37383
    (cherry picked from commit 462ebf154004b3272770386003ea2132dcfdd888)
    r=Starovoyt

---
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/linters/tslint/VueTypescriptWithTslintTest.java
@@ -0,0 +1,17 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.linters.tslint;
+
+import com.intellij.lang.javascript.linter.tslint.TypescriptServiceWithTslintTestBase;
+
+import static org.jetbrains.vuejs.language.VueTestUtilKt.vueRelativeTestDataPath;
+
+public class VueTypescriptWithTslintTest extends TypescriptServiceWithTslintTestBase {
+  @Override
+  protected String getBasePath() {
+    return vueRelativeTestDataPath() + "/linters/tslint/";
+  }
+
+  public void testFilterWhitespaceErrorsByScriptTag() {
+    doHighlightingTest("main", "vue");
+  }
+}
