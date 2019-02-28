f138c1d38ab89c6e70e09aba1f3e5b4e9734531c
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-02-28 20:16:49 +0300

    Fix pass status
    GitOrigin-RevId: 0672f5de91afa157b252b2c98acd567c52cf221f

--- ide-features-trainer/src/training/learn/lesson/kimpl/KLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/kimpl/KLesson.kt
@@ -3,14 +3,16 @@ package training.learn.lesson.kimpl
 import training.learn.interfaces.Lesson
 import training.learn.interfaces.Module
 import training.learn.lesson.LessonListener
+import training.learn.lesson.LessonState
+import training.learn.lesson.LessonStateManager
 
 abstract class KLesson(final override val name  : String,
                              override var module: Module,
                              override val lang  : String) : Lesson {
 
   abstract val lessonContent: LessonContext.() -> Unit
-  override val id: String = name
-  override var passed = false
+  final override val id: String = name
+  override var passed = LessonStateManager.getStateFromBase(id) == LessonState.PASSED
   override var isOpen = false
   override val lessonListeners: MutableList<LessonListener> = mutableListOf()
 }
