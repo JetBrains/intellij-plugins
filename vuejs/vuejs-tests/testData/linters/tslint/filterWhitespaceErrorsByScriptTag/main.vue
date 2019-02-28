462ebf154004b3272770386003ea2132dcfdd888
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-02-28 19:47:54 +0300

    test for WEB-37383

---
+++ vuejs/vuejs-tests/testData/linters/tslint/filterWhitespaceErrorsByScriptTag/main.vue
@@ -0,0 +1,16 @@
+<template>
+ <div>
+   <div>
+     <!--  after replacement with whitespace in service, this should violate some rules   -->
+     <div>
+       Hello
+     </div>
+   </div>
+ </div> 
+</template>
+
+<script lang="ts">
+  export default {
+    name: <warning><caret>"main"</warning>
+  }
+</script>
