7564756342a3d1a9db647782bb7d33413af1fe60
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-03-07 12:14:17 +0100

    IDEA-CR-44653: WEB-26592 Provide AngularJS specific inspection suppression.
    (cherry picked from commit 064fd2003d598b8357213f830817bedc447a1ca1)

---
+++ AngularJS/src/org/angularjs/inspections/AngularJSInspectionSuppressor.java
@@ -0,0 +1,25 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angularjs.inspections;
+
+import com.intellij.codeInspection.InspectionSuppressor;
+import com.intellij.codeInspection.SuppressQuickFix;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class AngularJSInspectionSuppressor implements InspectionSuppressor {
+
+  static final InspectionSuppressor INSTANCE = new AngularJSInspectionSuppressor();
+
+  @Override
+  public boolean isSuppressedFor(@NotNull PsiElement element, @NotNull String toolId) {
+    return false;
+  }
+
+  @NotNull
+  @Override
+  public SuppressQuickFix[] getSuppressActions(@Nullable PsiElement element, @NotNull String toolId) {
+    // AngularJS expressions do not allow for comments, so no per-expression suppression is possible
+    return SuppressQuickFix.EMPTY_ARRAY;
+  }
+}
