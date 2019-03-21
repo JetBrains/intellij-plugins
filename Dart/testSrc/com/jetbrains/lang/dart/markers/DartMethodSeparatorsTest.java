17d88466bafc23d53af494a673b2f000da94a155
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-03-21 17:51:40 +0300

    Lighten DartMethodSeparatorsTest

--- Dart/testSrc/com/jetbrains/lang/dart/markers/DartMethodSeparatorsTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/markers/DartMethodSeparatorsTest.java
@@ -1,91 +1,56 @@
-/*
- * Copyright 2000-2016 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.markers;
 
-import com.intellij.codeInsight.daemon.DaemonAnalyzerTestCase;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
-import com.jetbrains.lang.dart.DartFileType;
+import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase;
 
-public class DartMethodSeparatorsTest extends DaemonAnalyzerTestCase {
-  @Override
-  protected boolean doTestLineMarkers() {
-    return true;
-  }
+public class DartMethodSeparatorsTest extends LightPlatformCodeInsightFixtureTestCase {
 
-  public void testMethodSeparators() {
+  protected void doTest(String fileText) {
     DaemonCodeAnalyzerSettings.getInstance().SHOW_METHOD_SEPARATORS = true;
     try {
-      // methods
-      configureByText(DartFileType.INSTANCE, "abstract class A {\n" +
-                                             "  firstMethod(){}\n" + // no line marker
-                                             "  <lineMarker>bar</lineMarker>(){}\n" +
-                                             "   \n" +
-                                             "  <lineMarker>//comment</lineMarker>\n" +
-                                             "   \n" +
-                                             "  /// doc comment\n" +
-                                             "   \n" +
-                                             "  baz(){}\n" +
-                                             "\n" +
-                                             "  <lineMarker>abstract</lineMarker> abs();\n" +
-                                             "}");
-      doDoTest(false, false);
-
-      // getters and setters
-      configureByText(DartFileType.INSTANCE, "class A {\n" +
-                                             "  firstMethod(){}\n" + // no line marker
-                                             "  <lineMarker>@</lineMarker>foo\n" +
-                                             "  get v => 1;\n" +
-                                             "  <lineMarker>set</lineMarker> v(i) {}\n" +
-                                             "  <lineMarker>int</lineMarker> get i => 1;\n" +
-                                             "}");
-      doDoTest(false, false);
-
-      // constructors
-      configureByText(DartFileType.INSTANCE, "class A {\n" +
-                                             "  firstMethod(){}\n" + // no line marker
-                                             "  <lineMarker>A</lineMarker>() {}\n" +
-                                             "  <lineMarker>A</lineMarker>.b() {}\n" +
-                                             "}");
-      doDoTest(false, false);
-
-      // globals
-      configureByText(DartFileType.INSTANCE, "  firstFunction(){}\n" + // no line marker
-                                             "  <lineMarker>func</lineMarker>(){}\n" +
-                                             "  <lineMarker>/// doc</lineMarker>\n" +
-                                             "  get v => 1;\n" +
-                                             "  <lineMarker>set</lineMarker> v(i) {}\n" +
-                                             "  <lineMarker>int</lineMarker> get i => 1;");
-      doDoTest(false, false);
-
-      // any preceding sibling implies that the method will be marked
-      configureByText(DartFileType.INSTANCE, "class A {\n" +
-                                             "  var b;\n" + // no line marker
-                                             "  <lineMarker>bar</lineMarker>(){}\n" +
-                                             "}");
-      doDoTest(false, false);
-
-      // ignore when enclosed in something that is not a class: method
-      configureByText(DartFileType.INSTANCE, "bar() {\n" +
-                                             "  foo() {}\n"+
-                                             "  foo1() {}\n"+
-                                             "}");
-      doDoTest(false, false);
+      myFixture.configureByText("foo.dart", fileText);
+      myFixture.checkHighlighting();
     }
     finally {
       DaemonCodeAnalyzerSettings.getInstance().SHOW_METHOD_SEPARATORS = false;
     }
+  }
+
+  public void testMethodSeparators() {
+    doTest("firstFunction(){}\n" + // no line marker
+           "<lineMarker>func</lineMarker>(){}\n" +
+           "<lineMarker>/// doc</lineMarker>\n" +
+           "get v => 1;\n" +
+           "<lineMarker>set</lineMarker> v(i) {}\n" +
+           "<lineMarker>int</lineMarker> get i => 1;\n" +
+           "abstract class A {\n" +
+           "  firstMethod(){}\n" + // no line marker
+           "  <lineMarker>bar</lineMarker>(){}\n" +
+           "   \n" +
+           "  <lineMarker>//comment</lineMarker>\n" +
+           "   \n" +
+           "  /// doc comment\n" +
+           "   \n" +
+           "  baz(){}\n" +
+           "\n" +
+           "  <lineMarker>abstract</lineMarker> abs();\n" +
+           "}\n" +
+           "class A {\n" +
+           "  firstMethod(){}\n" + // no line marker
+           "  <lineMarker>@</lineMarker>foo\n" +
+           "  get v => 1;\n" +
+           "  <lineMarker>set</lineMarker> v(i) {}\n" +
+           "  <lineMarker>int</lineMarker> get i => 1;\n" +
+           "}\n" +
+           "class A {\n" +
+           "  firstMethod(){}\n" + // no line marker
+           "  <lineMarker>A</lineMarker>() {}\n" +
+           "  <lineMarker>A</lineMarker>.b() {}\n" +
+           "}\n" +
+           "class A {\n" +
+           "  var b;\n" + // no line marker
+           "  <lineMarker>bar</lineMarker>(){}\n" +
+           "}");
   }
 }
