e36870c87fbbc6da8c9fc6615d091164ebf90fc4
Nikita Sokolov <faucct@gmail.com>
2019-03-26 23:31:32 +0300

    Dependency slf4j should not be duplicated
    GitOrigin-RevId: 31f1034187f4d36b9241de424a864ab3b16f94e4

--- grazi/build.gradle.kts
+++ grazi/build.gradle.kts
@@ -32,19 +32,16 @@ detekt {
     }
 }
 
+configurations.all {
+    exclude("org.slf4j", "slf4j-api")
+}
+
 dependencies {
     compileOnly(kotlin("stdlib"))
     compileOnly(kotlin("reflect"))
     compile("org.languagetool", "languagetool-core", "4.4")
-    {
-        exclude("org.slf4j", "slf4j-api")
-    }
     compile("org.apache.commons", "commons-lang3", "3.5")
-
     compile("org.languagetool", "language-all", "4.4")
-    {
-        exclude("org.slf4j", "slf4j-api")
-    }
     testCompile("org.junit.jupiter", "junit-jupiter-api", "5.0.2")
     compile("com.github.ben-manes.caffeine", "caffeine", "2.7.0")
 }
