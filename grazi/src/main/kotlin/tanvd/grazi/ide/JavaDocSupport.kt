1682a8212885d958bddc3572e820ddf751a55b90
AlexKaneRUS <AlexKaneRUS@gmail.com>
2019-03-26 17:35:34 +0300

    javadoc support added
    GitOrigin-RevId: bd587fa1e95b0bb79a57ab59abdf64498bf190a1

--- grazi/src/main/kotlin/tanvd/grazi/ide/JavaDocSupport.kt
+++ grazi/src/main/kotlin/tanvd/grazi/ide/JavaDocSupport.kt
@@ -3,8 +3,12 @@ package tanvd.grazi.ide
 
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.*
+import com.intellij.psi.impl.PsiElementFactoryImpl
+import com.intellij.psi.impl.PsiManagerEx
 import com.intellij.psi.impl.source.javadoc.PsiDocCommentImpl
 import com.intellij.psi.util.PsiTreeUtil
+import org.intellij.plugins.markdown.lang.MarkdownFileType
+import org.intellij.plugins.markdown.lang.psi.impl.MarkdownFile
 
 class JavaDocSupport : GraziLanguageSupport {
     override fun extract(cls: PsiClass): List<TextBlock>? {
@@ -26,8 +30,9 @@ class JavaDocSupport : GraziLanguageSupport {
     }
 
     override fun replace(textBlock: TextBlock, range: TextRange, replacement: String) {
+        val elemFactory = PsiElementFactoryImpl(PsiManagerEx.getInstanceEx(textBlock.element.project))
         val newText = range.replace(textBlock.element.text, replacement)
-        val newDocComment = PsiDocCommentImpl(newText)
+        val newDocComment = elemFactory.createDocCommentFromText(newText, textBlock.element)
         textBlock.element.replace(PsiTreeUtil.collectElementsOfType(newDocComment, PsiDocCommentImpl::class.java).single())
     }
 
