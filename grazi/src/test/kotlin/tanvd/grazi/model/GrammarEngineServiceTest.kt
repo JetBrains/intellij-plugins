4bb54f3569bf5cf03b132bd9148a9899a2d5c131
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2019-03-28 12:19:27 +0300

    Test on ranges and speed
    GitOrigin-RevId: b4841071da1759df5547cfce155ce03b8cdeab58

--- grazi/src/test/kotlin/tanvd/grazi/model/GrammarEngineServiceTest.kt
+++ grazi/src/test/kotlin/tanvd/grazi/model/GrammarEngineServiceTest.kt
@@ -30,19 +30,45 @@ class GrammarEngineServiceTest {
     }
 
     @Test
+    fun testRanges() {
+        val fixes = GrammarEngineService().getFixes("hello. world,, tot he.\nThis are my friend")
+        assertEquals(5, fixes.size)
+        assertEquals(IntRange(0, 5), fixes[0].range)
+        assertEquals(IntRange(7, 12), fixes[1].range)
+        assertEquals(IntRange(12, 14), fixes[2].range)
+        assertEquals(IntRange(15, 21), fixes[3].range)
+        assertEquals(IntRange(23, 31), fixes[4].range)
+    }
+
+    @Test
     fun testNotCorrectText() {
         val fixes = GrammarEngineService().getFixes("A sentence with a error in the Hitchhiker's Guide tot he Galaxy")
         assertEquals(2, fixes.size)
     }
 
     @Test
-    fun testPerformanceMiddle() {
+    fun testCachedMiddle() {
         val text = File("src/test/resources/english_big.txt").readText()
         val grammar = GrammarEngineService()
-        var fixes = grammar.getFixes(text)
+        val fixes1 = grammar.getFixes(text)
+        var fixes2: List<Typo> = emptyList()
         val totalTime = measureTimeMillis {
-            fixes = grammar.getFixes(text)
+            fixes2 = grammar.getFixes(text)
         }
+        assertEquals(fixes1, fixes2)
+        assert(0.01 > totalTime / 1000)
+    }
+
+    @Test
+    fun testCachedBig() {
+        val text = File("src/test/resources/english_big2.txt").readText()
+        val grammar = GrammarEngineService()
+        val fixes1 = grammar.getFixes(text)
+        var fixes2: List<Typo> = emptyList()
+        val totalTime = measureTimeMillis {
+            fixes2 = grammar.getFixes(text)
+        }
+        assertEquals(fixes1, fixes2)
         assert(0.01 > totalTime / 1000)
     }
 }
