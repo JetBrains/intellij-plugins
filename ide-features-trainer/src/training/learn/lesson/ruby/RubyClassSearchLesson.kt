baea3a7121511d317714b643ab0c7ad89f44a78e
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-04-01 11:30:19 +0300

    TRNNG-CR-33 Missed space and cleanup
    GitOrigin-RevId: ea20d58812fb9471221634ac185275664e80c652

--- ide-features-trainer/src/training/learn/lesson/ruby/RubyClassSearchLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/RubyClassSearchLesson.kt
@@ -18,17 +18,13 @@ class RubyClassSearchLesson(module: Module) : KLesson("Class Search", module, "r
       task("date") {
         text("Type <code>$it</code> to see classes that contain the word <strong>$it</strong>")
         stateCheck { checkWordInSearch(it) }
-        test {
-          type(it)
-        }
+        test { type(it) }
       }
       task("datebe") {
-        text("You can search for a class by part of its name. Type <code>be</code> (the search string will be <code>$it</code>)" +
+        text("You can search for a class by part of its name. Type <code>be</code> (the search string will be <code>$it</code>) " +
             "to see classes that contain the words <strong>date</strong> and <strong>be</strong>")
         stateCheck { checkWordInSearch(it) }
-        test {
-          type("be")
-        }
+        test { type("be") }
       }
       task("QuickImplementations") {
         text("To check the selected class before navigating to it, you can use ${action(it)} to see its quick definition")
