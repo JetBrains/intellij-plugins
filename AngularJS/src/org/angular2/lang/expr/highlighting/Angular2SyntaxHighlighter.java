a996dbeb902b0369055c756642d20a2636f677ee
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-04-01 11:54:08 +0200

    IDEA-CR-45788: WEB-37963 Angular: code should be colored according to TypeScript color settings
    (cherry picked from commit b1676b85c1990e38f02f06b907dbab4b8eb6740b)

--- AngularJS/src/org/angular2/lang/expr/highlighting/Angular2SyntaxHighlighter.java
+++ AngularJS/src/org/angular2/lang/expr/highlighting/Angular2SyntaxHighlighter.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.lang.expr.highlighting;
 
-import com.intellij.lang.javascript.highlighting.JSHighlighter;
+import com.intellij.lang.javascript.highlighting.TypeScriptHighlighter;
 import com.intellij.lexer.Lexer;
 import com.intellij.psi.tree.TokenSet;
 import org.angular2.lang.expr.Angular2Language;
@@ -9,7 +9,7 @@ import org.angular2.lang.expr.lexer.Angular2Lexer;
 import org.angular2.lang.expr.lexer.Angular2TokenTypes;
 import org.jetbrains.annotations.NotNull;
 
-public class Angular2SyntaxHighlighter extends JSHighlighter {
+public class Angular2SyntaxHighlighter extends TypeScriptHighlighter {
 
   public Angular2SyntaxHighlighter() {
     super(Angular2Language.INSTANCE.getOptionHolder(), false);
