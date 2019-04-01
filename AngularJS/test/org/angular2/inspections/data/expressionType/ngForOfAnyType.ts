2a3212aa48e25e74aa04025074bce44b3d9436e8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-04-01 16:02:48 +0200

    IDEA-CR-45814: WEB-37943 Angular. Can't resolve the variable name for ngFor items of type 'any'
    (cherry picked from commit 76f77d7a867b26748287304b8b6614b70d6223b5)

---
+++ AngularJS/test/org/angular2/inspections/data/expressionType/ngForOfAnyType.ts
@@ -0,0 +1,13 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import {Component} from "@angular/core"
+
+@Component({
+  template:`
+    <div *ngFor='let item of $any(<error descr="Unresolved variable or type foo">foo</error>)'>
+      {{item.<weak_warning descr="Unresolved variable blibop">blibop</weak_warning>}}
+    </div>
+  `
+})
+export class MyComponent {
+
+}
