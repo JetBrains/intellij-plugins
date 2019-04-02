8bd60b8e78edbcb0b2bfe418b431de72d6121fbc
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2019-04-02 16:22:37 +0300

    Add Python doc
    GitOrigin-RevId: 90032405322854f8d1a80b9cadb33bf3ec81f5b1

---
+++ grazi/src/main/kotlin/tanvd/grazi/ide/language/python/PDocSupport.kt
@@ -0,0 +1,26 @@
+package tanvd.grazi.ide.language.python
+
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.psi.PyFile
+import com.jetbrains.python.psi.PyStringLiteralExpression
+import tanvd.grazi.grammar.Typo
+import tanvd.grazi.ide.language.LanguageSupport
+import tanvd.grazi.utils.buildSet
+import tanvd.grazi.utils.filterFor
+
+
+class PDocSupport : LanguageSupport {
+    override fun isSupported(file: PsiFile): Boolean {
+        return file is PyFile
+    }
+
+    override fun check(file: PsiFile) = buildSet<Typo> {
+        val strLiterals = file.filterFor<PyStringLiteralExpression>()
+        for (strElements in strLiterals.filter{ it.isDocString }.map{ it.stringElements }) {
+            addAll(PUtils.python.check(strElements))
+
+            ProgressManager.checkCanceled()
+        }
+    }
+}
