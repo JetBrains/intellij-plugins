79a84e5b9acfe601d3df29c98b762c561d5237e7
Aleksandr Khvorov <khvorov.aleksandr@gmail.com>
2019-04-06 13:59:43 +0300

    Add Python construct support
    GitOrigin-RevId: 7ec51974e4ad57a8d890a4c2e8f1685125c64aa7

---
+++ grazi/src/main/kotlin/tanvd/grazi/ide/language/python/PConstructSupport.kt
@@ -0,0 +1,30 @@
+package tanvd.grazi.ide.language.python
+
+import com.intellij.openapi.progress.ProgressManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiNamedElement
+import com.jetbrains.python.psi.*
+import tanvd.grazi.grammar.Typo
+import tanvd.grazi.ide.language.LanguageSupport
+import tanvd.grazi.spellcheck.GraziSpellchecker
+import tanvd.grazi.utils.*
+
+class PConstructsSupport : LanguageSupport {
+    override fun isSupported(file: PsiFile): Boolean {
+        return file is PyFile
+    }
+
+    override fun check(file: PsiFile) = buildSet<Typo> {
+        for (ident in file.filterFor<PsiNamedElement>()) {
+            ident.name?.let {
+                val indexOfName = ident.text.indexOf(it)
+                addAll(GraziSpellchecker.check(it).map { typo ->
+                    typo.copy(location = typo.location.copy(range = typo.location.range.withOffset(indexOfName),
+                            element = ident, shouldUseRename = true))
+                })
+            }
+        }
+
+        ProgressManager.checkCanceled()
+    }
+}
