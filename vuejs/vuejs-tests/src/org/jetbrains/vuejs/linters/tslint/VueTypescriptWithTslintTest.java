4faeb41c85cab48df88bea45cddf420bb068f0fe
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-04-11 16:09:31 +0300

    tslint-typescript with vue: fix line endings highlighting (WEB-38213, IDEA-CR-46215 followup)

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/linters/tslint/VueTypescriptWithTslintTest.java
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/linters/tslint/VueTypescriptWithTslintTest.java
@@ -1,7 +1,9 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.linters.tslint;
 
+import com.intellij.lang.javascript.JSTestUtils;
 import com.intellij.lang.javascript.linter.tslint.TypescriptServiceWithTslintTestBase;
+import com.intellij.util.LineSeparator;
 
 import static org.jetbrains.vuejs.language.VueTestUtilKt.vueRelativeTestDataPath;
 
@@ -17,5 +19,13 @@ public class VueTypescriptWithTslintTest extends TypescriptServiceWithTslintTest
 
   public void testFixAllErrorsWithWhitespaceRules() {
     doFixTest("main", "vue", "Fix all auto-fixable tslint failures");
+  }
+
+  public void testMatchingLineEndingsNotHighlighted() {
+    doHighlightingTest("main", "vue", () -> JSTestUtils.ensureLineSeparators(myFixture.getFile(), LineSeparator.CRLF));
+  }
+
+  public void testMismatchedLineEndingsHighlighted() {
+    doHighlightingTest("main", "vue", () -> JSTestUtils.ensureLineSeparators(myFixture.getFile(), LineSeparator.LF));
   }
 }
---
+++ vuejs/vuejs-tests/testData/linters/tslint/mismatchedLineEndingsHighlighted/main.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>
+      <div>
+        Hello
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+<warning descr="tslint: Expected linebreak to be 'CRLF' (linebreak-style)">    console.log('hello')</warning>
+<warning descr="tslint: Expected linebreak to be 'CRLF' (linebreak-style)">    console.log('world')</warning>
+<warning descr="tslint: Expected linebreak to be 'CRLF' (linebreak-style)"></warning></script>
