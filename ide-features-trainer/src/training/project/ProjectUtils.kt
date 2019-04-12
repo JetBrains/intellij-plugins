ee7ec1172de3001b507215ece22fd07f8e7ddcb8
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-04-12 14:30:37 +0300

    Save plugin version into learning project and rewrite the project if the version is changed
    GitOrigin-RevId: 7a1b548c1d23476b95f5faf2756d6b363cc8bc85

--- ide-features-trainer/src/training/project/ProjectUtils.kt
+++ ide-features-trainer/src/training/project/ProjectUtils.kt
@@ -8,8 +8,12 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
 import com.intellij.openapi.vfs.VfsUtil.findFileByIoFile
 import com.intellij.openapi.vfs.VirtualFile
+import training.util.featureTrainerVersion
 import java.io.File
+import java.io.PrintWriter
 import java.net.URL
+import java.nio.file.Files
+
 
 object ProjectUtils {
 
@@ -28,13 +32,37 @@ object ProjectUtils {
    */
   fun importOrOpenProject(projectPath: String, projectName: String): Project? {
     val dest = File(ideProjectsBasePath, projectName)
-    if (!checkProjectExistence(projectName)) {
+
+    if (!isSameVersion(dest)) {
+      if (dest.exists()) {
+        dest.deleteRecursively()
+      }
       val inputUrl: URL = javaClass.getResource(projectPath)
       FileUtils.copyResourcesRecursively(inputUrl, dest)
+      PrintWriter(versionFile(dest), "UTF-8").use {
+        it.println(featureTrainerVersion)
+      }
     }
     val toSelect = findFileByIoFile(dest, false) ?: throw Exception("Copied Learn project folder is null")
     return doImportOrOpenProject(toSelect)
   }
+
+  private fun isSameVersion(dest: File): Boolean {
+    if (!dest.exists()) {
+      return false
+    }
+    val versionFile = versionFile(dest)
+    if (!versionFile.exists()) {
+      return false
+    }
+    val res = Files.lines(versionFile.toPath()).findFirst()
+    if (res.isPresent) {
+      return featureTrainerVersion == res.get()
+    }
+    return false
+  }
+
+  private fun versionFile(dest: File) = File(dest, "feature-trainer-version.txt")
 
   private fun doImportOrOpenProject(projectDir: VirtualFile): Project? {
     val projectRef = Ref<Project>()
