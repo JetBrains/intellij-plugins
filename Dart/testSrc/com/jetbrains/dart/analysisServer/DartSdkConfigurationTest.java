b98973dd6a95b5dfaa133da4639e5dd1fd238cc7
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-04-15 18:48:04 +0300

    dart:ffi library added to the Dart SDK 2.2.1

--- Dart/testSrc/com/jetbrains/dart/analysisServer/DartSdkConfigurationTest.java
+++ Dart/testSrc/com/jetbrains/dart/analysisServer/DartSdkConfigurationTest.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.dart.analysisServer;
 
 import com.intellij.openapi.roots.OrderRootType;
@@ -36,6 +24,7 @@ public class DartSdkConfigurationTest extends CodeInsightFixtureTestCase {
       "file://" + sdkHomePath + "/lib/convert",
       "file://" + sdkHomePath + "/lib/core",
       "file://" + sdkHomePath + "/lib/developer",
+      "file://" + sdkHomePath + "/lib/ffi",
       "file://" + sdkHomePath + "/lib/html",
       "file://" + sdkHomePath + "/lib/indexed_db",
       "file://" + sdkHomePath + "/lib/io",
