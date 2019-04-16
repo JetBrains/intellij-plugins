6b668f326125d0f20f8944ec4a6096942939c1bf
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-04-16 16:34:12 +0300

    Dart plugin should treat Android Studio as an IDE with multi-module project support

--- Dart/src/com/jetbrains/lang/dart/sdk/DartSdkLibUtil.java
+++ Dart/src/com/jetbrains/lang/dart/sdk/DartSdkLibUtil.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.sdk;
 
 import com.google.common.annotations.VisibleForTesting;
@@ -42,7 +42,7 @@ public class DartSdkLibUtil {
   };
 
   public static boolean isIdeWithMultipleModuleSupport() {
-    return PlatformUtils.isIntelliJ();
+    return PlatformUtils.isIntelliJ() || "AndroidStudio".equals(PlatformUtils.getPlatformPrefix());
   }
 
   public static void ensureDartSdkConfigured(@NotNull final Project project, @NotNull final String sdkHomePath) {
