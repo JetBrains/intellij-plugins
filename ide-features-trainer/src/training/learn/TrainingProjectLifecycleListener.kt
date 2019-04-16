12de3e4bb034a3da541273c81d22c0f2a61b3fd6
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-04-16 17:55:34 +0300

    TRNNG-CR-42 Clarify path meaning and fix bug in Windows
    GitOrigin-RevId: 137d6aa11809087e2b361487a87ed05fcd6291f3

--- ide-features-trainer/src/training/learn/TrainingProjectLifecycleListener.kt
+++ ide-features-trainer/src/training/learn/TrainingProjectLifecycleListener.kt
@@ -3,12 +3,13 @@ package training.learn
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.impl.ProjectLifecycleListener
+import com.intellij.openapi.util.io.FileUtil
 import training.lang.LangManager
 
 class TrainingProjectLifecycleListener : ProjectLifecycleListener {
   override fun projectComponentsInitialized(project: Project) {
     val langSupport = LangManager.getInstance().getLangSupport() ?: return
-    if (project.basePath == NewLearnProjectUtil.projectFilePath(langSupport)) {
+    if (FileUtil.pathsEqual(project.basePath, NewLearnProjectUtil.projectFilePath(langSupport))) {
       langSupport.setProjectListeners(project)
     }
   }
