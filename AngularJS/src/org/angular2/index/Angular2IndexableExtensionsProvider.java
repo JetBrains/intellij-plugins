8db39ef86229bec7fd0dce9d56e285a89c06882b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-04-18 12:00:52 +0200

    WEB-38354 Angular: wrong exclusions with Angular libraries

---
+++ AngularJS/src/org/angular2/index/Angular2IndexableExtensionsProvider.java
@@ -0,0 +1,16 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.angular2.index;
+
+import com.intellij.lang.typescript.library.TypeScriptIndexableExtensionsProvider;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.angular2.entities.metadata.Angular2MetadataFileType.METADATA_SUFFIX;
+
+public class Angular2IndexableExtensionsProvider implements TypeScriptIndexableExtensionsProvider {
+  @Override
+  public List<String> getIndexableExtensions() {
+    return Collections.singletonList(METADATA_SUFFIX);
+  }
+}
