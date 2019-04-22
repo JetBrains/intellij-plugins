55b079142c69af19516f2d5ed7f7f32efd9b23c6
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-04-22 18:48:42 +0300

    Generalize tab-completion-lesson
    GitOrigin-RevId: 94fcbb9450a7a4aeb9d742b154bd79128fbf5867

---
+++ ide-features-trainer/src/training/learn/lesson/general/CompletionWithTabLesson.kt
@@ -0,0 +1,24 @@
+package training.learn.lesson.general
+
+import training.learn.interfaces.Module
+import training.learn.lesson.kimpl.KLesson
+import training.learn.lesson.kimpl.LessonContext
+import training.learn.lesson.kimpl.LessonSample
+
+abstract class CompletionWithTabLesson(module: Module, lang: String, private val proposal: String) :
+    KLesson("Completion with Tab", module, lang) {
+
+  abstract val sample: LessonSample
+
+  override val lessonContent: LessonContext.() -> Unit
+    get() = {
+      prepareSample(sample)
+
+      actionTask("CodeCompletion") { "Press ${action(it)} to show completion options." }
+
+      actionTask("EditorChooseLookupItemReplace") {
+        "Choose <code>$proposal</code>, for example, and press ${action("EditorTab")}." +
+          "This overwrites the word at the caret rather than simply inserting it."
+      }
+    }
+}
