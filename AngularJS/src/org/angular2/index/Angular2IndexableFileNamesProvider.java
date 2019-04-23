3ac3662ece58a8540f8f080e1ad33989d6d309c7
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-04-23 13:16:03 +0200

    IDEA-CR-44403: WEB-36722 WEB-35634 Node Modules: index indirect dependencies with typings - post review fixes
    (cherry picked from commit 2ed50fb446f396176d7c96df110a2d9a3bbf3372)
    
    GitOrigin-RevId: b1d297680151aefb3a6801cd9583f83c3643b45a

--- AngularJS/src/org/angular2/index/Angular2IndexableFileNamesProvider.java
+++ AngularJS/src/org/angular2/index/Angular2IndexableFileNamesProvider.java
@@ -2,6 +2,7 @@
 package org.angular2.index;
 
 import com.intellij.lang.typescript.library.TypeScriptIndexableFileNamesProvider;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.List;
 import static org.angular2.entities.metadata.Angular2MetadataFileType.METADATA_SUFFIX;
 
 public class Angular2IndexableFileNamesProvider implements TypeScriptIndexableFileNamesProvider {
+  @NotNull
   @Override
   public List<String> getIndexableExtensions() {
     return Collections.singletonList(METADATA_SUFFIX);
