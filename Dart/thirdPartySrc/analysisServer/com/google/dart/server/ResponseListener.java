15a09ba5abf9342142c99082445149248c266d06
Ari Aye <ariaye@google.com>
2019-04-24 11:37:49 -0700

    Make RequestListener and ResponseListener pass Strings instead of JsonObjects

--- Dart/thirdPartySrc/analysisServer/com/google/dart/server/ResponseListener.java
+++ Dart/thirdPartySrc/analysisServer/com/google/dart/server/ResponseListener.java
@@ -1,10 +1,8 @@
 package com.google.dart.server;
 
-import com.google.gson.JsonObject;
-
 /**
  * Listener for arbitrary responses received from the analysis server.
  */
 public interface ResponseListener {
-  void onResponse(JsonObject json);
+  void onResponse(String jsonString);
 }
