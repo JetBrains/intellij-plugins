2251c3c0d7876fe71207c88c9c6935d6bf5e943b
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-04-26 18:30:19 +0300

    Update Ruby hippie completion lesson
    Remove warnings and add backward step
    
    GitOrigin-RevId: 4cef08461f354ec0dd03f53c851a22870fd986a9

--- ide-features-trainer/src/training/learn/lesson/ruby/RubyHippieCompletionLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/RubyHippieCompletionLesson.kt
@@ -8,19 +8,22 @@ import training.learn.lesson.kimpl.START_TAG
 import training.learn.lesson.kimpl.parseLessonSample
 
 class RubyHippieCompletionLesson(module: Module) : KLesson("Hippie Completion", module, "ruby") {
-  private val sample = parseLessonSample("""class UsersController
-  def index
-    @users = User.where(activated: true).
-      paginate(page: params[:p<caret>])
+  private val sample = parseLessonSample("""class SomeExampleClass
+  attr_reader :callbacks
+
+  def initialize
+    @callbacks = [:before_create, :before_save]
   end
+
+  def be<caret>
 end
 """.trimIndent())
 
   override val lessonContent: LessonContext.() -> Unit
     get() = {
       prepareSample(sample)
-      val step1 = calculateResult("arams")
-      val step2 = calculateResult("age")
+      val step1 = calculateResult("fore_save")
+      val step2 = calculateResult("fore_create")
       task("HippieCompletion") {
         text("Sometimes you need to complete a word by textual similarity. Press ${action(it)} to invoke hippie completion.")
         trigger(it) { editor.document.text == step1 }
@@ -29,6 +32,11 @@ end
       task("HippieCompletion") {
         text("You can repeat ${action(it)} until the desired word is found. Try it once more now.")
         trigger(it) { editor.document.text == step2 }
+        test { actions(it) }
+      }
+      task("HippieBackwardCompletion") {
+        text("You can return to the previous variant with ${action(it)}. Use it now.")
+        trigger(it) { editor.document.text == step1 }
         test { actions(it) }
       }
     }
