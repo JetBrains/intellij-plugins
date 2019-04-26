4e4c2131b2257c98e4083b472b4c5d66c1e0e82e
develar <develar@gmail.com>
2019-04-26 17:33:44 +0200

    cleanup - get TextEditorHighlightingPassRegistrar directly
    GitOrigin-RevId: 287cb487372e22715ad53ee6758990af784236b3

--- vuejs/src/org/jetbrains/vuejs/typescript/service/VueTypeScriptServiceHighlightingPassFactory.kt
+++ vuejs/src/org/jetbrains/vuejs/typescript/service/VueTypeScriptServiceHighlightingPassFactory.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.typescript.service
 
-import com.intellij.codeHighlighting.TextEditorHighlightingPassRegistrar
 import com.intellij.lang.javascript.service.JSLanguageService
 import com.intellij.lang.javascript.service.highlighting.JSLanguageServiceHighlightingPassFactory
 import com.intellij.lang.typescript.compiler.TypeScriptServiceHighlightingPassFactory
@@ -9,8 +8,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import org.jetbrains.vuejs.VueFileType
 
-class VueTypeScriptServiceHighlightingPassFactory(project: Project, highlightingPassRegistrar: TextEditorHighlightingPassRegistrar) :
-  JSLanguageServiceHighlightingPassFactory(project, highlightingPassRegistrar) {
+internal class VueTypeScriptServiceHighlightingPassFactory(project: Project) : JSLanguageServiceHighlightingPassFactory(project) {
   override fun getService(file: PsiFile): JSLanguageService? {
     val service = TypeScriptServiceHighlightingPassFactory.getService(myProject, file)
     return if (service is VueTypeScriptService) service else null
