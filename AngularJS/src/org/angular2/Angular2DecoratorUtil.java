9e8c6627d35f7f300e05ce5c069aa7b58063db01
Maxim Kropotov <maxim.kropotov@jetbrains.com>
2019-05-17 19:04:58 +0300

    js: extract 'injections' package
    GitOrigin-RevId: 3522d64b4727d58d8b242028241d1f9c69350f83

--- AngularJS/src/org/angular2/Angular2DecoratorUtil.java
+++ AngularJS/src/org/angular2/Angular2DecoratorUtil.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2;
 
-import com.intellij.lang.javascript.JSInjectionController;
+import com.intellij.lang.javascript.injections.JSInjectionController;
 import com.intellij.lang.javascript.psi.*;
 import com.intellij.lang.javascript.psi.ecma6.ES6Decorator;
 import com.intellij.lang.javascript.psi.ecmal4.JSAttributeList;
