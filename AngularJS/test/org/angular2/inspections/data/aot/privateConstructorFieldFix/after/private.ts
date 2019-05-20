57e3cdb44f85f55e5e7232d4667a85eda4586b7d
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-05-20 17:37:44 +0200

    IDEA-CR-47415: WEB-38734 Angular: find usages of private component fields in templates and fix scope resolution
    (cherry picked from commit 966c0fb7495d6b1faaef7beea7adf23e06c5557b)
    
    GitOrigin-RevId: 151f81750089f11f54779767b09bd98383d7ae87

---
+++ AngularJS/test/org/angular2/inspections/data/aot/privateConstructorFieldFix/after/private.ts
@@ -0,0 +1,13 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import {Component} from "@angular/core"
+
+@Component({
+  templateUrl: "./private.html"
+})
+export class MyComponent {
+
+  constructor(public privateField: string) {
+
+  }
+
+}
