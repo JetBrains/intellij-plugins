437fd7d5d167240873e5fc6e019ddd15e3437ebb
Dave Shuckerow <djshuckerow@google.com>
2019-05-22 11:31:30 -0700

    Update everything to milliseconds

--- Dart/src/com/jetbrains/lang/dart/analyzer/DartAnalysisServerService.java
+++ Dart/src/com/jetbrains/lang/dart/analyzer/DartAnalysisServerService.java
@@ -97,25 +97,25 @@ public class DartAnalysisServerService implements Disposable {
   private static final long UPDATE_FILES_TIMEOUT = 300;
 
   private static final long CHECK_CANCELLED_PERIOD = 10;
-  private static final long SEND_REQUEST_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  private static final long EDIT_FORMAT_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
+  private static final long SEND_REQUEST_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
+  private static final long EDIT_FORMAT_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(3000);
   private static final long EDIT_ORGANIZE_DIRECTIVES_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(300);
-  private static final long EDIT_SORT_MEMBERS_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  private static final long GET_HOVER_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  private static final long GET_NAVIGATION_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
+  private static final long EDIT_SORT_MEMBERS_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(3000);
+  private static final long GET_HOVER_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
+  private static final long GET_NAVIGATION_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
   private static final long GET_ASSISTS_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(300);
   private static final long GET_FIXES_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(300);
   private static final long IMPORTED_ELEMENTS_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(100);
   private static final long POSTFIX_COMPLETION_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(100);
   private static final long POSTFIX_INITIALIZATION_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
   private static final long STATEMENT_COMPLETION_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(100);
-  private static final long GET_SUGGESTIONS_TIMEOUT = TimeUnit.SECONDS.toMillis(100);
+  private static final long GET_SUGGESTIONS_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(5000);
   private static final long GET_SUGGESTION_DETAILS_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(100);
-  private static final long FIND_ELEMENT_REFERENCES_TIMEOUT = TimeUnit.SECONDS.toMillis(100);
-  private static final long GET_TYPE_HIERARCHY_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  private static final long EXECUTION_CREATE_CONTEXT_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  private static final long EXECUTION_MAP_URI_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
-  private static final long ANALYSIS_IN_TESTS_TIMEOUT = TimeUnit.SECONDS.toMillis(10);
+  private static final long FIND_ELEMENT_REFERENCES_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
+  private static final long GET_TYPE_HIERARCHY_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(10000);
+  private static final long EXECUTION_CREATE_CONTEXT_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
+  private static final long EXECUTION_MAP_URI_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(1000);
+  private static final long ANALYSIS_IN_TESTS_TIMEOUT = TimeUnit.MILLISECONDS.toMillis(10000);
   private static final long TESTS_TIMEOUT_COEFF = 10;
 
   private static final Logger LOG = Logger.getInstance("#com.jetbrains.lang.dart.analyzer.DartAnalysisServerService");
