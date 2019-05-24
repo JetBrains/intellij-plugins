60cb8913991d273bc57b4dbf1452956f27c667a8
Andrey Vokin <andrey.vokin@jetbrains.com>
2019-05-24 10:01:03 +0200

    [Cucumber for Java] Calculate glue under progress indicator before running tests
    To prevent freeze when running Cucumber tests from context menu glue calculation should be performed under Progress Indicator.
    
    IDEA-194222 Trying to run Cucumber scenario by calling "Run <scenario name>" action from context menu leads to freezing UI
    [IDEA-CR-47583]
    
    (cherry picked from commit d79eeb3fc29531a621c8665f999f6f30e795da95)
    
    GitOrigin-RevId: 4149dacc3bb24d3c841734f4abcc5303b7ead285

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/run/CucumberJavaAllFeaturesInFolderRunConfigurationProducer.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/run/CucumberJavaAllFeaturesInFolderRunConfigurationProducer.java
@@ -2,58 +2,18 @@
 package org.jetbrains.plugins.cucumber.java.run;
 
 import com.intellij.execution.actions.ConfigurationContext;
-import com.intellij.openapi.util.NullableComputable;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.CucumberBundle;
-import org.jetbrains.plugins.cucumber.CucumberJvmExtensionPoint;
-import org.jetbrains.plugins.cucumber.psi.GherkinFile;
-
-import java.util.List;
-import java.util.Set;
 
 public class CucumberJavaAllFeaturesInFolderRunConfigurationProducer extends CucumberJavaRunConfigurationProducer {
   @Override
-  protected NullableComputable<String> getStepsGlue(@NotNull final PsiElement element) {
-    final Set<String> glues = getHookGlue(element);
+  protected CucumberGlueProvider getGlueProvider(@NotNull final PsiElement element) {
     if (element instanceof PsiDirectory) {
-      final PsiDirectory dir = (PsiDirectory)element;
-      final List<CucumberJvmExtensionPoint> extensions = CucumberJvmExtensionPoint.EP_NAME.getExtensionList();
-      return new NullableComputable<String>() {
-        @NotNull
-        @Override
-        public String compute() {
-          dir.accept(new PsiElementVisitor() {
-            @Override
-            public void visitFile(final PsiFile file) {
-              if (file instanceof GherkinFile) {
-                for (CucumberJvmExtensionPoint extension : extensions) {
-                  extension.getGlues((GherkinFile)file, glues);
-                }
-              }
-            }
-
-            @Override
-            public void visitDirectory(PsiDirectory dir) {
-              for (PsiDirectory subDir : dir.getSubdirectories()) {
-                subDir.accept(this);
-              }
-
-              for (PsiFile file : dir.getFiles()) {
-                file.accept(this);
-              }
-            }
-          });
-
-          return StringUtil.join(glues, " ");
-        }
-      };
+      return new CucumberJavaAllFeaturesInFolderGlueProvider((PsiDirectory) element);
     }
     return null;
   }
