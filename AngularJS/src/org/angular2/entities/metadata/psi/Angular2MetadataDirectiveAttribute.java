1bb06b78d66807b45b9cdffd973bd99fc9b78ce5
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-05-28 12:13:54 +0200

    IDEA-CR-47708: WEB-38855 Angular - support @Attribute decorator - improve initial performance
    (cherry picked from commit eb39e9ef99fcf0c4087f0d1a58acc31bc4447fb8)
    
    GitOrigin-RevId: c4303934d1dfa9d0fbe918cdcadf9a3746c224f1

--- AngularJS/src/org/angular2/entities/metadata/psi/Angular2MetadataDirectiveAttribute.java
+++ AngularJS/src/org/angular2/entities/metadata/psi/Angular2MetadataDirectiveAttribute.java
@@ -2,44 +2,49 @@ package org.angular2.entities.metadata.psi;
 
 import com.intellij.lang.javascript.psi.JSParameter;
 import com.intellij.lang.javascript.psi.JSType;
+import com.intellij.lang.javascript.psi.JSTypeInfoOwner;
+import com.intellij.openapi.util.NotNullComputable;
 import com.intellij.psi.PsiElement;
 import org.angular2.entities.Angular2DirectiveAttribute;
 import org.angular2.entities.Angular2EntityUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class Angular2MetadataDirectiveAttribute implements Angular2DirectiveAttribute {
-  private final JSParameter myParameter;
-  private final String myBindingName;
+import java.util.function.Supplier;
 
-  Angular2MetadataDirectiveAttribute(@NotNull final JSParameter parameter,
-                                     @NotNull final String bindingName) {
-    myParameter = parameter;
-    myBindingName = bindingName;
+import static com.intellij.util.ObjectUtils.doIfNotNull;
+import static com.intellij.util.ObjectUtils.notNull;
+
+public class Angular2MetadataDirectiveAttribute implements Angular2DirectiveAttribute {
+
+  private final Supplier<? extends JSParameter> myParameterSupplier;
+  private final NotNullComputable<? extends PsiElement> mySourceSupplier;
+  private final String myName;
+
+  Angular2MetadataDirectiveAttribute(@NotNull final Supplier<? extends JSParameter> parameterSupplier,
+                                     @NotNull final NotNullComputable<? extends PsiElement> sourceSupplier,
+                                     @NotNull final String name) {
+    myParameterSupplier = parameterSupplier;
+    mySourceSupplier = sourceSupplier;
+    myName = name;
   }
 
   @NotNull
   @Override
   public String getName() {
-    return myBindingName;
+    return myName;
   }
 
   @Nullable
   @Override
   public JSType getType() {
-    return myParameter.getJSType();
+    return doIfNotNull(myParameterSupplier.get(), JSTypeInfoOwner::getJSType);
   }
 
   @NotNull
   @Override
   public PsiElement getSourceElement() {
-    return myParameter;
-  }
-
-  @NotNull
-  @Override
-  public PsiElement getNavigableElement() {
-    return myParameter.getNavigationElement();
+    return notNull(myParameterSupplier.get(), mySourceSupplier.compute());
   }
 
   @Override
