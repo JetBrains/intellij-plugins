92dea84b22516899ba18562b2aa5199f9c469a4d
Henry Wylde <henry.wylde@jetbrains.com>
2019-06-07 15:20:59 +0200

    RUBY-23764 remove remaining usages of RubyCodeOccurrenceProcessor
    GitOrigin-RevId: 64eb990e76bb1c32c84fdfeb6a844d48d3d475b1

--- cucumber/src/org/jetbrains/plugins/cucumber/steps/search/CucumberStepSearchUtil.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/steps/search/CucumberStepSearchUtil.java
@@ -1,20 +1,17 @@
 package org.jetbrains.plugins.cucumber.steps.search;
 
-import com.intellij.openapi.application.ReadAction;
-import com.intellij.openapi.util.Computable;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.SearchScope;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.GherkinFileType;
 
 public class CucumberStepSearchUtil {
-  public static SearchScope restrictScopeToGherkinFiles(final Computable<? extends SearchScope> originalScopeComputation) {
-    return ReadAction.compute(() -> {
-      final SearchScope originalScope = originalScopeComputation.compute();
-      if (originalScope instanceof GlobalSearchScope) {
-        return GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)originalScope,
-                                                               GherkinFileType.INSTANCE);
-      }
-      return originalScope;
-    });
+  @NotNull
+  public static SearchScope restrictScopeToGherkinFiles(@NotNull final SearchScope originalScope) {
+    if (originalScope instanceof GlobalSearchScope) {
+      return GlobalSearchScope.getScopeRestrictedByFileTypes((GlobalSearchScope)originalScope, GherkinFileType.INSTANCE);
+    }
+
+    return originalScope;
   }
 }
