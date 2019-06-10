ef33d417238d1f9764ba071fd28d83ff2d835d96
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-06-10 20:05:47 +0300

    Refactor lesson sample API and add selection tag
    GitOrigin-RevId: 5f8ac1951824db52a6511492666a47e8a1df033a

--- ide-features-trainer/testSrc/training/simple/SampleParserTest.kt
+++ ide-features-trainer/testSrc/training/simple/SampleParserTest.kt
@@ -1,26 +1,42 @@
 package training.simple
 
 import com.intellij.testFramework.UsefulTestCase
-import training.learn.lesson.kimpl.START_TAG
 import training.learn.lesson.kimpl.parseLessonSample
 
 class SampleParserTest : UsefulTestCase() {
   fun testStartCaretTag1() {
-    val lesson = parseLessonSample("hello <caret>world")
-    assertSameLines("hello world", lesson.text)
-    assertSame(6, lesson.getInfo(START_TAG).startOffset)
+    val sample = parseLessonSample("hello <caret>world")
+    assertSameLines("hello world", sample.text)
+    assertSame(6, sample.startOffset)
   }
 
   fun testStartCaretTag2() {
-    val lesson = parseLessonSample("<caret>At the start")
-    assertSameLines("At the start", lesson.text)
-    assertSame(0, lesson.getInfo(START_TAG).startOffset)
+    val sample = parseLessonSample("<caret>At the start")
+    assertSameLines("At the start", sample.text)
+    assertSame(0, sample.startOffset)
   }
-
 
   fun testStartCaretTag3() {
-    val lesson = parseLessonSample("In the end<caret>")
-    assertSameLines("In the end", lesson.text)
-    assertSame(10, lesson.getInfo(START_TAG).startOffset)
+    val sample = parseLessonSample("In the end<caret>")
+    assertSameLines("In the end", sample.text)
+    assertSame(10, sample.startOffset)
   }
+
+
+  fun testSelectTag() {
+    val sample = parseLessonSample("""
+      hello <select>world
+      next </select>line
+    """.trimIndent())
+    assertSameLines("""
+      hello world
+      next line
+    """.trimIndent(), sample.text)
+    assertNotNull(sample.selection)
+    assertSame(25, sample.startOffset)
+    assertSame(6, sample.selection?.first ?: -1)
+    assertSame(17, sample.selection?.second ?: -1)
+  }
+
+
 }
