3e543d8525e2bfa1dbd8bf8ff004d9236ff6d360
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-06-11 09:54:18 +0200

    Vue.js: refactor component details providers into model.source package
    GitOrigin-RevId: 8863653c97497f54fc67f666a8641990ffc3c6e3

--- vuejs/src/org/jetbrains/vuejs/model/source/VueExtendsLocalComponentDetailsProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/model/source/VueExtendsLocalComponentDetailsProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.vuejs.codeInsight
+package org.jetbrains.vuejs.model.source
 
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
 import com.intellij.lang.javascript.psi.stubs.JSImplicitElement
