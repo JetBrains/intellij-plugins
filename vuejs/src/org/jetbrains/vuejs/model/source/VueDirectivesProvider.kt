8061b89e0735242bbb4ad9506de6998a24ea26ce
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-06-11 09:54:18 +0200

    IDEA-CR-48291: Vue.js: refactor component details providers into model.source package
    (cherry picked from commit 8863653c97497f54fc67f666a8641990ffc3c6e3)
    
    GitOrigin-RevId: 51a2957db7407baa8fa6cb1f9ba68d9e45415b9e

--- vuejs/src/org/jetbrains/vuejs/model/source/VueDirectivesProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/model/source/VueDirectivesProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.vuejs.codeInsight
+package org.jetbrains.vuejs.model.source
 
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
 import com.intellij.lang.javascript.psi.JSProperty
@@ -9,6 +9,8 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.vuejs.codeInsight.attributes.VueAttributeDescriptor
 import org.jetbrains.vuejs.codeInsight.attributes.findProperty
+import org.jetbrains.vuejs.codeInsight.fromAsset
+import org.jetbrains.vuejs.codeInsight.getContainingXmlFile
 import org.jetbrains.vuejs.index.*
 
 class VueDirectivesProvider {
