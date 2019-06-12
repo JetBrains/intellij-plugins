7a8aed8fab565ff4eaea47caac4d81ae43045ecc
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-06-12 16:53:53 +0200

    Vue.js: support for mixins and implementation of Vue model visitor
    GitOrigin-RevId: f467bc75e100836010cd4e27d76348a620978936

--- vuejs/src/org/jetbrains/vuejs/model/VueNamedEntity.kt
+++ vuejs/src/org/jetbrains/vuejs/model/VueNamedEntity.kt
@@ -1,12 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.model
 
-import com.intellij.psi.PsiElement
-
 interface VueNamedEntity {
 
-  val source: PsiElement?
-
   val defaultName: String?
-
 }
