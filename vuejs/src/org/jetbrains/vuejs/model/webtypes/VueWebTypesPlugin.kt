779d796c00dc48bfac869ec241a9a12764733e60
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-06-12 16:53:53 +0200

    Vue.js: support for mixins and implementation of Vue model visitor
    GitOrigin-RevId: f467bc75e100836010cd4e27d76348a620978936

--- vuejs/src/org/jetbrains/vuejs/model/webtypes/VueWebTypesPlugin.kt
+++ vuejs/src/org/jetbrains/vuejs/model/webtypes/VueWebTypesPlugin.kt
@@ -1,11 +1,15 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.model.webtypes
 
+import com.intellij.psi.PsiElement
 import org.jetbrains.vuejs.model.*
 import org.jetbrains.vuejs.model.webtypes.json.WebTypes
 import java.util.*
 
-class VueWebTypesPlugin(webTypes: WebTypes, override val nodeModule: String?) : VuePlugin {
+class VueWebTypesPlugin(webTypes: WebTypes) : VuePlugin {
+  override val source: PsiElement? = null
+  override val global: VueGlobal? = null
+  override val parents: List<VueEntitiesContainer> = emptyList()
   override val components: Map<String, VueComponent>
 
   override val directives: Map<String, VueDirective>
@@ -21,7 +25,7 @@ class VueWebTypesPlugin(webTypes: WebTypes, override val nodeModule: String?) : 
                    ?.html
                    ?.tags
                    ?.filter { it.name != null }
-                   ?.associateBy({ it.name!! }, { VueWebTypesComponent(it) })
+                   ?.associateBy({ it.name!! }, { VueWebTypesComponent(it, this) })
                  ?: Collections.emptyMap()
   }
 
