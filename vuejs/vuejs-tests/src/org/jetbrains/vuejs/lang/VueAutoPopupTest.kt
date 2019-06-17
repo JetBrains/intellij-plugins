d50e5e83d53ccd23f0c80bcc8f858b8d496e1184
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-06-17 20:43:43 +0300

    IDEA-CR-48574: javascript: migrate dependencies test to platform light tests
    (cherry picked from commit bfbc62e096c3d3f2c2169e79c751aea316555452)
    
    GitOrigin-RevId: 6c46c08371b4e75504bc4ab5d78c74013a9b13dd

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueAutoPopupTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueAutoPopupTest.kt
@@ -1,10 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.lang
 
-import com.intellij.codeInsight.completion.CompletionAutoPopupTestCase
+import com.intellij.lang.javascript.JSAutoPopupTestBase
 import junit.framework.TestCase
 
-class VueAutoPopupTest : CompletionAutoPopupTestCase() {
+class VueAutoPopupTest : JSAutoPopupTestBase() {
   fun testEventsAfterAt() {
     myFixture.configureByText("a.vue", "<div <caret>>")
     type("@")
