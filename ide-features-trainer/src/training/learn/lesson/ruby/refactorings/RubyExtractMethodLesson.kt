bc35d2e00e5aee6d92d2197cdefac20940fb0f28
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-06-19 17:02:03 +0300

    TRNNG-CR-51 Rework Ruby Extract Method lesson
    GitOrigin-RevId: 5cb8b4f69364adca45133e94005450c5052f2f78

--- ide-features-trainer/src/training/learn/lesson/ruby/refactorings/RubyExtractMethodLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/refactorings/RubyExtractMethodLesson.kt
@@ -10,11 +10,25 @@ import training.learn.lesson.kimpl.parseLessonSample
 
 class RubyExtractMethodLesson(module: Module) : KLesson("Extract Method", module, "ruby") {
   private val sample = parseLessonSample("""
-    class VeryStupidExample
-      def func(a)
-        b = 10
-    <select>    x = a + b
-    </select>    x + 1
+    def cocktail_sort(array, compare = lambda { |a, b| a <=> b })
+      loop do
+        swapped = false
+        0.upto(array.length - 2) do |i|
+          if compare.call(array[i], array[i + 1]) > 0
+            <select>array[i], array[i + 1] = array[i + 1], array[i]</select>
+            swapped = true
+          end
+        end
+        break unless swapped
+
+        swapped = false
+        (array.length - 2).downto(0) do |i|
+          if compare.call(array[i], array[i + 1]) > 0
+            array[i], array[i + 1] = array[i + 1], array[i]
+            swapped = true
+          end
+        end
+        break unless swapped
       end
     end
   """.trimIndent())
@@ -22,10 +36,19 @@ class RubyExtractMethodLesson(module: Module) : KLesson("Extract Method", module
   override val lessonContent: LessonContext.() -> Unit
     get() = {
       prepareSample(sample)
+
       task("ExtractMethod") {
         text("Press ${action(it)} to extract the selected code block into a method.")
         trigger(it, { editor.document.text }) { before, now->
-          before != now
+          if (before != now) {
+            // a little bit hacky way to print additional message between refactoring modal dialogs
+            text("Cocktail Sort has 2 swap places. Click <strong>Yes</strong> to extract both of them and " +
+                "then confirm the decision.")
+            true
+          }
+          else {
+            false
+          }
         }
 
         test {
@@ -33,6 +56,12 @@ class RubyExtractMethodLesson(module: Module) : KLesson("Extract Method", module
           with(TaskTestContext.guiTestCase) {
             dialog("Extract Method") {
               button("OK").click()
+              dialog("Extract Method") {
+                button("Yes").click()
+              }
+            }
+            dialog("Replace Fragment") {
+              button("Replace").click()
             }
           }
         }
