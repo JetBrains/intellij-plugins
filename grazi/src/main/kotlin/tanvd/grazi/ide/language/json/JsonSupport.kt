8914e0a41ce6480af4c9d3efdb8ccaaeca69acf3
vdtankov <vdtankov@gmail.com>
2019-06-21 19:39:18 +0300

    Issue #24. Add support of JSON
    GitOrigin-RevId: b72cdf5937686e04376e2dc13e831f4f9ce2e510

---
+++ grazi/src/main/kotlin/tanvd/grazi/ide/language/json/JsonSupport.kt
@@ -0,0 +1,43 @@
+package tanvd.grazi.ide.language.json
+
+import com.intellij.json.JsonLanguage
+import com.intellij.json.psi.JsonStringLiteral
+import com.intellij.lang.Language
+import com.intellij.psi.PsiElement
+import tanvd.grazi.grammar.SanitizingGrammarChecker
+import tanvd.grazi.grammar.Typo
+import tanvd.grazi.ide.language.LanguageSupport
+import tanvd.grazi.utils.*
+import tanvd.kex.ifTrue
+import tanvd.kex.orTrue
+
+class JsonSupport : LanguageSupport() {
+    companion object {
+        val tagsIgnoredCategories = listOf(Typo.Category.CASING)
+
+        val json = SanitizingGrammarChecker(
+                ignore = listOf({ str, cur ->
+                    str.lastOrNull()?.let { blankOrNewLineCharRegex.matches(it) }.orTrue() && blankOrNewLineCharRegex.matches(cur)
+                }, { _, cur -> cur == '\"' }),
+                replace = listOf({ _, cur ->
+                    newLineCharRegex.matches(cur).ifTrue { ' ' }
+                }),
+                ignoreToken = listOf({ str ->
+                    str.all { !it.isLetter() }
+                }))
+
+    }
+
+    override fun isSupported(language: Language): Boolean {
+        return language is JsonLanguage
+    }
+
+    override fun isRelevant(element: PsiElement): Boolean {
+        return element is JsonStringLiteral
+    }
+
+    override fun check(element: PsiElement): Set<Typo> {
+        require(element is JsonStringLiteral) { "Got non JsonStringLiteral in JsonSupport" }
+        return json.check(element).filterNot { it.info.category in tagsIgnoredCategories }.toSet()
+    }
+}
