2a490058b507c6a9ab8219b543c1d2eead1cf9d5
Vladislav Tankov <vdtankov@gmail.com>
2019-06-23 20:44:21 +0300

    Fix display level of commit message inspection. Use TYPO.
    GitOrigin-RevId: 7219d8abdc32d1a3c5bb043658758911c6033f8d

--- grazi/src/main/kotlin/tanvd/grazi/ide/GraziCommitInspection.kt
+++ grazi/src/main/kotlin/tanvd/grazi/ide/GraziCommitInspection.kt
@@ -1,5 +1,6 @@
 package tanvd.grazi.ide
 
+import com.intellij.codeHighlighting.HighlightDisplayLevel
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.PsiElementVisitor
@@ -10,6 +11,8 @@ class GraziCommitInspection : BaseCommitMessageInspection() {
     companion object {
         val graziInspection: LocalInspectionTool by lazy { GraziInspection() }
     }
+
+    override fun getDefaultLevel(): HighlightDisplayLevel = HighlightDisplayLevel.find("TYPO") ?: HighlightDisplayLevel.WARNING
 
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return graziInspection.buildVisitor(holder, isOnTheFly)
