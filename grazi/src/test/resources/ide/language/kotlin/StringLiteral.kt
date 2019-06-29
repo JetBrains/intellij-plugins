c332d1dbaf43a454fa8c19b22f148d32a3f86708
Vladislav Tankov <vdtankov@gmail.com>
2019-06-29 23:45:56 +0300

    Add all the needed examples for string literal checks in kotlin. Fix templates broken in Kotlin
    GitOrigin-RevId: 5b9f934d91d2f2fb7df237465673b56a9be85e78

--- grazi/src/test/resources/ide/language/kotlin/StringLiteral.kt
+++ grazi/src/test/resources/ide/language/kotlin/StringLiteral.kt
@@ -1,4 +1,26 @@
 package ide.language.kotlin
 
-val a = "It is <warning>friend</warning>"
+object OneLine {
+    val oneTypo = "It is <warning>friend</warning> of human"
+    val fewTypos = "It <warning>are</warning> working for <warning>much</warning> warnings"
+    val ignoreTemplate = "It is ${1} friend"
+    val notIgnoreOtherMistakes = "It is <warning>friend</warning>. <warning>But</warning> I have a ${1} here"
+}
+
+object MultiLine {
+    val oneTypo = """It is <warning>friend</warning> of human"""
+    val fewTypos = """It <warning>are</warning> working for <warning>much</warning> warnings"""
+    val ignoreTemplate = """It is ${1} friend"""
+    val notIgnoreOtherMistakes = """It is <warning>friend</warning>. <warning>But</warning> I have a ${1} here"""
+}
+
+object InFunc {
+    fun a(b: String) {
+        a("It is <warning>friend</warning> of human")
+        a("It <warning>are</warning> working for <warning>much</warning> warnings")
+        a("It is ${1} friend")
+        a("It is <warning>friend</warning>. <warning>But</warning> I have a ${1} here")
+    }
+}
+
 
