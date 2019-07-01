a6a1b1f55edb2bcdc3512971c060510ba6db630c
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-01 19:26:42 +0300

    Add TODO to member names in JS
    GitOrigin-RevId: c98908823ae5e065d587f1b773765712597c26c2

--- grazi/src/test/resources/ide/language/js/Constructs.js
+++ grazi/src/test/resources/ide/language/js/Constructs.js
@@ -4,7 +4,7 @@ var some_<warning>constnt</warning> = "1";
 class Some<warning>Classs</warning> {
 
     constructor(config) {
-        this.member_varibleleel = config;
+        this.member_varibleleel = config; // TODO Check member names
     }
 
     some_<warning>metod</warning>() {
