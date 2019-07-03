ee0dd4d9522a58b246acbd04098bb67fbf88d20e
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-07-03 17:35:39 +0200

    IDEA-CR-49531: WEB-39743 Vue.js: directives are not present in code completion on Vue.js tags
    (cherry picked from commit 98fd82efb10ce6cd146e5340bacb302d1c7d8163)
    
    GitOrigin-RevId: 1790da45769f367322fbd7c99c0d3dbb584e1e40

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueCompletionTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueCompletionTest.kt
@@ -1501,6 +1501,24 @@ $script""")
                    .sorted())
   }
 
+  fun testDirectiveCompletionOnComponent() {
+    myFixture.copyDirectoryToProject("../libs/vuetify/vuetify_137/node_modules", "./node_modules")
+    myFixture.configureByText("a-component.vue", """
+      <template>
+        <v-list>
+            <v-list-tile
+                    v-for="color in ['primary', 'secondary', 'info', 'success', 'warning', 'error']"
+                    :key="color"
+                    v<caret>
+            >
+                <v-list-tile-title>Item with "{{ color }}" class</v-list-tile-title>
+            </v-list-tile>
+        </v-list>
+      </template>
+    """)
+    myFixture.completeBasic()
+    assertContainsElements(myFixture.lookupElementStrings!!, "v-ripple", "v-resize", "v-scroll")
+  }
 
   private fun assertDoesntContainVueLifecycleHooks() {
     myFixture.completeBasic()
