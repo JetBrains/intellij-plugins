f75462d4b93122dcb8d3218e0f08a8dabee2337c
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-03 13:23:15 +0300

    Fix JSON test
    GitOrigin-RevId: 1a320f8e731fd7d049a86d34b52618058db3a88f

--- grazi/plugin/src/test/kotlin/tanvd/grazi/ide/language/JSONSupportTest.kt
+++ grazi/plugin/src/test/kotlin/tanvd/grazi/ide/language/JSONSupportTest.kt
@@ -5,6 +5,7 @@ import tanvd.grazi.GraziTestBase
 
 class JSONSupportTest : GraziTestBase(true) {
     fun `test grammar check in file`() {
+        // TODO remove jsonSchemas in resources
         runHighlightTestForFile("ide/language/json/Example.json")
     }
 }
