66e6dade23a0b239034ec2d2e2eae4d6a3720866
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-03 17:31:41 +0300

    Switching to a newer version
    GitOrigin-RevId: c9ed715c39b664715137230e1ff7deeb2fa0dacf

--- grazi/build.gradle.kts
+++ grazi/build.gradle.kts
@@ -1,17 +1,18 @@
 import org.jetbrains.intellij.tasks.PublishTask
+import org.jetbrains.intellij.tasks.RunIdeTask
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 import tanvd.grazi.Versions
 import tanvd.grazi.channel
 
 group = "tanvd.grazi"
-version = "2019.2-2.$channel"
+version = "2019.2-3.4.$channel"
 
 
 plugins {
-    id("tanvd.kosogor") version "1.0.5" apply true
+    id("tanvd.kosogor") version "1.0.6" apply true
     id("io.gitlab.arturbosch.detekt") version ("1.0.0-RC14") apply true
-    id("org.jetbrains.intellij") version "0.4.5" apply true
-    kotlin("jvm") version "1.3.31" apply true
+    id("org.jetbrains.intellij") version "0.4.9" apply true
+    kotlin("jvm") version "1.3.40" apply true
 }
 
 repositories {
@@ -21,15 +22,20 @@ repositories {
 
 intellij {
     pluginName = "Grazi"
-    version = "IC-192.4488-EAP-CANDIDATE-SNAPSHOT"
+    version = "IC-192.5438-EAP-CANDIDATE-SNAPSHOT"
     downloadSources = true
+    type = "IU"
 
     updateSinceUntilBuild = false
 
     setPlugins(
-            "org.intellij.plugins.markdown:192.4488.21",
-            "org.jetbrains.kotlin:1.3.31-release-IJ2019.1-1",
-            "PythonCore:2019.2.192.4488.21"
+            "markdown",
+            "PythonCore:2019.2.192.5438.14",
+            "org.rust.lang:0.2.99.2127-192",
+            "CSS",
+            "JavaScriptLanguage",
+            "Kotlin",
+            "java"
     )
 }
 
@@ -39,6 +45,10 @@ tasks.withType<KotlinJvmCompile> {
         languageVersion = "1.3"
         apiVersion = "1.3"
     }
+}
+
+tasks.withType<RunIdeTask> {
+    jvmArgs("-Xmx1g")
 }
 
 tasks.withType<PublishTask> {
@@ -65,7 +75,6 @@ val langs = setOf("en", "ru", "fr", "de", "pl", "it", "zh", "ja", "uk", "el", "r
 
 dependencies {
     compileOnly(kotlin("stdlib"))
-    compileOnly(kotlin("reflect"))
 
     compile("org.languagetool", "languagetool-core", Versions.languageTool) {
         exclude("org.slf4j", "slf4j-api")
@@ -78,14 +87,11 @@ dependencies {
 
     compile("org.apache.commons", "commons-lang3", "3.5")
 
-    compile("com.beust", "klaxon", "5.0.1")
-
-    testCompile("org.junit.jupiter", "junit-jupiter-api", "5.2.0")
-    testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.2.0")
+    compile("tanvd.kex", "kex", "0.1.1")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+    useJUnit()
 
     testLogging {
         events("passed", "skipped", "failed")
