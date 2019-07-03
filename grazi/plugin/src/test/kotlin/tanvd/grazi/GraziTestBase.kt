dd398335488c4c314c3ede6741e79a58cfadf041
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-03 12:58:57 +0300

    Fix markdown test
    GitOrigin-RevId: c8a1d4f01cbb7a024ba1ebe57f389d58ac1fbb2e

--- grazi/plugin/src/test/kotlin/tanvd/grazi/GraziTestBase.kt
+++ grazi/plugin/src/test/kotlin/tanvd/grazi/GraziTestBase.kt
@@ -1,6 +1,5 @@
 package tanvd.grazi
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.projectRoots.JavaSdk
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.pom.java.LanguageLevel
@@ -8,41 +7,45 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiPlainText
 import com.intellij.testFramework.LightProjectDescriptor
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl
+import org.junit.Before
 import tanvd.grazi.ide.GraziInspection
-import tanvd.grazi.ide.msg.GraziStateLifecycle
-import tanvd.grazi.language.Lang
 import tanvd.grazi.utils.filterFor
 import java.io.File
 
 abstract class GraziTestBase(private val withSpellcheck: Boolean) : LightCodeInsightFixtureTestCase() {
-
     override fun getTestDataPath(): String {
         return File("src/test/resources").canonicalPath
     }
 
+    @Before
+    fun beforeEach() {
+        GraziPlugin.invalidateCaches()
+    }
+
     override fun setUp() {
+        GraziPlugin.isTest = true
+
         super.setUp()
         myFixture.enableInspections(*inspectionTools)
 
-        GraziConfig.update { it.copy(enabledSpellcheck = withSpellcheck) }
-
-        while (ApplicationManager.getApplication().messageBus.hasUndeliveredEvents(GraziStateLifecycle.topic)) {
-            Thread.sleep(500)
-        }
-
-        GraziConfig.update { it.copy(enabledLanguages = it.enabledLanguages + Lang.RUSSIAN) }
+        GraziConfig.state.enabledSpellcheck = withSpellcheck
     }
 
     override fun getProjectDescriptor(): LightProjectDescriptor {
         return object : ProjectDescriptor(LanguageLevel.JDK_1_8) {
             override fun getSdk(): Sdk? {
-                return JavaSdk.getInstance().createJdk("jdk8", System.getenv("JAVA_HOME") ?: "/usr/lib/jvm/java-8-openjdk-amd64", false)
+                return JavaSdk.getInstance().createJdk("jdk8", System.getenv("JAVA_HOME"), false)
             }
         }
     }
 
     protected fun runHighlightTestForFile(file: String) {
         myFixture.configureByFile(file)
+
+        // Markdown changed PSI during highlighting
+        (myFixture as? CodeInsightTestFixtureImpl)?.canChangeDocumentDuringHighlighting(true)
+
         myFixture.testHighlighting(true, false, false, file)
     }
 
