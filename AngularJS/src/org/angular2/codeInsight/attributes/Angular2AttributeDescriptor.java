90538eff2a8a0242a28c195ee54ff08e499e0adb
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-07-05 10:14:47 +0200

    IDEA-CR-49643: WEB-39722 Angular: add hack for Ionic attributes - allow both types of attributes
    (cherry picked from commit 38e65b0725eaa5a75acb031d3ef7c93a1b768edf)
    
    GitOrigin-RevId: cf24a95920c6c15cdb071fce91640885ca0e4716

--- AngularJS/src/org/angular2/codeInsight/attributes/Angular2AttributeDescriptor.java
+++ AngularJS/src/org/angular2/codeInsight/attributes/Angular2AttributeDescriptor.java
@@ -525,7 +525,6 @@ public class Angular2AttributeDescriptor extends BasicXmlAttributeDescriptor imp
     private final Predicate<String> myShouldIncludeOneTimeBinding;
     private final XmlTag myTag;
     private List<Angular2AttributeDescriptor> myResult;
-    private Function<String, String> myOneTimeBindingNameHack;
 
     DirectiveAttributesProvider(@NotNull XmlTag tag,
                                 @NotNull Angular2Directive directive,
@@ -533,7 +532,6 @@ public class Angular2AttributeDescriptor extends BasicXmlAttributeDescriptor imp
       myTag = tag;
       myDirective = directive;
       myShouldIncludeOneTimeBinding = shouldIncludeOneTimeBinding;
-      myOneTimeBindingNameHack = Angular2LibrariesHacks.hackIonicComponentAttributeNames(directive);
     }
 
     public List<Angular2AttributeDescriptor> get() {
@@ -543,8 +541,12 @@ public class Angular2AttributeDescriptor extends BasicXmlAttributeDescriptor imp
       collectDirectiveDescriptors(myDirective.getOutputs(), this::createEventHandler);
       collectDirectiveDescriptors(myDirective.getInputs(), this::createOneTimeBinding);
       collectDirectiveDescriptors(myDirective.getAttributes(), this::createAttributeBinding);
+      //noinspection unchecked
+      Optional.ofNullable(Angular2LibrariesHacks.hackIonicComponentAttributeNames(myDirective, this::createOneTimeBinding))
+        .ifPresent(creator -> collectDirectiveDescriptors((Collection<Angular2DirectiveProperty>)myDirective.getInputs(), creator));
       return myResult;
     }
+
 
     private <T> void collectDirectiveDescriptors(
       @NotNull Collection<T> list,
@@ -572,7 +574,11 @@ public class Angular2AttributeDescriptor extends BasicXmlAttributeDescriptor imp
 
     @Nullable
     private Angular2AttributeDescriptor createOneTimeBinding(@NotNull Angular2DirectiveProperty info) {
-      String attributeName = myOneTimeBindingNameHack.apply(info.getName());
+      return createOneTimeBinding(info, info.getName());
+    }
+
+    @Nullable
+    private Angular2AttributeDescriptor createOneTimeBinding(@NotNull Angular2DirectiveProperty info, String attributeName) {
       return myShouldIncludeOneTimeBinding.test(attributeName)
              && isOneTimeBindingProperty(info)
              ? new Angular2AttributeDescriptor(myTag, attributeName, AttributePriority.HIGH,
