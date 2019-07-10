914262a0ee1487b244204241095e10f237e3c68a
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-07-10 20:58:02 +0200

    WEB-39865 Vue.js: add Vue formatter and revert previous changes to HTMLFormattingModelBuilder
    GitOrigin-RevId: 76721b4bc0585abce133588e3b4c242260fc56bb

---
+++ vuejs/src/org/jetbrains/vuejs/lang/html/psi/formatter/VueSyntheticBlock.kt
@@ -0,0 +1,14 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.lang.html.psi.formatter
+
+import com.intellij.formatting.Block
+import com.intellij.formatting.Indent
+import com.intellij.psi.formatter.xml.SyntheticBlock
+import com.intellij.psi.formatter.xml.XmlFormattingPolicy
+
+class VueSyntheticBlock(subBlocks: List<Block>,
+                        parent: Block,
+                        indent: Indent?,
+                        policy: XmlFormattingPolicy,
+                        childIndent: Indent?)
+  : SyntheticBlock(subBlocks, parent, indent, policy, childIndent)
