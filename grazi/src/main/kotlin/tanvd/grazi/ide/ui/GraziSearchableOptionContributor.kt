6a784adf03af9ed7cabd20818fed94dc4f8cc5da
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-10 14:21:41 +0300

    Add global preference search
    GitOrigin-RevId: 5dc1a911dbf79d96d27749ae2fffad5006017a41

---
+++ grazi/src/main/kotlin/tanvd/grazi/ide/ui/GraziSearchableOptionContributor.kt
@@ -0,0 +1,26 @@
+package tanvd.grazi.ide.ui
+
+import com.intellij.ide.ui.search.SearchableOptionContributor
+import com.intellij.ide.ui.search.SearchableOptionProcessor
+import tanvd.grazi.language.Lang
+
+class GraziSearchableOptionContributor : SearchableOptionContributor() {
+    private val id = "reference.settingsdialog.project.grazi"
+    private val displayName = "Grazi"
+
+    private fun SearchableOptionProcessor.addOptions(text: String, path: String, hit: String = text) {
+        addOptions(text, path, hit, id, displayName, true)
+    }
+
+    override fun processOptions(processor: SearchableOptionProcessor) {
+        with (processor) {
+            addOptions(msg("grazi.ui.settings.enable.text"), msg("grazi.ui.settings.config.text"))
+            addOptions(msg("grazi.ui.settings.rules.configuration.text"), msg("grazi.ui.settings.config.text"))
+            addOptions(msg("grazi.ui.settings.languages.native.text"), msg("grazi.ui.settings.languages.text"))
+
+            Lang.values().forEach { lang ->
+                addOptions(lang.displayName, msg("grazi.ui.settings.languages.text"))
+            }
+        }
+    }
+}
