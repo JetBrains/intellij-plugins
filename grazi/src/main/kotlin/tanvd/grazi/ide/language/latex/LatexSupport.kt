1d7e7b13136a58746256bb2d68de80d634aefeb8
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-10 17:07:20 +0300

    Fix tests
    GitOrigin-RevId: 5eb2dc6045ae016bcd66db0b08276dc7be23fb44

--- grazi/src/main/kotlin/tanvd/grazi/ide/language/latex/LatexSupport.kt
+++ grazi/src/main/kotlin/tanvd/grazi/ide/language/latex/LatexSupport.kt
@@ -3,22 +3,20 @@ package tanvd.grazi.ide.language.latex
 import com.intellij.psi.PsiElement
 import nl.hannahsten.texifyidea.psi.LatexMathEnvironment
 import nl.hannahsten.texifyidea.psi.LatexNormalText
+import nl.hannahsten.texifyidea.psi.LatexTypes.*
 import org.jetbrains.kotlin.psi.psiUtil.parents
 import tanvd.grazi.grammar.GrammarChecker
 import tanvd.grazi.grammar.Typo
 import tanvd.grazi.ide.language.LanguageSupport
+import tanvd.grazi.utils.filterForTokens
 
 class LatexSupport : LanguageSupport() {
-    companion object {
-        private val tagsIgnoredCategories = listOf(Typo.Category.CASING)
-    }
-
     override fun isRelevant(element: PsiElement): Boolean {
         return element is LatexNormalText && element.parents.none { it is LatexMathEnvironment }
     }
 
     override fun check(element: PsiElement): Set<Typo> {
         require(element is LatexNormalText) { "Got non LatexNormalText in LatexSupport" }
-        return GrammarChecker.default.check(element).filterNot { it.info.category in tagsIgnoredCategories }.toSet()
+        return GrammarChecker.default.check(element.filterForTokens<PsiElement>(NORMAL_TEXT_WORD)).toSet()
     }
 }
