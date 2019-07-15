4ed5a5edb34fc720e9782ec012e1ccc5d50c5a6b
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-07-15 09:56:10 +0200

    notify macOS Mojave users about Find Action workaround during corresponding lesson
    GitOrigin-RevId: 5d072fd79b9749839389be0000148c499b282459

--- ide-features-trainer/src/training/learn/lesson/general/GotoActionLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/general/GotoActionLesson.kt
@@ -3,6 +3,7 @@ package training.learn.lesson.general
 import com.intellij.ide.actions.searcheverywhere.SearchEverywhereUI
 import com.intellij.openapi.editor.ex.EditorSettingsExternalizable
 import com.intellij.openapi.editor.impl.EditorComponentImpl
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.testGuiFramework.framework.GuiTestUtil
 import com.intellij.testGuiFramework.util.Key
 import com.intellij.ui.components.fields.ExtendableTextField
@@ -15,12 +16,19 @@ import javax.swing.JPanel
 
 class GotoActionLesson(module: Module, lang: String, private val sample: LessonSample) :
     KLesson("Actions", module, lang) {
+
+  companion object {
+    private const val FIND_ACTION_WORKAROUND: String = "https://intellij-support.jetbrains.com/hc/en-us/articles/360005137400-Cmd-Shift-A-hotkey-opens-Terminal-with-apropos-search-instead-of-the-Find-Action-dialog"
+  }
+
   override val lessonContent: LessonContext.() -> Unit
     get() = {
       prepareSample(sample)
       actionTask("GotoAction") {
         "One of the most useful shortcuts is Find Action. It allows you to search through all available actions " +
-            "without having to know their individual shortcuts. Try it now with ${action(it)}."
+            "without having to know their individual shortcuts. Try it now with ${action(it)}." +
+                if (SystemInfo.isMacOSMojave) " (If Terminal search opens instead of Find Action " +
+                        "please read <a href=\"$FIND_ACTION_WORKAROUND\">this article</a>.)" else ""
       }
       actionTask("About") {
         "Let's say you want to read about the IDE, type <strong>about</strong> and press Enter."
