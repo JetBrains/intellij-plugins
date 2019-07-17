285f56a68bd248703bbc1e26781181f2f202db0a
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-07-17 14:01:37 +0300

    Small fixes
    GitOrigin-RevId: 6774c9eb0aa217135b1f3c8e8564cad7c541bb6c

--- grazi/src/main/kotlin/tanvd/grazi/grammar/LangToolFixes.kt
+++ grazi/src/main/kotlin/tanvd/grazi/grammar/LangToolFixes.kt
@@ -3,7 +3,8 @@ package tanvd.grazi.grammar
 import org.languagetool.rules.Rule
 
 enum class LangToolFixes(private val ruleId: String, private val fix: (suggestion: String) -> String) {
-    ARTICLE_MISSING("ARTICLE_MISSING", { suggestion ->
+    ARTICLE_MISSING("ARTICLE_MISSING", {
+        val suggestion = it.replace("\\s+".toRegex()," ")
         when {
             suggestion.startsWith("The") -> "The " + suggestion[4].toLowerCase() + suggestion.substring(5)
             suggestion.startsWith("A") -> "A " + suggestion[2].toLowerCase() + suggestion.substring(3)
@@ -12,13 +13,8 @@ enum class LangToolFixes(private val ruleId: String, private val fix: (suggestio
     });
 
     companion object {
-        fun fixSuggestion(ruleId: String, suggestion: String): String {
-            return values().find { it.ruleId == ruleId }?.let { it.fix(suggestion) } ?: suggestion
-        }
-
-        fun fixSuggestion(rule: Rule, suggestion: String): String {
-            return fixSuggestion(rule.id, suggestion)
-        }
+        fun fixSuggestion(ruleId: String, suggestion: String) = values().find { it.ruleId == ruleId }?.let { it.fix(suggestion) } ?: suggestion
+        fun fixSuggestion(rule: Rule, suggestion: String) = fixSuggestion(rule.id, suggestion)
     }
 }
 
