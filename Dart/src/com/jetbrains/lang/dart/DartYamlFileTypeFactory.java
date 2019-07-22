8e58373af0ada0d8e5c65918a950d43eebbfe8d6
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-07-22 20:07:39 +0300

    remove DartFileTypeFactory, use new way of file type registration
    (cherry picked from commit 7331767dba8b5f0d7d7afa0da4559f14ecd50869)
    
    GitOrigin-RevId: cbd9a50918ec374bb088fe8f2854a46baf0bb60f

--- Dart/src/com/jetbrains/lang/dart/DartYamlFileTypeFactory.java
+++ Dart/src/com/jetbrains/lang/dart/DartYamlFileTypeFactory.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart;
 
 import com.intellij.openapi.fileTypes.ExactFileNameMatcher;
@@ -6,6 +7,7 @@ import com.intellij.openapi.fileTypes.FileTypeConsumer;
 import com.intellij.openapi.fileTypes.FileTypeFactory;
 import org.jetbrains.annotations.NotNull;
 
+// TODO: switch to <fileType .../> registration when YAMLFileTypeLoader is removed and changed to <fileType .../> registration (at the moment of writing it would break 3rd party plugin)
 public class DartYamlFileTypeFactory extends FileTypeFactory {
 
   public static final String DOT_ANALYSIS_OPTIONS = ".analysis_options";
