cfd536c3fb07c48586e67cabf411fbd93c9cc031
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-07-23 13:30:15 +0300

    remove DartYamlFileTypeFactory, use new way of file type registration
    GitOrigin-RevId: 7c7b1a91a81e86e6264aca7086958b367c352e5b

--- Dart/src/com/jetbrains/lang/dart/ide/completion/DartServerCompletionContributor.java
+++ Dart/src/com/jetbrains/lang/dart/ide/completion/DartServerCompletionContributor.java
@@ -37,7 +37,6 @@ import com.intellij.ui.LayeredIcon;
 import com.intellij.util.PlatformIcons;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.lang.dart.DartLanguage;
-import com.jetbrains.lang.dart.DartYamlFileTypeFactory;
 import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
 import com.jetbrains.lang.dart.assists.AssistUtils;
 import com.jetbrains.lang.dart.assists.DartSourceEditException;
@@ -63,7 +62,7 @@ public class DartServerCompletionContributor extends CompletionContributor {
     extend(CompletionType.BASIC,
            or(psiElement().withLanguage(DartLanguage.INSTANCE),
               psiElement().inFile(psiFile().withLanguage(HTMLLanguage.INSTANCE)),
-              psiElement().inFile(psiFile().withName(DartYamlFileTypeFactory.DOT_ANALYSIS_OPTIONS))),
+              psiElement().inFile(psiFile().withName(".analysis_options"))),
            new CompletionProvider<CompletionParameters>() {
              @Override
              protected void addCompletions(@NotNull final CompletionParameters parameters,
@@ -431,7 +430,8 @@ public class DartServerCompletionContributor extends CompletionContributor {
           icon = IconManager.getInstance().createRowIcon(icon, AllIcons.Gutter.OverridingMethod);
         }
         else {
-          icon = IconManager.getInstance().createRowIcon(icon, element.isPrivate() ? PlatformIcons.PRIVATE_ICON : PlatformIcons.PUBLIC_ICON);
+          icon = IconManager.getInstance().createRowIcon(icon, element.isPrivate() ? PlatformIcons.PRIVATE_ICON
+                                                                                   : PlatformIcons.PUBLIC_ICON);
           icon = applyOverlay(icon, element.isFinal(), AllIcons.Nodes.FinalMark);
           icon = applyOverlay(icon, element.isConst(), AllIcons.Nodes.FinalMark);
         }
