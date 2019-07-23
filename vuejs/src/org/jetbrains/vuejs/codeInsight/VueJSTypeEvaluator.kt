cb884a4b033a3973985eebf114f6fd8f02b91dde
Anton Lobov <Anton.Lobov@jetbrains.com>
2019-07-23 16:20:01 +0200

    IDEA-CR-50473: vue js: proper type evaluation for destructuring patterns in v-for
    was not evaluated properly for nested patterns, as requires special handling
    
    (cherry picked from commit 24a44af327f919d42b1a793a839927d62514e775)
    
    GitOrigin-RevId: 87a98864a10f3b25df1f1dd1ddeb868f638d994b

---
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSTypeEvaluator.kt
@@ -0,0 +1,31 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.codeInsight
+
+import com.intellij.lang.javascript.psi.JSFieldVariable
+import com.intellij.lang.javascript.psi.resolve.JSEvaluateContext
+import com.intellij.lang.javascript.psi.resolve.JSTypeEvaluationHelper
+import com.intellij.lang.javascript.psi.resolve.JSTypeEvaluator
+import com.intellij.lang.javascript.psi.resolve.JSTypeProcessor
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.vuejs.lang.expr.VueVForExpression
+import org.jetbrains.vuejs.lang.expr.VueVForVariable
+
+class VueJSTypeEvaluator(context: JSEvaluateContext, processor: JSTypeProcessor, helper: JSTypeEvaluationHelper)
+  : JSTypeEvaluator(context, processor, helper) {
+  override fun addTypeFromVariableResolveResult(jsVariable: JSFieldVariable) {
+    if (jsVariable is VueVForVariable) {
+      val vForExpression = PsiTreeUtil.getParentOfType(jsVariable, VueVForExpression::class.java)
+      if (vForExpression != null) {
+        pushDestructuringContext(jsVariable)
+        val expression = myContext.processedExpression
+        val types = getComponentTypeFromArrayExpression(expression, vForExpression.getCollectionExpression())
+        for (type in types) {
+          addType(type, expression)
+        }
+        restoreEvaluationContextApplingElementsSize(myContext.jsElementsToApply.size)
+        return
+      }
+    }
+    super.addTypeFromVariableResolveResult(jsVariable)
+  }
+}
