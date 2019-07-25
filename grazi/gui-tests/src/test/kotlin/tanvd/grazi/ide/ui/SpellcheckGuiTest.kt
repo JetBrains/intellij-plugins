b436b426a2e4e30ce9ae7e511d7ceb5adcde66b0
Vladislav Tankov <vdtankov@gmail.com>
2019-07-26 01:57:37 +0300

    Fixes to CircleCI config and uncomment UI tests
    GitOrigin-RevId: 169fb12325b6192084bbf595c14e36f6b64cdf22

--- grazi/gui-tests/src/test/kotlin/tanvd/grazi/ide/ui/SpellcheckGuiTest.kt
+++ grazi/gui-tests/src/test/kotlin/tanvd/grazi/ide/ui/SpellcheckGuiTest.kt
@@ -1,87 +1,87 @@
-//package tanvd.grazi.ide.ui
-//
-//import com.intellij.lang.annotation.HighlightSeverity
-//import com.intellij.testGuiFramework.framework.RunWithIde
-//import com.intellij.testGuiFramework.impl.*
-//import com.intellij.testGuiFramework.launcher.ide.CommunityIde
-//import org.junit.Test
-//import tanvd.grazi.GraziGuiTestCase
-//
-//
-//@RunWithIde(CommunityIde::class)
-//class SpellcheckGuiTest : GraziGuiTestCase() {
-//    @Test
-//    fun `test spellcheck checkbox`() {
-//        simpleProject {
-//            waitAMoment()
-//
-//            ideFrame {
-//                projectView {
-//                    path("SimpleProject", "src", "Main.kt").doubleClick()
-//                }
-//
-//                editor {
-//                    moveToLine(1)
-//                    typeText("// text with eror")
-//                    waitAMoment()
-//
-//                    requireHighlights(HighlightSeverity.INFORMATION, "Typo: In word 'eror'")
-//                }
-//            }
-//
-//            settings {
-//                with(checkbox("Enable Grazi spellchecker")) {
-//                    assert(!isSelected)
-//                    click()
-//                }
-//            }
-//
-//            ideFrame {
-//                editor {
-//                    waitAMoment()
-//                    requireHighlights(HighlightSeverity.INFORMATION, "Possible spelling mistake")
-//                }
-//            }
-//
-//            ideFrame {
-//                invokeMainMenu("Start.Use.Vcs")
-//                dialog("Enable Version Control Integration") {
-//                    combobox("Select a version control system to associate with the project root:").selectItem("Git")
-//                    button("OK").click()
-//                }
-//
-//                invokeMainMenu("CheckinProject")
-//                dialog("Commit Changes") {
-//                    editor {
-//                        moveToLine(1)
-//                        typeText("text with eror")
-//                        waitAMoment()
-//
-//                        requireHighlights(HighlightSeverity.INFORMATION, "Possible spelling mistake")
-//                    }
-//
-//                    button("Cancel").click()
-//                }
-//            }
-//
-//            settings {
-//                with(checkbox("Enable Grazi spellchecker")) {
-//                    assert(isSelected)
-//                    click()
-//                }
-//            }
-//
-//            waitAMoment()
-//            ideFrame {
-//                invokeMainMenu("CheckinProject")
-//                dialog("Commit Changes") {
-//                    editor {
-//                        requireHighlights(HighlightSeverity.INFORMATION, "Typo: In word 'eror'")
-//                    }
-//
-//                    button("Cancel").click()
-//                }
-//            }
-//        }
-//    }
-//}
+package tanvd.grazi.ide.ui
+
+import com.intellij.lang.annotation.HighlightSeverity
+import com.intellij.testGuiFramework.framework.RunWithIde
+import com.intellij.testGuiFramework.impl.*
+import com.intellij.testGuiFramework.launcher.ide.CommunityIde
+import org.junit.Test
+import tanvd.grazi.GraziGuiTestBase
+
+
+@RunWithIde(CommunityIde::class)
+class SpellcheckGuiTest : GraziGuiTestBase() {
+    @Test
+    fun `test spellcheck checkbox`() {
+        simpleProject {
+            waitAMoment()
+
+            ideFrame {
+                projectView {
+                    path("SimpleProject", "src", "Main.kt").doubleClick()
+                }
+
+                editor {
+                    moveToLine(1)
+                    typeText("// text with eror")
+                    waitAMoment()
+
+                    requireHighlights(HighlightSeverity.INFORMATION, "Typo: In word 'eror'")
+                }
+            }
+
+            settings {
+                with(checkbox("Enable Grazi spellchecker")) {
+                    assert(!isSelected)
+                    click()
+                }
+            }
+
+            ideFrame {
+                editor {
+                    waitAMoment()
+                    requireHighlights(HighlightSeverity.INFORMATION, "Possible spelling mistake")
+                }
+            }
+
+            ideFrame {
+                invokeMainMenu("Start.Use.Vcs")
+                dialog("Enable Version Control Integration") {
+                    combobox("Select a version control system to associate with the project root:").selectItem("Git")
+                    button("OK").click()
+                }
+
+                invokeMainMenu("CheckinProject")
+                dialog("Commit Changes") {
+                    editor {
+                        moveToLine(1)
+                        typeText("text with eror")
+                        waitAMoment()
+
+                        requireHighlights(HighlightSeverity.INFORMATION, "Possible spelling mistake")
+                    }
+
+                    button("Cancel").click()
+                }
+            }
+
+            settings {
+                with(checkbox("Enable Grazi spellchecker")) {
+                    assert(isSelected)
+                    click()
+                }
+            }
+
+            waitAMoment()
+            ideFrame {
+                invokeMainMenu("CheckinProject")
+                dialog("Commit Changes") {
+                    editor {
+                        requireHighlights(HighlightSeverity.INFORMATION, "Typo: In word 'eror'")
+                    }
+
+                    button("Cancel").click()
+                }
+            }
+        }
+    }
+}
