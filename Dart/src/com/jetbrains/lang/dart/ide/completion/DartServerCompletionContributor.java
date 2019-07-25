1b1d71127175990348b266e39ade2bea4cb885c7
Ari Aye <ariaye@google.com>
2019-07-25 10:15:20 -0700

    Add unit test case

--- Dart/src/com/jetbrains/lang/dart/ide/completion/DartServerCompletionContributor.java
+++ Dart/src/com/jetbrains/lang/dart/ide/completion/DartServerCompletionContributor.java
@@ -471,10 +471,7 @@ public class DartServerCompletionContributor extends CompletionContributor {
         }
 
         if (element != null &&
-            (element.getKind().equals(ElementKind.FUNCTION) ||
-             element.getKind().equals(ElementKind.FUNCTION_INVOCATION) ||
-             element.getKind().equals(ElementKind.CONSTRUCTOR) ||
-             element.getKind().equals(ElementKind.CONSTRUCTOR_INVOCATION)) &&
+            (element.getKind().equals(ElementKind.CONSTRUCTOR)|| element.getKind().equals(ElementKind.FUNCTION)) &&
             suggestion.getParameterNames() != null) {
           handleFunctionInvocationInsertion(context, item, suggestion);
         }
