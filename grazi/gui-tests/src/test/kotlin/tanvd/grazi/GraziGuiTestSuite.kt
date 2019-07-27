425ce606bc2f947d9b971b28107fd9e553929212
Vladislav Tankov <vdtankov@gmail.com>
2019-07-28 02:50:51 +0300

    Enable back all tests
    GitOrigin-RevId: 85920c7f16bd35f67c7abca59414e26d5859c94c

--- grazi/gui-tests/src/test/kotlin/tanvd/grazi/GraziGuiTestSuite.kt
+++ grazi/gui-tests/src/test/kotlin/tanvd/grazi/GraziGuiTestSuite.kt
@@ -10,5 +10,5 @@ import tanvd.grazi.ide.ui.RulesTreeGuiTest
 import tanvd.grazi.ide.ui.SpellcheckGuiTest
 
 @RunWith(GuiTestSuiteRunner::class)
-@Suite.SuiteClasses(RulesTreeGuiTest::class, SpellcheckGuiTest::class)
+@Suite.SuiteClasses(RulesTreeGuiTest::class, SpellcheckGuiTest::class, LanguageListGuiTest::class, NativeLanguageGuiTest::class)
 class GraziGuiTestSuite : GuiTestSuite()
