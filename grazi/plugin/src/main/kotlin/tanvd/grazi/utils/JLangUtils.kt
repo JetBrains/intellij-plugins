01b3dcafe2e7ff2b77e163254e0f2c1a0f01e5f2
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-08-15 14:44:59 +0300

    Review fixes part 2
    GitOrigin-RevId: 902dde3b2e8c7310ae170c249dbde1805e16ab31

--- grazi/plugin/src/main/kotlin/tanvd/grazi/utils/JLangUtils.kt
+++ grazi/plugin/src/main/kotlin/tanvd/grazi/utils/JLangUtils.kt
@@ -2,6 +2,7 @@ package tanvd.grazi.utils
 
 import kotlinx.html.FlowOrPhrasingContent
 import kotlinx.html.strong
+import org.jetbrains.org.objectweb.asm.*
 import org.languagetool.Language
 import org.languagetool.Languages
 import org.languagetool.rules.ExampleSentence
@@ -11,8 +12,8 @@ import org.languagetool.rules.RuleMatch
 import tanvd.grazi.GraziPlugin
 import tanvd.grazi.grammar.Typo
 import tanvd.grazi.language.Lang
-import java.lang.reflect.Field
-import java.lang.reflect.Modifier
+import java.io.File
+import java.io.InputStream
 import java.util.regex.Pattern
 
 fun Iterable<Typo>.spellcheckOnly(): Set<Typo> = filter { it.isSpellingTypo }.toSet()
@@ -57,36 +58,53 @@ fun FlowOrPhrasingContent.toCorrectHtml(example: IncorrectExample) {
     }
 }
 
+object Resources {
+    fun getFile(file: String): File = File(GraziPlugin::class.java.getResource(file).file)
+    fun getStream(file: String): InputStream = GraziPlugin::class.java.getResourceAsStream(file)
+}
+
 object LangToolInstrumentation {
-    fun enableLatinLettersInSpellchecker(lang: Lang) {
-        when (lang) {
-            Lang.RUSSIAN ->
-                Class.forName("org.languagetool.rules.ru.MorfologikRussianSpellerRule").getDeclaredField("RUSSIAN_LETTERS")
-            Lang.UKRAINIAN ->
-                Class.forName("org.languagetool.rules.uk.MorfologikUkrainianSpellerRule").getDeclaredField("UKRAINIAN_LETTERS")
-            else -> null
-        }?.let { pattern ->
-            pattern.isAccessible = true
-
-            val mods = Field::class.java.getDeclaredField("modifiers")
-            mods.isAccessible = true
-            mods.setInt(pattern, pattern.modifiers and Modifier.FINAL.inv())
-
-            pattern.set(null, Pattern.compile(".*"))
-        }
-    }
+    fun enableLatinLettersInSpellchecker(lang: Lang) = when (lang) {
+        Lang.RUSSIAN -> "org.languagetool.rules.ru.MorfologikRussianSpellerRule" to "RUSSIAN_LETTERS"
+        Lang.UKRAINIAN -> "org.languagetool.rules.uk.MorfologikUkrainianSpellerRule" to "UKRAINIAN_LETTERS"
+        else -> null
+    }?.let { (cls, field) -> Class.forName(cls).setFinalStaticField(field, Pattern.compile(".*")) }
 
     fun registerLanguage(lang: Lang) {
-        val dynLanguages = Languages::class.java.getDeclaredField("dynLanguages")
-        dynLanguages.isAccessible = true
-
-        @Suppress("UNCHECKED_CAST")
-        val langs = dynLanguages.get(null) as MutableList<Language>
+        val langs = Languages::class.java.getStaticField<MutableList<Language>>("dynLanguages")
 
         lang.descriptor.langsClasses.forEach { className ->
             val qualifiedName = "org.languagetool.language.$className"
             if (langs.all { it::class.java.canonicalName != qualifiedName }) {
                 langs.add(GraziPlugin.loadClass(qualifiedName)!!.newInstance() as Language)
+            }
+        }
+    }
+
+    fun reloadEnglish() {
+        val reader = ClassReader(Resources.getStream("/org/languagetool/language/English.class"))
+        val writer = ClassWriter(ClassWriter.COMPUTE_MAXS)
+        reader.accept(EnglishChunkerInstrumentation(writer), 0)
+
+        val cls = writer.toByteArray()
+        GraziPlugin.classLoader.defineClass("org.languagetool.language.English", cls, 0, cls.size)
+    }
+
+    private class EnglishChunkerInstrumentation(val classVisitor: ClassVisitor) : ClassVisitor(Opcodes.ASM5, classVisitor) {
+        override fun visitMethod(access: Int, name: String?, descriptor: String?, signature: String?, exceptions: Array<out String>?): MethodVisitor {
+            val methodVisitor = classVisitor.visitMethod(access, name, descriptor, signature, exceptions)
+            return if (name == "getChunker") GetChunkerMethodVisitor(methodVisitor) else methodVisitor
+        }
+
+        class GetChunkerMethodVisitor(private val visitor: MethodVisitor) : MethodVisitor(Opcodes.ASM5, null) {
+            override fun visitCode() {
+                with(visitor) {
+                    visitCode()
+                    visitInsn(Opcodes.ACONST_NULL)
+                    visitInsn(Opcodes.ARETURN)
+                    visitMaxs(0, 0)
+                    visitEnd()
+                }
             }
         }
     }
--- grazi/plugin/src/main/kotlin/tanvd/grazi/utils/ReflectionUtils.kt
+++ grazi/plugin/src/main/kotlin/tanvd/grazi/utils/ReflectionUtils.kt
@@ -1,10 +1,23 @@
 package tanvd.grazi.utils
 
-fun ClassLoader.defineClass(name: String, data: ByteArray, offset: Int, size: Int) : Class<*> =
+import java.lang.reflect.Field
+import java.lang.reflect.Modifier
+
+fun ClassLoader.defineClass(name: String, data: ByteArray, offset: Int, size: Int): Class<*> =
         with(ClassLoader::class.java.getDeclaredMethod("defineClass", String::class.java, ByteArray::class.java, Int::class.java, Int::class.java)) {
             isAccessible = true
-            invoke(this, name, data, offset, size) as Class<*>
+            invoke(this@defineClass, name, data, offset, size) as Class<*>
         }
 
 @Suppress("UNCHECKED_CAST")
 fun <Type> Class<*>.getStaticField(name: String): Type = this.getDeclaredField(name).apply { isAccessible = true }.get(null) as Type
+
+fun Class<*>.setFinalStaticField(name: String, value: Any?) = with(this.getDeclaredField(name)) {
+    isAccessible = true
+
+    val mods = Field::class.java.getDeclaredField("modifiers")
+    mods.isAccessible = true
+    mods.setInt(this, this.modifiers and Modifier.FINAL.inv())
+
+    this.set(null, value)
+}
