f2db641a3024e189fb87fc9e7ee8ad8854c669c7
Dmitry Jemerov <yole@jetbrains.com>
2019-08-21 11:06:15 +0200

    Register syntax highlighters directly in XML when possible (work in progress)
    GitOrigin-RevId: 3480dde4157a0132d7e294ce8e4a68706ed724b4

--- tapestry/core/src/com/intellij/tapestry/lang/TelLanguage.java
+++ tapestry/core/src/com/intellij/tapestry/lang/TelLanguage.java
@@ -2,10 +2,6 @@ package com.intellij.tapestry.lang;
 
 import com.intellij.lang.InjectableLanguage;
 import com.intellij.lang.Language;
-import com.intellij.openapi.fileTypes.SingleLazyInstanceSyntaxHighlighterFactory;
-import com.intellij.openapi.fileTypes.SyntaxHighlighter;
-import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Alexey Chmutov
@@ -16,14 +12,6 @@ public class TelLanguage extends Language implements InjectableLanguage {
 
   private TelLanguage() {
     super("TEL");
-
-    SyntaxHighlighterFactory.LANGUAGE_FACTORY.addExplicitExtension(this, new SingleLazyInstanceSyntaxHighlighterFactory() {
-      @Override
-      @NotNull
-      protected SyntaxHighlighter createHighlighter() {
-        return new TelHighlighter();
-      }
-    });
   }
 
   @Override
