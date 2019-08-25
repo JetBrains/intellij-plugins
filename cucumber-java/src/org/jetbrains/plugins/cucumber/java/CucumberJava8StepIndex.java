d345591205a54927692e4c457e26af95ac3b9c69
Andrey Vokin <andrey.vokin@jetbrains.com>
2019-08-25 14:11:02 +0200

    [Cucumber for Java] support new package io.cucumber
    IDEA-221320 Support step definition declared with method from `io.cucumber.java8` package
    
    (cherry picked from commit 449830f9fdbd647c5d776b6a47b4a31cb323dafa)
    
    GitOrigin-RevId: 54e5d19e9d4401261c1d010edd8887fc060631ae

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJava8StepIndex.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJava8StepIndex.java
@@ -23,71 +23,72 @@ import java.util.*;
 
 import static com.intellij.psi.impl.source.tree.JavaElementType.*;
 
-public class CucumberJava8StepIndex extends FileBasedIndexExtension<Boolean, List<Integer>> implements PsiDependentIndex {
+public class CucumberJava8StepIndex extends FileBasedIndexExtension<Boolean, List<Integer>> {
   public static final ID<Boolean, List<Integer>> INDEX_ID = ID.create("java.cucumber.java8.step");
   private static final String JAVA_8_PACKAGE = "cucumber.api.java8.";
+  private static final String JAVA_8_CUCUMBER_4_5_PACKAGE = "io.cucumber.java8.";
 
   private static final List<String> STEP_KEYWORDS = Arrays.asList("Әмма", "Нәтиҗәдә", "Вә", "Әйтик", "Һәм", "Ләкин", "Әгәр",  "Und",
                                                                   "Angenommen", "Gegeben seien",  "Dann", "Aber", "Wenn", "Gegeben sei",
-                                                                  "यदि", "तदा", "अगर", "और", "कदा", "परन्तु", "चूंकि", "जब", "किन्तु", "तथा", "पर", 
-                                                                  "तब", "Dados", "Entao", "Dada", "Então", "Mas", "Dadas", "Dado",  
-                                                                  "Quando", "E", "Bet", "Ir", "Tada",  "Kai", "Duota", "awer", "a", "an", 
-                                                                  "wann", "mä", "ugeholl", "dann", "I", "Kada", "Kad", "Zadan", "Ali", 
-                                                                  "Onda", "Zadano",  "Zadani", "Bet", "Kad", "Tad", "Ja", "Un",  "E", 
-                                                                  "Sipoze ke", "Sipoze", "Epi",  "Men", "Le sa a", "Le", "Ak", "Lè", 
-                                                                  "Sipoze Ke", "Lè sa a", "Ha", "Adott", "De", "Amikor", "És", "Majd", 
+                                                                  "यदि", "तदा", "अगर", "और", "कदा", "परन्तु", "चूंकि", "जब", "किन्तु", "तथा", "पर",
+                                                                  "तब", "Dados", "Entao", "Dada", "Então", "Mas", "Dadas", "Dado",
+                                                                  "Quando", "E", "Bet", "Ir", "Tada",  "Kai", "Duota", "awer", "a", "an",
+                                                                  "wann", "mä", "ugeholl", "dann", "I", "Kada", "Kad", "Zadan", "Ali",
+                                                                  "Onda", "Zadano",  "Zadani", "Bet", "Kad", "Tad", "Ja", "Un",  "E",
+                                                                  "Sipoze ke", "Sipoze", "Epi",  "Men", "Le sa a", "Le", "Ak", "Lè",
+                                                                  "Sipoze Ke", "Lè sa a", "Ha", "Adott", "De", "Amikor", "És", "Majd",
                                                                   "Akkor", "Amennyiben",  "并且", "而且", "假如", "同时", "当", "假设", "那么",
-                                                                  "假定",  "但是", "Нехай", "Якщо", "І", "Припустимо, що", "Дано", 
+                                                                  "假定",  "但是", "Нехай", "Якщо", "І", "Припустимо, що", "Дано",
                                                                   "Припустимо", "Коли", "Та", "Але", "То", "Тоді",  "А також",
                                                                   "It's just unbelievable", "Yeah nah",  "Too right",
                                                                   "But at the end of the day I reckon", "Y'know", "Maka",  "Tapi",
-                                                                  "Ketika", "Dengan", "Dan", "اگر", "تب", "اور", "جب", "بالفرض", 
+                                                                  "Ketika", "Dengan", "Dan", "اگر", "تب", "اور", "جب", "بالفرض",
                                                                   "فرض کیا", "پھر", "لیکن", "Maar",  "En", "Dan", "Wanneer", "Gegewe",
                                                                   "Бирок", "Аммо", "Унда",  "Ва", "Лекин", "Агар", "Δεδομένου", "Τότε",
-                                                                   "Και", "Αλλά", "Όταν", "Aye", "Let go and haul", "Gangway!", 
+                                                                   "Και", "Αλλά", "Όταν", "Aye", "Let go and haul", "Gangway!",
                                                                   "Avast!", "Blimey!", "When", "Then", "Given", "But",  "And", "Kaj",
                                                                    "Do", "Se", "Sed", "Donitaĵo", "Ef", "Þegar", "Þá",  "En", "Og",
-                                                                  "Quando", "E", "Allora", "Dato", "Dati", "Date", "Data", "Ma", 
-                                                                  "Cuando", "Dada", "Pero", "Entonces", "Dados", "Y", "Dadas", "Dado", 
-                                                                  "Kui",  "Ja", "Eeldades", "Kuid", "Siis", "اذاً", "لكن", "و", 
+                                                                  "Quando", "E", "Allora", "Dato", "Dati", "Date", "Data", "Ma",
+                                                                  "Cuando", "Dada", "Pero", "Entonces", "Dados", "Y", "Dadas", "Dado",
+                                                                  "Kui",  "Ja", "Eeldades", "Kuid", "Siis", "اذاً", "لكن", "و",
                                                                   "متى", "بفرض", "ثم", "عندما", "Thì", "Khi", "Biết", "Và", "Cho", "Nhưng",
                                                                    "もし", "かつ", "但し", "ただし", "しかし", "ならば",  "前提", "A",
-                                                                  "Anrhegedig a", "Pryd",  "Yna", "Ond", "هنگامی",  "با فرض", "آنگاه", 
-                                                                  "اما", "و", "Dat fiind", "Dati fiind", "Atunci", "Dați fiind", "Dar", 
-                                                                  "Si", "Când", "Daţi fiind", "Și",  "Cand", "Şi", "Date fiind", "Als",  
-                                                                  "Maar", "Gegeven", "En", "Wanneer", "Stel", "Dan", "Gitt", "Så", "Når", 
-                                                                  "Men", "Og", "Mutta", "Ja",  "Oletetaan", "Kun", "Niin", "Пусть", 
-                                                                  "Допустим", "К тому же", "То", "Дано", "Когда", "Но", "Тогда", "Если", 
-                                                                  "И",  "А", "Также", "Дадено", "И",  "То", "Когато", "Но", "Maka", 
-                                                                  "Apabila", "Tapi", "Kemudian", "Dan", "Tetapi", "Diberi", "Bagi", 
-                                                                  "Etant donnés", "Alors", "Étant données", "Etant donné", "Étant donnée", 
-                                                                  "Lorsqu'", "Etant donnée", "Et", "Étant donné", "Quand", "Lorsque", 
-                                                                  "Mais", "Soit", "Etant données",  "Étant donnés", "Njuk", "Tapi", 
-                                                                  "Menawa", "Nalika", "Ananging", "Lan",  "Nanging", "Manawa", "Nalikaning", 
-                                                                  "Banjur", "Givun", "Youse know when youse got", "Youse know like when", 
-                                                                  "An", "Den youse gotta", "Buh", "Dun",  "Wun", "WEN", "I CAN HAZ", "BUT", 
-                                                                  "AN", "DEN", "Potom", "Za predpokladu", "Tak", "Pokiaľ", "A zároveň", "A", 
-                                                                  "Ak", "A taktiež", "Ale", "Keď", "A tiež", "Privzeto", "Ampak", "Takrat", 
-                                                                  "Ko", "Nato", "Zaradi", "Ce", "Potem", "Če", "Ter", "Kadar", "Toda", 
-                                                                  "Dano", "Podano", "Vendar", "In", "I", "Atesa", "Donada", "Aleshores", 
-                                                                  "Cal", "Però", "Donat", "Quan",  "Atès", "ನೀಡಿದ", "ಮತ್ತು",  "ಆದರೆ", "ನಂತರ", 
-                                                                  "ಸ್ಥಿತಿಯನ್ನು", "Så", "När", "Och", "Men", "Givet", "그러면",  "만약", "먼저", 
-                                                                  "조건", "단", "만일", "하지만", "그리고", "Mais",  "E", "Dada", "Pero", "Dados", 
-                                                                  "Logo", "Cando", "Dadas", "Dado", "Entón", "那麼", "假如", "而且", "同時", 
-                                                                  "假設", "當", "假定",  "但是", "並且", "And y'all", "But y'all", "When y'all",  
-                                                                  "Then y'all", "Given y'all", "Zadate", "I", "Kad", "Zatati", "Ali", 
-                                                                  "Kada", "Onda",  "Zadato", "Pak", "A", "Ale", "A také", "Když", 
-                                                                  "Za předpokladu", "Pokud", "ਅਤੇ", "ਜਿਵੇਂ ਕਿ", "ਪਰ", "ਜੇਕਰ", "ਜਦੋਂ", "ਤਦ", 
-                                                                  "Задато", "Кад", "Задати", "Када", "Задате", "Али", "Онда", "И",  
-                                                                  "ghu' noblu'", "DaH ghu' bejlu'", "latlh", "qaSDI'", "'ach", "'ej", "'a",  
-                                                                  "vaj", "กำหนดให้", "แต่", "และ",  "เมื่อ", "ดังนั้น", "మరియు", "అప్పుడు", 
-                                                                  "ఈ పరిస్థితిలో", "చెప్పబడినది", "కాని", "I",  "Gdy", "Kiedy", "Wtedy", "Ale", 
+                                                                  "Anrhegedig a", "Pryd",  "Yna", "Ond", "هنگامی",  "با فرض", "آنگاه",
+                                                                  "اما", "و", "Dat fiind", "Dati fiind", "Atunci", "Dați fiind", "Dar",
+                                                                  "Si", "Când", "Daţi fiind", "Și",  "Cand", "Şi", "Date fiind", "Als",
+                                                                  "Maar", "Gegeven", "En", "Wanneer", "Stel", "Dan", "Gitt", "Så", "Når",
+                                                                  "Men", "Og", "Mutta", "Ja",  "Oletetaan", "Kun", "Niin", "Пусть",
+                                                                  "Допустим", "К тому же", "То", "Дано", "Когда", "Но", "Тогда", "Если",
+                                                                  "И",  "А", "Также", "Дадено", "И",  "То", "Когато", "Но", "Maka",
+                                                                  "Apabila", "Tapi", "Kemudian", "Dan", "Tetapi", "Diberi", "Bagi",
+                                                                  "Etant donnés", "Alors", "Étant données", "Etant donné", "Étant donnée",
+                                                                  "Lorsqu'", "Etant donnée", "Et", "Étant donné", "Quand", "Lorsque",
+                                                                  "Mais", "Soit", "Etant données",  "Étant donnés", "Njuk", "Tapi",
+                                                                  "Menawa", "Nalika", "Ananging", "Lan",  "Nanging", "Manawa", "Nalikaning",
+                                                                  "Banjur", "Givun", "Youse know when youse got", "Youse know like when",
+                                                                  "An", "Den youse gotta", "Buh", "Dun",  "Wun", "WEN", "I CAN HAZ", "BUT",
+                                                                  "AN", "DEN", "Potom", "Za predpokladu", "Tak", "Pokiaľ", "A zároveň", "A",
+                                                                  "Ak", "A taktiež", "Ale", "Keď", "A tiež", "Privzeto", "Ampak", "Takrat",
+                                                                  "Ko", "Nato", "Zaradi", "Ce", "Potem", "Če", "Ter", "Kadar", "Toda",
+                                                                  "Dano", "Podano", "Vendar", "In", "I", "Atesa", "Donada", "Aleshores",
+                                                                  "Cal", "Però", "Donat", "Quan",  "Atès", "ನೀಡಿದ", "ಮತ್ತು",  "ಆದರೆ", "ನಂತರ",
+                                                                  "ಸ್ಥಿತಿಯನ್ನು", "Så", "När", "Och", "Men", "Givet", "그러면",  "만약", "먼저",
+                                                                  "조건", "단", "만일", "하지만", "그리고", "Mais",  "E", "Dada", "Pero", "Dados",
+                                                                  "Logo", "Cando", "Dadas", "Dado", "Entón", "那麼", "假如", "而且", "同時",
+                                                                  "假設", "當", "假定",  "但是", "並且", "And y'all", "But y'all", "When y'all",
+                                                                  "Then y'all", "Given y'all", "Zadate", "I", "Kad", "Zatati", "Ali",
+                                                                  "Kada", "Onda",  "Zadato", "Pak", "A", "Ale", "A také", "Když",
+                                                                  "Za předpokladu", "Pokud", "ਅਤੇ", "ਜਿਵੇਂ ਕਿ", "ਪਰ", "ਜੇਕਰ", "ਜਦੋਂ", "ਤਦ",
+                                                                  "Задато", "Кад", "Задати", "Када", "Задате", "Али", "Онда", "И",
+                                                                  "ghu' noblu'", "DaH ghu' bejlu'", "latlh", "qaSDI'", "'ach", "'ej", "'a",
+                                                                  "vaj", "กำหนดให้", "แต่", "และ",  "เมื่อ", "ดังนั้น", "మరియు", "అప్పుడు",
+                                                                  "ఈ పరిస్థితిలో", "చెప్పబడినది", "కాని", "I",  "Gdy", "Kiedy", "Wtedy", "Ale",
                                                                   "Jeżeli", "Jeśli", "Mając", "Zakładając", "Oraz", "Og", "Så", "Når",
                                                                    "Men", "Givet", "כאשר", "וגם", "אז",  "בהינתן", "אבל", "אזי",
                                                                   "Ond", "Ðurh", "Ða", "Ða ðe", "Ac", "Thurh", "Þa", "7", "Þa þe", "Tha",
                                                                   "Þurh",  "Tha the", "Ama", "Fakat", "O zaman",  "Ve", "Eğer ki",
                                                                   "Diyelim ki");
-  
+
   @NotNull
   @Override
   public ID<Boolean, List<Integer>> getName() {
@@ -98,15 +99,18 @@ public class CucumberJava8StepIndex extends FileBasedIndexExtension<Boolean, Lis
   @Override
   public DataIndexer<Boolean, List<Integer>, FileContent> getIndexer() {
     return inputData -> {
-      StringSearcher searcher = new StringSearcher(JAVA_8_PACKAGE, true, true);
+      StringSearcher searcher = new StringSearcher(JAVA_8_CUCUMBER_4_5_PACKAGE, true, true);
       CharSequence text = inputData.getContentAsText();
-      LighterAST lighterAst = ((PsiDependentFileContent)inputData).getLighterAST();
       if (!isCucumberStepDefinitionFile(searcher, text)) {
-        return Collections.emptyMap();
+        searcher = new StringSearcher(JAVA_8_PACKAGE, true, true);
+        if (!isCucumberStepDefinitionFile(searcher, text)) {
+          return Collections.emptyMap();
+        }
       }
 
+      LighterAST lighterAst = ((PsiDependentFileContent)inputData).getLighterAST();
       List<Integer> result = getAllStepDefinitionCalls(lighterAst, text);
-      HashMap<Boolean, List<Integer>> resultMap = new HashMap<>();
+      Map<Boolean, List<Integer>> resultMap = new HashMap<>();
       resultMap.put(true, result);
       return resultMap;
     };
@@ -126,7 +130,12 @@ public class CucumberJava8StepIndex extends FileBasedIndexExtension<Boolean, Lis
 
   @Override
   public int getVersion() {
-    return 0;
+    return 2;
+  }
+
+  @Override
+  public boolean hasSnapshotMapping() {
+    return true;
   }
 
   @NotNull
