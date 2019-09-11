d9cd8f9821afc72a1071c99bb981d521dc486ec6
Gregory.Shrago <gregory.shrago@jetbrains.com>
2019-09-11 18:36:53 +0300

    move CompletionAutoPopupTestCase to platform 2
    GitOrigin-RevId: 25e5eddf20546332d1396dfaa4449ccedb6e59bf

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueAutoPopupTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueAutoPopupTest.kt
@@ -1,10 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.lang
 
-import com.intellij.lang.javascript.JSAutoPopupTestBase
+import com.intellij.testFramework.fixtures.CompletionAutoPopupTestCase
 import junit.framework.TestCase
 
-class VueAutoPopupTest : JSAutoPopupTestBase() {
+class VueAutoPopupTest : CompletionAutoPopupTestCase() {
   fun testEventsAfterAt() {
     myFixture.configureByText("a.vue", "<div <caret>>")
     type("@")
