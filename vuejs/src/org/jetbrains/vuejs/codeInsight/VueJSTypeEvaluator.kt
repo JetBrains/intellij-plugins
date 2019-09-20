b8e07c790b49b18a2711fa6af23dea34fd569421
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-09-20 12:40:50 +0200

    WEB-41026 Vuejs v-for index is resolved as array type instead of number
    GitOrigin-RevId: df54b9c03e72a3d61fa295cefad4ca825fc6f368

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSTypeEvaluator.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueJSTypeEvaluator.kt
@@ -3,10 +3,9 @@ package org.jetbrains.vuejs.codeInsight
 
 import com.intellij.lang.javascript.psi.JSFieldVariable
 import com.intellij.lang.javascript.psi.JSType
-import com.intellij.lang.javascript.psi.resolve.JSEvaluateContext
-import com.intellij.lang.javascript.psi.resolve.JSTypeEvaluationHelper
-import com.intellij.lang.javascript.psi.resolve.JSTypeEvaluator
-import com.intellij.lang.javascript.psi.resolve.JSTypeProcessor
+import com.intellij.lang.javascript.psi.resolve.*
+import com.intellij.lang.javascript.psi.types.primitives.JSNumberType
+import com.intellij.lang.javascript.psi.types.primitives.JSStringType
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlAttribute
 import org.jetbrains.vuejs.lang.expr.psi.VueJSVForExpression
@@ -22,9 +21,16 @@ class VueJSTypeEvaluator(context: JSEvaluateContext, processor: JSTypeProcessor,
   private fun evaluateTypeFromVForVariable(jsVariable: JSFieldVariable): Boolean {
     if (jsVariable !is VueJSVForVariable) return false
     val vForExpression = PsiTreeUtil.getParentOfType(jsVariable, VueJSVForExpression::class.java) ?: return false
+    if (vForExpression.getVarStatement()?.variables?.indexOf(jsVariable) != 0) return false
+
     pushDestructuringContext(jsVariable)
     val expression = myContext.processedExpression
-    getComponentTypeFromArrayExpression(expression, vForExpression.getCollectionExpression()).forEach { addType(it, expression) }
+    when (val collectionType = JSResolveUtil.getElementJSType(vForExpression.getCollectionExpression())) {
+      is JSStringType -> addType(collectionType, expression)
+      is JSNumberType -> addType(collectionType, expression)
+      else -> getComponentTypeFromArrayExpression(expression, vForExpression.getCollectionExpression())
+        .forEach { addType(it, expression) }
+    }
     restoreEvaluationContextApplingElementsSize(myContext.jsElementsToApply.size)
     myContext.finishEvaluationWithStrictness(myContext.isStrict)
     return true
