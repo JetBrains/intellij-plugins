e67703baca35969a4570e645eb65176cf54c3efd
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-09-22 09:03:33 +0200

    GrazieAppInit - use one message bus connection
    GitOrigin-RevId: ce186909567663647883fe6782c14134c3b7fbe9

--- grazie/src/main/kotlin/com/intellij/grazie/ide/init/GrazieAppInit.kt
+++ grazie/src/main/kotlin/com/intellij/grazie/ide/init/GrazieAppInit.kt
@@ -1,22 +1,22 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.grazie.ide.init
 
-import com.intellij.application.subscribe
-import com.intellij.ide.ApplicationInitializedListener
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.grazie.ide.GrazieCommitInspection
 import com.intellij.grazie.ide.GrazieInspection
 import com.intellij.grazie.ide.msg.GrazieStateLifecycle
 import com.intellij.grazie.language.LangDetector
 import com.intellij.grazie.language.LangTool
 import com.intellij.grazie.spellcheck.GrazieSpellchecker
+import com.intellij.ide.ApplicationInitializedListener
+import com.intellij.openapi.application.ApplicationManager
 
 class GrazieAppInit : ApplicationInitializedListener {
   override fun componentsInitialized() {
-    GrazieStateLifecycle.topic.subscribe(ApplicationManager.getApplication(), LangTool)
-    GrazieStateLifecycle.topic.subscribe(ApplicationManager.getApplication(), LangDetector)
-    GrazieStateLifecycle.topic.subscribe(ApplicationManager.getApplication(), GrazieSpellchecker)
-    GrazieStateLifecycle.topic.subscribe(ApplicationManager.getApplication(), GrazieCommitInspection)
-    GrazieStateLifecycle.topic.subscribe(ApplicationManager.getApplication(), GrazieInspection)
+    val connection = ApplicationManager.getApplication().messageBus.connect()
+    connection.subscribe(GrazieStateLifecycle.topic, LangTool)
+    connection.subscribe(GrazieStateLifecycle.topic, LangDetector)
+    connection.subscribe(GrazieStateLifecycle.topic, GrazieSpellchecker)
+    connection.subscribe(GrazieStateLifecycle.topic, GrazieCommitInspection)
+    connection.subscribe(GrazieStateLifecycle.topic, GrazieInspection)
   }
 }
