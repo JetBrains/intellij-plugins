2b6b1e95c1f516c7663f72a0df86562cfe6d52a6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-10-01 14:04:04 +0200

    IDEA-220136 XXE in IDETalk plugin
    GitOrigin-RevId: 59ac57d539d1f53eb522b57b4a1e6559fe81a4e7

--- IDEtalk/src/p2p/jetbrains/communicator/p2p/commands/SendXmlMessageP2PCommand.java
+++ IDEtalk/src/p2p/jetbrains/communicator/p2p/commands/SendXmlMessageP2PCommand.java
@@ -1,20 +1,7 @@
-/*
- * Copyright 2000-2006 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package jetbrains.communicator.p2p.commands;
 
+import com.intellij.openapi.util.JDOMUtil;
 import jetbrains.communicator.core.EventBroadcaster;
 import jetbrains.communicator.core.dispatcher.Message;
 import jetbrains.communicator.core.transport.Transport;
@@ -22,12 +9,8 @@ import jetbrains.communicator.core.transport.XmlMessage;
 import jetbrains.communicator.core.transport.XmlResponseProvider;
 import jetbrains.communicator.util.CommunicatorStrings;
 import org.apache.log4j.Logger;
-import org.jdom.Document;
 import org.jdom.Element;
-import org.jdom.input.SAXBuilder;
 import org.jdom.output.XMLOutputter;
-
-import java.io.StringReader;
 
 /**
  * @author Kir Maximov
@@ -43,14 +26,11 @@ public class SendXmlMessageP2PCommand extends BaseP2PCommand {
 
   public String incomingMessage(String remoteUser, String messageText) {
     String xml = CommunicatorStrings.fromXMLSafeString(messageText);
-    SAXBuilder builder = new SAXBuilder();
     try {
-      Document document = builder.build(new StringReader(xml));
-      Element rootElement = document.getRootElement();
+      Element rootElement = JDOMUtil.load(xml);
       Element response = createResponse(rootElement, CommunicatorStrings.fromXMLSafeString(remoteUser));
       if (response == null) return "";
-
-      return new XMLOutputter().outputString(response);
+      return JDOMUtil.write(response);
     } catch (Throwable e) {
       LOG.info(e.getMessage(), e);
       return CommunicatorStrings.toXML(e);
