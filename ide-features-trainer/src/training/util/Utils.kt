ba6c64bea7fd0bc598b1e6cebe7096c8336bf827
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2019-10-02 17:46:17 +0200

    remove obsolete feedback parts
    GitOrigin-RevId: cb8fa5a6cc27e485aefbf55c7b0db26712d01a86

--- ide-features-trainer/src/training/util/Utils.kt
+++ ide-features-trainer/src/training/util/Utils.kt
@@ -8,7 +8,6 @@ import com.intellij.lang.Language
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
-import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.Balloon
@@ -17,101 +16,14 @@ import com.intellij.openapi.util.registry.Registry
 import com.intellij.util.ui.UIUtil
 import training.learn.CourseManager
 import java.awt.Point
-import java.io.File
-import java.io.FileFilter
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import javax.swing.Icon
 
-abstract class UrlProvider {
-  abstract val statsServerPostUrl: String
-
-  abstract val experimentDataUrl: String
-
-  companion object {
-    fun getInstance(): UrlProvider = ServiceManager.getService(UrlProvider::class.java)
-  }
-
-}
-
-abstract class FilePathProvider {
-  abstract fun getUniqueFile(): File
-  abstract fun getDataFiles(): List<File>
-  abstract fun getStatsDataDirectory(): File
-
-  companion object {
-    fun getInstance(): FilePathProvider = ServiceManager.getService(FilePathProvider::class.java)
-  }
-
-}
-
-class InternalUrlProvider : UrlProvider() {
-  private val localhost = "http://localhost"
-  private val internalHost = "http://unit-617.labs.intellij.net"
-
-  private val host: String
-    get() = if (isPropertyExists("stats.collector.localhost.server")) localhost else internalHost
-
-
-  override val statsServerPostUrl = "http://test.jetstat-resty.aws.intellij.net/uploadstats"
-  override val experimentDataUrl = "$host:8090/experiment/info"
-}
-
-
-open class UniqueFilesProvider(private val baseName: String, private val rootDirectoryComputer: () -> File) : FilePathProvider() {
-
-  constructor(baseName: String, rootDir: File) : this(baseName, { rootDir })
-
-  override fun getUniqueFile(): File {
-    val dir = getStatsDataDirectory()
-
-    val currentMaxIndex = dir
-        .listFiles(FileFilter { it.isFile })
-        .filter { it.name.startsWith(baseName) }
-        .map { it.name.substringAfter('_') }
-        .filter { it.isIntConvertable() }
-        .map { it.toInt() }
-        .max()
-
-    val newIndex = if (currentMaxIndex != null) currentMaxIndex + 1 else 0
-
-    val file = File(dir, "${baseName}_$newIndex")
-    return file
-  }
-
-  override fun getDataFiles(): List<File> {
-    val dir = getStatsDataDirectory()
-    return dir.listFiles(FileFilter { it.isFile })
-        .filter { it.name.startsWith(baseName) }
-        .filter { it.name.substringAfter('_').isIntConvertable() }
-        .sortedBy { it.getChunkNumber() }
-  }
-
-  override fun getStatsDataDirectory(): File {
-    val dir = File(rootDirectoryComputer(), "completion-stats-data")
-    if (!dir.exists()) {
-      dir.mkdir()
-    }
-    return dir
-  }
-
-  private fun File.getChunkNumber() = this.name.substringAfter('_').toInt()
-
-  private fun String.isIntConvertable(): Boolean {
-    try {
-      this.toInt()
-      return true
-    } catch (e: NumberFormatException) {
-      return false
-    }
-  }
-
-}
 
 fun createNamedSingleThreadExecutor(name: String): ExecutorService =
     Executors.newSingleThreadExecutor(ThreadFactoryBuilder().setNameFormat(name).build())
 
-fun isPropertyExists(name: String) = System.getProperty(name) != null
 
 fun findLanguageByID(id: String?): Language? {
   val effectiveId = if (id!!.toLowerCase() == "cpp") {
