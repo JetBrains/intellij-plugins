a877f95d831377092801b7b0ed96ce0a846268af
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-10-04 16:49:04 +0300

    Fix Dart test data (dartfmt now works for these cases)
    GitOrigin-RevId: 79f011d8db8b192e2ad592505e576bd9ae0540e7

--- Dart/testSrc/com/jetbrains/dart/analysisServer/DartCodeGenerationTest.java
+++ Dart/testSrc/com/jetbrains/dart/analysisServer/DartCodeGenerationTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.dart.analysisServer;
 
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
@@ -45,8 +45,7 @@ public class DartCodeGenerationTest extends CodeInsightFixtureTestCase {
                             "  @override\n" +
                             "  bool operator ==(Object other) =>\n" +
                             "      identical(this, other) ||\n" +
-                            "          other is Foo &&\n" +
-                            "              runtimeType == other.runtimeType;\n" +
+                            "      other is Foo && runtimeType == other.runtimeType;\n" +
                             "\n" +
                             "  @override\n" +
                             "  int get hashCode => 0;\n" +
@@ -70,13 +69,10 @@ public class DartCodeGenerationTest extends CodeInsightFixtureTestCase {
                             "  @override\n" +
                             "  bool operator ==(Object other) =>\n" +
                             "      identical(this, other) ||\n" +
-                            "          super == other &&\n" +
-                            "              other is Foo &&\n" +
-                            "              runtimeType == other.runtimeType;\n" +
+                            "      super == other && other is Foo && runtimeType == other.runtimeType;\n" +
                             "\n" +
                             "  @override\n" +
-                            "  int get hashCode =>\n" +
-                            "      super.hashCode;\n" +
+                            "  int get hashCode => super.hashCode;\n" +
                             "}");
   }
 
