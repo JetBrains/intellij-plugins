3f1053cc90b3e40fdff676ee8d68f297399d762b
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-10-19 19:48:58 +0300

    Add help button to spellchecker checkbox
    GitOrigin-RevId: d7c52ba084ed055582c68d0951fa757b99fdaa51

--- grazie/src/main/kotlin/com/intellij/grazie/ide/ui/components/settings/GrazieSpellcheckComponent.kt
+++ grazie/src/main/kotlin/com/intellij/grazie/ide/ui/components/settings/GrazieSpellcheckComponent.kt
@@ -2,8 +2,12 @@
 package com.intellij.grazie.ide.ui.components.settings
 
 import com.intellij.grazie.ide.ui.components.dsl.msg
-import com.intellij.grazie.ide.ui.components.dsl.wrapWithComment
+import com.intellij.grazie.ide.ui.components.dsl.padding
+import com.intellij.grazie.ide.ui.components.dsl.panel
+import com.intellij.ui.ContextHelpLabel
 import com.intellij.ui.components.JBCheckBox
+import com.intellij.ui.components.panels.HorizontalLayout
+import com.intellij.util.ui.JBUI
 
 class GrazieSpellcheckComponent {
   private val checkbox = JBCheckBox(msg("grazie.ui.settings.spellcheck.enable.text"))
@@ -13,5 +17,9 @@ class GrazieSpellcheckComponent {
       checkbox.isSelected = value
     }
 
-  val component = wrapWithComment(checkbox, msg("grazie.ui.settings.spellcheck.enable.note"))
+  val component = panel(HorizontalLayout(10)) {
+    border = padding(JBUI.insetsBottom(10))
+    add(checkbox)
+    add(ContextHelpLabel.create(msg("grazie.ui.settings.spellcheck.enable.note")))
+  }
 }
