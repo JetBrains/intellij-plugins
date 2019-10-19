1caf5c983c1454a8e67d6b27d412afa483f8e324
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-10-19 22:02:38 +0300

    Ignore whitespace repetition rule for XML files
    GitOrigin-RevId: 5058f2633017c7bb5d1966e1ef5aba2bcd412077

--- grazie/src/main/kotlin/com/intellij/grazie/ide/language/xml/XmlGrammarCheckingStrategy.kt
+++ grazie/src/main/kotlin/com/intellij/grazie/ide/language/xml/XmlGrammarCheckingStrategy.kt
@@ -2,6 +2,7 @@
 package com.intellij.grazie.ide.language.xml
 
 import com.intellij.grazie.grammar.strategy.GrammarCheckingStrategy
+import com.intellij.grazie.grammar.strategy.impl.RuleGroup
 import com.intellij.grazie.utils.isAtEnd
 import com.intellij.grazie.utils.isAtStart
 import com.intellij.psi.PsiElement
@@ -13,4 +14,6 @@ class XmlGrammarCheckingStrategy : GrammarCheckingStrategy {
   override fun isMyContextRoot(element: PsiElement) = element is XmlText || (element is XmlToken && element.tokenType == XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN)
 
   override fun isTypoAccepted(root: PsiElement, typoRange: IntRange, ruleRange: IntRange) = !typoRange.isAtStart(root) && !typoRange.isAtEnd(root)
+
+  override fun getIgnoredRuleGroup(root: PsiElement, child: PsiElement) = RuleGroup.WHITESPACES
 }
