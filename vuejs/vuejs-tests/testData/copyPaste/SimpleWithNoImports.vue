bf4349a60ecd9bb7a2be5ef564d154e9e548fafd
anstarovoyt <andrey.starovoyt@jetbrains.com>
2019-10-24 13:14:38 +0300

    vue: copy-paste with imports support WEB-42108
    GitOrigin-RevId: 0addeec765c951d8e4f477f7b26c74d4e4b1998b

---
+++ vuejs/vuejs-tests/testData/copyPaste/SimpleWithNoImports.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="list" />
+</template>
+
+<script>
+  import Component from './SimpleWithNoImports_2'
+  export default {
+    name: 'list',
+  }
+  <selection>console.log(Component)</selection>
+</script>
