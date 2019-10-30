cd2083325c39ed9711d74223756ce8c62ecd37a8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-10-30 10:51:32 +0100

    EA-215231 Angular: avoid using resolve and get directives directly from descriptors
    GitOrigin-RevId: 4560bf473c22182e09b7762cca885b5ab0105279

--- AngularJS/src/org/angular2/editor/Angular2TypeDeclarationProvider.java
+++ AngularJS/src/org/angular2/editor/Angular2TypeDeclarationProvider.java
@@ -5,13 +5,16 @@ import com.intellij.codeInsight.navigation.actions.TypeDeclarationProvider;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
-import org.angular2.Angular2InjectionUtils;
+import com.intellij.util.containers.ContainerUtil;
 import org.angular2.entities.Angular2Component;
+import org.angular2.entities.Angular2Directive;
 import org.angular2.entities.Angular2DirectiveSelectorPsiElement;
 import org.angular2.entities.Angular2EntitiesProvider;
 import org.angular2.lang.Angular2Bundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class Angular2TypeDeclarationProvider implements TypeDeclarationProvider {
 
@@ -32,9 +35,8 @@ public class Angular2TypeDeclarationProvider implements TypeDeclarationProvider 
   @Nullable
   @Override
   public String getActionText(@NotNull DataContext context) {
-    PsiElement symbol = Angular2InjectionUtils.getTargetElementFromContext(context);
-    if (symbol instanceof Angular2DirectiveSelectorPsiElement
-        && Angular2EntitiesProvider.findComponent((Angular2DirectiveSelectorPsiElement)symbol) != null) {
+    List<Angular2Directive> directives = Angular2EditorUtils.getDirectivesAtCaret(context);
+    if (ContainerUtil.find(directives, Angular2Directive::isComponent) != null) {
       return Angular2Bundle.message("angular.action.goto-type-declaration.component-template");
     }
     return null;
