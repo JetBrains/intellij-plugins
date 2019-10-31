651ab4c831aec3c14860fb9700bd9df19138722f
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-10-31 10:30:45 +0100

    add/update copyright
    GitOrigin-RevId: a0a8d012e353b101201db587352ca68d48a24c70

--- ide-features-trainer/src/training/learn/lesson/ruby/refactorings/RubyExtractMethodLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/refactorings/RubyExtractMethodLesson.kt
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 package training.learn.lesson.ruby.refactorings
 
 import com.intellij.testGuiFramework.impl.button
