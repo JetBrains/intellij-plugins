88d2d29e54cbe6e707adeef357e960fda8e54fa0
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-10-31 12:10:50 +0100

    convert SdkModuleProblemDialog.java to Kotlin (2)
    GitOrigin-RevId: 3e4fa7697ae093f5055d00084dcc5651b35f7d77

--- ide-features-trainer/src/training/learn/dialogs/SdkModuleProblemDialog.kt
+++ ide-features-trainer/src/training/learn/dialogs/SdkModuleProblemDialog.kt
@@ -1,91 +1,71 @@
 /*
  * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
-package training.learn.dialogs;
+package training.learn.dialogs
 
-import com.intellij.openapi.project.DumbService;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.roots.ui.configuration.ModulesConfigurator;
-import com.intellij.openapi.ui.DialogWrapper;
-import com.intellij.openapi.ui.Messages;
-import com.intellij.ui.StateRestoringCheckBox;
-import org.jetbrains.annotations.NotNull;
-import training.learn.LearnBundle;
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ui.configuration.ModulesConfigurator
+import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.openapi.ui.Messages
+import com.intellij.ui.StateRestoringCheckBox
+import training.learn.LearnBundle.message
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import java.awt.Insets
+import javax.swing.Action
+import javax.swing.JComponent
+import javax.swing.JLabel
+import javax.swing.JPanel
 
-import javax.swing.*;
-import java.awt.*;
+class SdkModuleProblemDialog(private val project: Project) : DialogWrapper(project, true) {
+  private var myCbOpenProjectSdkPreferences: StateRestoringCheckBox? = null
 
-public class SdkModuleProblemDialog extends DialogWrapper {
-    private final Project myProject;
+  override fun createActions(): Array<Action> {
+    return arrayOf(okAction, cancelAction)
+  }
 
-    private StateRestoringCheckBox myCbOpenProjectSdkPreferences;
+  override fun createNorthPanel(): JComponent? {
+    val panel = JPanel(GridBagLayout())
+    val gbc = GridBagConstraints()
+    val warningMessage = message("dialog.emptyModule.message")
+    gbc.insets = Insets(4, 8, 4, 8)
+    gbc.weighty = 1.0
+    gbc.weightx = 1.0
+    gbc.gridx = 0
+    gbc.gridy = 0
+    gbc.gridwidth = 2
+    gbc.fill = GridBagConstraints.BOTH
+    gbc.anchor = GridBagConstraints.WEST
+    panel.add(JLabel(warningMessage), gbc)
+    gbc.gridy++
+    gbc.gridx = 0
+    gbc.weightx = 0.0
+    gbc.gridwidth = 1
+    myCbOpenProjectSdkPreferences = StateRestoringCheckBox()
+    myCbOpenProjectSdkPreferences!!.text = message("dialog.emptyModule.checkbox")
+    panel.add(myCbOpenProjectSdkPreferences, gbc)
+    myCbOpenProjectSdkPreferences!!.isSelected = true
+    return panel
+  }
 
+  override fun createCenterPanel(): JComponent? {
+    return null
+  }
 
-    public SdkModuleProblemDialog(Project project) {
-        super(project, true);
-        myProject = project;
-        setTitle(LearnBundle.INSTANCE.message("dialog.emptyModule.title"));
-        init();
+  override fun doOKAction() {
+    if (DumbService.isDumb(project)) {
+      Messages.showMessageDialog(project, "Changing Project SDK is not available while indexing is in progress", "Indexing", null)
+      return
     }
-
-
-    @Override
-    @NotNull
-    protected Action[] createActions() {
-        return new Action[]{getOKAction(), getCancelAction()};
+    if (myCbOpenProjectSdkPreferences != null && myCbOpenProjectSdkPreferences!!.isSelected) {
+      ModulesConfigurator.showDialog(project, null, null)
+      super.doOKAction()
     }
+  }
 
-    @Override
-    protected JComponent createNorthPanel() {
-        final JPanel panel = new JPanel(new GridBagLayout());
-        final GridBagConstraints gbc = new GridBagConstraints();
-
-        final String warningMessage = LearnBundle.INSTANCE.message("dialog.emptyModule.message");
-
-        gbc.insets = new Insets(4, 8, 4, 8);
-        gbc.weighty = 1;
-        gbc.weightx = 1;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 2;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(new JLabel(warningMessage), gbc);
-
-        gbc.gridy++;
-        gbc.gridx = 0;
-        gbc.weightx = 0.0;
-        gbc.gridwidth = 1;
-        myCbOpenProjectSdkPreferences = new StateRestoringCheckBox();
-        myCbOpenProjectSdkPreferences.setText(LearnBundle.INSTANCE.message("dialog.emptyModule.checkbox"));
-        panel.add(myCbOpenProjectSdkPreferences, gbc);
-
-
-        myCbOpenProjectSdkPreferences.setSelected(true);
-
-        return panel;
-    }
-
-
-
-    @Override
-    protected JComponent createCenterPanel() {
-        return null;
-    }
-
-
-    @Override
-    protected void doOKAction() {
-        if (DumbService.isDumb(myProject)) {
-            Messages.showMessageDialog(myProject, "Changing Project SDK is not available while indexing is in progress", "Indexing", null);
-            return;
-        }
-
-        if (myCbOpenProjectSdkPreferences != null && myCbOpenProjectSdkPreferences.isSelected()) {
-            ModulesConfigurator.showDialog(myProject, null, null);
-            super.doOKAction();
-        }
-
-    }
-
+  init {
+    title = message("dialog.emptyModule.title")
+    init()
+  }
 }
