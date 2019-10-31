85811aec0e13cb0d161397b79d3e81aff7ae9db3
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-10-31 10:30:45 +0100

    add/update copyright
    GitOrigin-RevId: 81f6cc3aa83a2e2dd36171aadf551238375f7880

--- ide-features-trainer/src/training/learn/exceptons/LessonScriptStopped.kt
+++ ide-features-trainer/src/training/learn/exceptons/LessonScriptStopped.kt
@@ -1,3 +1,6 @@
+/*
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
 package training.learn.exceptons
 
 /** This exception is used to indicate that lesson is interrupted by another lesson or by some other reason */
