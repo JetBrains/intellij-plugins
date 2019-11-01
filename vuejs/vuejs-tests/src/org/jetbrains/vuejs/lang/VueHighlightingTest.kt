50ceb57655594e60a844a79c1f8267a11bd30632
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-11-01 15:27:19 +0300

    WEB-38529 Vue.js ::v-deep support
    GitOrigin-RevId: 5914097d401bf606d2818910d08475c3e1919075

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueHighlightingTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueHighlightingTest.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.lang
 
 import com.intellij.codeInsight.daemon.impl.analysis.XmlUnboundNsPrefixInspection
@@ -13,6 +13,7 @@ import com.intellij.lang.typescript.inspections.TypeScriptUnresolvedFunctionInsp
 import com.intellij.lang.typescript.inspections.TypeScriptUnresolvedVariableInspection
 import com.intellij.lang.typescript.inspections.TypeScriptValidateTypesInspection
 import com.intellij.openapi.application.PathManager
+import com.intellij.psi.css.inspections.invalid.CssInvalidPseudoSelectorInspection
 import com.intellij.spellchecker.inspections.SpellCheckingInspection
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
@@ -1486,6 +1487,14 @@ var <info descr="local variable">i</info>:<info descr="exported class">SpaceInte
     myFixture.checkHighlighting()
   }
 
+  fun testPseudoSelectors() {
+    myFixture.enableInspections(CssInvalidPseudoSelectorInspection::class.java)
+    myFixture.configureByText("foo.vue", """
+      |<style lang="scss">
+      |    div::v-deep::<error descr="Unknown pseudo selector 'v-incorrect'">v-incorrect</error> {}
+      |</style  >""".trimMargin())
+    myFixture.checkHighlighting()
+  }
 }
 
 fun createTwoClassComponents(fixture: CodeInsightTestFixture, tsLang: Boolean = false) {
