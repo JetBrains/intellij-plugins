3e0f0ff9e8fdbbd4eb9aaf0defe036a876a07cde
Dmitry Jemerov <yole@jetbrains.com>
2019-11-08 14:35:31 +0100

    Plugin description updates from azhebel; correct capitalization in run configuration type name
    GitOrigin-RevId: b9d4c0206d97dd93e75e6a98cbc7fc39fbd99875

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/run/CucumberJavaRunConfigurationType.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/run/CucumberJavaRunConfigurationType.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.java.run;
 
 import com.intellij.execution.configurations.ConfigurationFactory;
@@ -15,7 +15,7 @@ import javax.swing.*;
 
 public final class CucumberJavaRunConfigurationType extends ConfigurationTypeBase {
   public CucumberJavaRunConfigurationType() {
-    super("CucumberJavaRunConfigurationType", "Cucumber java", null,
+    super("CucumberJavaRunConfigurationType", "Cucumber Java", null,
           NotNullLazyValue.createValue(() -> CucumberJavaIcons.CucumberJavaRunConfiguration));
     addFactory(new ConfigurationFactory(this) {
       @Override
