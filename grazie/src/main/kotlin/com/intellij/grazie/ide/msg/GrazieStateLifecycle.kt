20f52565ec0a0c33220e547e5ead15b65ddd6693
Vladislav Tankov <vdtankov@gmail.com>
2019-11-09 19:13:27 +0300

    Remove application initialized listener, use just init block in GrazieStateLifecycle
    GitOrigin-RevId: 2d7fae6e7273b305fb3f37c943bb51b992805946

--- grazie/src/main/kotlin/com/intellij/grazie/ide/msg/GrazieStateLifecycle.kt
+++ grazie/src/main/kotlin/com/intellij/grazie/ide/msg/GrazieStateLifecycle.kt
@@ -1,7 +1,12 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.grazie.ide.msg
 
+import com.intellij.application.subscribe
 import com.intellij.grazie.GrazieConfig
+import com.intellij.grazie.ide.GrazieCommitInspection
+import com.intellij.grazie.ide.GrazieInspection
+import com.intellij.grazie.jlanguage.LangDetector
+import com.intellij.grazie.jlanguage.LangTool
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.util.messages.Topic
@@ -10,6 +15,13 @@ interface GrazieStateLifecycle {
   companion object {
     val topic = Topic.create("grazie_state_lifecycle_topic", GrazieStateLifecycle::class.java)
     val publisher by lazy { ApplicationManager.getApplication().messageBus.syncPublisher(topic) }
+
+    init {
+      topic.subscribe(ApplicationManager.getApplication(), LangTool)
+      topic.subscribe(ApplicationManager.getApplication(), LangDetector)
+      topic.subscribe(ApplicationManager.getApplication(), GrazieCommitInspection)
+      topic.subscribe(ApplicationManager.getApplication(), GrazieInspection)
+    }
   }
 
   /** Initialize Grazie with passed state */
