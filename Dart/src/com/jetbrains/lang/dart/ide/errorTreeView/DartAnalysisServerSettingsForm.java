f9ed3d663fd8f66a37fe614b57c538daf2567342
Devon Carew <devoncarew@google.com>
2019-11-11 16:51:33 +0300

    cosmetic changes to the Dart Analysis tool window context menu
    close #742
    
    (cherry picked from commit 151902ccd9beade02b2c08d9b390d3d6c4d864de)
    
    GitOrigin-RevId: f7dbb83de7e74818f4bfb85fef4afdc55b37942d

--- Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartAnalysisServerSettingsForm.java
+++ Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartAnalysisServerSettingsForm.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.errorTreeView;
 
 import com.intellij.openapi.project.Project;
@@ -30,7 +30,7 @@ public class DartAnalysisServerSettingsForm {
   }
 
   private void createUIComponents() {
-    myDartSettingsHyperlink = new HoverHyperlinkLabel(DartBundle.message("analysis.server.settings.sdk.settings"));
+    myDartSettingsHyperlink = new HoverHyperlinkLabel(DartBundle.message("open.dart.plugin.settings"));
     myDartSettingsHyperlink.addHyperlinkListener(new HyperlinkAdapter() {
       @Override
       protected void hyperlinkActivated(final HyperlinkEvent e) {
