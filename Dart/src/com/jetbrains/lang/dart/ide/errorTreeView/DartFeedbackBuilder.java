a70851861e737e5df5b2eb7f7aeb8b7a4034039d
Devon Carew <devoncarew@google.com>
2019-11-12 16:15:29 +0300

    re-work the dart analysis server's error handling
    close #743
    
    GitOrigin-RevId: f84c8b53f200b82e1e2661077b5ebe032e9f0371

--- Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartFeedbackBuilder.java
+++ Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartFeedbackBuilder.java
@@ -1,49 +1,26 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.errorTreeView;
 
-import com.intellij.notification.*;
-import com.intellij.notification.impl.NotificationsManagerImpl;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.application.ModalityState;
-import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.popup.Balloon;
-import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.wm.IdeFrame;
-import com.intellij.openapi.wm.WindowManager;
-import com.intellij.ui.BalloonLayout;
-import com.intellij.ui.BalloonLayoutData;
-import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-
-import javax.swing.event.HyperlinkEvent;
 
 /**
  * Plugins may define alternative implementations that cause issues to be opened
  * for projects other than the Dart plugin. For example, the Flutter plugin may
  * send issues to the flutter project on github.
+ *
+ * @deprecated This extension point and class are no longer supported.
  */
+@Deprecated
 public abstract class DartFeedbackBuilder {
-  // NOTIFICATION_GROUP is used to add an error to Event Log tool window. Red balloon is shown separately, like for IDE fatal errors.
-  // We do not show standard balloon using this NOTIFICATION_GROUP because it is not red enough.
-  private static final NotificationGroup NOTIFICATION_GROUP =
-    new NotificationGroup("Dart Analyzer Error", NotificationDisplayType.NONE, true);
-
-  private static final ExtensionPointName<DartFeedbackBuilder> EP_NAME = ExtensionPointName.create("Dart.feedbackBuilder");
-
-  @NotNull
-  public static DartFeedbackBuilder getFeedbackBuilder() {
-    final DartFeedbackBuilder[] builders = EP_NAME.getExtensions();
-    assert builders.length > 0;
-    return builders[0];
-  }
-
   /**
    * The title should indicate what sort of action will occur (eg open a browser).
    *
    * @return The string to display in the title of the confirmation dialog.
+   * @deprecated This extension point and class are no longer supported.
    */
+  @Deprecated
   public String title() {
     return "Open Browser";
   }
@@ -52,14 +29,18 @@ public abstract class DartFeedbackBuilder {
    * The prompt should indicate to the user where the issue report will be opened (eg github).
    *
    * @return The string to display in the confirmation dialog.
+   * @deprecated This extension point and class are no longer supported.
    */
+  @Deprecated
   public abstract String prompt();
 
   /**
    * The label should indicate what is going to happen when clicked (eg send feedback).
    *
    * @return The string to display as the label of the yes-button in the confirmation dialog.
+   * @deprecated This extension point and class are no longer supported.
    */
+  @Deprecated
   public String label() {
     return "Send feedback";
   }
@@ -70,55 +51,8 @@ public abstract class DartFeedbackBuilder {
    * @param project      the current project
    * @param errorMessage additional information for the issue, such as a tack trace
    * @param serverLog    recent requests made to the analysis server
+   * @deprecated This extension point and class are no longer supported.
    */
+  @Deprecated
   public abstract void sendFeedback(@NotNull Project project, @Nullable String errorMessage, @Nullable String serverLog);
-
-  /**
-   * Show a notification that allows the user to submit a feedback issue.
-   *
-   * @param message      an additional message to display before the prompt
-   * @param project      the current project
-   * @param errorMessage optional, may be used for stack trace
-   * @param debugLog     optional, server traffic log for debugging
-   */
-  public void showNotification(@NotNull String message,
-                               @NotNull Project project,
-                               @Nullable String errorMessage,
-                               @Nullable String debugLog) {
-    String content = message + "<br><a href=\"\">" + prompt() + "</a>";
-
-    NotificationListener listener = new NotificationListener.Adapter() {
-      @Override
-      protected void hyperlinkActivated(@NotNull Notification notification, @NotNull HyperlinkEvent e) {
-        notification.expire();
-        sendFeedback(project, errorMessage, debugLog);
-      }
-    };
-
-    Notification notification =
-      NOTIFICATION_GROUP.createNotification(NOTIFICATION_GROUP.getDisplayId(), content, NotificationType.ERROR, listener);
-
-    // this writes to Event Log tool window, but doesn't show balloon (standard balloon is not red enough,
-    // we want to show a different balloon similar to the IDE fatal error)
-    notification.notify(project);
-
-    // this shows red balloon line in case of an IDE fatal error
-    ApplicationManager.getApplication()
-      .invokeLater(() -> showErrorNotification(notification, project), ModalityState.NON_MODAL, project.getDisposed());
-  }
-
-  private static void showErrorNotification(@NotNull Notification notification, @NotNull Project project) {
-    // Adapted from IdeMessagePanel.showErrorNotification()
-    IdeFrame myFrame = WindowManager.getInstance().getIdeFrame(project);
-    BalloonLayout layout = myFrame.getBalloonLayout();
-    assert layout != null;
-
-    BalloonLayoutData layoutData = BalloonLayoutData.createEmpty();
-    layoutData.fadeoutTime = 5000;
-    layoutData.fillColor = new JBColor(0XF5E6E7, 0X593D41);
-    layoutData.borderColor = new JBColor(0XE0A8A9, 0X73454B);
-
-    Balloon balloon = NotificationsManagerImpl.createBalloon(myFrame, notification, false, false, new Ref<>(layoutData), project);
-    layout.add(balloon);
-  }
 }
