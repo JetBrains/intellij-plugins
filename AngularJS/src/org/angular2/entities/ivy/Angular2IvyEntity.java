595358b1d5ad84dbef78018a3e4e99bd0cf50e27
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2019-11-15 15:48:43 +0530

    IDEA-CR-55307: WEB-42220 Angular: add Ivy support - post review fixes.
    (cherry picked from commit a1eddfbab6ff67c87cdec8d7241d6f50be6c447b)
    
    GitOrigin-RevId: 73358a1e714273f63e9918ec0a260edfa897d174

--- AngularJS/src/org/angular2/entities/ivy/Angular2IvyEntity.java
+++ AngularJS/src/org/angular2/entities/ivy/Angular2IvyEntity.java
@@ -4,32 +4,21 @@ package org.angular2.entities.ivy;
 import com.intellij.lang.javascript.psi.ecma6.ES6Decorator;
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptClass;
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptField;
-import com.intellij.lang.javascript.psi.util.JSClassUtils;
-import com.intellij.openapi.util.UserDataHolderBase;
-import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.util.CachedValueProvider;
-import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.psi.util.PsiTreeUtil;
-import org.angular2.entities.Angular2Entity;
-import org.angular2.entities.Angular2EntityUtils;
-import org.angular2.lang.Angular2Bundle;
+import org.angular2.entities.source.Angular2SourceEntityBase;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.Objects;
 
-public abstract class Angular2IvyEntity extends UserDataHolderBase implements Angular2Entity {
+public abstract class Angular2IvyEntity extends Angular2SourceEntityBase {
 
   protected final TypeScriptField myDefField;
-  private final TypeScriptClass myClass;
 
   protected Angular2IvyEntity(@NotNull TypeScriptField defField) {
+    super(Objects.requireNonNull(PsiTreeUtil.getContextOfType(defField, TypeScriptClass.class)));
     myDefField = defField;
-    myClass = PsiTreeUtil.getContextOfType(myDefField, TypeScriptClass.class);
-    assert myClass != null;
   }
 
   @Nullable
@@ -48,39 +37,6 @@ public abstract class Angular2IvyEntity extends UserDataHolderBase implements An
   @Override
   public PsiElement getSourceElement() {
     return myDefField;
-  }
-
-  @NotNull
-  @Override
-  public String getName() {
-    return StringUtil.notNullize(myClass.getName(), Angular2Bundle.message("angular.description.unnamed"));
-  }
-
-  @NotNull
-  @Override
-  public TypeScriptClass getTypeScriptClass() {
-    return myClass;
-  }
-
-  protected <T> T getCachedValue(@NotNull CachedValueProvider<T> provider) {
-    return CachedValuesManager.getManager(myDefField.getProject()).getCachedValue(this, provider);
-  }
-
-  @NotNull
-  protected Collection<Object> getClassModificationDependencies() {
-    return getCachedValue(() -> {
-      Collection<Object> dependencies = new HashSet<>();
-      JSClassUtils.processClassesInHierarchy(myClass, true, (aClass, typeSubstitutor, fromImplements) -> {
-        dependencies.add(aClass);
-        return true;
-      });
-      return CachedValueProvider.Result.create(dependencies, dependencies);
-    });
-  }
-
-  @Override
-  public String toString() {
-    return Angular2EntityUtils.toString(this);
   }
 
   @Override
