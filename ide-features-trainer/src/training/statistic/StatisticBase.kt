e746d1498caa04cb1d22010842678ed01af4d4b1
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2019-11-15 16:00:36 +0100

    [IFT] remove redundant persistence; use concurrent hash map for lessons state in the session (IFT-3)
    GitOrigin-RevId: 9311a0d611fe9adabbab18a8bdf0292d41c5526c

--- ide-features-trainer/src/training/statistic/StatisticBase.kt
+++ ide-features-trainer/src/training/statistic/StatisticBase.kt
@@ -5,80 +5,50 @@ package training.statistic
 
 import com.intellij.internal.statistic.eventLog.FeatureUsageData
 import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
-import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
-import com.intellij.openapi.components.State
-import com.intellij.openapi.components.Storage
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.util.xmlb.annotations.Transient
 import training.learn.interfaces.Lesson
 import training.statistic.FeatureUsageStatisticConsts.DURATION
 import training.statistic.FeatureUsageStatisticConsts.LANGUAGE
 import training.statistic.FeatureUsageStatisticConsts.LESSON_ID
 import training.statistic.FeatureUsageStatisticConsts.PASSED
 import training.statistic.FeatureUsageStatisticConsts.START
-import training.util.trainerPluginConfigName
+import java.util.concurrent.ConcurrentHashMap
 
 @Suppress("PropertyName")
-@State(name = "StatisticBase", storages = [Storage(value = trainerPluginConfigName)])
-class StatisticBase : PersistentStateComponent<StatisticBase> {
+class StatisticBase {
 
-  override fun getState(): StatisticBase = this
-
-  override fun loadState(persistedState: StatisticBase) {
-    this.persistedLessonId2State.putAll(persistedState.persistedLessonId2State)
-  }
-
-  private val persistedLessonId2State: LinkedHashMap<String, StatisticData> = linkedMapOf()
-  //non persisted history of lesson starting timestamp
-  @Transient
-  val sessionLessonTimestamp: MutableMap<String, Long> = mutableMapOf()
+  private val sessionLessonTimestamp: ConcurrentHashMap<String, Long> = ConcurrentHashMap()
+  private val LOG = Logger.getInstance(StatisticBase::class.java)
+  //should be the same as res/META-INF/plugin.xml <statistics.counterUsagesCollector groupId="ideFeaturesTrainer" .../>
+  private val GROUP_ID = "ideFeaturesTrainer"
 
   companion object {
     val instance: StatisticBase by lazy { ServiceManager.getService(StatisticBase::class.java) }
   }
 
-  val LOG = Logger.getInstance(StatisticBase::class.java)
-
-  enum class StatisticState { STARTED, PASSED }
-
-  data class StatisticData(var state: StatisticState? = null, var timestamp: Long? = null)
-
   fun onStartLesson(lesson: Lesson) {
-    val statisticData = StatisticData(StatisticState.STARTED, System.currentTimeMillis())
-    persistedLessonId2State[lesson.id] = statisticData
     sessionLessonTimestamp[lesson.id] = System.currentTimeMillis()
     logEvent(START, FeatureUsageData()
-        .addData(LESSON_ID, lesson.id)
-        .addData(LANGUAGE, lesson.lang.toLowerCase()))
+      .addData(LESSON_ID, lesson.id)
+      .addData(LANGUAGE, lesson.lang.toLowerCase()))
   }
 
   fun onPassLesson(lesson: Lesson) {
-    synchronized(persistedLessonId2State) {
-      if (persistedLessonId2State[lesson.id] == null) {
-        LOG.warn("Unable to find ${StatisticState.STARTED} state for a lesson: ${lesson.name}")
-        return
-      }
-      if (sessionLessonTimestamp[lesson.id] == null) {
-        LOG.warn("Unable to find timestamp for a lesson: ${lesson.name}")
-        return
-      }
-      val timestamp = sessionLessonTimestamp[lesson.id] ?: return
-      val delta = System.currentTimeMillis() - timestamp
-      val statisticData = StatisticData(StatisticState.PASSED, delta)
-      persistedLessonId2State[lesson.id] = statisticData
-      logEvent(PASSED, FeatureUsageData()
-          .addData(LESSON_ID, lesson.id)
-          .addData(LANGUAGE, lesson.lang.toLowerCase())
-          .addData(DURATION, delta))
+    val timestamp = sessionLessonTimestamp[lesson.id]
+    if (timestamp == null) {
+      LOG.warn("Unable to find timestamp for a lesson: ${lesson.name}")
+      return
     }
+    val delta = System.currentTimeMillis() - timestamp
+    logEvent(PASSED, FeatureUsageData()
+      .addData(LESSON_ID, lesson.id)
+      .addData(LANGUAGE, lesson.lang.toLowerCase())
+      .addData(DURATION, delta))
   }
+
+  private fun logEvent(event: String, featureUsageData: FeatureUsageData) {
+    FUCounterUsageLogger.getInstance().logEvent(GROUP_ID, event, featureUsageData)
+  }
+
 }
-
-private fun logEvent(event: String, featureUsageData: FeatureUsageData) {
-  FUCounterUsageLogger.getInstance().logEvent(GROUP_ID, event, featureUsageData)
-}
-
-//should be the same as res/META-INF/plugin.xml <statistics.counterUsagesCollector groupId="ideFeaturesTrainer" .../>
-private const val GROUP_ID = "ideFeaturesTrainer"
-
