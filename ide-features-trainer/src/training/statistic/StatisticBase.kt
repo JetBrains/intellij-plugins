5e81ff13af4b2f5e190c64d205aa6a0ff8dd9857
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2019-11-15 12:04:25 +0100

    IDEA-CR-55335: [IFT] count all lessons interaction (IFT-3)
    (cherry picked from commit 34b05b50792c4c595855c116b2d52b39362aea9e)
    
    GitOrigin-RevId: 093c4b1ef86d0c53c638b01666750bfcb22535fd

--- ide-features-trainer/src/training/statistic/StatisticBase.kt
+++ ide-features-trainer/src/training/statistic/StatisticBase.kt
@@ -9,6 +9,7 @@ import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.State
 import com.intellij.openapi.components.Storage
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.util.xmlb.annotations.Transient
 import training.learn.interfaces.Lesson
 import training.statistic.FeatureUsageStatisticConsts.DURATION
@@ -18,6 +19,7 @@ import training.statistic.FeatureUsageStatisticConsts.PASSED
 import training.statistic.FeatureUsageStatisticConsts.START
 import training.util.trainerPluginConfigName
 
+@Suppress("PropertyName")
 @State(name = "StatisticBase", storages = [Storage(value = trainerPluginConfigName)])
 class StatisticBase : PersistentStateComponent<StatisticBase> {
 
@@ -28,39 +30,43 @@ class StatisticBase : PersistentStateComponent<StatisticBase> {
   }
 
   private val persistedLessonId2State: LinkedHashMap<String, StatisticData> = linkedMapOf()
-  //non persisted history of lessons states
+  //non persisted history of lesson starting timestamp
   @Transient
-  val sessionLessonId2State: MutableList<Pair<String, StatisticData>> = ArrayList()
+  val sessionLessonTimestamp: MutableMap<String, Long> = mutableMapOf()
 
   companion object {
     val instance: StatisticBase by lazy { ServiceManager.getService(StatisticBase::class.java) }
   }
+
+  val LOG = Logger.getInstance(StatisticBase::class.java)
 
   enum class StatisticState { STARTED, PASSED }
 
   data class StatisticData(var state: StatisticState? = null, var timestamp: Long? = null)
 
   fun onStartLesson(lesson: Lesson) {
-    //if persisted base has already state for this lesson do not add
-    if (persistedLessonId2State.contains(lesson.id)) return // because it contains already info about this lesson. And al least this lesson has been started.
     val statisticData = StatisticData(StatisticState.STARTED, System.currentTimeMillis())
     persistedLessonId2State[lesson.id] = statisticData
-    sessionLessonId2State.add(Pair(lesson.id, statisticData))
+    sessionLessonTimestamp[lesson.id] = System.currentTimeMillis()
     logEvent(START, FeatureUsageData()
         .addData(LESSON_ID, lesson.id)
         .addData(LANGUAGE, lesson.lang.toLowerCase()))
   }
 
   fun onPassLesson(lesson: Lesson) {
-    //if lesson already has a passed state in a persisted map than skip this step
-    if (persistedLessonId2State.containsKey(lesson.id) && persistedLessonId2State[lesson.id]?.state == StatisticState.PASSED) return
     synchronized(persistedLessonId2State) {
-      persistedLessonId2State[lesson.id] ?: return //TODO: add log here
-      val timestamp = persistedLessonId2State[lesson.id]!!.timestamp ?: return
+      if (persistedLessonId2State[lesson.id] == null) {
+        LOG.warn("Unable to find ${StatisticState.STARTED} state for a lesson: ${lesson.name}")
+        return
+      }
+      if (sessionLessonTimestamp[lesson.id] == null) {
+        LOG.warn("Unable to find timestamp for a lesson: ${lesson.name}")
+        return
+      }
+      val timestamp = sessionLessonTimestamp[lesson.id] ?: return
       val delta = System.currentTimeMillis() - timestamp
       val statisticData = StatisticData(StatisticState.PASSED, delta)
       persistedLessonId2State[lesson.id] = statisticData
-      sessionLessonId2State.add(Pair(lesson.id, statisticData))
       logEvent(PASSED, FeatureUsageData()
           .addData(LESSON_ID, lesson.id)
           .addData(LANGUAGE, lesson.lang.toLowerCase())
@@ -73,6 +79,6 @@ private fun logEvent(event: String, featureUsageData: FeatureUsageData) {
   FUCounterUsageLogger.getInstance().logEvent(GROUP_ID, event, featureUsageData)
 }
 
-//should be the same as res/META-INF/plugin.xml <statistics.counterUsagesCollector groupId="ideFeaturesTrainer" version="2"/>
+//should be the same as res/META-INF/plugin.xml <statistics.counterUsagesCollector groupId="ideFeaturesTrainer" .../>
 private const val GROUP_ID = "ideFeaturesTrainer"
 
