438dede4b2d715b87a4107743296d6ff6791769e
Leonid Shalupov <leonid@shalupov.com>
2019-11-17 19:37:10 +0200

    cloudFormation: rewrite file type detection logic
    Use standard file type detector
    
    GitOrigin-RevId: beff690ffaf8e0b730deb847da956b9524af6195

---
+++ CloudFormation/src/test/kotlin/com/intellij/aws/cloudformation/tests/CloudFormationFileTypeDetectionTest.kt
@@ -0,0 +1,87 @@
+package com.intellij.aws.cloudformation.tests
+
+import com.intellij.aws.cloudformation.CloudFormationPsiUtils
+import com.intellij.json.psi.JsonFile
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.testFramework.LightPlatformCodeInsightTestCase
+import org.jetbrains.yaml.psi.YAMLFile
+import org.junit.Assert
+
+class CloudFormationFileTypeDetectionTest: LightPlatformCodeInsightTestCase() {
+  fun `test some random file`() =
+    assertNotCloudFormationFile("my1.some_random_extension", "Nothing about CloudFormation")
+
+  fun `test yaml file without cloudFormation content`() =
+    assertNotCloudFormationFile("my1.yaml", "Nothing about CloudFormation")
+
+  fun `test yaml file with cloudFormation format version`() =
+    assertCloudFormationFile("my2.yaml", """
+      Parameters:
+      AWSTemplateFormatVersion: '2010-09-09'
+    """.trimIndent()) { file -> Assert.assertTrue("File: $file instead of Yaml", file is YAMLFile) }
+
+  fun `test template file with format version and yaml format`() =
+    assertCloudFormationFile("my2.template", """
+      Parameters:
+      AWSTemplateFormatVersion: '2010-09-09'
+    """.trimIndent()) { file -> Assert.assertTrue("File: $file instead of Yaml", file is YAMLFile) }
+
+  fun `test json file without cloudFormation content`() =
+    assertNotCloudFormationFile("my1.json", "{}")
+
+  fun `test json file with cloudFormation format version`() =
+    assertCloudFormationFile("my2.json", """
+      {
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Resources": ["XXX"]
+      }
+    """.trimIndent()) { file -> Assert.assertTrue("File: $file instead of Json", file is JsonFile) }
+
+  fun `test template file with format version and json format`() =
+    assertCloudFormationFile("my2.template", """
+      {
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Resources": ["XXX"]
+      }
+    """.trimIndent()) { file -> Assert.assertTrue("File: $file instead of Json", file is JsonFile) }
+
+  private fun assertNotCloudFormationFile(fileName: String, fileText: String, check: (PsiFile) -> Unit = {}) {
+    val document = configureFromFileText(fileName, fileText)
+    val file = FileDocumentManager.getInstance().getFile(document)
+    Assert.assertNotNull(file)
+
+    val psiFile = PsiManager.getInstance(project).findFile(file!!)
+    Assert.assertNotNull("Can't get psi file for file name '$fileName' with text '$fileText'", psiFile)
+
+    if (CloudFormationPsiUtils.isCloudFormationFile(psiFile!!)) {
+      Assert.assertNotNull("A cloudFormation file, while it should not be. file name '$fileName' with text '$fileText'", psiFile)
+    }
+
+    try {
+      check(psiFile)
+    } catch (t: Throwable) {
+      throw AssertionError("Failed check, file name '$fileName' with text '$fileText'. Cause: ${t.message}", t)
+    }
+  }
+
+  private fun assertCloudFormationFile(fileName: String, fileText: String, check: (PsiFile) -> Unit = {}) {
+    val document = configureFromFileText(fileName, fileText)
+    val file = FileDocumentManager.getInstance().getFile(document)
+    Assert.assertNotNull(file)
+
+    val psiFile = PsiManager.getInstance(project).findFile(file!!)
+    Assert.assertNotNull("Can't get psi file for file name '$fileName' with text '$fileText'", psiFile)
+
+    if (!CloudFormationPsiUtils.isCloudFormationFile(psiFile!!)) {
+      Assert.assertNotNull("Not a CloudFormation file '$fileName' with text '$fileText'", psiFile)
+    }
+
+    try {
+      check(psiFile)
+    } catch (t: Throwable) {
+      throw AssertionError("Failed check, file name '$fileName' with text '$fileText'. Cause: ${t.message}", t)
+    }
+  }
+}
