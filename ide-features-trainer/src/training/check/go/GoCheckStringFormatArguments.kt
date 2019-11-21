60fd264a67517f93c37de565c3f0979cc32df00b
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-11-21 16:41:24 +0100

    [IFT] extract GoCheck to reuse common code
    GitOrigin-RevId: d1ac8449f683748e9197bc12f0d3f09808496a97

--- ide-features-trainer/src/training/check/go/GoCheckStringFormatArguments.kt
+++ ide-features-trainer/src/training/check/go/GoCheckStringFormatArguments.kt
@@ -3,30 +3,12 @@
  */
 package training.check.go
 
-import com.goide.psi.GoFile
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import training.check.Check
+class GoCheckStringFormatArguments : GoCheck() {
 
-class GoCheckStringFormatArguments : Check {
-
-  private lateinit var project: Project
-  private lateinit var editor: Editor
-
-  override fun set(project: Project, editor: Editor) {
-    this.project = project
-    this.editor = editor
-  }
-
-  override fun before() {}
-
-  override fun check(): Boolean {
-    val document = editor.document
-    val manager = PsiDocumentManager.getInstance(project)
-    val file = manager.getPsiFile(document) as? GoFile ?: return false
-    val block = file.functions.find { it.name == "main" }?.block ?: return false
-    return block.statementList.any { it.text == """fmt.Printf("hello %s #%d", subj.name, subj.id)""" }
-  }
+  override fun check(): Boolean = goFile
+          ?.findFunction("main")
+          ?.block
+          ?.statementList
+          ?.any { it.text == """fmt.Printf("hello %s #%d", subj.name, subj.id)""" } == true
 
 }
