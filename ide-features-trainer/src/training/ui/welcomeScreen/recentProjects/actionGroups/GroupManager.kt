771d9644bccc043c7ddda95bf33984a0b8c00145
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2019-11-21 13:18:58 +0100

    IDEA-CR-55639: [IFT] store state of action groups on the welcome frame (IFT-4)
    We want to store expansion/collision state for Projects/Tutorials nodes on RecentProjectsPanel of welcome screen.
    
    (cherry picked from commit b66b91c89805414d04a830bf408ae053d59df7bb)
    
    GitOrigin-RevId: 72f85ecfd56a4b2ad9c8af38dda1ee414be4dd78

--- ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/actionGroups/GroupManager.kt
+++ ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/actionGroups/GroupManager.kt
@@ -1,11 +1,38 @@
 package training.ui.welcomeScreen.recentProjects.actionGroups
 
+import com.intellij.openapi.components.*
 import training.ui.welcomeScreen.recentProjects.actionGroups.commonActions.DefaultProjectsActionGroup
 import training.ui.welcomeScreen.recentProjects.actionGroups.commonActions.TutorialsActionGroup
+import training.util.trainerPluginConfigName
 
-object GroupManager {
+@State(name = "GroupManager", storages = [Storage(value = trainerPluginConfigName)])
+class GroupManager : PersistentStateComponent<GroupManager> {
 
-    val projectsGroup = DefaultProjectsActionGroup().apply { isExpanded = true }
-    val tutorialsGroup = TutorialsActionGroup().apply { isExpanded = true }
+  private val stateLock = Any()
+
+  companion object {
+    @JvmStatic
+    val instance: GroupManager
+      get() = service()
+  }
+
+  var expandedByName: MutableMap<String, Boolean> = mutableMapOf()
+
+  val registeredGroups: List<CommonActionGroup> = listOf(
+    DefaultProjectsActionGroup().apply { val expanded = expandedByName[name] ?: true; isExpanded = expanded },
+    TutorialsActionGroup().apply { val expanded = expandedByName[name] ?: true; isExpanded = expanded }
+  )
+
+  override fun getState(): GroupManager {
+    registeredGroups.forEach { expandedByName[it.name] = it.isExpanded }
+    return this
+  }
+
+  override fun loadState(state: GroupManager) {
+    synchronized(stateLock) {
+      expandedByName = state.expandedByName
+      registeredGroups.forEach { val expanded = state.expandedByName[it.name] ?: true; it.apply { isExpanded = expanded } }
+    }
+  }
 
 }
