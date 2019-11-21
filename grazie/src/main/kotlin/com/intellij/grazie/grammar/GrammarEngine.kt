a14af745a505dd4f67570c648d037b0d2f2e362b
Vladislav.Tankov <vdtankov@gmail.com>
2019-11-21 15:17:27 +0300

    Add \n for separators
    GitOrigin-RevId: 1f49958e5570f61c1614d18c3af57a871399fd83

--- grazie/src/main/kotlin/com/intellij/grazie/grammar/GrammarEngine.kt
+++ grazie/src/main/kotlin/com/intellij/grazie/grammar/GrammarEngine.kt
@@ -16,7 +16,7 @@ object GrammarEngine {
   private const val maxChars = 10_000
   private const val minChars = 2
 
-  private val separators = listOf('?', '!', '.', ';', ',', ' ', '\t')
+  private val separators = listOf('?', '!', '.', ';', ',', '\n', ' ', '\t')
 
   private const val minNumberOfWords = 3
 
