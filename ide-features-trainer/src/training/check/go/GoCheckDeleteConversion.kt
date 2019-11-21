6c024fdb2e942e00b9ae5e3421dec434a80d1aa5
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-11-21 16:41:24 +0100

    [IFT] extract GoCheck to reuse common code
    (cherry picked from commit d1ac8449f683748e9197bc12f0d3f09808496a97)
    
    GitOrigin-RevId: 3d776be41dd030cce6162cc98a6e1d2038badd98

--- ide-features-trainer/src/training/check/go/GoCheckDeleteConversion.kt
+++ ide-features-trainer/src/training/check/go/GoCheckDeleteConversion.kt
@@ -1,31 +1,9 @@
 package training.check.go
 
-import com.goide.psi.GoFile
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import training.check.Check
+class GoCheckDeleteConversion : GoCheck() {
 
-class GoCheckDeleteConversion : Check {
-
-  private lateinit var project: Project
-  private lateinit var editor: Editor
-
-  override fun set(project: Project, editor: Editor) {
-    this.project = project
-    this.editor = editor
-  }
-
-  override fun before() {}
-
-  override fun check(): Boolean {
-    val document = editor.document
-    val manager = PsiDocumentManager.getInstance(project)
-    val file = manager.getPsiFile(document) as? GoFile ?: return false
-    val block = file.functions.find { it.name == "main" }?.block ?: return false
-    return block.text == "{\n" +
-            "\t_ = ioutil.WriteFile(\"./out.txt\", getData(), 0644)\n" +
-            "}"
-  }
+  override fun check(): Boolean = goFile?.findFunction("main")?.block?.text == "{\n" +
+          "\t_ = ioutil.WriteFile(\"./out.txt\", getData(), 0644)\n" +
+          "}"
 
 }
--- ide-features-trainer/src/training/check/go/GoCheckFillStructFields.kt
+++ ide-features-trainer/src/training/check/go/GoCheckFillStructFields.kt
@@ -1,36 +1,14 @@
 package training.check.go
 
-import com.goide.psi.GoFile
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import training.check.Check
+class GoCheckFillStructFields : GoCheck() {
 
-class GoCheckFillStructFields : Check {
-
-  private lateinit var project: Project
-  private lateinit var editor: Editor
-
-  override fun set(project: Project, editor: Editor) {
-    this.project = project
-    this.editor = editor
-  }
-
-  override fun before() {}
-
-  override fun check(): Boolean {
-    val document = editor.document
-    val manager = PsiDocumentManager.getInstance(project)
-    val file = manager.getPsiFile(document) as? GoFile ?: return false
-    val block = file.functions.find { it.name == "main" }?.block ?: return false
-    return block.text == "{\n" +
-            "\tp := Person{\n" +
-            "\t\tname:    \"\",\n" +
-            "\t\tage:     0,\n" +
-            "\t\taddress: Address{},\n" +
-            "\t}\n" +
-            "\tfmt.Println(p)\n" +
-            "}"
-  }
+  override fun check(): Boolean = goFile?.findFunction("main")?.block?.text == "{\n" +
+          "\tp := Person{\n" +
+          "\t\tname:    \"\",\n" +
+          "\t\tage:     0,\n" +
+          "\t\taddress: Address{},\n" +
+          "\t}\n" +
+          "\tfmt.Println(p)\n" +
+          "}"
 
 }
