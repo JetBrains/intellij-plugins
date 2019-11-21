e603b15c660071a6c7e0fd3eefb9b464c35706f3
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-11-21 16:41:24 +0100

    [IFT] extract GoCheck to reuse common code
    (cherry picked from commit d1ac8449f683748e9197bc12f0d3f09808496a97)
    
    GitOrigin-RevId: f9c15a3e9c78bc84f36d5aab502af8e88f8aa482

--- ide-features-trainer/src/training/check/go/GoCheckImplementsStringer.kt
+++ ide-features-trainer/src/training/check/go/GoCheckImplementsStringer.kt
@@ -3,29 +3,8 @@
  */
 package training.check.go
 
-import com.goide.psi.GoFile
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiDocumentManager
-import training.check.Check
+class GoCheckImplementsStringer : GoCheck() {
 
-class GoCheckImplementsStringer : Check {
-
-  private lateinit var project: Project
-  private lateinit var editor: Editor
-
-  override fun set(project: Project, editor: Editor) {
-    this.project = project
-    this.editor = editor
-  }
-
-  override fun before() {}
-
-  override fun check(): Boolean {
-    val document = editor.document
-    val manager = PsiDocumentManager.getInstance(project)
-    val file = manager.getPsiFile(document) as? GoFile ?: return false
-    return file.methods.any { it.name == "String" }
-  }
+  override fun check(): Boolean = goFile?.findMethod("String") != null
 
 }
