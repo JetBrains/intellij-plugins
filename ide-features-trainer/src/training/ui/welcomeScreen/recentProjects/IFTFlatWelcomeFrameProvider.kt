dce7f6d489289744411f68cea8fcf724602e548f
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2019-11-22 15:06:30 +0100

    IDEA-CR-55773: [IFT] use registry keys for showing learn action on welcome frame, show action tree for tutorials and projects (IFT-4)
    (cherry picked from commit 9edb5b7829bfcfd7aa03000ebcd50f8aebd92a13)
    
    GitOrigin-RevId: e66fcb07b117dd1d68c791c28b5933921db14194

--- ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTFlatWelcomeFrameProvider.kt
+++ ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTFlatWelcomeFrameProvider.kt
@@ -1,8 +1,19 @@
 package training.ui.welcomeScreen.recentProjects
 
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.Constraints
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.wm.IdeFrame
 import com.intellij.openapi.wm.WelcomeFrameProvider
+import training.actions.StartLearnAction
 
 class IFTFlatWelcomeFrameProvider : WelcomeFrameProvider {
-  override fun createFrame(): IdeFrame = IFTFlatWelcomeFrame()
+  override fun createFrame(): IdeFrame {
+    if (Registry.`is`("ideFeaturesTrainer.welcomeScreen.showLearnAction")) {
+      val groupQuickStart = ActionManager.getInstance().getAction("WelcomeScreen.QuickStart")
+      (groupQuickStart as DefaultActionGroup).addAction(StartLearnAction(), Constraints.LAST, ActionManager.getInstance())
+    }
+    return IFTFlatWelcomeFrame()
+  }
 }
