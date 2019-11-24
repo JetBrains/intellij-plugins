b10f9753c5ceb09c4251dbab5a501d4c910569eb
Leonid Shalupov <leonid@shalupov.com>
2019-11-24 15:46:18 +0200

    cloudFormation: handle possible exceptions in CloudFormationLanguageSubstitutor
    GitOrigin-RevId: ebd2f53dd326188e300c5c2e170c6ca334e1311d

--- CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationLanguageSubstitutor.kt
+++ CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationLanguageSubstitutor.kt
@@ -3,15 +3,25 @@ package com.intellij.aws.cloudformation
 import com.google.common.primitives.Bytes
 import com.intellij.json.JsonLanguage
 import com.intellij.lang.Language
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.LanguageSubstitutor
 import org.jetbrains.yaml.YAMLLanguage
+import java.io.FileNotFoundException
 
 class CloudFormationLanguageSubstitutor: LanguageSubstitutor() {
   override fun getLanguage(file: VirtualFile, project: Project): Language? {
-    val bytes = FileUtil.loadFirstAndClose(file.inputStream, 10024)
+    val bytes = try {
+      FileUtil.loadFirstAndClose(file.inputStream, 10 * 1024)
+    } catch (_: FileNotFoundException) {
+      return null
+    } catch (t: Throwable) {
+      Logger.getInstance(javaClass).warn("Unable to read first bytes of file ${file.path}", t)
+      return null
+    }
+
     if (Bytes.indexOf(bytes, bytes1) >= 0 || Bytes.indexOf(bytes, bytes2) >= 0) {
       val string = String(bytes, Charsets.UTF_8)
       if (CloudFormationFileTypeDetector.isJson(string)) {
