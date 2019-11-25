32057ba236e40a4d4bd2927041337048729c0e4b
Sergey Karashevich <sergey.karashevich@jetbrains.com>
2019-11-25 14:33:26 +0100

    [IFT] add statistics (IFT-4), (IFT-7)
    GitOrigin-RevId: 53c39c257e56da68c66f1c53e913d6f75034d0b3

--- ide-features-trainer/src/training/statistic/FeatureUsageStatisticConsts.kt
+++ ide-features-trainer/src/training/statistic/FeatureUsageStatisticConsts.kt
@@ -9,4 +9,17 @@ object FeatureUsageStatisticConsts {
   const val DURATION = "duration"
   const val START = "start"
   const val PASSED = "passed"
+  const val GROUP_EVENT = "group_event"
+  const val GROUP_NAME = "group_name"
+  const val GROUP_STATE = "group_state"
+  const val GROUP_EXPANDED = "expanded"
+  const val GROUP_COLLAPSED = "collapsed"
+  const val START_MODULE_ACTION = "start_module_action"
+  const val MODULE_NAME = "module_name"
+  const val PROGRESS_PERCENTAGE = "progress_percentage"
+}
+
+enum class GroupNames {
+  TUTORIALS,
+  PROJECTS
 }
