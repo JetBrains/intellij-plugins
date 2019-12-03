357a629a7113ff7e6985ffa4e75d0d2945009dd3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2019-12-03 17:11:09 +0100

    run dynamically registered dumb-aware post startup activities not in EDT
    GitOrigin-RevId: c7e7799da7ee1dce561ba6fe8fb7df71523852de

--- prettierJS/src/com/intellij/prettierjs/PrettierCompatibleCodeStyleInstaller.java
+++ prettierJS/src/com/intellij/prettierjs/PrettierCompatibleCodeStyleInstaller.java
@@ -1,8 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.prettierjs;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.module.Module;
-import com.intellij.openapi.project.DumbAwareRunnable;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
 import com.intellij.openapi.util.Ref;
@@ -12,17 +12,20 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
 
-public class PrettierCompatibleCodeStyleInstaller implements DirectoryProjectConfigurator {
-
+final class PrettierCompatibleCodeStyleInstaller implements DirectoryProjectConfigurator {
   @Override
   public void configureProject(@NotNull Project project, @NotNull VirtualFile baseDir, @NotNull Ref<Module> moduleRef, boolean newProject) {
-    if (project.isDefault() || project.getBaseDir() == null || project.isDisposed()) return;
-    StartupManager.getInstance(project).runWhenProjectIsInitialized((DumbAwareRunnable)() -> installCodeStyle(project));
+    if (project.isDefault() || project.getBaseDir() == null || project.isDisposed()) {
+      return;
+    }
+    StartupManager.getInstance(project).runAfterOpened(() -> {
+      ApplicationManager.getApplication().runReadAction(() -> installCodeStyle(project));
+    });
   }
 
   private static void installCodeStyle(@NotNull Project project) {
     VirtualFile configVFile = PrettierUtil.findSingleConfigInContentRoots(project);
-    final PsiFile configPsiFile = configVFile != null ? PsiManager.getInstance(project).findFile(configVFile) : null;
+    PsiFile configPsiFile = configVFile != null ? PsiManager.getInstance(project).findFile(configVFile) : null;
     if (configPsiFile != null) {
       new PrettierCodeStyleImporter(true).importConfigFileWhenToolInstalled(configPsiFile);
     }
