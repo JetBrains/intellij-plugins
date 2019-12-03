187c985db20280141e2c84a44152467d8ebdffdd
Pavel Bakhvalov <pavel.bakhvalov@jetbrains.com>
2019-12-03 22:48:13 +0300

    Add support of XML comments
    GitOrigin-RevId: b4af1d03c4b588eeaa72ed2277792a116e2ee5bd

--- grazie/src/main/kotlin/com/intellij/grazie/ide/language/xml/XmlGrammarCheckingStrategy.kt
+++ grazie/src/main/kotlin/com/intellij/grazie/ide/language/xml/XmlGrammarCheckingStrategy.kt
@@ -2,7 +2,8 @@
 package com.intellij.grazie.ide.language.xml
 
 import com.intellij.grazie.grammar.strategy.GrammarCheckingStrategy
-import com.intellij.grazie.grammar.strategy.impl.RuleGroup
+import com.intellij.grazie.grammar.strategy.impl.ReplaceCharRule
+import com.intellij.grazie.utils.Text
 import com.intellij.grazie.utils.isAtEnd
 import com.intellij.grazie.utils.isAtStart
 import com.intellij.psi.PsiElement
@@ -11,9 +12,12 @@ import com.intellij.psi.xml.XmlToken
 import com.intellij.psi.xml.XmlTokenType
 
 class XmlGrammarCheckingStrategy : GrammarCheckingStrategy {
-  override fun isMyContextRoot(element: PsiElement) = element is XmlText || (element is XmlToken && element.tokenType == XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN)
+  override fun isMyContextRoot(element: PsiElement) = element is XmlText ||
+    (element is XmlToken && element.tokenType in setOf(XmlTokenType.XML_ATTRIBUTE_VALUE_TOKEN, XmlTokenType.XML_COMMENT_CHARACTERS))
 
   override fun isTypoAccepted(root: PsiElement, typoRange: IntRange, ruleRange: IntRange) = !typoRange.isAtStart(root) && !typoRange.isAtEnd(root)
 
-  override fun getIgnoredRuleGroup(root: PsiElement, child: PsiElement) = RuleGroup.WHITESPACES
+  override fun getReplaceCharRules(root: PsiElement) = emptyList<ReplaceCharRule>()
+
+  override fun getStealthyRanges(root: PsiElement, text: CharSequence) = Text.indentIndexes(text, setOf(' '))
 }
