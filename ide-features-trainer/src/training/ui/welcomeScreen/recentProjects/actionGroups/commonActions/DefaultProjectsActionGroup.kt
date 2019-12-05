9c282622eda9e4ac33bedba23072f7eec24c724b
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-12-05 11:07:44 +0100

    IDEA-CR-56232: [IFT] IDEA-228114 use RecentProjectListActionProvider instead of RecentDirectoryProjectsManager
    (cherry picked from commit 25d0e4dcd231f838b94a9a8685ccf15672b32dd6)
    
    GitOrigin-RevId: f5a00df77659cef41eb7938e9e2ae2f8394f8b07

--- ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/actionGroups/commonActions/DefaultProjectsActionGroup.kt
+++ ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/actionGroups/commonActions/DefaultProjectsActionGroup.kt
@@ -1,17 +1,17 @@
 package training.ui.welcomeScreen.recentProjects.actionGroups.commonActions
 
 import com.intellij.openapi.actionSystem.AnAction
-import training.ui.welcomeScreen.recentProjects.IFTRecentProjectsManager
+import training.ui.welcomeScreen.recentProjects.IFTRecentProjectListActionProvider
 import training.ui.welcomeScreen.recentProjects.actionGroups.CommonActionGroup
 
 class DefaultProjectsActionGroup : CommonActionGroup("Projects", emptyList()) {
 
   override fun getActions(): Array<AnAction> {
-    val elements = IFTRecentProjectsManager.manager
-      .getOriginalRecentProjectsActions()
-    if (elements.isNullOrEmpty())
-      return emptyArray()
-    if (!isExpanded) return arrayOf(this)
-    return arrayOf(this, *elements)
+    val elements = IFTRecentProjectListActionProvider.instance.getOriginalRecentProjectsActions()
+    return when {
+      elements.isEmpty() -> emptyArray()
+      !isExpanded -> arrayOf(this)
+      else -> arrayOf(this, *elements)
+    }
   }
 }
