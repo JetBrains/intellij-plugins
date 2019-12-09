d08e9a24db3a3da1ac38325a19ffa94dec4cd7df
Max Medvedev <maxim.medvedev@jetbrains.com>
2019-12-09 16:28:40 +0300

    Extract extension point for file type statistic providers; remove duplicating code
    "com.intellij.fileTypeStatisticProvider" extension point introduced
    CloudFormation, Terraform & Clion-Swift's implementations are migrated to new API
    
    GitOrigin-RevId: 9885980846d999b0007da5987f7f5382b76dbd0e

--- CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationUpdateComponent.kt
+++ CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationUpdateComponent.kt
@@ -1,36 +1,3 @@
-package com.intellij.aws.cloudformation
-
-import com.intellij.ide.plugins.PluginManagerCore
-import com.intellij.ide.util.PropertiesComponent
-import com.intellij.json.JsonFileType
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.PermanentInstallationID
-import com.intellij.openapi.application.ex.ApplicationInfoEx
-import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.EditorFactory
-import com.intellij.openapi.editor.event.EditorFactoryEvent
-import com.intellij.openapi.editor.event.EditorFactoryListener
-import com.intellij.openapi.editor.ex.EditorEx
-import com.intellij.openapi.editor.ex.FocusChangeListener
-import com.intellij.openapi.extensions.PluginId
-import com.intellij.openapi.fileEditor.FileDocumentManager
-import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
-import com.intellij.openapi.util.JDOMUtil
-import com.intellij.openapi.util.SystemInfo
-import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiManager
-import com.intellij.util.io.HttpRequests
-import org.jdom.JDOMException
-import org.jetbrains.yaml.YAMLFileType
-import java.io.IOException
-import java.net.URLEncoder
-import java.net.UnknownHostException
-import java.util.concurrent.TimeUnit
-
 /*
  * Copyright 2000-2016 JetBrains s.r.o.
  *
@@ -46,120 +13,47 @@ import java.util.concurrent.TimeUnit
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * Based on org.rust.ide.update.UpdateComponent
- */
-class CloudFormationUpdateComponent : Disposable {
-  init {
-    Disposer.register(ApplicationManager.getApplication(), this)
-    val pluginId = PluginManagerCore.getPluginByClassName(this.javaClass.name)
-    val descriptor = PluginManagerCore.getPlugin(pluginId)
-    ourCurrentPluginVersion = descriptor?.version
 
-    val application = ApplicationManager.getApplication()
-    if (!application.isUnitTestMode) {
-      EditorFactory.getInstance().addEditorFactoryListener(EditorListener(this), this)
+package com.intellij.aws.cloudformation
+
+import com.intellij.internal.statistic.fileTypes.FileTypeStatisticProvider
+import com.intellij.json.JsonFileType
+import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.editor.event.EditorFactoryEvent
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.psi.PsiManager
+import org.jetbrains.yaml.YAMLFileType
+
+class CloudFormationFileTypeStatisticProvider : FileTypeStatisticProvider {
+  private val LOG = logger<CloudFormationFileTypeStatisticProvider>()
+
+  override fun getPluginId(): String = "AWSCloudFormation"
+
+  override fun accept(event: EditorFactoryEvent, fileType: FileType): Boolean {
+    if (fileType === YamlCloudFormationFileType.INSTANCE ||
+        fileType === JsonCloudFormationFileType.INSTANCE) {
+      return true
     }
+
+    return checkYamlOrJson(fileType, event)
   }
 
-  override fun dispose() {
-  }
+  private fun checkYamlOrJson(fileType: FileType,
+                              event: EditorFactoryEvent): Boolean {
+    if (!(fileType === YAMLFileType.YML || fileType === JsonFileType.INSTANCE)) return false
 
-  private class EditorListener(val parentDisposable: Disposable) : EditorFactoryListener {
-    private fun checkPsi(project: Project, file: VirtualFile): Boolean {
-      if (file.fileType === YamlCloudFormationFileType.INSTANCE ||
-          file.fileType === JsonCloudFormationFileType.INSTANCE)
-        return true
+    val document = event.editor.document
+    val file = FileDocumentManager.getInstance().getFile(document) ?: return false
+    val project = event.editor.project ?: return false
 
-      try {
-        val psiFile = PsiManager.getInstance(project).findFile(file)
-        return psiFile != null && CloudFormationPsiUtils.isCloudFormationFile(psiFile)
-      } catch (t: Throwable) {
-        LOG.debug("Unable to detect whether file ${file.path} is CloudFormation file")
-        return false
-      }
+    return try {
+      val psiFile = PsiManager.getInstance(project).findFile(file)
+      psiFile != null && CloudFormationPsiUtils.isCloudFormationFile(psiFile)
     }
-
-    override fun editorCreated(event: EditorFactoryEvent) {
-      val document = event.editor.document
-      val file = FileDocumentManager.getInstance().getFile(document)
-      val project = event.editor.project
-      if (project != null && file != null && file.fileType in FILE_TYPES && checkPsi(project, file)) {
-        updateOnPooledThread()
-
-        (event.editor as? EditorEx)?.addFocusListener(object : FocusChangeListener {
-          override fun focusGained(editor: Editor) {
-            updateOnPooledThread()
-          }
-
-          override fun focusLost(editor: Editor) { }
-        }, parentDisposable)
-      }
-    }
-  }
-
-  companion object {
-    private val lock: Any = Any()
-    private const val PLUGIN_ID: String = "AWSCloudFormation"
-    private const val LAST_UPDATE: String = "$PLUGIN_ID.LAST_UPDATE"
-    private const val LAST_VERSION: String = "$PLUGIN_ID.LAST_VERSION"
-
-    private val FILE_TYPES: Set<FileType> = setOf(
-        YAMLFileType.YML,
-        JsonFileType.INSTANCE,
-        JsonCloudFormationFileType.INSTANCE,
-        YamlCloudFormationFileType.INSTANCE
-    )
-
-    private val LOG = Logger.getInstance(CloudFormationUpdateComponent::class.java)
-
-    private var ourCurrentPluginVersion: String? = null
-
-    fun updateOnPooledThread() = ApplicationManager.getApplication().executeOnPooledThread { update() }
-
-    fun update() {
-      val properties = PropertiesComponent.getInstance()
-      synchronized(lock) {
-        val lastPluginVersion = properties.getValue(LAST_VERSION)
-        val lastUpdate = properties.getOrInitLong(LAST_UPDATE, 0L)
-        val shouldUpdate = lastUpdate == 0L
-            || System.currentTimeMillis() - lastUpdate > TimeUnit.DAYS.toMillis(1)
-            || lastPluginVersion == null
-            || lastPluginVersion != ourCurrentPluginVersion
-        if (shouldUpdate) {
-          properties.setValue(LAST_UPDATE, System.currentTimeMillis().toString())
-          properties.setValue(LAST_VERSION, ourCurrentPluginVersion)
-        } else {
-          return
-        }
-      }
-
-      val url = updateUrl
-      try {
-        HttpRequests.request(url).connect {
-          try {
-            JDOMUtil.load(it.reader)
-          } catch (e: JDOMException) {
-            LOG.warn(e)
-          }
-          LOG.info("updated: $url")
-        }
-      } catch (ignored: UnknownHostException) {
-        // No internet connections, no need to log anything
-      } catch (e: IOException) {
-        LOG.warn(e)
-      }
-    }
-
-    private val updateUrl: String get() {
-      val applicationInfo = ApplicationInfoEx.getInstanceEx()
-      val buildNumber = applicationInfo.build.asString()
-      val plugin = PluginManagerCore.getPlugin(PluginId.getId(PLUGIN_ID))!!
-      val pluginId = plugin.pluginId.idString
-      val os = URLEncoder.encode("${SystemInfo.OS_NAME} ${SystemInfo.OS_VERSION}", Charsets.UTF_8.name())
-      val uid = PermanentInstallationID.get()
-      val baseUrl = "https://plugins.jetbrains.com/plugins/list"
-      return "$baseUrl?pluginId=$pluginId&build=$buildNumber&pluginVersion=${plugin.version}&os=$os&uuid=$uid"
+    catch (t: Throwable) {
+      LOG.debug("Unable to detect whether file ${file.path} is CloudFormation file")
+      false
     }
   }
 }
