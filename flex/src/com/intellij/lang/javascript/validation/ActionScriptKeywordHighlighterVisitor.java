6a0842a895aea49793bfc9cfa87464f9d1edb996
Alexey Kudravtsev <cdr@intellij.com>
2019-12-17 15:32:14 +0300

    remove unnecessary AnnotationHolder in JS inspections
    GitOrigin-RevId: 9c8100fc471d09a7e5313109723094510ccc11b0

--- flex/src/com/intellij/lang/javascript/validation/ActionScriptKeywordHighlighterVisitor.java
+++ flex/src/com/intellij/lang/javascript/validation/ActionScriptKeywordHighlighterVisitor.java
@@ -1,8 +1,8 @@
 package com.intellij.lang.javascript.validation;
 
 
+import com.intellij.codeInsight.daemon.impl.analysis.HighlightInfoHolder;
 import com.intellij.lang.actionscript.psi.impl.ActionScriptGotoStatementImpl;
-import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.javascript.JSTokenTypes;
 import com.intellij.lang.javascript.highlighting.ECMAL4Highlighter;
 import com.intellij.lang.javascript.psi.JSFunction;
@@ -10,7 +10,7 @@ import com.intellij.lang.javascript.psi.JSStatementWithLabelReference;
 import org.jetbrains.annotations.NotNull;
 
 public class ActionScriptKeywordHighlighterVisitor extends JSKeywordHighlighterVisitor {
-  public ActionScriptKeywordHighlighterVisitor(AnnotationHolder holder) {
+  public ActionScriptKeywordHighlighterVisitor(HighlightInfoHolder holder) {
     super(holder, new ECMAL4Highlighter());
   }
 
