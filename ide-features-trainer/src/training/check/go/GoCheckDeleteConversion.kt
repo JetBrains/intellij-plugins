7f0eb826b15fda882a34bdff69ac578ef245d5ab
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2019-12-18 15:37:13 +0300

    [ITF] Remove Go from IFT plugin
    GitOrigin-RevId: 22bf75e782aa27b9f19fec4642a59dac9077b2e9

--- ide-features-trainer/src/training/check/go/GoCheckDeleteConversion.kt
+++
@@ -1,9 +0,0 @@
-package training.check.go
-
-class GoCheckDeleteConversion : GoCheck() {
-
-  override fun check(): Boolean = goFile?.findFunction("main")?.block?.text == "{\n" +
-          "\t_ = ioutil.WriteFile(\"./out.txt\", getData(), 0644)\n" +
-          "}"
-
-}
