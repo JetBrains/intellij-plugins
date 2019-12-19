8960b6e221cb70c70590050740c518a45940f6f2
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-19 17:19:52 +0300

    more bundles
    GitOrigin-RevId: 345a735e92f8eabe5ba6afce218aa1b3a6abc677

--- AngularJS/src/org/angular2/lang/Angular2Bundle.java
+++ AngularJS/src/org/angular2/lang/Angular2Bundle.java
@@ -1,33 +1,19 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.lang;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
-
-public class Angular2Bundle {
-
-  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
+public class Angular2Bundle extends DynamicBundle {
   @NonNls public static final String BUNDLE = "messages.Angular2Bundle";
+  private static final Angular2Bundle INSTANCE = new Angular2Bundle();
 
-  private Angular2Bundle() {
-  }
+  private Angular2Bundle() { super(BUNDLE); }
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+  @NotNull
+  public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
+    return INSTANCE.getMessage(key, params);
   }
 }
--- Dart/src/com/jetbrains/lang/dart/DartBundle.java
+++ Dart/src/com/jetbrains/lang/dart/DartBundle.java
@@ -1,32 +1,18 @@
 package com.jetbrains.lang.dart;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+public class DartBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "com.jetbrains.lang.dart.DartBundle";
+  private static final DartBundle INSTANCE = new DartBundle();
 
-public class DartBundle {
+  private DartBundle() { super(BUNDLE); }
 
+  @NotNull
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
-  @NonNls public static final String BUNDLE = "com.jetbrains.lang.dart.DartBundle";
-
-  private DartBundle() {
-  }
-
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+    return INSTANCE.getMessage(key, params);
   }
 }
