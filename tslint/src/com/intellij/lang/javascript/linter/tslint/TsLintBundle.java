5aee85548a490c933eb6bc8233e46957eac408ab
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-19 23:05:54 +0300

    more bundles
    GitOrigin-RevId: 65962fd2f2158e1aaeed619497ec0545bbf8d2e8

--- tslint/src/com/intellij/lang/javascript/linter/tslint/TsLintBundle.java
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/TsLintBundle.java
@@ -1,34 +1,18 @@
 package com.intellij.lang.javascript.linter.tslint;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+public class TsLintBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "messages.TsLintBundle";
+  private static final TsLintBundle INSTANCE = new TsLintBundle();
 
+  private TsLintBundle() { super(BUNDLE); }
 
-public class TsLintBundle {
-
+  @NotNull
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  @NonNls
-  public static final String BUNDLE = "messages.TsLintBundle";
-  private static Reference<ResourceBundle> ourBundle;
-
-  private TsLintBundle() {
-  }
-
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+    return INSTANCE.getMessage(key, params);
   }
 }
