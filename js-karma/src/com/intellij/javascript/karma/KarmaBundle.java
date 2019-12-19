ed6409b8d646f60264bd620311d368b0b904e373
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-20 00:00:07 +0300

    more bundles
    GitOrigin-RevId: 24107d94985cc60fc0019864fb5b246b9ceb1338

--- js-karma/src/com/intellij/javascript/karma/KarmaBundle.java
+++ js-karma/src/com/intellij/javascript/karma/KarmaBundle.java
@@ -1,32 +1,18 @@
 package com.intellij.javascript.karma;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+public class KarmaBundle extends DynamicBundle {
+  @NonNls private static final String BUNDLE = "com.intellij.javascript.karma.KarmaBundle";
+  private static final KarmaBundle INSTANCE = new KarmaBundle();
 
-public class KarmaBundle {
+  private KarmaBundle() { super(BUNDLE); }
 
+  @NotNull
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
-  @NonNls public static final String BUNDLE = "com.intellij.javascript.karma.KarmaBundle";
-
-  private KarmaBundle() {
-  }
-
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+    return INSTANCE.getMessage(key, params);
   }
 }
