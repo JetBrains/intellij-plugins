5226f7e474ab8acaa949199dd9234886061e1b8d
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-12-20 11:37:04 +0100

    [IFT] add StartLearnAction to welcome screen when action manager is already initialized
    GitOrigin-RevId: 4a197992858ae3a150805921538b3b94a06140fa

--- ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTFlatWelcomeFrameProvider.kt
+++ ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTFlatWelcomeFrameProvider.kt
@@ -1,20 +1,8 @@
 package training.ui.welcomeScreen.recentProjects
 
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.Constraints
-import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.wm.IdeFrame
 import com.intellij.openapi.wm.WelcomeFrameProvider
-import training.actions.StartLearnAction
 
 class IFTFlatWelcomeFrameProvider : WelcomeFrameProvider {
-  override fun createFrame(): IdeFrame {
-    if (!showCustomWelcomeScreen) {
-      val actionLinksGroup = ActionManager.getInstance().getAction("WelcomeScreen.QuickStart") as DefaultActionGroup
-      if (actionLinksGroup.getChildren(null).none { it is StartLearnAction }) {
-        actionLinksGroup.addAction(StartLearnAction(), Constraints.LAST, ActionManager.getInstance())
-      }
-    }
-    return IFTFlatWelcomeFrame()
-  }
+  override fun createFrame(): IdeFrame = IFTFlatWelcomeFrame()
 }
---
+++ ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTWelcomeScreenActionCustomizer.kt
@@ -0,0 +1,18 @@
+package training.ui.welcomeScreen.recentProjects
+
+import com.intellij.openapi.actionSystem.ActionManager
+import com.intellij.openapi.actionSystem.Constraints
+import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.intellij.openapi.actionSystem.impl.ActionConfigurationCustomizer
+import training.actions.StartLearnAction
+
+class IFTWelcomeScreenActionCustomizer : ActionConfigurationCustomizer {
+  override fun customize(actionManager: ActionManager) {
+    if (!showCustomWelcomeScreen) {
+      val actionLinksGroup = actionManager.getAction("WelcomeScreen.QuickStart") as DefaultActionGroup
+      if (actionLinksGroup.getChildren(null, actionManager).none { it is StartLearnAction }) {
+        actionLinksGroup.addAction(StartLearnAction(), Constraints.LAST, actionManager)
+      }
+    }
+  }
+}
