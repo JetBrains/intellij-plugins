b9ee24b244e6dead282782d275ce934b8c6b0807
Artem Khvastunov <artem.khvastunov@jetbrains.com>
2019-12-20 12:43:06 +0100

    [IFT] switch LearnBundle to DynamicBundle
    GitOrigin-RevId: de2575bf591b4ed0f9e5e1579f05cbb06c73f5b4

--- ide-features-trainer/src/training/learn/LearnBundle.kt
+++ ide-features-trainer/src/training/learn/LearnBundle.kt
@@ -1,38 +1,19 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn
 
-import com.intellij.CommonBundle
-import com.intellij.reference.SoftReference
+import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import java.lang.ref.Reference
-import java.util.*
+
+@NonNls
+private const val BUNDLE = "training.learn.LearnBundle"
 
 open class BundlePlace(val bundleAppendix: String)
 
-object LearnBundle {
+object LearnBundle : DynamicBundle(BUNDLE) {
 
-  private var ourBundle: Reference<ResourceBundle>? = null
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
 
-  @NonNls
-  private const val BUNDLE = "training.learn.LearnBundle"
-
-  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
-    return CommonBundle.message(bundle, key, *params)
-  }
-
-  fun messageInPlace(@PropertyKey(resourceBundle = BUNDLE) key: String, bundlePlace: BundlePlace, vararg params: Any): String {
-    return CommonBundle.message(bundle, key + bundlePlace.bundleAppendix, *params)
-  }
-
-  // Cached loading
-  private val bundle: ResourceBundle
-    get() {
-      var bundle = SoftReference.dereference(ourBundle)
-      if (bundle == null) {
-        bundle = ResourceBundle.getBundle(BUNDLE)
-        ourBundle = SoftReference(bundle)
-      }
-      return bundle!!
-    }
+  fun messageInPlace(@PropertyKey(resourceBundle = BUNDLE) key: String, bundlePlace: BundlePlace, vararg params: Any): String =
+    getMessage(key + bundlePlace.bundleAppendix, *params)
 }
