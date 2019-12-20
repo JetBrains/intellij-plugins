35df1a6cbfb2e66252b900900b9142058c8183f2
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-20 13:08:04 +0300

    more bundles
    GitOrigin-RevId: bbea950072f383951c48ff9c842387dda4469428

--- prettierJS/src/com/intellij/prettierjs/PrettierBundle.java
+++ prettierJS/src/com/intellij/prettierjs/PrettierBundle.java
@@ -1,32 +1,18 @@
 package com.intellij.prettierjs;
 
-import com.intellij.CommonBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
+public class PrettierBundle extends DynamicBundle {
+  @NonNls public static final String BUNDLE = "PrettierBundle";
+  private static final PrettierBundle INSTANCE = new PrettierBundle();
 
-public class PrettierBundle {
-  @NonNls
-  public static final String BUNDLE = "PrettierBundle";
-  private static Reference<ResourceBundle> ourBundle;
-  
-  private PrettierBundle() {
-  }
+  private PrettierBundle() { super(BUNDLE); }
 
+  @NotNull
   public static String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, @NotNull Object... params) {
-    return CommonBundle.message(getBundle(), key, params);
-  }
-
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+    return INSTANCE.getMessage(key, params);
   }
 }
