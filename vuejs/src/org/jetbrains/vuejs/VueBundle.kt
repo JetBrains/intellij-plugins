b172a19f28ebb51f972f0f6266e9ba956131dbf5
Sergey Ignatov <sergey.ignatov@jetbrains.com>
2019-12-21 15:57:39 +0300

    vue bundle
    GitOrigin-RevId: 14d35a79a83269d1cd25256c988444c9c0b6031e

--- vuejs/src/org/jetbrains/vuejs/VueBundle.kt
+++ vuejs/src/org/jetbrains/vuejs/VueBundle.kt
@@ -1,32 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs
 
-import com.intellij.CommonBundle
+import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
-import java.lang.ref.Reference
-import java.lang.ref.SoftReference
-import java.util.*
 
-class VueBundle {
-  companion object {
-    private const val BUNDLE = "messages.VueBundle"
-
-    fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
-      val bundle = getBundle() ?: return ""
-      return CommonBundle.message(bundle, key, *params)
-    }
-
-    private var ourBundle: Reference<ResourceBundle>? = null
-    @NonNls
-
-    private fun getBundle(): ResourceBundle? {
-      var bundle = com.intellij.reference.SoftReference.dereference(ourBundle)
-      if (bundle == null) {
-        bundle = ResourceBundle.getBundle(BUNDLE)
-        ourBundle = SoftReference(bundle)
-      }
-      return bundle
-    }
-  }
+@NonNls private const val BUNDLE = "messages.VueBundle"
+object VueBundle : DynamicBundle(BUNDLE) {
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
 }
