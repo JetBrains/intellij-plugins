56df91beda0796027346cf6cc5001f8d50b85fa8
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-12-25 17:34:15 +0300

    Don't call DartAnalysisServerService.ensureAnalysisRootsUpToDate() when roots are definitely up-to-date.
    GitOrigin-RevId: 935c49592530f5e0de7585249d65c381db46382a

--- Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartProblemsView.java
+++ Dart/src/com/jetbrains/lang/dart/ide/errorTreeView/DartProblemsView.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.errorTreeView;
 
 import com.intellij.execution.runners.ExecutionUtil;
@@ -278,7 +278,9 @@ public class DartProblemsView implements PersistentStateComponent<DartProblemsVi
       }
     }
 
-    DartAnalysisServerService.getInstance(myProject).ensureAnalysisRootsUpToDate();
+    if (myPresentationHelper.getScopedAnalysisMode() == DartProblemsViewSettings.ScopedAnalysisMode.DartPackage) {
+      DartAnalysisServerService.getInstance(myProject).ensureAnalysisRootsUpToDate();
+    }
   }
 
   public void updateErrorsForFile(@NotNull final String filePath, @NotNull final List<AnalysisError> errors) {
