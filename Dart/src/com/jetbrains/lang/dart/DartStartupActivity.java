ec6e2bcb932cc7a58bb70ef6ae231cd4e74a9e81
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2019-12-25 16:39:14 +0300

    DartStartupActivity: quick exit if there's no Dart in project.
    (cherry picked from commit 23e9f884408383d658e2495dee695a5c485ab11e)
    
    GitOrigin-RevId: 32c8ccfd23ee4e0cc11499c1110f4413f715d389

--- Dart/src/com/jetbrains/lang/dart/DartStartupActivity.java
+++ Dart/src/com/jetbrains/lang/dart/DartStartupActivity.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart;
 
 import com.intellij.ProjectTopics;
@@ -50,7 +50,9 @@ public class DartStartupActivity implements StartupActivity {
       }
     }
 
-    DartFileListener.scheduleDartPackageRootsUpdate(project);
+    if (!pubspecYamlFiles.isEmpty()) {
+      DartFileListener.scheduleDartPackageRootsUpdate(project);
+    }
   }
 
   public static void excludeBuildAndPackagesFolders(final @NotNull Module module, final @NotNull VirtualFile pubspecYamlFile) {
