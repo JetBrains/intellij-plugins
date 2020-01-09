f2f639db235013b52963bcbd07c4b7ab551380d8
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-01-09 13:19:27 +0300

    check project.isDisposed() in DartAnalysisServerMessages
    (cherry picked from commit ec2ea636dde0cfddbe701c076b56bb170e058599)
    
    (cherry picked from commit 507c7c77ddb76923fe2818c9bceace8c2d7edd66)
    
    GitOrigin-RevId: 17bb3b75ec6453d3841aa56ef943a3d57782674c

--- Dart/src/com/jetbrains/lang/dart/analyzer/DartAnalysisServerMessages.java
+++ Dart/src/com/jetbrains/lang/dart/analyzer/DartAnalysisServerMessages.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.analyzer;
 
 import com.intellij.openapi.project.Project;
@@ -18,6 +19,8 @@ public class DartAnalysisServerMessages {
   }
 
   public static void sendAnalysisStarted(@NotNull Project project, boolean isStarting) {
+    if (project.isDisposed()) return;
+
     final MessageBus bus = project.getMessageBus();
     final DartAnalysisNotifier publisher = bus.syncPublisher(DART_ANALYSIS_TOPIC);
     if (isStarting) {
