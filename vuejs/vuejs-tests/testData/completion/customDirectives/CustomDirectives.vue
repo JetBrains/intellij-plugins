0f1c8814311742d8d4d1afaceaaa1caa385c6ca8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-01-15 19:15:01 +0100

    WEB-42213 No completion of custom directives in component with split template and script
    GitOrigin-RevId: d85c65e62fcf1b347238c7e4d70cf8fde9405a27

---
+++ vuejs/vuejs-tests/testData/completion/customDirectives/CustomDirectives.vue
@@ -0,0 +1,30 @@
+<template>
+  <label>
+    <input v-focus v-local-directive v-some-other-directive v-click-outside/>
+  </label>
+  <client-comp v-imported-directive></client-comp>
+  <div    style=""></div>
+</template>
+
+<script>
+  import importedDirective from './importedDirective'
+
+  let someOtherDirective = {
+    render: function() {
+
+    }
+  };
+  export default {
+    name: "client-comp",
+    directives: {
+      localDirective: {
+        // directive definition
+        inserted: function (el) {
+          el.focus()
+        }
+      },
+      someOtherDirective,
+      importedDirective
+    }
+  }
+</script>
