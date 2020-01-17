e615df9302c799a520088766e3c13eec06a8100b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-01-17 11:50:28 +0100

    IDEA-CR-57461: WEB-42209 Vue: ship core Vue libraries web-types with the plugin - post review changes
    IDEA-CR-57461
    
    (cherry picked from commit 3808c89d4e57b52053f9a43150828de576daab30)
    
    GitOrigin-RevId: 7e88261ee27e1af22ca44ab526dc756251867d3c

--- vuejs/src/org/jetbrains/vuejs/model/webtypes/registry/VueWebTypesJsonsCache.kt
+++ vuejs/src/org/jetbrains/vuejs/model/webtypes/registry/VueWebTypesJsonsCache.kt
@@ -4,10 +4,12 @@ package org.jetbrains.vuejs.model.webtypes.registry
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.StandardFileSystems
 import com.intellij.util.io.HttpRequests
 import org.apache.commons.compress.archivers.ArchiveEntry
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
+import org.jetbrains.vuejs.model.webtypes.registry.VueWebTypesRegistry.Companion.WEB_TYPES_FILE_SUFFIX
 import java.io.*
 import java.net.URI
 
@@ -16,11 +18,11 @@ class VueWebTypesJsonsCache {
   companion object {
 
     fun getWebTypesJson(url: String): InputStream {
-      if (url.endsWith("json") && url.startsWith("file:")) {
+      if (url.endsWith("json") && url.startsWith(StandardFileSystems.FILE_PROTOCOL_PREFIX)) {
         return FileInputStream(File(URI(url)))
       }
       val downloadedJson = File(PathManager.getSystemPath(),
-                                "web-types/" + File(url).nameWithoutExtension + ".web-types.json")
+                                "web-types/" + File(url).nameWithoutExtension + WEB_TYPES_FILE_SUFFIX)
       if (!downloadedJson.exists()) {
         downloadedJson.parentFile.mkdirs()
         val content = downloadWebTypesJson(url)
@@ -38,7 +40,7 @@ class VueWebTypesJsonsCache {
       val input = TarArchiveInputStream(gzi)
       var e: ArchiveEntry? = input.nextEntry
       while (e != null) {
-        if (e.name.endsWith(".web-types.json")) {
+        if (e.name.endsWith(WEB_TYPES_FILE_SUFFIX)) {
           if (input.canReadEntryData(e)) {
             return FileUtil.loadTextAndClose(input)
           }
