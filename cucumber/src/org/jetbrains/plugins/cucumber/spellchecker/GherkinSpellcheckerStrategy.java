a625dd3c84fd2244f0a7e25f7eb534ff168b00eb
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-21 16:33:08 +0100

    remove author (only some, where it is ok)
    GitOrigin-RevId: 7dbab7524fe4efc5d17a04b95dee88b6f80fbe8d

--- cucumber/src/org/jetbrains/plugins/cucumber/spellchecker/GherkinSpellcheckerStrategy.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/spellchecker/GherkinSpellcheckerStrategy.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.spellchecker;
 
 import com.intellij.lang.ASTNode;
@@ -12,9 +13,6 @@ import com.intellij.spellchecker.tokenizer.Tokenizer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.GherkinElementType;
 
-/**
- * @author oleg
- */
 public class GherkinSpellcheckerStrategy extends SpellcheckingStrategy {
   @NotNull
   @Override
