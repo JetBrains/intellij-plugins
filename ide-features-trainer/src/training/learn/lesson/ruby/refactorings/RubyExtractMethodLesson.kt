2ddc755d6e1cb5564a545dc598163d40b57ab1bc
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-01-22 21:41:29 +0300

    [IFT] Upgrade Ruby Extract Method lesson
    GitOrigin-RevId: d076c46e8c1eb07f86ea5e1c6554ee9b066e9b7f

--- ide-features-trainer/src/training/learn/lesson/ruby/refactorings/RubyExtractMethodLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/refactorings/RubyExtractMethodLesson.kt
@@ -1,13 +1,18 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn.lesson.ruby.refactorings
 
+import com.intellij.openapi.wm.IdeFrame
 import com.intellij.testGuiFramework.impl.button
+import com.intellij.ui.ComponentUtil
+import com.intellij.util.ui.UIUtil
 import training.commands.kotlin.TaskTestContext
 import training.lang.RubyLangSupport
 import training.learn.interfaces.Module
 import training.learn.lesson.kimpl.KLesson
 import training.learn.lesson.kimpl.LessonContext
 import training.learn.lesson.kimpl.parseLessonSample
+import javax.swing.JDialog
+import javax.swing.JTextPane
 
 class RubyExtractMethodLesson(module: Module) : KLesson("Extract Method", module, "ruby") {
   private val sample = parseLessonSample("""
@@ -38,29 +43,57 @@ class RubyExtractMethodLesson(module: Module) : KLesson("Extract Method", module
     get() = {
       prepareSample(sample)
 
-      task("ExtractMethod") {
-        text("Press ${action(it)} to extract the selected code block into a method.")
-        trigger(it, { editor.document.text }) { before, now ->
-          if (before != now) {
-            // a little bit hacky way to print additional message between refactoring modal dialogs
-            text("Cocktail Sort has 2 swap places. Click <strong>Yes</strong> to extract both of them and " +
-                 "then confirm the decision.")
-            true
-          }
-          else {
-            false
-          }
+      actionTask("ExtractMethod") {
+        "Press ${action(it)} to extract the selected code block into a method."
+      }
+      // Now will be open the first dialog
+
+      task {
+        text("Click <strong>Ok</strong> to start refactoring.")
+
+        // Wait until the second dialog
+        stateCheck {
+          val parentOfType = ComponentUtil.getParentOfType(JDialog::class.java, focusOwner)
+          // Only the second dialog has JTextPane. We do not need to check title
+          UIUtil.uiTraverser(parentOfType).traverse().filter(JTextPane::class.java).first() != null
         }
 
         test {
-          actions(it)
+          with(TaskTestContext.guiTestCase) {
+            dialog("Extract Method", needToKeepDialog=true) {
+              button("OK").click()
+            }
+          }
+        }
+      }
+
+      task {
+        text("Cocktail Sort has 2 swap places. The first fragment has just been extracted. Click <strong>Yes</strong> to extract both of them.")
+
+        // Wait until the third dialog
+        stateCheck {
+          val parentOfType = ComponentUtil.getParentOfType(JDialog::class.java, focusOwner)
+          // Only the second dialog has JTextPane. We do not need to check title
+          parentOfType?.title == "Replace Fragment"
+        }
+
+        test {
           with(TaskTestContext.guiTestCase) {
             dialog("Extract Method") {
-              button("OK").click()
-              dialog("Extract Method") {
-                button("Yes").click()
-              }
+              button("Yes").click()
             }
+          }
+        }
+      }
+      task {
+        text("Now you can confirm or reject replacement of the second fragment.")
+
+        stateCheck {
+          ComponentUtil.getParentOfType(IdeFrame::class.java, focusOwner) != null
+        }
+
+        test {
+          with(TaskTestContext.guiTestCase) {
             dialog("Replace Fragment") {
               button("Replace").click()
             }
