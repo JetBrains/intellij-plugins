b12a6637ddd1a2d4b872047f317730f26ad165d6
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-01-29 14:26:15 +0100

    TrainingProjectLifecycleListener must be as lazy listener
    GitOrigin-RevId: 69b1408f402c76a891dc059482059caaaf7e010e

--- ide-features-trainer/src/training/learn/TrainingProjectLifecycleListener.kt
+++ ide-features-trainer/src/training/learn/TrainingProjectLifecycleListener.kt
@@ -1,22 +1,16 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn
 
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.impl.ProjectLifecycleListener
 import com.intellij.openapi.util.io.FileUtil
 import training.lang.LangManager
 
-class TrainingProjectLifecycleListener : ProjectLifecycleListener {
+internal class TrainingProjectLifecycleListener : ProjectLifecycleListener {
   override fun projectComponentsInitialized(project: Project) {
     val langSupport = LangManager.getInstance().getLangSupport() ?: return
     if (FileUtil.pathsEqual(project.basePath, NewLearnProjectUtil.projectFilePath(langSupport))) {
       langSupport.setProjectListeners(project)
     }
   }
-
-  init {
-    ApplicationManager.getApplication().messageBus.connect().subscribe(ProjectLifecycleListener.TOPIC, this)
-  }
-
 }
