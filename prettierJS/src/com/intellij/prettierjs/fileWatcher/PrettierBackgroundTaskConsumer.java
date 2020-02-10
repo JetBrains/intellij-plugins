93b3eb6e69b4c3dbd526e977c15a7615ef9f9b68
nik <Nikolay.Chashnikov@jetbrains.com>
2020-02-10 13:25:24 +0300

    [core] move properties used in platform.core and platform.core.impl modules to CoreBundle (IDEA-210732)
    GitOrigin-RevId: 6defe72135a40bfdfba02cedcb5d2d8f235c1c13

--- prettierJS/src/com/intellij/prettierjs/fileWatcher/PrettierBackgroundTaskConsumer.java
+++ prettierJS/src/com/intellij/prettierjs/fileWatcher/PrettierBackgroundTaskConsumer.java
@@ -1,13 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.prettierjs.fileWatcher;
 
+import com.intellij.core.CoreBundle;
 import com.intellij.ide.macro.FilePathRelativeToProjectRootMacro;
 import com.intellij.ide.macro.ProjectFileDirMacro;
 import com.intellij.lang.javascript.JavaScriptFileType;
 import com.intellij.plugins.watcher.config.BackgroundTaskConsumer;
 import com.intellij.plugins.watcher.model.TaskOptions;
 import com.intellij.prettierjs.PrettierBundle;
-import com.intellij.psi.PsiBundle;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
@@ -24,7 +24,7 @@ public class PrettierBackgroundTaskConsumer extends BackgroundTaskConsumer {
     options.setName("Prettier");
     options.setDescription(PrettierBundle.message("file.watcher.description"));
     options.setFileExtension(JavaScriptFileType.INSTANCE.getDefaultExtension());
-    options.setScopeName(PsiBundle.message("psi.search.scope.project"));
+    options.setScopeName(CoreBundle.message("psi.search.scope.project"));
 
     String projectFileDirMacro = new ProjectFileDirMacro().getName();
     String projectRootRelativePathMacro = new FilePathRelativeToProjectRootMacro().getName();
