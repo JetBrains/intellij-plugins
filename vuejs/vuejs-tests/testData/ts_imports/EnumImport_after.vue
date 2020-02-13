7b05cecd52e831d6f799e705604c3c6ab9be0b32
anstarovoyt <andrey.starovoyt@jetbrains.com>
2020-02-13 16:53:14 +0300

    typescript: fix test
    GitOrigin-RevId: 947863b4a3d227d3894cbe6b3724ec0425906a2f

--- vuejs/vuejs-tests/testData/ts_imports/EnumImport_after.vue
+++ vuejs/vuejs-tests/testData/ts_imports/EnumImport_after.vue
@@ -1,8 +1,8 @@
 <template><div></div></template>
 <script lang="ts">
- import {Hello} from './enum'
- 
- export default {
+    import {Hello} from './enum'
+
+    export default {
    accept(e: Hello) {
       if (e ==Hello.Foo   ) {}
    }
