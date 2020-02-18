c6ad9eaa4fed9842c8661fe74a1afa861265cb72
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-02-18 19:52:19 +0300

    [IFT] Add restore and UI logic to Surround and Unwrap lesson
    GitOrigin-RevId: 0cc14d6ce6086442c4694835eb07de0ae5084799

--- ide-features-trainer/src/training/learn/lesson/general/SurroundAndUnwrapLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/general/SurroundAndUnwrapLesson.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn.lesson.general
 
+import training.commands.kotlin.TaskContext
 import training.learn.interfaces.Module
 import training.learn.lesson.kimpl.KLesson
 import training.learn.lesson.kimpl.LessonContext
@@ -21,8 +22,16 @@ abstract class SurroundAndUnwrapLesson(module: Module, lang: String) :
     get() = {
       prepareSample(sample)
 
-      actionTask("SurroundWith") {
-        "Press ${action(it)} to surround selected code with some template code."
+      task("SurroundWith") {
+        proposeIfModified {
+          editor.caretModel.currentCaret.selectionStart != previous.sample.selection?.first ||
+          editor.caretModel.currentCaret.selectionEnd != previous.sample.selection?.second
+        }
+        text("Press ${action(it)} to surround selected code with some template code.")
+        triggerByListItem { item ->
+          surroundItems.all { need -> wordIsPresent(item.toString(), need) }
+        }
+        test { actions(it) }
       }
 
       task {
@@ -32,16 +41,30 @@ abstract class SurroundAndUnwrapLesson(module: Module, lang: String) :
             surroundItems.all { sequence.contains(it) }
           }
         }
+        restoreByUi()
         test {
           type("${surroundItems.joinToString(separator = " ")}\n") }
       }
-      task("Unwrap") {
+
+      task {
         editor.caretModel.currentCaret.moveCaretRelatively(0, lineShiftBeforeUnwrap, false, true)
+      }
+      task { // restore point
+        prepareSample(previous.sample)
+      }
+
+      task("Unwrap") {
+        proposeIfModified {
+          editor.caretModel.currentCaret.logicalPosition.line != previous.position.line
+        }
         text("Lets return to the initial file with unwrapping action by ${action(it)}.")
-        trigger(it)
+        triggerByListItem { item ->
+          wordIsPresent(item.toString(), surroundItems[0])
+        }
         test { actions(it) }
       }
       task {
+        restoreByUi()
         text("Choose <code>Unwrap ${surroundItems[0]}</code> item.")
         stateCheck {
           editor.document.charsSequence.let { sequence ->
@@ -51,4 +74,28 @@ abstract class SurroundAndUnwrapLesson(module: Module, lang: String) :
         test { type("${surroundItems[0]}\n") }
       }
     }
+
+  private fun wordIsPresent(text: String, word: String): Boolean {
+    var index = 0
+    while(index != -1 && index < text.length) {
+      index = text.indexOf(word, startIndex = index)
+      if (index != -1) {
+        if ((index == 0 || !text[index - 1].isLetterOrDigit()) &&
+            (index + word.length == text.length || !text[index + word.length + 1].isLetterOrDigit()))
+          return true
+        index = index + word.length
+      }
+    }
+    return false
+  }
+
+  private fun TaskContext.proposeIfModified(checkCaret: () -> Boolean) {
+    proposeRestore {
+      when {
+        editor.document.text != previous.text -> TaskContext.RestoreProposal.Modification
+        checkCaret() -> TaskContext.RestoreProposal.Caret
+        else -> TaskContext.RestoreProposal.None
+      }
+    }
+  }
 }
