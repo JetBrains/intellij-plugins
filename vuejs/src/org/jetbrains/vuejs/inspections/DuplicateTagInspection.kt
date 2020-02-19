97a9343ebce67ea65575382518bba553a4c3f863
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-02-19 12:17:16 +0100

    VueJS: replace DeleteTagFix with RemoveTagIntentionFix
    GitOrigin-RevId: 3fa0ba09d9e40f93de3308eb61ca77460b4e5add

--- vuejs/src/org/jetbrains/vuejs/inspections/DuplicateTagInspection.kt
+++ vuejs/src/org/jetbrains/vuejs/inspections/DuplicateTagInspection.kt
@@ -1,14 +1,11 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.inspections
 
+import com.intellij.codeInsight.daemon.impl.analysis.RemoveTagIntentionFix
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.LocalInspectionToolSession
-import com.intellij.codeInspection.LocalQuickFixOnPsiElement
 import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementVisitor
-import com.intellij.psi.PsiFile
 import com.intellij.psi.XmlElementVisitor
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlDocument
@@ -26,18 +23,10 @@ class DuplicateTagInspection : LocalInspectionTool() {
         if (TEMPLATE_TAG_NAME != tag.name && !HtmlUtil.isScriptTag(tag)) return
         val parent = tag.parent as? XmlDocument ?: return
         if (PsiTreeUtil.getChildrenOfType(parent, XmlTag::class.java).any { it != tag && it.name == tag.name }) {
-          holder.registerProblem(tag, VueBundle.message("vue.inspection.message.duplicate.tag", tag.name), DeleteTagFix(tag))
+          holder.registerProblem(tag, VueBundle.message("vue.inspection.message.duplicate.tag", tag.name),
+                                 RemoveTagIntentionFix(tag.name, tag))
         }
       }
     }
-  }
-}
-
-class DeleteTagFix(tag: XmlTag, private val tagName: String = tag.name) : LocalQuickFixOnPsiElement(tag) {
-  override fun getFamilyName(): String = VueBundle.message("vue.quickfix.delete.tag.family")
-  override fun getText(): String = VueBundle.message("vue.quickfix.delete.tag.text", tagName)
-
-  override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
-    startElement.delete()
   }
 }
