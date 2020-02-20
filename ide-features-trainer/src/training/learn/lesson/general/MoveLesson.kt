bfa9946c444261125c61ceedff64287bbb8a308d
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-02-20 13:02:34 +0300

    [IFT] Fix Move lesson check
    GitOrigin-RevId: d4927043b1a299933bd450ec06bfd85cd09962f9

--- ide-features-trainer/src/training/learn/lesson/general/MoveLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/general/MoveLesson.kt
@@ -1,8 +1,6 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn.lesson.general
 
-import com.intellij.diff.tools.util.text.LineOffsetsUtil
-import com.intellij.openapi.vcs.ex.compareLines
 import training.commands.ShowLineNumberCommand
 import training.lang.JavaLangSupport
 import training.learn.interfaces.Module
@@ -39,5 +37,30 @@ class MoveLesson(module: Module, lang: String, private val sample: LessonSample)
 }
 
 fun checkSwapMoreThan2Lines(before: String, now: String): Boolean {
-  return compareLines(before, now, LineOffsetsUtil.create(before), LineOffsetsUtil.create(now)).iterateChanges().toList().size > 1
+  val beforeLines = before.split("\n")
+  val nowLines = now.split("\n")
+
+  if (beforeLines.size != nowLines.size) {
+    return false
+  }
+  val chane = beforeLines.size - commonPrefix(beforeLines, nowLines) - commonSuffix(beforeLines, nowLines)
+  return chane >= 4
+}
+
+private fun <T> commonPrefix(list1: List<T>, list2: List<T>): Int {
+  val size = Integer.min(list1.size, list2.size)
+  for (i in 0 until size) {
+    if (list1[i] != list2[i])
+      return i
+  }
+  return size
+}
+
+private fun <T> commonSuffix(list1: List<T>, list2: List<T>): Int {
+  val size = Integer.min(list1.size, list2.size)
+  for (i in 0 until size) {
+    if (list1[list1.size - i - 1] != list2[list2.size - i -1])
+      return i
+  }
+  return size
 }
