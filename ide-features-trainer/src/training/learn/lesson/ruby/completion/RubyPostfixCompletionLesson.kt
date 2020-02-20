b5ff51ccc8489278d6b1e52f822d91f7d3c98061
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-02-20 21:46:01 +0300

    [IFT] Add restore logic to Postfix lesson
    GitOrigin-RevId: b591722e4f48010c9390f79284b6eb59cfc7d281

--- ide-features-trainer/src/training/learn/lesson/ruby/completion/RubyPostfixCompletionLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/completion/RubyPostfixCompletionLesson.kt
@@ -1,10 +1,9 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn.lesson.ruby.completion
 
-import com.intellij.testGuiFramework.framework.GuiTestUtil.shortcut
 import com.intellij.testGuiFramework.framework.GuiTestUtil.typeText
 import com.intellij.testGuiFramework.impl.jList
-import com.intellij.testGuiFramework.util.Key
+import training.commands.kotlin.TaskContext
 import training.lang.RubyLangSupport
 import training.learn.interfaces.Module
 import training.learn.lesson.kimpl.KLesson
@@ -33,28 +32,68 @@ end
     get() = {
       prepareSample(sample)
       task {
-        text("The IDE can offer postfix shortcuts. Type ${code(".if")} and press ${action("EditorEnter")}.")
-        trigger("EditorChooseLookupItem")
+        text("The IDE can offer postfix shortcuts. Type ${code(".if")}.")
+        triggerByListItem {
+          it.toString() == ".if"
+        }
+        proposeRestore {
+          checkEditorState()
+        }
         test {
           ideFrame {
             typeText(".if")
-            jList("if")
-            shortcut(Key.ENTER)
           }
         }
       }
+      task {
+        text("Now just press ${action("EditorEnter")} to choose the first postfix template.")
+        triggerByListItem {
+          it.toString() == "string_array.length > 1"
+        }
+        restoreByUi(200)
+        test {
+          ideFrame {
+            jList("if").item(0).doubleClick()
+          }
+        }
+      }
+
       task("string_array.length > 1") {
         text("Now choose the second item, ${code(it)}.")
         stateCheck { editor.document.text == result }
+        restoreByUi()
         test {
           ideFrame {
-            jList(it)
-            shortcut(Key.DOWN)
-            shortcut(Key.ENTER)
+            jList(it).click()
           }
         }
       }
     }
+
+  private fun TaskContext.checkEditorState(): TaskContext.RestoreProposal {
+    val prefix = sample.text.substring(0, sample.startOffset)
+    val postfix = sample.text.substring(sample.startOffset)
+
+    val docText = editor.document.charsSequence
+    return if (docText.startsWith(prefix) && docText.endsWith(postfix)) {
+      val middle = docText.subSequence(prefix.length, docText.length - postfix.length)
+      if (".if".startsWith(middle)) {
+        val offset = editor.caretModel.offset
+        if (prefix.length <= offset && offset <= prefix.length + middle.length) {
+          TaskContext.RestoreProposal.None
+        }
+        else {
+          TaskContext.RestoreProposal.Caret
+        }
+      }
+      else {
+        TaskContext.RestoreProposal.Modification
+      }
+    }
+    else {
+      TaskContext.RestoreProposal.Modification
+    }
+  }
 
   override val existedFile = RubyLangSupport.sandboxFile
 }
