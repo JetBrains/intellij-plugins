0d9bbfa8eb6192ec75b5ad7c7b8e6f13c97020bf
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-02-23 20:06:33 +0300

    IDEA-CR-59110: [IFT] Extract general script for Basic Completion from RubyMine lesson
    (cherry picked from commit aff3a68a136011fddaba754e09696b449ec1ab57)
    
    GitOrigin-RevId: 62a0fe31f0146e698c17985c54b2cba593860b94

--- ide-features-trainer/src/training/learn/lesson/ruby/completion/RubyBasicCompletionLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/completion/RubyBasicCompletionLesson.kt
@@ -1,94 +1,50 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn.lesson.ruby.completion
 
-import com.intellij.openapi.project.Project
-import com.intellij.testGuiFramework.framework.GuiTestUtil.shortcut
-import com.intellij.testGuiFramework.framework.GuiTestUtil.typeText
-import com.intellij.testGuiFramework.impl.jList
-import com.intellij.testGuiFramework.util.Key
-import org.jetbrains.plugins.ruby.ruby.codeInsight.symbols.Types
-import org.jetbrains.plugins.ruby.ruby.codeInsight.symbols.fqn.FQN
-import org.jetbrains.plugins.ruby.ruby.codeInsight.symbols.structure.SymbolUtil
-import org.jetbrains.plugins.ruby.ruby.codeInsight.symbols.v2.ClassModuleSymbol
 import training.lang.RubyLangSupport
 import training.learn.interfaces.Module
-import training.learn.lesson.javascript.textBeforeCaret
-import training.learn.lesson.kimpl.KLesson
-import training.learn.lesson.kimpl.LessonContext
+import training.learn.lesson.general.completion.BasicCompletionLessonBase
 import training.learn.lesson.kimpl.parseLessonSample
 
-class RubyBasicCompletionLesson(module: Module) : KLesson("Basic Completion", module, "ruby") {
-  private val sample1 = parseLessonSample("""class Animal
-  def speak
-    'Hello!'
-  end
-end
-
-class Cat < <caret>
-end
+class RubyBasicCompletionLesson(module: Module) : BasicCompletionLessonBase(module, "ruby") {
+  override val sample1 = parseLessonSample("""
+    class AnimalClass
+      def speak
+        'Hello!'
+      end
+    end
+    
+    class Anatomy
+    end
+    
+    class Cat < <caret>
+    end
 """.trimIndent())
 
-  private val sample2 = parseLessonSample("""class Animal
-  def speak
-    'Hello!'
-  end
-end
+  override val sample2 = parseLessonSample("""
+    class AnimalClass
+      def speak
+        'Hello!'
+      end
+    end
+    
+    class Anatomy
+    end
+    
+    class Cat < AnimalClass
+      def meow
+        'Meow'
+      end
+    
+      def speak
+        <caret>
+      end
+    end
+  """.trimIndent())
 
-class Cat < Animal
-  def meow
-    'Meow'
-  end
-
-  def speak
-    <caret>
-  end
-end
-""".trimIndent())
-
-  override val lessonContent: LessonContext.() -> Unit
-    get() = {
-      prepareSample(sample1)
-      task {
-        text("By default, the IDE completes your code instantly. Start typing <code>An</code> right where " +
-             "the caret is, and you will see the Lookup Menu with matching suggestions. Choose the first item " +
-             "<code>Animal</code> from the Lookup menu by pressing <action>EditorEnter</action>.")
-        trigger("EditorChooseLookupItem",
-                { editor.document.text },
-                { before, now -> checkHierarchy(project) && now != before })
-        test {
-          ideFrame {
-            typeText("An")
-            jList("Animal")
-            shortcut(Key.ENTER)
-          }
-        }
-      }
-      waitBeforeContinue(500)
-      prepareSample(sample2)
-      task("CodeCompletion") {
-        text("To activate Basic Completion explicitly, press ${action(it)}. " +
-             "Select <code>meow</code> and press <action>EditorEnter</action>.")
-        trigger(it)
-        trigger("EditorChooseLookupItem") { textBeforeCaret("meow") }
-        test {
-          actions(it)
-          ideFrame {
-            jList("meow")
-            shortcut(Key.ENTER)
-          }
-        }
-      }
-    }
-
-  private fun checkHierarchy(project: Project): Boolean {
-    val catSymbol = SymbolUtil.findConstantByFQN(
-      project,
-      Types.MODULE_OR_CLASS_OR_CONSTANT,
-      FQN.of("Cat"),
-      null)
-
-    return catSymbol is ClassModuleSymbol && catSymbol.getSuperClassSymbol(null)?.name == "Animal"
-  }
+  override val item1StartToType = "Ani"
+  override val item1CompletionPrefix = "AnimalClass"
+  override val item2Completion = "meow"
 
   override val existedFile = RubyLangSupport.sandboxFile
 }
