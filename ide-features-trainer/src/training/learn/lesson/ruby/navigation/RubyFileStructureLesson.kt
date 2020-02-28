56fe1e37f34f52ae45ce4519a36261f2301767b1
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-02-28 12:29:08 +0300

    [IFT] Return RubyFileStructureLesson class
    GitOrigin-RevId: 35cd2bd507bb17dc58d980e4f33535e4fab15a66

---
+++ ide-features-trainer/src/training/learn/lesson/ruby/navigation/RubyFileStructureLesson.kt
@@ -0,0 +1,10 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package training.learn.lesson.ruby.navigation
+
+import training.learn.interfaces.Module
+import training.learn.lesson.general.navigation.FileStructureLesson
+
+class RubyFileStructureLesson(module: Module) : FileStructureLesson(module, "ruby") {
+  override val memberPrefix: String = "ch"
+  override val existedFile: String = "lib/active_support/core_ext/date/calculations.rb"
+}
