d45b71a45df4da349dd9c224c2a2d4474708e89b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-03-02 13:15:19 +0100

    IDEA-CR-59142: WEB-43720 Allow to customize VueJs language injection
    (cherry picked from commit c8f24afc3d697be2dd9a4db549411b0f400c747e)
    
    GitOrigin-RevId: dfbb419407a03490bbfabe69a40e0b94b2fe355b

--- vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
+++ vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
@@ -2,10 +2,15 @@
 package org.jetbrains.vuejs.context
 
 import com.intellij.lang.injection.InjectedLanguageManager
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectRootModificationTracker
+import com.intellij.openapi.roots.ex.ProjectRootManagerEx
+import com.intellij.openapi.util.EmptyRunnable
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.UserDataHolder
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.*
@@ -22,33 +27,43 @@ import org.jetbrains.vuejs.lang.html.VueFileType
 import org.jetbrains.vuejs.lang.html.VueLanguage
 
 private val VUE_CONTEXT_KEY: Key<CachedValue<Boolean>> = Key("vue.isContext")
+private val VUE_PREV_CONTEXT_KEY = Key<Boolean>("vue.isContext.prev")
+private val VUE_CONTEXT_RELOAD_MARKER_KEY = Key<Any>("vue.isContext.reloadMarker")
+private val reloadMonitor = Any()
 
 fun isVueContext(context: PsiElement): Boolean {
   if (!context.isValid) {
     return false
   }
   if (context is PsiDirectory) {
-    return isVueContext(context)
+    return withContextChangeCheck(context, context.project) { isVueContext(context) }
   }
   val psiFile = InjectedLanguageManager.getInstance(context.project).getTopLevelFile(context) ?: return false
-  if (psiFile.language == VueLanguage.INSTANCE || isSimpleVueHtml(psiFile)) return true
   val file = psiFile.originalFile.virtualFile
+  if ((file != null && file.fileType == VueFileType.INSTANCE)
+      || (file == null && psiFile.language == VueLanguage.INSTANCE)
+      || isSimpleVueHtml(psiFile)) {
+    return true
+  }
+
   @Suppress("DEPRECATION")
   return isVueContext(if (file != null && file.isInLocalFileSystem) file
                       else psiFile.project.baseDir ?: return false,
                       psiFile.project)
 }
 
-fun isVueContext(context: VirtualFile, project: Project): Boolean {
+fun isVueContext(contextFile: VirtualFile, project: Project): Boolean {
   // TODO merge with Angular
-  var file: VirtualFile? = context
+  var file: VirtualFile? = contextFile
   while (file is LightVirtualFileBase) {
     file = file.originalFile
   }
   val psiDir = file?.parent
                  ?.let { if (it.isValid) PsiManager.getInstance(project).findDirectory(it) else null }
                ?: return false
-  return isVueContext(psiDir)
+  return withContextChangeCheck(contextFile, project) {
+    isVueContext(psiDir) && !isVueContextForbiddenFromProviders(contextFile, project)
+  }
 }
 
 private fun isVueContext(directory: PsiDirectory): Boolean {
@@ -106,6 +121,10 @@ fun enableVueTSService(project: Project): Boolean {
   }
 }
 
+private fun isVueContextForbiddenFromProviders(context: VirtualFile, project: Project): Boolean =
+  VueContextProvider.VUE_CONTEXT_PROVIDER_EP.extensionList.any { it.isVueContextForbidden(context, project) }
+
+
 private fun isVueContextFromProviders(psiDir: PsiDirectory): CachedValueProvider.Result<Boolean> {
   val dependencies = mutableSetOf<Any>()
   for (provider in VueContextProvider.VUE_CONTEXT_PROVIDER_EP.extensionList) {
@@ -116,4 +135,31 @@ private fun isVueContextFromProviders(psiDir: PsiDirectory): CachedValueProvider
     dependencies.addAll(result.dependencyItems)
   }
   return CachedValueProvider.Result(false, *dependencies.toTypedArray())
+}
+
+private fun withContextChangeCheck(dataHolder: UserDataHolder, project: Project, stateProvider: () -> Boolean): Boolean {
+  val currentState = stateProvider()
+  val prevState = dataHolder.getUserData(VUE_PREV_CONTEXT_KEY)
+  if (prevState != null && prevState != currentState) {
+    reloadProject(project)
+  }
+  dataHolder.putUserData(VUE_PREV_CONTEXT_KEY, currentState)
+  return currentState
+}
+
+private fun reloadProject(project: Project) {
+  synchronized(reloadMonitor) {
+    if (project.getUserData(VUE_CONTEXT_RELOAD_MARKER_KEY) != null) {
+      return
+    }
+    project.putUserData(VUE_CONTEXT_RELOAD_MARKER_KEY, true)
+  }
+  ApplicationManager.getApplication().invokeLater(
+    Runnable {
+      WriteAction.run<RuntimeException> {
+        ProjectRootManagerEx.getInstanceEx(project)
+          .makeRootsChange(EmptyRunnable.getInstance(), false, true)
+        project.putUserData(VUE_CONTEXT_RELOAD_MARKER_KEY, null)
+      }
+    }, project.disposed)
 }
