4cb7206ce2ddaf311645ba6c6cb4e5cb193288ab
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-03-05 21:51:11 +0300

    [IFT] Fix Python methods
    GitOrigin-RevId: 4b93f2e5d2ca3b0fdd12e5d563ff7534fd4a3e4f

--- ide-features-trainer/src/training/learn/lesson/ruby/navigation/RubyDeclarationAndUsagesLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/ruby/navigation/RubyDeclarationAndUsagesLesson.kt
@@ -7,5 +7,6 @@ import training.learn.lesson.kimpl.LessonContext
 
 class RubyDeclarationAndUsagesLesson(module: Module) : DeclarationAndUsagesLesson(module, "ruby") {
   override fun LessonContext.setInitialPosition() = caret(20, 45)
+  override val typeOfEntity = "an attribute accessor"
   override val existedFile: String = "lib/active_support/core_ext/date/calculations.rb"
 }
