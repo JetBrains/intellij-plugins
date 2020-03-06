f45dda9a15369652902fe609d083f0f1cd9c8c10
Dmitry Jemerov <yole@jetbrains.com>
2020-03-06 13:31:25 +0100

    IDEA-CR-59516: Simplify disposer registration in VueLanguageServiceProvider (IDEA-CR-59516)
    (cherry picked from commit fca9ef3231dccbfda141998dcc96866020bd7b44)
    
    GitOrigin-RevId: 31673f3d2e0a1a5c1c2348cb5b8d9851a90966ec

--- vuejs/src/org/jetbrains/vuejs/lang/typescript/service/VueLanguageServiceProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/typescript/service/VueLanguageServiceProvider.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.lang.typescript.service
 
 import com.intellij.ide.plugins.DynamicPlugins
@@ -18,15 +18,10 @@ class VueLanguageServiceProvider(project: Project) : JSLanguageServiceProvider {
   init {
     myLanguageService = AtomicNotNullLazyValue.createValue<VueTypeScriptService> {
       val service = VueTypeScriptService(project)
-      Disposer.register(project, service)
+      val parentDisposable = DynamicPlugins.pluginDisposable(VueLanguageServiceProvider::class.java, project)
+      Disposer.register(parentDisposable, service)
       return@createValue service
     }
-
-    Disposer.register(DynamicPlugins.pluginDisposable(VueLanguageServiceProvider::class.java, project), Disposable {
-      if (myLanguageService.isComputed) {
-        Disposer.dispose(myLanguageService.value)
-      }
-    })
   }
 
   override fun getAllServices(): List<JSLanguageService> {
