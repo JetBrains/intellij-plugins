8e23f70c79d2425a928c4dbf30064498a4928b1b
Yurii Uvarov <yurii.uvarov@solid.software>
2020-03-11 20:25:07 +0200

    Add string literal spellchecker tests

--- Dart/testData/highlighting/Spelling.dart
+++ Dart/testData/highlighting/Spelling.dart
@@ -8,3 +8,28 @@ class <TYPO descr="Typo: In word 'Baad'">Baad</TYPO><TYPO descr="Typo: In word '
 
 void correctSpelling2(){}
 void <TYPO descr="Typo: In word 'baad'">baad</TYPO><TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>2(){}
+
+var emptyStr ='';
+
+var correctSingleQuotes = 'Spelling';
+var wrongSingleQuotes = '<TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>';
+
+var correctDoubleQuotes = "Spelling";
+var wrongDoubleQuotes = "<TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>";
+
+var correctMultiLine = '''
+Good
+Spelling
+''';
+var wrongMultiLine = '''
+<TYPO descr="Typo: In word 'Baad'">Baad</TYPO>
+<TYPO descr="Typo: In word 'Sspelling'">Sspelling</TYPO>
+''';
+
+var correctNestedDoubleQuotes = 'good "good"';
+var wrongNestedDoubleQuotes = 'good "<TYPO descr="Typo: In word 'baad'">baad</TYPO>"';
+var wrongNestedDoubleQuotesTwoTypos = '<TYPO descr="Typo: In word 'baad'">baad</TYPO> "<TYPO descr="Typo: In word 'baad'">baad</TYPO>"';
+
+var correctNestedSingleQuotes = "good 'good'";
+var wrongNestedSingleQuotes = "good '<TYPO descr="Typo: In word 'baad'">baad</TYPO>'";
+var wrongNestedSingleQuotesTwoTypos = "<TYPO descr="Typo: In word 'baad'">baad</TYPO> '<TYPO descr="Typo: In word 'baad'">baad</TYPO>'";
