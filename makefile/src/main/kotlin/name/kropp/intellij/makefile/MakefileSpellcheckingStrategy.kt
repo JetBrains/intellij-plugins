0bc0e5afccb4a774e81d76e81ac30afa1eb7fb69
Victor Kropp <victor.kropp@gmail.com>
2020-03-21 20:40:32 +0100

    add spellchecker in comments fixes #127
    GitOrigin-RevId: c7aa3dde59f42250bc8ae593479c1c0d4105be7f

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileSpellcheckingStrategy.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileSpellcheckingStrategy.kt
@@ -1,10 +1,26 @@
 package name.kropp.intellij.makefile
 
 import com.intellij.psi.*
+import com.intellij.spellchecker.inspections.*
 import com.intellij.spellchecker.tokenizer.*
+import name.kropp.intellij.makefile.psi.*
 
 class MakefileSpellcheckingStrategy : SpellcheckingStrategy() {
-  override fun getTokenizer(element: PsiElement): Tokenizer<*> {
-    return super.getTokenizer(element)
+  override fun getTokenizer(element: PsiElement): Tokenizer<out PsiElement> = when (element) {
+    is MakefileComment -> MakefileCommentTokenizer
+    is MakefileDocComment -> MakefileDocCommentTokenizer
+    else -> super.getTokenizer(element)
+  }
+}
+
+object MakefileCommentTokenizer : Tokenizer<MakefileComment>() {
+  override fun tokenize(element: MakefileComment, consumer: TokenConsumer) {
+    consumer.consumeToken(element, PlainTextSplitter.getInstance())
+  }
+}
+
+object MakefileDocCommentTokenizer : Tokenizer<MakefileDocComment>() {
+  override fun tokenize(element: MakefileDocComment, consumer: TokenConsumer) {
+    consumer.consumeToken(element, PlainTextSplitter.getInstance())
   }
 }
