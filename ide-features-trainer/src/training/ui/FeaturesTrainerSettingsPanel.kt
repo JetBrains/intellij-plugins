d51f9eba4bc45c1082fdb84db2e136dac3c5324f
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-03-27 13:17:43 +0300

    IFT-44 Show language name instead of ID
    GitOrigin-RevId: 870d8f669110bc5b70248582abef98d972d8ea30

--- ide-features-trainer/src/training/ui/FeaturesTrainerSettingsPanel.kt
+++ ide-features-trainer/src/training/ui/FeaturesTrainerSettingsPanel.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.ui
 
+import com.intellij.lang.Language
 import com.intellij.openapi.options.BoundConfigurable
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.showYesNoDialog
@@ -24,13 +25,15 @@ class FeaturesTrainerSettingsPanel : BoundConfigurable(LearnBundle.message("lear
     if (languagesExtensions.isNotEmpty()) {
       row {
         label(LearnBundle.message("learn.option.main.language"))
-        val options = languagesExtensions.map { it.language }.toTypedArray()
-        comboBox(DefaultComboBoxModel<String>(options), {
-          LangManager.getInstance().state.languageName?.takeIf { options.contains(it) } ?: options[0]
+        val options = languagesExtensions.mapNotNull { Language.findLanguageByID(it.language) }
+          .map { LanguageOption(it) }.toTypedArray()
+        comboBox<LanguageOption>(DefaultComboBoxModel(options), {
+          val languageName = LangManager.getInstance().state.languageName
+          options.find { it.id == languageName } ?: options[0]
         }, { language ->
-          val chosen = LangManager.getInstance().supportedLanguagesExtensions.first { it.language == language }
-          resetPrimaryLanguage(chosen.instance)
-        })
+                             val chosen = LangManager.getInstance().supportedLanguagesExtensions.first { it.language == language?.id }
+                             resetPrimaryLanguage(chosen.instance)
+                           })
       }
     }
     row {
@@ -45,5 +48,10 @@ class FeaturesTrainerSettingsPanel : BoundConfigurable(LearnBundle.message("lear
   private fun updateWelcomeFrame(show: Boolean) {
     StartLearnAction.updateState(show)
     GroupManager.instance.showTutorialsOnWelcomeFrame = show
+  }
+
+  private data class LanguageOption(val language: Language) {
+    override fun toString(): String = language.displayName
+    val id get() = language.id
   }
 }
