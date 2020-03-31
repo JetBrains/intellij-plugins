340c6b5e364f82ed5f901f7276ff202d5f649ce0
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-03-31 11:28:15 +0200

    WEB-44601 Angular: attributes contributed with @Attribute are sometimes not recognized.
    GitOrigin-RevId: d88033b3b26ef4b4d674ccd384f8779f87b21135

--- AngularJS/test/org/angular2/codeInsight/data/attributes/object_in_dec.ts
+++ AngularJS/test/org/angular2/codeInsight/data/attributes/object_in_dec.ts
@@ -1,9 +1,9 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import {Component} from '@angular/core';
+import {Component, Attribute, Input, OnInit} from '@angular/core';
 import {EventEmitter} from "events";
 
 @Component({selector: 'todo-cmp,[todo-cmp]', inputs: ["modelek:model", "id"], outputs: ["complete_: complete"]})
-export class TodoCmp {
+export class TodoCmp implements OnInit{
     modelek: Object;
     @Input() oneTime: string;
     @Input() oneTimeList: FloatPlaceholderType;
@@ -12,8 +12,9 @@ export class TodoCmp {
 
     constructor(
         @Attribute('testAttrOne') attrOne: FloatPlaceholderType,
-        @Attribute('testAttrTwo') private attrTwo: string
-    ) {}
+        @Attribute('testAttrTwo') private attrTwo: string,
+        @Attribute('testAttrThree') public attrThree: string
+) {}
 
     onCompletedButton() {
         this.complete_.emit("completed"); // this fires an event
