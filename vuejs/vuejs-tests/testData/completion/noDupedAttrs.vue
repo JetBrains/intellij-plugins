c682e380b043a9625312af8a9e1281338f191a6e
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-04-01 17:15:13 +0200

    IDEA-CR-60997: WEB-42056 Vue.js: don't suggest same attribute again
    (cherry picked from commit a05737777af6c4e64950b28c27b43fb95255c820)
    
    GitOrigin-RevId: 79c640be081dca01dd0770e3d1315ede2ba012e3

---
+++ vuejs/vuejs-tests/testData/completion/noDupedAttrs.vue
@@ -0,0 +1,13 @@
+<template>
+  <Foo :foo="12" @click.left="" v-on:click.right="" :style="" v-bind:id="" v-slot:foo v-dir1:aaa <caret>>
+</template>
+<script>
+  export default {
+    name: "Foo",
+    props: ["foo", "bar"],
+    directives: {
+       dir1: function(){},
+       dir2: function(){}
+    }
+  }
+</script>
