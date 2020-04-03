4f16187ee4be885a4381528d1af758be23e59917
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-04-03 21:41:50 +0300

    CPP-19380 PlatformIO: Improve New Project usability when framework isn't installed
    (cherry picked from commit 1023b695e867fa93777ba487c4331d95d4d838ad)
    
    GitOrigin-RevId: 8554525f2dafe5f3f9dc92b00eab0de6a53903ff

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioService.java
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioService.java
@@ -1,5 +1,11 @@
 package com.jetbrains.cidr.cpp.embedded.platformio.project;
 
+import com.intellij.ide.BrowserUtil;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
@@ -8,6 +14,8 @@ import org.jetbrains.annotations.Nullable;
 
 @Service
 public final class PlatformioService {
+  public static final String PLATFORMIO_IS_NOT_FOUND = "PlatformIO utility is not found";
+  public static final String INSTALL_GUIDE = "Install Guide...";
   private State myState = State.NONE;
 
   public PlatformioService() {
@@ -25,10 +33,29 @@ public final class PlatformioService {
     myState = b ? State.OK : State.NONE;
   }
 
+  public static void openInstallGuide() {
+    BrowserUtil.browse("https://docs.platformio.org/en/latest/core/installation.html");
+  }
+
   public static State getState(@Nullable Project project) {
     if (project == null || project.isDefault()) return State.NONE;
     PlatformioService platformioService = ServiceManager.getServiceIfCreated(project, PlatformioService.class);
     return platformioService == null ? State.NONE : platformioService.getState();
+  }
+
+  public static void notifyPlatformioNotFound(@Nullable Project project) {
+    Notification notification = new Notification("PlatformIO plugin",
+                                                 PLATFORMIO_IS_NOT_FOUND,
+                                                 "Please check system path",
+                                                 NotificationType.ERROR);
+    notification.addAction(new AnAction(INSTALL_GUIDE) {
+      @Override
+      public void actionPerformed(@NotNull AnActionEvent e) {
+        openInstallGuide();
+      }
+    });
+
+    Notifications.Bus.notify(notification, project);
   }
 
   public enum State {
