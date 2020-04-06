0060f2d12fca056376b92d5bcc552743d4175fac
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-04-06 21:00:01 +0300

    CPP-19478 Platformio: CLion does not handle `CMAKE_CONFIGURATION_TYPES` from CMakeLists.txt
    (cherry picked from commit 688a56ba7263ca8dfde5bf61b48bded9b03b9dd0)
    
    GitOrigin-RevId: d457914abff5c55a095ffc0bf17415c0f51c1ecb

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioListener.java
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioListener.java
@@ -1,10 +1,14 @@
 package com.jetbrains.cidr.cpp.embedded.platformio.project;
 
 import com.intellij.openapi.components.ServiceManager;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.project.ProjectManagerListener;
+import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
@@ -14,6 +18,8 @@ import com.jetbrains.cidr.cpp.embedded.platformio.PlatformioFileType;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class PlatformioListener implements BulkFileListener, ProjectManagerListener {
@@ -46,10 +52,24 @@ public class PlatformioListener implements BulkFileListener, ProjectManagerListe
 
   @Override
   public void projectOpened(@NotNull Project project) {
-    boolean platformioFound = Stream.of(ProjectRootManager.getInstance(project).getContentRoots())
-      .anyMatch(root -> root.findChild(PlatformioFileType.FILE_NAME) != null);
-    if (platformioFound) {
+    Optional<VirtualFile> actualRoot = Stream.of(ProjectRootManager.getInstance(project).getContentRoots())
+      .filter(root -> root.findChild(PlatformioFileType.FILE_NAME) != null)
+      .findFirst();
+    if (actualRoot.isPresent()) {
       ServiceManager.getService(project, PlatformioService.class).enable(true);
+      boolean cMakeFound = actualRoot.get().findChild("CMakeLists.txt") != null;
+      if (!cMakeFound) {
+        int open = Messages.showYesNoDialog(project, "CLion configuration is not found. Create?", "Project Open", null);
+        if (open == Messages.YES) {
+          for (Module module : ModuleManager.getInstance(project).getModules()) {
+            for (VirtualFile root : ModuleRootManager.getInstance(module).getContentRoots()) {
+              if (Objects.equals(root, actualRoot.get())) {
+                new PlatformioProjectGenerator().doGenerateProject(project, actualRoot.get(), "", false);
+              }
+            }
+          }
+        }
+      }
     }
   }
 }
