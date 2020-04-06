ee89c19f14d4861daa445f79696cbeb5099c3302
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-04-06 21:08:46 +0300

    WEB-44567 Dart plugin: Support pub outdated action
    (cherry picked from commit 2a6d912c6590db63068082048c4cedc28c698394)
    
    GitOrigin-RevId: 6aab85ffc3e6026ed8c8e48929b9946a5973911f

--- Dart/src/com/jetbrains/lang/dart/ide/actions/DartPubBuildAction.java
+++ Dart/src/com/jetbrains/lang/dart/ide/actions/DartPubBuildAction.java
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -30,12 +18,12 @@ public class DartPubBuildAction extends DartPubActionBase {
     super.update(e);
     final Project project = e.getProject();
     if (project != null && DartWebdev.INSTANCE.useWebdev(DartSdk.getDartSdk(project))) {
-      e.getPresentation().setText("Webdev: Build...");
-      e.getPresentation().setDescription("Run 'webdev build'");
+      e.getPresentation().setText(DartBundle.message("action.text.webdev.build"));
+      e.getPresentation().setDescription(DartBundle.message("action.description.run.webdev.build"));
     }
     else {
-      e.getPresentation().setText("Pub: Build...");
-      e.getPresentation().setDescription("Run 'pub build'");
+      e.getPresentation().setText(DartBundle.message("action.text.pub.build"));
+      e.getPresentation().setDescription(DartBundle.message("action.description.run.pub.build"));
     }
   }
 
