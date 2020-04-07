9f282422d5936ed302e93a65a1e65981743c5c1a
anstarovoyt <andrey.starovoyt@jetbrains.com>
2020-04-07 16:48:50 +0300

    IDEA-CR-61251: js trainer: fix blog post link
    (cherry picked from commit 2657af13dc61465667bbd99e1d2aa192d7afeec5)
    
    GitOrigin-RevId: c41e314b8e26ebe411a5d395327de934deca95b7

--- ide-features-trainer/src/training/lang/JavaScriptLangSupport.kt
+++ ide-features-trainer/src/training/lang/JavaScriptLangSupport.kt
@@ -20,7 +20,7 @@ class JavaScriptLangSupport : AbstractLangSupport() {
 
   override val langCourseFeedback: String?
     get() = """
-        Read <a href="http://blog.jetbrains.com/webstorm/2020/02/learning-plugin-for-webstorm/">this</a> blog post to learn more about how to get the most out of this training.<br><br>
+        Read <a href="http://blog.jetbrains.com/webstorm/2020/04/learning-plugin-for-webstorm/">this</a> blog post to learn more about how to get the most out of this training.<br><br>
         Have an idea how to make the training experience better? Then please complete <a href="https://forms.gle/EhBiJmN5R638htFv9">this</a> short survey so we can improve the training for you and other WebStorm users.""".trimIndent()
 
   override fun applyToProjectAfterConfigure(): (Project) -> Unit = {
