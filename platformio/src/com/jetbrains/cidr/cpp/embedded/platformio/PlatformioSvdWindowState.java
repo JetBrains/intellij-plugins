573a68e372002685a205d059cfebb9b384409657
Eugene Petrenko <eugene.petrenko@gmail.com>
2020-04-10 15:02:42 +0200

    replace string with known constant
    GitOrigin-RevId: 2c45bf49f3fdaa96e96eb625c34cb27d73f96f4e

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/PlatformioSvdWindowState.java
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/PlatformioSvdWindowState.java
@@ -2,11 +2,12 @@ package com.jetbrains.cidr.cpp.embedded.platformio;
 
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
+import com.intellij.openapi.components.StoragePathMacros;
 import com.jetbrains.cidr.cpp.execution.debugger.embedded.svd.SvdWindowState;
 
 @State(
   name = "PlatformioSvdWindowState",
-  storages = {@Storage("$WORKSPACE_FILE$")}
+  storages = {@Storage(StoragePathMacros.WORKSPACE_FILE)}
 )
 public class PlatformioSvdWindowState extends SvdWindowState {
 
