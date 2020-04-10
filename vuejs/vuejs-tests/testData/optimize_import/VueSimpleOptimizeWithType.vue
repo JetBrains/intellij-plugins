d56f6d764d3b475a43821aeb828b5ff86797bafd
anstarovoyt <andrey.starovoyt@jetbrains.com>
2020-04-10 17:30:18 +0300

    IDEA-CR-61362: vue: better optimize import in typescript WEB-44859 (respect type keyword)
    (cherry picked from commit 16179a945e6107c3ade7cf828b7c48ba18bf0196)
    
    GitOrigin-RevId: afeb1194bda75311135c42d619656dbd5ab97925

---
+++ vuejs/vuejs-tests/testData/optimize_import/VueSimpleOptimizeWithType.vue
@@ -0,0 +1,9 @@
+<template>
+<p>test</p>
+</template>
+<script lang="ts">
+import type {Foo, Foo2} from './VueSimpleOptimize_2';
+
+declare let z: Foo;
+console.log(z);
+</script>
---
+++ vuejs/vuejs-tests/testData/optimize_import/VueSimpleOptimizeWithType_2.ts
@@ -0,0 +1,5 @@
+export class Foo {
+}
+
+export type Foo2 = {
+}
