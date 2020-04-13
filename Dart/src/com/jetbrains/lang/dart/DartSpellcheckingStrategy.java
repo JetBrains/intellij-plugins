c93632d0cdd9ebb2ca5a56a101cc7d6ba09e6704
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-04-13 19:03:26 +0300

    WEB-34709 Spelling typos are not shown in Dart string literals
    close #767
    
    (cherry picked from commit 3d538b2d7cdbe29b43a4a3a5cc9b30eba858b572)
    
    (cherry picked from commit fee9f9ccb7fc27e1db4bb6f75afa7b29a9f71f75)
    
    GitOrigin-RevId: c047d48f9b8e258cbc77aed93b745f0a4c6c7676

--- Dart/src/com/jetbrains/lang/dart/DartSpellcheckingStrategy.java
+++ Dart/src/com/jetbrains/lang/dart/DartSpellcheckingStrategy.java
@@ -1,15 +1,21 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart;
 
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiNameIdentifierOwner;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
+import com.intellij.spellchecker.inspections.PlainTextSplitter;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
+import com.intellij.spellchecker.tokenizer.TokenConsumer;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
+import com.jetbrains.lang.dart.ide.annotator.DartAnnotator;
 import com.jetbrains.lang.dart.psi.DartComponentName;
-import com.jetbrains.lang.dart.psi.DartStringLiteralExpression;
 import org.jetbrains.annotations.NotNull;
 
 public class DartSpellcheckingStrategy extends SpellcheckingStrategy {
-  //private final Tokenizer myStringLiteralTokenizer = new DartStringLiteralTokenizer();
+  private final Tokenizer<PsiElement> myStringLiteralTokenizer = new DartStringLiteralTokenizer();
 
   @Override
   @NotNull
@@ -17,10 +23,29 @@ public class DartSpellcheckingStrategy extends SpellcheckingStrategy {
     if (element instanceof PsiNameIdentifierOwner && !(element instanceof DartComponentName)) {
       return EMPTY_TOKENIZER;
     }
-    else if (element instanceof DartStringLiteralExpression) {
-      // return myStringLiteralTokenizer; todo
+    else if (element instanceof LeafPsiElement && element.getNode().getElementType() == DartTokenTypes.REGULAR_STRING_PART) {
+      return myStringLiteralTokenizer;
     }
-
     return super.getTokenizer(element);
+  }
+
+  private static class DartStringLiteralTokenizer extends Tokenizer<PsiElement> {
+    @Override
+    public void tokenize(@NotNull PsiElement element, @NotNull TokenConsumer consumer) {
+      String text = element.getText();
+      int startIndex = 0;
+      for (Pair<TextRange, Boolean> rangeAndValidity : DartAnnotator.getEscapeSequenceRangesAndValidity(text)) {
+        TextRange escapeRange = rangeAndValidity.first;
+        int escapeStartOffset = escapeRange.getStartOffset();
+        if (escapeStartOffset > startIndex) {
+          consumer.consumeToken(element, text, false, 0, TextRange.create(startIndex, escapeStartOffset), PlainTextSplitter.getInstance());
+        }
+        startIndex = escapeRange.getEndOffset();
+      }
+
+      if (startIndex < text.length()) {
+        consumer.consumeToken(element, text, false, 0, TextRange.create(startIndex, text.length()), PlainTextSplitter.getInstance());
+      }
+    }
   }
 }
