4176990d713273622a4f2cdd9590cb22aae2bd9a
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-04-20 10:57:58 +0200

    WEB-44920 Vuex: support doesn't work with Nuxt.js project structure - correct testdata on 201.
    (cherry picked from commit c15ddc21ecdf68e620e52df07939bb36d5f03c6b)
    
    GitOrigin-RevId: 7f7cee330d8c78eae5e06c01deb69e027ea3da98

--- vuejs/vuejs-tests/testData/optimize_import/VueSimpleOptimizeWithType_after.vue
+++ vuejs/vuejs-tests/testData/optimize_import/VueSimpleOptimizeWithType_after.vue
@@ -2,8 +2,8 @@
 <p>test</p>
 </template>
 <script lang="ts">
-import type {Foo} from './VueSimpleOptimize_2';
+    import type {Foo} from './VueSimpleOptimize_2';
 
-declare let z: Foo;
+    declare let z: Foo;
 console.log(z);
 </script>
