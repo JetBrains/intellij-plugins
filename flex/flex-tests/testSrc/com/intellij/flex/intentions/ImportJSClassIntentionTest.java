0e59420058d851078068baf8bcc96bfb187be748
Alexey Kudravtsev <cdr@intellij.com>
2020-05-06 19:59:17 +0300

    When "Add unambiguous imports on the fly" option is on, try to add these imports during "Optimize Imports" batch action to fix IDEABKL-6064 'Optimize Imports' should add missing imports
    GitOrigin-RevId: fc37b70918b9a065670a668f5c27306f7ece0a86

--- flex/flex-tests/testSrc/com/intellij/flex/intentions/ImportJSClassIntentionTest.java
+++ flex/flex-tests/testSrc/com/intellij/flex/intentions/ImportJSClassIntentionTest.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.flex.intentions;
 
+import com.intellij.codeInsight.CodeInsightSettings;
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzerSettings;
 import com.intellij.flex.util.FlexTestUtils;
 import com.intellij.ide.util.PropertiesComponent;
@@ -30,8 +31,9 @@ public class ImportJSClassIntentionTest extends BaseJSIntentionTestCase {
 
   public void testUnambiguousImportsOnTheFly() {
     ((CodeInsightTestFixtureImpl)myFixture).canChangeDocumentDuringHighlighting(true);
-    boolean oldHintsEnabled = DaemonCodeAnalyzerSettings.getInstance().isImportHintEnabled();
+    boolean oldHintsEnabled = CodeInsightSettings.getInstance().ADD_UNAMBIGIOUS_IMPORTS_ON_THE_FLY;
 
+    CodeInsightSettings.getInstance().ADD_UNAMBIGIOUS_IMPORTS_ON_THE_FLY = true;
     try {
       PropertiesComponent.getInstance().setValue("ActionScript.add.unambiguous.imports.on.the.fly", true);
       DaemonCodeAnalyzerSettings.getInstance().setImportHintEnabled(true);
@@ -45,7 +47,7 @@ public class ImportJSClassIntentionTest extends BaseJSIntentionTestCase {
     }
     finally {
       PropertiesComponent.getInstance().unsetValue("ActionScript.add.unambiguous.imports.on.the.fly");
-      DaemonCodeAnalyzerSettings.getInstance().setImportHintEnabled(oldHintsEnabled);
+      CodeInsightSettings.getInstance().ADD_UNAMBIGIOUS_IMPORTS_ON_THE_FLY = oldHintsEnabled;
     }
   }
 
