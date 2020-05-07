1fd93e20bf2e99a1ab8c6415e25e7b2e30cd5430
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-05-07 22:07:13 +0300

    WEB-45424 LinterCodeStyleImportSourceTracker uses Project as parent disposable for PsiTreeChangeListener
    GitOrigin-RevId: fbbf795b306f3b50de906fe02ada4ac49dd3af05

--- tslint/src/com/intellij/lang/javascript/linter/tslint/codestyle/TsLintImportCodeStyleAction.java
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/codestyle/TsLintImportCodeStyleAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.javascript.linter.tslint.codestyle;
 
 import com.intellij.lang.javascript.linter.tslint.TslintUtil;
@@ -8,6 +8,7 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiFile;
+import com.intellij.ui.EditorNotifications;
 import org.jetbrains.annotations.NotNull;
 
 public class TsLintImportCodeStyleAction extends AnAction {
@@ -31,5 +32,6 @@ public class TsLintImportCodeStyleAction extends AnAction {
       return;
     }
     new TsLintCodeStyleImporter(false).importConfigFile(configPsi);
+    EditorNotifications.getInstance(project).updateAllNotifications();
   }
 }
--- tslint/src/com/intellij/lang/javascript/linter/tslint/editor/TsLintCodeStyleEditorNotificationProvider.kt
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/editor/TsLintCodeStyleEditorNotificationProvider.kt
@@ -1,7 +1,7 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.lang.javascript.linter.tslint.editor
 
-import com.intellij.lang.javascript.linter.LinterCodeStyleImportSourceTracker
+import com.intellij.ide.util.PropertiesComponent
 import com.intellij.lang.javascript.linter.tslint.TsLintBundle
 import com.intellij.lang.javascript.linter.tslint.TslintUtil
 import com.intellij.lang.javascript.linter.tslint.codestyle.TsLintImportCodeStyleAction
@@ -19,28 +19,37 @@ import com.intellij.ui.EditorNotifications
 
 private val KEY = Key.create<EditorNotificationPanel>("TsLint.Import.Code.Style.Notification")
 
-class TsLintCodeStyleEditorNotificationProvider(project: Project) : EditorNotifications.Provider<EditorNotificationPanel>() {
-  private val sourceTracker = LinterCodeStyleImportSourceTracker(project, "tslint.code.style.apply.dismiss", TslintUtil::isConfigFile)
+class TsLintCodeStyleEditorNotificationProvider(private val project: Project) : EditorNotifications.Provider<EditorNotificationPanel>() {
+
+  private val NOTIFICATION_DISMISSED_PROPERTY = "tslint.code.style.apply.dismiss"
+
+  private fun isNotificationDismissed(file: VirtualFile): Boolean {
+    return PropertiesComponent.getInstance(project).getBoolean(NOTIFICATION_DISMISSED_PROPERTY) ||
+           !TslintUtil.isConfigFile(file)
+  }
+
+  private fun dismissNotification() {
+    PropertiesComponent.getInstance(project).setValue(NOTIFICATION_DISMISSED_PROPERTY, true)
+    EditorNotifications.getInstance(project).updateAllNotifications()
+  }
 
   override fun getKey() = KEY
 
   override fun createNotificationPanel(file: VirtualFile, fileEditor: FileEditor, project: Project): EditorNotificationPanel? {
     if (fileEditor !is TextEditor || fileEditor.editor !is EditorEx) return null
-
-    if (sourceTracker.shouldDismiss(file)) return null
+    if (isNotificationDismissed(file)) return null
 
     val psiFile = PsiManager.getInstance(project).findFile(file) ?: return null
     val wrapper = TsLintConfigWrapper.getConfigForFile(psiFile) ?: return null
-    sourceTracker.registerPsiChangedListener()
     val rules = wrapper.getRulesToApply(project)
 
     if (rules.isEmpty()) return null
 
     return object : EditorNotificationPanel(EditorColors.GUTTER_BACKGROUND) {
       init {
-        setText(TsLintBundle.message("tslint.code.style.apply.message"))
+        text = TsLintBundle.message("tslint.code.style.apply.message")
         createActionLabel(TsLintBundle.message("tslint.code.style.apply.text"), TsLintImportCodeStyleAction.ACTION_ID, false)
-        createActionLabel(TsLintBundle.message("tslint.code.style.dismiss.text"), sourceTracker.dismissAction, false)
+        createActionLabel(TsLintBundle.message("tslint.code.style.dismiss.text"), Runnable { dismissNotification() }, false)
       }
     }
   }
