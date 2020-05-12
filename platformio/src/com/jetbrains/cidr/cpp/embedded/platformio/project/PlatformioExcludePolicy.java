e6cb5cd45fb117e4fb0ca655fc6a42ef19615e30
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2020-05-13 01:12:27 +0300

    CPP-19422 PlatformIO: exclude platformio build directory automatically.
    (cherry picked from commit f432130fde7aff4b2fd35ecbb08dff8a2280bf9a)
    
    GitOrigin-RevId: ef46f8520ecbfd5e763a7d5045855910f6d52b3d

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioExcludePolicy.java
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioExcludePolicy.java
@@ -4,7 +4,6 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.roots.impl.DirectoryIndexExcludePolicy;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.ArrayUtil;
 import com.jetbrains.cidr.cpp.embedded.platformio.PlatformioFileType;
 import org.jetbrains.annotations.NotNull;
 
@@ -20,9 +19,6 @@ public class PlatformioExcludePolicy implements DirectoryIndexExcludePolicy {
 
   @Override
   public String @NotNull [] getExcludeUrlsForProject() {
-    if (PlatformioService.getState(myProject) == PlatformioService.State.NONE) {
-      return ArrayUtil.EMPTY_STRING_ARRAY;
-    }
     return
       Stream.of(ProjectRootManager.getInstance(myProject).getContentRoots())
         .filter(root -> root.findChild(PlatformioFileType.FILE_NAME) != null)
