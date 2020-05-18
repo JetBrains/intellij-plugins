a1e7f0fe03747e698390e92517531df31e5394ef
Peter Gromov <peter@jetbrains.com>
2020-05-18 20:45:36 +0200

    don't run CucumberJavaReferenceProvider when searching for Java methods
    GitOrigin-RevId: 14e096917ad9620f6677b7b0f591a460246358fe

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/reference/CucumberJavaReferenceProvider.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/steps/reference/CucumberJavaReferenceProvider.java
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.java.steps.reference;
 
+import com.intellij.pom.PomTargetPsiElement;
 import com.intellij.psi.*;
 import com.intellij.psi.impl.source.resolve.reference.impl.manipulators.StringLiteralManipulator;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -13,6 +14,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CucumberJavaReferenceProvider extends PsiReferenceProvider {
+
+  @Override
+  public boolean acceptsTarget(@NotNull PsiElement target) {
+    return target instanceof PomTargetPsiElement &&
+           ((PomTargetPsiElement)target).getTarget() instanceof CucumberJavaParameterPomTarget;
+  }
+
   @Override
   public PsiReference @NotNull [] getReferencesByElement(@NotNull PsiElement element, @NotNull ProcessingContext context) {
     if (!(element instanceof PsiLiteralExpression)) {
