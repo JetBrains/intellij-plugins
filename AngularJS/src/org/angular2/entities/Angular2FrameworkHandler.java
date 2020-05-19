92883c5c19599846f044abc6e8cb96dc1a199a94
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-05-19 12:22:09 +0200

    WEB-44250 Angular: mark new Angular2FrameworkHandler API as experimental
    IDEA-CR-62548
    
    GitOrigin-RevId: 0a8cd2243dd9b188704724631740155dca3c3cfb

--- AngularJS/src/org/angular2/entities/Angular2FrameworkHandler.java
+++ AngularJS/src/org/angular2/entities/Angular2FrameworkHandler.java
@@ -4,6 +4,7 @@ package org.angular2.entities;
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptClass;
 import com.intellij.openapi.extensions.ExtensionPointName;
 import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -14,6 +15,7 @@ import java.util.List;
 /**
  * Angular framework handler allows to tailor Angular plugin behaviour to properly support a particular framework.
  */
+@ApiStatus.Experimental
 public interface Angular2FrameworkHandler {
 
   ExtensionPointName<Angular2FrameworkHandler>
