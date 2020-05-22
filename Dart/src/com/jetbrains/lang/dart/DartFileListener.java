c42716d240cfb424036495e121f7ff457641e17a
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-05-22 14:42:13 +0300

    WIP WEB-45665 Make Dart plugin dynamic: change smartInvokeLater to invokeLater with correct expiredCondition
    this helps to avoid leaks via DumbServiceImpl.runWhenSmartQueue and EdtQueue.skippedItems,
    and in fact these runnables do not need smart mode.
    
    GitOrigin-RevId: 8faef399558f7b8d82034bcdd4974d6ab97123f2

--- Dart/src/com/jetbrains/lang/dart/DartFileListener.java
+++ Dart/src/com/jetbrains/lang/dart/DartFileListener.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -7,7 +7,6 @@ import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleUtilCore;
-import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.roots.*;
@@ -138,7 +137,8 @@ public class DartFileListener implements AsyncFileListener {
       }
     };
 
-    DumbService.getInstance(project).smartInvokeLater(runnable, ModalityState.NON_MODAL);
+    ApplicationManager.getApplication()
+      .invokeLater(runnable, ModalityState.NON_MODAL, DartAnalysisServerService.getInstance(project).getDisposedCondition());
   }
 
   @Nullable
