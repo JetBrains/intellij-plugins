7808c358ecc82d6b8c20229b5fb31531b3b046ae
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2020-05-25 11:27:34 +0300

    deno: compatibility with 201
    GitOrigin-RevId: 2c12bac77c21b545f6e7cb99864154cda8ac14b7

--- Deno/src/com/intellij/deno/service/DenoTypeScriptServiceCompletionContributor.kt
+++ Deno/src/com/intellij/deno/service/DenoTypeScriptServiceCompletionContributor.kt
@@ -6,7 +6,6 @@ import com.intellij.codeInsight.completion.PrioritizedLookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.deno.DenoSettings
 import com.intellij.lang.ecmascript6.psi.ES6ImportSpecifier
-import com.intellij.lang.javascript.completion.JSCompletionContributor
 import com.intellij.lang.javascript.service.JSLanguageServiceUtil
 import com.intellij.lang.typescript.compiler.languageService.ide.TypeScriptLanguageServiceCompletionContributor
 import com.intellij.lang.typescript.compiler.languageService.protocol.commands.response.TypeScriptCompletionResponse.CompletionEntryDetail
@@ -15,7 +14,7 @@ import java.util.function.Consumer
 class DenoTypeScriptServiceCompletionContributor : TypeScriptLanguageServiceCompletionContributor() {
   override fun fillCompletionVariants(parameters: CompletionParameters, resultSet: CompletionResultSet) {
     if (!DenoSettings.getService(parameters.position.project).isUseDeno()) return
-    
+
     val position = parameters.position.parent
     if (position !is ES6ImportSpecifier) return
     val fromClause = position.declaration?.fromClause ?: return
@@ -27,12 +26,11 @@ class DenoTypeScriptServiceCompletionContributor : TypeScriptLanguageServiceComp
     val future = getServiceEntriesFuture(tsService, file, parameters)
     val details = JSLanguageServiceUtil.awaitFuture(future, TIMEOUT_MILLS, JSLanguageServiceUtil.QUOTA_MILLS, null)
     if (details == null || details.isEmpty()) return
-    val finalResultSet = resultSet.withRelevanceSorter(JSCompletionContributor.createOwnSorter(parameters))
     details.forEach(Consumer { item: CompletionEntryDetail ->
       var el = createLookupElementImpl(item, null)
       if (el is PrioritizedLookupElement<*>) el = el.delegate
       if (el is LookupElementBuilder) {
-        finalResultSet.consume(el.withInsertHandler(null))
+        resultSet.consume(el.withInsertHandler(null))
       }
     })
   }
