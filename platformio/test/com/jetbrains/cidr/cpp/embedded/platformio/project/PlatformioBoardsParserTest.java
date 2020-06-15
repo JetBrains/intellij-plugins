2447b7e985c6ba4cc705fc9a706e14562f12b885
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2020-06-15 13:28:14 +0300

    [platform io] migrate to java 11: add explicit cast to fix compilation under JDK 11
    And suppress a warning about it this time to ensure that the cast won't be deleted.
    
    GitOrigin-RevId: a3e8d05d4baa367535c1034ee39528f2d5760d49

--- platformio/test/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioBoardsParserTest.java
+++ platformio/test/com/jetbrains/cidr/cpp/embedded/platformio/project/PlatformioBoardsParserTest.java
@@ -5,6 +5,7 @@ import com.intellij.util.ResourceUtil;
 
 import javax.swing.tree.TreeNode;
 import java.util.Collections;
+import java.util.Enumeration;
 import java.util.stream.Stream;
 
 import static com.jetbrains.cidr.cpp.embedded.platformio.project.DeviceTreeNode.TYPE.*;
@@ -37,8 +38,8 @@ public class PlatformioBoardsParserTest extends UsefulTestCase {
                                                  "--board", "disco_f334c8", "-O", "framework=stm32cube"));
   }
 
-  @SuppressWarnings("unchecked")
+  @SuppressWarnings({"unchecked", "RedundantCast"})
   private static Stream<DeviceTreeNode> childrenStream(TreeNode treeNode) {
-    return Collections.list(treeNode.children()).stream();
+    return Collections.list((Enumeration)treeNode.children()).stream();
   }
 }
