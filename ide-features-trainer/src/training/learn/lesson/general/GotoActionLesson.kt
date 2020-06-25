39ee4e1b6c0d2ed7cba89d92c56f2d49adba7113
Artem Pronichev <artem.pronichev@jetbrains.com>
2020-06-25 14:36:18 +0300

    IFT: Regrouped lessons for GoLand
    (cherry picked from commit 74f3e8787dc77a96c6312a13fb2cffc8830b1624)
    
    GitOrigin-RevId: b1701f1bcc2222e6a8c19fae5c37c3b41fedd8ce

--- ide-features-trainer/src/training/learn/lesson/general/GotoActionLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/general/GotoActionLesson.kt
@@ -17,7 +17,7 @@ import training.learn.lesson.kimpl.LessonSample
 import javax.swing.JPanel
 
 class GotoActionLesson(module: Module, lang: String, private val sample: LessonSample) :
-  KLesson("Actions", module, lang) {
+  KLesson("Search for actions", module, lang) {
 
   companion object {
     private const val FIND_ACTION_WORKAROUND: String = "https://intellij-support.jetbrains.com/hc/en-us/articles/360005137400-Cmd-Shift-A-hotkey-opens-Terminal-with-apropos-search-instead-of-the-Find-Action-dialog"
@@ -27,10 +27,10 @@ class GotoActionLesson(module: Module, lang: String, private val sample: LessonS
     get() = {
       prepareSample(sample)
       actionTask("GotoAction") {
-        "One of the most useful shortcuts is Find Action. It lets you search through all available actions " +
+        "One of the most useful shortcuts is <strong>Find Action</strong>. It lets you search through all available actions " +
                 "without having to know their individual shortcuts.\nTry it now with ${action(it)}." +
-                if (SystemInfo.isMacOSMojave) " (If Terminal search opens instead of Find Action, " +
-                        "refer to <a href=\"$FIND_ACTION_WORKAROUND\">this article</a>.)" else ""
+                if (SystemInfo.isMacOSMojave) "\nIf <strong>Terminal</strong> search opens instead of <strong>Find Action</strong>, " +
+                        "refer to <a href=\"$FIND_ACTION_WORKAROUND\">this article</a>." else ""
       }
       actionTask("About") {
         "Let's say you want to read about the IDE, type <strong>about</strong> and press ${action("EditorEnter")}."
