69cdf214e1776bb604342e8de8da97151cf6c33e
Peter Gromov <peter@jetbrains.com>
2020-06-30 13:06:07 +0200

    ColorSampleLookupValue: unimplement *LookupValue interfaces, create LookupElement directly
    GitOrigin-RevId: 79feac2c46cbf0806f06e0eb33333cf6454440f1

--- flex/src/com/intellij/javascript/flex/FlexColorReference.java
+++ flex/src/com/intellij/javascript/flex/FlexColorReference.java
@@ -1,21 +1,19 @@
 package com.intellij.javascript.flex;
 
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.resolve.reference.impl.providers.BasicAttributeValueReference;
+import com.intellij.util.containers.JBIterable;
 import com.intellij.xml.util.ColorSampleLookupValue;
 import com.intellij.xml.util.UserColorLookup;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @author Eugene.Kudelevsky
  */
 public class FlexColorReference extends BasicAttributeValueReference {
-  private static Object[] ourValues;
 
   public FlexColorReference(final PsiElement element, TextRange range) {
     super(element, range);
@@ -28,25 +26,18 @@ public class FlexColorReference extends BasicAttributeValueReference {
    }
 
    @Override
-   @SuppressWarnings({"AssignmentToStaticFieldFromInstanceMethod"})
-   public Object @NotNull [] getVariants() {
-     if (ourValues == null) {
-       ColorSampleLookupValue[] colors = ColorSampleLookupValue.getColors();
-       List<Object> mxmlColors = new ArrayList<>();
-
-       for (ColorSampleLookupValue color : colors) {
+   public LookupElement @NotNull [] getVariants() {
+     return JBIterable
+       .of(ColorSampleLookupValue.getColors())
+       .map(color -> {
          String value = color.getValue();
          if (value.startsWith("#") && value.length() > 1) {
            value = "0x" + value.substring(1);
          }
-         mxmlColors.add(new ColorSampleLookupValue(color.getName(), value, color.isIsStandard()));
-       }
-
-       mxmlColors.add(new UserColorLookup());
-       ourValues = mxmlColors.toArray();
-     }
-
-     return ourValues;
+         return new ColorSampleLookupValue(color.getName(), value, color.isIsStandard()).toLookupElement();
+       })
+       .append(new UserColorLookup())
+       .toArray(LookupElement.EMPTY_ARRAY);
    }
 
    @Override
