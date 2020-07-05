786f711908d706aa327e2844e699ceac919086ba
Svetlana.Zemlyanskaya <Svetlana.Zemlyanskaya@jetbrains.com>
2020-07-05 12:04:27 +0200

    IDEA-CR-64473: FUS: rename 'CustomWhitelistRule' EP (IDEA-243826)
    (cherry picked from commit e043f33e021be84f491296c592dfc2786d154167)
    
    GitOrigin-RevId: 0b0df13f6b12f9c0451c38f5ac4c3b114f7d3d5c

--- ide-features-trainer/src/training/statistic/IdeFeaturesTrainerModuleRuleValidator.kt
+++ ide-features-trainer/src/training/statistic/IdeFeaturesTrainerModuleRuleValidator.kt
@@ -3,11 +3,11 @@ package training.statistic
 
 import com.intellij.internal.statistic.eventLog.validator.ValidationResultType
 import com.intellij.internal.statistic.eventLog.validator.rules.EventContext
-import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomWhiteListRule
+import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomValidationRule
 import training.learn.CourseManager
 import training.statistic.FeatureUsageStatisticConsts.MODULE_NAME
 
-private class IdeFeaturesTrainerModuleRuleValidator : CustomWhiteListRule() {
+private class IdeFeaturesTrainerModuleRuleValidator : CustomValidationRule() {
   override fun acceptRuleId(ruleId: String?): Boolean = (ruleId == MODULE_NAME)
 
   override fun doValidate(data: String, context: EventContext): ValidationResultType {
