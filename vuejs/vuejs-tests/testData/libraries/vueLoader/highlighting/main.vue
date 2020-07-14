0e758a10df642746b879d1fc704590f035471b87
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-07-14 15:41:15 +0200

    WEB-38331 Support default Vue-loader setup in Vue templates
    GitOrigin-RevId: a0527ef3d53823bf634eb70c9216ac0e084c20dd

---
+++ vuejs/vuejs-tests/testData/libraries/vueLoader/highlighting/main.vue
@@ -0,0 +1,26 @@
+<template>
+  <img src="./src/logo.png"/>
+  <img src="./src/<warning descr="Cannot resolve file 'foo.png'">foo.png</warning>"/>
+  <img src="@/logo.png"/>
+  <img src="~@/logo.png"/>
+  <img src="~foo/logo.png"/>
+  <img src="~foo/<warning descr="Cannot resolve symbol 'foo.png'">foo.png</warning>"/>
+  <img src="~<warning descr="Cannot resolve symbol 'bar'">bar</warning>/<warning descr="Cannot resolve symbol 'logo.png'">logo.png</warning>"/>
+  <img src="~bar_module/logo.png"/>
+  <img src="~<warning descr="Cannot resolve symbol 'bar_module'">bar_module</warning>/<warning descr="Cannot resolve symbol 'foo.png'">foo.png</warning>"/>
+</template>
+<style>
+@import "~@/logo.png";
+.cls {
+  background: url('<error descr="Cannot resolve file 'bar.png'">bar.png</error>');
+}
+.cls1 {
+  background: url('~@/logo.png');
+}
+.cls2 {
+  background: url('~foo/logo.png');
+}
+.cls3 {
+  background: url('~foo/<weak_warning descr="Cannot resolve file 'bar.png'">bar.png</weak_warning>');
+}
+</style>
---
+++ vuejs/vuejs-tests/testData/libraries/vueLoader/resolve/main.vue
@@ -0,0 +1,26 @@
+<template>
+  <img src="./src/logo.png"/>
+  <img src="./src/foo.png"/>
+  <img src="@/logo.png"/>
+  <img src="~@/logo.png"/>
+  <img src="~foo/logo.png"/>
+  <img src="~foo/foo.png"/>
+  <img src="~bar/logo.png"/>
+  <img src="~bar_module/logo.png"/>
+  <img src="~bar_module/foo.png"/>
+</template>
+<style>
+@import "~@/logo.png";
+.cls {
+  background: url('bar.png');
+}
+.cls1 {
+  background: url('~@/logo.png');
+}
+.cls2 {
+  background: url('~foo/logo.png');
+}
+.cls3 {
+  background: url('~foo/bar.png');
+}
+</style>
