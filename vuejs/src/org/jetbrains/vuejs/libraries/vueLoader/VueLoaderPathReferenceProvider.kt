aff439a5e7dc2ccbfe7b28b58b8bdc5aed38c96b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-07-15 19:06:02 +0200

    WEB-38331 Support default Vue-loader setup in Vue templates - make the code more declarative
    IDEA-CR-65096
    
    GitOrigin-RevId: a1c36fa51f79e5f2cdcc603667261e3983173951

--- vuejs/src/org/jetbrains/vuejs/libraries/vueLoader/VueLoaderPathReferenceProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/libraries/vueLoader/VueLoaderPathReferenceProvider.kt
@@ -7,8 +7,10 @@ import com.intellij.openapi.paths.PathReferenceProviderBase
 import com.intellij.openapi.paths.PsiDynaReference
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiPolyVariantReference
 import com.intellij.psi.PsiReference
 import com.intellij.util.SmartList
+import com.intellij.util.containers.ContainerUtil
 import org.jetbrains.vuejs.context.isVueContext
 import org.jetbrains.vuejs.lang.html.VueFileType
 import org.jetbrains.vuejs.lang.html.VueLanguage
@@ -20,46 +22,33 @@ class VueLoaderPathReferenceProvider : PathReferenceProviderBase() {
                                 text: String,
                                 references: MutableList<PsiReference>,
                                 soft: Boolean): Boolean {
+    // Vue-loader strips `~` and treats the rest as module reference (require parameter)
+    // and if first char is `@` the whole string is treated as module reference.
+    // https://vue-loader.vuejs.org/guide/asset-url.html#transform-rules
+
     val leadingTilde = text.startsWith("~")
-    if ((leadingTilde || text.startsWith("@"))
-        && isApplicable(psiElement)) {
-      val initialOffset = if (leadingTilde) 1 else 0
-      val moduleRefs = JSModuleReferenceContributor.getReferences(
-        text.substring(initialOffset), psiElement, offset + initialOffset, null, true)
-        .groupByTo(mutableMapOf()) { it.rangeInElement }
+    if (!(leadingTilde || text.startsWith("@")) || !isApplicable(psiElement)) return false
 
-      // For HTML inspection we need to convert soft references for each path segment into strong ones
-      var otherRefResolved = false
-      val segmentRefs = pathSegments(text.substring(initialOffset), offset + initialOffset)
-        .map { Pair(it, moduleRefs.remove(it)) }
-        .filter { !it.second.isNullOrEmpty() }
-        .map { (range, refs) ->
-          object : PsiDynaReference<PsiElement>(psiElement) {
-            override fun isSoft(): Boolean = otherRefResolved
-            override fun getRangeInElement(): TextRange = range
-          }.also { it.addReferences(refs) }
-        }
-        .toList()
+    val initialOffset = if (leadingTilde) 1 else 0
 
-      // Unless we have a resolved reference, which spans multiple segments
-      val otherRefs = moduleRefs.asSequence()
-        .map { (range, refs) ->
-          object : PsiDynaReference<PsiElement>(psiElement) {
-            override fun getRangeInElement(): TextRange = range
-          }.also { it.addReferences(refs) }
-        }
-        .toList()
-      otherRefResolved = otherRefs.any { it.multiResolve(false).isNotEmpty() }
+    val moduleRefs = JSModuleReferenceContributor.getReferences(
+      text.substring(initialOffset), psiElement, offset + initialOffset, null, true)
 
-      if (segmentRefs.isNotEmpty() || otherRefs.isNotEmpty()) {
-        references.clear()
-        references.addAll(segmentRefs)
-        // If any of the other references is resolved, include them
-        if (otherRefResolved) {
-          references.addAll(otherRefs)
-        }
-        return true
-      }
+    val pathSegments = pathSegments(text.substring(initialOffset), offset + initialOffset)
+
+    // All module references are soft, so we need to convert them to hard references
+    // for HtmlUnknownTargetInspection to highlighting issues. However, if there are any
+    // not-per-segment references resolved, then we can return original soft references
+    if (anyNonSegmentRefResolved(moduleRefs, pathSegments)) {
+      references.clear()
+      ContainerUtil.addAll(references, *moduleRefs)
+      return true
+    }
+    val segmentRefs = createPerSegmentHardRefs(psiElement, moduleRefs, pathSegments)
+    if (segmentRefs.isNotEmpty()) {
+      references.clear()
+      references.addAll(segmentRefs)
+      return true
     }
     return false
   }
@@ -78,6 +67,27 @@ class VueLoaderPathReferenceProvider : PathReferenceProviderBase() {
     }
   }
 
+  private fun createPerSegmentHardRefs(psiElement: PsiElement,
+                                       moduleRefs: Array<PsiReference>,
+                                       pathSegments: Set<TextRange>): Collection<PsiReference> =
+    moduleRefs.asSequence()
+      .filter { pathSegments.contains(it.rangeInElement) }
+      .groupingBy { it.rangeInElement }
+      .aggregate { key, accumulator: PsiDynaReference<PsiElement>?, element, _ ->
+        (accumulator ?: object : PsiDynaReference<PsiElement>(psiElement) {
+          override fun isSoft(): Boolean = false
+          override fun getRangeInElement(): TextRange = key
+        }).also { it.addReference(element) }
+      }.values
+
+  private fun anyNonSegmentRefResolved(moduleRefs: Array<PsiReference>, pathSegments: Set<TextRange>): Boolean =
+    moduleRefs.asSequence()
+      .filter { !pathSegments.contains(it.rangeInElement) }
+      .any {
+        if (it is PsiPolyVariantReference) it.multiResolve(false).isNotEmpty()
+        else it.resolve() != null
+      }
+
   private fun isApplicable(psiElement: PsiElement): Boolean {
     return psiElement.containingFile
              .let {
@@ -87,7 +97,7 @@ class VueLoaderPathReferenceProvider : PathReferenceProviderBase() {
            && isVueContext(psiElement)
   }
 
-  private fun pathSegments(path: String, offset: Int): Sequence<TextRange> {
+  private fun pathSegments(path: String, offset: Int): Set<TextRange> {
     var segmentStart = 0
     return path.asSequence().mapIndexedNotNull { index, c ->
       when {
@@ -95,6 +105,6 @@ class VueLoaderPathReferenceProvider : PathReferenceProviderBase() {
         index == path.length - 1 -> TextRange(segmentStart + offset, index + offset + 1)
         else -> null
       }
-    }
+    }.toSet()
   }
 }
