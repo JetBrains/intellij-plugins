79e5186d66531db1cce3d06aa6ac5bb64b2670e9
Dani Carles <dani.carles@workday.com>
2020-07-24 11:29:30 +0200

    Add support for language injection
    GitOrigin-RevId: 5ae0e56f71d0052e9a8d5329e6e4751e5aae399d

--- cucumber/src/org/jetbrains/plugins/cucumber/psi/impl/GherkinPystringImpl.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/psi/impl/GherkinPystringImpl.java
@@ -1,11 +1,14 @@
 package org.jetbrains.plugins.cucumber.psi.impl;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.LiteralTextEscaper;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.GherkinElementVisitor;
 import org.jetbrains.plugins.cucumber.psi.GherkinPystring;
 
-public class GherkinPystringImpl extends GherkinPsiElementBase implements GherkinPystring {
+public class GherkinPystringImpl extends GherkinPsiElementBase implements GherkinPystring, PsiLanguageInjectionHost {
   public GherkinPystringImpl(@NotNull final ASTNode node) {
     super(node);
   }
@@ -18,5 +21,26 @@ public class GherkinPystringImpl extends GherkinPsiElementBase implements Gherki
   @Override
   public String toString() {
     return "GherkinPystring";
+  }
+
+  @Override
+  public boolean isValidHost() {
+    return true;
+  }
+
+  @Override
+  public PsiLanguageInjectionHost updateText(@NotNull final String text) {
+    final String docStringSep = getFirstChild().getText();
+    final int startOffset = text.startsWith(docStringSep) ? docStringSep.length() : 0;
+    final int endOffset = text.endsWith(docStringSep) ? docStringSep.length() : 0;
+    ((LeafPsiElement) getFirstChild().getNextSibling()).
+            replaceWithText(text.substring(startOffset, text.length() - endOffset));
+    getFirstChild().getNextSibling().getNextSibling().replace(getLastChild());
+    return this;
+  }
+
+  @Override
+  public @NotNull LiteralTextEscaper<? extends PsiLanguageInjectionHost> createLiteralTextEscaper() {
+    return LiteralTextEscaper.createSimple(this);
   }
 }
