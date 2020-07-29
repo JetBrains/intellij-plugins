af44877acf45b73a5fca9aeded0cad4a922c7051
Victor Kropp <victor.kropp@gmail.com>
2020-07-29 21:37:01 +0200

    maintenance
    GitOrigin-RevId: c080a5ac640e5d02d55e936ed436e3edc39aa555

--- makefile/build.gradle.kts
+++ makefile/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.*
 
 plugins {
   kotlin("jvm") version "1.3.72"
-  id("org.jetbrains.intellij") version "0.4.17"
+  id("org.jetbrains.intellij") version "0.4.21"
 }
 
 group = "name.kropp.intellij"
@@ -13,28 +13,22 @@ repositories {
 }
 
 intellij {
-  version = "2020.1"
+  version = "2020.2"
   pluginName = rootProject.name
   updateSinceUntilBuild = false
-
-  setPlugins("com.jetbrains.sh:201.6668.74")
 }
 
 sourceSets["main"].java.srcDir("gen")
 
 tasks.withType<JavaCompile> {
-  sourceCompatibility = "1.8"
-  targetCompatibility = "1.8"
+  sourceCompatibility = "11"
+  targetCompatibility = "11"
 }
 
 tasks.withType<KotlinCompile> {
-  sourceCompatibility = "1.8"
-  targetCompatibility = "1.8"
-  kotlinOptions {
-    jvmTarget = "1.8"
-    apiVersion = "1.3"
-    languageVersion = "1.3"
-  }
+  sourceCompatibility = "11"
+  targetCompatibility = "11"
+  kotlinOptions.jvmTarget = "11"
 }
 
 dependencies {
