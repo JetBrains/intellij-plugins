e007291820e06b8beaab4f0f865544b3873c8299
Vladislav.Tankov <vladislav.tankov@jetbrains.com>
2020-07-31 00:17:57 +0300

    [IDEA-236165] [Intention] [Spellchecker] Implement support of in-popup suggestions. Used in spellchecker and Grazie.
    Adds IntentionActionWithChoice interface that provides support for suggestions that will be rendered right in quickfix popup. In general, IntentionActionWithChoice is some kind of LookupAction embedded right into a popup.
    
    GitOrigin-RevId: 546b6b129d5feddcf09316218b1e2b1d85b94594

--- cucumber/src/org/jetbrains/plugins/cucumber/spellchecker/GherkinSpellcheckerStrategy.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/spellchecker/GherkinSpellcheckerStrategy.java
@@ -1,17 +1,19 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.spellchecker;
 
+import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.lang.ASTNode;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.impl.source.tree.LeafElement;
 import com.intellij.spellchecker.quickfixes.ChangeTo;
 import com.intellij.spellchecker.quickfixes.SaveTo;
-import com.intellij.spellchecker.quickfixes.SpellCheckerQuickFix;
 import com.intellij.spellchecker.tokenizer.SpellcheckingStrategy;
 import com.intellij.spellchecker.tokenizer.Tokenizer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.GherkinElementType;
+
+import java.util.ArrayList;
 
 public class GherkinSpellcheckerStrategy extends SpellcheckingStrategy {
   @NotNull
@@ -27,11 +29,12 @@ public class GherkinSpellcheckerStrategy extends SpellcheckingStrategy {
   }
 
   @Override
-  public SpellCheckerQuickFix[] getRegularFixes(PsiElement element,
-                                                int offset,
-                                                @NotNull TextRange textRange,
-                                                boolean useRename,
-                                                String wordWithTypo) {
-    return new SpellCheckerQuickFix[]{new ChangeTo(wordWithTypo), new SaveTo(wordWithTypo)};
+  public LocalQuickFix[] getRegularFixes(PsiElement element,
+                                         @NotNull TextRange textRange,
+                                         boolean useRename,
+                                         String typo) {
+    ArrayList<LocalQuickFix> result = new ArrayList<>(new ChangeTo(typo, element, textRange).getAllAsFixes());
+    result.add(new SaveTo(typo));
+    return result.toArray(LocalQuickFix.EMPTY_ARRAY);
   }
 }
