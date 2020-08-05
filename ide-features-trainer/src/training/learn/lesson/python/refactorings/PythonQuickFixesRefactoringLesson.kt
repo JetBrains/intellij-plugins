6a7607408f9b051916c37f706f8c7bc1a917043a
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-08-06 01:29:42 +0300

    [IFT] Improve Quick Fixes lesson for Python
    Add UI highlighting and intermediate step
    
    GitOrigin-RevId: 427d7f012cfd916d8f7c3d6254e973ecea7c87f0

--- ide-features-trainer/src/training/learn/lesson/python/refactorings/PythonQuickFixesRefactoringLesson.kt
+++ ide-features-trainer/src/training/learn/lesson/python/refactorings/PythonQuickFixesRefactoringLesson.kt
@@ -1,9 +1,16 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.learn.lesson.python.refactorings
 
+import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.editor.impl.EditorComponentImpl
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.wm.IdeFocusManager
 import com.intellij.testGuiFramework.framework.GuiTestUtil
 import com.intellij.testGuiFramework.impl.button
 import com.intellij.testGuiFramework.util.Key
+import com.intellij.util.ui.UIUtil
+import com.intellij.util.ui.table.JBTableRow
 import com.jetbrains.python.inspections.quickfix.PyChangeSignatureQuickFix
 import training.commands.kotlin.TaskContext
 import training.commands.kotlin.TaskTestContext
@@ -13,7 +20,7 @@ import training.learn.lesson.kimpl.LessonContext
 import training.learn.lesson.kimpl.LessonUtil
 import training.learn.lesson.kimpl.parseLessonSample
 import javax.swing.JDialog
-import javax.swing.JLabel
+import javax.swing.JTable
 
 class PythonQuickFixesRefactoringLesson(module: Module) : KLesson("refactoring.quick.fix", "Quick fix refactoring", module, "Python") {
   private val sample = parseLessonSample("""
@@ -72,8 +79,12 @@ class PythonQuickFixesRefactoringLesson(module: Module) : KLesson("refactoring.q
     task {
       text("Choose <strong>Change signature</strong> quick fix.")
 
-      triggerByUiComponentAndHighlight(highlightBorder = false, highlightInside = false) { dialog: JDialog ->
-        dialog.title == "Change Signature"
+      triggerByPartOfComponent { table : JTable ->
+        val model = table.model
+        if (model.rowCount >= 2 && (model.getValueAt(1, 0) as? JBTableRow)?.getValueAt(0) == "y") {
+          table.getCellRect(1, 0, true)
+        }
+        else null
       }
       restoreByUi(500)
       test {
@@ -85,44 +96,71 @@ class PythonQuickFixesRefactoringLesson(module: Module) : KLesson("refactoring.q
       text("Let's set the default value for the new parameter. Click at the new parameter line. " +
            "Alternatively, you can set focus to the parameter without mouse by ${action("EditorTab")} and then ${action("EditorEnter")}.")
 
-      triggerByUiComponentAndHighlight(highlightBorder = false, highlightInside = false) { label: JLabel ->
-        label.text == "Default value:"
+      val selector = { collection: Collection<EditorComponentImpl> ->
+        collection.takeIf { it.size > 2 }?.maxBy { it.locationOnScreen.x }
+      }
+      triggerByUiComponentAndHighlight(selector = selector) { editor : EditorComponentImpl ->
+        UIUtil.getParentOfType(JDialog::class.java, editor) != null
       }
       restoreByUi()
       test {
-        GuiTestUtil.shortcut(Key.TAB)
+        invokeAndWaitIfNeeded(ModalityState.any()) {
+          val ui = previous.ui ?: return@invokeAndWaitIfNeeded
+          IdeFocusManager.getInstance(project).requestFocus(ui, true)
+        }
         GuiTestUtil.shortcut(Key.ENTER)
       }
     }
+    task {
+      text("You may navigate through the fields (and the checkbox) by using ${action("EditorTab")}. " +
+           "With the checkbox you can let IDE inline the default value to the other callers or set it as the default value for the new parameter. " +
+           "The Signature Preview will help understand the difference. Now set the default value as 0.")
+      restoreByUi()
+      stateCheck {
+        (previous.ui as? EditorComponentImpl)?.text == "0"
+      }
+      test {
+        invokeAndWaitIfNeeded(ModalityState.any()) {
+          val ui = previous.ui ?: return@invokeAndWaitIfNeeded
+          IdeFocusManager.getInstance(project).requestFocus(ui, true)
+        }
+        GuiTestUtil.shortcut(Key.BACK_SPACE)
+        type("0")
+        //ideFrame {
+        //  previous.ui?.let { usagesTab -> jComponent(usagesTab).click() }
+        //}
+      }
+    }
+
     task {
       lateinit var beforeRefactoring: String
       before {
         beforeRefactoring = editor.document.text
       }
-      text("You may navigate through the fields (and the checkbox) by using ${action("EditorTab")}. " +
-           "With the checkbox you can let IDE inline the default value to the other callers or set it as the default value for the new parameter. " +
-           "The Signature Preview will help understand the difference. Now set the default value as 0 and press <raw_action>Ctrl + Enter</raw_action> " +
-           "(or click <strong>Do Refactor</strong>) to finish the refactoring.")
+      text("Press <raw_action>${if (SystemInfo.isMacOSMojave) "\u2318\u23CE" else "Ctrl + Enter"}</raw_action> " +
+           "(or click <strong>Refactor</strong>) to finish the refactoring.")
 
       stateCheck {
         val b = editor.document.text != beforeRefactoring
-        b && Thread.currentThread().stackTrace.any {
-          it.className == PyChangeSignatureQuickFix::class.java.name
-        }
+        b && stackInsideDialogRefactoring()
       }
-      restoreByUi(500)
+      restoreState(500) {
+        !stackInsideDialogRefactoring()
+      }
 
       test {
-        GuiTestUtil.shortcut(Key.TAB)
-        GuiTestUtil.shortcut(Key.BACK_SPACE)
-        type("0")
-        GuiTestUtil.shortcut(Key.ENTER)
         with(TaskTestContext.guiTestCase) {
           dialog("Change Signature") {
             button("Refactor").click()
           }
         }
       }
+    }
+  }
+
+  private fun stackInsideDialogRefactoring(): Boolean {
+    return Thread.currentThread().stackTrace.any {
+      it.className == PyChangeSignatureQuickFix::class.java.name
     }
   }
 
