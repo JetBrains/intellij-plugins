9fb49f0ab570fdb5ea51731d01690dc2eb47174a
Alexey Merkulov <alexey.merkulov@jetbrains.com>
2020-08-06 17:45:50 +0300

    [IFT] EA-236745 Do not provide welcome frame in case of new welcome frame enabled
    GitOrigin-RevId: ade86b2686210f2898ea7b6c21434d12d67246ae

--- ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTFlatWelcomeFrameProvider.kt
+++ ide-features-trainer/src/training/ui/welcomeScreen/recentProjects/IFTFlatWelcomeFrameProvider.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package training.ui.welcomeScreen.recentProjects
 
+import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.wm.IdeFrame
 import com.intellij.openapi.wm.WelcomeFrameProvider
 import training.actions.StartLearnAction
@@ -10,5 +11,5 @@ class IFTFlatWelcomeFrameProvider : WelcomeFrameProvider {
   init {
     StartLearnAction.updateState(GroupManager.instance.showTutorialsOnWelcomeFrame)
   }
-  override fun createFrame(): IdeFrame = IFTFlatWelcomeFrame()
+  override fun createFrame(): IdeFrame? = if (Registry.`is`("use.tabbed.welcome.screen")) null else IFTFlatWelcomeFrame()
 }
