e54df5079c7da53e688f881721d39568998d95dd
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-08-10 13:50:40 +0200

    IDEA-CR-65928: [ruby] Do not change gem load process for minitest 5.0 and newer.
    In modern version of minitest it's possible to add the reporter and make monkey patch via file "minitest/*_plugin.rb". So it's possible not to break the load process.
    
    For older minitest versions (the latest version of 4.7 was released in 2013) we still have to load rubymine_test_framework_initializer.rb before the main application. But it's only ~ 1% of our users.
    
    RUBY-26848 2020.2 uses minitest_runner.rb for running/debugging individual tests and it's causing problems (at_exit handler of a gem runs before the test)
    
    (cherry picked from commit 460b2859ad88f742d0006603d8d0a37166b0ac98)
    
    GitOrigin-RevId: 84c410dc07b7a212012a5644ef3c3d53b39d0ef8

--- ruby-testing/src/rb/testing/patch/testunit/rubymine_test_framework_initializer.rb
+++ ruby-testing/src/rb/testing/patch/testunit/rubymine_test_framework_initializer.rb
@@ -1,44 +1,31 @@
-is_minitest_run = false
-begin
-  require "minitest/autorun"
-rescue LoadError
-  # it's ok in case of minitest 1.4
-end
-
-begin
-  require "minitest/unit"
-  require 'minitest/rm_reporter_plugin'
-  is_minitest_run = true
-  begin
-    require 'minitest/reporters'
-  rescue LoadError
-    # ok, there is no minitest-reporters gem
-  end
-rescue LoadError
-  begin
-    require 'test/unit'
-  rescue LoadError
-    puts "Neither minitest nor test-unit libraries available"
+minitest_5_or_newer = false
+Gem::Specification::find_all_by_name('minitest').each do |gem|
+  if Gem::Version.new(gem.version) >= Gem::Version.new('5.0')
+    minitest_5_or_newer = true
+    break
   end
 end
 
-if is_minitest_run
-  #this patch is to simulate before_test hook, see file rubymine_minitest_patch.rb
-  Minitest.rubymine_reporter = Minitest::MyRubyMineReporter.new
-  if defined? Minitest::Unit.runner=
-    Minitest::Unit.runner = Minitest.rubymine_reporter
-  elsif defined? MiniTest::Unit.runner
-    MiniTest::Unit.runner = Minitest.rubymine_reporter
+unless minitest_5_or_newer
+  begin
+    require "minitest/autorun"
+  rescue LoadError
+    # it's ok in case of minitest 1.4
   end
 
-  if defined? Minitest::Reporters.use!
-    Minitest::Reporters.module_eval do
-      def self.use!(*args)
-        # do not register RubyMineReporter from minitest-reporters
-      end
+  begin
+    require "minitest/unit"
+    begin
+      require 'minitest/reporters'
+    rescue LoadError
+      # ok, there is no minitest-reporters gem
     end
-  end
-  if defined? Minitest::Reporters.reporters
-    Minitest::Reporters.reporters = [Minitest.rubymine_reporter]
+    require 'minitest/rm_reporter_plugin'
+  rescue LoadError
+    begin
+      require 'test/unit'
+    rescue LoadError
+      puts "Neither minitest nor test-unit libraries available"
+    end
   end
 end
