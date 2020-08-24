b624a36875d1211a3916dda2939580dcea060a8b
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-08-24 16:30:56 +0300

    WEB-47147 Images disappeared from the Dart doc preview
    (cherry picked from commit 910a8db2fab9be38031f140cf16538f72a1c5b55)
    
    GitOrigin-RevId: fd7b87faecfb0da1a69a09177b0351f4646d4f4f

--- Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocUtil.java
+++ Dart/src/com/jetbrains/lang/dart/ide/documentation/DartDocUtil.java
@@ -181,7 +181,6 @@ public class DartDocUtil {
     if (docText != null) {
       List<String> lines = StringUtil.split(docText, "\n", true, false);
       MarkdownUtil.replaceCodeBlock(lines);
-      MarkdownUtil.removeImages(lines);
       MarkdownUtil.replaceHeaders(lines);
       MarkdownUtil.generateLists(lines);
 
