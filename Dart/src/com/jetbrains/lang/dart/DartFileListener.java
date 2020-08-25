dc360b45cd5b4e1ae59cab3f2d3b35f9a74a5477
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2020-08-25 11:46:18 +0300

    follow-up on WEB-47145 Dart plugin: stop auto-excluding 'packages' folders.
    GitOrigin-RevId: 5424573a68d4988fab8e56bca48b7e549691a9a7

--- Dart/src/com/jetbrains/lang/dart/DartFileListener.java
+++ Dart/src/com/jetbrains/lang/dart/DartFileListener.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart;
 
 import com.intellij.openapi.application.ApplicationManager;
@@ -110,7 +110,7 @@ public class DartFileListener implements VirtualFileListener {
 
       final Module module = ModuleUtilCore.findModuleForFile(pubspec, project);
       if (module != null && !module.isDisposed() && !project.isDisposed()) {
-        DartStartupActivity.excludeBuildAndPackagesFolders(module, pubspec);
+        DartStartupActivity.excludeBuildAndToolCacheFolders(module, pubspec);
       }
     }
   }
