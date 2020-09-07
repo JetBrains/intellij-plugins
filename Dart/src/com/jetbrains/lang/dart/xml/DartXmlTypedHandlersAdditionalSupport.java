759ac3b7e29eab06842a9d100d99e5780eb734c8
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2020-09-07 18:48:36 +0300

    html: remove hardcoded languages, provide extendable API for better handling mdx
    GitOrigin-RevId: 528f3cdb4b43d958e9e797fc5e7a4a4f9458ebdd

---
+++ Dart/src/com/jetbrains/lang/dart/xml/DartXmlTypedHandlersAdditionalSupport.java
@@ -0,0 +1,16 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package com.jetbrains.lang.dart.xml;
+
+import com.intellij.lang.Language;
+import com.intellij.openapi.editor.XmlTypedHandlersAdditionalSupport;
+import com.intellij.psi.PsiFile;
+import com.jetbrains.lang.dart.DartLanguage;
+import org.jetbrains.annotations.NotNull;
+
+public class DartXmlTypedHandlersAdditionalSupport implements XmlTypedHandlersAdditionalSupport {
+  
+  @Override
+  public boolean isAvailable(@NotNull PsiFile psiFile, @NotNull Language lang) {
+    return lang.isKindOf(DartLanguage.INSTANCE);
+  }
+}
