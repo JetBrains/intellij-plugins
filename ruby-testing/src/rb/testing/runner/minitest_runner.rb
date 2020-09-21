80fde5ed57bacee55b5dec477b83a77a522fd714
Andrey Vokin <andrey.vokin@jetbrains.com>
2020-09-21 10:11:14 +0200

    [ruby] pass test file path via environment variable
    This is necessary when test run in preload server
    
    RUBY-26947 Can't run tests after upgrading to RubyMine 2020.2
    
    GitOrigin-RevId: b0c44b7e6086a088a652a710821fcb479c0b1387

--- ruby-testing/src/rb/testing/runner/minitest_runner.rb
+++ ruby-testing/src/rb/testing/runner/minitest_runner.rb
@@ -2,11 +2,9 @@ require File.dirname(__FILE__) + '/runner_settings'
 require 'teamcity/utils/runner_utils'
 require 'rubymine_test_framework_initializer'
 
-if ARGV.length > 0
-  file = ARGV.shift
-  require file
-else
+file = ENV["INTELLIJ_IDEA_RUN_CONF_TEST_FILE_PATH"]
+if file.nil? or file.length == 0
    raise Exception.new("A test file must be provided")
 end
 
-
+require file
