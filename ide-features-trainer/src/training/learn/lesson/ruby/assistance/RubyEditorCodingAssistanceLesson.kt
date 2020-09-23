b4ea5e0a990b9275dbf68734952d055856748035
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-09-23 13:37:30 +0300

    Convert Code Assistance lessons from XML to Kotlin DSL, update these lessons and generalize them
    Add Code Assistance lessons for Python and Ruby
    
    GitOrigin-RevId: dc1d84cd7ffdbeeb0fd1e1be970b297e9cbd9a7c

---
+++ ide-features-trainer/src/training/learn/lesson/ruby/assistance/RubyEditorCodingAssistanceLesson.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package training.learn.lesson.ruby.assistance
+
+import org.jetbrains.plugins.ruby.RBundle
+import training.commands.kotlin.TaskRuntimeContext
+import training.learn.interfaces.Module
+import training.learn.lesson.general.assistance.EditorCodingAssistanceLesson
+import training.learn.lesson.kimpl.LessonSample
+
+class RubyEditorCodingAssistanceLesson(module: Module, lang: String, sample: LessonSample) :
+  EditorCodingAssistanceLesson(module, lang, sample) {
+
+  override fun TaskRuntimeContext.checkErrorFixed(): Boolean {
+    return editor.document.charsSequence.contains("cat.say_meow()")
+  }
+
+  override val intentionDisplayName: String
+    get() = RBundle.message("inspection.argcount.extra.argument.fix")
+
+  override val variableNameToHighlight: String = "@happiness"
+}
