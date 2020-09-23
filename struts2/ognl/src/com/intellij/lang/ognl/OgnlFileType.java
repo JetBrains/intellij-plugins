9e3935f6170672758732882e8e9b146ea04ccb07
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2020-09-23 07:21:20 +0200

    cleanup (don't use deprecated method: scale -> scaleIcon)
    GitOrigin-RevId: d67bf091a1fbf601d7dfa3df376815f7821bbbe3

--- struts2/ognl/src/com/intellij/lang/ognl/OgnlFileType.java
+++ struts2/ognl/src/com/intellij/lang/ognl/OgnlFileType.java
@@ -20,7 +20,7 @@ import com.intellij.openapi.fileTypes.LanguageFileType;
 import com.intellij.openapi.util.AtomicNotNullLazyValue;
 import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.ui.LayeredIcon;
-import com.intellij.util.ui.JBUI;
+import com.intellij.ui.scale.JBUIScale;
 import icons.OgnlIcons;
 import org.jetbrains.annotations.NotNull;
 
@@ -40,7 +40,7 @@ public final class OgnlFileType extends LanguageFileType {
       final LayeredIcon icon = new LayeredIcon(2);
       icon.setIcon(FileTypes.PLAIN_TEXT.getIcon(), 0);
       icon.setIcon(OgnlIcons.Action_small, 1, 0, 6);
-      return JBUI.scale(icon);
+      return JBUIScale.scaleIcon(icon);
     }
   };
 
