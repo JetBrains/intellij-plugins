27b598bc46851ed868db3247c9a5a86d99579dd6
Victor Kropp <victor.kropp@gmail.com>
2020-09-24 17:00:18 +0200

    fix deprecation
    GitOrigin-RevId: b34c3a8e49e27d7ac67e0e707785de0e012b6721

--- makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileCodeStyleSettingsProvider.kt
+++ makefile/src/main/kotlin/name/kropp/intellij/makefile/MakefileCodeStyleSettingsProvider.kt
@@ -2,12 +2,11 @@ package name.kropp.intellij.makefile;
 
 import com.intellij.application.options.*
 import com.intellij.lang.*
-import com.intellij.openapi.options.*
 import com.intellij.psi.codeStyle.*
 
 class MakefileCodeStyleSettingsProvider : CodeStyleSettingsProvider() {
-  override fun createSettingsPage(settings: CodeStyleSettings, originalSettings: CodeStyleSettings?): Configurable {
-    return object : CodeStyleAbstractConfigurable(settings, originalSettings, "Makefile") {
+  override fun createConfigurable(settings: CodeStyleSettings, modelSettings: CodeStyleSettings): CodeStyleConfigurable {
+    return object : CodeStyleAbstractConfigurable(settings, modelSettings, "Makefile") {
       override fun createPanel(settings: CodeStyleSettings): CodeStyleAbstractPanel {
         return object : TabbedLanguageCodeStylePanel(MakefileLanguage, currentSettings, settings) {
           override fun initTabs(settings: CodeStyleSettings) {
