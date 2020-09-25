83e27f323ea86e3a02b502002e9d872a439e01de
Aleksandr Prokopyev <aleksandr.prokopyev@jetbrains.com>
2020-09-25 13:47:28 +0300

    RUBY-27056: Check if attribute writer is present
    In case of seedbank gem, which works very hacky way, refining Object in global scope, Minitest::Unit.runner resolves to refined method runner in Object, but runner= is absent there, so this gem causes failures.
    
    GitOrigin-RevId: 149ac3f725d182671d4f9cfd73d16d8851e91ba5

--- ruby-testing/src/rb/testing/patch/testunit/minitest/rubymine_minitest_initializer.rb
+++ ruby-testing/src/rb/testing/patch/testunit/minitest/rubymine_minitest_initializer.rb
@@ -1,8 +1,8 @@
 #this patch is to simulate before_test hook, see file rubymine_minitest_patch.rb
 Minitest.rubymine_reporter = Minitest::MyRubyMineReporter.new
-if defined? Minitest::Unit.runner
+if defined?(Minitest::Unit) && Minitest::Unit.method_defined?(:runner=)
   Minitest::Unit.runner = Minitest.rubymine_reporter
-elsif defined? MiniTest::Unit.runner
+elsif defined?(MiniTest::Unit) && MiniTest::Unit.method_defined?(:runner=)
   MiniTest::Unit.runner = Minitest.rubymine_reporter
 end
 
