c91ee2ad3ea1f11441787be7271f15bcfd349981
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2020-10-15 21:00:19 +0200

    [osgi] JPS module i18n
    GitOrigin-RevId: 40450a4502df9a4c88c6f933cfd1b44f03dca22c

--- osmorc/osmorc-jps-plugin/src/org/jetbrains/osgi/jps/build/OsmorcBuilder.java
+++ osmorc/osmorc-jps-plugin/src/org/jetbrains/osgi/jps/build/OsmorcBuilder.java
@@ -1,6 +1,7 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.osgi.jps.build;
 
+import com.intellij.openapi.util.NlsSafe;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.builders.BuildOutputConsumer;
 import org.jetbrains.jps.builders.BuildRootDescriptor;
@@ -16,15 +17,14 @@ import java.util.Collections;
  * @author michael.golubev
  */
 public class OsmorcBuilder extends TargetBuilder<BuildRootDescriptor, OsmorcBuildTarget> {
-  public static final String ID = "osgi";
+  public static final @NlsSafe String ID = "osgi";
 
   public OsmorcBuilder() {
     super(Collections.singletonList(OsmorcBuildTargetType.INSTANCE));
   }
 
-  @NotNull
   @Override
-  public String getPresentableName() {
+  public @NotNull String getPresentableName() {
     return ID;
   }
 
