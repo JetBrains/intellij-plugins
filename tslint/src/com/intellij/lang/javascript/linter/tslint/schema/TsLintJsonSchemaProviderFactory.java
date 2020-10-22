a05e99f19d00c08f8b9d11bfa8ea240c4723610e
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2020-10-22 22:22:59 +0300

    tslint: i18n TSLint name
    (cherry picked from commit c94c418256ace6922a91b1e974ec7ee93d8ff670)
    
    IDEA-CR-67961
    
    GitOrigin-RevId: 90ad7cf8f1d7beb0d89159d64de8eb9abe6addd9

--- tslint/src/com/intellij/lang/javascript/linter/tslint/schema/TsLintJsonSchemaProviderFactory.java
+++ tslint/src/com/intellij/lang/javascript/linter/tslint/schema/TsLintJsonSchemaProviderFactory.java
@@ -3,6 +3,7 @@ package com.intellij.lang.javascript.linter.tslint.schema;
 import com.intellij.lang.javascript.EmbeddedJsonSchemaFileProvider;
 import com.intellij.lang.javascript.linter.tslint.TslintUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.NlsSafe;
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider;
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory;
 import org.jetbrains.annotations.NotNull;
@@ -14,10 +15,11 @@ import java.util.List;
 public final class TsLintJsonSchemaProviderFactory implements JsonSchemaProviderFactory {
   private static final String TSLINT_SCHEMA_JSON = "tslint-schema.json";
   private static final String TSLINT_JSON_SCHEMA_DIR = "/tslintJsonSchema";
+  private static final @NlsSafe String NAME = "TSLint";
 
   @NotNull
   private static EmbeddedJsonSchemaFileProvider createProvider() {
-    return new EmbeddedJsonSchemaFileProvider(TSLINT_SCHEMA_JSON, "TSLint", "http://json.schemastore.org/tslint",
+    return new EmbeddedJsonSchemaFileProvider(TSLINT_SCHEMA_JSON, NAME, "http://json.schemastore.org/tslint",
                                               TsLintJsonSchemaProviderFactory.class, TSLINT_JSON_SCHEMA_DIR + '/', TslintUtil.CONFIG_FILE_NAMES);
   }
 
