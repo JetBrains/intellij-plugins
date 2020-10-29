155cc82859435e7c187dac842dd46f3b1b28f6ed
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-10-29 10:05:40 +0100

    WEB-46511 Vue 3: Support <script setup> - enable TS service validation
    GitOrigin-RevId: 500b6c366b19b1712d7120223423bcc2dd540fe5

---
+++ vuejs/vuejs-tests/testData/ts_ls_highlighting/ScriptSetup/ScriptSetup2.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+import {defineComponent} from "vue"
+import MyComponent from "./my-component.vue"
+import <error>MyComponent2</error> from "./my-component2.vue"
+
+export default defineComponent({
+  components: {
+    MyComponent,
+    MyComponent2,
+    <error>MyComponent3</error>
+  }
+})
+</script>
