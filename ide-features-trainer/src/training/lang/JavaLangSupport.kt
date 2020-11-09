ca0080c2bc6c36275631915783f04edf0fc30a11
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-11-09 15:05:01 +0300

    [IFT] Edit the way of choosing JDK for Java learning project
    Earlier if we couldn't find local JDK, it chooses the JDK that bundled in IDEA (JBR). But it is forbidden to use JBR outside of IDEA.
    Now JDK lookup is done using SdkLookup. It will try to find the local JDK, if not, it will prompt the user to download the latest version of JDK.
    
    IDEA-CR-68001
    
    GitOrigin-RevId: ac771bdee47a2503aa2e2febe9cc7c68eb093052

--- ide-features-trainer/src/training/lang/JavaLangSupport.kt
+++ ide-features-trainer/src/training/lang/JavaLangSupport.kt
@@ -2,18 +2,21 @@
 package training.lang
 
 import com.intellij.ide.impl.NewProjectUtil
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.invokeAndWaitIfNeeded
+import com.intellij.openapi.application.runInEdt
+import com.intellij.openapi.application.runWriteAction
 import com.intellij.openapi.command.CommandProcessor
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.projectRoots.*
-import com.intellij.openapi.projectRoots.impl.JavaSdkImpl
+import com.intellij.openapi.projectRoots.JavaSdk
+import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.roots.impl.LanguageLevelProjectExtensionImpl
-import com.intellij.openapi.util.Computable
+import com.intellij.openapi.roots.ui.configuration.SdkLookup
+import com.intellij.openapi.roots.ui.configuration.SdkLookupDecision
+import com.intellij.openapi.ui.Messages
 import com.intellij.pom.java.LanguageLevel
+import com.intellij.util.lang.JavaVersion
 import training.learn.LearnBundle
-import training.learn.exceptons.InvalidSdkException
-import training.learn.exceptons.NoSdkException
-import training.util.JdkSetupUtil
+import java.nio.file.Path
 
 class JavaLangSupport : AbstractLangSupport() {
   override val primaryLanguage: String = "JAVA"
@@ -22,11 +25,61 @@ class JavaLangSupport : AbstractLangSupport() {
 
   override val projectSandboxRelativePath: String = "Sample.java"
 
+  override fun installAndOpenLearningProject(projectPath: Path,
+                                             projectToClose: Project?,
+                                             postInitCallback: (learnProject: Project) -> Unit) {
+    super.installAndOpenLearningProject(projectPath, projectToClose) { project ->
+      findJavaSdkAsync { sdk ->
+        if (sdk != null) {
+          applyProjectSdk(sdk, project)
+        }
+      }
+      postInitCallback(project)
+    }
+  }
+
+  private fun findJavaSdkAsync(onSdkSearchCompleted: (Sdk?) -> Unit) {
+    val javaSdkType = JavaSdk.getInstance()
+    SdkLookup.newLookupBuilder()
+      .withSdkType(javaSdkType)
+      .withVersionFilter {
+        JavaVersion.tryParse(it)?.isAtLeast(6) == true
+      }
+      .onDownloadableSdkSuggested { sdkFix ->
+        val userDecision = invokeAndWaitIfNeeded {
+          Messages.showYesNoDialog(
+            LearnBundle.message("learn.project.initializing.jdk.download.message", sdkFix.downloadDescription),
+            LearnBundle.message("learn.project.initializing.jdk.download.title"),
+            null
+          )
+        }
+        if (userDecision == Messages.YES) {
+          SdkLookupDecision.CONTINUE
+        }
+        else {
+          onSdkSearchCompleted(null)
+          SdkLookupDecision.STOP
+        }
+      }
+      .onSdkResolved { sdk ->
+        if (sdk != null && sdk.sdkType === javaSdkType) {
+          onSdkSearchCompleted(sdk)
+        }
+      }
+      .executeLookup()
+  }
+
+  override fun getSdkForProject(project: Project): Sdk? {
+    return null
+  }
+
   override fun applyProjectSdk(sdk: Sdk, project: Project) {
     val applySdkAction = {
-      ApplicationManager.getApplication().runWriteAction { NewProjectUtil.applyJdkToProject(project, sdk) }
+      runWriteAction { NewProjectUtil.applyJdkToProject(project, sdk) }
     }
-    CommandProcessor.getInstance().executeCommand(project, applySdkAction, null, null)
+    runInEdt {
+      CommandProcessor.getInstance().executeCommand(project, applySdkAction, null, null)
+    }
   }
 
   override fun applyToProjectAfterConfigure(): (Project) -> Unit = { newProject ->
@@ -34,66 +87,7 @@ class JavaLangSupport : AbstractLangSupport() {
     LanguageLevelProjectExtensionImpl.getInstanceImpl(newProject).currentLevel = LanguageLevel.JDK_1_6
   }
 
-  //Java SDK and project configuration staff
-  override fun getSdkForProject(project: Project): Sdk? {
-    return if (ApplicationManager.getApplication().isUnitTestMode)
-      ApplicationManager.getApplication().runWriteAction(Computable<Sdk> { getJavaSdk() })
-    else
-      getJavaSdk()
-  }
-
-  private fun getJavaSdk(): Sdk {
-
-    //check for stored jdk
-    val jdkList = getJdkList()
-    if (jdkList.isNotEmpty()) {
-      jdkList
-        .filter { JavaSdk.getInstance().getVersion(it) != null && JavaSdk.getInstance().getVersion(it)!!.isAtLeast(JavaSdkVersion.JDK_1_6) }
-        .forEach { return it }
-    }
-
-    //if no predefined jdks -> add bundled jdk to available list and return it
-    val javaSdk = JavaSdk.getInstance()
-
-    val bundleList = JdkSetupUtil.findJdkPaths().bundles
-    //we believe that Idea has at least one bundled jdk
-    val jdkBundle = bundleList.first()
-    val jdkBundleLocation = JdkSetupUtil.getJavaHomePath(jdkBundle)
-    val jdkName = "JDK_" + jdkBundle.bundleVersion.toString()
-    val newJdk = javaSdk.createJdk(jdkName, jdkBundleLocation, false)
-
-    val foundJdk = ProjectJdkTable.getInstance().findJdk(newJdk.name, newJdk.sdkType.name)
-    if (foundJdk == null) ApplicationManager.getApplication().runWriteAction { ProjectJdkTable.getInstance().addJdk(newJdk) }
-
-    ApplicationManager.getApplication().runWriteAction {
-      val modifier = newJdk.sdkModificator
-      JavaSdkImpl.attachJdkAnnotations(modifier)
-      modifier.commitChanges()
-    }
-
-    return newJdk
-  }
-
-  override fun checkSdk(sdk: Sdk?, project: Project) {
-    val sdkTypeId = sdk?.sdkType
-    if (sdkTypeId is JavaSdk) {
-      val version = sdkTypeId.getVersion(sdk)
-      if (version != null && !version.isAtLeast(JavaSdkVersion.JDK_1_6)) {
-        throw InvalidSdkException(LearnBundle.message("dialog.message.jdk.java.1.6.required"))
-      }
-    }
-    else {
-      throw NoSdkException()
-    }
-  }
-
-  private fun getJdkList(): MutableList<Sdk> {
-    val type = JavaSdk.getInstance()
-    val allJdks = ProjectJdkTable.getInstance().allJdks
-    return allJdks.filterTo(mutableListOf()) { isCompatibleJdk(it, type) }
-  }
-
-  private fun isCompatibleJdk(projectJdk: Sdk, type: SdkType?) = (type == null || projectJdk.sdkType === type)
+  override fun checkSdk(sdk: Sdk?, project: Project) {}
 
   companion object {
     @JvmStatic
