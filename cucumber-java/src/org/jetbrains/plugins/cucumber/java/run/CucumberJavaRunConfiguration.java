257c59b4043f7405aa8feb5764ae8f82219aa8b6
Kirill Timofeev <kirill.timofeev@jetbrins.com>
2020-11-10 21:38:49 +0300

    Use `JavaRunConfigurationExtensionManager` instead of manually iterating over configuration extensions
    This save us from accidentally updating java parameters from disabled RunConfigurationExtension
    
    This is also a proper fix for IDEA-254972
    
    GitOrigin-RevId: 797318367a9d61895d954d70513add6482143d05

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/run/CucumberJavaRunConfiguration.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/run/CucumberJavaRunConfiguration.java
@@ -83,9 +83,8 @@ public class CucumberJavaRunConfiguration extends ApplicationConfiguration {
         }
 
         params.setMainClass(getMainClassName());
-        for (RunConfigurationExtension ext : RunConfigurationExtension.EP_NAME.getExtensionList()) {
-          ext.updateJavaParameters(CucumberJavaRunConfiguration.this, params, getRunnerSettings(), executor);
-        }
+        JavaRunConfigurationExtensionManager.getInstance()
+          .updateJavaParameters(CucumberJavaRunConfiguration.this, params, getRunnerSettings(), executor);
 
         final String glueValue = getGlue();
         if (glueValue != null && !StringUtil.isEmpty(glueValue)) {
