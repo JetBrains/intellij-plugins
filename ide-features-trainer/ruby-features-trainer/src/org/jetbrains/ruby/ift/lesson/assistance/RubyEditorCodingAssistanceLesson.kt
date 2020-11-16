023340c4062acb7d0d2804ac667dcb3317825d30
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-11-16 14:53:22 +0300

    [IFT] Fix tests in EditorCodingAssistance lesson
    IDEA-CR-68261
    
    (cherry picked from commit d32db9d926583be9797e21f6a470201240972ab9)
    
    GitOrigin-RevId: c4b5a27f18b247015f7470c6e72986b10e04280d

--- ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/assistance/RubyEditorCodingAssistanceLesson.kt
+++ ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/assistance/RubyEditorCodingAssistanceLesson.kt
@@ -1,8 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.ruby.ift.lesson.assistance
 
+import com.intellij.testGuiFramework.fixtures.IdeFrameFixture
+import com.intellij.testGuiFramework.impl.jList
 import org.jetbrains.plugins.ruby.RBundle
-import training.commands.kotlin.TaskRuntimeContext
 import training.learn.interfaces.Module
 import training.learn.lesson.general.assistance.EditorCodingAssistanceLesson
 import training.learn.lesson.kimpl.LessonSample
@@ -10,12 +11,14 @@ import training.learn.lesson.kimpl.LessonSample
 class RubyEditorCodingAssistanceLesson(module: Module, lang: String, sample: LessonSample) :
   EditorCodingAssistanceLesson(module, lang, sample) {
 
-  override fun TaskRuntimeContext.checkErrorFixed(): Boolean {
-    return editor.document.charsSequence.contains("cat.say_meow()")
+  override fun IdeFrameFixture.simulateErrorFixing() {
+    jList(intentionDisplayName).item(intentionDisplayName).doubleClick()
   }
+
+  override val fixedText: String = "cat.say_meow()"
 
   override val intentionDisplayName: String
     get() = RBundle.message("inspection.argcount.extra.argument.fix")
 
-  override val variableNameToHighlight: String = "@happiness"
+  override val variableNameToHighlight: String = "happiness"
 }
