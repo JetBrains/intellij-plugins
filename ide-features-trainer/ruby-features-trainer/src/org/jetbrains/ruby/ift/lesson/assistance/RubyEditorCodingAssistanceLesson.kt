fc47ec143312d1dced9db194f0bc08aa8e8f753d
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2020-11-20 19:27:25 +0300

    [IFT] Edit editor coding assistance lesson
    IDEA-CR-69023
    
    GitOrigin-RevId: ffd14dfc259fd68a94c3a341b3ba008e8c3db6cf

--- ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/assistance/RubyEditorCodingAssistanceLesson.kt
+++ ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/assistance/RubyEditorCodingAssistanceLesson.kt
@@ -1,8 +1,6 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.ruby.ift.lesson.assistance
 
-import com.intellij.testGuiFramework.fixtures.IdeFrameFixture
-import com.intellij.testGuiFramework.impl.jList
 import org.jetbrains.plugins.ruby.RBundle
 import training.learn.interfaces.Module
 import training.learn.lesson.general.assistance.EditorCodingAssistanceLesson
@@ -10,15 +8,13 @@ import training.learn.lesson.kimpl.LessonSample
 
 class RubyEditorCodingAssistanceLesson(module: Module, lang: String, sample: LessonSample) :
   EditorCodingAssistanceLesson(module, lang, sample) {
-
-  override fun IdeFrameFixture.simulateErrorFixing() {
-    jList(intentionDisplayName).item(intentionDisplayName).doubleClick()
-  }
-
-  override val fixedText: String = "cat.say_meow()"
-
-  override val intentionDisplayName: String
+  override val errorIntentionText: String
     get() = RBundle.message("inspection.argcount.extra.argument.fix")
+  override val warningIntentionText: String
+    get() = RBundle.message("inspection.parentheses.around.conditional.message")
+
+  override val errorFixedText: String = "cat.say_meow()"
+  override val warningFixedText: String = "cat.say_meow\n"
 
   override val variableNameToHighlight: String = "happiness"
 }
