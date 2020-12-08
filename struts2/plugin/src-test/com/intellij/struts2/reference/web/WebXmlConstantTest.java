602f02ade5637f2c184b36a8f21d400f3c61573a
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2020-12-08 20:30:54 +0300

    Java EE: remove overridden getShortName from inspections
    GitOrigin-RevId: af7b114374abd05c280e542147cf2d4fda411248

--- struts2/plugin/src-test/com/intellij/struts2/reference/web/WebXmlConstantTest.java
+++ struts2/plugin/src-test/com/intellij/struts2/reference/web/WebXmlConstantTest.java
@@ -21,6 +21,7 @@ import com.intellij.j2ee.web.highlighting.WebXmlInspection;
 import com.intellij.struts2.BasicLightHighlightingTestCase;
 import com.intellij.struts2.model.constant.StrutsConstant;
 import com.intellij.struts2.model.constant.contributor.StrutsCoreConstantContributor;
+import com.intellij.testFramework.InspectionTestUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.jetbrains.annotations.NotNull;
 
@@ -35,7 +36,10 @@ public class WebXmlConstantTest extends BasicLightHighlightingTestCase {
 
   @Override
   protected LocalInspectionTool[] getHighlightingInspections() {
-    return new LocalInspectionTool[]{new WebXmlInspection(), new WebWarningInspection()};
+    return new LocalInspectionTool[]{
+      InspectionTestUtil.instantiateTool(WebXmlInspection.class),
+      InspectionTestUtil.instantiateTool(WebWarningInspection.class)
+    };
   }
 
   @Override
