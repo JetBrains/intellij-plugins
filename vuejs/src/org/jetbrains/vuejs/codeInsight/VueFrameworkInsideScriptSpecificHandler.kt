e2dc226da3b3ff413c23e0256da46305126714c9
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-12-09 09:51:58 +0100

    WEB-46889 Support special resolve on `this` in TS context
    IDEA-CR-69590
    
    (cherry picked from commit 91859312f570a11379816b3c8005029e9d01d7ad)
    
    GitOrigin-RevId: 4e925e380c61193110ddb47b279b865a78a61c82

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueFrameworkInsideScriptSpecificHandler.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueFrameworkInsideScriptSpecificHandler.kt
@@ -9,8 +9,7 @@ import com.intellij.lang.javascript.psi.JSFile
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
 import com.intellij.lang.javascript.psi.JSType
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptTypeAlias
-import com.intellij.lang.javascript.psi.types.JSCompositeTypeFactory
-import com.intellij.lang.javascript.psi.types.JSUnionType
+import com.intellij.lang.javascript.psi.types.*
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlTag
@@ -19,6 +18,10 @@ import org.jetbrains.vuejs.index.VUE_MODULE
 import org.jetbrains.vuejs.index.VueFrameworkHandler
 import org.jetbrains.vuejs.lang.expr.VueJSLanguage
 import org.jetbrains.vuejs.lang.html.VueFileType
+import org.jetbrains.vuejs.model.VueModelManager
+import org.jetbrains.vuejs.model.source.COMPUTED_PROP
+import org.jetbrains.vuejs.model.source.METHODS_PROP
+import org.jetbrains.vuejs.model.source.WATCH_PROP
 
 class VueFrameworkInsideScriptSpecificHandler : JSFrameworkSpecificHandler {
   companion object {
@@ -42,16 +45,34 @@ class VueFrameworkInsideScriptSpecificHandler : JSFrameworkSpecificHandler {
     return null
   }
 
-  private fun getObjectLiteralTypeForComponent(obj: JSObjectLiteralExpression): JSType? =
-    resolveSymbolFromNodeModule(obj, VUE_MODULE, "Component", TypeScriptTypeAlias::class.java)
+  private fun getObjectLiteralTypeForComponent(obj: JSObjectLiteralExpression): JSType? {
+    /* The information about `this` type is processed only in TS context. */
+    val thisType = VueModelManager.findEnclosingComponent(obj)
+      ?.thisType
+      ?.let {
+        JSGenericTypeImpl(it.source, JSNamedTypeFactory.createType("ThisType", it.source, JSTypeContext.INSTANCE), it)
+      }
+    val propertiesWithThisType =
+      if (thisType != null)
+        JSRecordTypeImpl(thisType.source, sequenceOf(COMPUTED_PROP, METHODS_PROP, WATCH_PROP)
+          .map { JSRecordTypeImpl.PropertySignatureImpl(it, thisType, true, false) }
+          .toList()
+        )
+      else null
+    val definitionType = resolveSymbolFromNodeModule(obj, VUE_MODULE, "Component", TypeScriptTypeAlias::class.java)
       ?.typeDeclaration
       ?.jsType
+    return listOfNotNull(definitionType, thisType, propertiesWithThisType)
+      .takeIf { it.isNotEmpty() }
+      ?.let {
+        JSCompositeTypeFactory.createUnionType(it[0].source, it)
+      }
       ?.let {
         if (it is JSUnionType) {
           JSCompositeTypeFactory.createContextualUnionType(it.types, it.source)
         }
-        else {
-          it
-        }
+        else it
       }
+  }
+
 }
---
+++ vuejs/vuejs-tests/testData/resolve/gotoDeclarationTS.vue
@@ -0,0 +1,52 @@
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'TrackManager',
+
+  watch: {
+    async folderId() {
+      await this.fetchTracks();
+    },
+
+    async tableOptions(options, oldOptions) {
+      if (oldOptions.page !== options.page) {
+        // await this.$vuetify.goTo(0);
+      }
+      await this.fetchTracks/*watch*/();
+    },
+  },
+
+  computed: {
+    foo_comp: function() {
+      return await this.fetchTracks/*computed*/();
+    }
+  }
+
+  async created() {
+    await this.fetchTracks/*base*/();
+  },
+
+  methods: {
+    async fetchTracks() {
+      this.loading = true;
+
+      // const { page, itemsPerPage } = this.tableOptions;
+
+      try {
+        // this.tracks = await getTracks(this.folderId, {
+        //   limit: itemsPerPage,
+        //   offset: (page - 1) * itemsPerPage,
+        // });
+      } catch (e) {
+        // this.$reportError(e, { while: 'loading tracks' });
+      } finally {
+        this.loading = false;
+      }
+    },
+    async foo() {
+      this.fetchTracks/*methods*/()
+    }
+  },
+});
+</script>
