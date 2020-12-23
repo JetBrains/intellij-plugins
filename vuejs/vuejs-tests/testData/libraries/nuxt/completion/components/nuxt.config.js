96bacc91d9f05b70da3e443e936b8a6c826abd81
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2020-12-23 20:35:38 +0100

    WEB-48837 Nuxt: Issue auto-importing components from paths ending with '/'
    GitOrigin-RevId: 4a342d295bca6e2f7dd5f7f9b9d9fd11452e4dd4

--- vuejs/vuejs-tests/testData/libraries/nuxt/completion/components/nuxt.config.js
+++ vuejs/vuejs-tests/testData/libraries/nuxt/completion/components/nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: [
     "~/components",
-    {path: "components/special", extensions: ["vue"]},
-    {path: "@/prefixed", prefix: "prefixed"}
+    {path: "components/special//", extensions: ["vue"]},
+    {path: "@/prefixed/", prefix: "prefixed"}
   ]
 }
