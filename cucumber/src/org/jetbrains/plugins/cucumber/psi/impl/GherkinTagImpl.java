1a1ae7ea469d2b207e8a5ae903394dd35d991d25
Andrey Vokin <andrey.vokin@jetbrains.com>
2021-01-07 12:21:17 +0100

    [cucumber] use ReferenceProvidersRegistry to retrieve references
    GitOrigin-RevId: 9c0308a01315aa3fdcbcee6fc2bd9fa082921ffe

--- cucumber/src/org/jetbrains/plugins/cucumber/psi/impl/GherkinTagImpl.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/psi/impl/GherkinTagImpl.java
@@ -1,9 +1,13 @@
 package org.jetbrains.plugins.cucumber.psi.impl;
 
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.impl.source.resolve.reference.ReferenceProvidersRegistry;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.CachedValuesManager;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.GherkinElementVisitor;
 import org.jetbrains.plugins.cucumber.psi.GherkinTag;
-import org.jetbrains.annotations.NotNull;
-import com.intellij.lang.ASTNode;
 
 /**
  * @author yole
@@ -21,6 +25,15 @@ public class GherkinTagImpl extends GherkinPsiElementBase implements GherkinTag 
   @Override
   public String getName() {
     return getText();
+  }
+
+  @Override
+  public PsiReference @NotNull [] getReferences() {
+    return CachedValuesManager.getCachedValue(this, () -> CachedValueProvider.Result.create(getReferencesInner(), this));
+  }
+
+  private PsiReference[] getReferencesInner() {
+    return ReferenceProvidersRegistry.getReferencesFromProviders(this);
   }
 
   @Override
