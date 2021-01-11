14122675d131f6760ecaf78c94c69614284eb054
Dmitry Jemerov <yole@jetbrains.com>
2021-01-11 20:17:20 +0100

    Delete unused child role machinery in XML PSI
    GitOrigin-RevId: b0223dd2d7266f8d1eb73436b23476df52533324

--- AngularJS/src/org/angular2/lang/html/psi/impl/Angular2HtmlLetImpl.java
+++ AngularJS/src/org/angular2/lang/html/psi/impl/Angular2HtmlLetImpl.java
@@ -1,13 +1,10 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.lang.html.psi.impl;
 
-import com.intellij.lang.ASTNode;
 import com.intellij.lang.javascript.psi.JSVariable;
 import com.intellij.psi.PsiElementVisitor;
 import com.intellij.psi.XmlElementVisitor;
-import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.util.PsiTreeUtil;
-import com.intellij.psi.xml.XmlChildRole;
 import com.intellij.psi.xml.XmlElement;
 import org.angular2.lang.html.parser.Angular2HtmlElementTypes.Angular2ElementType;
 import org.angular2.lang.html.parser.Angular2HtmlVarAttrTokenType;
@@ -20,15 +17,6 @@ public class Angular2HtmlLetImpl extends Angular2HtmlBoundAttributeImpl implemen
 
   public Angular2HtmlLetImpl(@NotNull Angular2ElementType type) {
     super(type);
-  }
-
-  @Override
-  public int getChildRole(@NotNull ASTNode child) {
-    IElementType i = child.getElementType();
-    if (i == Angular2HtmlVarAttrTokenType.LET) {
-      return XmlChildRole.XML_NAME;
-    }
-    return super.getChildRole(child);
   }
 
   @Override
