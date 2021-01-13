e9254aaa0b1d7a5818e5aad34291640722f86412
Nikolay Chashnikov <Nikolay.Chashnikov@jetbrains.com>
2021-01-13 13:16:44 +0300

    API cleanup: delete unused scheduled for removal API (IDEA-259329)
    GitOrigin-RevId: 32f1c7d8ce0223d549c6a2a89b8dc3164ed4ac9c

--- cucumber/src/org/jetbrains/plugins/cucumber/steps/AbstractCucumberExtension.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/steps/AbstractCucumberExtension.java
@@ -1,23 +1,7 @@
 package org.jetbrains.plugins.cucumber.steps;
 
-import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiElement;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.CucumberJvmExtensionPoint;
 
-import java.util.List;
-
 public abstract class AbstractCucumberExtension implements CucumberJvmExtensionPoint {
-  @Override
-  public void flush(@NotNull final Project project) {
-  }
 
-  @Override
-  public void reset(@NotNull final Project project) {
-  }
-
-  @Override
-  public Object getDataObject(@NotNull Project project) {
-    return null;
-  }
 }
