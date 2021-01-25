5a78f86af530d6d5195db98e9d078d1cc0b831c9
Aleksandr Prokopyev <aleksandr.prokopyev@jetbrains.com>
2021-01-25 16:04:45 +0300

    RUBY-21118: Minitest multiple files
    GitOrigin-RevId: 73ed22ada724db8c8d35d045882a1bd6543234b4

--- ruby-testing/src/rb/testing/runner/minitest_runner.rb
+++ ruby-testing/src/rb/testing/runner/minitest_runner.rb
@@ -2,9 +2,11 @@ require File.dirname(__FILE__) + '/runner_settings'
 require 'teamcity/utils/runner_utils'
 require 'rubymine_test_framework_initializer'
 
-file = ENV["INTELLIJ_IDEA_RUN_CONF_TEST_FILE_PATH"]
-if file.nil? or file.length == 0
+files = ENV["INTELLIJ_IDEA_RUN_CONF_TEST_FILE_PATH"] # separated by "||" filenames
+if files.nil? or files.length == 0
    raise Exception.new("A test file must be provided")
 end
 
-require file
+files.split("||").each do |file|
+   require file
+end
