ded41949169b0f7971bd32d9ed3ddf3bf962c4db
Semyon Kolton <semyon.kolton@jetbrains.com>
2021-01-29 10:28:17 +0300

    CPP-16933 Update icons for Makefile and Makefile Language plugins
    GitOrigin-RevId: c3976f87ef6f0c39dc7e1bd9b114dbd06d196e5d

--- makefile/gen/icons/MakefileIcons.java
+++ makefile/gen/icons/MakefileIcons.java
@@ -13,5 +13,6 @@ public final class MakefileIcons {
   private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, MakefileIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x15 */ public static final @NotNull Icon Makefile = load("icons/makefile.png", 0L, 1);
+  /** 16x16 */ public static final @NotNull Icon Makefile = load("icons/makefile.svg", -8376093009317382483L, 0);
+  /** 13x13 */ public static final @NotNull Icon MakefileToolWindow = load("icons/makefileToolWindow.svg", -3030850275229720534L, 2);
 }
