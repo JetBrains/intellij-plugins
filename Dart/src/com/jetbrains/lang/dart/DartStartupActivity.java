3a15c56caa1561a470f5bd51ebbabdd118f9cc5a
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2021-02-14 19:48:25 +0300

    EA-251596 Slow operations are prohibited in the EDT (DartStartupActivity)
    GitOrigin-RevId: ecadbc178ae40ab6e5e02fc66f62c0d35f188480

--- Dart/src/com/jetbrains/lang/dart/DartStartupActivity.java
+++ Dart/src/com/jetbrains/lang/dart/DartStartupActivity.java
@@ -1,7 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart;
 
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.roots.ModuleRootManager;
@@ -9,15 +13,19 @@ import com.intellij.openapi.roots.ModuleRootModificationUtil;
 import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.startup.StartupActivity;
 import com.intellij.openapi.util.Condition;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.search.FileTypeIndex;
 import com.intellij.psi.search.FilenameIndex;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.util.containers.ContainerUtil;
+import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.jetbrains.lang.dart.projectWizard.DartModuleBuilder;
 import com.jetbrains.lang.dart.sdk.DartSdk;
+import com.jetbrains.lang.dart.sdk.DartSdkLibUtil;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -31,22 +39,37 @@ import static com.jetbrains.lang.dart.util.PubspecYamlUtil.PUBSPEC_YAML;
  * @see DartFileListener
  * @see DartModuleRootListener
  */
-public final class DartStartupActivity implements StartupActivity {
+public final class DartStartupActivity implements StartupActivity.Background {
   @Override
   public void runActivity(@NotNull Project project) {
     final Collection<VirtualFile> pubspecYamlFiles =
-      FilenameIndex.getVirtualFilesByName(project, PUBSPEC_YAML, GlobalSearchScope.projectScope(project));
+      ReadAction.compute(() -> FilenameIndex.getVirtualFilesByName(project, PUBSPEC_YAML, GlobalSearchScope.projectScope(project)));
 
-    for (VirtualFile pubspecYamlFile : pubspecYamlFiles) {
-      final Module module = ModuleUtilCore.findModuleForFile(pubspecYamlFile, project);
-      if (module != null && FileTypeIndex.containsFileOfType(DartFileType.INSTANCE, module.getModuleContentScope())) {
-        excludeBuildAndToolCacheFolders(module, pubspecYamlFile);
+    if (pubspecYamlFiles.isEmpty()) {
+      return;
+    }
+
+    Collection<Pair<Module, VirtualFile>> modulesAndPubspecs = new ArrayList<>(pubspecYamlFiles.size());
+    ReadAction.run(() -> {
+      for (VirtualFile pubspecYamlFile : pubspecYamlFiles) {
+        final Module module = ModuleUtilCore.findModuleForFile(pubspecYamlFile, project);
+        if (module != null) {
+          modulesAndPubspecs.add(Pair.create(module, pubspecYamlFile));
+        }
       }
+    });
+
+    if (!modulesAndPubspecs.isEmpty()) {
+      ApplicationManager.getApplication().invokeLater(() -> {
+        for (Pair<Module, VirtualFile> moduleAndPubspec : modulesAndPubspecs) {
+          excludeBuildAndToolCacheFolders(moduleAndPubspec.first, moduleAndPubspec.second);
+        }
+      }, ModalityState.NON_MODAL, DartAnalysisServerService.getInstance(project).getDisposedCondition());
     }
 
-    if (!pubspecYamlFiles.isEmpty()) {
-      DartFileListener.scheduleDartPackageRootsUpdate(project);
-    }
+    DartFileListener.scheduleDartPackageRootsUpdate(project);
+
+    startAnalysisServerIfNeeded(project);
   }
 
   public static void excludeBuildAndToolCacheFolders(final @NotNull Module module, final @NotNull VirtualFile pubspecYamlFile) {
@@ -102,5 +125,22 @@ public final class DartStartupActivity implements StartupActivity {
     newExcludedPackagesUrls.add(root.getUrl() + "/build");
 
     return newExcludedPackagesUrls;
+  }
+
+  private static void startAnalysisServerIfNeeded(@NotNull Project project) {
+    if (DartModuleBuilder.isPubGetScheduledForNewlyCreatedProject(project)) {
+      // We want to start Analysis Server after initial 'pub get' is finished, this will be done in DartPubActionBase
+      return;
+    }
+
+    final DartSdk sdk = DartSdk.getDartSdk(project);
+    if (sdk == null) return;
+
+    for (final Module module : ModuleManager.getInstance(project).getModules()) {
+      if (DartSdkLibUtil.isDartSdkEnabled(module)) {
+        ReadAction.run(() -> DartAnalysisServerService.getInstance(project).serverReadyForRequest());
+        break;
+      }
+    }
   }
 }
