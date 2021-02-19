7610b033fb7601131b8013edb785d80534f70fd0
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-02-19 15:33:03 +0100

    WEB-34380 Allow to inject references into JSProperty name
    (cherry picked from commit 95d5c202b7e118437ca8a962ba0e37b1acf2e20e)
    
    IJ-CR-6158
    
    GitOrigin-RevId: 1083fdae8de19ffde145315dbe86873f8f5cc01a

--- AngularJS/src/org/angular2/lang/expr/parser/Angular2StubElementTypes.java
+++ AngularJS/src/org/angular2/lang/expr/parser/Angular2StubElementTypes.java
@@ -6,8 +6,7 @@ import com.intellij.lang.javascript.psi.JSVariable;
 import com.intellij.lang.javascript.psi.stubs.JSVariableStub;
 
 public interface Angular2StubElementTypes {
-  int STUB_VERSION = 2;
+  int STUB_VERSION = 3;
 
-  Angular2PropertyElementType PROPERTY = new Angular2PropertyElementType();
   JSStubElementType<JSVariableStub<JSVariable>, JSVariable> TEMPLATE_VARIABLE = new Angular2TemplateVariableElementType();
 }
