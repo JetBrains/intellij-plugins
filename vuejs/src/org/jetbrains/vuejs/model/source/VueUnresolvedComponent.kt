ab0eca080971da9e02ea5c49a9d23633815b378f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-02-23 17:01:15 +0100

    WEB-31701 WEB-47477 Support $refs attributes - tests
    GitOrigin-RevId: 1a929c7dc686336178c61bdbf538ad5e12189aec

--- vuejs/src/org/jetbrains/vuejs/model/source/VueUnresolvedComponent.kt
+++ vuejs/src/org/jetbrains/vuejs/model/source/VueUnresolvedComponent.kt
@@ -1,14 +1,20 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.model.source
 
+import com.intellij.lang.javascript.psi.JSType
+import com.intellij.lang.javascript.psi.types.JSAnyType
 import com.intellij.psi.PsiElement
 import org.jetbrains.vuejs.model.VueComponent
 import org.jetbrains.vuejs.model.VueEntitiesContainer
+import org.jetbrains.vuejs.model.getDefaultVueComponentInstanceType
 
-class VueUnresolvedComponent : VueComponent {
+class VueUnresolvedComponent(private val context: PsiElement) : VueComponent {
 
   override val defaultName: String? = null
   override val source: PsiElement? = null
   override val parents: List<VueEntitiesContainer> = emptyList()
+
+  override val thisType: JSType
+    get() = getDefaultVueComponentInstanceType(context) ?: JSAnyType.get(context, false)
 
 }
