67a86a1e637ae7a0c1d2af9f6dca9602f4ae5eb2
Jaime Wren <jwren@google.com>
2021-02-24 20:08:57 +0300

    Include fix_data.yaml as a Dart plugin supported yaml file
    close #793
    
    (cherry picked from commit e51b5fefa477af2515c86826d7794cd7863d769a)
    
    IJ-CR-6364
    
    GitOrigin-RevId: 8f506e4cfc9d0c9f1deea54b2a22a802dc1a3104

--- Dart/src/com/jetbrains/lang/dart/analyzer/DartAnalysisServerService.java
+++ Dart/src/com/jetbrains/lang/dart/analyzer/DartAnalysisServerService.java
@@ -864,6 +864,7 @@ public final class DartAnalysisServerService implements Disposable {
            fileName.equals(".analysis_options") ||
            fileName.equals("analysis_options.yaml") ||
            fileName.equals("pubspec.yaml") ||
+           fileName.equals("fix_data.yaml") ||
            fileName.equals("androidmanifest.xml");
   }
 
