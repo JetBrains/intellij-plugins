4fe92b3784fdc23ad8cf36d92ce8ef681d8b82be
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-02-24 10:07:45 +0100

    WEB-49122 Avoid accessing index in EDT for CreateVueSingleFileComponentAction
    GitOrigin-RevId: a23f7fcdbf17a45e2f984aee5820ce4de33b4c81

--- vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
+++ vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.context
 
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
@@ -10,6 +11,7 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import org.jetbrains.vuejs.VueFramework
+import org.jetbrains.vuejs.codeInsight.edtSafeEval
 import org.jetbrains.vuejs.lang.html.VueFileType
 
 
@@ -17,9 +19,13 @@ fun isVueContext(context: PsiElement): Boolean = VueFramework.instance.isContext
 
 fun isVueContext(contextFile: VirtualFile, project: Project): Boolean = VueFramework.instance.isContext(contextFile, project)
 
-fun hasVueFiles(project: Project): Boolean = CachedValuesManager.getManager(project).getCachedValue(project) {
-  CachedValueProvider.Result.create(
-    FileTypeIndex.containsFileOfType(VueFileType.INSTANCE, GlobalSearchScope.projectScope(project)),
-    VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS
-  )
+fun hasVueFiles(project: Project): Boolean = edtSafeEval(project, 5, false) {
+  CachedValuesManager.getManager(project).getCachedValue(project) {
+    CachedValueProvider.Result.create(
+      ReadAction.compute<Boolean, Exception> {
+        FileTypeIndex.containsFileOfType(VueFileType.INSTANCE, GlobalSearchScope.projectScope(project))
+      },
+      VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS
+    )
+  }
 }
