93c2c5825c383e3e89b7c224cb5a56f68f1913bb
Dmitry Kozhevnikov <dmitry.kozhevnikov@jetbrains.com>
2021-03-01 18:48:12 +0300

    CPP-23944: hide makefile tool window for non-makefile projects in CLion by default
    Indended behavior:
    
    1. Outside of CLion (or when clion-makefile plugin is disabled), the toolwindow appears on the stripe, as before
    2. In CLion, in a Makefile project, the toolwindow appears on the stripe, as before
    3. In CLion, for a non-Makefile project, if the tool window was opened before in this project, its state stays the same
    4. Otherwise (in CLion, for a non-Makefile project, when where was no toolwindow before), it doesn't appear on the stripe.
    
    It can always be brought up again by the 'View | Tool Windows' menu.
    
    (cherry picked from commit f4607d2cf93a22d0fed52013eb54e2f4bf7f897b)
    
    IJ-CR-6447
    
    GitOrigin-RevId: 061de184e7eb31efa820146f18205c2f7e8402d7

---
+++ makefile/src/com/jetbrains/lang/makefile/toolWindow/MakefileToolWindowRegistrar.kt
@@ -0,0 +1,47 @@
+package com.jetbrains.lang.makefile.toolWindow
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.wm.RegisterToolWindowTask
+import com.intellij.openapi.wm.ToolWindowAnchor
+import com.intellij.openapi.wm.ToolWindowManager
+import com.intellij.openapi.wm.ex.ToolWindowManagerEx
+import com.jetbrains.lang.makefile.MakefileLangBundle
+import icons.MakefileIcons
+
+private const val TOOLWINDOW_ID = "make" // the ID is unfortunate, but should be kept compatible with older versions
+
+class MakefileToolWindowRegistrar : StartupActivity {
+  override fun runActivity(project: Project) {
+    val toolWindowManager = ToolWindowManager.getInstance(project)
+    val toolWindow = toolWindowManager.registerToolWindow(RegisterToolWindowTask(
+      id = TOOLWINDOW_ID,
+      stripeTitle = MakefileLangBundle.messagePointer("tool.window.title"),
+      icon = MakefileIcons.MakefileToolWindow,
+      contentFactory = MakeToolWindowFactory(),
+      anchor = ToolWindowAnchor.RIGHT,
+      sideTool = true))
+
+
+    if (shouldDisableStripeButton(project, toolWindowManager)) {
+      toolWindow.isShowStripeButton = false
+    }
+  }
+
+  private fun shouldDisableStripeButton(project: Project, manager: ToolWindowManager): Boolean {
+    val windowInfo = (manager as ToolWindowManagerEx).layout.getInfo(TOOLWINDOW_ID)
+
+    // toolwindow existed in ths project before - show it
+    if (windowInfo != null && windowInfo.isFromPersistentSettings) {
+      return false
+    }
+
+    // any extension reported that it's desired to hide it by default (i.e. it's CLion's non-Makefile project) - hide it
+    if (MakefileToolWindowStripeController.EP_NAME.extensions().anyMatch { it.shouldHideStripeIconFor(project) }) {
+      return true
+    }
+
+    // show it otherwise
+    return false
+  }
+}
