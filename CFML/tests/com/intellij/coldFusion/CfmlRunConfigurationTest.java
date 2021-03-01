ba921c957207ac011b68fdd8346f9f38ab6e662d
Andrey.Cherkasov <andrey.cherkasov@jetbrains.com>
2021-03-01 07:00:54 +0300

    Add the "Redundant File object creation" inspection to zero-tolerance profile
    GitOrigin-RevId: 6c4ae26aab7254c7bc773d06720ec57480b962f4

--- CFML/tests/com/intellij/coldFusion/CfmlRunConfigurationTest.java
+++ CFML/tests/com/intellij/coldFusion/CfmlRunConfigurationTest.java
@@ -121,7 +121,7 @@ public class CfmlRunConfigurationTest extends CfmlCodeInsightFixtureTestCase {
 
   public PsiFile createCfmPsiFile(String filename) throws IOException {
     String filePath = getDataPath() + filename;
-    FileInputStream fileInputStream = new FileInputStream(new File(filePath));
+    FileInputStream fileInputStream = new FileInputStream(filePath);
     try {
       String testText = StringUtil.convertLineSeparators(FileUtil.loadTextAndClose(fileInputStream));
       return myFixture.addFileToProject(WWWROOT_SRC + filename, testText);
