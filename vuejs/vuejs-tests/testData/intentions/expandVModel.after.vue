9e6b2a2af7fad95ee8530308ddb4a2702ee51fd5
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-03-02 10:06:39 +0100

    WEB-49845 Add "Expand v-model" intention - minor fixes.
    (cherry picked from commit 855307b438d879222a37af17f4cfcd17213f7a78)
    
    IJ-CR-6509
    
    GitOrigin-RevId: fbf9923c8e2108fc7222806dd41290b60bb78b8b

---
+++ vuejs/vuejs-tests/testData/intentions/expandVModel.after.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <main-menu :value='bar' @fooChange="bar = $event"></main-menu>
+    <main-menu :value='bar' @change="bar = $event"></main-menu>
+    <main-menu :value='bar' @fooChange="bar = isNaN(parseFloat($event)) ? $event : parseFloat($event)"></main-menu>
+    <main-menu :value='fooo' @fooChange="fooo = typeof $event === 'string' ? $event.trim() : $event"></main-menu>
+  </div>
+</template>
+<script lang='ts'>
+import {Component, Vue} from 'vue-property-decorator'
+import ICUPageMixin, {data} from './mixins'
+
+@Component({
+  name: "MainMenu",
+  model: {
+    event: 'fooChange'
+  }
+})
+export default class MainMenu extends Vue {
+
+}
+</script>
