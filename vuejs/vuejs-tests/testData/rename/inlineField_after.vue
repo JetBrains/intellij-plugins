1b96799709597069fc8e03192e58a79362eb0796
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-03-03 18:23:07 +0100

    WEB-45292 Can‘t Refactor in TypeScript File
    GitOrigin-RevId: 0ece834643362e2171011a96b049bfda398ec0ba

---
+++ vuejs/vuejs-tests/testData/rename/inlineField_after.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home" :about='foo'>
+    {{ foo }} | {{ test1 }}
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+
+@Component({})
+export default class Home extends Vue {
+  private foo = 'foo';
+  private test1 = 'test1'
+}
+</script>
