5d1f296146ff46584be050255f5780b9e49152d9
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2021-03-18 19:02:53 +0300

    IFT-181 Improve Editor coding assistance lesson
    IDEA-CR-70982
    
    GitOrigin-RevId: 4fb6581892660b07906bbbcf00b6588345e102c9

--- ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/assistance/RubyEditorCodingAssistanceLesson.kt
+++ ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/assistance/RubyEditorCodingAssistanceLesson.kt
@@ -1,8 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.ruby.ift.lesson.assistance
 
+import com.intellij.codeInsight.daemon.impl.runActionCustomShortcutSet
+import com.intellij.openapi.actionSystem.KeyboardShortcut
 import org.jetbrains.plugins.ruby.RBundle
+import org.jetbrains.ruby.ift.RubyLessonsBundle
 import training.dsl.LessonSample
+import training.dsl.LessonUtil
 import training.learn.lesson.general.assistance.EditorCodingAssistanceLesson
 
 class RubyEditorCodingAssistanceLesson(sample: LessonSample) :
@@ -16,4 +20,9 @@ class RubyEditorCodingAssistanceLesson(sample: LessonSample) :
   override val warningFixedText: String = "cat.say_meow\n"
 
   override val variableNameToHighlight: String = "happiness"
+
+  override fun getFixWarningText(): String {
+    val shortcut = runActionCustomShortcutSet.shortcuts.first() as KeyboardShortcut
+    return RubyLessonsBundle.message("ruby.editor.coding.assistance.press.to.fix", LessonUtil.rawKeyStroke(shortcut.firstKeyStroke))
+  }
 }
