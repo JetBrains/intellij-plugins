211c5e1ef48f4fd4b3a09e91cf2c4dc4d937fbcd
Dmitry Batkovich <dmitry.batkovich@jetbrains.com>
2021-03-22 08:47:58 +0300

    index: set project to FileContent instance on its creation
    GitOrigin-RevId: d5b68bf0115a7e8080da752329104e5e977625f2

--- flex/src/com/intellij/javascript/flex/MxmlTodoIndexer.java
+++ flex/src/com/intellij/javascript/flex/MxmlTodoIndexer.java
@@ -30,7 +30,7 @@ public class MxmlTodoIndexer extends XmlTodoIndexer {
                                                                            PlatformIdTableBuilding.getTodoIndexer(file.getFileType());
                                                                          if (indexer != null) {
                                                                            Map<TodoIndexEntry, Integer> injectedMap = indexer.map(
-                                                                             FileContentImpl.createByText(injectedFile, file.getText())
+                                                                             FileContentImpl.createByText(injectedFile, file.getText(), inputData.getProject())
                                                                            );
                                                                            for (Map.Entry<TodoIndexEntry, Integer> e : injectedMap
                                                                              .entrySet()) {
