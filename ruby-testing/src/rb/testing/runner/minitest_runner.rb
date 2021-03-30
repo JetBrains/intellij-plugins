54d5c61eccf2341786c1728aa509840b5022a02a
Alexandr Evstigneev <Alexandr.Evstigneev@jetbrains.com>
2021-03-30 14:45:42 +0300

    RUBY-27957 Removed minitest initializer for Minitest pre-5.0
    IJ-CR-7843
    
    GitOrigin-RevId: 8bc38cfd7d3a2dadaf14f25e5db7be220e1ca344

--- ruby-testing/src/rb/testing/runner/minitest_runner.rb
+++ ruby-testing/src/rb/testing/runner/minitest_runner.rb
@@ -1,6 +1,7 @@
+# Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 require File.dirname(__FILE__) + '/runner_settings'
 require 'teamcity/utils/runner_utils'
-require 'rubymine_test_framework_initializer'
 
 files = ENV["INTELLIJ_IDEA_RUN_CONF_TEST_FILE_PATH"] # separated by "||" filenames
 if files.nil? || files.length == 0
