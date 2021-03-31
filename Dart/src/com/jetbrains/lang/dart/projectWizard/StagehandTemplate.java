ff11771a70e0f661d029f6304d78b5e067f30939
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2021-03-31 17:16:00 +0300

    (wip) WEB-45841 Dart plugin should use the updated entry point for Stagehand
    (cherry picked from commit a7ff251b6e43bddcb5d49a52ad5508a1dece9d66)
    
    IJ-CR-8441
    
    (cherry picked from commit 1b3cf79f0eaa49e949fd6d2b7f640e2c1b87d801)
    
    GitOrigin-RevId: aa897b002bfa9b2f48b7d6f12fa798b9c0f0ca7e

--- Dart/src/com/jetbrains/lang/dart/projectWizard/StagehandTemplate.java
+++ Dart/src/com/jetbrains/lang/dart/projectWizard/StagehandTemplate.java
@@ -1,5 +1,7 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.projectWizard;
 
+import com.intellij.execution.ExecutionException;
 import com.intellij.ide.highlighter.HtmlFileType;
 import com.intellij.openapi.fileTypes.FileTypeRegistry;
 import com.intellij.openapi.module.Module;
@@ -37,7 +39,7 @@ class StagehandTemplate extends DartProjectTemplate {
     try {
       myStagehand.generateInto(sdkRoot, baseDir, myTemplate.myId);
     }
-    catch (Stagehand.StagehandException e) {
+    catch (ExecutionException e) {
       throw new IOException(e);
     }
 
@@ -47,7 +49,7 @@ class StagehandTemplate extends DartProjectTemplate {
       LocalFileSystem.getInstance().refreshAndFindFileByPath(baseDir.getPath() + "/" + PubspecYamlUtil.PUBSPEC_YAML);
     ContainerUtil.addIfNotNull(files, pubspec);
 
-    final VirtualFile mainFile = myTemplate.myEntrypoint.isEmpty()
+    final VirtualFile mainFile = StringUtil.isEmpty(myTemplate.myEntrypoint)
                                  ? null
                                  : LocalFileSystem.getInstance()
                                    .refreshAndFindFileByPath(baseDir.getPath() + "/" + myTemplate.myEntrypoint);
@@ -58,7 +60,7 @@ class StagehandTemplate extends DartProjectTemplate {
 
     ContainerUtil.addIfNotNull(files, mainFile);
 
-    if (!myTemplate.myEntrypoint.isEmpty() && mainFile != null) {
+    if (!StringUtil.isEmpty(myTemplate.myEntrypoint) && mainFile != null) {
       if (myTemplate.myEntrypoint.startsWith("bin/") && FileTypeRegistry.getInstance().isFileOfType(mainFile, DartFileType.INSTANCE)) {
         createCmdLineRunConfiguration(module, mainFile);
       }
