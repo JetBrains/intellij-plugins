39d64c5e1307fe21be9e2c4de44e1cb0c3833686
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-04-19 16:41:58 +0200

    WEB-50321 Provide support for Angular 12
    (cherry picked from commit 0300b1f0d281d01b6569b0b79b566f4cf8e476c1)
    
    IJ-CR-8387
    
    GitOrigin-RevId: b3004e3ca71662d13b00755dcc44bcd3692712e0

---
+++ AngularJS/test/org/angular2/codeInsight/data/modules/ng12-common/node_modules/@angular/material/radio/radio-module.d.ts
@@ -0,0 +1,17 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import * as ɵngcc0 from '@angular/core';
+import * as ɵngcc1 from './radio';
+import * as ɵngcc2 from '@angular/material/core';
+export declare class MatRadioModule {
+    static ɵfac: ɵngcc0.ɵɵFactoryDeclaration<MatRadioModule, never>;
+    static ɵmod: ɵngcc0.ɵɵNgModuleDeclaration<MatRadioModule, [typeof ɵngcc1.MatRadioGroup, typeof ɵngcc1.MatRadioButton], [typeof ɵngcc2.MatRippleModule, typeof ɵngcc2.MatCommonModule], [typeof ɵngcc1.MatRadioGroup, typeof ɵngcc1.MatRadioButton, typeof ɵngcc2.MatCommonModule]>;
+    static ɵinj: ɵngcc0.ɵɵInjectorDeclaration<MatRadioModule>;
+}
+
+//# sourceMappingURL=radio-module.d.ts.map
