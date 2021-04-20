3012f973a8b038dd650ea2910f797213a22ae8ff
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-04-20 10:30:06 +0200

    WEB-47799 Support new component options syntax in vue-class-component v8
    GitOrigin-RevId: 5674146b8bb31c3a198a38e0fba416c0533bff30

---
+++ vuejs/src/org/jetbrains/vuejs/libraries/componentDecorator/VueComponentDecoratorUtils.kt
@@ -0,0 +1,15 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.libraries.componentDecorator
+
+import com.intellij.lang.javascript.psi.ecma6.ES6Decorator
+import com.intellij.lang.javascript.psi.ecmal4.JSClass
+
+private val COMPONENT_DECS = setOf("Component", "Options")
+
+fun findComponentDecorator(clazz: JSClass): ES6Decorator? =
+  clazz.attributeList
+    ?.decorators
+    ?.find { COMPONENT_DECS.contains(it.decoratorName) }
+
+fun isComponentDecorator(decorator: ES6Decorator): Boolean =
+  COMPONENT_DECS.contains(decorator.decoratorName)
