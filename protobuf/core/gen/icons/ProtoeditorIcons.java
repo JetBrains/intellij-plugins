9add32f7bfac9a6618f2f2c1209e3cc7dc05b6be
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-04-26 21:13:48 +0300

    [protobuf-plugin] Generated icon accessor class
    GitOrigin-RevId: fd419d2a540342d11c70f6321907a263ad479099

---
+++ protobuf/core/gen/icons/ProtoeditorIcons.java
@@ -0,0 +1,18 @@
+package icons;
+
+import com.intellij.ui.IconManager;
+import org.jetbrains.annotations.NotNull;
+
+import javax.swing.*;
+
+/**
+ * NOTE THIS FILE IS AUTO-GENERATED
+ * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
+ */
+public final class ProtoeditorIcons {
+  private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, ProtoeditorIcons.class.getClassLoader(), cacheKey, flags);
+  }
+  /** 16x16 */ public static final @NotNull Icon ProtoFile = load("protoFile.png", 0L, 1);
+  /** 16x16 */ public static final @NotNull Icon ProtoMessage = load("protoMessage.png", 0L, 1);
+}
