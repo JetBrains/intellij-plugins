cb31873b8c7be55aabeb2fff24b97ebcc5f26b35
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-04-27 13:26:55 +0300

    [protobuf-plugin] Regenerated icons
    GitOrigin-RevId: 9692c414e14de8ffdb6e3403a481aec030430a00

--- protobuf/protoeditor-core/gen/com/intellij/protobuf/icons/ProtoeditorCoreIcons.java
+++ protobuf/protoeditor-core/gen/com/intellij/protobuf/icons/ProtoeditorCoreIcons.java
@@ -1,4 +1,4 @@
-package icons;
+package com.intellij.protobuf.icons;
 
 import com.intellij.ui.IconManager;
 import org.jetbrains.annotations.NotNull;
@@ -9,9 +9,9 @@ import javax.swing.*;
  * NOTE THIS FILE IS AUTO-GENERATED
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
-public final class ProtoeditorIcons {
+public final class ProtoeditorCoreIcons {
   private static @NotNull Icon load(@NotNull String path, long cacheKey, int flags) {
-    return IconManager.getInstance().loadRasterizedIcon(path, ProtoeditorIcons.class.getClassLoader(), cacheKey, flags);
+    return IconManager.getInstance().loadRasterizedIcon(path, ProtoeditorCoreIcons.class.getClassLoader(), cacheKey, flags);
   }
   /** 16x16 */ public static final @NotNull Icon ProtoFile = load("protoFile.png", 0L, 1);
   /** 16x16 */ public static final @NotNull Icon ProtoMessage = load("protoMessage.png", 0L, 1);
