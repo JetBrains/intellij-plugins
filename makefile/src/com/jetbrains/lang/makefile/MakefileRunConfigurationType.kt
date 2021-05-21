013e91ff8b34e2ba9ac177a1399ad8ed37cf3e59
Andrey Shcheglov <andrey.shcheglov@jetbrains.com>
2021-05-21 16:02:14 +0300

    CPP-23917 Convert `MakefileRunConfigurationType` to a class
    (cherry picked from commit a71d228793b3db7ae36b3b79d59377f3ae0db93e)
    
    GitOrigin-RevId: f1d797e10c05ef87741e8ed44770ab7797586b86

--- makefile/src/com/jetbrains/lang/makefile/MakefileRunConfigurationType.kt
+++ makefile/src/com/jetbrains/lang/makefile/MakefileRunConfigurationType.kt
@@ -1,11 +1,12 @@
 package com.jetbrains.lang.makefile
 
 import com.intellij.execution.configurations.ConfigurationType
+import com.intellij.execution.configurations.ConfigurationTypeUtil.findConfigurationType
 import icons.MakefileIcons
 import org.jetbrains.annotations.Contract
 import org.jetbrains.annotations.NonNls
 
-object MakefileRunConfigurationType : ConfigurationType {
+class MakefileRunConfigurationType : ConfigurationType {
   override fun getDisplayName() = MakefileLangBundle.message("run.configuration.name")
   override fun getIcon() = MakefileIcons.Makefile
   override fun getConfigurationTypeDescription() = MakefileLangBundle.message("run.configuration.description")
@@ -17,4 +18,11 @@ object MakefileRunConfigurationType : ConfigurationType {
   @Contract(pure = true)
   override fun getHelpTopic(): @NonNls String =
     "reference.dialogs.rundebug.MakefileTarget"
+
+  companion object {
+    @JvmStatic
+    val instance: MakefileRunConfigurationType
+      get() =
+        findConfigurationType(MakefileRunConfigurationType::class.java)
+  }
 }
