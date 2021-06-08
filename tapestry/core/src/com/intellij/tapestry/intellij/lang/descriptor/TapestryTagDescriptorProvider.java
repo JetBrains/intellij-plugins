430c45e850086a810f29977ac30518d3ba4cf70e
Roman Ivanov <roman.ivanov@jetbrains.com>
2021-06-08 15:57:26 +0200

    Tapestry: handle dumb mode: EA-261290
    GitOrigin-RevId: c44dd5ffc98166d4529150c3b38f1af29a5f9852

--- tapestry/core/src/com/intellij/tapestry/intellij/lang/descriptor/TapestryTagDescriptorProvider.java
+++ tapestry/core/src/com/intellij/tapestry/intellij/lang/descriptor/TapestryTagDescriptorProvider.java
@@ -1,17 +1,21 @@
 package com.intellij.tapestry.intellij.lang.descriptor;
 
+import com.intellij.openapi.project.DumbService;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.impl.source.xml.XmlElementDescriptorProvider;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.tapestry.psi.TmlFile;
 import com.intellij.xml.XmlElementDescriptor;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Alexey Chmutov
  */
 public class TapestryTagDescriptorProvider implements XmlElementDescriptorProvider {
   @Override
+  @Nullable
   public XmlElementDescriptor getDescriptor(XmlTag tag) {
+    if (DumbService.isDumb(tag.getProject())) return null;
     PsiFile file = tag.getContainingFile();
     return file instanceof TmlFile ? DescriptorUtil.getTmlOrHtmlTagDescriptor(tag) : null;
   }
