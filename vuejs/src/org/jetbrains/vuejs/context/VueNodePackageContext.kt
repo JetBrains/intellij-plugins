3e8b3c67c4fd91fb4065e72c619bd93736d96f1e
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-06-13 21:10:54 +0200

    Web-types: fix issue with stale context calculations and remove programmatic WebContext detection in Vue
    GitOrigin-RevId: f13cfa837a023e794f65a2a73501a7325f8df106

--- vuejs/src/org/jetbrains/vuejs/context/VueNodePackageContext.kt
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.vuejs.context
-
-import com.intellij.javascript.web.context.WebFrameworkNodePackageContext
-import org.jetbrains.vuejs.index.VUE_CLI_SERVICE_MODULE
-import org.jetbrains.vuejs.index.VUE_MODULE
-
-private class VueNodePackageContext : WebFrameworkNodePackageContext(VUE_MODULE, VUE_CLI_SERVICE_MODULE)
