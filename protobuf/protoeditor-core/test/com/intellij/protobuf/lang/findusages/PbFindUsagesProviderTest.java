9c21d2260b360021c574027165caffdfe5507180
Rohan Joyce <rfj@google.com>
2021-06-17 03:36:30 +0300

    [protobuf-plugin] Address a test failure where PbUsageTypeProvider's constructor caused an NPE when run before project was set up.
    (cherry picked from commit 9aa3db161272fa8d1fd9b60b7b5e5e6c75ce05d6)
    
    IJ-CR-12232
    
    GitOrigin-RevId: 86b433e6135d7f60869d6d6db61a8b60712b5861

--- protobuf/protoeditor-core/test/com/intellij/protobuf/lang/findusages/PbFindUsagesProviderTest.java
+++ protobuf/protoeditor-core/test/com/intellij/protobuf/lang/findusages/PbFindUsagesProviderTest.java
@@ -19,7 +19,6 @@ import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.usageView.UsageInfo;
 import com.intellij.usages.impl.rules.UsageType;
-import com.intellij.usages.impl.rules.UsageTypeProvider;
 import com.intellij.util.containers.ContainerUtil;
 import com.intellij.protobuf.TestUtils;
 import com.intellij.protobuf.fixtures.PbCodeInsightFixtureTestCase;
@@ -33,8 +32,6 @@ import static com.intellij.protobuf.TestUtils.notNull;
 
 /** Tests for {@link PbFindUsagesProvider}. */
 public class PbFindUsagesProviderTest extends PbCodeInsightFixtureTestCase {
-
-  private static final UsageTypeProvider USAGE_TYPE_PROVIDER = new PbUsageTypeProvider();
 
   @Override
   public void setUp() throws Exception {
@@ -253,7 +250,7 @@ public class PbFindUsagesProviderTest extends PbCodeInsightFixtureTestCase {
         .stream()
         .map(
             usageInfo -> {
-              UsageType usageType = USAGE_TYPE_PROVIDER.getUsageType(usageInfo.getElement());
+              UsageType usageType = new PbUsageTypeProvider().getUsageType(usageInfo.getElement());
               return usageType != null ? usageType.toString() : UsageType.UNCLASSIFIED.toString();
             })
         .collect(Collectors.toList());
