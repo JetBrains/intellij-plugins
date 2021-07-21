1e1b582f3a5b97bb41aa30161b76c116c2390a4f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-07-21 16:10:03 +0200

    WEB-51463 Vue.js: False-Positive "Unrecognized attribute or property name" on <textarea :value />
    (cherry picked from commit 93008b4343232f83355289fec3694cb077277327)
    
    IJ-CR-12141
    
    GitOrigin-RevId: 3300f6ae5c6fecebda6a71889829fff6b83ce8ff

---
+++ vuejs/vuejs-tests/testData/highlighting/textarea.vue
@@ -0,0 +1,15 @@
+<template>
+        <textarea
+          :<warning descr="Unrecognized attribute or property name">foo</warning>="12"
+          :value="value"
+          @change="e => $emit('input', e.target.value)"
+          @keyup="e => $emit('input', e.target.value)"
+        />
+</template>
+<script>
+export default {
+  props: {
+    value: String
+  }
+}
+</script>
