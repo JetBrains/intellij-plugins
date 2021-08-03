91b12fe0c92e22fa581263e83ce92571f31b7eb4
Pavel Sokolov <Pavel.Sokolov@jetbrains.com>
2021-08-03 14:20:13 +0300

    DenoSettings: restart all TypeScriptService-s, not only TypeScriptCompilerService-s
    (cherry picked from commit 6a01d8cb6ee682d4a137a99113b51ad470d1219b)
    
    IJ-MR-13343
    
    GitOrigin-RevId: 37aecff20834da7be2e0afc1404a1684bdcbf514

--- Deno/src/com/intellij/deno/DenoSettings.kt
+++ Deno/src/com/intellij/deno/DenoSettings.kt
@@ -1,7 +1,7 @@
 package com.intellij.deno
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
-import com.intellij.lang.typescript.compiler.TypeScriptCompilerService
+import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.components.PersistentStateComponent
@@ -44,14 +44,14 @@ class DenoSettings(val project: Project) : PersistentStateComponent<DenoState> {
   }
 
   fun setUseDenoAndReload(useDeno: Boolean) {
-    ApplicationManager.getApplication().isWriteAccessAllowed
+    ApplicationManager.getApplication().assertWriteAccessAllowed()
     val libraryProvider = AdditionalLibraryRootsProvider.EP_NAME.findExtensionOrFail(DenoLibraryProvider::class.java)
     val oldRoots = libraryProvider.getRootsToWatch(project)
     setUseDeno(useDeno)
 
     WriteAction.run(
       ThrowableRunnable<ConfigurationException> {
-        TypeScriptCompilerService.restartServices(project)
+        TypeScriptService.restartServices(project)
 
         val newRoots = libraryProvider.getRootsToWatch(project)
         AdditionalLibraryRootsListener.fireAdditionalLibraryChanged(project, null, oldRoots, newRoots, "Deno")
