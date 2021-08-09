1c4738fc117a276c9832f1d637330189b896e277
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-08-09 18:54:37 +0200

    WEB-52111 Warnings when using dynamic slots and scoped slots in Vue!
    (cherry picked from commit b65bb135d3ef496efaf0ac97e1b528372c639b9a)
    
    IJ-CR-12833
    
    GitOrigin-RevId: b85b9193cd05b9c94a11437e5a7a3fa134d8ea50

---
+++ vuejs/vuejs-tests/testData/highlighting/slotNameBinding.vue
@@ -0,0 +1,31 @@
+<template>
+  <template v-for="item in items">
+    <slot :name="`item.${item.prop}`" v-bind:item="item">
+      {{item.value}}
+    </slot>
+  </template>
+  <div v-bind:<warning descr="Unrecognized attribute or property name">foo</warning>="12"/>
+  <Bar>
+    <template #<warning descr="Unrecognized Slot name">errorLine</warning>></template>
+    <template #item.bar></template>
+    <template #item.foo="{item}">
+      {{ item.value }}
+    </template>
+  </Bar>
+</template>
+<script>
+import Foo from "./Foo"
+export default {
+  name:"Bar",
+  components: {
+    Foo
+  },
+  data() {
+    return {
+      items: [
+        { prop: "foo", value: 12}
+      ]
+    }
+  }
+}
+</script>
