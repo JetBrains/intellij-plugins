60cc39847f2f0550f8ec34d4c1c00aedf7ab8a20
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-08-16 16:51:15 +0200

    WEB-52140 Vue: Should not report "Unrecognized attribute or property name" for bound data attributes
    (cherry picked from commit c7a4c6b8698934650b8645a68cad33d798d6fc4b)
    
    IJ-CR-13083
    
    GitOrigin-RevId: 36a9f8fff7af280213604dc0eb3e22175d24ad27

---
+++ vuejs/vuejs-tests/testData/highlighting/bindingToDataAttributes.vue
@@ -0,0 +1,7 @@
+<template>
+  <div :data-foo="12"/>
+  <div data-foo="12"/>
+  <div v-bind:data-foo="12"/>
+  <div :<warning descr="Unrecognized attribute or property name">datas-foo</warning>="12"/>
+  <span :data-foo="12"/>
+</template>
