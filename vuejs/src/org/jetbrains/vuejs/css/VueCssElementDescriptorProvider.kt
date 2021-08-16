2ae52a06a0f2b850ce0411f66f02c2291ef47ff9
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-08-16 14:25:48 +0200

    WEB-52145 Vue.js 3 style selectors
    GitOrigin-RevId: 6881f601d7cff4f4398f873e81d5ab36f2d04861

--- vuejs/src/org/jetbrains/vuejs/css/VueCssElementDescriptorProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/css/VueCssElementDescriptorProvider.kt
@@ -9,16 +9,30 @@ import com.intellij.psi.css.descriptor.CssPseudoSelectorDescriptorStub
 import org.jetbrains.vuejs.context.isVueContext
 
 class VueCssElementDescriptorProvider : CssElementDescriptorProvider() {
-  private val V_DEEP = "v-deep"
-  private val PSEUDO_SELECTORS = listOf(CssPseudoSelectorDescriptorStub(V_DEEP, true))
+
+  private val ALL_SELECTORS = listOf(
+
+    CssPseudoSelectorDescriptorStub("v-deep", true, false), // Deprecated
+
+    CssPseudoSelectorDescriptorStub("v-deep", true, true),
+    CssPseudoSelectorDescriptorStub("deep", false, true),
+
+    CssPseudoSelectorDescriptorStub("v-slotted", true, true),
+    CssPseudoSelectorDescriptorStub("slotted", false, true),
+
+    CssPseudoSelectorDescriptorStub("v-global", true, true),
+    CssPseudoSelectorDescriptorStub("global", false, true),
+
+  )
 
   override fun isMyContext(context: PsiElement?): Boolean = context?.let { isVueContext(it) } == true
 
-  override fun findPseudoSelectorDescriptors(name: String, context: PsiElement?): Collection<CssPseudoSelectorDescriptor> {
-    return if (context != null && V_DEEP == name) PSEUDO_SELECTORS else emptyList()
-  }
+  override fun findPseudoSelectorDescriptors(name: String, context: PsiElement?): Collection<CssPseudoSelectorDescriptor> =
+    if (context != null) ALL_SELECTORS.filter { it.id == name } else emptyList()
 
-  override fun getAllPseudoSelectorDescriptors(context: PsiElement?): Collection<CssPseudoSelectorDescriptor> = PSEUDO_SELECTORS
+  override fun getAllPseudoSelectorDescriptors(context: PsiElement?): Collection<CssPseudoSelectorDescriptor> =
+    ALL_SELECTORS
 
   override fun getDeclarationsForSimpleSelector(selector: CssSimpleSelector): Array<PsiElement> = PsiElement.EMPTY_ARRAY
+
 }
