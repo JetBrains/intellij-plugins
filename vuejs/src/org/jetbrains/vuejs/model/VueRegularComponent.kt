19ab6148e8ce2646a92aa8745d999edb239620fd
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-08-16 22:29:14 +0200

    WEB-52212 Vue: Indirect component imports marked as unused in script setup
    (cherry picked from commit a8b35dad7b44207f8f4257ef3d67afb62c2d5045)
    
    IJ-CR-13082
    
    GitOrigin-RevId: 3460182832c546d2620371810cb1a26ca57f14dd

--- vuejs/src/org/jetbrains/vuejs/model/VueRegularComponent.kt
+++ vuejs/src/org/jetbrains/vuejs/model/VueRegularComponent.kt
@@ -1,4 +1,8 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.model
 
-interface VueRegularComponent : VueComponent, VueContainer
+import com.intellij.psi.PsiElement
+
+interface VueRegularComponent : VueComponent, VueContainer {
+  val nameElement: PsiElement?
+}
