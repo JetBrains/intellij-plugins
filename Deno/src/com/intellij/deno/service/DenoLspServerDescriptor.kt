3083b257f68b59e8de8d610d50ff13d6a1a3f77f
Pavel Sokolov <Pavel.Sokolov@jetbrains.com>
2021-08-17 13:41:06 +0300

    + LSP server for Deno
    (cherry picked from commit fdb52cfd087daf0f4a838b303f126ea2c1caba8a)
    
    IJ-MR-13343
    
    GitOrigin-RevId: 53b70a24dad75a8270c752654479c1bebd8d61b9

--- Deno/src/com/intellij/deno/service/DenoLspServerDescriptor.kt
+++ Deno/src/com/intellij/deno/service/DenoLspServerDescriptor.kt
@@ -1,11 +1,8 @@
 package com.intellij.deno.service
 
-import com.intellij.deno.DenoSettings
 import com.intellij.deno.DenoUtil
 import com.intellij.execution.configurations.GeneralCommandLine
-import com.intellij.lsp.LspServerDescriptor
 import com.intellij.lsp.LspServerDescriptorBase
-import com.intellij.lsp.LspServerSupportProvider
 import com.intellij.lsp.SocketModeDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -15,11 +12,13 @@ class DenoLspServerDescriptor(project: Project, root: VirtualFile) : LspServerDe
     GeneralCommandLine(it, "lsp")
   } ?: throw Error("deno is not installed")
 
+  override fun createInitializationOptions() = DenoInitializationOptions()
+
   override fun getSocketModeDescriptor(): SocketModeDescriptor? = null
+
+  override fun diagnosticReceived(file: VirtualFile?) {
+    // do nothing, highlighting will be requested separately
+  }
 }
 
-class DenoLspServerSupportProvider : LspServerSupportProvider {
-  override fun getServerDescriptor(project: Project, virtualFile: VirtualFile) =
-    if (DenoSettings.getService(project).isUseDeno()) DenoLspServerDescriptor(project, virtualFile)
-    else LspServerDescriptor.emptyDescriptor()
-}
+data class DenoInitializationOptions(val enable: Boolean = true)
--- Deno/src/com/intellij/deno/service/DenoTypeScriptService.kt
+++ Deno/src/com/intellij/deno/service/DenoTypeScriptService.kt
@@ -4,108 +4,107 @@ import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.lang.javascript.DialectDetector
 import com.intellij.lang.javascript.dialects.TypeScriptLanguageDialect
 import com.intellij.lang.javascript.integration.JSAnnotationError
+import com.intellij.lang.javascript.integration.JSAnnotationError.*
+import com.intellij.lang.javascript.psi.JSFunctionType
+import com.intellij.lang.parameterInfo.CreateParameterInfoContext
 import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionEntry
-import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy.*
+import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy.MERGE
+import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy.NON
+import com.intellij.lsp.LspServer
+import com.intellij.lsp.data.LspCompletionItem
+import com.intellij.lsp.data.LspDiagnostic
+import com.intellij.lsp.data.LspSeverity.*
+import com.intellij.lsp.methods.*
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.event.DocumentEvent
+import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.util.PathUtil
 import com.intellij.util.containers.toArray
-import kotlinx.coroutines.*
-import kotlinx.coroutines.future.future
 import java.util.concurrent.CompletableFuture
-import kotlin.streams.asStream
+import java.util.concurrent.CompletableFuture.completedFuture
+import java.util.concurrent.Future
+import java.util.stream.Stream
 
-class DenoTypeScriptService(private val project: Project) : TypeScriptService, DenoLspServer.Client, Disposable {
-  private var connection: DenoLspServer? = null
-  private var coroutineScope: CoroutineScope? = null
+class DenoTypeScriptService(private val project: Project) : TypeScriptService, DocumentListener, Disposable {
+  private var connection: LspServer? = null
   private val openedFiles = mutableSetOf<VirtualFile>()
-  private val highlights = mutableMapOf<String, List<JSAnnotationError>>()
 
   companion object {
     fun getInstance(project: Project): DenoTypeScriptService = project.getService(DenoTypeScriptService::class.java)
   }
 
-  // TODO: call it somewhere
-  fun start(scope: CoroutineScope = MainScope()) {
-    coroutineScope = scope
-    scope.launch {
-      connection = DenoLspServer(this@DenoTypeScriptService).also { it.connect() }
+  private fun getConnection(file: VirtualFile) =
+    connection ?: LspServer(DenoLspServerDescriptor(project, file)).also {
+      connection = it
+      it.start()
     }
-  }
 
   override fun isDisabledByContext(context: VirtualFile) = false
 
-  override fun getStatusText() = connection?.let { "Deno ${it.version}" } ?: "Deno"
+  override fun isServiceCreated() = connection != null
+
+  override fun getStatusText() = when {
+    connection!!.isRunning -> "Deno LSP: running"
+    connection!!.isMalfunctioned -> "Deno LSP: error"
+    else -> "Deno LSP"
+  }
 
   override fun getCompletionMergeStrategy(parameters: CompletionParameters, file: PsiFile, context: PsiElement) =
     if (parameters.isExtendedCompletion) NON else MERGE
 
-  override fun updateAndGetCompletionItems(virtualFile: VirtualFile, document: Document, positionInFileOffset: Int) =
-    connection?.let {
-      coroutineScope?.future {
-        it.updateAndGetCompletionItems(virtualFile, document, positionInFileOffset)?.toList()
-      }
-    }
+  override fun updateAndGetCompletionItems(virtualFile: VirtualFile, parameters: CompletionParameters): Future<List<CompletionEntry>?>? =
+    completedFuture(getConnection(virtualFile).invokeSynchronously(CompletionMethod(parameters))?.map(::DenoCompletionEntry))
 
   override fun getDetailedCompletionItems(virtualFile: VirtualFile,
                                           items: List<CompletionEntry>,
                                           document: Document,
-                                          positionInFileOffset: Int) =
-    connection?.let {
-      coroutineScope?.future {
-        it.getDetailedCompletionItems(virtualFile, items.asSequence(), document, positionInFileOffset)?.toList()
-      }
-    }
+                                          positionInFileOffset: Int): Future<List<CompletionEntry>?>? {
+    val connection = getConnection(virtualFile)
+    return completedFuture(items.map {
+      connection
+        .invokeSynchronously(ResolveCompletionItemMethod((it as DenoCompletionEntry).item))
+        ?.let(::DenoCompletionEntry) ?: it
+    })
+  }
 
-  override fun getNavigationFor(document: Document, sourceElement: PsiElement) =
-    connection?.getNavigationFor(document, sourceElement)?.toList()?.toArray(emptyArray())
+  override fun getNavigationFor(document: Document, sourceElement: PsiElement) = getFile(document)?.let {
+    getConnection(it).invokeSynchronously(ElementDefinitionMethod.create(sourceElement))?.toArray(emptyArray())
+  }
 
-  override fun getSignatureHelp(file: PsiFile, document: Document, offset: Int) =
-    connection?.let {
-      coroutineScope?.future {
-        it.getSignatureHelp(file, document, offset)?.asStream()
-      }
-    }
+  override fun getSignatureHelp(file: PsiFile, context: CreateParameterInfoContext): Future<Stream<JSFunctionType>?>? = null
 
-  override fun getQuickInfoAt(element: PsiElement, originalElement: PsiElement, originalFile: VirtualFile) =
-    connection?.let {
-      coroutineScope?.future {
-        it.getQuickInfoAt(element, originalElement, originalFile)
-      }
-    }
+  override fun getQuickInfoAt(element: PsiElement, originalElement: PsiElement, originalFile: VirtualFile): CompletableFuture<String?> =
+    completedFuture(getConnection(element.containingFile.virtualFile).invokeSynchronously(HoverMethod.create(element)))
 
   override fun terminateStartedProcess() {
     val server = connection ?: return
     connection = null
-    Disposer.dispose(server)
-
-    val scope = coroutineScope ?: return
-    coroutineScope = null
-    scope.cancel("Server process was terminated")
-
+    server.stop(true, false)
     openedFiles.clear()
-    highlights.clear()
   }
 
-  override fun highlight(file: PsiFile) = highlights[file.virtualFile.path]?.let { CompletableFuture.completedFuture(it) }
+  override fun highlight(file: PsiFile): CompletableFuture<List<JSAnnotationError>>? {
+    val virtualFile = file.virtualFile
+    return completedFuture(getConnection(virtualFile).getDiagnostics(virtualFile)?.map {
+      DenoAnnotationError(it, virtualFile.canonicalPath)
+    })
+  }
 
   override fun canHighlight(file: PsiFile) = DialectDetector.isTypeScript(file)
 
   override fun isAcceptable(file: VirtualFile) = DialectDetector.getLanguageDialect(file, project) is TypeScriptLanguageDialect
 
   override fun openEditor(file: VirtualFile) {
-    val connection = connection ?: return
     val document = getDocument(file) ?: return
     if (openedFiles.add(file)) {
-      document.addDocumentListener(connection, connection)
-      connection.openFile(file)
+      document.addDocumentListener(this, this)
+      getConnection(file).fileOpened(file)
     }
   }
 
@@ -113,18 +112,40 @@ class DenoTypeScriptService(private val project: Project) : TypeScriptService, D
     val connection = connection ?: return
     val document = getDocument(file) ?: return
     if (openedFiles.remove(file)) {
-      connection.closeFile(file)
-      document.removeDocumentListener(connection)
+      connection.fileClosed(file)
+      document.removeDocumentListener(this)
     }
   }
 
-  override fun dispose() = terminateStartedProcess()
-
-  override fun onPublishHighlights(uri: String, diagnostics: Sequence<JSAnnotationError>) {
-    highlights[canonicalize(uri)] = diagnostics.toList()
+  override fun beforeDocumentChange(event: DocumentEvent) {
+    connection?.invoke(DidChangeMethod.create(event))
   }
+
+  override fun dispose() = terminateStartedProcess()
+}
+
+class DenoCompletionEntry(val item: LspCompletionItem) : CompletionEntry {
+  override val name: String get() = item.label
+
+  override fun intoLookupElement() = item.intoLookupElement()
 }
 
 fun getDocument(file: VirtualFile) = FileDocumentManager.getInstance().getDocument(file)
 
-fun canonicalize(uri: String) = PathUtil.driveLetterToLowerCase(uri.replace('\\', '/'))
+fun getFile(document: Document) = FileDocumentManager.getInstance().getFile(document)
+
+class DenoAnnotationError(private val diagnostic: LspDiagnostic, private val path: String?) : JSAnnotationError {
+  override fun getLine() = diagnostic.range.start.line
+
+  override fun getColumn() = diagnostic.range.start.character
+
+  override fun getAbsoluteFilePath(): String? = path
+
+  override fun getDescription(): String = diagnostic.message
+
+  override fun getCategory() = when (diagnostic.severity) {
+    Error -> ERROR_CATEGORY
+    Warning -> WARNING_CATEGORY
+    Hint, Information -> INFO_CATEGORY
+  }
+}
