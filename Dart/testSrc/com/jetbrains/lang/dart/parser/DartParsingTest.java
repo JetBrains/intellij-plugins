7d6f440e780ca8b639e260ddd543a28a915d0c83
Jaime Wren <jwren@google.com>
2021-08-19 12:01:55 +0300

    WEB-52293 (wip) Support new Dart syntax: Constructor tearoffs
    close #819
    
    (cherry picked from commit be32521dcccff3790fdc665e229931d998a9bae9)
    
    IJ-CR-14281
    
    (cherry picked from commit 4525761d9f441d1a786b669a99061fb77e4e59d4)
    
    GitOrigin-RevId: c592eda34d32f0cfd9e89fa1da7069c7715cbf7d

--- Dart/testSrc/com/jetbrains/lang/dart/parser/DartParsingTest.java
+++ Dart/testSrc/com/jetbrains/lang/dart/parser/DartParsingTest.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.parser;
 
 import com.intellij.testFramework.ParsingTestCase;
@@ -38,6 +38,10 @@ public class DartParsingTest extends ParsingTestCase {
   }
 
   public void testConstructors() {
+    doTest();
+  }
+
+  public void testConstructorTearoffs() {
     doTest();
   }
 
