cdaefd7eebfa31df924f06eb8354c45ca995297b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-08-20 14:33:39 +0200

    WEB-52111 Vue: update v-slot definitions across Vue versions
    GitOrigin-RevId: 3f808f7d9b8012bbcb2f6102c10143b3eaa40ec8

--- vuejs/vuejs-tests/testData/highlighting/slotNameBinding.vue
+++ vuejs/vuejs-tests/testData/highlighting/slotNameBinding.vue
@@ -3,11 +3,16 @@
     <slot :name="`item.${item.prop}`" v-bind:item="item">
       {{item.value}}
     </slot>
+    <slot name="okSlot"/>
   </template>
   <div v-bind:<warning descr="Unrecognized attribute or property name">foo</warning>="12"/>
   <Bar>
-    <template #<warning descr="Unrecognized Slot name">errorLine</warning>></template>
+    <template #<warning descr="Unrecognized slot name">errorLine</warning>></template>
+    <template v-slot:<warning descr="Unrecognized slot name">errorLine</warning>></template>
     <template #item.bar></template>
+    <template v-slot:item.bar></template>
+    <template #okSlot></template>
+    <template v-slot:okSlot></template>
     <template #item.foo="{item}">
       {{ item.value }}
     </template>
