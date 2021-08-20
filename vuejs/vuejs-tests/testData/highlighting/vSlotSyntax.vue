dda6c5c244b13a4fd413be2bc41fd38ea5c4958d
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-08-20 14:33:39 +0200

    WEB-52111 Vue: update v-slot definitions across Vue versions
    (cherry picked from commit 3f808f7d9b8012bbcb2f6102c10143b3eaa40ec8)
    
    IJ-CR-13270
    
    GitOrigin-RevId: 18bc785f57e0077e3f6f9a2c19877ec3576fc498

--- vuejs/vuejs-tests/testData/highlighting/vSlotSyntax.vue
+++ vuejs/vuejs-tests/testData/highlighting/vSlotSyntax.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <div v-slot:<warning descr="Unrecognized Slot name">name</warning>="propName">
+    <div v-slot:<warning descr="Unrecognized slot name">name</warning>="propName">
       {{ propName + <weak_warning descr="Unresolved variable or type wrongName">wrongName</weak_warning> }}
     </div>
-    <div v-slot:<warning descr="Unrecognized Slot name">name</warning>="{prop1, prop2}">
+    <div v-slot:<warning descr="Unrecognized slot name">name</warning>="{prop1, prop2}">
       {{ prop1 + <weak_warning descr="Unresolved variable or type wrongName">wrongName</weak_warning>}}
     </div>
-    <div v-slot:<warning descr="Unrecognized Slot name">name</warning>></div>
+    <div v-slot:<warning descr="Unrecognized slot name">name</warning>></div>
     <div v-slot="propName"></div>
     <div v-slot></div>
 
-    <div #<warning descr="Unrecognized Slot name">name</warning>="propName"></div>
-    <div #<warning descr="Unrecognized Slot name">name</warning>></div>
+    <div #<warning descr="Unrecognized slot name">name</warning>="propName"></div>
+    <div #<warning descr="Unrecognized slot name">name</warning>></div>
 
     <div v-<warning descr="Unrecognized Vue directive">slots</warning>:<warning descr="Unrecognized argument">name</warning>="<weak_warning descr="Unresolved variable or type propName">propName</weak_warning>"></div>
     <div v-<warning descr="Unrecognized Vue directive">slots</warning>:<warning descr="Unrecognized argument">name</warning>></div>
