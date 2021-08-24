1b0f6949736ad3b036b14ebafaf38d88689d8017
Pavel Sokolov <Pavel.Sokolov@jetbrains.com>
2021-08-24 13:46:48 +0300

    rely more on com.intellij.lsp
    (cherry picked from commit 4ae6058dda41aee1ab5d10dab3fba8bae0ea65c9)
    
    IJ-MR-13343
    
    GitOrigin-RevId: 01b6a051d232882e85cb3313a09caa78edab5327

--- Deno/src/com/intellij/deno/service/DenoLspServerDescriptor.kt
+++ Deno/src/com/intellij/deno/service/DenoLspServerDescriptor.kt
@@ -1,11 +1,23 @@
 package com.intellij.deno.service
 
+import com.intellij.deno.DenoSettings
 import com.intellij.deno.DenoUtil
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.lsp.LspServerDescriptor
 import com.intellij.lsp.LspServerDescriptorBase
+import com.intellij.lsp.LspServerSupportProvider
 import com.intellij.lsp.SocketModeDescriptor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+
+class DenoLspSupportProvider : LspServerSupportProvider {
+  override fun getServerDescriptor(project: Project, virtualFile: VirtualFile) =
+    if (DenoSettings.getService(project).isUseDeno()) {
+      DenoLspServerDescriptor(project, virtualFile)
+    } else {
+      LspServerDescriptor.emptyDescriptor()
+    }
+}
 
 class DenoLspServerDescriptor(project: Project, root: VirtualFile) : LspServerDescriptorBase(project, root) {
   override fun createStdioServerStartingCommandLine() = DenoUtil.getDenoExecutablePath()?.let {
@@ -16,9 +28,9 @@ class DenoLspServerDescriptor(project: Project, root: VirtualFile) : LspServerDe
 
   override fun getSocketModeDescriptor(): SocketModeDescriptor? = null
 
-  override fun diagnosticReceived(file: VirtualFile?) {
-    // do nothing, highlighting will be requested separately
-  }
+  override fun useGenericCompletion() = false
+
+  override fun useGenericHighlighting() = false
 }
 
 data class DenoInitializationOptions(val enable: Boolean = true)
--- Deno/src/com/intellij/deno/service/DenoTypeScriptService.kt
+++ Deno/src/com/intellij/deno/service/DenoTypeScriptService.kt
@@ -1,97 +1,97 @@
 package com.intellij.deno.service
 
 import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.deno.DenoSettings
 import com.intellij.lang.javascript.DialectDetector
 import com.intellij.lang.javascript.dialects.TypeScriptLanguageDialect
 import com.intellij.lang.javascript.integration.JSAnnotationError
 import com.intellij.lang.javascript.integration.JSAnnotationError.*
 import com.intellij.lang.javascript.psi.JSFunctionType
+import com.intellij.lang.javascript.service.JSLanguageServiceProvider
 import com.intellij.lang.parameterInfo.CreateParameterInfoContext
+import com.intellij.lang.typescript.compiler.TypeScriptCompilerSettings
 import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionEntry
 import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy.MERGE
 import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy.NON
-import com.intellij.lsp.LspServer
-import com.intellij.lsp.data.LspCompletionItem
+import com.intellij.lsp.LspServerDescriptor
+import com.intellij.lsp.LspServerManager
 import com.intellij.lsp.data.LspDiagnostic
 import com.intellij.lsp.data.LspSeverity.*
-import com.intellij.lsp.methods.*
-import com.intellij.openapi.Disposable
+import com.intellij.lsp.methods.ForceDidChangeMethod
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.editor.event.DocumentEvent
-import com.intellij.openapi.editor.event.DocumentListener
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import com.intellij.util.containers.toArray
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletableFuture.completedFuture
 import java.util.concurrent.Future
 import java.util.stream.Stream
 
-class DenoTypeScriptService(private val project: Project) : TypeScriptService, DocumentListener, Disposable {
-  private var connection: LspServer? = null
-  private val openedFiles = mutableSetOf<VirtualFile>()
+class DenoTypeScriptServiceProvider(val project: Project) : JSLanguageServiceProvider {
 
+  override fun isHighlightingCandidate(file: VirtualFile) = TypeScriptCompilerSettings.acceptFileType(file.fileType)
+
+  override fun getService(file: VirtualFile) = allServices.firstOrNull()
+
+  override fun getAllServices() =
+    if (DenoSettings.getService(project).isUseDeno()) listOf(DenoTypeScriptService.getInstance(project)) else emptyList()
+}
+
+class DenoTypeScriptService(private val project: Project) : TypeScriptService {
   companion object {
+    private val LOG = Logger.getInstance(DenoTypeScriptService::class.java)
     fun getInstance(project: Project): DenoTypeScriptService = project.getService(DenoTypeScriptService::class.java)
   }
 
-  private fun getConnection(file: VirtualFile) =
-    connection ?: LspServer(DenoLspServerDescriptor(project, file)).also {
-      connection = it
-      it.start()
-    }
+  private var descriptor: LspServerDescriptor? = null
+
+  private fun createDescriptor(element: PsiElement) =
+    LspServerManager.getServerDescriptors(element).find { it is DenoLspServerDescriptor }
+
+  private fun getDescriptor(element: PsiElement) = descriptor ?: createDescriptor(element)
 
   override fun isDisabledByContext(context: VirtualFile) = false
-
-  override fun isServiceCreated() = connection != null
-
-  override fun getStatusText() = when {
-    connection!!.isRunning -> "Deno LSP: running"
-    connection!!.isMalfunctioned -> "Deno LSP: error"
-    else -> "Deno LSP"
-  }
 
   override fun getCompletionMergeStrategy(parameters: CompletionParameters, file: PsiFile, context: PsiElement) =
     if (parameters.isExtendedCompletion) NON else MERGE
 
-  override fun updateAndGetCompletionItems(virtualFile: VirtualFile, parameters: CompletionParameters): Future<List<CompletionEntry>?>? =
-    completedFuture(getConnection(virtualFile).invokeSynchronously(CompletionMethod(parameters))?.map(::DenoCompletionEntry))
+  // TODO
+  override fun updateAndGetCompletionItems(virtualFile: VirtualFile, parameters: CompletionParameters): Future<List<CompletionEntry>?>? {
+    return null
+  }
 
+  // TODO
   override fun getDetailedCompletionItems(virtualFile: VirtualFile,
                                           items: List<CompletionEntry>,
                                           document: Document,
-                                          positionInFileOffset: Int): Future<List<CompletionEntry>?>? {
-    val connection = getConnection(virtualFile)
-    return completedFuture(items.map {
-      connection
-        .invokeSynchronously(ResolveCompletionItemMethod((it as DenoCompletionEntry).item))
-        ?.let(::DenoCompletionEntry) ?: it
-    })
-  }
+                                          positionInFileOffset: Int): Future<List<CompletionEntry>?>? = null
 
-  override fun getNavigationFor(document: Document, sourceElement: PsiElement) = getFile(document)?.let {
-    getConnection(it).invokeSynchronously(ElementDefinitionMethod.create(sourceElement))?.toArray(emptyArray())
-  }
+  // TODO
+  override fun getNavigationFor(document: Document, sourceElement: PsiElement): Array<PsiElement>? = null
 
+  // TODO
   override fun getSignatureHelp(file: PsiFile, context: CreateParameterInfoContext): Future<Stream<JSFunctionType>?>? = null
 
+  // TODO
   override fun getQuickInfoAt(element: PsiElement, originalElement: PsiElement, originalFile: VirtualFile): CompletableFuture<String?> =
-    completedFuture(getConnection(element.containingFile.virtualFile).invokeSynchronously(HoverMethod.create(element)))
+    completedFuture(null)
 
   override fun terminateStartedProcess() {
-    val server = connection ?: return
-    connection = null
-    server.stop(true, false)
-    openedFiles.clear()
+    descriptor?.stopServer()
   }
 
   override fun highlight(file: PsiFile): CompletableFuture<List<JSAnnotationError>>? {
+    LOG.info("highlight")
+    val server = getDescriptor(file)?.server
     val virtualFile = file.virtualFile
-    return completedFuture(getConnection(virtualFile).getDiagnostics(virtualFile)?.map {
+    FileDocumentManager.getInstance().getDocument(virtualFile)?.let {
+      server?.invoke(ForceDidChangeMethod(virtualFile, it))
+    }
+    return completedFuture(server?.getDiagnostics(virtualFile)?.map {
       DenoAnnotationError(it, virtualFile.canonicalPath)
     })
   }
@@ -99,40 +99,7 @@ class DenoTypeScriptService(private val project: Project) : TypeScriptService, D
   override fun canHighlight(file: PsiFile) = DialectDetector.isTypeScript(file)
 
   override fun isAcceptable(file: VirtualFile) = DialectDetector.getLanguageDialect(file, project) is TypeScriptLanguageDialect
-
-  override fun openEditor(file: VirtualFile) {
-    val document = getDocument(file) ?: return
-    if (openedFiles.add(file)) {
-      document.addDocumentListener(this, this)
-      getConnection(file).fileOpened(file)
-    }
-  }
-
-  override fun closeLastEditor(file: VirtualFile) {
-    val connection = connection ?: return
-    val document = getDocument(file) ?: return
-    if (openedFiles.remove(file)) {
-      connection.fileClosed(file)
-      document.removeDocumentListener(this)
-    }
-  }
-
-  override fun beforeDocumentChange(event: DocumentEvent) {
-    connection?.invoke(DidChangeMethod.create(event))
-  }
-
-  override fun dispose() = terminateStartedProcess()
 }
-
-class DenoCompletionEntry(val item: LspCompletionItem) : CompletionEntry {
-  override val name: String get() = item.label
-
-  override fun intoLookupElement() = item.intoLookupElement()
-}
-
-fun getDocument(file: VirtualFile) = FileDocumentManager.getInstance().getDocument(file)
-
-fun getFile(document: Document) = FileDocumentManager.getInstance().getFile(document)
 
 class DenoAnnotationError(private val diagnostic: LspDiagnostic, private val path: String?) : JSAnnotationError {
   override fun getLine() = diagnostic.range.start.line
