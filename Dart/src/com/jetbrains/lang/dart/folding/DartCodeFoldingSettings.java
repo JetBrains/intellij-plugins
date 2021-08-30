f210acf1e4844ebc4d6760dd2ee644f7a8193ee1
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2021-08-30 21:57:09 +0300

    Categorise plugins' components for Settings Sync
    GitOrigin-RevId: e4d0846c23fd83597e134fe70689cfa022a4afdf

--- Dart/src/com/jetbrains/lang/dart/folding/DartCodeFoldingSettings.java
+++ Dart/src/com/jetbrains/lang/dart/folding/DartCodeFoldingSettings.java
@@ -3,12 +3,13 @@ package com.jetbrains.lang.dart.folding;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.PersistentStateComponent;
+import com.intellij.openapi.components.SettingsCategory;
 import com.intellij.openapi.components.State;
 import com.intellij.openapi.components.Storage;
 import com.intellij.util.xmlb.XmlSerializerUtil;
 import org.jetbrains.annotations.NotNull;
 
-@State(name = "DartCodeFoldingSettings", storages = @Storage("editor.xml"))
+@State(name = "DartCodeFoldingSettings", storages = @Storage("editor.xml"), category = SettingsCategory.CODE)
 public class DartCodeFoldingSettings implements PersistentStateComponent<DartCodeFoldingSettings> {
   private boolean myCollapseParts = true;
   private boolean myCollapseGenericParams;
