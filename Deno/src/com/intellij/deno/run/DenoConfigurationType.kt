5e5073703653593d708b760c20c53c6ca4397759
Andrew Kozlov <andrew.kozlov@jetbrains.com>
2021-09-15 14:26:08 +0200

    [js] migrated to the new plugin model #IDEA-278108
    GitOrigin-RevId: 1c973d78e2021248b426efaff494ba28745ceac9

--- Deno/src/com/intellij/deno/run/DenoConfigurationType.kt
+++ Deno/src/com/intellij/deno/run/DenoConfigurationType.kt
@@ -3,10 +3,10 @@ package com.intellij.deno.run
 import com.intellij.deno.DenoBundle
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.execution.configurations.SimpleConfigurationType
+import com.intellij.javascript.debugger.JavaScriptDebuggerIcons
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NotNullLazyValue
-import icons.JavaScriptDebuggerIcons
 
 class DenoConfigurationType :
   SimpleConfigurationType("DenoConfigurationType", DenoBundle.message("deno.name"), DenoBundle.message("deno.run.configuration.description"), NotNullLazyValue.createConstantValue(
