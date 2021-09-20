3f5e9fdf09e33a3666f3f0f784b2b06f5e2f9210
Mikhail Zarechenskiy <mikhail.zarechenskiy@jetbrains.com>
2021-09-20 11:49:12 +0300

    Merge 212.5080.55 platform into kt-212-master
    Commit is made by ./plugins/kotlin/update-platform-in-kt-branch.sh
    
    GitOrigin-RevId: 80e1d4ea7a1537bddb2b90577ca691ae269dff86

--- vuejs/vuejs-tests/testData/highlighting/scriptSetupComplexImports/hooks/Modal/index.ts
+++
@@ -1,8 +0,0 @@
-import TreeModal from './components/Tree2Modal.vue'
-import BasicModal from './components/BasicModal.vue'
-import ComplexModal from './components/ComplexModal.vue'
-import DefaultModal from './components/DefaultModal.vue'
-
-export {BasicModal,TreeModal,ComplexModal,DefaultModal}
-
-export const Bar = 12
