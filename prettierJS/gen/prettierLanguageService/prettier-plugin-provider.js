9679ea3423d5a40a145723f7d482037337900e06
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2021-10-06 16:27:23 +0300

    WEB-52638 Prettier plugin doesn't take into account prettierrc when detecting if a file can be formatted
    close #823
    
    (cherry picked from commit 54f952bf4722a71ff7bd248c130d9939e22c7b35)
    
    GitOrigin-RevId: b867bc19796bd93d9b0631e19ee7603c3886a015

--- prettierJS/gen/prettierLanguageService/prettier-plugin-provider.js
+++ prettierJS/gen/prettierLanguageService/prettier-plugin-provider.js
@@ -1,5 +1,7 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 "use strict";
 exports.__esModule = true;
+exports.factory = void 0;
 var prettier_plugin_1 = require("./prettier-plugin");
 var PluginFactory = /** @class */ (function () {
     function PluginFactory() {
