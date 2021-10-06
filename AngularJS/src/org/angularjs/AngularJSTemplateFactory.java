13a546203ef02dfd2f4fb9c6d5a1fabf5d4fd188
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-10-06 12:38:23 +0300

    platform: re-write project generators — Angular CLI (and hide Angular.JS one) IDEA-270273
    GitOrigin-RevId: e12ba41c8a222d1d6f83dc3645d7c66fd1c5d953

--- AngularJS/src/org/angularjs/AngularJSTemplateFactory.java
+++ AngularJS/src/org/angularjs/AngularJSTemplateFactory.java
@@ -1,25 +1,18 @@
 package org.angularjs;
 
 import com.intellij.ide.util.projectWizard.WizardContext;
-import com.intellij.openapi.module.WebModuleBuilder;
+import com.intellij.lang.javascript.boilerplate.JavaScriptNewTemplatesFactoryBase;
 import com.intellij.platform.ProjectTemplate;
-import com.intellij.platform.ProjectTemplatesFactory;
 import org.angular2.cli.AngularCliProjectGenerator;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Dennis.Ushakov
  */
-public class AngularJSTemplateFactory extends ProjectTemplatesFactory {
-  @Override
-  public String @NotNull [] getGroups() {
-    return new String[]{WebModuleBuilder.GROUP_NAME};
-  }
-
+public class AngularJSTemplateFactory extends JavaScriptNewTemplatesFactoryBase {
 
   @Override
-  public ProjectTemplate @NotNull [] createTemplates(@Nullable String group, WizardContext context) {
+  public ProjectTemplate @NotNull [] createTemplates(WizardContext context) {
     return new ProjectTemplate[]{
       new AngularJSProjectGenerator(),
       new AngularCliProjectGenerator()
