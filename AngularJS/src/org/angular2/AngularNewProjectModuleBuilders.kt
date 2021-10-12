ad9995d969fec662fe87c2891359b77bd9f0ae42
Andrey Vlasovskikh <andrey.vlasovskikh@jetbrains.com>
2021-10-12 16:55:15 +0300

    [NPW] Converted WebTemplateNewProjectWizardBuilder to GeneratorNewProjectWizard to share the same wizard API for all project generators
    GitOrigin-RevId: f085e4dc413d90dfd5d2e6a04e6980a7421555a3

--- AngularJS/src/org/angular2/AngularNewProjectModuleBuilders.kt
+++ AngularJS/src/org/angular2/AngularNewProjectModuleBuilders.kt
@@ -1,15 +1,16 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2
 
-import com.intellij.ide.util.projectWizard.WebTemplateNewProjectWizardBuilder
+import com.intellij.ide.util.projectWizard.WebTemplateNewProjectWizard
 import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.ide.wizard.GeneratorNewProjectWizardBuilderAdapter
 import com.intellij.lang.javascript.boilerplate.JavaScriptNewTemplatesFactoryBase
 import com.intellij.platform.ProjectTemplate
 import org.angular2.cli.AngularCliProjectGenerator
 import org.angularjs.AngularJSProjectGenerator
 
 //no angular.js for new API
-class AngularCLIModuleBuilder : WebTemplateNewProjectWizardBuilder(AngularCliProjectGenerator())
+class AngularCLIModuleBuilder : GeneratorNewProjectWizardBuilderAdapter(WebTemplateNewProjectWizard(AngularCliProjectGenerator()))
 
 class AngularAndAngularJSTemplateFactory : JavaScriptNewTemplatesFactoryBase() {
   override fun createTemplates(context: WizardContext?): Array<ProjectTemplate> =
--- vuejs/src/org/jetbrains/vuejs/cli/VueNewProjectModuleBuilder.kt
+++ vuejs/src/org/jetbrains/vuejs/cli/VueNewProjectModuleBuilder.kt
@@ -1,14 +1,13 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.cli
 
-import com.intellij.ide.util.projectWizard.WebTemplateNewProjectWizardBuilder
+import com.intellij.ide.util.projectWizard.WebTemplateNewProjectWizard
 import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.ide.wizard.GeneratorNewProjectWizardBuilderAdapter
 import com.intellij.lang.javascript.boilerplate.JavaScriptNewTemplatesFactoryBase
-import com.intellij.openapi.module.WebModuleBuilder
 import com.intellij.platform.ProjectTemplate
-import com.intellij.platform.ProjectTemplatesFactory
 
-class VueCLIProjectModuleBuilder : WebTemplateNewProjectWizardBuilder(VueCliProjectGenerator())
+class VueCLIProjectModuleBuilder : GeneratorNewProjectWizardBuilderAdapter(WebTemplateNewProjectWizard(VueCliProjectGenerator()))
 
 class VueProjectTemplateFactory : JavaScriptNewTemplatesFactoryBase() {
   override fun createTemplates(context: WizardContext?): Array<ProjectTemplate> = arrayOf(VueCliProjectGenerator())
