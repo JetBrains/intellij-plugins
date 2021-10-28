e4b9946fd089247d846331345b0b0212ec278054
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-10-28 13:46:55 +0300

    deno: fix EA-397842 - NPE: DenoModuleReferenceContributorKt.extractImportMapPath
    (cherry picked from commit 650edfed8aa8c04018d08e2e930b898bdc63e60f)
    
    IJ-CR-16125
    
    GitOrigin-RevId: fa6b1d7ffc46f9173383344df21d28f2020812b8

--- Deno/test/src/com/intellij/deno/codeInsight/DenoModulesTest.kt
+++ Deno/test/src/com/intellij/deno/codeInsight/DenoModulesTest.kt
@@ -1,7 +1,9 @@
 package com.intellij.deno.codeInsight
 
 import com.intellij.codeInsight.completion.CompletionType
+import com.intellij.deno.DenoSettings
 import com.intellij.deno.DenoTestBase
+import com.intellij.lang.javascript.JSDaemonAnalyzerLightTestCase
 
 class DenoModulesTest : DenoTestBase() {
 
@@ -19,5 +21,41 @@ class DenoModulesTest : DenoTestBase() {
     myFixture.configureByText("usage.ts", "import {} from './hel<caret>'")
     myFixture.complete(CompletionType.BASIC)
     myFixture.checkResult("import {} from './hello.ts'")
+  }
+
+  fun testImportMap() {
+    myFixture.enableInspections(*JSDaemonAnalyzerLightTestCase.defaultInspections())
+    myFixture.configureByText("import_map.json", """
+      {
+        "imports": {
+            "#hello": "./hello.ts"
+         }
+      }
+    """.trimIndent())
+    myFixture.configureByText("hello.ts", "export class Hello")
+    myFixture.configureByText("usage.ts", "import {Hello} from '#hello'")
+    myFixture.testHighlighting()
+  }
+
+  fun testImportMapNoResolve() {
+    myFixture.enableInspections(*JSDaemonAnalyzerLightTestCase.defaultInspections())
+    val service = DenoSettings.getService(project)
+    val oldInit = service.getDenoInit()
+    try {
+      service.setDenoInit("{}")
+      myFixture.configureByText("import_map.json", """
+      {
+        "imports": {
+            "#hello": "./hello.ts"
+         }
+      }
+    """.trimIndent())
+      myFixture.configureByText("hello.ts", "export class Hello")
+      myFixture.configureByText("usage.ts", "import {Hello} from '<error>#hello</error>'")
+      myFixture.testHighlighting()
+    }
+    finally {
+      service.setDenoInit(oldInit)
+    }
   }
 }
