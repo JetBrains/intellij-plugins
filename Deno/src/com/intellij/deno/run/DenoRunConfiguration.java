0e242d6144582a81dd1705a70200b84f5046fa13
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-11-01 11:46:33 +0300

    deno: fix NPE WEB-53336
    GitOrigin-RevId: a1400f893ad7c88f38701ea81f950a2c7446fd21

--- Deno/src/com/intellij/deno/run/DenoRunConfiguration.java
+++ Deno/src/com/intellij/deno/run/DenoRunConfiguration.java
@@ -67,6 +67,7 @@ public class DenoRunConfiguration extends DebuggableProcessRunConfigurationBase 
     applicationArguments = newArguments;
   }
   
+  @Nullable
   public String getApplicationArguments() {
     return applicationArguments;
   }
