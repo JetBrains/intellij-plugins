89bec9538cd01c6effd87bcb85c39916bd9daa3f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2021-11-10 07:50:23 +0100

    docs, cleanup
    GitOrigin-RevId: 0cc75624fdf51450bd829d2ee0584e420fe6b96b

--- Dart/src/com/jetbrains/lang/dart/pubServer/PubServerPathHandler.kt
+++ Dart/src/com/jetbrains/lang/dart/pubServer/PubServerPathHandler.kt
@@ -1,16 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.pubServer
 
 import com.google.common.net.UrlEscapers
@@ -36,7 +24,7 @@ import org.jetbrains.builtInWebServer.*
 
 private val LOG = logger<PubServerPathHandler>()
 
-class PubServerPathHandler : WebServerPathHandlerAdapter() {
+private class PubServerPathHandler : WebServerPathHandlerAdapter() {
   override fun process(path: String, project: Project, request: FullHttpRequest, context: ChannelHandlerContext): Boolean {
     val sdk = DartSdk.getDartSdk(project)
     if (sdk == null || StringUtil.compareVersionNumbers(sdk.version, "1.6") < 0) {
@@ -116,7 +104,7 @@ private fun getServedDirAndPathForPubServer(project: Project, path: String): Pai
     }
   }
 
-  return servedDir?.let { Pair.create(it, escapeUrl(pubServePath!!)) }
+  return servedDir?.let { Pair(it, escapeUrl(pubServePath!!)) }
 }
 
 fun getServedDirAndPathForPubServer(project: Project, file: VirtualFile): Pair<VirtualFile, String>? {
@@ -130,7 +118,7 @@ fun getServedDirAndPathForPubServer(project: Project, file: VirtualFile): Pair<V
   }
 
   val pubServePath = relativePath!!.substring(slashIndex)
-  return Pair.create(dartRoot.findChild(folderName), escapeUrl(pubServePath))
+  return Pair(dartRoot.findChild(folderName), escapeUrl(pubServePath))
 }
 
 private fun escapeUrl(path: String): String {
