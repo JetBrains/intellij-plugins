07f376c9d7d37514d08d9d2599ddce3a31bc6d37
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-11-25 15:15:14 +0100

    Web-types: improve validation of enum attribute values.
    (cherry picked from commit fa0cdd6689b7fdb42b25a28f86aa91bf255dfbb9)
    
    IJ-CR-17834
    
    GitOrigin-RevId: ef85ce0684a80c2f2b06374e079bbc12ce6ac1df

--- vuejs/vuejs-tests/testData/libraries/i18n/highlighting.vue
+++ vuejs/vuejs-tests/testData/libraries/i18n/highlighting.vue
@@ -5,5 +5,5 @@
   "foo"<EOLError descr="':' expected, got '}'"></EOLError>
 }
 </i18n>
-<i18n lang=<warning descr="foo is not a valid value for lang">"foo"</warning>></i18n>
+<i18n lang=<warning descr="foo is not a valid value for lang. Expected: json or yaml">"foo"</warning>></i18n>
 <i18n <warning descr="Attribute foo is not allowed here">foo</warning>="foo"></i18n>
