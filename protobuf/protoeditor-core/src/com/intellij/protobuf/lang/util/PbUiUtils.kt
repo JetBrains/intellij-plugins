46b16b5ff2cf280d07dd487e46aaf38f41d98842
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-11-25 19:16:25 +0300

    [protoeditor] IDEA-283097 Popup with suggested paths to import
    GitOrigin-RevId: abc09d24324ef53bf6ee64e6798a799f36ea5cef

--- protobuf/protoeditor-core/src/com/intellij/protobuf/lang/util/PbUiUtils.kt
+++ protobuf/protoeditor-core/src/com/intellij/protobuf/lang/util/PbUiUtils.kt
@@ -1,24 +1,25 @@
 package com.intellij.protobuf.lang.util
 
 import com.intellij.icons.AllIcons
-import com.intellij.ide.util.DefaultPsiElementCellRenderer
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.ui.popup.PopupStep
 import com.intellij.openapi.ui.popup.util.BaseListPopupStep
-import com.intellij.protobuf.ide.settings.PbProjectSettings
 import com.intellij.protobuf.lang.PbLangBundle
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.ui.ColoredListCellRenderer
+import com.intellij.ui.SimpleTextAttributes
 import java.awt.BorderLayout
+import javax.swing.JList
 import javax.swing.JPanel
 import javax.swing.ListCellRenderer
 
 internal object PbUiUtils {
-  fun selectItemAndApply(variants: List<PbProjectSettings.ImportPathEntry>,
+  fun selectItemAndApply(variants: List<ImportPathData>,
                          editor: Editor,
                          project: Project,
-                         selectAction: (PbProjectSettings.ImportPathEntry) -> Unit) {
+                         selectAction: (ImportPathData) -> Unit) {
     PsiDocumentManager.getInstance(project).commitAllDocuments()
 
     if (variants.isEmpty()) return
@@ -27,22 +28,22 @@ internal object PbUiUtils {
       return
     }
 
+    val customRenderer = createRenderer()
     JBPopupFactory.getInstance().createListPopup(project, createPopup(variants, project, selectAction)) {
-      val psiRenderer = DefaultPsiElementCellRenderer()
-
-      ListCellRenderer<PbProjectSettings.ImportPathEntry> { list, value, index, isSelected, cellHasFocus ->
+      ListCellRenderer<ImportPathData> { list, value, index, isSelected, cellHasFocus ->
         JPanel(BorderLayout()).apply {
-          add(psiRenderer.getListCellRendererComponent(list, value.location, index, isSelected, cellHasFocus))
+          add(customRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus))
         }
       }
     }.showInBestPositionFor(editor)
   }
 
-  private fun createPopup(variants: List<PbProjectSettings.ImportPathEntry>,
+  private fun createPopup(variants: List<ImportPathData>,
                           project: Project,
-                          selectAction: (PbProjectSettings.ImportPathEntry) -> Unit): BaseListPopupStep<PbProjectSettings.ImportPathEntry> {
-    return object : BaseListPopupStep<PbProjectSettings.ImportPathEntry>(PbLangBundle.message("intention.add.import.path.popup.title"), variants) {
-      override fun onChosen(selectedValue: PbProjectSettings.ImportPathEntry?, finalChoice: Boolean): PopupStep<String>? {
+                          selectAction: (ImportPathData) -> Unit): BaseListPopupStep<ImportPathData> {
+    return object : BaseListPopupStep<ImportPathData>(PbLangBundle.message("intention.add.import.path.popup.title"), variants) {
+
+      override fun onChosen(selectedValue: ImportPathData?, finalChoice: Boolean): PopupStep<Any>? {
         if (selectedValue == null || project.isDisposed) return null
 
         if (finalChoice) {
@@ -53,9 +54,22 @@ internal object PbUiUtils {
 
       override fun isSpeedSearchEnabled() = true
       override fun isAutoSelectionEnabled() = false
-      override fun hasSubstep(selectedValue: PbProjectSettings.ImportPathEntry?) = false
-      override fun getTextFor(value: PbProjectSettings.ImportPathEntry) = value.location
-      override fun getIconFor(value: PbProjectSettings.ImportPathEntry) = AllIcons.ObjectBrowser.AbbreviatePackageNames
+      override fun hasSubstep(selectedValue: ImportPathData?) = false
+      override fun getTextFor(value: ImportPathData) = value.presentablePath
+      override fun getIconFor(value: ImportPathData) = AllIcons.ObjectBrowser.AbbreviatePackageNames
+    }
+  }
+
+  private fun createRenderer(): ListCellRenderer<ImportPathData> {
+    return object : ColoredListCellRenderer<ImportPathData>() {
+
+      override fun customizeCellRenderer(list: JList<out ImportPathData>,
+                                         value: ImportPathData?,
+                                         index: Int,
+                                         selected: Boolean,
+                                         hasFocus: Boolean) {
+        if (value != null) append(value.presentablePath, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+      }
     }
   }
 }
