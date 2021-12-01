24e2a0205970bbd961d7d5e7814604e618c68daa
Pavel Porvatov <Pavel.Porvatov@jetbrains.com>
2021-12-01 23:07:52 +0300

    IDEA-283764 Port Auto Import settings to new UI DSL
    GitOrigin-RevId: 82b7b27f64f33569e22c902da2e29e585313e360

--- Dart/src/com/jetbrains/lang/dart/ide/editor/DartAutoImportOptionsProvider.kt
+++ Dart/src/com/jetbrains/lang/dart/ide/editor/DartAutoImportOptionsProvider.kt
@@ -1,3 +1,4 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.ide.editor
 
 import com.intellij.application.options.editor.AutoImportOptionsProvider
