975c29a82bf18b98001b03df331d155775816647
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2021-12-02 18:36:25 +0300

    html/xml: remove "tag" support, keep only tag name for simplicity WEB-21035
    GitOrigin-RevId: 02fd163c3bd385450d0d6d17e2d688b7c667ef50

---
+++ AngularJS/test/org/angular2/lang/html/semantics/data/customComponent2.ts
@@ -0,0 +1,11 @@
+// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import {Component} from '@angular/core';
+
+@Component({
+             selector: 'custom-component-2',
+             template: `
+      <div></div>`,
+           })
+export class CustomComponent2 {
+
+}
--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueModuleImportTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueModuleImportTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.lang
 
+import com.intellij.lang.javascript.JSTestOption
 import com.intellij.lang.javascript.JSTestOptions
 import com.intellij.lang.javascript.JavaScriptFormatterTestBase
 import com.intellij.lang.javascript.JavascriptLanguage
@@ -75,5 +76,10 @@ class VueModuleImportTest : JSImportHighlightingAndCompletionLightTestBase() {
 
   fun testReExportWIthJs() {
     doTestWithCopyDirectory(1, true, "ts")
+  }
+  
+  @JSTestOptions(JSTestOption.WithSymbolNames)
+  fun testCustomComponentHighlighting() {
+    doTestWithCopyDirectory()
   }
 }
