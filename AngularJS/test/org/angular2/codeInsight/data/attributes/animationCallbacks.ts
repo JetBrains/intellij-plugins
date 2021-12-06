ff461885669651296bfa14dcd2be8a9bc021c00f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-12-06 13:12:30 +0100

    WEB-53861 Angular animation callbacks not recognized
    GitOrigin-RevId: 7781d5303e104750566afbfe37548d3ddb8e0a34

---
+++ AngularJS/test/org/angular2/codeInsight/data/attributes/animationCallbacks.ts
@@ -0,0 +1,14 @@
+// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'my-comp',
+    templateUrl: "./animationCallbacks.html"
+})
+export class AnimationCallbacks<T> {
+  isOpen: boolean
+
+  onAnimationEvent($event: any) {
+
+  }
+}
