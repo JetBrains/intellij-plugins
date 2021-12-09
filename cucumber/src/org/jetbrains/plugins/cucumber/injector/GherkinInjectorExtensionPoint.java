fd6918f73b88dbdf21e6e52eb49b11ccedd918b8
Dani Carles <dani.carles@workday.com>
2021-12-09 18:18:49 +0000

    Adding extension point to allow for configuration of language injection prefixes and suffixes

---
+++ cucumber/src/org/jetbrains/plugins/cucumber/injector/GherkinInjectorExtensionPoint.java
@@ -0,0 +1,28 @@
+package org.jetbrains.plugins.cucumber.injector;
+
+import com.intellij.openapi.extensions.ExtensionPointName;
+
+public interface GherkinInjectorExtensionPoint {
+    ExtensionPointName<GherkinInjectorExtensionPoint> EP_NAME =
+            ExtensionPointName.create("org.jetbrains.plugins.cucumber.injector.injectorExtensionPoint");
+
+    /**
+     * Returns the prefix to be injected for the specified language
+     *
+     * @param language injected language
+     * @return injection prefix
+     */
+    default String getPrefix(String language) {
+        return "";
+    }
+
+    /**
+     * Returns the suffix to be injected for the specified language
+     *
+     * @param language injected language
+     * @return injection prefix
+     */
+    default String getSuffix(String language) {
+        return "";
+    }
+}
--- cucumber/src/org/jetbrains/plugins/cucumber/injector/GherkinLanguageInjector.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/injector/GherkinLanguageInjector.java
@@ -1,6 +1,16 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.injector;
 
+import static org.jetbrains.plugins.cucumber.psi.GherkinLexer.PYSTRING_MARKER;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.intellij.plugins.intelliLang.inject.InjectorUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.plugins.cucumber.psi.GherkinPystring;
+
 import com.intellij.lang.Language;
 import com.intellij.lang.injection.MultiHostInjector;
 import com.intellij.lang.injection.MultiHostRegistrar;
@@ -8,14 +18,6 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiLanguageInjectionHost;
-import org.intellij.plugins.intelliLang.inject.InjectorUtils;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.cucumber.psi.GherkinPystring;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.jetbrains.plugins.cucumber.psi.GherkinLexer.PYSTRING_MARKER;
 
 public class GherkinLanguageInjector implements MultiHostInjector {
     @NotNull
@@ -32,7 +34,7 @@ public class GherkinLanguageInjector implements MultiHostInjector {
 
         final PsiLanguageInjectionHost host = (PsiLanguageInjectionHost) context;
 
-        String hostText = host.getText();
+        final String hostText = host.getText();
         int newLineCharacterOffset = 0;
         while (newLineCharacterOffset < hostText.length() && hostText.charAt(newLineCharacterOffset) != '\n') {
             newLineCharacterOffset++;
@@ -40,15 +42,20 @@ public class GherkinLanguageInjector implements MultiHostInjector {
         if (newLineCharacterOffset >= hostText.length()) {
             return;
         }
-        String languageMarker = StringUtil.trimTrailing(hostText.substring(PYSTRING_MARKER.length(), newLineCharacterOffset));
+        final String languageMarker = StringUtil.trimTrailing(hostText.substring(PYSTRING_MARKER.length(), newLineCharacterOffset));
         final Language language = InjectorUtils.getLanguageByString(languageMarker);
 
         if (language != null) {
-            TextRange range = TextRange.create(newLineCharacterOffset, host.getTextLength() - PYSTRING_MARKER.length());
+            final TextRange range = TextRange.create(newLineCharacterOffset, host.getTextLength() - PYSTRING_MARKER.length());
 
             if (!range.isEmpty()) {
+                final Optional<GherkinInjectorExtensionPoint> extensionPoint =
+                        GherkinInjectorExtensionPoint.EP_NAME.getExtensionList().stream().findFirst();
+                final String prefix = extensionPoint.map(ep -> ep.getPrefix(languageMarker)).orElse(null);
+                final String suffix = extensionPoint.map(ep -> ep.getSuffix(languageMarker)).orElse(null);
+
                 registrar.startInjecting(language);
-                registrar.addPlace(null, null, host, range);
+                registrar.addPlace(prefix, suffix, host, range);
                 registrar.doneInjecting();
             }
         }
