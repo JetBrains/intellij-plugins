65a08d19c20f564ec8386e92595f06ba5796355c
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2021-12-23 18:46:59 +0300

    [protoeditor] IDEA-283099 Preserve `auto configuration` setting state during import paths configuration being re/un-done
    GitOrigin-RevId: c951567206ebba86a34f4249874535cc0a97dc83

--- protobuf/protoeditor-core/src/com/intellij/protobuf/lang/intentions/PbAddImportPathUndoableAction.kt
+++ protobuf/protoeditor-core/src/com/intellij/protobuf/lang/intentions/PbAddImportPathUndoableAction.kt
@@ -10,10 +10,12 @@ import com.intellij.protobuf.lang.util.ImportPathData
 internal class PbAddImportPathUndoableAction(pathToAdd: ImportPathData, private val project: Project) : UndoableAction {
   private val currentDocumentReference = DocumentReferenceManager.getInstance().create(pathToAdd.originalPbVirtualFile)
   private val importPath = PbProjectSettings.ImportPathEntry(pathToAdd.effectiveImportPathUrl, "")
+  private val originalAutoConfigSetting: Boolean = PbProjectSettings.getInstance(project).isAutoConfigEnabled
 
   override fun undo() {
     if (project.isDisposed) return
     val projectSettings = PbProjectSettings.getInstance(project)
+    projectSettings.isAutoConfigEnabled = originalAutoConfigSetting
     projectSettings.importPathEntries = projectSettings.importPathEntries.filter { it != importPath }
     PbProjectSettings.notifyUpdated(project)
   }
@@ -21,6 +23,7 @@ internal class PbAddImportPathUndoableAction(pathToAdd: ImportPathData, private 
   override fun redo() {
     if (project.isDisposed) return
     val projectSettings = PbProjectSettings.getInstance(project)
+    projectSettings.isAutoConfigEnabled = false
     projectSettings.importPathEntries = listOf(*projectSettings.importPathEntries.toTypedArray(), importPath)
     PbProjectSettings.notifyUpdated(project)
   }
