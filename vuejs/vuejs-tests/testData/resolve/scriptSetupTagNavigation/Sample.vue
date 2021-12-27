3594ccf318a56b456895f88644b750bb37ed9178
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-12-27 13:54:33 +0100

    WEB-53569 Vue.js: "Navigate | Declaration" on custom component doesn't jump to definition when using "script setup"
    (cherry picked from commit 4eec9d7a3c0ab77050b3dd99c4c1be54fbdcaf95)
    
    IJ-CR-19153
    
    GitOrigin-RevId: d7e8574c49832aa98d63c1efd18cd5a192678c7b

---
+++ vuejs/vuejs-tests/testData/resolve/scriptSetupTagNavigation/Sample.vue
@@ -0,0 +1,14 @@
+<template>
+
+<ul>
+
+<li v-for="(item, key) in items" :key="key" @click="clicked(item)">{{ item }}</li>
+
+</ul>
+
+</template>
+
+<script setup>
+const items = ["apple", "banana", "orange"];
+const clicked = (item) => console.log(item);
+</script>
