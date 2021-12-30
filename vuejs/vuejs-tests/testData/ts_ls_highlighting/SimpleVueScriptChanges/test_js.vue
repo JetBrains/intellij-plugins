b4e1c09bc94c26078331242193f353bbd6443d44
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2021-12-30 15:47:04 +0100

    Vue: fix error message in test
    GitOrigin-RevId: 30a755daaf4c3c378eef77571cd4acb045744755

--- vuejs/vuejs-tests/testData/ts_ls_highlighting/SimpleVueScriptChanges/test_js.vue
+++ vuejs/vuejs-tests/testData/ts_ls_highlighting/SimpleVueScriptChanges/test_js.vue
@@ -1,6 +1,6 @@
 <template><div></div></template>
 <script lang="js">
-    declare<error descr="Expecting newline or semicolon"> </error>var React;
+    declare<error descr="Newline or semicolon expected"> </error>var React;
     export let a:<error descr="Types are not supported by current JavaScript version">string</error> = 1;
 
     function test() {
