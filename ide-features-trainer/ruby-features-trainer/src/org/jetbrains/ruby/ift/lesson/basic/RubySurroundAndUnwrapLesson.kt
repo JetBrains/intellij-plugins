58c346ff36039c85714411ca0f4009e7906385eb
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2022-02-01 17:31:08 +0300

    [IFT] Complete Editor Basics module for Kotlin
    GitOrigin-RevId: 5149c76760979206dde31b6bb8d753b8754e419d

--- ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/basic/RubySurroundAndUnwrapLesson.kt
+++ ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/basic/RubySurroundAndUnwrapLesson.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.ruby.ift.lesson.basic
 
+import org.jetbrains.plugins.ruby.RBundle
 import training.dsl.LessonSample
 import training.dsl.parseLessonSample
 import training.learn.lesson.general.SurroundAndUnwrapLesson
@@ -18,4 +19,6 @@ class RubySurroundAndUnwrapLesson : SurroundAndUnwrapLesson() {
 
   override val surroundItemName: String
     get() = "if...end"
+
+  override val unwrapTryText: String = RBundle.message("unwrap.if")
 }
