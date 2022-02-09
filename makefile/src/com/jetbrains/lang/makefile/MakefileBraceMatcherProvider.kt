27d87fbfac6ad066e63b59b09e26798c2407e30d
Andrey Shcheglov <andrey.shcheglov@jetbrains.com>
2022-02-09 11:50:41 +0300

    CPP-24010 Correct the highlighting of `ifdef ... else ... endif` blocks in Makefiles
    GitOrigin-RevId: ed502fcba7166c403fce1fbe7bc0283cfa8eadee

--- makefile/src/com/jetbrains/lang/makefile/MakefileBraceMatcherProvider.kt
+++ makefile/src/com/jetbrains/lang/makefile/MakefileBraceMatcherProvider.kt
@@ -1,20 +1,23 @@
 package com.jetbrains.lang.makefile
 
-import com.intellij.lang.*
-import com.intellij.psi.*
-import com.intellij.psi.tree.*
-import com.jetbrains.lang.makefile.psi.*
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import com.jetbrains.lang.makefile.psi.MakefileTypes
 
 private val PAIRS = arrayOf(
-    BracePair(MakefileTypes.KEYWORD_IFDEF, MakefileTypes.KEYWORD_ENDIF, true),
-    BracePair(MakefileTypes.KEYWORD_IFNDEF, MakefileTypes.KEYWORD_ENDIF, true),
-    BracePair(MakefileTypes.KEYWORD_IFEQ, MakefileTypes.KEYWORD_ENDIF, true),
-    BracePair(MakefileTypes.KEYWORD_IFNEQ, MakefileTypes.KEYWORD_ENDIF, true),
     BracePair(MakefileTypes.KEYWORD_DEFINE, MakefileTypes.KEYWORD_ENDEF, true),
     BracePair(MakefileTypes.OPEN_CURLY, MakefileTypes.CLOSE_CURLY, true),
     BracePair(MakefileTypes.OPEN_PAREN, MakefileTypes.CLOSE_PAREN, true)
 )
 
+/**
+ * More complex cases, like `ifdef/ifndef/ifeq/ifneq ... else ... endif` are
+ * handled via [MakefileCodeBlockSupportHandler].
+ *
+ * @see MakefileCodeBlockSupportHandler
+ */
 class MakefileBraceMatcherProvider : PairedBraceMatcher {
   override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int = openingBraceOffset
   override fun getPairs(): Array<BracePair> = PAIRS
---
+++ makefile/src/com/jetbrains/lang/makefile/MakefileCodeBlockSupportHandler.kt
@@ -0,0 +1,81 @@
+package com.jetbrains.lang.makefile
+
+import com.intellij.codeInsight.highlighting.AbstractCodeBlockSupportHandler
+import com.intellij.psi.tree.IElementType
+import com.intellij.psi.tree.TokenSet
+import com.jetbrains.lang.makefile.MakefileFoldingBuilder.MakefileConditionalFoldingDescriptor
+import com.jetbrains.lang.makefile.psi.MakefileTypes.*
+
+/**
+ * Code block handler (which highlights matching tokens) for constructs like
+ *
+ * ```
+ * ifdef ...
+ *   ...
+ * else ifeq ...
+ *   ...
+ * endif
+ * ```
+ *
+ * Pairs of `define ... endef` are handled (in a simpler way) via
+ * [MakefileBraceMatcherProvider].
+ *
+ * Block Folding is handled via [MakefileConditionalFoldingDescriptor].
+ *
+ * @see MakefileBraceMatcherProvider
+ * @see MakefileConditionalFoldingDescriptor
+ */
+class MakefileCodeBlockSupportHandler : AbstractCodeBlockSupportHandler() {
+  override fun getTopLevelElementTypes(): TokenSet =
+    TOP_LEVEL_ELEMENTS
+
+  override fun getKeywordElementTypes(): TokenSet =
+    KEYWORD_ELEMENTS
+
+  override fun getBlockElementTypes(): TokenSet =
+    BLOCK_ELEMENTS
+
+  override fun getDirectChildrenElementTypes(parentElementType: IElementType?): TokenSet =
+    when (parentElementType) {
+      null -> TokenSet.EMPTY
+      else -> HIERARCHY_MAP[parentElementType] ?: TokenSet.EMPTY
+    }
+
+  private companion object {
+    private val HIERARCHY_MAP = mapOf(
+      CONDITIONAL to TokenSet.create(KEYWORD_IFDEF,
+                                     KEYWORD_IFNDEF,
+                                     KEYWORD_IFEQ,
+                                     KEYWORD_IFNEQ,
+                                     CONDITIONAL_ELSE,
+                                     KEYWORD_ENDIF),
+      CONDITIONAL_ELSE to TokenSet.create(KEYWORD_ELSE,
+                                          KEYWORD_IFDEF,
+                                          KEYWORD_IFNDEF,
+                                          KEYWORD_IFEQ,
+                                          KEYWORD_IFNEQ),
+    )
+
+    private val TOP_LEVEL_ELEMENTS: TokenSet
+
+    private val KEYWORD_ELEMENTS: TokenSet
+
+    private val BLOCK_ELEMENTS: TokenSet
+
+    init {
+      val parents = mutableSetOf<IElementType>()
+      val children = mutableSetOf<IElementType>()
+
+      HIERARCHY_MAP.forEach { (parent, immediateChildren) ->
+        parents += parent
+        children += immediateChildren.types
+      }
+
+      TOP_LEVEL_ELEMENTS = TokenSet.create(*(parents - children).toTypedArray())
+
+      KEYWORD_ELEMENTS = TokenSet.create(*(children - parents).toTypedArray())
+
+      BLOCK_ELEMENTS = TokenSet.create(*parents.toTypedArray())
+    }
+  }
+}
