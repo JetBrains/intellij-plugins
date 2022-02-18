0cb3b5a3fa8d570d5500be482277cdf50c166eb1
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2022-02-18 13:09:33 +0300

    jsx: support public/static directories and %PUBLIC_URL% in general WEB-54865
    GitOrigin-RevId: 9564bfcf7a6eae56b8a1ff4f28be2492882459b1

--- vuejs/vuejs-tests/testData/libraries/nuxt/highlighting/ref-to-static/b/page/test.vue
+++ vuejs/vuejs-tests/testData/libraries/nuxt/highlighting/ref-to-static/b/page/test.vue
@@ -2,5 +2,5 @@
   <img src="<warning descr="Cannot resolve file 'a.svg'">a.svg</warning>"/>
   <img src="/<warning descr="Cannot resolve file 'a.svg'">a.svg</warning>"/>
   <img src="<warning descr="Cannot resolve file 'b.svg'">b.svg</warning>"/>
-  <img src="/<warning descr="Cannot resolve file 'b.svg'">b.svg</warning>"/>
+  <img src="/b.svg"/>
 </template>
