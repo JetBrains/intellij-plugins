65fd69eb8391dba801fe9838d95e941beb0f38d0
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2022-03-14 21:45:31 +0100

    [vue] prevent occasional SOE in NuxtDotFolderSyntheticLibraryProvider (WEB-54948) (EA-446476)
    (cherry picked from commit eb262ae89df1ef2e6e2e1052cdb6244f013b9ceb)
    
    IJ-CR-22009
    
    GitOrigin-RevId: 6e90d8515b61981e84a56bd4c5e0078ce1d7abda

--- vuejs/src/org/jetbrains/vuejs/libraries/nuxt/library/NuxtDotFolderSyntheticLibraryProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/libraries/nuxt/library/NuxtDotFolderSyntheticLibraryProvider.kt
@@ -2,18 +2,19 @@
 package org.jetbrains.vuejs.libraries.nuxt.library
 
 import com.intellij.javascript.nodejs.packageJson.PackageJsonFileManager
-import com.intellij.json.JsonUtil
 import com.intellij.lang.javascript.ecmascript6.TypeScriptUtil
 import com.intellij.lang.javascript.library.JSSyntheticLibraryProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.AdditionalLibraryRootsProvider
 import com.intellij.openapi.roots.SyntheticLibrary
+import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.vuejs.libraries.nuxt.NUXT_OUTPUT_FOLDER
 
 class NuxtDotFolderSyntheticLibraryProvider : AdditionalLibraryRootsProvider(), JSSyntheticLibraryProvider {
 
-  override fun getAdditionalProjectLibraries(project: Project): Collection<SyntheticLibrary> =
-    PackageJsonFileManager.getInstance(project)
+  override fun getAdditionalProjectLibraries(project: Project): Collection<SyntheticLibrary> {
+    return PackageJsonFileManager.getInstance(project)
       .validPackageJsonFiles
       .mapNotNull { it ->
         val dotNuxt = it.parent?.findChild(NUXT_OUTPUT_FOLDER) ?: return@mapNotNull null
@@ -21,8 +22,16 @@ class NuxtDotFolderSyntheticLibraryProvider : AdditionalLibraryRootsProvider(), 
           if (it.isDirectory)
             it != dotNuxt
           else
-            !TypeScriptUtil.isDefinitionFile(it) && !JsonUtil.isJsonFile(it, project)
+            !TypeScriptUtil.isDefinitionFile(it) &&
+            !isJsonFile(it)
         }
       }
+  }
 
+  /**
+   * Using JsonUtil.isJsonFile here can produce StackOverflowError
+   */
+  private fun isJsonFile(file: VirtualFile): Boolean {
+    return StringUtil.endsWith(file.nameSequence, ".json")
+  }
 }
