baedd5653bc562ba4708562caf35c2604a8571da
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2022-04-11 17:00:59 +0200

    [vue] support references in defineProps type declaration (WEB-54034)
    GitOrigin-RevId: aeb74d8527000e6f5a694bbd16a7e79ea20e2faa

---
+++ vuejs/vuejs-tests/testData/component/DefinePropsTypeDeclarationTypeAliasTS.vue
@@ -0,0 +1,18 @@
+<!--
+Expected Component Model:
+Prop(name=optional, required=false, presentableType=string | undefined)
+Prop(name=required, required=true, presentableType=string)
+-->
+<script setup lang="ts">
+type Props = {
+  optional?: string,
+  required: string,
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  {{optional}}
+  {{required}}
+</template>
