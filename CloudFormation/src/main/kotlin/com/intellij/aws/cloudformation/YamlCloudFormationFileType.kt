5b54f764e5eeedb34173ebe73d34fc78083094c8
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2022-04-15 20:41:25 +0300

    [cloudformation] Icon provider for YAML/JSON files
    Replace CloudFormationIconProvider with icon in file type
    
    GitOrigin-RevId: aca7c2c35d8ac2a7969c7818cff02e380b727cf7

--- CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/YamlCloudFormationFileType.kt
+++ CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/YamlCloudFormationFileType.kt
@@ -1,6 +1,5 @@
 package com.intellij.aws.cloudformation
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.fileTypes.LanguageFileType
 import org.jetbrains.yaml.YAMLLanguage
 import javax.swing.Icon
@@ -9,11 +8,11 @@ class YamlCloudFormationFileType private constructor(): LanguageFileType(YAMLLan
   override fun getName(): String = "AWSCloudFormation (YAML)"
   override fun getDescription(): String = CloudFormationBundle.message("filetype.aws.cloudformation.templates.yaml.description")
   override fun getDefaultExtension(): String = ""
-  override fun getIcon(): Icon? = AllIcons.FileTypes.Yaml
+  override fun getIcon(): Icon = CloudFormationIcons.AwsFile
   override fun getDisplayName(): String = CloudFormationBundle.message("filetype.aws.cloudformation.templates.yaml.display.name")
 
   companion object {
     @JvmField
-    val INSTANCE = YamlCloudFormationFileType()
+    val INSTANCE: LanguageFileType = YamlCloudFormationFileType()
   }
 }
