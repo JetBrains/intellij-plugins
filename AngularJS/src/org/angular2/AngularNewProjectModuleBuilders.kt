df1071284161509dfaa66d1f140664fb8de2bc89
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2022-04-20 23:32:42 +0300

    WEB-52629 Remove AngularJS template from project wizard
    GitOrigin-RevId: 05eca3f50bfdb9be82618b49998c750f5bc4c65d

--- AngularJS/src/org/angular2/AngularNewProjectModuleBuilders.kt
+++ AngularJS/src/org/angular2/AngularNewProjectModuleBuilders.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2
 
 import com.intellij.ide.util.projectWizard.WebTemplateNewProjectWizard
@@ -7,17 +7,12 @@ import com.intellij.ide.wizard.GeneratorNewProjectWizardBuilderAdapter
 import com.intellij.lang.javascript.boilerplate.JavaScriptNewTemplatesFactoryBase
 import com.intellij.platform.ProjectTemplate
 import org.angular2.cli.AngularCliProjectGenerator
-import org.angularjs.AngularJSProjectGenerator
 
 //no angular.js for new API
 class AngularCLIModuleBuilder : GeneratorNewProjectWizardBuilderAdapter(WebTemplateNewProjectWizard(AngularCliProjectGenerator())){
   override fun getWeight() = WEB_WEIGHT + 10
 }
 
-class AngularAndAngularJSTemplateFactory : JavaScriptNewTemplatesFactoryBase() {
-  override fun createTemplates(context: WizardContext?): Array<ProjectTemplate> =
-    arrayOf(
-      AngularCliProjectGenerator(),
-      AngularJSProjectGenerator(),
-    )
+class AngularProjectTemplatesFactory : JavaScriptNewTemplatesFactoryBase() {
+  override fun createTemplates(context: WizardContext?): Array<ProjectTemplate> = arrayOf(AngularCliProjectGenerator())
 }
