d949caf2318ea7c688d75ef618547b8028af7315
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2022-04-20 16:30:54 +0300

    WEB-55587 dart - language injections for `RegExp` strings
    (cherry picked from commit 0d0420d978f733b8264346ea60e0b4a3a213f5e5)
    
    IJ-CR-23599
    
    GitOrigin-RevId: f4dde5e8b21ad30d58a209315513ed5a0c111fa0

--- Dart/testData/injection/RegExp.dart
+++ Dart/testData/injection/RegExp.dart
@@ -9,5 +9,6 @@ var a = new RegEx(r'.');
 var a = new RegEx(r".");
 var a = RegExp(r'.');
 var a = RegExp(r".");
+var a = RegExps(r".");
 var a = new RegExp(r'.' r".");
 final _specialChars = new RegExp(r'[\\()$^.+[\]{}|]');
