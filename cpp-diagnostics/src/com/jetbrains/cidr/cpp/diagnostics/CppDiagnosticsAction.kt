9b46fe6625db31fe401242b85b710c16b99cb111
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2022-04-21 17:40:28 +0300

    CPP-29008: connect CppDiagnosticsAction to TroubleInfoCollector
    GitOrigin-RevId: 7a24d833354d6fe9ea2a19e953883a8adc0f2979

--- cpp-diagnostics/src/com/jetbrains/cidr/cpp/diagnostics/CppDiagnosticsAction.kt
+++ cpp-diagnostics/src/com/jetbrains/cidr/cpp/diagnostics/CppDiagnosticsAction.kt
@@ -11,15 +11,16 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.DumbAwareAction
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsContexts
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.testFramework.LightVirtualFile
+import com.intellij.troubleshooting.TroubleInfoCollector
 import com.intellij.util.io.Compressor
 import java.io.File
 import java.io.IOException
-
 
 /**
  * This action may act as an example of how to access various C/C++ project model information from a plugin
@@ -29,9 +30,17 @@ import java.io.IOException
  * @see collectOCWorkspace
  * @see collectOCWorkspaceEvents
  */
-class CppDiagnosticsAction : DumbAwareAction() {
+class CppDiagnosticsAction : DumbAwareAction(), TroubleInfoCollector {
   override fun update(e: AnActionEvent) {
     e.presentation.isEnabledAndVisible = getEventProject(e) != null
+  }
+
+  override fun collectInfo(project: Project): String {
+    val indicator = ProgressManager.getGlobalProgressIndicator()
+
+    return collectData(indicator, project).fold(StringBuilder("=====CLION SUMMARY=====").appendLine()) { acc, fileWithContents ->
+      acc.append(fileWithContents.filename).appendLine().append(fileWithContents.contents).appendLine()
+    }.toString()
   }
 
   override fun actionPerformed(e: AnActionEvent) {
@@ -39,29 +48,7 @@ class CppDiagnosticsAction : DumbAwareAction() {
 
     val task = object : Task.Modal(project, CppDiagnosticsBundle.message("cpp.diagnostics.progress.title"), true) {
       override fun run(indicator: ProgressIndicator) {
-        val toolchains = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.toolchains")) {
-          collectToolchains(project)
-        }
-
-        val cidrWorkspaces = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.workspace")) {
-          collectCidrWorkspaces(project)
-        }
-
-        val ocWorkspace = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.ocWorkspace")) {
-          collectOCWorkspace(project)
-        }
-
-        val ocWorkspaceEvents = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.ocWorkspaceEvents")) {
-          collectOCWorkspaceEvents(project)
-        }
-
-
-        val files = listOf(
-          FileWithContents("Toolchains.txt", toolchains),
-          FileWithContents("CidrWorkspaces.txt", cidrWorkspaces),
-          FileWithContents("OCWorkspaceEvents.txt", ocWorkspaceEvents),
-          FileWithContents("OCWorkspace.txt", ocWorkspace)
-        )
+        val files = collectData(indicator, project)
 
         zipAndReveal(files)
 
@@ -79,11 +66,38 @@ class CppDiagnosticsAction : DumbAwareAction() {
     ProgressManager.getInstance().run(task)
   }
 
+  private fun collectData(indicator: ProgressIndicator,
+                          project: Project): List<FilenameAndContent> {
+    val toolchains = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.toolchains")) {
+      collectToolchains(project)
+    }
+
+    val cidrWorkspaces = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.workspace")) {
+      collectCidrWorkspaces(project)
+    }
+
+    val ocWorkspace = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.ocWorkspace")) {
+      collectOCWorkspace(project)
+    }
+
+    val ocWorkspaceEvents = readActionWithText(indicator, CppDiagnosticsBundle.message("cpp.diagnostics.progress.ocWorkspaceEvents")) {
+      collectOCWorkspaceEvents(project)
+    }
+
+    val files = listOf(
+      FilenameAndContent("Toolchains.txt", toolchains),
+      FilenameAndContent("CidrWorkspaces.txt", cidrWorkspaces),
+      FilenameAndContent("OCWorkspaceEvents.txt", ocWorkspaceEvents),
+      FilenameAndContent("OCWorkspace.txt", ocWorkspace)
+    )
+    return files
+  }
+
   companion object {
-    private data class FileWithContents(val filename: String, val contents: String)
+    private data class FilenameAndContent(val filename: String, val contents: String)
 
     @Throws(IOException::class)
-    private fun createZip(files: Iterable<FileWithContents>): File {
+    private fun createZip(files: Iterable<FilenameAndContent>): File {
       val productName = StringUtil.toLowerCase(ApplicationNamesInfo.getInstance().lowercaseProductName)
       val zippedLogsFile = FileUtil.createTempFile(productName + "-cpp-diag-" + formatCurrentTime(), ".zip")
       try {
@@ -100,7 +114,7 @@ class CppDiagnosticsAction : DumbAwareAction() {
       return zippedLogsFile
     }
 
-    private fun zipAndReveal(files: Iterable<FileWithContents>) {
+    private fun zipAndReveal(files: Iterable<FilenameAndContent>) {
       try {
         val zippedDiagnostics = createZip(files)
 
