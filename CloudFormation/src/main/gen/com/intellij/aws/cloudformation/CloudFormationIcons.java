68bd739d69ab22d101c5fe99b0231f9e3d250b42
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2022-05-04 12:02:50 +0200

    [cloudformation] Fix AWS file icon
    GitOrigin-RevId: 5c47ec2e16d38c834fa71ea9bd581d7c979311f7

--- CloudFormation/src/main/gen/com/intellij/aws/cloudformation/CloudFormationIcons.java
+++ CloudFormation/src/main/gen/com/intellij/aws/cloudformation/CloudFormationIcons.java
@@ -14,5 +14,5 @@ public final class CloudFormationIcons {
     return IconManager.getInstance().loadRasterizedIcon(path, CloudFormationIcons.class.getClassLoader(), cacheKey, flags);
   }
   /** 16x16 */ public static final @NotNull Icon Aws = load("icons/aws.svg", -607101785, 0);
-  /** 16x16 */ public static final @NotNull Icon AwsFile = load("icons/awsFile.svg", 398845985, 0);
+  /** 16x16 */ public static final @NotNull Icon AwsFile = load("icons/awsFile.svg", 857372646, 0);
 }
