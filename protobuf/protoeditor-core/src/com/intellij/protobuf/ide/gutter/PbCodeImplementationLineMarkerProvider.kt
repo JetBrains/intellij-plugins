a077bc5f44c250d3faf967f4af83354eafb2ee5c
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2022-05-17 10:22:05 +0200

    [protoeditor] IDEA-294180 Line marker provider and API for implementations search
    GitOrigin-RevId: 1fbee68ffa6501d6a59ac983f7283ca249c85ed8

---
+++ protobuf/protoeditor-core/src/com/intellij/protobuf/ide/gutter/PbCodeImplementationLineMarkerProvider.kt
@@ -0,0 +1,85 @@
+package com.intellij.protobuf.ide.gutter
+
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.editor.markup.GutterIconRenderer
+import com.intellij.openapi.extensions.ExtensionPointName
+import com.intellij.openapi.util.NotNullFactory
+import com.intellij.protobuf.ide.PbIdeBundle
+import com.intellij.protobuf.lang.psi.PbElement
+import com.intellij.psi.NavigatablePsiElement
+import com.intellij.psi.PsiElement
+import kotlin.streams.asSequence
+
+internal class PbCodeImplementationLineMarkerProvider : RelatedItemLineMarkerProvider() {
+  override fun getId(): String {
+    return "PbCodeImplementationLineMarkerProvider"
+  }
+
+  override fun getName(): String? {
+    return PbIdeBundle.message("line.marker.overridden.element")
+  }
+
+  override fun collectNavigationMarkers(element: PsiElement, result: MutableCollection<in RelatedItemLineMarkerInfo<*>>) {
+    val marker =
+      when (element) {
+        is PbElement ->
+          if (hasImplementations(element)) createOverriddenElementMarker(element) else null
+        else ->
+          if (hasProtoDefinition(element)) createImplementedElementMarker(element) else null
+      } ?: return
+
+    result.add(marker)
+  }
+
+  private fun createOverriddenElementMarker(protoElement: PbElement): RelatedItemLineMarkerInfo<PsiElement> {
+    return object : RelatedItemLineMarkerInfo<PsiElement>(
+      protoElement,
+      protoElement.textRange,
+      AllIcons.General.OverridenMethod,
+      { null },
+      null,
+      GutterIconRenderer.Alignment.LEFT,
+      NotNullFactory {
+        mutableListOf()
+      }
+    ) {}
+  }
+
+  private fun createImplementedElementMarker(psiElement: PsiElement): RelatedItemLineMarkerInfo<PsiElement> {
+    return object : RelatedItemLineMarkerInfo<PsiElement>(
+      psiElement,
+      psiElement.textRange,
+      AllIcons.General.ImplementingMethod,
+      { null },
+      null,
+      GutterIconRenderer.Alignment.LEFT,
+      NotNullFactory {
+        mutableListOf()
+      }
+    ) {}
+  }
+
+  private fun hasImplementations(pbElement: PbElement): Boolean {
+    return EP_NAME.extensions().asSequence()
+      .flatMap { it.findImplementationsForProtoElement(pbElement) }
+      .any()
+  }
+
+  private fun hasProtoDefinition(psiElement: PsiElement): Boolean {
+    return EP_NAME.extensions().asSequence()
+      .flatMap { it.findDeclarationsForCodeElement(psiElement) }
+      .any()
+  }
+
+  companion object {
+    private val EP_NAME =
+      ExtensionPointName.create<PbCodeImplementationSearcher>("com.intellij.protobuf.codeImplementationSearcher")
+  }
+}
+
+internal interface PbCodeImplementationSearcher {
+  fun findImplementationsForProtoElement(pbElement: PbElement): Sequence<NavigatablePsiElement>
+  fun findDeclarationsForCodeElement(psiElement: PsiElement): Sequence<PbElement>
+}
