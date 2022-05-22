55da77c56443a27a8bdab237a3c3b3878e145c48
Alexey Gerasimov <aleksey.g@miro.com>
2022-05-23 01:20:15 +0400

    Adapt test data path to be able to run gradle test

--- makefile/test/com/jetbrains/lang/makefile/MakefileTestUtils.kt
+++ makefile/test/com/jetbrains/lang/makefile/MakefileTestUtils.kt
@@ -1,8 +1,16 @@
 package com.jetbrains.lang.makefile
 
 import com.intellij.openapi.application.PathManager
+import java.io.File
 
 /**
  * Test data directory
  */
-internal val BASE_TEST_DATA_PATH: String = PathManager.getHomePath() + "/contrib/makefile/testData"
+internal val BASE_TEST_DATA_PATH: String = findTestDataPath()
+
+private fun findTestDataPath(): String {
+    val f = File("testData")
+    return if (f.exists()) {
+        f.absolutePath
+    } else PathManager.getHomePath() + "/contrib/makefile/testData"
+}
