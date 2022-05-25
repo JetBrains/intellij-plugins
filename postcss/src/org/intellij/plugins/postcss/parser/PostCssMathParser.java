20b712dbdca4848bd5eff8592295281ed2adad84
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2022-05-26 01:03:41 +0300

    WEB-21705 Incorrect "unexpected term" error when using variable interpolation for defining operation in calc()
    GitOrigin-RevId: bb2a8b0f9418894749de7b812c3a4cacf510fb2c

--- postcss/src/org/intellij/plugins/postcss/parser/PostCssMathParser.java
+++ postcss/src/org/intellij/plugins/postcss/parser/PostCssMathParser.java
@@ -3,6 +3,7 @@ package org.intellij.plugins.postcss.parser;
 import com.intellij.psi.css.impl.parsing.CssMathParser;
 import com.intellij.psi.tree.IElementType;
 import org.intellij.plugins.postcss.lexer.PostCssTokenTypes;
+import org.jetbrains.annotations.Nullable;
 
 public class PostCssMathParser extends CssMathParser {
   private final PostCssParser myParser;
@@ -13,7 +14,7 @@ public class PostCssMathParser extends CssMathParser {
   }
 
   @Override
-  protected boolean parseTerm(IElementType prevOperation) {
+  protected boolean parseTerm(@Nullable IElementType prevOperation) {
     if (getTokenType() == PostCssTokenTypes.POST_CSS_SIMPLE_VARIABLE_TOKEN) {
       myParser.parseSimpleVariable();
       return true;
