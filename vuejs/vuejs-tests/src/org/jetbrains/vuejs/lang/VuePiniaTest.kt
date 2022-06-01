cab92911ab1f6c883000fcd8ab3eab236b8a6787
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2022-06-01 16:52:45 +0200

    [vue] evaluate initializers accurately (WEB-54970)
    (cherry picked from commit 1a5749e89a029762e1310051671d093555fc55a3)
    
    IJ-CR-25171
    
    GitOrigin-RevId: 68ca68d1a78943a9126c8731cdf09f1993e3ae02

---
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VuePiniaTest.kt
@@ -0,0 +1,14 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang
+
+import com.intellij.lang.javascript.modules.JSTempDirWithNodeInterpreterTest
+
+class VuePiniaTest : JSTempDirWithNodeInterpreterTest() {
+  override fun getBasePath(): String {
+    return vueRelativeTestDataPath() + "/pinia"
+  }
+
+  fun testDefineStoreInJSFile() {
+    doCopyDirectoryWithNpmInstallHighlightingTest(".vue")
+  }
+}
