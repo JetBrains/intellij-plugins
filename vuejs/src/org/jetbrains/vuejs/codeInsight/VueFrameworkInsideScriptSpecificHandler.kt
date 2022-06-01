08e67b254fc182dc8564b5446cb5a46240078989
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2022-06-01 16:52:45 +0200

    [vue] evaluate initializers accurately (WEB-54970)
    GitOrigin-RevId: 1a5749e89a029762e1310051671d093555fc55a3

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueFrameworkInsideScriptSpecificHandler.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueFrameworkInsideScriptSpecificHandler.kt
@@ -77,4 +77,5 @@ class VueFrameworkInsideScriptSpecificHandler : JSFrameworkSpecificHandler {
       }
   }
 
+  override fun useMoreAccurateEvaluation(context: PsiElement): Boolean = isInsideScript(context)
 }
---
+++ vuejs/vuejs-tests/testData/pinia/DefineStoreInJSFile/DefineStoreInJSFile.vue
@@ -0,0 +1,10 @@
+<script setup>
+import {useUserStore} from './user'
+
+const store = useUserStore()
+store.init()
+console.log(store.userInfo);
+
+store.<weak_warning descr="Unresolved function or method otherInit()">otherInit</weak_warning>();
+console.log(store.<weak_warning descr="Unresolved variable otherStateField">otherStateField</weak_warning>);
+</script>
