dd6c17e27194e8adafde5d3f31950fc5bf40f6c6
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2022-06-09 18:02:10 +0200

    [protoeditor] Handle bundled resources management in case of non-platform classloader
    GitOrigin-RevId: 38de62552a2a436ef49bf7042c72536e2e00ab36

--- protobuf/protoeditor-core/src/com/intellij/protobuf/ide/settings/BundledProtoResourcesMigrationPostStartupActivity.kt
+++ protobuf/protoeditor-core/src/com/intellij/protobuf/ide/settings/BundledProtoResourcesMigrationPostStartupActivity.kt
@@ -6,28 +6,29 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.io.exists
 import com.intellij.util.io.outputStream
 import java.io.IOException
 
 internal class BundledProtoResourcesMigrationPostStartupActivity : StartupActivity.Background {
   private val bundledProtoFiles = setOf(
-    "any.proto",
-    "api.proto",
-    "duration.proto",
-    "descriptor.proto",
-    "empty.proto",
-    "field_mask.proto",
-    "source_context.proto",
-    "struct.proto",
-    "timestamp.proto",
-    "type.proto",
-    "wrappers.proto"
+    "google/protobuf/any.proto",
+    "google/protobuf/api.proto",
+    "google/protobuf/duration.proto",
+    "google/protobuf/descriptor.proto",
+    "google/protobuf/empty.proto",
+    "google/protobuf/field_mask.proto",
+    "google/protobuf/source_context.proto",
+    "google/protobuf/struct.proto",
+    "google/protobuf/timestamp.proto",
+    "google/protobuf/type.proto",
+    "google/protobuf/wrappers.proto"
   )
 
   override fun runActivity(project: Project) {
     val baseExtractionPath = DefaultConfigurator.getExtractedProtoPath()
-    if (!bundledProtoFiles.all { baseExtractionPath.resolve("google/protobuf/$it").exists() }) {
+    if (!bundledProtoFiles.all { baseExtractionPath.resolve(it).exists() }) {
       extractBundledProtoToTempDirectory()
     }
 
@@ -36,28 +37,40 @@ internal class BundledProtoResourcesMigrationPostStartupActivity : StartupActivi
 
     if (oldSettings.contains(extractedProtoPath)) return
 
-    val oldStyleEntry = findBundledInJarProtoPath(oldSettings) ?: return
+    val oldStyleEntry = findBundledInJarProtoPath(oldSettings) ?: findBundledPathWithUnknownClassloader(oldSettings)
     val newSettings = oldSettings.filter { it != oldStyleEntry }.plus(extractedProtoPath)
     PbProjectSettings.getInstance(project).importPathEntries = newSettings
     PbProjectSettings.notifyUpdated(project)
   }
 
   private fun findBundledInJarProtoPath(oldSettings: List<PbProjectSettings.ImportPathEntry>): PbProjectSettings.ImportPathEntry? {
-    val bundledProtoFiles = this::class.java.classLoader.getResource("include")!!
-    val protoVfsUrl = runReadAction { VfsUtil.findFileByURL(bundledProtoFiles)?.url } ?: run {
-      thisLogger().warn("Bundled proto files are not found in plugin distributive")
+    val bundledDirectory = this::class.java.classLoader.getResource("include")
+    val protoDirectoryUrl = runReadAction { bundledDirectory?.let(VfsUtil::findFileByURL)?.url } ?: run {
+      thisLogger().warn("Bundled proto files directory is not found in plugin distributive")
       return null
     }
 
-    return oldSettings.firstOrNull { it.location == protoVfsUrl }
+    return oldSettings.firstOrNull { it.location == protoDirectoryUrl }
+  }
+
+  private fun findBundledPathWithUnknownClassloader(oldSettings: List<PbProjectSettings.ImportPathEntry>): PbProjectSettings.ImportPathEntry? {
+    val anyBundledProto = "include/${bundledProtoFiles.first()}"
+    val parentDirectoriesCount = anyBundledProto.split('/').size
+    val bundledProtoFile = this::class.java.classLoader.getResource("include/google/protobuf/any.proto")
+    val protoFile = runReadAction { bundledProtoFile?.let(VfsUtil::findFileByURL) } ?: run {
+      thisLogger().warn("Bundled proto files are not found in plugin distributive")
+      return null
+    }
+    val protoDirectoryUrl = generateSequence(protoFile, VirtualFile::getParent).take(parentDirectoriesCount).lastOrNull()?.url
+    return oldSettings.firstOrNull { it.location == protoDirectoryUrl }
   }
 
   private fun extractBundledProtoToTempDirectory() {
     try {
       for (bundledProtoFile in bundledProtoFiles) {
-        val extractedFilePath = DefaultConfigurator.getExtractedProtoPath().resolve("google/protobuf/$bundledProtoFile")
+        val extractedFilePath = DefaultConfigurator.getExtractedProtoPath().resolve(bundledProtoFile)
         if (!extractedFilePath.exists()) {
-          val resourceUrl = this::class.java.classLoader.getResource("include/google/protobuf/$bundledProtoFile")!!
+          val resourceUrl = this::class.java.classLoader.getResource("include/$bundledProtoFile")!!
           resourceUrl.openStream().use {
             FileUtil.copy(it, extractedFilePath.outputStream(false))
           }
