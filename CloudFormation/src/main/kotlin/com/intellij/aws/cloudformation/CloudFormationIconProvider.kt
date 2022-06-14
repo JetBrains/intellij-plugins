09d5da6c11ce23b14b87ab571447f2c8b5aff572
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2022-06-14 18:34:24 +0200

    [cloudformation] IDEA-295926 Plugin identifies any yaml containing AWSTemplateFormatVersion as CFN file
    GitOrigin-RevId: 6f8736d7b6e951f3ff3f88d6b30f455bfc8e34c2

--- CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationIconProvider.kt
+++ CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationIconProvider.kt
@@ -3,10 +3,13 @@ package com.intellij.aws.cloudformation
 import com.intellij.ide.IconProvider
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil
 import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValueProvider.Result
 import com.intellij.psi.util.CachedValuesManager
+import org.jetbrains.yaml.psi.YAMLDocument
 import org.jetbrains.yaml.psi.YAMLFile
+import org.jetbrains.yaml.psi.YAMLMapping
 import javax.swing.Icon
 
 internal class CloudFormationIconProvider : IconProvider() {
@@ -22,6 +25,25 @@ internal class CloudFormationIconProvider : IconProvider() {
 private fun isAwsCloudFormationFile(element: YAMLFile): Boolean {
   return CachedValuesManager.getManager(element.project).getCachedValue(element, CachedValueProvider {
     val text = element.text ?: element.virtualFile?.let { LoadTextUtil.loadText(it, 200) }
-    Result.create(CloudFormationFileTypeDetector.isYaml(text ?: ""), element)
+
+    if (!CloudFormationFileTypeDetector.isYaml(text ?: "")) {
+      return@CachedValueProvider Result.create(false, element)
+    }
+
+    Result.create(isCloudFormationYaml(element), element)
   })
+}
+
+internal fun isCloudFormationYaml(element: PsiFile): Boolean {
+  if (element !is YAMLFile) return false
+
+  for (child in element.children) {
+    if (child is YAMLDocument) {
+      val topLevelValue = child.topLevelValue
+      if (topLevelValue is YAMLMapping) {
+        return topLevelValue.keyValues.firstOrNull()?.keyText == CloudFormationSection.FormatVersion.id
+      }
+    }
+  }
+  return false
 }
