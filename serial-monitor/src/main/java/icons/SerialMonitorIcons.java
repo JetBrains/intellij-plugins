69de6112198f867a61596d335d161c403203d343
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2022-06-30 20:09:01 +0300

    [Serial Monitor Plugin] Fix deprecated API, new icons
    (cherry picked from commit d86350c8b3f6810c015dd512e078538983ccc944)
    
    GitOrigin-RevId: 5f521b06ea03e973c1ca7a627af48328789ef503

--- serial-monitor/src/main/java/icons/SerialMonitorIcons.java
+++ serial-monitor/src/main/java/icons/SerialMonitorIcons.java
@@ -13,8 +13,7 @@ public final class SerialMonitorIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, SerialMonitorIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon ConnectedSerial = load("icons/connected-serial.png", 0, 0);
-  /** 16x16 */ public static final @NotNull Icon DisconnectedSerial = load("icons/disconnected-serial.png", 0, 0);
-  /** 16x16 */ public static final @NotNull Icon HexSerial = load("icons/hex-serial.png", 0, 1);
-  /** 16x16 */ public static final @NotNull Icon OpenSerial = load("icons/open-serial.png", 0, 0);
+  /** 16x16 */ public static final @NotNull Icon ConnectedSerial = load("icons/connectedSerial.svg", -1407852838, 2);
+  /** 16x16 */ public static final @NotNull Icon DisconnectedSerial = load("icons/disconnectedSerial.svg", -1954232865, 2);
+  /** 16x16 */ public static final @NotNull Icon HexSerial = load("icons/hexSerial.svg", 1602757131, 2);
 }
