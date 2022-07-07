cd185817cc9b34419132aae000c4909320de4db2
Andrey Starovoyt <andrey.starovoyt@jetbrains.com>
2022-07-07 19:05:53 +0300

    typescript: handle vue extension /WEB-48458
    (cherry picked from commit 94cb32f857a88a025e3c3fc6fc6be2d33f513933)
    
    IJ-CR-26962
    
    GitOrigin-RevId: ed0d43742f2858f9a408d11e3b1e6c77d8cbbf10

---
+++ vuejs/vuejs-tests/testData/ts_imports/VueFileNameCompletion/VueFileNameCompletion_2.vue
@@ -0,0 +1,6 @@
+<template><div></div></template>
+<script lang="ts">
+  //no errors
+  export default {}
+
+</script>
---
+++ vuejs/vuejs-tests/testData/ts_imports/VueFileNameCompletion_after.vue
@@ -0,0 +1,7 @@
+<template><div></div></template>
+<script lang="ts">
+  //no errors
+  import {  } from './VueFileNameCompletion_2.vue';
+
+
+</script>
