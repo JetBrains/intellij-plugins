3b9c974262d89bd18e8298b7791cd76fc8a88dae
Vladimir Panimaskin <Vladimir.Panimaskin@jetbrains.com>
2022-08-03 21:51:38 +0300

    WEB-56658 Pinia: resolve store methods from the script
    (cherry picked from commit 4a87c7e9dabb261959de4183f843e074451ffd56)
    
    IJ-CR-94232
    
    GitOrigin-RevId: 80286e214b59f4f3b6b482adaf9d39ae5a8ed8ea

--- vuejs/vuejs-tests/testData/pinia/DefineStoreInJSFile/DefineStoreInJSFile.vue
+++ vuejs/vuejs-tests/testData/pinia/DefineStoreInJSFile/DefineStoreInJSFile.vue
@@ -8,3 +8,12 @@ console.log(store.userInfo);
 store.<weak_warning descr="Unresolved function or method otherInit()">otherInit</weak_warning>();
 console.log(store.<weak_warning descr="Unresolved variable otherStateField">otherStateField</weak_warning>);
 </script>
+
+<template>
+  <p>
+    <button @click="store.init()">
+      Click me!
+    </button>
+    User info: {{ store.userInfo }}
+  </p>
+</template>
