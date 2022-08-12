c7329c17b241408443e92471d17fc159f3ea7995
Alexander Bubenchikov <alexander.bubenchikov@jetbrains.com>
2022-08-12 10:57:45 +0200

    [maven] IDEA-297661 - completely remove maven2 code out of intellij repository
    GitOrigin-RevId: 5bb3b171df925b9b7f1d9b1a38f4ee1b8e8ec23d

--- flex/flex-tests/testSrc/com/intellij/flex/maven/NonJarDependenciesImportingTest.java
+++ flex/flex-tests/testSrc/com/intellij/flex/maven/NonJarDependenciesImportingTest.java
@@ -1,12 +1,10 @@
 package com.intellij.flex.maven;
 
 import com.intellij.lang.javascript.flex.sdk.FlexmojosSdkType;
+import com.intellij.maven.testFramework.MavenImportingTestCase;
 import com.intellij.openapi.application.WriteAction;
 import com.intellij.openapi.projectRoots.ProjectJdkTable;
 import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.maven.testFramework.MavenImportingTestCase;
-import org.jetbrains.idea.maven.project.MavenWorkspaceSettingsComponent;
-import org.jetbrains.idea.maven.server.MavenServerManager;
 
 import java.io.File;
 
@@ -25,47 +23,6 @@ public class NonJarDependenciesImportingTest extends MavenImportingTestCase {
     finally {
       super.tearDown();
     }
-  }
-
-  public void testArtifactTypeProvidedByExtensionPlugin() {
-    // this test is basically the same as com.intellij.flex.maven.Flexmojos3ImporterTest.testConfiguringResourceBundleDependency
-    // This test ensures that we download all necessary extension plugins.
-    MavenWorkspaceSettingsComponent.getInstance(myProject).getSettings().generalSettings.setMavenHome(MavenServerManager.BUNDLED_MAVEN_2);
-    importProject("<groupId>test</groupId>" +
-                  "<artifactId>project</artifactId>" +
-                  "<version>1</version>" +
-                  "<packaging>swf</packaging>" +
-
-                  "<dependencies>" +
-                  "  <dependency>" +
-                  "    <groupId>com.adobe.flex.framework</groupId>" +
-                  "    <artifactId>framework</artifactId>" +
-                  "    <version>3.2.0.3958</version>" +
-                  "    <type>resource-bundle</type>" +
-                  "    <classifier>en_US</classifier>" +
-                  "  </dependency>" +
-                  "</dependencies>" +
-
-                  "<build>" +
-                  "  <plugins>" +
-                  "    <plugin>" +
-                  "      <groupId>org.sonatype.flexmojos</groupId>" +
-                  "      <artifactId>flexmojos-maven-plugin</artifactId>" +
-                  "      <version>3.5.0</version>" +
-                  "      <extensions>true</extensions>" +
-                  "    </plugin>" +
-                  "  </plugins>" +
-                  "</build>");
-
-    // flex plugin handles 'resource-bundle' dependencies in a special way.
-    //
-    assertModuleLibDep("project", "Maven: com.adobe.flex.framework:framework:resource-bundle:en_US:3.2.0.3958",
-                       "jar://" + getRepositoryPath() +
-                       "/com/adobe/flex/framework/framework/3.2.0.3958/framework-3.2.0.3958-en_US.rb.swc!/",
-                       "jar://" + getRepositoryPath() +
-                       "/com/adobe/flex/framework/framework/3.2.0.3958/framework-3.2.0.3958-sources.jar!/",
-                       "jar://" + getRepositoryPath() +
-                       "/com/adobe/flex/framework/framework/3.2.0.3958/framework-3.2.0.3958-asdoc.zip!/");
   }
 
   public void testUpdateRootEntriesWithActualPathForNonJarDependencies() {
