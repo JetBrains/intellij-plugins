962ba2e17df81e58e8cd2bcb086b4dd28b60f157
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2022-08-24 16:38:08 +0200

    [angular] convert Angular2SpecificHandlersFactory to Kotlin
    GitOrigin-RevId: 08b6adcd8e1808154a0f498325e151ad51ae896f

--- AngularJS/src/org/angular2/codeInsight/Angular2SpecificHandlersFactory.kt
+++ AngularJS/src/org/angular2/codeInsight/Angular2SpecificHandlersFactory.kt
@@ -1,67 +1,55 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.angular2.codeInsight;
+package org.angular2.codeInsight
 
-import com.intellij.lang.javascript.DialectDetector;
-import com.intellij.lang.javascript.JavaScriptSpecificHandlersFactory;
-import com.intellij.lang.javascript.ecmascript6.TypeScriptQualifiedItemProcessor;
-import com.intellij.lang.javascript.findUsages.JSDialectSpecificReadWriteAccessDetector;
-import com.intellij.lang.javascript.psi.ecmal4.JSClass;
-import com.intellij.lang.javascript.psi.impl.JSReferenceExpressionImpl;
-import com.intellij.lang.javascript.psi.resolve.*;
-import com.intellij.lang.typescript.resolve.TypeScriptTypeHelper;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.impl.source.resolve.ResolveCache;
-import org.angular2.codeInsight.refs.Angular2ReferenceExpressionResolver;
-import org.angular2.entities.Angular2ComponentLocator;
-import org.angular2.findUsages.Angular2ReadWriteAccessDetector;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.intellij.lang.javascript.DialectDetector
+import com.intellij.lang.javascript.JavaScriptSpecificHandlersFactory
+import com.intellij.lang.javascript.ecmascript6.TypeScriptQualifiedItemProcessor
+import com.intellij.lang.javascript.findUsages.JSDialectSpecificReadWriteAccessDetector
+import com.intellij.lang.javascript.psi.ecmal4.JSClass
+import com.intellij.lang.javascript.psi.impl.JSReferenceExpressionImpl
+import com.intellij.lang.javascript.psi.resolve.*
+import com.intellij.lang.typescript.resolve.TypeScriptTypeHelper
+import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.resolve.ResolveCache.PolyVariantResolver
+import org.angular2.codeInsight.refs.Angular2ReferenceExpressionResolver
+import org.angular2.entities.Angular2ComponentLocator
+import org.angular2.findUsages.Angular2ReadWriteAccessDetector
 
-public class Angular2SpecificHandlersFactory extends JavaScriptSpecificHandlersFactory {
-
-  @Override
-  public @NotNull ResolveCache.PolyVariantResolver<JSReferenceExpressionImpl> createReferenceExpressionResolver(
-    JSReferenceExpressionImpl referenceExpression, boolean ignorePerformanceLimits) {
-    return new Angular2ReferenceExpressionResolver(referenceExpression, ignorePerformanceLimits);
+class Angular2SpecificHandlersFactory : JavaScriptSpecificHandlersFactory() {
+  override fun createReferenceExpressionResolver(
+    referenceExpression: JSReferenceExpressionImpl, ignorePerformanceLimits: Boolean): PolyVariantResolver<JSReferenceExpressionImpl> {
+    return Angular2ReferenceExpressionResolver(referenceExpression, ignorePerformanceLimits)
   }
 
-  @Override
-  public <T extends ResultSink> QualifiedItemProcessor<T> createQualifiedItemProcessor(@NotNull T sink, @NotNull PsiElement place) {
-    JSClass clazz = Angular2ComponentLocator.findComponentClass(place);
-    if (clazz != null && DialectDetector.isTypeScript(clazz)) {
-      return new TypeScriptQualifiedItemProcessor<>(sink, place.getContainingFile());
+  override fun <T : ResultSink> createQualifiedItemProcessor(sink: T, place: PsiElement): QualifiedItemProcessor<T> {
+    val clazz: JSClass? = Angular2ComponentLocator.findComponentClass(place)
+    return if (clazz != null && DialectDetector.isTypeScript(clazz)) {
+      TypeScriptQualifiedItemProcessor(sink, place.containingFile)
     }
-
-    return super.createQualifiedItemProcessor(sink, place);
+    else super.createQualifiedItemProcessor(sink, place)
   }
 
-  @Override
-  public @NotNull JSImportHandler getImportHandler() {
-    return new Angular2ImportHandler();
+  override fun getImportHandler(): JSImportHandler {
+    return Angular2ImportHandler()
   }
 
-  @Override
-  public @NotNull JSTypeEvaluator newTypeEvaluator(@NotNull JSEvaluateContext context) {
-    return new Angular2TypeEvaluator(context);
+  override fun newTypeEvaluator(context: JSEvaluateContext): JSTypeEvaluator {
+    return Angular2TypeEvaluator(context)
   }
 
-  @Override
-  public @NotNull AccessibilityProcessingHandler createAccessibilityProcessingHandler(@Nullable PsiElement place, boolean skipNsResolving) {
-    return new Angular2AccessibilityProcessingHandler(place);
+  override fun createAccessibilityProcessingHandler(place: PsiElement?, skipNsResolving: Boolean): AccessibilityProcessingHandler {
+    return Angular2AccessibilityProcessingHandler(place)
   }
 
-  @Override
-  public @NotNull JSDialectSpecificReadWriteAccessDetector getReadWriteAccessDetector() {
-    return Angular2ReadWriteAccessDetector.INSTANCE;
+  override fun getReadWriteAccessDetector(): JSDialectSpecificReadWriteAccessDetector {
+    return Angular2ReadWriteAccessDetector.INSTANCE
   }
 
-  @Override
-  public @NotNull JSTypeGuardEvaluator getTypeGuardEvaluator() {
-    return Angular2TypeGuardEvaluator.INSTANCE;
+  override fun getTypeGuardEvaluator(): JSTypeGuardEvaluator {
+    return Angular2TypeGuardEvaluator.INSTANCE
   }
 
-  @Override
-  public @NotNull JSTypeHelper getTypeHelper() {
-    return TypeScriptTypeHelper.getInstance();
+  override fun getTypeHelper(): JSTypeHelper {
+    return TypeScriptTypeHelper.getInstance()
   }
 }
