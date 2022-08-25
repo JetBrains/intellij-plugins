68a09fce0731ead52e2d4080635d0bf8f8a36822
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2022-08-25 12:00:40 +0200

    [prettier] make read action non-blocking (FL-13401)
    GitOrigin-RevId: e2e9bebce0b792d223e062ff8ef101dd4daba4dc

--- prettierJS/src/com/intellij/prettierjs/PrettierCompatibleCodeStyleInstaller.java
+++ prettierJS/src/com/intellij/prettierjs/PrettierCompatibleCodeStyleInstaller.java
@@ -1,7 +1,7 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.prettierjs;
 
-import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupManager;
@@ -24,7 +24,10 @@ final class PrettierCompatibleCodeStyleInstaller implements DirectoryProjectConf
       return;
     }
     StartupManager.getInstance(project).runAfterOpened(() -> {
-      ApplicationManager.getApplication().runReadAction(() -> installCodeStyle(project));
+      ReadAction.nonBlocking(() -> {
+        installCodeStyle(project);
+        return null;
+      }).executeSynchronously();
     });
   }
 
