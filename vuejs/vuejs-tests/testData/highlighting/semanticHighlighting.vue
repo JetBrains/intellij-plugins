61ae246180ea5414f6c04c5f0bf6d497d4741f92
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2022-10-06 20:42:00 +0200

    [js] replace TextAttributeKeyInfo with JSHighlightDescriptor
    GitOrigin-RevId: 081ec40b87bb51ec1244e6b1835e55273adb7d5d

--- vuejs/vuejs-tests/testData/highlighting/semanticHighlighting.vue
+++ vuejs/vuejs-tests/testData/highlighting/semanticHighlighting.vue
@@ -4,6 +4,6 @@ export class <info descr="exported class">SpaceInterface</info> {
 }
 var <info descr="static field">i</info>:<info descr="exported class">SpaceInterface</info>;
 }
-import <info descr="exported class">SpaceInterface</info> = <info descr="moduleName">space</info>.<info descr="exported class">SpaceInterface</info>;
+import <info descr="exported class">SpaceInterface</info> = <info descr="module name">space</info>.<info descr="exported class">SpaceInterface</info>;
 var <info descr="local variable">i</info>:<info descr="exported class">SpaceInterface</info>;
 </script>
