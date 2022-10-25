6458a23c9b3ffb5c5d82ac61cdc1575071e2451f
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2022-10-26 02:52:31 +0300

    [webpack] rename package to com.intellij.webpack
    (cherry picked from commit 3dea8817aa5b5e8431ac2be382a0f04f4491d5b6)
    
    IJ-CR-97233
    
    GitOrigin-RevId: b77f695ee2deb73cce97b931834d14c395a65aed

--- vuejs/src/org/jetbrains/vuejs/libraries/nuxt/model/NuxtComponentProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/libraries/nuxt/model/NuxtComponentProvider.kt
@@ -1,8 +1,8 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.libraries.nuxt.model
 
-import com.intellij.lang.javascript.buildTools.webpack.WebpackConfigManager
-import com.intellij.lang.javascript.buildTools.webpack.WebpackReferenceContributor
+import com.intellij.webpack.WebpackConfigManager
+import com.intellij.webpack.WebpackReferenceContributor
 import com.intellij.lang.javascript.library.JSLibraryUtil
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
