d41ec5ff314f920f5b377818e20b514245acec2a
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2022-11-21 14:45:47 +0100

    [angular] fix tsconfig custom data inheritance (WEB-58248)
    GitOrigin-RevId: 367167cc0d27ba92c4cf678e1dc6ab52b5ddbd85

--- AngularJS/src/org/angular2/codeInsight/Angular2TypeScriptConfigCustomizer.kt
+++ AngularJS/src/org/angular2/codeInsight/Angular2TypeScriptConfigCustomizer.kt
@@ -4,32 +4,35 @@ package org.angular2.codeInsight
 import com.intellij.json.psi.JsonObject
 import com.intellij.lang.typescript.tsconfig.TypeScriptConfig
 import com.intellij.lang.typescript.tsconfig.TypeScriptConfigCustomizer
-import com.intellij.lang.typescript.tsconfig.TypeScriptConfigParsingUtil.getBooleanValue
 import com.intellij.lang.typescript.tsconfig.TypeScriptConfigParsingUtil.getObjectOfProperty
+import com.intellij.openapi.util.Key
 
-data class AngularCompilerOptions(val strictTemplates: Boolean, val strictNullInputTypes: Boolean)
-
-class Angular2TypeScriptConfigCustomizer : TypeScriptConfigCustomizer<AngularCompilerOptions?> {
+class Angular2TypeScriptConfigCustomizer : TypeScriptConfigCustomizer {
   companion object {
     @JvmStatic
-    fun getAngularCompilerOptions(fullConfig: TypeScriptConfig?): AngularCompilerOptions? {
-      return fullConfig?.getCustomOption(CUSTOM_KEY) as AngularCompilerOptions?
+    fun getStrictTemplates(fullConfig: TypeScriptConfig?): Boolean {
+      return fullConfig?.getCustomOption(STRICT_TEMPLATES) ?: false
     }
 
-    private const val CUSTOM_KEY = "Angular2TypeScriptConfigCustomizer"
+    val STRICT_TEMPLATES = Key.create<Boolean>("angularCompilerOptions.strictTemplates")
+    val STRICT_NULL_INPUT_TYPES = Key.create<Boolean>("angularCompilerOptions.strictNullInputTypes")
   }
 
-  override val customConfigKey get() = CUSTOM_KEY
-
-  override fun parseJsonObject(jsonObject: JsonObject): AngularCompilerOptions? {
+  override fun parseJsonObject(jsonObject: JsonObject): Map<Key<*>, Any>? {
     val compilerOptions = getObjectOfProperty(jsonObject, "angularCompilerOptions")
 
     compilerOptions ?: return null
 
-    val strictTemplates = getBooleanValue(compilerOptions, "strictTemplates", false)
-    val strictNullInputTypes = getBooleanValue(compilerOptions, "strictNullInputTypes", false)
+    val result = mutableMapOf<Key<*>, Any>()
+    getBooleanValue(compilerOptions, "strictTemplates")?.let { result.put(STRICT_TEMPLATES, it) }
+    getBooleanValue(compilerOptions, "strictNullInputTypes")?.let { result.put(STRICT_NULL_INPUT_TYPES, it) }
 
-    return AngularCompilerOptions(strictTemplates, strictNullInputTypes)
+    return result
   }
 
+  private fun getBooleanValue(jsonObject: JsonObject, name: String): Boolean? {
+    val compileOnSave = jsonObject.findProperty(name)
+    val compileOnSaveValue = compileOnSave?.value
+    return if (compileOnSaveValue == null) null else java.lang.Boolean.parseBoolean(compileOnSaveValue.text)
+  }
 }
