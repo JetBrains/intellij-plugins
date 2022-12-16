acdae57a028df32f41f54df1b133915e96e26ffc
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2022-12-16 04:53:43 +0200

    [prisma] fix gradle build script
    GitOrigin-RevId: 57c01bbf28dc0f141a6c19236a72b8be9c1549eb

--- prisma/build.gradle.kts
+++ prisma/build.gradle.kts
@@ -1,119 +1,89 @@
-import org.jetbrains.changelog.markdownToHTML
-
 apply(from = "../contrib-configuration/common.gradle.kts")
 
 fun properties(key: String) = project.findProperty(key).toString()
 
 plugins {
-    id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.10.0"
-    id("org.jetbrains.changelog") version "1.3.1"
+  id("java")
+  id("org.jetbrains.kotlin.jvm") version "1.7.10"
+  id("org.jetbrains.intellij") version "1.10.0"
+  id("org.jetbrains.changelog") version "1.3.1"
 }
 
 group = properties("pluginGroup")
 version = properties("pluginVersion")
 
 idea {
-    module {
-        generatedSourceDirs.add(file("src/main/gen"))
-    }
+  module {
+    generatedSourceDirs.add(file("gen"))
+  }
 }
 
 java {
-    sourceSets {
-        main {
-            java.srcDir("src/main/gen")
-        }
+  sourceSets {
+    main {
+      java.srcDir("gen")
+      resources.srcDir("resources")
     }
+
+    test {
+      java.srcDirs("test")
+      resources.srcDir("test/testData")
+    }
+  }
 }
 
 kotlin {
-    jvmToolchain {
-        languageVersion.set(JavaLanguageVersion.of(properties("jvmVersion")))
+  sourceSets {
+    main {
+      kotlin.srcDir("src")
+      resources.srcDir("resources")
     }
+
+    test {
+      kotlin.srcDir("test")
+      resources.srcDir("test/testData")
+    }
+  }
+
+  jvmToolchain {
+    languageVersion.set(JavaLanguageVersion.of(properties("jvmVersion")))
+  }
 }
 
 intellij {
-    pluginName.set(properties("pluginName"))
-    version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
-
-    plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
-}
-
-changelog {
-    version.set(properties("pluginVersion"))
-    groups.set(emptyList())
+  pluginName.set(properties("pluginName"))
+  version.set(properties("platformVersion"))
+  type.set(properties("platformType"))
+  plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions {
-            jvmTarget = properties("jvmVersion")
-        }
+  compileKotlin {
+    kotlinOptions {
+      jvmTarget = properties("jvmVersion")
     }
+  }
 
-    prepareSandbox {
-        doLast {
-            copy {
-                from("${project.projectDir}/language-server/dist")
-                into("${destinationDir.path}/${properties("pluginName")}/")
-            }
-        }
+  prepareSandbox {
+    doLast {
+      copy {
+        from("${project.projectDir}/gen/language-server")
+        into("${destinationDir.path}/${properties("pluginName")}/language-server")
+      }
     }
+  }
 
-    wrapper {
-        gradleVersion = properties("gradleVersion")
-    }
+  wrapper {
+    gradleVersion = properties("gradleVersion")
+  }
 
-    patchPluginXml {
-        version.set(properties("pluginVersion"))
-        sinceBuild.set(properties("pluginSinceBuild"))
-        untilBuild.set(properties("pluginUntilBuild"))
+  runPluginVerifier {
+    ideVersions.set(properties("pluginVerifierIdeVersions").split(',').map(String::trim).filter(String::isNotEmpty))
+  }
 
-        // Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
-        pluginDescription.set(
-            projectDir.resolve("README.md").readText().lines().run {
-                val start = "<!-- Plugin description -->"
-                val end = "<!-- Plugin description end -->"
-
-                if (!containsAll(listOf(start, end))) {
-                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-                }
-                subList(indexOf(start) + 1, indexOf(end))
-            }.joinToString("\n").run { markdownToHTML(this) }
-        )
-
-        // Get the latest available change notes from the changelog file
-        changeNotes.set(provider {
-            changelog.run {
-                getOrNull(properties("pluginVersion")) ?: getLatest()
-            }.toHTML()
-        })
-    }
-
-    // Configure UI tests plugin
-    // Read more: https://github.com/JetBrains/intellij-ui-test-robot
-    runIdeForUiTests {
-        systemProperty("robot-server.port", "8082")
-        systemProperty("ide.mac.message.dialogs.as.sheets", "false")
-        systemProperty("jb.privacy.policy.text", "<!--999.999-->")
-        systemProperty("jb.consents.confirmation.enabled", "false")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        dependsOn("patchChangelog")
-        token.set(System.getenv("PUBLISH_TOKEN"))
-        // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
-        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
-        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
-    }
+  patchPluginXml {
+    version.set(properties("pluginVersion"))
+    sinceBuild.set(properties("pluginSinceBuild"))
+    untilBuild.set(properties("pluginUntilBuild"))
+  }
 }
