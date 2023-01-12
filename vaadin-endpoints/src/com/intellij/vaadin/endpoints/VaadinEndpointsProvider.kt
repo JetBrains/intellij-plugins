5ecf7278b50d9cfd61485ecaf9265718e15a7c10
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2023-01-12 17:56:55 +0100

    [microservices] Move Vaadin endpoints to contrib as example of microservices API usage
    GitOrigin-RevId: e3441dc9936dfd4f79873dc3aad84406d05d0e6c

---
+++ vaadin-endpoints/src/com/intellij/vaadin/endpoints/VaadinEndpointsProvider.kt
@@ -0,0 +1,67 @@
+package com.intellij.vaadin.endpoints
+
+import com.intellij.microservices.endpoints.*
+import com.intellij.microservices.endpoints.EndpointsProvider.Status
+import com.intellij.microservices.endpoints.presentation.HttpUrlPresentation
+import com.intellij.microservices.url.UrlPath
+import com.intellij.navigation.ItemPresentation
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.ModificationTracker
+import com.intellij.psi.PsiElement
+import com.intellij.uast.UastModificationTracker
+
+internal class VaadinEndpointsProvider : EndpointsProvider<VaadinRoute, VaadinRoute> {
+  override val endpointType: EndpointType = HTTP_SERVER_TYPE
+
+  override val presentation: FrameworkPresentation =
+      FrameworkPresentation("Vaadin", "Vaadin Flow", VaadinEndpointsIcons.VaadinRoute)
+
+  override fun getStatus(project: Project): Status {
+    if (hasVaadinFlow(project)) return Status.HAS_ENDPOINTS
+
+    return Status.UNAVAILABLE
+  }
+
+  override fun getModificationTracker(project: Project): ModificationTracker {
+    return UastModificationTracker.getInstance(project)
+  }
+
+  override fun getEndpointGroups(project: Project, filter: EndpointsFilter): Iterable<VaadinRoute> {
+    if (filter !is ModuleEndpointsFilter) return emptyList()
+    if (!hasVaadinFlow(filter.module)) return emptyList()
+
+    return findVaadinRoutes(project, filter.searchScope)
+  }
+
+  override fun getEndpoints(group: VaadinRoute): Iterable<VaadinRoute> {
+    return listOf(group)
+  }
+
+  override fun isValidEndpoint(group: VaadinRoute, endpoint: VaadinRoute): Boolean {
+    return group.isValid()
+  }
+
+  override fun getEndpointPresentation(group: VaadinRoute, endpoint: VaadinRoute): ItemPresentation {
+    return HttpUrlPresentation(normalizeUrl(group.urlMapping), group.locationString, VaadinEndpointsIcons.VaadinRoute)
+  }
+
+  private fun normalizeUrl(urlMapping: String): String {
+    val urlString = run {
+      if (urlMapping.isBlank()) return@run "/"
+      if (!urlMapping.startsWith("/")) return@run "/$urlMapping"
+      return@run urlMapping
+    }
+
+    return parseVaadinUrlMapping(urlString).getPresentation(VaadinUrlRenderer)
+  }
+
+  override fun getDocumentationElement(group: VaadinRoute, endpoint: VaadinRoute): PsiElement? {
+    return endpoint.anchor.retrieve()
+  }
+}
+
+private object VaadinUrlRenderer : UrlPath.PathSegmentRenderer {
+  override fun visitVariable(variable: UrlPath.PathSegment.Variable): String {
+    return "{${variable.variableName}}"
+  }
+}
---
+++ vaadin-endpoints/src/com/intellij/vaadin/endpoints/VaadinUrlResolver.kt
@@ -0,0 +1,92 @@
+package com.intellij.vaadin.endpoints
+
+import com.intellij.microservices.jvm.cache.ModuleCacheValueHolder
+import com.intellij.microservices.jvm.cache.SourceLibSearchProvider
+import com.intellij.microservices.jvm.cache.UastCachedSearchUtils.sequenceWithCache
+import com.intellij.microservices.url.*
+import com.intellij.microservices.url.UrlPath.PathSegment
+import com.intellij.microservices.url.references.UrlPksParser
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiAnchor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.PartiallyKnownString
+import com.intellij.psi.util.SplitEscaper
+import javax.swing.Icon
+
+internal class VaadinUrlResolverFactory : UrlResolverFactory {
+  override fun forProject(project: Project): UrlResolver? {
+    return if (hasVaadinFlow(project)) VaadinUrlResolver(project) else null
+  }
+}
+
+internal class VaadinUrlResolver(private val project: Project) : UrlResolver {
+  override val supportedSchemes: List<String>
+    get() = HTTP_SCHEMES
+
+  override fun getVariants(): Iterable<UrlTargetInfo> {
+    val modules = ModuleManager.getInstance(project).modules
+
+    return modules.asSequence()
+        .flatMap(::getVariants)
+        .map(::VaadinUrlTargetInfo)
+        .asIterable()
+  }
+
+  override fun resolve(request: UrlResolveRequest): Iterable<UrlTargetInfo> {
+    if (request.method != HttpMethods.GET) return emptyList()
+
+    return getVariants().asSequence()
+        .filter { it.path.isCompatibleWith(request.path) }
+        .asIterable()
+  }
+}
+
+internal val VAADIN_ROUTES_SEARCH: SourceLibSearchProvider<List<VaadinRoute>, Module> =
+    SourceLibSearchProvider("VAADIN_ROUTES") { p, _, scope ->
+      findVaadinRoutes(p, scope).toList()
+    }
+
+private fun getVariants(module: Module): Sequence<VaadinRoute> {
+  if (!hasVaadinFlow(module)) return emptySequence()
+
+  return sequenceWithCache(ModuleCacheValueHolder(module), VAADIN_ROUTES_SEARCH)
+}
+
+private class VaadinUrlTargetInfo(route: VaadinRoute) : UrlTargetInfo {
+  private val anchor: PsiAnchor = route.anchor
+
+  override val authorities: List<Authority>
+    get() = emptyList()
+
+  override val path: UrlPath = parseVaadinUrlMapping(route.urlMapping)
+
+  override val icon: Icon
+    get() = VaadinEndpointsIcons.VaadinRoute
+
+  override val schemes: List<String>
+    get() = HTTP_SCHEMES
+
+  override fun resolveToPsiElement(): PsiElement? = anchor.retrieve()
+}
+
+internal val vaadinUrlPksParser: UrlPksParser = UrlPksParser(
+    splitEscaper = { _, _ -> SplitEscaper.AcceptAll },
+    customPathSegmentExtractor = { part ->
+      if (part.startsWith(":")) {
+        val varName = part.removePrefix(":")
+            .substringBefore("?")
+            .substringBefore("(")
+        PathSegment.Variable(varName)
+      } else {
+        PathSegment.Exact(part)
+      }
+    }
+)
+
+internal fun parseVaadinUrlMapping(urlMapping: String): UrlPath {
+  return vaadinUrlPksParser
+      .parseUrlPath(PartiallyKnownString(urlMapping))
+      .urlPath
+}
