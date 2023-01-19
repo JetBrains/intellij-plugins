d0086f47d2126b4d17a4a71450ce41de07a2c344
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-01-19 15:32:15 +0100

    Make Dart hierarchy tests reliable.
    GitOrigin-RevId: 0a7e2c43084e3179faa2e9de54f8bf17ae2a1f81

--- Dart/testSrc/com/jetbrains/dart/analysisServer/DartHierarchyTestBase.java
+++ Dart/testSrc/com/jetbrains/dart/analysisServer/DartHierarchyTestBase.java
@@ -4,10 +4,9 @@ package com.jetbrains.dart.analysisServer;
 import com.intellij.ide.hierarchy.HierarchyTreeStructure;
 import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.util.io.FileUtil;
-import com.intellij.psi.PsiFile;
 import com.intellij.testFramework.codeInsight.hierarchy.HierarchyViewTestFixture;
 import com.intellij.testFramework.fixtures.CodeInsightFixtureTestCase;
-import com.jetbrains.lang.dart.analyzer.DartAnalysisServerService;
+import com.intellij.testFramework.fixtures.impl.CodeInsightTestFixtureImpl;
 import com.jetbrains.lang.dart.util.DartTestUtils;
 
 import java.io.File;
@@ -18,29 +17,23 @@ public abstract class DartHierarchyTestBase extends CodeInsightFixtureTestCase {
   @Override
   public void setUp() throws Exception {
     super.setUp();
+    DartTestUtils.configureDartSdk(myModule, myFixture.getTestRootDisposable(), true);
     myFixture.setTestDataPath(DartTestUtils.BASE_TEST_DATA_PATH + getBasePath());
-    DartTestUtils.configureDartSdk(myModule, getTestRootDisposable(), true);
-    DartAnalysisServerService.getInstance(getProject()).serverReadyForRequest();
+    ((CodeInsightTestFixtureImpl)myFixture).canChangeDocumentDuringHighlighting(true);
   }
 
   protected void doHierarchyTest(final Computable<? extends HierarchyTreeStructure> treeStructureComputable,
                                  final String... fileNames) throws IOException {
-    PsiFile[] files = myFixture.configureByFiles(fileNames);
-    ensureNavigationInfoAvailable(files);
+    myFixture.configureByFiles(fileNames);
+    myFixture.doHighlighting(); // warm up
 
     final String verificationFilePath = myFixture.getTestDataPath() + "/" + getTestName(false) + "_verification.xml";
     HierarchyTreeStructure structure = treeStructureComputable.compute();
     File file = new File(verificationFilePath);
     if (!file.exists()) {
-      FileUtil.writeToFile(file, HierarchyViewTestFixture.dump(structure, null, null,0));
+      FileUtil.writeToFile(file, HierarchyViewTestFixture.dump(structure, null, null, 0));
       throw new IllegalStateException("File: " + file.getPath() + " doesn't exist. A new file created.");
     }
     HierarchyViewTestFixture.doHierarchyTest(structure, file);
-  }
-
-  private void ensureNavigationInfoAvailable(PsiFile[] files) {
-    for (PsiFile file : files) {
-      DartAnalysisServerService.getInstance(getProject()).analysis_getNavigation(file.getVirtualFile(), 0, file.getTextLength());
-    }
   }
 }
