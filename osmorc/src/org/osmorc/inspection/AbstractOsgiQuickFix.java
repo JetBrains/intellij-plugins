6dc29580ea498c7de111123ddf1c739e407a37c9
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2023-02-01 11:21:19 +0100

    [osgi] "unregistered activator" quick fixes preview
    GitOrigin-RevId: 78504288c176f6674a2b0fb2bd2ddaa66001ad54

--- osmorc/src/org/osmorc/inspection/AbstractOsgiQuickFix.java
+++ osmorc/src/org/osmorc/inspection/AbstractOsgiQuickFix.java
@@ -1,40 +1,21 @@
-/*
- * Copyright 2000-2017 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.osmorc.inspection;
 
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.notification.NotificationType;
-import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.openapi.roots.ModuleRootManager;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
 import com.intellij.refactoring.util.CommonRefactoringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.lang.manifest.psi.ManifestFile;
 import org.osmorc.facet.OsmorcFacet;
-import org.osmorc.facet.OsmorcFacetConfiguration;
 import org.osmorc.i18n.OsmorcBundle;
 
 public abstract class AbstractOsgiQuickFix implements LocalQuickFix {
-  @NotNull
   @Override
-  public final String getFamilyName() {
+  public final @NotNull String getFamilyName() {
     return OsmorcBundle.message("inspection.group");
   }
 
@@ -43,26 +24,35 @@ public abstract class AbstractOsgiQuickFix implements LocalQuickFix {
     return false;
   }
 
-  @Nullable
-  protected ManifestFile getVerifiedManifestFile(@NotNull PsiElement element) {
-    Module module = ModuleUtilCore.findModuleForPsiElement(element);
+  protected @Nullable ManifestFile getVerifiedManifestFile(@NotNull PsiElement element) {
+    var manifestFile = getManifestFile(element);
+    if (manifestFile != null && CommonRefactoringUtil.checkReadOnlyStatus(manifestFile)) {
+      return manifestFile;
+    }
+    else {
+      OsmorcBundle.notification(getFamilyName(), OsmorcBundle.message("inspection.fix.no.manifest"), NotificationType.WARNING).notify(element.getProject());
+      return null;
+    }
+  }
+
+  protected @Nullable ManifestFile getManifestFile(@NotNull PsiElement element) {
+    var module = ModuleUtilCore.findModuleForPsiElement(element);
     assert module != null : element;
 
-    OsmorcFacet facet = OsmorcFacet.getInstance(module);
+    var facet = OsmorcFacet.getInstance(module);
     if (facet != null) {
-      OsmorcFacetConfiguration configuration = facet.getConfiguration();
-      for (VirtualFile root : ModuleRootManager.getInstance(module).getContentRoots()) {
-        VirtualFile file = root.findFileByRelativePath(configuration.getManifestLocation());
+      var configuration = facet.getConfiguration();
+      for (var root : ModuleRootManager.getInstance(module).getContentRoots()) {
+        var file = root.findFileByRelativePath(configuration.getManifestLocation());
         if (file != null) {
-          PsiFile psiFile = element.getManager().findFile(file);
-          if (psiFile instanceof ManifestFile && CommonRefactoringUtil.checkReadOnlyStatus(psiFile)) {
+          var psiFile = element.getManager().findFile(file);
+          if (psiFile instanceof ManifestFile) {
             return (ManifestFile)psiFile;
           }
         }
       }
     }
 
-    OsmorcBundle.notification(getFamilyName(), OsmorcBundle.message("inspection.fix.no.manifest"), NotificationType.WARNING).notify(element.getProject());
     return null;
   }
 }
