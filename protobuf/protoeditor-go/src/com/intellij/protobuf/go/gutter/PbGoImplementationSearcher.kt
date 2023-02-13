7d2103f5353d71505d88b845a3fa75dabb4ff2c8
Nikita Katkov <Nikita.Katkov@jetbrains.com>
2023-02-13 15:20:37 +0100

    [protoeditor, grpc] IDEA-294183 Go -> pb message navigation
    GitOrigin-RevId: 343796cfe76500871b0fd2cd404694de6fb3fd03

--- protobuf/protoeditor-go/src/com/intellij/protobuf/go/gutter/PbGoImplementationSearcher.kt
+++ protobuf/protoeditor-go/src/com/intellij/protobuf/go/gutter/PbGoImplementationSearcher.kt
@@ -1,12 +1,17 @@
 package com.intellij.protobuf.go.gutter
 
+import com.goide.GoLanguage
+import com.goide.psi.GoTypeSpec
 import com.goide.stubs.index.GoTypesIndex
+import com.intellij.openapi.components.service
 import com.intellij.protobuf.ide.gutter.PbCodeImplementationSearcher
 import com.intellij.protobuf.ide.gutter.PbGeneratedCodeConverter
 import com.intellij.protobuf.lang.psi.PbElement
 import com.intellij.protobuf.lang.psi.PbMessageDefinition
+import com.intellij.protobuf.lang.stub.ProtoFileAccessor
 import com.intellij.psi.PsiElement
 import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.util.containers.sequenceOfNotNull
 
 internal class PbGoImplementationSearcher : PbCodeImplementationSearcher {
   override fun findImplementationsForProtoElement(pbElement: PbElement,
@@ -19,11 +24,20 @@ internal class PbGoImplementationSearcher : PbCodeImplementationSearcher {
 
   override fun findDeclarationsForCodeElement(psiElement: PsiElement,
                                               converters: Collection<PbGeneratedCodeConverter>): Sequence<PbElement> {
-    return emptySequence()
+    return when {
+      psiElement.language != GoLanguage.INSTANCE -> emptySequence()
+      psiElement is GoTypeSpec -> findMessageDeclaration(psiElement)
+      else -> emptySequence()
+    }
   }
 
   private fun handleMessageImplementations(pbElement: PbMessageDefinition): Sequence<PsiElement> {
-    val goPackage = pbElement.pbFile.packageQualifiedName.toString()
+    val explicitGoPackage = pbElement.pbFile.options
+      .firstOrNull { it.optionName.text == PB_GO_PACKAGE_OPTION }
+      ?.stringValue
+      ?.asString
+      ?.substringAfterLast("/")
+    val goPackage = explicitGoPackage ?: pbElement.pbFile.packageQualifiedName.toString()
     val messageName = pbElement.name ?: return emptySequence()
 
     return GoTypesIndex.find(messageName, pbElement.project, GlobalSearchScope.projectScope(pbElement.project), null)
@@ -33,4 +47,14 @@ internal class PbGoImplementationSearcher : PbCodeImplementationSearcher {
         packageName.isNullOrBlank() || packageName == goPackage
       }
   }
+
+  private fun findMessageDeclaration(typeSpec: GoTypeSpec): Sequence<PbElement> {
+    val specName = typeSpec.name ?: return emptySequence()
+    val goPackage = typeSpec.containingFile.packageName
+    val pbMessageFqn = if (goPackage.isNullOrBlank()) specName else "$goPackage.$specName"
+    val messageDefinition = typeSpec.project.service<ProtoFileAccessor>().findMessageByFqn(pbMessageFqn)
+    return sequenceOfNotNull(messageDefinition)
+  }
 }
+
+private const val PB_GO_PACKAGE_OPTION = "go_package"
