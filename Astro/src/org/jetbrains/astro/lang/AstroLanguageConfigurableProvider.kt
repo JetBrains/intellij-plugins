5424ca0b93644210e20358dac7de5697e68f7dfb
Christian Ivicevic <christian.ivicevic@jetbrains.com>
2023-02-21 09:15:23 +0000

    WEB-59344 Fix replace with template string in Astro files
    For version 231.
    
    Merge-request: IJ-MR-103015
    Merged-by: Christian Ivicevic <Christian.Ivicevic@jetbrains.com>
    (cherry picked from commit 040010e008e7f026c2d6d5abad3a2ab392f3e88e)
    
    IJ-MR-103015
    
    GitOrigin-RevId: 2566b6821b51f97e3ef33d105490798458d7c2b0

--- Astro/src/org/jetbrains/astro/lang/AstroLanguageConfigurableProvider.kt
+++ Astro/src/org/jetbrains/astro/lang/AstroLanguageConfigurableProvider.kt
@@ -3,24 +3,30 @@ package org.jetbrains.astro.lang
 
 import com.intellij.lang.Language
 import com.intellij.lang.javascript.JSLanguageDialect
+import com.intellij.lang.javascript.psi.JSBlockStatement
 import com.intellij.lang.javascript.psi.JSElement
 import com.intellij.lang.javascript.psi.JSEmbeddedContent
 import com.intellij.lang.javascript.psi.JSInheritedLanguagesConfigurableProvider
+import com.intellij.lang.javascript.psi.ecma6.TypeScriptFunctionExpression
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.util.childrenOfType
 import org.jetbrains.astro.codeInsight.astroContentRoot
 
 class AstroLanguageConfigurableProvider : JSInheritedLanguagesConfigurableProvider() {
-
   override fun getLanguage(): Language {
     return AstroLanguage.INSTANCE
   }
 
   override fun createJSContentFromText(project: Project, text: String, dialect: JSLanguageDialect?): JSElement {
-    val astroFile = PsiFileFactory.getInstance(project).createFileFromText("dummy.astro", language, "{$text}", false, true)
-    return (astroFile as AstroFileImpl).astroContentRoot()!!.childrenOfType<JSEmbeddedContent>().first()
+    val statement = "{() => {$text}}"
+    val astroFile = PsiFileFactory.getInstance(project).createFileFromText("dummy.astro", language, statement, false, true)
+    return (astroFile as AstroFileImpl)
+      .astroContentRoot()!!
+      .childrenOfType<JSEmbeddedContent>().first()
+      .childrenOfType<TypeScriptFunctionExpression>().first()
+      .childrenOfType<JSBlockStatement>().first()
+      // Ensure the firstChild is the JSBlockStatement and not some LeafPsiElement
+      .apply { children[0].delete() }
   }
-
-
 }
