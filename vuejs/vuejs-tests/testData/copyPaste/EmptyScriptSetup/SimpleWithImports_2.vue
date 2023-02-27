986b986595729eb149bbbfafc26e04c0167dacbf
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-02-27 10:46:38 +0100

    WEB-59707 Vue: copy/paste into empty script setup removes "script" attribute
    GitOrigin-RevId: 7d85576c9a940ae1e97d2f4be005b545e9208cb1

---
+++ vuejs/vuejs-tests/testData/copyPaste/EmptyScriptSetup/SimpleWithImports_2.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="list" />
+</template>
+
+<script>
+  export default {
+    name: 'list2',
+  }
+</script>
---
+++ vuejs/vuejs-tests/testData/copyPaste/EmptyScriptSetup/Source.vue
@@ -0,0 +1,7 @@
+<template>
+    <selection><Component class="list" /></selection>
+</template>
+
+<script setup>
+  import Component from './SimpleWithImports_2'
+</script>
