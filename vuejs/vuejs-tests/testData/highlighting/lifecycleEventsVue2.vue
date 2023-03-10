3b836d6c93037df242ddcecdadd12d0506575bd7
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-03-10 11:36:12 +0100

    WEB-36212 WEB-38671 WEB-49271 WEB-49288 WEB-50364 WEB-51047 WEB-51156 WEB-54966 Vue: recognize 'export default' and Vue component properties as always used.
    (cherry picked from commit fa13173351b7dec9dc7cc45d4d800b0c1b52a3e9)
    
    IJ-CR-104503
    
    GitOrigin-RevId: 83977b75a37292214ca2f2e568aee4ac7fcf8e68

---
+++ vuejs/vuejs-tests/testData/highlighting/lifecycleEventsVue2.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button v-on:click="decrement">-</button>
+    {{ count }}
+    <button v-on:click="increment">+</button>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+// Define the component in class-style
+@Component({
+             mounted() {
+
+             },
+             <warning descr="Unused function bar">bar</warning>() {
+
+             }
+           })
+export default class Counter extends Vue {
+  // Class properties will be component data
+  count = 0
+
+  created() {
+  }
+
+  <warning descr="Unused method foo">foo</warning>() {
+
+  }
+
+  // Methods will be component methods
+  increment() {
+    this.count++
+  }
+
+  decrement() {
+    this.count--
+  }
+}
+</script>
