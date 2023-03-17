9e9f429f641a7c270e663087b8de5b3c13b06842
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-03-17 18:23:46 +0100

    WEB-45336 WEB-46869 WEB-51644 In Vue files can't find usages of names ended with $ symbol & have unused color
    (cherry picked from commit ce03fcac18098fd09b8c552e0fc778e5f900eb70)
    
    IJ-CR-104957
    
    GitOrigin-RevId: 4ed8a671f3ac1128de1ca7eb00b456ea815a6203

--- vuejs/src/org/jetbrains/vuejs/lang/html/index/VueIdIndexer.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/html/index/VueIdIndexer.kt
@@ -1,18 +1,21 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.lang.html.index
 
-import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.psi.impl.cache.impl.BaseFilterLexerUtil
 import com.intellij.psi.impl.cache.impl.id.IdIndexEntry
 import com.intellij.psi.impl.cache.impl.id.LexingIdIndexer
 import com.intellij.util.indexing.FileContent
-import org.jetbrains.vuejs.lang.html.VueLanguage
+import org.jetbrains.vuejs.lang.LangMode
+import org.jetbrains.vuejs.lang.html.highlighting.VueSyntaxHighlighterFactory
 
 class VueIdIndexer : LexingIdIndexer {
   override fun map(inputData: FileContent): Map<IdIndexEntry, Int> {
     return BaseFilterLexerUtil.calcIdEntries(inputData) { consumer ->
-      VueFilterLexer(consumer, SyntaxHighlighterFactory.getSyntaxHighlighter(
-        VueLanguage.INSTANCE, inputData.project, inputData.file).highlightingLexer)
+      // We need to choose lexer lang mode, since we don't have access to the indices here.
+      // Let's use TypeScript lang, because it's a superset over JavaScript and should
+      // work well at most times in both JS and TS cases.
+      VueFilterLexer(consumer, VueSyntaxHighlighterFactory.getSyntaxHighlighter(
+        inputData.project, inputData.file, LangMode.HAS_TS).highlightingLexer)
     }
   }
 
---
+++ vuejs/vuejs-tests/testData/highlighting/idIndexer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <button @click="go$">go$</button>
+    <button @click="click">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    go$() {
+      console.log("go$")
+    },
+    <warning descr="Unused function go_">go_</warning>() {
+
+    },
+    click() {
+      this.go$()
+    },
+  },
+}
+</script>
