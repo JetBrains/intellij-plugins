42b793f353d33a7c5ed96882d4a387e7c776e9b2
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-03-20 17:06:48 +0100

    [vue] make resolve from template variables case-sensitive (WEB-57388 WEB-58054 WEB-59089)
    GitOrigin-RevId: 911e0594e549f9a935e917d526096da739a356c4

---
+++ vuejs/vuejs-tests/testData/highlighting/scriptCaseSensitivity.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+interface Example {
+  uniqueField: number;
+}
+
+const example: Example = {
+  uniqueField: 0,
+}
+</script>
+
+<template>
+  <div>{{ example.uniqueField }}</div>
+  <div>{{ example.<error descr="Unresolved variable name">name</error> }}</div>
+  <div>{{ example.<error descr="Unresolved variable invalidUniqueName">invalidUniqueName</error> }}</div>
+</template>
---
+++ vuejs/vuejs-tests/testData/typeResolve/scriptCaseSensitivity.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+interface Example {
+  bool: boolean;
+}
+
+interface RealType {
+  uniqueField: number;
+}
+
+const example: RealType = {
+  uniqueField: 0,
+}
+</script>
+
+<template>
+  <div>{{ example }}</div>
+</template>
