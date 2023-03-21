181ec2886366d34ec7e1981f2ccb425fb8c86338
Christian Ivicevic <christian.ivicevic@jetbrains.com>
2023-03-21 12:54:43 +0100

    WEB-59792 Consolidate conditional tag handling in Astro
    GitOrigin-RevId: 3915626303d156232b8ef34641447e6a89c2dff8

--- Astro/src/org/jetbrains/astro/lang/psi/AstroHtmlTag.kt
+++ Astro/src/org/jetbrains/astro/lang/psi/AstroHtmlTag.kt
@@ -57,11 +57,7 @@ class AstroHtmlTag(type: IElementType) : XmlTagImpl(type), HtmlTag, JSLiteralExp
         .createFileFromText("foo.astro", language, text, false, true)
       val root = file.firstChild
       assert(root is AstroContentRoot) { "Failed to parse as tag $text" }
-      val tag = (root as AstroContentRoot).let {
-        // script is no longer an AstroHtmlTag
-        if (text == "<script/>") childrenOfType<HtmlTag>()
-        else childrenOfType<AstroHtmlTag>()
-      }.firstOrNull()
+      val tag = (root as AstroContentRoot).childrenOfType<HtmlTag>().firstOrNull()
       assert(tag != null) { "Failed to parse as tag $text" }
       return tag!!
     }
