419ccf24f1acc247d276900f352fa93520663020
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-03-28 15:47:14 +0200

    WEB-60179 Do not allow for end tag omission in .vue files - post review fix
    IJ-CR-105446
    
    (cherry picked from commit 0830923157b0430f7d07d8dd744874a4e2f1a113)
    
    GitOrigin-RevId: 4ac63fdd09e41d0da5a182f3fd373767f54a62a7

--- vuejs/vuejs-tests/testData/highlighting/noDoubleSpellCheckingInAttributesWithEmbeddedContents.vue
+++ vuejs/vuejs-tests/testData/highlighting/noDoubleSpellCheckingInAttributesWithEmbeddedContents.vue
@@ -2,7 +2,8 @@
   <div>
     <ul>
       <li v-for="<TYPO descr="Typo: In word 'somewordd'">somewordd</TYPO> in <weak_warning descr="Unresolved variable or type someObject">someObject</weak_warning>">{{ somewordd }}
-    <error descr="Element li is not closed"><</error>/ul>
+      </li>
+    </ul>
   </div>
 </template>
 <script>
