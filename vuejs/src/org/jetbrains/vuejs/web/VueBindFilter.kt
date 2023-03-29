e0887c6b7520ad39bd86a04682e02d428bd82cf8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-03-29 17:01:12 +0200

    Vue: fix issue with on* props being incorrectly filtered out
    IJ-CR-105604
    
    (cherry picked from commit 34c87a135c059c68dc48d448c8604fa5e87638e9)
    
    GitOrigin-RevId: f8b3b27476fb2115286fcb1a863c0d1d1c9a161c

--- vuejs/src/org/jetbrains/vuejs/web/VueBindFilter.kt
+++ vuejs/src/org/jetbrains/vuejs/web/VueBindFilter.kt
@@ -1,28 +1,35 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.web
 
-import com.intellij.webSymbols.*
+import com.intellij.webSymbols.WebSymbol
+import com.intellij.webSymbols.WebSymbolsScope
 import com.intellij.webSymbols.completion.WebSymbolCodeCompletionItem
-import com.intellij.webSymbols.webTypes.filters.WebSymbolsFilter
 import com.intellij.webSymbols.query.WebSymbolsQueryExecutor
+import com.intellij.webSymbols.webTypes.filters.WebSymbolsFilter
 import org.jetbrains.vuejs.web.VueWebSymbolsQueryConfigurator.Companion.KIND_VUE_COMPONENT_PROPS
 
 class VueBindFilter : WebSymbolsFilter {
   override fun filterCodeCompletions(codeCompletions: List<WebSymbolCodeCompletionItem>,
                                      queryExecutor: WebSymbolsQueryExecutor,
                                      scope: List<WebSymbolsScope>,
-                                     properties: Map<String, Any>): List<WebSymbolCodeCompletionItem> {
-    val props = queryExecutor.runNameMatchQuery(WebSymbol.NAMESPACE_HTML, KIND_VUE_COMPONENT_PROPS, "",
-                                                scope = scope).mapTo(HashSet()) { it.name }
-    return codeCompletions.filter { !it.name.startsWith("on") || props.contains(it.name) }
-  }
+                                     properties: Map<String, Any>): List<WebSymbolCodeCompletionItem> =
+    codeCompletions.filterHtmlEventAttributes(queryExecutor, scope) { name }
 
   override fun filterNameMatches(matches: List<WebSymbol>,
                                  queryExecutor: WebSymbolsQueryExecutor,
                                  scope: List<WebSymbolsScope>,
-                                 properties: Map<String, Any>): List<WebSymbol> {
+                                 properties: Map<String, Any>): List<WebSymbol> =
+    matches.filterHtmlEventAttributes(queryExecutor, scope) { name }
+
+  private fun <T> List<T>.filterHtmlEventAttributes(queryExecutor: WebSymbolsQueryExecutor,
+                                                    scope: List<WebSymbolsScope>,
+                                                    getName: T.() -> String): List<T> {
     val props = queryExecutor.runNameMatchQuery(WebSymbol.NAMESPACE_HTML, KIND_VUE_COMPONENT_PROPS, "",
                                                 scope = scope).mapTo(HashSet()) { it.name }
-    return matches.filter { !it.name.startsWith("on") || props.contains(it.name) }
+    return filter {
+      val name = it.getName()
+      !name.startsWith("on") || name.startsWith("on-") || name == "on" || props.contains(name)
+    }
   }
+
 }
