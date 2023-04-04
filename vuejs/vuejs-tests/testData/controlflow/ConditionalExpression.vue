1e457466db0b9fea71c28918721798a3a754d9e0
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-04-04 17:47:33 +0200

    [vue] properly close CFG with conditional expressions (WEB-59826)
    (cherry picked from commit ebc7f27f523c4c1ec867b275631b3a9c7af7eff8)
    
    IJ-CR-106023
    
    GitOrigin-RevId: 4405b3ddb934baf54138b684ca7090ea0d9cdc9f

---
+++ vuejs/vuejs-tests/testData/controlflow/ConditionalExpression.vue
@@ -0,0 +1,11 @@
+<script setup lang='ts'>
+const bar: string | number = 'hello';
+
+function isString(x: unknown): x is string {
+  return typeof x === "string"
+}
+</script>
+
+<template>
+  <div v-text='isString(bar) ? bar.length : bar.<error descr="Unresolved variable length">length</error>'></div>
+</template>
