9608472e4f3ce33adb22423b5e2a713ee9feda31
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-04-06 19:15:06 +0200

    [angular] more generic way to resolve tsconfig from template files (WEB-57214)
    (cherry picked from commit 4a0fe65dd97e01676ced3e3b3f4fb5e62c6604b8)
    
    IJ-CR-105883
    
    GitOrigin-RevId: 79af595e532be0b6b6fc3952b2deb0c302199e42

--- AngularJS/src/org/angular2/lang/html/Angular2HtmlFile.kt
+++ AngularJS/src/org/angular2/lang/html/Angular2HtmlFile.kt
@@ -1,16 +1,17 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2.lang.html
 
+import com.intellij.lang.javascript.config.JSConfig
 import com.intellij.lang.javascript.psi.JSControlFlowScope
 import com.intellij.lang.javascript.psi.controlflow.JSControlFlowService
-import com.intellij.lang.typescript.psi.impl.TypeScriptConfigRedirectingFile
+import com.intellij.lang.javascript.config.JSConfigProvider
+import com.intellij.lang.typescript.tsconfig.TypeScriptConfigUtil
 import com.intellij.psi.FileViewProvider
-import com.intellij.psi.PsiFile
 import com.intellij.psi.impl.source.html.HtmlFileImpl
 import org.angular2.entities.Angular2ComponentLocator
 
 class Angular2HtmlFile(viewProvider: FileViewProvider) : HtmlFileImpl(viewProvider, Angular2HtmlFileElementType.INSTANCE),
-                                                         JSControlFlowScope, TypeScriptConfigRedirectingFile {
+                                                         JSControlFlowScope, JSConfigProvider {
 
   override fun isTopmostControlFlowScope() = true
 
@@ -19,7 +20,8 @@ class Angular2HtmlFile(viewProvider: FileViewProvider) : HtmlFileImpl(viewProvid
     JSControlFlowService.getService(project).resetControlFlow(this)
   }
 
-  override fun redirectToConfigIncludedFile(): PsiFile {
-    return Angular2ComponentLocator.findComponentClass(this)?.containingFile ?: this
+  override fun getJSConfig(): JSConfig? {
+    val importGraphIncludedFile = Angular2ComponentLocator.findComponentClass(this)?.containingFile ?: return null
+    return TypeScriptConfigUtil.getConfigForPsiFile(importGraphIncludedFile)
   }
 }
