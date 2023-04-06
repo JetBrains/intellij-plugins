79a94d748a1e422d3c701e902a39760cd3665640
Dmitry Kichinsky <dmitry.kichinsky@jetbrains.com>
2023-04-06 17:15:55 +0200

    [maven] IDEA-317404 MavenEmbedderWrapper: cleanup customizeForResolve methods
    GitOrigin-RevId: e95d5417833d733ae4dff52ecd2638c00afe036b

--- flex/src/com/intellij/javascript/flex/maven/Flexmojos3GenerateConfigTask.java
+++ flex/src/com/intellij/javascript/flex/maven/Flexmojos3GenerateConfigTask.java
@@ -78,7 +78,7 @@ public class Flexmojos3GenerateConfigTask extends MavenProjectsProcessorBasicTas
           MavenWorkspaceMap workspaceMap = new MavenWorkspaceMap();
           temporaryFiles = mavenIdToOutputFileMapping(workspaceMap, project, myTree.getProjects());
 
-          embedder.customizeForResolve(workspaceMap, console, indicator);
+          embedder.customizeForResolve(console, indicator, false, workspaceMap, null);
           final String generateConfigGoal = FlexmojosImporter.FLEXMOJOS_GROUP_ID + ":" + FlexmojosImporter.FLEXMOJOS_ARTIFACT_ID +
                                             ":generate-config-" + myMavenProject.getPackaging();
           final MavenExplicitProfiles profilesIds = myMavenProject.getActivatedProfilesIds();
