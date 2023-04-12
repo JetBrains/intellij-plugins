9b18d88bdfc0b0b0d013fe4bd34f274305373bc3
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-04-12 10:02:40 +0200

    WEB-60482 Vue: fix issue with using resolve in VueEmptyComponentInitializersIndex
    (cherry picked from commit 6bba2bef2e1ad10a1debc678b264023efb7bb517)
    
    IJ-CR-106206
    
    GitOrigin-RevId: b96b72a0871a9526fcdaaa684ffee555e8ddf9e0

--- vuejs/src/org/jetbrains/vuejs/index/VueEmptyComponentInitializersIndex.kt
+++ vuejs/src/org/jetbrains/vuejs/index/VueEmptyComponentInitializersIndex.kt
@@ -1,13 +1,13 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.index
 
-import com.intellij.lang.ecmascript6.psi.JSExportAssignment
-import com.intellij.lang.ecmascript6.resolve.ES6PsiUtil
+import com.intellij.lang.ecmascript6.psi.ES6ExportDefaultAssignment
 import com.intellij.lang.javascript.psi.JSCallExpression
 import com.intellij.lang.javascript.psi.JSEmbeddedContent
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
 import com.intellij.lang.javascript.psi.ecmal4.JSClass
 import com.intellij.lang.javascript.psi.impl.JSPropertyImpl
+import com.intellij.lang.javascript.psi.util.getStubSafeChildren
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.xml.XmlFile
@@ -37,8 +37,7 @@ class VueEmptyComponentInitializersIndex : ScalarIndexExtension<Boolean>() {
         else {
           val module = PsiTreeUtil.getStubChildOfType(script, JSEmbeddedContent::class.java)
           if (module != null) {
-            val exportedElement = (ES6PsiUtil.findDefaultExport(module) as? JSExportAssignment)
-              ?.stubSafeElement
+            val exportedElement = module.getStubSafeChildren<ES6ExportDefaultAssignment>().firstOrNull()?.stubSafeElement
             if (exportedElement is JSObjectLiteralExpression || exportedElement is JSCallExpression) {
               VueComponents.getSourceComponentDescriptor(exportedElement)
                 ?.initializer
@@ -58,7 +57,7 @@ class VueEmptyComponentInitializersIndex : ScalarIndexExtension<Boolean>() {
     }
   }
 
-  override fun getVersion(): Int = 9
+  override fun getVersion(): Int = 10
 
   override fun getInputFilter(): FileBasedIndex.InputFilter = object : DefaultFileTypeSpecificInputFilter(VueFileType.INSTANCE) {
     override fun acceptInput(file: VirtualFile): Boolean {
