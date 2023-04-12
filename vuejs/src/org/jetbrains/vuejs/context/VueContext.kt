8e4031ad4ede2ee0cd71031846404deafe892b8b
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-04-12 09:55:23 +0200

    WEB-56464 Vue: New Vue Component action - fix issues with Class API scaffolding.
    (cherry picked from commit 473efa3aba3e612d4d51adda5b0376fad1299557)
    
    IJ-CR-106160
    
    GitOrigin-RevId: 6e87eccdca1e047387446187e8f0e376951fd604

--- vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
+++ vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
@@ -26,10 +26,7 @@ import org.jetbrains.vuejs.codeInsight.SETUP_ATTRIBUTE_NAME
 import org.jetbrains.vuejs.codeInsight.withoutPreRelease
 import org.jetbrains.vuejs.index.VUE_MODULE
 import org.jetbrains.vuejs.lang.html.VueFileType
-import org.jetbrains.vuejs.libraries.KIND_VUE_CLASS_COMPONENT_LIBRARY
-import org.jetbrains.vuejs.libraries.KIND_VUE_STORE
-import org.jetbrains.vuejs.libraries.VUE_STORE_PINIA
-import org.jetbrains.vuejs.libraries.VUE_STORE_VUEX
+import org.jetbrains.vuejs.libraries.*
 import org.jetbrains.vuejs.libraries.componentDecorator.COMPONENT_DEC
 import org.jetbrains.vuejs.libraries.componentDecorator.OPTIONS_DEC
 import org.jetbrains.vuejs.web.VueFramework
@@ -61,14 +58,16 @@ fun getVueClassComponentLibrary(location: PsiElement): String? =
   WebSymbolsContext.get(KIND_VUE_CLASS_COMPONENT_LIBRARY, location)
 
 fun getVueClassComponentDecoratorName(location: PsiElement): String =
-  if (isVue3(location))
-    OPTIONS_DEC
-  else
+  if (detectPkgVersion(location, VUE_CLASS_COMPONENT).let {
+      it == null || it < VERSION_8_0_0
+    })
     COMPONENT_DEC
+  else
+    OPTIONS_DEC
 
 fun isVue3(context: PsiElement): Boolean =
-  isVueContext(context) && detectVueVersion(context).let {
-    it == null || it >= VUE_3_0_0
+  isVueContext(context) && detectPkgVersion(context, VUE_MODULE).let {
+    it == null || it >= VERSION_3_0_0
   }
 
 fun supportsScriptSetup(context: PsiElement?): Boolean =
@@ -81,9 +80,10 @@ fun supportsScriptSetup(context: PsiElement?): Boolean =
     ?.firstOrNull() != null
 
 
-private val VUE_3_0_0 = SemVer("3.0.0", 3, 0, 0)
+private val VERSION_3_0_0 = SemVer("3.0.0", 3, 0, 0)
+private val VERSION_8_0_0 = SemVer("8.0.0", 8, 0, 0)
 
-private fun detectVueVersion(context: PsiElement): SemVer? {
+private fun detectPkgVersion(context: PsiElement, packageName: String): SemVer? {
   val vf = context.containingFile?.originalFile?.virtualFile
            ?: context.asSafely<PsiDirectory>()?.virtualFile
            ?: return null
@@ -91,10 +91,10 @@ private fun detectVueVersion(context: PsiElement): SemVer? {
   var exact: SemVer? = null
   PackageJsonUtil.processUpPackageJsonFilesInAllScope(vf) { pkgJson ->
     val data = PackageJsonData.getOrCreate(pkgJson)
-    fromRange = data.allDependencyEntries[VUE_MODULE]
+    fromRange = data.allDependencyEntries[packageName]
       ?.takeIf { it.versionRange.let { range -> !range.contains(" ") && !range.startsWith('<') } }
       ?.parseVersion()
-    exact = pkgJson.parent.findFileByRelativePath(NodeModuleUtil.NODE_MODULES + "/" + VUE_MODULE + "/" + PackageJsonUtil.FILE_NAME)
+    exact = pkgJson.parent.findFileByRelativePath(NodeModuleUtil.NODE_MODULES + "/" + packageName + "/" + PackageJsonUtil.FILE_NAME)
       ?.let { PackageJsonData.getOrCreate(it).version }
     fromRange == null && exact == null
   }
