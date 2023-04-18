4f2e782208402296f2e37f7f1d2162b4f2e0b2a8
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-04-18 19:41:50 +0200

    [angular] test non-null assertion in template
    GitOrigin-RevId: 3fa9bda7b1bdeb8928fbd700142fc191046ed79a

--- AngularJS/test/org/angular2/inspections/data/expressionType/NullChecks.ts
+++ AngularJS/test/org/angular2/inspections/data/expressionType/NullChecks.ts
@@ -1,15 +1,19 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import {Component, Input} from '@angular/core';
-
+import {CommonModule} from "@angular/common";
 
 @Component({
-    selector: 'null-checks',
-    templateUrl: './NullChecks.html'
+  selector: 'null-checks',
+  imports: [CommonModule],
+  standalone: true,
+  templateUrl: './NullChecks.html'
 })
 export class TestComponent {
+  @Input foo!: string;
+
   bar: string | undefined = (() => undefined)();
 
-  @Input foo: string;
+  bazPromise = Promise.resolve("hello");
 
   acceptString(x: string): string {
     return x;
