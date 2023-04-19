a37244117a11b13b22ac650c15e68b628cca4c25
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-04-19 20:09:04 +0200

    [angular] upgrade ngrxLet tests (WEB-60721 WEB-52068)
    GitOrigin-RevId: 9319d84701323298e3e607a36c459fd43e6c6bf2

--- AngularJS/test/org/angular2/inspections/data/expressionType/NgrxLetContextGuard.ts
+++ AngularJS/test/org/angular2/inspections/data/expressionType/NgrxLetContextGuard.ts
@@ -10,15 +10,15 @@ import {LetDirective} from "./let.directive";
   standalone: true,
   template: `
     <ng-container *ngrxLet="number$ as number">{{number.toFixed()}}</ng-container>
-    <ng-container *ngrxLet="number$OrNot as number">{{number.toFixed()}}</ng-container>
-    <ng-container *ngrxLet="numberOrNot$ as number">{{number.<error descr="Unresolved function or method toFixed()">toFixed</error>()}}</ng-container>
+    <ng-container *ngrxLet="number$OrNot as number">{{number.<error descr="Qualifier of 'toFixed' is possibly undefined">toFixed</error>()}}</ng-container>
+    <ng-container *ngrxLet="numberOrNot$ as number">{{number.<error descr="Qualifier of 'toFixed' is possibly undefined">toFixed</error>()}}</ng-container>
     
     <ng-container *ngrxLet="number$ as n1; let n2; error as e; complete as c">
       {{n1.toFixed()}}
       {{n2.toFixed()}}
       {{acceptBoolean(e)}}
       {{acceptBoolean(c)}}
-      {{acceptBoolean(<error descr="Argument type LetViewContextValue<Observable<number>> is not assignable to parameter type boolean  Type number is not assignable to type boolean">n1</error>)}}
+      {{acceptBoolean(<error descr="Argument type number is not assignable to parameter type boolean">n1</error>)}}
     </ng-container>
     
     <ng-container *ngrxLet="{ number1: number$, number2: numberOrNot$ } as vm">
@@ -44,7 +44,7 @@ import {LetDirective} from "./let.directive";
     </ng-container>
     
     <ng-container *ngrxLet="foo.bar.baz as baz">{{baz?.toUpperCase()}}</ng-container>
-    <ng-container *ngrxLet="foo.bar.baz as baz">{{baz.toUpperCase()}}</ng-container>
+    <ng-container *ngrxLet="foo.bar.baz as baz">{{baz.<error descr="Qualifier of 'toUpperCase' is possibly undefined">toUpperCase</error>()}}</ng-container>
   `,
 })
 export class TestComponent {
