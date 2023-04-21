9b06e02846ccd952f449419d85887ce69f3e50f6
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-04-21 15:01:57 +0200

    [angular] convert Angular2TemplateInspectionsProvider to Kotlin
    GitOrigin-RevId: 93fd111752de1283eda7fb535ef6fa8e793f7b35

--- AngularJS/test/org/angular2/inspections/Angular2TemplateInspectionsProvider.kt
+++ AngularJS/test/org/angular2/inspections/Angular2TemplateInspectionsProvider.kt
@@ -1,60 +1,57 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.angular2.inspections;
+package org.angular2.inspections
 
-import com.intellij.codeInspection.InspectionToolProvider;
-import com.intellij.codeInspection.LocalInspectionTool;
-import com.intellij.codeInspection.htmlInspections.*;
-import com.intellij.htmltools.codeInspection.htmlInspections.HtmlDeprecatedAttributeInspection;
-import com.intellij.htmltools.codeInspection.htmlInspections.HtmlDeprecatedTagInspection;
-import com.intellij.lang.javascript.inspections.JSUnresolvedReferenceInspection;
-import com.intellij.lang.javascript.inspections.JSUnusedGlobalSymbolsInspection;
-import com.intellij.lang.javascript.inspections.JSUnusedLocalSymbolsInspection;
-import com.intellij.lang.javascript.inspections.JSValidateTypesInspection;
-import com.intellij.lang.typescript.inspections.TypeScriptUnresolvedReferenceInspection;
-import com.intellij.lang.typescript.inspections.TypeScriptValidateJSTypesInspection;
-import com.intellij.lang.typescript.inspections.TypeScriptValidateTypesInspection;
-import com.intellij.xml.util.CheckEmptyTagInspection;
-import com.sixrr.inspectjs.validity.ThisExpressionReferencesGlobalObjectJSInspection;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.codeInspection.InspectionToolProvider
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.htmlInspections.*
+import com.intellij.htmltools.codeInspection.htmlInspections.HtmlDeprecatedAttributeInspection
+import com.intellij.htmltools.codeInspection.htmlInspections.HtmlDeprecatedTagInspection
+import com.intellij.lang.javascript.inspections.JSUnresolvedReferenceInspection
+import com.intellij.lang.javascript.inspections.JSUnusedGlobalSymbolsInspection
+import com.intellij.lang.javascript.inspections.JSUnusedLocalSymbolsInspection
+import com.intellij.lang.javascript.inspections.JSValidateTypesInspection
+import com.intellij.lang.typescript.inspections.TypeScriptUnresolvedReferenceInspection
+import com.intellij.lang.typescript.inspections.TypeScriptValidateJSTypesInspection
+import com.intellij.lang.typescript.inspections.TypeScriptValidateTypesInspection
+import com.intellij.xml.util.CheckEmptyTagInspection
+import com.sixrr.inspectjs.validity.ThisExpressionReferencesGlobalObjectJSInspection
 
-public class Angular2TemplateInspectionsProvider implements InspectionToolProvider {
-  @Override
-  public Class<? extends LocalInspectionTool> @NotNull [] getInspectionClasses() {
-    //noinspection unchecked
-    return new Class[]{
+class Angular2TemplateInspectionsProvider : InspectionToolProvider {
+  override fun getInspectionClasses(): Array<Class<out LocalInspectionTool>> {
+    return arrayOf(
       // Angular
-      AngularIncorrectTemplateDefinitionInspection.class,
-      AngularInsecureBindingToEventInspection.class,
-      AngularInvalidAnimationTriggerAssignmentInspection.class,
-      AngularInvalidTemplateReferenceVariableInspection.class,
-      AngularMissingEventHandlerInspection.class,
-      AngularMissingRequiredDirectiveInputBindingInspection.class,
-      AngularMultipleStructuralDirectivesInspection.class,
-      AngularNonEmptyNgContentInspection.class,
-      AngularUndefinedBindingInspection.class,
-      AngularUndefinedTagInspection.class,
-      AngularAmbiguousComponentTagInspection.class,
-      AngularNgOptimizedImageInspection.class,
+      AngularIncorrectTemplateDefinitionInspection::class.java,
+      AngularInsecureBindingToEventInspection::class.java,
+      AngularInvalidAnimationTriggerAssignmentInspection::class.java,
+      AngularInvalidTemplateReferenceVariableInspection::class.java,
+      AngularMissingEventHandlerInspection::class.java,
+      AngularMissingRequiredDirectiveInputBindingInspection::class.java,
+      AngularMultipleStructuralDirectivesInspection::class.java,
+      AngularNonEmptyNgContentInspection::class.java,
+      AngularUndefinedBindingInspection::class.java,
+      AngularUndefinedTagInspection::class.java,
+      AngularAmbiguousComponentTagInspection::class.java,
+      AngularNgOptimizedImageInspection::class.java,
       // TS
-      TypeScriptUnresolvedReferenceInspection.class,
-      TypeScriptValidateTypesInspection.class,
-      TypeScriptValidateJSTypesInspection.class,
+      TypeScriptUnresolvedReferenceInspection::class.java,
+      TypeScriptValidateTypesInspection::class.java,
+      TypeScriptValidateJSTypesInspection::class.java,
       // JS
-      ThisExpressionReferencesGlobalObjectJSInspection.class,
-      JSUnusedGlobalSymbolsInspection.class,
-      JSUnusedLocalSymbolsInspection.class,
-      JSUnresolvedReferenceInspection.class,
-      JSValidateTypesInspection.class,
+      ThisExpressionReferencesGlobalObjectJSInspection::class.java,
+      JSUnusedGlobalSymbolsInspection::class.java,
+      JSUnusedLocalSymbolsInspection::class.java,
+      JSUnresolvedReferenceInspection::class.java,
+      JSValidateTypesInspection::class.java,
       // HTML
-      HtmlUnknownAttributeInspection.class,
-      HtmlUnknownTagInspection.class,
-      HtmlUnknownBooleanAttributeInspection.class,
-      HtmlDeprecatedTagInspection.class,
-      HtmlDeprecatedAttributeInspection.class,
-      HtmlWrongAttributeValueInspection.class,
-      RequiredAttributesInspection.class,
+      HtmlUnknownAttributeInspection::class.java,
+      HtmlUnknownTagInspection::class.java,
+      HtmlUnknownBooleanAttributeInspection::class.java,
+      HtmlDeprecatedTagInspection::class.java,
+      HtmlDeprecatedAttributeInspection::class.java,
+      HtmlWrongAttributeValueInspection::class.java,
+      RequiredAttributesInspection::class.java,
       // XML
-      CheckEmptyTagInspection.class
-    };
+      CheckEmptyTagInspection::class.java
+    )
   }
 }
