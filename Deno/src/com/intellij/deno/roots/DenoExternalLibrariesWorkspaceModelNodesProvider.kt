8d1f6b574948a9775765acc9effae0b180b78ec9
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2023-05-09 16:06:59 +0200

    ExternalLibrariesWorkspaceModelNode for IDEA-319848 Provide substitution for `SyntheticLibraryElementNode`
    GitOrigin-RevId: c442e24c85062c93ab2a69d1955ba7b86026dd64

--- Deno/src/com/intellij/deno/roots/DenoExternalLibrariesWorkspaceModelNodesProvider.kt
+++ Deno/src/com/intellij/deno/roots/DenoExternalLibrariesWorkspaceModelNodesProvider.kt
@@ -1,10 +1,10 @@
 package com.intellij.deno.roots
 
-import com.intellij.deno.DenoLibrary
+import com.intellij.deno.DenoBundle
 import com.intellij.deno.entities.DenoEntity
 import com.intellij.ide.projectView.ViewSettings
+import com.intellij.ide.projectView.impl.nodes.ExternalLibrariesWorkspaceModelNode
 import com.intellij.ide.projectView.impl.nodes.ExternalLibrariesWorkspaceModelNodesProvider
-import com.intellij.ide.projectView.impl.nodes.SyntheticLibraryElementNode
 import com.intellij.ide.util.treeView.AbstractTreeNode
 import com.intellij.openapi.project.Project
 import com.intellij.workspaceModel.ide.virtualFile
@@ -15,12 +15,8 @@ class DenoExternalLibrariesWorkspaceModelNodesProvider : ExternalLibrariesWorksp
   override fun createNode(entity: DenoEntity, project: Project, settings: ViewSettings?): AbstractTreeNode<*>? {
     if (!useWorkspaceModel()) return null
     if (entity.denoTypes == null && entity.depsFile == null) return null
-    val denoLibrary = DenoLibrary(listOfNotNull(entity.denoTypes?.virtualFile, entity.depsFile?.virtualFile))
-    return SyntheticLibraryElementNode(
-      project,
-      denoLibrary,
-      denoLibrary,
-      settings
-    )
+    return ExternalLibrariesWorkspaceModelNode(project,
+                                               listOfNotNull(entity.denoTypes?.virtualFile, entity.depsFile?.virtualFile),
+                                               DenoBundle.message("deno.library.name"), settings)
   }
 }
