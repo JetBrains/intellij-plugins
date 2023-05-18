44dd4617c9dfbbdfccde7929d183db4d0325305e
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-18 18:40:02 +0200

    cleanup(final)
    GitOrigin-RevId: 52390bbcd9a378a04d1c08d1afeb9da31f4d0501

--- flex/src/com/intellij/lang/actionscript/highlighting/ActionScriptSemanticHighlightingUtil.java
+++ flex/src/com/intellij/lang/actionscript/highlighting/ActionScriptSemanticHighlightingUtil.java
@@ -1,11 +1,11 @@
-// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.actionscript.highlighting;
 
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.javascript.highlighting.JSSemanticHighlightingVisitor;
 import com.intellij.lang.javascript.psi.ecmal4.JSAttribute;
 
-public class ActionScriptSemanticHighlightingUtil {
+public final class ActionScriptSemanticHighlightingUtil {
 
   public static void highlight(JSAttribute attribute, AnnotationHolder holder) {
     JSSemanticHighlightingVisitor.lineMarker(attribute, ECMAL4Highlighter.ECMAL4_METADATA, "attribute", holder);
