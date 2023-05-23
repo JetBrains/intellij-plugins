43f58309f80f5098f001e4c90499b4aab1c219af
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2023-05-23 08:52:47 +0300

    don't include html comment into icon hash
    GitOrigin-RevId: 18baf186591787dafbdd6487ecd5b015909adfa7

--- terraform/gen/org/intellij/terraform/TerraformIcons.java
+++ terraform/gen/org/intellij/terraform/TerraformIcons.java
@@ -13,5 +13,5 @@ public final class TerraformIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, TerraformIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Terraform = load("icons/terraform.svg", -1168770954, 2);
+  /** 16x16 */ public static final @NotNull Icon Terraform = load("icons/terraform.svg", -922083640, 2);
 }
