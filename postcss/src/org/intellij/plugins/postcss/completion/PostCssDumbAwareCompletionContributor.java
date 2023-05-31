72162874760e860fdcf10407e5fcf21789a33610
Sofya.Povarova <sofya.povarova@jetbrains.com>
2023-05-31 17:39:11 +0300

    WEB-57875 fixed properties completion in CSS, cleanup code
    GitOrigin-RevId: 09da28a5395edf1d547e5244a3e7d6d158195502

--- postcss/src/org/intellij/plugins/postcss/completion/PostCssDumbAwareCompletionContributor.java
+++ postcss/src/org/intellij/plugins/postcss/completion/PostCssDumbAwareCompletionContributor.java
@@ -8,40 +8,26 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.patterns.ElementPattern;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.css.CssBlock;
-import com.intellij.psi.css.CssSimpleSelector;
 import com.intellij.psi.css.impl.CssElementTypes;
 import com.intellij.psi.css.impl.util.completion.provider.PropertyNamesCompletionProvider;
 import com.intellij.psi.css.impl.util.completion.provider.TagsCompletionProvider;
 import com.intellij.psi.css.util.CssCompletionUtil;
 import com.intellij.psi.tree.IElementType;
-import com.intellij.psi.tree.TokenSet;
 import org.intellij.plugins.postcss.PostCssStubElementTypes;
 import org.intellij.plugins.postcss.completion.handler.PostCssOneLineAtRuleInsertHandler;
 import org.intellij.plugins.postcss.psi.PostCssPsiUtil;
 import org.jetbrains.annotations.NotNull;
 
 import static com.intellij.patterns.PlatformPatterns.psiElement;
-import static com.intellij.patterns.StandardPatterns.or;
-import static com.intellij.patterns.StandardPatterns.string;
+import static com.intellij.psi.css.impl.util.completion.CssDumbAwareCompletionContributor.propertyDeclaration;
 import static com.intellij.psi.css.impl.util.completion.CssDumbAwareCompletionContributor.propertyName;
 
 public class PostCssDumbAwareCompletionContributor extends CompletionContributor implements DumbAware {
-  private static final TokenSet BLOCK_START_OR_END_SET =
-    TokenSet.create(CssElementTypes.CSS_LBRACE, CssElementTypes.CSS_SEMICOLON, CssElementTypes.CSS_RBRACE);
-
   private static final PostCssOneLineAtRuleInsertHandler ONE_LINE_STATEMENT_HANDLER = new PostCssOneLineAtRuleInsertHandler();
 
   public PostCssDumbAwareCompletionContributor() {
     extend(CompletionType.BASIC, selector(), new TagsCompletionProvider());
     extend(CompletionType.BASIC, propertyDeclaration(), new PropertyNamesCompletionProvider());
-  }
-
-  private static ElementPattern<? extends PsiElement> propertyDeclaration() {
-    return inPostCssFile(CssElementTypes.CSS_IDENT)
-      .withParent(CssSimpleSelector.class).inside(CssBlock.class)
-      .afterLeafSkipping(or(emptyElement(), spaceElement()), blockStartOrEnd())
-      .beforeLeafSkipping(or(emptyElement(), spaceElement()), blockStartOrEnd());
   }
 
   private static ElementPattern<PsiElement> selector() {
@@ -63,17 +49,5 @@ public class PostCssDumbAwareCompletionContributor extends CompletionContributor
         resultSet.addElement(CssCompletionUtil.lookupForKeyword("@custom-media", ONE_LINE_STATEMENT_HANDLER));
       }
     }
-  }
-
-  private static ElementPattern<PsiElement> blockStartOrEnd() {
-    return or(psiElement().withElementType(BLOCK_START_OR_END_SET), psiElement().withText(string().contains("\n")));
-  }
-
-  private static PsiElementPattern.Capture<PsiElement> emptyElement() {
-    return psiElement().withText("");
-  }
-
-  private static PsiElementPattern.Capture<PsiElement> spaceElement() {
-    return psiElement().withText(" ");
   }
 }
