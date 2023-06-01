86c921ac95a378b24af681d70477b861e71c0a51
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-06-01 17:23:34 +0200

    [ts] extract BaseLspTypeScriptService from Deno and Volar impls
    GitOrigin-RevId: 11f87ce02124243f68aa6cf95903572f92f33692

--- Deno/src/com/intellij/deno/service/DenoTypeScriptService.kt
+++ Deno/src/com/intellij/deno/service/DenoTypeScriptService.kt
@@ -3,7 +3,6 @@ package com.intellij.deno.service
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.intention.IntentionAction
-import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.deno.DenoBundle
 import com.intellij.deno.DenoSettings
 import com.intellij.execution.ExecutionException
@@ -13,39 +12,30 @@ import com.intellij.lang.ecmascript6.resolve.JSFileReferencesUtil
 import com.intellij.lang.javascript.DialectDetector
 import com.intellij.lang.javascript.JSStringUtil
 import com.intellij.lang.javascript.JSTokenTypes
-import com.intellij.lang.javascript.completion.JSInsertHandler
 import com.intellij.lang.javascript.frameworks.modules.JSUrlImportsUtil
 import com.intellij.lang.javascript.integration.JSAnnotationError
-import com.intellij.lang.javascript.integration.JSAnnotationError.*
 import com.intellij.lang.javascript.library.JSCorePredefinedLibrariesProvider
-import com.intellij.lang.javascript.psi.JSFunctionType
 import com.intellij.lang.javascript.service.JSLanguageServiceProvider
-import com.intellij.lang.parameterInfo.CreateParameterInfoContext
 import com.intellij.lang.typescript.compiler.TypeScriptCompilerSettings
-import com.intellij.lang.typescript.compiler.TypeScriptService
-import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionEntry
 import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptLanguageServiceUtil
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptMessageBus
 import com.intellij.lang.typescript.compiler.languageService.protocol.commands.response.TypeScriptQuickInfoResponse
 import com.intellij.lang.typescript.library.TypeScriptLibraryProvider
+import com.intellij.lang.typescript.lsp.LspAnnotationError
+import com.intellij.lang.typescript.lsp.BaseLspTypeScriptService
 import com.intellij.lsp.LspServer
 import com.intellij.lsp.api.LspServerManager
 import com.intellij.lsp.methods.HoverMethod
-import com.intellij.openapi.Disposable
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import org.eclipse.lsp4j.CompletionItem
+import com.intellij.util.text.SemVer
 import org.eclipse.lsp4j.Diagnostic
-import org.eclipse.lsp4j.DiagnosticSeverity
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletableFuture.completedFuture
-import java.util.concurrent.Future
-import java.util.stream.Stream
 
 class DenoTypeScriptServiceProvider(val project: Project) : JSLanguageServiceProvider {
 
@@ -57,33 +47,26 @@ class DenoTypeScriptServiceProvider(val project: Project) : JSLanguageServicePro
     if (DenoSettings.getService(project).isUseDeno()) listOf(DenoTypeScriptService.getInstance(project)) else emptyList()
 }
 
-class DenoTypeScriptService(private val project: Project) : TypeScriptService, Disposable {
+class DenoTypeScriptService(project: Project) : BaseLspTypeScriptService(project) {
   companion object {
     private val LOG = Logger.getInstance(DenoTypeScriptService::class.java)
     fun getInstance(project: Project): DenoTypeScriptService = project.getService(DenoTypeScriptService::class.java)
   }
 
-  private fun <T> withServer(action: LspServer.() -> T): T? =
-    LspServerManager.getInstance(project).getServersForProvider(DenoLspSupportProvider::class.java).firstOrNull()?.action()
+  override fun getLspServers(): Collection<LspServer> =
+    LspServerManager.getInstance(project).getServersForProvider(DenoLspSupportProvider::class.java)
 
   override val name = "Deno LSP"
-
-  override fun isDisabledByContext(context: VirtualFile) = false
-
-  override fun isServiceCreated() = withServer { isRunning || isMalfunctioned } ?: false
-
-  override fun showStatusBar() = withServer { isRunning } ?: false
+  override val serverVersion: SemVer? = null
 
   override fun getStatusText() = withServer {
+    // TODO use super method (& display serverVersion)
     when {
       isRunning -> "Deno LSP"
       isMalfunctioned -> "Deno LSP ⚠"
       else -> "..."
     }
   }
-
-  override fun openEditor(file: VirtualFile) {}
-  override fun closeLastEditor(file: VirtualFile) {}
 
   override fun getCompletionMergeStrategy(parameters: CompletionParameters, file: PsiFile, context: PsiElement): CompletionMergeStrategy {
     if (JSTokenTypes.STRING_LITERALS.contains(context.node.elementType)) {
@@ -95,10 +78,6 @@ class DenoTypeScriptService(private val project: Project) : TypeScriptService, D
     }
 
     return TypeScriptLanguageServiceUtil.getCompletionMergeStrategy(parameters, file, context)
-  }
-
-  override fun updateAndGetCompletionItems(virtualFile: VirtualFile, parameters: CompletionParameters): Future<List<CompletionEntry>?>? {
-    return withServer { completedFuture(getCompletionItems(parameters).map(::DenoCompletionEntry)) }
   }
 
   override fun getServiceFixes(file: PsiFile, element: PsiElement?, result: JSAnnotationError): Collection<IntentionAction> {
@@ -126,43 +105,21 @@ class DenoTypeScriptService(private val project: Project) : TypeScriptService, D
     return emptyList()
   }
 
-  override fun getDetailedCompletionItems(virtualFile: VirtualFile,
-                                          items: List<CompletionEntry>,
-                                          document: Document,
-                                          positionInFileOffset: Int): Future<List<CompletionEntry>?>? {
-    return withServer { completedFuture(items.map { DenoCompletionEntry(getResolvedCompletionItem((it as DenoCompletionEntry).item)) }) }
-  }
-
-  override fun getNavigationFor(document: Document, sourceElement: PsiElement): Array<PsiElement> =
-    withServer { getElementDefinitions(sourceElement).toTypedArray() } ?: emptyArray()
-
-  override fun getSignatureHelp(file: PsiFile, context: CreateParameterInfoContext): Future<Stream<JSFunctionType>?>? = null
-
-  fun quickInfo(element: PsiElement): TypeScriptQuickInfoResponse? {
-    val server = LspServerManager.getInstance(project).getServersForProvider(DenoLspSupportProvider::class.java).firstOrNull()
-                 ?: return null
+  private fun quickInfo(element: PsiElement): TypeScriptQuickInfoResponse? {
+    val server = getServer() ?: return null
     val raw = server.invokeSynchronously(HoverMethod.create(server, element)) ?: return null
     LOG.info("Quick info for $element : $raw")
-    val response = TypeScriptQuickInfoResponse()
-    response. displayString = raw.substring("<html><body><pre>".length, raw.length - "</pre></body></html>".length)
+    val response = TypeScriptQuickInfoResponse().apply {
+      displayString = raw.substring("<html><body><pre>".length, raw.length - "</pre></body></html>".length) //
+    }
     return response
   }
 
   override fun getQuickInfoAt(element: PsiElement, originalElement: PsiElement, originalFile: VirtualFile): CompletableFuture<TypeScriptQuickInfoResponse?> =
     completedFuture(quickInfo(element))
 
-  override fun restart(recreateToolWindow: Boolean) {
-    LspServerManager.getInstance(project).stopAndRestartIfNeeded(DenoLspSupportProvider::class.java)
-    TypeScriptMessageBus.get(project).changed()
-  }
-
-  override fun highlight(file: PsiFile): CompletableFuture<List<JSAnnotationError>>? {
-    val server = LspServerManager.getInstance(project).getServersForProvider(DenoLspSupportProvider::class.java).firstOrNull()
-                 ?: return completedFuture(emptyList())
-    val virtualFile = file.virtualFile
-    return completedFuture(server.getDiagnostics(virtualFile)?.map {
-      DenoAnnotationError(it, virtualFile.canonicalPath)
-    })
+  override fun createAnnotationError(diagnostic: Diagnostic, virtualFile: VirtualFile): LspAnnotationError {
+    return DenoAnnotationError(diagnostic, virtualFile.canonicalPath)
   }
 
   override fun canHighlight(file: PsiFile) = DialectDetector.isTypeScript(file)
@@ -173,30 +130,12 @@ class DenoTypeScriptService(private val project: Project) : TypeScriptService, D
     !DenoTypings.getInstance(project).isDenoTypings(file) &&
     !TypeScriptLibraryProvider.isLibraryOrBundledLibraryFile(project, file)
 
-  override fun dispose() {}
-}
-
-class DenoCompletionEntry(internal val item: CompletionItem) : CompletionEntry {
-  override val name: String get() = item.label
-
-  override fun intoLookupElement() =
-    LookupElementBuilder.create(item.label)
-      .withTypeText(item.detail, true)
-      .withInsertHandler(JSInsertHandler.DEFAULT)
-}
-
-class DenoAnnotationError(val diagnostic: Diagnostic, private val path: String?) : JSAnnotationError {
-  override fun getLine() = diagnostic.range.start.line
-
-  override fun getColumn() = diagnostic.range.start.character
-
-  override fun getAbsoluteFilePath(): String? = path
-
-  override fun getDescription(): String = DenoBundle.message("deno.inspection.message.prefix", diagnostic.message)
-
-  override fun getCategory() = when (diagnostic.severity) {
-    DiagnosticSeverity.Error -> ERROR_CATEGORY
-    DiagnosticSeverity.Warning -> WARNING_CATEGORY
-    else -> INFO_CATEGORY
+  override fun restart(recreateToolWindow: Boolean) {
+    LspServerManager.getInstance(project).stopAndRestartIfNeeded(DenoLspSupportProvider::class.java)
+    TypeScriptMessageBus.get(project).changed()
   }
+}
+
+class DenoAnnotationError(diagnostic: Diagnostic, path: String?) : LspAnnotationError(diagnostic, path) {
+  override fun getDescription(): String = DenoBundle.message("deno.inspection.message.prefix", diagnostic.message)
 }
--- vuejs/src/org/jetbrains/vuejs/lang/typescript/service/volar/VolarTypeScriptService.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/typescript/service/volar/VolarTypeScriptService.kt
@@ -1,92 +1,39 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.service.volar
 
-import com.intellij.codeInsight.completion.CompletionParameters
-import com.intellij.codeInsight.lookup.LookupElementBuilder
-import com.intellij.lang.javascript.completion.JSInsertHandler
 import com.intellij.lang.javascript.ecmascript6.TypeScriptAnnotatorCheckerProvider
-import com.intellij.lang.javascript.integration.JSAnnotationError
-import com.intellij.lang.javascript.integration.JSAnnotationError.*
-import com.intellij.lang.javascript.psi.JSFunctionType
-import com.intellij.lang.parameterInfo.CreateParameterInfoContext
 import com.intellij.lang.typescript.compiler.TypeScriptLanguageServiceAnnotatorCheckerProvider
-import com.intellij.lang.typescript.compiler.TypeScriptService
-import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionEntry
-import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy
-import com.intellij.lang.typescript.compiler.languageService.TypeScriptLanguageServiceUtil
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptMessageBus
 import com.intellij.lang.typescript.compiler.languageService.protocol.commands.response.TypeScriptQuickInfoResponse
+import com.intellij.lang.typescript.lsp.LspAnnotationError
+import com.intellij.lang.typescript.lsp.BaseLspTypeScriptService
 import com.intellij.lsp.LspServer
 import com.intellij.lsp.api.LspServerManager
 import com.intellij.lsp.methods.HoverMethod
-import com.intellij.openapi.Disposable
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import org.eclipse.lsp4j.CompletionItem
 import org.eclipse.lsp4j.Diagnostic
-import org.eclipse.lsp4j.DiagnosticSeverity
 import org.jetbrains.vuejs.VueBundle
 import org.jetbrains.vuejs.lang.typescript.service.isVolarEnabled
 import org.jetbrains.vuejs.lang.typescript.service.isVolarFileTypeAcceptable
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletableFuture.completedFuture
-import java.util.concurrent.Future
-import java.util.stream.Stream
 
-class VolarTypeScriptService(private val project: Project) : TypeScriptService, Disposable {
-
-
-  private fun <T> withServer(action: LspServer.() -> T): T? =
-    LspServerManager.getInstance(project).getServersForProvider(VolarSupportProvider::class.java).firstOrNull()?.action()
+class VolarTypeScriptService(project: Project) : BaseLspTypeScriptService(project) {
+  override fun getLspServers(): Collection<LspServer> =
+    LspServerManager.getInstance(project).getServersForProvider(VolarSupportProvider::class.java)
 
   override val name = "Volar"
-
-  override fun isDisabledByContext(context: VirtualFile) = false
-
-  override fun isServiceCreated() = withServer { isRunning || isMalfunctioned } ?: false
-
-  override fun showStatusBar() = withServer { isRunning } ?: false
-
-  override fun getStatusText() = withServer {
-    when {
-      isRunning -> "Volar $volarVersion"
-      isMalfunctioned -> "Volar ⚠"
-      else -> "..."
-    }
-  }
-
-  override fun openEditor(file: VirtualFile) {}
-  override fun closeLastEditor(file: VirtualFile) {}
-
-  override fun getCompletionMergeStrategy(parameters: CompletionParameters, file: PsiFile, context: PsiElement): CompletionMergeStrategy {
-    return TypeScriptLanguageServiceUtil.getCompletionMergeStrategy(parameters, file, context)
-  }
-
-  override fun updateAndGetCompletionItems(virtualFile: VirtualFile, parameters: CompletionParameters): Future<List<CompletionEntry>?>? {
-    return withServer { completedFuture(getCompletionItems(parameters).map(::VolarCompletionEntry)) }
-  }
-
-  override fun getDetailedCompletionItems(virtualFile: VirtualFile,
-                                          items: List<CompletionEntry>,
-                                          document: Document,
-                                          positionInFileOffset: Int): Future<List<CompletionEntry>?>? {
-    return withServer { completedFuture(items.map { VolarCompletionEntry(getResolvedCompletionItem((it as VolarCompletionEntry).item)) }) }
-  }
-
-  override fun getNavigationFor(document: Document, sourceElement: PsiElement): Array<PsiElement> =
-    withServer { getElementDefinitions(sourceElement).toTypedArray() } ?: emptyArray()
-
-  override fun getSignatureHelp(file: PsiFile, context: CreateParameterInfoContext): Future<Stream<JSFunctionType>?>? = null
+  override val serverVersion = volarVersion
 
   private fun quickInfo(element: PsiElement): TypeScriptQuickInfoResponse? {
-    val server = LspServerManager.getInstance(project).getServersForProvider(VolarSupportProvider::class.java).firstOrNull()
-                 ?: return null
+    val server = getServer() ?: return null
     val raw = server.invokeSynchronously(HoverMethod.create(server, element)) ?: return null
-    val response = TypeScriptQuickInfoResponse()
-    response.displayString = raw.substring("<html><body><pre>".length, raw.length - "</pre></body></html>".length)
+    val response = TypeScriptQuickInfoResponse().apply {
+      displayString = raw.substring("<html><body><pre>".length, raw.length - "</pre></body></html>".length)
+    }
     return response
   }
 
@@ -95,21 +42,8 @@ class VolarTypeScriptService(private val project: Project) : TypeScriptService, 
                               originalFile: VirtualFile): CompletableFuture<TypeScriptQuickInfoResponse?> =
     completedFuture(quickInfo(element))
 
-  override fun restart(recreateToolWindow: Boolean) {
-    val lspServerManager = LspServerManager.getInstance(project)
-    val lspServers = lspServerManager.getServersForProvider(VolarSupportProvider::class.java)
-    lspServers.forEach { lspServerManager.stopServer(it) }
-    updateVolarLsp(project, true)
-    TypeScriptMessageBus.get(project).changed()
-  }
-
-  override fun highlight(file: PsiFile): CompletableFuture<List<JSAnnotationError>>? {
-    val server = LspServerManager.getInstance(project).getServersForProvider(VolarSupportProvider::class.java).firstOrNull()
-                 ?: return completedFuture(emptyList())
-    val virtualFile = file.virtualFile
-    return completedFuture(server.getDiagnostics(virtualFile)?.map {
-      VolarAnnotationError(it, virtualFile.canonicalPath)
-    })
+  override fun createAnnotationError(diagnostic: Diagnostic, virtualFile: VirtualFile): LspAnnotationError {
+    return VolarAnnotationError(diagnostic, virtualFile.canonicalPath)
   }
 
   override fun canHighlight(file: PsiFile): Boolean {
@@ -121,30 +55,14 @@ class VolarTypeScriptService(private val project: Project) : TypeScriptService, 
 
   override fun isAcceptable(file: VirtualFile) = isVolarEnabled(project, file)
 
-  override fun dispose() {}
-}
-
-class VolarCompletionEntry(internal val item: CompletionItem) : CompletionEntry {
-  override val name: String get() = item.label
-
-  override fun intoLookupElement() =
-    LookupElementBuilder.create(item.label)
-      .withTypeText(item.detail, true)
-      .withInsertHandler(JSInsertHandler.DEFAULT)
-}
-
-class VolarAnnotationError(val diagnostic: Diagnostic, private val path: String?) : JSAnnotationError {
-  override fun getLine() = diagnostic.range.start.line
-
-  override fun getColumn() = diagnostic.range.start.character
-
-  override fun getAbsoluteFilePath(): String? = path
-
-  override fun getDescription(): String = VueBundle.message("volar.error.prefix", diagnostic.message)
-
-  override fun getCategory() = when (diagnostic.severity) {
-    DiagnosticSeverity.Error -> ERROR_CATEGORY
-    DiagnosticSeverity.Warning -> WARNING_CATEGORY
-    else -> INFO_CATEGORY
+  override fun restart(recreateToolWindow: Boolean) {
+    val lspServerManager = LspServerManager.getInstance(project)
+    getLspServers().forEach { lspServerManager.stopServer(it) }
+    updateVolarLsp(project, true)
+    TypeScriptMessageBus.get(project).changed()
   }
+}
+
+class VolarAnnotationError(diagnostic: Diagnostic, path: String?) : LspAnnotationError(diagnostic, path) {
+  override fun getDescription(): String = VueBundle.message("volar.error.prefix", diagnostic.message)
 }
