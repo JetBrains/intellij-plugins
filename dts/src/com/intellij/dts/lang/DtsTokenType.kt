dd33c56856237849f2464f85c2fc5908e31c0e4b
Daniel Brauner <daniel.brauner@jetbrains.com>
2023-06-12 17:25:38 +0200

    CPP-24932 better names for token types in parser error messages
    GitOrigin-RevId: cfff9694bb9b8cd6bcec6a9aecaa8f4ea496597b

--- dts/src/com/intellij/dts/lang/DtsTokenType.kt
+++ dts/src/com/intellij/dts/lang/DtsTokenType.kt
@@ -1,9 +1,75 @@
 package com.intellij.dts.lang
 
+import com.intellij.dts.lang.psi.DtsTypes
 import com.intellij.psi.tree.IElementType
 
 class DtsTokenType(debugName: String) : IElementType(debugName, DtsLanguage) {
     override fun toString(): String {
-        return "DtsTokenType." + super.toString()
+        return when (this) {
+            DtsTypes.LABEL -> "label"
+            DtsTypes.NAME -> "name"
+            DtsTypes.INT_VALUE -> "integer"
+            DtsTypes.BYTE_VALUE -> "byte"
+            DtsTypes.STRING_VALUE -> "string"
+            DtsTypes.CHAR_VALUE -> "char"
+            DtsTypes.PATH -> "path"
+
+            DtsTypes.SEMICOLON -> ";"
+            DtsTypes.ASSIGN -> "="
+            DtsTypes.COMMA -> ","
+            DtsTypes.SLASH -> "/"
+            DtsTypes.HANDLE -> "&"
+            DtsTypes.DQUOTE -> "\""
+            DtsTypes.SQUOTE -> "'"
+
+            // compiler directives
+            DtsTypes.V1 -> "/dts-v1/"
+            DtsTypes.PLUGIN -> "/plugin/"
+            DtsTypes.INCLUDE -> "/include/"
+            DtsTypes.MEMRESERVE -> "/memreserve/"
+            DtsTypes.DELETE_NODE -> "/delete-node/"
+            DtsTypes.DELETE_PROP -> "/delete-property/"
+            DtsTypes.OMIT_NODE -> "/omit-if-no-ref/"
+            DtsTypes.BITS -> "/bits/"
+
+            // braces
+            DtsTypes.LBRACE -> "{"
+            DtsTypes.RBRACE -> "}"
+            DtsTypes.LPAREN -> "("
+            DtsTypes.RPAREN -> ")"
+            DtsTypes.LBRAC -> "["
+            DtsTypes.RBRAC -> "]"
+            DtsTypes.LANGL -> "<"
+            DtsTypes.RANGL -> ">"
+
+            // expressions
+            DtsTypes.ADD -> "+"
+            DtsTypes.SUB -> "-"
+            DtsTypes.MUL -> "*"
+            DtsTypes.DIV -> "/"
+            DtsTypes.MOD -> "%"
+            DtsTypes.AND -> "&"
+
+            DtsTypes.OR -> "|"
+            DtsTypes.XOR -> "^"
+            DtsTypes.NOT -> "~"
+            DtsTypes.LSH -> "<<"
+            DtsTypes.RSH -> ">>"
+
+            DtsTypes.L_AND -> "&&"
+            DtsTypes.L_OR -> "||"
+            DtsTypes.L_NOT -> "!"
+
+            DtsTypes.LES -> "<"
+            DtsTypes.GRT -> ">"
+            DtsTypes.LEQ -> "<="
+            DtsTypes.GEQ -> ">="
+            DtsTypes.EQ -> "=="
+            DtsTypes.NEQ -> "!="
+            DtsTypes.COLON -> ":"
+            DtsTypes.TERNARY -> "?"
+
+            else -> super.toString()
+        }
     }
 }
