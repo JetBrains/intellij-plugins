3fb48905b889f0f62ba795e9bf01cb8d0e4c1eae
Yuriy Mikhalev <yuriy.mikhalev@jetbrains.com>
2023-06-14 17:39:35 +0200

    [P4] Add "Click to connect" tooltip to perforce widget in old UI
    GitOrigin-RevId: 685b456b3976a7ad8ee223eff157cc007a5d9b3a

--- PerforceIntegration/src/org/jetbrains/idea/perforce/actions/PerforceWokspaceComboBoxAction.kt
+++ PerforceIntegration/src/org/jetbrains/idea/perforce/actions/PerforceWokspaceComboBoxAction.kt
@@ -1,10 +1,16 @@
 package org.jetbrains.idea.perforce.actions
 
+import com.intellij.ide.DataManager
 import com.intellij.ide.ui.ToolbarSettings
 import com.intellij.openapi.actionSystem.*
 import com.intellij.openapi.actionSystem.ex.ComboBoxAction
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.text.HtmlBuilder
+import com.intellij.openapi.util.text.HtmlChunk
+import com.intellij.ui.ColorUtil
+import com.intellij.util.ui.UIUtil
+import org.jetbrains.idea.perforce.PerforceBundle
 import org.jetbrains.idea.perforce.application.PerforceManager
 import org.jetbrains.idea.perforce.perforce.PerforceSettings
 import java.awt.event.ActionEvent
@@ -13,6 +19,7 @@ import javax.swing.JComponent
 class PerforceWorkspaceComboBoxAction : ComboBoxAction(), DumbAware {
   companion object {
     private val multipleWorkspacesKey = Key.create<Boolean>("P4_MULTIPLE_WORKSPACES")
+    private val isConnectedKey = Key.create<Boolean>("P4_IS_CONNECTED")
   }
 
   override fun shouldShowDisabledActions(): Boolean = true
@@ -36,7 +43,7 @@ class PerforceWorkspaceComboBoxAction : ComboBoxAction(), DumbAware {
     with (presentation) {
       isEnabledAndVisible = true
       icon = PerforceToolbarWidgetHelper.getIcon(perforceSettings, isNoConnections, false)
-      description = PerforceToolbarWidgetHelper.getDescription(workspace, isNoConnections, perforceSettings.ENABLED)
+      description = getDescription(workspace, isNoConnections, perforceSettings.ENABLED)
 
       val text = PerforceToolbarWidgetHelper.getText(workspace, isNoConnections, perforceSettings.ENABLED)
       setText(text, false)
@@ -45,20 +52,41 @@ class PerforceWorkspaceComboBoxAction : ComboBoxAction(), DumbAware {
     }
   }
 
+  private fun getDescription(workspace: String?, isNoConnections: Boolean, isOnline: Boolean) : String {
+    if (!isOnline && !isNoConnections) {
+      val color = ColorUtil.toHex(UIUtil.getInactiveTextColor())
+      val builder = HtmlBuilder()
+        .append(HtmlChunk.html().addText(PerforceBundle.message("connection.cannot.connect"))
+                  .child(HtmlChunk.br())
+                  .child(HtmlChunk.font(color).addText(PerforceBundle.message("connection.cannot.connect.click.to.go.online"))))
+      return builder.toString()
+    }
+
+    return PerforceToolbarWidgetHelper.getDescription(workspace, isNoConnections, isOnline)
+  }
+
   override fun createComboBoxButton(presentation: Presentation): ComboBoxButton = PerforceWorkspaceComboBox(presentation)
 
   override fun updateCustomComponent(component: JComponent, presentation: Presentation) {
     val workspaceComboBox = component as? PerforceWorkspaceComboBox ?: return
     val isMultipleWorkspaces = presentation.getClientProperty(multipleWorkspacesKey) ?: return
+    val isOnline = presentation.getClientProperty(isConnectedKey) ?: return
     workspaceComboBox.isMultipleWorkspaces = isMultipleWorkspaces
+    workspaceComboBox.isOnline = isOnline
   }
 
   private inner class PerforceWorkspaceComboBox(presentation: Presentation) : ComboBoxButton(presentation) {
     var isMultipleWorkspaces = false
+    var isOnline = false
 
-    override fun isArrowVisible(): Boolean = isMultipleWorkspaces
+    override fun isArrowVisible(): Boolean = isOnline && isMultipleWorkspaces
 
     override fun fireActionPerformed(event: ActionEvent?) {
+      if (!isOnline) {
+        val context = DataManager.getInstance().getDataContext(this)
+        PerforceSettings.getSettings(context.getData(CommonDataKeys.PROJECT)).enable()
+        return
+      }
       if (isMultipleWorkspaces)
         super.fireActionPerformed(event)
     }
