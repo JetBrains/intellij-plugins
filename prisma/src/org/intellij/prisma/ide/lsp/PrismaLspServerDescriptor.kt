74af46ee3f639e2d02d86776a59455aa8f3a0665
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-06-20 19:33:01 +0200

    Prisma LSP server doesn't work on WSL (yet); some code comments added
    (cherry picked from commit f015e52aeddb657f4af018f8d805248e31f3cba9)
    
    IJ-CR-109042
    
    GitOrigin-RevId: 932f3649dd9d647354bbf0a411fd8c2f29a3d176

--- prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerDescriptor.kt
+++ prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerDescriptor.kt
@@ -5,7 +5,6 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.javascript.nodejs.interpreter.NodeCommandLineConfigurator
 import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
 import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
-import com.intellij.javascript.nodejs.interpreter.wsl.WslNodeInterpreter
 import com.intellij.lang.javascript.service.JSLanguageServiceUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -19,12 +18,14 @@ class PrismaLspServerDescriptor(project: Project) : ProjectWideLspServerDescript
 
   override fun createCommandLine(): GeneralCommandLine {
     val interpreter = NodeJsInterpreterManager.getInstance(project).interpreter
-    if (interpreter !is NodeJsLocalInterpreter && interpreter !is WslNodeInterpreter) {
+    if (interpreter !is NodeJsLocalInterpreter) {
+      // shouldn't happen, checked in PrismaLspServerSupportProvider
       throw ExecutionException(PrismaBundle.message("prisma.interpreter.not.configured"))
     }
 
     val lsp = JSLanguageServiceUtil.getPluginDirectory(javaClass, "language-server/prisma-language-server.js")
     if (lsp == null || !lsp.exists()) {
+      // broken plugin installation?
       throw ExecutionException(PrismaBundle.message("prisma.language.server.not.found"))
     }
 
@@ -39,6 +40,9 @@ class PrismaLspServerDescriptor(project: Project) : ProjectWideLspServerDescript
     }
   }
 
+  // references resolution is implemented without using the LSP server
   override val lspGoToDefinitionSupport = false
+
+  // code completion is implemented without using the LSP server
   override val lspCompletionSupport = null
 }
--- prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerSupportProvider.kt
+++ prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerSupportProvider.kt
@@ -2,7 +2,6 @@ package org.intellij.prisma.ide.lsp
 
 import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
 import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
-import com.intellij.javascript.nodejs.interpreter.wsl.WslNodeInterpreter
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerSupportProvider
@@ -11,9 +10,7 @@ import org.intellij.prisma.lang.PrismaFileType
 class PrismaLspServerSupportProvider : LspServerSupportProvider {
   override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerSupportProvider.LspServerStarter) {
     if (file.fileType != PrismaFileType) return
-
-    val node = NodeJsInterpreterManager.getInstance(project).interpreter
-    if (node !is NodeJsLocalInterpreter && node !is WslNodeInterpreter) return
+    if (NodeJsInterpreterManager.getInstance(project).interpreter !is NodeJsLocalInterpreter) return
 
     serverStarter.ensureServerStarted(PrismaLspServerDescriptor(project))
   }
