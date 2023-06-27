35d2e7a301b22256d5e5b97c7d12bd87560afb9a
Yuriy Mikhalev <yuriy.mikhalev@jetbrains.com>
2023-06-27 23:17:25 +0200

    [P4] Cleanup P4IgnoresMappingsHelper
    (cherry picked from commit db5adf5ba7100ee0ba628bcc2b6b0cd57b6b89d9)
    
    IJ-CR-109662
    
    GitOrigin-RevId: eef6566a313a523e004e46b1fd43d3e645ffd24f

--- PerforceIntegration/src/org/jetbrains/idea/perforce/application/P4IgnoresMappingsHelper.kt
+++ PerforceIntegration/src/org/jetbrains/idea/perforce/application/P4IgnoresMappingsHelper.kt
@@ -13,17 +13,12 @@ class P4IgnoresMappingsHelper(project : Project, connection: P4Connection) {
   init {
     val perforceRunner = PerforceRunner.getInstance(project);
     val result = perforceRunner.ignores(connection)
-    val mappingsList = ArrayList<MappingEntry>();
+    val mappingsList = ArrayList<MappingEntry>()
     for (line in result.stdout.split("\n".toRegex())) {
       if (line.isEmpty())
         continue
       val excluded = line[0] == '!'
-      val trimmedLine = if (excluded) {
-        line.substring(1)
-      }
-      else {
-        line
-      }
+      val trimmedLine = line.removePrefix("!")
 
       val entry = MappingEntry(trimmedLine.replace("...", "**"), excluded)
       mappingsList.add(entry)
@@ -32,13 +27,11 @@ class P4IgnoresMappingsHelper(project : Project, connection: P4Connection) {
     mappings = mappingsList.toTypedArray()
   }
 
-  public fun isIgnored(file: VirtualFile): Boolean {
+  fun isIgnored(file: VirtualFile): Boolean {
     val path = file.toNioPath()
 
-    for (i in mappings.indices) {
-      val rule = mappings[i]
-      val res = rule.pathMatcher.matches(path)
-      if (res) {
+    for (rule in mappings) {
+      if (rule.pathMatcher.matches(path)) {
         return !rule.isExcluded
       }
     }
@@ -46,11 +39,7 @@ class P4IgnoresMappingsHelper(project : Project, connection: P4Connection) {
     return false
   }
 
-  private class MappingEntry(val pattern: String, val isExcluded: Boolean) {
+  private data class MappingEntry(val pattern: String, val isExcluded: Boolean) {
     val pathMatcher: PathMatcher = FileSystems.getDefault().getPathMatcher("glob:${pattern}")
-
-    override fun toString(): String {
-      return pattern
-    }
   }
 }
