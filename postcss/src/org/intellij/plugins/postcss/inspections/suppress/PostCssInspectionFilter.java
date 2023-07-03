e7f6f5374d9dd22aa6fdc69bd813eddb96a91937
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-07-03 19:56:55 +0200

    WEB-61786 Don't report CSS nesting problems in the style part in Vue
    GitOrigin-RevId: 3f9e51a549015fa99a98ff8afcbb52a0a2f224c1

--- postcss/src/org/intellij/plugins/postcss/inspections/suppress/PostCssInspectionFilter.java
+++ postcss/src/org/intellij/plugins/postcss/inspections/suppress/PostCssInspectionFilter.java
@@ -7,6 +7,7 @@ import com.intellij.psi.css.inspections.CssApiBaseInspection;
 import com.intellij.psi.css.inspections.CssInspectionFilter;
 import com.intellij.psi.css.inspections.invalid.CssInvalidAtRuleInspection;
 import com.intellij.psi.css.inspections.invalid.CssInvalidImportInspection;
+import com.intellij.psi.css.inspections.invalid.CssInvalidNestedSelectorInspection;
 import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.util.containers.ContainerUtil;
 import org.intellij.plugins.postcss.lexer.PostCssTokenTypes;
@@ -16,8 +17,11 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Set;
 
 public class PostCssInspectionFilter extends CssInspectionFilter {
-  private static final Set<Class<? extends CssApiBaseInspection>> UNSUPPORTED_INSPECTIONS =
-    ContainerUtil.newHashSet(CssInvalidImportInspection.class, CssInvalidAtRuleInspection.class);
+  private static final Set<Class<? extends CssApiBaseInspection>> UNSUPPORTED_INSPECTIONS = ContainerUtil.newHashSet(
+    CssInvalidImportInspection.class,
+    CssInvalidAtRuleInspection.class,
+    CssInvalidNestedSelectorInspection.class
+  );
 
   @Override
   public boolean isSupported(@NotNull Class<? extends CssApiBaseInspection> clazz, @NotNull PsiElement context) {
