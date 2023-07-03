b1ada9782e0b418eb83d6f4e6f9eda50fbc33ad2
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2023-07-03 15:43:45 +0300

    [vue] support for provide/inject in setup() WEB-61763
    GitOrigin-RevId: e33e10805ba00d2b2ae1f8ee9b9644a1a781b980

--- vuejs/src/org/jetbrains/vuejs/model/source/VueCompositionInfoProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/model/source/VueCompositionInfoProvider.kt
@@ -1,7 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.model.source
 
+import com.intellij.lang.javascript.JSStubElementTypes
 import com.intellij.lang.javascript.evaluation.JSCodeBasedTypeFactory
+import com.intellij.lang.javascript.psi.JSCallExpression
 import com.intellij.lang.javascript.psi.JSFunction
 import com.intellij.lang.javascript.psi.JSObjectLiteralExpression
 import com.intellij.lang.javascript.psi.JSType
@@ -10,15 +12,15 @@ import com.intellij.lang.javascript.psi.types.JSAsyncReturnType
 import com.intellij.lang.javascript.psi.types.JSGenericTypeImpl
 import com.intellij.lang.javascript.psi.types.JSTypeImpl
 import com.intellij.lang.javascript.psi.types.evaluable.JSReturnedExpressionType
+import com.intellij.lang.javascript.psi.util.JSStubBasedPsiTreeUtil
+import com.intellij.psi.tree.TokenSet
 import com.intellij.psi.util.CachedValueProvider
 import com.intellij.psi.util.CachedValuesManager
 import com.intellij.psi.util.PsiModificationTracker
 import com.intellij.util.asSafely
 import org.jetbrains.vuejs.codeInsight.resolveElementTo
-import org.jetbrains.vuejs.model.VueComputedProperty
-import org.jetbrains.vuejs.model.VueDataProperty
-import org.jetbrains.vuejs.model.VueMethod
-import org.jetbrains.vuejs.model.VueNamedSymbol
+import org.jetbrains.vuejs.index.VueFrameworkHandler
+import org.jetbrains.vuejs.model.*
 import org.jetbrains.vuejs.model.source.VueContainerInfoProvider.VueContainerInfo
 
 class VueCompositionInfoProvider : VueContainerInfoProvider {
@@ -31,19 +33,27 @@ class VueCompositionInfoProvider : VueContainerInfoProvider {
   class VueCompositionInfo(val initializer: JSObjectLiteralExpression) : VueContainerInfo {
 
     override val computed: List<VueComputedProperty>
-      get() = rawBindings.filterIsInstance(VueComputedProperty::class.java)
+      get() = symbols.filterIsInstance(VueComputedProperty::class.java)
 
     override val data: List<VueDataProperty>
-      get() = rawBindings.filterIsInstance(VueDataProperty::class.java)
+      get() = symbols.filterIsInstance(VueDataProperty::class.java)
 
     override val methods: List<VueMethod>
-      get() = rawBindings.filterIsInstance(VueMethod::class.java)
+      get() = symbols.filterIsInstance(VueMethod::class.java)
 
-    private val rawBindings: List<VueNamedSymbol>
+    override val provides: List<VueProvide>
+      get() = symbols.filterIsInstance(VueProvide::class.java)
+
+    override val injects: List<VueInject>
+      get() = symbols.filterIsInstance(VueInject::class.java)
+
+    private val symbols: List<VueNamedSymbol>
       get() = CachedValuesManager.getCachedValue(initializer) {
-        CachedValueProvider.Result.create(VueCompositionInfoHelper.createRawBindings(
+        val rawBindings = VueCompositionInfoHelper.createRawBindings(
           initializer, getSetupFunctionType(initializer)
-        ), PsiModificationTracker.MODIFICATION_COUNT)
+        )
+        val methodCalls = getSetupCalls(initializer)
+        CachedValueProvider.Result.create(rawBindings + methodCalls, PsiModificationTracker.MODIFICATION_COUNT)
       }
 
     private fun getSetupFunctionType(initializer: JSObjectLiteralExpression): JSType? =
@@ -61,6 +71,18 @@ class VueCompositionInfoProvider : VueContainerInfoProvider {
           }
           ?: returnType
         }
+
+    private fun getSetupCalls(initializer: JSObjectLiteralExpression): List<VueNamedSymbol> =
+      resolveElementTo(initializer.findProperty(SETUP_METHOD), JSFunction::class)
+        ?.let { JSStubBasedPsiTreeUtil.findDescendants<JSCallExpression>(it, TokenSet.create(JSStubElementTypes.CALL_EXPRESSION)) }
+        ?.mapNotNull {
+          when (VueFrameworkHandler.getFunctionNameFromVueIndex(it)) {
+            PROVIDE_FUN -> analyzeProvide(it)
+            INJECT_FUN -> analyzeInject(it)
+            else -> null
+          }
+        }
+      ?: emptyList()
 
     override fun equals(other: Any?): Boolean =
       (other as? VueCompositionInfo)?.initializer == initializer
---
+++ vuejs/vuejs-tests/testData/typeResolve/injectCallTypeEvaluationVue2/Provide.vue
@@ -0,0 +1,17 @@
+<script>
+import {provide} from "vue";
+import Inject from "./InjectCallTypeEvaluationVue2.vue"
+import {myInjectionKey} from "./injectionKey";
+
+export default {
+  components: {Inject},
+  setup() {
+    provide('stringKey', 'abcd')
+    provide(myInjectionKey, true)
+  }
+}
+</script>
+
+<template>
+  <Inject/>
+</template>
