eace928eb90038fcc1f727481d29b46d25d1a464
Christian Ivicevic <christian.ivicevic@jetbrains.com>
2023-07-10 13:13:43 +0200

    WEB-61123 WEB-61880 Add improved CSS comment handling to Astro and take style options into account
    GitOrigin-RevId: 3c55fde2c7a8e8a906ee99545989b6fd30da192d

--- Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroCommenterTest.kt
+++ Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroCommenterTest.kt
@@ -1,62 +1,82 @@
 package org.jetbrains.astro.codeInsight
 
+import com.intellij.lang.javascript.JSTestUtils
 import com.intellij.openapi.actionSystem.IdeActions
+import org.intellij.plugins.postcss.settings.PostCssCodeStyleSettings
 import org.jetbrains.astro.AstroCodeInsightTestCase
+import org.jetbrains.astro.codeInsight.AstroCommenterTest.CommentStyle.BLOCK
+import org.jetbrains.astro.codeInsight.AstroCommenterTest.CommentStyle.LINE
 
 class AstroCommenterTest : AstroCodeInsightTestCase() {
-  fun testBlockComment() = doBlockCommentTest()
+  fun testSimple() = doTest()
 
-  fun testBlockCommentMultilineSelection() = doBlockCommentTest()
+  fun testMultilineSelection() = doTest()
 
-  fun testBlockCommentRootElement() = doBlockCommentTest()
+  fun testRootElement() = doTest()
 
-  fun testBlockCommentRootElementMultilineSelection() = doBlockCommentTest()
+  fun testRootElementSelection() = doTest()
 
-  fun testBlockCommentRootElementSelection() = doBlockCommentTest()
+  fun testRootElementMultilineSelection() = doTest()
 
-  fun testBlockCommentSelection() = doBlockCommentTest()
+  fun testSelection() = doTest()
 
-  fun testLineComment() = doLineCommentTest()
+  fun testNestedScript() = doTest()
 
-  fun testLineCommentMultilineSelection() = doLineCommentTest()
+  fun testNestedScriptSelection() = doTest()
 
-  fun testLineCommentRootElement() = doLineCommentTest()
+  fun testNestedScriptMultilineSelection() = doTest()
 
-  fun testLineCommentRootElementMultilineSelection() = doLineCommentTest()
+  fun testFrontmatterBlockUncomment() = doTest(BLOCK)
 
-  fun testLineCommentRootElementSelection() = doLineCommentTest()
-
-  fun testLineCommentSelection() = doLineCommentTest()
-
-  fun testNestedScriptBlockComment() = doBlockCommentTest()
-
-  fun testNestedScriptBlockCommentMultilineSelection() = doBlockCommentTest()
-
-  fun testNestedScriptBlockCommentSelection() = doBlockCommentTest()
-
-  fun testNestedScriptLineComment() = doLineCommentTest()
-
-  fun testNestedScriptLineCommentMultilineSelection() = doLineCommentTest()
-
-  fun testNestedScriptLineCommentSelection() = doLineCommentTest()
-
-  fun testFrontmatterBlockUncomment() = doBlockCommentTest()
-
-  fun testBlockCommentStyle() = doBlockCommentTest()
-
-  fun testLineCommentStyle() = doLineCommentTest()
+  fun testCss() = JSTestUtils.testWithTempCodeStyleSettings<Throwable>(myFixture.project) {
+    val settings = it.getCustomSettings(PostCssCodeStyleSettings::class.java)
+    val initialValue = settings.COMMENTS_INLINE_STYLE
+    try {
+      settings.COMMENTS_INLINE_STYLE = true
+      doTest(0, LINE)
+      settings.COMMENTS_INLINE_STYLE = false
+      doTest(1, LINE)
+    } finally {
+      settings.COMMENTS_INLINE_STYLE = initialValue
+    }
+  }
+  fun testSass() = doTest(LINE)
+  fun testScss() = doTest(LINE)
+  fun testLess() = doTest(LINE)
 
   override fun getBasePath() = "codeInsight/commenter"
 
-  private fun doLineCommentTest() {
-    configure()
-    myFixture.performEditorAction(IdeActions.ACTION_COMMENT_LINE)
-    checkResult()
+  private fun doTest(commentStyle: CommentStyle) {
+    val name = getTestName(true)
+    myFixture.configureByFile("$name.astro")
+    myFixture.performEditorAction(
+      if (commentStyle == LINE) IdeActions.ACTION_COMMENT_LINE
+      else IdeActions.ACTION_COMMENT_BLOCK
+    )
+    myFixture.checkResultByFile("${name}_after.astro")
   }
 
-  private fun doBlockCommentTest() {
-    configure()
-    myFixture.performEditorAction(IdeActions.ACTION_COMMENT_BLOCK)
-    checkResult()
+  private fun doTest(id: Int, commentStyle: CommentStyle) {
+    val name = getTestName(true)
+    myFixture.configureByFile("$name.astro")
+    try {
+      myFixture.performEditorAction(
+        if (commentStyle == LINE) IdeActions.ACTION_COMMENT_LINE
+        else IdeActions.ACTION_COMMENT_BLOCK
+      )
+      myFixture.checkResultByFile("${name}_after_$id.astro")
+    } finally {
+      myFixture.configureByFile("$name.astro")
+    }
+  }
+
+  private fun doTest() {
+    doTest(0, LINE)
+    doTest(1, BLOCK)
+  }
+
+  private enum class CommentStyle {
+    LINE,
+    BLOCK
   }
 }
--- Astro/src/org/jetbrains/astro/editor/AstroCommentProvider.kt
+++ Astro/src/org/jetbrains/astro/editor/AstroCommentProvider.kt
@@ -3,6 +3,7 @@ package org.jetbrains.astro.editor
 
 import com.intellij.lang.Commenter
 import com.intellij.lang.Language
+import com.intellij.lang.LanguageCommenters
 import com.intellij.lang.css.CSSLanguage
 import com.intellij.lang.javascript.JavascriptLanguage
 import com.intellij.lang.javascript.editing.JavascriptCommenter
@@ -10,28 +11,54 @@ import com.intellij.lang.javascript.psi.JSEmbeddedContent
 import com.intellij.lang.xml.XmlCommenter
 import com.intellij.openapi.editor.Editor
 import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.css.impl.util.editor.CssCommenter
 import com.intellij.psi.templateLanguages.MultipleLangCommentProvider
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.PsiUtilCore
+import org.intellij.plugins.postcss.PostCssCommentProvider
+import org.intellij.plugins.postcss.PostCssLanguage
 import org.jetbrains.astro.lang.AstroLanguage
 import org.jetbrains.astro.lang.frontmatter.AstroFrontmatterLanguage
 import org.jetbrains.astro.lang.psi.AstroContentRoot
 
 class AstroCommentProvider : MultipleLangCommentProvider {
   override fun getLineCommenter(file: PsiFile, editor: Editor, lineStartLanguage: Language, lineEndLanguage: Language): Commenter {
-    val element = file.findElementAt(editor.caretModel.offset)
-    return if (element?.language?.isKindOf(CSSLanguage.INSTANCE) == true) CssCommenter()
-    else if (
-      lineStartLanguage == AstroFrontmatterLanguage.INSTANCE ||
-      lineStartLanguage.baseLanguage == JavascriptLanguage.INSTANCE ||
-      element?.language == JavascriptLanguage.INSTANCE ||
-      (element?.parent is JSEmbeddedContent && element.parent !is AstroContentRoot) ||
-      // Used for block comments because their language suddenly changes to Astro
-      element?.parent?.language == AstroFrontmatterLanguage.INSTANCE) JavascriptCommenter()
-    else XmlCommenter()
+    val minimalElement = editor.caretModel.currentCaret
+      .let { findMinimalElementContainingRange(file, it.selectionStart, it.selectionEnd) }
+    return minimalElement
+      ?.let { PsiUtilCore.findLanguageFromElement(it) }
+      ?.let {
+        if (it == PostCssLanguage.INSTANCE)
+          return PostCssCommentProvider().getLineCommenter(file, editor, lineStartLanguage, lineEndLanguage)!!
+
+        val element = file.findElementAt(editor.caretModel.offset)
+        when {
+          element?.language?.isKindOf(CSSLanguage.INSTANCE) == true ->
+            CssCommenter()
+          lineStartLanguage == AstroFrontmatterLanguage.INSTANCE ||
+          lineStartLanguage.baseLanguage == JavascriptLanguage.INSTANCE ||
+          element?.language == JavascriptLanguage.INSTANCE ||
+          element?.parent is JSEmbeddedContent &&
+          element.parent !is AstroContentRoot ||
+          // Used for block comments because their language suddenly changes to Astro
+          element?.parent?.language == AstroFrontmatterLanguage.INSTANCE ->
+            JavascriptCommenter()
+          else ->
+            XmlCommenter()
+        }
+      } ?: lineStartLanguage.let { LanguageCommenters.INSTANCE.forLanguage(it) }
   }
 
   override fun canProcess(file: PsiFile, viewProvider: FileViewProvider): Boolean {
     return file.language === AstroLanguage.INSTANCE
+  }
+
+  private fun findMinimalElementContainingRange(file: PsiFile, startOffset: Int, endOffset: Int): PsiElement? {
+    val element1 = file.viewProvider.findElementAt(startOffset, AstroLanguage.INSTANCE)
+    val element2 = file.viewProvider.findElementAt(endOffset - 1, AstroLanguage.INSTANCE)
+    if (element2 == null || element1 == null) return null
+    return PsiTreeUtil.findCommonParent(element1, element2)
   }
 }
