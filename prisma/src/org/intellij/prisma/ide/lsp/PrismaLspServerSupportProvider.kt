65d5987383eafaa595377227da6d78e04fbff1d7
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-07-12 17:08:20 +0200

    WEB-61932 Prisma: Prisma doesn't work in WSL
    GitOrigin-RevId: 1357b65c63f08e4b06966e8216745dff8657e26f

--- prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerSupportProvider.kt
+++ prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerSupportProvider.kt
@@ -2,6 +2,7 @@ package org.intellij.prisma.ide.lsp
 
 import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
 import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
+import com.intellij.javascript.nodejs.interpreter.wsl.WslNodeInterpreter
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.lsp.api.LspServerSupportProvider
@@ -10,7 +11,9 @@ import org.intellij.prisma.lang.PrismaFileType
 class PrismaLspServerSupportProvider : LspServerSupportProvider {
   override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerSupportProvider.LspServerStarter) {
     if (file.fileType != PrismaFileType) return
-    if (NodeJsInterpreterManager.getInstance(project).interpreter !is NodeJsLocalInterpreter) return
+
+    val node = NodeJsInterpreterManager.getInstance(project).interpreter
+    if (node !is NodeJsLocalInterpreter && node !is WslNodeInterpreter) return
 
     serverStarter.ensureServerStarted(PrismaLspServerDescriptor(project))
   }
