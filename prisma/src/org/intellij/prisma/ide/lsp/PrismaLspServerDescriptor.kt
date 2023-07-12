af978c5313b0d76c39513f5ec9483aba442490c6
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-07-12 17:08:20 +0200

    WEB-61932 Prisma: Prisma doesn't work in WSL
    (cherry picked from commit 1357b65c63f08e4b06966e8216745dff8657e26f)
    
    IJ-CR-110872
    
    GitOrigin-RevId: 619751be15dc3c313db0d28c0ee430fb1408103d

--- prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerDescriptor.kt
+++ prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerDescriptor.kt
@@ -5,6 +5,7 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.javascript.nodejs.interpreter.NodeCommandLineConfigurator
 import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
 import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreter
+import com.intellij.javascript.nodejs.interpreter.wsl.WslNodeInterpreter
 import com.intellij.lang.javascript.service.JSLanguageServiceUtil
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -18,7 +19,7 @@ class PrismaLspServerDescriptor(project: Project) : ProjectWideLspServerDescript
 
   override fun createCommandLine(): GeneralCommandLine {
     val interpreter = NodeJsInterpreterManager.getInstance(project).interpreter
-    if (interpreter !is NodeJsLocalInterpreter) {
+    if (interpreter !is NodeJsLocalInterpreter && interpreter !is WslNodeInterpreter) {
       // shouldn't happen, checked in PrismaLspServerSupportProvider
       throw ExecutionException(PrismaBundle.message("prisma.interpreter.not.configured"))
     }
