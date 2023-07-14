bfe77644ee82805b3b86d87f99d8388c041611bb
Konstantin Hudyakov <konstantin.hudyakov@jetbrains.com>
2023-07-14 11:29:00 +0300

    IFT-478 Fix Declarations and Usages lesson for Python and Ruby
    GitOrigin-RevId: 5b8aacb292c8be1adc7dde27ae807b16936c5dc9

--- ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/navigation/RubyDeclarationAndUsagesLesson.kt
+++ ide-features-trainer/ruby-features-trainer/src/org/jetbrains/ruby/ift/lesson/navigation/RubyDeclarationAndUsagesLesson.kt
@@ -1,6 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.ruby.ift.lesson.navigation
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
+import org.jetbrains.plugins.ruby.ruby.lang.psi.impl.variables.RIdentifierImpl
 import training.dsl.LessonContext
 import training.learn.lesson.general.navigation.DeclarationAndUsagesLesson
 
@@ -8,4 +11,8 @@ class RubyDeclarationAndUsagesLesson : DeclarationAndUsagesLesson() {
   override fun LessonContext.setInitialPosition() = caret(11, 19)
   override val sampleFilePath: String = "src/declaration_and_usages_demo.rb"
   override val entityName = "discriminant"
+
+  override fun getParentExpression(element: PsiElement): PsiElement? {
+    return element.parentOfType<RIdentifierImpl>(false)
+  }
 }
