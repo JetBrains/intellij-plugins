14b43f870be24dca5af3203bb5954752c712970e
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-07-17 16:08:36 +0200

    Angular: modernise GotoSymbol navigation test
    GitOrigin-RevId: 3081df87034e2867d3b8e3764157c3a3db3f80a3

--- AngularJS/test/org/angular2/codeInsight/navigation/Angular2GotoSymbolTest.kt
+++ AngularJS/test/org/angular2/codeInsight/navigation/Angular2GotoSymbolTest.kt
@@ -1,57 +1,16 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2.codeInsight.navigation
 
-import com.intellij.ide.util.gotoByName.GotoSymbolModel2
-import com.intellij.navigation.NavigationItem
-import com.intellij.openapi.vfs.VirtualFileFilter
-import com.intellij.psi.impl.PsiManagerEx
-import com.intellij.testFramework.UsefulTestCase
-import org.angular2.Angular2CodeInsightFixtureTestCase
-import org.angularjs.AngularTestUtil
+import org.angular2.Angular2TestCase
 
-class Angular2GotoSymbolTest : Angular2CodeInsightFixtureTestCase() {
-  override fun getTestDataPath(): String {
-    return AngularTestUtil.getBaseTestDataPath() + "navigation/symbol/" + getTestName(true)
-  }
+class Angular2GotoSymbolTest : Angular2TestCase("navigation/symbol/") {
 
-  fun testElementSelector() {
-    myFixture.configureByFiles("my-table.component.ts", "package.json")
-    doTest("app-my-table", true,
-           "app-my-table (MyTableComponent, my-table.component.ts) - my-table.component.ts [342]")
-  }
+  fun testElementSelector() = checkGotoSymbol("app-my-table")
 
-  fun testAttributeSelector() {
-    myFixture.configureByFiles("my-table.component.ts", "package.json")
-    doTest("app-my-table", true,
-           "app-my-table (MyTableComponent, my-table.component.ts) - my-table.component.ts [343]")
-  }
+  fun testAttributeSelector() = checkGotoSymbol("app-my-table")
 
-  fun testAttrAndElementSelector() {
-    myFixture.configureByFiles("my-table.component.ts", "package.json")
-    doTest("app-my-table", true,
-           "app-my-table (MyTableComponent, my-table.component.ts) - my-table.component.ts [342]",
-           "app-my-table (MyTableComponent, my-table.component.ts) - my-table.component.ts [355]")
-  }
+  fun testAttrAndElementSelector() = checkGotoSymbol("app-my-table")
 
-  fun testPipe() {
-    myFixture.configureByFiles("foo.pipe.ts", "package.json")
-    doTest("foo", false,
-           "foo (foo.pipe.ts)")
-  }
+  fun testPipe() = checkGotoSymbol("foo", detailed = false)
 
-  private fun doTest(name: String, detailed: Boolean, vararg expectedItems: String) {
-    (myFixture.getPsiManager() as PsiManagerEx).setAssertOnFileLoadingFilter(VirtualFileFilter.ALL, myFixture.testRootDisposable)
-    val model = GotoSymbolModel2(myFixture.getProject(), myFixture.testRootDisposable)
-    UsefulTestCase.assertContainsElements(listOf(*model.getNames(false)), name)
-    val actual = ArrayList<String>()
-    for (o in model.getElementsByName(name, false, "")) {
-      if (o is NavigationItem) {
-        val presentation = o.getPresentation()
-        assertNotNull(presentation)
-        actual.add(presentation!!.getPresentableText() + " " + presentation.locationString +
-                   if (detailed) " - $o" else "")
-      }
-    }
-    UsefulTestCase.assertSameElements(actual, *expectedItems)
-  }
 }
