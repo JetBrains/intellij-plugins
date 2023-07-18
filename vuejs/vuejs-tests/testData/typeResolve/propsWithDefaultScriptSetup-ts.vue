50ab4f0a910cf4afb720c8111d8ee91244589d33
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2023-07-18 05:58:09 +0300

    [vue] respect withDefaults for script setup props inside a component WEB-60438
    GitOrigin-RevId: c7c9a3138be3104af83f83104bfccf7ba7bb2630

---
+++ vuejs/vuejs-tests/testData/typeResolve/propsWithDefaultScriptSetup-ts.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{
+  msg?: string;
+}>(), {
+  msg: 'This is the default'
+})
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
