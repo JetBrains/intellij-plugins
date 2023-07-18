d2408593ce7940335e5086f2689939f3ca38c330
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-07-18 20:07:10 +0200

    simplify LspServerSupportProvider.fileOpened() implementations
    GitOrigin-RevId: fc7671714a66a5ab8cd8df4ac8934f43e529f24f

--- Astro/src/org/jetbrains/astro/service/AstroLspServerSupportProvider.kt
+++ Astro/src/org/jetbrains/astro/service/AstroLspServerSupportProvider.kt
@@ -1,4 +1,4 @@
-  // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.astro.service
 
 import com.intellij.lang.javascript.library.typings.TypeScriptPackageName
@@ -6,11 +6,10 @@ import com.intellij.lang.typescript.lsp.JSFrameworkLspServerDescriptor
 import com.intellij.lang.typescript.lsp.LspServerDownloader
 import com.intellij.lang.typescript.lsp.getLspServerExecutablePath
 import com.intellij.lang.typescript.lsp.scheduleLspServerDownloading
-import com.intellij.platform.lsp.api.LspServerDescriptor
-import com.intellij.platform.lsp.api.LspServerSupportProvider
-import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServerSupportProvider
+import com.intellij.platform.lsp.api.LspServerSupportProvider.LspServerStarter
 import com.intellij.util.text.SemVer
 import org.jetbrains.annotations.ApiStatus
 
@@ -21,17 +20,13 @@ val serverPackageName = TypeScriptPackageName(npmPackage, astroLanguageToolsVers
 
 class AstroLspServerSupportProvider : LspServerSupportProvider {
   override fun fileOpened(project: Project, file: VirtualFile, serverStarter: LspServerStarter) {
-    getAstroServerDescriptor(project, file)?.let { serverStarter.ensureServerStarted(it) }
+    if (isServiceEnabledAndAvailable(project, file)) {
+      serverStarter.ensureServerStarted(AstroLspServerDescriptor(project))
+    }
   }
 }
 
-fun getAstroServerDescriptor(project: Project, file: VirtualFile): LspServerDescriptor? {
-  if (!isServiceEnabledAndAvailable(project, file)) return null
-  return AstroLspServerDescriptor(project)
-}
-
-class AstroLspServerDescriptor(project: Project)
-  : JSFrameworkLspServerDescriptor(project, "Astro") {
+class AstroLspServerDescriptor(project: Project) : JSFrameworkLspServerDescriptor(project, "Astro") {
   override val relativeScriptPath = packageRelativePath
   override val npmPackage = serverPackageName
 
