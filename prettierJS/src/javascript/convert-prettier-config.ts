b0bfddcbd406eac4e342da4be0a0df39d693ae1f
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2023-07-19 19:54:15 +0200

    WEB-61964 Prettier 3.0: Code style from Prettier config is not applied
    (cherry picked from commit 98bdd66966682551dc76c154488682de168d4659)
    
    IJ-CR-111474
    
    GitOrigin-RevId: e6380bd5f263bb4f12f4208da7579e92d8db2e03

--- prettierJS/src/javascript/convert-prettier-config.ts
+++ prettierJS/src/javascript/convert-prettier-config.ts
@@ -5,6 +5,9 @@ const modulePath = process.argv[2];
 const configFilePath = process.argv[3];
 const prettier: PrettierApi = require(modulePath)
 
-let config = prettier.resolveConfig.sync(configFilePath, {config: configFilePath, editorconfig: false})
+async function main() {
+  let config = await prettier.resolveConfig(configFilePath, {config: configFilePath, editorconfig: false})
+  console.log(JSON.stringify(config))
+}
 
-console.log(JSON.stringify(config))
+main()
