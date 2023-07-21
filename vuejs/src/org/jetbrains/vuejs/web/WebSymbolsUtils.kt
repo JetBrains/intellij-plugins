18a6abe5ac881a51cf2486c7d4870d8c81d8ed67
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-07-21 11:28:50 +0200

    Web Symbols: allow to provide optional search or rename target through RenameableWebSymbol and SearchTargetWebSymbol.
    (cherry picked from commit fe5018641645e12b4f12a1de0cb67c24b8e74574)
    
    IJ-CR-111698
    
    GitOrigin-RevId: 8d17246b414082c79f55e8e9a2fbca77be2fdad9

--- vuejs/src/org/jetbrains/vuejs/web/WebSymbolsUtils.kt
+++ vuejs/src/org/jetbrains/vuejs/web/WebSymbolsUtils.kt
@@ -2,15 +2,11 @@
 package org.jetbrains.vuejs.web
 
 import com.intellij.html.webSymbols.elements.WebSymbolElementDescriptor
-import com.intellij.lang.javascript.psi.JSLiteralExpression
-import com.intellij.model.Symbol
-import com.intellij.refactoring.rename.api.RenameTarget
 import com.intellij.util.containers.Stack
 import com.intellij.webSymbols.WebSymbol
 import com.intellij.webSymbols.WebSymbol.Companion.NAMESPACE_HTML
 import com.intellij.webSymbols.WebSymbolsScope
 import com.intellij.webSymbols.query.WebSymbolsNameMatchQueryParams
-import com.intellij.webSymbols.refactoring.WebSymbolRenameTarget
 import com.intellij.webSymbols.utils.match
 import org.jetbrains.vuejs.codeInsight.toAsset
 import org.jetbrains.vuejs.model.*
@@ -20,7 +16,6 @@ import org.jetbrains.vuejs.web.VueWebSymbolsQueryConfigurator.Companion.PROP_VUE
 import org.jetbrains.vuejs.web.VueWebSymbolsQueryConfigurator.Companion.PROP_VUE_MODEL_PROP
 import org.jetbrains.vuejs.web.symbols.VueComponentSymbol
 import org.jetbrains.vuejs.web.symbols.VueDirectiveSymbol
-import org.jetbrains.vuejs.web.symbols.VueScopeElementSymbol
 import org.jetbrains.vuejs.web.symbols.VueScriptSetupLocalDirectiveSymbol
 
 fun WebSymbolElementDescriptor.getModel(): VueModelDirectiveProperties =
@@ -38,11 +33,6 @@ fun VueScopeElement.asWebSymbol(name: String, forcedProximity: VueModelVisitor.P
     is VueDirective -> VueDirectiveSymbol(name, this, forcedProximity)
     else -> null
   }
-
-fun createRenameTarget(symbol: Symbol): RenameTarget? =
-  if (symbol is VueScopeElementSymbol<*> && symbol.source is JSLiteralExpression)
-    WebSymbolRenameTarget(symbol)
-  else null
 
 fun VueModelVisitor.Proximity.asWebSymbolPriority(): WebSymbol.Priority =
   when (this) {
