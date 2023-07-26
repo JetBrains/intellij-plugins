d342b291fbc17c73a9a52182790ba85b524a8211
Elena Shaverdova <elena.shaverdova@jetbrains.com>
2023-07-26 20:28:09 +0200

    Remove option to switch Deno support from WorkspaceFileIndexContributor
    GitOrigin-RevId: 66cd1b2b5d25897c9a387d22307e4097f430cd70

--- Deno/src/com/intellij/deno/roots/DenoWorkspaceFileIndexContributor.kt
+++ Deno/src/com/intellij/deno/roots/DenoWorkspaceFileIndexContributor.kt
@@ -2,20 +2,19 @@ package com.intellij.deno.roots
 
 import com.intellij.deno.entities.DenoEntity
 import com.intellij.deno.excludeCondition
-import com.intellij.openapi.util.registry.Registry
+import com.intellij.platform.backend.workspace.virtualFile
+import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileKind
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetRegistrar
 import com.intellij.workspaceModel.core.fileIndex.impl.ModuleOrLibrarySourceRootData
-import com.intellij.platform.backend.workspace.virtualFile
-import com.intellij.platform.workspace.storage.EntityStorage
 
 class DenoWorkspaceFileIndexContributor : WorkspaceFileIndexContributor<DenoEntity> {
   override val entityClass: Class<DenoEntity>
     get() = DenoEntity::class.java
 
   override fun registerFileSets(entity: DenoEntity, registrar: WorkspaceFileSetRegistrar, storage: EntityStorage) {
-    if (!useWorkspaceModel() || !useWorkspaceFileIndexContributor()) return
+    if (!useWorkspaceModel()) return
     if (entity.denoTypes == null && entity.depsFile == null) return
     val allRootUrls = listOfNotNull(entity.denoTypes, entity.depsFile)
     val predicate = excludeCondition.transformToCondition(allRootUrls.mapNotNull { it.virtualFile })
@@ -25,6 +24,4 @@ class DenoWorkspaceFileIndexContributor : WorkspaceFileIndexContributor<DenoEnti
     }
   }
 }
-
-internal fun useWorkspaceFileIndexContributor() = Registry.`is`("deno.use.workspace.file.index.contributor.api")
 private class DenoFileSetData : ModuleOrLibrarySourceRootData
