c85090cb219d6bc30ae2ada75b742bf3b6eb5fe5
Sofya.Povarova <sofya.povarova@jetbrains.com>
2023-07-31 10:38:50 +0300

    WEB-61934 support for CSS trigonometric functions
    (cherry picked from commit a1a7f67b7aea9c0e0f8863e1a2727d5d6a609a48)
    
    IJ-CR-112298
    
    GitOrigin-RevId: 6342c1ab093d8c833449636fd515bc8200d37cae

--- vuejs/vuejs-tests/testData/highlighting/scssBuiltInModules.vue
+++ vuejs/vuejs-tests/testData/highlighting/scssBuiltInModules.vue
@@ -5,7 +5,7 @@
 //noinspection CssOverwrittenProperties
 * {
    height: <warning descr="Cannot find variable 'pi'">$pi</warning>;
-   height: <error descr="Unknown function 'asin'">asin</error>(0);
+   height: asin(0);
    height: slice('', 0, 1);
    height: str-slice('', 0, 1);
    height: <error descr="Unknown function 'bad'">bad</error>('', 0, 1);
