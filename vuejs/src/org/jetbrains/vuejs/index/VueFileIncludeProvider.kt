43916549d7f2935f87ee8557a68ceaef09ae60a2
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2023-08-02 19:09:10 +0200

    [js] extract JSFrameworkFileIncludeProvider (WEB-59680)
    (cherry picked from commit 85694d605ed68190485f938f01f30c549859b1e9)
    
    IJ-CR-112451
    
    GitOrigin-RevId: ac3ba69c928fde08ecf433b60cddb723da6e97f3

---
+++ vuejs/src/org/jetbrains/vuejs/index/VueFileIncludeProvider.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.index
+
+import com.intellij.lang.ecmascript6.index.ES6FileIncludeProvider
+import com.intellij.lang.ecmascript6.index.JSFrameworkFileIncludeProvider
+import com.intellij.lang.ecmascript6.psi.impl.ES6ImportPsiUtil
+import com.intellij.psi.impl.include.FileIncludeInfo
+import com.intellij.util.indexing.FileContent
+import org.jetbrains.vuejs.lang.html.VueFileType
+
+/**
+ * ES6FileIncludeProvider doesn't work for vue files but we need these files in index for building ts imports graph
+ * @see ES6FileIncludeProvider
+ */
+class VueFileIncludeProvider : JSFrameworkFileIncludeProvider(VueFileType.INSTANCE) {
+  override fun getIncludeInfos(content: FileContent): Array<FileIncludeInfo> {
+    if (!ES6FileIncludeProvider.checkTextHasFromKeyword(content)) return emptyArray()
+
+    val psiFile = content.psiFile
+    val importDeclarations = (findModule(psiFile, false)?.let { ES6ImportPsiUtil.getImportDeclarations(it) } ?: emptyList()) +
+                             (findModule(psiFile, true)?.let { ES6ImportPsiUtil.getImportDeclarations(it) } ?: emptyList())
+
+    return createFileIncludeInfos(importDeclarations)
+  }
+}
