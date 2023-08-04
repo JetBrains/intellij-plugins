720abb78126946629867edfdf8618fe8596f2ec8
Daniel Brauner <daniel.brauner@jetbrains.com>
2023-08-04 11:50:01 +0200

    CPP-33773 and CPP-33780 test for documentation from zephyr bindings
    GitOrigin-RevId: 95e4fb1a21a80a12e532d29c88b5203a3a29bbc1

--- dts/test/com/intellij/dts/DtsTestBase.kt
+++ dts/test/com/intellij/dts/DtsTestBase.kt
@@ -1,28 +1,35 @@
 package com.intellij.dts
 
+import com.intellij.dts.settings.DtsZephyrSettings
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.psi.PsiFile
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import java.nio.file.Files
+import java.nio.file.Path
 import java.util.*
+import kotlin.io.path.absolute
+import kotlin.io.path.pathString
 
 abstract class DtsTestBase : BasePlatformTestCase() {
     protected val testName: String
-        get() = getTestName(false).toPascalCase()
+        get() = getTestName(false)
+            .replace(Regex("[_.,-]+"), " ")
+            .toPascalCase()
 
     protected val testFile: String
         get() = "$testName.${getTestFileExtension()}"
 
     protected val testFilePath: String
-        get() = "$testDataPath/$testFile"
+        get() = "$testDataPath/$testName.${getTestFileExtension()}"
 
     protected open fun getTestFileExtension(): String = "dtsi"
 
     override fun getTestDataPath(): String = "testData/$basePath"
 
-    override fun getTestName(lowercaseFirstLetter: Boolean): String = super.getTestName(false)
-
     protected fun configureByText(text: String): String {
-        val fileName = "${text.hashCode()}.dtsi"
-        myFixture.configureByText(fileName, text)
+        val fileName = "${text.hashCode()}.${getTestFileExtension()}"
+        myFixture.configureByText(fileName, text.replaceCaret())
 
         return fileName
     }
@@ -30,10 +37,29 @@ abstract class DtsTestBase : BasePlatformTestCase() {
     protected fun addFile(path: String, text: String): PsiFile {
         return myFixture.addFileToProject(path, text)
     }
+
+    protected fun addZephyr() {
+        val zephyr = Path.of("testData/zephyr").absolute()
+
+        val manager = ModuleManager.getInstance(project)
+        ModuleRootModificationUtil.addContentRoot(manager.modules.first(), zephyr.pathString)
+
+        val settings = DtsZephyrSettings.of(project)
+        settings.board = "esp32"
+        settings.arch = "xtensa"
+    }
+
+    protected fun getTestFixture(extension: String): String {
+        return Files.readString(Path.of(testDataPath, "$testName.$extension")).replaceCaret()
+    }
 }
 
 private fun String.toPascalCase(): String {
     val capitalize = { word: String -> word.replaceFirstChar { it.titlecase(Locale.getDefault()) } }
 
     return split(' ').joinToString("", transform = capitalize)
+}
+
+private fun String.replaceCaret(): String {
+    return replace("/*caret*/", "<caret>")
 }
---
+++ dts/test/com/intellij/dts/documentation/DtsZephyrDocumentationTest.kt
@@ -0,0 +1,53 @@
+package com.intellij.dts.documentation
+
+import com.intellij.dts.DtsTestBase
+import com.intellij.lang.documentation.ide.IdeDocumentationTargetProvider
+import com.intellij.platform.backend.documentation.impl.computeDocumentationBlocking
+
+class DtsZephyrDocumentationTest : DtsTestBase() {
+    override fun getBasePath(): String = "documentation"
+
+    override fun setUp() {
+        super.setUp()
+        addZephyr()
+    }
+
+    fun `test property binding`() = doTest()
+
+    fun `test property default binding`() = doTest()
+
+    fun `test property inherited binding`() = doTest()
+
+    fun `test property default`() = doTest()
+
+    fun `test node binding`() = doTest()
+
+    fun `test node inherited binding`() = doTest()
+
+    fun `test node reference`() = doTest()
+
+    fun `test node default`() = doTest()
+
+    fun `test dts code`() = doTest()
+
+    private fun doTest() {
+        val content = getTestFixture("overlay")
+        val fixture = getTestFixture("html")
+
+        myFixture.configureByText("esp32.overlay", content)
+
+        val documentations = IdeDocumentationTargetProvider
+            .getInstance(project)
+            .documentationTargets(myFixture.editor, myFixture.file, myFixture.caretOffset - 1)
+            .mapNotNull { computeDocumentationBlocking(it.createPointer())?.html }
+
+        assertOneElement(documentations)
+        assertEquals(formatHtml(fixture), formatHtml(documentations.first()))
+    }
+
+    private fun formatHtml(text: String): String {
+        return text
+            .replace(Regex("\\s*\\n\\s*"), "")
+            .replace(Regex("(<[^>]+>(?=\\s*<))"), "$0\n")
+    }
+}
