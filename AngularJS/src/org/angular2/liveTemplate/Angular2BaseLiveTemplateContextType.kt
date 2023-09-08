2c289c0a5e8c0e07e3ce8a54c3663d70dbed3270
Christian Ivicevic <christian.ivicevic@jetbrains.com>
2023-09-08 12:17:50 +0200

    WEB-62739 Fix missing unambiguous auto-import feature for Angular live templates
    GitOrigin-RevId: 8edf691e46ebd1ac7b2d25d27f928812d74b8cf7

--- AngularJS/src/org/angular2/liveTemplate/Angular2BaseLiveTemplateContextType.kt
+++ AngularJS/src/org/angular2/liveTemplate/Angular2BaseLiveTemplateContextType.kt
@@ -2,10 +2,11 @@
 package org.angular2.liveTemplate
 
 import com.intellij.codeInsight.template.TemplateContextType
+import com.intellij.lang.javascript.liveTemplates.JSLikeTemplateContextType
 import com.intellij.psi.PsiFile
 import org.angular2.lang.Angular2Bundle
 import org.angular2.lang.Angular2LangUtil
 
-class Angular2BaseLiveTemplateContextType : TemplateContextType(Angular2Bundle.message("angular.live.template.context.angular")) {
+class Angular2BaseLiveTemplateContextType : TemplateContextType(Angular2Bundle.message("angular.live.template.context.angular")), JSLikeTemplateContextType {
   override fun isInContext(file: PsiFile, offset: Int) = Angular2LangUtil.isAngular2Context(file, offset)
 }
