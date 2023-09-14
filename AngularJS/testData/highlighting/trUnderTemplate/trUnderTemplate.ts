b4610f20a1053abde289e903902cef2fe79a6026
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-09-14 09:53:03 +0200

    WEB-62508 WEB-62998 Angular: fix issues with standard elements treated as components.
    GitOrigin-RevId: 84ac5980a8daf9183e2d5597e9d789951569bc6a

---
+++ AngularJS/testData/highlighting/trUnderTemplate/trUnderTemplate.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './trUnderTemplate.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+})
+export class AppComponent {
+}
