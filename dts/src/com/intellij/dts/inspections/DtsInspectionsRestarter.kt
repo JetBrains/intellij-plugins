755a9ee9fcda756271d1909743f393d8e230bfaa
Daniel Brauner <daniel.brauner@jetbrains.com>
2023-10-16 18:13:24 +0200

    [devicetree language support] CPP-35489 restart inspections on settings change
    GitOrigin-RevId: 854909d9ed73cd83e0a65a45f4fa3ace64d1ab2d

---
+++ dts/src/com/intellij/dts/inspections/DtsInspectionsRestarter.kt
@@ -0,0 +1,23 @@
+package com.intellij.dts.inspections
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
+import com.intellij.dts.lang.DtsFileType
+import com.intellij.dts.settings.DtsSettings
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiManager
+
+class DtsInspectionsRestarter(private val project: Project) : DtsSettings.ChangeListener {
+    override fun settingsChanged(settings: DtsSettings) = ApplicationManager.getApplication().runWriteAction {
+        val codeAnalyzer = DaemonCodeAnalyzer.getInstance(project)
+        val psiManger = PsiManager.getInstance(project)
+        val fileEditor = FileEditorManager.getInstance(project)
+
+        fileEditor.allEditors
+            .map { it.file }
+            .filter { it.fileType == DtsFileType }
+            .mapNotNull { psiManger.findFile(it) }
+            .forEach { codeAnalyzer.restart(it) }
+    }
+}
