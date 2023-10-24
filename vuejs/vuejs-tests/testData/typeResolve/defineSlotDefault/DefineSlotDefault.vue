daa1892e07b5a745604cedd656ee652605bb1209
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2023-10-24 12:10:22 +0300

    [vue] default v-slot support with defineSlots WEB-61142
    (cherry picked from commit 132a81d77c7842fdd78604df18300c2899f71d6f)
    
    IJ-CR-117941
    
    GitOrigin-RevId: 107414f037b133897042f211bfd01905cc0c3298

---
+++ vuejs/vuejs-tests/testData/typeResolve/defineSlotDefault/DefineSlotDefault.vue
@@ -0,0 +1,21 @@
+<script setup lang='ts'>
+import TextField from './TextField.vue'
+</script>
+
+<template>
+  <TextField v-slot='{ field }'>
+    {{ field.class }}
+  </TextField>
+
+  <TextField>
+    <template v-slot='{ field }'>
+      {{ field.style }}
+    </template>
+  </TextField>
+
+  <TextField>
+    <template v-slot:default='{ field }'>
+      {{ field.value }}
+    </template>
+  </TextField>
+</template>
