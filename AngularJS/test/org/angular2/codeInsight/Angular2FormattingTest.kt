e285398ea2727222fcc3d86421d0696d0a17902f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-10-27 18:38:52 +0200

    WEB-31573 Angular: add tests for interpolations
    IJ-CR-118094
    
    (cherry picked from commit f5ad83f5fc5ac0679ac25cf82ac79b8875a024b8)
    
    GitOrigin-RevId: 100306ff64e78fd03fc00c941fd66d90f5cfeeed

--- AngularJS/test/org/angular2/codeInsight/Angular2FormattingTest.kt
+++ AngularJS/test/org/angular2/codeInsight/Angular2FormattingTest.kt
@@ -2,12 +2,16 @@
 package org.angular2.codeInsight
 
 import com.intellij.javascript.web.WebFrameworkTestModule
+import com.intellij.lang.html.HTMLLanguage
+import com.intellij.lang.javascript.JSTestUtils
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings
 import com.intellij.psi.formatter.xml.HtmlCodeStyleSettings
 import org.angular2.Angular2TestCase
+import org.angular2.lang.html.Angular2HtmlLanguage
+import org.angular2.lang.html.psi.formatter.Angular2HtmlCodeStyleSettings
 
 class Angular2FormattingTest : Angular2TestCase("formatting") {
 
@@ -39,13 +43,74 @@ class Angular2FormattingTest : Angular2TestCase("formatting") {
     myFixture.type("\ntest2\n[test]=\"\"\n[(banana)]=\"\"\nother\n")
   }
 
+  fun testInterpolationNoNewLineDoNotWrap() = testInterpolation(
+    newLineAfterStart = false,
+    newLineBeforeEnd = false,
+    wrap = CommonCodeStyleSettings.DO_NOT_WRAP
+  )
+
+  fun testInterpolationNoNewLineWrapAsNeeded() = testInterpolation(
+    newLineAfterStart = false,
+    newLineBeforeEnd = false,
+    wrap = CommonCodeStyleSettings.WRAP_AS_NEEDED
+  )
+
+  fun testInterpolationNoNewLineWrapAlways() = testInterpolation(
+    newLineAfterStart = false,
+    newLineBeforeEnd = false,
+    wrap = CommonCodeStyleSettings.WRAP_ALWAYS
+  )
+
+  fun testInterpolationNewLineDoNotWrap() = testInterpolation(
+    newLineAfterStart = true,
+    newLineBeforeEnd = true,
+    wrap = CommonCodeStyleSettings.DO_NOT_WRAP
+  )
+
+  fun testInterpolationNewLineWrapAsNeeded() = testInterpolation(
+    newLineAfterStart = true,
+    newLineBeforeEnd = true,
+    wrap = CommonCodeStyleSettings.WRAP_AS_NEEDED
+  )
+
+  fun testInterpolationNewLineWrapAlways() = testInterpolation(
+    newLineAfterStart = true,
+    newLineBeforeEnd = true,
+    wrap = CommonCodeStyleSettings.WRAP_ALWAYS
+  )
+
+  fun testInterpolationNewLineBeforeEnd() = testInterpolation(
+    newLineAfterStart = false,
+    newLineBeforeEnd = true,
+    wrap = CommonCodeStyleSettings.WRAP_AS_NEEDED
+  )
+
+  fun testInterpolationNewLineAfterStart() = testInterpolation(
+    newLineAfterStart = true,
+    newLineBeforeEnd = false,
+    wrap = CommonCodeStyleSettings.WRAP_AS_NEEDED
+  )
+
   private fun testFormatting(vararg modules: WebFrameworkTestModule,
                              dir: Boolean = false,
                              extension: String = "ts",
+                             configureFileName: String = "$testName.$extension",
                              configureCodeStyleSettings: CodeStyleSettings.() -> Unit = {}) =
-    doConfiguredTest(*modules, dir = dir, extension = extension, checkResult = true, configureCodeStyleSettings = configureCodeStyleSettings) {
+    doConfiguredTest(*modules, dir = dir, extension = extension,
+                     checkResult = true, configureFileName = configureFileName,
+                     configureCodeStyleSettings = configureCodeStyleSettings) {
       val codeStyleManager = CodeStyleManager.getInstance(project)
       WriteCommandAction.runWriteCommandAction(project) { codeStyleManager.reformat(file) }
+    }
+
+  private fun testInterpolation(newLineAfterStart: Boolean, newLineBeforeEnd: Boolean, wrap: Int) =
+    testFormatting(configureFileName = "interpolation.html") {
+      val vueSettings = getCustomSettings(Angular2HtmlCodeStyleSettings::class.java)
+      vueSettings.SPACES_WITHIN_INTERPOLATION_EXPRESSIONS = false
+      getLanguageIndentOptions(HTMLLanguage.INSTANCE).INDENT_SIZE = 4
+      vueSettings.INTERPOLATION_NEW_LINE_BEFORE_END_DELIMITER = newLineBeforeEnd
+      vueSettings.INTERPOLATION_NEW_LINE_AFTER_START_DELIMITER = newLineAfterStart
+      vueSettings.INTERPOLATION_WRAP = wrap
     }
 
 }
