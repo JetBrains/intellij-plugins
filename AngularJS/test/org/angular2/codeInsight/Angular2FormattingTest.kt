a60524cfbd2885440852c23880d8f3ddfac66b84
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-10-27 17:48:34 +0200

    WEB-31573 Angular: modernize formatting test
    GitOrigin-RevId: 0e202eb626b1f27158fbd92238c0608ac51e1dbc

--- AngularJS/test/org/angular2/codeInsight/Angular2FormattingTest.kt
+++ AngularJS/test/org/angular2/codeInsight/Angular2FormattingTest.kt
@@ -1,70 +1,51 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2.codeInsight
 
-import com.intellij.application.options.CodeStyle
+import com.intellij.javascript.web.WebFrameworkTestModule
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.psi.PsiElement
 import com.intellij.psi.codeStyle.CodeStyleManager
 import com.intellij.psi.codeStyle.CodeStyleSettings
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings
 import com.intellij.psi.formatter.xml.HtmlCodeStyleSettings
-import org.angular2.Angular2CodeInsightFixtureTestCase
-import org.angularjs.AngularTestUtil
+import org.angular2.Angular2TestCase
 
-class Angular2FormattingTest : Angular2CodeInsightFixtureTestCase() {
-  override fun getTestDataPath(): String {
-    return AngularTestUtil.getBaseTestDataPath() + "formatting"
-  }
+class Angular2FormattingTest : Angular2TestCase("formatting") {
 
-  fun testStyles() {
-    doTest("stylesFormatting_after.ts", "stylesFormatting.ts")
-  }
+  fun testStyles() = testFormatting()
 
-  fun testTemplate() {
-    doTest("templateFormatting_after.ts", "templateFormatting.ts")
-  }
+  fun testTemplate() = testFormatting()
 
-  fun testAttrs() {
-    val htmlSettings = settings.getCustomSettings(HtmlCodeStyleSettings::class.java)
+  fun testAttrs() = testFormatting(extension = "html") {
+    val htmlSettings = getCustomSettings(HtmlCodeStyleSettings::class.java)
     htmlSettings.HTML_ATTRIBUTE_WRAP = CommonCodeStyleSettings.WRAP_ALWAYS
     htmlSettings.HTML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE = true
-    doTest("attrFormatting_after.html", "attrFormatting.html", "attrFormatting.ts")
   }
 
-  fun testInnerAttrs() {
-    val htmlSettings = settings.getCustomSettings(HtmlCodeStyleSettings::class.java)
+  fun testInnerAttrs() = testFormatting {
+    val htmlSettings = getCustomSettings(HtmlCodeStyleSettings::class.java)
     htmlSettings.HTML_ATTRIBUTE_WRAP = CommonCodeStyleSettings.WRAP_ALWAYS
     htmlSettings.HTML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE = true
-    doTest("innerAttrFormatting_after.ts", "innerAttrFormatting.ts")
   }
 
-  fun testNoKeepLineBreaks() {
-    val htmlSettings = settings.getCustomSettings(HtmlCodeStyleSettings::class.java)
+  fun testNoKeepLineBreaks() = testFormatting(extension = "html") {
+    val htmlSettings = getCustomSettings(HtmlCodeStyleSettings::class.java)
     htmlSettings.HTML_ATTRIBUTE_WRAP = CommonCodeStyleSettings.WRAP_AS_NEEDED
     htmlSettings.HTML_SPACE_AROUND_EQUALITY_IN_ATTRIBUTE = false
     htmlSettings.HTML_KEEP_LINE_BREAKS = false
     htmlSettings.HTML_KEEP_BLANK_LINES = 0
-    doTest("noKeepLineBreakFormatting_after.html", "noKeepLineBreakFormatting.html")
   }
 
-  fun testAttributeTyping() {
-    myFixture.configureByFiles("attrTyping.html", "package.json")
+  fun testAttributeTyping() = doConfiguredTest(extension = "html", checkResult = true) {
     myFixture.type("\ntest2\n[test]=\"\"\n[(banana)]=\"\"\nother\n")
-    myFixture.checkResultByFile("attrTyping_after.html")
   }
 
-  private fun doTest(expectedFile: String, vararg before: String) {
-    myFixture.configureByFile("package.json")
-    val psiFile = myFixture.configureByFiles(*before)[0]
-    doReformat(psiFile)
-    myFixture.checkResultByFile(expectedFile)
-  }
+  private fun testFormatting(vararg modules: WebFrameworkTestModule,
+                             dir: Boolean = false,
+                             extension: String = "ts",
+                             configureCodeStyleSettings: CodeStyleSettings.() -> Unit = {}) =
+    doConfiguredTest(*modules, dir = dir, extension = extension, checkResult = true, configureCodeStyleSettings = configureCodeStyleSettings) {
+      val codeStyleManager = CodeStyleManager.getInstance(project)
+      WriteCommandAction.runWriteCommandAction(project) { codeStyleManager.reformat(file) }
+    }
 
-  private fun doReformat(file: PsiElement) {
-    val codeStyleManager = CodeStyleManager.getInstance(project)
-    WriteCommandAction.runWriteCommandAction(project) { codeStyleManager.reformat(file) }
-  }
-
-  private val settings: CodeStyleSettings
-    get() = CodeStyle.getSettings(project)
 }
