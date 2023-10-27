84733921f58e3f00a179d4a474cffbfbfe7830b6
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-10-27 23:04:48 +0200

    WEB-63600 Angular 17: Brace matching for Angular blocks
    IJ-CR-118094
    
    (cherry picked from commit 7db1e7f26fa725d5666af8a2673c23f93428181c)
    
    GitOrigin-RevId: 67c57c14e02d7ec002998ed0a991a88338111dfa

---
+++ AngularJS/src/org/angular2/lang/html/Angular17HtmlBraceMatcher.kt
@@ -0,0 +1,37 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.angular2.lang.html
+
+import com.intellij.lang.BracePair
+import com.intellij.lang.PairedBraceMatcher
+import com.intellij.openapi.fileTypes.FileType
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.IElementType
+import com.intellij.xml.impl.XmlBraceMatcher
+import org.angular2.lang.html.lexer.Angular2HtmlTokenTypes
+
+class Angular17HtmlBraceMatcher : XmlBraceMatcher() {
+
+  override fun getPairedBraceMatcher(tokenType: IElementType?): PairedBraceMatcher =
+    super.getPairedBraceMatcher(tokenType) ?: AngularBlocksBraceMatcher
+
+  override fun isFileTypeWithSingleHtmlTags(fileType: FileType?): Boolean =
+    fileType == Angular17HtmlFileType.INSTANCE
+
+  private object AngularBlocksBraceMatcher : PairedBraceMatcher {
+
+    private val pairs = arrayOf(
+      BracePair(Angular2HtmlTokenTypes.BLOCK_START, Angular2HtmlTokenTypes.BLOCK_END, false),
+      BracePair(Angular2HtmlTokenTypes.BLOCK_PARAMETERS_START, Angular2HtmlTokenTypes.BLOCK_PARAMETERS_END, false)
+    )
+
+    override fun getPairs(): Array<BracePair> = pairs
+
+    override fun isPairedBracesAllowedBeforeType(lbraceType: IElementType, contextType: IElementType?): Boolean =
+      true
+
+    override fun getCodeConstructStart(file: PsiFile?, openingBraceOffset: Int): Int =
+      openingBraceOffset
+
+  }
+
+}
