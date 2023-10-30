691d8852f070ff566d681165de62909ccc72274d
Daniel Brauner <daniel.brauner@jetbrains.com>
2023-10-30 15:17:02 +0100

    [devicetree language support] CPP-35489 tests for DtsRequiredPropertyInspection
    (cherry picked from commit 27762072c41b11137d5b35ef245df082079bbd8a)
    
    IJ-CR-118204
    
    GitOrigin-RevId: 058431b3e23732885ddbc5ffb8de6e1e483ba28b

---
+++ dts/test/com/intellij/dts/inspections/DtsRequiredPropertyInspectionTest.kt
@@ -0,0 +1,27 @@
+package com.intellij.dts.inspections
+
+class DtsRequiredPropertyInspectionTest : DtsInspectionTest(DtsRequiredPropertyInspection::class) {
+    override fun getBasePath(): String = "inspections/requiredProperty"
+
+    override fun getTestFileExtension(): String = "dts"
+
+    override fun setUp() {
+        super.setUp()
+        addZephyr()
+
+        val declaration = getFixture("inspections/requiredProperty/Declaration.dtsi")
+        addFile("Declaration.dtsi", declaration)
+    }
+
+    fun `test node`() = doTest()
+
+    fun `test ref node`() = doTest()
+
+    fun `test delete property`() = doTest()
+
+    fun `test delete node`() = doTest()
+
+    fun `test delete node ref`() = doTest()
+
+    fun `test delete node root`() = doTest()
+}
