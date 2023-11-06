42d03e0b88ab55f5871a89ed3263235448a3baf6
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-11-06 15:45:36 +0100

    WEB-63684 Don't apply signal highlighting to Signal type and other types
    (cherry picked from commit ca10bc975a30730b8d61436a5de2b6ca342d8bfc)
    
    IJ-CR-118983
    
    GitOrigin-RevId: e61676ee755dd44da0ba7d67b55104872782db8c

--- AngularJS/src/org/angular2/signals/Angular2SignalUtils.kt
+++ AngularJS/src/org/angular2/signals/Angular2SignalUtils.kt
@@ -3,7 +3,6 @@ package org.angular2.signals
 
 import com.intellij.javascript.web.js.WebJSResolveUtil
 import com.intellij.lang.javascript.psi.JSExpression
-import com.intellij.lang.javascript.psi.JSTypeUtils
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptFunction
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptTypeAlias
 import com.intellij.lang.javascript.psi.resolve.JSResolveUtil
@@ -12,6 +11,7 @@ import com.intellij.lang.javascript.psi.types.JSCompositeTypeFactory
 import com.intellij.lang.javascript.psi.types.JSGenericTypeImpl
 import com.intellij.lang.javascript.psi.types.JSUnionOrIntersectionType
 import com.intellij.lang.javascript.psi.types.guard.TypeScriptTypeRelations
+import com.intellij.lang.javascript.psi.types.recordImpl.ComputedPropertySignatureImpl
 import com.intellij.psi.PsiElement
 import org.angular2.lang.Angular2LangUtil
 
@@ -35,23 +35,27 @@ object Angular2SignalUtils {
   fun supportsSignals(context: PsiElement?) =
     signalTypeAlias(context) != null
 
-  fun isSignal(elementOrExpression: PsiElement?): Boolean {
-    val signalType = signalTypeAlias(elementOrExpression)
+  fun isSignal(targetElement: PsiElement?): Boolean {
+    val signalTypeAlias = signalTypeAlias(targetElement)
+    if (signalTypeAlias == targetElement) return false
+
+    val signalType = signalTypeAlias
       ?.jsType
       ?.let { JSGenericTypeImpl(it.source, it, JSAnyType.get(it.source)) }
     if (signalType != null) {
       val elementType = JSCompositeTypeFactory.optimizeTypeIfComposite(
         TypeScriptTypeRelations.expandAndOptimizeTypeRecursive(
-          if (elementOrExpression is JSExpression)
-            JSResolveUtil.getExpressionJSType(elementOrExpression)
+          if (targetElement is JSExpression)
+            JSResolveUtil.getExpressionJSType(targetElement)
           else
-            JSResolveUtil.getElementJSType(elementOrExpression),
+            JSResolveUtil.getElementJSType(targetElement),
         ),
         JSUnionOrIntersectionType.OptimizedKind.OPTIMIZED_REMOVED_NULL_UNDEFINED
       )
       if (elementType != null
-          && !JSTypeUtils.isAnyType(elementType)
-          && signalType.isDirectlyAssignableType(elementType, null)) {
+          && elementType.asRecordType().properties.any { it is ComputedPropertySignatureImpl && it.memberName == "SIGNAL" }
+          && signalType.isDirectlyAssignableType(elementType, null)
+      ) {
         return true
       }
     }
