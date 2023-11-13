fba23155cf88f03c2d6f47c0da168a81fa7b76b8
Daniel Brauner <daniel.brauner@jetbrains.com>
2023-11-13 12:04:29 +0100

    [devicetree language support] CPP-35494 error shown on value instead of property name
    GitOrigin-RevId: 9e9fb5bde61d2e066bd4013792ced944c3dae65e

--- dts/src/com/intellij/dts/inspections/DtsPropertyTypeInspection.kt
+++ dts/src/com/intellij/dts/inspections/DtsPropertyTypeInspection.kt
@@ -4,13 +4,26 @@ import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.dts.lang.dtsAssignableTo
 import com.intellij.dts.lang.psi.DtsNode
+import com.intellij.dts.lang.psi.DtsProperty
 import com.intellij.dts.lang.psi.dtsVisitor
+import com.intellij.dts.util.relativeTo
 import com.intellij.dts.zephyr.binding.DtsZephyrBindingProvider
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.refactoring.suggested.endOffset
+import com.intellij.refactoring.suggested.startOffset
 
 class DtsPropertyTypeInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitor {
         return dtsVisitor(DtsNode::class) { checkPropertyType(it, holder) }
+    }
+
+    private fun rangeInProperty(property: DtsProperty): TextRange? {
+        val values = property.dtsValues
+        if (values.isEmpty()) return null
+
+        val valuesRange = TextRange(values.first().startOffset, values.last().endOffset)
+        return valuesRange.relativeTo(property.textRange)
     }
 
     private fun checkPropertyType(node: DtsNode, holder: ProblemsHolder) {
@@ -24,6 +37,7 @@ class DtsPropertyTypeInspection : LocalInspectionTool() {
                 property,
                 bundleKey = "inspections.property_type.error",
                 bundleParam = propertyBinding.type.typeName,
+                rangeInElement = rangeInProperty(property),
             )
         }
     }
---
+++ dts/test/com/intellij/dts/ide/DtsIterateLeafsTest.kt
@@ -0,0 +1,76 @@
+package com.intellij.dts.ide
+
+import com.intellij.dts.DtsTestBase
+import com.intellij.dts.lang.DtsTokenSets
+import com.intellij.dts.util.DtsUtil
+import com.intellij.psi.TokenType
+import com.intellij.psi.impl.source.tree.LeafElement
+import com.intellij.psi.impl.source.tree.RecursiveTreeElementWalkingVisitor
+import com.intellij.psi.impl.source.tree.TreeElement
+
+private class PsiToString : RecursiveTreeElementWalkingVisitor() {
+    val buffer = StringBuffer()
+
+    override fun visitLeaf(leaf: LeafElement) {
+        val type = leaf.elementType
+
+        if (type !in DtsTokenSets.comments && type != TokenType.WHITE_SPACE) {
+            buffer.append(leaf.text)
+        }
+    }
+}
+
+class DtsIterateLeafsTest : DtsTestBase() {
+    private val unproductiveStatements = """
+        // comment
+        /include/ "file"
+        #include "file"
+        /*comment*/
+    """
+
+    override fun getBasePath(): String = "ide/iterateLeafs"
+
+    fun `test root node`() = doTest(
+        input = "/ {};",
+        leafs = listOf("/", "{", "}", ";"),
+    )
+
+    fun `test root node with unproductive statement`() = doTest(
+        input = "/ { $unproductiveStatements };",
+        leafs = listOf("/", "{", "}", ";"),
+    )
+
+    fun `test root node with unproductive`() = doTest(
+        input = "/ { };",
+        leafs = listOf("/", "{", "}", ";"),
+    )
+
+    fun `test property`() = doTest(
+        input = "/ { prop = $unproductiveStatements <>; };",
+        leafs = listOf("/", "{", "prop", "=", "<", ">", ";", "}", ";")
+    )
+
+    fun `test file`() {
+        val fixture = getTestFixture("dts")
+
+        configureByText(fixture)
+        val file = myFixture.file
+
+        val start = file.findElementAt(0)
+        val actual = DtsUtil.iterateLeafs(start!!, strict = false).map { it.text }.joinToString("")
+
+        val visitor = PsiToString()
+        (file.node as TreeElement).acceptTree(visitor)
+
+        assertEquals(visitor.buffer.toString(), actual)
+    }
+
+    private fun doTest(input: String, leafs: List<String>) {
+        configureByText(input)
+
+        val start = myFixture.file.findElementAt(0)
+        val actual = DtsUtil.iterateLeafs(start!!, strict = false).map { it.text }.toList()
+
+        assertOrderedEquals(actual, leafs)
+    }
+}
