e8601e4ef14b20012f48a05d2f8a7c749645ac3e
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-11-16 09:53:14 +0100

    WEB-63853 Angular: false validation message with ngSrc.
    (cherry picked from commit 04907010ebd6f4129211b21ccaac5e9ca003988a)
    
    IJ-CR-120008
    
    GitOrigin-RevId: ac2b30c901cb47e7c3dbbc04350832dcd15f1357

--- AngularJS/test/org/angular2/codeInsight/Angular2HighlightingTest.kt
+++ AngularJS/test/org/angular2/codeInsight/Angular2HighlightingTest.kt
@@ -144,6 +144,9 @@ class Angular2HighlightingTest : Angular2TestCase("highlighting") {
 
   fun testStandardAttributesStrictMode() = checkHighlighting(ANGULAR_CORE_16_2_8, ANGULAR_MATERIAL_16_2_8, strictTemplates = true, extension = "ts")
 
+  fun testNgSrcAttribute() = checkHighlighting(ANGULAR_CORE_17_0_0_RC_0, ANGULAR_COMMON_17_0_0_RC_0,
+                                               strictTemplates = true, extension = "ts", dir = true)
+
   override fun setUp() {
     super.setUp()
     myFixture.enableInspections(Angular2TemplateInspectionsProvider())
