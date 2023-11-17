9370ceea6fc5048e6d825157d95d95c4434a108d
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-11-17 12:51:32 +0100

    WEB-63744 Angular 17: support type narrowing - support @if control flow
    GitOrigin-RevId: 4320d145bbc3f2cdf646b3d7d1f86c542bfce88d

---
+++ AngularJS/testData/controlflow/IfBlock.ts
@@ -0,0 +1,39 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import { Component } from '@angular/core';
+
+@Component({
+ selector: 'todos',
+ template: `
+    @if (foo === 'b')  {
+      {{ test(<error descr="Argument type \"b\" is not assignable to parameter type \"f\"">foo</error>) }}
+    } @else if (foo === 'd') {
+      {{ test(<error descr="Argument type \"a\" | undefined is not assignable to parameter type \"f\"  Type \"a\" is not assignable to type \"f\"">foo</error>) }}
+    } @else if (foo === 'a') {
+      {{ test(<error descr="Argument type \"a\" is not assignable to parameter type \"f\"">foo</error>) }}
+    } @else {
+      {{ test(<error descr="Argument type undefined is not assignable to parameter type \"f\"">foo</error>) }}
+    }
+  `,
+   standalone: true,
+ })
+export class ComponentStoreTodosComponent {
+
+  foo!: 'a' | 'b' | undefined;
+
+  test(a: 'f'): boolean {
+    return a === 'f'
+  }
+
+  constructor() {
+    if (this.foo === 'b') {
+      this.test(<error descr="Argument type \"b\" is not assignable to parameter type \"f\"">this.foo</error>);
+    } else if (this.foo === 'd') {
+      this.test(<error descr="Argument type \"a\" | undefined is not assignable to parameter type \"f\"  Type \"a\" is not assignable to type \"f\"">this.foo</error>);
+    } else if (this.foo === 'a') {
+      this.test(<error descr="Argument type \"a\" is not assignable to parameter type \"f\"">this.foo</error>);
+    } else {
+      this.test(<error descr="Argument type undefined is not assignable to parameter type \"f\"">this.foo</error>);
+    }
+  }
+
+}
