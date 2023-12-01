fd817a6cca26e3b5e78819175fc6e2617cdc743c
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-01 14:10:25 +0100

    WEB-42600 Not respecting indention in inline templates in Angular - set OverrideLanguageOptions to true when using EditorConfig.
    GitOrigin-RevId: 68493ae723de0e408aed3384cc17fb5dcca5178b

---
+++ AngularJS/testData/formatting/editorConfigWithInjection/before/editorConfigWithInjection.ts
@@ -0,0 +1,23 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {CdkVirtualForOf} from "@angular/cdk/scrolling";
+
+@Component({
+       selector: 'app-root',
+   standalone: true,
+  imports: [CommonModule, RouterOutlet, CdkVirtualForOf],
+template:  `
+      <div class="left-side">
+    <div *cdkVirtualFor="let item    of items; let i = index">
+  {{ item.b }}
+                         </div>
+   </div>
+  `,
+  styleUrl: './app.component.css'})
+export class AppComponent {
+title = 'untitled372';
+
+        items = [{a: 12}, {a: 14}]
+}
