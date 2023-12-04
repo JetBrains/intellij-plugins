efb2a05877c58932cf364e9df8b3bc8ea0e2e038
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-04 10:03:09 +0100

    WEB-63576 Angular 17: Documentation support for Angular blocks - basic use case
    (cherry picked from commit f6277fdf4a1b1acb67004a8137c8dc485628192e)
    
    IJ-CR-121811
    
    GitOrigin-RevId: 12b964ca2b0cde2b03e7ab453c10d75be1d7b31e

---
+++ AngularJS/src/org/angular2/web/references/Angular2BlockReferencesProvider.kt
@@ -0,0 +1,15 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.angular2.web.references
+
+import com.intellij.webSymbols.WebSymbol
+import com.intellij.webSymbols.references.WebSymbolReferenceProvider
+import org.angular2.lang.html.psi.Angular2HtmlBlock
+
+class Angular2BlockReferencesProvider : WebSymbolReferenceProvider<Angular2HtmlBlock>() {
+
+  override fun getSymbol(psiElement: Angular2HtmlBlock): WebSymbol? =
+    psiElement.definition?.symbol
+
+  override fun getSymbolNameOffset(psiElement: Angular2HtmlBlock): Int = 1
+
+}
