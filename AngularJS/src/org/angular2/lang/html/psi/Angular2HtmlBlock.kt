cad0565c87122113acd6cf4cae7ae2041106cca6
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-04 10:03:09 +0100

    WEB-63576 Angular 17: Documentation support for Angular blocks - basic use case
    GitOrigin-RevId: f6277fdf4a1b1acb67004a8137c8dc485628192e

--- AngularJS/src/org/angular2/lang/html/psi/Angular2HtmlBlock.kt
+++ AngularJS/src/org/angular2/lang/html/psi/Angular2HtmlBlock.kt
@@ -1,11 +1,12 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2.lang.html.psi
 
+import com.intellij.model.psi.PsiExternalReferenceHost
 import com.intellij.psi.PsiElement
 import org.angular2.codeInsight.blocks.Angular2HtmlBlockDefinition
 import org.angular2.lang.expr.psi.Angular2BlockParameter
 
-interface Angular2HtmlBlock : PsiElement {
+interface Angular2HtmlBlock : PsiElement, PsiExternalReferenceHost {
 
   fun getName(): String
 
