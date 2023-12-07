91f1b51570bd286e32d164be015c26dcdaf81ea7
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-07 16:09:00 +0100

    WEB-64476 Angular @for block no error when not using an iterator
    (cherry picked from commit f9bf304a0b18bc5e0dc7e56189af630729dc7b80)
    
    IJ-CR-121811
    
    GitOrigin-RevId: e899823d209b19bd45a4d9bf5aa16e82ce097617

---
+++ AngularJS/testData/highlighting/forBlockIterableType.ts
@@ -0,0 +1,17 @@
+// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+import { Component } from '@angular/core';
+
+@Component({
+ selector: 'todos',
+ template: `
+    @for (foo of iter; track $index)  {}
+    @for (foo of <error descr="Type { name: string } must have a [Symbol.iterator]() method that returns an iterator.">nonIter</error>; track $index)  {}
+  `,
+   standalone: true,
+ })
+export class ComponentStoreTodosComponent {
+
+  iter!: { name: string }[];
+  nonIter!: { name: string };
+
+}
