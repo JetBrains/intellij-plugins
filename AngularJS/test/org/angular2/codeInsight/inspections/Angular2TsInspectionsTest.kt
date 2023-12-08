a7b5107522844e72c53306a66a2d0caa6e8e05b9
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-08 10:00:56 +0100

    WEB-63956 Angular 17: migrate Angular2TsInspectionsTest to Angular2TestCase
    (cherry picked from commit 06edaea722fe687ca555339c4231cb83fcc84216)
    
    IJ-CR-121840
    
    GitOrigin-RevId: d643dac4366548c9aea4970989cd803ff3528727

--- AngularJS/test/org/angular2/codeInsight/inspections/Angular2TsInspectionsTest.kt
+++ AngularJS/test/org/angular2/codeInsight/inspections/Angular2TsInspectionsTest.kt
@@ -8,105 +8,80 @@ import com.intellij.lang.javascript.JSTestUtils
 import com.intellij.lang.javascript.inspections.*
 import com.intellij.lang.typescript.inspections.TypeScriptUnresolvedReferenceInspection
 import com.intellij.webSymbols.moveToOffsetBySignature
-import org.angular2.Angular2CodeInsightFixtureTestCase
-import org.angularjs.AngularTestUtil
+import org.angular2.Angular2TestCase
 
 /**
  * @see Angular2DecoratorInspectionsTest
  * @see Angular2TemplateInspectionsTest
  */
-class Angular2TsInspectionsTest : Angular2CodeInsightFixtureTestCase() {
-  override fun getTestDataPath(): String {
-    return AngularTestUtil.getBaseTestDataPath() + "inspections/ts"
-  }
+class Angular2TsInspectionsTest : Angular2TestCase("inspections/ts") {
 
-  fun testUnusedSymbol() {
-    myFixture.enableInspections(JSUnusedGlobalSymbolsInspection::class.java,
-                                JSUnusedLocalSymbolsInspection::class.java)
-    myFixture.configureByFiles("unused.ts", "unused.html", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testUnusedSymbol() =
+    checkHighlighting(configureFileName = "unused.ts",
+                      dir = true,
+                      inspections = listOf(JSUnusedGlobalSymbolsInspection::class.java,
+                                           JSUnusedLocalSymbolsInspection::class.java))
 
-  fun testUnusedSetter() {
-    myFixture.enableInspections(JSUnusedGlobalSymbolsInspection::class.java,
-                                JSUnusedLocalSymbolsInspection::class.java)
-    myFixture.configureByFiles("unusedSetter.ts", "unusedSetter.html", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testUnusedSetter() =
+    checkHighlighting(dir = true,
+                      inspections = listOf(JSUnusedGlobalSymbolsInspection::class.java,
+                                           JSUnusedLocalSymbolsInspection::class.java))
 
-  fun testMethodCanBeStatic() {
-    val canBeStaticInspection = JSMethodCanBeStaticInspection()
-    JSTestUtils.setInspectionHighlightLevel(project, canBeStaticInspection, HighlightDisplayLevel.WARNING, testRootDisposable)
-    myFixture.enableInspections(canBeStaticInspection)
-    myFixture.configureByFiles("methodCanBeStatic.ts", "methodCanBeStatic.html", "package.json")
-    myFixture.checkHighlighting()
-  }
-
-  fun testUnterminated() {
-    myFixture.enableInspections(UnterminatedStatementJSInspection::class.java)
-    myFixture.configureByFiles("unterminated.ts", "package.json")
-    myFixture.checkHighlighting()
-  }
-
-  fun testUnusedReference() {
-    myFixture.enableInspections(JSUnusedGlobalSymbolsInspection::class.java,
-                                JSUnusedLocalSymbolsInspection::class.java)
-    myFixture.configureByFiles("unusedReference.html", "unusedReference.ts", "package.json")
-    myFixture.checkHighlighting()
-    for (attrToRemove in mutableListOf("notUsedRef", "anotherNotUsedRef", "notUsedRefWithAttr", "anotherNotUsedRefWithAttr")) {
-      myFixture.moveToOffsetBySignature("<caret>$attrToRemove")
-      myFixture.launchAction(
-        try {
-          myFixture.findSingleIntention("Remove unused variable '$attrToRemove'")
-        }
-        catch (e: AssertionError) {
-          myFixture.findSingleIntention("Remove unused constant '$attrToRemove'")
-        })
+  fun testMethodCanBeStatic() =
+    checkHighlighting(dir = true) {
+      val canBeStaticInspection = JSMethodCanBeStaticInspection()
+      JSTestUtils.setInspectionHighlightLevel(project, canBeStaticInspection, HighlightDisplayLevel.WARNING, testRootDisposable)
+      enableInspections(canBeStaticInspection)
     }
-    myFixture.checkResultByFile("unusedReference.after.html")
-  }
 
-  fun testId() {
-    myFixture.enableInspections(JSUnusedLocalSymbolsInspection::class.java,
-                                JSUnusedGlobalSymbolsInspection::class.java)
-    myFixture.configureByFiles("object.ts", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testUnterminated() =
+    checkHighlighting(inspections = listOf(UnterminatedStatementJSInspection::class.java))
 
-  fun testPipeAndArgResolution() {
-    myFixture.enableInspections(TypeScriptUnresolvedReferenceInspection::class.java)
-    myFixture.configureByFiles("pipeAndArgResolution.html", "lowercase_pipe.ts", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testUnusedReference() =
+    doConfiguredTest(dir = true, checkResult = true, extension = "html") {
+      enableInspections(JSUnusedGlobalSymbolsInspection::class.java,
+                        JSUnusedLocalSymbolsInspection::class.java)
+      checkHighlighting()
+      for (attrToRemove in mutableListOf("notUsedRef", "anotherNotUsedRef", "notUsedRefWithAttr", "anotherNotUsedRefWithAttr")) {
+        moveToOffsetBySignature("<caret>$attrToRemove")
+        launchAction(
+          try {
+            findSingleIntention("Remove unused variable '$attrToRemove'")
+          }
+          catch (e: AssertionError) {
+            findSingleIntention("Remove unused constant '$attrToRemove'")
+          })
+      }
+    }
 
-  fun testHtmlTargetWithInterpolation() {
-    myFixture.enableInspections(HtmlUnknownTargetInspection::class.java)
-    myFixture.configureByFiles("htmlTargetWithInterpolation.html", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testId() =
+    checkHighlighting(inspections = listOf(JSUnusedGlobalSymbolsInspection::class.java,
+                                           JSUnusedLocalSymbolsInspection::class.java))
 
-  fun testDuplicateDeclarationOff() {
-    myFixture.enableInspections(JSDuplicatedDeclarationInspection())
-    myFixture.configureByFiles("duplicateDeclarationOff.html", "duplicateDeclarationOff.ts", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testPipeAndArgResolution() =
+    checkHighlighting(inspections = listOf(TypeScriptUnresolvedReferenceInspection::class.java),
+                      dir = true,
+                      extension = "html")
 
-  fun testDuplicateDeclarationOffTemplate() {
-    myFixture.enableInspections(JSDuplicatedDeclarationInspection())
-    myFixture.configureByFiles("duplicateDeclarationOffLocalTemplate.ts", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testHtmlTargetWithInterpolation() =
+    checkHighlighting(inspections = listOf(HtmlUnknownTargetInspection::class.java),
+                      extension = "html")
 
-  fun testEmptyVarDefinition() {
-    myFixture.enableInspections(JSUnusedLocalSymbolsInspection())
-    myFixture.configureByFiles("package.json")
-    myFixture.configureByText("template.html", "<ng-template ngFor let- ></ng-template>")
-    myFixture.checkHighlighting()
-  }
+  fun testDuplicateDeclarationOff() =
+    checkHighlighting(inspections = listOf(JSDuplicatedDeclarationInspection::class.java),
+                      dir = true,
+                      extension = "html")
 
-  fun testMissingLabelSuppressed() {
-    myFixture.enableInspections(HtmlFormInputWithoutLabelInspection())
-    myFixture.configureByFiles("missingLabelSuppressed.html", "missingLabelSuppressed.ts", "package.json")
-    myFixture.checkHighlighting()
-  }
+  fun testDuplicateDeclarationOffTemplate() =
+    checkHighlighting(inspections = listOf(JSDuplicatedDeclarationInspection::class.java))
+
+  fun testEmptyVarDefinition() =
+    checkHighlighting(inspections = listOf(JSUnusedLocalSymbolsInspection::class.java),
+                      extension = "html")
+
+  fun testMissingLabelSuppressed() =
+    checkHighlighting(inspections = listOf(HtmlFormInputWithoutLabelInspection::class.java),
+                      dir = true,
+                      extension = "html")
+
 }
