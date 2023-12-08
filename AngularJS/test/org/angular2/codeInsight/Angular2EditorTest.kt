cc20282aacb3ef14bc3dbffc37b3b6ef31afe5f8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-08 12:41:52 +0100

    WEB-63880 Double closing brackets when closing new control flow if statement
    GitOrigin-RevId: 04cc33c2725f25ef7e716cc3bb9283dab153b1d3

--- AngularJS/test/org/angular2/codeInsight/Angular2EditorTest.kt
+++ AngularJS/test/org/angular2/codeInsight/Angular2EditorTest.kt
@@ -10,13 +10,19 @@ class Angular2EditorTest : Angular2TestCase("editor"){
   fun testBlockTyping() =
     doConfiguredTest(Angular2TestModule.ANGULAR_CORE_17_0_0_RC_0,
                      fileContents = "<div><caret></div>", extension = "html", checkResult = true) {
-      myFixture.type("@if (")
-      myFixture.moveToOffsetBySignature("()<caret>")
-      myFixture.type("{\n<div>")
+      type("@if (")
+      moveToOffsetBySignature("()<caret>")
+      type("{\n<div>")
     }
 
   fun testBlockFolding() {
     doFoldingTest(Angular2TestModule.ANGULAR_CORE_17_0_0_RC_0,
                   extension = "html")
   }
+
+  fun testClosingBraceWithInterpolation() =
+    doConfiguredTest(Angular2TestModule.ANGULAR_CORE_17_0_0_RC_0,
+                     fileContents = "@if (foo) { {{foo}} } @else { <caret>", extension = "html", checkResult = true) {
+      type("}")
+    }
 }
