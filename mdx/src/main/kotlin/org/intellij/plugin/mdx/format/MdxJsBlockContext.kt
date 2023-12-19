3ef334ec78b4136d83da75999ebba5a932c49edf
Dmitry Osinovskiy <dmitry.osinovsky@jetbrains.com>
2023-12-19 19:17:17 +0100

    JS formatter: remove unneeded myDialectSettings from MdxJsBlockContext (a field from a base class would be used)
    (cherry picked from commit bf0c0fc5c8b0fbe7181d842da5b17375ce292f49)
    
    NET-CR-13070
    
    GitOrigin-RevId: 6e110a55506ffeb7efb5b625ad8fe26acceb0b95

--- mdx/src/main/kotlin/org/intellij/plugin/mdx/format/MdxJsBlockContext.kt
+++ mdx/src/main/kotlin/org/intellij/plugin/mdx/format/MdxJsBlockContext.kt
@@ -14,8 +14,6 @@ class MdxJsBlockContext(topSettings: CodeStyleSettings,
                         dialect: Language,
                         explicitSettings: JSCodeStyleSettings?,
                         formattingMode: FormattingMode) : JSBlockContext(topSettings, dialect, explicitSettings, formattingMode) {
-  private val myDialectSettings = ObjectUtils.coalesce(explicitSettings,
-                                                       topSettings.getCustomSettings(JSCodeStyleSettings.getSettingsClass(dialect)))
 
 
   override fun createSpacingStrategy(node: ASTNode): JSSpacingStrategy {
