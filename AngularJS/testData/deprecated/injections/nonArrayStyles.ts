6d101ac73e859026af1eac1f6b2cccf4fdcfd0fc
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2023-12-20 12:20:45 +0100

    WEB-63976 Angular 17: Auto-inject CSS in Angular inline styles
    (cherry picked from commit 3aa99c968f8a09de208c4489ae9fa543c3c78fc2)
    
    IJ-CR-123024
    
    GitOrigin-RevId: eb21f0da3fc5ac84312f2c7426ac5926d078d0c8

---
+++ AngularJS/testData/deprecated/injections/nonArrayStyles.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core"
+
+@Component({selector: 'todo-cmp',
+    styles:`
+  div {
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  }
+`
+})
+export class TodoCmp {
+}
