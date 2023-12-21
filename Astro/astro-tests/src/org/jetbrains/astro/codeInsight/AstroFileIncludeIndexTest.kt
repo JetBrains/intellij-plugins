0c4cd6b34938699f874bf267fef01adaf5a60481
Aleksei Berezkin <aleksei.berezkin@jetbrains.com>
2023-12-21 13:51:18 +0100

    WEB-59503 Added FileIncludeProvider for Astro -- to resolve imported files
    (cherry picked from commit 13ddf942b7ab40d9e29590ea5648ad2c4c0c7232)
    
    IJ-CR-120122
    
    GitOrigin-RevId: 922ca5034f47d7945a2abaaf7a9b8de594f11a66

---
+++ Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroFileIncludeIndexTest.kt
@@ -0,0 +1,14 @@
+package org.jetbrains.astro.codeInsight
+
+import com.intellij.psi.impl.include.FileIncludeIndex
+import org.jetbrains.astro.AstroCodeInsightTestCase
+import org.junit.Assert
+
+class AstroFileIncludeIndexTest : AstroCodeInsightTestCase("codeInsight/fileIncludeIndex") {
+  fun testBasic() {
+    val astroFile = myFixture.configureByFile("$testName.astro")
+    val includes = FileIncludeIndex.getIncludes(astroFile.virtualFile, project)
+    Assert.assertEquals(1, includes.size)
+    Assert.assertEquals("foo.ts", includes[0].fileName)
+  }
+}
