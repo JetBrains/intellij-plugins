fb1d50cf3d3a8b24b174ca6823e45ed2ae09a0ba
Daniel Brauner <daniel.brauner@jetbrains.com>
2024-01-08 15:45:32 +0100

    [devicetree language support] CPP-35496 added new file dialog
    (cherry picked from commit 129ba836a5135b152fdd1656627a438efc731f53)
    
    IJ-CR-123389
    
    GitOrigin-RevId: a7801f213b3d6634263018a6853731d2c7991e13

---
+++ dts/src/com/intellij/dts/ide/DtsNewFileAction.kt
@@ -0,0 +1,27 @@
+package com.intellij.dts.ide
+
+import com.intellij.dts.DtsBundle
+import com.intellij.ide.actions.CreateFileFromTemplateAction
+import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.openapi.project.DumbAware
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiDirectory
+
+class DtsNewFileAction : CreateFileFromTemplateAction(), DumbAware {
+  companion object {
+    private const val SOURCE_FILE_TEMPLATE = "devicetree_source"
+    private const val INCLUDE_FILE_TEMPLATE = "devicetree_include"
+    private const val OVERLAY_FILE_TEMPLATE = "devicetree_overlay"
+  }
+
+  override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
+    builder.setTitle(DtsBundle.message("action.dts_new_file.title"))
+      .addKind(DtsBundle.message("action.dts_new_file.kind.source"), null, SOURCE_FILE_TEMPLATE)
+      .addKind(DtsBundle.message("action.dts_new_file.kind.include"), null, INCLUDE_FILE_TEMPLATE)
+      .addKind(DtsBundle.message("action.dts_new_file.kind.overlay"), null, OVERLAY_FILE_TEMPLATE)
+  }
+
+  override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String {
+    return DtsBundle.message("action.dts_new_file.name", newName)
+  }
+}
