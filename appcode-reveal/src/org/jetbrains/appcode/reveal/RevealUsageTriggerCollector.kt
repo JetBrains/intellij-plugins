2794fe90de323a1a4c3df94f38027dd2994371e6
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-01-14 16:32:39 +0100

    [fus] IDEA-326820 Migrate the rest of counter usage collectors to Kotlin object
    GitOrigin-RevId: 82cc558fe31af9224a3c4891d6051310d8502058

--- appcode-reveal/src/org/jetbrains/appcode/reveal/RevealUsageTriggerCollector.kt
+++ appcode-reveal/src/org/jetbrains/appcode/reveal/RevealUsageTriggerCollector.kt
@@ -5,22 +5,16 @@ package org.jetbrains.appcode.reveal
 
 import com.intellij.internal.statistic.eventLog.EventLogGroup
 import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
-import com.intellij.internal.statistic.service.fus.collectors.FUCounterUsageLogger
-import com.intellij.openapi.project.Project
 
-class RevealUsageTriggerCollector : CounterUsagesCollector() {
+internal object RevealUsageTriggerCollector : CounterUsagesCollector() {
   override fun getGroup(): EventLogGroup = GROUP
 
-  companion object {
-    private val GROUP = EventLogGroup("appcode.reveal", 2)
+  private val GROUP = EventLogGroup("appcode.reveal", 2)
 
-    @JvmField
-    val INSTALL_ON_DEVICE = GROUP.registerEvent("installOnDevice")
-
-    @JvmField
-    val INJECT = GROUP.registerEvent("inject")
-
-    @JvmField
-    val SHOW_IN_REVEAL = GROUP.registerEvent("showInReveal")
-  }
+  @JvmField
+  val INSTALL_ON_DEVICE = GROUP.registerEvent("installOnDevice")
+  @JvmField
+  val INJECT = GROUP.registerEvent("inject")
+  @JvmField
+  val SHOW_IN_REVEAL = GROUP.registerEvent("showInReveal")
 }
