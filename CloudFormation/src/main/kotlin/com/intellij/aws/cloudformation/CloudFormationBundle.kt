0e4caf86d5aa98a664d96198a254d2f755744a33
Andrey Cherkasov <andrey.cherkasov@jetbrains.com>
2024-01-15 18:45:54 +0400

    [cloudformation] Cleanup
    See description of the "Eager creation of action presentation" inspection
    
    ^IDEA-315381
    
    GitOrigin-RevId: 983f69f527c247018695d75c29364c5b0c690214

--- CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationBundle.kt
+++ CloudFormation/src/main/kotlin/com/intellij/aws/cloudformation/CloudFormationBundle.kt
@@ -10,10 +10,13 @@ private const val BUNDLE = "messages.CloudFormationBundle"
 
 object CloudFormationBundle : DynamicBundle(BUNDLE) {
   @Nls
-  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key,
-                                                                                                          *params)
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String {
+    return if (containsKey(key)) getMessage(key, *params) else CloudFormationDeprecatedMessagesBundle.message(key, *params)
+  }
 
   @JvmStatic
   fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String,
-                     vararg params: Any): java.util.function.Supplier<String> = getLazyMessage(key, *params)
+                     vararg params: Any): java.util.function.Supplier<String> {
+    return if (containsKey(key))  getLazyMessage(key, *params) else CloudFormationDeprecatedMessagesBundle.messagePointer(key, *params)
+  }
 }
