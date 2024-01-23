e242a47a33a610c17381a27ea60a76ebec368ac0
Vladimir Panimaskin <Vladimir.Panimaskin@jetbrains.com>
2024-01-23 11:20:51 +0200

    [vue] show component usages UX improvements WEB-63640
    GitOrigin-RevId: cfce9039f843f8d8afca42d9a1114ada9408a51d

--- Astro/src/org/jetbrains/astro/codeInsight/AstroComponentUsageProvider.kt
+++ Astro/src/org/jetbrains/astro/codeInsight/AstroComponentUsageProvider.kt
@@ -1,6 +1,36 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.astro.codeInsight
 
+import com.intellij.lang.javascript.JSTokenTypes
 import com.intellij.lang.javascript.hints.JSComponentUsageProvider
+import com.intellij.openapi.editor.Document
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiFile
+import com.intellij.psi.tree.TokenSet
+import com.intellij.psi.util.elementType
+import com.intellij.psi.xml.XmlElementType
+import com.intellij.psi.xml.XmlTokenType
+import org.jetbrains.astro.lang.AstroFileImpl
 
-class AstroComponentUsageProvider : JSComponentUsageProvider
+private val SKIP_TOKENS = TokenSet.orSet(
+  JSTokenTypes.COMMENTS_AND_WHITESPACES,
+  XmlTokenType.COMMENTS,
+  XmlTokenType.WHITESPACES,
+  TokenSet.create(XmlElementType.XML_COMMENT),
+)
+
+class AstroComponentUsageProvider : JSComponentUsageProvider {
+  override fun computeRange(file: PsiFile, document: Document): TextRange? {
+    if (file is AstroFileImpl) {
+      var anchor = file.astroContentRoot()?.firstChild
+      while (anchor != null) {
+        if (anchor.elementType !in SKIP_TOKENS) {
+          return anchor.textRange
+        }
+        anchor = anchor.nextSibling
+      }
+    }
+
+    return super.computeRange(file, document)
+  }
+}
