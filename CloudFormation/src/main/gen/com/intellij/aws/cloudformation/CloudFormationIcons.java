7a8716e1934a2e0d800dfa27e5856a4e9b823138
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-01-24 15:22:27 +0100

    [icons] Move Rabbit MQ, Kafka and Active MQ icons to AllIcons.Providers
    GitOrigin-RevId: 05b8dc4b38d4a592cefbee7b19e840bdb17fd670

--- CloudFormation/src/main/gen/com/intellij/aws/cloudformation/CloudFormationIcons.java
+++ CloudFormation/src/main/gen/com/intellij/aws/cloudformation/CloudFormationIcons.java
@@ -13,6 +13,5 @@ public final class CloudFormationIcons {
   private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, CloudFormationIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Aws = load("icons/aws.svg", -1062270596, 0);
   /** 16x16 */ public static final @NotNull Icon AwsFile = load("icons/awsFile.svg", -424873503, 0);
 }
