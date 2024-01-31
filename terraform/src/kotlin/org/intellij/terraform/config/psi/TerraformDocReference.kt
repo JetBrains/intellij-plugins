7626892c6db655902c3b2b6d8254f45709c381cf
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-01-31 14:45:59 +0100

    [terraform] IDEA-297270 Cleanup according to review
    GitOrigin-RevId: 1253c864682554647faff9584b7408002798522b

--- terraform/src/kotlin/org/intellij/terraform/config/psi/TerraformDocReference.kt
+++ terraform/src/kotlin/org/intellij/terraform/config/psi/TerraformDocReference.kt
@@ -1,23 +1,12 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.config.psi
 
-import com.intellij.openapi.paths.WebReference
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiReference
+import com.intellij.psi.PsiReferenceBase
 
-/**
- * Represents a reference to a Terraform documentation URL. It is a delegating wrapper over [com.intellij.openapi.paths.WebReference]
- * but it contains specific logic related to PSI text range. Also, it returns specific PsiReference different from what we have in WebReference.
- * Hence, it is not worth extending [com.intellij.psi.impl.source.resolve.reference.impl.PsiDelegateReference] for this specific case.
- *
- * @property element The underlying element of the reference.
- * @property webReference The web reference associated with the Terraform documentation URL.
- * @property displayText The display text for the reference.
- */
-internal class TerraformDocReference(element: PsiElement, url: String?) : PsiReference {
+internal class TerraformDocReference(element: PsiElement, val url: String) : PsiReferenceBase<PsiElement>(element) {
 
-  private val webReference: WebReference
   private val displayText: String
 
   init {
@@ -27,28 +16,9 @@ internal class TerraformDocReference(element: PsiElement, url: String?) : PsiRef
     else {
       TextRange(1, element.textLength - 1)
     }
-    webReference = WebReference(element, range, url)
+    rangeInElement = range
     displayText = range.substring(element.text)
   }
 
-  override fun getElement(): PsiElement = webReference.element
-
-  override fun getRangeInElement(): TextRange = webReference.rangeInElement
-
-  override fun resolve(): PsiElement = TerraformDocumentPsi(webReference.element, webReference.rangeInElement, displayText,
-                                                            webReference.url)
-
-  override fun getCanonicalText(): String = webReference.canonicalText
-
-  override fun handleElementRename(newElementName: String): PsiElement = webReference.handleElementRename(newElementName)
-
-  override fun bindToElement(element: PsiElement): PsiElement = webReference.bindToElement(element)
-
-  override fun isReferenceTo(element: PsiElement): Boolean = webReference.isReferenceTo(element)
-
-  override fun isSoft(): Boolean = true
-
-  override fun getAbsoluteRange(): TextRange = webReference.absoluteRange
-
-  override fun getVariants(): Array<Any> = webReference.variants
+  override fun resolve(): PsiElement = TerraformDocumentPsi(element, rangeInElement, displayText, url)
 }
