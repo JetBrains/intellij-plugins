8159f2fd0bbcd5f8037bfcf6b923f8ac0450e60d
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-02-02 19:05:20 +0100

    [terraform] IDEA-297270 Cleanup according to review
    GitOrigin-RevId: 43a7a826a1ab37fef45e95bdd5ffc6561add9f09

--- terraform/src/kotlin/org/intellij/terraform/config/psi/TerraformDocumentPsi.kt
+++ terraform/src/kotlin/org/intellij/terraform/config/psi/TerraformDocumentPsi.kt
@@ -7,17 +7,6 @@ import com.intellij.psi.PsiElement
 import com.intellij.psi.SyntheticElement
 import com.intellij.psi.impl.FakePsiElement
 
-/**
- * Synthetic PSI element whick represents a Terraform documentation reference.
- * We need this to cover issue with HCL parser which does not handle terraform module type properly. In contrast to
- * [com.intellij.openapi.paths.WebReference.MyFakePsiElement] returns meaningful text instead of simple URL, so
- * it looks better in documentation popups.
- *
- * @property element The underlying PSI element associated with the Terraform document.
- * @property rangeInElement The text range within the PSI element occupied by the Terraform document.
- * @property text The text content of the Terraform document.
- * @property url The URL of the Terraform document.
- */
 internal class TerraformDocumentPsi(val element: PsiElement,
                                     private val rangeInElement: TextRange,
                                     private val text: String,
