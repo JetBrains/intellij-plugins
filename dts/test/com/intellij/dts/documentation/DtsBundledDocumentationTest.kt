a66a3c853275661c9c7738a88063f79b485f842f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-02-06 11:42:46 +0100

    BuildKmmPluginBuildTarget - use JDK to read/write text
    GitOrigin-RevId: ca7505ce511aa95697f613893d392e6dde288688

--- dts/test/com/intellij/dts/documentation/DtsBundledDocumentationTest.kt
+++ dts/test/com/intellij/dts/documentation/DtsBundledDocumentationTest.kt
@@ -7,10 +7,10 @@ import com.intellij.dts.zephyr.binding.DtsZephyrBundledBindings
 import com.networknt.schema.JsonSchemaFactory
 import com.networknt.schema.SpecVersion
 import kotlinx.coroutines.runBlocking
+import java.nio.file.Files
 import java.nio.file.Path
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.name
-import kotlin.io.path.readText
 
 class DtsBundledDocumentationTest : DtsDocumentationTest() {
   override fun getBasePath(): String = "documentation/bundled"
@@ -25,7 +25,7 @@ class DtsBundledDocumentationTest : DtsDocumentationTest() {
 
   fun `test only ascii in bundled bindings`() {
     for (file in Path.of(DTS_TEST_ROOT_PATH, "resources/bindings").listDirectoryEntries()) {
-      for (character in file.readText()) {
+      for (character in Files.readString(file)) {
         if (character.code > 0x7e) {
           fail("invalid character '$character' in binding: ${file.fileName}")
         }
@@ -36,11 +36,11 @@ class DtsBundledDocumentationTest : DtsDocumentationTest() {
   fun `test bundled bindings match schema`() {
     val factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7)
     val mapper = ObjectMapper(YAMLFactory())
-    val yamlFactory = JsonSchemaFactory.builder(factory).objectMapper(mapper).build()
+    val yamlFactory = JsonSchemaFactory.builder(factory).jsonMapper(mapper).build()
     val schema = yamlFactory.getSchema(getFixture("documentation/BindingSchema.json"))
 
     for (file in Path.of(DTS_TEST_ROOT_PATH, "resources/bindings").listDirectoryEntries()) {
-      for (result in schema.validate(mapper.readTree(file.readText()))) {
+      for (result in schema.validate(mapper.readTree(Files.readString(file)))) {
         fail("${file.name}: ${result.message}")
       }
     }
