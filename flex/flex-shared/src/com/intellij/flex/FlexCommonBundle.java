6a7e61de75b33f18db23eba034c490aa2d96af73
Vera Petrenkova <Vera.Petrenkova@jetbrains.com>
2024-02-08 16:31:34 +0100

    IDEA-344652 [plugins] Replace AbstractBundle with DynamicBundle
    GitOrigin-RevId: f7cb27d453e71b36bd21037c13730dda4d7cc112

--- flex/flex-shared/src/com/intellij/flex/FlexCommonBundle.java
+++ flex/flex-shared/src/com/intellij/flex/FlexCommonBundle.java
@@ -1,34 +1,20 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.intellij.flex;
 
-import com.intellij.AbstractBundle;
+import com.intellij.DynamicBundle;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.PropertyKey;
 
-import java.lang.ref.Reference;
-import java.lang.ref.SoftReference;
-import java.util.ResourceBundle;
-
 public final class FlexCommonBundle {
-
-  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
-    return AbstractBundle.message(getBundle(), key, params);
-  }
-
-  private static Reference<ResourceBundle> ourBundle;
   @NonNls public static final String BUNDLE = "messages.FlexCommonBundle";
+  private static final DynamicBundle INSTANCE = new DynamicBundle(FlexCommonBundle.class, BUNDLE);
 
   private FlexCommonBundle() {
   }
 
-  private static ResourceBundle getBundle() {
-    ResourceBundle bundle = com.intellij.reference.SoftReference.dereference(ourBundle);
-    if (bundle == null) {
-      bundle = ResourceBundle.getBundle(BUNDLE);
-      ourBundle = new SoftReference<>(bundle);
-    }
-    return bundle;
+  public static @Nls String message(@NotNull @PropertyKey(resourceBundle = BUNDLE) String key, Object @NotNull ... params) {
+    return INSTANCE.getMessage(key, params);
   }
 }
