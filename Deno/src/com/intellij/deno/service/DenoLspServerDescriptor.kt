6954a291ee9e91788c056772757efb6cff98451c
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-02-11 21:55:00 +0100

    IDEA-330687 (wip): move classes to com.intellij.platform.lang... package to make IntelliJProjectPackageNamesTest happy
    GitOrigin-RevId: 2a3bac6cfce1f4a28d7189a66af76569844f6b9d

--- Deno/src/com/intellij/deno/service/DenoLspServerDescriptor.kt
+++ Deno/src/com/intellij/deno/service/DenoLspServerDescriptor.kt
@@ -18,7 +18,7 @@ import com.intellij.openapi.project.IndexNotReadyException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.platform.lsWidget.LanguageServiceWidgetContext
+import com.intellij.platform.lang.lsWidget.LanguageServiceWidgetContext
 import com.intellij.platform.lsp.api.LspServer
 import com.intellij.platform.lsp.api.LspServerSupportProvider
 import com.intellij.platform.lsp.api.ProjectWideLspServerDescriptor
