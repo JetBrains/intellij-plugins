128311aa2a06e41b60647e4a1de225329593828f
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2024-02-13 18:03:02 +0100

    [terraform] IDEA-343733 init floating action
    GitOrigin-RevId: 4bf5f6297c64b658ed236fd1c209574612b187f6

--- terraform/src/java/org/intellij/terraform/config/actions/TFInitAction.kt
+++ terraform/src/java/org/intellij/terraform/config/actions/TFInitAction.kt
@@ -1,21 +1,41 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.config.actions
 
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.components.service
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.ide.progress.withBackgroundProgress
-import org.intellij.terraform.config.util.TFExecutor
-import org.intellij.terraform.config.util.executeSuspendable
+import org.intellij.terraform.TerraformIcons
+import org.intellij.terraform.config.model.local.LocalSchemaService
 import org.jetbrains.annotations.Nls
 
-class TFInitAction : TFExternalToolsAction() {
+open class TFInitAction : TFExternalToolsAction() {
+
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    e.presentation.icon = TerraformIcons.Terraform
+  }
 
   override suspend fun invoke(project: Project, module: Module?, title: @Nls String, virtualFile: VirtualFile) {
     withBackgroundProgress(project, title) {
       project.service<TerraformActionService>().runTerraformInit(virtualFile, project, module, title)
     }
+  }
+}
+
+class TFInitRequiredAction : TFInitAction() {
+
+  override fun update(e: AnActionEvent) {
+    super.update(e)
+    if (!e.presentation.isEnabledAndVisible) {
+      e.presentation.isVisible = false
+      return
+    }
+    val file = e.getData(CommonDataKeys.VIRTUAL_FILE) ?: return
+    val project = e.project ?: return
+    e.presentation.isEnabledAndVisible = project.service<LocalSchemaService>().findLockFile(file) == null
   }
 }
