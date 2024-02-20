f710f7ada956914ca9ab1182a57c1fca56432e72
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-02-20 12:54:14 +0100

    Angular: move block support to appropriate package
    (cherry picked from commit 60c3305cf823b72b01c0e18dc6f4399d3e0c70d1)
    
    IJ-CR-126852
    
    GitOrigin-RevId: 85a320148266cf9a92bd055ade671b2ee79637a2

--- Angular/src/org/angular2/codeInsight/blocks/Angular2BlockReferencesProvider.kt
+++ Angular/src/org/angular2/codeInsight/blocks/Angular2BlockReferencesProvider.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.angular2.web.references
+package org.angular2.codeInsight.blocks
 
 import com.intellij.webSymbols.WebSymbol
 import com.intellij.webSymbols.references.WebSymbolReferenceProvider
