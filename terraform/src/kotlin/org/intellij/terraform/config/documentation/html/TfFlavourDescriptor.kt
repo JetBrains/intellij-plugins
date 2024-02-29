be843bc7d6271db0c40ade525a627f813fcb1951
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-02-29 11:31:21 +0100

    [terraform] IDEA-344717 HCL. Improve the view of the quick help.
    Navigating to properties in the documentation.
    
    (cherry picked from commit e6b618e0b3c85bed1cd6851e2a2c13d627c945ad)
    
    IJ-CR-127117
    
    GitOrigin-RevId: 56936ac504bf6e6620f00dfad8de7f10db42d357

---
+++ terraform/src/kotlin/org/intellij/terraform/config/documentation/html/TfFlavourDescriptor.kt
@@ -0,0 +1,80 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.intellij.terraform.config.documentation.html
+
+import com.intellij.openapi.util.text.StringUtilRt
+import com.intellij.util.containers.tail
+import org.intellij.markdown.IElementType
+import org.intellij.markdown.MarkdownElementTypes
+import org.intellij.markdown.ast.ASTNode
+import org.intellij.markdown.ast.LeafASTNode
+import org.intellij.markdown.ast.accept
+import org.intellij.markdown.ast.getTextInNode
+import org.intellij.markdown.ast.impl.ListCompositeNode
+import org.intellij.markdown.ast.impl.ListItemCompositeNode
+import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
+import org.intellij.markdown.html.GeneratingProvider
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.html.SimpleTagProvider
+import org.intellij.markdown.html.entities.EntityConverter
+import org.intellij.markdown.lexer.Compat
+import org.intellij.markdown.parser.LinkMap
+import java.net.URI
+
+class TfFlavourDescriptor : GFMFlavourDescriptor(true, false) {
+
+  override fun createHtmlGeneratingProviders(linkMap: LinkMap, baseURI: URI?): Map<IElementType, GeneratingProvider> {
+    val parentProviders = super.createHtmlGeneratingProviders(linkMap, baseURI).toMutableMap()
+    parentProviders[MarkdownElementTypes.LIST_ITEM] = ListItemGeneratingProvider()
+
+    return parentProviders
+  }
+
+  internal class ListItemGeneratingProvider : SimpleTagProvider("li") {
+    override fun processNode(visitor: HtmlGenerator.HtmlGeneratingVisitor, text: String, node: ASTNode) {
+      Compat.assert(node is ListItemCompositeNode)
+      openTag(visitor, text, node)
+      val listNode = node.parent
+      Compat.assert(listNode is ListCompositeNode)
+      for (child in node.children) {
+        if (child.type == MarkdownElementTypes.PARAGRAPH) {
+          ListElementAnchorProvider.processNode(visitor, text, child)
+        }
+        else {
+          child.accept(visitor)
+        }
+      }
+      closeTag(visitor, text, node)
+    }
+
+    object ListElementAnchorProvider : SimpleTagProvider("p") {
+      override fun processNode(visitor: HtmlGenerator.HtmlGeneratingVisitor, text: String, node: ASTNode) {
+        val nodes = node.children
+        if (nodes.isEmpty()) return
+        val codeSpan = nodes[0].takeIf { it.type == MarkdownElementTypes.CODE_SPAN }
+        if (codeSpan != null) {
+          val textInNode = EntityConverter.replaceEntities(StringUtilRt.unquoteString (codeSpan.getTextInNode(text).toString(), '`'),
+                                                           true,
+                                                           true)
+          visitor.consumeTagOpen(codeSpan, "a", """href="#${textInNode}"""", """id="${textInNode}"""", """name="${textInNode}"""")
+          visitor.consumeTagOpen(codeSpan, "code")
+          visitor.consumeHtml(textInNode)
+          visitor.consumeTagClose("code")
+          visitor.consumeTagClose("a")
+          nodes.tail().forEach { child ->
+            when (child) {
+              is LeafASTNode -> {
+                visitor.visitLeaf(child)
+              }
+              else -> {
+                child.accept(visitor)
+              }
+            }
+          }
+        }
+        else {
+          node.accept(visitor)
+        }
+      }
+    }
+  }
+}
