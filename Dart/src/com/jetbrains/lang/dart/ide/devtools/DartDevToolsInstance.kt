ed36ac9aa4ca90f3cffbe4fbd179b76aceb094ac
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-03-08 14:22:03 +0100

    Remove unused DartDevToolsInstance class
    GitOrigin-RevId: e13bf39430c9c5e8255db72c3c558833e4c57a58

--- Dart/src/com/jetbrains/lang/dart/ide/devtools/DartDevToolsInstance.kt
+++
@@ -1,4 +0,0 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.jetbrains.lang.dart.ide.devtools
-
-data class DartDevToolsInstance(val host: String, val port: Int)
