a77d628d1d9cdde5e0afcfa3b69c0c79fb98e5e1
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-03-11 14:49:07 +0100

    follow-up on d32a3dbdf: remove StopDartWebdevServerAction constructor
    GitOrigin-RevId: dbafb545f17b65df4fa6cf9518753aa538afedd1

--- Dart/src/com/jetbrains/lang/dart/pubServer/StopDartWebdevServerAction.java
+++ Dart/src/com/jetbrains/lang/dart/pubServer/StopDartWebdevServerAction.java
@@ -1,22 +1,12 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package com.jetbrains.lang.dart.pubServer;
 
-import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.project.DumbAwareAction;
-import com.jetbrains.lang.dart.DartBundle;
 import org.jetbrains.annotations.NotNull;
 
 public class StopDartWebdevServerAction extends DumbAwareAction {
-
-  public StopDartWebdevServerAction() {
-    //noinspection DialogTitleCapitalization
-    super(DartBundle.messagePointer("stop.dart.webdev.server"),
-          DartBundle.messagePointer("stop.dart.webdev.server"),
-          AllIcons.Actions.Suspend);
-  }
-
   @Override
   public void update(@NotNull final AnActionEvent e) {
     e.getPresentation().setEnabled(e.getProject() != null && PubServerManager.getInstance(e.getProject()).hasAlivePubServerProcesses());
