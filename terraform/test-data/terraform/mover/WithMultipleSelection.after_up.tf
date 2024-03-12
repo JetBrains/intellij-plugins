12583e73f6c41c1e775f43fc112de2a196397de8
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-03-12 17:55:54 +0200

    [terraform] IDEA-348376 Fix: Move Statement Up/Down Doesn't Work if we select several blocks to move
    GitOrigin-RevId: 41ec291a13b3e5c8e4b5fb423aa9b6d9eb5513a3

---
+++ terraform/test-data/terraform/mover/WithMultipleSelection.after_up.tf
@@ -0,0 +1,35 @@
+resource "azurerm_kubernetes_cluster" "dso_aks_cluster" {
+  name                = "example-aks1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "exampleaks1"
+
+
+  default_node_pool {
+    identity {
+      type = "SystemAssigned"
+    }
+    name       = "default"
+    node_count = 1
+
+    tags = {
+      Environment = "Production"
+    }
+    vm_size    = "Standard_DS2_v2"
+  }
+
+}
+
+resource "azurerm_resource_group" "example" {
+  location = "East US"
+  name     = "example-resources"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+output "kubelet_identity_object" {
+  value       = azurerm_kubernetes_cluster.dso_aks_cluster.kubelet_identity.0
+  description = "Specifies the object ID of the kubelet identity"
+}
