1c02f2eba307229f8b2304973ab102ef441abb20
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-03-12 13:02:25 +0100

    WEB-65987 WebSymbols: indirect dependencies are not recognized when using Yarn Pnp
    (cherry picked from commit 90abfd207ea0eb59798cf2cf208ce7e656498307)
    
    IJ-CR-128699
    
    GitOrigin-RevId: d9bc0a8b1145e564ac3a44c8a953484296f81279

--- vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/service/VueYarnPnpIntegrationTest.kt
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/service/VueYarnPnpIntegrationTest.kt
@@ -4,11 +4,13 @@ package org.jetbrains.vuejs.service
 import com.intellij.javascript.nodejs.library.yarn.pnp.YarnPnpManager
 import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
 import com.intellij.lang.javascript.nodejs.library.yarn.AbstractYarnPnpIntegrationTest
+import com.intellij.openapi.application.ReadAction
+import com.intellij.webSymbols.context.WebSymbolsContext
 import org.jetbrains.vuejs.lang.VueInspectionsProvider
 import org.jetbrains.vuejs.lang.vueRelativeTestDataPath
 import org.junit.Assert
 
-class VueYarnPnpIntegrationTest: AbstractYarnPnpIntegrationTest() {
+class VueYarnPnpIntegrationTest : AbstractYarnPnpIntegrationTest() {
 
   override fun getBasePath(): String {
     return vueRelativeTestDataPath() + "/yarnPnp"
@@ -23,6 +25,15 @@ class VueYarnPnpIntegrationTest: AbstractYarnPnpIntegrationTest() {
     myFixture.configureFromTempProjectFile("test.vue")
     myFixture.enableInspections(VueInspectionsProvider())
     myFixture.checkHighlighting()
+  }
+
+  fun testNuxt() {
+    val root = myFixture.copyDirectoryToProject("nuxt", ".")
+    configureYarnBerryAndRunYarnInstall(root)
+    val file = myFixture.tempDirFixture.getFile("index.html")!!
+    ReadAction.run<Throwable> {
+      Assert.assertEquals("vue", WebSymbolsContext.get("framework", file, project))
+    }
   }
 
 }
