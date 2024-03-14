317a5e4e87d1770ce2f7e40bdcd5a3900102b4e1
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2024-03-14 11:30:48 +0100

    [js] tsc: unify lsp and ts api
    (cherry picked from commit c9867704b9d1ef13bd7fc8f23b49cec969b486be)
    (cherry picked from commit 916a551f28718af92ecf2d0842ba597b43b5d0f7)
    
    IJ-MR-129661
    
    GitOrigin-RevId: 233d55614e036b6784bea3be3706d557ef0f8082

--- vuejs/src/org/jetbrains/vuejs/lang/typescript/service/volar/VolarTypeScriptService.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/typescript/service/volar/VolarTypeScriptService.kt
@@ -1,8 +1,7 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.service.volar
 
 import com.google.gson.JsonElement
-import com.intellij.lang.typescript.compiler.languageService.protocol.commands.LspGetElementTypeRequestArgs
 import com.intellij.lang.typescript.compiler.languageService.protocol.commands.TypeScriptGetElementTypeRequestArgs
 import com.intellij.lang.typescript.lsp.BaseLspTypeScriptService
 import com.intellij.lang.typescript.lsp.JSFrameworkLsp4jServer
@@ -19,11 +18,8 @@ class VolarTypeScriptService(project: Project) : BaseLspTypeScriptService(projec
 
   override fun isAcceptable(file: VirtualFile) = isVolarEnabledAndAvailable(project, file)
 
-  override suspend fun getIdeType(virtualFile: VirtualFile, args: TypeScriptGetElementTypeRequestArgs): JsonElement? {
+  override suspend fun getIdeType(args: TypeScriptGetElementTypeRequestArgs): JsonElement? {
     val server = getServer() ?: return null
-    val params = LspGetElementTypeRequestArgs(file = server.descriptor.getFileUri(virtualFile),
-                                              range = args.range,
-                                              forceReturnType = args.forceReturnType)
-    return server.sendRequest { (it as JSFrameworkLsp4jServer).getElementType(params) }
+    return server.sendRequest { (it as JSFrameworkLsp4jServer).getElementType(args) }
   }
 }
