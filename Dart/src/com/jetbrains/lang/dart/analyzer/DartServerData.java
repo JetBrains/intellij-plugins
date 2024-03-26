9a9227652299717eebd60a85f85886f2c8dde103
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-03-26 17:52:58 +0100

    IJ-CR-128396 Dart: don't send completion request twice to the server
    The fix changes the way how ResolveCache is cleared. Ulike previously used PsiManager.dropPsiCaches(), the ResolveCache.clearCache() call doesn't take write action.
    
    This is what was causing the problem:
    - a user types a letter
    - IDE sends the “analysis.updateContent” request
    - IDE starts a completion session: it takes the read lock and sends the “completion.getSuggestions2” request
    - IDE receives an "analysis.navigation" notification from the server
    - to forget the previously cached Dart navigation information, the Dart plugin calls PsiManager.dropPsiCaches()
    - dropPsiCaches() internally takes the write lock
    - the write lock has priority, so it cancels all running read actions
    - so the completion session gets canceled and restarted
    - the response to the “completion.getSuggestions2” request is ignored
    - the second “completion.getSuggestions2” request is sent as a result of the restarted code completion session
    - the response to the second “completion.getSuggestions2” request is used to show the completion popup
    
    (cherry picked from commit b73cd61a3d3a1509050088003b03b8822f4da674)
    
    GitOrigin-RevId: 890943acdedee511c20b8bd37d1b5761dca6dbde

--- Dart/src/com/jetbrains/lang/dart/analyzer/DartServerData.java
+++ Dart/src/com/jetbrains/lang/dart/analyzer/DartServerData.java
@@ -12,7 +12,7 @@ import com.intellij.openapi.util.NlsSafe;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiManager;
+import com.intellij.psi.impl.source.resolve.ResolveCache;
 import com.intellij.psi.search.SearchScope;
 import com.intellij.util.EventDispatcher;
 import com.intellij.util.SmartList;
@@ -307,15 +307,15 @@ public final class DartServerData {
   private void forceFileAnnotation(@Nullable final VirtualFile file, final boolean clearCache) {
     if (file != null) {
       final Project project = myService.getProject();
+      if (clearCache) {
+        ResolveCache.getInstance(project).clearCache(true);
+      }
 
       // It's ok to call DaemonCodeAnalyzer.restart() right in this thread, without invokeLater(),
       // but it will cache RemoteAnalysisServerImpl$ServerResponseReaderThread in FileStatusMap.threads and as a result,
       // DartAnalysisServerService.myProject will be leaked in tests
       ApplicationManager.getApplication()
         .invokeLater(() -> {
-                       if (clearCache) {
-                         PsiManager.getInstance(project).dropPsiCaches();
-                       }
                        DaemonCodeAnalyzer.getInstance(project).restart();
                      },
                      ModalityState.nonModal(),
