5ea3d0a0ec4a3911d4e375c0b95c439f059025e8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-03-28 15:24:43 +0100

    WEB-65796 WebSymbols: Improve support for expected type evaluation within JSObjectLiterals with WebSymbols
    GitOrigin-RevId: 27db7e043bfec6edad0d5fa3049c79c92087314e

--- vuejs/vuejs-tests/testData/highlighting/propsValidation/propsValidation.vue
+++ vuejs/vuejs-tests/testData/highlighting/propsValidation/propsValidation.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ImportedComponent :bar="<weak_warning descr="Type string is not assignable to type number">'a'</weak_warning>" :foo="12"></ImportedComponent>
-    <WebTypesComponent :foo="<weak_warning descr="Type { a: number } is not assignable to type Foo">{a:12}</weak_warning>"></WebTypesComponent>
+    <WebTypesComponent :foo="<weak_warning descr="Type { a: number } is not assignable to type Foo">{a:<weak_warning descr="Type number is not assignable to type \"a\" | \"b\" | \"c\"  Type number is not assignable to type \"c\"    Type number is not assignable to type \"b\"">12</weak_warning>}</weak_warning>"></WebTypesComponent>
     <WebTypesComponent :foo="{a:'a', b: true}"></WebTypesComponent>
   </div>
 </template>
