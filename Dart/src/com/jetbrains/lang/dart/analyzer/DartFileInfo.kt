87dd3d21d336cadc167ac715896dbfd044a1f745
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-04-02 17:17:44 +0200

    IJ-CR-131092 IDEA-349057 Dart Macros (wip): fix uri -> path conversion on Windows
    (cherry picked from commit 12ec4f5f499768a385372b4891e333ebf72620ec)
    
    GitOrigin-RevId: dc7727ee9d252207095389224d1e0b562430f56b

--- Dart/src/com/jetbrains/lang/dart/analyzer/DartFileInfo.kt
+++ Dart/src/com/jetbrains/lang/dart/analyzer/DartFileInfo.kt
@@ -3,7 +3,9 @@ package com.jetbrains.lang.dart.analyzer
 import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.SystemInfo
 import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.util.io.OSAgnosticPathUtil
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import java.net.URI
@@ -20,7 +22,6 @@ data class DartLocalFileInfo(val filePath: String) : DartFileInfo() {
 data class DartNotLocalFileInfo(private val project: Project, val fileUri: String) : DartFileInfo() {
   override fun findFile(): VirtualFile? =
     DartAnalysisServerService.getInstance(project).getNotLocalVirtualFile(fileUri)
-
 }
 
 
@@ -28,7 +29,11 @@ fun getDartFileInfo(project: Project, filePathOrUri: String): DartFileInfo = whe
   !filePathOrUri.contains("://") -> DartLocalFileInfo(FileUtil.toSystemIndependentName(filePathOrUri))
   !filePathOrUri.startsWith("file://") -> DartNotLocalFileInfo(project, filePathOrUri)
   else -> try {
-    DartLocalFileInfo(URI(filePathOrUri).path)
+    var path = URI(filePathOrUri).path
+    if (SystemInfo.isWindows && path.length >= 3 && path[0] == '/' && OSAgnosticPathUtil.startsWithWindowsDrive(path.substring(1))) {
+      path = path.substring(1)
+    }
+    DartLocalFileInfo(path)
   }
   catch (e: URISyntaxException) {
     logger<DartFileInfo>().warn("Malformed URI: $filePathOrUri")
