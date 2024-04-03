0034339a722c0ecc6d274bb794822d27a9222896
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-04-03 16:02:08 +0200

    WEB-66321 Introduce hasFilesOfType utility
    (cherry picked from commit f20d0c362f46e0936e66ab3f92460b90ef6b53a9)
    
    IJ-CR-130618
    
    GitOrigin-RevId: 154dbf90672432f92e84ab6f3ffd912f6838e6e5

--- Astro/src/org/jetbrains/astro/context/AstroContext.kt
+++ Astro/src/org/jetbrains/astro/context/AstroContext.kt
@@ -1,16 +1,10 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.astro.context
 
-import com.intellij.openapi.project.DumbService
+import com.intellij.javascript.web.hasFilesOfType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiElement
-import com.intellij.psi.search.FileTypeIndex
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.util.CachedValueProvider
-import com.intellij.psi.util.CachedValuesManager
-import com.intellij.util.indexing.FileBasedIndexImpl
 import com.intellij.webSymbols.context.WebSymbolsContext
 import org.jetbrains.astro.AstroFramework
 import org.jetbrains.astro.lang.AstroFileType
@@ -31,12 +25,4 @@ fun isAstroFrameworkContext(contextFile: VirtualFile, project: Project): Boolean
   WebSymbolsContext.get(WebSymbolsContext.KIND_FRAMEWORK, contextFile, project) == AstroFramework.ID
 
 fun hasAstroFiles(project: Project): Boolean =
-  CachedValuesManager.getManager(project).getCachedValue(project) {
-    CachedValueProvider.Result.create(
-      FileBasedIndexImpl.disableUpToDateCheckIn<Boolean, Exception> {
-        FileTypeIndex.containsFileOfType(AstroFileType, GlobalSearchScope.projectScope(project))
-      },
-      VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS,
-      DumbService.getInstance(project)
-    )
-  }
+  hasFilesOfType(project, AstroFileType)
