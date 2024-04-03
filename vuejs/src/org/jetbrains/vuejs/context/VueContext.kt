2e46eb21d9d21bb002e5e9b8b7cdfff294324d6e
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-04-03 16:02:08 +0200

    WEB-66321 Introduce hasFilesOfType utility
    GitOrigin-RevId: f20d0c362f46e0936e66ab3f92460b90ef6b53a9

--- vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
+++ vuejs/src/org/jetbrains/vuejs/context/VueContext.kt
@@ -3,20 +3,14 @@ package org.jetbrains.vuejs.context
 
 import com.intellij.javascript.nodejs.PackageJsonData
 import com.intellij.javascript.web.WebFramework
+import com.intellij.javascript.web.hasFilesOfType
 import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
 import com.intellij.lang.javascript.modules.NodeModuleUtil
-import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.psi.PsiDirectory
 import com.intellij.psi.PsiElement
-import com.intellij.psi.search.FileTypeIndex
-import com.intellij.psi.search.GlobalSearchScope
-import com.intellij.psi.util.CachedValueProvider
-import com.intellij.psi.util.CachedValuesManager
 import com.intellij.util.asSafely
-import com.intellij.util.indexing.FileBasedIndexImpl
 import com.intellij.util.text.SemVer
 import com.intellij.webSymbols.WebSymbol
 import com.intellij.webSymbols.context.WebSymbolsContext
@@ -39,15 +33,7 @@ fun isVueContext(context: PsiElement): Boolean = vueFrameworkInstance.isInContex
 fun isVueContext(contextFile: VirtualFile, project: Project): Boolean = vueFrameworkInstance.isInContext(contextFile, project)
 
 fun hasVueFiles(project: Project): Boolean =
-  CachedValuesManager.getManager(project).getCachedValue(project) {
-    CachedValueProvider.Result.create(
-      FileBasedIndexImpl.disableUpToDateCheckIn<Boolean, Exception> {
-        FileTypeIndex.containsFileOfType(VueFileType, GlobalSearchScope.projectScope(project))
-      },
-      VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS,
-      DumbService.getInstance(project)
-    )
-  }
+  hasFilesOfType(project, VueFileType)
 
 fun hasPinia(context: PsiElement) =
   WebSymbolsContext.get(KIND_VUE_STORE, context) == VUE_STORE_PINIA
