7915195c1474b8942208366e9cb92b4d2e4e7d46
Gregory.Shrago <gregory.shrago@jetbrains.com>
2024-04-17 04:17:43 +0400

    simplify PlatformioActionGroup
    GitOrigin-RevId: 8de87a04e443a5f0fd98a5d2c523ae624918a6ae

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/ui/PlatformioActionGroup.kt
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/ui/PlatformioActionGroup.kt
@@ -1,28 +1,18 @@
 package com.jetbrains.cidr.cpp.embedded.platformio.ui
 
-import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.DefaultActionGroup
 import com.intellij.openapi.components.service
-import com.jetbrains.cidr.cpp.embedded.platformio.ClionEmbeddedPlatformioBundle
+import com.intellij.openapi.project.DumbAware
 import com.jetbrains.cidr.cpp.embedded.platformio.project.PlatformioWorkspace
-import icons.ClionEmbeddedPlatformioIcons
 
-class PlatformioActionGroup : ActionGroup(ClionEmbeddedPlatformioBundle.message("platformio.actiongroup.name"),
-                                          ClionEmbeddedPlatformioBundle.message("platformio.support"),
-                                          ClionEmbeddedPlatformioIcons.Platformio) {
-  override fun getChildren(e: AnActionEvent?): Array<AnAction> {
-    return getChildren(e, ActionManager.getInstance())
-  }
-
-  override fun getChildren(e: AnActionEvent?, manager: ActionManager): Array<AnAction> {
-    return (manager.getAction("platformio-group") as ActionGroup).getChildren(e, manager)
-  }
-
-  override fun update(e: AnActionEvent) {
-    e.presentation.isEnabledAndVisible =
-      e.project?.service<PlatformioWorkspace>()?.isInitialized ?: false
-  }
+class PlatformioActionGroup : DefaultActionGroup(), DumbAware {
 
   override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
 
-  override fun isDumbAware(): Boolean = true
+  override fun update(e: AnActionEvent) {
+    e.presentation.isEnabledAndVisible =
+      e.project?.service<PlatformioWorkspace>()?.isInitialized == true
+  }
 }
