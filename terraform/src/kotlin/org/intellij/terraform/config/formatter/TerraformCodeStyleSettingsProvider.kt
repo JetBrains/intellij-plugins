e53202bc660864af7f65ac20c1b8bf3934678778
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-04-19 13:02:32 +0300

    [terraform] Left one source folder and merged two gen folders
    GitOrigin-RevId: cc1d9045ba4da37cf307545e97905dafd0947a39

--- terraform/src/kotlin/org/intellij/terraform/config/formatter/TerraformCodeStyleSettingsProvider.kt
+++
@@ -1,8 +0,0 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.intellij.terraform.config.formatter
-
-import org.intellij.terraform.hcl.formatter.HCLCodeStyleSettingsProvider
-import org.intellij.terraform.config.TerraformLanguage
-
-class TerraformCodeStyleSettingsProvider : HCLCodeStyleSettingsProvider(TerraformLanguage)
-
