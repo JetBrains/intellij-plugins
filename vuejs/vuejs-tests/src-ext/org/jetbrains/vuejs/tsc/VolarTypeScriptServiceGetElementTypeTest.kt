d4c63a37d1f3af1f15bb55406a58ecb61fade118
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2024-05-16 04:46:37 +0200

    [js] tsc: enable type declarations conversion in some tests
    GitOrigin-RevId: 18d1ce56ed7f3459d4285cf7c20ab30744eae68a

--- vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/tsc/VolarTypeScriptServiceGetElementTypeTest.kt
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/tsc/VolarTypeScriptServiceGetElementTypeTest.kt
@@ -1,19 +1,13 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.tsc
 
-import com.intellij.lang.javascript.psi.JSRecordType
-import com.intellij.lang.javascript.psi.JSReferenceExpression
 import com.intellij.lang.javascript.psi.JSType
-import com.intellij.lang.javascript.psi.types.JSAliasTypeImpl
-import com.intellij.lang.javascript.psi.types.TypeScriptMappedJSTypeImpl
 import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.lang.typescript.tsc.TypeScriptServiceGetElementTypeTest
 import com.intellij.lang.typescript.tsc.TypeScriptServiceTestMixin
 import com.intellij.platform.lsp.tests.waitUntilFileOpenedByLspServer
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.testFramework.UsefulTestCase
-import junit.framework.TestCase
 import org.intellij.lang.annotations.Language
 import org.jetbrains.vuejs.lang.typescript.service.markVolarForceEnabled
 import org.jetbrains.vuejs.lang.typescript.service.volar.VolarTypeScriptService
@@ -36,12 +30,11 @@ class VolarTypeScriptServiceGetElementTypeTest : TypeScriptServiceGetElementType
     }
   }
 
-  // FIXME Mapped type js type declaration is not ready
   /**
    * @see [testInstantiateMappedType]
    */
   @Test
-  fun testInstantiateMappedTypeVue() = TypeScriptServiceTestMixin.temporarilyDisableJsTypeDeclarations {
+  fun testInstantiateMappedTypeVue() {
     //myFixture.configureVueDependencies(VueTestModule.VUE_3_0_0)
 
     @Language("TypeScript") val code = """
@@ -50,23 +43,9 @@ class VolarTypeScriptServiceGetElementTypeTest : TypeScriptServiceGetElementType
       const unwrapped = unwrap({foo: 123, bar: 456})
       unwrapp<caret>ed
     """.trimIndent()
-    val vueCode = "<script setuo lang='ts'>\n" + code + "\n</script>"
+    val vueCode = "<script setup lang='ts'>\n" + code + "\n</script>"
     myFixture.configureByText("a.vue", vueCode)
-
-    val ref = PsiTreeUtil.getParentOfType(myFixture.file.findElementAt(myFixture.caretOffset), JSReferenceExpression::class.java, false)!!
-    val type = calculateType(ref, true)
-
-    UsefulTestCase.assertInstanceOf(type, JSAliasTypeImpl::class.java)
-    type as JSAliasTypeImpl
-    UsefulTestCase.assertInstanceOf(type.alias, TypeScriptMappedJSTypeImpl::class.java)
-    UsefulTestCase.assertInstanceOf(type.originalType, JSRecordType::class.java)
-
-    val recordType = type.originalType as JSRecordType
-    TestCase.assertEquals("{foo: 123, bar: 456}", recordType.sourceElement?.text)
-    TestCase.assertEquals(
-      setOf("foo: 123", "bar: 456"),
-      recordType.properties.map { it.memberSource.singleElement?.text }.toSet()
-    )
+    doTestInstantiateMappedType()
   }
 
 }
