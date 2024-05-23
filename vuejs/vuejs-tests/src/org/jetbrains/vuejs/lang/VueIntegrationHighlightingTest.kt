4b9da34ed7921fa1202f98c4b152a4e0b031fad7
Andrey Vorobev <andrey.vorobev@jetbrains.com>
2024-05-23 16:43:45 +0000

    WEB-59986 Typescript/tsx: Code completion does not honor overrides via module augmentation
    Merge-request: IJ-MR-120132
    Merged-by: Andrey Vorobev <Andrey.Vorobev@jetbrains.com>
    
    GitOrigin-RevId: d10cd8b0e923875c587ed72f5a96214bdd67eac9

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueIntegrationHighlightingTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueIntegrationHighlightingTest.kt
@@ -2,9 +2,13 @@
 package org.jetbrains.vuejs.lang
 
 import com.intellij.codeInspection.htmlInspections.HtmlUnknownTagInspection
+import com.intellij.html.webSymbols.elements.WebSymbolElementDescriptor
 import com.intellij.javascript.debugger.NodeJsAppRule
 import com.intellij.javascript.nodejs.npm.NpmUtil
 import com.intellij.lang.javascript.linter.JSExternalToolIntegrationTest
+import com.intellij.psi.xml.XmlTag
+import com.intellij.util.asSafely
+import com.intellij.webSymbols.PsiSourcedWebSymbol
 import org.jetbrains.vuejs.inspections.VueMissingComponentImportInspection
 
 class VueIntegrationHighlightingTest: JSExternalToolIntegrationTest() {
@@ -16,6 +20,23 @@ class VueIntegrationHighlightingTest: JSExternalToolIntegrationTest() {
     myFixture.enableInspections(VueMissingComponentImportInspection::class.java, HtmlUnknownTagInspection::class.java)
     myFixture.configureFromTempProjectFile("src/HelloWorld.vue")
     myFixture.checkHighlighting()
+  }
+
+  fun testResolveWithJsConfig() {
+    copyTestDirectoryAndInstallDependencies()
+    myFixture.configureFromTempProjectFile("src/HelloWorld.vue")
+    val tagAtCaret = myFixture.elementAtCaret.asSafely<XmlTag>()
+    assertNotNull(tagAtCaret)
+
+    val webSymbolDescriptor = tagAtCaret?.descriptor.asSafely<WebSymbolElementDescriptor>()
+    assertNotNull(webSymbolDescriptor)
+
+    val webSymbol = webSymbolDescriptor?.symbol.asSafely<PsiSourcedWebSymbol>()
+    val webSymbolSource = webSymbol?.source
+    assertNotNull(webSymbolSource)
+
+    val sourceFileName = webSymbolSource!!.containingFile.name
+    assertTrue("Wrong source file: $sourceFileName", sourceFileName == "index.d.ts")
   }
 
   private fun copyTestDirectoryAndInstallDependencies() {
