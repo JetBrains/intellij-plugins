ea4c830accce94d718bf0607110946415c7b5edd
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2024-05-31 21:17:05 +0200

    [vue] WEB-63964 propagate the import specifier used during resolve according to JS resolve logic
    GitOrigin-RevId: 0aba4fee51032d284883bfb69a9ceefc31e10cab

--- vuejs/src/org/jetbrains/vuejs/codeInsight/template/VueScriptSetupScopeProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/template/VueScriptSetupScopeProvider.kt
@@ -1,10 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.codeInsight.template
 
+import com.intellij.lang.ecmascript6.psi.ES6ImportSpecifier
 import com.intellij.lang.javascript.psi.JSExecutionScope
 import com.intellij.lang.javascript.psi.JSPsiNamedElementBase
+import com.intellij.lang.javascript.psi.resolve.JSResolveResult
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.ResolveResult
 import org.jetbrains.vuejs.codeInsight.VueExprStubBasedScopeHandler
 import org.jetbrains.vuejs.codeInsight.resolveIfImportSpecifier
@@ -25,9 +26,12 @@ class VueScriptSetupScopeProvider : VueTemplateScopesProvider() {
     override fun resolve(consumer: Consumer<in ResolveResult>) {
       VueExprStubBasedScopeHandler.processDeclarationsInScope(module, { element, _ ->
         val resolved = (element as? JSPsiNamedElementBase)?.resolveIfImportSpecifier()
+        // let's patch back in JavaScript plugin resolve conventions
+        val importSpecifier = if (element != resolved && element is ES6ImportSpecifier) { element } else null
         val elementToConsume = VueCompositionInfoHelper.getUnwrappedRefElement(resolved, module)
                                ?: resolved ?: element
-        consumer.accept(PsiElementResolveResult(elementToConsume, true)).let { true }
+        consumer.accept(JSResolveResult(elementToConsume, importSpecifier, null))
+        true
       }, true)
     }
   }
