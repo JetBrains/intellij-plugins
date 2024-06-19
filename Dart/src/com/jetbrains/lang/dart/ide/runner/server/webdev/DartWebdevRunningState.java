8c8799fe5d411f23f91cc8feb766fac0fadda131
Alexander Doroshko <alexander.doroshko@jetbrains.com>
2024-06-19 14:23:38 +0200

    [Dart] cleanup in DartWebdevRunningState: remove unused field myObservatoryUrlConsumers
    IJ-CR-139868
    (cherry picked from commit a88d595fe26363ada596fb810a413028f122b451)
    
    GitOrigin-RevId: 1a8431fa04cfc82f65510ef7ad8201e3c0956c81

--- Dart/src/com/jetbrains/lang/dart/ide/runner/server/webdev/DartWebdevRunningState.java
+++ Dart/src/com/jetbrains/lang/dart/ide/runner/server/webdev/DartWebdevRunningState.java
@@ -2,7 +2,6 @@
 package com.jetbrains.lang.dart.ide.runner.server.webdev;
 
 import com.intellij.execution.ExecutionException;
-import com.intellij.execution.Executor;
 import com.intellij.execution.configurations.CommandLineState;
 import com.intellij.execution.configurations.GeneralCommandLine;
 import com.intellij.execution.configurations.RuntimeConfigurationError;
@@ -13,32 +12,21 @@ import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.process.ProcessHandler;
 import com.intellij.execution.process.ProcessTerminatedListener;
 import com.intellij.execution.runners.ExecutionEnvironment;
-import com.intellij.execution.ui.ConsoleView;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.Separator;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.util.Consumer;
 import com.jetbrains.lang.dart.DartBundle;
 import com.jetbrains.lang.dart.ide.actions.DartPubActionBase;
 import com.jetbrains.lang.dart.ide.runner.DartRelativePathsConsoleFilter;
-import com.jetbrains.lang.dart.ide.runner.server.OpenDartObservatoryUrlAction;
 import com.jetbrains.lang.dart.pubServer.DartWebdev;
 import com.jetbrains.lang.dart.sdk.DartSdk;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
 public class DartWebdevRunningState extends CommandLineState {
 
   @NotNull protected DartWebdevParameters myDartWebdevParameters;
-
-  private final Collection<Consumer<String>> myObservatoryUrlConsumers = new ArrayList<>();
 
   public DartWebdevRunningState(final @NotNull ExecutionEnvironment env) throws ExecutionException {
     super(env);
@@ -63,28 +51,6 @@ public class DartWebdevRunningState extends CommandLineState {
     catch (RuntimeConfigurationError ignore) {/* can't happen because checked in myDartWebdevParameters.check(project) */}
 
     DartWebdevConsoleView.install(env.getProject(), this);
-  }
-
-  public void addObservatoryUrlConsumer(@NotNull final Consumer<String> consumer) {
-    myObservatoryUrlConsumers.add(consumer);
-  }
-
-  @Override
-  protected AnAction @NotNull [] createActions(final ConsoleView console, final ProcessHandler processHandler, final Executor executor) {
-    // These actions are effectively added only to the Run tool window. For Debug see DartCommandLineDebugProcess.registerAdditionalActions()
-    final List<AnAction> actions = new ArrayList<>(Arrays.asList(super.createActions(console, processHandler, executor)));
-    addObservatoryActions(actions, processHandler);
-    return actions.toArray(AnAction.EMPTY_ARRAY);
-  }
-
-  private void addObservatoryActions(List<AnAction> actions, final ProcessHandler processHandler) {
-    actions.add(new Separator());
-
-    final OpenDartObservatoryUrlAction openObservatoryAction =
-      new OpenDartObservatoryUrlAction(null, () -> !processHandler.isProcessTerminated());
-    addObservatoryUrlConsumer(openObservatoryAction::setUrl);
-
-    actions.add(openObservatoryAction);
   }
 
   @NotNull
