3cc179df2e9f14fdc2548ba14950a645251db9a9
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-06-21 22:25:20 +0200

    WEB-67260 Angular: ensure no events left on EDT
    GitOrigin-RevId: 0d60e540fe2e5fa3ef84f0fdcca2cd0c2b38b9de

--- Angular/test/org/angular2/Angular2TestCase.kt
+++ Angular/test/org/angular2/Angular2TestCase.kt
@@ -9,6 +9,7 @@ import com.intellij.lang.typescript.tsc.TypeScriptServiceTestMixin
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.util.registry.Registry
 import com.intellij.testFramework.runInEdtAndWait
+import com.intellij.util.ui.UIUtil
 import org.angular2.lang.expr.service.AngularTypeScriptService
 
 abstract class Angular2TestCase(override val testCasePath: String) : WebFrameworkTestCase() {
@@ -30,6 +31,9 @@ abstract class Angular2TestCase(override val testCasePath: String) : WebFramewor
   }
 
   override fun beforeConfiguredTest() {
+    runInEdtAndWait {
+      UIUtil.dispatchAllInvocationEvents()
+    }
     TypeScriptServiceTestMixin.setUpTypeScriptService(myFixture, TypeScriptUseServiceState.USE_FOR_EVERYTHING) {
       it::class.java == TypeScriptServerServiceImpl::class.java
     }
