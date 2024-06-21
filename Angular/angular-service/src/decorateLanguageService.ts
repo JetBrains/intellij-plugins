3478dc002b5c9ba69a25f07cade9c2fdaf836481
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-06-21 15:55:05 +0200

    WEB-67260 Angular: add reverse mapper to allow mapping type sources back to template expressions
    GitOrigin-RevId: 3e4399834eeb61d052a011c35cc20f62f7dd04f5

--- Angular/angular-service/src/decorateLanguageService.ts
+++ Angular/angular-service/src/decorateLanguageService.ts
@@ -1,10 +1,18 @@
-import {Language} from "@volar/language-core"
+import {type CodeInformation, Language, type SourceScript, type TypeScriptServiceScript} from "@volar/language-core"
 import type * as TS from "./languageService"
 import type {GetElementTypeResponse, Range} from "tsc-ide-plugin/protocol"
 import type {ReverseMapper} from "tsc-ide-plugin/ide-get-element-type"
 import {getServiceScript} from "@volar/typescript/lib/node/utils"
-import {toGeneratedRange} from "@volar/typescript/lib/node/transform"
+import {toGeneratedRange, toSourceRanges} from "@volar/typescript/lib/node/transform"
 import {AngularVirtualCode} from "./code"
+import type * as ts from "typescript"
+
+function toSourceRange(sourceScript: SourceScript<string> | undefined, language: Language<string>, serviceScript: TypeScriptServiceScript, range: ts.TextRange, filter: (data: CodeInformation) => boolean): [fileName: string, range: ts.TextRange] | undefined {
+  for (const result of toSourceRanges(sourceScript, language, serviceScript, range, filter)) {
+    return result
+  }
+  return undefined
+}
 
 export function decorateIdeLanguageServiceExtensions(language: Language<string>, languageService: TS.LanguageService) {
 
@@ -12,6 +20,36 @@ export function decorateIdeLanguageServiceExtensions(language: Language<string>,
 
   if (webStormGetElementType === undefined || webStormGetElementType === undefined || webStormGetSymbolType === undefined)
     return
+
+  const ngReverseMapper = function (ts: typeof TS, sourceFile: TS.SourceFile, generatedRange: Range): { pos: number, end: number, fileName: string } | undefined {
+    const [serviceScript, targetScript, sourceScript] =
+      getServiceScript(language, sourceFile.fileName);
+    if (targetScript?.associatedOnly) {
+      return undefined;
+    }
+    const program = languageService.getProgram()
+    const generatedFile = targetScript ? program?.getSourceFile(targetScript.id) : undefined;
+    if (serviceScript && sourceFile && generatedFile) {
+      try {
+        let generatedRangePosStart = ts.getPositionOfLineAndCharacter(generatedFile, generatedRange.start.line, generatedRange.start.character)
+        let generatedRangePosEnd = ts.getPositionOfLineAndCharacter(generatedFile, generatedRange.end.line, generatedRange.end.character)
+
+        const sourceRange = toSourceRange(sourceScript, language, serviceScript, {pos: generatedRangePosStart, end: generatedRangePosEnd}, () => true);
+
+        if (sourceRange !== undefined) {
+          return {
+            fileName: sourceRange[0],
+            pos: sourceRange[1].pos,
+            end: sourceRange[1].end
+          }
+        }
+      }
+      catch (e) {
+        // ignore
+      }
+    }
+    return undefined;
+  }
 
   languageService.webStormGetElementType = (
     ts: typeof TS,
@@ -39,9 +77,7 @@ export function decorateIdeLanguageServiceExtensions(language: Language<string>,
         if (generatedRange !== undefined) {
           const start = ts.getLineAndCharacterOfPosition(generatedFile, generatedRange.pos)
           const end = ts.getLineAndCharacterOfPosition(generatedFile, generatedRange.end)
-
-          //TODO support reverseMapper
-          return webStormGetElementType(ts as any, targetScript.id, {start, end}, forceReturnType)
+          return webStormGetElementType(ts as any, targetScript.id, {start, end}, forceReturnType, ngReverseMapper.bind(null, ts))
         }
       }
       catch (e) {
@@ -57,19 +93,17 @@ export function decorateIdeLanguageServiceExtensions(language: Language<string>,
   languageService.webStormGetSymbolType = (
     ts,
     symbolId: number,
-    reverseMapper?: ReverseMapper,
+    _reverseMapper?: ReverseMapper,
   ) => {
-    //TODO support reverseMapper
-    return webStormGetSymbolType(ts, symbolId, reverseMapper)
+    return webStormGetSymbolType(ts, symbolId, ngReverseMapper.bind(null, ts))
   }
 
   languageService.webStormGetTypeProperties = (
     ts,
     typeId: number,
-    reverseMapper?: ReverseMapper,
+    _reverseMapper?: ReverseMapper,
   ) => {
-    //TODO support reverseMapper
-    return webStormGetTypeProperties(ts, typeId, reverseMapper)
+    return webStormGetTypeProperties(ts, typeId, ngReverseMapper.bind(null, ts))
   }
 
 }
