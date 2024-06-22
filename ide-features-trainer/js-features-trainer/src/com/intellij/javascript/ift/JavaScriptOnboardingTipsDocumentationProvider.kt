5f48a244886f92a3ed6ebbfed5816a0c45a67a09
Andrey Vorobev <andrey.vorobev@jetbrains.com>
2024-06-22 20:49:44 +0000

    WEB-66320 Add JavaScript and TypeScript project templates
    Merge-request: IJ-MR-137404
    Merged-by: Andrey Vorobev <Andrey.Vorobev@jetbrains.com>
    
    GitOrigin-RevId: f93ed1c4afae9db3a458156044d22149204bfd08

---
+++ ide-features-trainer/js-features-trainer/src/com/intellij/javascript/ift/JavaScriptOnboardingTipsDocumentationProvider.kt
@@ -0,0 +1,13 @@
+package com.intellij.javascript.ift
+
+import com.intellij.lang.javascript.JSTokenTypes
+import com.intellij.lang.javascript.psi.JSFile
+import com.intellij.psi.PsiFile
+import training.onboarding.AbstractOnboardingTipsDocumentationProvider
+
+
+class JavaScriptOnboardingTipsDocumentationProvider: AbstractOnboardingTipsDocumentationProvider(JSTokenTypes.END_OF_LINE_COMMENT) {
+  override fun isLanguageFile(file: PsiFile): Boolean {
+    return file is JSFile
+  }
+}
