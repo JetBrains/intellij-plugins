6bd5eff8e35acc08098d56558518ae9855d85092
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-07-01 17:36:01 +0200

    [terraform] IJPL-157474 Moved GIST initialization to an app-level service
    (cherry picked from commit 30b4cc4eebc7b119721de68ff04d65ea13f6c8e3)
    
    IJ-MR-138448
    
    GitOrigin-RevId: d5ca270b0edb75e7c7c3b0427e2a9729b0585b0b

---
+++ terraform/src/org/intellij/terraform/config/model/local/LocalProviderNamesService.kt
@@ -0,0 +1,55 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.intellij.terraform.config.model.local
+
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.util.ThrowableComputable
+import com.intellij.openapi.util.io.DataInputOutputUtilRt
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiRecursiveElementVisitor
+import com.intellij.util.ThrowableConsumer
+import com.intellij.util.gist.GistManager
+import com.intellij.util.io.DataExternalizer
+import org.intellij.terraform.config.model.TypeModel.Companion.getTerraformBlock
+import org.intellij.terraform.config.patterns.TerraformPatterns.RequiredProvidersData
+import org.intellij.terraform.config.patterns.TerraformPatterns.RequiredProvidersSource
+import org.intellij.terraform.hcl.psi.HCLProperty
+import java.io.DataInput
+import java.io.DataOutput
+import java.io.IOException
+
+@Service
+internal class LocalProviderNamesService {
+
+  val providersNamesGist =
+    GistManager.getInstance().newPsiFileGist<Map<String, String>>("TF_PROVIDER_LIST", 1, object : DataExternalizer<Map<String, String>> {
+      override fun save(out: DataOutput, value: Map<String, String>) {
+        DataInputOutputUtilRt.writeMap(out, value,
+                                       ThrowableConsumer { out.writeUTF(it) },
+                                       ThrowableConsumer { out.writeUTF(it) })
+      }
+
+      override fun read(input: DataInput): Map<String, String> {
+        return DataInputOutputUtilRt.readMap(input,
+                                             ThrowableComputable<String, IOException> { input.readUTF() },
+                                             ThrowableComputable<String, IOException> { input.readUTF() })
+      }
+    }) { psiFile ->
+      val localNames = mutableMapOf<String, String>()
+      val terraformRootBlock = getTerraformBlock(psiFile)
+      terraformRootBlock?.accept(RequiredProvidersVisitor(localNames))
+      localNames
+    }
+
+  private class RequiredProvidersVisitor(private val localNames: MutableMap<String, String>) : PsiRecursiveElementVisitor() {
+    override fun visitElement(element: PsiElement) {
+      if (RequiredProvidersData.accepts(element)) {
+        element as HCLProperty
+        val localName = element.name
+        val source = element.value?.children?.filter { RequiredProvidersSource.accepts(it) }?.firstOrNull() as? HCLProperty
+        source?.value?.name?.let { localNames.put(localName, it) }
+      }
+      element.acceptChildren(this)
+    }
+  }
+
+}
