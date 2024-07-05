55b61cce65e18d0714b065e4cf37dfc6061b990a
Vlad Minaev <vladislav.minaev@jetbrains.com>
2024-07-05 16:09:43 +0300

    [js] WEB-67436 Suggest to download Node.js interpreter from the editor (tighten language list WI-78129)
    GitOrigin-RevId: fdc426ea3512a5e0c4b6d6ee6fe270a3b6926d08

---
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueNodeDownloadEditorNotificationProviderTest.kt
@@ -0,0 +1,24 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang
+
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterManager
+import com.intellij.javascript.nodejs.interpreter.NodeJsInterpreterRef
+import com.intellij.javascript.nodejs.interpreter.download.NodeDownloadEditorNotificationProvider
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+
+class VueNodeDownloadEditorNotificationProviderTest : BasePlatformTestCase() {
+
+  public override fun setUp() {
+    super.setUp()
+    NodeJsInterpreterManager.getInstance(myFixture.project).setInterpreterRef(NodeJsInterpreterRef.create("noop"))
+    myFixture.addFileToProject("package.json", "{}")
+  }
+
+  fun testNotificationAppears() {
+    myFixture.configureByText("node_download_notification.vue", "")
+    assertNotNull(getNotificationData())
+  }
+
+  private fun getNotificationData() = NodeDownloadEditorNotificationProvider().collectNotificationData(myFixture.project, myFixture.file.virtualFile)
+
+}
