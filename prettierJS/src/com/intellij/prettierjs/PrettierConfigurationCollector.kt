cdc1846c8a7cdde5e89f2882467d3f77d89cc013
Dmitry Makhnev <dmitry.makhnev@jetbrains.com>
2024-07-08 20:10:00 +0200

    [Prettier] WEB-67956: Add FUS for enabling reformat with Prettier by default
    - Add and integrate FUS collector
    
    IJ-CR-139258
    (cherry picked from commit b92ba8b68bbb3cad6785954396f81162e13736e1)
    
    GitOrigin-RevId: f96583a7cd1b33a6dc02bafbe86a9002dd9f590f

---
+++ prettierJS/src/com/intellij/prettierjs/PrettierConfigurationCollector.kt
@@ -0,0 +1,44 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.prettierjs
+
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields.Enum
+import com.intellij.internal.statistic.eventLog.events.EventId3
+import com.intellij.internal.statistic.service.fus.collectors.CounterUsagesCollector
+
+internal object PrettierConfigurationCollector : CounterUsagesCollector() {
+  private val GROUP: EventLogGroup = EventLogGroup("prettier.configuration", 1)
+
+  override fun getGroup(): EventLogGroup = GROUP
+
+  private val AUTO_ENABLE_IN_NEW_PROJECT: EventId3<EnabledStatus, PackageDeclarationLocation, ConfigLocation> = GROUP.registerEvent(
+    "auto.enable.in.new.project",
+    Enum("enabled_status", EnabledStatus::class.java),
+    Enum("package_declaration_location", PackageDeclarationLocation::class.java),
+    Enum("config_location", ConfigLocation::class.java)
+  )
+
+  enum class EnabledStatus {
+    AUTOMATIC,
+    UNCHANGED
+  }
+
+  enum class PackageDeclarationLocation {
+    IN_PROJECT_ROOT_PACKAGE,
+    NONE
+  }
+
+  enum class ConfigLocation {
+    CONFIG_FILE,
+    PACKAGE_JSON,
+    NONE
+  }
+
+  fun logAutoEnableInNewProject(
+    resolvedConfigMode: EnabledStatus,
+    packageDeclarationPlace: PackageDeclarationLocation = PackageDeclarationLocation.NONE,
+    configLocation: ConfigLocation = ConfigLocation.NONE,
+  ) {
+    AUTO_ENABLE_IN_NEW_PROJECT.log(resolvedConfigMode, packageDeclarationPlace, configLocation)
+  }
+}
