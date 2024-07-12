aa395eb6013873b85ce546ee0e5bd75fa9dfe915
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2024-07-12 19:03:18 +0200

    [vue] WEB-68084 tsc: fix template variable type
    GitOrigin-RevId: cb73de690582f1865da59e9edbaebb3c6eabceaf

--- vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/tsc/VolarTypeScriptServiceGetElementTypeTest.kt
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/tsc/VolarTypeScriptServiceGetElementTypeTest.kt
@@ -1,17 +1,24 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.tsc
 
+import com.intellij.lang.javascript.JSTestUtils
 import com.intellij.lang.javascript.TrackFailedTestRule
+import com.intellij.lang.javascript.evaluation.JSTypeEvaluationLocationProvider
 import com.intellij.lang.javascript.psi.JSType
+import com.intellij.lang.javascript.psi.JSVariable
 import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.lang.typescript.tsc.TypeScriptServiceGetElementTypeTest
 import com.intellij.lang.typescript.tsc.TypeScriptServiceTestMixin
 import com.intellij.platform.lsp.tests.waitUntilFileOpenedByLspServer
 import com.intellij.psi.PsiElement
 import com.intellij.testFramework.UsefulTestCase
+import junit.framework.TestCase
 import org.intellij.lang.annotations.Language
+import org.jetbrains.vuejs.lang.VueTestModule
+import org.jetbrains.vuejs.lang.configureVueDependencies
 import org.jetbrains.vuejs.lang.typescript.service.VueServiceSetActivationRule
 import org.jetbrains.vuejs.lang.typescript.service.volar.VolarTypeScriptService
+import org.jetbrains.vuejs.types.VueUnwrapRefType
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
@@ -59,4 +66,22 @@ class VolarTypeScriptServiceGetElementTypeTest : TypeScriptServiceGetElementType
     doTestInstantiateMappedType()
   }
 
+  @Test
+  fun testUnwrapRefType() {
+    // WEB-68084
+    myFixture.configureVueDependencies(VueTestModule.VUE_3_4_0)
+    val vueCode = """
+      <script setup lang='ts'>
+      const a:number = 42
+      </script>
+    """.trimIndent()
+    myFixture.configureByText("a.vue", vueCode)
+    val element = JSTestUtils.findElementByText(myFixture, "a:number = 42", JSVariable::class.java)
+    val jsType = calculateType(element, true)
+    TestCase.assertNotNull(jsType)
+    JSTypeEvaluationLocationProvider.withTypeEvaluationLocation(element) {
+      val unwrapRefType = VueUnwrapRefType(jsType!!, element).substitute()
+      assertEquals("number", unwrapRefType.getTypeText(JSType.TypeTextFormat.PRESENTABLE))
+    }
+  }
 }
