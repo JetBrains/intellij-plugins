4988866f193b52e2096a95023097cc7bce89b291
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-07-23 18:42:31 +0300

    [terraform] IJPL-158303 Added more templates for tf files and some refactoring
    (cherry picked from commit 2e7e4079276f65f60cc5602f6318cd311d96c262)
    
    GitOrigin-RevId: 7a0b09f4246a2dc3d4a4c72e9bf7b8d33358b18b

--- terraform/src/org/intellij/terraform/config/actions/TfCreateFileFromTemplateAction.kt
+++ terraform/src/org/intellij/terraform/config/actions/TfCreateFileFromTemplateAction.kt
@@ -3,9 +3,11 @@ package org.intellij.terraform.config.actions
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction
 import com.intellij.ide.actions.CreateFileFromTemplateDialog
+import com.intellij.ide.fileTemplates.FileTemplate
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiDirectory
+import com.intellij.psi.PsiFile
 import org.intellij.terraform.TerraformIcons
 import org.intellij.terraform.hcl.HCLBundle
 
@@ -13,10 +15,36 @@ class TfCreateFileFromTemplateAction : CreateFileFromTemplateAction(), DumbAware
   override fun buildDialog(project: Project, directory: PsiDirectory, builder: CreateFileFromTemplateDialog.Builder) {
     builder.run {
       setTitle(HCLBundle.message("action.create.terraform.file.title"))
-      addKind(HCLBundle.message("action.NewTerraformFile.description"), TerraformIcons.Terraform, "Terraform File")
+      setDefaultText(DEFAULT_FILE_NAME)
+
+      addKind(HCLBundle.message("action.new.empty.terraform.file.description"), TerraformIcons.Terraform, EMPTY_TF_TEMPLATE)
+      addKind(HCLBundle.message("action.new.template.terraform.file.description"), TerraformIcons.Terraform, TEMPLATE_TF_TEMPLATE)
+      addKind(HCLBundle.message("action.new.outputs.terraform.file.description"), TerraformIcons.Terraform, OUTPUTS_TF_TEMPLATE)
+      addKind(HCLBundle.message("action.new.variables.terraform.file.description"), TerraformIcons.Terraform, VARIABLES_TF_TEMPLATE)
     }
   }
 
   override fun getActionName(directory: PsiDirectory?, newName: String, templateName: String?): String =
     HCLBundle.message("action.create.terraform.file", templateName)
+
+  override fun createFileFromTemplate(name: String?, template: FileTemplate?, dir: PsiDirectory?): PsiFile =
+    super.createFileFromTemplate(handleFileName(name, template), template, dir)
+
+  private fun handleFileName(name: String?, template: FileTemplate?): String? {
+    val isDefaultName = name.isNullOrEmpty() || name == DEFAULT_FILE_NAME
+
+    val newName = when (template?.name) {
+      OUTPUTS_TF_TEMPLATE -> "outputs"
+      VARIABLES_TF_TEMPLATE -> "variables"
+      else -> name
+    }
+    return if (isDefaultName) newName else name
+  }
 }
+
+private const val EMPTY_TF_TEMPLATE = "Empty File"
+private const val TEMPLATE_TF_TEMPLATE = "Template File"
+private const val OUTPUTS_TF_TEMPLATE = "Outputs File"
+private const val VARIABLES_TF_TEMPLATE = "Variables File"
+
+private const val DEFAULT_FILE_NAME = "main"
--- terraform/src/org/intellij/terraform/hcl/formatter/HCLCodeStylePanel.kt
+++ terraform/src/org/intellij/terraform/hcl/formatter/HCLCodeStylePanel.kt
@@ -18,7 +18,7 @@ class HCLCodeStylePanel(private val language: Language, settings: CodeStyleSetti
 
   override fun initTables() {
     // Format alignment
-    val alignmentValues = HCLCodeStyleSettings.PropertyAlignment.entries.toTypedArray()
+    val alignmentValues = HCLCodeStyleSettings.PropertyAlignment.entries
     val alignmentStrings = arrayOfNulls<String>(alignmentValues.size)
     val alignmentInts = IntArray(alignmentValues.size)
     for (i in alignmentValues.indices) {
@@ -27,7 +27,7 @@ class HCLCodeStylePanel(private val language: Language, settings: CodeStyleSetti
     }
 
     // Line Commenter character
-    val lineCommenterPrefixValues = HCLCodeStyleSettings.LineCommenterPrefix.entries.toTypedArray()
+    val lineCommenterPrefixValues = HCLCodeStyleSettings.LineCommenterPrefix.entries
     val lineCommenterPrefixStrings = arrayOfNulls<String>(lineCommenterPrefixValues.size)
     val lineCommenterPrefixInts = IntArray(lineCommenterPrefixValues.size)
     for (i in lineCommenterPrefixValues.indices) {
@@ -49,19 +49,17 @@ class HCLCodeStylePanel(private val language: Language, settings: CodeStyleSetti
   override fun getDefaultLanguage(): Language = language
 
   override fun getPreviewText(): String = ALIGNMENT_SAMPLE
-
-  companion object {
-    const val ALIGNMENT_SAMPLE: String = "#first logical block\n" +
-                                         "first = true\n" +
-                                         "second = false\n" +
-                                         "t_h_i_r_d = 1\n" +
-                                         "object = {\n" +
-                                         "  nested = true\n" +
-                                         "  z = false\n" +
-                                         "}\n" +
-                                         "\n" +
-                                         "#another logical block\n" +
-                                         "x = true\n" +
-                                         "long = 'acceptable'"
-  }
 }
+
+private const val ALIGNMENT_SAMPLE: String = "#first logical block\n" +
+                                             "first = true\n" +
+                                             "second = false\n" +
+                                             "t_h_i_r_d = 1\n" +
+                                             "object = {\n" +
+                                             "  nested = true\n" +
+                                             "  z = false\n" +
+                                             "}\n" +
+                                             "\n" +
+                                             "#another logical block\n" +
+                                             "x = true\n" +
+                                             "long = 'acceptable'"
