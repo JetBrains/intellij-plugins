3ea89d3dd44a9046b04980bcb41bcc3f08436e20
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2024-07-25 13:26:57 +0200

    [js] WEB-68347 fix JavaScriptLineMarkerProvider class initialization, cleanup
    GitOrigin-RevId: 90f04d5b72abf686687924764d932461aeca1737

--- flex/src/com/intellij/javascript/flex/mxml/XmlBackedClassLineMarkerProvider.java
+++ flex/src/com/intellij/javascript/flex/mxml/XmlBackedClassLineMarkerProvider.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.javascript.flex.mxml;
 
 import com.intellij.codeInsight.daemon.LineMarkerInfo;
@@ -47,7 +48,7 @@ public final class XmlBackedClassLineMarkerProvider extends LineMarkerProviderDe
         XmlToken nameElement = XmlTagUtil.getStartTagNameElement((XmlTag)parent);
         if (classQuery.findFirst() != null && nameElement != null) {
           result.add(new LineMarkerInfo<>(element, nameElement.getTextRange(), AllIcons.Gutter.OverridenMethod,
-                                          JavaScriptLineMarkerProvider.ourClassInheritorsTooltipProvider,
+                                          JavaScriptLineMarkerProvider::getClassInheritorsTooltip,
                                           JavaScriptLineMarkerProvider.ourClassInheritorsNavHandler, GutterIconRenderer.Alignment.RIGHT));
         }
       }
