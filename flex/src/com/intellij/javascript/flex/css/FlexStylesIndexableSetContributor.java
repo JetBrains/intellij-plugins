1a22a4b1507231fe620cdb80cf995c8d3606c267
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-08-05 21:23:37 +0200

    IJPL-159596 prefer SynchronizedClearableLazy
    GitOrigin-RevId: 0409e7a776832a4f30e02106edc390dbddd8bf52

--- flex/src/com/intellij/javascript/flex/css/FlexStylesIndexableSetContributor.java
+++ flex/src/com/intellij/javascript/flex/css/FlexStylesIndexableSetContributor.java
@@ -2,22 +2,23 @@
 package com.intellij.javascript.flex.css;
 
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.util.NotNullLazyValue;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.search.DelegatingGlobalSearchScope;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.util.concurrency.SynchronizedClearableLazy;
 import com.intellij.util.indexing.IndexableSetContributor;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URL;
 import java.util.Collections;
 import java.util.Set;
+import java.util.function.Supplier;
 
 public final class FlexStylesIndexableSetContributor extends IndexableSetContributor {
   private static final Logger LOG = Logger.getInstance(FlexStylesIndexableSetContributor.class);
 
-  private static final NotNullLazyValue<Set<VirtualFile>> ourFiles = NotNullLazyValue.atomicLazy(() -> {
+  private static final Supplier<Set<VirtualFile>> ourFiles = new SynchronizedClearableLazy<>(() -> {
     URL libFileUrl = FlexStylesIndexableSetContributor.class.getResource("FlexStyles.as");
     VirtualFile file = VfsUtil.findFileByURL(libFileUrl);
     if (file != null) {
@@ -31,14 +32,14 @@ public final class FlexStylesIndexableSetContributor extends IndexableSetContrib
 
   @Override
   public @NotNull Set<VirtualFile> getAdditionalRootsToIndex() {
-    return ourFiles.getValue();
+    return ourFiles.get();
   }
 
   public static @NotNull GlobalSearchScope enlarge(final @NotNull GlobalSearchScope scope) {
     return scope.union(new DelegatingGlobalSearchScope(scope) {
       @Override
       public boolean contains(final @NotNull VirtualFile file) {
-        return ourFiles.getValue().contains(file);
+        return ourFiles.get().contains(file);
       }
     });
   }
