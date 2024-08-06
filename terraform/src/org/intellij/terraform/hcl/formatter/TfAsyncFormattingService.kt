ca85726ebdb05233b16fc8ccd09b689812f0a57a
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-08-06 17:03:28 +0300

    [terraform] IJPL-156030 Update file extensions for 'terraform fmt'
    GitOrigin-RevId: cd6d5c6cf3669ad7159e4a1ee0032d35aebe5aee

--- terraform/src/org/intellij/terraform/hcl/formatter/TfAsyncFormattingService.kt
+++ terraform/src/org/intellij/terraform/hcl/formatter/TfAsyncFormattingService.kt
@@ -6,17 +6,15 @@ import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.formatting.service.AsyncDocumentFormattingService
 import com.intellij.formatting.service.AsyncFormattingRequest
 import com.intellij.formatting.service.FormattingService
-import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VfsUtil
 import com.intellij.psi.PsiFile
 import org.intellij.terraform.config.Constants.TF_FMT
 import org.intellij.terraform.config.TerraformConstants.EXECUTION_NOTIFICATION_GROUP
+import org.intellij.terraform.config.TerraformFileType
 import org.intellij.terraform.config.actions.isTerraformExecutable
 import org.intellij.terraform.config.util.TFExecutor
 import org.intellij.terraform.hcl.HCLBundle
-import org.intellij.terraform.hcl.psi.HCLFile
-import org.intellij.terraform.isTerraformFileExtension
 
 class TfAsyncFormattingService : AsyncDocumentFormattingService() {
   override fun getName(): String = TF_FMT
@@ -27,26 +25,17 @@ class TfAsyncFormattingService : AsyncDocumentFormattingService() {
 
   override fun canFormat(file: PsiFile): Boolean {
     val settings = CodeStyle.getCustomSettings(file, HclCodeStyleSettings::class.java)
-    return file is HCLFile && settings.RUN_TF_FMT_ON_REFORMAT
+    return (file.fileType is TerraformFileType || file.name.endsWith(TestsExtension)) && settings.RUN_TF_FMT_ON_REFORMAT
   }
 
-  override fun createFormattingTask(formattingRequest: AsyncFormattingRequest): FormattingTask? {
-    val context = formattingRequest.context
+  override fun createFormattingTask(request: AsyncFormattingRequest): FormattingTask? {
+    val context = request.context
     val project = context.project
     if (!isTerraformExecutable(project)) {
       return null
     }
 
     val virtualFile = context.virtualFile ?: return null
-    if (!isTerraformFileExtension(virtualFile.extension) && !virtualFile.name.endsWith(".tftest.hcl")) {
-      EXECUTION_NOTIFICATION_GROUP.createNotification(
-        HCLBundle.message("terraform.formatter.error.title"),
-        HCLBundle.message("terraform.formatter.file.extension.error"),
-        NotificationType.ERROR).notify(project)
-
-      return null
-    }
-
     val filePath = virtualFile.canonicalPath ?: return null
     val commandLine = createCommandLine(project, filePath)
     return object : FormattingTask {
@@ -56,14 +45,14 @@ class TfAsyncFormattingService : AsyncDocumentFormattingService() {
           val exitCode = process.waitFor()
           if (exitCode == 0) {
             VfsUtil.markDirtyAndRefresh(true, false, false, virtualFile)
-            formattingRequest.onTextReady(null)
+            request.onTextReady(null)
           } else {
-            formattingRequest.onError(HCLBundle.message("terraform.formatter.error.title"),
+            request.onError(HCLBundle.message("terraform.formatter.error.title"),
                                       HCLBundle.message("terraform.formatter.error.message", virtualFile.name))
           }
         }
         catch (e: Exception) {
-          formattingRequest.onError(HCLBundle.message("terraform.formatter.error.title"),
+          request.onError(HCLBundle.message("terraform.formatter.error.title"),
                                     HCLBundle.message("terraform.formatter.error.message", virtualFile.name))
         }
       }
@@ -82,3 +71,5 @@ class TfAsyncFormattingService : AsyncDocumentFormattingService() {
       .withParameters("fmt", filePath)
       .createCommandLine()
 }
+
+private const val TestsExtension = "tftest.hcl"
