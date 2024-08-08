9cd868e1f482b38262c78eddae6b05651da487b0
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2024-08-08 15:31:14 +0200

    [opentofu] IJPL-157423 Support for OpenTofu in Terraform and HCL Plugin: updated icon
    GitOrigin-RevId: f101336d9ca59a7b48ee0b8a50175a19b4641e87

--- terraform/gen/org/intellij/terraform/TerraformIcons.java
+++ terraform/gen/org/intellij/terraform/TerraformIcons.java
@@ -16,6 +16,6 @@ public final class TerraformIcons {
   private static @NotNull Icon load(@NotNull String expUIPath, @NotNull String path, int cacheKey, int flags) {
     return IconManager.getInstance().loadRasterizedIcon(path, expUIPath, TerraformIcons.class.getClassLoader(), cacheKey, flags);
   }
-  /** 16x16 */ public static final @NotNull Icon Opentofu = load("icons/opentofu.svg", -778658980, 0);
+  /** 16x16 */ public static final @NotNull Icon Opentofu = load("icons/opentofu.svg", -1454201568, 0);
   /** 16x16 */ public static final @NotNull Icon Terraform = load("icons/newui/terraform.svg", "icons/terraform.svg", -828836963, 2);
 }
