f7d3e834240ff3661285cc9d6eb3fc1a4d8b760b
Jan Papesch <jan.papesch@jetbrains.com>
2024-08-28 14:43:53 +0200

    [serial-monitor] CPP-38444 Better handling of CR and newlines in general
    GitOrigin-RevId: 3f833a683c64aa44874e4c15adcd2d58eb83dffe

--- serial-monitor/src/main/java/com/intellij/plugins/serialmonitor/ui/console/CustomJeditermEmulator.kt
+++ serial-monitor/src/main/java/com/intellij/plugins/serialmonitor/ui/console/CustomJeditermEmulator.kt
@@ -11,7 +11,7 @@ class CustomJeditermEmulator(dataStream: TerminalDataStream?, terminal: Terminal
   var newLine: NewLine = NewLine.CRLF
 
   var isTimestamped = false
-  private var onNewLineStart = true
+  private var lastCharCR = false
   private val myFormatter = SimpleDateFormat(TIMESTAMP_FORMAT)
   private fun getTimestamp(): String = myFormatter.format(Date())
 
@@ -19,37 +19,39 @@ class CustomJeditermEmulator(dataStream: TerminalDataStream?, terminal: Terminal
     const val TIMESTAMP_FORMAT = "[HH:mm:ss.SSS] "
   }
 
-  private fun maybeInsertTimestamp(terminal: Terminal) {
-    if (isTimestamped && onNewLineStart) {
-      terminal.writeCharacters(getTimestamp())
-    }
+  private fun Terminal.isOnLineStart(): Boolean = this.cursorX == 1 // cursorX starts at 1
+
+  private fun maybeInsertTimestamp(nextChar: Char, terminal: Terminal) {
+    if (!isTimestamped) return // Insert only when enabled
+    if (!terminal.isOnLineStart()) return // Insert only at the beginning of lines
+    if (lastCharCR && nextChar == '\n' && newLine == NewLine.CRLF) return // Don't insert in the middle of CRLF
+
+    terminal.writeCharacters(getTimestamp())
   }
 
   override fun processChar(ch: Char, terminal: Terminal) {
-
-    maybeInsertTimestamp(terminal)
-
+    maybeInsertTimestamp(ch, terminal)
     when (ch) {
       '\r' -> {
         terminal.carriageReturn()
         if (newLine == NewLine.CR) {
           terminal.newLine()
-          onNewLineStart = true
+        }
+        else if (isTimestamped) {
+          // Move the cursor after the timestamp (and leave the timestamp unchanged)
+          terminal.cursorForward(TIMESTAMP_FORMAT.length)
         }
       }
       '\n' -> {
-        if (newLine == NewLine.LF) {
+        if (newLine == NewLine.LF || isTimestamped) {
+          // move the cursor to the start of the line, CR moves it after the timestamp.
           terminal.carriageReturn()
         }
         terminal.newLine()
-        onNewLineStart = true
       }
-      else -> {
-        super.processChar(ch, terminal)
-        onNewLineStart = false
-      }
-
+      else -> super.processChar(ch, terminal)
     }
+    lastCharCR = ch == '\r'
   }
 
   override fun unsupported(vararg sequenceChars: Char) {
