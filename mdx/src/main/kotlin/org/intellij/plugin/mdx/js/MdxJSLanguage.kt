02620979bfd212675888f8f4349ebf91477225fa
Victor Turansky <victor.turansky@jetbrains.com>
2024-09-19 19:27:14 +0000

    WEB-67476 Remove deprecated `JSLanguageDialect.getFileExtension()`
    Merge-request: IJ-MR-145153
    Merged-by: Victor Turansky <victor.turansky@jetbrains.com>
    
    GitOrigin-RevId: 0c25fcc443133c675e6f3d44a8bce01f03830fcc

--- mdx/src/main/kotlin/org/intellij/plugin/mdx/js/MdxJSLanguage.kt
+++ mdx/src/main/kotlin/org/intellij/plugin/mdx/js/MdxJSLanguage.kt
@@ -5,13 +5,17 @@ import com.intellij.lang.javascript.DialectOptionHolder.JS_WITH_JSX
 import com.intellij.lang.javascript.JSLanguageDialect
 import com.intellij.lang.javascript.JavaScriptSupportLoader.ECMA_SCRIPT_6
 import com.intellij.lang.javascript.parsing.JavaScriptParser
+import com.intellij.openapi.fileTypes.LanguageFileType
+import org.intellij.plugin.mdx.lang.MdxFileType
 
 class MdxJSLanguage private constructor() : JSLanguageDialect("MdxJS", JS_WITH_JSX, ECMA_SCRIPT_6) {
   companion object {
     val INSTANCE = MdxJSLanguage()
   }
 
-  override fun getFileExtension(): String = "mdx"
+  override fun getAssociatedFileType(): LanguageFileType =
+    MdxFileType.INSTANCE
+
   override fun createParser(builder: PsiBuilder): JavaScriptParser<*, *, *, *> {
     return MdxJSLanguageParser(builder)
   }
