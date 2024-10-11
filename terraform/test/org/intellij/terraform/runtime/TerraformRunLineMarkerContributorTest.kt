9bdc8b2bcd52b0c4ad6b7d1b9dcb51842cbda3c4
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-10-11 18:46:34 +0500

    [terraform] IJPL-148557 Removed checking configuration in RunLineMarkerContributor tests
    GitOrigin-RevId: 5cfc124323ac2c875d0730643caef57a2147eec3

--- terraform/test/org/intellij/terraform/runtime/TerraformRunLineMarkerContributorTest.kt
+++ terraform/test/org/intellij/terraform/runtime/TerraformRunLineMarkerContributorTest.kt
@@ -1,11 +1,7 @@
 package org.intellij.terraform.runtime
 
-import com.intellij.execution.actions.ConfigurationContext
-import com.intellij.execution.configurations.RuntimeConfigurationException
 import com.intellij.icons.AllIcons
-import com.intellij.psi.PsiElement
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import com.intellij.ui.IconManager
 import org.intellij.terraform.TerraformTestUtils
 
 class TerraformRunLineMarkerContributorTest : BasePlatformTestCase() {
@@ -16,22 +12,14 @@ class TerraformRunLineMarkerContributorTest : BasePlatformTestCase() {
     val info = file.findElementAt(myFixture.caretOffset)?.let { TerraformRunLineMarkerContributor().getInfo(it) }
 
     assertNotNull(info)
-    assertEquals(IconManager.getInstance().createLayered(AllIcons.RunConfigurations.TestState.Run, AllIcons.Nodes.WarningMark), info?.icon)
-
-    val configuration = getTerraformConfiguration(myFixture.elementAtCaret)
-    assertEquals("plan", configuration.programParameters)
-    assertEquals("/src", configuration.workingDirectory)
-    assertTrue(configuration.envs.isEmpty())
-
-    configuration.workingDirectory = ""
-    assertThrows(RuntimeConfigurationException::class.java) { configuration.checkConfiguration() }
+    assertEquals(AllIcons.RunConfigurations.TestState.Run, info?.icon)
   }
 
   fun testLineMarkerWithComment() {
     val file = myFixture.configureByFile("with_comment.tf")
     val info = file.findElementAt(myFixture.caretOffset)?.let { TerraformRunLineMarkerContributor().getInfo(it) }
     assertNotNull(info)
-    val warnedRun = IconManager.getInstance().createLayered(AllIcons.RunConfigurations.TestState.Run, AllIcons.Nodes.WarningMark)
+    val warnedRun = AllIcons.RunConfigurations.TestState.Run
     assertEquals(warnedRun, info?.icon)
 
     val gutter = myFixture.findGutter("with_comment.tf")
@@ -45,11 +33,5 @@ class TerraformRunLineMarkerContributorTest : BasePlatformTestCase() {
 
     val gutters = myFixture.findAllGutters("whole_commented.tf")
     assertEmpty(gutters)
-  }
-
-  private fun getTerraformConfiguration(psiElement: PsiElement): TerraformRunConfiguration {
-    val configuration = ConfigurationContext(psiElement).configuration?.configuration
-    assertInstanceOf(configuration, TerraformRunConfiguration::class.java)
-    return configuration as TerraformRunConfiguration
   }
 }
