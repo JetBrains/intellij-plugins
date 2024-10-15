a7d431c0e283884de9088a93a38a7bfb080444d4
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-10-15 12:27:03 +0500

    [terraform] IJPL-148557 Keep displaying an uninitialized terraform gutter with a warning
    (cherry picked from commit 7ab65e1d6c6f1c2ca5a1231673f1626296044385)
    
    
    (cherry picked from commit 548584b2308924466574ebe0e16814f343218716)
    
    IJ-CR-148052
    
    GitOrigin-RevId: 953520597dfb3a443f7ad97111059c2adece4ad1

--- terraform/test/org/intellij/terraform/runtime/TfRunLineMarkerContributorTest.kt
+++ terraform/test/org/intellij/terraform/runtime/TfRunLineMarkerContributorTest.kt
@@ -2,24 +2,26 @@ package org.intellij.terraform.runtime
 
 import com.intellij.icons.AllIcons
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.ui.IconManager
 import org.intellij.terraform.TerraformTestUtils
 
-class TerraformRunLineMarkerContributorTest : BasePlatformTestCase() {
+class TfRunLineMarkerContributorTest : BasePlatformTestCase() {
   override fun getTestDataPath(): String = TerraformTestUtils.getTestDataPath() + "/runtime"
 
   fun testSimpleLineMarker() {
     val file = myFixture.configureByFile("simple.tf")
-    val info = file.findElementAt(myFixture.caretOffset)?.let { TerraformRunLineMarkerContributor().getInfo(it) }
+    val info = file.findElementAt(myFixture.caretOffset)?.let { TfRunLineMarkerContributor().getInfo(it) }
 
     assertNotNull(info)
-    assertEquals(AllIcons.RunConfigurations.TestState.Run, info?.icon)
+    val warnedRun = IconManager.getInstance().createLayered(AllIcons.RunConfigurations.TestState.Run, AllIcons.Nodes.WarningMark)
+    assertEquals(warnedRun, info?.icon)
   }
 
   fun testLineMarkerWithComment() {
     val file = myFixture.configureByFile("with_comment.tf")
-    val info = file.findElementAt(myFixture.caretOffset)?.let { TerraformRunLineMarkerContributor().getInfo(it) }
+    val info = file.findElementAt(myFixture.caretOffset)?.let { TfRunLineMarkerContributor().getInfo(it) }
     assertNotNull(info)
-    val warnedRun = AllIcons.RunConfigurations.TestState.Run
+    val warnedRun = IconManager.getInstance().createLayered(AllIcons.RunConfigurations.TestState.Run, AllIcons.Nodes.WarningMark)
     assertEquals(warnedRun, info?.icon)
 
     val gutter = myFixture.findGutter("with_comment.tf")
