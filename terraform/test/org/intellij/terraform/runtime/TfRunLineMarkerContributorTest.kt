e3e5de32d494b2408b62bc33da60e4422e9a7abd
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2024-10-21 19:04:18 +0500

    [terraform] IJ-CR-146601 Added tests to check run config action in TfRunLineMarkerContributor
    (cherry picked from commit 6c373cbc2b0de8ef2951d7ef9a840c26d829acbb)
    
    
    (cherry picked from commit 59eedc76b906c345575cf4f47d8b0ff3100549c0)
    
    IJ-CR-148052
    
    GitOrigin-RevId: 2b679f5d46d4a1033d95e564950fa9f7c9d590f0

--- terraform/test/org/intellij/terraform/runtime/TfRunLineMarkerContributorTest.kt
+++ terraform/test/org/intellij/terraform/runtime/TfRunLineMarkerContributorTest.kt
@@ -1,5 +1,8 @@
 package org.intellij.terraform.runtime
 
+import com.intellij.execution.RunManager
+import com.intellij.execution.RunnerAndConfigurationSettings
+import com.intellij.execution.lineMarker.RunLineMarkerContributor.Info
 import com.intellij.icons.AllIcons
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.ui.IconManager
@@ -11,18 +14,28 @@ class TfRunLineMarkerContributorTest : BasePlatformTestCase() {
   fun testSimpleLineMarker() {
     val file = myFixture.configureByFile("simple.tf")
     val info = file.findElementAt(myFixture.caretOffset)?.let { TfRunLineMarkerContributor().getInfo(it) }
+    if (info == null) {
+      fail("Info of RunLineMarker not should be empty")
+      return
+    }
 
-    assertNotNull(info)
     val warnedRun = IconManager.getInstance().createLayered(AllIcons.RunConfigurations.TestState.Run, AllIcons.Nodes.WarningMark)
-    assertEquals(warnedRun, info?.icon)
+    assertEquals(warnedRun, info.icon)
+    checkActionNames(info)
   }
 
   fun testLineMarkerWithComment() {
     val file = myFixture.configureByFile("with_comment.tf")
     val info = file.findElementAt(myFixture.caretOffset)?.let { TfRunLineMarkerContributor().getInfo(it) }
-    assertNotNull(info)
+    if (info == null) {
+      fail("Info of RunLineMarker not should be empty")
+      return
+    }
+
     val warnedRun = IconManager.getInstance().createLayered(AllIcons.RunConfigurations.TestState.Run, AllIcons.Nodes.WarningMark)
-    assertEquals(warnedRun, info?.icon)
+    assertEquals(warnedRun, info.icon)
+    checkActionNames(info)
+    testRunConfigActions(info)
 
     val gutter = myFixture.findGutter("with_comment.tf")
     assertNotNull(gutter)
@@ -36,4 +49,45 @@ class TfRunLineMarkerContributorTest : BasePlatformTestCase() {
     val gutters = myFixture.findAllGutters("whole_commented.tf")
     assertEmpty(gutters)
   }
+
+  private fun checkActionNames(info: Info) {
+    val actions = info.actions
+    assertEquals(lineMarkerActionsSize, actions?.size)
+
+    val actionNames = actions?.take(5)?.map { it.templateText }
+    assertEquals(runTemplateConfigActionsName, actionNames)
+
+    assertEquals(actions?.last()?.templateText, "Edit Configurations…")
+  }
+
+  private fun testRunConfigActions(info: Info) {
+    val actions = info.actions
+    val runManager = RunManager.getInstance(project)
+    assertEmpty(runManager.allSettings)
+
+    val initAction = myFixture.testAction(actions.first())
+    assertNull(initAction.icon)
+    assertEquals(initAction.text, "Init src")
+    testTfConfiguration(runManager.allSettings.first(), TfMainCommand.INIT)
+    assertEquals(1, runManager.allSettings.size)
+
+    val applyAction = myFixture.testAction(actions[3])
+    assertNull(applyAction.icon)
+    assertEquals(applyAction.text, "Apply src")
+    testTfConfiguration(runManager.allSettings.first(), TfMainCommand.APPLY)
+    assertEquals(2, runManager.allSettings.size)
+  }
+
+  private fun testTfConfiguration(settings: RunnerAndConfigurationSettings, mainCommand: TfMainCommand) {
+    val configuration = settings.configuration
+    assertInstanceOf(configuration, TerraformRunConfiguration::class.java)
+    configuration as TerraformRunConfiguration
+
+    assertEquals(mainCommand.command, configuration.programParameters)
+    assertEquals("/src", configuration.workingDirectory)
+    assertTrue(configuration.envs.isEmpty())
+  }
 }
+
+private const val lineMarkerActionsSize = 7
+private val runTemplateConfigActionsName = listOf("Init src", "Validate src", "Plan src", "Apply src", "Destroy src")
