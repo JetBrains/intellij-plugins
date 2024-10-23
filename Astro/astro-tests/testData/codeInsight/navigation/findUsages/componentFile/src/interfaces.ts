db164ab4d49f59993b62c6e437d6ada5f8a8d87a
Vladimir Panimaskin <Vladimir.Panimaskin@jetbrains.com>
2024-10-24 01:37:29 +0300

    [js] fix missing component usages not fully displayed WEB-69862
    GitOrigin-RevId: 701483762a6da3bb61214536c0151116e3053b16

---
+++ Astro/astro-tests/testData/codeInsight/navigation/findUsages/componentFile/src/interfaces.ts
@@ -0,0 +1,46 @@
+export interface IStory {
+    id: string;
+    title: string;
+    user: string;
+}
+
+export function greet(){
+    return "Hello World!";
+}
+
+export function hello() {
+    // return
+}
+
+/**
+ * test test etst
+ * @example
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * 1
+ * find me
+ */
+type T = string;
