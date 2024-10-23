337a2e95b8cc4539174733f23caab1994755fd64
Vladimir Panimaskin <Vladimir.Panimaskin@jetbrains.com>
2024-10-24 01:37:29 +0300

    [js] fix missing component usages not fully displayed WEB-69862
    (cherry picked from commit 701483762a6da3bb61214536c0151116e3053b16)
    
    IJ-CR-147649
    
    GitOrigin-RevId: ba0502a2e253eb4b770d89ead5104ce1d6cf2e4d

--- Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroFindUsagesTest.kt
+++ Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroFindUsagesTest.kt
@@ -4,4 +4,6 @@ import org.jetbrains.astro.AstroCodeInsightTestCase
 
 class AstroFindUsagesTest : AstroCodeInsightTestCase("codeInsight/navigation/findUsages") {
   fun testAstroComponentProp() = checkUsages()
+
+  fun testComponentFile() = checkFileUsages(fileName = "src/components/temp/ManyImportsUpd.astro")
 }
