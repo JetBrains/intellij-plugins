b1460c7c971b7022feb3563a170c07a1d75a9881
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-11-01 15:15:15 -0500

    WEB-69755: Implement Nuxt Project Template using Nuxi
    GitOrigin-RevId: 60a735131cacf088041754bbafa46b5703d93cc1

---
+++ vuejs/src/org/jetbrains/vuejs/cli/NuxtProjectGenerator.kt
@@ -0,0 +1,123 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.cli
+
+import com.intellij.execution.filters.Filter
+import com.intellij.javascript.CreateRunConfigurationUtil
+import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreterUtil
+import com.intellij.javascript.nodejs.npm.InstallNodeLocalDependenciesAction
+import com.intellij.javascript.nodejs.packages.NodePackageUtil
+import com.intellij.javascript.nodejs.util.NodePackage
+import com.intellij.lang.javascript.boilerplate.NpmPackageProjectGenerator
+import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
+import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.util.PathUtil
+import org.jetbrains.vuejs.VueBundle
+import org.jetbrains.vuejs.VuejsIcons
+import java.io.File
+import javax.swing.Icon
+import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.SmartPsiElementPointer
+import com.intellij.psi.createSmartPointer
+
+
+class NuxtProjectGenerator : NpmPackageProjectGenerator() {
+
+  private val NUXT_CLI_PACKAGE_NAME = "nuxi@latest"
+  private val CREATE_COMMAND = "init"
+
+  override fun getId(): String {
+    return VueBundle.message("vue.project.generator.nuxt.name")
+  }
+
+  override fun getName(): String {
+    return VueBundle.message("vue.project.generator.nuxt.name")
+  }
+
+  override fun getDescription(): String {
+    return VueBundle.message("vue.project.generator.nuxt.description")
+  }
+
+  override fun getIcon(): Icon {
+    return VuejsIcons.Nuxt
+  }
+
+
+  override fun customizeModule(baseDir: VirtualFile, entry: ContentEntry) {}
+
+  override fun generatorArgs(project: Project?, dir: VirtualFile, settings: Settings): Array<String> {
+    val workingDir = dir.name
+    return arrayOf(CREATE_COMMAND, workingDir, "--no-install")
+  }
+
+  override fun generatorArgs(project: Project, baseDir: VirtualFile): Array<String> {
+    return emptyArray()
+  }
+
+  override fun filters(project: Project, baseDir: VirtualFile): Array<Filter> {
+    return emptyArray()
+  }
+
+  override fun executable(pkg: NodePackage): String {
+    return pkg.systemDependentPath + File.separator + "bin" + File.separator + "vue.js"
+  }
+
+  override fun packageName(): String {
+    return NUXT_CLI_PACKAGE_NAME
+  }
+
+  override fun presentablePackageName(): String {
+    return VueBundle.message("vue.project.generator.nuxt.presentable.package.name")
+  }
+
+  override fun getNpxCommands(): List<NpxPackageDescriptor.NpxCommand> {
+    return listOf(
+      NpxPackageDescriptor.NpxCommand(NUXT_CLI_PACKAGE_NAME, NUXT_CLI_PACKAGE_NAME)
+    )
+  }
+
+  override fun validateProjectPath(path: String): String? {
+    val error = NodePackageUtil.validateNpmPackageName(PathUtil.getFileName(path))
+    return error ?: super.validateProjectPath(path)
+  }
+
+  override fun onGettingSmartAfterProjectGeneration(project: Project, baseDir: VirtualFile) {
+    super.onGettingSmartAfterProjectGeneration(project, baseDir)
+    CreateRunConfigurationUtil.debugConfiguration(project, 3000)
+    CreateRunConfigurationUtil.npmConfiguration(project, "dev")
+  }
+
+  override fun generateInTemp(): Boolean = true
+
+  // trigger npm install
+  override fun postInstall(project: Project,
+                           baseDir: VirtualFile,
+                           workingDir: File): Runnable {
+    return Runnable {
+      ApplicationManager.getApplication().executeOnPooledThread {
+        val packageJson = findFileByNames(project, baseDir, arrayOf(PackageJsonUtil.FILE_NAME))
+        if (packageJson != null && NodeJsLocalInterpreterUtil.detectAllLocalInterpreters().isNotEmpty()) {
+          InstallNodeLocalDependenciesAction.runAndShowConsole(project, packageJson.virtualFile)
+        }
+        super.postInstall(project, baseDir, workingDir).run()
+      }
+    }
+  }
+
+
+  private fun findFileByNames(project: Project, baseDir: VirtualFile, names: Array<String>): SmartPsiElementPointer<PsiFile>? {
+    var result: SmartPsiElementPointer<PsiFile>? = null
+    VfsUtilCore.processFilesRecursively(baseDir) { file ->
+      if (names.any { file.name.startsWith(it) }) {
+        result = PsiManager.getInstance(project).findFile(file)?.createSmartPointer()
+      }
+      true
+    }
+    return result
+  }
+}
