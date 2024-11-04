add2e4ebf8eec3ae2931f0c59e0ca7a400e78b4b
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2024-11-04 11:15:48 +0100

    [vue] WEB-69667 fix VolarServiceTest
    GitOrigin-RevId: 18423a243a7f9df30802ff2aaec4642d1092aad6

--- vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/service/VueLspServiceTestBase.kt
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/service/VueLspServiceTestBase.kt
@@ -1,11 +1,10 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.service
 
-import com.intellij.lang.javascript.service.BaseLspTypeScriptServiceTest
 import com.intellij.lang.javascript.library.typings.TypeScriptExternalDefinitionsRegistry
+import com.intellij.lang.javascript.service.BaseLspTypeScriptServiceTest
 import com.intellij.lang.typescript.compiler.languageService.TypeScriptLanguageServiceUtil
 import com.intellij.lang.typescript.library.download.TypeScriptDefinitionFilesDirectory
-import com.intellij.openapi.util.Disposer
 import com.intellij.util.text.SemVer
 import org.jetbrains.vuejs.lang.typescript.service.VueLspServerLoader
 import org.jetbrains.vuejs.lang.typescript.service.lsp.VueLspTypeScriptService
@@ -26,17 +25,23 @@ abstract class VueLspServiceTestBase : BaseLspTypeScriptServiceTest() {
   override fun setUp() {
     super.setUp()
     val vueSettings = getVueSettings(project)
-    val old = vueSettings.serviceType
     TypeScriptLanguageServiceUtil.setUseService(true)
     TypeScriptExternalDefinitionsRegistry.testTypingsRootPath = TypeScriptDefinitionFilesDirectory.getGlobalAutoDownloadTypesDirectoryPath()
-
-    Disposer.register(testRootDisposable) {
-      vueSettings.serviceType = old
-      TypeScriptLanguageServiceUtil.setUseService(false)
-    }
     vueSettings.serviceType = VueServiceSettings.VOLAR
 
     ensureServerDownloaded(VueLspServerLoader)
+  }
+
+  override fun tearDown() {
+    try {
+      TypeScriptLanguageServiceUtil.setUseService(false)
+    }
+    catch (e: Throwable) {
+      addSuppressedException(e)
+    }
+    finally {
+      super.tearDown()
+    }
   }
 
   protected fun assertCorrectService(version: SemVer? = null) {
