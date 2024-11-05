2b09d87f9de7e1e00af4f515f03461346a5066eb
Dima Golovinov <dmitry.golovinov@jetbrains.com>
2024-11-05 09:29:34 +0100

    [qodana] Get rid of GlobalTaintOutputConsumer. Fix inspection name in Local one
    (cherry picked from commit a968730519bf6b9f5094196910eaf4578e4c6cfc)
    
    IJ-MR-148600
    
    GitOrigin-RevId: 499689b9979d2dd4b6f290913d9c56038c1193cc

--- qodana/jvm/java/src/org/jetbrains/qodana/jvm/java/JvmGlobalTaintOutputConsumer.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.qodana.jvm.java
-
-import org.jetbrains.qodana.staticAnalysis.inspections.runner.globalOutput.GlobalFlowOutputConsumer
-
-class JvmGlobalTaintOutputConsumer: GlobalFlowOutputConsumer() {
-  override fun getInspectionName() = "JvmTaintAnalysis.Global"
-}
