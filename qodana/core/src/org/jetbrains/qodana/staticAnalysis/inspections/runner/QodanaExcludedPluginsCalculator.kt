9dde47fec2c7e168829d8a1ea8840c0f456bf779
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2024-11-07 11:12:42 +0100

    QD-10231 Pay respect to <dependencies> section in plugins for Qodana required plugins calculation
    GitOrigin-RevId: 22aa7f68349fc429fce49b551d39f9ecbfb530cb

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/runner/QodanaExcludedPluginsCalculator.kt
+++ qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/runner/QodanaExcludedPluginsCalculator.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.qodana.staticAnalysis.inspections.runner
 
 import com.intellij.ide.plugins.IdeaPluginDescriptor
+import com.intellij.ide.plugins.IdeaPluginDescriptorImpl
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.application.ApplicationStarter
 import com.intellij.openapi.application.ex.ApplicationManagerEx
@@ -126,9 +127,13 @@ class QodanaExcludedPluginsCalculator : ApplicationStarter {
     processed.add(idString)
     include.add(descriptor)
 
+    val required = if (descriptor is IdeaPluginDescriptorImpl) {
+      descriptor.dependencies.plugins.mapNotNull { reference -> if (processed.contains(reference.id.idString)) null else reference.id.idString}
+    } else emptyList()
+
     return descriptor.dependencies.mapNotNull {
       if (processed.contains(it.pluginId.idString) || it.isOptional) null else it.pluginId.idString
-    }
+    } + required
   }
 }
 
@@ -136,5 +141,4 @@ fun getKeptButDisabledPlugins(): List<String> {
   if (PlatformUtils.isRider()) return listOf("intellij.rider.plugins.cwm")
   if (PlatformUtils.isCommunityEdition()) return listOf()
   return listOf("com.jetbrains.codeWithMe")
-
 }
