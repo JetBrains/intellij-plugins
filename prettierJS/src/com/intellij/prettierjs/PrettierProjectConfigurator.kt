db127027bcc1b1c943939364f661cb1fe9849e11
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2024-11-11 12:09:43 +0200

    [Prettier] WEB-67396: use service instead of getService
    GitOrigin-RevId: 005dee30bd8ca210d0fdb5d406431fcd900cda24

--- prettierJS/src/com/intellij/prettierjs/PrettierProjectConfigurator.kt
+++ prettierJS/src/com/intellij/prettierjs/PrettierProjectConfigurator.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.prettierjs
 
+import com.intellij.openapi.components.service
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Ref
@@ -9,7 +10,7 @@ import com.intellij.platform.DirectoryProjectConfigurator
 
 class PrettierProjectConfigurator : DirectoryProjectConfigurator.AsyncDirectoryProjectConfigurator() {
   override suspend fun configure(project: Project, baseDir: VirtualFile, moduleRef: Ref<Module>, isProjectCreatedWithWizard: Boolean) {
-    project.getService(PrettierConfiguratorService::class.java).configure(baseDir)
+    project.service<PrettierConfiguratorService>().configure(baseDir)
   }
 }
 
