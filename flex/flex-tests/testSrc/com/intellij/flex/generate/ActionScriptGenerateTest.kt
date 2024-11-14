b8d12ae1996bec9856e252ca8b5c575dab956f90
Victor Turansky <victor.turansky@jetbrains.com>
2024-11-14 17:54:54 +0200

    WEB-67135 Migrate `ActionScriptGenerateTest` on JUnit 4
    GitOrigin-RevId: 122b808b009ff3449d2361248cc63a6aed002869

--- flex/flex-tests/testSrc/com/intellij/flex/generate/ActionScriptGenerateTest.kt
+++ flex/flex-tests/testSrc/com/intellij/flex/generate/ActionScriptGenerateTest.kt
@@ -7,7 +7,11 @@ import com.intellij.flex.util.FlexTestUtils
 import com.intellij.lang.javascript.generate.JSGenerateTestBase
 import com.intellij.testFramework.LightProjectDescriptor
 import org.jetbrains.annotations.NonNls
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class ActionScriptGenerateTest : JSGenerateTestBase() {
   companion object {
     const val BASE_PATH: @NonNls String = "/js2_highlighting"
@@ -34,38 +38,47 @@ class ActionScriptGenerateTest : JSGenerateTestBase() {
     return "js2"
   }
 
+  @Test
   fun testGenerateGetter() {
     doGenerateTest("Generate.GetAccessor.JavaScript")
   }
 
+  @Test
   fun testGenerateSetter() {
     doGenerateTest("Generate.SetAccessor.JavaScript")
   }
 
+
+  @Test
   @FlexTestOptions(FlexTestOption.WithFlexFacet)
   fun testGenerateGetterAndSetter() {
     FlexTestUtils.setupFlexSdk(getModule(), getTestName(false), javaClass, myFixture.testRootDisposable)
     doGenerateTest("Generate.GetSetAccessor.JavaScript")
   }
 
+  @Test
   fun testGenerateConstructor() {
     doGenerateTest("Generate.Constructor.JavaScript")
   }
 
+  @Test
   fun testGenerateConstructor2() {
     doGenerateTest("Generate.Constructor.JavaScript")
   }
 
+  @Test
   fun testGenerateConstructor3() {
     doGenerateTest("Generate.Constructor.JavaScript")
   }
 
+  @Test
   fun testGenerateToString() {
     doGenerateTest("Generate.ToString.Actionscript", "", "js2")
     doGenerateTest("Generate.ToString.Actionscript", "_2", "js2")
     doGenerateTest("Generate.ToString.Actionscript", "_3", "as")
   }
 
+  @Test
   fun testGenerateToString2() {
     doGenerateTest("Generate.ToString.Actionscript", "", "js2")
     doGenerateTest("Generate.ToString.Actionscript", "_2", "js2")
