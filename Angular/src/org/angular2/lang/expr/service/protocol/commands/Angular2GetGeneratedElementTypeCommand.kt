f90d5955146c2efebd9329f85a61981f3d9e8eeb
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2024-11-25 11:39:01 +0100

    WEB-70478 TS: Refactor APIs around custom TS commands - simplify defining new commands on the IDE side.
    GitOrigin-RevId: 8f36a27406e6a404648be38f5adfde5e4331d1ed

--- Angular/src/org/angular2/lang/expr/service/protocol/commands/Angular2GetGeneratedElementTypeCommand.kt
+++ Angular/src/org/angular2/lang/expr/service/protocol/commands/Angular2GetGeneratedElementTypeCommand.kt
@@ -1,7 +1,23 @@
 package org.angular2.lang.expr.service.protocol.commands
 
-import com.intellij.lang.javascript.service.protocol.JSLanguageServiceObject
-import com.intellij.lang.typescript.compiler.languageService.protocol.commands.TypeScriptCommandWithArguments
+import com.google.gson.JsonObject
+import com.intellij.lang.javascript.psi.resolve.JSEvaluationStatisticsCollector
+import com.intellij.lang.typescript.compiler.TypeScriptServiceQueueCommand
+import com.intellij.lang.typescript.compiler.TypeScriptServiceRequest
+import com.intellij.lang.typescript.compiler.languageService.TypeScriptServerServiceImpl
+import com.intellij.openapi.components.service
+import com.intellij.util.application
 
 class Angular2GetGeneratedElementTypeCommand(args: Angular2GetGeneratedElementTypeRequestArgs)
-  : JSLanguageServiceObject, TypeScriptCommandWithArguments<Angular2GetGeneratedElementTypeRequestArgs>("ngGetGeneratedElementType", args)
+  : TypeScriptServiceQueueCommand<Angular2GetGeneratedElementTypeRequestArgs, JsonObject>("ngGetGeneratedElementType", args) {
+
+  override fun processResult(answer: JsonObject): JsonObject? =
+    answer.getAsJsonObject(TypeScriptServerServiceImpl.BODY_FIELD)
+
+  override fun onServiceTaskCompleted(task: TypeScriptServiceRequest<Angular2GetGeneratedElementTypeRequestArgs, JsonObject>) {
+    if (JSEvaluationStatisticsCollector.State.isEnabled()) {
+      application.service<JSEvaluationStatisticsCollector>().responseReady(!task.wasExecuted)
+    }
+  }
+
+}
