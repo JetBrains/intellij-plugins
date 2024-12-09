a4479ed54bbd4569bc9d0bbe74e19c6eb33fa0da
Ilya Korennoy <ilya.korennoy@jetbrains.com>
2024-12-09 16:44:33 +0100

    [workspace model] IJPL-149669 Add FlexCompositeSdkRootProvider as a workaround for FlexCompositeSdk
    GitOrigin-RevId: 92441070dac0b3c94aeaa395610cdec57c011455

---
+++ flex/src/com/intellij/javascript/flex/FlexCompositeSdkRootProvider.kt
@@ -0,0 +1,28 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package com.intellij.javascript.flex
+
+import com.intellij.lang.javascript.flex.projectStructure.FlexCompositeSdk
+import com.intellij.openapi.application.ReadAction
+import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.*
+import org.jetbrains.annotations.Unmodifiable
+
+class FlexCompositeSdkRootProvider : AdditionalLibraryRootsProvider() {
+
+  override fun getAdditionalProjectLibraries(project: Project): @Unmodifiable Collection<SyntheticLibrary> {
+    val result = mutableListOf<SyntheticLibrary>()
+    for (module in ModuleManager.getInstance(project).modules) {
+      val model = ReadAction
+        .compute<ModifiableRootModel, RuntimeException> { ModuleRootManager.getInstance(module).getModifiableModel() }
+      val sdk = model.sdk
+      if (sdk != null && sdk is FlexCompositeSdk) {
+        val rootProvider = sdk.rootProvider
+        val sourceRoots = rootProvider.getFiles(OrderRootType.SOURCES).toList()
+        val binaryRoots = rootProvider.getFiles(OrderRootType.CLASSES).toList()
+        result.add(SyntheticLibrary.newImmutableLibrary(sdk.name, sourceRoots, binaryRoots, emptySet(), null))
+      }
+    }
+    return result
+  }
+}
