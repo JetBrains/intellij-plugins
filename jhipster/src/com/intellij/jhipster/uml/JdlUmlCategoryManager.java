1bc081b33d5147cd72de785a2f29297513bbf229
Tagir Valeev <tagir.valeev@jetbrains.com>
2024-12-17 14:34:24 +0100

    [jhipster] i18n
    GitOrigin-RevId: 05a90f4e7bf8bf06dda1bed67352ef12fc759717

--- jhipster/src/com/intellij/jhipster/uml/JdlUmlCategoryManager.java
+++ jhipster/src/com/intellij/jhipster/uml/JdlUmlCategoryManager.java
@@ -6,14 +6,15 @@ import com.intellij.diagram.AbstractDiagramNodeContentManager;
 import com.intellij.diagram.DiagramBuilder;
 import com.intellij.diagram.DiagramCategory;
 import com.intellij.icons.AllIcons;
+import com.intellij.jhipster.JdlBundle;
 import com.intellij.jhipster.uml.model.JdlEntityNodeField;
 import com.intellij.jhipster.uml.model.JdlEnumNodeItem;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 final class JdlUmlCategoryManager extends AbstractDiagramNodeContentManager {
-  private static final @NotNull DiagramCategory FIELDS = new DiagramCategory(() -> "Fields", AllIcons.Nodes.Field, true, false);
-  private static final @NotNull DiagramCategory ENUM_VALUES = new DiagramCategory(() -> "Enum Values", AllIcons.Nodes.Enum, true, false);
+  private static final @NotNull DiagramCategory FIELDS = new DiagramCategory(() -> JdlBundle.message("fields"), AllIcons.Nodes.Field, true, false);
+  private static final @NotNull DiagramCategory ENUM_VALUES = new DiagramCategory(() -> JdlBundle.message("enum.values"), AllIcons.Nodes.Enum, true, false);
 
   private static final DiagramCategory[] CATEGORIES = new DiagramCategory[]{ENUM_VALUES, FIELDS};
 
