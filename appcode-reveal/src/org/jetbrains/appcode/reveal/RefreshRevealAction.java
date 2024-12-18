c54a19d8f39c7c73a4cab00cda119dd80004f198
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2024-12-18 19:24:15 +0100

    do not export deps blindly (part 3)
    GitOrigin-RevId: 7277ece9ded25f1111febc8dccaba9ac8cc75c21

--- appcode-reveal/src/org/jetbrains/appcode/reveal/RefreshRevealAction.java
+++ appcode-reveal/src/org/jetbrains/appcode/reveal/RefreshRevealAction.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.appcode.reveal;
 
 import com.intellij.execution.ExecutionException;
@@ -26,7 +26,7 @@ import org.jetbrains.annotations.Nullable;
 import javax.swing.*;
 import java.io.File;
 
-public class RefreshRevealAction extends DumbAwareAction {
+public final class RefreshRevealAction extends DumbAwareAction {
   public static final Icon ICON = AppcodeRevealIcons.RunWithReveal;
 
   @NotNull private final AppCodeRunConfiguration myConfiguration;
