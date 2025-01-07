497a33b15eb1a7937acfd5c6d53e9f9d30cde98f
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-01-07 18:17:06 +0100

    ability to provide own impl of ModuleBuildTarget, create JavaSourceRootDescriptor by NIO root
    GitOrigin-RevId: 5215d33b9b0a93057953be8af50e1c186a16a515

--- flex/flex-shared/src/com/intellij/flex/build/FlexSourceRootDescriptor.java
+++ flex/flex-shared/src/com/intellij/flex/build/FlexSourceRootDescriptor.java
@@ -1,3 +1,4 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.flex.build;
 
 import org.jetbrains.jps.builders.BuildTarget;
@@ -5,8 +6,8 @@ import org.jetbrains.jps.builders.impl.BuildRootDescriptorImpl;
 
 import java.io.File;
 
-class FlexSourceRootDescriptor extends BuildRootDescriptorImpl {
-  FlexSourceRootDescriptor(final BuildTarget target, final File root) {
+final class FlexSourceRootDescriptor extends BuildRootDescriptorImpl {
+  FlexSourceRootDescriptor(BuildTarget<?> target, File root) {
     super(target, root);
   }
 }
