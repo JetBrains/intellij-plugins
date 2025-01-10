bf7cdf19ffdb2b3b7f25a439f2422dcabcc35a40
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2025-01-10 19:29:56 +0100

    [kotlin] K2: adding sources for .kts and .main.kts
    ^KTIJ-32696
    
    GitOrigin-RevId: 502ff89eb90f0ed7ce7835be98b0b921971cb319

--- qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/k2.kt
+++ qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/k2.kt
@@ -1,7 +1,12 @@
 package org.jetbrains.qodana.inspectionKts.kotlin.script
 
 import com.intellij.openapi.project.Project
+import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.platform.workspace.storage.MutableEntityStorage
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 import kotlinx.coroutines.CoroutineScope
+import org.jetbrains.kotlin.idea.core.script.KotlinScriptEntitySource
+import org.jetbrains.kotlin.idea.core.script.getUpdatedStorage
 import org.jetbrains.kotlin.idea.core.script.k2.BundledScriptConfigurationsSource
 import org.jetbrains.kotlin.idea.core.script.scriptDefinitionsSourceOfType
 import org.jetbrains.kotlin.scripting.definitions.ScriptDefinition
@@ -15,7 +20,22 @@ internal class QodanaScriptConfigurationSource(
   override fun getScriptDefinitionsSource(): ScriptDefinitionsSource? {
     return project.scriptDefinitionsSourceOfType<QodanaScriptDefinitionSource>()
   }
+
+  override suspend fun updateModules(storage: MutableEntityStorage?) {
+    val updatedStorage = getUpdatedStorage(
+      project, data.get()
+    ) { QodanaKotlinScriptEntitySource(it) }
+
+    project.workspaceModel.update("updating .inspection.kts modules") {
+      it.replaceBySource(
+        { source -> source is QodanaKotlinScriptEntitySource }, updatedStorage
+      )
+    }
+  }
 }
+
+open class QodanaKotlinScriptEntitySource(override val virtualFileUrl: VirtualFileUrl?) :
+  KotlinScriptEntitySource(virtualFileUrl)
 
 internal class QodanaScriptDefinitionSource : ScriptDefinitionsSource {
   override val definitions: Sequence<ScriptDefinition>
