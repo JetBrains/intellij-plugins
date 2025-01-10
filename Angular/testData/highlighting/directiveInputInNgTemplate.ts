4f8362a218e4e90c8ec111855bb79b557b5f2f68
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-01-10 15:44:06 +0100

    Angular: improve test
    GitOrigin-RevId: c8e64be069ebd9771317b0a4cf9e91348248bc55

--- Angular/testData/highlighting/directiveInputInNgTemplate.ts
+++ Angular/testData/highlighting/directiveInputInNgTemplate.ts
@@ -22,6 +22,7 @@ class TestDirective {
     <ng-template 
      [test]="'hello world'"
      (close)="$event" 
+     <error descr="Event click is not emitted by any applicable directive on an embedded template">(click)</error>="$event" 
      <error descr="Property foo is not provided by any applicable directive on an embedded template">[foo]</error>="12"
     >
       Hello
