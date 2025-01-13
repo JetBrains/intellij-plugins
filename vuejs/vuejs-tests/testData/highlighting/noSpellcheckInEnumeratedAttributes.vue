076a9f188c098d971895abae7ba10d3ddd6a8add
Peter Gromov <peter@jetbrains.com>
2025-01-13 08:30:41 +0100

    IJPL-164421 Update spellchecker dictionaries
    GitOrigin-RevId: 68f147eb5e8aa57849d482cecdc7e5a7f603eef5

--- vuejs/vuejs-tests/testData/highlighting/noSpellcheckInEnumeratedAttributes.vue
+++ vuejs/vuejs-tests/testData/highlighting/noSpellcheckInEnumeratedAttributes.vue
@@ -1,4 +1,4 @@
 <template>
   <a rel="noopener" ></a>
-  <div title="<TYPO descr="Typo: In word 'foof'">foof</TYPO>"></div>
+  <div title="<TYPO descr="Typo: In word 'fooff'">fooff</TYPO>"></div>
 </template>
