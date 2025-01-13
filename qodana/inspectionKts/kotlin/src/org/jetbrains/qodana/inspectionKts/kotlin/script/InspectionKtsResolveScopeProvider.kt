4c21b599e1c9684ef71d201d687cac7cb17913c9
Mikhail Shagvaliev <mikhail.shagvaliev@jetbrains.com>
2025-01-13 12:18:49 +0100

    [qodana] QD-9518 Don't do flexinspect resolve when it's not cancellable
    - debugger uses resolve API on EDT
    
    GitOrigin-RevId: 6cedd5ec447e50bca5daac1da02835a81919b051

--- qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/InspectionKtsResolveScopeProvider.kt
+++ qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/InspectionKtsResolveScopeProvider.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.qodana.inspectionKts.kotlin.script
 
+import com.intellij.openapi.progress.isInCancellableContext
 import com.intellij.openapi.progress.runBlockingCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -10,6 +11,12 @@ import org.jetbrains.qodana.registry.QodanaRegistry
 // QD-8717 FlexInspect (.inspection.kts): unresolved Kotlin PSI APIs if the Kotlin is present in the gradle kts project
 internal class InspectionKtsResolveScopeProvider : ResolveScopeProvider() {
   override fun getResolveScope(file: VirtualFile, project: Project): GlobalSearchScope? {
+    // QD-9518 Debugger calls getResolveScope on EDT.
+    // We don't want to do non-cancellable runBlocking because it can lead to deadlock,
+    // so instead we just don't compute anything when there is no indicator (when the call is not cancellable).
+    // Regular resolve is cancellable, so it's fine
+    if (!isInCancellableContext()) return null
+
     val inspectionKtsDependenciesService = InspectionKtsClasspathService.getInstanceIfCreated() ?: return null
     if (!QodanaRegistry.limitedInspectionKtsDependencies) return null
 
