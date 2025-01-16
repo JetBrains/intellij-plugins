3195c587135395151ea9a114bed1192d83d9b435
Arsenii Iusufov <arsenii.iusufov@jetbrains.com>
2025-01-16 13:01:51 +0000

    WEB-69755 Bring nuxt project generator to work
    - Generate project in place (not temp folder)
    - Hardcode `npm` as package manager
    
    
    Merge-request: IJ-MR-152960
    Merged-by: Arsenii Iusufov <arsenii.iusufov@jetbrains.com>
    
    GitOrigin-RevId: c25463de46750b6caa61bac1d6537d70e241ce0c

--- vuejs/src/org/jetbrains/vuejs/cli/NuxtProjectGenerator.kt
+++ vuejs/src/org/jetbrains/vuejs/cli/NuxtProjectGenerator.kt
@@ -3,20 +3,15 @@ package org.jetbrains.vuejs.cli
 
 import com.intellij.execution.filters.Filter
 import com.intellij.javascript.CreateRunConfigurationUtil
-import com.intellij.javascript.nodejs.interpreter.local.NodeJsLocalInterpreterUtil
-import com.intellij.javascript.nodejs.npm.InstallNodeLocalDependenciesAction
 import com.intellij.javascript.nodejs.packages.NodePackageUtil
 import com.intellij.lang.javascript.boilerplate.NpmPackageProjectGenerator
 import com.intellij.lang.javascript.boilerplate.NpxPackageDescriptor
-import com.intellij.lang.javascript.buildTools.npm.PackageJsonUtil
-import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.util.PathUtil
 import org.jetbrains.vuejs.VueBundle
 import org.jetbrains.vuejs.VuejsIcons
-import java.io.File
 import javax.swing.Icon
 
 class NuxtProjectGenerator : NpmPackageProjectGenerator() {
@@ -42,8 +37,20 @@ class NuxtProjectGenerator : NpmPackageProjectGenerator() {
   override fun customizeModule(baseDir: VirtualFile, entry: ContentEntry?) {}
 
   override fun generatorArgs(project: Project, dir: VirtualFile, settings: Settings): Array<String> {
-    val workingDir = dir.name
-    return arrayOf(CREATE_COMMAND, workingDir, "--no-install")
+    return arrayOf(
+      CREATE_COMMAND,
+      "--force",
+      "--gitInit",
+      /**
+       * - Removes the user's ability to choose for himself
+       * + Prevents an error if the selected manager is not installed
+       *
+       * It's suggested to remove this flag in the future
+       * E.g. it can be removed after implementation of the possibility to choose
+       * a package manager in UI
+       */
+      "--packageManager=npm",
+    )
   }
 
   override fun generatorArgs(project: Project, baseDir: VirtualFile): Array<String> {
@@ -77,24 +84,5 @@ class NuxtProjectGenerator : NpmPackageProjectGenerator() {
     super.onGettingSmartAfterProjectGeneration(project, baseDir)
     CreateRunConfigurationUtil.debugConfiguration(project, 3000)
     CreateRunConfigurationUtil.npmConfiguration(project, "dev")
-  }
-
-  override fun generateInTemp(): Boolean = true
-
-  // trigger npm install
-  override fun postInstall(
-    project: Project,
-    baseDir: VirtualFile,
-    workingDir: File,
-  ): Runnable {
-    return Runnable {
-      ApplicationManager.getApplication().executeOnPooledThread {
-        val packageJson = baseDir.findChild(PackageJsonUtil.FILE_NAME)
-        if (packageJson != null && NodeJsLocalInterpreterUtil.detectAllLocalInterpreters().isNotEmpty()) {
-          InstallNodeLocalDependenciesAction.runAndShowConsole(project, packageJson)
-        }
-        super.postInstall(project, baseDir, workingDir).run()
-      }
-    }
   }
 }
