702ae070800f0224fd190be3e4ef694e487cb9e2
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-01-27 10:08:57 +0100

    [contrib] Update configuration for Angular and handlebars plugins to support gradle build
    GitOrigin-RevId: 3c0afbb25fd4f564943780102d56ecc3189f6071

--- contrib-configuration/common.gradle.kts
+++ contrib-configuration/common.gradle.kts
@@ -1,15 +1,12 @@
 repositories {
   mavenCentral()
-  maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
-  maven("https://www.jetbrains.com/intellij-repository/snapshots")
-  maven("https://packages.jetbrains.team/maven/p/grazi/grazie-platform-public")
-  maven("https://download.jetbrains.com/teamcity-repository")
 }
 
+rootProject.extensions.add("platform.version", "2024.3")
 rootProject.extensions.add("gradle.version", "8.5")
 
-rootProject.extensions.add("kotlin.jvmTarget", "17")
+rootProject.extensions.add("kotlin.jvmTarget", "21")
 rootProject.extensions.add("kotlin.freeCompilerArgs", listOf("-Xjvm-default=all"))
 
-rootProject.extensions.add("java.sourceCompatibility", "17")
-rootProject.extensions.add("java.targetCompatibility", "17")
+rootProject.extensions.add("java.sourceCompatibility", "21")
+rootProject.extensions.add("java.targetCompatibility", "21")
