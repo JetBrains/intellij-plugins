5606d53fb14ae3c9bb0b352a25bfae0a83b8d1b7
Victor Turansky <victor.turansky@jetbrains.com>
2025-01-28 21:48:33 +0200

    WEB-67476 Extract `JSLiteralExpressionElementType` marker interface
    GitOrigin-RevId: 17124f70cb6b8f092f2832063c9ba1f86e757c10

--- Angular/src/org/angular2/lang/expr/parser/Angular2DeferredTimeLiteralExpressionElementType.kt
+++ Angular/src/org/angular2/lang/expr/parser/Angular2DeferredTimeLiteralExpressionElementType.kt
@@ -2,11 +2,11 @@
 package org.angular2.lang.expr.parser
 
 import com.intellij.lang.ASTNode
-import com.intellij.lang.javascript.types.JSLiteralExpressionElementType
+import com.intellij.lang.javascript.types.JSLiteralExpressionElementTypeImpl
 import com.intellij.psi.PsiElement
 import org.angular2.lang.expr.psi.impl.Angular2DeferredTimeLiteralExpressionImpl
 
-class Angular2DeferredTimeLiteralExpressionElementType : JSLiteralExpressionElementType("NG:DEFERRED_TIME_LITERAL_EXPRESSION") {
+class Angular2DeferredTimeLiteralExpressionElementType : JSLiteralExpressionElementTypeImpl("NG:DEFERRED_TIME_LITERAL_EXPRESSION") {
 
   override fun construct(node: ASTNode): PsiElement? {
     return Angular2DeferredTimeLiteralExpressionImpl(node)
