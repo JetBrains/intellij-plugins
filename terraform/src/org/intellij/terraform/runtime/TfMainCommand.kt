7b7aa07cf1a454bd6921a66c25b88a43969c9620
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2025-01-29 17:19:04 +0200

    [terraform] Terraform fix UI cosmetic in run configurations
    GitOrigin-RevId: 11ebf850900b34158e8fba02ba75fef2ce0e55c0

--- terraform/src/org/intellij/terraform/runtime/TfMainCommand.kt
+++ terraform/src/org/intellij/terraform/runtime/TfMainCommand.kt
@@ -1,7 +1,9 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.runtime
 
-internal enum class TfCommand(val command: String) {
+import com.intellij.openapi.util.NlsSafe
+
+internal enum class TfCommand(val command: @NlsSafe String) {
   INIT("init"),
   VALIDATE("validate"),
   PLAN("plan"),
