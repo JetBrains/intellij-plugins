0ab15534a725d996ebf9be6d5896162971efb56f
Victor Turansky <victor.turansky@jetbrains.com>
2025-02-15 13:46:24 +0200

    [astro] WEB-68929 Non-static import for `HtmlFileElementType.getHtmlStubVersion` method
    GitOrigin-RevId: 90cf18ee8ea3c2a84dff80bc50bd17c9960ee1c0

--- Astro/src/org/jetbrains/astro/lang/AstroFileElementType.kt
+++ Astro/src/org/jetbrains/astro/lang/AstroFileElementType.kt
@@ -3,13 +3,13 @@ package org.jetbrains.astro.lang
 
 import com.intellij.psi.stubs.PsiFileStub
 import com.intellij.psi.tree.IStubFileElementType
-import com.intellij.psi.xml.HtmlFileElementType.getHtmlStubVersion
+import com.intellij.psi.xml.HtmlFileElementType
 import org.jetbrains.astro.lang.parser.AstroStubElementTypes
 
 class AstroFileElementType : IStubFileElementType<PsiFileStub<*>>("astro", AstroLanguage.INSTANCE) {
 
   override fun getStubVersion(): Int {
-    return getHtmlStubVersion() + 3 + AstroStubElementTypes.STUB_VERSION
+    return HtmlFileElementType.getHtmlStubVersion() + 3 + AstroStubElementTypes.STUB_VERSION
   }
 
   companion object {
