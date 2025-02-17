c328a03bf521d47f241dd959fa35fe2c04daca00
Dima Golovkov <dmitry.golovkov@jetbrains.com>
2025-02-17 12:42:45 +0200

    Add handlebar icons for UX-3210
    GitOrigin-RevId: 95683e388ee98b3e0c5aeba35e9ed1c085965a3e

--- handlebars/gen/icons/HandlebarsIcons.java
+++ handlebars/gen/icons/HandlebarsIcons.java
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package icons;
 
 import com.intellij.ui.IconManager;
@@ -11,17 +11,17 @@ import javax.swing.*;
  * DO NOT EDIT IT BY HAND, run "Generate icon classes" configuration instead
  */
 public final class HandlebarsIcons {
-  private static @NotNull Icon load(@NotNull String path, int cacheKey, int flags) {
-    return IconManager.getInstance().loadRasterizedIcon(path, HandlebarsIcons.class.getClassLoader(), cacheKey, flags);
+  private static @NotNull Icon load(@NotNull String expUIPath, @NotNull String path, int cacheKey, int flags) {
+    return IconManager.getInstance().loadRasterizedIcon(path, expUIPath, HandlebarsIcons.class.getClassLoader(), cacheKey, flags);
   }
 
   public static final class Elements {
-    /** 16x16 */ public static final @NotNull Icon OpenBlock = load("icons/elements/openBlock.svg", 135067759, 2);
-    /** 16x16 */ public static final @NotNull Icon OpenInverse = load("icons/elements/openInverse.svg", -787685707, 2);
-    /** 16x16 */ public static final @NotNull Icon OpenMustache = load("icons/elements/openMustache.svg", 176760331, 2);
-    /** 16x16 */ public static final @NotNull Icon OpenPartial = load("icons/elements/openPartial.svg", 73034127, 2);
-    /** 16x16 */ public static final @NotNull Icon OpenUnescaped = load("icons/elements/openUnescaped.svg", -1395361920, 2);
+    /** 16x16 */ public static final @NotNull Icon OpenBlock = load("icons/expui/elements/openBlock.svg", "icons/elements/openBlock.svg", 135067759, 2);
+    /** 16x16 */ public static final @NotNull Icon OpenInverse = load("icons/expui/elements/openInverse.svg", "icons/elements/openInverse.svg", -787685707, 2);
+    /** 16x16 */ public static final @NotNull Icon OpenMustache = load("icons/expui/elements/openMustache.svg", "icons/elements/openMustache.svg", 176760331, 2);
+    /** 16x16 */ public static final @NotNull Icon OpenPartial = load("icons/expui/elements/openPartial.svg", "icons/elements/openPartial.svg", 73034127, 2);
+    /** 16x16 */ public static final @NotNull Icon OpenUnescaped = load("icons/expui/elements/openUnescaped.svg", "icons/elements/openUnescaped.svg", -1395361920, 2);
   }
 
-  /** 16x16 */ public static final @NotNull Icon Handlebars_icon = load("icons/handlebars_icon.svg", 1457277380, 0);
+  /** 16x16 */ public static final @NotNull Icon Handlebars_icon = load("icons/expui/handlebars.svg", "icons/handlebars_icon.svg", 1457277380, 0);
 }
