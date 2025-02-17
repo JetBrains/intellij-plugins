41322889315c8867285535eae5ceef3443b09432
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-02-17 11:08:25 +0100

    [pug] WEB-70618 Pug: open source the plugin - update build configuration files after directory move
    GitOrigin-RevId: 84d608abd886367a3a0865162d526a3d7c8442a3

---
+++ jade/build.gradle.kts
@@ -0,0 +1,76 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+apply(from = "../contrib-configuration/common.gradle.kts")
+
+plugins {
+  id("java")
+  id("org.jetbrains.kotlin.jvm")
+  id("org.jetbrains.intellij.platform")
+}
+
+repositories {
+  intellijPlatform {
+    defaultRepositories()
+    snapshots()
+  }
+}
+
+intellijPlatform {
+  pluginConfiguration {
+    name = "Jade"
+  }
+}
+
+sourceSets {
+  main {
+    java {
+      setSrcDirs(listOf("src", "gen"))
+    }
+    resources {
+      setSrcDirs(listOf("resources"))
+    }
+  }
+  test {
+    java {
+      setSrcDirs(listOf("tests"))
+    }
+    resources {
+      setSrcDirs(listOf("testData", "testResources"))
+    }
+  }
+}
+
+dependencies {
+  intellijPlatform {
+    bundledPlugins("JavaScript", "JSIntentionPowerPack", "HtmlTools", "com.intellij.css", "com.intellij.plugins.watcher")
+
+    jetbrainsRuntime()
+    intellijIdeaUltimate(ext("platform.version"), useInstaller = false)
+    testFramework(TestFrameworkType.Platform)
+  }
+  testImplementation("junit:junit:${ext("junit.version")}")
+}
+
+kotlin {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.fromTarget(ext("kotlin.jvmTarget")))
+    @Suppress("UNCHECKED_CAST")
+    freeCompilerArgs.addAll(rootProject.extensions["kotlin.freeCompilerArgs"] as List<String>)
+  }
+}
+
+tasks {
+  java {
+    sourceCompatibility = JavaVersion.toVersion(ext("java.sourceCompatibility"))
+    targetCompatibility = JavaVersion.toVersion(ext("java.targetCompatibility"))
+  }
+  wrapper {
+    gradleVersion = ext("gradle.version")
+  }
+}
+
+fun ext(name: String): String =
+  rootProject.extensions[name] as? String
+  ?: error("Property `$name` is not defined")
