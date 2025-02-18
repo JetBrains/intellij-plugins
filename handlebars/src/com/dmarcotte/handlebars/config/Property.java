6b0fcfdc82b7c10e0aa79277d7e5300e1951581b
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-18 14:46:36 +0100

    fix TMHAssertionGenerator1 visibility
    GitOrigin-RevId: 305f017803cca1fd67db24ed11e93b538f7f9b60

--- handlebars/src/com/dmarcotte/handlebars/config/Property.java
+++ handlebars/src/com/dmarcotte/handlebars/config/Property.java
@@ -1,13 +1,15 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.dmarcotte.handlebars.config;
 
 import com.intellij.lang.html.HTMLLanguage;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * Formalizes the properties which we will persist using {@link com.intellij.ide.util.PropertiesComponent}
  */
-enum Property {
+@ApiStatus.Internal
+public enum Property {
   AUTO_GENERATE_CLOSE_TAG {
     @Override
     public @NotNull String getStringName() {
