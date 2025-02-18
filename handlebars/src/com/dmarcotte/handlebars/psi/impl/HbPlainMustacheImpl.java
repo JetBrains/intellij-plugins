17f2dde2dab1bb53521a273180f2a0165ee14af3
Vladimir Krivosheev <vladimir.krivosheev@jetbrains.com>
2025-02-18 12:13:03 +0100

    fix HbPlainMustacheImpl visibility
    GitOrigin-RevId: cde2f23f5c86453091c9ef81eb8777ce5688dcc2

--- handlebars/src/com/dmarcotte/handlebars/psi/impl/HbPlainMustacheImpl.java
+++ handlebars/src/com/dmarcotte/handlebars/psi/impl/HbPlainMustacheImpl.java
@@ -1,10 +1,13 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.dmarcotte.handlebars.psi.impl;
 
 import com.dmarcotte.handlebars.psi.HbPlainMustache;
 import com.intellij.lang.ASTNode;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-class HbPlainMustacheImpl extends HbMustacheImpl implements HbPlainMustache {
+@ApiStatus.Internal
+public class HbPlainMustacheImpl extends HbMustacheImpl implements HbPlainMustache {
   HbPlainMustacheImpl(@NotNull ASTNode astNode) {
     super(astNode);
   }
