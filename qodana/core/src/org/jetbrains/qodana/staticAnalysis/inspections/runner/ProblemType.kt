00fcee863fff53dff9659e7acb885d89739ed2a5
Evgeniia Kirillova <evgeniia.kirillova@jetbrains.com>
2025-02-20 12:19:18 +0100

    [qodana] QD-10764: Add selecting problems for relatedLocations so that they can be merged into bigger snippets
    Problems are selected in IncorrectFormattingResultHandlerQodana by dividing them into groups where the difference between the first and last problem is not more than SNIPPET_SIZE, then SELECTED_SNIPPETS_AMOUNT of these groups is chosen and corresponding results added to relatedLocations. Also INCORRECT_FORMATTING problem type is added.
    
    
    (cherry picked from commit ff1d5724b417c57c2d0966d5a45c7a87c3868ac4)
    
    IJ-CR-153894
    
    GitOrigin-RevId: 3ff8663d3173502648d0b660f8b0993fafd38183

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/runner/ProblemType.kt
+++ qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/runner/ProblemType.kt
@@ -5,6 +5,5 @@ enum class ProblemType {
   TAINT,
   DUPLICATES,
   VULNERABLE_API_WITH_RELATED_LOCATIONS,
-  // incorrect formatting type is for future development of a separate structural differences for this inspection
   INCORRECT_FORMATTING
 }
