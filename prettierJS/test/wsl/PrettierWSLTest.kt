2ae58166ebc98cd56df300ead1b3f53b1017eb71
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2025-02-28 16:01:34 +0200

    [javascript][typescript] WEB-71846".prettierignore" doesn't respected when Node.js in WSL
    GitOrigin-RevId: 7ac0e6f686f0b1b9a2f1710e2bd77a1c75d32552

--- prettierJS/test/wsl/PrettierWSLTest.kt
+++ prettierJS/test/wsl/PrettierWSLTest.kt
@@ -1,5 +1,6 @@
 package com.intellij.prettier.wsl
 
+import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.prettierjs.PrettierConfiguration
 import com.intellij.testFramework.PlatformTestUtil
 import com.intellij.wsl.WSLTempDirWithNodeInterpreterBase
@@ -26,5 +27,27 @@ class PrettierWSLTest : WSLTempDirWithNodeInterpreterBase() {
     fixture.performEditorAction("SaveAll")
     EventQueue.invokeAndWait { PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue() }
     fixture.checkResult("var a = \"\";\n")
+  }
+
+  @Test
+  fun testPrettierIgnoreOnSave() {
+    fixture.addFileToProject("package.json", "{\n" +
+                                             "  \"version\": \"1.0.0\",\n" +
+                                             "  \"devDependencies\": {\n" +
+                                             "    \"prettier\": \"latest\"\n" +
+                                             "  }\n" +
+                                             "}\n")
+    fixture.addFileToProject(".prettierrc", "{}")
+    val ignoreFile = fixture.addFileToProject(".customignore", "foo.js")
+    val prettierConfiguration = PrettierConfiguration.getInstance(fixture.project).state
+    prettierConfiguration.configurationMode = PrettierConfiguration.ConfigurationMode.MANUAL
+    prettierConfiguration.customIgnorePath = VfsUtilCore.virtualToIoFile(ignoreFile.virtualFile).absolutePath
+    prettierConfiguration.runOnSave = true
+    runNpmInstall()
+    fixture.configureByText("foo.js", "var  a=''")
+    fixture.type(' ')
+    fixture.performEditorAction("SaveAll")
+    EventQueue.invokeAndWait { PlatformTestUtil.dispatchAllInvocationEventsInIdeEventQueue() }
+    fixture.checkResult("var  a='' ")
   }
 }
