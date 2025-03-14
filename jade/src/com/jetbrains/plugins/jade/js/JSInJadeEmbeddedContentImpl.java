867d38e9c62e5b318e2f217adb558a0e024d5e09
Connor McLaughlin <connor1211.mcl@gmail.com>
2025-03-14 23:24:53 +0000

    [pug] WEB-57370 Pug: support var const let references in unbuffered code
    GitOrigin-RevId: c0b754275a093446ecee2d2ace2f1c1f16bcf8d9

--- jade/src/com/jetbrains/plugins/jade/js/JSInJadeEmbeddedContentImpl.java
+++ jade/src/com/jetbrains/plugins/jade/js/JSInJadeEmbeddedContentImpl.java
@@ -4,8 +4,12 @@ package com.jetbrains.plugins.jade.js;
 import com.intellij.extapi.psi.ASTWrapperPsiElement;
 import com.intellij.lang.ASTNode;
 import com.intellij.lang.Language;
-import com.intellij.lang.javascript.psi.JSEmbeddedContent;
+import com.intellij.lang.javascript.psi.*;
 import com.intellij.lang.javascript.psi.controlflow.JSControlFlowService;
+import com.intellij.lang.javascript.psi.resolve.JSResolveUtil;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.ResolveState;
+import com.intellij.psi.scope.PsiScopeProcessor;
 import com.intellij.psi.tree.IElementType;
 import com.jetbrains.plugins.jade.psi.JadeTokenTypes;
 import org.jetbrains.annotations.NotNull;
@@ -19,6 +23,18 @@ public class JSInJadeEmbeddedContentImpl extends ASTWrapperPsiElement implements
   @Override
   public @NotNull Language getLanguage() {
     return JavaScriptInJadeLanguageDialect.INSTANCE;
+  }
+
+  @Override
+  public boolean processDeclarations(@NotNull PsiScopeProcessor processor,
+                                     @NotNull ResolveState state,
+                                     PsiElement lastParent,
+                                     @NotNull PsiElement place) {
+    boolean result = JSResolveUtil.processDeclarationsInScope(this, processor, state, lastParent, place);
+    if (result) {
+      processor.handleEvent(PsiScopeProcessor.Event.SET_DECLARATION_HOLDER, this);
+    }
+    return result;
   }
 
   @Override
