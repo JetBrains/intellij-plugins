b1436404c79187fc7d9266fda047c9e6edb60a33
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2025-03-20 18:48:33 +0200

    [javascript][typescript] WEB-47852 Prettier plugin - editor caret jumps abruptly on reformat
    (cherry picked from commit f11f3dd9516759d835b403ba3c4a8144c807e46d)
    
    IJ-CR-159141
    
    GitOrigin-RevId: a91e309eb2363972a739e703428c7e5b73f24a93

--- prettierJS/testData/reformat/caretPosition/first/toReformat.js
+++
@@ -1,8 +0,0 @@
-function f1() {
-  const i1 = 0;
-
-  function Connect() {
-
-    <caret>
-  }
-}
---
+++ prettierJS/testData/reformat/caretPosition/toReformat.js
@@ -0,0 +1,20 @@
+function f1(
+
+
+
+
+
+
+) {
+  const i1 = 0;
+
+  function Connect(
+
+
+  <caret>
+
+  ) {
+
+
+  }
+}
