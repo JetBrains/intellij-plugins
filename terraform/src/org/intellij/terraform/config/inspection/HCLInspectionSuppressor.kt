4fb7a8ed37a92b2b8b8317a675e9f78dd0a35902
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2025-03-28 15:17:16 +0200

    [terraform] IJPL-149116 Take the comment symbol into account when initializing SUPPRESS_IN_LINE_COMMENT_PATTERN
    GitOrigin-RevId: a3e249a70c51b67a24bedda62711d6c6c1aac027

--- terraform/src/org/intellij/terraform/config/inspection/HCLInspectionSuppressor.kt
+++ terraform/src/org/intellij/terraform/config/inspection/HCLInspectionSuppressor.kt
@@ -1,20 +1,42 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.config.inspection
 
+import com.intellij.application.options.CodeStyle
 import com.intellij.codeInspection.InspectionSuppressor
 import com.intellij.codeInspection.SuppressQuickFix
 import com.intellij.codeInspection.SuppressionUtil
+import com.intellij.openapi.application.ReadAction
 import com.intellij.psi.PsiElement
 import org.intellij.terraform.hcl.HCLBundle
+import org.intellij.terraform.hcl.formatter.HclCodeStyleSettings
 import org.intellij.terraform.hcl.psi.HCLBlock
 import org.intellij.terraform.hcl.psi.HCLProperty
 import org.intellij.terraform.hcl.psi.getNameElementUnquoted
+import java.util.regex.Pattern
 
-class HCLInspectionSuppressor : InspectionSuppressor {
-  override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean {
-    // TODO: Support suppression for file, module
-    return SuppressionUtil.isSuppressedInStatement(element, toolId, HCLBlock::class.java)
-        || SuppressionUtil.isSuppressedInStatement(element, toolId, HCLProperty::class.java)
+internal class HCLInspectionSuppressor : InspectionSuppressor {
+  override fun isSuppressedFor(element: PsiElement, toolId: String): Boolean = ReadAction.compute<Boolean, RuntimeException> {
+    if (getSuppressedInBlock(element, toolId) != null) {
+      return@compute true
+    }
+    if (getSuppressedProperty(element, toolId) != null) {
+      return@compute true
+    }
+    false
+  }
+
+  private fun getSuppressedInBlock(element: PsiElement, toolId: String): PsiElement? {
+    return SuppressionUtil.getStatementToolSuppressedIn(element, toolId, HCLBlock::class.java, getInLineCommentPattern(element))
+  }
+
+  private fun getSuppressedProperty(element: PsiElement, toolId: String): PsiElement? {
+    return SuppressionUtil.getStatementToolSuppressedIn(element, toolId, HCLProperty::class.java, getInLineCommentPattern(element))
+  }
+
+  private fun getInLineCommentPattern(element: PsiElement): Pattern {
+    val settings = CodeStyle.getCustomSettings(element.containingFile, HclCodeStyleSettings::class.java)
+    val commenterChar = settings.LINE_COMMENTER_CHARACTER.prefix
+    return Pattern.compile(commenterChar + SuppressionUtil.COMMON_SUPPRESS_REGEXP + ".*")
   }
 
   override fun getSuppressActions(element: PsiElement?, toolId: String): Array<SuppressQuickFix> {
