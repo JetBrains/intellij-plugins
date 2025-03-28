5f9f859e728977b23d50c2ab7cd98b4774cc1ac3
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-03-28 12:19:09 +0100

    [angular] WEB-71890 Angular: analyzing in html file never ends - ensure that CRLF files are handled properly
    (cherry picked from commit dc686a25389125c18734e15ae594c4e4bad8463c)
    
    IJ-CR-158974
    
    GitOrigin-RevId: becca3e8d2eda4ca07c5a5590acaace18c20c640

--- Angular/src/org/angular2/lang/expr/service/Angular2TypeScriptServiceEvaluationSupport.kt
+++ Angular/src/org/angular2/lang/expr/service/Angular2TypeScriptServiceEvaluationSupport.kt
@@ -4,10 +4,12 @@ import com.intellij.lang.javascript.psi.JSType
 import com.intellij.lang.typescript.compiler.TypeScriptServiceEvaluationSupport
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiFile
+import com.intellij.util.concurrency.annotations.RequiresReadLock
 import org.angular2.lang.expr.service.tcb.Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile
 
 interface Angular2TypeScriptServiceEvaluationSupport : TypeScriptServiceEvaluationSupport {
 
+  @RequiresReadLock
   fun getGeneratedElementType(transpiledFile: TranspiledDirectiveFile, templateFile: PsiFile, generatedRange: TextRange): JSType?
 
 }
