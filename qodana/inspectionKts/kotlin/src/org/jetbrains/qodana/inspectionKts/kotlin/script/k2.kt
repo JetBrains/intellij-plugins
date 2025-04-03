8f678a43d2ed406a10bc90714e5dabb8df12a666
Vlad Koshkin <vladislav.koshkin@jetbrains.com>
2025-04-03 23:02:53 +0000

    [kotlin] introduce ide scripting refinement api
    Merge-request: IJ-MR-159069
    Merged-by: Vlad Koshkin <Vladislav.Koshkin@jetbrains.com>
    
    GitOrigin-RevId: dab6bf1bec7d32a6d0cb24cadff9477bdaf766bd

--- qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/k2.kt
+++ qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/k2.kt
@@ -1,43 +1,11 @@
 package org.jetbrains.qodana.inspectionKts.kotlin.script
 
 import com.intellij.openapi.project.Project
-import com.intellij.platform.backend.workspace.workspaceModel
-import com.intellij.platform.workspace.storage.MutableEntityStorage
-import com.intellij.platform.workspace.storage.url.VirtualFileUrl
-import kotlinx.coroutines.CoroutineScope
-import org.jetbrains.kotlin.idea.core.script.KotlinScriptEntitySource
-import org.jetbrains.kotlin.idea.core.script.getUpdatedStorage
-import org.jetbrains.kotlin.idea.core.script.k2.BundledScriptConfigurationsSource
-import org.jetbrains.kotlin.idea.core.script.scriptDefinitionsSourceOfType
 import org.jetbrains.kotlin.scripting.definitions.ScriptDefinition
 import org.jetbrains.kotlin.scripting.definitions.ScriptDefinitionsSource
 import kotlin.script.experimental.jvm.defaultJvmScriptingHostConfiguration
 
-internal class QodanaScriptConfigurationSource(
-  override val project: Project,
-  cs: CoroutineScope
-) : BundledScriptConfigurationsSource(project, cs) {
-  override fun getDefinitions(): Sequence<ScriptDefinition>? {
-    return project.scriptDefinitionsSourceOfType<QodanaScriptDefinitionSource>()?.definitions
-  }
-
-  override suspend fun updateModules(storage: MutableEntityStorage?) {
-    val updatedStorage = getUpdatedStorage(
-      project, data.get()
-    ) { QodanaKotlinScriptEntitySource(it) }
-
-    project.workspaceModel.update("updating .inspection.kts modules") {
-      it.replaceBySource(
-        { source -> source is QodanaKotlinScriptEntitySource }, updatedStorage
-      )
-    }
-  }
-}
-
-open class QodanaKotlinScriptEntitySource(override val virtualFileUrl: VirtualFileUrl?) :
-  KotlinScriptEntitySource(virtualFileUrl)
-
-internal class QodanaScriptDefinitionSource : ScriptDefinitionsSource {
+internal class QodanaScriptDefinitionSource(val project: Project) : ScriptDefinitionsSource {
   override val definitions: Sequence<ScriptDefinition>
     get() = sequenceOf(
       ScriptDefinition.FromConfigurations(
