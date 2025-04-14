f4eae40f1790ede3df924844b7b64990290847f9
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2025-04-14 18:42:09 +0300

    [javascript][typescript] WEB-72377: Remove `readAction` from the resolve prettier configuration for the file
    GitOrigin-RevId: 77719e766193c8c0e77f34e46590c8f7fc94a52f

--- prettierJS/src/com/intellij/prettierjs/PrettierConfigUtil.kt
+++ prettierJS/src/com/intellij/prettierjs/PrettierConfigUtil.kt
@@ -1,18 +1,18 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.prettierjs
 
-import com.intellij.openapi.application.readAction
 import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
+import com.intellij.util.concurrency.annotations.RequiresReadLock
 import kotlinx.coroutines.future.await
 
+@RequiresReadLock
 internal suspend fun resolveConfigForFile(file: PsiFile): PrettierLanguageService.ResolveConfigResult? {
   val project = file.project
-  val filePath = readAction {
-    if (file.isValid) file.virtualFile.path else null
-  } ?: return null
+  val filePath = if (file.isValid) file.virtualFile.path else null
+  if (filePath == null) return null
 
   val nodePackage = PrettierConfiguration.getInstance(project).getPackage(file)
   val service = PrettierLanguageService.getInstance(project, file.virtualFile, nodePackage)
