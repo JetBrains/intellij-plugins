663af7fa6147b26219d0f7017ecbb47d450982d8
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-04-17 10:18:43 +0200

    [angular] WEB-53245 Make sure that indexes are always the same regardless of the presence or absence of Angular context.
    GitOrigin-RevId: 6016b2879005a321fbebc7e15ee348e01a452c06

--- Angular/src/org/angular2/lang/Angular2LangUtil.kt
+++ Angular/src/org/angular2/lang/Angular2LangUtil.kt
@@ -1,12 +1,15 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.angular2.lang
 
+import com.intellij.lang.javascript.psi.impl.JSStubElementImpl
 import com.intellij.openapi.fileTypes.FileType
 import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiElement
 import com.intellij.webSymbols.context.WebSymbolsContext
+import com.intellij.webSymbols.context.WebSymbolsContext.Companion.KIND_FRAMEWORK
+import org.angular2.Angular2Framework
 import org.angular2.angular2Framework
 import org.angular2.lang.html.Angular2HtmlDialect
 import org.angular2.lang.html.Angular2TemplateSyntax
@@ -27,16 +30,18 @@ object Angular2LangUtil {
 
   @JvmStatic
   fun isAtLeastAngularVersion(context: PsiElement, version: AngularVersion): Boolean {
-    WebSymbolsContext.get("angular-version", context )
+    WebSymbolsContext.get("angular-version", context)
       ?.let { AngularVersion.valueOf(it) }
       ?.let { return it.ordinal >= version.ordinal }
     return version == AngularVersion.V_2
   }
 
   @JvmStatic
-  fun isAngular2Context(context: PsiElement): Boolean {
-    return angular2Framework.isInContext(context)
-  }
+  fun isAngular2Context(context: PsiElement): Boolean =
+    if (JSStubElementImpl.isBuildingStubs())
+      true
+    else
+      WebSymbolsContext.get(KIND_FRAMEWORK, context) == Angular2Framework.ID
 
   @JvmStatic
   fun getTemplateSyntax(context: PsiElement?): Angular2TemplateSyntax =
