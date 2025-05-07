0e12cf710f08c4b7c0ec86bded674d68a1a0deb7
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2025-05-07 20:22:03 +0500

    [terraform] IJPL-174559 Replaced killProcess with cleanup using `NonCancellable` in try/finally, improved error handling
    GitOrigin-RevId: 4cffd8e9ea32abea7b1c90dbf5f7dda7529a3756

--- terraform/src/org/intellij/terraform/hcl/formatter/TfAsyncFormattingService.kt
+++ terraform/src/org/intellij/terraform/hcl/formatter/TfAsyncFormattingService.kt
@@ -5,10 +5,9 @@ import com.intellij.application.options.CodeStyle
 import com.intellij.formatting.service.AsyncDocumentFormattingService
 import com.intellij.formatting.service.AsyncFormattingRequest
 import com.intellij.formatting.service.FormattingService
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.diagnostic.logger
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.progress.runBlockingCancellable
-import com.intellij.openapi.progress.runBlockingMaybeCancellable
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.platform.eel.EelProcess
@@ -18,10 +17,7 @@ import com.intellij.platform.eel.provider.getEelDescriptor
 import com.intellij.platform.eel.provider.utils.readWholeText
 import com.intellij.platform.eel.provider.utils.sendWholeText
 import com.intellij.psi.PsiFile
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import org.intellij.terraform.config.Constants.TF_FMT
 import org.intellij.terraform.config.TerraformFileType
 import org.intellij.terraform.config.TfConstants
@@ -30,6 +26,7 @@ import org.intellij.terraform.hcl.HCLBundle
 import org.intellij.terraform.install.TfToolType
 import org.intellij.terraform.runtime.TfToolPathDetector
 import org.intellij.terraform.runtime.showIncorrectPathNotification
+import java.util.concurrent.CancellationException
 
 internal class TfAsyncFormattingService : AsyncDocumentFormattingService() {
   override fun getName(): String = TF_FMT
@@ -51,47 +48,54 @@ internal class TfAsyncFormattingService : AsyncDocumentFormattingService() {
 
     return object : FormattingTask {
       private var job: Job? = null
-      private var eelProcess: EelProcess? = null
 
       override fun run() {
-        configureTfPath(project, toolType)
-        val exePath = toolType.getToolSettings(project).toolPath
+        try {
+          configureTfPath(project, toolType)
+          val exePath = toolType.getToolSettings(project).toolPath
 
-        runBlockingCancellable {
-          job = launch {
-            try {
-              withContext(Dispatchers.IO) {
-                val eelApi = project.getEelDescriptor().upgrade()
-                eelProcess = eelApi.exec.execute(exePath).args("fmt", "-").getOrThrow()
-                val process = eelProcess ?: throw ProcessCanceledException()
+          runBlockingCancellable {
+            job = launch {
+              var process: EelProcess? = null
+              try {
+                withContext(Dispatchers.IO) {
+                  val eelApi = project.getEelDescriptor().upgrade()
+                  process = eelApi.exec.execute(exePath).args("fmt", "-").getOrThrow()
 
-                process.stdin.sendWholeText(request.documentText).getOrThrow()
-                process.stdin.close()
+                  process.stdin.sendWholeText(request.documentText).getOrThrow()
+                  process.stdin.close()
 
-                val formattedText = process.stdout.readWholeText().getOrThrow()
-                val exitCode = process.exitCode.await()
-
-                if (exitCode == 0) {
-                  request.onTextReady(formattedText)
+                  val formattedText = process.stdout.readWholeText().getOrThrow()
+                  val exitCode = process.exitCode.await()
+                  if (exitCode == 0) {
+                    request.onTextReady(formattedText)
+                  }
+                  else {
+                    @NlsSafe val errorMessage = process.stderr.readWholeText().getOrThrow()
+                    request.onError(HCLBundle.message("terraform.formatter.error.title", toolType.executableName), errorMessage)
+                  }
                 }
-                else {
-                  @NlsSafe val errorMessage = process.stderr.readWholeText().getOrThrow()
-                  request.onError(HCLBundle.message("terraform.formatter.error.title", toolType.executableName), errorMessage)
+              }
+              finally {
+                withContext(NonCancellable) {
+                  process?.kill()
                 }
               }
             }
-            catch (e: Exception) {
-              request.onError(HCLBundle.message("terraform.formatter.error.title", toolType.executableName),
-                              HCLBundle.message("terraform.formatter.error.message", virtualFile.name, toolType.executableName))
-              throw e
-            }
           }
+        }
+        catch (e: CancellationException) {
+          throw e
+        }
+        catch (e: Exception) {
+          logger<TfAsyncFormattingService>().warn("Failed to run FormattingTask", e)
+          request.onError(HCLBundle.message("terraform.formatter.error.title", toolType.executableName),
+                          HCLBundle.message("terraform.formatter.error.message", virtualFile.name, toolType.executableName))
         }
       }
 
       override fun cancel(): Boolean {
         job?.cancel()
-        eelProcess?.let { killProcess(it) }
         return true
       }
 
@@ -107,16 +111,6 @@ internal class TfAsyncFormattingService : AsyncDocumentFormattingService() {
     if (!isToolConfigured) {
       showIncorrectPathNotification(project, toolType)
       throw ProcessCanceledException()
-    }
-  }
-
-  private fun killProcess(process: EelProcess) {
-    ApplicationManager.getApplication().executeOnPooledThread {
-      runBlockingMaybeCancellable {
-        withContext(Dispatchers.IO) {
-          process.kill()
-        }
-      }
     }
   }
 }
