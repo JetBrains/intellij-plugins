bbb310738f8f550d1f4a6777f73d3322b21c73d8
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2025-05-07 09:10:27 +0200

    [jps build] IJI-1676 IDEA-372025 Support running tests on compilation output jars: fix `osgi.project.BuildProcessClasspathTest`
    GitOrigin-RevId: ae5e58dad5c5acdc4d5a963a74542f39acda3d2a

--- osmorc/src_test/org/jetbrains/osgi/project/BuildProcessClasspathTest.kt
+++ osmorc/src_test/org/jetbrains/osgi/project/BuildProcessClasspathTest.kt
@@ -1,7 +1,8 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.osgi.project
 
 import com.intellij.compiler.server.impl.BuildProcessClasspathManager
+import com.intellij.openapi.application.PathManager
 import com.intellij.openapi.project.DefaultProjectFactory
 import com.intellij.psi.impl.light.LightJavaModule
 import com.intellij.testFramework.fixtures.BareTestFixtureTestCase
@@ -13,6 +14,17 @@ class BuildProcessClasspathTest : BareTestFixtureTestCase() {
   @Test fun testBuildProcessClasspath() {
     val classpath = BuildProcessClasspathManager(testRootDisposable).getBuildProcessClasspath(DefaultProjectFactory.getInstance().defaultProject)
     val libs = classpath.mapTo(HashSet()) { LightJavaModule.moduleName(File(it).name) }
-    assertThat(libs).contains("intellij.osgi.jps", "biz.aQute.bndlib", "biz.aQute.repository", "biz.aQute.resolve", "plexus.utils")
+
+    // libraries have the same names even when running from archived compilation outputs
+    assertThat(libs).contains("biz.aQute.bndlib", "biz.aQute.repository", "biz.aQute.resolve", "plexus.utils")
+
+    val module = "intellij.osgi.jps"
+    val mapping = PathManager.getArchivedCompiledClassesMapping()
+    if (mapping != null) {
+      assertThat(classpath.toSet()).contains(mapping["production/$module"])
+    }
+    else {
+      assertThat(libs).contains(module)
+    }
   }
 }
