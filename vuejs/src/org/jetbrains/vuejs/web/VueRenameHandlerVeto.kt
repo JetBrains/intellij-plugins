1f2d9a5243c1f469f5262a45440252d551aaac16
Victor Turansky <victor.turansky@jetbrains.com>
2025-05-11 21:54:09 +0300

    [vue] Optimize imports in `VueRenameHandlerVeto`
    GitOrigin-RevId: faf7f9014b475a03306b3f375fa9c1b06cf20468

--- vuejs/src/org/jetbrains/vuejs/web/VueRenameHandlerVeto.kt
+++ vuejs/src/org/jetbrains/vuejs/web/VueRenameHandlerVeto.kt
@@ -1,11 +1,11 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.web
 
-import com.intellij.webSymbols.declarations.WebSymbolDeclarationProvider
 import com.intellij.lang.javascript.psi.JSLiteralExpression
 import com.intellij.lang.javascript.psi.stubs.JSImplicitElement
 import com.intellij.openapi.util.Condition
 import com.intellij.psi.PsiElement
+import com.intellij.webSymbols.declarations.WebSymbolDeclarationProvider
 import org.jetbrains.vuejs.context.isVueContext
 
 class VueRenameHandlerVeto : Condition<PsiElement> {
