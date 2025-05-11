cc7ce0e0e9de4a3f797837a46e7e1b7cdc84b84a
Victor Turansky <victor.turansky@jetbrains.com>
2025-05-11 23:51:03 +0300

    [vue] Fix `VueFileImportsResolver` formatting
    GitOrigin-RevId: d632d964858f4fb2e95b001eb5d2ab9821dfcb2e

--- vuejs/src/org/jetbrains/vuejs/lang/typescript/VueFileImportsResolver.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/typescript/VueFileImportsResolver.kt
@@ -13,10 +13,11 @@ import org.jetbrains.vuejs.index.VUE_DEFAULT_EXTENSIONS_WITH_DOT
 import org.jetbrains.vuejs.index.VUE_MODULE
 import org.jetbrains.vuejs.lang.html.VueFileType
 
-class VueFileImportsResolver(project: Project,
-                             resolveContext: JSImportResolveContext,
-                             private val contextFile: VirtualFile) :
-  TypeScriptFileImportsResolverImpl(project, resolveContext, VUE_DEFAULT_EXTENSIONS_WITH_DOT, listOf(VueFileType)) {
+class VueFileImportsResolver(
+  project: Project,
+  resolveContext: JSImportResolveContext,
+  private val contextFile: VirtualFile,
+) : TypeScriptFileImportsResolverImpl(project, resolveContext, VUE_DEFAULT_EXTENSIONS_WITH_DOT, listOf(VueFileType)) {
 
   override fun processAllFilesInScope(includeScope: GlobalSearchScope, processor: Processor<in VirtualFile>) {
     if (includeScope == GlobalSearchScope.EMPTY_SCOPE) return
