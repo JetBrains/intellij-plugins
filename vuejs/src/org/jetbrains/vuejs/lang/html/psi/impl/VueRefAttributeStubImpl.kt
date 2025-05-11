faf41772ea99785fdcce1c601181d29ff93c2b68
Victor Turansky <victor.turansky@jetbrains.com>
2025-05-11 23:49:47 +0300

    [vue] Fix `VueRefAttributeStubImpl` formatting
    GitOrigin-RevId: e4d283ae52e0d3e2bd66519d54861683d16a16f0

--- vuejs/src/org/jetbrains/vuejs/lang/html/psi/impl/VueRefAttributeStubImpl.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/html/psi/impl/VueRefAttributeStubImpl.kt
@@ -5,23 +5,29 @@ import com.intellij.psi.impl.source.xml.stub.XmlAttributeStub
 import com.intellij.psi.stubs.*
 import org.jetbrains.vuejs.lang.html.psi.VueRefAttribute
 
-class VueRefAttributeStubImpl : StubBase<VueRefAttribute>, XmlAttributeStub<VueRefAttribute> {
+class VueRefAttributeStubImpl :
+  StubBase<VueRefAttribute>,
+  XmlAttributeStub<VueRefAttribute> {
 
   val containingTagName: String
   val isList: Boolean
   val value: String
 
-  constructor(parent: StubElement<*>?,
-              dataStream: StubInputStream,
-              elementType: IStubElementType<out VueRefAttributeStubImpl, out VueRefAttributeImpl>) : super(parent, elementType) {
+  constructor(
+    parent: StubElement<*>?,
+    dataStream: StubInputStream,
+    elementType: IStubElementType<out VueRefAttributeStubImpl, out VueRefAttributeImpl>,
+  ) : super(parent, elementType) {
     value = dataStream.readNameString() ?: ""
     isList = dataStream.readBoolean()
     containingTagName = dataStream.readNameString() ?: ""
   }
 
-  constructor(psi: VueRefAttributeImpl,
-              parent: StubElement<*>?,
-              elementType: IStubElementType<out VueRefAttributeStubImpl, out VueRefAttributeImpl>) : super(parent, elementType) {
+  constructor(
+    psi: VueRefAttributeImpl,
+    parent: StubElement<*>?,
+    elementType: IStubElementType<out VueRefAttributeStubImpl, out VueRefAttributeImpl>,
+  ) : super(parent, elementType) {
     value = psi.value ?: ""
     isList = psi.isList
     containingTagName = psi.containingTagName
