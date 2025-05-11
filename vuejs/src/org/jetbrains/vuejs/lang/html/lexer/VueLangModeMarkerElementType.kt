8883567b366141ce84f18d7420ab780dc709f9dc
Victor Turansky <victor.turansky@jetbrains.com>
2025-05-11 23:44:47 +0300

    [vue] Fix `VueLangModeMarkerElementType` formatting
    GitOrigin-RevId: 812ef7eead82473d684a819e3ebb5a5c2df0bf34

--- vuejs/src/org/jetbrains/vuejs/lang/html/lexer/VueLangModeMarkerElementType.kt
+++ vuejs/src/org/jetbrains/vuejs/lang/html/lexer/VueLangModeMarkerElementType.kt
@@ -8,9 +8,12 @@ import com.intellij.psi.tree.ILeafElementType
 import org.jetbrains.vuejs.lang.LangMode
 import org.jetbrains.vuejs.lang.html.VueLanguage
 
-class VueLangModeMarkerElementType(val langMode: LangMode) : IElementType("VUE_LANG_MODE_$langMode", VueLanguage.INSTANCE),
-                                                             ILeafElementType {
-    override fun createLeafNode(leafText: CharSequence): ASTNode {
-      return LeafPsiElement(this, leafText)
-    }
+class VueLangModeMarkerElementType(
+  val langMode: LangMode,
+) : IElementType("VUE_LANG_MODE_$langMode", VueLanguage.INSTANCE),
+    ILeafElementType {
+
+  override fun createLeafNode(leafText: CharSequence): ASTNode {
+    return LeafPsiElement(this, leafText)
   }
+}
