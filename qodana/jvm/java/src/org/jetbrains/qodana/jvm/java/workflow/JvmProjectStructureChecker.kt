312a8aac174a0a825845bac2209ca535ab992816
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2025-05-12 01:47:09 +0200

    [qodana] IJ-CR-162157 QD-11615 Minor refactorings
    (cherry picked from commit 4412ffffcbbdff84360b79690509b4af083b84ae)
    
    GitOrigin-RevId: 263ef8e8295613b7ffb8e084a00d87bbc640ac3a

--- qodana/jvm/java/src/org/jetbrains/qodana/jvm/java/workflow/JvmProjectStructureChecker.kt
+++ qodana/jvm/java/src/org/jetbrains/qodana/jvm/java/workflow/JvmProjectStructureChecker.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.qodana.jvm.java.workflow
 
 import com.intellij.codeInspection.InspectionsBundle
+import com.intellij.openapi.components.serviceAsync
 import com.intellij.openapi.module.ModuleType
 import com.intellij.openapi.project.modules
 import com.intellij.openapi.roots.JdkOrderEntry
@@ -11,38 +12,44 @@ import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.VirtualFile
 import org.jetbrains.qodana.QodanaBundle
 import org.jetbrains.qodana.staticAnalysis.inspections.runner.QodanaRunContext
-import org.jetbrains.qodana.staticAnalysis.sarif.notifications.addSanityNotification
+import org.jetbrains.qodana.staticAnalysis.sarif.notifications.RuntimeNotificationCollector
+import org.jetbrains.qodana.staticAnalysis.sarif.notifications.createSanityNotification
 import org.jetbrains.qodana.staticAnalysis.workflow.QodanaWorkflowExtension
 
 class JvmProjectStructureChecker : QodanaWorkflowExtension {
   override suspend fun beforeLaunch(context: QodanaRunContext) {
     val project = context.project
+    val notificationCollector = project.serviceAsync<RuntimeNotificationCollector>()
     for (module in project.modules) {
       val rootManager = ModuleRootManager.getInstance(module)
       val entries = rootManager.getOrderEntries()
       for (entry in entries) {
-        if (entry is JdkOrderEntry) {
-          if (!ModuleType.get(module).isValidSdk(module, null)) {
-            addSanityNotification(project, InspectionsBundle.message("offline.inspections.module.jdk.not.found", entry.getJdkName(),
-                                                                     module.getName()))
-
+        when {
+          entry is JdkOrderEntry && !ModuleType.get(module).isValidSdk(module, null) -> {
+            val message = InspectionsBundle.message("offline.inspections.module.jdk.not.found",
+                                                    entry.getJdkName(),
+                                                    module.getName()
+            )
+            notificationCollector.add(createSanityNotification(message))
           }
-        }
-        else if (entry is LibraryOrderEntry) {
-          val library = entry.getLibrary()
-          if (library == null) {
-            addSanityNotification(project, QodanaBundle.message("offline.inspections.library.was.not.resolved",
-                                                                entry.getPresentableName(), module.getName()))
-          }
-          else {
-            val detectedUrls = library.getFiles(OrderRootType.CLASSES).map { obj: VirtualFile -> obj.url }.toSet()
-            val declaredUrls = library.getUrls(OrderRootType.CLASSES).toMutableSet()
-            declaredUrls.removeAll(detectedUrls)
-            declaredUrls.removeIf { url: String -> library.isJarDirectory(url) }
-            if (!declaredUrls.isEmpty()) {
-              addSanityNotification(project, QodanaBundle.message("offline.inspections.library.urls.were.not.resolved",
-                                                                  StringUtil.join(declaredUrls, ", "),
-                                                                  entry.getPresentableName(), module.getName()))
+          entry is LibraryOrderEntry -> {
+            val library = entry.getLibrary()
+            if (library == null) {
+              val message = QodanaBundle.message("offline.inspections.library.was.not.resolved",
+                                                 entry.getPresentableName(), module.getName())
+              notificationCollector.add(createSanityNotification(message))
+            }
+            else {
+              val detectedUrls = library.getFiles(OrderRootType.CLASSES).map { obj: VirtualFile -> obj.url }.toSet()
+              val declaredUrls = library.getUrls(OrderRootType.CLASSES).toMutableSet()
+              declaredUrls.removeAll(detectedUrls)
+              declaredUrls.removeIf { url: String -> library.isJarDirectory(url) }
+              if (!declaredUrls.isEmpty()) {
+                val message = QodanaBundle.message("offline.inspections.library.urls.were.not.resolved",
+                                                   StringUtil.join(declaredUrls, ", "),
+                                                   entry.getPresentableName(), module.getName())
+                notificationCollector.add(createSanityNotification(message))
+              }
             }
           }
         }
