3b7f9c46816e9c17d59df5c6ccb4d0e4adc15ec0
Victor Turansky <victor.turansky@jetbrains.com>
2025-05-11 21:48:41 +0300

    [vue] Optimize imports in `VueThisInstanceCompletionProvider`
    GitOrigin-RevId: a6c085df7c6842c126378c8c4460dd99806e32a7

--- vuejs/src/org/jetbrains/vuejs/codeInsight/VueThisInstanceCompletionProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/VueThisInstanceCompletionProvider.kt
@@ -1,7 +1,9 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.vuejs.codeInsight
 
-import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.CompletionProvider
+import com.intellij.codeInsight.completion.CompletionResultSet
 import com.intellij.lang.javascript.completion.JSCompletionContributor
 import com.intellij.lang.javascript.completion.JSLookupPriority
 import com.intellij.lang.javascript.completion.JSLookupUtilImpl
