7fa32c46cc9c4966343454966197a42abc919ad5
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2025-05-12 01:47:09 +0200

    [qodana] IJ-CR-162157 QD-11615 Minor refactorings
    GitOrigin-RevId: 4412ffffcbbdff84360b79690509b4af083b84ae

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/sarif/notifications/notifications.kt
+++ qodana/core/src/org/jetbrains/qodana/staticAnalysis/sarif/notifications/notifications.kt
@@ -1,16 +1,10 @@
 package org.jetbrains.qodana.staticAnalysis.sarif.notifications
 
-import com.intellij.openapi.components.service
-import com.intellij.openapi.project.Project
 import com.jetbrains.qodana.sarif.model.Message
 import com.jetbrains.qodana.sarif.model.Notification
 import org.jetbrains.qodana.staticAnalysis.profile.SanityInspectionGroup.Companion.SANITY_FAILURE_NOTIFICATION
 import org.jetbrains.qodana.staticAnalysis.sarif.withKind
 import java.time.Instant
-
-fun addSanityNotification(project: Project, message: String) {
-  project.service<RuntimeNotificationCollector>().add(createSanityNotification(message))
-}
 
 fun createSanityNotification(message: String): Notification =
   Notification()
