3a3ed9b66ad318571629c1989e300e038b77010d
Sergei Vorobyov <sergey.vorobyov@jetbrains.com>
2025-05-14 16:33:43 +0200

    [Gradle|Exec] new: add advanced settings for disabling run delegation with enabled build delegation
    ### Issues
      * IDEA-359288 Cannot run Gradle compound run configurations because of fighting due to multiple daemons
      * IDEA-316623 Split Build and Run using Gradle option
    
    GitOrigin-RevId: fb39248705bb16e4859659dcc5ce21dc770e74fb

---
+++ cucumber-java/test/org/jetbrains/plugins/cucumber/java/run/CucumberGradleProjectTaskRunnerTest.kt
@@ -0,0 +1,33 @@
+// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.plugins.cucumber.java.run
+
+import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.use
+import org.gradle.util.GradleVersion
+import org.jetbrains.plugins.gradle.execution.build.GradleProjectTaskRunnerTestCase
+import org.jetbrains.plugins.gradle.testFramework.annotations.BaseGradleVersionSource
+import org.junit.jupiter.params.ParameterizedTest
+
+class CucumberGradleProjectTaskRunnerTest : GradleProjectTaskRunnerTestCase() {
+
+  @ParameterizedTest(name = "[{index}] {0} delegatedBuild={1}, delegatedRun={2}")
+  @BaseGradleVersionSource("""
+    true:true:   true:false,
+    true:false:  true:false,
+    false:true:  false:false,
+    false:false: false:false
+  """)
+  fun `test GradleProjectTaskRunner#canRun for CucumberJavaRunConfiguration`(
+    gradleVersion: GradleVersion,
+    delegatedBuild: Boolean, delegatedRun: Boolean,
+    shouldBuild: Boolean, shouldRun: Boolean,
+  ) {
+    testEmptyProject(gradleVersion) {
+      Disposer.newDisposable().use { testDisposable ->
+        val configurationType = CucumberJavaRunConfigurationType.getInstance()
+        setupGradleDelegationMode(delegatedBuild, delegatedRun, testDisposable)
+        assertGradleProjectTaskRunnerCanRun(configurationType, shouldBuild, shouldRun)
+      }
+    }
+  }
+}
