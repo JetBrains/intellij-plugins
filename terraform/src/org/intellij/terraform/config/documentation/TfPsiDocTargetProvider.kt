37d2fd35eed6f670bba6548595464553c983e2a7
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2025-06-02 17:41:27 +0300

    [terraform] IJPL-158005 Do not show documentation on 'custom_name' for resource and data blocks
    GitOrigin-RevId: 5a45862b8cd9d74a952ac8ec1f6b277ca249c56e

--- terraform/src/org/intellij/terraform/config/documentation/TfPsiDocTargetProvider.kt
+++ terraform/src/org/intellij/terraform/config/documentation/TfPsiDocTargetProvider.kt
@@ -1,13 +1,24 @@
-// Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.config.documentation
 
 import com.intellij.platform.backend.documentation.DocumentationTarget
 import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
 import com.intellij.psi.PsiElement
+import org.intellij.terraform.hcl.psi.HCLBlock
+import org.intellij.terraform.hcl.psi.HCLStringLiteral
+import org.intellij.terraform.hcl.psi.getNameElementUnquoted
 
 internal class TfPsiDocTargetProvider : TfBaseDocumentationProvider(), PsiDocumentationTargetProvider {
 
   override fun documentationTarget(element: PsiElement, originalElement: PsiElement?): DocumentationTarget? {
+    if (element is HCLBlock) {
+      val name = element.getNameElementUnquoted(2)
+      val blockName = originalElement?.parent as? HCLStringLiteral
+      if (name != null && name == blockName?.unquotedText) {
+        return null
+      }
+    }
+
     return computeDocumentationTarget(element)
   }
 
