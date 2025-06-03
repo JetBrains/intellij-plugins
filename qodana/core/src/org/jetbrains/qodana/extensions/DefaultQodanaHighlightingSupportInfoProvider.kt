7ef3da0cdc8233c30db02d55fb35c06e40d96fa5
Konstantin Nisht <konstantin.nisht@jetbrains.com>
2025-06-03 16:35:49 +0200

    [threading] IJPL-190260: Remove `blockingContext` #3
    GitOrigin-RevId: 6ee85633990898bf184ab769763dcc71f4cac878

--- qodana/core/src/org/jetbrains/qodana/extensions/DefaultQodanaHighlightingSupportInfoProvider.kt
+++ qodana/core/src/org/jetbrains/qodana/extensions/DefaultQodanaHighlightingSupportInfoProvider.kt
@@ -2,7 +2,6 @@ package org.jetbrains.qodana.extensions
 
 import com.intellij.codeHighlighting.Pass
 import com.intellij.codeInspection.ex.LocalInspectionToolWrapper
-import com.intellij.openapi.progress.blockingContext
 import com.intellij.openapi.project.Project
 import com.intellij.profile.codeInspection.InspectionProfileManager
 import kotlinx.coroutines.Dispatchers
@@ -11,11 +10,9 @@ import kotlinx.coroutines.withContext
 class DefaultQodanaHighlightingSupportInfoProvider : QodanaHighlightingSupportInfoProvider {
   override suspend fun getEnabledInspections(project: Project): Set<String> {
     return withContext(Dispatchers.Default) {
-      blockingContext {
-        InspectionProfileManager.getInstance(project).currentProfile.getAllEnabledInspectionTools(project)
-          .filter { it.isEnabled && it.tool is LocalInspectionToolWrapper }
-          .map { it.tool.shortName }.toSet()
-      }
+      InspectionProfileManager.getInstance(project).currentProfile.getAllEnabledInspectionTools(project)
+        .filter { it.isEnabled && it.tool is LocalInspectionToolWrapper }
+        .map { it.tool.shortName }.toSet()
     }
   }
 
