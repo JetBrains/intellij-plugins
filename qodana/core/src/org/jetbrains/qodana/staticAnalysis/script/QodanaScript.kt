7fabaa0f0b6904a53f4ade504b3d40c0fe3ce868
Dima Golovinov <dmitry.golovinov@jetbrains.com>
2025-06-15 13:46:48 +0200

    QD-11885: refactor QodanaRunner to allow any script substitute a run object
    (cherry picked from commit f8ac63ab5c98c588d7bc557fd9db9f49fcc059b6)
    
    IJ-MR-165821
    
    (cherry picked from commit 6e5c58438821f97999a083a291a7392f8a3660f6)
    
    
    (cherry picked from commit d490d4f3a7f7d7931e79f78a2fa3f46a1c6a67db)
    
    IJ-MR-169467
    
    GitOrigin-RevId: a62c77eb435044e73865baedc0ea08c646e349a9

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/script/QodanaScript.kt
+++ qodana/core/src/org/jetbrains/qodana/staticAnalysis/script/QodanaScript.kt
@@ -1,15 +1,9 @@
 package org.jetbrains.qodana.staticAnalysis.script
 
-import com.intellij.psi.PsiElement
 import com.jetbrains.qodana.sarif.model.SarifReport
-import kotlinx.coroutines.withContext
-import org.jetbrains.qodana.staticAnalysis.StaticAnalysisDispatchers
 import org.jetbrains.qodana.staticAnalysis.inspections.coverageData.CoverageStatisticsData
-import org.jetbrains.qodana.staticAnalysis.inspections.coverageData.precomputedCoverageFiles
 import org.jetbrains.qodana.staticAnalysis.inspections.runner.QodanaGlobalInspectionContext
-import org.jetbrains.qodana.staticAnalysis.inspections.runner.QodanaToolResultDatabase
 import org.jetbrains.qodana.staticAnalysis.profile.QodanaProfile
-import java.io.File
 import java.nio.file.Path
 
 interface QodanaScript {
@@ -20,29 +14,13 @@ data class QodanaScriptResult(
   val profileState: QodanaProfile.QodanaProfileState,
   val outputPath: Path,
   val coverageStats: CoverageStatisticsData?,
-  val coverageFiles: List<Path>,
-  val inspectionNames: Map<String, String>
 ) {
 
   companion object {
-    suspend fun create(ctx: QodanaGlobalInspectionContext) = QodanaScriptResult(
+    fun create(ctx: QodanaGlobalInspectionContext): QodanaScriptResult = QodanaScriptResult(
       profileState = ctx.profileState,
       outputPath = ctx.outputPath,
-      coverageStats = ctx.coverageStatisticsData,
-      coverageFiles = ctx.getUserData(precomputedCoverageFiles)
-        .orEmpty()
-        .map(File::toPath),
-      inspectionNames = withContext(StaticAnalysisDispatchers.IO) {
-        val nullPsiElement: PsiElement? = null
-
-        QodanaToolResultDatabase.open(ctx.outputPath).use {
-          it.selectTriggeredInspectionIds()
-            .use { stmt ->
-              stmt.executeQuery()
-                .associateWith { id -> ctx.effectiveProfile.getInspectionTool(id, nullPsiElement)?.displayName ?: id }
-            }
-        }
-      }
+      coverageStats = ctx.coverageStatisticsData
     )
   }
 }
