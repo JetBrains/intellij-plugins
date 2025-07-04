00bed130cb634b0cd7db82ac3ad14e45dd0135a4
Jan Papesch <jan.papesch@jetbrains.com>
2025-07-04 17:44:40 +0200

    [clion, serial-monitor] Migrated Serial Monitor plugin to Gradle Plugin 2.x
    IntelliJ Platform Gradle Plugin 1.x has been deprecated.
    Also changes the target IDE to CLion.
    
    GitOrigin-RevId: 85938e2a90d5862a02cbc77de3e2d5a5c9d1c4f6

--- serial-monitor/build.gradle.kts
+++ serial-monitor/build.gradle.kts
@@ -1,39 +1,55 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 apply(from = "../contrib-configuration/common.gradle.kts")
 
 plugins {
   id("java")
   id("org.jetbrains.kotlin.jvm")
-  id("org.jetbrains.intellij")
+  id("org.jetbrains.intellij.platform")
 }
 
-intellij {
-  pluginName.set("Serial Monitor")
-  version.set("LATEST-EAP-SNAPSHOT")
-  type.set("IU")
+repositories {
+  intellijPlatform {
+    defaultRepositories()
+    snapshots()
+  }
+}
+
+intellijPlatform {
+  pluginConfiguration {
+    name.set("Serial Monitor")
+  }
 }
 
 dependencies {
+  intellijPlatform {
+    jetbrainsRuntime()
+    clion("LATEST-EAP-SNAPSHOT", useInstaller = false)
+  }
   implementation ("io.github.java-native:jssc:2.9.4")  {
     exclude("org.slf4j", "slf4j-api")
   }
 }
 
-tasks {
-  compileKotlin {
-    kotlinOptions.jvmTarget = ext("kotlin.jvmTarget")
+kotlin {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.fromTarget(ext("kotlin.jvmTarget")))
     @Suppress("UNCHECKED_CAST")
-    kotlinOptions.freeCompilerArgs = rootProject.extensions["kotlin.freeCompilerArgs"] as List<String>
+    freeCompilerArgs.addAll(rootProject.extensions["kotlin.freeCompilerArgs"] as List<String>)
   }
+}
+
+tasks {
   java {
     sourceCompatibility = JavaVersion.toVersion(ext("java.sourceCompatibility"))
     targetCompatibility = JavaVersion.toVersion(ext("java.targetCompatibility"))
   }
   wrapper {
-    gradleVersion = "8.5"
+    gradleVersion = ext("gradle.version")
   }
   runIde {
-    autoReloadPlugins.set(false)
+     autoReload = false
   }
 }
 
