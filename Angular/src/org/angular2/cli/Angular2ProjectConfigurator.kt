3e43be48b451dfd00c3aa8f80b4bb4ff5555b9a1
Ilia Malakhov <ilia.malakhov@jetbrains.com>
2025-07-14 10:51:28 +0200

    [angular2] IJPL-188230 Use workspace model to register angular excludes to avoid using Module
    GitOrigin-RevId: 7a67779888b083363bd0a36939443f860bdc0589

--- Angular/src/org/angular2/cli/Angular2ProjectConfigurator.kt
+++ Angular/src/org/angular2/cli/Angular2ProjectConfigurator.kt
@@ -1,36 +1,43 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2.cli
 
-import com.intellij.ide.projectView.actions.MarkRootsManager
-import com.intellij.openapi.application.ApplicationManager
+import com.intellij.javascript.library.exclude.JsExcludeEntity
+import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ContentEntry
-import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.util.Ref
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.DirectoryProjectConfigurator
+import com.intellij.platform.backend.workspace.toVirtualFileUrl
+import com.intellij.platform.backend.workspace.workspaceModel
+import com.intellij.platform.workspace.storage.url.VirtualFileUrl
 
 /**
  * @author Dennis.Ushakov
  */
 class Angular2ProjectConfigurator : DirectoryProjectConfigurator {
   override fun configureProject(project: Project, baseDir: VirtualFile, moduleRef: Ref<Module>, isProjectCreatedWithWizard: Boolean) {
-    val module = moduleRef.get() ?: return
-
     val cliJson = AngularCliUtil.findCliJson(baseDir)
-    val model = ModuleRootManager.getInstance(module).modifiableModel
-    val entry = MarkRootsManager.findContentEntry(model, baseDir)
-    if (entry != null && cliJson != null) {
-      entry.addDefaultAngularExcludes(baseDir)
-      ApplicationManager.getApplication().runWriteAction { model.commit() }
-      project.save()
-      AngularCliUtil.createRunConfigurations(project, baseDir)
+    if (cliJson == null) return
+
+    val workspaceModel = project.workspaceModel
+    val virtualFileUrlManager = workspaceModel.getVirtualFileUrlManager()
+    val excludes = defaultAngularExcludes(baseDir.toVirtualFileUrl(virtualFileUrlManager))
+
+    WriteAction.run<Throwable> {
+      workspaceModel.updateProjectModel("create default angular excludes") { storage ->
+        for (exclude in excludes) {
+          storage.addEntity(JsExcludeEntity(exclude, JsExcludeEntity.MyEntitySource))
+        }
+      }
     }
-    else {
-      model.dispose()
-    }
+    AngularCliUtil.createRunConfigurations(project, baseDir)
   }
+}
+
+private fun defaultAngularExcludes(baseDir: VirtualFileUrl): List<VirtualFileUrl> {
+  return listOf(baseDir.append("dist"), baseDir.append("tmp"))
 }
 
 fun ContentEntry.addDefaultAngularExcludes(baseDir: VirtualFile) {
