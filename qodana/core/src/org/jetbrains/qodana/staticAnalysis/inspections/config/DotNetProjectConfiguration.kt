4820d9846bc7b0a74cfe0a567b7b356d3694e442
Andrei Iurko <andrei.iurko@jetbrains.com>
2025-07-17 19:07:56 +0300

    [qodana] QD-12151 add the option to skip files not analyzed by SWEA in dotnet
    GitOrigin-RevId: b5999ba8ad4d346b013df381320bd0ba5fc315a2

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/config/DotNetProjectConfiguration.kt
+++ qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/config/DotNetProjectConfiguration.kt
@@ -4,4 +4,5 @@ data class DotNetProjectConfiguration(val solution: String? = null,
                                       val project: String? = null,
                                       val configuration: String? = null,
                                       val platform: String? = null,
-                                      val frameworks: String? = null)
+                                      val frameworks: String? = null,
+                                      val fastAnalysisMode: Boolean = false)
