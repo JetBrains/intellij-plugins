8d83b7d887201ce33619126587342ce442435ee3
Jan Papesch <jan.papesch@jetbrains.com>
2025-07-18 15:22:03 +0200

    [clion, platformio] CPP-19425 Disable valgrind memcheck for PlatformIO
    GitOrigin-RevId: 91fbfe9999370dd436a1fa4855d5cfe5a6bef032

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/PlatformioProfileDisablers.kt
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/PlatformioProfileDisablers.kt
@@ -1,8 +1,13 @@
 package com.jetbrains.cidr.cpp.embedded.platformio
 
+import com.intellij.cidr.profiling.valgrind.ValgrindDisabler
 import com.intellij.execution.configurations.RunProfile
 import com.intellij.profiler.clion.CLionProfilerDisabler
 
 class PlatformioProfilerDisabler : CLionProfilerDisabler {
   override fun disableCLionProfilerFor(runConfiguration: RunProfile): Boolean = runConfiguration is PlatformioDebugConfiguration
+}
+
+class PlatformioValgrindDisabler : ValgrindDisabler {
+  override fun disableValgrindFor(runConfiguration: RunProfile): Boolean = runConfiguration is PlatformioDebugConfiguration
 }
