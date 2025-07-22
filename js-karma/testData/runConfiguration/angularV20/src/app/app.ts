0efb0c9801b0a6f12197ab21bddbce4cee9bb299
Dmitry Makhnev <dmitry.makhnev@jetbrains.com>
2025-07-22 20:43:41 +0200

    [Karma] WEB-73511: Fix Karma runner for Angular 20
    - Fix running Karma configuration for Angular 20
    - Add a test for this case
    - Extend KarmaRunConfigurationRunner.kt (in tests) to forward Karma server output in error cases
    - Add forwarding Karma server output for server starting error cases
    - Add forwarding Karma server output for failed run configuration tests
    - Update Node.js 22 LTS version to 22.17.0
    
    Merge-request: IJ-MR-169289
    Merged-by: Dmitry Makhnev <Dmitry.Makhnev@jetbrains.com>
    (cherry picked from commit 8b1d5407c49bb64957cafd2a45ec0175afcd074c)
    
    IJ-CR-162472
    
    GitOrigin-RevId: 4b7a58c72f9e83d2d63632f25435067f42a94c0a

---
+++ js-karma/testData/runConfiguration/angularV20/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected title = 'WEB-73511-angular-20-karma-sp';
+}
