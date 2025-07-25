bb70ede71437b7aa934d4de866325c3524f36eb7
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2025-07-25 12:45:38 +0200

    [angular] WEB-74046 fix deadlock from LS cache
    (cherry picked from commit fc8cfe53045b416688b27a5e10d720cf2dac65f0)
    
    IJ-CR-170426
    
    GitOrigin-RevId: ecebe96443ba6253d3e0bfef28ceebc2aa18c830

--- Angular/src/org/angular2/lang/expr/service/Angular2LanguageServiceCache.kt
+++ Angular/src/org/angular2/lang/expr/service/Angular2LanguageServiceCache.kt
@@ -1,64 +1,39 @@
 package org.angular2.lang.expr.service
 
-import com.intellij.lang.javascript.service.protocol.JSLanguageServiceCommand
-import com.intellij.lang.javascript.service.protocol.JSLanguageServiceObject
-import com.intellij.lang.javascript.service.protocol.JSLanguageServiceSimpleCommand
-import com.intellij.lang.typescript.compiler.languageService.protocol.JSLanguageServiceObjectWithStateUpdater
+import com.intellij.lang.javascript.service.JSLanguageServiceQueue
 import com.intellij.lang.typescript.compiler.languageService.protocol.TypeScriptLanguageServiceCache
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.text.StringHash
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.PsiManager
 import org.angular2.lang.expr.service.protocol.commands.Angular2TranspiledTemplateCommand
 import org.angular2.lang.expr.service.protocol.commands.toAngular2TranspiledTemplateRequestArgs
-import org.angular2.lang.expr.service.tcb.Angular2TranspiledDirectiveFileBuilder
 import org.angular2.lang.expr.service.tcb.Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile
 import java.util.concurrent.ConcurrentHashMap
 
 class Angular2LanguageServiceCache(project: Project) : TypeScriptLanguageServiceCache(project) {
-
   private val transpiledComponentCache: MutableMap<VirtualFile, TranspiledComponentInfo> = ConcurrentHashMap()
 
-  override fun tryGetServiceObjectWithoutStateUpdater(input: JSLanguageServiceCommand): JSLanguageServiceObjectWithStateUpdater? =
-    if (input is Angular2TranspiledTemplateCommand)
-      null
-    else
-      super.tryGetServiceObjectWithoutStateUpdater(input)
-
-  override fun getServiceObjectWithStateUpdater(input: JSLanguageServiceCommand): JSLanguageServiceObjectWithStateUpdater? =
-    if (input is Angular2TranspiledTemplateCommand)
-      getUpdateTemplateServiceObject(input)
-    else
-      super.getServiceObjectWithStateUpdater(input)
-
-
-  private fun getUpdateTemplateServiceObject(input: Angular2TranspiledTemplateCommand): JSLanguageServiceObjectWithStateUpdater? {
-    val templateFile = PsiManager.getInstance(myProject).findFile(input.templateFile)
-                       ?: return null
-    val componentFile = Angular2TranspiledDirectiveFileBuilder.findDirectiveFile(templateFile)
-    val componentVirtualFile = componentFile?.virtualFile ?: return null
-    val newContents = Angular2TranspiledDirectiveFileBuilder.getTranspiledDirectiveFile(componentFile)
-
+  suspend fun refreshAndCacheTranspiledTemplate(
+    process: JSLanguageServiceQueue,
+    componentVirtualFile: VirtualFile,
+    newContents: TranspiledDirectiveFile?,
+  ) {
     if (newContents == null) {
       transpiledComponentCache.remove(componentVirtualFile)
-      return null
+      return
     }
 
     val newInfo = TranspiledComponentInfo(newContents)
     val oldInfo = transpiledComponentCache[componentVirtualFile]
 
     if (oldInfo == newInfo) {
-      return null
+      return
     }
     val serviceObject = newContents.toAngular2TranspiledTemplateRequestArgs(myProject, componentVirtualFile)
-    return JSLanguageServiceObjectWithStateUpdater(
-      listOf(object : Angular2TranspiledTemplateCommand(componentVirtualFile), JSLanguageServiceSimpleCommand {
-        override fun toSerializableObject(): JSLanguageServiceObject = serviceObject
-      }),
-      listOf({
-               transpiledComponentCache[componentVirtualFile] = newInfo
-             })
-    )
+    val command = Angular2TranspiledTemplateCommand(serviceObject)
+    if (process.execute(command) != null) {
+      transpiledComponentCache[componentVirtualFile] = newInfo
+    }
   }
 
   private class TranspiledComponentInfo(contents: TranspiledDirectiveFile) {
