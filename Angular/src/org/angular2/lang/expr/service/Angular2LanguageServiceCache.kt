91ced620abf739d28b5307f1e182c95539333f3f
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2025-07-28 15:33:46 +0200

    [angular] WEB-74046 prevent concurrent update of transpiled components (IJ-CR-170426)
    (cherry picked from commit f981f4dbccb059725fad90846aaf3c88e0559e7a)
    
    IJ-CR-170426
    
    GitOrigin-RevId: d58495ce578392b6b7a829505c4a76070be7c710

--- Angular/src/org/angular2/lang/expr/service/Angular2LanguageServiceCache.kt
+++ Angular/src/org/angular2/lang/expr/service/Angular2LanguageServiceCache.kt
@@ -5,6 +5,8 @@ import com.intellij.lang.typescript.compiler.languageService.protocol.TypeScript
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.text.StringHash
 import com.intellij.openapi.vfs.VirtualFile
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import org.angular2.lang.expr.service.protocol.commands.Angular2TranspiledTemplateCommand
 import org.angular2.lang.expr.service.protocol.commands.toAngular2TranspiledTemplateRequestArgs
 import org.angular2.lang.expr.service.tcb.Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile
@@ -12,6 +14,7 @@ import java.util.concurrent.ConcurrentHashMap
 
 class Angular2LanguageServiceCache(project: Project) : TypeScriptLanguageServiceCache(project) {
   private val transpiledComponentCache: MutableMap<VirtualFile, TranspiledComponentInfo> = ConcurrentHashMap()
+  private val transpiledComponentCacheWriteLock: Mutex = Mutex()
 
   suspend fun refreshAndCacheTranspiledTemplate(
     process: JSLanguageServiceQueue,
@@ -19,20 +22,26 @@ class Angular2LanguageServiceCache(project: Project) : TypeScriptLanguageService
     newContents: TranspiledDirectiveFile?,
   ) {
     if (newContents == null) {
-      transpiledComponentCache.remove(componentVirtualFile)
+      transpiledComponentCacheWriteLock.withLock {
+        transpiledComponentCache.remove(componentVirtualFile)
+      }
       return
     }
 
     val newInfo = TranspiledComponentInfo(newContents)
-    val oldInfo = transpiledComponentCache[componentVirtualFile]
 
-    if (oldInfo == newInfo) {
+    if (transpiledComponentCache[componentVirtualFile] == newInfo) {
       return
     }
     val serviceObject = newContents.toAngular2TranspiledTemplateRequestArgs(myProject, componentVirtualFile)
     val command = Angular2TranspiledTemplateCommand(serviceObject)
-    if (process.execute(command) != null) {
-      transpiledComponentCache[componentVirtualFile] = newInfo
+    transpiledComponentCacheWriteLock.withLock {
+      if (transpiledComponentCache[componentVirtualFile] == newInfo) {
+        return
+      }
+      if (process.execute(command) != null) {
+        transpiledComponentCache[componentVirtualFile] = newInfo
+      }
     }
   }
 
