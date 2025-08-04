e723c4c89eb5fd615d043bc9be3e8efb6d9d616d
Vyacheslav Karpukhin <Vyacheslav.Karpukhin@jetbrains.com>
2025-08-04 20:55:44 +0200

    IJPL-197533 Move JS formatter to intellij.javascript.common
    GitOrigin-RevId: 62b975f9d92704a13af7de7acccb3227e52c1b70

--- flex/src/com/intellij/javascript/flex/ActionScriptHandlersFactory.java
+++ flex/src/com/intellij/javascript/flex/ActionScriptHandlersFactory.java
@@ -4,8 +4,6 @@ package com.intellij.javascript.flex;
 import com.intellij.javascript.flex.completion.ActionScriptCompletionKeywordsContributor;
 import com.intellij.lang.javascript.completion.JSCompletionKeywordsContributor;
 import com.intellij.lang.javascript.dialects.JSHandlersFactory;
-import com.intellij.lang.javascript.formatter.ECMA4CodeStyleSettings;
-import com.intellij.lang.javascript.formatter.JSCodeStyleSettings;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -16,10 +14,5 @@ public final class ActionScriptHandlersFactory extends JSHandlersFactory {
   @Override
   public @NotNull JSCompletionKeywordsContributor newCompletionKeywordsContributor() {
     return new ActionScriptCompletionKeywordsContributor();
-  }
-
-  @Override
-  public @NotNull Class<? extends JSCodeStyleSettings> getCodeStyleSettingsClass() {
-    return ECMA4CodeStyleSettings.class;
   }
 }
