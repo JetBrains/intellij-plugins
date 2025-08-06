9f7aefc234c52f93d847629f93054f3cd647e9be
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-08-06 16:14:31 +0200

    [angular] WEB-73437 Fix template syntax detection for injected expressions.
    GitOrigin-RevId: f15f3cd96f29e530600e790b557ad96de70c4304

--- Angular/testData/templateTranspiler/voidKeyword.ts
+++ Angular/testData/templateTranspiler/voidKeyword.ts
@@ -7,12 +7,19 @@ import {Component} from '@angular/core';
     {{void check(true)}}
     <div [title]="void check(true)"></div>
     <div title="foo-{{void check(true)}}"></div>
- `
+ `,
+  host: {
+    '(click)': 'void handleClick()',
+  }
 })
 export class RobotProfileComponent {
   value!: string | number
 
   check (value: boolean) {
 
+  }
+
+  handleClick() {
+    return false;
   }
 }
