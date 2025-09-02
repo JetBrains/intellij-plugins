2d819ca1f312ad2c6ff2364487af986bfe4a0cbc
Victor Turansky <victor.turansky@jetbrains.com>
2025-09-02 20:28:10 +0200

    [vue] WEB-74086 Vapor. Exclude `$slots` and `$props` properties from component templates
    GitOrigin-RevId: 3888c55217c4a7b49d4fa43f9d648ca5c87a9f9a

--- vuejs/src/org/jetbrains/vuejs/codeInsight/template/VueContainerScopeProvider.kt
+++ vuejs/src/org/jetbrains/vuejs/codeInsight/template/VueContainerScopeProvider.kt
@@ -4,9 +4,20 @@ package org.jetbrains.vuejs.codeInsight.template
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiElementResolveResult
 import com.intellij.psi.ResolveResult
+import org.jetbrains.vuejs.model.VueComponent
 import org.jetbrains.vuejs.model.VueEntitiesContainer
 import org.jetbrains.vuejs.model.VueModelManager
+import org.jetbrains.vuejs.model.source.INSTANCE_PROPS_PROP
+import org.jetbrains.vuejs.model.source.INSTANCE_SLOTS_PROP
 import java.util.function.Consumer
+
+/**
+ * [Details](https://github.com/vuejs/core/releases/tag/v3.6.0-alpha.1#about-vapor-mode:~:text=Implicit%20instance%20properties%20like%20%24slots%20and%20%24props%20are%20not%20available%20in%20Vapor%20template%20expressions)
+ */
+private val VAPOR_EXCLUDED_PROPERTIES = setOf(
+  INSTANCE_SLOTS_PROP,
+  INSTANCE_PROPS_PROP,
+)
 
 class VueContainerScopeProvider : VueTemplateScopesProvider() {
 
@@ -25,10 +36,16 @@ class VueContainerScopeProvider : VueTemplateScopesProvider() {
     override fun resolve(
       consumer: Consumer<in ResolveResult>,
     ) {
+      val excludedProperties = if (container is VueComponent && container.vapor) {
+        VAPOR_EXCLUDED_PROPERTIES
+      }
+      else emptySet()
+
       container.thisType
         .asRecordType()
         .properties
         .asSequence()
+        .filter { it.memberName !in excludedProperties }
         .mapNotNull { it.memberSource.singleElement }
         .map { PsiElementResolveResult(it, true) }
         .forEach { consumer.accept(it) }
