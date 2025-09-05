e76fd471463a4f534c1d0c040c2bc1c83e61e568
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2025-09-05 12:10:01 +0300

    [terraform] IJPL-65991 Adjusted Terragrunt extension and file type patterns
    GitOrigin-RevId: 51163416eeee8319332ddf503d336d5daed80e7d

--- terraform/src/org/intellij/terraform/hil/HILFileType.kt
+++ terraform/src/org/intellij/terraform/hil/HILFileType.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.hil
 
 import com.intellij.openapi.fileTypes.LanguageFileType
@@ -25,5 +25,5 @@ object HILFileType : LanguageFileType(HILLanguage) {
     return "HIL"
   }
 
-  override fun toString() = name
+  override fun toString(): String = name
 }
