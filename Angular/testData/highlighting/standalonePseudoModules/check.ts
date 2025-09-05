e706483264efb2184df1705a3c95775b11c51a09
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-09-05 13:13:49 +0200

    [angular] WEB-73325 "Component is never used in a component template" when not all imports from array are used
    GitOrigin-RevId: 58b3f0f4d821e2f205cb04bd86888659e2e53c8d

--- Angular/testData/highlighting/standalonePseudoModules/check.ts
+++ Angular/testData/highlighting/standalonePseudoModules/check.ts
@@ -1,15 +1,25 @@
 import {Component} from '@angular/core';
 import {IGX_DIALOG_DIRECTIVES} from "igniteui-angular";
-import {IgxAccordionComponent, IgxExpansionPanelComponent, IgxDropDownComponent, IgxDropDownItemComponent} from "igniteui-angular"
+import {IgxAccordionComponent, IgxExpansionPanelComponent, IgxDropDownComponent, IgxDropDownItemComponent, IgxExpansionPanelIconDirective} from "igniteui-angular"
 
 export const IGX_DROP_DOWN_DIRECTIVES = [IgxDropDownComponent, IgxDropDownItemComponent];
 
 const IGX_ACCORDION_DIRECTIVES = [IgxAccordionComponent, IgxExpansionPanelComponent] as const;
 
+const IGX_ACCORDION_DIRECTIVES_UNUSED = [IgxExpansionPanelIconDirective, IgxExpansionPanelComponent] as const;
+
+export const IGX_ACCORDION_DIRECTIVES_UNUSED_MODULE = [IgxExpansionPanelIconDirective, IgxExpansionPanelComponent];
+
 @Component({
    selector: 'app-root',
    standalone: true,
-   imports: [IGX_DIALOG_DIRECTIVES, <error descr="Component IgxExpansionPanelComponent is never used in a component template">IGX_ACCORDION_DIRECTIVES</error>, IGX_DROP_DOWN_DIRECTIVES],
+   imports: [
+     IGX_DIALOG_DIRECTIVES,
+     IGX_DROP_DOWN_DIRECTIVES,
+     <error descr="None of the declarations provided by IGX_ACCORDION_DIRECTIVES_UNUSED_MODULE are used in a component template">IGX_ACCORDION_DIRECTIVES_UNUSED_MODULE</error>,
+     IGX_ACCORDION_DIRECTIVES,
+     IGX_ACCORDION_DIRECTIVES_UNUSED,
+   ],
    template: `
     <igx-dialog></igx-dialog>
     <igx-accordion></igx-accordion>
