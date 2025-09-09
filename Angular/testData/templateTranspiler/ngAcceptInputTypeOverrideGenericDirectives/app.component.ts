8d7c97da7dc5eec597a7e9eddbbb7cbb7e8317f4
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-09-09 09:31:32 +0200

    [angular] WEB-72984 Error with boolean inputs with generic directive
    (cherry picked from commit 05101c4f7ef89c5152d6337422862f4dc72031f2)
    
    IJ-CR-175048
    
    GitOrigin-RevId: f6410cd9b6b33e85e158bf431e6ce612852b7fcf

---
+++ Angular/testData/templateTranspiler/ngAcceptInputTypeOverrideGenericDirectives/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+import {SbbDateInput} from './date-input';
+
+@Component({
+  selector: 'app-root',
+  imports: [SbbDateInput],
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+}
