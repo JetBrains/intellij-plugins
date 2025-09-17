5d9eea31f40e81a8a3ada5f1e37e7329c09096cc
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-09-17 10:15:43 +0200

    [angular] WEB-74883 Issue with type guard inside event listener inside nested @ifs
    GitOrigin-RevId: 47b1152d5cd26429176bb8db101e5186522cb450

---
+++ Angular/testData/highlighting/listenerInNestedIfBlocks.ts
@@ -0,0 +1,45 @@
+import {Component, TemplateRef} from '@angular/core';
+
+enum MediaCampo {
+  PRINT,
+  <warning descr="Unused readonly field SCREEN">SCREEN</warning>,
+}
+
+interface Campo {
+  label: string;
+  value: string | number | null | string[] | number[] | CampoTemplate;
+  fullWidth?: boolean;
+  link?: ($event: UIEvent) => void;
+  media?: MediaCampo;
+}
+
+interface CampoTemplate {
+  value: any;
+  template: TemplateRef<any>;
+  media?: MediaCampo;
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    @for (item of items; track item) {
+      @if (item.media === undefined || item.media === media) {
+        @if (!isTemplate(item) && item.link && item.value) {
+          <a (click)="item.link($event)">{{ item.value }}</a>
+        }
+      }
+    }
+  `,
+  styleUrl: './app.css'
+})
+export class App {
+  <warning descr="Unused field title">title</warning> = 'idea-undefined-ts-bug';
+
+  media: MediaCampo = MediaCampo.PRINT;
+
+  items: Campo[] = [];
+
+  isTemplate(item: any): item is CampoTemplate {
+    return item && item.template instanceof TemplateRef;
+  }
+}
