70a83b6adebbbf286e46a7f45447245629f2e2b1
Evgenii Im <evgenii.im@jetbrains.com>
2025-09-22 18:22:23 +0200

    [qodana] QD-12443 Resolve scope only for kotlin and kotlin binaries
    (cherry picked from commit ddcbe0c5a5f8c7b97977b48194a36f81f80bec95)
    
    IJ-MR-176262
    
    GitOrigin-RevId: d73f98fe387511811c728263ae830311490fa17e

--- qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/InspectionKtsResolveScopeProvider.kt
+++ qodana/inspectionKts/kotlin/src/org/jetbrains/qodana/inspectionKts/kotlin/script/InspectionKtsResolveScopeProvider.kt
@@ -4,11 +4,15 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.ResolveScopeProvider
 import com.intellij.psi.search.GlobalSearchScope
+import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.idea.base.projectStructure.isKotlinBinary
 import org.jetbrains.qodana.registry.QodanaRegistry
 
 // QD-8717 FlexInspect (.inspection.kts): unresolved Kotlin PSI APIs if the Kotlin is present in the gradle kts project
 internal class InspectionKtsResolveScopeProvider : ResolveScopeProvider() {
   override fun getResolveScope(file: VirtualFile, project: Project): GlobalSearchScope? {
+    val fileType = file.fileType
+    if (!fileType.isKotlinBinary && fileType != KotlinFileType.INSTANCE) return null
     val classpathProvider = inspectionKtsClasspathProvider(project, doInitialize = false)
     val currentDependenciesScope = classpathProvider.currentDependenciesScope()
 
