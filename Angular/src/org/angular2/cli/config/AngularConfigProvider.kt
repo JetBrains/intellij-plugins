5f3f263513cb474554751467681fd1d29f340acc
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-09-23 09:57:02 +0200

    [angular] WEB-74893 Add cache in AngularConfigProvider.findAngularProject - searching for projects may be costly in Nx workspaces
    GitOrigin-RevId: 8eaa52f1e98ee53bd01a871fe3e49d461ecec89c

--- Angular/src/org/angular2/cli/config/AngularConfigProvider.kt
+++ Angular/src/org/angular2/cli/config/AngularConfigProvider.kt
@@ -4,6 +4,12 @@ package org.angular2.cli.config
 import com.intellij.openapi.extensions.ExtensionPointName
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.vfs.VirtualFileManager
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.CachedValueProvider
+import com.intellij.psi.util.CachedValuesManager
+import com.intellij.psi.util.PsiModificationTracker
+import com.intellij.util.containers.ContainerUtil
 
 interface AngularConfigProvider {
 
@@ -14,7 +20,15 @@ interface AngularConfigProvider {
     private val EP_NAME = ExtensionPointName.create<AngularConfigProvider>("org.angular2.configProvider")
 
     fun findAngularProject(project: Project, context: VirtualFile): AngularProject? {
-      return findAngularConfig(project, context)?.getProject(context)
+      val config = findAngularConfig(project, context) ?: return null
+      val configFile = PsiManager.getInstance(project).findFile(config.file)
+                       ?: return config.getProject(context)
+      return CachedValuesManager.getCachedValue(configFile) {
+        CachedValueProvider.Result.create(ContainerUtil.createConcurrentWeakMap<VirtualFile, AngularProject>(),
+                                          configFile, VirtualFileManager.VFS_STRUCTURE_MODIFICATIONS)
+      }.computeIfAbsent(context) {
+        config.getProject(it)
+      }
     }
 
     fun findAngularConfig(project: Project, context: VirtualFile): AngularConfig? =
