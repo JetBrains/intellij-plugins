7172b45f83e5d69881b37a9556a8da50ddf5cfff
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-09-26 01:18:42 +0200

    [cucumber] StepDefinitionCreator: reformat
    GitOrigin-RevId: 3a5a82320d95938467ed2fbfd2616da263352cd3

--- cucumber/src/org/jetbrains/plugins/cucumber/StepDefinitionCreator.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/StepDefinitionCreator.java
@@ -11,46 +11,47 @@ import org.jetbrains.plugins.cucumber.psi.GherkinStep;
 public interface StepDefinitionCreator {
   /**
    * Creates step definition file
-   * @param dir where to create file
+   *
+   * @param dir  where to create file
    * @param name of created file
    * @return PsiFile object of created file
    */
-  @NotNull
-  PsiFile createStepDefinitionContainer(final @NotNull PsiDirectory dir, final @NotNull String name);
+  @NotNull PsiFile createStepDefinitionContainer(@NotNull PsiDirectory dir, @NotNull String name);
 
   /**
    * Creates step definition
-   * @param step to implement
-   * @param file where to create step definition
+   *
+   * @param step         to implement
+   * @param file         where to create step definition
    * @param withTemplate should or not run template builder around regex and step body. We should not force user to go through
    *                     number of templates in case of "Create All Step Definitions" action invoked
    * @return true if success, false otherwise
    */
-  default boolean createStepDefinition(final @NotNull GherkinStep step, final @NotNull PsiFile file, boolean withTemplate) {
+  default boolean createStepDefinition(@NotNull GherkinStep step, @NotNull PsiFile file, boolean withTemplate) {
     return false;
   }
 
   /**
    * Validates name of new step definition file
+   *
    * @param fileName name of file to check
    * @return true if name is valid, false otherwise
    */
-  default boolean validateNewStepDefinitionFileName(final @NotNull Project project, final @NotNull String fileName) {
+  default boolean validateNewStepDefinitionFileName(@NotNull Project project, @NotNull String fileName) {
     return true;
   }
 
-  @NotNull
-  String getDefaultStepDefinitionFolderPath(final @NotNull GherkinStep step);
+  @NotNull String getDefaultStepDefinitionFolderPath(@NotNull GherkinStep step);
 
   /**
    * @return step definition file path relative to step definition folder
    */
   @NlsSafe
-  @NotNull
-  String getStepDefinitionFilePath(final @NotNull PsiFile file);
+  @NotNull String getStepDefinitionFilePath(@NotNull PsiFile file);
 
   /**
    * Provides default name of step definition file
+   *
    * @param step step we want to create definition container for
    * @return String representing default name of step definition file
    */
