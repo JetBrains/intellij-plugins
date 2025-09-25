c1bd8b03a36642e6be5365cef9288ae730629bcc
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-09-25 23:25:22 +0200

    [cucumber] BDDFrameworkType: convert to record
    GitOrigin-RevId: ae4b9083e67cfc93682b037b0229daef15328dea

--- cucumber/src/org/jetbrains/plugins/cucumber/BDDFrameworkType.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/BDDFrameworkType.java
@@ -5,72 +5,23 @@ import com.intellij.openapi.fileTypes.FileType;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
-
 /**
  * Type of BDD framework. Cucumber, behave etc
  *
+ * @param fileType       file type to be used as step definitions for this framework
+ * @param additionalInfo additional information about this framework to be displayed to user (when filetype is not enough)
  * @author Ilya.Kazakevich
  */
-public class BDDFrameworkType {
-  private final @NotNull FileType myFileType;
-  private final @Nullable String myAdditionalInfo;
-
-  /**
-   * @param fileType file type to be used as step definitions for this framework
-   */
-  public BDDFrameworkType(final @NotNull FileType fileType) {
+public record BDDFrameworkType(@NotNull FileType fileType, @Nullable String additionalInfo) {
+  public BDDFrameworkType(@NotNull FileType fileType) {
     this(fileType, null);
   }
 
-  /**
-   * @param fileType       file type to be used as step definitions for this framework
-   * @param additionalInfo additional information about this framework to be displayed to user (when filetype is not enough)
-   */
-  public BDDFrameworkType(final @NotNull FileType fileType,
-                          final @Nullable String additionalInfo) {
-    myFileType = fileType;
-    myAdditionalInfo = additionalInfo;
-  }
-
-
-  /**
-   * @return file type to be used as step definitions for this framework
-   */
   public @NotNull FileType getFileType() {
-    return myFileType;
+    return fileType;
   }
 
-  /**
-   * @return additional information about this framework to be displayed to user (when filetype is not enough)
-   */
   public @Nullable String getAdditionalInfo() {
-    return myAdditionalInfo;
-  }
-
-  @Override
-  public String toString() {
-    return "BDDFrameworkType{" +
-           "myFileType=" + myFileType +
-           ", myAdditionalInfo='" + myAdditionalInfo + '\'' +
-           '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (!(o instanceof BDDFrameworkType type)) return false;
-
-    if (!Objects.equals(myAdditionalInfo, type.myAdditionalInfo)) return false;
-    if (!myFileType.equals(type.myFileType)) return false;
-
-    return true;
-  }
-
-  @Override
-  public int hashCode() {
-    int result = myFileType.hashCode();
-    result = 31 * result + (myAdditionalInfo != null ? myAdditionalInfo.hashCode() : 0);
-    return result;
+    return additionalInfo;
   }
 }
