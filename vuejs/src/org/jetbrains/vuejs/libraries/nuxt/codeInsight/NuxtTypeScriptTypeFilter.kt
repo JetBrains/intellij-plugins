858ba1ea3a5fdeed0e5a81b92cb3a3ca86de5d07
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2025-09-28 21:38:29 +0200

    [js] WEB-74985 fix TypeScriptCompilerTypePatcher api
    GitOrigin-RevId: 325aa09b12d0f13b96ae8d64ab8c8eae13b156d8

--- vuejs/src/org/jetbrains/vuejs/libraries/nuxt/codeInsight/NuxtTypeScriptTypeFilter.kt
+++ vuejs/src/org/jetbrains/vuejs/libraries/nuxt/codeInsight/NuxtTypeScriptTypeFilter.kt
@@ -1,30 +1,27 @@
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.libraries.nuxt.codeInsight
 
+import com.intellij.javascript.types.TSObjectType
+import com.intellij.javascript.types.TSType
 import com.intellij.lang.ecmascript6.psi.ES6ImportCall
-import com.intellij.lang.javascript.psi.JSType
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptIndexedAccessType
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptSingleType
 import com.intellij.lang.javascript.psi.types.JSAnyType
 import com.intellij.lang.typescript.psi.TypeScriptPsiUtil
-import com.intellij.lang.typescript.resolve.TypeScriptCompilerTypePatcher
-import com.intellij.lang.typescript.tsc.types.TypeScriptCompilerObjectTypeImpl
+import com.intellij.lang.typescript.resolve.TypeScriptCompilerTypeFilter
 import com.intellij.openapi.util.text.StringUtil
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.PsiTreeUtil
 import org.jetbrains.vuejs.context.hasNuxt
 import org.jetbrains.vuejs.index.VUE_FILE_EXTENSION
 
-class NuxtTypeScriptTypePatcher : TypeScriptCompilerTypePatcher {
-  override fun adjustTypeFromServer(type: JSType, element: PsiElement): JSType? {
-    if (isAdjustableType(type) && isNuxtComponentGlobalDeclaration(element) && hasNuxt(element)) {
-      return null
-    }
-    return type
-  }
+class NuxtTypeScriptTypeFilter : TypeScriptCompilerTypeFilter {
+  override fun isAcceptableType(type: TSType, element: PsiElement): Boolean =
+    !(isAdjustableType(type) && isNuxtComponentGlobalDeclaration(element) && hasNuxt(element))
 
-  private fun isAdjustableType(type: JSType): Boolean =
-    if (type is TypeScriptCompilerObjectTypeImpl)
-      isVueFilePath(type.tscType.value.symbol?.escapedName)
+  private fun isAdjustableType(type: TSType): Boolean =
+    if (type is TSObjectType)
+      isVueFilePath(type.symbol?.escapedName)
     else
       type is JSAnyType
 
