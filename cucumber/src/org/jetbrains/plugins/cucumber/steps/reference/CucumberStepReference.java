62d4c2c17631f378161146cca7cf343194d990f3
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-09-30 00:39:20 +0200

    [cucumber] CucumberStepReference: annotate with `@NotNullByDefault`
    GitOrigin-RevId: e79aca92e82cbedcb6f369df056bdb99836363c5

--- cucumber/src/org/jetbrains/plugins/cucumber/steps/reference/CucumberStepReference.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/steps/reference/CucumberStepReference.java
@@ -11,61 +11,58 @@ import com.intellij.psi.PsiPolyVariantReference;
 import com.intellij.psi.ResolveResult;
 import com.intellij.psi.impl.source.resolve.ResolveCache;
 import com.intellij.util.IncorrectOperationException;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.CucumberJvmExtensionPoint;
 import org.jetbrains.plugins.cucumber.psi.impl.GherkinStepImpl;
 import org.jetbrains.plugins.cucumber.steps.AbstractStepDefinition;
 import org.jetbrains.plugins.cucumber.steps.CucumberStepHelper;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.*;
 
+@NotNullByDefault
 public class CucumberStepReference implements PsiPolyVariantReference {
-  private final PsiElement myStep;
-  private final TextRange myRange;
+  private final PsiElement step;
+  private final TextRange range;
 
   public CucumberStepReference(PsiElement step, TextRange range) {
-    myStep = step;
-    myRange = range;
+    this.step = step;
+    this.range = range;
   }
 
   @Override
-  public @NotNull PsiElement getElement() {
-    return myStep;
+  public PsiElement getElement() {
+    return step;
   }
 
   @Override
-  public @NotNull TextRange getRangeInElement() {
-    return myRange;
+  public TextRange getRangeInElement() {
+    return range;
   }
 
   @Override
-  public PsiElement resolve() {
+  public @Nullable PsiElement resolve() {
     final ResolveResult[] result = multiResolve(true);
     return result.length == 1 ? result[0].getElement() : null;
   }
 
   @Override
-  public @NotNull String getCanonicalText() {
-    return myStep.getText();
+  public String getCanonicalText() {
+    return step.getText();
   }
 
   @Override
-  public PsiElement handleElementRename(@NotNull String newElementName) throws IncorrectOperationException {
-    return myStep;
+  public PsiElement handleElementRename(String newElementName) throws IncorrectOperationException {
+    return step;
   }
 
   @Override
-  public PsiElement bindToElement(@NotNull PsiElement element) throws IncorrectOperationException {
-    return myStep;
+  public PsiElement bindToElement(PsiElement element) throws IncorrectOperationException {
+    return step;
   }
 
   @Override
-  public boolean isReferenceTo(@NotNull PsiElement element) {
+  public boolean isReferenceTo(PsiElement element) {
     ResolveResult[] resolvedResults = multiResolve(false);
     for (ResolveResult rr : resolvedResults) {
       if (getElement().getManager().areElementsEquivalent(rr.getElement(), element)) {
@@ -81,31 +78,36 @@ public class CucumberStepReference implements PsiPolyVariantReference {
   }
 
   @Override
-  public ResolveResult @NotNull [] multiResolve(boolean incompleteCode) {
-    Project project = getElement().getProject();
+  public ResolveResult[] multiResolve(boolean incompleteCode) {
+    final Project project = getElement().getProject();
     return ResolveCache.getInstance(project).resolveWithCaching(this, MyResolver.INSTANCE, false, incompleteCode);
   }
 
   private ResolveResult[] multiResolveInner() {
-    final Module module = ModuleUtilCore.findModuleForPsiElement(myStep);
+    final Module module = ModuleUtilCore.findModuleForPsiElement(step);
     if (module == null) {
       return ResolveResult.EMPTY_ARRAY;
     }
 
-    List<CucumberJvmExtensionPoint> frameworks = CucumberJvmExtensionPoint.EP_NAME.getExtensionList();
-    Collection<String> stepVariants =
-      frameworks.stream().map(e -> e.getStepName(myStep)).filter(Objects::nonNull).collect(Collectors.toSet());
+    final List<CucumberJvmExtensionPoint> extensionPoints = CucumberJvmExtensionPoint.EP_NAME.getExtensionList();
+    final Set<String> stepVariants = new HashSet<>();
+    for (final CucumberJvmExtensionPoint ep : extensionPoints) {
+      final String stepName = ep.getStepName(step);
+      if (stepName != null) {
+        stepVariants.add(stepName);
+      }
+    }
     if (stepVariants.isEmpty()) {
       return ResolveResult.EMPTY_ARRAY;
     }
 
-    final List<AbstractStepDefinition> stepDefinitions = CucumberStepHelper.loadStepsFor(myStep.getContainingFile(), module);
+    final List<AbstractStepDefinition> stepDefinitions = CucumberStepHelper.loadStepsFor(step.getContainingFile(), module);
 
-    List<PsiElement> resolvedElements = new ArrayList<>();
+    final List<PsiElement> resolvedElements = new ArrayList<>();
     for (final AbstractStepDefinition stepDefinition : stepDefinitions) {
-      if (stepDefinition.supportsStep(myStep)) {
+      if (stepDefinition.supportsStep(step)) {
         for (String stepVariant : stepVariants) {
-          PsiElement element = stepDefinition.getElement();
+          final PsiElement element = stepDefinition.getElement();
           if (stepDefinition.matches(stepVariant) && element != null && !resolvedElements.contains(element)) {
             resolvedElements.add(element);
             break;
@@ -114,33 +116,23 @@ public class CucumberStepReference implements PsiPolyVariantReference {
       }
     }
 
-    return resolvedElements.stream()
-      .map(PsiElementResolveResult::new)
-      .toArray(ResolveResult[]::new);
+    return resolvedElements.stream().map(PsiElementResolveResult::new).toArray(ResolveResult[]::new);
   }
 
-  /**
-   * @return first definition (if any) or null if no definition found
-   * @see #resolveToDefinitions()
-   */
   public @Nullable AbstractStepDefinition resolveToDefinition() {
     final Collection<AbstractStepDefinition> definitions = resolveToDefinitions();
     return (definitions.isEmpty() ? null : definitions.iterator().next());
   }
 
-  /**
-   * @return step definitions
-   * @see #resolveToDefinition()
-   */
-  public @NotNull Collection<AbstractStepDefinition> resolveToDefinitions() {
-    return CucumberStepHelper.findStepDefinitions(myStep.getContainingFile(), ((GherkinStepImpl)myStep));
+  public Collection<AbstractStepDefinition> resolveToDefinitions() {
+    return CucumberStepHelper.findStepDefinitions(step.getContainingFile(), ((GherkinStepImpl)step));
   }
 
   private static class MyResolver implements ResolveCache.PolyVariantResolver<CucumberStepReference> {
     private static final MyResolver INSTANCE = new MyResolver();
 
     @Override
-    public ResolveResult @NotNull [] resolve(@NotNull CucumberStepReference ref, boolean incompleteCode) {
+    public ResolveResult[] resolve(CucumberStepReference ref, boolean incompleteCode) {
       return ref.multiResolveInner();
     }
   }
