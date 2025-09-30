e31e41a4e225991a4f480a960c5b8cd9b8c031f9
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-09-30 18:20:24 +0200

    [cucumber-java] CucumberReferenceContributor: annotate with `@NotNullByDefault`
    GitOrigin-RevId: c467800b08a0d536e76d325de0b33a3b90b3e75b

--- cucumber/src/org/jetbrains/plugins/cucumber/steps/reference/CucumberReferenceContributor.java
+++ cucumber/src/org/jetbrains/plugins/cucumber/steps/reference/CucumberReferenceContributor.java
@@ -4,14 +4,13 @@ package org.jetbrains.plugins.cucumber.steps.reference;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.PsiReferenceContributor;
 import com.intellij.psi.PsiReferenceRegistrar;
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.NotNullByDefault;
 import org.jetbrains.plugins.cucumber.psi.impl.GherkinStepImpl;
 
+@NotNullByDefault
 public final class CucumberReferenceContributor extends PsiReferenceContributor {
   @Override
-  public void registerReferenceProviders(final @NotNull PsiReferenceRegistrar registrar) {
-    registrar.registerReferenceProvider(PlatformPatterns.psiElement(GherkinStepImpl.class),
-                                        new CucumberStepReferenceProvider());
-
+  public void registerReferenceProviders(PsiReferenceRegistrar registrar) {
+    registrar.registerReferenceProvider(PlatformPatterns.psiElement(GherkinStepImpl.class), new CucumberStepReferenceProvider());
   }
 }
