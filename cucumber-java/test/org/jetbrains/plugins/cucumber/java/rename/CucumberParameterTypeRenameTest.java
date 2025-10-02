7c810eda4cc5ee67663adc3ac763339d52311df3
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-10-02 21:28:49 +0200

    [cucumber-java] CucumberParameterTypeRenameTest: refactor to use the before/after pattern
    Align CucumberParameterTypeRenameTest with CucumberStepRenameTest.
    This approach is more reliable and easier to understand.
    Also switch to use `CodeInsightTestFixture#renameElementAtCaretUsingHandler`.
    
    GitOrigin-RevId: 20454422509bb5f2a2e8b2e7d8d5058a4c2b1349

--- cucumber-java/test/org/jetbrains/plugins/cucumber/java/rename/CucumberParameterTypeRenameTest.java
+++ cucumber-java/test/org/jetbrains/plugins/cucumber/java/rename/CucumberParameterTypeRenameTest.java
@@ -1,23 +1,23 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 package org.jetbrains.plugins.cucumber.java.rename;
 
-import com.intellij.psi.PsiElement;
 import com.intellij.testFramework.LightProjectDescriptor;
 import org.jetbrains.plugins.cucumber.CucumberCodeInsightTestCase;
 import org.jetbrains.plugins.cucumber.java.CucumberJavaTestUtil;
 
 public class CucumberParameterTypeRenameTest extends CucumberCodeInsightTestCase {
+  private void doTest(String newName) {
+    myFixture.copyDirectoryToProject(getTestName(true) + "/before", "");
+    myFixture.configureByFiles("Steps.java");
+    myFixture.testHighlighting("Steps.java");
+
+    myFixture.renameElementAtCaretUsingHandler(newName);
+
+    myFixture.checkResultByFile("Steps.java", getTestName(true) + "/after/Steps.java", false);
+  }
+
   public void testParameterTypeRename() {
-    myFixture.copyDirectoryToProject("", "");
-    myFixture.configureFromExistingVirtualFile(myFixture.findFileInTempDir("ParameterTypeSteps.java"));
-
-    assertTrue("File should contain definition and usages of ParameterType 'iso-date'", myFixture.getFile().getText().contains("iso-date"));
-
-    final int offset = findOffsetBySignature("today is {iso-d<caret>ate}");
-    PsiElement parameterType = myFixture.getFile().findReferenceAt(offset).resolve();
-
-    myFixture.renameElement(parameterType, "new-iso-value");
-    assertFalse("ParameterType and all its references should be renamed", myFixture.getFile().getText().contains("iso-date"));
+    doTest("new-iso-value");
   }
 
   @Override
---
+++ cucumber-java/testData/renameParameterType/parameterTypeRename/after/Steps.java
@@ -0,0 +1,23 @@
+package example;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.TypeRegistry;
+import io.cucumber.cucumberexpressions.ParameterType;
+
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Steps {
+  @Given("today is {new-iso-value}")
+  public void step_method(Date arg1) throws Throwable {
+  }
+
+  public void configureTypeRegistry(TypeRegistry typeRegistry) {
+    typeRegistry.defineParameterType(new ParameterType<>(
+            "new-iso-value",
+      "\\d{4}-\\d{2}-\\d{2}",
+      Date.class,
+      (String s) -> new SimpleDateFormat("yyyy-mm-dd").parse(s)
+    ));
+  }
+}
