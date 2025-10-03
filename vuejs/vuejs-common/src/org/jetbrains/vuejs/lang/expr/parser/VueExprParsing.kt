1b7abc4f3bf3bc3474c848657cf531182bfee6b4
Arsenii Iusufov <arsenii.iusufov@jetbrains.com>
2025-10-03 20:58:49 +0200

    WEB-74979 Move `VueExprParsing` to `intellij.vuejs.common`
    GitOrigin-RevId: dd94d0109259f916cc719e608c377457d2baf9fa

--- vuejs/vuejs-common/src/org/jetbrains/vuejs/lang/expr/parser/VueExprParsing.kt
+++ vuejs/vuejs-common/src/org/jetbrains/vuejs/lang/expr/parser/VueExprParsing.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.expr.parser
 
 import com.intellij.lang.ASTNode
@@ -41,7 +41,7 @@ object VueExprParsing {
                   ""
               }
               .let {
-                VueAttributeNameParser.parse(it)
+                VueAttributeNameParser.Companion.parse(it)
               }
             createExprParser(builder).parseEmbeddedExpression(root, info)
           }
