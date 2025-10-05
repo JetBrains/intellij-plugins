6da7ff88faf805e28632aee1190234bd5c0eed20
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2025-10-05 04:01:50 +0200

    [js] WEB-75116 reduce usages of AnnotationHolder in JSSemanticHighlightingVisitor
    GitOrigin-RevId: e79d168350f80c4b63eea85433f8425703f5ca80

--- flex/src/com/intellij/lang/actionscript/highlighting/ActionScriptSemanticHighlightingUtil.java
+++ flex/src/com/intellij/lang/actionscript/highlighting/ActionScriptSemanticHighlightingUtil.java
@@ -1,14 +1,20 @@
-// Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.lang.actionscript.highlighting;
 
+import com.intellij.codeInsight.daemon.impl.HighlightInfoType;
+import com.intellij.lang.annotation.AnnotationBuilder;
 import com.intellij.lang.annotation.AnnotationHolder;
-import com.intellij.lang.javascript.highlighting.JSSemanticHighlightingVisitor;
 import com.intellij.lang.javascript.psi.ecmal4.JSAttribute;
+import com.intellij.openapi.application.ApplicationManager;
 
 public final class ActionScriptSemanticHighlightingUtil {
 
   public static void highlight(JSAttribute attribute, AnnotationHolder holder) {
-    JSSemanticHighlightingVisitor.lineMarker(attribute, ECMAL4Highlighter.ECMAL4_METADATA, "attribute", holder);
+    AnnotationBuilder builder =
+      ApplicationManager.getApplication().isUnitTestMode()
+      ? holder.newAnnotation(HighlightInfoType.SYMBOL_TYPE_SEVERITY, "attribute")
+      : holder.newSilentAnnotation(HighlightInfoType.SYMBOL_TYPE_SEVERITY);
+    builder.range(attribute).textAttributes(ECMAL4Highlighter.ECMAL4_METADATA).create();
   }
 
 }
