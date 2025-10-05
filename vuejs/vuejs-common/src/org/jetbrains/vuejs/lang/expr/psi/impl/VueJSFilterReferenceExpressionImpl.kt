43ed1f1fb5d24f6368e6539ec058467da2a28810
Arsenii Iusufov <arsenii.iusufov@jetbrains.com>
2025-10-05 21:22:10 +0200

    WEB-74979 Move `VueJSFilterReferenceExpression`, `VueJSFilterReferenceExpressionImpl` to `intellij.vuejs.common`
    GitOrigin-RevId: b730cbecec097784b332d23fbb46f27a896b9f1d

--- vuejs/vuejs-common/src/org/jetbrains/vuejs/lang/expr/psi/impl/VueJSFilterReferenceExpressionImpl.kt
+++ vuejs/vuejs-common/src/org/jetbrains/vuejs/lang/expr/psi/impl/VueJSFilterReferenceExpressionImpl.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.expr.psi.impl
 
 import com.intellij.lang.javascript.psi.JSFunction
