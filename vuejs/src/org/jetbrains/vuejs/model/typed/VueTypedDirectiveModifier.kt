a9ffa83203cdfb1e74053b47e44bb3b3e1cb50a5
Victor Turansky <victor.turansky@jetbrains.com>
2025-10-08 22:43:46 +0200

    [vue] WEB-74086 Support Vapor directive modifiers
    GitOrigin-RevId: 037392ba4d530c8537bed24d71076d26299cd23b

--- vuejs/src/org/jetbrains/vuejs/model/typed/VueTypedDirectiveModifier.kt
+++ vuejs/src/org/jetbrains/vuejs/model/typed/VueTypedDirectiveModifier.kt
@@ -1,22 +1,22 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.model.typed
 
-import com.intellij.lang.javascript.psi.ecma6.TypeScriptPropertySignature
 import com.intellij.model.Pointer
+import com.intellij.psi.PsiElement
 import com.intellij.psi.createSmartPointer
 import org.jetbrains.vuejs.model.VueDirectiveModifier
 
 data class VueTypedDirectiveModifier(
-  override val source: TypeScriptPropertySignature,
+  override val name: String,
+  override val source: PsiElement,
 ) : VueDirectiveModifier {
-  override val name: String
-    get() = source.memberName
 
   override fun createPointer(): Pointer<out VueTypedDirectiveModifier> {
+    val name = this.name
     val sourcePtr = source.createSmartPointer()
     return Pointer {
       val source = sourcePtr.dereference() ?: return@Pointer null
-      VueTypedDirectiveModifier(source)
+      VueTypedDirectiveModifier(name, source)
     }
   }
 }
--- vuejs/src/org/jetbrains/vuejs/model/typed/VueTypedDirectives.kt
+++ vuejs/src/org/jetbrains/vuejs/model/typed/VueTypedDirectives.kt
@@ -1,11 +1,14 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.model.typed
 
+import com.intellij.lang.javascript.psi.JSFunction
 import com.intellij.lang.javascript.psi.JSFunctionType
+import com.intellij.lang.javascript.psi.JSRecordType.TypeMember
 import com.intellij.lang.javascript.psi.JSType
 import com.intellij.lang.javascript.psi.JSTypeOwner
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptPropertySignature
 import com.intellij.psi.PsiElement
+import org.jetbrains.vuejs.model.VueDirectiveModifier
 import org.jetbrains.vuejs.model.VueMode
 import org.jetbrains.vuejs.model.source.DIRECTIVE_MODIFIERS_PROP
 import org.jetbrains.vuejs.model.source.DIRECTIVE_MOUNTED_FUN
@@ -15,55 +18,77 @@ internal object VueTypedDirectives {
   fun getDirectiveModifiers(
     source: PsiElement,
     mode: VueMode,
-  ): List<VueTypedDirectiveModifier> {
-    val declaration = findDeclaration(source) as? JSTypeOwner
+  ): List<VueDirectiveModifier> {
+    val declaration = findDeclaration(source)
                       ?: return emptyList()
 
-    return getDirectiveModifiers(declaration.jsType, mode)
+    return getDirectiveModifiersInternal(declaration, mode)
   }
 
   fun getDirectiveModifiers(
     source: TypeScriptPropertySignature,
     mode: VueMode,
-  ): List<VueTypedDirectiveModifier> {
-    return getDirectiveModifiers(source.jsType, mode)
+  ): List<VueDirectiveModifier> {
+    return getDirectiveModifiersInternal(source, mode)
   }
 
-  private fun getDirectiveModifiers(
-    jsType: JSType?,
+  private fun getDirectiveModifiersInternal(
+    source: PsiElement,
     mode: VueMode,
-  ): List<VueTypedDirectiveModifier> {
-    jsType ?: return emptyList()
-
-    val modifiers = when (mode) {
-      VueMode.CLASSIC -> {
-        val mounted = jsType.asRecordType()
-                        .findPropertySignature(DIRECTIVE_MOUNTED_FUN)
-                      ?: return emptyList()
-
-        val bindingType = mounted.jsType
-                            ?.functionParameterType(1)
-                          ?: return emptyList()
-
-        bindingType.asRecordType()
-          .findPropertySignature(DIRECTIVE_MODIFIERS_PROP)
-          ?.jsType
-      }
-
-      VueMode.VAPOR -> {
-        // implement
-        null
-      }
+  ): List<VueDirectiveModifier> {
+    val modifiersType = when (mode) {
+      VueMode.CLASSIC -> getClassicDirectiveModifiersType(source)
+      VueMode.VAPOR -> source.functionParameterType(3)
     }
 
-    modifiers ?: return emptyList()
+    modifiersType ?: return emptyList()
 
-    return modifiers.asRecordType()
-      .properties.asSequence()
-      .filterIsInstance<TypeScriptPropertySignature>()
-      .map(::VueTypedDirectiveModifier)
-      .toList()
+    return modifiersType.asRecordType()
+      .properties
+      .mapNotNull { signature ->
+        val source = when (signature) {
+          is PsiElement -> signature
+          is TypeMember -> signature.memberSource.singleElement
+          else -> null
+        }
+
+        if (source != null) {
+          VueTypedDirectiveModifier(signature.memberName, source)
+        }
+        else null
+      }
   }
+
+  private fun getClassicDirectiveModifiersType(
+    source: PsiElement,
+  ): JSType? {
+    if (source !is JSTypeOwner)
+      return null
+
+    val jsType = source.jsType
+                 ?: return null
+
+    val mounted = jsType.asRecordType()
+                    .findPropertySignature(DIRECTIVE_MOUNTED_FUN)
+                  ?: return null
+
+    val bindingType = mounted.jsType
+                        ?.functionParameterType(1)
+                      ?: return null
+
+    return bindingType.asRecordType()
+      .findPropertySignature(DIRECTIVE_MODIFIERS_PROP)
+      ?.jsType
+  }
+
+  private fun PsiElement.functionParameterType(
+    parameterIndex: Int,
+  ): JSType? =
+    when (this) {
+      is JSFunction -> parameters.getOrNull(parameterIndex)?.jsType
+      is JSTypeOwner -> jsType?.functionParameterType(parameterIndex)
+      else -> null
+    }
 
   private fun JSType.functionParameterType(
     parameterIndex: Int,
