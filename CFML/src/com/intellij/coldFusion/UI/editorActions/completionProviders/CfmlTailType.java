4c973985ba18691fd92edf5bd1e9cea33266b907
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-10-09 13:09:21 +0200

    [completion] IJPL-207762 TailType nullability
    GitOrigin-RevId: 73a919ee73797ac4aa114421f719d78718bd098b

--- CFML/src/com/intellij/coldFusion/UI/editorActions/completionProviders/CfmlTailType.java
+++ CFML/src/com/intellij/coldFusion/UI/editorActions/completionProviders/CfmlTailType.java
@@ -1,16 +1,17 @@
-// Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package com.intellij.coldFusion.UI.editorActions.completionProviders;
 
 import com.intellij.codeInsight.TailType;
 import com.intellij.coldFusion.model.lexer.CfscriptTokenTypes;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.highlighter.HighlighterIterator;
+import org.jetbrains.annotations.NotNull;
 
 public class CfmlTailType extends TailType {
   public static final TailType PARENTHS = new CfmlTailType();
 
   @Override
-  public int processTail(Editor editor, int tailOffset) {
+  public int processTail(@NotNull Editor editor, int tailOffset) {
     HighlighterIterator iterator = editor.getHighlighter().createIterator(tailOffset);
     if (iterator.getTokenType() != CfscriptTokenTypes.L_BRACKET) {
       editor.getDocument().insertString(tailOffset, "()");
