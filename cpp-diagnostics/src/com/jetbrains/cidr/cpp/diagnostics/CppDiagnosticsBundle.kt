826a7159b0c91b47edf59820b0466872f5c19fc0
Daniel Below <daniel.below@jetbrains.com>
2025-10-12 18:56:45 +0200

    [clion] cleanup bundles
    fix DynamicBundle deprecations
    remove unused methods
    
    GitOrigin-RevId: 09b982c9b3d1d2769737030f465dbac9fd8bf975

--- cpp-diagnostics/src/com/jetbrains/cidr/cpp/diagnostics/CppDiagnosticsBundle.kt
+++ cpp-diagnostics/src/com/jetbrains/cidr/cpp/diagnostics/CppDiagnosticsBundle.kt
@@ -3,15 +3,13 @@ package com.jetbrains.cidr.cpp.diagnostics
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.PropertyKey
-import java.util.function.Supplier
 
 private const val BUNDLE = "messages.CppDiagnosticsBundle"
 
-object CppDiagnosticsBundle : DynamicBundle(BUNDLE) {
-  @JvmStatic
-  @Nls
-  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): String = getMessage(key, *params)
+object CppDiagnosticsBundle {
+  private val INSTANCE = DynamicBundle(javaClass, BUNDLE)
 
-  @JvmStatic
-  fun messagePointer(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): Supplier<String> = getLazyMessage(key, *params)
+  fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any): @Nls String {
+    return INSTANCE.getMessage(key, *params)
+  }
 }
