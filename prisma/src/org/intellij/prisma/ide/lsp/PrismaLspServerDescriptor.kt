a5278b3c499a6be551e4a716a34f0f0459cb5800
Tomasz Blachut <tomasz.blachut@jetbrains.com>
2025-10-13 12:12:19 +0200

    [lsp] IJPL-189552 update monorepo descriptors for textDocument/documentSymbol
    GitOrigin-RevId: b0113a90a389e01b1798288a8396868a164ea904

--- prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerDescriptor.kt
+++ prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerDescriptor.kt
@@ -27,6 +27,7 @@ class PrismaLspServerDescriptor(project: Project)
     override val foldingRangeCustomizer = LspFoldingRangeDisabled
     override val inlayHintCustomizer: LspInlayHintCustomizer = LspInlayHintDisabled
     override val documentHighlightsCustomizer: LspDocumentHighlightsCustomizer = LspDocumentHighlightsDisabled
+    override val documentSymbolCustomizer: LspDocumentSymbolCustomizer = LspDocumentSymbolDisabled
     override val signatureHelpCustomizer: LspSignatureHelpCustomizer = LspSignatureHelpDisabled
 
     override val diagnosticsCustomizer: LspDiagnosticsCustomizer = object : LspDiagnosticsSupport() {
