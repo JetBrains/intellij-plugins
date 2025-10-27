2feea0af678e42fa78e3a8327f21dd2f831cf0df
Jan Papesch <jan.papesch@jetbrains.com>
2025-10-27 13:48:04 +0100

    [clion platformio] CPP-46948 Use PIO build configurations only for PIO projects.
    GitOrigin-RevId: 2d849baf85360161df8f05f6642b57fd1b090b47

--- platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/builds/PlatformioBuildConfigurationProvider.kt
+++ platformio/src/com/jetbrains/cidr/cpp/embedded/platformio/project/builds/PlatformioBuildConfigurationProvider.kt
@@ -3,12 +3,20 @@ package com.jetbrains.cidr.cpp.embedded.platformio.project.builds
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.roots.ProjectModelExternalSource
 import com.jetbrains.cidr.cpp.embedded.platformio.project.ID
+import com.jetbrains.cidr.cpp.embedded.platformio.project.PlatformioWorkspace
 import com.jetbrains.cidr.execution.CidrBuildConfiguration
 import com.jetbrains.cidr.execution.build.CidrBuildConfigurationProvider
 
 class PlatformioBuildConfigurationProvider : CidrBuildConfigurationProvider {
-  override fun getBuildableConfigurations(project: Project): List<CidrBuildConfiguration> = PlatformioBuildConfigurations
+  override fun hasBuildableConfigurations(project: Project): Boolean = PlatformioWorkspace.isPlatformioProject(project)
 
+  override fun getBuildableConfigurations(project: Project): List<CidrBuildConfiguration> = buildList {
+    if (hasBuildableConfigurations(project)) {
+      addAll(PlatformioBuildConfigurations)
+    }
+  }
+
+  override fun hasCleanableConfigurations(project: Project): Boolean = hasBuildableConfigurations(project)
   override fun getCleanableConfigurations(project: Project): List<CidrBuildConfiguration> = getBuildableConfigurations(project)
 }
 
