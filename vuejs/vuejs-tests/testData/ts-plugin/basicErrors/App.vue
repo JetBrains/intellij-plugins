ec12621e6043b50ca8c584e6c50f7e0cdbd301a8
Victor Turansky <victor.turansky@jetbrains.com>
2025-10-31 21:48:22 +0100

    [vue] WEB-56522 Extract test data for highlighting tests
    (cherry picked from commit 382259db373b18714bba779d58a8702470be6175)
    
    IJ-CR-181387
    
    GitOrigin-RevId: b8d5908eb03910bcb985785b3595bbc25a48062e

---
+++ vuejs/vuejs-tests/testData/ts-plugin/basicErrors/App.vue
@@ -0,0 +1,4 @@
+<script lang="ts">
+const <error descr="TS6133: 'unusedVar' is declared but its value is never read.">unusedVar</error> = 213;
+const <error descr="TS2322: Type 'number' is not assignable to type 'string'."><error descr="TS6133: 'abc' is declared but its value is never read.">abc</error></error>: string = 213;
+</script>
