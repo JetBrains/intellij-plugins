93d1c8e8f595ec8c0cf4580b929e0245963b6f1e
Bartek Pacia <bartek.pacia@jetbrains.com>
2025-11-05 18:00:34 +0100

    [java-tests] IDEA-381559 convert 2 Cucumber tests to JUnit 5
    GitOrigin-RevId: fa856a56cf28bcb5fdc0483ef526b4a3a8197b5e

--- cucumber-java/test/org/jetbrains/plugins/cucumber/java/rename/CucumberJavaStepParameterRenameTest.java
+++ cucumber-java/test/org/jetbrains/plugins/cucumber/java/rename/CucumberJavaStepParameterRenameTest.java
@@ -1,53 +1,111 @@
 package org.jetbrains.plugins.cucumber.java.rename;
 
+import com.intellij.ide.impl.OpenProjectTask;
 import com.intellij.idea.TestFor;
-import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ModifiableRootModel;
+import com.intellij.openapi.roots.ModuleRootManager;
+import com.intellij.platform.testFramework.junit5.codeInsight.fixture.CodeInsightFixtureKt;
+import com.intellij.project.IntelliJProjectConfiguration;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiPackage;
+import com.intellij.testFramework.PsiTestUtil;
+import com.intellij.testFramework.TestDataPath;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import com.intellij.testFramework.junit5.RunInEdt;
+import com.intellij.testFramework.junit5.RunMethodInEdt;
+import com.intellij.testFramework.junit5.TestApplication;
+import com.intellij.testFramework.junit5.fixture.FixturesKt;
+import com.intellij.testFramework.junit5.fixture.TestFixture;
+import com.intellij.util.text.VersionComparatorUtil;
 import org.jetbrains.plugins.cucumber.java.CucumberJavaTestUtil;
 import org.jetbrains.plugins.cucumber.refactoring.rename.GherkinStepParameterRenameTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+
+import static org.jetbrains.plugins.cucumber.java.CucumberJavaVersionUtil.CUCUMBER_CORE_VERSION_5;
 
 /// Tests that [GherkinStepParameterRenameTest][GherkinStepParameterRenameTest]
 /// works fine in a language-specific setting (Java).
 ///
 /// The "language-specific-setting" part matters! To learn why, see IDEA-376182.
-public class CucumberJavaStepParameterRenameTest extends BasePlatformTestCase {
+@TestDataPath("$PROJECT_ROOT/contrib/cucumber-java/testData/renameStepParameter")
+@RunInEdt(writeIntent = true, allMethods = false)
+@TestApplication
+public class CucumberJavaStepParameterRenameTest {
 
-  @Override
-  protected LightProjectDescriptor getProjectDescriptor() {
-    return CucumberJavaTestUtil.createCucumber7ProjectDescriptor();
-  }
+  private final TestFixture<String> testNameFixture = FixturesKt.testNameFixture();
+  private final TestFixture<Path> tempDir = FixturesKt.tempPathFixture(null, "IJ");
+  private final TestFixture<Project> project = FixturesKt.projectFixture(tempDir, OpenProjectTask.build(), true);
+  @SuppressWarnings("unused")
+  private final TestFixture<Module> module = FixturesKt.moduleFixture(project, tempDir, false);
+  private final TestFixture<CodeInsightTestFixture> fixture = CodeInsightFixtureKt.codeInsightFixture(project, tempDir);
 
-  @Override
-  protected String getBasePath() {
-    return CucumberJavaTestUtil.RELATED_TEST_DATA_PATH + "renameStepParameter";
+  @RunMethodInEdt
+  @BeforeEach
+  public void setUpProjectDescriptor() {
+    String version = "7";
+    ModifiableRootModel model = ModuleRootManager.getInstance(module.get()).getModifiableModel();
+    IntelliJProjectConfiguration.LibraryRoots libraryRoots;
+    libraryRoots = IntelliJProjectConfiguration.getModuleLibrary("intellij.cucumber.java", "cucumber-core-" + version);
+    PsiTestUtil.addProjectLibrary(model, "cucumber-core", libraryRoots.getClassesPaths());
+
+    libraryRoots = IntelliJProjectConfiguration.getModuleLibrary("intellij.cucumber.java", "cucumber-java-" + version);
+    PsiTestUtil.addProjectLibrary(model, "cucumber-java", libraryRoots.getClassesPaths());
+
+    if (VersionComparatorUtil.compare(version, CUCUMBER_CORE_VERSION_5) >= 0) {
+      libraryRoots = IntelliJProjectConfiguration.getModuleLibrary("intellij.cucumber.java", "cucumber-java8-" + version);
+      PsiTestUtil.addProjectLibrary(model, "cucumber-java8", libraryRoots.getClassesPaths());
+    }
+
+    CucumberJavaTestUtil.attachCucumberExpressionsLibrary(model);
+
+    WriteAction.runAndWait(() -> {
+      model.commit();
+    });
   }
 
   private void doTest(String newName) {
-    myFixture.copyDirectoryToProject(getTestName(true) + "/before", "");
+    CodeInsightTestFixture myFixture = fixture.get();
+    myFixture.copyDirectoryToProject(testNameFixture.get() + "/before", "");
     myFixture.configureByFile("test.feature");
     myFixture.testHighlighting("test.feature"); // ensure everything is resolved
 
-    myFixture.renameElementAtCaretUsingHandler(newName);
+    PsiPackage cucumberPackage = ReadAction.compute(() -> {
+      return JavaPsiFacade.getInstance(myFixture.getProject()).findPackage("io.cucumber.java.en");
+    });
+    Assertions.assertNotNull(cucumberPackage); // Verify that cucumber-jvm JAR is attached
+    myFixture.renameElementAtCaretUsingHandler(newName); // FIXME: for some reason doesn't work with JUnit 5
 
-    myFixture.checkResultByFile("test.feature", getTestName(true) + "/after/test.feature", false);
+    myFixture.checkResultByFile("test.feature", testNameFixture.get() + "/after/test.feature", false);
   }
 
-  public void testParameterDefinition() {
+  @Test
+  public void parameterDefinition() {
     doTest("newStart");
   }
 
+  @Test
   @TestFor(issues = "IDEA-376182")
-  public void testParameterUsage() {
+  public void parameterUsage() {
     doTest("newStart");
   }
 
+  @Test
   @TestFor(issues = "IDEA-376182")
-  public void testParameterUsageOnUnresolvedStep() {
+  public void parameterUsageOnUnresolvedStep() {
     doTest("newStart");
   }
 
+  @Test
   @TestFor(issues = "IDEA-374108")
-  public void testParameterUsageWithCaretJustBeforeClosingBracket() {
+  public void parameterUsageWithCaretJustBeforeClosingBracket() {
     doTest("helloDarkness");
   }
 }
--- cucumber/test/org/jetbrains/plugins/cucumber/refactoring/rename/GherkinStepParameterRenameTest.java
+++ cucumber/test/org/jetbrains/plugins/cucumber/refactoring/rename/GherkinStepParameterRenameTest.java
@@ -1,28 +1,45 @@
 package org.jetbrains.plugins.cucumber.refactoring.rename;
 
-import com.intellij.testFramework.fixtures.BasePlatformTestCase;
-import org.jetbrains.plugins.cucumber.CucumberTestUtil;
+import com.intellij.ide.impl.OpenProjectTask;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.platform.testFramework.junit5.codeInsight.fixture.CodeInsightFixtureKt;
+import com.intellij.testFramework.TestDataPath;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import com.intellij.testFramework.junit5.TestApplication;
+import com.intellij.testFramework.junit5.fixture.FixturesKt;
+import com.intellij.testFramework.junit5.fixture.TestFixture;
+import org.junit.jupiter.api.Test;
 
-public class GherkinStepParameterRenameTest extends BasePlatformTestCase {
+import java.nio.file.Path;
 
-  @Override
-  protected String getTestDataPath() {
-    return CucumberTestUtil.getTestDataPath() + "/refactoring/rename";
-  }
+@TestApplication
+@TestDataPath("$PROJECT_ROOT/contrib/cucumber/testData/refactoring/rename")
+public class GherkinStepParameterRenameTest {
+
+  private final TestFixture<String> testNameFixture = FixturesKt.testNameFixture();
+  private final TestFixture<Path> tempDir = FixturesKt.tempPathFixture(null, "IJ");
+  private final TestFixture<Project> project = FixturesKt.projectFixture(tempDir, OpenProjectTask.build(), true);
+  @SuppressWarnings("unused")
+  private final TestFixture<Module> module = FixturesKt.moduleFixture(project, tempDir, false);
+  private final TestFixture<CodeInsightTestFixture> fixture = CodeInsightFixtureKt.codeInsightFixture(project, tempDir);
 
   private void doTest(String newName) {
-    myFixture.configureByFile(getTestName(true) + ".feature");
+    CodeInsightTestFixture myFixture = fixture.get();
+    myFixture.configureByFile(testNameFixture.get() + ".feature");
 
-    myFixture.renameElementAtCaretUsingHandler(newName);
+    myFixture.renameElementAtCaretUsingHandler(newName); // FIXME: for some reason doesn't work with JUnit 5
 
-    myFixture.checkResultByFile(getTestName(true) + ".after.feature");
+    myFixture.checkResultByFile(testNameFixture.get() + ".after.feature");
   }
 
-  public void testRenameParameterUsage() {
+  @Test
+  public void renameParameterUsage() {
     doTest("newStart");
   }
 
-  public void testRenameParameterDefinition() {
+  @Test
+  public void renameParameterDefinition() {
     doTest("newStart");
   }
 }
