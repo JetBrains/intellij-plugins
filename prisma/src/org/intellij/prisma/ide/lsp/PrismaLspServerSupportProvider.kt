d663aa51bdfa061f56f475a01934d9f780d52796
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2025-11-12 17:08:41 +0200

    [prisma] WEB-75622 update LSP to 6.19.0
    (cherry picked from commit 6c86f8f40ce32e3c361e4cc4360c423c08df6041)
    
    IJ-CR-182572
    
    GitOrigin-RevId: 687a5ace038fda6206fe772d6fe40916fa9068f4

--- prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerSupportProvider.kt
+++ prisma/src/org/intellij/prisma/ide/lsp/PrismaLspServerSupportProvider.kt
@@ -17,7 +17,7 @@ import org.intellij.prisma.PrismaIcons
 import org.intellij.prisma.ide.ui.PrismaSettingsConfigurable
 
 private object PrismaLspServerPackageDescriptor : LspServerPackageDescriptor("@prisma/language-server",
-                                                                             PackageVersion.downloadable("6.16.1"),
+                                                                             PackageVersion.downloadable("6.19.0"),
                                                                              "/dist/bin.js") {
   private val sinceNewServiceLayoutVersion = SemVer.parseFromText("5.15.0")!! // inclusive
 
