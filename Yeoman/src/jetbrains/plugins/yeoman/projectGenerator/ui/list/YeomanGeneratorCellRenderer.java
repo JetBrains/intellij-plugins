e40930c7763bd2f0a049bf2c6cc930991003a0d9
Alexander Zolotov <zolotov@jetbrains.com>
2025-11-17 12:51:52 +0100

    refactoring [text-matching]: extract MatchingMode
    Extracted MatchingMode to enable moving the entire NameUtil class from the library code to the platform layer, as MatchingCaseSensitivity is the only thing actually used by matcher.
    This move will support parameterizing matchers with a layout-fixing strategy. While this parametrization is a healthy step for a library — shifting state management responsibility to the client — it also increases complexity for IntelliJ Platform clients.
    
    To mitigate this, matcher builders will be moved into the platform core module, which will centrally handle layout-fixing conversions for all IntelliJ clients.
    
    GitOrigin-RevId: 44a3cb761a2052e0d5234cb5b6458b5dde3d6ed9

--- Yeoman/src/jetbrains/plugins/yeoman/projectGenerator/ui/list/YeomanGeneratorCellRenderer.java
+++ Yeoman/src/jetbrains/plugins/yeoman/projectGenerator/ui/list/YeomanGeneratorCellRenderer.java
@@ -23,6 +23,7 @@ import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.speedSearch.SpeedSearchSupply;
 import com.intellij.ui.speedSearch.SpeedSearchUtil;
 import com.intellij.util.text.Matcher;
+import com.intellij.util.text.matching.MatchingMode;
 import com.intellij.util.ui.JBUI;
 import com.intellij.util.ui.StartupUiUtil;
 import com.intellij.util.ui.UIUtil;
@@ -66,7 +67,7 @@ public final class YeomanGeneratorCellRenderer extends DefaultTableCellRenderer 
                                                            UIUtil.getListForeground(isSelected, hasFocus),
                                                            JBColor.RED,
                                                            SimpleTextAttributes.STYLE_PLAIN);
-      Matcher matcher = NameUtil.buildMatcher("*" + query, NameUtil.MatchingCaseSensitivity.NONE);
+      Matcher matcher = NameUtil.buildMatcher("*" + query, MatchingMode.IGNORE_CASE);
       if (query instanceof String) {
         SpeedSearchUtil.appendColoredFragmentForMatcher(generatorName, myName, attr, matcher, UIUtil.getTableBackground(isSelected, hasFocus), true);
       }
