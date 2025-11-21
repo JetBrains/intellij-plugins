0d1fecac6e61587364d85771a154eb4e1780e4fb
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2025-11-21 16:02:02 +0100

    WEB-70985 HbFileType: convert to Kotlin
    GitOrigin-RevId: 1a1bfc74f65bf3f757467ac2200afc36e97aa5fb

--- handlebars/src/com/dmarcotte/handlebars/file/HbFileType.kt
+++ handlebars/src/com/dmarcotte/handlebars/file/HbFileType.kt
@@ -1,84 +1,48 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package com.dmarcotte.handlebars.file;
+package com.dmarcotte.handlebars.file
 
-import com.dmarcotte.handlebars.HbBundle;
-import com.dmarcotte.handlebars.HbLanguage;
-import com.intellij.ide.highlighter.XmlLikeFileType;
-import com.intellij.lang.Language;
-import com.intellij.openapi.fileTypes.CharsetUtil;
-import com.intellij.openapi.fileTypes.LanguageFileType;
-import com.intellij.openapi.fileTypes.TemplateLanguageFileType;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings;
-import icons.HandlebarsIcons;
-import org.jetbrains.annotations.NonNls;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import com.dmarcotte.handlebars.HbBundle
+import com.dmarcotte.handlebars.HbLanguage
+import com.intellij.ide.highlighter.XmlLikeFileType
+import com.intellij.lang.Language
+import com.intellij.openapi.fileTypes.CharsetUtil
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.fileTypes.TemplateLanguageFileType
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.templateLanguages.TemplateDataLanguageMappings
+import icons.HandlebarsIcons
+import org.jetbrains.annotations.NonNls
+import java.nio.charset.Charset
+import javax.swing.Icon
 
-import javax.swing.*;
-import java.nio.charset.Charset;
+open class HbFileType protected constructor(lang: Language) : XmlLikeFileType(lang), TemplateLanguageFileType {
+  private constructor() : this(HbLanguage.INSTANCE)
 
-public class HbFileType extends XmlLikeFileType implements TemplateLanguageFileType {
-  public static final LanguageFileType INSTANCE = new HbFileType();
+  override fun getName(): String = "Handlebars/Mustache"
 
-  public static final @NonNls String DEFAULT_EXTENSION = "handlebars;hbs;mustache";
+  override fun getDescription(): String = HbBundle.message("filetype.hb.description")
 
-  private HbFileType() {
-    this(HbLanguage.INSTANCE);
-  }
+  override fun getDefaultExtension(): String = DEFAULT_EXTENSION
 
-  protected HbFileType(Language lang) {
-    super(lang);
-  }
+  override fun getIcon(): Icon = HandlebarsIcons.Handlebars_icon
 
-  @Override
-  public @NotNull String getName() {
-    return "Handlebars/Mustache";
-  }
+  override fun extractCharsetFromFileContent(
+    project: Project?,
+    file: VirtualFile?,
+    content: CharSequence,
+  ): Charset? =
+    getAssociatedFileType(file, project)
+      ?.let { CharsetUtil.extractCharsetFromFileContent(project, file, it, content) }
 
-  @Override
-  public @NotNull String getDescription() {
-    return HbBundle.message("filetype.hb.description");
-  }
+  companion object {
+    @JvmField val INSTANCE: LanguageFileType = HbFileType()
 
-  @Override
-  public @NotNull String getDefaultExtension() {
-    return DEFAULT_EXTENSION;
-  }
-
-  @Override
-  public Icon getIcon() {
-    return HandlebarsIcons.Handlebars_icon;
-  }
-
-  @Override
-  public Charset extractCharsetFromFileContent(final @Nullable Project project,
-                                               final @Nullable VirtualFile file,
-                                               final @NotNull CharSequence content) {
-    LanguageFileType associatedFileType = getAssociatedFileType(file, project);
-
-    if (associatedFileType == null) {
-      return null;
-    }
-
-    return CharsetUtil.extractCharsetFromFileContent(project, file, associatedFileType, content);
-  }
-
-  private static LanguageFileType getAssociatedFileType(VirtualFile file, Project project) {
-    if (project == null) {
-      return null;
-    }
-    Language language = TemplateDataLanguageMappings.getInstance(project).getMapping(file);
-
-    LanguageFileType associatedFileType = null;
-    if (language != null) {
-      associatedFileType = language.getAssociatedFileType();
-    }
-
-    if (language == null || associatedFileType == null) {
-      associatedFileType = HbLanguage.getDefaultTemplateLang();
-    }
-    return associatedFileType;
+    @NonNls
+    const val DEFAULT_EXTENSION: @NonNls String = "handlebars;hbs;mustache"
   }
 }
+
+private fun getAssociatedFileType(file: VirtualFile?, project: Project?): LanguageFileType? =
+  project?.let { TemplateDataLanguageMappings.getInstance(project).getMapping(file)?.associatedFileType }
+  ?: HbLanguage.getDefaultTemplateLang()
