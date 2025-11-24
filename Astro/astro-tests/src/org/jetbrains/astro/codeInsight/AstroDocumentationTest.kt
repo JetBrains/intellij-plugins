b8b84898990bb6dc857458b7d95d07d8a609ee3c
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2025-11-24 09:35:31 +0200

    [astro] WEB-67534 Adjust documentation tests with TypeScript provider and increase timeout in AstroDocumentationTest setup
    GitOrigin-RevId: 897c927b70510e109022c1b89fd1b92dfb14389f

--- Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroDocumentationTest.kt
+++ Astro/astro-tests/src/org/jetbrains/astro/codeInsight/AstroDocumentationTest.kt
@@ -1,11 +1,21 @@
 package org.jetbrains.astro.codeInsight
 
+import com.intellij.lang.javascript.service.JSLanguageServiceUtil
+import com.intellij.lang.typescript.documentation.TypeScriptDocumentationTargetProvider
+import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider
 import com.intellij.polySymbols.testFramework.LookupElementInfo
 import com.intellij.polySymbols.testFramework.checkDocumentationAtCaret
 import com.intellij.polySymbols.testFramework.checkLookupItems
 import org.jetbrains.astro.AstroCodeInsightTestCase
 
 class AstroDocumentationTest : AstroCodeInsightTestCase("codeInsight/documentation", useLsp = true) {
+
+  override fun setUp() {
+    super.setUp()
+    PsiDocumentationTargetProvider.EP_NAME.point
+      .registerExtension(TypeScriptDocumentationTargetProvider(), testRootDisposable)
+    JSLanguageServiceUtil.setTimeout(10000, myFixture.testRootDisposable)
+  }
 
   fun testHtmlTag() = doTest()
 
@@ -35,7 +45,7 @@ class AstroDocumentationTest : AstroCodeInsightTestCase("codeInsight/documentati
   }
 
   private fun doLookupTest(
-    lookupFilter: (item: LookupElementInfo) -> Boolean = { true }
+    lookupFilter: (item: LookupElementInfo) -> Boolean = { true },
   ) {
     doConfiguredTest {
       checkLookupItems(renderPriority = true, renderTypeText = true, checkDocumentation = true, lookupItemFilter = lookupFilter)
