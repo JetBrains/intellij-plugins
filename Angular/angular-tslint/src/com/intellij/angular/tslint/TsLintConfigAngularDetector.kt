865bee8fa7bf26f5f959b31daac1491a7acd6431
Andrey Vorobev <andrey.vorobev@jetbrains.com>
2025-12-30 10:15:04 +0200

    [angular] WEB-76169 - convert angular plugin to v2
    - convert all optional depends into content modules
    - create angular.backend and angular.plugin modules
    - create angular.free module for single distribution support
    
    GitOrigin-RevId: c4b700e0a86f550a926cfac8b0edec55917b6656

--- Angular/angular-tslint/src/com/intellij/angular/tslint/TsLintConfigAngularDetector.kt
+++ Angular/angular-tslint/src/com/intellij/angular/tslint/TsLintConfigAngularDetector.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.angular2.tslint
+package com.intellij.angular.tslint
 
 import com.intellij.lang.javascript.linter.tslint.config.TsLintConfigDetector
 import com.intellij.lang.typescript.tsconfig.TypeScriptConfigUtil.configGraphIncludesFile
