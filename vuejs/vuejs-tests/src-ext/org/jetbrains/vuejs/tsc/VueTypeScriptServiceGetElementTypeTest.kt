69f770c7254fa290e9bdf4a175f698253438f6fb
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-07 00:55:01 +0100

    [vue] WEB-74847 Enable `VueTypeScriptServiceGetElementTypeTest.testCancellation`
    GitOrigin-RevId: 68d0a241787c607479803f6e893528342cfb2bab

--- vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/tsc/VueTypeScriptServiceGetElementTypeTest.kt
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/tsc/VueTypeScriptServiceGetElementTypeTest.kt
@@ -6,18 +6,15 @@ import com.intellij.lang.javascript.TrackFailedTestRule
 import com.intellij.lang.javascript.evaluation.JSTypeEvaluationLocationProvider
 import com.intellij.lang.javascript.psi.JSType
 import com.intellij.lang.javascript.psi.JSVariable
-import com.intellij.lang.typescript.compiler.TypeScriptService
 import com.intellij.lang.typescript.compiler.TypeScriptServiceHolder
 import com.intellij.lang.typescript.compiler.languageService.protocol.commands.TypeScriptTypeRequestKind
 import com.intellij.lang.typescript.tsc.TypeScriptServiceGetElementTypeTest
 import com.intellij.lang.typescript.tsc.TypeScriptServiceTestMixin
-import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiElement
 import org.jetbrains.vuejs.lang.VueTestModule
 import org.jetbrains.vuejs.lang.configureVueDependencies
 import org.jetbrains.vuejs.lang.typescript.service.VuePluginTypeScriptService
 import org.jetbrains.vuejs.types.VueUnwrapRefType
-import org.junit.Assume
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
@@ -43,15 +40,6 @@ class VueTypeScriptServiceGetElementTypeTest :
     return super.calculateType(element, typeRequestKind).also {
       assertInstanceOf(TypeScriptServiceHolder.getForFile(project, file.virtualFile), VuePluginTypeScriptService::class.java)
     }
-  }
-
-  @Test
-  override fun testCancellation() {
-    // TODO Once we are able to figure out how to perform cancellation on the Volar LSP,
-    //      we can enable this test. Most likely we will need to use the same approach as
-    //      is in the TypeScript language server - a marker file.
-    Assume.assumeTrue("Volar does not yet support cancellation", false)
-    super.testCancellation()
   }
 
   /**
@@ -87,9 +75,5 @@ class VueTypeScriptServiceGetElementTypeTest :
       val unwrapRefType = VueUnwrapRefType(jsType!!, element).substitute()
       assertEquals("number", unwrapRefType.getTypeText(JSType.TypeTextFormat.PRESENTABLE))
     }
-  }
-
-  override suspend fun sendTestCancellationCommand(service: TypeScriptService) {
-    Disposer.dispose(service as VuePluginTypeScriptService)
   }
 }
