bc593e56ab724bb4057b6abe885a82fdb0582d9f
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-08 00:50:45 +0100

    cleanup [vue]: fix formatting
    GitOrigin-RevId: dab00b88e3a25600514bd80c67b18da7fe29815d

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/stubs/serializers/VueScriptSetupEmbeddedContentStubSerializer.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/stubs/serializers/VueScriptSetupEmbeddedContentStubSerializer.kt
@@ -12,10 +12,12 @@ import org.jetbrains.vuejs.lang.expr.VueTSLanguage
 import org.jetbrains.vuejs.lang.html.parser.VueScriptSetupEmbeddedContentElementType
 import org.jetbrains.vuejs.lang.html.stub.impl.VueScriptSetupEmbeddedContentStubImpl
 
-internal class VueScriptSetupEmbeddedContentStubSerializer(elementTypeSupplier: () -> VueScriptSetupEmbeddedContentElementType)
-  : JSStubSerializer<JSEmbeddedContentStub, JSEmbeddedContent>(elementTypeSupplier) {
-  
+internal class VueScriptSetupEmbeddedContentStubSerializer(
+  elementTypeSupplier: () -> VueScriptSetupEmbeddedContentElementType,
+) : JSStubSerializer<JSEmbeddedContentStub, JSEmbeddedContent>(elementTypeSupplier) {
+
   private val typedElementType: VueScriptSetupEmbeddedContentElementType by lazy { elementTypeSupplier() }
+
   override fun deserialize(dataStream: StubInputStream, parentStub: StubElement<*>?): JSEmbeddedContentStub =
     VueScriptSetupEmbeddedContentStubImpl(dataStream, parentStub, elementType)
 
