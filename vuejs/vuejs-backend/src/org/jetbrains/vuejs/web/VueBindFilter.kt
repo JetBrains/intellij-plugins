bd7a2aa6ddb4e2c2f2ba81330db8b9d5e500b613
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-01-14 11:46:53 +0100

    WEB-76344 Vue: fix wrong behavior of bind filter.
    GitOrigin-RevId: 10369cc344d03b5dcedcefc74b9971280b263504

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/web/VueBindFilter.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/web/VueBindFilter.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.vuejs.web
 
 import com.intellij.polySymbols.PolySymbol
+import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.polySymbols.completion.PolySymbolCodeCompletionItem
 import com.intellij.polySymbols.query.PolySymbolQueryExecutor
 import com.intellij.polySymbols.query.PolySymbolQueryStack
@@ -14,7 +15,7 @@ class VueBindFilter : PolySymbolFilter {
     stack: PolySymbolQueryStack,
     properties: Map<String, Any>,
   ): List<PolySymbolCodeCompletionItem> =
-    codeCompletions.filterHtmlEventAttributes(queryExecutor, stack) { name }
+    codeCompletions.filterHtmlEventAttributes(queryExecutor, stack, { name }, { this.symbol?.kind })
 
   override fun filterNameMatches(
     matches: List<PolySymbol>,
@@ -22,18 +23,20 @@ class VueBindFilter : PolySymbolFilter {
     stack: PolySymbolQueryStack,
     properties: Map<String, Any>,
   ): List<PolySymbol> =
-    matches.filterHtmlEventAttributes(queryExecutor, stack) { name }
+    matches.filterHtmlEventAttributes(queryExecutor, stack, { name }, { kind })
 
   private fun <T> List<T>.filterHtmlEventAttributes(
     queryExecutor: PolySymbolQueryExecutor,
     stack: PolySymbolQueryStack,
     getName: T.() -> String,
+    symbolKind: T.() -> PolySymbolKind?,
   ): List<T> {
     val props = queryExecutor.listSymbolsQuery(VUE_COMPONENT_PROPS, true)
       .additionalScope(stack)
       .run()
       .mapTo(HashSet()) { it.name }
     return filter {
+      if (it.symbolKind() == VUE_COMPONENT_PROPS) return@filter true
       val name = it.getName()
       !name.startsWith("on") || name.startsWith("on-") || name == "on" || props.contains(name)
     }
