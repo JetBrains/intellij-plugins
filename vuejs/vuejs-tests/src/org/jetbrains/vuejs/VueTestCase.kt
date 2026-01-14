30ac33e6b881dcbb7a7fe5926b40e22873249dfc
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-14 17:52:01 +0100

    [vue] WEB-74847 `VueTestCase`. Add `useTsc` option
    GitOrigin-RevId: f7f7f579d6c20d07b4f3f2092cc723fb73c53e03

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/VueTestCase.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/VueTestCase.kt
@@ -2,11 +2,43 @@
 package org.jetbrains.vuejs
 
 import com.intellij.javascript.testFramework.web.WebFrameworkTestCase
+import com.intellij.lang.javascript.HybridTestMode
+import com.intellij.lang.javascript.waitEmptyServiceQueueForService
+import com.intellij.lang.typescript.compiler.languageService.TypeScriptServerServiceImpl
+import com.intellij.lang.typescript.tsc.TypeScriptServiceTestMixin
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.testFramework.runInEdtAndWait
 import org.jetbrains.vuejs.lang.getVueTestDataPath
+import org.jetbrains.vuejs.lang.typescript.service.VuePluginTypeScriptService
 
 abstract class VueTestCase(
   override val testCasePath: String,
-) : WebFrameworkTestCase() {
+  private val useTsc: Boolean = false,
+) : WebFrameworkTestCase(if (useTsc) HybridTestMode.CodeInsightFixture else HybridTestMode.BasePlatform) {
+
+  override fun beforeConfiguredTest(configuration: TestConfiguration) {
+    if (useTsc) {
+      val service = TypeScriptServiceTestMixin.setUpTypeScriptService(myFixture) {
+        it::class == VuePluginTypeScriptService::class
+      } as TypeScriptServerServiceImpl
+      
+      service.assertProcessStarted()
+      runInEdtAndWait {
+        waitEmptyServiceQueueForService(service)
+      }
+
+      // TODO: support config
+      /*
+      if (configuration.configurators.any { it is VueTsConfigFile }) {
+        TypeScriptServerServiceImpl.requireTSConfigsForTypeEvaluation(testRootDisposable, myFixture.tempDirFixture.getFile("tsconfig.json")!!)
+      }
+      */
+      runInEdtAndWait {
+        FileDocumentManager.getInstance().saveAllDocuments()
+      }
+    }
+  }
+
 
   override val testDataRoot: String
     get() = getVueTestDataPath()
