96062d21a16f8f26ba5e1da6435279d74af987a0
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-14 19:57:06 +0100

    [vue] WEB-74847 Use default import for `ts`
    GitOrigin-RevId: 8e24bf8a0c4dc3803e8ae29d531f7ff142a6fe93

--- vuejs/vuejs-backend/src-js/ws-typescript-vue-plugin/src/index.ts
+++ vuejs/vuejs-backend/src-js/ws-typescript-vue-plugin/src/index.ts
@@ -1,5 +1,5 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-import type * as ts from "tsc-ide-plugin/tsserverlibrary.shim";
+import type ts from "tsc-ide-plugin/tsserverlibrary.shim";
 import {decorateIdeLanguageServiceExtensions} from "./decorateLanguageService"
 
 const init: ts.server.PluginModuleFactory = () => ({
