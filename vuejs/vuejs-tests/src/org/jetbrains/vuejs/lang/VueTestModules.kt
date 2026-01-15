e434e513e9d3a93260e9c670024b1f9f51c10610
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-15 20:47:12 +0100

    [vue] WEB-74847 Fix `VueTestModule` formatting
    GitOrigin-RevId: 3c655282fa3f5c54fb52f2a307460becfbedaa42

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueTestModules.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueTestModules.kt
@@ -5,16 +5,27 @@ import com.intellij.javascript.testFramework.web.WebFrameworkTestModule
 import com.intellij.javascript.testFramework.web.configureDependencies
 import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 
-fun CodeInsightTestFixture.configureVueDependencies(vararg additionalDependencies: Pair<String, String>) {
+fun CodeInsightTestFixture.configureVueDependencies(
+  vararg additionalDependencies: Pair<String, String>,
+) {
   configureVueDependencies(additionalDependencies = additionalDependencies.toMap())
 }
 
-fun CodeInsightTestFixture.configureVueDependencies(vararg modules: VueTestModule,
-                                                    additionalDependencies: Map<String, String> = emptyMap()) {
-  configureDependencies(getVueTestDataPath(), mapOf("vue" to "*") + additionalDependencies, *modules)
+fun CodeInsightTestFixture.configureVueDependencies(
+  vararg modules: VueTestModule,
+  additionalDependencies: Map<String, String> = emptyMap(),
+) {
+  configureDependencies(
+    testDataRoot = getVueTestDataPath(),
+    defaultDependencies = mapOf("vue" to "*") + additionalDependencies,
+    modules = modules,
+  )
 }
 
-enum class VueTestModule(private val location: String, vararg packageNames: String) : WebFrameworkTestModule {
+enum class VueTestModule(
+  private val location: String,
+  vararg packageNames: String,
+) : WebFrameworkTestModule {
   BOOTSTRAP_VUE_2_0_0_RC_11("bootstrap-vue"),
   BUEFY_0_6_2("buefy"),
   COMPOSITION_API_0_4_0("composition-api/0.4.0", "@vue/composition-api"),
@@ -30,14 +41,17 @@ enum class VueTestModule(private val location: String, vararg packageNames: Stri
 
   // This version contains updated Web-Types and volar.d.ts included from index.d.ts
   NAIVE_UI_2_33_2_PATCHED("naive-ui/2.33.2-patched", "naive-ui"),
+
   NUXT_2_8_1("nuxt/2.8.1", "nuxt"),
   NUXT_2_9_2("nuxt/2.9.2", "nuxt", "@nuxt/types"),
   NUXT_2_13_2("nuxt/2.13.2", "nuxt", "@nuxt/types"),
   NUXT_2_15_6("nuxt/2.15.6", "nuxt", "@nuxt/types"),
+
   PRIMEVUE_3_8_2("primevue/3.8.2", "primevue"),
   PINIA_2_0_22("pinia/2.0.22", "pinia", "vue-demi"),
   QUASAR_2_6_5("quasar/2.6.5", "quasar"),
   SHARDS_VUE_1_0_5("shards-vue", "@shards/vue"),
+
   VUE_2_5_3("vue/2.5.3", "vue"),
   VUE_2_6_10("vue/2.6.10", "vue"),
   VUE_2_7_14("vue/2.7.14", "vue"),
@@ -48,16 +62,27 @@ enum class VueTestModule(private val location: String, vararg packageNames: Stri
   VUE_3_4_0("vue/3.4.0", "vue"),
   VUE_3_5_0("vue/3.5.0", "vue"),
   VUE_3_6_0("vue/3.6.0", "vue"),
+
   VUELIDATE_0_7_13("vuelidate", "@types/vuelidate", "vuelidate"),
+
   VUETIFY_0_17_2("vuetify/0.17.2", "vuetify"),
   VUETIFY_1_2_10("vuetify/1.2.10", "vuetify"),
   VUETIFY_1_3_7("vuetify/1.3.7", "vuetify"),
   VUETIFY_3_3_3("vuetify/3.3.3", "vuetify"),
-  VUEUSE_9_3_0("vueuse/9.3.0", "@vueuse/core"),
-  VUEX_3_1_0("vuex/3.1.0", "vuex"),
-  VUEX_4_0_0("vuex/4.0.0", "vuex");
 
-  override val packageNames: List<String> = if (packageNames.isEmpty()) listOf(location) else packageNames.toList()
+  VUEUSE_9_3_0("vueuse/9.3.0", "@vueuse/core"),
+
+  VUEX_3_1_0("vuex/3.1.0", "vuex"),
+  VUEX_4_0_0("vuex/4.0.0", "vuex"),
+
+  ;
+
+  override val packageNames: List<String> =
+    if (packageNames.isNotEmpty())
+      packageNames.toList()
+    else
+      listOf(location)
+
   override val folder: String
     get() = "$location/node_modules"
 }
