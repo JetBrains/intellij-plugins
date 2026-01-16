4769632ef84082bc943f7663ed1c70a0906ca476
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2026-01-16 15:22:28 +0200

    [astro] WEB-76003 Improve attribute insert handling for Astro
    Space-RevId: 89d349e837e10f77861ac1b0fd3534dfab7474cb
    
    GitOrigin-RevId: e61ff48a5fc55575dd3acdeda14ccbfc2e852374

--- Astro/src/org/jetbrains/astro/AstroFramework.kt
+++ Astro/src/org/jetbrains/astro/AstroFramework.kt
@@ -1,16 +1,35 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.astro
 
-import com.intellij.polySymbols.html.attributes.HtmlAttributeSymbolDescriptor
-import com.intellij.polySymbols.html.attributes.HtmlAttributeSymbolInfo
+import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.completion.InsertHandler
+import com.intellij.codeInsight.completion.XmlAttributeValueQuotationHandler
+import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.javascript.web.WebFramework
 import com.intellij.javascript.web.html.WebFrameworkHtmlFileType
 import com.intellij.lang.Language
+import com.intellij.lang.javascript.psi.JSType
+import com.intellij.lang.javascript.psi.JSTypeUtils
+import com.intellij.lang.javascript.psi.types.JSPrimitiveLiteralType
+import com.intellij.lang.javascript.psi.types.JSStringLiteralTypeImpl
+import com.intellij.lang.javascript.psi.types.JSUnionType
+import com.intellij.lang.javascript.psi.types.primitives.JSStringType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.polySymbols.PolySymbol
+import com.intellij.polySymbols.completion.PolySymbolCodeCompletionItem
+import com.intellij.polySymbols.html.attributes.HtmlAttributeSymbolDescriptor
+import com.intellij.polySymbols.html.attributes.HtmlAttributeSymbolInfo
+import com.intellij.polySymbols.js.jsType
+import com.intellij.polySymbols.js.symbols.JSPropertySymbol
+import com.intellij.polySymbols.js.types.TypeScriptSymbolTypeSupport
 import com.intellij.psi.xml.XmlTag
+import com.intellij.util.ThreeState
 import org.jetbrains.astro.codeInsight.attributes.AstroAttributeDescriptor
 import javax.swing.Icon
+
+val astroFramework: WebFramework
+  get() = WebFramework.get(AstroFramework.ID)
 
 class AstroFramework : WebFramework() {
   companion object {
@@ -28,7 +47,49 @@ class AstroFramework : WebFramework() {
 
   override fun createHtmlAttributeDescriptor(info: HtmlAttributeSymbolInfo, tag: XmlTag?): HtmlAttributeSymbolDescriptor =
     AstroAttributeDescriptor(info, tag)
-}
 
-val astroFramework: WebFramework
-  get() = WebFramework.get(AstroFramework.ID)
+  override fun createAttributeInsertHandler(
+    parameters: CompletionParameters,
+    item: PolySymbolCodeCompletionItem,
+    info: HtmlAttributeSymbolInfo,
+  ): InsertHandler<LookupElement> {
+    return if (shouldUseBracesForAttributeType(item.symbol)) {
+      XmlAttributeValueQuotationHandler.BRACES
+    }
+    else {
+      XmlAttributeValueQuotationHandler.QUOTES
+    }
+  }
+
+  override fun shouldInsertAttributeValue(
+    parameters: CompletionParameters,
+    item: PolySymbolCodeCompletionItem,
+    info: HtmlAttributeSymbolInfo,
+  ): Boolean {
+    if (isBooleanType(item.symbol)) return false
+    if (shouldUseBracesForAttributeType(item.symbol)) return true
+    return info.acceptsValue && !info.acceptsNoValue
+  }
+
+  private fun isBooleanType(symbol: PolySymbol?): Boolean {
+    val jsType = extractJSType(symbol) ?: return false
+    return TypeScriptSymbolTypeSupport.isBoolean(jsType) == ThreeState.YES
+  }
+
+  private fun shouldUseBracesForAttributeType(symbol: PolySymbol?): Boolean {
+    val jsType = extractJSType(symbol) ?: return false
+    val actualType = JSTypeUtils.removeNullableComponents(jsType.substitute())
+
+    if (actualType is JSStringType || actualType is JSStringLiteralTypeImpl) return false
+    if (actualType is JSUnionType && actualType.types.all { it is JSPrimitiveLiteralType<*> && it.literal is String }) {
+      return false
+    }
+
+    return true
+  }
+
+  private fun extractJSType(symbol: PolySymbol?): JSType? {
+    if (symbol == null) return null
+    return (symbol as? JSPropertySymbol)?.type ?: symbol.jsType
+  }
+}
