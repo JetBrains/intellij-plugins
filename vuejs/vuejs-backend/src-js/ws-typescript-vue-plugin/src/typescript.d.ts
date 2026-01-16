2ff47fb8b6a2d2ef0640501c261fd915283159a7
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-16 21:29:10 +0100

    [vue] WEB-74847 Use augmentation from `tsc-ide-plugin`
    GitOrigin-RevId: 6d8ad014e9d231f28b33836e8a76058c0a1aed1d

--- vuejs/vuejs-backend/src-js/ws-typescript-vue-plugin/src/typescript.d.ts
+++ vuejs/vuejs-backend/src-js/ws-typescript-vue-plugin/src/typescript.d.ts
@@ -1,8 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 import type {Language} from "@volar/language-core"
 
-// TODO: patch `typescript` module instead
-declare module "tsc-ide-plugin/tsserverlibrary.shim" {
+declare module "typescript/lib/tsserverlibrary" {
   namespace server {
     interface Project {
       readonly __vue__: Readonly<{
