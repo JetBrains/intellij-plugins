5b6c23823d8996f5dec0c4b8e4887d87a98dd3e7
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-26 16:27:59 +0100

    [vue] WEB-74847 Cleanup range calculation
    GitOrigin-RevId: 156f37ae2f8440479c9f4aab1137cd6fbf5e1f42

--- vuejs/vuejs-backend/src-js/ws-typescript-vue-plugin/src/ranges.ts
+++ vuejs/vuejs-backend/src-js/ws-typescript-vue-plugin/src/ranges.ts
@@ -1,6 +1,6 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 import type ts from "typescript/lib/tsserverlibrary"
-import type {Language, Mapper, SourceScript} from "@volar/language-core"
+import type {Language, Mapper} from "@volar/language-core"
 import {forEachEmbeddedCode} from "@volar/language-core"
 import type {ReverseMapper} from "tsc-ide-plugin/ide-get-element-type"
 import type {Position, Range} from "tsc-ide-plugin/protocol"
@@ -26,9 +26,7 @@ function toSourceRange(
   sourceFile: ts.SourceFile,
   generatedRange: Range,
 ): Range | undefined {
-  for (const [mapper, sourceScript] of scriptMappers(language, sourceFile.fileName)) {
-    const tsShift = sourceScript.snapshot.getLength()
-
+  for (const [mapper, tsShift] of scriptMappers(language, sourceFile.fileName)) {
     const sourceStartOffset = toSourceOffset(mapper, getOffset(sourceFile, generatedRange.start, tsShift))
     if (sourceStartOffset === undefined) continue
 
@@ -58,14 +56,13 @@ export function toGeneratedRange(
   startOffset: number,
   endOffset: number,
 ): [startOffset: number, endOffset: number] | undefined {
-  for (const [mapper, sourceScript] of scriptMappers(language, fileName)) {
+  for (const [mapper, tsShift] of scriptMappers(language, fileName)) {
     const generatedStartOffset = toGeneratedOffset(mapper, startOffset)
     if (generatedStartOffset === undefined) continue
 
     const generatedEndOffset = toGeneratedOffset(mapper, endOffset)
     if (generatedEndOffset === undefined) continue
 
-    const tsShift = sourceScript.snapshot.getLength()
     return [
       generatedStartOffset + tsShift,
       generatedEndOffset + tsShift,
@@ -78,7 +75,7 @@ export function toGeneratedRange(
 function* scriptMappers(
   language: Language<string>,
   fileName: string,
-): Generator<[mapper: Mapper, sourceScript: SourceScript<string>], void, undefined> {
+): Generator<[mapper: Mapper, tsShift: number], void, undefined> {
   const sourceScript = language.scripts.get(fileName)
   if (!sourceScript)
     return
@@ -87,11 +84,13 @@ function* scriptMappers(
   if (!virtualCode)
     return
 
+  const tsShift = sourceScript.snapshot.getLength()
+
   for (const code of forEachEmbeddedCode(virtualCode)) {
     if (!code.id.startsWith('script_'))
       continue
 
-    yield [language.maps.get(code, sourceScript), sourceScript]
+    yield [language.maps.get(code, sourceScript), tsShift]
   }
 }
 
