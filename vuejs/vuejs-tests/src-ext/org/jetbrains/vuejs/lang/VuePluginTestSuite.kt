f322707156f892659a9dcf8c6d8b81e1cb6b5a23
Victor Turansky <victor.turansky@jetbrains.com>
2026-01-29 18:11:18 +0100

    [vue] WEB-74847 Test suite for tests with TS plugin
    GitOrigin-RevId: 601d2d652ccebf29613e34452ba87bc69251c283

---
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/lang/VuePluginTestSuite.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+package org.jetbrains.vuejs.lang
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+  VueAutoPopupTest::class,
+  VueCompletionTest::class,
+  VueExtractComponentTest::class,
+  VueNewComponentTest::class,
+  VueRenameTest::class,
+)
+class VuePluginTestSuite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+  VueAutoPopupTest.WithoutServiceTest::class,
+  VueCompletionTest.WithoutServiceTest::class,
+  VueExtractComponentTest.WithoutServiceTest::class,
+  VueNewComponentTest.WithoutServiceTest::class,
+  VueRenameTest.WithoutServiceTest::class,
+)
+class VueWithoutServiceTestSuite
