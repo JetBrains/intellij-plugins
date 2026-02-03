219282fd28ca8c6be83e6add62f8869343db41da
Aleksei Berezkin <aleksei.berezkin@jetbrains.com>
2026-02-03 11:08:12 +0100

    WEB-76504 [js] TS7: Deprecate, remove support for `baseUrl`
    GitOrigin-RevId: b1d1691c9b3c8297da78b216c48c7f6b835119d3

--- Angular/angular-backend/src/org/angular2/css/refs/Angular2CssFileReferenceHelper.kt
+++ Angular/angular-backend/src/org/angular2/css/refs/Angular2CssFileReferenceHelper.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2023 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.angular2.css.refs
 
+import com.intellij.javascript.library.JSLibraryVersions
 import com.intellij.lang.ecmascript6.resolve.JSFileReferencesUtil
 import com.intellij.lang.javascript.frameworks.modules.langs.TildeFileSystemItemCompletion
 import com.intellij.lang.javascript.frameworks.modules.langs.getSyntheticResolveContext
@@ -112,7 +113,10 @@ private class Angular2OverrideContextFilesProvider(
 private fun getOverrideContextFiles(angularProject: AngularProject, element: PsiElement): MutableSet<VirtualFile> {
   val result = mutableSetOf<VirtualFile>()
   val tsConfig = TypeScriptConfigService.Provider.parseConfigFile(element.project, angularProject.tsConfigFile)
-  val baseUrl = tsConfig?.baseUrl
+  val baseUrl = if (JSLibraryVersions.isTS7OrNewer(element.project, PsiUtilCore.getVirtualFile(element)))
+    null
+  else
+    tsConfig?.baseUrl
   if (baseUrl != null) {
     result.add(baseUrl)
   }
