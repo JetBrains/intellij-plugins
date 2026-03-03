d9b1c91a7604b63a53d71542159335815c745236
Ilya Korennoy <ilya.korennoy@jetbrains.com>
2026-03-03 15:23:05 +0400

    IJPL-237424 Support correct comparsion for exclusion conditions
    GitOrigin-RevId: 441a07d0333d8ebef1509d8bc92901c87ab0be37

--- Deno/src/com/intellij/deno/roots/DenoWorkspaceFileIndexContributor.kt
+++ Deno/src/com/intellij/deno/roots/DenoWorkspaceFileIndexContributor.kt
@@ -2,10 +2,13 @@ package com.intellij.deno.roots
 
 import com.intellij.deno.entities.DenoEntity
 import com.intellij.deno.excludeCondition
+import com.intellij.openapi.roots.SyntheticLibrary
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.platform.backend.workspace.virtualFile
 import com.intellij.platform.workspace.storage.EntityStorage
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileIndexContributor
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileKind
+import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetExclusionCondition
 import com.intellij.workspaceModel.core.fileIndex.WorkspaceFileSetRegistrar
 import com.intellij.workspaceModel.core.fileIndex.impl.ModuleOrLibrarySourceRootData
 
@@ -17,11 +20,22 @@ class DenoWorkspaceFileIndexContributor : WorkspaceFileIndexContributor<DenoEnti
     if (!useWorkspaceModel()) return
     if (entity.denoTypes == null && entity.depsFile == null) return
     val allRootUrls = listOfNotNull(entity.denoTypes, entity.depsFile)
-    val predicate = excludeCondition.transformToCondition(allRootUrls.mapNotNull { it.virtualFile })
+    val allRootFiles = allRootUrls.mapNotNull { it.virtualFile }
+    val exclusionCondition = SyntheticLibraryExcludeFileSetCondition(excludeCondition, allRootFiles)
     allRootUrls.forEach { url ->
       registrar.registerFileSet(url, WorkspaceFileKind.EXTERNAL_SOURCE, entity, DenoFileSetData())
-      registrar.registerExclusionCondition(url, { predicate.value(it) }, entity)
+      registrar.registerExclusionCondition(url, exclusionCondition, entity)
     }
   }
 }
+
+private data class SyntheticLibraryExcludeFileSetCondition(
+  private val condition: SyntheticLibrary.ExcludeFileCondition,
+  private val allRoots: Collection<VirtualFile>,
+) : WorkspaceFileSetExclusionCondition {
+  private val transformedCondition = condition.transformToCondition(allRoots)
+
+  override fun shouldExclude(file: VirtualFile): Boolean = transformedCondition.value(file)
+}
+
 private class DenoFileSetData : ModuleOrLibrarySourceRootData
