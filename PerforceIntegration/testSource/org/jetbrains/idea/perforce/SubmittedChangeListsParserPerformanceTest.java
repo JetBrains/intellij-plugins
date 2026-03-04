7659626f1f2d99dfa58e9c8e38e5365024ae3763
Maxim.Kolmakov <maxim.kolmakov@jetbrains.com>
2026-03-04 15:27:45 +0100

    IJPL-237679 test perforce: Disable test
    GitOrigin-RevId: abdc6685730deb6303ce48c99e8eb175aea3e8f2

--- PerforceIntegration/testSource/org/jetbrains/idea/perforce/SubmittedChangeListsParserPerformanceTest.java
+++ PerforceIntegration/testSource/org/jetbrains/idea/perforce/SubmittedChangeListsParserPerformanceTest.java
@@ -1,9 +1,11 @@
 package org.jetbrains.idea.perforce;
 
+import com.intellij.idea.IgnoreJUnit3;
 import com.intellij.openapi.application.PathManager;
 import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.testFramework.PerformanceUnitTest;
 import org.jetbrains.idea.perforce.perforce.OutputMessageParser;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.File;
@@ -14,6 +16,7 @@ import static org.junit.Assert.assertTrue;
 @PerformanceUnitTest
 public class SubmittedChangeListsParserPerformanceTest {
   @Test
+  @Ignore("https://youtrack.jetbrains.com/issue/IJPL-237679/Failing-test-org.jetbrains.idea.perforce.SubmittedChangeListsParserPerformanceTest.test")
   public void test() throws IOException {
     File testData = new File(PathManager.getHomePath() + "/plugins/PerforceIntegration/testData/changes.txt");
     final String output = FileUtil.loadFile(testData);
