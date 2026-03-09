ff343fb26297b873f8b773467f886b08954d5f0e
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2026-03-09 21:40:01 +0500

    IJPL-173371 [terraform]: fix finding required providers in `getTerraformBlockWithProvidersOrFirst` and adjust test
    GitOrigin-RevId: 1789146a3d35857fb2c32dc09b901fffe399100e

--- terraform/test-data/terraform/addRequiredProviders/addProviderToAnotherFile/providers.after.tf
+++ terraform/test-data/terraform/addRequiredProviders/addProviderToAnotherFile/providers.after.tf
@@ -1,9 +1,6 @@
 terraform {
+
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.17.0"
-    }
     alicloud = {
       source  = "aliyun/alicloud"
       version = "1.261.0"
