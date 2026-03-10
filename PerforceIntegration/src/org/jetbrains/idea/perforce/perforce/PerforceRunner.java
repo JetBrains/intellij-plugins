78b0d130b6b259e54a82b90ac1d60a504d0fd0d3
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-03-10 13:46:27 +0100

    refactoring [*]: getting rid of async file deletion
    GitOrigin-RevId: 4a9367ab4520d3daecd72bf4c53094f8768faa85

--- PerforceIntegration/src/org/jetbrains/idea/perforce/perforce/PerforceRunner.java
+++ PerforceIntegration/src/org/jetbrains/idea/perforce/perforce/PerforceRunner.java
@@ -1,18 +1,4 @@
-/*
- * Copyright 2000-2006 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.idea.perforce.perforce;
 
 import com.google.common.collect.Lists;
@@ -1736,7 +1722,7 @@ public final class PerforceRunner implements PerforceRunnerI {
     }
     finally {
       if (tempFile != null) {
-        FileUtil.asyncDelete(tempFile);
+        FileUtil.delete(tempFile);
       }
     }
 
