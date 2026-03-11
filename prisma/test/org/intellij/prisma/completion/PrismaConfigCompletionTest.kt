d23158942929509a1131b3326f30bea9396ac1cd
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2026-03-11 04:43:13 +0200

    WEB-77155 [js]: disable PrismaConfigCompletionTest.testConfigSplitSchema
    GitOrigin-RevId: a71ee04a2d37b47de07285afef28a344a33a61f6

--- prisma/test/org/intellij/prisma/completion/PrismaConfigCompletionTest.kt
+++ prisma/test/org/intellij/prisma/completion/PrismaConfigCompletionTest.kt
@@ -18,24 +18,26 @@ class PrismaConfigCompletionTest : PrismaIntegrationTestBase() {
 
   override fun getCoroutineTimeout(): Duration = Duration.ofMinutes(4)
 
-  fun testConfigSplitSchema() = runBlockingCancellable {
-    myFixture.copyDirectoryToProject(getTestName(true), "")
-    performNpmInstallForPackageJson("package.json")
-    val schemaDir = "prisma/schema/subDir"
-    myFixture.configureFromTempProjectFile("$schemaDir/enum.prisma")
-    val config = PrismaConfigManager.getInstance(project).getConfigForFile(myFixture.file.virtualFile, true)
-    check(config != null)
-    assertTrue(config.data?.schema?.let { PathUtil.toSystemIndependentName(it) }?.endsWith(schemaDir) == true)
-    val lookupElements = myFixture.complete(CompletionType.BASIC, 1)!!
-      .toList()
-      .filter { it.lookupString !in PrismaConstants.PrimitiveTypes.ALL }
-    assertSameElements(
-      lookupElements.map { it.lookupString },
-      "AuthRole",
-      "Session",
-      "UserView",
-      "VerificationToken",
-      "Test",
-    )
-  }
+  // FIXME: WEB-77155
+  //
+  //fun testConfigSplitSchema() = runBlockingCancellable {
+  //  myFixture.copyDirectoryToProject(getTestName(true), "")
+  //  performNpmInstallForPackageJson("package.json")
+  //  val schemaDir = "prisma/schema/subDir"
+  //  myFixture.configureFromTempProjectFile("$schemaDir/enum.prisma")
+  //  val config = PrismaConfigManager.getInstance(project).getConfigForFile(myFixture.file.virtualFile, true)
+  //  check(config != null)
+  //  assertTrue(config.data?.schema?.let { PathUtil.toSystemIndependentName(it) }?.endsWith(schemaDir) == true)
+  //  val lookupElements = myFixture.complete(CompletionType.BASIC, 1)!!
+  //    .toList()
+  //    .filter { it.lookupString !in PrismaConstants.PrimitiveTypes.ALL }
+  //  assertSameElements(
+  //    lookupElements.map { it.lookupString },
+  //    "AuthRole",
+  //    "Session",
+  //    "UserView",
+  //    "VerificationToken",
+  //    "Test",
+  //  )
+  //}
 }
