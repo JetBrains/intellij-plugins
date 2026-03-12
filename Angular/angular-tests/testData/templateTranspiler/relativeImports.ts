e1bed0af8b13bfc3c7ef04bebf52123f2f7e8db1
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-03-12 09:28:26 +0100

    WEB-75092 Angular: during TCB generation fallback to relative paths if regular resolve fails. Don't throw errors, log them.
    (cherry picked from commit 0eab22dabf76a0c553784b3b73c377805cca959f)
    
    IJ-CR-195825
    
    GitOrigin-RevId: b0910d069b3ce315f13c04e707e3a2873873e037

---
+++ Angular/angular-tests/testData/templateTranspiler/relativeImports.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'robot-profile',
+  standalone: true,
+  imports: [IonicModule],
+  template: `
+    <div routerLink="goo"></div>
+ `
+})
+export class RobotProfileComponent {
+}
