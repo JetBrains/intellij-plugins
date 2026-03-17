b4e683de24b67abe0055732a890488920715a3aa
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2026-03-17 13:06:44 +0200

    [starter] AT-2967 Compile time dependency on dedicated IDE info
    GitOrigin-RevId: e66f4a9d56ad7a2ef698a2465702e2c6f1e01c28

--- qodana/rust/tests/IntegrationTest.kt
+++ qodana/rust/tests/IntegrationTest.kt
@@ -1,12 +1,13 @@
 package org.intellij.qodana.rust
 
 import com.intellij.ide.starter.extended.allure.Subsystems
+import com.intellij.ide.starter.models.IdeInfo
 import com.intellij.ide.starter.ide.IDETestContext
-import com.intellij.ide.starter.ide.IdeProductProvider
 import com.intellij.ide.starter.models.TestCase
 import com.intellij.ide.starter.project.LocalProjectInfo
 import com.intellij.openapi.application.PathManager
 import com.intellij.rustrover.integration.testFramework.createRustRoverTestContext
+import com.intellij.tools.ide.starter.build.server.rustrover.RustRover
 import java.nio.file.Path
 import kotlin.io.path.div
 
@@ -17,7 +18,7 @@ abstract class IntegrationTest : utilities.qodana.IntegrationTest() {
 
     override fun createTestContext(projectDir: Path): IDETestContext =
         createRustRoverTestContext(
-            testName,
-            TestCase(IdeProductProvider.RR, LocalProjectInfo(projectDir)),
+          testName,
+          TestCase(IdeInfo.RustRover, LocalProjectInfo(projectDir)),
         )
 }
