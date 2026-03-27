a5de99f63d8ba035c49d1114e6b011bc4b9deaa3
Nikita Kudrin <nikita.kudrin@jetbrains.com>
2026-03-27 10:28:42 +0200

    [starter] AT-2967 Fixes after rebase
    GitOrigin-RevId: 3fa498f4ed0b64f7fda5edb6bbffa3cd5813130a

--- qodana/cpp/tests/CppIntegrationTest.kt
+++ qodana/cpp/tests/CppIntegrationTest.kt
@@ -1,14 +1,15 @@
 package org.jetbrains.qodana.cpp
 
-import com.intellij.ide.starter.extended.CppTestContext
-import com.intellij.ide.starter.extended.LanguageEngine
 import com.intellij.ide.starter.extended.allure.Subsystems
-import com.intellij.ide.starter.extended.getCLionContext
+import com.intellij.ide.starter.extended.clion.CppTestContext
+import com.intellij.ide.starter.extended.clion.LanguageEngine
+import com.intellij.ide.starter.extended.clion.getCLionContext
 import com.intellij.ide.starter.ide.IDETestContext
-import com.intellij.ide.starter.ide.IdeProductProvider
+import com.intellij.ide.starter.models.IdeInfo
 import com.intellij.ide.starter.models.TestCase
 import com.intellij.ide.starter.project.LocalProjectInfo
 import com.intellij.openapi.application.PathManager
+import com.intellij.tools.ide.starter.build.server.clion.CLion
 import utilities.qodana.IntegrationTest
 import java.nio.file.Path
 import kotlin.io.path.div
@@ -21,7 +22,7 @@ abstract class CppIntegrationTest : IntegrationTest() {
   override fun createTestContext(projectDir: Path): IDETestContext =
     getCLionContext(
       testName,
-      TestCase(IdeProductProvider.CL, LocalProjectInfo(projectDir)),
+      TestCase(IdeInfo.CLion, LocalProjectInfo(projectDir)),
       context = CppTestContext.CLION,
       engine = LanguageEngine.NOVA,
     )
