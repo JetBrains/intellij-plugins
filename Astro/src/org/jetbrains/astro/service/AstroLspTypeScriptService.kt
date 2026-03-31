e391274758c98d1badb52553d1dbce425a7edb19
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2026-03-31 12:43:42 +0300

    WEB-77350 astro: restart daemon on diagnostics to ensure updated highlights
    GitOrigin-RevId: d4f0809f367228697bbb2332bf6aec7f38a75f6c

--- Astro/src/org/jetbrains/astro/service/AstroLspTypeScriptService.kt
+++ Astro/src/org/jetbrains/astro/service/AstroLspTypeScriptService.kt
@@ -2,6 +2,7 @@
 package org.jetbrains.astro.service
 
 import com.intellij.codeInsight.completion.CompletionParameters
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.lang.javascript.integration.JSAnnotationError
 import com.intellij.lang.typescript.compiler.TypeScriptService.CompletionMergeStrategy
@@ -9,15 +10,38 @@ import com.intellij.lang.typescript.lsp.JSFrameworkLspTypeScriptService
 import com.intellij.lang.typescript.lsp.LspAnnotationError
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.platform.lsp.api.LspServer
+import com.intellij.platform.lsp.api.LspServerManager
+import com.intellij.platform.lsp.api.LspServerManagerListener
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiManager
 import com.intellij.psi.xml.XmlTag
 import com.intellij.psi.xml.XmlTokenType
+import com.intellij.util.application
 
 class AstroLspTypeScriptService(project: Project)
   : JSFrameworkLspTypeScriptService(project, AstroLspServerSupportProvider::class.java, AstroLspServerActivationRule) {
   override val name: String = "Astro LSP"
   override val prefix: String = "Astro"
+
+  init {
+    // Volar-based language server sends publishDiagnostics in two phases (syntactic, then semantic).
+    // The platform's awaitPendingDiagnostics mechanism cannot reliably synchronize with multi-phase delivery,
+    // so we restart the daemon when diagnostics arrive to trigger JSLanguageServiceHighlightingPass with fresh data.
+    // See: https://github.com/volarjs/volar.js/blob/master/packages/language-service/lib/features/provideDiagnostics.ts
+    LspServerManager.getInstance(project).addLspServerManagerListener(object : LspServerManagerListener {
+      override fun diagnosticsReceived(lspServer: LspServer, file: VirtualFile) {
+        if (lspServer.providerClass != AstroLspServerSupportProvider::class.java) return
+        application.runReadAction {
+          PsiManager.getInstance(project).findFile(file)?.let { psiFile ->
+            DaemonCodeAnalyzer.getInstance(project).restart(psiFile, "AstroLspTypeScriptService.diagnosticsReceived")
+          }
+        }
+      }
+    }, this)
+  }
 
   override fun getCompletionMergeStrategy(parameters: CompletionParameters, file: PsiFile, context: PsiElement): CompletionMergeStrategy = CompletionMergeStrategy.MERGE
 
