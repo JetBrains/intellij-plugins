083ec961301e242a026e4a1774f6333a3402e8cb
Anna Zhukova <anna.zhukova@jetbrains.com>
2026-04-01 16:12:07 +0000

    QD-14082 Qodana Rust: update test projects to edition 2024, add reproduction test
    Merge-request: IJ-MR-197661
    Merged-by: Anna Zhukova <anna.zhukova@jetbrains.com>
    
    (cherry picked from commit 3926bfc221a9f6aa03f4c994c2f22bb4458b494e)
    
    IJ-MR-197661
    
    GitOrigin-RevId: d009b4470e2431f651893da38538910b9af233df

--- qodana/rust/tests/AnalysisTest.kt
+++ qodana/rust/tests/AnalysisTest.kt
@@ -1,12 +1,15 @@
 package org.intellij.qodana.rust
 
 import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import org.junit.jupiter.api.DisplayNameGeneration
 import org.junit.jupiter.api.DisplayNameGenerator
 import org.junit.jupiter.api.Test
+import kotlin.io.path.div
+import kotlin.io.path.writeText
 
 @DisplayNameGeneration(DisplayNameGenerator.Simple::class)
 class AnalysisTest : IntegrationTest() {
@@ -69,6 +72,35 @@ class AnalysisTest : IntegrationTest() {
     result.ok.shouldBeTrue()
     result.findIssue("RsUnusedImport").shouldNotBeNull()
     result.findIssue("RsUnnecessaryReturn").shouldNotBeNull()
+  }
+
+  /**
+   * Reproduces the intermittent CI failure where `basic` and `dirty project` produce zero results.
+   *
+   * Root cause (from CI build 905876656 logs):
+   * Some CI agents have an old Cargo that doesn't support `edition = "2024"`.
+   * `cargo metadata` fails with: "this version of Cargo is older than the `2024` edition,
+   * and only supports `2015`, `2018`, and `2021` editions."
+   * The Cargo project model fails to load, and Qodana analysis produces zero inspection results.
+   *
+   * This test simulates the same failure by using `edition = "2099"` (unsupported by any Cargo version).
+   */
+  @Test
+  fun `no results when cargo does not support the project edition`() {
+    val workdir = checkout("rust-test")
+    (workdir / "Cargo.toml").writeText("""
+      [package]
+      name = "rust-test"
+      version = "0.1.0"
+      edition = "2099"
+
+      [dependencies]
+    """.trimIndent())
+
+    val result = analyze(workdir)
+
+    result.ok.shouldBeTrue()
+    result.results!!.filter { it.ruleId?.startsWith("Rs") == true }.shouldBeEmpty()
   }
 
 }
