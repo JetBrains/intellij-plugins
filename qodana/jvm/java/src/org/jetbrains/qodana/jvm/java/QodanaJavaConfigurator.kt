68250e523829837de64d4fb041b44dba2529ec01
Andrei Iurko <andrei.iurko@jetbrains.com>
2026-04-03 14:42:06 +0300

    [qodana] QD-14291 get rid of race condition in Qodana JVM configuration
    (cherry picked from commit 989d138edfddc5f6c90f13c2061d19ec70eba61a)
    
    IJ-MR-199031
    
    GitOrigin-RevId: 406769153fb8d5393e6f6caedf3630043134a178

--- qodana/jvm/java/src/org/jetbrains/qodana/jvm/java/QodanaJavaConfigurator.kt
+++ qodana/jvm/java/src/org/jetbrains/qodana/jvm/java/QodanaJavaConfigurator.kt
@@ -1,27 +1,35 @@
-package org.jetbrains.qodana.staticAnalysis.workflow
+package org.jetbrains.qodana.jvm.java
 
 import com.intellij.openapi.application.edtWriteAction
+import com.intellij.openapi.components.service
+import com.intellij.openapi.diagnostic.ControlFlowException
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.projectRoots.JavaSdkType
 import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.projectRoots.Sdk
 import com.intellij.openapi.projectRoots.SdkType
 import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTracker
 import org.jetbrains.qodana.staticAnalysis.inspections.config.QodanaConfig
+import org.jetbrains.qodana.staticAnalysis.workflow.QodanaWorkflowExtension
 
-private val LOG = logger<QodanaInvalidSdkCleaner>()
+private val LOG = logger<QodanaJavaConfigurator>()
 
-class QodanaInvalidSdkCleaner : QodanaWorkflowExtension {
+class QodanaJavaConfigurator : QodanaWorkflowExtension {
   override suspend fun beforeProjectOpened(config: QodanaConfig) {
-    val jdkTable = ProjectJdkTable.getInstance()
-    val invalidSdks = jdkTable.allJdks.filter { isInvalidSdk(it) }
-    if (invalidSdks.isEmpty()) return
+    removeInvalidJdks()
+    service<QodanaConfigJdkService>().configureJdk(config)
+  }
 
-    for (sdk in invalidSdks) {
-      LOG.info("Removing invalid cached SDK '${sdk.name}' (homePath: ${sdk.homePath})")
-    }
+  private suspend fun removeInvalidJdks() {
+    val jdkTable = ProjectJdkTable.getInstance()
+    val invalidSdks = jdkTable.allJdks
+      .filter { isInvalidSdk(it) }
+      .filter { it.sdkType is JavaSdkType }
+    if (invalidSdks.isEmpty()) return
 
     edtWriteAction {
       for (sdk in invalidSdks) {
+        LOG.info("Removing invalid cached SDK '${sdk.name}' (homePath: ${sdk.homePath})")
         jdkTable.removeJdk(sdk)
       }
     }
@@ -37,6 +45,7 @@ class QodanaInvalidSdkCleaner : QodanaWorkflowExtension {
       homePath == null || !sdkType.isValidSdkHome(homePath)
     }
     catch (e: Exception) {
+      if (e is ControlFlowException) throw e
       LOG.warn("Failed to validate SDK '${sdk.name}': ${e.message}", e)
       false // match platform behavior: keep SDK if validation throws
     }
