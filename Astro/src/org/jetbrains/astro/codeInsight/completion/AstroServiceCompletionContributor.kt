86de8b9f2144e00a16aa14c4542a78cbc4340bd9
Konstantin Ulitin <konstantin.ulitin@jetbrains.com>
2026-04-08 06:04:50 +0200

    WEB-77525 js: disable redundant TypeScript service completion for unqualified reference expressions
    GitOrigin-RevId: 58f40b298c2d49c06cacb6587edd2dec43fccc1c

--- Astro/src/org/jetbrains/astro/codeInsight/completion/AstroServiceCompletionContributor.kt
+++ Astro/src/org/jetbrains/astro/codeInsight/completion/AstroServiceCompletionContributor.kt
@@ -3,11 +3,18 @@ package org.jetbrains.astro.codeInsight.completion
 
 import com.intellij.codeInsight.completion.CompletionParameters
 import com.intellij.codeInsight.completion.CompletionResultSet
+import com.intellij.lang.javascript.JSDocTokenTypes
 import com.intellij.lang.typescript.compiler.languageService.ide.TypeScriptServiceCompletionContributor
 import com.intellij.openapi.util.registry.Registry
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 
 class AstroServiceCompletionContributor : TypeScriptServiceCompletionContributor() {
   override val serviceItemsLimit: Int get() = Registry.get("astro.language.server.completion.serviceItemsLimit").asInteger()
+
+  override fun isServiceCompletionRedundant(position: PsiElement): Boolean {
+    return position.elementType == JSDocTokenTypes.DOC_TAG_NAME
+  }
 
   override fun fillCompletionVariants(parameters: CompletionParameters, resultSet: CompletionResultSet) {
     super.fillCompletionVariants(parameters, resultSet)
