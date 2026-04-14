73dbab8992fbaa4dc03bcee852b069d450c5f587
Victor Turansky <victor.turansky@jetbrains.com>
2026-04-14 21:35:56 +0200

    [vue] WEB-74847 `VueResolveTest` - common base class for tests with plugin
    GitOrigin-RevId: dab90ccdc7871d0215c9c6084ef4757a0d2c8861

--- vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueResolveTest.kt
+++ vuejs/vuejs-tests/src/org/jetbrains/vuejs/lang/VueResolveTest.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2024 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang
 
+import com.intellij.lang.javascript.TrackFailedTestRule
 import com.intellij.lang.javascript.psi.JSArrayLiteralExpression
 import com.intellij.lang.javascript.psi.JSCallExpression
 import com.intellij.lang.javascript.psi.JSFunctionItem
@@ -44,20 +45,33 @@ import org.jetbrains.vuejs.model.VueModelManager
 import org.jetbrains.vuejs.model.VueSymbol
 import org.jetbrains.vuejs.web.scopes.VueBindingShorthandSymbol
 import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @Ignore
 class VueResolveTest :
-  VueResolveTestBase() {
+  VueResolveWithPluginTestBase() {
 
   @Ignore
   class WithLegacyPluginTest :
-    VueResolveTestBase(testMode = VueTestMode.LEGACY_PLUGIN)
+    VueResolveWithPluginTestBase(testMode = VueTestMode.LEGACY_PLUGIN)
 
   class WithoutServiceTest :
     VueResolveTestBase(testMode = VueTestMode.NO_PLUGIN)
+}
+
+abstract class VueResolveWithPluginTestBase(
+  testMode: VueTestMode = VueTestMode.DEFAULT,
+) : VueResolveTestBase(testMode = testMode) {
+
+  @Rule
+  @JvmField
+  val rule: TestRule = TrackFailedTestRule(
+    "testResolveLocallyInsideComponent",
+  )
 }
 
 @RunWith(JUnit4::class)
