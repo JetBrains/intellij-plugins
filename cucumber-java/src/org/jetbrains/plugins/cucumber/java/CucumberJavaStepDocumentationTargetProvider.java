7eaad1e0411873279500a07f73166708bd922697
Louis Vignier <louis.vignier@jetbrains.com>
2026-04-17 16:56:26 +0200

    IDEA-324298 cucumber: fix javadoc tests after API migration
    GitOrigin-RevId: 7bdd813655428984b7c7e45e0a42a45f7cfb7da6

--- cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJavaStepDocumentationTargetProvider.java
+++ cucumber-java/src/org/jetbrains/plugins/cucumber/java/CucumberJavaStepDocumentationTargetProvider.java
@@ -1,13 +1,13 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.plugins.cucumber.java;
 
-import com.intellij.lang.documentation.psi.UtilKt;
+import com.intellij.lang.java.JavaDocumentationTarget;
 import com.intellij.platform.backend.documentation.DocumentationTarget;
-import com.intellij.platform.backend.documentation.PsiDocumentationTargetProvider;
+import com.intellij.platform.backend.documentation.DocumentationTargetProvider;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.jetbrains.plugins.cucumber.psi.GherkinStep;
 import org.jetbrains.plugins.cucumber.steps.AbstractStepDefinition;
 
@@ -16,20 +16,23 @@ import java.util.List;
 
 /// Provides documentation for Gherkin steps by resolving them to their Java step definitions.
 ///
-/// It's invoked when the user triggers Quick Documentation on an already-typed ste in a .feature file.
-public final class CucumberJavaStepDocumentationTargetProvider implements PsiDocumentationTargetProvider {
+/// Invoked when the user triggers Quick Documentation on an already-typed step in a .feature file.
+public final class CucumberJavaStepDocumentationTargetProvider implements DocumentationTargetProvider {
   @Override
-  public @Nullable DocumentationTarget documentationTarget(@NotNull PsiElement element, @Nullable PsiElement originalElement) {
+  public @NotNull List<? extends @NotNull DocumentationTarget> documentationTargets(@NotNull PsiFile file, int offset) {
+    PsiElement originalElement = file.findElementAt(offset);
+    if (originalElement == null) return List.of();
+
     GherkinStep step = PsiTreeUtil.getParentOfType(originalElement, GherkinStep.class, false);
-    if (step == null) return null;
+    if (step == null) return List.of();
 
     List<AbstractStepDefinition> definitions = new ArrayList<>(step.findDefinitions());
-    if (definitions.isEmpty()) return null;
+    if (definitions.isEmpty()) return List.of();
 
     AbstractStepDefinition definition = definitions.getFirst();
     PsiElement navigationElement = definition.getNavigationElement();
-    if (navigationElement == null) return null;
+    if (navigationElement == null) return List.of();
 
-    return UtilKt.createPsiDocumentationTarget(navigationElement, originalElement);
+    return List.of(new JavaDocumentationTarget(navigationElement, originalElement, false));
   }
 }
