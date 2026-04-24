11a74578e272924c46470bc3d8e9707823c254d3
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-04-24 11:17:48 +0200

    WEB-77722 Kolar: make KolarTranspilerService thread-safe
    GitOrigin-RevId: 93fc230091948408488bc4e7d41a62fac8c4b5ef

--- Angular/angular-backend/src/org/angular2/lang/expr/service/kolar/AngularKolarTranspiler.kt
+++ Angular/angular-backend/src/org/angular2/lang/expr/service/kolar/AngularKolarTranspiler.kt
@@ -1,30 +1,37 @@
 package org.angular2.lang.expr.service.kolar
 
 import com.intellij.ide.highlighter.HtmlFileType
+import com.intellij.lang.javascript.TypeScriptFileType
 import com.intellij.lang.javascript.modules.NodeModuleUtil
+import com.intellij.lang.typescript.kolar.CodeMapping
+import com.intellij.lang.typescript.kolar.KolarCodeInformation
 import com.intellij.lang.typescript.kolar.KolarCodegenContext
 import com.intellij.lang.typescript.kolar.KolarScriptSnapshot
-import com.intellij.lang.typescript.kolar.KolarServiceScript
 import com.intellij.lang.typescript.kolar.KolarTranspiler
 import com.intellij.lang.typescript.kolar.KolarVirtualCode
+import com.intellij.lang.typescript.kolar.sourceMap.KolarMapping
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
+import com.intellij.testFramework.LightVirtualFile
 import com.intellij.util.asSafely
 import com.intellij.util.indexing.SubstitutedFileType
 import org.angular2.Angular2DecoratorUtil.isHostBindingExpression
 import org.angular2.entities.Angular2EntitiesProvider
 import org.angular2.lang.expr.Angular2ExprDialect
+import org.angular2.lang.expr.service.tcb.Angular2TemplateTranspiler.SourceMappingFlag
 import org.angular2.lang.expr.service.tcb.Angular2TranspiledDirectiveFileBuilder
 import org.angular2.lang.html.Angular2HtmlDialect
 import org.intellij.images.fileTypes.impl.SvgFileType
+import java.util.EnumSet
 
 private const val ANGULAR_HTML_LANG = "angular-html"
 
-internal class AngularKolarTranspiler(private val project: Project) : KolarTranspiler<AngularVirtualCode> {
+internal class AngularKolarTranspiler(private val project: Project) : KolarTranspiler {
 
   override fun getLanguageId(file: VirtualFile): String? =
     if (isAcceptableHtmlFile(file)) ANGULAR_HTML_LANG else null
@@ -45,40 +52,14 @@ internal class AngularKolarTranspiler(private val project: Project) : KolarTrans
     languageId: String,
     snapshot: KolarScriptSnapshot,
     ctx: KolarCodegenContext,
-  ): AngularVirtualCode? {
+  ): KolarVirtualCode? {
     val fileName = file.name
     if (languageId == "typescript" &&
         !fileName.endsWith(".d.ts") &&
         !NodeModuleUtil.hasNodeModulesDirInPath(file, null)) {
-      val virtualCode = AngularVirtualCode(file)
-      return updateVirtualCode(file, virtualCode, snapshot, ctx)
+      return buildVirtualCode(file, snapshot, ctx)
     }
     return null
-  }
-
-  override fun updateVirtualCode(
-    file: VirtualFile,
-    virtualCode: AngularVirtualCode,
-    newSnapshot: KolarScriptSnapshot,
-    ctx: KolarCodegenContext,
-  ): AngularVirtualCode {
-    val transpiledFile = if (ApplicationManager.getApplication().isReadAccessAllowed)
-      getTranspiledDirectiveFile(file)
-    else
-      ReadAction.computeCancellable<Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile?, Throwable> {
-        getTranspiledDirectiveFile(file)
-      }
-    return virtualCode.sourceFileUpdated(newSnapshot, ctx, transpiledFile)
-  }
-
-  override fun getServiceScript(root: KolarVirtualCode): KolarServiceScript? {
-    if (root !is AngularVirtualCode) return null
-
-    return KolarServiceScript(
-      code = root,
-      preventLeadingOffset = root.preventLeadingOffset,
-      fileName = root.file.path
-    )
   }
 
   private fun getTranspiledDirectiveFile(file: VirtualFile): Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile? =
@@ -91,4 +72,192 @@ internal class AngularKolarTranspiler(private val project: Project) : KolarTrans
         .asSafely<SubstitutedFileType>()?.language is Angular2HtmlDialect
     }
 
+  fun buildVirtualCode(
+    file: VirtualFile,
+    snapshot: KolarScriptSnapshot,
+    ctx: KolarCodegenContext,
+  ): KolarVirtualCode {
+    val transpiledFile = if (ApplicationManager.getApplication().isReadAccessAllowed)
+      getTranspiledDirectiveFile(file)
+    else
+      ReadAction.computeCancellable<Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile?, Throwable> {
+        getTranspiledDirectiveFile(file)
+      }
+    val fileName = file.path
+    if (transpiledFile != null && isTemplateInSync(snapshot, ctx, transpiledFile, fileName)) {
+      val newMappings = mutableListOf<CodeMapping>()
+      val newAssociatedScriptMappings = mutableMapOf<String, MutableList<CodeMapping>>()
+
+      transpiledFile.fileMappings.values.forEach { fileMapping ->
+        val normalizedMappingFileName = fileMapping.fileName.replace('\\', '/')
+        val mappingsWithData: MutableList<CodeMapping> = if (normalizedMappingFileName == fileName.replace('\\', '/')) {
+          newMappings
+        }
+        else {
+          // Do not register source -> generated mappings for external files
+          val scriptId = if (fileMapping.externalFile) {
+            normalizedMappingFileName
+          }
+          else {
+            ctx.getAssociatedScript(fileMapping.fileName)?.id
+          }
+
+          if (scriptId != null) {
+            newAssociatedScriptMappings.computeIfAbsent(scriptId) { mutableListOf() }
+          }
+          else {
+            return@forEach
+          }
+        }
+
+        // Split the mapping set for Volar - iterate over source mappings
+        fileMapping.sourceMappings.filter { !it.ignored }.forEach { sourceMapping ->
+          val sourceOffset = sourceMapping.sourceOffset
+          val sourceLength = sourceMapping.sourceLength
+          val diagnosticsOffset = sourceMapping.diagnosticsOffset ?: -1
+          val diagnosticsLength = sourceMapping.diagnosticsLength ?: -1
+          val generatedOffset = sourceMapping.generatedOffset
+          val generatedLength = sourceMapping.generatedLength
+
+          mappingsWithData.add(
+            KolarMapping(
+              sourceOffsets = intArrayOf(sourceOffset),
+              lengths = intArrayOf(sourceLength),
+              generatedOffsets = intArrayOf(generatedOffset),
+              generatedLengths = intArrayOf(generatedLength),
+              data = createCodeInformation(sourceMapping.flags, diagnosticsOffset == sourceOffset && diagnosticsLength == sourceLength)
+            )
+          )
+
+          if (diagnosticsOffset >= 0 && (diagnosticsOffset != sourceOffset || diagnosticsLength != sourceLength)) {
+            mappingsWithData.add(
+              KolarMapping(
+                sourceOffsets = intArrayOf(diagnosticsOffset),
+                lengths = intArrayOf(diagnosticsLength),
+                generatedOffsets = intArrayOf(generatedOffset),
+                generatedLengths = intArrayOf(generatedLength),
+                data = KolarCodeInformation(
+                  verification = KolarCodeInformation.VerificationInfo.Enabled,
+                  format = false,
+                  completion = null,
+                  navigation = null,
+                  semantic = null,
+                  structure = false
+                )
+              )
+            )
+          }
+        }
+      }
+      return KolarVirtualCode(
+        transpiler = this,
+        id = "main",
+        // Create a new non-physical VirtualFile for the transpiled template
+        virtualFile = createLightVirtualFileWithParent(fileName, transpiledFile.generatedCode),
+        languageId = "typescript",
+        snapshot = KolarScriptSnapshot.create(transpiledFile.generatedCode),
+        preventLeadingOffset = !requiresTrailingSpaces(snapshot, transpiledFile),
+        mappings = newMappings,
+        associatedScriptMappings = newAssociatedScriptMappings,
+      )
+    }
+    else {
+      return KolarVirtualCode(
+        transpiler = this,
+        id = "main",
+        // Point to the existing virtual file from the local filesystem if possible
+        virtualFile = file,
+        languageId = "typescript",
+        snapshot = snapshot,
+        preventLeadingOffset = false,
+        mappings = listOf(
+          KolarMapping(
+            generatedOffsets = intArrayOf(0),
+            sourceOffsets = intArrayOf(0),
+            lengths = intArrayOf(snapshot.length),
+            data = KolarCodeInformation(
+              format = true,
+              completion = KolarCodeInformation.CompletionInfo.Enabled,
+              navigation = KolarCodeInformation.NavigationInfo.Enabled,
+              semantic = KolarCodeInformation.SemanticInfo.Enabled,
+              structure = true,
+              verification = KolarCodeInformation.VerificationInfo.Enabled
+            )
+          )
+        ),
+        associatedScriptMappings = emptyMap(),
+      )
+    }
+  }
+
+  private fun isTemplateInSync(
+    snapshot: KolarScriptSnapshot,
+    ctx: KolarCodegenContext,
+    transpiledFile: Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile,
+    fileName: String,
+  ): Boolean {
+    val sourceText = snapshot.text
+    val normalizedFileName = fileName.replace('\\', '/')
+
+    // Find the file mapping for the current file
+    val currentFileMapping = transpiledFile.fileMappings.values.find {
+      it.fileName.replace('\\', '/') == normalizedFileName
+    } ?: return false
+
+    if (sourceText != currentFileMapping.sourceFile.text) {
+      return false
+    }
+
+    return transpiledFile.fileMappings.values.none { fileMapping ->
+      if (fileMapping.externalFile) {
+        false
+      }
+      else {
+        false
+        //val associatedSnapshot = ctx.getAssociatedScript(fileMapping.fileName)?.snapshot
+        //associatedSnapshot != null &&
+        //associatedSnapshot.getText(0, associatedSnapshot.getLength()) != fileMapping.sourceFile.text
+      }
+    }
+  }
+
+  private fun requiresTrailingSpaces(
+    snapshot: KolarScriptSnapshot,
+    transpiledFile: Angular2TranspiledDirectiveFileBuilder.TranspiledDirectiveFile,
+  ): Boolean {
+    val sourceText = snapshot.text
+    return !transpiledFile.generatedCode.startsWith(sourceText)
+  }
+
+  private fun createCodeInformation(flags: EnumSet<SourceMappingFlag>, verification: Boolean): KolarCodeInformation {
+    return KolarCodeInformation(
+      format = flags.contains(SourceMappingFlag.FORMAT),
+      completion = if (flags.contains(SourceMappingFlag.COMPLETION)) KolarCodeInformation.CompletionInfo.Enabled else null,
+      navigation = if (flags.contains(SourceMappingFlag.NAVIGATION)) KolarCodeInformation.NavigationInfo.Enabled else null,
+      semantic = if (flags.contains(SourceMappingFlag.SEMANTIC)) KolarCodeInformation.SemanticInfo.Enabled else null,
+      structure = flags.contains(SourceMappingFlag.STRUCTURE),
+      verification = if (verification) KolarCodeInformation.VerificationInfo.Enabled else null,
+      types = flags.contains(SourceMappingFlag.TYPES),
+      reverseTypes = flags.contains(SourceMappingFlag.REVERSE_TYPES),
+    )
+  }
+
+  private fun createLightVirtualFileWithParent(fullPath: String, content: String): VirtualFile {
+    val normalizedPath = fullPath.replace('\\', '/')
+    val lastSlashIndex = normalizedPath.lastIndexOf('/')
+
+    return if (lastSlashIndex > 0) {
+      val parentPath = normalizedPath.substring(0, lastSlashIndex)
+      val fileName = normalizedPath.substring(lastSlashIndex + 1)
+      val parentDir = LocalFileSystem.getInstance().findFileByPath(parentPath)
+
+      object : LightVirtualFile(fileName, TypeScriptFileType, content) {
+        override fun getParent(): VirtualFile? = parentDir
+        override fun getPath(): String = normalizedPath
+      }
+    }
+    else {
+      LightVirtualFile(normalizedPath, TypeScriptFileType, content)
+    }
+  }
 }
