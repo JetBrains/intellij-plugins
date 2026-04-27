08d23b96480dfd481a92259122f7444f374e592e
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2026-04-27 11:47:01 +0300

    IJPL-208964 [terraform]: new file type for Terraform Test files
    GitOrigin-RevId: afa47b5b8f2520092eb9420c281fb2213c664f8d

---
+++ terraform/src/org/intellij/terraform/test/TfTestFileType.kt
@@ -0,0 +1,27 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.intellij.terraform.test
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import com.intellij.openapi.util.NlsContexts
+import com.intellij.openapi.util.NlsSafe
+import org.intellij.terraform.TerraformIcons
+import org.intellij.terraform.hcl.HCLBundle
+import org.intellij.terraform.hcl.HCLLanguage
+import org.intellij.terraform.hcl.HCL_DEFAULT_EXTENSION
+import org.jetbrains.annotations.Nls
+import org.jetbrains.annotations.NonNls
+import javax.swing.Icon
+
+internal object TfTestFileType : LanguageFileType(HCLLanguage) {
+
+  override fun getName(): @NonNls String = "Terraform Test"
+
+  override fun getDescription(): @NlsContexts.Label String = HCLBundle.message("terraform.test.name")
+
+  override fun getDefaultExtension(): @NlsSafe String = HCL_DEFAULT_EXTENSION
+
+  // TODO: replace with new icon: UX-3758
+  override fun getIcon(): Icon = TerraformIcons.Terraform
+
+  override fun getDisplayName(): @Nls String = HCLBundle.message("terraform.test.name")
+}
