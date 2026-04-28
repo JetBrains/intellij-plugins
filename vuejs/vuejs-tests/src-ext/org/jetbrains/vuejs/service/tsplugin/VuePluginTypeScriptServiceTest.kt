881f26a0f6fa6a8b3d5716b10c36ed370367d205
Victor Turansky <victor.turansky@jetbrains.com>
2026-04-28 02:41:47 +0200

    [vue] WEB-73932 Cleanup
    GitOrigin-RevId: f1c4a73c1b96c2e971450cb82451212729ae4a0a

--- vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/service/tsplugin/VuePluginTypeScriptServiceTest.kt
+++ vuejs/vuejs-tests/src-ext/org/jetbrains/vuejs/service/tsplugin/VuePluginTypeScriptServiceTest.kt
@@ -30,7 +30,7 @@ class VuePluginTypeScriptServiceTest :
   override fun setUp() {
     super.setUp()
     myFixture.enableInspections(VueInspectionsProvider())
-    myFixture.configureByText(VueTsConfigFile.Companion.FILE_NAME, VueTsConfigFile.Companion.DEFAULT_TSCONFIG_CONTENT)
+    myFixture.configureByText(VueTsConfigFile.FILE_NAME, VueTsConfigFile.DEFAULT_TSCONFIG_CONTENT)
   }
 
   private fun configureVueDependencies(
