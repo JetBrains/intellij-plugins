0cf67d0095625ee655058b0a49a787397a8fc66f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-05-12 11:35:52 +0200

    WEB-77249 WEB-77481 WEB-77818 WEB-77846 Vue: fix caching on incorrect PsiElement in VueCodeModelSymbolScope.
    IJ-CR-204065
    
    GitOrigin-RevId: 236ceb9f006269b3d193772c93e83cc22ce41d1c

---
+++ vuejs/vuejs-tests/testData/typing/InvalidElementOnTyping.vue
@@ -0,0 +1,41 @@
+<template>
+   <view class="page">
+      <div class="item" v-for="(item, index) in dataList" :key="item.id || index">
+      </div>
+   </view>
+</template>
+
+<script>
+   const ZPMixin = {
+      methods: {
+         isPagingRefNotFound() {
+            return !this.$refs.paging;
+         },
+      },
+   };
+
+   export default {
+      mixins: [ZPMixin],
+      data() {<caret>
+         return {
+            dataList: [],
+            tabList: [
+               {
+                  name: '已确认',
+                  code: 5,
+               },
+            ],
+         };
+      },
+      computed: {},
+      methods: {
+         queryList(pageNo, pageSize) {
+         },
+      },
+      watch: {},
+
+      // 页面周期函数--监听页面加载
+      onLoad() {
+      }
+   };
+</script>
