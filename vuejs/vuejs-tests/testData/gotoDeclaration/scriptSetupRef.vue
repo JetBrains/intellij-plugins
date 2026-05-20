fbf51be747a5fc46915789d7e9928ed9fe9c1abe
Victor Turansky <victor.turansky@jetbrains.com>
2026-05-20 21:58:12 +0200

    WEB-78047 Vue: update `testScriptSetupRef` test data
    GitOrigin-RevId: 8a0cef4db9e8311f1df93832af331fa1458c179f

--- vuejs/vuejs-tests/testData/gotoDeclaration/scriptSetupRef.vue
+++ vuejs/vuejs-tests/testData/gotoDeclaration/scriptSetupRef.vue
@@ -1,13 +1,13 @@
+<script setup>
+import {ref} from "vue";
+
+const foo2 = ref("12")
+const foo3 = ref(12)
+</script>
+
 <template>
   <div ref='foo2'></div>
   <div ref='foo'></div>
   {{ $refs.foo2 }}
   {{ $refs.foo }}
 </template>
-
-<script setup lang='js'>
-import {ref} from "vue";
-
-const foo2 = ref("12")
-const foo3 = ref(12)
-</script>
