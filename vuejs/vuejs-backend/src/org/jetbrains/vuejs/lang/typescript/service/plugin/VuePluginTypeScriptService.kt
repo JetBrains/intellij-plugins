78daa3149f56bc86aa6e1e900aa7b8b7c95deece
Victor Turansky <victor.turansky@jetbrains.com>
2026-05-25 22:09:24 +0200

    WEB-78047 Vue: enable SPTE is Vue components with JS language
    GitOrigin-RevId: 9cf2e71da33a38a1fc47f87ea20fdbca4ad46ca2

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/service/plugin/VuePluginTypeScriptService.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/service/plugin/VuePluginTypeScriptService.kt
@@ -223,6 +223,7 @@ open class VuePluginTypeScriptService(
     element: PsiElement,
   ): Boolean =
     element.language is VueTSLanguage
+    || element.language is VueJSLanguage
     || super.supportsTypeEvaluation(virtualFile, element)
 
   private inner class VueCompilerServiceEvaluationSupport(
