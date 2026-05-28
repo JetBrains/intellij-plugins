11625b8c0c09e7beb931233fe73573cf4718143c
Andrei Iurko <andrei.iurko@jetbrains.com>
2026-05-28 13:55:53 +0300

    [qodana] QD-14790 Open coverage report by default when Qodana report is loaded
    (cherry picked from commit 4702905fe16635d9d9f31f4d44e25cd7e0981bc8)
    
    
    (cherry picked from commit dd74efcf960fcdc8973cabaa2be1002bd10c6856)
    
    IJ-MR-207737
    
    GitOrigin-RevId: a4ad2a9d1578c03de102f23cb0f605ad7d4ec5f4

--- qodana/coverage/src/org/jetbrains/qodana/staticAnalysis/inspections/coverage/CoverageListenerService.kt
+++ qodana/coverage/src/org/jetbrains/qodana/staticAnalysis/inspections/coverage/CoverageListenerService.kt
@@ -3,9 +3,11 @@ package org.jetbrains.qodana.staticAnalysis.inspections.coverage
 import com.intellij.coverage.CoverageDataManager
 import com.intellij.coverage.CoverageSuitesBundle
 import com.intellij.coverage.view.CoverageViewManager
+import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.util.awaitCancellationAndInvoke
 import kotlinx.coroutines.CoroutineScope
@@ -22,7 +24,7 @@ import org.jetbrains.qodana.registry.QodanaRegistry.openCoverageReportEnabled
  * Listener service responsible for spinning up coverage reports on the screen once they arrive from the cloud.
  */
 @Service(Service.Level.PROJECT)
-class CoverageListenerService(private val project: Project, scope: CoroutineScope) {
+class CoverageListenerService(private val project: Project, scope: CoroutineScope) : Disposable.Default {
   companion object {
     fun getInstance(project: Project): CoverageListenerService = project.service()
   }
@@ -54,9 +56,12 @@ class CoverageListenerService(private val project: Project, scope: CoroutineScop
 
   private suspend fun openCoverageSuiteAndCloseOnCancellation(suite: CoverageSuitesBundle) {
     val coverageDataManager = CoverageDataManager.getInstance(project)
+    val listenerLifetime: Disposable = Disposer.newDisposable(this, "QodanaCoverageToolWindowActivator")
+    coverageDataManager.addSuiteListener(QodanaCoverageToolWindowActivator(project, suite), listenerLifetime)
     withContext(QodanaDispatchers.Ui) {
       coverageDataManager.chooseSuitesBundle(suite)
       awaitCancellationAndInvoke {
+        Disposer.dispose(listenerLifetime)
         if (!project.isDisposed) {
           coverageDataManager.closeSuitesBundle(suite)
         }
