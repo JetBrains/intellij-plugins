882cdfa01466b87d13ef0d1866e501eb550cd401
Denis Bezrukov <denis.bezrukov@jetbrains.com>
2026-05-29 18:12:17 +0300

    [vuejs] WEB-74240 decouple from the ESLint plugin via an optional content module
    GitOrigin-RevId: ee27ae9f3e1b20cc47f629c246563a296bb0fc1d

--- vuejs/vuejs-eslint/src/com/intellij/vuejs/eslint/VueEslintRuleMappersFactory.kt
+++ vuejs/vuejs-eslint/src/com/intellij/vuejs/eslint/VueEslintRuleMappersFactory.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package org.jetbrains.vuejs.libraries.eslint
+package com.intellij.vuejs.eslint
 
 import com.intellij.json.psi.JsonNumberLiteral
 import com.intellij.json.psi.JsonObject
