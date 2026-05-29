0d3683830be6d710e5a2db4207efd9e6dcf2eae1
Victor Turansky <victor.turansky@jetbrains.com>
2026-05-29 20:10:14 +0200

    WEB-78162 Vue/Kolar: Simple test projects for transpiled data extractor
    GitOrigin-RevId: f23524cf197b74162e364d4638c3a37ee426aaac

---
+++ vuejs/vuejs-tests/testData/kolar/single-file-app-with-refs/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const count = 1
+</script>
+
+<template>
+  <div ref="first"></div>>
+  <div ref="second"></div>>
+  {{ count }}
+  {{ $refs.first.tabIndex }}
+  {{ $refs.second.tabIndex }}
+</template>
