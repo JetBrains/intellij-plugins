17ce577b7c94ded4cb03c530cbaf7132b13f4dd0
Victor Turansky <victor.turansky@jetbrains.com>
2026-05-29 21:54:11 +0200

    WEB-78162 Vue/Kolar: Fix formatting
    GitOrigin-RevId: 24c3641337f58fc0525612241f093daa0e29e4e8

--- vuejs/vuejs-tests/testData/kolar/__generator__/default-project-data/generate.ts
+++ vuejs/vuejs-tests/testData/kolar/__generator__/default-project-data/generate.ts
@@ -1,88 +1,88 @@
 import {spawn} from 'node:child_process'
-import { parse, resolve } from 'node:path'
+import {parse, resolve} from 'node:path'
 import {cwd, execPath, exit} from 'node:process'
 
 const currentDirName = parse(cwd()).name
 const transpiledDir = resolve(cwd(), '../..', currentDirName + "__transpiled")
 
 const tsserver = spawn(
-    execPath,
-    [
-        "./node_modules/typescript/lib/tsserver.js",
-        '--globalPlugins',
-        '@vue/typescript-plugin,vue-transpiled-data-plugin',
-        '--pluginProbeLocations',
-        "./node_modules",
-    ],
-    {
-        stdio: ['pipe', 'pipe', 'inherit'],
-        env: { TRANSPILED_DIR: transpiledDir },
-    }
+  execPath,
+  [
+    "./node_modules/typescript/lib/tsserver.js",
+    '--globalPlugins',
+    '@vue/typescript-plugin,vue-transpiled-data-plugin',
+    '--pluginProbeLocations',
+    "./node_modules",
+  ],
+  {
+    stdio: ['pipe', 'pipe', 'inherit'],
+    env: {TRANSPILED_DIR: transpiledDir},
+  }
 )
 
 function processResponse(
-    content: string,
+  content: string,
 ) {
-    const response = JSON.parse(content)
+  const response = JSON.parse(content)
 
-    if (response.request_seq === undefined) {
-        return
-    }
+  if (response.request_seq === undefined) {
+    return
+  }
 
-    if (response.success !== true) {
-        console.error("INVALID RESPONSE:", content)
-        tsserver.kill()
-        exit(1)
-    }
+  if (response.success !== true) {
+    console.error("INVALID RESPONSE:", content)
+    tsserver.kill()
+    exit(1)
+  }
 
-    if (response.request_seq === 36) {
-        sendRequest(38, 'typeDefinition', {
-            file: cwd() + '/src/force-vue-transpiler-data-generation.ts',
-            line: 1,
-            offset: 0,
-        })
-        return
-    }
+  if (response.request_seq === 36) {
+    sendRequest(38, 'typeDefinition', {
+      file: cwd() + '/src/force-vue-transpiler-data-generation.ts',
+      line: 1,
+      offset: 0,
+    })
+    return
+  }
 
-    if (response.request_seq === 38) {
-        console.log("Processing finished! SUCCESS!")
-        tsserver.kill()
-        exit()
-    }
+  if (response.request_seq === 38) {
+    console.log("Processing finished! SUCCESS!")
+    tsserver.kill()
+    exit()
+  }
 }
 
 tsserver.stdout.on('data', (data) => {
-    const dataString = data.toString().trim()
-    if (!dataString.startsWith('Content-Length: '))
-        return
+  const dataString = data.toString().trim()
+  if (!dataString.startsWith('Content-Length: '))
+    return
 
-    console.log("---------------")
-    console.log(dataString)
-    console.log("---------------")
+  console.log("---------------")
+  console.log(dataString)
+  console.log("---------------")
 
-    dataString.split('\r\n\r\n')
-        .slice(1)
-        .map(s => s.split('\nContent-Length: ')[0])
-        .forEach(s => processResponse(s))
+  dataString.split('\r\n\r\n')
+    .slice(1)
+    .map(s => s.split('\nContent-Length: ')[0])
+    .forEach(s => processResponse(s))
 })
 
 function sendRequest(
-  seq: number, 
-  command: string, 
+  seq: number,
+  command: string,
   args: Record<string, unknown>,
 ) {
-    const payload = {
-        seq,
-        type: 'request',
-        command,
-        arguments: args,
-    }
+  const payload = {
+    seq,
+    type: 'request',
+    command,
+    arguments: args,
+  }
 
-    // tsserver expects valid JSON stringified on a single line
-    const jsonStr = JSON.stringify(payload) + '\n'
-    tsserver.stdin.write(jsonStr)
+  // tsserver expects valid JSON stringified on a single line
+  const jsonStr = JSON.stringify(payload) + '\n'
+  tsserver.stdin.write(jsonStr)
 }
 
 sendRequest(36, 'open', {
-    file: cwd() + '/src/force-vue-transpiler-data-generation.ts',
+  file: cwd() + '/src/force-vue-transpiler-data-generation.ts',
 })
--- vuejs/vuejs-tests/testData/kolar/__generator__/vue-transpiled-data-plugin/src/index.ts
+++ vuejs/vuejs-tests/testData/kolar/__generator__/vue-transpiled-data-plugin/src/index.ts
@@ -4,38 +4,38 @@ import path from 'node:path'
 import process from 'node:process'
 
 function init() {
-    function create(info: ts.server.PluginCreateInfo) {
-        const project = info.project;
-        const transpiledDir = process.env.TRANSPILED_DIR as string;
+  function create(info: ts.server.PluginCreateInfo) {
+    const project = info.project;
+    const transpiledDir = process.env.TRANSPILED_DIR as string;
 
-        if (existsSync(transpiledDir)) {
-            rmSync(transpiledDir, {recursive: true})
-        }
-
-        info.languageService.getTypeDefinitionAtPosition = (fileName) => {
-            for (const scriptFileName of project.getScriptFileNames()) {
-                if (!scriptFileName.endsWith('.vue'))
-                    continue;
-
-                const scriptInfo = project.getScriptInfo(scriptFileName)!
-                const sourceFile = project.getSourceFile(scriptInfo.path)!
-
-                const transpiledFilePath = path.resolve(
-                    transpiledDir,
-                    path.relative(process.cwd(), scriptFileName) + ".ts",
-                )
-
-                mkdirSync(path.dirname(transpiledFilePath), { recursive: true });
-                writeFileSync(transpiledFilePath, sourceFile.getFullText())
-            }
-
-            return undefined;
-        }
-
-        return info.languageService;
+    if (existsSync(transpiledDir)) {
+      rmSync(transpiledDir, {recursive: true})
     }
 
-    return {create};
+    info.languageService.getTypeDefinitionAtPosition = (fileName) => {
+      for (const scriptFileName of project.getScriptFileNames()) {
+        if (!scriptFileName.endsWith('.vue'))
+          continue;
+
+        const scriptInfo = project.getScriptInfo(scriptFileName)!
+        const sourceFile = project.getSourceFile(scriptInfo.path)!
+
+        const transpiledFilePath = path.resolve(
+          transpiledDir,
+          path.relative(process.cwd(), scriptFileName) + ".ts",
+        )
+
+        mkdirSync(path.dirname(transpiledFilePath), {recursive: true});
+        writeFileSync(transpiledFilePath, sourceFile.getFullText())
+      }
+
+      return undefined;
+    }
+
+    return info.languageService;
+  }
+
+  return {create};
 }
 
 export = init;
