191d21d2d8b207d33197c170e0917830a7bb45a0
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-04 19:44:52 +0200

    WEB-78162 Vue/Kolar: Update "Simple app with components (vapor)" test case
    GitOrigin-RevId: a47c7453a86282bcdcfd47100f8b4a4425ed2141

--- vuejs/vuejs-tests/testData/kolar/simple-app-with-components__vapor__transpiled/src/App.vue.ts
+++ vuejs/vuejs-tests/testData/kolar/simple-app-with-components__vapor__transpiled/src/App.vue.ts
@@ -16,6 +16,11 @@
 import MyCounter from './MyCounter.vue'
 import MyLabel from './MyLabel.vue'
 
+const __VLS_export = ((
+	__VLS_props: NonNullable<Awaited<typeof __VLS_setup>>['props'],
+	__VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, 'attrs' | 'emit' | 'slots'>>,
+	__VLS_exposed?: NonNullable<Awaited<typeof __VLS_setup>>['expose'],
+	__VLS_setup = (async () => {
 const count = 1
 // @ts-ignore
 declare const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, }: typeof import('vue');
@@ -47,6 +52,14 @@ const __VLS_7 = __VLS_6({
 }, ...__VLS_functionalComponentArgsRest(__VLS_6));
 // @ts-ignore
 [count,];
-const __VLS_export = (await import('vue')).defineComponent({
-});
+return {} as {
+	props: import('vue').PublicProps & (typeof globalThis extends { __VLS_PROPS_FALLBACK: infer P } ? P : {});
+	expose: (exposed: {}) => void;
+	attrs: any;
+	slots: {};
+	emit: {};
+};
+})(),
+) => ({} as import('vue').VNode & { __ctx?: Awaited<typeof __VLS_setup> }));
 export default {} as typeof __VLS_export;
+type __VLS_PrettifyLocal<T> = (T extends any ? { [K in keyof T]: T[K]; } : { [K in keyof T as K]: T[K]; }) & {};
--- vuejs/vuejs-tests/testData/kolar/simple-app-with-components__vapor__transpiled/src/MyCounter.vue.ts
+++ vuejs/vuejs-tests/testData/kolar/simple-app-with-components__vapor__transpiled/src/MyCounter.vue.ts
@@ -8,6 +8,11 @@
 /// <reference types="../node_modules/@vue/language-core/types/template-helpers.d.ts" />
 /// <reference types="../node_modules/@vue/language-core/types/props-fallback.d.ts" />
 
+const __VLS_export = ((
+	__VLS_props: NonNullable<Awaited<typeof __VLS_setup>>['props'],
+	__VLS_ctx?: __VLS_PrettifyLocal<Pick<NonNullable<Awaited<typeof __VLS_setup>>, 'attrs' | 'emit' | 'slots'>>,
+	__VLS_exposed?: NonNullable<Awaited<typeof __VLS_setup>>['expose'],
+	__VLS_setup = (async () => {
 const count = 1
 // @ts-ignore
 declare const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, }: typeof import('vue');
@@ -27,6 +32,14 @@ let __VLS_directives!: __VLS_LocalDirectives & import('vue').GlobalDirectives;
 ( __VLS_ctx.count );
 // @ts-ignore
 [count,];
-const __VLS_export = (await import('vue')).defineComponent({
-});
+return {} as {
+	props: import('vue').PublicProps & (typeof globalThis extends { __VLS_PROPS_FALLBACK: infer P } ? P : {});
+	expose: (exposed: {}) => void;
+	attrs: any;
+	slots: {};
+	emit: {};
+};
+})(),
+) => ({} as import('vue').VNode & { __ctx?: Awaited<typeof __VLS_setup> }));
 export default {} as typeof __VLS_export;
+type __VLS_PrettifyLocal<T> = (T extends any ? { [K in keyof T]: T[K]; } : { [K in keyof T as K]: T[K]; }) & {};
