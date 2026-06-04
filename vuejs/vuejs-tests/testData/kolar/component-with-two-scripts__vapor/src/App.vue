fef6b02c0debaecd90859e17b0e04b414b6d7afd
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-04 19:48:50 +0200

    WEB-78162 Vue/Kolar: Add "component with 2 scripts (vapor)" test case draft
    GitOrigin-RevId: 7cde22048840feebdd8b8346f18c60589fdd0e97

---
+++ vuejs/vuejs-tests/testData/kolar/component-with-two-scripts__vapor/src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import MyCounter from './MyCounter.vue'
+import MyLabel from './MyLabel.vue'
+
+const count = 1
+</script>
+
+<script>
+export interface MyUser {
+  name: string
+  age: number
+}
+
+export type OtherUser = Readonly<{
+  name: string
+  age: number
+}>
+</script>
+
+<template>
+  {{ count }}
+  
+  <MyCounter/>
+  <MyLabel/>
+</template>
---
+++ vuejs/vuejs-tests/testData/kolar/component-with-two-scripts__vapor__transpiled/src/App.vue.ts
@@ -0,0 +1,76 @@
+                        
+                                       
+                                   
+
+               
+         
+
+        
+                         
+              
+             
+ 
+
+                                  
+              
+             
+  
+         
+
+          
+             
+  
+              
+            
+           /// <reference types="../node_modules/@vue/language-core/types/template-helpers.d.ts" />
+/// <reference types="../node_modules/@vue/language-core/types/props-fallback.d.ts" />
+
+import MyCounter from './MyCounter.vue'
+import MyLabel from './MyLabel.vue'
+
+
+export interface MyUser {
+  name: string
+  age: number
+}
+
+export type OtherUser = Readonly<{
+  name: string
+  age: number
+}>
+export default {} as typeof __VLS_export;
+const __VLS_export = await (async () => {
+const count = 1
+// @ts-ignore
+declare const { defineProps, defineSlots, defineEmits, defineExpose, defineModel, defineOptions, withDefaults, }: typeof import('vue');
+type __VLS_SetupExposed = import('vue').ShallowUnwrapRef<{
+count: typeof count;
+}>;
+const __VLS_ctx = {
+...{} as import('vue').ComponentPublicInstance,
+...{} as __VLS_SetupExposed,
+};
+type __VLS_LocalComponents = __VLS_SetupExposed;
+type __VLS_GlobalComponents = import('vue').GlobalComponents;
+let __VLS_components!: __VLS_LocalComponents & __VLS_GlobalComponents;
+let __VLS_intrinsics!: import('vue/jsx-runtime').JSX.IntrinsicElements;
+type __VLS_LocalDirectives = __VLS_SetupExposed;
+let __VLS_directives!: __VLS_LocalDirectives & import('vue').GlobalDirectives;
+( __VLS_ctx.count );
+const __VLS_0 = MyCounter;
+// @ts-ignore
+const __VLS_1 = __VLS_asFunctionalComponent1(__VLS_0, new __VLS_0({
+}));
+const __VLS_2 = __VLS_1({
+}, ...__VLS_functionalComponentArgsRest(__VLS_1));
+const __VLS_5 = MyLabel;
+// @ts-ignore
+const __VLS_6 = __VLS_asFunctionalComponent1(__VLS_5, new __VLS_5({
+}));
+const __VLS_7 = __VLS_6({
+}, ...__VLS_functionalComponentArgsRest(__VLS_6));
+// @ts-ignore
+[count,];
+return (await import('vue')).defineComponent({
+});
+})();
