11c31028d629c5627f3076252a8bf04fa9daca7f
Roman Shevchenko <roman.shevchenko@jetbrains.com>
2026-06-05 20:58:44 +0200

    refactoring [*]: replacing deprecated custom FUS validation rule
    ... + cleanup (minor optimization; typos; formatting)
    
    GitOrigin-RevId: 61a7f6001e7431ca0bc5ba0689c5c8612f4df0c6

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/stat/InspectionIdValidationRule.kt
+++ qodana/core/src/org/jetbrains/qodana/staticAnalysis/stat/InspectionIdValidationRule.kt
@@ -1,17 +1,13 @@
 package org.jetbrains.qodana.staticAnalysis.stat
 
-import com.intellij.internal.statistic.eventLog.validator.ValidationResultType
-import com.intellij.internal.statistic.eventLog.validator.rules.EventContext
 import com.intellij.internal.statistic.eventLog.validator.rules.impl.CustomValidationRule
+import com.jetbrains.fus.reporting.api.IEventContext
+import com.jetbrains.fus.reporting.api.ValidationResultType
 
 class InspectionIdValidationRule : CustomValidationRule() {
   override fun getRuleId(): String = "inspection_id_rule"
 
-  override fun doValidate(data: String, context: EventContext): ValidationResultType {
-    return if (data == FLEXINSPECT_STATS_INSPECTION_ID || isThirdPartyValue(data)) {
-      ValidationResultType.ACCEPTED
-    } else {
-      acceptWhenReportedByPluginFromPluginRepository(context)
-    }
-  }
+  override fun doValidate(data: String, context: IEventContext): ValidationResultType =
+    if (data == FLEXINSPECT_STATS_INSPECTION_ID || isThirdPartyValue(data)) ValidationResultType.ACCEPTED
+    else acceptWhenReportedByPluginFromPluginRepository(context)
 }
