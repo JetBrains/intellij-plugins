bc87510406f21a4311a9b904724b7817b7c004ee
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-08 19:38:20 +0200

    WEB-78162 Vue/Kolar: Convert `TemplateCodegenOptions` interface
    GitOrigin-RevId: a617f9af4cc4f789b4fabbfd1bdc4f642197c97b

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/codegen/template/TemplateCodegenOptions.kt
@@ -0,0 +1,17 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.template
+
+import org.jetbrains.vuejs.config.VueCompilerOptions
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRTemplate
+
+data class TemplateCodegenOptions(
+  val vueCompilerOptions: VueCompilerOptions,
+  val template: IRTemplate,
+  val setupRefs: Set<String>,
+  val setupConsts: Set<String>,
+  val hasDefineSlots: Boolean = false,
+  val propsAssignName: String? = null,
+  val slotsAssignName: String? = null,
+  val inheritAttrs: Boolean,
+  val componentName: String,
+)
