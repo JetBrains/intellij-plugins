abaeb9845ff4d612163a6e4c58ac51e4a10f1822
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-08 21:18:22 +0200

    WEB-78162 Vue/Kolar: `VueCodeInformation` - data class
    GitOrigin-RevId: dd878dc67fe4532f4f9e6663682245522ab055c7

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/VueCodeInformation.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/VueCodeInformation.kt
@@ -4,15 +4,15 @@ package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core
 import com.intellij.lang.typescript.kolar.KolarCodeInformation
 import org.jetbrains.vuejs.lang.typescript.kolar.js.symbol.Symbol
 
-class VueCodeInformation(
-  verification: VerificationInfo? = null,
-  completion: CompletionInfo? = null,
-  semantic: SemanticInfo? = null,
-  navigation: NavigationInfo? = null,
-  structure: Boolean = false,
-  format: Boolean = false,
-  types: Boolean = false,
-  reverseTypes: Boolean = false,
+data class VueCodeInformation(
+  override val verification: VerificationInfo? = null,
+  override val completion: CompletionInfo? = null,
+  override val semantic: SemanticInfo? = null,
+  override val navigation: NavigationInfo? = null,
+  override val structure: Boolean = false,
+  override val format: Boolean = false,
+  override val types: Boolean = false,
+  override val reverseTypes: Boolean = false,
   val __importCompletion: Boolean = false,
   val __propsCompletion: Boolean = false,
   val __shorthandExpression: ShorthandExpression? = null,
@@ -29,31 +29,10 @@ class VueCodeInformation(
   reverseTypes = reverseTypes,
 ) {
 
-  enum class ShorthandExpression { 
-    html, 
+  enum class ShorthandExpression {
+    html,
     js,
-    
+
     ;
-  }
-
-  override fun equals(other: Any?): Boolean {
-    if (!super.equals(other)) return false
-    other as VueCodeInformation
-    if (__importCompletion != other.__importCompletion) return false
-    if (__propsCompletion != other.__propsCompletion) return false
-    if (__shorthandExpression != other.__shorthandExpression) return false
-    if (__combineToken != other.__combineToken) return false
-    if (__linkedToken != other.__linkedToken) return false
-    return true
-  }
-
-  override fun hashCode(): Int {
-    var result = super.hashCode()
-    result = 31 * result + __importCompletion.hashCode()
-    result = 31 * result + __propsCompletion.hashCode()
-    result = 31 * result + __shorthandExpression.hashCode()
-    result = 31 * result + __combineToken.hashCode()
-    result = 31 * result + __linkedToken.hashCode()
-    return result
   }
 }
