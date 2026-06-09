6c9fd8a9f0f2425753b9e736761cdb967aa37666
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 16:45:48 +0200

    WEB-78162 Vue/Kolar: Convert `generateCamelized` function
    GitOrigin-RevId: cb5f7318953851d08c206279a5961a72a88c0655

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/codegen/utils/camelized.kt
@@ -0,0 +1,40 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils
+
+import org.jetbrains.vuejs.lang.typescript.kolar.js.symbol.Symbol
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.DataSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.Code
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.VueCodeInformation
+
+fun generateCamelized(
+  code: String,
+  source: String,
+  offset: Int,
+  features: VueCodeInformation,
+): Sequence<Code> = sequence {
+  val parts = code.split('-')
+  val combineToken = features.__combineToken ?: Symbol()
+  var currentOffset = offset
+
+  for ((i, part) in parts.withIndex()) {
+    if (part.isNotEmpty()) {
+      if (i == 0) {
+        yield(DataSegment(
+          text = part,
+          source = source,
+          sourceOffset = currentOffset,
+          data = features.copy(__combineToken = combineToken),
+        ))
+      }
+      else {
+        yield(DataSegment(
+          text = part.replaceFirstChar { it.uppercaseChar() },
+          source = source,
+          sourceOffset = currentOffset,
+          data = VueCodeInformation(__combineToken = combineToken),
+        ))
+      }
+    }
+    currentOffset += part.length + 1
+  }
+}
