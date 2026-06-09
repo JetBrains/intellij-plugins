f36d2488eed112c642658859a1979d376e0f76cc
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 21:05:04 +0200

    WEB-78162 Vue/Kolar: Generate placeholders for TypeScript type guards
    GitOrigin-RevId: a1117f16fae369339f196ce9e7c515a566f7cb03

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/typescript/Nodes.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/typescript/Nodes.kt
@@ -22,3 +22,65 @@ interface FunctionExpression : Expression
 interface AccessorDeclaration : Node
 
 interface MethodDeclaration : Node
+
+interface Statement : Node
+
+interface EmptyStatement : Statement
+
+interface ExpressionStatement : Statement
+
+interface VariableStatement : Statement
+
+interface Block : Statement
+
+interface FunctionDeclaration : Statement
+
+interface ClassDeclaration : Statement
+
+interface EnumDeclaration : Statement
+
+interface ImportDeclaration : Statement
+
+interface ImportEqualsDeclaration : Statement
+
+interface ExportDeclaration : Statement
+
+interface ExportAssignment : Statement
+
+interface TypeAliasDeclaration : Statement
+
+interface InterfaceDeclaration : Statement
+
+interface CallExpression : Expression
+
+interface ParenthesizedExpression : Expression
+
+interface PropertyAccessExpression : Expression
+
+interface ArrayLiteralExpression : Expression
+
+interface TypeNode : Node
+
+interface TypeLiteralNode : TypeNode
+
+interface UnionTypeNode : TypeNode
+
+interface TypeQueryNode : TypeNode
+
+interface FunctionLikeDeclaration : Node
+
+interface ArrayBindingPattern : Node
+
+interface ObjectBindingPattern : Node
+
+interface ComputedPropertyName : Node
+
+interface PropertyAssignment : Node
+
+interface ShorthandPropertyAssignment : Node
+
+interface SpreadAssignment : Node
+
+interface CallSignatureDeclaration : Node
+
+interface NamedImports : Node
