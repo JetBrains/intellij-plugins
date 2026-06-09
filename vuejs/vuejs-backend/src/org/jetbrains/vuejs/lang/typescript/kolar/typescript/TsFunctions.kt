a3be07ed9efaf94bcd234e0c3d9e46d64b9c8d85
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 21:43:51 +0200

    WEB-78162 Vue/Kolar: Generate placeholders for TypeScript functions
    GitOrigin-RevId: 1eb1b0165f187683b1e4f099470e6079709a15a6

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/typescript/TsFunctions.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.typescript
+
+fun <T> forEachChild(
+  node: Node,
+  cbNode: (Node) -> T?,
+): T? =
+  TODO()
+
+fun getLeadingCommentRanges(
+  text: String,
+  pos: Int,
+): List<CommentRange>? =
+  TODO()
+
+fun getTokenPosOfNode(
+  node: Node,
+  sourceFile: SourceFile? = null,
+): Int =
+  TODO()
+
+fun createSourceFile(
+  fileName: String,
+  sourceText: String,
+  languageVersion: ScriptTarget,
+  setParentNodes: Boolean = false,
+  scriptKind: ScriptKind? = null,
+): SourceFile =
+  TODO()
