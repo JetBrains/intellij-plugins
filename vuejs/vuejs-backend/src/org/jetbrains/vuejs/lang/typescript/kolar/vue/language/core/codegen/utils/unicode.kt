42e079ab5d217248282ae77a9d0e833a54632201
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 17:23:50 +0200

    WEB-78162 Vue/Kolar: Convert `generateUnicode` function
    GitOrigin-RevId: c2845732c915529d21bfdc8dbb365615487dd3b0

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/codegen/utils/unicode.kt
@@ -0,0 +1,37 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils
+
+import org.jetbrains.vuejs.lang.typescript.kolar.js.generator.yield
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.DataSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.StringSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.Code
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.VueCodeInformation
+
+fun generateUnicode(
+  code: String,
+  offset: Int,
+  info: VueCodeInformation,
+): Sequence<Code> = sequence {
+  if (needToUnicode(code)) {
+    val token = yield(startBoundary("template", offset, info))
+    yield(StringSegment(toUnicode(code)))
+    yield(endBoundary(token, offset + code.length))
+  }
+  else {
+    yield(DataSegment(
+      text = code,
+      source = "template",
+      sourceOffset = offset,
+      data = info,
+    ))
+  }
+}
+
+private fun needToUnicode(str: String): Boolean =
+  str.contains('\\') || str.contains('\n')
+
+private fun toUnicode(str: String): String =
+  str.map { char ->
+    val hex = char.code.toString(16).padStart(4, '0')
+    if (hex.length > 2) "\\u$hex" else char.toString()
+  }.joinToString("")
