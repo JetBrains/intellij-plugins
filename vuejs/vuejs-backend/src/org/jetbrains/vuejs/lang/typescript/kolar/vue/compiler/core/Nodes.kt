1a9f35b96318e3139b712a4fb64170bff4be9ddb
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 16:03:49 +0200

    WEB-78162 Vue/Kolar: Convert simple node types
    GitOrigin-RevId: 54298f483d49e0a9a49a4da09ffc9506137cee73

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/Nodes.kt
@@ -0,0 +1,18 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core
+
+// CompilerDOM.Node — base for all AST nodes
+interface Node {
+  val type: Int
+  val loc: SourceLocation
+}
+
+// CompilerDOM.CommentNode
+data class CommentNode(
+  override val type: Int,  // NodeTypes.COMMENT
+  val content: String,
+  override val loc: SourceLocation,
+) : Node
+
+// CompilerDOM.ElementNode (minimal — full definition to be added when element.ts is converted)
+interface ElementNode : Node
