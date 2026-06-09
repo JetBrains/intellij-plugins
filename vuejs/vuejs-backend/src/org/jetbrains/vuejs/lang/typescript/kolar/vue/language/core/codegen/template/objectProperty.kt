4b9acbf39a0ba7ce897fbdf248052187df17fffb
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-10 01:12:09 +0200

    WEB-78162 Vue/Kolar: Convert `generateObjectProperty` function
    GitOrigin-RevId: bd4c023895a9b300539cbccb039f29a7be3a8c91

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/codegen/template/objectProperty.kt
@@ -0,0 +1,67 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.template
+
+import org.jetbrains.vuejs.lang.typescript.kolar.js.generator.yield
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.DataSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.Code
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.VueCodeInformation
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.names
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.endBoundary
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.generateCamelized
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.generateStringLiteralKey
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.identifierRegex
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.startBoundary
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.yield
+
+fun generateObjectProperty(
+  options: TemplateCodegenOptions,
+  ctx: TemplateCodegenContext,
+  code: String,
+  offset: Int,
+  features: VueCodeInformation,
+  shouldCamelize: Boolean = false,
+  shouldBeConstant: Boolean = false,
+): Sequence<Code> = sequence {
+  if (code.startsWith("[") && code.endsWith("]")) {
+    if (shouldBeConstant) {
+      yieldAll(generateInterpolation(
+        options,
+        ctx,
+        options.template,
+        features,
+        code.substring(1, code.length - 1),
+        offset + 1,
+        "[${names.tryAsConstant}(",
+        ")]",
+      ))
+    }
+    else {
+      yieldAll(generateInterpolation(options, ctx, options.template, features, code, offset))
+    }
+  }
+  else if (shouldCamelize) {
+    if (identifierRegex.matches(camelize(code))) {
+      yieldAll(generateCamelized(code, "template", offset, features))
+    }
+    else {
+      val token = yield(startBoundary("template", offset, features))
+      yield("'")
+      yieldAll(generateCamelized(code, "template", offset, VueCodeInformation(__combineToken = token)))
+      yield("'")
+      yield(endBoundary(token, offset + code.length))
+    }
+  }
+  else {
+    if (identifierRegex.matches(code)) {
+      yield(DataSegment(text = code, source = "template", sourceOffset = offset, data = features))
+    }
+    else {
+      yieldAll(generateStringLiteralKey(code, offset, features))
+    }
+  }
+}
+
+private fun camelize(code: String): String =
+  code.splitToSequence('-')
+    .mapIndexed { i, s -> if (i == 0) s else s.replaceFirstChar { it.uppercaseChar() } }
+    .joinToString("")
