a8f305ba3dcb6dba7c070297a8209304103d496b
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 19:14:21 +0200

    WEB-78162 Vue/Kolar: Convert `generateStyleModules` function
    GitOrigin-RevId: 2b09860f185ad835552e9dd6628912e372e394dc

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/codegen/style/modules.kt
@@ -0,0 +1,54 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.style
+
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.DataSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.StringSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.Code
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRAttr
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.codeFeatures
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.names
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.template.TemplateCodegenContext
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.endOfLine
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils.newLine
+
+fun generateStyleModules(
+  options: StyleCodegenOptions,
+  ctx: TemplateCodegenContext,
+): Sequence<Code> = sequence {
+  val styleModules = options.styles.filter { it.module != null }
+  if (styleModules.isEmpty()) return@sequence
+
+  ctx.generatedTypes.add(names.StyleModules)
+
+  yield(StringSegment("type ${names.StyleModules} = {$newLine"))
+  for (style in styleModules) {
+    when (val module = style.module) {
+      is IRAttr.Present -> yield(StringSegment("\$style"))
+      is IRAttr.WithText -> yield(DataSegment(
+        text = module.text,
+        source = "main",
+        sourceOffset = module.offset,
+        data = codeFeatures.navigation,
+      ))
+      null -> Unit
+    }
+    yield(StringSegment(": "))
+    if (!options.vueCompilerOptions.strictCssModules) {
+      yield(StringSegment("Record<string, string> & "))
+    }
+    yield(StringSegment("${names.PrettifyGlobal}<{}"))
+    if (options.vueCompilerOptions.resolveStyleImports) {
+      yieldAll(generateStyleImports(style))
+    }
+    for ((text, offset) in style.classNames) {
+      yieldAll(generateClassProperty(
+        source = style.name,
+        classNameWithDot = text,
+        offset = offset,
+        propertyType = "string",
+      ))
+    }
+    yield(StringSegment(">$endOfLine"))
+  }
+  yield(StringSegment("}$endOfLine"))
+}
