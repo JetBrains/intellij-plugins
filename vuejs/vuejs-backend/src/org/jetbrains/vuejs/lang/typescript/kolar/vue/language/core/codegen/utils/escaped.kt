5443e983be667934c90b79a11aeba8ea452f9d1e
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-09 16:51:27 +0200

    WEB-78162 Vue/Kolar: Convert `generateEscaped` function
    GitOrigin-RevId: b633cd02edf1da1b7e52bf728ec1b1b5b78d5536

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/codegen/utils/escaped.kt
@@ -0,0 +1,57 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.codegen.utils
+
+import org.jetbrains.vuejs.lang.typescript.kolar.js.symbol.Symbol
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.DataSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.StringSegment
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.Code
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.VueCodeInformation
+
+fun generateEscaped(
+  text: String,
+  source: String,
+  offset: Int,
+  features: VueCodeInformation,
+  escapeTarget: Regex,
+): Sequence<Code> = sequence {
+  val combineToken = features.__combineToken ?: Symbol()
+  var currentOffset = offset
+  var lastIndex = 0
+  var isFirst = true
+
+  for (match in escapeTarget.findAll(text)) {
+    val part = text.substring(lastIndex, match.range.first)
+    yield(DataSegment(
+      text = part,
+      source = source,
+      sourceOffset = currentOffset,
+      data = if (isFirst) {
+        features.copy(__combineToken = combineToken)
+      }
+      else {
+        VueCodeInformation(__combineToken = combineToken)
+      },
+    ))
+    currentOffset += part.length
+    isFirst = false
+
+    yield(StringSegment("\\"))
+
+    yield(DataSegment(
+      text = match.value,
+      source = source,
+      sourceOffset = currentOffset,
+      data = VueCodeInformation(__combineToken = combineToken),
+    ))
+    currentOffset += match.value.length
+    lastIndex = match.range.last + 1
+  }
+
+  val remaining = text.substring(lastIndex)
+  yield(DataSegment(
+    text = remaining,
+    source = source,
+    sourceOffset = currentOffset,
+    data = if (isFirst) features.copy(__combineToken = combineToken) else VueCodeInformation(__combineToken = combineToken),
+  ))
+}
