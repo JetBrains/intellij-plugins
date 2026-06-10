166dca50f7d5e446dc0f855a9a2b85d02dd80496
Evgenii Novozhilov <evgenii.novozhilov@jetbrains.com>
2026-06-10 15:39:21 +0300

    cleanup [cidr]: make CidrProjectActivity.runActivity suspend
    GitOrigin-RevId: eab91f768f35c577a0ef65fe6f63c20ff1aa2f65

--- qodana/cpp/src/QodanaCppInitializationActivity.kt
+++ qodana/cpp/src/QodanaCppInitializationActivity.kt
@@ -6,7 +6,7 @@ import com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace
 import com.jetbrains.cidr.lang.workspace.RunAfterOCWorkspaceIsInitialized
 
 class QodanaCppInitializationActivity : RunAfterOCWorkspaceIsInitialized {
-  override fun runActivity(project: Project) {
+  override suspend fun runActivitySuspend(project: Project) {
     if (!PlatformUtils.isQodana()) return  // Only force a reload in a headless environment
     CMakeWorkspace.forceReloadOnOpening(project.baseDir)
   }
