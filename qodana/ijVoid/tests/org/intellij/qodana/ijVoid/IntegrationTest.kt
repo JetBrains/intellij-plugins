23cb94abb3de7db0d58c7c34605211cad27379ae
Aleksander Movsesov <aleksander.movsesov@jetbrains.com>
2026-06-12 12:43:32 +0200

    QD-14968 Add Qodana IJ Void tests
    (cherry picked from commit bcd3439d2155d7ecfacaee4e5d80b851e6269288)
    
    IJ-MR-208161
    
    (cherry picked from commit cdf4785e87c1304ff5479d0217838339f7b175ac)
    (cherry picked from commit 8500f0f634541b28a580171cf453d52e3d0c5f0a)
    
    
    (cherry picked from commit fa1feb8fd30b5e8a4ef75864a3c3fe4b74c287c3)
    
    IJ-MR-208913
    
    GitOrigin-RevId: c4057fe1a3947e6154b077589d29bad9e6d026ff

---
+++ qodana/ijVoid/tests/org/intellij/qodana/ijVoid/IntegrationTest.kt
@@ -0,0 +1,33 @@
+package org.intellij.qodana.ijVoid
+
+import com.intellij.ide.starter.extended.allure.Subsystems
+import com.intellij.ide.starter.extended.engine.newTestContainerExtended
+import com.intellij.ide.starter.ide.IDETestContext
+import com.intellij.ide.starter.models.IdeInfo
+import com.intellij.ide.starter.models.TestCase
+import com.intellij.ide.starter.project.LocalProjectInfo
+import com.intellij.ide.starter.runner.Starter
+import com.intellij.openapi.application.PathManager
+import com.intellij.tools.ide.starter.product.idea.ultimate.IdeaUltimate
+import java.nio.file.Path
+import kotlin.io.path.div
+
+@Subsystems.Startup
+abstract class IntegrationTest : org.jetbrains.qodana.tests.utils.IntegrationTest() {
+  override val testDataPath: Path
+    get() = PathManager.getHomeDir() / "contrib/qodana/ijVoid/test-data"
+
+  override fun createTestContext(projectDir: Path): IDETestContext =
+    Starter.newTestContainerExtended().newContext(
+      testName,
+      TestCase(QODANA_IJ_VOID, LocalProjectInfo(projectDir)),
+    )
+
+  private companion object {
+    val QODANA_IJ_VOID: IdeInfo = IdeInfo.IdeaUltimate.copy(
+      platformPrefix = "QodanaIJVoid",
+      qodanaProductCode = "QDIV",
+      fullName = "Qodana for IJ Void",
+    )
+  }
+}
