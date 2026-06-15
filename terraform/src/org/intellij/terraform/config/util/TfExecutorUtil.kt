4034f38b6b481361f1474b3d15f2a486af81ce13
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2026-06-15 14:57:48 +0300

    IJPL-247370 [terraform]: use file type instead of extension when getting toolType
    GitOrigin-RevId: 7c651acfdaab249268f0f6c4649e3caf3dd7767d

--- terraform/src/org/intellij/terraform/config/util/TfExecutorUtil.kt
+++ terraform/src/org/intellij/terraform/config/util/TfExecutorUtil.kt
@@ -1,4 +1,4 @@
-// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.config.util
 
 import com.intellij.execution.ExecutionModes
@@ -6,7 +6,6 @@ import com.intellij.openapi.components.Service
 import com.intellij.openapi.components.service
 import com.intellij.openapi.progress.ProcessCanceledException
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.io.FileUtilRt
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.isFile
@@ -55,7 +54,7 @@ internal suspend fun TfExecutor.executeSuspendable(): Boolean {
 internal fun getApplicableToolType(file: VirtualFile): TfToolType {
   val moduleFolder = if (file.isFile) file.parent else file
   val moduleFiles = moduleFolder?.children?.asList()?.takeIf { it.isNotEmpty() } ?: listOf(file)
-  return if (moduleFiles.any { it.isFile && FileUtilRt.extensionEquals(it.name, OpenTofuFileType.defaultExtension) })
+  return if (moduleFiles.any { it.isFile && it.fileType == OpenTofuFileType })
     TfToolType.OPENTOFU
   else {
     TfToolType.TERRAFORM
