3c7c52ad8e51210a17d94ad59841437cc58ebdfa
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-18 20:25:21 +0200

    WEB-78162 Vue/Kolar: Basic `VueLanguagePlugin` declaration
    GitOrigin-RevId: 468fe41ecf34a1dcb64182ba7796d0e2a1b77c8e

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/VueLanguagePlugin.kt
@@ -0,0 +1,16 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core
+
+import org.jetbrains.vuejs.config.VueCompilerOptions
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.virtualCode.VueEmbeddedCode
+
+abstract class VueLanguagePlugin(
+  val vueCompilerOptions: VueCompilerOptions,
+) {
+  open fun resolveEmbeddedCode(
+    fileName: String,
+    ir: IR,
+    embeddedFile: VueEmbeddedCode,
+  ) {
+  }
+}
