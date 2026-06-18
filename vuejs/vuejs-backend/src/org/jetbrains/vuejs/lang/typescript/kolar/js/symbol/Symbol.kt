c8e60afbf0e807bcb52fd2c864d8ae2f3640a573
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-18 19:22:09 +0200

    WEB-78162 Vue/Kolar: Open `Symbol`
    GitOrigin-RevId: 45a37e05d6ea001fe5da89c711579af2a29bc600

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/js/symbol/Symbol.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/js/symbol/Symbol.kt
@@ -1,6 +1,14 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.js.symbol
 
-class Symbol(
+open class Symbol(
   val description: String? = null,
-)
+) {
+  final override fun equals(other: Any?): Boolean {
+    return super.equals(other)
+  }
+
+  final override fun hashCode(): Int {
+    return super.hashCode()
+  }
+}
