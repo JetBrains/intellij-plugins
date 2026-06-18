051dbe3202e9219546fd1ddfaf52016ba2c5ce93
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-18 21:49:15 +0200

    WEB-78162 Vue/Kolar: Basic `VueTranspiledFileBuilder` logic
    GitOrigin-RevId: 2e5d112f1047ebe80087fcccf379d474b4d380e3

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/VueTranspiledFileBuilder.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/VueTranspiledFileBuilder.kt
@@ -2,15 +2,51 @@
 package org.jetbrains.vuejs.lang.typescript.kolar
 
 import com.intellij.psi.PsiFile
+import org.jetbrains.vuejs.config.VueCompilerOptions
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.toString
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IR
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.VueLanguagePlugin
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.VueMapping
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.plugins.VueTsxPlugin
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.virtualCode.VueEmbeddedCode
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.virtualCode.getMappingsForCode
 
 object VueTranspiledFileBuilder {
+  private val plugin: VueLanguagePlugin =
+    VueTsxPlugin(VueCompilerOptions())
+
   fun getTranspiledFile(
     file: PsiFile,
-  ): TranspiledFile? {
-    // TBD
-    return null
+  ): TranspiledFile {
+    val code = VueEmbeddedCode(
+      id = "script",
+      lang = "ts",
+      content = emptyList(),
+    )
+
+    plugin.resolveEmbeddedCode(fileName = file.name, ir(file), code)
+
+    val generatedCode = toString(code.content)
+    val mappings = getMappingsForCode(code, emptyMap())
+
+    return TranspiledFile(
+      generatedCode = generatedCode,
+      mappings = mappings,
+    )
   }
+
+  private fun ir(
+    file: PsiFile,
+  ): IR =
+    IR(
+      content = file.text,
+      comments = emptyList(),
+      template = null,
+      script = null,
+      scriptSetup = null,
+      styles = emptyList(),
+      customBlocks = emptyList(),
+    )
 
   class TranspiledFile(
     val generatedCode: String,
