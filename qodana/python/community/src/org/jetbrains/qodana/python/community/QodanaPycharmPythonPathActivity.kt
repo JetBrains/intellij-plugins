76209aaeb85c7c284944e3769acbc339d5fd5311
Ilya.Kazakevich <ilya.kazakevich@jetbrains.com>
2026-06-18 13:54:08 +0200

    PY-89965: Fix and rewrite `PySdkFromEnvironmentVariable` and cover with test.
    To unify logic among Qodana and  `PySdkFromEnvironmentVariableConfigurator` (the one that configures PyCharm) and make it testable, we moved all logic to `PySdkFromEnvironmentVariable`.
    
    We now have `PySdkFromEnvironmentVariable` used by dead-simple `PySdkFromEnvironmentVariableConfigurator` and `QodanaPycharmPythonPathActivity`.
    
    GitOrigin-RevId: 8609e46b022b4168ba698f4dff72334ee2ff6aa0

--- qodana/python/community/src/org/jetbrains/qodana/python/community/QodanaPycharmPythonPathActivity.kt
+++ qodana/python/community/src/org/jetbrains/qodana/python/community/QodanaPycharmPythonPathActivity.kt
@@ -1,52 +1,53 @@
 package org.jetbrains.qodana.python.community
 
+import com.intellij.openapi.application.readAction
+import com.intellij.openapi.diagnostic.fileLogger
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.ModuleListener
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.platform.backend.observation.ActivityKey
 import com.intellij.platform.backend.observation.trackActivity
-import com.jetbrains.python.sdk.ModuleOrProject
 import com.jetbrains.python.sdk.PySdkFromEnvironmentVariable
+import com.jetbrains.python.sdk.getOrLog
+import com.jetbrains.python.statistics.modules
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.jetbrains.qodana.coroutines.QodanaDispatchers
 import org.jetbrains.qodana.staticAnalysis.QodanaLinterProjectActivity
 
 private const val QODANA_PYTHON_PATH_ENV = "QODANA_PYTHON_PATH"
 private const val QODANA_PYTHON_PATH_PROPERTY = "qodana.python.path"
 
-class QodanaPycharmPythonPathActivity : QodanaLinterProjectActivity() {
-  object Key : ActivityKey {
+internal class QodanaPycharmPythonPathActivity : QodanaLinterProjectActivity() {
+  private object Key : ActivityKey {
     override val presentableName: String = "$QODANA_PYTHON_PATH_ENV environment update"
+  }
+
+  private companion object {
+    val logger = fileLogger()
   }
 
   override suspend fun run(project: Project) {
     coroutineScope {
       project.trackActivity(Key) {
-        val pycharmPythonPath = pythonPath()
-        if (pycharmPythonPath.isNullOrEmpty()) {
-          return@trackActivity
+        val variable =
+          PySdkFromEnvironmentVariable.create(project, propertyName = QODANA_PYTHON_PATH_PROPERTY, envVarName = QODANA_PYTHON_PATH_ENV)
+            ?.getOrLog(logger)
+          ?: return@trackActivity
+
+        suspend fun configureSdkForModules(modules: List<Module>) {
+          variable.configureSdkForModulesLogIfError(logger, modules.toTypedArray())
         }
 
-        val pycharmPythonPathSdk = withContext(QodanaDispatchers.Ui) {
-          PySdkFromEnvironmentVariable.findOrCreateSdkByPath(pycharmPythonPath, ModuleOrProject.ProjectOnly(project))
-        }.getOr {
-          return@trackActivity
-        }
-        val projectSdk = ProjectRootManager.getInstance(project).projectSdk
+        val modules = readAction { project.modules.toList() }
+        configureSdkForModules(modules)
 
         fun setSdkForModules(modules: List<Module>) {
-          launch(QodanaDispatchers.Default) {
+          this@coroutineScope.launch(QodanaDispatchers.Default) {
             project.trackActivity(Key) {
-              modules.forEach { module ->
-                withContext(QodanaDispatchers.Ui) {
-                  PySdkFromEnvironmentVariable.setModuleSdk(module, projectSdk, pycharmPythonPathSdk, pycharmPythonPath)
-                }
-              }
+              configureSdkForModules(modules)
             }
           }
         }
@@ -62,11 +63,5 @@ class QodanaPycharmPythonPathActivity : QodanaLinterProjectActivity() {
       }
       awaitCancellation()
     }
-  }
-
-  private fun pythonPath(): String? {
-    return System.getenv(QODANA_PYTHON_PATH_ENV)
-           ?: System.getProperty(QODANA_PYTHON_PATH_PROPERTY)
-           ?: PySdkFromEnvironmentVariable.getPycharmPythonPathProperty()
   }
 }
