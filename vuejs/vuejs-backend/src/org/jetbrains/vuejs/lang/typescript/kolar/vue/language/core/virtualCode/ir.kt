396eac11f5a7f64ff3651e18ac2593f7ae369b34
Victor Turansky <victor.turansky@jetbrains.com>
2026-06-19 21:09:46 +0200

    WEB-78162 Vue/Kolar: `IR*` factory functions
    GitOrigin-RevId: b3e03c3e20cdfe6689ccddf731539145d312bf2c

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/virtualCode/ir.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/virtualCode/ir.kt
@@ -3,14 +3,35 @@ package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.virtualCode
 
 import org.jetbrains.vuejs.lang.html.VueFile
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IR
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRScript
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRScriptSetup
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRTemplate
 
 fun useIR(
   file: VueFile,
 ): IR {
   return IR(
-    template = null,
-    script = null,
-    scriptSetup = null,
+    template = getTemplate(file),
+    script = getScript(file),
+    scriptSetup = getScriptSetup(file),
     styles = emptyList(),
   )
+}
+
+private fun getTemplate(
+  file: VueFile,
+): IRTemplate? {
+  TODO()
+}
+
+private fun getScript(
+  file: VueFile,
+): IRScript? {
+  TODO()
+}
+
+private fun getScriptSetup(
+  file: VueFile,
+): IRScriptSetup? {
+  TODO()
 }
