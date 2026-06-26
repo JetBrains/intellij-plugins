226709691d9edf2d05e4120a117f8871756e1e7f
Aleksander Movsesov <aleksander.movsesov@jetbrains.com>
2026-06-26 18:59:49 +0200

    QD-15147 add tests and allow to consume global opengrep inspection results with taint rule enabled in profile
    (cherry picked from commit 192de981e08fdaae62ce39d169b4a44a2e5ec726)
    
    IJ-MR-210370
    
    GitOrigin-RevId: 835efd6acf8f4f5b9f244df3d3031a94915488dd

--- qodana/core/src/org/jetbrains/qodana/staticAnalysis/inspections/runner/globalOutput/OpenGrepGlobalInspectionOutputConsumer.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.qodana.staticAnalysis.inspections.runner.globalOutput
-
-internal class OpenGrepGlobalInspectionOutputConsumer: GlobalTraceFlowOutputConsumer() {
-  override fun getInspectionName(): String {
-    return "OpenGrepGlobalInspection"
-  }
-}
