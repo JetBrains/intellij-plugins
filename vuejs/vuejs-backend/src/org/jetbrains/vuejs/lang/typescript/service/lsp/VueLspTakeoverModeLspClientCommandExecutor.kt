6c648b7256757d80379e1558f30f92bcae5e22a1
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-06-29 10:33:14 +0200

    WEB-78521 TS LSP: wire handleCustomTsServerCommand through TypeScriptLspClientCommandExecutor
    GitOrigin-RevId: a9016198802c81308764f09985cdce303e576fa7

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/service/lsp/VueLspTakeoverModeLspClientCommandExecutor.kt
@@ -0,0 +1,21 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.service.lsp
+
+import com.google.gson.JsonElement
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceObject
+import com.intellij.lang.typescript.compiler.TypeScriptServiceQueueCommand
+import com.intellij.lang.typescript.lsp.TypeScriptLspClientCommandExecutor
+import com.intellij.platform.lsp.impl.LspClientImpl
+
+internal class VueLspTakeoverModeLspClientCommandExecutor(client: LspClientImpl) : TypeScriptLspClientCommandExecutor(client) {
+
+  override suspend fun handleCustomTsServerCommand(
+    command: TypeScriptServiceQueueCommand<out JSLanguageServiceObject, out Any>,
+    requiresNewEval: Boolean,
+  ): JsonElement? =
+    if (requiresNewEval)
+      null
+    else
+      sendCustomTsServerCommand(command.command, command.arguments)
+
+}
--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/service/lsp/VueLspTakeoverModeTypeScriptService.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/service/lsp/VueLspTakeoverModeTypeScriptService.kt
@@ -1,12 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.service.lsp
 
-import com.google.gson.JsonElement
-import com.intellij.lang.javascript.service.protocol.JSLanguageServiceObject
-import com.intellij.lang.typescript.compiler.TypeScriptServiceQueueCommand
 import com.intellij.lang.typescript.compiler.languageService.protocol.commands.response.TypeScriptQuickInfoResponse
-import com.intellij.lang.typescript.lsp.JSFrameworkLsp4jServer
-import com.intellij.lang.typescript.lsp.JSFrameworkLsp4jServer.LspCustomTypeScriptCommandRequest
 import com.intellij.lang.typescript.lsp.JSFrameworkLspTypeScriptService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
@@ -55,17 +50,6 @@ class VueLspTakeoverModeTypeScriptService(
       }
     }
     return super.createQuickInfoResponse(markupContent)
-  }
-
-  override suspend fun handleCustomTsServerCommand(
-    command: TypeScriptServiceQueueCommand<out JSLanguageServiceObject, out Any>,
-    requiresNewEval: Boolean,
-  ): JsonElement? {
-    if (requiresNewEval) return null
-    val server = forceInitLspClient() ?: return null
-    return server.sendRequest {
-      (it as JSFrameworkLsp4jServer).handleCustomTsServerCommand(LspCustomTypeScriptCommandRequest(command.command, command.arguments))
-    }
   }
 
   override fun supportsTypeEvaluation(
