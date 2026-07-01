af283cd577321716f52ae1c9ad30925b3fd20f07
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-01 17:18:07 +0200

    WEB-78162 Vue/Kolar: IR. Initial implementation
    GitOrigin-RevId: bbec4fa4ef2322710e22d5c78949990fdcbbdd7a

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/virtualCode/ir.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/language/core/virtualCode/ir.kt
@@ -1,8 +1,19 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.virtualCode
 
+import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlTag
+import com.intellij.xml.util.HtmlUtil.TEMPLATE_TAG_NAME
+import org.jetbrains.vuejs.index.findScriptTag
+import org.jetbrains.vuejs.index.findTopLevelVueTag
 import org.jetbrains.vuejs.lang.html.VueFile
+import org.jetbrains.vuejs.lang.typescript.kolar.muggle.string.Source
+import org.jetbrains.vuejs.lang.typescript.kolar.typescript.DiagnosticWithLocation
+import org.jetbrains.vuejs.lang.typescript.kolar.typescript.Node
+import org.jetbrains.vuejs.lang.typescript.kolar.typescript.SourceFile
+import org.jetbrains.vuejs.lang.typescript.kolar.typescript.SyntaxKind
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IR
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRContent
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRScript
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRScriptSetup
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.language.core.IRTemplate
@@ -21,17 +32,88 @@ fun useIR(
 private fun getTemplate(
   file: VueFile,
 ): IRTemplate? {
-  TODO()
+  val templateTag = findTopLevelVueTag(file, TEMPLATE_TAG_NAME)
+                    ?: return null
+
+  return IRTemplate(
+    name = Source("template"),
+    lang = "html",
+    content = IRContentImpl(templateTag),
+    ast = null, // TBD
+  )
 }
 
 private fun getScript(
   file: VueFile,
 ): IRScript? {
-  TODO()
+  val scriptTag = findScriptTag(file, setup = false)
+                  ?: return null
+
+  return IRScript(
+    name = Source("script"),
+    lang = lang(file),
+    content = IRContentImpl(scriptTag),
+    src = null, // TBD
+    ast = SourceFileImpl(scriptTag),
+  )
 }
 
 private fun getScriptSetup(
   file: VueFile,
 ): IRScriptSetup? {
-  TODO()
+  val scriptTag = findScriptTag(file, setup = false)
+                  ?: return null
+
+  return IRScriptSetup(
+    name = Source("scriptSetup"),
+    lang = lang(file),
+    content = IRContentImpl(scriptTag),
+    generic = null, // TBD
+    ast = SourceFileImpl(scriptTag),
+  )
+}
+
+private fun lang(file: VueFile): String =
+  file.langMode.canonicalAttrValue
+
+private class IRContentImpl(
+  element: PsiElement,
+) : IRContent {
+  override val length: Int
+    get() = TODO("not implemented")
+
+  override fun get(index: Int): Char {
+    TODO("not implemented")
+  }
+
+  override fun indexOf(string: String, startIndex: Int): Int {
+    TODO("not implemented")
+  }
+
+  override fun substring(startIndex: Int, endIndex: Int): String {
+    TODO("not implemented")
+  }
+}
+
+private class SourceFileImpl(
+  scriptTag: XmlTag,
+) : SourceFile {
+  override val text: String
+    get() = TODO("not implemented")
+  override val parseDiagnostics: List<DiagnosticWithLocation>
+    get() = TODO("not implemented")
+  override val statements: List<Node>
+    get() = TODO("not implemented")
+  override val endOfFileToken: Node
+    get() = TODO("not implemented")
+  override val kind: SyntaxKind
+    get() = TODO("not implemented")
+  override val pos: Int
+    get() = TODO("not implemented")
+  override val end: Int
+    get() = TODO("not implemented")
+
+  override fun getStart(sourceFile: SourceFile): Int {
+    TODO("not implemented")
+  }
 }
