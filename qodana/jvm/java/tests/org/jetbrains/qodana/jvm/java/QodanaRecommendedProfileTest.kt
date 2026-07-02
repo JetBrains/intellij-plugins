f0c27d9331330784da4d65de288027347248cde2
Maxim Gorbatin <mgorbatin@griddynamics.com>
2026-07-02 13:57:47 +0000

    QD-15230 com.intellij.testFramework.TestLoggerFactory$TestLoggerAssertionError: Can't find highlight display level: SYNTAX_UPDATE;
    QD-15230 | review comments
    
    QD-15230 com.intellij.testFramework.TestLoggerFactory$TestLoggerAssertionError: Can't find highlight display level: SYNTAX_UPDATE;
    
    
    Merge-request: IJ-MR-211315
    Merged-by: Maxim Gorbatin <maxim.gorbatin.external@jetbrains.com>
    
    GitOrigin-RevId: 2461bb5418974dc11d254cf9c25f6fc231ff0ce9

--- qodana/jvm/java/tests/org/jetbrains/qodana/jvm/java/QodanaRecommendedProfileTest.kt
+++ qodana/jvm/java/tests/org/jetbrains/qodana/jvm/java/QodanaRecommendedProfileTest.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.qodana.jvm.java
 
+import com.intellij.codeInspection.ex.ApplicationInspectionProfileManager
 import com.intellij.codeInspection.nullable.NullableStuffInspection
 import com.intellij.openapi.project.Project
 import com.intellij.testFramework.ApplicationRule
@@ -10,6 +11,7 @@ import junit.framework.TestCase.assertTrue
 import kotlinx.coroutines.runBlocking
 import org.jetbrains.qodana.staticAnalysis.profile.providers.QodanaEmbeddedProfile
 import org.jetbrains.qodana.staticAnalysis.profile.providers.QodanaEmbeddedProfilesProvider
+import org.junit.Before
 import org.junit.ClassRule
 import org.junit.Rule
 import org.junit.Test
@@ -29,6 +31,21 @@ class QodanaRecommendedProfileTest {
   @Rule
   @JvmField
   val initInspectionRule = InitInspectionRule()
+
+  /**
+   * Forces initialization of [ApplicationInspectionProfileManager], which is a lazy service .
+   * Its [init] block calls [com.intellij.codeInspection.ex.ApplicationInspectionProfileManager.syncProvidedSeverities],
+   * registering plugin-provided [com.intellij.codeHighlighting.HighlightDisplayLevel] entries
+   * (e.g., SYNTAX_UPDATE from Go, STYLE_SUGGESTION from Grazi) into [com.intellij.codeHighlighting.HighlightDisplayLevel.LEVEL_MAP].
+   * Without this, [com.intellij.codeInspection.InspectionEP.getDefaultLevel]
+   * throws [com.intellij.testFramework.TestLoggerFactory.TestLoggerAssertionError] for unknown severity names
+   * when [com.intellij.codeInspection.ex.InspectionProfileImpl.initInspectionTools] processes inspections
+   * during [com.intellij.codeInspection.inspectionProfile.YamlInspectionProfileImpl.buildEffectiveProfile].
+   */
+  @Before
+  fun setUp() {
+    ApplicationInspectionProfileManager.getInstanceImpl()
+  }
 
   private val embeddedProfilesProvider = QodanaEmbeddedProfilesProvider()
 
