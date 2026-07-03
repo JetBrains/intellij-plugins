5f3633a5950317fa59d11141ac5d3753b566af00
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2026-07-03 18:28:29 +0300

    cleanup [terraform]: cosmetics - remove `beta` from Terragrunt executable
    (cherry picked from commit dc4dc7b409be7467ea6c3c382e2321334b84122a)
    
     IJ-CR-212992
    
    GitOrigin-RevId: fd0773a03ddb566ac86ff3ccc19388d274df2a43

--- terraform/src/org/intellij/terraform/runtime/TfToolConfigurable.kt
+++ terraform/src/org/intellij/terraform/runtime/TfToolConfigurable.kt
@@ -1,7 +1,6 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.intellij.terraform.runtime
 
-import com.intellij.icons.AllIcons
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.fileChooser.FileChooserDescriptorFactory
 import com.intellij.openapi.options.BoundConfigurable
@@ -11,7 +10,6 @@ import com.intellij.openapi.ui.DialogPanel
 import com.intellij.openapi.ui.emptyText
 import com.intellij.openapi.ui.validation.DialogValidation
 import com.intellij.openapi.ui.validation.validationErrorIf
-import com.intellij.ui.components.JBLabel
 import com.intellij.ui.dsl.builder.AlignX
 import com.intellij.ui.dsl.builder.Panel
 import com.intellij.ui.dsl.builder.bindText
@@ -46,10 +44,7 @@ internal class TfToolConfigurable(private val project: Project) : BoundConfigura
       group(HCLBundle.message("opentofu.name")) {
         executableToolSettingsPanel(this, openTofuConfig, TfToolType.OPENTOFU, disposable)
       }
-      group(JBLabel(HCLBundle.message("terragrunt.name")).apply {
-        icon = AllIcons.General.Beta
-        horizontalTextPosition = JBLabel.LEFT
-      }) {
+      group(HCLBundle.message("terragrunt.name")) {
         executableToolSettingsPanel(this, terragruntConfig, TfToolType.TERRAGRUNT, disposable)
       }
     }
