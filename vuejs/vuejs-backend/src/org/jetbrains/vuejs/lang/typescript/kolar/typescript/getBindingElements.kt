fb546c0eac8bd57c9dcd50072218ed571b1a3277
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-10 16:53:06 +0200

    WEB-78162 Vue/Kolar: Binding elements calculation
    GitOrigin-RevId: 75a2f827b51b37734d25ef4d337b22ce322c036a

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/typescript/getBindingElements.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/typescript/getBindingElements.kt
@@ -1,17 +1,74 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.typescript
 
+import com.intellij.lang.javascript.psi.JSDestructuringArray
 import com.intellij.lang.javascript.psi.JSDestructuringContainer
+import com.intellij.lang.javascript.psi.JSDestructuringObject
 import com.intellij.lang.javascript.psi.JSExpression
+import com.intellij.lang.javascript.psi.JSNamedElement
 import com.intellij.psi.PsiElement
 
-fun JSDestructuringContainer.getBindingElements(): List<BindingElement> =
-  TODO("not implemented")
+fun JSDestructuringContainer.getBindingElements(): Sequence<BindingElement> =
+  when (this) {
+    is JSDestructuringObject -> getBindingElements(this)
+    is JSDestructuringArray -> getBindingElements(this)
+    else -> error("Unexpected destructuring container!")
+  }
 
-interface BindingElement {
-  val source: PsiElement
+private fun getBindingElements(
+  o: JSDestructuringObject,
+): Sequence<BindingElement> = sequence {
+  for (property in o.properties) {
+    val nameIdentifier = (property as? JSNamedElement)?.nameIdentifier
+                         ?: continue
 
-  val nameIdentifier: PsiElement
-  val isRest: Boolean
-  val initializer: JSExpression?
+    yield(
+      BindingElement(
+        source = property,
+        nameIdentifier = nameIdentifier,
+        isRest = property.isRest,
+        initializer = property.destructuringElement?.initializer,
+      )
+    )
+  }
 }
+
+private fun getBindingElements(
+  array: JSDestructuringArray,
+): Sequence<BindingElement> = sequence {
+  for (element in array.elements) {
+    val nameIdentifier = (element as? JSNamedElement)?.nameIdentifier
+                         ?: continue
+
+    yield(
+      BindingElement(
+        source = element,
+        nameIdentifier = nameIdentifier,
+        isRest = false,
+        initializer = element.initializer,
+      )
+    )
+  }
+
+  val restElement = array.restElement
+  if (restElement != null) {
+    val nameIdentifier = restElement.variable?.nameIdentifier
+    if (nameIdentifier != null) {
+      yield(
+        BindingElement(
+          source = restElement,
+          nameIdentifier = nameIdentifier,
+          isRest = true,
+          initializer = null,
+        )
+      )
+    }
+  }
+}
+
+class BindingElement(
+  val source: PsiElement,
+  val nameIdentifier: PsiElement,
+  val isRest: Boolean,
+  val initializer: JSExpression?,
+)
