112f300f0bbdbb27dabfffae1290869745643e25
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-13 20:51:37 +0200

    WEB-78162 Vue/Kolar: Initial `ElementNode` implementation
    GitOrigin-RevId: fe92a40779f5e4452ee833db86293de2ca772253

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/ElementNode.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/ElementNode.kt
@@ -1,6 +1,8 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core
 
+import com.intellij.psi.xml.XmlTag
+
 // CompilerDOM.ElementNode (minimal — full definition to be added when element.ts is converted)
 interface ElementNode : Node {
   val tag: String
@@ -8,4 +10,26 @@ interface ElementNode : Node {
   val isSelfClosing: Boolean
   val children: List<Node>
   val props: List<Node>
+}
+
+class ElementNodeImpl(
+  private val element: XmlTag,
+): ElementNode {
+  override val loc: SourceLocation
+    get() = PsiSourceLocation(element)
+  
+  override val tag: String
+    get() = element.localName
+
+  override val tagType: ElementTypes
+    get() = TODO("not implemented")
+
+  override val isSelfClosing: Boolean
+    get() = TODO("not implemented")
+
+  override val children: List<Node>
+    get() = TODO("not implemented")
+
+  override val props: List<Node>
+    get() = TODO("not implemented")
 }
