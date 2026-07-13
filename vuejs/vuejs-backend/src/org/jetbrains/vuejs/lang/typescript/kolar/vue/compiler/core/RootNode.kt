59ddfd2fd66f3d2a4452086bb0fd5efe1147f711
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-13 21:00:59 +0200

    WEB-78162 Vue/Kolar: Initial `RootNode.children` implementation
    GitOrigin-RevId: de420ce8c159f6505e5d8c1f757ae047480aca98

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/RootNode.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/RootNode.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core
 
+import com.intellij.psi.xml.XmlComment
 import com.intellij.psi.xml.XmlTag
 
 // CompilerDOM.RootNode
@@ -15,9 +16,15 @@ class RootNodeImpl(
   override val loc: SourceLocation
     get() = PsiSourceLocation(template)
 
-  override val children: List<Node> =
-    // TBD
-    emptyList()
+  override val children: List<Node> by lazy {
+    template.children.mapNotNull { child -> 
+      when (child) {
+        is XmlTag -> ElementNodeImpl(child)
+        is XmlComment -> CommentNodeImpl(child)
+        else -> null
+      }
+    }
+  }
 
   override val components: List<String> =
     // TBD
