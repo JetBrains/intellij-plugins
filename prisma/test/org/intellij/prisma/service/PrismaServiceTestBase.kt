a16a1e81b89233c2786143c98880f8bf9312da6d
Vladimir Panimaskin <vladimir.panimaskin@jetbrains.com>
2026-07-13 13:21:36 +0300

    WEB-78305 WEB-77155 [prisma] revert explicit LSP cancellation
    GitOrigin-RevId: 2f65d95538910425a745a535ae62ec81b61b3166

--- prisma/test/org/intellij/prisma/service/PrismaServiceTestBase.kt
+++ prisma/test/org/intellij/prisma/service/PrismaServiceTestBase.kt
@@ -5,17 +5,10 @@ import com.intellij.lang.javascript.library.typings.TypeScriptExternalDefinition
 import com.intellij.lang.javascript.service.BaseLspTypeScriptServiceTest
 import com.intellij.lang.typescript.library.download.TypeScriptDefinitionFilesDirectory
 import com.intellij.openapi.util.Disposer
-import com.intellij.platform.lsp.api.LspClient
-import com.intellij.platform.lsp.api.LspClientManager
-import com.intellij.platform.lsp.api.LspClientManagerListener
-import com.intellij.platform.lsp.api.LspServerState
-import com.intellij.testFramework.PlatformTestUtil
-import org.intellij.prisma.ide.lsp.PrismaLspIntegrationProvider
 import org.intellij.prisma.ide.lsp.PrismaLspServerActivationRule
 import org.intellij.prisma.ide.lsp.PrismaLspServerLoader
 import org.intellij.prisma.ide.lsp.PrismaServiceMode
 import org.intellij.prisma.ide.lsp.PrismaServiceSettings
-import java.util.Collections
 
 abstract class PrismaServiceTestBase : BaseLspTypeScriptServiceTest() {
   override fun setUp() {
@@ -33,46 +26,5 @@ abstract class PrismaServiceTestBase : BaseLspTypeScriptServiceTest() {
     serviceSettings.serviceMode = PrismaServiceMode.ENABLED
 
     ensureServerDownloaded(PrismaLspServerLoader)
-  }
-
-  override fun tearDown() {
-    try {
-      stopPrismaLspServers()
-    }
-    catch (e: Throwable) {
-      addSuppressedException(e)
-    }
-    finally {
-      super.tearDown()
-    }
-  }
-
-  /**
-   * The tests start a real `@prisma/language-server` Node process (via [PrismaLspIntegrationProvider]) but nothing stops it,
-   * so its process/reader threads survive into the platform thread-leak checker. Stop the client(s) and wait until they
-   * actually reach a shutdown state before `super.tearDown()`.
-   */
-  private fun stopPrismaLspServers() {
-    val lspManager = LspClientManager.getInstance(project)
-    val clients = lspManager.getClients(PrismaLspIntegrationProvider::class.java)
-    if (clients.isEmpty()) return
-
-    val shutdownClients = Collections.synchronizedSet(HashSet<LspClient>())
-    lspManager.addListener(object : LspClientManagerListener {
-      override fun serverStateChanged(lspClient: LspClient) {
-        if (lspClient.state == LspServerState.ShutdownNormally ||
-            lspClient.state == LspServerState.ShutdownUnexpectedly) {
-          shutdownClients.add(lspClient)
-        }
-      }
-    }, testRootDisposable, sendEventsForExistingClients = true)
-
-    lspManager.stopClients(PrismaLspIntegrationProvider::class.java)
-
-    PlatformTestUtil.waitWithEventsDispatching(
-      "Prisma LSP servers did not shut down within timeout",
-      { shutdownClients.size >= clients.size },
-      10, // seconds
-    )
   }
 }
