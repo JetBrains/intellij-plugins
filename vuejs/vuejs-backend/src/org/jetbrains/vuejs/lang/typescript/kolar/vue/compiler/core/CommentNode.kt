dc5411447bbff5176ce7cbf61b8ca8728e79d3ad
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-13 20:54:53 +0200

    WEB-78162 Vue/Kolar: `CommentNode` implementation
    GitOrigin-RevId: 434f6354a2bb3d963c9eac5e63a980cd5922b84f

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/CommentNode.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/CommentNode.kt
@@ -1,7 +1,19 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core
 
+import com.intellij.psi.xml.XmlComment
+
 // CompilerDOM.CommentNode
 interface CommentNode : Node {
   val content: String
+}
+
+class CommentNodeImpl(
+  private val comment: XmlComment,
+) : CommentNode {
+  override val loc: SourceLocation
+    get() = PsiSourceLocation(comment)
+
+  override val content: String
+    get() = comment.commentText
 }
