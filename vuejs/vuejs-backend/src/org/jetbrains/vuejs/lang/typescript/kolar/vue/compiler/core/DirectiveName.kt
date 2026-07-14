6a10a7c1357617413bc679181476d3c1fb69135d
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-14 17:50:06 +0200

    WEB-78162 Vue/Kolar: Extract directive name check
    GitOrigin-RevId: 07759db41ee62a99388cf3981b8363bdb1f611ec

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/DirectiveName.kt
@@ -0,0 +1,11 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core
+
+private val DIRECTIVE_ATTRIBUTE_STARTS: Set<String> =
+  setOf(":", ".", "@", "#", "v-")
+
+fun isDirectiveAttributeName(
+  name: String,
+): Boolean =
+  DIRECTIVE_ATTRIBUTE_STARTS
+    .any { name.startsWith(it) }
