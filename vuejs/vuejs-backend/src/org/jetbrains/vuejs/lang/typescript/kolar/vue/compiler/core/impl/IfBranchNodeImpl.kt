9112b7511b2c49f174ef19eb82478bdb77cfffca
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-14 20:42:52 +0200

    WEB-78162 Vue/Kolar: Initial `IfBranchNode` implementation
    GitOrigin-RevId: 868b3603672fe44266313e1a1033ab18b769fd33

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/IfBranchNodeImpl.kt
@@ -0,0 +1,25 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.impl
+
+import com.intellij.psi.xml.XmlTag
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.ExpressionNode
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.IfBranchNode
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.Node
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.SourceLocation
+
+class IfBranchNodeImpl(
+  private val tag: XmlTag,
+) : IfBranchNode {
+  override val loc: SourceLocation
+    get() = PsiSourceLocation(tag)
+
+  override val condition: ExpressionNode
+    get() = TODO("not implemented")
+
+  override val children: List<Node> by lazy {
+    if (tag.localName == TEMPLATE_TAG_NAME)
+      getChildren(tag)
+    else
+      listOf(ElementNodeImpl(tag))
+  }
+}
