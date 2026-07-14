f0bc520eb8a3524499a8ab75a2118f144c0d1c80
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-14 18:41:02 +0200

    WEB-78162 Vue/Kolar: Directive name constants
    GitOrigin-RevId: 4fbe5527e010ddf1b5c7a327efce06c579340bd5

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/DirectiveName.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/DirectiveName.kt
@@ -1,8 +1,13 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.impl
 
+const val V_IF: String = "v-if"
+const val V_ELSE_IF: String = "v-else-if"
+const val V_ELSE: String = "v-else"
+const val V_FOR: String = "v-for"
+
 val STRUCTURAL_DIRECTIVE_NAMES: Set<String> =
-  setOf("v-if", "v-else-if", "v-else", "v-for")
+  setOf(V_IF, V_ELSE_IF, V_ELSE, V_FOR)
 
 private val DIRECTIVE_ATTRIBUTE_STARTS: Set<String> =
   setOf(":", ".", "@", "#", "v-")
