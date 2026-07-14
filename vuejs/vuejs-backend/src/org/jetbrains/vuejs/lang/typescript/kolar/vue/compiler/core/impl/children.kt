540aadec1ace7452516c3d8011184a7ad05898fa
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-14 20:24:47 +0200

    WEB-78162 Vue/Kolar: Extract `getChild` helper
    GitOrigin-RevId: 12136c2a2d837fc3b862f1b4d10d7d8db17df23d

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/children.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/children.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.impl
 
+import com.intellij.psi.PsiElement
 import com.intellij.psi.xml.XmlComment
 import com.intellij.psi.xml.XmlTag
 import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.Node
@@ -8,11 +9,17 @@ import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.Node
 fun children(
   tag: XmlTag,
 ): Lazy<List<Node>> = lazy {
-  tag.children.mapNotNull { child ->
-    when (child) {
-      is XmlTag -> ElementNodeImpl(child)
-      is XmlComment -> CommentNodeImpl(child)
-      else -> null
-    }
-  }
+  tag.children.mapNotNull(::getChild)
+}
+
+private fun getChild(
+  child: PsiElement,
+): Node? {
+  if (child is XmlTag)
+    return ElementNodeImpl(child)
+
+  if (child is XmlComment)
+    return CommentNodeImpl(child)
+
+  return null
 }
