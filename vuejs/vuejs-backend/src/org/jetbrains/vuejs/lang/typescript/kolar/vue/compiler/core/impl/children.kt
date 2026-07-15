c1e3a3f674569692162b5889da59b247a52c0913
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-15 19:01:23 +0200

    WEB-78162 Vue/Kolar: Initial `v-if` and `v-for` processing
    GitOrigin-RevId: e07f749d7d5ea7436dbd83b8cf6e8bdf709915cf

--- vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/children.kt
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/children.kt
@@ -1,6 +1,7 @@
 // Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.impl
 
+import com.intellij.lang.javascript.psi.util.stubSafeGetAttribute
 import com.intellij.psi.PsiElement
 import com.intellij.psi.util.childrenSequence
 import com.intellij.psi.xml.XmlComment
@@ -20,11 +21,25 @@ fun getChildren(
     .mapNotNull(::getChild)
     .toList()
 
+private val NODE_FACTORY_MAP: Map<String, ((XmlTag) -> Node)?> = mapOf(
+  V_IF to ::IfNodeImpl,
+  V_ELSE_IF to null,
+  V_ELSE to null,
+  V_FOR to ::ForNodeImpl,
+)
+
 private fun getChild(
   child: PsiElement,
 ): Node? {
-  if (child is XmlTag)
+  if (child is XmlTag) {
+    for ((directiveName, factory) in NODE_FACTORY_MAP) {
+      if (child.stubSafeGetAttribute(directiveName) != null) {
+        return factory?.invoke(child)
+      }
+    }
+
     return ElementNodeImpl(child)
+  }
 
   if (child is XmlComment)
     return CommentNodeImpl(child)
