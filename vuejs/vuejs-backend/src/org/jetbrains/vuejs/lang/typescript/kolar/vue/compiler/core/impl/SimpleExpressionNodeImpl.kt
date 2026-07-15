f2896014d89c6c114f8bc212e825e192e738b8b6
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-15 17:22:25 +0200

    WEB-78162 Vue/Kolar: Initial `SimpleExpressionNode` implementation
    GitOrigin-RevId: 6e8d2bc5419d1acc4ab28561f43d54b047b5b1e3

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/SimpleExpressionNodeImpl.kt
@@ -0,0 +1,23 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.impl
+
+import com.intellij.psi.xml.XmlAttributeValue
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.ConstantTypes
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.SimpleExpressionNode
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.SourceLocation
+
+class SimpleExpressionNodeImpl(
+  private val value: XmlAttributeValue,
+) : SimpleExpressionNode {
+  override val loc: SourceLocation
+    get() = PsiSourceLocation(value)
+
+  override val content: String
+    get() = value.value
+
+  override val isStatic: Boolean
+    get() = false
+
+  override val constType: ConstantTypes
+    get() = ConstantTypes.NOT_CONSTANT
+}
