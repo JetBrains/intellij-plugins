d874c9bd2278749e25ce2acf06f74270a5982db3
Victor Turansky <victor.turansky@jetbrains.com>
2026-07-20 20:12:54 +0200

    WEB-78162 Vue/Kolar: Initial `InterpolationNode` implementation
    GitOrigin-RevId: 03d94ea6bb6b91f663b7139a4a0baf8e61c3c076

---
+++ vuejs/vuejs-backend/src/org/jetbrains/vuejs/lang/typescript/kolar/vue/compiler/core/impl/InterpolationNodeImpl.kt
@@ -0,0 +1,18 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.impl
+
+import com.intellij.openapi.util.TextRange
+import org.jetbrains.vuejs.lang.expr.psi.VueJSEmbeddedExpressionContent
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.ExpressionNode
+import org.jetbrains.vuejs.lang.typescript.kolar.vue.compiler.core.InterpolationNode
+
+class InterpolationNodeImpl(
+  private val expressionContent: VueJSEmbeddedExpressionContent,
+  private val parentRange: TextRange,
+) : NodeImpl(expressionContent),
+    InterpolationNode {
+  override val content: ExpressionNode by lazy {
+    // TODO: support compound expressions
+    SimpleExpressionNodeImpl(expressionContent)
+  }
+}
