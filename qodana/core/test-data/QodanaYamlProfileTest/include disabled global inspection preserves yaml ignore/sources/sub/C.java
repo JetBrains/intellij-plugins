c4efbea1ed7192d719bdde73968357a4f46c130c
Jakub Andrzejewski <jakub.andrzejewski@jetbrains.com>
2026-07-27 14:04:55 +0000

    QD-14349 Improve shouldCheckModuleInScope for CheckModuleLicenses
    cherry-picked from master
    
    Merge-request: IJ-MR-215404
    Merged-by: Jakub Andrzejewski <jakub.andrzejewski@jetbrains.com>
    
    GitOrigin-RevId: 412e1da9a269628f8ccfc481919db40529abc5b5

---
+++ qodana/core/test-data/QodanaYamlProfileTest/include disabled global inspection preserves yaml ignore/sources/sub/C.java
@@ -0,0 +1,14 @@
+class C {
+  public static void main() {
+    System.out.println("Hello world");
+    if (1 == 1) {
+      System.out.println("Another");
+    }
+    unusedResult();
+    unusedResult();
+  }
+
+  public static int unusedResult() {
+    return 5;
+  }
+}
