c8b53c1e728c99911c8a509f857ae92c5543ba1d
Nurullokhon Gulomkodirov <nurullokhon.gulomkodirov@jetbrains.com>
2026-09-02 12:50:19 +0300

    IJPL-250234 [terraform]: support read/write access filters in HCL Find Usages, refactor HclUsageTypeProvider
    GitOrigin-RevId: 9c3c53ca242d13167515bf98705293f88596a863

---
+++ terraform/src/org/intellij/terraform/config/findUsages/HclReadWriteAccessDetector.kt
@@ -0,0 +1,29 @@
+// Copyright 2000-2026 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+package org.intellij.terraform.config.findUsages
+
+import com.intellij.codeInsight.highlighting.ReadWriteAccessDetector
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiReference
+import org.intellij.terraform.config.patterns.TfPsiPatterns
+import org.intellij.terraform.hcl.psi.HCLBlock
+import org.intellij.terraform.hcl.psi.HCLProperty
+import org.intellij.terraform.hcl.psi.HCLPsiUtil
+
+internal class HclReadWriteAccessDetector : ReadWriteAccessDetector() {
+
+  override fun isReadWriteAccessible(element: PsiElement): Boolean =
+    element is HCLBlock || isLocalValue(element)
+
+  override fun isDeclarationWriteAccess(element: PsiElement): Boolean = isLocalValue(element)
+
+  override fun getReferenceAccess(referencedElement: PsiElement, reference: PsiReference): Access =
+    getExpressionAccess(reference.element)
+
+  override fun getExpressionAccess(expression: PsiElement): Access = when {
+    expression is HCLBlock || expression is HCLProperty || HCLPsiUtil.isPropertyKey(expression) -> Access.Write
+    else -> Access.Read
+  }
+
+  private fun isLocalValue(element: PsiElement): Boolean =
+    element is HCLProperty && TfPsiPatterns.LocalsVariable.accepts(element)
+}
