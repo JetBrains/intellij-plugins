799120416018a27bc13cac4ccaf96cc7c40ee9a1
Victor Turansky <victor.turansky@jetbrains.com>
2026-09-15 16:41:54 +0200

    cleanup [Vue] remove deprecated `VueLanguage` API
    GitOrigin-RevId: cbfbb89216dcf7765ccc778f168fae03f13c9227

--- vuejs/vuejs-common/src/org/jetbrains/vuejs/lang/html/VueLanguage.kt
+++ vuejs/vuejs-common/src/org/jetbrains/vuejs/lang/html/VueLanguage.kt
@@ -2,17 +2,6 @@
 package org.jetbrains.vuejs.lang.html
 
 import com.intellij.javascript.web.html.WebFrameworkHtmlDialect
-import org.jetbrains.annotations.ApiStatus
 
-object VueLanguage : WebFrameworkHtmlDialect("Vue") {
-  @Deprecated("Use VueLanguage instead", ReplaceWith("VueLanguage"))
-  @ApiStatus.ScheduledForRemoval
-  val INSTANCE: VueLanguage = VueLanguage
-
-  object Companion {
-    @Deprecated("Use VueLanguage instead", ReplaceWith("VueLanguage"))
-    @ApiStatus.ScheduledForRemoval
-    @get:ApiStatus.ScheduledForRemoval
-    val INSTANCE: VueLanguage = VueLanguage
-  }
-}
+object VueLanguage :
+  WebFrameworkHtmlDialect("Vue")
